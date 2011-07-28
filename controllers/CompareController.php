b81e389bc0fdcfac7760b987d340862cd5b54cb2
rMalie <raphael.malie@prestashop.com>
2011-07-28 14:38:46 +0000

    // Fix #PSFV-12

--- controllers/CompareController.php
+++ controllers/CompareController.php
@@ -35,7 +35,7 @@ class CompareControllerCore extends FrontController
 		$this->addCSS(_THEME_CSS_DIR_.'/comparator.css');
 		
 		if (Configuration::get('PS_COMPARATOR_MAX_ITEM') > 0)
-			Tools::addJS(_THEME_JS_DIR_.'products-comparison.js');
+			$this->addJS(_THEME_JS_DIR_.'products-comparison.js');
 	}
 
 	public function preProcess()
