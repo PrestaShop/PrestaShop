60c004493eb3fbc9942da0b373ad101305f61389
lLefevre <loic.lefevre@prestashop.com>
2011-09-26 08:30:14 +0000

    [+] BO : add SQL manager
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@8760 b9a71923-0436-4b27-9f14-aed3839534dd

--- admin-dev/tabs/AdminImages.php
+++ admin-dev/tabs/AdminImages.php
@@ -460,11 +460,11 @@ class AdminImages extends AdminTab
 				<legend><img src="../img/admin/picture.gif" /> '.$this->l('Move images').'</legend><br />'.
 				$this->l('You can choose to keep your images stored in the previous system - nothing wrong with that.').'<br />'.
 				$this->l('You can also decide to move your images to the new storage system: in this case, click on the "Move images" button below.	Please be patient, as this can take several minutes.').
-				'<br /><br /><p class="hint clear" style="display: block;">&nbsp;'.
+				'<br /><br /><div class="hint clear" style="display: block;">&nbsp;'.
 				$this->l('After moving all of your product images, for best performance go to the '). 
 				'<a style="text-decoration:underline" href="index.php?tab=AdminPPreferences&token='.Tools::getAdminTokenLite('AdminPPreferences').'#PS_LEGACY_IMAGES_on">'.$this->l('product preferences tab').'</a>'.
 				$this->l(' and set "Activate legacy images compatibility" to NO.').'
-				</p>
+				</div>
 				<center><input type="Submit" name="submitMoveImages'.$this->table.'" value="'.$this->l('Move images').'" class="button space" onclick="return confirm(\''.$this->l('Are you sure?', __CLASS__, true, false).'\');" /></center>
 			</fieldset>
 		</form>';
