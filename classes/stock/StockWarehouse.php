2a0305f31e0320ec3d163f9392f29dfaed56e5c6
bMancone <brice.mancone@prestashop.com>
2011-10-10 12:27:16 +0000

    // StockWarehouse : updated __PS_USE_SQL_SLAVE__
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@9179 b9a71923-0436-4b27-9f14-aed3839534dd

--- classes/stock/StockWarehouse.php
+++ classes/stock/StockWarehouse.php
@@ -98,8 +98,8 @@ class StockWarehouseCore extends ObjectModel
 		foreach ($id_shop_list as $id_shop)
 			$row_to_insert = array($this->reference => $this->id, 'id_shop' => $id_shop);
 
-		Db::getInstance(_PS_USE_SQL_SLAVE_)->Execute('DELETE FROM `warehouse_shop` ws WHERE ws.'.$this->identifier.' = '.(int)$this->id);
-		Db::getInstance(_PS_USE_SQL_SLAVE_)->autoExecute('warehouse_shop', $row_to_insert, 'INSERT');
+		Db::getInstance()->Execute('DELETE FROM `warehouse_shop` ws WHERE ws.'.$this->identifier.' = '.(int)$this->id);
+		Db::getInstance()->autoExecute('warehouse_shop', $row_to_insert, 'INSERT');
 	}
 
 	/**
