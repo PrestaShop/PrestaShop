a6f2eb56398e67e219b1268bd55dfccc1f29a9b4
rMalie <raphael.malie@prestashop.com>
2011-10-12 15:49:03 +0000

    // Norme ConfigurationTest

--- classes/ConfigurationTest.php
+++ classes/ConfigurationTest.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* 2007-2011 PrestaShop 
+* 2007-2011 PrestaShop
 *
 * NOTICE OF LICENSE
 *
@@ -25,76 +25,74 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-class	ConfigurationTestCore
+class ConfigurationTestCore
 {
-	static function check($tests)
+	public static function check($tests)
 	{
 		$res = array();
-		foreach ($tests AS $key => $test)
+		foreach ($tests as $key => $test)
 			$res[$key] = self::run($key, $test);
 		return $res;
 	}
-	
-	static function run($ptr, $arg = 0)
+
+	public static function run($ptr, $arg = 0)
 	{
 		if (call_user_func(array('ConfigurationTest', 'test_'.$ptr), $arg))
 			return ('ok');
 		return ('fail');
 	}
-	
-	// Misc functions	
-	static function test_phpversion()
+
+	public static function test_phpversion()
 	{
-		return version_compare(substr(phpversion(), 0, 3), '5.0', '>=');
+		return version_compare(substr(phpversion(), 0, 3), '5.1', '>=');
 	}
-	
-	static function test_mysql_support()
+
+	public static function test_mysql_support()
 	{
 		return function_exists('mysql_connect');
 	}
-	
-	static function test_magicquotes()
+
+	public static function test_magicquotes()
 	{
 		return !get_magic_quotes_gpc();
 	}
 
-	static	function test_upload()
+	public static function test_upload()
 	{
 		return  ini_get('file_uploads');
 	}
 
-	static function test_fopen()
+	public static function test_fopen()
 	{
 		return ini_get('allow_url_fopen');
 	}
 
-	static function test_system($funcs)
+	public static function test_system($funcs)
 	{
-		foreach ($funcs AS $func)
+		foreach ($funcs as $func)
 			if (!function_exists($func))
 				return false;
 		return true;
 	}
 
-	static function test_gd()
+	public static function test_gd()
 	{
 		return function_exists('imagecreatetruecolor');
 	}
-	
-	static function test_register_globals()
+
+	public static function test_register_globals()
 	{
 		return !ini_get('register_globals');
 	}
-	
-	static function test_gz()
+
+	public static function test_gz()
 	{
 		if (function_exists('gzencode'))
-			return !(@gzencode('dd') === false); 
+			return @gzencode('dd') !== false;
 		return false;
 	}
-	
-	// is_writable dirs	
-	static function test_dir($dir, $recursive = false)
+
+	public static function test_dir($dir, $recursive = false)
 	{
 		if (!file_exists($dir) OR !$dh = opendir($dir))
 			return false;
@@ -105,125 +103,124 @@ class	ConfigurationTestCore
 			if (!$recursive)
 				return true;
 		}
-		elseif (!is_writable($dir))
+		else if (!is_writable($dir))
 			return false;
 		if ($recursive)
 		{
 			while (($file = readdir($dh)) !== false)
-				if (is_dir($dir.$file) AND $file != '.' AND $file != '..')
+				if (is_dir($dir.$file) && $file != '.' && $file != '..')
 					if (!self::test_dir($dir.$file, true))
 						return false;
 		}
 		closedir($dh);
 		return true;
 	}
-	
-	// is_writable files	
-	static function test_file($file)
+
+	public static function test_file($file)
 	{
 		return (file_exists($file) AND is_writable($file));
 	}
-	
-	static function test_config_dir($dir)
+
+	public static function test_config_dir($dir)
 	{
 		return self::test_dir($dir);
 	}
-	
-	static function test_sitemap($dir)
+
+	public static function test_sitemap($dir)
 	{
 		return self::test_file($dir);
 	}
-	
-	static function test_root_dir($dir)
+
+	public static function test_root_dir($dir)
 	{
 		return self::test_dir($dir);
 	}
 
-	static function test_log_dir($dir)
+	public static function test_log_dir($dir)
 	{
 		return self::test_dir($dir);
 	}
 
-	static function test_admin_dir($dir)
+	public static function test_admin_dir($dir)
 	{
 		return self::test_dir($dir);
 	}
-	
-	static function test_img_dir($dir)
+
+	public static function test_img_dir($dir)
 	{
 		return self::test_dir($dir, true);
 	}
-	
-	static function test_module_dir($dir)
+
+	public static function test_module_dir($dir)
 	{
 		return self::test_dir($dir, true);
 	}
-	
-	static function test_tools_dir($dir)
+
+	public static function test_tools_dir($dir)
 	{
 		return self::test_dir($dir);
 	}
-	
-	static function test_cache_dir($dir)
+
+	public static function test_cache_dir($dir)
 	{
 		return self::test_dir($dir);
 	}
-	
-	static function test_tools_v2_dir($dir)
+
+	public static function test_tools_v2_dir($dir)
 	{
 		return self::test_dir($dir);
 	}
-	
-	static function test_cache_v2_dir($dir)
+
+	public static function test_cache_v2_dir($dir)
 	{
 		return self::test_dir($dir);
 	}
-	
-	static function test_download_dir($dir)
+
+	public static function test_download_dir($dir)
 	{
 		return self::test_dir($dir);
 	}
-	
-	static function test_mails_dir($dir)
+
+	public static function test_mails_dir($dir)
 	{
 		return self::test_dir($dir, true);
 	}
-	
-	static function test_translations_dir($dir)
+
+	public static function test_translations_dir($dir)
 	{
 		return self::test_dir($dir, true);
 	}
-	
-	static function test_theme_lang_dir($dir)
-	{
-		if (!file_exists($dir))
-			return true;
-		return self::test_dir($dir, true);
-	}
-	
-	static function test_theme_cache_dir($dir)
+
+	public static function test_theme_lang_dir($dir)
 	{
 		if (!file_exists($dir))
 			return true;
 		return self::test_dir($dir, true);
 	}
 
-	static function test_customizable_products_dir($dir)
+	public static function test_theme_cache_dir($dir)
+	{
+		if (!file_exists($dir))
+			return true;
+		return self::test_dir($dir, true);
+	}
+
+	public static function test_customizable_products_dir($dir)
 	{
 		return self::test_dir($dir);
 	}
-	
-	static function test_virtual_products_dir($dir)
+
+	public static function test_virtual_products_dir($dir)
 	{
 		return self::test_dir($dir);
 	}
-	
-	static function test_mcrypt()
+
+	public static function test_mcrypt()
 	{
 		return function_exists('mcrypt_encrypt');
 	}
-	
-	static function test_dom()
+
+	public static function test_dom()
 	{
 		return extension_loaded('Dom');
 	}
