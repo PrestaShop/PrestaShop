502369ee4f9de2f6ed1aaad96ea727ca82c705f9
lLefevre <loic.lefevre@prestashop.com>
2011-10-13 09:07:36 +0000

    [*] BO #PSFV-94 : update of the displayform method by helperForm in AdminGroupShopController
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@9308 b9a71923-0436-4b27-9f14-aed3839534dd

--- controllers/admin/AdminGroupShopController.php
+++ controllers/admin/AdminGroupShopController.php
@@ -47,7 +47,147 @@ class AdminGroupShopControllerCore extends AdminController
 			'active' => array('title' => $this->l('Enabled'), 'align' => 'center', 'active' => 'status', 'type' => 'bool', 'orderby' => false, 'filter_key' => 'active'),
 		);
 
-		$this->template = 'adminGroupShop.tpl';
+		$enabled = '<img src="../img/admin/enabled.gif" alt="'.$this->l('Enabled').'" title="'.$this->l('Enabled').'" />';
+		$disabled = '<img src="../img/admin/disabled.gif" alt="'.$this->l('Disabled').'" title="'.$this->l('Disabled').'" />';
+
+		$this->fields_form = array(
+			'legend' => array(
+				'title' => $this->l('GroupShop')
+			),
+			'input' => array(
+				array(
+					'type' => 'text',
+					'label' => $this->l('GroupShop name:'),
+					'name' => 'name'
+				),
+				array(
+					'type' => 'radio',
+					'label' => $this->l('Share customers:'),
+					'name' => 'share_customer',
+					'required' => true,
+					'class' => 't',
+					'values' => array(
+						array(
+							'id' => 'share_customer_on',
+							'value' => 1,
+							'label' => $enabled
+						),
+						array(
+							'id' => 'share_customer_off',
+							'value' => 0,
+							'label' => $disabled
+						)
+					),
+					'p' => $this->l('Share customers between shops of this group')
+				),
+				array(
+					'type' => 'radio',
+					'label' => $this->l('Share stock:'),
+					'name' => 'share_stock',
+					'required' => true,
+					'class' => 't',
+					'values' => array(
+						array(
+							'id' => 'share_stock_on',
+							'value' => 1,
+							'label' => $enabled
+						),
+						array(
+							'id' => 'share_stock_off',
+							'value' => 0,
+							'label' => $disabled
+						)
+					),
+					'p' => $this->l('Share stock between shops of this group')
+				),
+				array(
+					'type' => 'radio',
+					'label' => $this->l('Share orders:'),
+					'name' => 'share_order',
+					'required' => true,
+					'class' => 't',
+					'values' => array(
+						array(
+							'id' => 'share_order_on',
+							'value' => 1,
+							'label' => $enabled
+						),
+						array(
+							'id' => 'share_order_off',
+							'value' => 0,
+							'label' => $disabled
+						)
+					),
+					'p' => $this->l('Share orders and carts between shops of this group (you can share orders only if you share customers and stock)')
+				),
+				array(
+					'type' => 'radio',
+					'label' => $this->l('Status:'),
+					'name' => 'active',
+					'required' => true,
+					'class' => 't',
+					'values' => array(
+						array(
+							'id' => 'active_on',
+							'value' => 1,
+							'label' => $enabled
+						),
+						array(
+							'id' => 'active_off',
+							'value' => 0,
+							'label' => $disabled
+						)
+					),
+					'p' => $this->l('Enable or disable shop')
+				)
+			),
+			'submit' => array(
+				'title' => $this->l('   Save   '),
+				'class' => 'button'
+			)
+		);
+
+		$import_data = array(
+			'attribute_group' => $this->l('Attribute groups'),
+			'attribute' => $this->l('Attributes'),
+			//'customer_group' => $this->l('Customer groups'),
+			'feature' => $this->l('Features'),
+			'group' => $this->l('Groups'),
+			'manufacturer' => $this->l('Manufacturers'),
+			'supplier' => $this->l('Suppliers'),
+			'tax_rules_group' => $this->l('Tax rules groups'),
+			'zone' => $this->l('Zones'),
+		);
+
+		$this->fields_import_form = array(
+			'legend' => array(
+				'title' => $this->l('Import data from another group shop')
+			),
+			'label' => $this->l('Duplicate data from group shop'),
+			'checkbox' => array(
+				'type' => 'checkbox',
+				'label' => $this->l('Duplicate data from group shop'),
+				'name' => 'useImportData',
+				'value' => 1
+			),
+			'select' => array(
+				'type' => 'select',
+				'name' => 'importFromShop',
+				'options' => array(
+					'query' => Shop::getTree(),
+					'name' => 'name'
+				)
+			),
+			'allcheckbox' => array(
+				'type' => 'checkbox',
+				'values' => $import_data
+			),
+			'p' => $this->l('Use this option to associate data (products, modules, etc.) the same way as the selected shop'),
+			'submit' => array(
+				'title' => $this->l('   Save   '),
+				'class' => 'button'
+			)
+		);
 
 		parent::__construct();
 	}
@@ -68,6 +208,47 @@ class AdminGroupShopControllerCore extends AdminController
 		return parent::postProcess();
 	}
 
+	public function initContent()
+	{
+		if (!($obj = $this->loadObject(true)))
+			return;
+
+		if (Shop::getTotalShops() > 1 && $obj->id)
+			$disabled = array(
+				'share_customer' => true,
+				'share_stock' => true,
+				'share_order' => true,
+				'active' => false
+			);
+		else
+			$disabled = false;
+
+		$this->context->smarty->assign(array(
+			'disabled' => $disabled,
+			'checked' => (Tools::getValue('addgroup_shop') !== false) ? true : false,
+			'defaultGroup' => Shop::getInstance(Configuration::get('PS_SHOP_DEFAULT'))->getGroupID()
+		));
+
+		if ($this->display != 'edit' && $this->display != 'add')
+			$this->display = 'list';
+
+		parent::initContent();
+
+		if ($this->display == 'edit' || $this->display == 'add')
+		{
+			$helper = new HelperForm();
+			// Check if form template has been overriden
+			if (file_exists($this->context->smarty->template_dir.'/'.$this->tpl_folder.'form_import.tpl'))
+				$helper->tpl = $this->tpl_folder.'form_import.tpl';
+			$helper::$currentIndex = self::$currentIndex;
+			$helper->token = $this->token;
+			$helper->table = $this->table;
+			$helper->id = $obj->id;
+			$helper->fields_value = $this->getFieldsValue($obj);
+			$this->content .= $helper->generateForm($this->fields_import_form);
+		}
+	}
+
 	public function afterAdd($new_group_shop)
 	{
 		if (Tools::getValue('useImportData') && ($import_data = Tools::getValue('importData')) && is_array($import_data))
@@ -79,57 +260,6 @@ class AdminGroupShopControllerCore extends AdminController
 		if (Tools::getValue('useImportData') && ($import_data = Tools::getValue('importData')) && is_array($import_data))
 			$new_group_shop->copyGroupShopData(Tools::getValue('importFromShop'), $import_data);
 	}
-
-	public function displayForm($is_main_tab = true)
-	{
-		parent::displayForm($is_main_tab);
-
-		if (!($obj = $this->loadObject(true)))
-			return;
-
-		if (Shop::getTotalShops() > 1 && $obj->id)
-			$disabled = 'disabled="disabled"';
-		else
-			$disabled = '';
-
-		$import_data = array(
-			'attribute_group' => $this->l('Attribute groups'),
-			'attribute' => $this->l('Attributes'),
-			//'customer_group' => $this->l('Customer groups'),
-			'feature' => $this->l('Features'),
-			'group' => $this->l('Groups'),
-			'manufacturer' => $this->l('Manufacturers'),
-			'supplier' => $this->l('Suppliers'),
-			'tax_rules_group' => $this->l('Tax rules groups'),
-			'zone' => $this->l('Zones'),
-		);
-
-		$this->context->smarty->assign('tab_form', array(
-			'current' => self::$currentIndex,
-			'table' => $this->table,
-			'token' => $this->token,
-			'id' => $obj->id,
-			'name' => $this->getFieldValue($obj, 'name'),
-			'disabled' => $disabled,
-			'share_customer' => $this->getFieldValue($obj, 'share_customer') ? true : false,
-			'share_stock' => $this->getFieldValue($obj, 'share_stock') ? true : false,
-			'share_order' => $this->getFieldValue($obj, 'share_order') ? true : false,
-			'active' => $this->getFieldValue($obj, 'active') ? true : false,
-			'importData' => $import_data,
-			'getTree' => Shop::getTree(),
-			'checked' => (Tools::getValue('addgroup_shop') !== false) ? true : false,
-			'defaultGroup' => Shop::getInstance(Configuration::get('PS_SHOP_DEFAULT'))->getGroupID()
-		));
-	}
-
-	public function initContent()
-	{
-		if ($this->display != 'edit' && $this->display != 'add')
-			$this->display = 'list';
-
-		parent::initContent();
-	}
-
 }
 
 
