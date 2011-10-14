cb175ba261ac1655182964768b0674812297ade5
mDeflotte <maxence.deflotte@prestashop.com>
2011-10-14 07:55:45 +0000

    // Add stock defaut quantities and a default warehouse
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@9353 b9a71923-0436-4b27-9f14-aed3839534dd

--- install-dev/sql/db.sql
+++ install-dev/sql/db.sql
@@ -2003,13 +2003,14 @@ CREATE TABLE `PREFIX_warehouse_shop` (
 ) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
 
 CREATE TABLE `PREFIX_stock_available` (
+`id_stock_available` INT(11) UNSIGNED NOT NULL,
 `id_product` INT(11) UNSIGNED NOT NULL,
 `id_product_attribute` INT(11) UNSIGNED NOT NULL,
 `id_shop` INT(11) UNSIGNED NOT NULL,
 `quantity` INT(10) NOT NULL DEFAULT '0',
 `depends_on_stock` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
 `out_of_stock` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id_product`, `id_product_attribute`, `id_shop`),
+  PRIMARY KEY (`id_stock_available`),
   KEY `id_shop` (`id_shop`),
   KEY `id_product` (`id_product`),
   KEY `id_product_attribute` (`id_product_attribute`)
