1122f9a28e34138f4f6e4b6fce2473da7c07d26b
aFolletete <alain.folletete@prestashop.com>
2011-10-24 14:47:33 +0000

    // fixed order of Orders in AdminOrdersController
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@9599 b9a71923-0436-4b27-9f14-aed3839534dd

--- controllers/admin/AdminOrdersController.php
+++ controllers/admin/AdminOrdersController.php
@@ -52,6 +52,7 @@ class AdminOrdersControllerCore extends AdminController
 		LEFT JOIN `'._DB_PREFIX_.'order_state` os ON (os.`id_order_state` = oh.`id_order_state`)
 		LEFT JOIN `'._DB_PREFIX_.'order_state_lang` osl ON (os.`id_order_state` = osl.`id_order_state` AND osl.`id_lang` = '.(int)$this->context->language->id.')';
 		$this->_where = 'AND oh.`id_order_history` = (SELECT MAX(`id_order_history`) FROM `'._DB_PREFIX_.'order_history` moh WHERE moh.`id_order` = a.`id_order` GROUP BY moh.`id_order`)';
+		$this->_orderWay = 'DESC';
 
 		$statesArray = array();
 		$states = OrderState::getOrderStates((int)$this->context->language->id);
