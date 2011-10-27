c59864c6633a0b501e4a27f64bd7ff9f12134e40
hAitmansour <hakim.aitmansour@prestashop.com>
2011-10-27 09:43:32 +0000

    //fix add ;
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@9669 b9a71923-0436-4b27-9f14-aed3839534dd

--- controllers/admin/AdminGroupShopController.php
+++ controllers/admin/AdminGroupShopController.php
@@ -150,7 +150,7 @@ class AdminGroupShopControllerCore extends AdminController
 			'defaultGroup' => Shop::getInstance(Configuration::get('PS_SHOP_DEFAULT'))->getGroupID()
 		));
 
-		$this->content .= parent::initForm()
+		$this->content .= parent::initForm();
 
 		$import_data = array(
 			'attribute_group' => $this->l('Attribute groups'),
