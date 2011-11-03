605b684ea267a49af2c67af1f33cd4609388ea2d
tDidierjean <thomas.didierjean@prestashop.com>
2011-11-03 16:17:06 +0000

    [*] BO : #PSFV-94 - added AdminZonesController

---
+++ controllers/admin/AdminZonesController.php
@@ -0,0 +1,95 @@
+<?php
+/*
+* 2007-2011 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Open Software License (OSL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/osl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author PrestaShop SA <contact@prestashop.com>
+*  @copyright  2007-2011 PrestaShop SA
+*  @version  Release: $Revision: 7300 $
+*  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
+class AdminZonesControllerCore extends AdminController
+{
+	public function __construct()
+	{
+	 	$this->table = 'zone';
+	 	$this->className = 'Zone';
+	 	$this->lang = false;
+
+		$this->addRowAction('edit');
+		$this->addRowAction('delete');
+	 	$this->bulk_actions = array('delete' => array('text' => $this->l('Delete selected'), 'confirm' => $this->l('Delete selected items?')));
+
+		$this->fieldsDisplay = array(
+		'id_zone' => array('title' => $this->l('ID'), 'align' => 'center', 'width' => 25),
+		'name' => array('title' => $this->l('Zone'), 'width' => 'auto'),
+		'active' => array('title' => $this->l('Enabled'), 'width' => '70', 'align' => 'center', 'active' => 'status', 'type' => 'bool', 'orderby' => false)
+		);
+
+		$this->fields_form = array(
+			'legend' => array(
+				'title' => $this->l('Zones'),
+				'image' => '../img/admin/world.gif'
+			),
+			'input' => array(
+				array(
+					'type' => 'text',
+					'label' => $this->l('Name'),
+					'name' => 'name',
+					'size' => 33,
+					'required' => true,
+					'p' => $this->l('Zone name, e.g., Africa, West Coast, Neighboring Countries'),
+				),
+				array(
+					'type' => 'radio',
+					'label' => $this->l('Status:'),
+					'name' => 'active',
+					'required' => false,
+					'class' => 't',
+					'is_bool' => true,
+					'values' => array(
+						array(
+							'id' => 'active_on',
+							'value' => 1,
+							'label' => $this->l('Enabled')
+						),
+						array(
+							'id' => 'active_off',
+							'value' => 0,
+							'label' => $this->l('Disabled')
+						)
+					),
+					'p' => $this->l('Allow or disallow shipping to this zone')
+				),
+				array(
+					'type' => 'asso_shop',
+					'name' => ''
+				)
+			),
+			'submit' => array(
+				'title' => $this->l('   Save   '),
+				'class' => 'button'
+			)
+		);
+		parent::__construct();
+	}
+}
+
+
