63283c94c2cb185bcad0ae63a3d74978e9b7e87b
fBrignoli <franck.brignoli@prestashop.com>
2011-11-17 15:40:34 +0000

    [+] PDF : TCPDF Library
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@10222 b9a71923-0436-4b27-9f14-aed3839534dd

---
+++ tools/tcpdf/config/lang/ara.php
@@ -0,0 +1,47 @@
+<?php
+//============================================================+
+// File name   : ara.php
+// Begin       : 2010-10-26
+// Last Update : 2010-10-26
+//
+// Description : Language module for TCPDF
+//               (contains translated texts)
+//               Arabic
+//
+// Author: Nicola Asuni
+//
+// (c) Copyright:
+//               Nicola Asuni
+//               Tecnick.com s.r.l.
+//               Via Della Pace, 11
+//               09044 Quartucciu (CA)
+//               ITALY
+//               www.tecnick.com
+//               info@tecnick.com
+//============================================================+
+
+/**
+ * TCPDF language file (contains translated texts).
+ * @package com.tecnick.tcpdf
+ * @brief TCPDF language file: Arabic
+ * @author Nicola Asuni
+ * @since 2010-10-26
+ */
+
+// Arabic
+
+global $l;
+$l = Array();
+
+// PAGE META DESCRIPTORS --------------------------------------
+
+$l['a_meta_charset'] = 'UTF-8';
+$l['a_meta_dir'] = 'rtl';
+$l['a_meta_language'] = 'ar';
+
+// TRANSLATIONS --------------------------------------
+$l['w_page'] = 'صفحة';
+
+//============================================================+
+// END OF FILE
+//============================================================+
