48a8719d2f9ca22220da86ecc40a7c15cbf2a054
fSerny <fabien.serny@prestashop.com>
2011-11-23 00:54:51 +0000

    [-] MO : TNT Carrier (1.2)
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@10484 b9a71923-0436-4b27-9f14-aed3839534dd

--- modules/tntcarrier/relaisColis/postRelaisData.php
+++ modules/tntcarrier/relaisColis/postRelaisData.php
@@ -1,4 +1,30 @@
 <?php
+/*
+* 2007-2011 PrestaShop 
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author PrestaShop SA <contact@prestashop.com>
+*  @copyright  2007-2011 PrestaShop SA
+*  @version  Release: $Revision: 1.4 $
+*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
 
 require('../../../config/config.inc.php');
 
@@ -13,10 +39,9 @@ $data = Db::getInstance()->ExecuteS('SELECT * FROM `'._DB_PREFIX_.'tnt_carrier_d
 if (count($data) > 0)
 {
 	echo "ok";
-	Db::getInstance()->Execute('UPDATE `'._DB_PREFIX_.'tnt_carrier_drop_off` SET `code` = "'.$code.'", `name` = "'.$name.'",
-								`address` = "'.$address.'", `zipcode` = "'.$zipcode.'", `city` = "'.$city.'" WHERE `id_cart` = "'.(int)($id_cart).'"');
+	Db::getInstance()->Execute('UPDATE `'._DB_PREFIX_.'tnt_carrier_drop_off` SET `code` = "'.pSQL($code).'", `name` = "'.pSQL($name).'",
+								`address` = "'.pSQL($address).'", `zipcode` = "'.pSQL($zipcode).'", `city` = "'.pSQL($city).'" WHERE `id_cart` = "'.(int)($id_cart).'"');
 }
 else
 	Db::getInstance()->ExecuteS('INSERT INTO `'._DB_PREFIX_.'tnt_carrier_drop_off` (`id_cart`, `code`, `name`, `address`, `zipcode`, `city`) 
-							VALUES ("'.(int)($id_cart).'", "'.$code.'", "'.$name.'", "'.$address.'", "'.$zipcode.'", "'.$city.'")');
-?>
+							VALUES ("'.(int)($id_cart).'", "'.pSQL($code).'", "'.pSQL($name).'", "'.pSQL($address).'", "'.pSQL($zipcode).'", "'.pSQL($city).'")');
