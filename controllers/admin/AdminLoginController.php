aeeb8e1a39b4f3a3e76d3d4f5a2c6b87584266d1
dMetzger <damien.metzger@prestashop.com>
2011-11-23 22:04:01 +0000

    // BO login bug fix

--- controllers/admin/AdminLoginController.php
+++ controllers/admin/AdminLoginController.php
@@ -196,7 +196,7 @@ class AdminLoginController extends AdminController
 				if (Mail::Send((int)Configuration::get('PS_LANG_DEFAULT'), 'password', Mail::l('Your new admin password'), $params, $employee->email, $employee->firstname.' '.$employee->lastname))
 					die(Tools::jsonEncode(array('hasErrors' => false, 'confirm' => $this->l('Your password has been e-mailed to you'))));
 				else
-					die(Tools::jsonEncode(array('hasErrors' => true, 'errors' => Tools::displayError('An error occurred during your password change.'))));
+					die(Tools::jsonEncode(array('hasErrors' => true, 'errors' => array(Tools::displayError('An error occurred during your password change.')))));
 			}
 		
 		}
