035b85dfe52c931228ddfa9341f3503229424878
dMetzger <damien.metzger@prestashop.com>
2011-11-23 19:22:23 +0000

    // Cart rules fixed in admin customers

--- classes/CartRule.php
+++ classes/CartRule.php
@@ -192,7 +192,7 @@ class CartRuleCore extends ObjectModel
 		if (!CartRule::isFeatureActive())
 			return array();
 
-		Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS('
+		$result = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS('
         SELECT *
 		FROM `'._DB_PREFIX_.'cart_rule` cr
 		LEFT JOIN `'._DB_PREFIX_.'cart_rule_lang` crl ON (cr.`id_cart_rule` = crl.`id_cart_rule` AND crl.`id_lang` = '.(int)$id_lang.')
