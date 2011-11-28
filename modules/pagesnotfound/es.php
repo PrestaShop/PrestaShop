b957188797b03252fdfed78f71a4c5a9f3242419
mDeflotte <maxence.deflotte@prestashop.com>
2011-11-28 10:51:48 +0000

    // Translation files
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@10699 b9a71923-0436-4b27-9f14-aed3839534dd

--- modules/pagesnotfound/es.php
+++ modules/pagesnotfound/es.php
@@ -14,7 +14,6 @@ $_MODULE['<{pagesnotfound}prestashop>pagesnotfound_d8847bc418fc4f5a3e37c2e8390bb
 $_MODULE['<{pagesnotfound}prestashop>pagesnotfound_254b5e94768b90388cc7002d362351f0'] = 'Suprimir TODAS las páginas no encontradas';
 $_MODULE['<{pagesnotfound}prestashop>pagesnotfound_6602bbeb2956c035fb4cb5e844a4861b'] = 'Guía';
 $_MODULE['<{pagesnotfound}prestashop>pagesnotfound_3604249130acf7fda296e16edc996e5b'] = 'error 404';
-$_MODULE['<{pagesnotfound}prestashop>pagesnotfound_1c4e115caef0bed8f5ca76da1909f967'] = 'Error 404 es un código HTTP que significa que el archivo pedido por el usuario no se encuentra. En su caso esto significa que uno de sus visitantes introdujo una URL incorrecta en la  barra de direcciones o que usted tiene un enlace roto en alguna parte. Cuando está disponible, el referente se muestra, así puede encontrar la página que contiene el enlace roto. Si no, significa generalmente que es un acceso directo que ya no existe.';
 $_MODULE['<{pagesnotfound}prestashop>pagesnotfound_a90083861c168ef985bf70763980aa60'] = '¿Como prevenir estos errores?';
 $_MODULE['<{pagesnotfound}prestashop>pagesnotfound_535ff57deda0b45d32cb37fd430accc8'] = 'Si su servidor soporta el archivo .htaccess, puede crear uno en el directorio raíz de PrestaShop e insertar la línea siguiente:';
 $_MODULE['<{pagesnotfound}prestashop>pagesnotfound_9cfaa8e935f9e3e65b0959b15b070b15'] = 'El usuario que busca una página que no existe será reenviado a la página';
--- modules/sekeywords/it.php
+++ modules/sekeywords/it.php
@@ -15,9 +15,7 @@ $_MODULE['<{sekeywords}prestashop>sekeywords_03e0e9ef8b776b5f7bcc590e51bfa63b'] 
 $_MODULE['<{sekeywords}prestashop>sekeywords_7b48f6cc4a1dde7fca9597e717c2465f'] = 'Nessuna parola chiave';
 $_MODULE['<{sekeywords}prestashop>sekeywords_6602bbeb2956c035fb4cb5e844a4861b'] = 'Guida';
 $_MODULE['<{sekeywords}prestashop>sekeywords_39f040a1f174811285297763fcaada12'] = 'Identifica parole chiave motori di ricerca estenri';
-$_MODULE['<{sekeywords}prestashop>sekeywords_f4e8acc0e0cfc0653d6354c3ef878d29'] = 'Ci sono molti modi per trovare un sito web, ma uno dei più comuni è quello di trovarlo con un motore di ricerca. Identificare la maggior parte delle parole chiave \"visitatore-making\"  inserite dai nuovi visitatori è molto importante, ti permette di vedere quale prodotto mettere in vista se vuoi più visitatori e clienti.';
 $_MODULE['<{sekeywords}prestashop>sekeywords_359f9e79e746fa9f684e5cda9e60ca2e'] = 'Come funziona?';
-$_MODULE['<{sekeywords}prestashop>sekeywords_b20af255c34d0a4ace56863dbd8f0c90'] = 'Quando un visitatore arriva al tuo sito web, il server conosce la sua posizione precedente. Questo modulo analizza l\'URL e trova le parole chiave in esso. Attualmente, gestisce i seguenti motori di ricerca:';
 $_MODULE['<{sekeywords}prestashop>sekeywords_be5d5d37542d75f93a87094459f76678'] = 'e';
 $_MODULE['<{sekeywords}prestashop>sekeywords_474c03326cba5cb2ce6c07ddf3e1e656'] = 'Presto sarà possibile aggiungere dinamicamente un nuovo motore di ricerca e contribuire a questo modulo!';
 $_MODULE['<{sekeywords}prestashop>sekeywords_8d01ac22a68176ddd7067da99508751d'] = 'Le prime 10 parole chiave ';
