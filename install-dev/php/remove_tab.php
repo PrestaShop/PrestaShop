c9be0c6c2d3c2786f3901fd789874c8ebf6f2a1c
fBrignoli <franck.brignoli@prestashop.com>
2011-12-01 14:08:49 +0000

    // ps_ => PREFIX_

--- install-dev/php/remove_tab.php
+++ install-dev/php/remove_tab.php
@@ -5,7 +5,7 @@ function remove_tab($tabname)
 {
 	Db::getInstance()->execute('
 	DELETE t, l
-	FROM `ps_tab` t LEFT JOIN `PREFIX_tab_lang` l ON (t.id_tab = l.id_tab)
+	FROM `PREFIX_tab` t LEFT JOIN `PREFIX_tab_lang` l ON (t.id_tab = l.id_tab)
 	WHERE t.`class_name` = '.pSQL($tabname));
 }
 
