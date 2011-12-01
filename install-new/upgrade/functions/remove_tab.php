5f103766fcb70886a0fc4256d2cbfb83b7112a01
fBrignoli <franck.brignoli@prestashop.com>
2011-12-01 15:10:10 +0000

    // prefix
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@10835 b9a71923-0436-4b27-9f14-aed3839534dd

--- install-new/upgrade/functions/remove_tab.php
+++ install-new/upgrade/functions/remove_tab.php
@@ -5,7 +5,7 @@ function remove_tab($tabname)
 {
 	Db::getInstance()->execute('
 	DELETE t, l
-	FROM `PREFIX_tab` t LEFT JOIN `PREFIX_tab_lang` l ON (t.id_tab = l.id_tab)
+	FROM `'._DB_PREFIX_.'tab` t LEFT JOIN `'._DB_PREFIX_.'tab_lang` l ON (t.id_tab = l.id_tab)
 	WHERE t.`class_name` = '.pSQL($tabname));
 }
 
