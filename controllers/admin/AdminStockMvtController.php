d0d7a9912f03f2e79c461cbde33dc436135818d9
bMancone <brice.mancone@prestashop.com>
2011-12-09 16:47:16 +0000

    // Controllers: improvement on CSV

--- controllers/admin/AdminStockMvtController.php
+++ controllers/admin/AdminStockMvtController.php
@@ -229,11 +229,14 @@ class AdminStockMvtControllerCore extends AdminController
 	 */
 	public function initToolbar()
 	{
-		$this->toolbar_btn['export-stock-mvt-csv'] = array(
-			'short' => 'Export this list as CSV',
-			'href' => $this->context->link->getAdminLink('AdminStockMvt').'&amp;csv&amp;id_warehouse='.(int)$this->getCurrentWarehouseId(),
-			'desc' => $this->l('Export (CSV)'),
-		);
+		if (count($this->_list) > 0)
+		{
+			$this->toolbar_btn['export-stock-mvt-csv'] = array(
+				'short' => 'Export this list as CSV',
+				'href' => $this->context->link->getAdminLink('AdminStockMvt').'&amp;csv&amp;id_warehouse='.(int)$this->getCurrentWarehouseId(),
+				'desc' => $this->l('Export (CSV)'),
+			);
+		}
 		parent::initToolbar();
 		unset($this->toolbar_btn['new']);
 	}
