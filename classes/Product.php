bd402c3514b2d4b252ae02a80ea82f6872774f80
mMarinetti <michael.marinetti@prestashop.com>
2011-12-09 16:33:32 +0000

    // cleanning code
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@11095 b9a71923-0436-4b27-9f14-aed3839534dd

--- classes/Product.php
+++ classes/Product.php
@@ -2430,7 +2430,10 @@ class ProductCore extends ObjectModel
 		}
 		$result = self::$_pricesLevel2[$cache_id_2];
 
-		$price = (float)(!$specific_price || $specific_price['price'] == 0) ? $result['price'] : $specific_price['price'];
+    if (!$specific_price || $specific_price['price'] ==0)
+      $price = (float)$result['price'];
+     else
+      $price = (float)$specific_price['price'];
 		// convert only if the specific price is in the default currency (id_currency = 0)
 		if (!$specific_price || !($specific_price['price'] > 0 && $specific_price['id_currency']))
 			$price = Tools::convertPrice($price, $id_currency);
