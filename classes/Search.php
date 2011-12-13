91cb67a25db19c8faca08636b756b0c2f7f510ec
fGaillard <franois.gaillard@prestashop.com>
2011-12-13 09:09:54 +0000

    [-] BO : BugFix : Search index re-build
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@11160 b9a71923-0436-4b27-9f14-aed3839534dd

--- classes/Search.php
+++ classes/Search.php
@@ -445,7 +445,7 @@ class SearchCore
 		$total_languages = count(Language::getLanguages(false));
 
 		// Products are processed 50 by 50 in order to avoid overloading MySQL
-		while (($products = Search::getProductsToIndex($total_languages, $id_product, 50)) && $count > 0)
+		while (($products = Search::getProductsToIndex($total_languages, $id_product, 50)) && (count($products) > 0))
 		{
 			// Now each non-indexed product is processed one by one, langage by langage
 			foreach ($products as $product)
