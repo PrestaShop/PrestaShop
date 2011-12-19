821e08a74135812db4f333858e424cddefc98626
lBrieu <laurent.brieu@prestashop.com>
2011-12-19 15:35:13 +0000

    [*] Update messages notifications to display customer threads

--- classes/Notification.php
+++ classes/Notification.php
@@ -31,7 +31,7 @@ class NotificationCore
 
 	public function __construct()
 	{
-		$this->types = array('order', 'message', 'customer');
+		$this->types = array('order', 'customer_message', 'customer');
 	}
 
 	/**
@@ -42,16 +42,26 @@ class NotificationCore
 	 */
 	public function getLastElements()
 	{
+		global $cookie;
+		
 		$notifications = array();
 		$employee_infos = Db::getInstance(_PS_USE_SQL_SLAVE_)->getRow('
-				SELECT id_last_order, id_last_message, id_last_customer
+				SELECT id_last_order, id_last_customer_message, id_last_customer
 				FROM `'._DB_PREFIX_.'employee`
-				WHERE `id_employee` = '.(int)Context::getContext()->employee->id);
+				WHERE `id_employee` = '.(int)$cookie->id_employee);
 
 		foreach ($this->types as $type)
 			$notifications[$type] = Notification::getLastElementsIdsByType($type, $employee_infos['id_last_'.$type]);
 
 		return $notifications;
+	}
+	
+	public function installDb()
+	{
+		Db::getInstance(_PS_USE_SQL_SLAVE_)->Execute('ALTER TABLE `'._DB_PREFIX_.'employee`
+						ADD `id_last_order` INT(10) unsigned NOT NULL default "0"
+						ADD `id_last_customer_message` INT(10) unsigned NOT NULL default "0"
+						ADD `id_last_message` INT(10) unsigned NOT NULL default "0"');
 	}
 
 	/**
@@ -65,32 +75,39 @@ class NotificationCore
 	public static function getLastElementsIdsByType($type, $id_last_element)
 	{
 
-		if ($type == 'order' || $type == 'message')
-			$sql = 'SELECT id_order, id_customer, '.(($type == 'order') ? 'total_paid_real' : 'message').'
-					FROM `'._DB_PREFIX_.(($type == 'order') ? bqSQL($type).'s' : bqSQL($type)).'`
-					WHERE `id_'.bqSQL($type).'` > '.(int)$id_last_element.'
-					ORDER BY `id_'.bqSQL($type).'` DESC LIMIT 5';
+		if ($type == 'order' || $type == 'customer_message')
+			$sql = 'SELECT '.(($type == 'order') ? 'id_order, id_customer, total_paid_real' : 'c.id_customer_message as id_customer_message, ct.id_customer as id_customer, ct.id_customer_thread as id_customer_thread, ct.email as email').'
+					FROM `'._DB_PREFIX_.(($type == 'order') ? bqSQL($type).'s`' : bqSQL($type).'` as c LEFT JOIN `'._DB_PREFIX_.'customer_thread` as ct ON c.id_customer_thread = ct.id_customer_thread').'
+					WHERE '.(($type == 'customer_message') ? 'c.' : '').'`id_'.bqSQL($type).'` > '.(int)$id_last_element.
+					(($type == 'customer_message') ? ' AND c.`id_employee` = 0' : '').'
+					ORDER BY '.(($type == 'customer_message') ? 'c.' : '').'`id_'.bqSQL($type).'` DESC';
 		else
 			$sql = 'SELECT id_'.bqSQL($type).'
 					FROM `'._DB_PREFIX_.bqSQL($type).'`
 					WHERE `id_'.bqSQL($type).'` > '.(int)$id_last_element.'
-					ORDER BY `id_'.bqSQL($type).'` DESC LIMIT 5';
+					ORDER BY `id_'.bqSQL($type).'` DESC';
 
 		$json = array();
-		foreach (Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($sql) as $key => $value)
+		foreach (Db::getInstance(_PS_USE_SQL_SLAVE_)->ExecuteS($sql) as $key => $value)
 		{
+			$customer = NULL;
+			$order = NULL;
+			$currency = NULL;
 			if (isset($value['id_order']))
 			{
 				$order = new Order(intval($value['id_order']));
 				$currency = new Currency(intval($order->id_currency));
 			}
-			$customer = new Customer(intval($value['id_customer']));
+			if (!empty($value['id_customer']))
+				$customer = new Customer(intval($value['id_customer']));
+			
 			$json[] = array(
-				'id_order' => ((isset($value['id_order'])) ? (int)$value['id_order'] : 0),
-				'id_customer' => ((isset($value['id_customer'])) ? (int)$value['id_customer'] : 0),
-				'total_paid_real' => ((isset($value['total_paid_real'])) ? Tools::displayPrice((float)$value['total_paid_real'], $currency, false) : 0),
-				'customer_name' => $customer->firstname.' '.$customer->lastname,
-				'message_customer' => ((isset($value['message'])) ? substr(strip_tags($value['message']), 0, 20) : '')
+				'id_order' => ((!empty($value['id_order'])) ? (int)$value['id_order'] : 0),
+				'id_customer' => ((!empty($value['id_customer'])) ? (int)$value['id_customer'] : 0),
+				'id_customer_message' => ((!empty($value['id_customer_message'])) ? (int)$value['id_customer_message'] : 0),
+				'id_customer_thread' => ((!empty($value['id_customer_thread'])) ? (int)$value['id_customer_thread'] : 0),				
+				'total_paid_real' => ((!empty($value['total_paid_real'] && $currency != NULL)) ? Tools::displayPrice((float)$value['total_paid_real'], $currency, false) : 0),
+				'customer_name' => (($customer != NULL) ? $customer->firstname.' '.$customer->lastname : (isset($value['email']) ? $value['email'] : ''))
 			);
 		}
 
@@ -105,14 +122,16 @@ class NotificationCore
 	 * @return boolean if type exists or not
 	 */
 	public function updateEmployeeLastElement($type)
-	{
+	{	
+		global $cookie;
+		
 		if (in_array($type, $this->types))
 			// We update the last item viewed
-			return Db::getInstance()->execute('
+			return Db::getInstance()->Execute('
 					UPDATE `'._DB_PREFIX_.'employee`
-					SET `id_last_'.bqSQL($type).'` = (SELECT MAX(`id_'.bqSQL($type).'`)
+					SET `id_last_'.bqSQL($type).'` = (SELECT MAX(`id_'.$type.'`)
 					FROM `'._DB_PREFIX_.(($type == 'order') ? bqSQL($type).'s' : bqSQL($type)).'`)
-					WHERE `id_employee` = '.(int)Context::getContext()->employee->id);
+					WHERE `id_employee` = '.(int)$cookie->id_employee);
 		else
 			return false;
 	}
