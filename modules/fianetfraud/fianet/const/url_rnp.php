a1e0b74fcc933de9e2197bddaf51a33cede475e4
fSerny <fabien.serny@prestashop.com>
2011-12-20 23:05:48 +0000

    [*] PROJECT : Moving partner module to another repository
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@11445 b9a71923-0436-4b27-9f14-aed3839534dd

--- modules/fianetfraud/fianet/const/url_rnp.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('URL_RNP_PROD', 'https://secure.fia-net.com/wallet');
-define('URL_RNP_TEST', 'https://secure.fia-net.com/walter');
-
-define('URL_RNP_FRONTLINE', '/engine/frontline.cgi');
-
