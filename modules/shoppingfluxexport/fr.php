f4dbabbe300b50d7aaee25e48be3521ef56441ad
fSerny <fabien.serny@prestashop.com>
2011-12-20 23:05:48 +0000

    [*] PROJECT : Moving partner module to another repository

--- modules/shoppingfluxexport/fr.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-global $_MODULE;
-$_MODULE = array();
-$_MODULE['<{shoppingfluxexport}prestashop>shoppingfluxexport_fea9f8736d4903ba7394eae6c6c4a48b'] = 'Export Shopping Flux';
-$_MODULE['<{shoppingfluxexport}prestashop>shoppingfluxexport_532e375ed3b53b3c888a6bf214329e60'] = 'Export du catalogue pour Shopping Flux';
-$_MODULE['<{shoppingfluxexport}prestashop>shoppingfluxexport_5abb27355099ec51416a3f003fe63711'] = 'Êtes-vous sur de vouloir supprimer ce module ?';
-$_MODULE['<{shoppingfluxexport}prestashop>shoppingfluxexport_68a59c7fd2c1bf8103225a22a6088235'] = 'Adresse du fichier :';
--- modules/treepodia/export/export.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-// This file is deprecated BUT need to stay there ad vitam eternam because the previous version MUST be overwritten in an update
