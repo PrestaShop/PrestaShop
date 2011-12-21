3bae3d127a0ba691de9f1637d6e676ff45037513
vAugagneur <vincent.augagneur@prestashop.com>
2011-12-21 09:59:53 +0000

    //added translation
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@11457 b9a71923-0436-4b27-9f14-aed3839534dd

--- controllers/admin/AdminHomeController.php
+++ controllers/admin/AdminHomeController.php
@@ -162,7 +162,7 @@ class AdminHomeControllerCore extends AdminController
 	protected function warnDomainName()
 	{
 		if ($_SERVER['HTTP_HOST'] != Configuration::get('PS_SHOP_DOMAIN') AND $_SERVER['HTTP_HOST'] != Configuration::get('PS_SHOP_DOMAIN_SSL'))
-			$this->displayWarning($this->l('Your are currently connected with the following domain name:').' <span style="color: #CC0000;">'.$_SERVER['HTTP_HOST'].'</span><br />'.
+			$this->displayWarning($this->l('You are currently connected with the following domain name:').' <span style="color: #CC0000;">'.$_SERVER['HTTP_HOST'].'</span><br />'.
 			$this->l('This one is different from the main shop domain name set in "Preferences > SEO & URLs":').' <span style="color: #CC0000;">'.Configuration::get('PS_SHOP_DOMAIN').'</span><br />
 			<a href="index.php?tab=AdminMeta&token='.Tools::getAdminTokenLite('AdminMeta').'#SEO%20%26%20URLs">'.
 			$this->l('Click here if you want to modify the main shop domain name').'</a>');
