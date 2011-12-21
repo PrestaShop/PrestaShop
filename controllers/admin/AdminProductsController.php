ca1f912571df254062d20cc3bbaa2cb14aae5393
lLefevre <loic.lefevre@prestashop.com>
2011-12-21 17:03:01 +0000

    // fix fatal error

--- controllers/admin/AdminProductsController.php
+++ controllers/admin/AdminProductsController.php
@@ -2110,8 +2110,6 @@ class AdminProductsControllerCore extends AdminController
 			$this->_errors[] = 'Unable to load object';
 		else
 		{
-			if ($this->object->id && !Shop::isProductAvailable($this->object->id))
-				$this->_displayUnavailableProductWarning();
 			$this->_displayDraftWarning($this->object->active);
 
 			$this->initPack($this->object);
