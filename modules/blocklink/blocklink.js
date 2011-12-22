cfb5ff9f86f4dfb6a3fc767567d9e44f11adc0fc
rGaillard <remi.gaillard@prestashop.com>
2011-12-22 16:46:02 +0000

    // BlockLink

--- modules/blocklink/blocklink.js
+++ modules/blocklink/blocklink.js
@@ -26,20 +26,10 @@
 
 function linkEdition(linkId)
 {
- 	getE('id').value = linkId;
- 	getE('url').value = links[linkId][0];
- 	getE('newWindow').checked = links[linkId][1];
-	var beg = parseInt(getE('languageFirst').value);
- 	for (var i = 0; i <= parseInt(getE('languageNb').value - 1); i++) {
- 		getE('textInput_'+ (beg + i)).value = links[linkId][i + 2];
- 	}
- 	getE('submitLinkUpdate').disabled = '';
- 	getE('submitLinkUpdate').setAttribute('class', 'button');
- 	/* ##### IE */
- 	getE('submitLinkUpdate').setAttribute('className', 'button');
+	document.location.replace(currentUrl+'&id_link='+linkId+'&token='+token);
 }
 
 function linkDeletion(linkId)
 {
- 	document.location.replace(currentUrl+'&id='+linkId+'&token='+token);
+ 	document.location.replace(currentUrl+'&id='+linkId+'&delete_link=1&token='+token);
 }
