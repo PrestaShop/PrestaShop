72ab3037f8656daa8199eda0dd5818f5255590e9
vKham <vKham@prestashop.com>
2011-12-26 14:55:51 +0000

    //fix bug IE7 > delete font-size:118px on fileuploader js, add gradiant images ie7 table

--- js/fileuploader.js
+++ js/fileuploader.js
@@ -696,7 +696,7 @@ qq.UploadButton.prototype = {
             top: 0,
             fontFamily: 'Arial',
             // 4 persons reported this, the max values that worked for them were 243, 236, 236, 118
-            fontSize: '118px',
+            fontSize: '18px',
             margin: 0,
             padding: 0,
             cursor: 'pointer',
