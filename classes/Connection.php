7365ad3768305d6ded20447b7d61db0f47b82271
lLefevre <loic.lefevre@prestashop.com>
2012-01-02 09:27:20 +0000

    [-] BO : #PSTEST-243 : update date_add for customer connections

--- classes/Connection.php
+++ classes/Connection.php
@@ -61,6 +61,7 @@ class ConnectionCore extends ObjectModel
 			'http_referer' => 	array('type' => self::TYPE_STRING, 'validate' => 'isAbsoluteUrl'),
 			'id_shop' => 		array('type' => self::TYPE_INT, 'required' => true),
 			'id_group_shop' => 	array('type' => self::TYPE_INT, 'required' => true),
+			'date_add' => 		array('type' => self::TYPE_DATE, 'validate' => 'isDate')
 		),
 	);
 
@@ -82,21 +83,29 @@ class ConnectionCore extends ObjectModel
 	public static function setPageConnection($cookie, $full = true)
 	{
 		// The connection is created if it does not exist yet and we get the current page id
-		if (!isset($cookie->id_connections) OR !strstr(isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '', Tools::getHttpHost(false, false)))
+		if (!isset($cookie->id_connections) || !strstr(isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '', Tools::getHttpHost(false, false)))
 			$id_page = Connection::setNewConnection($cookie);
-		if (!isset($id_page) OR !$id_page)
+		if (!isset($id_page) || !$id_page)
 			$id_page = Page::getCurrentId();
 		if (!Configuration::get('PS_STATSDATA_CUSTOMER_PAGESVIEWS'))
 			return array('id_page' => $id_page);
 
 		// The ending time will be updated by an ajax request when the guest will close the page
 		$time_start = date('Y-m-d H:i:s');
-		Db::getInstance()->AutoExecute(_DB_PREFIX_.'connections_page', array('id_connections' => (int)($cookie->id_connections), 'id_page' => (int)($id_page), 'time_start' => $time_start), 'INSERT');
+		Db::getInstance()->AutoExecute(
+			_DB_PREFIX_.'connections_page',
+			array(
+				'id_connections' => (int)$cookie->id_connections,
+				'id_page' => (int)$id_page,
+				'time_start' => $time_start
+			),
+			'INSERT'
+		);
 
 		// This array is serialized and used by the ajax request to identify the page
 		return array(
-			'id_connections' => (int)($cookie->id_connections),
-			'id_page' => (int)($id_page),
+			'id_connections' => (int)$cookie->id_connections,
+			'id_page' => (int)$id_page,
 			'time_start' => $time_start);
 	}
 
@@ -106,7 +115,7 @@ class ConnectionCore extends ObjectModel
 			$shop = Context::getContext()->shop;
 
 		if (isset($_SERVER['HTTP_USER_AGENT'])
-			AND preg_match('/BotLink|ahoy|AlkalineBOT|anthill|appie|arale|araneo|AraybOt|ariadne|arks|ATN_Worldwide|Atomz|bbot|Bjaaland|Ukonline|borg\-bot\/0\.9|boxseabot|bspider|calif|christcrawler|CMC\/0\.01|combine|confuzzledbot|CoolBot|cosmos|Internet Cruiser Robot|cusco|cyberspyder|cydralspider|desertrealm, desert realm|digger|DIIbot|grabber|downloadexpress|DragonBot|dwcp|ecollector|ebiness|elfinbot|esculapio|esther|fastcrawler|FDSE|FELIX IDE|ESI|fido|H�m�h�kki|KIT\-Fireball|fouineur|Freecrawl|gammaSpider|gazz|gcreep|golem|googlebot|griffon|Gromit|gulliver|gulper|hambot|havIndex|hotwired|htdig|iajabot|INGRID\/0\.1|Informant|InfoSpiders|inspectorwww|irobot|Iron33|JBot|jcrawler|Teoma|Jeeves|jobo|image\.kapsi\.net|KDD\-Explorer|ko_yappo_robot|label\-grabber|larbin|legs|Linkidator|linkwalker|Lockon|logo_gif_crawler|marvin|mattie|mediafox|MerzScope|NEC\-MeshExplorer|MindCrawler|udmsearch|moget|Motor|msnbot|muncher|muninn|MuscatFerret|MwdSearch|sharp\-info\-agent|WebMechanic|NetScoop|newscan\-online|ObjectsSearch|Occam|Orbsearch\/1\.0|packrat|pageboy|ParaSite|patric|pegasus|perlcrawler|phpdig|piltdownman|Pimptrain|pjspider|PlumtreeWebAccessor|PortalBSpider|psbot|Getterrobo\-Plus|Raven|RHCS|RixBot|roadrunner|Robbie|robi|RoboCrawl|robofox|Scooter|Search\-AU|searchprocess|Senrigan|Shagseeker|sift|SimBot|Site Valet|skymob|SLCrawler\/2\.0|slurp|ESI|snooper|solbot|speedy|spider_monkey|SpiderBot\/1\.0|spiderline|nil|suke|http:\/\/www\.sygol\.com|tach_bw|TechBOT|templeton|titin|topiclink|UdmSearch|urlck|Valkyrie libwww\-perl|verticrawl|Victoria|void\-bot|Voyager|VWbot_K|crawlpaper|wapspider|WebBandit\/1\.0|webcatcher|T\-H\-U\-N\-D\-E\-R\-S\-T\-O\-N\-E|WebMoose|webquest|webreaper|webs|webspider|WebWalker|wget|winona|whowhere|wlm|WOLP|WWWC|none|XGET|Nederland\.zoek/i', $_SERVER['HTTP_USER_AGENT']))
+			&& preg_match('/BotLink|ahoy|AlkalineBOT|anthill|appie|arale|araneo|AraybOt|ariadne|arks|ATN_Worldwide|Atomz|bbot|Bjaaland|Ukonline|borg\-bot\/0\.9|boxseabot|bspider|calif|christcrawler|CMC\/0\.01|combine|confuzzledbot|CoolBot|cosmos|Internet Cruiser Robot|cusco|cyberspyder|cydralspider|desertrealm, desert realm|digger|DIIbot|grabber|downloadexpress|DragonBot|dwcp|ecollector|ebiness|elfinbot|esculapio|esther|fastcrawler|FDSE|FELIX IDE|ESI|fido|H�m�h�kki|KIT\-Fireball|fouineur|Freecrawl|gammaSpider|gazz|gcreep|golem|googlebot|griffon|Gromit|gulliver|gulper|hambot|havIndex|hotwired|htdig|iajabot|INGRID\/0\.1|Informant|InfoSpiders|inspectorwww|irobot|Iron33|JBot|jcrawler|Teoma|Jeeves|jobo|image\.kapsi\.net|KDD\-Explorer|ko_yappo_robot|label\-grabber|larbin|legs|Linkidator|linkwalker|Lockon|logo_gif_crawler|marvin|mattie|mediafox|MerzScope|NEC\-MeshExplorer|MindCrawler|udmsearch|moget|Motor|msnbot|muncher|muninn|MuscatFerret|MwdSearch|sharp\-info\-agent|WebMechanic|NetScoop|newscan\-online|ObjectsSearch|Occam|Orbsearch\/1\.0|packrat|pageboy|ParaSite|patric|pegasus|perlcrawler|phpdig|piltdownman|Pimptrain|pjspider|PlumtreeWebAccessor|PortalBSpider|psbot|Getterrobo\-Plus|Raven|RHCS|RixBot|roadrunner|Robbie|robi|RoboCrawl|robofox|Scooter|Search\-AU|searchprocess|Senrigan|Shagseeker|sift|SimBot|Site Valet|skymob|SLCrawler\/2\.0|slurp|ESI|snooper|solbot|speedy|spider_monkey|SpiderBot\/1\.0|spiderline|nil|suke|http:\/\/www\.sygol\.com|tach_bw|TechBOT|templeton|titin|topiclink|UdmSearch|urlck|Valkyrie libwww\-perl|verticrawl|Victoria|void\-bot|Voyager|VWbot_K|crawlpaper|wapspider|WebBandit\/1\.0|webcatcher|T\-H\-U\-N\-D\-E\-R\-S\-T\-O\-N\-E|WebMoose|webquest|webreaper|webs|webspider|WebWalker|wget|winona|whowhere|wlm|WOLP|WWWC|none|XGET|Nederland\.zoek/i', $_SERVER['HTTP_USER_AGENT']))
 		{
 			// This is a bot and we have to retrieve its connection ID
 			$sql = 'SELECT `id_connections` FROM `'._DB_PREFIX_.'connections`
@@ -124,26 +133,27 @@ class ConnectionCore extends ObjectModel
 		// A new connection is created if the guest made no actions during 30 minutes
 		$sql = 'SELECT `id_guest`
 				FROM `'._DB_PREFIX_.'connections`
-				WHERE `id_guest` = '.(int)($cookie->id_guest).'
+				WHERE `id_guest` = '.(int)$cookie->id_guest.'
 					AND DATE_ADD(`date_add`, INTERVAL 30 MINUTE) > \''.pSQL(date('Y-m-d H:i:00')).'\'
 					'.$shop->addSqlRestriction(Shop::SHARE_CUSTOMER).'
 				ORDER BY `date_add` DESC';
 		$result = Db::getInstance()->getRow($sql);
-		if (!$result['id_guest'] AND (int)($cookie->id_guest))
+		if (!$result['id_guest'] && (int)$cookie->id_guest)
 		{
 			// The old connections details are removed from the database in order to spare some memory
 			Connection::cleanConnectionsPages();
 
 			$referer = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
-			$arrayUrl = parse_url($referer);
-			if (!isset($arrayUrl['host']) OR preg_replace('/^www./', '', $arrayUrl['host']) == preg_replace('/^www./', '', Tools::getHttpHost(false, false)))
+			$array_url = parse_url($referer);
+			if (!isset($array_url['host']) || preg_replace('/^www./', '', $array_url['host']) == preg_replace('/^www./', '', Tools::getHttpHost(false, false)))
 				$referer = '';
 			$connection = new Connection();
-			$connection->id_guest = (int)($cookie->id_guest);
+			$connection->id_guest = (int)$cookie->id_guest;
 			$connection->id_page = Page::getCurrentId();
 			$connection->ip_address = Tools::getRemoteAddr() ? ip2long(Tools::getRemoteAddr()) : '';
 			$connection->id_shop = Context::getContext()->shop->getID();
 			$connection->id_group_shop = Context::getContext()->shop->getGroupID();
+			$connection->date_add = $cookie->date_add;
 			if (Validate::isAbsoluteUrl($referer))
 				$connection->http_referer = $referer;
 			$connection->add();
@@ -155,8 +165,8 @@ class ConnectionCore extends ObjectModel
 	public static function setPageTime($id_connections, $id_page, $time_start, $time)
 	{
 		if (!Validate::isUnsignedId($id_connections)
-			OR !Validate::isUnsignedId($id_page)
-			OR !Validate::isDate($time_start))
+			|| !Validate::isUnsignedId($id_page)
+			|| !Validate::isDate($time_start))
 			return;
 
 		// Limited to 5 minutes because more than 5 minutes is considered as an error
@@ -165,8 +175,8 @@ class ConnectionCore extends ObjectModel
 		Db::getInstance()->execute('
 		UPDATE `'._DB_PREFIX_.'connections_page`
 		SET `time_end` = `time_start` + INTERVAL '.(int)($time / 1000).' SECOND
-		WHERE `id_connections` = '.(int)($id_connections).'
-		AND `id_page` = '.(int)($id_page).'
+		WHERE `id_connections` = '.(int)$id_connections.'
+		AND `id_page` = '.(int)$id_page.'
 		AND `time_start` = \''.pSQL($time_start).'\'');
 	}
 
