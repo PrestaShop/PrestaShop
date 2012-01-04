e9a190ff0a28b5af7858ea258e1c6333e8c59db8
aNiassy <augustin.niassy@prestashop.com>
2012-01-04 13:46:36 +0000

    [-] BO : modified the treatment in order to add specials characters like password in Preferences/Emails
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@12126 b9a71923-0436-4b27-9f14-aed3839534dd

--- admin-dev/ajax_send_mail_test.php
+++ admin-dev/ajax_send_mail_test.php
@@ -30,18 +30,17 @@ include_once(dirname(__FILE__).'/../config/config.inc.php');
 /* Getting cookie or logout */
 require_once(dirname(__FILE__).'/init.php');
 
-$smtpChecked = (trim($_GET['mailMethod']) ==  'smtp');
-$smtpServer = $_GET['smtpSrv'];
-$content = $_GET['testMsg'];
-$subject = $_GET['testSubject'];
+$smtpChecked = (trim($_POST['mailMethod']) ==  'smtp');
+$smtpServer = $_POST['smtpSrv'];
+$content = $_POST['testMsg'];
+$subject = $_POST['testSubject'];
 $type = 'text/html';
-$to =  $_GET['testEmail'];
+$to =  $_POST['testEmail'];
 $from = Configuration::get('PS_SHOP_EMAIL');
-$smtpLogin = $_GET['smtpLogin'];
-$smtpPassword = $_GET['smtpPassword'];
-$smtpPort = $_GET['smtpPort'];
-$smtpEncryption = $_GET['smtpEnc'];
-
+$smtpLogin = $_POST['smtpLogin'];
+$smtpPassword = $_POST['smtpPassword'];
+$smtpPort = $_POST['smtpPort'];
+$smtpEncryption = $_POST['smtpEnc'];
 $result = Mail::sendMailTest(Tools::htmlentitiesUTF8($smtpChecked), Tools::htmlentitiesUTF8($smtpServer), Tools::htmlentitiesUTF8($content), Tools::htmlentitiesUTF8($subject), Tools::htmlentitiesUTF8($type), Tools::htmlentitiesUTF8($to), Tools::htmlentitiesUTF8($from), Tools::htmlentitiesUTF8($smtpLogin), Tools::htmlentitiesUTF8($smtpPassword), Tools::htmlentitiesUTF8($smtpPort), Tools::htmlentitiesUTF8($smtpEncryption));
 die($result === true ? 'ok' : $result);
 
