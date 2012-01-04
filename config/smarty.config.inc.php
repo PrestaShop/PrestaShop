23bff70fa11eb3a0d3f386d63eaae38122ab6a0b
rMalie <raphael.malie@prestashop.com>
2012-01-04 10:41:08 +0000

    // Add hook params on {hook] smarty function

--- config/smarty.config.inc.php
+++ config/smarty.config.inc.php
@@ -156,7 +156,7 @@ function smartyHook($params, &$smarty)
 {
 	if (isset($params['h']) && !empty($params['h']))
 	{
-		unset($hook_params['h']);d($hook_params);
+		unset($hook_params['h']);
 		return Hook::exec($params['h'], $hook_params);
 	}
 }
