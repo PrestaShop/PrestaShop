15b9644aece086d012d135c12bfc89a90382f083
mBertholino <mathieu.bertholino@prestashop.com>
2012-01-04 15:41:40 +0000

    [-] BO : #PSTEST-316 : Bug fixed : Warnings are not displayed anymore after a wrong configuration mail
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@12145 b9a71923-0436-4b27-9f14-aed3839534dd

--- admin-dev/ajax_send_mail_test.php
+++ admin-dev/ajax_send_mail_test.php
@@ -30,7 +30,6 @@ include_once(dirname(__FILE__).'/../config/config.inc.php');
 /* Getting cookie or logout */
 require_once(dirname(__FILE__).'/init.php');
 
-$smtpChecked = (trim($_POST['mailMethod']) ==  'smtp');
 $smtpServer = $_POST['smtpSrv'];
 $content = $_POST['testMsg'];
 $subject = $_POST['testSubject'];
@@ -41,6 +40,10 @@ $smtpLogin = $_POST['smtpLogin'];
 $smtpPassword = $_POST['smtpPassword'];
 $smtpPort = $_POST['smtpPort'];
 $smtpEncryption = $_POST['smtpEnc'];
-$result = Mail::sendMailTest(Tools::htmlentitiesUTF8($smtpChecked), Tools::htmlentitiesUTF8($smtpServer), Tools::htmlentitiesUTF8($content), Tools::htmlentitiesUTF8($subject), Tools::htmlentitiesUTF8($type), Tools::htmlentitiesUTF8($to), Tools::htmlentitiesUTF8($from), Tools::htmlentitiesUTF8($smtpLogin), Tools::htmlentitiesUTF8($smtpPassword), Tools::htmlentitiesUTF8($smtpPort), Tools::htmlentitiesUTF8($smtpEncryption));
-die($result === true ? 'ok' : $result);
+if (isset($_POST['mailMethod']))
+{
+	$smtpChecked = (trim($_POST['mailMethod']) ==  'smtp');
+	$result = Mail::sendMailTest(Tools::htmlentitiesUTF8($smtpChecked), Tools::htmlentitiesUTF8($smtpServer), Tools::htmlentitiesUTF8($content), Tools::htmlentitiesUTF8($subject), Tools::htmlentitiesUTF8($type), Tools::htmlentitiesUTF8($to), Tools::htmlentitiesUTF8($from), Tools::htmlentitiesUTF8($smtpLogin), Tools::htmlentitiesUTF8($smtpPassword), Tools::htmlentitiesUTF8($smtpPort), Tools::htmlentitiesUTF8($smtpEncryption));
+	die($result === true ? 'ok' : $result);
+}
 
