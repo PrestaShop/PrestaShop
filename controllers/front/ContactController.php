02d5c6030cba9e0dfe51199fb8e0d10d7fd2411b
vSchoener <vincent.schoener@prestashop.com>
2012-01-12 14:56:53 +0000

    [-] BO : Fixed bug #PSFV-363. The mails are more detailed for the customer

--- controllers/front/ContactController.php
+++ controllers/front/ContactController.php
@@ -117,11 +117,17 @@ class ContactControllerCore extends FrontController
 				}
 				if (!empty($contact->email))
 				{
+					$mailVarList = array(
+						'{email}' => $from,
+						'{message}' => stripslashes($message),
+						'{id_order}' => (int)Tools::getValue('id_order'),
+						'{attached_file}' => $_FILES['fileUpload']['name'] ? $_FILES['fileUpload']['name'] : '');
+
 					if (Mail::Send($this->context->language->id, 'contact', Mail::l('Message from contact form'),
-					array('{email}' => $from, '{message}' => stripslashes($message)), $contact->email, $contact->name, $from, ($customer->id ? $customer->firstname.' '.$customer->lastname : ''),
-					$fileAttachment)
-						&& Mail::Send($this->context->language->id, 'contact_form', Mail::l('Your message has been correctly sent'), array('{message}' => stripslashes($message)), $from))
-						$this->context->smarty->assign('confirmation', 1);
+								$mailVarList, $contact->email, $contact->name, $from, ($customer->id ? $customer->firstname.' '.$customer->lastname : ''),
+								$fileAttachment) &&
+							Mail::Send($this->context->language->id, 'contact_form', Mail::l('Your message has been correctly sent'), $mailVarList, $from))
+								$this->context->smarty->assign('confirmation', 1);
 					else
 						$this->errors[] = Tools::displayError('An error occurred while sending message.');
 				}
