7c2417e0e1eadbf9fed59b5e13414f0b1d63db79
bMancone <brice.mancone@prestashop.com>
2012-01-16 12:57:15 +0000

    // Fixed norm

--- classes/order/OrderHistory.php
+++ classes/order/OrderHistory.php
@@ -125,7 +125,6 @@ class OrderHistoryCore extends ObjectModel
 				// if waiting for payment => payment error/canceled
 				else if (!$new_os->logable && !$old_os->logable &&
 						 ($new_os->id == Configuration::get('PS_OS_ERROR') || $new_os->id == Configuration::get('PS_OS_CANCELED')))
-
 						 StockAvailable::updateQuantity($product['product_id'], $product['product_attribute_id'], (int)$product['product_quantity'], $order->id_shop);
 				// @since 1.5.0 : if the order is being shipped and this products uses the advanced stock management :
 				// decrements the physical stock using $id_warehouse
