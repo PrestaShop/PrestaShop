853c54aa4846d634d973f37bdec4643d3c4294a5
mMarinetti <michael.marinetti@prestashop.com>
2012-01-18 15:31:29 +0000

    // installer fix
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@12536 b9a71923-0436-4b27-9f14-aed3839534dd

--- install-new/upgrade/sql/1.5.0.3.sql
+++ install-new/upgrade/sql/1.5.0.3.sql
@@ -1,6 +1,8 @@
 SET NAMES 'utf8';
 
-ALTER TABLE PREFIX_theme ADD COLUMN directory varchar(64) NOT NULL;
+ALTER TABLE `PREFIX_theme` ADD COLUMN directory varchar(64) NOT NULL;
+
+UPDATE `PREFIX_theme` SET directory = name;
 
 /* Supply Order modification as of 1.5.0.3 */
 ALTER TABLE `PREFIX_supply_order` DROP INDEX `reference`;
