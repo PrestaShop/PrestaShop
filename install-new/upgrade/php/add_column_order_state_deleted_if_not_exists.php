a9244280ad79833b29781566932987fb31ddd53f
vChabot <vincent.chabot@prestashop.com>
2012-01-19 14:26:21 +0000

    // little fix upgrader

--- install-new/upgrade/php/add_column_order_state_deleted_if_not_exists.php
+++ install-new/upgrade/php/add_column_order_state_deleted_if_not_exists.php
@@ -28,7 +28,6 @@
 function add_column_order_state_deleted_if_not_exists()
 {
 	$column = Db::getInstance()->executeS('SHOW FIELDS FROM `'._DB_PREFIX_.'order_state` LIKE "deleted"');
-	error(Db::getInstance()->getMsgError());
 	if (empty($column))
 		return Db::getInstance()->execute('ALTER TABLE `PREFIX_order_state` 
 			ADD COLUMN `deleted` tinyint(1) UNSIGNED NOT NULL default "0" AFTER `paid`');
