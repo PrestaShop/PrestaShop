8a4b9f7afe305bc5f0524922bb073b6f7b0f4c3f
dMetzger <damien.metzger@prestashop.com>
2012-01-26 17:17:18 +0000

    [-] FO : free gift fixed
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@12726 b9a71923-0436-4b27-9f14-aed3839534dd

--- classes/order/OrderCartRule.php
+++ classes/order/OrderCartRule.php
@@ -58,7 +58,7 @@ class OrderCartRuleCore extends ObjectModel
 			'id_order' => 			array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId', 'required' => true),
 			'id_cart_rule' => 		array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId', 'required' => true),
 			'id_order_invoice' =>	array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId'),
-			'name' => 				array('type' => self::TYPE_STRING, 'validate' => 'isGenericName', 'required' => true),
+			'name' => 				array('type' => self::TYPE_STRING, 'validate' => 'isCleanHtml', 'required' => true),
 			'value' => 				array('type' => self::TYPE_FLOAT, 'validate' => 'isFloat', 'required' => true),
 			'value_tax_excl' => 	array('type' => self::TYPE_FLOAT, 'validate' => 'isFloat', 'required' => true)
 		)
