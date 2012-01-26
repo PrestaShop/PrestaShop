3418f482c6506a2af012cb9834c6ee72b4a38810
mMarinetti <michael.marinetti@prestashop.com>
2012-01-26 17:22:02 +0000

    // minor fix / add return values in php upgrade scripts

--- install-new/upgrade/php/generate_order_reference.php
+++ install-new/upgrade/php/generate_order_reference.php
@@ -2,6 +2,7 @@
 
 function generate_order_reference()
 {
+	$res = true;
 	// Get all orders
 	$orders = Db::getInstance()->executeS('SELECT id_order FROM '._DB_PREFIX_.'orders');
 	foreach ($orders as $order)
@@ -9,9 +10,10 @@ function generate_order_reference()
 		$random_ref = '';
 		for ($i = 0, $passwd = ''; $i < 9; $i++)
 			$random_ref .= substr('ABCDEFGHIJKLMNOPQRSTUVWXYZ', mt_rand(0,25), 1);
-		Db::getInstance()->execute('
+		$res &= Db::getInstance()->execute('
 			UPDATE '._DB_PREFIX_.'orders
 			SET reference = \''.$random_ref.'\'
 			WHERE id_order = '.(int)$order['id_order']);
 	}
+	return $res;
 }
--- install-new/upgrade/php/update_mailalerts_add_column_idshop.php
+++ install-new/upgrade/php/update_mailalerts_add_column_idshop.php
@@ -9,9 +9,12 @@ function update_mailalerts_add_column_idshop()
 	$id_mailalerts = Db::getInstance()->getValue('SELECT id_module FROM  `'._DB_PREFIX_.'module` WHERE name = "mailalerts"');
 
 	if($id_mailalerts)
+	{
 		$res = Db::getInstance()->execute('ALTER TABLE `'._DB_PREFIX_.'mailalert_customer_oos` ADD COLUMN `id_shop` int(11) NOT NULL default "0" AFTER `id_customer`');
-	if ($res)
-		return true;
-	else
-		return array('error' => 1, 'msg' => sprintf('unable to create column id_shop (%s)', Db::getInstance ()->getMsgError()));
+		if ($res)
+			return true;
+		else
+			return array('error' => 1, 'msg' => sprintf('unable to create column id_shop (%s)', Db::getInstance ()->getMsgError()));
+	}
+	return true;
 }
