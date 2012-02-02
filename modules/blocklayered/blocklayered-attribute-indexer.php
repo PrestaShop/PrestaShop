9136bc5ddb3e181efd23e647c286f30bfd95f0e4
mDeflotte <maxence.deflotte@prestashop.com>
2012-02-02 10:48:48 +0000

    // Blocklayered Fix bug with SSL
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@12926 b9a71923-0436-4b27-9f14-aed3839534dd

--- modules/blocklayered/blocklayered-attribute-indexer.php
+++ modules/blocklayered/blocklayered-attribute-indexer.php
@@ -1,7 +1,6 @@
 <?php
 
 include(dirname(__FILE__).'/../../config/config.inc.php');
-include(dirname(__FILE__).'/../../init.php');
 include(dirname(__FILE__).'/blocklayered.php');
 
 if (substr(Tools::encrypt('blocklayered/index'),0,10) != Tools::getValue('token') || !Module::isInstalled('blocklayered'))
