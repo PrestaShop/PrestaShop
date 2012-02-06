dcdc546c931b0c166319fe49595b5ba637a6a46c
dMetzger <damien.metzger@prestashop.com>
2012-02-06 14:57:39 +0000

    [*] BO : shop restriction added to the cart rules

--- install-dev/upgrade/sql/1.5.0.5.sql
+++ install-dev/upgrade/sql/1.5.0.5.sql
@@ -1,1 +1,9 @@
 SET NAMES 'utf8';
+
+ALTER TABLE `PREFIX_cart_rule` ADD `shop_restriction` tinyint(1) unsigned NOT NULL default 0 AFTER `product_restriction`;
+
+CREATE TABLE `PREFIX_cart_rule_shop` (
+	`id_cart_rule` int(10) unsigned NOT NULL,
+	`id_shop` int(10) unsigned NOT NULL,
+	PRIMARY KEY  (`id_cart_rule`, `id_shop`)
+) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
