d92db7153e8ae2ab9e4d8a4756497ab93da013db
mDeflotte <maxence.deflotte@prestashop.com>
2012-02-06 09:02:29 +0000

    // Reverse modification

--- controllers/admin/AdminTrackingController.php
+++ controllers/admin/AdminTrackingController.php
@@ -73,7 +73,6 @@ class AdminTrackingControllerCore extends AdminController
 		$this->_orderWay = 'DESC';
 		self::$currentIndex = 'index.php?controller=AdminCategories';
 		$this->token = Tools::getAdminTokenLite('AdminCategories');
-		$this->_orderBy = null;
 
 		$this->addRowAction('edit');
 		$this->addRowAction('delete');
@@ -109,7 +108,6 @@ class AdminTrackingControllerCore extends AdminController
 		self::$currentIndex = 'index.php?controller=AdminProducts';
 		$this->token = Tools::getAdminTokenLite('AdminProducts');
 		$this->show_toolbar = false;
-		$this->_orderBy = null;
 
 		$this->addRowAction('edit');
 		$this->addRowAction('delete');
@@ -148,7 +146,6 @@ class AdminTrackingControllerCore extends AdminController
 		$this->show_toolbar = false;
 		self::$currentIndex = 'index.php?controller=AdminProducts';
 		$this->token = Tools::getAdminTokenLite('AdminProducts');
-		$this->_orderBy = null;
 
 		$this->addRowAction('edit');
 		$this->addRowAction('delete');
@@ -190,7 +187,6 @@ class AdminTrackingControllerCore extends AdminController
 		$this->show_toolbar = false;
 		self::$currentIndex = 'index.php?controller=AdminProducts';
 		$this->token = Tools::getAdminTokenLite('AdminProducts');
-		$this->_orderBy = null;
 
 		$this->addRowAction('edit');
 		$this->addRowAction('delete');
