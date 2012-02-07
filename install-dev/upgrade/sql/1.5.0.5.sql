0916e09a04a0a2ec5b446b1906cd4a677824f1bb
mDeflotte <maxence.deflotte@prestashop.com>
2012-02-07 09:32:04 +0000

    // Add logo configuration durring upgrade
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@13057 b9a71923-0436-4b27-9f14-aed3839534dd

--- install-dev/upgrade/sql/1.5.0.5.sql
+++ install-dev/upgrade/sql/1.5.0.5.sql
@@ -7,3 +7,9 @@ CREATE TABLE `PREFIX_cart_rule_shop` (
 	`id_shop` int(10) unsigned NOT NULL,
 	PRIMARY KEY  (`id_cart_rule`, `id_shop`)
 ) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
+
+INSERT INTO `PREFIX_configuration`(`id_group_shop`, `id_shop`, `name`, `value`, `date_add`, `date_upd`)
+VALUES
+	(NULL, NULL, 'PS_LOGO', 'logo.jpg', NOw(), NOW())
+	(NULL, NULL, 'PS_LOGO_MAIL', 'logo_mail.jpg', NOw(), NOW())
+	(NULL, NULL, 'PS_LOGO_INVOICE', 'logo_invoice.jpg', NOw(), NOW());
