6b1da50a3626e128bbcf3ff6cd1113005d8924ef
mMarinetti <michael.marinetti@prestashop.com>
2012-02-08 10:15:12 +0000

    // fix a false positive issue in upgrade process (thanks again to Vincent Grimaud)

--- install-dev/upgrade/php/remove_module_from_hook.php
+++ install-dev/upgrade/php/remove_module_from_hook.php
@@ -27,7 +27,7 @@
 
 function remove_module_from_hook($module_name, $hook_name)
 {
-	$result = false;
+	$result = true;
 
 	$id_module = Db::getInstance()->getValue('
 	SELECT `id_module` FROM `'._DB_PREFIX_.'module`
@@ -42,7 +42,7 @@ function remove_module_from_hook($module_name, $hook_name)
 
 		if ((int)$id_hook > 0)
 		{
-			$result = Db::getInstance()->execute('
+			$result &= Db::getInstance()->execute('
 			DELETE FROM `'._DB_PREFIX_.'hook_module`
 			WHERE `id_module` = '.(int)$id_module.' AND `id_hook` = '.(int)$id_hook);
 		}
