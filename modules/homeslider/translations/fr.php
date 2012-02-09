ac1c477fb866b6c6c4745b725d5c33aa0f2401d8
dMetzger <damien.metzger@prestashop.com>
2012-02-09 17:27:37 +0000

    // Go for the eyes Boo, go for the eyes!

--- modules/homeslider/translations/fr.php
+++ modules/homeslider/translations/fr.php
@@ -2,7 +2,7 @@
 
 global $_MODULE;
 $_MODULE = array();
-$_MODULE['<{homeslider}prestashop>homeslider_693b83f5eca43e2bb1675287c37ce9e2'] = 'Slider pour votre page d\'acceuil';
+$_MODULE['<{homeslider}prestashop>homeslider_693b83f5eca43e2bb1675287c37ce9e2'] = 'Slider pour votre page d\'accueil';
 $_MODULE['<{homeslider}prestashop>homeslider_c17aed434289cedd02618451e12c8da6'] = 'Ajouter une image';
 $_MODULE['<{homeslider}prestashop>homeslider_17167898123700e4b4f9bc17c1e0608d'] = 'Configuration du Slider';
 $_MODULE['<{homeslider}prestashop>homeslider_e933dc24fb245d863a43b4fefe9b45f5'] = 'Hauteur:';
