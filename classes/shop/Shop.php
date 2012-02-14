b2ad614fd97c18a867b9fe4387e209fd438a15ad
rMalie <raphael.malie@prestashop.com>
2012-02-14 13:50:04 +0000

    // Remove shop url when a shop is deleted #PSTEST-747
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@13304 b9a71923-0436-4b27-9f14-aed3839534dd

--- classes/shop/Shop.php
+++ classes/shop/Shop.php
@@ -171,9 +171,10 @@ class ShopCore extends ObjectModel
 		}
 
 		// removes stock available
-		$res &= Db::getInstance()->execute('
-			DELETE FROM '._DB_PREFIX_.'stock_available
-			WHERE id_shop = '.(int)$this->id);
+		$res &= Db::getInstance()->delete(_DB_PREFIX_.'stock_available', 'id_shop = '.(int)$this->id);
+
+		// Remove urls
+		$res &= Db::getInstance()->delete(_DB_PREFIX_.'shop_url', 'id_shop = '.(int)$this->id);
 
 		Shop::cacheShops(true);
 
