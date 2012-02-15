3c096af7e951202d0c2a66529ec6ddd1e4bc8ca6
rMalie <raphael.malie@prestashop.com>
2012-02-15 15:51:21 +0000

    // French translations (by Xavier)

--- modules/feeder/translations/fr.php
+++ modules/feeder/translations/fr.php
@@ -2,5 +2,5 @@
 
 global $_MODULE;
 $_MODULE = array();
-$_MODULE['<{feeder}prestashop>feeder_4dce2f12546b229088d3cc214c3c2f7d'] = 'Flux RSS de produits';
-$_MODULE['<{feeder}prestashop>feeder_d70a880c7153de483014eababfa65f11'] = 'Générer un Flux RSS de produits';
+$_MODULE['<{feeder}prestashop>feeder_4dce2f12546b229088d3cc214c3c2f7d'] = 'Flux RSS des produits';
+$_MODULE['<{feeder}prestashop>feeder_d70a880c7153de483014eababfa65f11'] = 'Générer un flux RSS des produits';
--- modules/graphartichow/translations/fr.php
+++ modules/graphartichow/translations/fr.php
@@ -3,4 +3,4 @@
 global $_MODULE;
 $_MODULE = array();
 $_MODULE['<{graphartichow}prestashop>graphartichow_6b1e6241446c46f7ab8d28ae83d53e71'] = 'Artichow';
-$_MODULE['<{graphartichow}prestashop>graphartichow_83196a03f22b50fec58a7fd9507b378a'] = 'Artichow est une librairie qui permet d\'afficher des graphiques simples basés sur des images utilisant PHP et GD.';
+$_MODULE['<{graphartichow}prestashop>graphartichow_83196a03f22b50fec58a7fd9507b378a'] = 'Artichow est une bibliothèque qui permet d\'afficher des graphiques simples utilisant PHP et GD.';
