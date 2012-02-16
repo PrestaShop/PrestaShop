3da09167ba5d8405d4e3483aa1a8f862f174f964
mDeflotte <maxence.deflotte@prestashop.com>
2012-02-16 08:31:27 +0000

    // Blocklayered : Fix bug with category_box from ajax-back always empty
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@13371 b9a71923-0436-4b27-9f14-aed3839534dd

--- modules/blocklayered/blocklayered-ajax-back.php
+++ modules/blocklayered/blocklayered-ajax-back.php
@@ -34,10 +34,12 @@ if (substr(Tools::encrypt('blocklayered/index'),0,10) != Tools::getValue('layere
 
 include(dirname(__FILE__).'/blocklayered.php');
 
+$category_box = Tools::getValue('categoryBox');
+
 /* Clean categoryBox before use */
-if (isset($_GET['categoryBox']) AND is_array($_GET['categoryBox']))
-	foreach ($_GET['categoryBox'] AS &$value)
+if (is_array($category_box))
+	foreach ($category_box AS &$value)
 		$value = (int)$value;
 
 $blockLayered = new BlockLayered();
-echo $blockLayered->ajaxCallBackOffice(Tools::getValue('categoryBox'), Tools::getValue('id_layered_filter'));
+echo $blockLayered->ajaxCallBackOffice($category_box, Tools::getValue('id_layered_filter'));
