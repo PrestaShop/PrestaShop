7ac8bf93850f52937907776f0d713b3d5a15a577
aFolletete <alain.folletete@prestashop.com>
2012-02-22 17:25:04 +0000

    // Update installer

--- install-dev/upgrade/sql/1.5.0.6.sql
+++ install-dev/upgrade/sql/1.5.0.6.sql
@@ -9,8 +9,3 @@ SET o.`current_state` = (
 	LIMIT 1
 );
 
-/* PHP:add_unknown_gender(); */;
-
-ALTER TABLE `PREFIX_cart_rule` ADD `gift_product_attribute` int(10) unsigned NOT NULL default 0 AFTER `gift_product`;
-
-UPDATE `PREFIX_product` set is_virtual = 1 WHERE id_product IN (SELECT id_product FROM `PREFIX_product_download` WHERE active = 1);
