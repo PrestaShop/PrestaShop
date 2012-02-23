ad41e83fbc67918777f463cba1d7b92df9c1b025
dMetzger <damien.metzger@prestashop.com>
2012-02-23 14:42:23 +0000

    [+] BO : BO width can now be fixed or flexible (you can set it up in your employee account)

--- install-dev/data/db_structure.sql
+++ install-dev/data/db_structure.sql
@@ -692,6 +692,7 @@ CREATE TABLE `PREFIX_employee` (
   `stats_date_to` date default NULL,
   `bo_color` varchar(32) default NULL,
   `bo_theme` varchar(32) default NULL,
+  `bo_width` int(10) unsigned NOT NULL DEFAULT 0,
   `bo_show_screencast` tinyint(1) NOT NULL default '1',
   `active` tinyint(1) unsigned NOT NULL default '0',
   `id_last_order` tinyint(1) unsigned NOT NULL default '0',
