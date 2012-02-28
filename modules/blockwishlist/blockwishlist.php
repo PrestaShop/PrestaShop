5a54004fc8cc9ab42737b995b7b6136ccf9f689f
mBertholino <mathieu.bertholino@prestashop.com>
2012-02-28 09:42:17 +0000

    [*] MO : updated module blockwishlist for mobile theme

--- modules/blockwishlist/blockwishlist.php
+++ modules/blockwishlist/blockwishlist.php
@@ -221,6 +221,12 @@ class BlockWishList extends Module
 	{
 		return $this->display(__FILE__, 'my-account.tpl');
 	}
+
+	public function hookDisplayMobileCustomerAccount($params)
+	{
+		$this->smarty->assign('mobile_hook', true);
+		return $this->display(__FILE__, 'my-account.tpl');
+	}
 	
 	public function hookMyAccountBlock($params)
 	{
