0546f91456f71a2610e63a47ffac67eaeb0a356b
bMancone <brice.mancone@prestashop.com>
2012-03-06 13:35:05 +0000

    // Improved filters
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@13886 b9a71923-0436-4b27-9f14-aed3839534dd

--- controllers/admin/AdminStockCoverController.php
+++ controllers/admin/AdminStockCoverController.php
@@ -190,8 +190,13 @@ class AdminStockCoverControllerCore extends AdminController
 		$this->_select = 'a.id_product as id, COUNT(pa.id_product_attribute) as variations, SUM(s.usable_quantity) as stock';
 		$this->_join = 'LEFT JOIN `'._DB_PREFIX_.'product_attribute` pa ON (pa.id_product = a.id_product)
 						INNER JOIN `'._DB_PREFIX_.'stock` s ON (s.id_product = a.id_product)';
+
+		self::$currentIndex .= '&coverage_period='.(int)$this->getCurrentCoveragePeriod().'&warn_days='.(int)$this->getCurrentWarning();
 		if ($this->getCurrentCoverageWarehouse() != -1)
-			$this->_where .= ' AND s.id_warehouse = '.$this->getCurrentCoverageWarehouse();
+		{
+			$this->_where .= ' AND s.id_warehouse = '.(int)$this->getCurrentCoverageWarehouse();
+			self::$currentIndex .= '&id_warehouse='.(int)$this->getCurrentCoverageWarehouse();
+		}
 
 		// Hack for multi shop ..
 		$this->_where .= ' AND b.id_shop = 1';
