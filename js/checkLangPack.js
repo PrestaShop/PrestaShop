c899a277cc858ca89ab6fd3e20cada92f7b4971d
tDidierjean <thomas.didierjean@prestashop.com>
2012-03-07 16:35:29 +0000

    // Fix translation pack fetching in AdminLanguages form
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@13938 b9a71923-0436-4b27-9f14-aed3839534dd

--- js/checkLangPack.js
+++ js/checkLangPack.js
@@ -27,6 +27,8 @@
 function checkLangPack(token){
 	if ($('#iso_code').val().length == 2)
 	{
+		$('#lang_pack_loading').show();
+		$('#lang_pack_msg').hide();
 		doAdminAjax(
 			{
 				controller:'AdminLanguages',
@@ -38,6 +40,7 @@ function checkLangPack(token){
 			},
 			function(ret)
 			{
+				$('#lang_pack_loading').hide();
 				ret = $.parseJSON(ret);
 				if( ret.status == 'ok')
 				{
@@ -46,7 +49,8 @@ function checkLangPack(token){
 						+'<br />' + langPackVersion + ' ' + content['version']
 						+ ' <a href="http://www.prestashop.com/download/lang_packs/gzip/' + content['version'] + '/'
 						+ $('#iso_code').val()+'.gzip" target="_blank" class="link">'+download+'</a><br />' + langPackInfo;
-					showSuccessMessage(message);
+					$('#lang_pack_msg').html(message);
+					$('#lang_pack_msg').show();
 				}
 				else
 					showErrorMessage(ret.error);
