07322065089b60b678b8163a98ce196866b90d3d
bMancone <brice.mancone@prestashop.com>
2012-03-07 09:09:23 +0000

    // Fixed #PSTEST-885
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@13913 b9a71923-0436-4b27-9f14-aed3839534dd

--- controllers/admin/AdminSupplyOrdersController.php
+++ controllers/admin/AdminSupplyOrdersController.php
@@ -1636,7 +1636,7 @@ class AdminSupplyOrdersControllerCore extends AdminController
 		$query->leftJoin('attribute', 'atr', 'atr.id_attribute = pac.id_attribute');
 		$query->leftJoin('attribute_lang', 'al', 'al.id_attribute = atr.id_attribute AND al.id_lang = '.$id_lang);
 		$query->leftJoin('attribute_group_lang', 'agl', 'agl.id_attribute_group = atr.id_attribute_group AND agl.id_lang = '.$id_lang);
-		$query->leftJoin('product_supplier', 'ps', 'ps.id_product = p.id_product AND ps.id_product_attribute = pa.id_product_attribute');
+		$query->leftJoin('product_supplier', 'ps', 'ps.id_product = p.id_product AND ps.id_product_attribute = IFNULL(pa.id_product_attribute, 0)');
 
 		$query->where('(pl.name LIKE \'%'.$pattern.'%\' OR p.reference LIKE \'%'.$pattern.'%\' OR ps.product_supplier_reference LIKE \'%'.$pattern.'%\')');
 		$query->where('p.id_product NOT IN (SELECT pd.id_product FROM `'._DB_PREFIX_.'product_download` pd WHERE (pd.id_product = p.id_product))');
