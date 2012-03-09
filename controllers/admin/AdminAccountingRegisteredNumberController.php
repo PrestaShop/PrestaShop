755f6faa5ee8807daaca91f5975aff5ca6d770a4
vSchoener <vincent.schoener@prestashop.com>
2012-03-09 14:45:45 +0000

    // fix norme
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@14017 b9a71923-0436-4b27-9f14-aed3839534dd

--- controllers/admin/AdminAccountingRegisteredNumberController.php
+++ controllers/admin/AdminAccountingRegisteredNumberController.php
@@ -32,7 +32,7 @@ class AdminAccountingRegisteredNumberControllerCore extends AdminController
 	public function __construct()
 	{
 		parent::__construct();
-		
+
 		$this->className = 'Accounting';
 		$id_lang = $this->context->language->id;
 
@@ -66,7 +66,7 @@ class AdminAccountingRegisteredNumberControllerCore extends AdminController
 			// Gift wrapping definition
 			'gift_wrapping' => array(
 				'func_call' => 'getAccountingNumberConfiguration',
-                'key' => 'account_gift_wripping',
+				'key' => 'account_gift_wripping',
 				'fields' => array(
 					'account_number' => $this->l('Account number'),
 					'total' => $this->l('Number of gift-wrapping associated to this account')
@@ -74,27 +74,27 @@ class AdminAccountingRegisteredNumberControllerCore extends AdminController
 				'title' => $this->l('Gift wrapping account number list'),
 				'list' => array()),
 
-            // Submited shipping charge definition
-            'submited_shipping_charge' => array(
-                'func_call' => 'getAccountingNumberConfiguration',
-                'key' => 'account_submit_shipping_charge',
-                'fields' => array(
-                    'account_number' => $this->l('Account number'),
-                    'total' => $this->l('Number of submited shipping charge associated to this account')
-                ),
-                'title' => $this->l('Submited shipping charge account number list'),
-                'list' => array()),
+			// Submited shipping charge definition
+			'submited_shipping_charge' => array(
+				'func_call' => 'getAccountingNumberConfiguration',
+				'key' => 'account_submit_shipping_charge',
+				'fields' => array(
+					'account_number' => $this->l('Account number'),
+					'total' => $this->l('Number of submited shipping charge associated to this account')
+				),
+				'title' => $this->l('Submited shipping charge account number list'),
+				'list' => array()),
 
-            // Unsubmited shipping charge definition
-            'unsubmited_shipping_charge' => array(
-                'func_call' => 'getAccountingNumberConfiguration',
-                'key' => 'account_unsubmit_shipping_charge',
-                'fields' => array(
-                    'account_number' => $this->l('Account number'),
-                    'total' => $this->l('Number of unsubmited shipping charge associated to this account')
-                ),
-                'title' => $this->l('Unsubmited shipping charge account number list'),
-                'list' => array()),
+			// Unsubmited shipping charge definition
+			'unsubmited_shipping_charge' => array(
+				'func_call' => 'getAccountingNumberConfiguration',
+				'key' => 'account_unsubmit_shipping_charge',
+				'fields' => array(
+					'account_number' => $this->l('Account number'),
+					'total' => $this->l('Number of unsubmited shipping charge associated to this account')
+				),
+				'title' => $this->l('Unsubmited shipping charge account number list'),
+				'list' => array()),
 
 			// Customer definition
 			'customer' => array(
@@ -142,48 +142,50 @@ class AdminAccountingRegisteredNumberControllerCore extends AdminController
 		parent::initContent();
 	}
 
-    /**
-     * Return the value configuration requested.
-     *
-     * @TODO : Add the possibility to check in all shop
-     * @param string $key of the Accounting configuration
-     * @return array
-     */
-    public function getAccountingNumberConfiguration($key)
-    {
-        if (($num = Accounting::getConfiguration($key)))
-            return array(array($num, '1'));
-        return array();
-    }
+	/**
+	 * Return the value configuration requested.
+	 *
+	 * @TODO : Add the possibility to check in all shop
+	 * @param string $key of the Accounting configuration
+	 * @return array
+	 */
+	public function getAccountingNumberConfiguration($key)
+	{
+		if (($num = Accounting::getConfiguration($key)))
+			return array(array($num, '1'));
+		return array();
+	}
 
-    public function initAccountNumberList()
-    {
-        foreach ($this->account_number_list as $name => &$detail)
-        {
-            if (isset($detail['table']) && !empty($detail['table']))
-            {
-                $join = '';
+	public function initAccountNumberList()
+	{
+		foreach ($this->account_number_list as $name => &$detail)
+		{
+			if (isset($detail['table']) && !empty($detail['table']))
+			{
+				$join = '';
 
-                if (isset($detail['left_join']))
-                {
-                    $join = 'LEFT JOIN '._DB_PREFIX_.$detail['left_join']['table'].' s ON (';
-                    foreach ($detail['left_join']['on'] as $on)
-                        $join .= 'p.'.$on.' = s.'.$on.' AND ';
-                    $join = rtrim($join, '  AND ').')';
-                }
+				if (isset($detail['left_join']))
+				{
+					$join = 'LEFT JOIN '._DB_PREFIX_.$detail['left_join']['table'].' s ON (';
+					foreach ($detail['left_join']['on'] as $on)
+						$join .= 'p.'.$on.' = s.'.$on.' AND ';
+					$join = rtrim($join, '  AND ').')';
+				}
 
-                $query = '
-                    SELECT '.implode(', ', array_keys($detail['fields'])).'
+				$query = '
+					SELECT '.implode(', ', array_keys($detail['fields'])).'
 					FROM `'._DB_PREFIX_.$detail['table'].'` p '.$join.'
 					WHERE '.$detail['condition'];
 
-                if (isset($detail['group_by']))
-                    $query .= ' GROUP BY '.$detail['group_by'];
+				if (isset($detail['group_by']))
+					$query .= ' GROUP BY '.$detail['group_by'];
 
-                $detail['list'] = Db::getInstance()->executeS($query);
-            }
-            else if (isset($detail['func_call']) && isset($detail['key']) && method_exists($this, $detail['func_call']))
-                $detail['list'] = $this->{$detail['func_call']}($detail['key']);
-        }
-    }
+				$detail['list'] = Db::getInstance()->executeS($query);
+			}
+			else if (isset($detail['func_call']) &&
+				isset($detail['key']) &&
+				method_exists($this, $detail['func_call']))
+				$detail['list'] = $this->{$detail['func_call']}($detail['key']);
+		}
+	}
 }
