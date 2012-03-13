1f3d5e8eba50bc1f61228ea4f150709f5e1e5316
mDeflotte <maxence.deflotte@prestashop.com>
2012-03-13 15:52:21 +0000

    // Blocklayered: Fix bug with nb of products per pages + Change revision number

--- modules/blocklayered/blocklayered.php
+++ modules/blocklayered/blocklayered.php
@@ -39,7 +39,7 @@ class BlockLayered extends Module
 	{
 		$this->name = 'blocklayered';
 		$this->tab = 'front_office_features';
-		$this->version = '1.8.1';
+		$this->version = '1.8.2';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
