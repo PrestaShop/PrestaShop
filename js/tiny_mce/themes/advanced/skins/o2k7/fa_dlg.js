66a060479f8675c92069d9c04659371ba2079575
rGaillard <remi.gaillard@prestashop.com>
2012-03-26 15:50:07 +0000

    [*] Core: TinyMCE updated to version 3.4.9
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@14275 b9a71923-0436-4b27-9f14-aed3839534dd

---
+++ js/tiny_mce/themes/advanced/skins/o2k7/fa_dlg.js
@@ -0,0 +1,1 @@
+tinyMCE.addI18n('fa.advhr_dlg',{size:"\u0627\u0631\u062a\u0641\u0627\u0639",noshade:"\u0628\u062f\u0648\u0646 \u0633\u0627\u06cc\u0647",width:"\u067e\u0647\u0646\u0627",normal:"Normal",widthunits:"Units"});
