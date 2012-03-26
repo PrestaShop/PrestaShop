081470629d661b5485b51a32144cc01a5bdb57bf
rGaillard <remi.gaillard@prestashop.com>
2012-03-26 15:50:07 +0000

    [*] Core: TinyMCE updated to version 3.4.9

---
+++ js/tiny_mce/plugins/paste/langs/de_dlg.js
@@ -0,0 +1,1 @@
+tinyMCE.addI18n('de.paste_dlg',{"word_title":"Dr\u00fccken Sie auf Ihrer Tastatur Strg+V, um den Text einzuf\u00fcgen.","text_linebreaks":"Zeilenumbr\u00fcche beibehalten","text_title":"Dr\u00fccken Sie auf Ihrer Tastatur Strg+V, um den Text einzuf\u00fcgen."});
