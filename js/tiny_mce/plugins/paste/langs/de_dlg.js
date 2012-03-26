a8fefda168a734aa573b4efe433a1771c106b8e5
rGaillard <remi.gaillard@prestashop.com>
2012-03-26 15:48:13 +0000

    // tinyMCE update
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@14274 b9a71923-0436-4b27-9f14-aed3839534dd

--- js/tiny_mce/plugins/paste/langs/de_dlg.js
+++
@@ -1,1 +0,0 @@
-tinyMCE.addI18n('de.paste_dlg',{word_title:"Dr\u00fccken Sie auf Ihrer Tastatur Strg+V, um den Text einzuf\u00fcgen.",text_linebreaks:"Zeilenumbr\u00fcche beibehalten",text_title:"Dr\u00fccken Sie auf Ihrer Tastatur Strg+V, um den Text einzuf\u00fcgen."});
