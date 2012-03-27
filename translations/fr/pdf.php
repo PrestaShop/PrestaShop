703a24d43985124885687dbdb2ea7afbbd0cd76b
fBrignoli <franck.brignoli@prestashop.com>
2012-03-27 15:47:23 +0000

    [*] PDF : PDF templates are now into the root directory, you can override those templates into the /theme/<name>/pdf/ directory

--- translations/fr/pdf.php
+++ translations/fr/pdf.php
@@ -2,7 +2,5 @@
 
 global $_LANGPDF;
 $_LANGPDF = array();
-$_LANGPDF['PDF_invoice396124314c5fd1fa5bf463274896dc80'] = 'Pas de méthode d\'affichage de prix défini pour ce groupe d\'utilisateur';
-$_LANGPDF['PDF_invoice5082245495aa792b3d0874088d2b111b'] = 'plopd';
 
 ?>
