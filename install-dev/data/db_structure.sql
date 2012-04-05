e2d881dc3c8fef6faff4d8cd26176f972075ef7e
bMancone <brice.mancone@prestashop.com>
2012-04-05 15:48:33 +0000

    // Fixed #PSFV-810
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@14504 b9a71923-0436-4b27-9f14-aed3839534dd

--- install-dev/data/db_structure.sql
+++ install-dev/data/db_structure.sql
@@ -2300,16 +2300,6 @@ CREATE TABLE `PREFIX_supplier_rates` (
   PRIMARY KEY (`id_product_supplier`)
 ) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `ps_accounting_export` (
-  `id_accounting_export` int(11) NOT NULL AUTO_INCREMENT,
-  `begin_to` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `end_to` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `type` int(11) NOT NULL,
-  `file` varchar(256) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id_accounting_export`)
-) ENGINE=ENGINE_TYPE  DEFAULT CHARSET=utf8;
-
 CREATE TABLE IF NOT EXISTS `PREFIX_accounting_export` (
   `id_accounting_export` int(11) NOT NULL AUTO_INCREMENT,
   `begin_to` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
