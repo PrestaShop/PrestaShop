46cfc8d2b1e183ba7163d43292dc5e63dc1c955b
lLefevre <loic.lefevre@prestashop.com>
2012-04-06 12:09:23 +0000

    // Fix remove include width _Module.php file
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@14526 b9a71923-0436-4b27-9f14-aed3839534dd

--- config/config.inc.php
+++ config/config.inc.php
@@ -63,7 +63,6 @@ require_once(dirname(__FILE__).'/autoload.php');
 if (_PS_DEBUG_PROFILING_)
 {
 	include_once(_PS_ROOT_DIR_.'/override/classes/controller/_Controller.php');
-	include_once(_PS_ROOT_DIR_.'/override/classes/_Module.php');
 	include_once(_PS_ROOT_DIR_.'/override/classes/_ObjectModel.php');
 	include_once(_PS_ROOT_DIR_.'/override/classes/_Hook.php');
 	include_once(_PS_ROOT_DIR_.'/override/classes/db/_Db.php');
