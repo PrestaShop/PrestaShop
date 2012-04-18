92c520c42eb3717cf11fb6e4df6ca24562e25238
vChabot <vincent.chabot@prestashop.com>
2012-04-18 08:10:25 +0000

    [-] FO : BugFix : #PSFV-844 : Categories links are now handled with multishop in footer

--- modules/blockcategories/blockcategories.php
+++ modules/blockcategories/blockcategories.php
@@ -249,6 +249,7 @@ class BlockCategories extends Module
 			if (!$result = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS('
 				SELECT c.id_parent, c.id_category, cl.name, cl.description, cl.link_rewrite
 				FROM `'._DB_PREFIX_.'category` c
+				'.Shop::addSqlAssociation('category', 'c').'
 				LEFT JOIN `'._DB_PREFIX_.'category_lang` cl ON (c.`id_category` = cl.`id_category` AND cl.`id_lang` = '.$id_lang.Shop::addSqlRestrictionOnLang('cl').')
 				LEFT JOIN `'._DB_PREFIX_.'category_group` cg ON (cg.`id_category` = c.`id_category`)
 				WHERE (c.`active` = 1 OR c.`id_category` = 1)
