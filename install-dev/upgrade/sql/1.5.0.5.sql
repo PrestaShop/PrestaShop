ea4f61df54a876377a18b0975c9d608b3d739160
mMarinetti <michael.marinetti@prestashop.com>
2012-05-09 16:02:06 +0000

    // find & replace id_group_shop > id_shop_group was missing ... AGAIN
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@15129 b9a71923-0436-4b27-9f14-aed3839534dd

--- install-dev/upgrade/sql/1.5.0.5.sql
+++ install-dev/upgrade/sql/1.5.0.5.sql
@@ -1,14 +1,14 @@
 SET NAMES 'utf8';
 
 ALTER TABLE `PREFIX_cart_rule` ADD `shop_restriction` tinyint(1) unsigned NOT NULL default 0 AFTER `product_restriction`;
-
+	
 CREATE TABLE `PREFIX_cart_rule_shop` (
 	`id_cart_rule` int(10) unsigned NOT NULL,
 	`id_shop` int(10) unsigned NOT NULL,
 	PRIMARY KEY  (`id_cart_rule`, `id_shop`)
 ) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
 
-INSERT INTO `PREFIX_configuration`(`id_group_shop`, `id_shop`, `name`, `value`, `date_add`, `date_upd`)
+INSERT INTO `PREFIX_configuration`(`id_shop_group`, `id_shop`, `name`, `value`, `date_add`, `date_upd`)
 VALUES
 	(NULL, NULL, 'PS_LOGO', 'logo.jpg', NOw(), NOW()),
 	(NULL, NULL, 'PS_LOGO_MAIL', 'logo_mail.jpg', NOw(), NOW()),
