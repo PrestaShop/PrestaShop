8cede9197848307765bd94c4fa7d053eb308b983
vAugagneur <vincent.augagneur@prestashop.com>
2012-05-10 09:10:52 +0000

    [-] MO : fixed bug #PSCFV-2270 - JS  refacto module favorite products
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@15151 b9a71923-0436-4b27-9f14-aed3839534dd

---
+++ modules/favoriteproducts/favoriteproducts.js
@@ -0,0 +1,74 @@
+$('document').ready(function(){
+	$('#favoriteproducts_block_extra_add').click(function(){
+		$.ajax({
+			url: favorite_products_url_add,
+			type: "POST",
+			data: {
+				"id_product": favorite_products_id_product
+			},
+			success: function(result){
+				if (result == '0')
+				{
+			    	$('#favoriteproducts_block_extra_add').slideUp(function() {
+			    		$('#favoriteproducts_block_extra_added').slideDown("slow");
+			    	});
+
+				}
+		 	}
+		});
+	});
+	$('#favoriteproducts_block_extra_remove').click(function(){
+		$.ajax({
+			url: favorite_products_url_remove,
+			type: "POST",
+			data: {
+				"id_product": favorite_products_id_product
+			},
+			success: function(result){
+				if (result == '0')
+				{
+			    	$('#favoriteproducts_block_extra_remove').slideUp(function() {
+			    		$('#favoriteproducts_block_extra_removed').slideDown("slow");
+			    	});
+
+				}
+		 	}
+		});
+	});
+	$('#favoriteproducts_block_extra_added').click(function(){
+		$.ajax({
+			url: favorite_products_url_remove,
+			type: "POST",
+			data: {
+				"id_product": favorite_products_id_product
+			},
+			success: function(result){
+				if (result == '0')
+				{
+			    	$('#favoriteproducts_block_extra_added').slideUp(function() {
+			    		$('#favoriteproducts_block_extra_removed').slideDown("slow");
+			    	});
+
+				}
+		 	}
+		});
+	});
+	$('#favoriteproducts_block_extra_removed').click(function(){
+		$.ajax({
+			url: favorite_products_url_add,
+			type: "POST",
+			data: {
+				"id_product": favorite_products_id_product
+			},
+			success: function(result){
+				if (result == '0')
+				{
+			    	$('#favoriteproducts_block_extra_removed').slideUp(function() {
+			    		$('#favoriteproducts_block_extra_added').slideDown("slow");
+			    	});
+
+				}
+		 	}
+		});
+	});
+})
