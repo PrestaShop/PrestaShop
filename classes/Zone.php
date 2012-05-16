acb38f0904f9426e2315bf2d05cc15a45e4ef787
vChabot <vincent.chabot@prestashop.com>
2012-05-16 12:46:38 +0000

    [-] BO : BugFix : #PSCFV-2372 : Created zones do not appear
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@15334 b9a71923-0436-4b27-9f14-aed3839534dd

--- classes/Zone.php
+++ classes/Zone.php
@@ -39,6 +39,7 @@ class ZoneCore extends ObjectModel
 	public static $definition = array(
 		'table' => 'zone',
 		'primary' => 'id_zone',
+		'multishop' => true,
 		'fields' => array(
 			'name' => 	array('type' => self::TYPE_STRING, 'validate' => 'isGenericName', 'required' => true, 'size' => 64),
 			'active' => array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
