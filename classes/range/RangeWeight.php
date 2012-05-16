ab5affbb091af4cddd28fe806a2eae0002b4d1a9
vAugagneur <vincent.augagneur@prestashop.com>
2012-05-16 14:16:20 +0000

    //php version compatibility fix
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@15345 b9a71923-0436-4b27-9f14-aed3839534dd

--- classes/range/RangeWeight.php
+++ classes/range/RangeWeight.php
@@ -25,11 +25,12 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-class RangeWeightCore extends Range
+class RangeWeight extends ObjectModel
 {
-	protected static $range_table = 'range_weight';
-	protected static $range_identifier = 'id_range_weight';
-
+	public $id_carrier;
+	public $delimiter1;
+	public $delimiter2;
+	
 	/**
 	 * @see ObjectModel::$definition
 	 */
@@ -77,5 +78,42 @@ class RangeWeightCore extends Range
 		$carrier->addDeliveryPrice($price_list);
 
 		return true;
+	}
+	
+	/**
+	* Get all available price ranges
+	*
+	* @return array Ranges
+	*/
+	public static function getRanges($id_carrier)
+	{
+		return Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS('
+			SELECT * 
+			FROM `'._DB_PREFIX_.'range_weight` 
+			WHERE `id_carrier` = '.(int)$id_carrier.'
+			ORDER BY `delimiter1` ASC');
+	}
+	
+	public static function rangeExist($id_carrier, $delimiter1, $delimiter2)
+	{
+		return Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue('
+			SELECT count(*)
+			FROM `'._DB_PREFIX_.'range_weight`
+			WHERE `id_carrier` = '.(int)$id_carrier.'
+			AND `delimiter1` = '.(float)$delimiter1.' AND `delimiter2`='.(float)$delimiter2);
+	}
+	
+	public static function isOverlapping($id_carrier, $delimiter1, $delimiter2, $id_rang = null)
+	{
+		return Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue('
+			SELECT count(*)
+			FROM `'._DB_PREFIX_.'range_weight`
+			WHERE `id_carrier` = '.(int)$id_carrier.'
+			AND ((`delimiter1` >= '.(float)$delimiter1.' AND `delimiter1` < '.(float)$delimiter2.')
+			    OR (`delimiter2` > '.(float)$delimiter1.' AND `delimiter2` < '.(float)$delimiter2.')
+			    OR ('.(float)$delimiter1.' > `delimiter1` AND '.(float)$delimiter1.' < `delimiter2`)
+			    OR ('.(float)$delimiter2.' < `delimiter1` AND '.(float)$delimiter2.' > `delimiter2`)
+			    )
+			'.(!is_null($id_rang) ? ' AND `id_range_weight` != '.(int)$id_rang : ''));
 	}
 }
