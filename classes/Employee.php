5395188ba361db23189441c00f0f43e161cc0d43
vChabot <vincent.chabot@prestashop.com>
2012-05-16 14:23:33 +0000

    // employee can now be multishop
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@15346 b9a71923-0436-4b27-9f14-aed3839534dd

--- classes/Employee.php
+++ classes/Employee.php
@@ -78,6 +78,7 @@ class EmployeeCore extends ObjectModel
 	public static $definition = array(
 		'table' => 'employee',
 		'primary' => 'id_employee',
+		'multishop' => 'true',
 		'fields' => array(
 			'lastname' => 			array('type' => self::TYPE_STRING, 'validate' => 'isName', 'required' => true, 'size' => 32),
 			'firstname' => 			array('type' => self::TYPE_STRING, 'validate' => 'isName', 'required' => true, 'size' => 32),
