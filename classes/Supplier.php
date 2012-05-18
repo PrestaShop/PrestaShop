dc242942d7f381ad7f47b954e12c97adcf4d0272
aFolletete <alain.folletete@prestashop.com>
2012-05-18 14:43:56 +0000

    [-] BO : fixed bug #PSCFV-2390 - Suppliers multishop issue
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@15382 b9a71923-0436-4b27-9f14-aed3839534dd

--- classes/Supplier.php
+++ classes/Supplier.php
@@ -66,6 +66,7 @@ class SupplierCore extends ObjectModel
 		'table' => 'supplier',
 		'primary' => 'id_supplier',
 		'multilang' => true,
+		'multishop' => true,
 		'fields' => array(
 			'name' => 				array('type' => self::TYPE_STRING, 'validate' => 'isCatalogName', 'required' => true, 'size' => 64),
 			'active' => 			array('type' => self::TYPE_BOOL),
