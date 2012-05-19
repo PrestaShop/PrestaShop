bf054f56807b9df4ce44d2ff7129f5b032ed7d68
tDidierjean <thomas.didierjean@prestashop.com>
2012-05-19 12:56:55 +0000

    // fix B2B missing upgrade sql
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@15438 b9a71923-0436-4b27-9f14-aed3839534dd

--- install-dev/upgrade/sql/1.5.0.12.sql
+++ install-dev/upgrade/sql/1.5.0.12.sql
@@ -26,8 +26,32 @@ UPDATE PREFIX_order_detail_tax odt
 LEFT JOIN PREFIX_order_detail od ON (od.id_order_detail = odt.id_order_detail)
 SET total_amount = odt.unit_amount * od.product_quantity;
 
-ALTER TABLE `PREFIX_pagenotfound` ADD `id_shop_group` INT(10) AFTER `id_pagenotfound`;
-ALTER TABLE `PREFIX_pagenotfound` ADD `id_shop` INT(10) AFTER `id_pagenotfound`;
+ALTER TABLE `PREFIX_pagenotfound` ADD `id_shop_group` INT(10) AFTER `id_pagenotfound`, ADD `id_shop` INT(10) AFTER `id_pagenotfound`;
 
 /* PHP:add_new_groups('Non identifié', 'Unidentified'); */;
 /* PHP:editorial_update_multishop(); */;
+
+ALTER TABLE `PREFIX_customer`
+	ADD `id_risk` int(10) unsigned NOT NULL DEFAULT '1',
+	ADD `company` varchar(64),
+	ADD `siret` varchar(14),
+	ADD `ape` varchar(5),
+	ADD `website` varchar(128),
+	ADD `outstanding_allow_amount` DECIMAL( 10,6 ) NOT NULL default '0.00',
+	ADD `show_public_prices` tinyint(1) unsigned NOT NULL default '0',
+	ADD `max_payment_days` int(10) unsigned NOT NULL default '60';
+
+CREATE TABLE IF NOT EXISTS `PREFIX_risk` (
+  `id_risk` int(11) NOT NULL AUTO_INCREMENT,
+  `percent` tinyint(3) NOT NULL,
+  `color` varchar(32) NULL,
+  PRIMARY KEY (`id_risk`)
+) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `PREFIX_risk_lang` (
+  `id_risk` int(10) unsigned NOT NULL,
+  `id_lang` int(10) unsigned NOT NULL,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_risk`,`id_lang`),
+  KEY `id_risk` (`id_risk`)
+) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
