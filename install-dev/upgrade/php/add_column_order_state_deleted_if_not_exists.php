c48fbdde9df8660700d2481fd286764a346c8e41
mMarinetti <michael.marinetti@prestashop.com>
2012-05-19 18:54:24 +0000

    // fix PREFIX_table missing

--- install-dev/upgrade/php/add_column_order_state_deleted_if_not_exists.php
+++ install-dev/upgrade/php/add_column_order_state_deleted_if_not_exists.php
@@ -29,7 +29,9 @@ function add_column_order_state_deleted_if_not_exists()
 {
 	$column = Db::getInstance()->executeS('SHOW FIELDS FROM `'._DB_PREFIX_.'order_state` LIKE "deleted"');
 	if (empty($column))
-		return Db::getInstance()->execute('ALTER TABLE `PREFIX_order_state` 
+		$res = Db::getInstance()->execute('ALTER TABLE `'._DB_PREFIX_.'order_state` 
 			ADD COLUMN `deleted` tinyint(1) UNSIGNED NOT NULL default "0" AFTER `paid`');
+	if (!$res)
+		return array('error' => Db::getInstance()->getNumberError(), 'msg' => Db::getInstance()->getMsgError());
 	return true;
 }
