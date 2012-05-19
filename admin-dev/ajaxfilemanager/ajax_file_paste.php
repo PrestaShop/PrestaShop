e49afede06f6046954507ff667a93328b093a98a
jObregon <jorge.obregon@prestashop.com>
2012-05-19 22:03:35 +0000

    [-] BO : Fixed bug #PSCFV-2288. _PS_ADMIN_DIR_ was not defined

--- admin-dev/ajaxfilemanager/ajax_file_paste.php
+++ admin-dev/ajaxfilemanager/ajax_file_paste.php
@@ -1,6 +1,7 @@
 <?
 	include_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . "inc" . DIRECTORY_SEPARATOR . "config.php");		
-	define('_PS_ADMIN_DIR_', getcwd());
+	if (!defined('_PS_ADMIN_DIR_'))
+		define('_PS_ADMIN_DIR_', getcwd());
 	require_once('../../config/config.inc.php');
 	require_once('../init.php');
 	$error = '';
