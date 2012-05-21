c75f93d63e14a93bb02dea29330bb29c45336912
dMetzger <damien.metzger@prestashop.com>
2012-05-21 14:19:54 +0000

    [-] FO : added the concept of product package (multi-shipping) for the cart rules in getContextualValue()

--- classes/Cart.php
+++ classes/Cart.php
@@ -1459,7 +1459,7 @@ class CartCore extends ObjectModel
 						$in_order = true;
 					else
 						foreach ($products as $products)
-							if ($cart_rule['obj']->gift_product == $product['id_product'] && ($cart_rule['obj']->gift_product_attribute == 0 || $cart_rule['obj']->gift_product_attribute = $product['id_product_attribute']))
+							if ($cart_rule['obj']->gift_product == $product['id_product'] && $cart_rule['obj']->gift_product_attribute = $product['id_product_attribute'])
 								$in_order = true;
 
 					if ($in_order)
@@ -1468,7 +1468,11 @@ class CartCore extends ObjectModel
 
 				// If the cart rule offers a reduction, the amount is prorated (with the products in the package)
 				if ($cart_rule['obj']->reduction_percent > 0 || $cart_rule['obj']->reduction_amount > 0)
-					$order_total_discount += Tools::ps_round($cart_rule['obj']->getContextualValue($with_taxes, $virtual_context, CartRule::FILTER_ACTION_REDUCTION, $products), 2);
+				{
+					$id_address_delivery = (is_null($products) ? $this->id_address_delivery : $products[0]['id_address_delivery']);
+					$package = array('id_address' => $id_address_delivery, 'products' => $products);
+					$order_total_discount += Tools::ps_round($cart_rule['obj']->getContextualValue($with_taxes, $virtual_context, CartRule::FILTER_ACTION_REDUCTION, $package), 2);
+				}
 			}
 
 			$order_total_discount = min(Tools::ps_round($order_total_discount, 2), $wrapping_fees + $order_total_products + $shipping_fees);
