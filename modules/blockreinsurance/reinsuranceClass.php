aeafbd2d1df29f557a764c159354eec483e719c4
vAugagneur <vincent.augagneur@prestashop.com>
2012-06-01 14:26:26 +0000

    //module reinsurance is now multi-shop and multi-lang

---
+++ modules/blockreinsurance/reinsuranceClass.php
@@ -0,0 +1,75 @@
+<?php
+/*
+* 2007-2012 PrestaShop 
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author PrestaShop SA <contact@prestashop.com>
+*  @copyright  2007-2012 PrestaShop SA
+*  @version  Release: $Revision: 6844 $
+*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
+class reinsuranceClass extends ObjectModel
+{
+	/** @var integer reinsurance id*/
+	public $id;
+	
+	/** @var integer reinsurance id shop*/
+	public $id_shop;
+	
+	/** @var string reinsurance file name icon*/
+	public $file_name;
+
+	/** @var string reinsurance text*/
+	public $text;
+
+
+	/**
+	 * @see ObjectModel::$definition
+	 */
+	public static $definition = array(
+		'table' => 'reinsurance',
+		'primary' => 'id_reinsurance',
+		'multilang' => true,
+		'fields' => array(
+			'id_shop' =>				array('type' => self::TYPE_INT, 'validate' => 'isunsignedInt', 'required' => true),
+			'file_name' =>				array('type' => self::TYPE_STRING, 'validate' => 'isFileName'),
+			// Lang fields
+			'text' =>					array('type' => self::TYPE_STRING, 'lang' => true, 'validate' => 'isGenericName', 'required' => true),
+		)
+	);
+
+	public function copyFromPost()
+	{
+		/* Classical fields */
+		foreach ($_POST AS $key => $value)
+			if (key_exists($key, $this) AND $key != 'id_'.$this->table)
+				$this->{$key} = $value;
+
+		/* Multilingual fields */
+		if (sizeof($this->fieldsValidateLang))
+		{
+			$languages = Language::getLanguages(false);
+			foreach ($languages AS $language)
+				foreach ($this->fieldsValidateLang AS $field => $validation)
+					if (isset($_POST[$field.'_'.(int)($language['id_lang'])]))
+						$this->{$field}[(int)($language['id_lang'])] = $_POST[$field.'_'.(int)($language['id_lang'])];
+		}
+	}
+}
