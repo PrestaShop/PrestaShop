99342ff375f6421d4407679b701663c6f4d105b0
vAugagneur <vincent.augagneur@prestashop.com>
2012-06-04 13:10:15 +0000

    //fixed bug #PSCFV-2666
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@15825 b9a71923-0436-4b27-9f14-aed3839534dd

--- install-dev/upgrade/sql/1.5.0.13.sql
+++ install-dev/upgrade/sql/1.5.0.13.sql
@@ -35,3 +35,8 @@ ALTER TABLE `PREFIX_order_payment`
 	DROP COLUMN `id_order`,
 	DROP COLUMN `id_order_invoice`;
 
+-- update for all employee the last ids for notifications
+UPDATE `PREFIX_employee` 
+	SET `id_last_order`= (SELECT max(`id_order`) FROM `PREFIX_orders`),
+	SET `id_last_customer_message`= (SELECT max(`id_customer_message`) FROM `PREFIX_customer_message`),
+	SET `id_last_customer`= (SELECT max(`id_customer`) FROM `PREFIX_customer`);
