7b10496294b33c8f35479b4a885c0f1f54763bbb
mDeflotte <maxence.deflotte@prestashop.com>
2012-06-18 13:01:42 +0000

    [-] MO : #PSCFV-2858 - Fix bugs with non latin chars. Htmlentities was not usefull
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@16051 b9a71923-0436-4b27-9f14-aed3839534dd

--- modules/blocklayered/blocklayered.js
+++ modules/blocklayered/blocklayered.js
@@ -398,7 +398,7 @@ function reloadContent(params_plus)
 		cache: false, // @todo see a way to use cache and to add a timestamps parameter to refresh cache each 10 minutes for example
 		success: function(result)
 		{
-			$('#layered_block_left').replaceWith(result.filtersBlock);
+			$('#layered_block_left').replaceWith(utf8_decode(result.filtersBlock));
 			
 			$('.category-product-count').html(result.categoryCount);
 			
