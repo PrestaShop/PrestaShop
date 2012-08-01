218236c1ee5f84340e367c32e6c7c40520044982
lLefevre <loic.lefevre@prestashop.com>
2012-08-01 07:45:00 +0000

    [-] TR : fix #PSCFV-3403 : check if is an object before loading stock data
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@16634 b9a71923-0436-4b27-9f14-aed3839534dd

--- classes/Product.php
+++ classes/Product.php
@@ -4808,15 +4808,17 @@ class ProductCore extends ObjectModel
 	 */
 	public function loadStockData()
 	{
-		// By default, the product quantity correspond to the available quantity to sell in the current shop
-		$this->quantity = StockAvailable::getQuantityAvailableByProduct($this->id, 0);
-		$this->out_of_stock = StockAvailable::outOfStock($this->id);
-		$this->depends_on_stock = StockAvailable::dependsOnStock($this->id);
-		if (Context::getContext()->shop->getContext() == Shop::CONTEXT_GROUP && Context::getContext()->shop->getContextShopGroup()->share_stock == 1)
-			
-			$this->advanced_stock_management = Db::getInstance()->getValue('SELECT `advanced_stock_management`
-																FROM '._DB_PREFIX_.'product_shop
-																WHERE id_product='.(int)$this->id.Shop::addSqlRestriction());
+		if (Validate::isLoadedObject($this))
+		{
+			// By default, the product quantity correspond to the available quantity to sell in the current shop
+			$this->quantity = StockAvailable::getQuantityAvailableByProduct($this->id, 0);
+			$this->out_of_stock = StockAvailable::outOfStock($this->id);
+			$this->depends_on_stock = StockAvailable::dependsOnStock($this->id);
+			if (Context::getContext()->shop->getContext() == Shop::CONTEXT_GROUP && Context::getContext()->shop->getContextShopGroup()->share_stock == 1)
+				$this->advanced_stock_management = Db::getInstance()->getValue('SELECT `advanced_stock_management`
+																	FROM '._DB_PREFIX_.'product_shop
+																	WHERE id_product='.(int)$this->id.Shop::addSqlRestriction());
+		}
 	}
 
 	/**
