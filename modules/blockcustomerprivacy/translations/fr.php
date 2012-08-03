a8263ee9ad010c2bf5c5c9a57d2fb622330f93d1
dMetzger <damien.metzger@prestashop.com>
2012-08-03 07:56:35 +0000

    // Filled a default message in customer privacy #PSCFV-3471
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@16676 b9a71923-0436-4b27-9f14-aed3839534dd

--- modules/blockcustomerprivacy/translations/fr.php
+++ modules/blockcustomerprivacy/translations/fr.php
@@ -4,6 +4,8 @@ global $_MODULE;
 $_MODULE = array();
 $_MODULE['<{blockcustomerprivacy}prestashop>blockcustomerprivacy_0cec62d1bea02fbb1484d55d14f493dd'] = 'Bloc confidentialité des données clients';
 $_MODULE['<{blockcustomerprivacy}prestashop>blockcustomerprivacy_3a32ec2dbd8d955f6240d95d6004557c'] = 'Ajoute un bloc qui affiche un message concernant la confidentialité des données clients.';
+$_MODULE['<{blockcustomerprivacy}prestashop>blockcustomerprivacy_77cb2fa873bee41280d3a1c9ffd9b821'] = 'Les informations personnelles que nous collectons sont destinées à mieux répondre à vos demandes et traiter vos commandes.';
+$_MODULE['<{blockcustomerprivacy}prestashop>blockcustomerprivacy_0560788430987c4914fea998886d3977'] = 'Vous disposez à tout moment d’un droit d’accès, de modification et de suppression de vos informations personnelles que vous pouvez exercer via la page \"mon compte\" de ce site internet.';
 $_MODULE['<{blockcustomerprivacy}prestashop>blockcustomerprivacy_20015706a8cbd457cbb6ea3e7d5dc9b3'] = 'Configuration mise à jour';
 $_MODULE['<{blockcustomerprivacy}prestashop>blockcustomerprivacy_b75fe4f32f17eb695f145704a8909146'] = 'Message concernant la confidentialité des données clients';
 $_MODULE['<{blockcustomerprivacy}prestashop>blockcustomerprivacy_03e6791c07164957bb87bfad5030f846'] = 'Le message sera affiché sur le formulaire de création de compte.';
