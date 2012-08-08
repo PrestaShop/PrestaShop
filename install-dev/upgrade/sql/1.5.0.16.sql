8157f836a35412d49756a7cb70fed61986959791
dMetzger <damien.metzger@prestashop.com>
2012-08-08 13:47:01 +0000

    [-] Installer : deprecated tab removed in the 1.4 > 1.5 upgrade #PSCFV-3564
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@16766 b9a71923-0436-4b27-9f14-aed3839534dd

--- install-dev/upgrade/sql/1.5.0.16.sql
+++ install-dev/upgrade/sql/1.5.0.16.sql
@@ -3,3 +3,8 @@ SET NAMES 'utf8';
 ALTER TABLE `PREFIX_order_detail` ADD `id_shop` INT(11) UNSIGNED NOT NULL AFTER `id_warehouse`, ADD INDEX (`id_shop`);
 
 UPDATE `PREFIX_order_detail` od SET `id_shop`=(SELECT `id_shop` FROM `PREFIX_orders` WHERE `id_order`=od.`id_order`);
+
+DELETE FROM `PREFIX_tab` WHERE `class_name` = 'AdminAddonsMyAccount';
+DELETE FROM `PREFIX_tab_lang` WHERE `id_tab` NOT IN (SELECT `id_tab` FROM `PREFIX_tab`);
+DELETE FROM `PREFIX_access` WHERE `id_tab` NOT IN (SELECT `id_tab` FROM `PREFIX_tab`);
+ 
