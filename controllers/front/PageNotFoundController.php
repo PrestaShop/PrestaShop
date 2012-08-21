d9afd88155df143ed9f8ee802d5efe116f05551d
rGaillard <remi.gaillard@prestashop.com>
2012-08-21 09:18:00 +0000

    // 404 should return 404...
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@16925 b9a71923-0436-4b27-9f14-aed3839534dd

--- controllers/front/PageNotFoundController.php
+++ controllers/front/PageNotFoundController.php
@@ -36,6 +36,8 @@ class PageNotFoundControllerCore extends FrontController
 	 */
 	public function initContent()
 	{
+		header('HTTP/1.1 404 Not Found');
+		header('Status: 404 Not Found');
 		parent::initContent();
 
 		$this->setTemplate(_PS_THEME_DIR_.'404.tpl');
