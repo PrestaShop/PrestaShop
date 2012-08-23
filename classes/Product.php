cf3dfc54f79fd72694a23001048bec7e17da1df8
rGaillard <remi.gaillard@prestashop.com>
2012-08-23 17:42:34 +0000

    // Small fix

--- classes/Product.php
+++ classes/Product.php
@@ -2147,8 +2147,9 @@ class ProductCore extends ObjectModel
 		$ret = array();
 		$row = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS('
 			SELECT cp.`id_category`, cl.`name`, cl.`link_rewrite` FROM `'._DB_PREFIX_.'category_product` cp
+			LEFT JOIN `'._DB_PREFIX_.'category` c ON (c.id_category = cp.id_category)
 			LEFT JOIN `'._DB_PREFIX_.'category_lang` cl ON (cp.`id_category` = cl.`id_category`'.Shop::addSqlRestrictionOnLang('cl').')
-			'.Shop::addSqlAssociation('category', 'cp').'
+			'.Shop::addSqlAssociation('category', 'c').'
 			WHERE cp.`id_product` = '.(int)$id_product.'
 				AND cl.`id_lang` = '.(int)$id_lang
 		);
