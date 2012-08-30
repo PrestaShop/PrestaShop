ce8517413dfcbc5cc5637a23e8c33d01bc4284b9
rGaillard <remi.gaillard@prestashop.com>
2012-08-30 15:19:45 +0000

    // Small fix
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@17102 b9a71923-0436-4b27-9f14-aed3839534dd

--- modules/homefeatured/homefeatured.php
+++ modules/homefeatured/homefeatured.php
@@ -99,7 +99,7 @@ class HomeFeatured extends Module
 	{
 		$category = new Category(Context::getContext()->shop->getCategory(), (int)Context::getContext()->language->id);
 		$nb = (int)(Configuration::get('HOME_FEATURED_NBR'));
-		$products = $category->getProducts($params['cookie']->id_lang, 1, ($nb ? $nb : 10));
+		$products = $category->getProducts((int)Context::getContext()->language->id, 1, ($nb ? $nb : 10));
 
 		$this->smarty->assign(array(
 			'products' => $products,
