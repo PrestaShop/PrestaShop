41daac1092b6439924b5fc64c8157aeb1d816585
vAugagneur <vincent.augagneur@prestashop.com>
2012-09-04 12:38:09 +0000

    //oups
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@17162 b9a71923-0436-4b27-9f14-aed3839534dd

--- install-dev/upgrade/sql/1.5.0.17.sql
+++ install-dev/upgrade/sql/1.5.0.17.sql
@@ -2,3 +2,5 @@ SET NAMES 'utf8';
 
 ALTER TABLE `PREFIX_order_detail_tax` CHANGE `unit_amount` `unit_amount` DECIMAL(16, 6) NOT NULL DEFAULT '0.000000';
 ALTER TABLE `PREFIX_order_detail_tax` CHANGE `total_amount` `unit_amount` DECIMAL(16, 6) NOT NULL DEFAULT '0.000000';
+
+ALTER TABLE `PREFIX_customer_message` ADD `read` tinyint(1) NOT NULL default '0' AFTER `private`;
