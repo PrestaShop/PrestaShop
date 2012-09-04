0a41c8466636a1dd18b5d84a8475a802e30710b2
vAugagneur <vincent.augagneur@prestashop.com>
2012-09-04 12:38:09 +0000

    //oups

--- install-dev/data/db_structure.sql
+++ install-dev/data/db_structure.sql
@@ -585,6 +585,7 @@ CREATE TABLE `PREFIX_customer_message` (
   `user_agent` varchar(128) default NULL,
   `date_add` datetime NOT NULL,
   `private` TINYINT NOT NULL DEFAULT  '0',
+  `read` tinyint(1) NOT NULL default '0',
   PRIMARY KEY  (`id_customer_message`),
   KEY `id_customer_thread` (`id_customer_thread`),
   KEY `id_employee` (`id_employee`)
