85f9330df2c44852d78852c8bf03665f536b140d
lLefevre <loic.lefevre@prestashop.com>
2012-09-06 15:57:20 +0000

    [-] BO : theme mobile : #PSCFV-3880 : Now you have 4 options for enable the theme mobile
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@17217 b9a71923-0436-4b27-9f14-aed3839534dd

--- install-dev/upgrade/sql/1.5.0.17.sql
+++ install-dev/upgrade/sql/1.5.0.17.sql
@@ -4,3 +4,5 @@ ALTER TABLE `PREFIX_order_detail_tax` CHANGE `unit_amount` `unit_amount` DECIMAL
 ALTER TABLE `PREFIX_order_detail_tax` CHANGE `total_amount` `unit_amount` DECIMAL(16, 6) NOT NULL DEFAULT '0.000000';
 
 ALTER TABLE `PREFIX_customer_message` ADD `read` tinyint(1) NOT NULL default '0' AFTER `private`;
+
+INSERT INTO `PREFIX_configuration`(`name`, `value`, `date_add`, `date_upd`) VALUES ('PS_ALLOW_MOBILE_DEVICE', '1', NOW(), NOW());
