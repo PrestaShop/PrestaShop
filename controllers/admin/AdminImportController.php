96104ecbf5b9cc5018657a4aacd7dc878c85d27b
lLefevre <loic.lefevre@prestashop.com>
2012-09-10 13:38:53 +0000

    [-] BO : fix #PSCFV-3928
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@17268 b9a71923-0436-4b27-9f14-aed3839534dd

--- controllers/admin/AdminImportController.php
+++ controllers/admin/AdminImportController.php
@@ -2551,10 +2551,7 @@ class AdminImportControllerCore extends AdminController
 				switch ($_FILES['file']['error'])
 				{
 					case UPLOAD_ERR_INI_SIZE:
-						$this->errors[] = Tools::displayError('The uploaded file exceeds the upload_max_filesize directive in php.ini. If your server configuration allows it, you may add a directive in your .htaccess, for example:')
-						.'<br/><a href="'.$this->context->link->getAdminLink('AdminMeta').'" >
-						<code>php_value upload_max_filesize 20M</code> '.
-						Tools::displayError('(click to open "Generators" tab)').'</a>';
+						$this->errors[] = Tools::displayError('The uploaded file exceeds the upload_max_filesize directive in php.ini. If your server configuration allows it, you may add a directive in your .htaccess.');
 						break;
 					case UPLOAD_ERR_FORM_SIZE:
 						$this->errors[] = Tools::displayError('The uploaded file exceeds the post_max_size directive in php.ini.
