ebaec285c8c1773ee2184f0e725ed581c2087766
dMetzger <damien.metzger@prestashop.com>
2012-09-13 15:20:47 +0000

    // Fixed wrong path (I guess)

--- install-dev/upgrade/php/p15015_blockadvertising_extension.php
+++ install-dev/upgrade/php/p15015_blockadvertising_extension.php
@@ -27,9 +27,12 @@
 
 function p15015_blockadvertising_extension()
 {
+	if (!defined('_PS_ROOT_DIR_'))
+		define('_PS_ROOT_DIR_', realpath(INSTALL_PATH.'/../'));
+
 	// Try to update with the extension of the image that exists in the module directory
-	if (file_exists(_PS_MODULE_DIR_.'blockadvertising'))
-		foreach (scandir(_PS_MODULE_DIR_.'blockadvertising') as $file)
+	if (file_exists(_PS_ROOT_DIR_.'modules/blockadvertising'))
+		foreach (scandir(_PS_ROOT_DIR_.'modules/blockadvertising') as $file)
 			if (in_array($file, array('advertising.jpg', 'advertising.gif', 'advertising.png')))
 				Db::getInstance()->execute('
 				REPLACE INTO `'._DB_PREFIX_.'configuration` (name, value)
