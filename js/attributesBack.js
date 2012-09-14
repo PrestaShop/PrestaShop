0b1faa3382478c91c74a640d666eb3145f8c72fd
rGaillard <remi.gaillard@prestashop.com>
2012-09-14 15:45:11 +0000

    [-] BO: Fix weight impact and default combination on combinations generator

--- js/attributesBack.js
+++ js/attributesBack.js
@@ -127,7 +127,7 @@ function create_attribute_row(id, id_group, name, price, weight)
 	html += 		'<td><input type="hidden" value="'+id+'" name="options['+id_group+']['+id+']" />'+name+'</td>';
 	html += 		'<td>'+i18n_tax_exc+'<input id="related_to_price_impact_ti_'+id+'" class="price_impact" style="width:50px" type="text" value="'+price+'" name="price_impact_'+id+'" onkeyup="calcPrice($(this), false)"></td>';
 	html += 		'<td>'+i18n_tax_inc+'<input id="related_to_price_impact_'+id+'" class="price_impact_ti" style="width:50px" type="text" value="" name="price_impact_ti_'+id+'" onkeyup="calcPrice($(this), true)"></td>';
-	html += 		'<td><input style="width:50px" type="text" value="'+weight+'" name="weight_impact['+id+']"></td>';
+	html += 		'<td><input style="width:50px" type="text" value="'+weight+'" name="weight_impact_'+id+'"></td>';
 	html += '</tr>';
 
 	return html;
