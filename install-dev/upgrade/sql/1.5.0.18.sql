022c1c6cdbd76b648e09113baf863c2e28c495f5
rGaillard <remi.gaillard@prestashop.com>
2012-09-19 09:03:47 +0000

    // Fix #PSCFV-4183

--- install-dev/upgrade/sql/1.5.0.18.sql
+++ install-dev/upgrade/sql/1.5.0.18.sql
@@ -2,4 +2,5 @@ SET NAMES 'utf8';
 
 ALTER TABLE `PREFIX_image_shop` ADD `cover` TINYINT(1) UNSIGNED NOT NULL AFTER `id_shop`;
 ALTER TABLE `PREFIX_image_shop` DROP PRIMARY KEY;
-ALTER TABLE `PREFIX_image_shop` ADD  ADD INDEX (`id_image`, `id_shop`, `cover`);
+ALTER TABLE `PREFIX_image_shop` ADD INDEX (`id_image`, `id_shop`, `cover`);
+UPDATE `PREFIX_image_shop` image_shop SET image_shop.`cover`=1 WHERE `id_image` IN (SELECT `id_image` FROM `PREFIX_image` i WHERE i.`cover`=1);
