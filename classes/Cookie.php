f1156969c0041f3d2213d41892f708e12fec94d8
rGaillard <remi.gaillard@prestashop.com>
2012-09-24 12:02:17 +0000

    // FIX #PSCFV-4302
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@17497 b9a71923-0436-4b27-9f14-aed3839534dd

--- classes/Cookie.php
+++ classes/Cookie.php
@@ -82,7 +82,10 @@ class CookieCore
 	protected function getDomain($shared_urls = null)
 	{
 		$r = '!(?:(\w+)://)?(?:(\w+)\:(\w+)@)?([^/:]+)?(?:\:(\d*))?([^#?]+)?(?:\?([^#]+))?(?:#(.+$))?!i';
-		preg_match ($r, Tools::getHttpHost(false, false), $out);
+
+		if (!preg_match ($r, Tools::getHttpHost(false, false), $out) || !isset($out[4]))
+			return false;
+
 		if (preg_match('/^(((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]{1}[0-9]|[1-9]).)'.
 			'{1}((25[0-5]|2[0-4][0-9]|[1]{1}[0-9]{2}|[1-9]{1}[0-9]|[0-9]).)'.
 			'{2}((25[0-5]|2[0-4][0-9]|[1]{1}[0-9]{2}|[1-9]{1}[0-9]|[0-9]){1}))$/', $out[4]))
