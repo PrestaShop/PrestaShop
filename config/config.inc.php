5f214dabfa199653e35378b82c062ee457376e07
rGaillard <remi.gaillard@prestashop.com>
2012-09-24 12:02:17 +0000

    // FIX #PSCFV-4302

--- config/config.inc.php
+++ config/config.inc.php
@@ -71,13 +71,18 @@ if (_PS_DEBUG_PROFILING_)
 /* Redefine REQUEST_URI if empty (on some webservers...) */
 if (!isset($_SERVER['REQUEST_URI']) || empty($_SERVER['REQUEST_URI']))
 {
-	if (basename($_SERVER['SCRIPT_NAME']) == 'index.php' && empty($_SERVER['QUERY_STRING']))
-		$_SERVER['REQUEST_URI'] = dirname($_SERVER['SCRIPT_NAME']).'/';
-	else
+	if (!isset($_SERVER['SCRIPT_NAME']) && isset($_SERVER['SCRIPT_FILENAME']))
+		$_SERVER['SCRIPT_NAME'] = $_SERVER['SCRIPT_FILENAME'];
+	if (isset($_SERVER['SCRIPT_NAME']))
 	{
-		$_SERVER['REQUEST_URI'] = $_SERVER['SCRIPT_NAME'];
-		if (isset($_SERVER['QUERY_STRING']) && !empty($_SERVER['QUERY_STRING']))
-			$_SERVER['REQUEST_URI'] .= '?'.$_SERVER['QUERY_STRING'];
+		if (basename($_SERVER['SCRIPT_NAME']) == 'index.php' && empty($_SERVER['QUERY_STRING']))
+			$_SERVER['REQUEST_URI'] = dirname($_SERVER['SCRIPT_NAME']).'/';
+		else
+		{
+			$_SERVER['REQUEST_URI'] = $_SERVER['SCRIPT_NAME'];
+			if (isset($_SERVER['QUERY_STRING']) && !empty($_SERVER['QUERY_STRING']))
+				$_SERVER['REQUEST_URI'] .= '?'.$_SERVER['QUERY_STRING'];
+		}
 	}
 }
 
