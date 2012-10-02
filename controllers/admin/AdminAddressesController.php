0535b473ed4370dfbe981e7ac59df5afeff1d9c7
vAugagneur <vincent.augagneur@prestashop.com>
2012-10-02 08:36:23 +0000

    //
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@17672 b9a71923-0436-4b27-9f14-aed3839534dd

--- controllers/admin/AdminAddressesController.php
+++ controllers/admin/AdminAddressesController.php
@@ -71,7 +71,6 @@ class AdminAddressesControllerCore extends AdminController
 			LEFT JOIN `'._DB_PREFIX_.'country_lang` cl ON (cl.`id_country` = a.`id_country` AND cl.`id_lang` = '.(int)$this->context->language->id.')
 			LEFT JOIN `'._DB_PREFIX_.'customer` c ON a.id_customer = c.id_customer
 		';
-
 		$this->_where = 'AND a.id_customer != 0 '.Shop::addSqlRestriction(Shop::SHARE_CUSTOMER, 'c');
 
 		return parent::renderList();
