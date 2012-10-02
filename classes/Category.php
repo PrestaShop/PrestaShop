ed625e6cefff0c1e040d6984df71781b0a73979e
vAugagneur <vincent.augagneur@prestashop.com>
2012-10-02 07:59:44 +0000

    [-] BO : fixed bug #PSCFV-4472

--- classes/Category.php
+++ classes/Category.php
@@ -778,14 +778,14 @@ class CategoryCore extends ObjectModel
 					)' : '0').' AS nbSelectedSubCat
 				FROM `'._DB_PREFIX_.'category` c
 				LEFT JOIN `'._DB_PREFIX_.'category_lang` cl ON (c.`id_category` = cl.`id_category` '.Shop::addSqlRestrictionOnLang('cl', $id_shop).')';
-		if (Shop::getContext() == Shop::CONTEXT_SHOP && $use_shop_context)
-			$sql .= ' LEFT JOIN `'._DB_PREFIX_.'category_shop` cs ON (c.`id_category` = cs.`id_category` AND cs.`id_shop` = '.(int)$id_shop.')';
+		$sql .= ' LEFT JOIN `'._DB_PREFIX_.'category_shop` cs ON (c.`id_category` = cs.`id_category` AND cs.`id_shop` = '.(int)$id_shop.')';
 		$sql .= ' WHERE `id_lang` = '.(int)$id_lang;
 		if (Shop::getContext() == Shop::CONTEXT_SHOP && $use_shop_context)
 			$sql .= ' AND cs.`id_shop` = '.(int)$shop->id;
 		$sql .= ' AND c.`id_parent` = '.(int)$id_parent;
-		if (Shop::getContext() == Shop::CONTEXT_SHOP && $use_shop_context)
+		if (!Shop::isFeatureActive() || Shop::getContext() == Shop::CONTEXT_SHOP && $use_shop_context)
 			$sql .= ' ORDER BY cs.`position` ASC';
+
 		return Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($sql);
 	}
 
