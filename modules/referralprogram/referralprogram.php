dea775261c6d18341f4ef43096a67f51beaab21d
dMetzger <damien.metzger@prestashop.com>
2012-10-03 12:17:40 +0000

    [-] MO : fixed link in referral program #PNM-532

--- modules/referralprogram/referralprogram.php
+++ modules/referralprogram/referralprogram.php
@@ -34,7 +34,7 @@ class ReferralProgram extends Module
 	{
 		$this->name = 'referralprogram';
 		$this->tab = 'advertising_marketing';
-		$this->version = '1.5';
+		$this->version = '1.5.1';
 		$this->author = 'PrestaShop';
 
 		parent::__construct();
