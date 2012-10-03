f25b983a77270e8f8c92a06d7c2267995d0a48da
rGaillard <remi.gaillard@prestashop.com>
2012-10-03 16:04:33 +0000

    // Fix #PSCFV-4679

--- classes/AttributeGroup.php
+++ classes/AttributeGroup.php
@@ -45,7 +45,7 @@ class AttributeGroupCore extends ObjectModel
 		'multilang' => true,
 		'fields' => array(
 			'is_color_group' => array('type' => self::TYPE_BOOL, 'validate' => 'isBool'),
-			'group_type' => 	array('type' => self::TYPE_STRING),
+			'group_type' => 	array('type' => self::TYPE_STRING, 'required' => true),
 			'position' => 		array('type' => self::TYPE_INT, 'validate' => 'isInt'),
 
 			// Lang fields
