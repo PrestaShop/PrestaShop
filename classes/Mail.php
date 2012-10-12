9ae97271974798c5c77df92267f28c374061cea2
vAugagneur <vincent.augagneur@prestashop.com>
2012-10-12 14:59:26 +0000

    [-] CORE : fixed bug #PSCFV-4595
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@17850 b9a71923-0436-4b27-9f14-aed3839534dd

--- classes/Mail.php
+++ classes/Mail.php
@@ -234,6 +234,9 @@ class MailCore
 
 			$template_vars['{shop_name}'] = Tools::safeOutput(Configuration::get('PS_SHOP_NAME'));
 			$template_vars['{shop_url}'] = Tools::getShopDomain(true, true).__PS_BASE_URI__.'index.php';
+			$template_vars['{my_account_url}'] = Context::getContext()->link->getPageLink('my-account', true, Context::getContext()->language->id);
+			$template_vars['{guest_tracking_url}'] = Context::getContext()->link->getPageLink('guest-tracking', true, Context::getContext()->language->id);
+			$template_vars['{history_url}'] = Context::getContext()->link->getPageLink('history', true, Context::getContext()->language->id);
 			$swift->attachPlugin(new Swift_Plugin_Decorator(array($to_plugin => $template_vars)), 'decorator');
 			if ($configuration['PS_MAIL_TYPE'] == Mail::TYPE_BOTH || $configuration['PS_MAIL_TYPE'] == Mail::TYPE_TEXT)
 				$message->attach(new Swift_Message_Part($template_txt, 'text/plain', '8bit', 'utf-8'));
