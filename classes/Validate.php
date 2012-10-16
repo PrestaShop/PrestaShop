2f4df32eaf20b7406190901ed76d92714c5ec284
lLefevre <loic.lefevre@prestashop.com>
2012-10-16 09:52:07 +0000

    [-] BO : fix #PSCFV-4053

--- classes/Validate.php
+++ classes/Validate.php
@@ -366,7 +366,7 @@ class ValidateCore
 	 */
 	public static function isGenericName($name)
 	{
-		return empty($name) || preg_match('/^[^<>;=#{}]*$/u', $name);
+		return empty($name) || preg_match('/^[^<>=#{}]*$/u', $name);
 	}
 
 	/**
