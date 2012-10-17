0320d9701278909e9c1e33ccb4db8054f2878870
lLefevre <loic.lefevre@prestashop.com>
2012-10-17 09:35:11 +0000

    // #PSCFV-5017 : update bad licenses in all files
    git-svn-id: http://dev.prestashop.com/svn/v1/branches/1.5.x@17891 b9a71923-0436-4b27-9f14-aed3839534dd

--- override/controllers/index.php
+++ override/controllers/index.php
@@ -4,10 +4,10 @@
 *
 * NOTICE OF LICENSE
 *
-* This source file is subject to the Open Software License (OSL 3.0)
+* This source file is subject to the Academic Free License (AFL 3.0)
 * that is bundled with this package in the file LICENSE.txt.
 * It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/osl-3.0.php
+* http://opensource.org/licenses/afl-3.0.php
 * If you did not receive a copy of the license and are unable to
 * obtain it through the world-wide-web, please send an email
 * to license@prestashop.com so we can send you a copy immediately.
@@ -21,7 +21,7 @@
 *  @author PrestaShop SA <contact@prestashop.com>
 *  @copyright  2007-2012 PrestaShop SA
 *  @version  Release: $Revision$
-*  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
 			    	
