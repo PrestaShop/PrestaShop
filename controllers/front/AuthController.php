bfb52fbfbc286b7a790079e6e6f77a42b61bb96c
vAugagneur <vincent.augagneur@prestashop.com>
2012-10-17 10:02:18 +0000

    [-] FO : fixed bug #PSCFV-4950

--- controllers/front/AuthController.php
+++ controllers/front/AuthController.php
@@ -582,7 +582,7 @@ class AuthControllerCore extends FrontController
 							if (!Configuration::get('PS_REGISTRATION_PROCESS_TYPE') && !$this->ajax && !Tools::isSubmit('submitGuestAccount'))
 								Tools::redirect('index.php?controller=address');
 							if ($back = Tools::getValue('back'))
-								Tools::redirect('index.php?controller='.$back);
+								Tools::redirect($back);
 							Tools::redirect('index.php?controller=my-account');
 							// redirection: if cart is not empty : redirection to the cart
 							if (count($this->context->cart->getProducts(true)) > 0)
