f3fc050fa42c05470cac06dba0cd524aa5d0f6e5
rGaillard <remi.gaillard@prestashop.com>
2012-10-29 11:08:19 +0000

    // display_errors and sql debug is automatically set to on when dev mode is activated

--- config/config.inc.php
+++ config/config.inc.php
@@ -26,8 +26,17 @@
 */
 
 /* Debug only */
-@ini_set('display_errors', 'on');
-define('_PS_DEBUG_SQL_', true);
+define('_PS_MODE_DEV_', true);
+if (_PS_MODE_DEV_)
+{
+	@ini_set('display_errors', 'on');	
+	define('_PS_DEBUG_SQL_', true);
+}
+else
+{
+	@ini_set('display_errors', 'off');
+	define('_PS_DEBUG_SQL_', false);
+}
 
 $start_time = microtime(true);
 
