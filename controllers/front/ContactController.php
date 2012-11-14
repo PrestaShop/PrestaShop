4d2ebaa0f572bace4866e0cf482d80d93d23664f
dMetzger <damien.metzger@prestashop.com>
2012-11-14 14:54:36 +0000

    [-] FO : replaced order id by order reference in the contact form (but kept the id_order for retrocompatibility) #PSCFV-5491

--- controllers/front/ContactController.php
+++ controllers/front/ContactController.php
@@ -280,7 +280,6 @@ class ContactControllerCore extends FrontController
 				$tmp = $order->getProducts();
 				foreach ($tmp as $key => $val)
 					$products[$row['id_order']][$val['product_id']] = array('value' => $val['product_id'], 'label' => $val['product_name']);
-					p(Tools::getValue('id_order'));
 				$order_tab[] = array('value' => $order->id, 'label' => $order->getUniqReference().' - '.Tools::displayDate($date[0], $this->context->language->id), 'selected' => (int)Tools::getValue('id_order') == $order->id);
 			}
 
