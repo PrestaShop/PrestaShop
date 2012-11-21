ac1bcdf674ec7ad1e218fc65f6f93ea7e6b369b9
Damon Skelhorn <damon@eighty-six.co.uk>
2012-11-21 17:05:08 +0000

    Revert "Bug #PSCFV-5626 - Amended SQL query to correctly sum the totals for each day."
    This reverts commit b52dda80a041be057a95ca0313a1873477fda063.

--- controllers/admin/AdminHomeController.php
+++ controllers/admin/AdminHomeController.php
@@ -374,15 +374,15 @@ class AdminHomeControllerCore extends AdminController
 
 		$chart = new Chart();
 		$chart->getCurve(1)->setType('bars');
-		$result = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS('SELECT sum(total_paid / conversion_rate) as total_converted, left(invoice_date, 10) as trimmed_invoice_date
+		$result = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS('
+			SELECT total_paid / conversion_rate as total_converted, left(invoice_date, 10) as invoice_date
 			FROM '._DB_PREFIX_.'orders o
 			WHERE valid = 1
 				AND invoice_date BETWEEN \''.date('Y-m-d', strtotime('-7 DAYS', time())).' 00:00:00\' AND \''.date('Y-m-d H:i:s').'\'
 				'.Shop::addSqlRestriction(Shop::SHARE_ORDER).'
-				GROUP BY trimmed_invoice_date
 		');
 		foreach ($result as $row)
-			$chart->getCurve(1)->setPoint(strtotime($row['trimmed_invoice_date'].' 02:00:00'), $row['total_converted']);
+			$chart->getCurve(1)->setPoint(strtotime($row['invoice_date'].' 02:00:00'), $row['total_converted']);
 		$chart->setSize(580, 170);
 		$chart->setTimeMode(strtotime('-7 DAYS', time()), time(), 'd');
 		$currency = Tools::setCurrency($this->context->cookie);
