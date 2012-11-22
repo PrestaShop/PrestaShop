be946319beef9a14c660c42df9e837f58a594456
vAugagneur <vincent.augagneur@prestashop.coom>
2012-11-22 11:57:40 +0100

    //remove svn keyword @version  Release: $

--- modules/blocklayered/blocklayered-ajax-back.php
+++ modules/blocklayered/blocklayered-ajax-back.php
@@ -19,8 +19,7 @@
 * needs please refer to http://www.prestashop.com for more information.
 *
 *  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2012 PrestaShop SA
-*  @version  Release: $Revision$
+*  @copyright  2007-2012 PrestaShop SA
 *  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registred Trademark & Property of PrestaShop SA
 */
