131f99eb693490d5d4754c37a4ff47075d306f19
Rémi Gaillard <remi.gaillard@prestashop.com>
2012-12-10 15:57:59 +0100

    [-] Core: send email when payment is remotly accepted #PSCFV-6064

--- install-dev/upgrade/sql/1.5.2.1.sql
+++ install-dev/upgrade/sql/1.5.2.1.sql
@@ -11,3 +11,8 @@ UPDATE `PREFIX_order_state` SET `delivery` = 0 WHERE `id_order_state` = 3;
 ALTER TABLE  `PREFIX_product_shop` ADD `id_product_redirected` int(10) unsigned NOT NULL default '0' AFTER `active` , ADD `available_for_order` tinyint(1) NOT NULL default '1' AFTER `id_product_redirected`;
 
 ALTER TABLE  `PREFIX_product` ADD `id_product_redirected` int(10) unsigned NOT NULL default '0' AFTER `active` , ADD `available_for_order` tinyint(1) NOT NULL default '1' AFTER `id_product_redirected`;
+
+UPDATE `PREFIX_order_state` SET `send_email` = 1 WHERE `id_order_state` = (SELECT `value` FROM `PREFIX_configuration` WHERE `name` = 'PS_OS_WS_PAYMENT' LIMIT 1);
+
+UPDATE `PREFIX_order_state_lang` SET `template` = 'payment' WHERE `id_order_state` = (SELECT `value` FROM `PREFIX_configuration` WHERE `name` = 'PS_OS_WS_PAYMENT' LIMIT 1);
+
