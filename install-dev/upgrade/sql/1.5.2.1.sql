843345c4db1acb50766cfdb0ea37576ecc7c4b04
PrestaEdit <j.danse@prestaedit.com>
2012-12-12 22:56:22 +0100

    [+] Installer: add id_product_download

--- install-dev/upgrade/sql/1.5.2.1.sql
+++ install-dev/upgrade/sql/1.5.2.1.sql
@@ -1,7 +1,7 @@
 SET NAMES 'utf8';
 
 ALTER TABLE `PREFIX_address` CHANGE  `outstanding_allow_amount` `outstanding_allow_amount` DECIMAL(20, 6) NOT NULL DEFAULT '0.000000';
-
+ALTER TABLE `PREFIX_product_download` ADD `id_product_attribute` INT( 10 ) NOT NULL AFTER `id_product`;
 /* PHP:blocknewsletter1530(); */;
 
 /* PHP:block_category_1521(); */;
