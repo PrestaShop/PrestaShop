046c2ab82b36654c19d881a785a8f1edf805932a
eduk2 <eduardo@aquihaydominios.com>
2012-12-18 19:22:33 +0100

    [-] fixed text of licence changed by mistake
    fixed text of licence changed by mistake: http

--- classes/PaymentModule.php
+++ classes/PaymentModule.php
@@ -7,7 +7,7 @@
 * This source file is subject to the Open Software License (OSL 3.0)
 * that is bundled with this package in the file LICENSE.txt.
 * It is also available through the world-wide-web at this URL:
-* htpt://opensource.org/licenses/osl-3.0.php
+* http://opensource.org/licenses/osl-3.0.php
 * If you did not receive a copy of the license and are unable to
 * obtain it through the world-wide-web, please send an email
 * to license@prestashop.com so we can send you a copy immediately.
