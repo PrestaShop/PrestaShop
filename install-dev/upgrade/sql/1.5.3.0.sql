2e38587070f7cdf5fd9e029fda7903a1ef77531c
vAugagneur <vincent.augagneur@prestashop.com>
2012-12-28 16:08:21 +0100

    //small fix in upgrade

--- install-dev/upgrade/sql/1.5.3.0.sql
+++ install-dev/upgrade/sql/1.5.3.0.sql
@@ -1,6 +1,6 @@
 SET NAMES 'utf8';
 
-ALTER TABLE `PREFIX_address` CHANGE  `outstanding_allow_amount` `outstanding_allow_amount` DECIMAL(20, 6) NOT NULL DEFAULT '0.000000';
+/* PHP:outstanding_allow_amount1530(); */;
 
 /* PHP:blocknewsletter1530(); */;
 
