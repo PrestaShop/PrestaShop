d8feb5dba8ae429762d38c1d770eb400da46fb9e
Damien Metzger <damien.metzger@prestashop.com>
2012-12-31 15:17:35 +0100

    // Fix upgrade

--- install-dev/upgrade/sql/1.5.3.1.sql
+++ install-dev/upgrade/sql/1.5.3.1.sql
@@ -1,4 +1,5 @@
-ALTER TABLE  `PREFIX_product` ADD `redirect_type` ENUM('', '404', '301', '302') NOT NULL DEFAULT '404' AFTER `active`;
-ALTER TABLE  `PREFIX_product_shop` ADD `redirect_type` ENUM('', '404', '301', '302') NOT NULL DEFAULT '404' AFTER `active`;
+SET NAMES 'utf8';
+
+/* PHP:p1531_redirect_type(); */;
 
 ALTER TABLE `PREFIX_cart` CHANGE `delivery_option` `delivery_option` TEXT NOT NULL;
