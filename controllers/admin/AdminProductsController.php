f7754045fbc246835001cba8a9230447eba1d53f
vAugagneur <vincent.augagneur@prestashop.com>
2013-01-02 17:09:39 +0100

    [-] BO : fixed bug #PSCFV-6130 - Wrong currency in product specific prices administration

--- controllers/admin/AdminProductsController.php
+++ controllers/admin/AdminProductsController.php
@@ -3084,7 +3084,7 @@ class AdminProductsControllerCore extends AdminController
 						<td class="cell border">'.($id_shop_sp ? $shops[$id_shop_sp]['name'] : $this->l('All shops')).'</td>';
 					}
 					$price = Tools::ps_round($specific_price['price'], 2);
-					$fixed_price = ($price == Tools::ps_round($obj->price, 2) || $specific_price['price'] == -1) ? '--' : Tools::displayPrice($price);
+					$fixed_price = ($price == Tools::ps_round($obj->price, 2) || $specific_price['price'] == -1) ? '--' : Tools::displayPrice($price, $current_specific_currency);
 					$content .= '
 						<td class="cell border">'.($specific_price['id_currency'] ? $currencies[$specific_price['id_currency']]['name'] : $this->l('All currencies')).'</td>
 						<td class="cell border">'.($specific_price['id_country'] ? $countries[$specific_price['id_country']]['name'] : $this->l('All countries')).'</td>
