f66da4a6f8e5e7adbce46f03982c122aef0456f9
Damien Metzger <damien.metzger@prestashop.com>
2013-01-16 14:43:54 +0100

    [-] BO : fixed quick access for upgrade from 1.4 #PSCFV-6370

--- install-dev/upgrade/sql/1.5.4.0.sql
+++ install-dev/upgrade/sql/1.5.4.0.sql
@@ -8,7 +8,5 @@ ALTER TABLE `PREFIX_customer` ADD `id_lang` INT UNSIGNED NULL AFTER `id_default_
 UPDATE `PREFIX_customer` SET id_lang = (SELECT `value` FROM `PREFIX_configuration` WHERE name = 'PS_LANG_DEFAULT' LIMIT 1);
 UPDATE `PREFIX_customer` c, `PREFIX_orders` o SET c.id_lang = o.id_lang WHERE c.id_customer = o.id_customer;
 
-
-
-
+UPDATE `PREFIX_quick_access` SET `link` = 'index.php?controller=AdminCartRules&addcart_rule' WHERE `link` = 'index.php?tab=AdminDiscounts&adddiscount';
 
