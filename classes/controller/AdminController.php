c80e0f48be5a40da11cd8422f13fa82aa09a452d
Rémi Gaillard <remi.gaillard@prestashop.com>
2013-02-05 15:28:09 +0100

    [-] BO: Don't display delete icon for order carts

--- classes/controller/AdminController.php
+++ classes/controller/AdminController.php
@@ -175,6 +175,9 @@ class AdminControllerCore extends Controller
 
 	/** @var array required_fields to display in the Required Fields form */
 	public $required_fields = array();
+	
+	/** @var Helper */
+	protected $helper;
 
 	/**
 	 * @var array actions to execute on multiple selections
@@ -1635,6 +1638,8 @@ class AdminControllerCore extends Controller
 
 		// For each action, try to add the corresponding skip elements list
 		$helper->list_skip_actions = $this->list_skip_actions;
+		
+		$this->helper = $helper;
 	}
 
 	public function setMedia()
