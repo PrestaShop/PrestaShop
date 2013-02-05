b02369bb4f568869c3c087b69721761696525530
DevNet <git@dev-net.fr>
2013-02-05 11:25:11 +0100

    Update controllers/admin/AdminTranslationsController.php
    Don't forget the quote/doublequote for optional params

--- controllers/admin/AdminTranslationsController.php
+++ controllers/admin/AdminTranslationsController.php
@@ -1070,7 +1070,7 @@ class AdminTranslationsControllerCore extends AdminController
 					if ($type_file == 'php')
 						$regex = '/->l\(\''._PS_TRANS_PATTERN_.'\'(, ?\'(.+)\')?(, ?(.+))?\)/U';
 					else
-						$regex = '/\{l\s*s=[\'\"]'._PS_TRANS_PATTERN_.'[\'\"](\s*sprintf=.*)?(\s*mod=\'.+\')?(\s*js=1)?\s*\}/U';
+						$regex = '/\{l\s*s=[\'\"]'._PS_TRANS_PATTERN_.'[\'\"](\s*sprintf=.*)?(\s*mod=[\'\"].+[\'\"])?(\s*js=1)?\s*\}/U';
 				break;
 
 			case 'pdf':
@@ -1078,7 +1078,7 @@ class AdminTranslationsControllerCore extends AdminController
 					if ($type_file == 'php')
 						$regex = '/HTMLTemplate.*::l\(\''._PS_TRANS_PATTERN_.'\'[\)|\,]/U';
 					else
-						$regex = '/\{l\s*s=[\'\"]'._PS_TRANS_PATTERN_.'[\'\"](\s*sprintf=.*)?(\s*js=1)?(\s*pdf=\'true\')?\s*\}/U';
+						$regex = '/\{l\s*s=[\'\"]'._PS_TRANS_PATTERN_.'[\'\"](\s*sprintf=.*)?(\s*js=1)?(\s*pdf=[\'\"]true[\'\"])?\s*\}/U';
 				break;
 		}
 
