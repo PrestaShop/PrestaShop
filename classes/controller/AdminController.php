6d18f2e7f4644878d66fec7409febc22a5d35290
vAugagneur <vincent.augagneur@prestashop.com>
2013-02-07 10:46:31 +0100

    //small fix

--- classes/controller/AdminController.php
+++ classes/controller/AdminController.php
@@ -1418,7 +1418,7 @@ class AdminControllerCore extends Controller
 		$this->tab_modules_list = Tab::getTabModulesList($this->id);
 
 		if (is_array($this->tab_modules_list['default_list']) && count($this->tab_modules_list['default_list']))
-			$this->filter_modules_list = $tab_modules_list['default_list'];	
+			$this->filter_modules_list = $this->tab_modules_list['default_list'];	
 		else if (is_array($this->tab_modules_list['slider_list']) && count($this->tab_modules_list['slider_list']))
 		{
 			$this->addToolBarModulesListButton();
