cdde16496e731eaf8564f185769879eca4536c3e
Rémi Gaillard <remi.gaillard@prestashop.com>
2013-02-07 16:06:32 +0100

    [-] FO: Fix #PSCFV-7627 customer group reduction was applied on combinations for unidentified users on the product page

--- classes/Group.php
+++ classes/Group.php
@@ -106,10 +106,10 @@ class GroupCore extends ObjectModel
 	public static function getReduction($id_customer = null)
 	{
 		if (!isset(self::$cache_reduction['customer'][(int)$id_customer]))
-        {
-            $id_group = $id_customer ? Customer::getDefaultGroupId((int)$id_customer) : (int)Configuration::get('PS_CUSTOMER_GROUP');
-			self::$cache_reduction['customer'][(int)$id_customer] = Group::getReductionByIdGroup($id_group);
-        }
+		{
+				$id_group = $id_customer ? Customer::getDefaultGroupId((int)$id_customer) : (int)Group::getCurrent()->id;
+				self::$cache_reduction['customer'][(int)$id_customer] = Group::getReductionByIdGroup($id_group);
+		}
 		return self::$cache_reduction['customer'][(int)$id_customer];
 	}
 
