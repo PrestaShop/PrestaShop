36b856c686e4d99048f5c69f0a794294b80f59d8
gRoussac <gregory.roussac@prestashop.com>
2013-02-26 19:24:07 +0100

    [*] CORE: Dissasemble CURLOPT_CONNECTTIMEOUT and CURLOPT_TIMEOUT in Tools::file_get_contents

--- classes/Tools.php
+++ classes/Tools.php
@@ -1286,7 +1286,7 @@ class ToolsCore
 			$curl = curl_init();
 			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($curl, CURLOPT_URL, $url);
-			curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $curl_timeout);
+			curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
 			curl_setopt($curl, CURLOPT_TIMEOUT, $curl_timeout);
 			curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);			
 			$opts = stream_context_get_options($stream_context);
