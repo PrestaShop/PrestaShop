778a57d9396d8929222df402b93d575427d6d946
fram <fm.de.jouvencel@gmail.com>
2013-02-27 16:19:43 +0100

    // fixed translations in English pack

--- modules/blocksearch/translations/en.php
+++ modules/blocksearch/translations/en.php
@@ -2,3 +2,5 @@
 
 global $_MODULE;
 $_MODULE = array();
+$_MODULE['<{blocksearch}prestashop>blocksearch-top_24c29702dc9c63dedb98e92f3028e068'] = 'Search';
+$_MODULE['<{blocksearch}prestashop>blocksearch_24c29702dc9c63dedb98e92f3028e068'] = 'Search';
--- translations/en/admin.php
+++ translations/en/admin.php
@@ -2,3 +2,10 @@
 
 global $_LANGADM;
 $_LANGADM = array();
+$_LANGADM['AdminCustomerThreads6a1e265f92087bb6dd18194833fe946b'] = 'Email';
+$_LANGADM['AdminEmails6a1e265f92087bb6dd18194833fe946b'] = 'Email';
+$_LANGADM['AdminModules24c29702dc9c63dedb98e92f3028e068'] = 'Search';
+$_LANGADM['AdminReferrers24c29702dc9c63dedb98e92f3028e068'] = 'Search';
+$_LANGADM['AdminSearchConf24c29702dc9c63dedb98e92f3028e068'] = 'Search';
+
+?>
