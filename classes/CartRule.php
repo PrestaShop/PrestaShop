0256ee7c59a88f3ecd1dfd097a8223241465ff0e
gRoussac <gregory.roussac@prestashop.com>
2013-04-12 18:03:38 +0200

    [-] FO : add cart rul when cart rul has several groups

--- classes/CartRule.php
+++ classes/CartRule.php
@@ -1110,7 +1110,7 @@ class CartRuleCore extends ObjectModel
 				FROM '._DB_PREFIX_.'customer_group cg
 				LEFT JOIN '._DB_PREFIX_.'cart_rule_group crg ON cg.id_group = crg.id_group
 				WHERE cr.id_cart_rule = crg.id_cart_rule
-				AND cg.id_customer = '.(int)$context->customer->id.'
+				AND cg.id_customer = '.(int)$context->customer->id.' LIMIT 1
 			)' : '').'
 		)
 		AND (
