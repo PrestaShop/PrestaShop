d16f30f32235e859c6e66490784e79e6a5c2fce9
Damien Metzger <damien.metzger@prestashop.com>
2013-04-23 20:25:28 +0200

    // Fix module translation in the core

--- controllers/admin/AdminTranslationsController.php
+++ controllers/admin/AdminTranslationsController.php
@@ -827,7 +827,7 @@ class AdminTranslationsControllerCore extends AdminController
 					}
 					else
 					{
-						$post_key = md5(strtolower($module_name).'_'.self::DEFAULT_THEME_NAME.'_'.strtolower($template_name).'_'.md5($key));
+						$post_key = md5(strtolower($module_name).'_'.strtolower($template_name).'_'.md5($key));
 						$pattern = '\'<{'.strtolower($module_name).'}prestashop>'.strtolower($template_name).'_'.md5($key).'\'';
 					}
 
