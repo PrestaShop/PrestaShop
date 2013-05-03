abcb083cb361e8293c89a9a667fbd0c89be427dc
Rémi Gaillard <remi.gaillard@prestashop.com>
2013-05-03 17:01:12 +0200

    [-] BO: You can now disable the email sent after account creation

--- install-dev/upgrade/sql/1.5.5.0.sql
+++ install-dev/upgrade/sql/1.5.5.0.sql
@@ -1,3 +1,5 @@
 SET NAMES 'utf8';
 
 ALTER TABLE  `PREFIX_store` CHANGE  `latitude`  `latitude` DECIMAL( 13, 8 ) NULL DEFAULT NULL , CHANGE  `longitude`  `longitude` DECIMAL( 13, 8 ) NULL DEFAULT NULL ;
+
+INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VALUES('PS_CUSTOMER_CREATION_EMAIL', 1, NOW(), NOW());
