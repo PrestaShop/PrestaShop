c097697c4d8e1156588e80d88f034dbb07b97dfd
gRoussac <gregory.roussac@prestashop.com>
2013-05-15 15:18:13 +0200

    [-] INSTALLER : Fix bug #PSCFI-7075 too short column in PREFIX_webservice_account

--- install-dev/upgrade/sql/1.5.5.0.sql
+++ install-dev/upgrade/sql/1.5.5.0.sql
@@ -3,3 +3,6 @@ SET NAMES 'utf8';
 ALTER TABLE  `PREFIX_store` CHANGE  `latitude`  `latitude` DECIMAL( 13, 8 ) NULL DEFAULT NULL , CHANGE  `longitude`  `longitude` DECIMAL( 13, 8 ) NULL DEFAULT NULL ;
 
 INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VALUES('PS_CUSTOMER_CREATION_EMAIL', 1, NOW(), NOW());
+
+ALTER TABLE `PREFIX_webservice_account` CHANGE `class_name` `class_name` VARCHAR(64) NOT NULL DEFAULT 'WebserviceRequest',
+CHANGE `module_name` `module_name` VARCHAR(64) NULL DEFAULT NULL;
