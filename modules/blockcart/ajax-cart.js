57dd992c28d70f1899fc577fa0133da846acba9d
Damien Metzger <damien.metzger@prestashop.com>
2013-06-26 16:22:57 +0200

    [-] FO : fixed entities issue in javascript alerts #PSCFV-9001

--- modules/blockcart/ajax-cart.js
+++ modules/blockcart/ajax-cart.js
@@ -568,10 +568,10 @@ var ajaxCart = {
 		if (jsonData.hasError)
 		{
 			var errors = '';
-			for(error in jsonData.errors)
+			for (error in jsonData.errors)
 				//IE6 bug fix
-				if(error != 'indexOf')
-					errors += jsonData.errors[error] + "\n";
+				if (error != 'indexOf')
+					errors += $('<div />').html(jsonData.errors[error]).text() + "\n";
 			alert(errors);
 		}
 		else
