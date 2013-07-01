c7325f348d88337112d60e8e44ad7bda206b47f8
gRoussac <gregory.roussac@prestashop.com>
2013-07-01 19:23:42 +0200

    (-] INSTALLER Remove errors during upgrade

--- install-dev/upgrade/sql/1.5.0.0.sql
+++ install-dev/upgrade/sql/1.5.0.0.sql
@@ -312,7 +312,8 @@ PRIMARY KEY (`id_scene`, `id_shop`),
 ) ENGINE=ENGINE_TYPE  DEFAULT CHARSET=utf8;
 INSERT INTO `PREFIX_scene_shop` (id_shop, id_scene) (SELECT 1, id_scene FROM PREFIX_scene);
 
-ALTER TABLE `PREFIX_delivery` ADD `id_shop` INT UNSIGNED NULL DEFAULT NULL AFTER `id_delivery`, ADD `id_group_shop` INT UNSIGNED NULL DEFAULT NULL AFTER `id_shop`;
+ALTER TABLE `PREFIX_delivery` ADD `id_shop` INT UNSIGNED NULL DEFAULT NULL AFTER `id_delivery`;
+ALTER TABLE `PREFIX_delivery` ADD `id_group_shop` INT UNSIGNED NULL DEFAULT NULL AFTER `id_shop`;
 
 /* PHP:create_multistore(); */;
 
