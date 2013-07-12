740e8c96e07e03277a3403ff84857d5af55c81e3
Daniel <daniel@prestaworks.se>
2013-07-12 18:24:18 +0800

    PNM-1241 Issue with backbutton
    This will solve the issue with url indexing not working for all languages.

--- modules/blocklayered/blocklayered.php
+++ modules/blocklayered/blocklayered.php
@@ -445,7 +445,10 @@ class BlockLayered extends Module
 		static $_MODULES = array();
 		global $_MODULE;
 
-		$file = _PS_MODULE_DIR_.$this->name.'/'.Language::getIsoById($id_lang).'.php';
+		if (version_compare(_PS_VERSION_,'1.5','>'))
+			$file = _PS_MODULE_DIR_.$this->name.'/translations/'.Language::getIsoById($id_lang).'.php';
+		else
+			$file = _PS_MODULE_DIR_.$this->name.'/'.Language::getIsoById($id_lang).'.php';
 
 		if (!array_key_exists($id_lang, $_MODULES))
 		{
