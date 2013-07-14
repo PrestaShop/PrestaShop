f02949e44d38e0252e29d39b13ca636f38d9bd51
Captain FLAM <davmaps@numericable.fr>
2013-07-14 19:10:55 +0100

    Update 1.5.5.0.sql

--- install-dev/upgrade/sql/1.5.5.0.sql
+++ install-dev/upgrade/sql/1.5.5.0.sql
@@ -7,6 +7,7 @@ INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VAL
 ALTER TABLE `PREFIX_webservice_account` CHANGE `class_name` `class_name` VARCHAR(64) NOT NULL DEFAULT 'WebserviceRequest',
 CHANGE `module_name` `module_name` VARCHAR(64) NULL DEFAULT NULL;
 
+/* PHP:add_accounting_tab(); */;
 /* PHP:add_module_to_hook(blockcart, actionCartListOverride); */;
 /* PHP:add_module_to_hook(blockmanufacturer, actionObjectManufacturerDeleteAfter); */;
 /* PHP:add_module_to_hook(blockmanufacturer, actionObjectManufacturerAddAfter); */;
