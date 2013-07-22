27f90a032a0754ef78dc29c8e36c6046e05e9e42
Captain FLAM <davmaps@numericable.fr>
2013-07-22 21:31:24 +0100

    Update AdminProductsController.php

--- controllers/admin/AdminProductsController.php
+++ controllers/admin/AdminProductsController.php
@@ -192,7 +192,7 @@ class AdminProductsControllerCore extends AdminController
 			$this->_join .= 'LEFT JOIN `'._DB_PREFIX_.'category_lang` cl ON ('.$alias.'.`id_category_default` = cl.`id_category` AND b.`id_lang` = cl.`id_lang` AND cl.id_shop = 1)';
 		}
 
-		$this->_select .= 'MAX('.$alias_image.'.id_image) id_image, cl.name `name_category`, '.$alias.'.`price`, 0 AS price_final, sav.`quantity` as sav_quantity, '.$alias.'.`active`, ';
+		$this->_select .= 'MAX('.$alias_image.'.id_image) id_image, cl.name `name_category`, '.$alias.'.`price`, 0 AS price_final, sav.`quantity` as sav_quantity, '.$alias.'.`active` ';
 		
 		if ($join_category)
 		{
