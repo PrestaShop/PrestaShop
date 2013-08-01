af9397a11b7788cb381a1e752d28f1f06fc11873
Damien Metzger <damien.metzger@prestashop.com>
2013-08-01 10:50:21 +0200

    [-] BO : fixed domain warning

--- controllers/admin/AdminHomeController.php
+++ controllers/admin/AdminHomeController.php
@@ -170,10 +170,16 @@ class AdminHomeControllerCore extends AdminController
 
 		$shop = Context::getContext()->shop;
 		if ($_SERVER['HTTP_HOST'] != $shop->domain && $_SERVER['HTTP_HOST'] != $shop->domain_ssl && Tools::getValue('ajax') == false)
-			$this->displayWarning($this->l('You are currently connected under the following domain name:').' <span style="color: #CC0000;">'.$_SERVER['HTTP_HOST'].'</span><br />'.
-			$this->l('This is different from the main shop domain name set in the "Multistore" page under the "Advanced Parameters" menu:').' <span style="color: #CC0000;">'.$shop->domain.'</span><br />
-			<a href="index.php?controller=AdminMeta&token='.Tools::getAdminTokenLite('AdminMeta').'#conf_id_domain">'.
-			$this->l('Click here if you want to modify your main shop\'s domain name.').'</a>');
+		{
+			$warning = $this->l('You are currently connected under the following domain name:').' <span style="color: #CC0000;">'.$_SERVER['HTTP_HOST'].'</span><br />';
+			if (Configuration::get('PS_MULTISHOP_FEATURE_ACTIVE'))
+				$warning .= sprintf($this->l('This is different from the shop domain name set in the Multistore settings: "%s".'), $shop->domain).'
+				'.preg_replace('@{link}(.*){/link}@', '<a href="index.php?controller=AdminShopUrl&id_shop_url='.(int)$shop->id.'&updateshop_url&token='.Tools::getAdminTokenLite('AdminShopUrl').'">$1</a>', $this->l('If this is your main domain, please {link}change it now{/link}.'));
+			else
+				$warning .= $this->l('This is different from the domain name set in the "SEO & URLs" tab.').'
+				'.preg_replace('@{link}(.*){/link}@', '<a href="index.php?controller=AdminMeta&token='.Tools::getAdminTokenLite('AdminMeta').'#conf_id_domain">$1</a>', $this->l('If this is your main domain, please {link}change it now{/link}.'));
+			$this->displayWarning($warning);
+		}
 	}
 
 	protected function getQuickLinks()
