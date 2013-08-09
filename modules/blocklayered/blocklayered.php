971b45e6307272917e538d19c735ee72e075d399
Damien Metzger <damien.metzger@prestashop.com>
2013-08-09 17:21:13 +0200

    // fixed variable that does not exists

--- modules/blocklayered/blocklayered.php
+++ modules/blocklayered/blocklayered.php
@@ -2197,7 +2197,7 @@ class BlockLayered extends Module
 		if ($id_parent == 1)
 			return false;
 
-		$query_filters_where = ' AND '.$alias_where.'.`active` = 1 AND product_shop.`visibility` IN ("both", "catalog")';
+		$query_filters_where = ' AND product_shop.`active` = 1 AND product_shop.`visibility` IN ("both", "catalog")';
 		$query_filters_from = '';
 		
 		$parent = new Category((int)$id_parent);
