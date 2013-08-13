6b8d78f0c14f9742afc207b7df410671daa1ea22
indesign47 <transfertpcmac@neuf.fr>
2013-08-14 00:12:21 +0200

    Update index.php

--- cache/cachefs/index.php
+++ cache/cachefs/index.php
@@ -24,7 +24,7 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 			    	
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT""");
 header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
 	
 header("Cache-Control: no-store, no-cache, must-revalidate");
