4a162c49e8ec839257e2dfec1853181eefc13240
gRoussac <gregory.roussac@prestashop.com>
2013-08-13 16:29:40 +0200

    [-] CORE : Do not delete index.php in smarty cache or /img/tmp/

--- install-dev/models/install.php
+++ install-dev/models/install.php
@@ -369,10 +369,13 @@ class InstallModelInstall extends InstallAbstractModel
 	{
 		// Clear smarty cache
 		Tools::clearSmartyCache();
-		
+
 		//clear image cache in tmp folder
-		Tools::deleteDirectory(_PS_TMP_IMG_DIR_, false);
-		
+		if (file_exists(_PS_TMP_IMG_DIR_))
+			foreach (scandir(_PS_TMP_IMG_DIR_) as $file)
+				if ($file[0] != '.' && $file != 'index.php')
+					self::deleteDirectory($dir.DIRECTORY_SEPARATOR.$file);
+
 		$default_data = array(
 			'shop_name' =>		'My Shop',
 			'shop_activity' =>	'',
@@ -395,7 +398,7 @@ class InstallModelInstall extends InstallAbstractModel
 
 		// use the old image system if the safe_mod is enabled otherwise the installer will fail with the fixtures installation
 		if (InstallSession::getInstance()->safe_mode)
-			Configuration::updateGlobalValue('PS_LEGACY_IMAGES', 				1);
+			Configuration::updateGlobalValue('PS_LEGACY_IMAGES', 1);
 	
 		$id_country = Country::getByIso($data['shop_country']);
 
