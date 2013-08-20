e33e383029b8d90273f5ac3ccedcf5fe592b7207
indesign47 <transfertpcmac@neuf.fr>
2013-08-20 09:35:46 +0200

    Revert "test"
    This reverts commit 197a5ccf1629f9855c1ecf39eac9cb9a2f1a5daa.

--- controllers/admin/AdminStatusesController.php
+++ controllers/admin/AdminStatusesController.php
@@ -494,7 +494,6 @@ class AdminStatusesControllerCore extends AdminController
 
 			return parent::postProcess();
 		}
-		//new function
 		else if (isset($_GET['send_email'.$this->table]))
     		{
 		  $this->processSend_email();
