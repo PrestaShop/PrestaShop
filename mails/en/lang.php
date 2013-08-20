32ad90224023d36df8d56f48bd9579d17783b693
Damien Metzger <damien.metzger@prestashop.com>
2013-08-20 10:31:03 +0200

    // Updated some meta and translations

--- mails/en/lang.php
+++ mails/en/lang.php
@@ -26,38 +26,35 @@
 
 global $_LANGMAIL;
 $_LANGMAIL = array();
-$_LANGMAIL['Welcome!'] = 'Welcome!';
-$_LANGMAIL['Message from contact form'] = 'Customer message from contact form';
-$_LANGMAIL['Your message has been correctly sent'] = 'Your message has been sent successfully';
-$_LANGMAIL['New credit slip regarding your order'] = 'New credit slip regarding your order';
+
 $_LANGMAIL['Virtual product to download'] = 'Virtual product(s) available for download';
-$_LANGMAIL['Fwd: Customer message'] = 'Fwd: Customer message';
 $_LANGMAIL['Your guest account has been transformed to customer account'] = 'Your guest account has been transformed to a customer account';
-$_LANGMAIL['Package in transit'] = 'Package in transit';
-$_LANGMAIL['[Log'] = '[Log';
-$_LANGMAIL['Order confirmation'] = 'Order confirmation';
-$_LANGMAIL['Message from a customer'] = 'Message from a customer';
-$_LANGMAIL['New message regarding your order'] = 'New message regarding your order';
 $_LANGMAIL['Your order return state has changed'] = 'Your order return status has changed';
-$_LANGMAIL['Your password'] = 'Your password has been reset';
 $_LANGMAIL['Password query confirmation'] = 'Forgot password';
-$_LANGMAIL['An answer to your message is available'] = 'New response to your inquiry';
 $_LANGMAIL['New voucher regarding your order %s'] = 'New voucher for your order %s';
-$_LANGMAIL['Happy birthday!'] = 'Happy birthday!';
 $_LANGMAIL['Newsletter confirmation'] = 'Newsletter subscription confirmation';
-$_LANGMAIL['Newsletter voucher'] = 'Newsletter voucher';
-$_LANGMAIL['Your wishlist\\\'s link'] = 'Your wishlist\'s link';
-$_LANGMAIL['Message from '] = 'Message from ';
-$_LANGMAIL['$subject'] = '';
 $_LANGMAIL['Your cart and your discount'] = 'Discount offer for items in your cart';
 $_LANGMAIL['Thanks for your order'] = 'Thanks for your order!';
 $_LANGMAIL['You are one of our best customers'] = 'Thank you for being one of our best customers';
-$_LANGMAIL['We miss you'] = 'We miss you';
 $_LANGMAIL['Product available'] = 'Your watched product is now available';
 $_LANGMAIL['Product out of stock'] = 'One or more products are almost out of stock';
-$_LANGMAIL['Error reporting from your PayPal module'] = 'Error reporting from your PayPal module';
+$_LANGMAIL['We miss you'] = 'We miss you';
+$_LANGMAIL['Email verification'] = 'Email verification';
+$_LANGMAIL['Newsletter voucher'] = 'Newsletter voucher';
+$_LANGMAIL['Stock coverage'] = 'Stock coverage';
 $_LANGMAIL['Congratulations!'] = 'Congratulations!';
-$_LANGMAIL['Referral Program'] = 'Referral program';
-$_LANGMAIL['A friend sent you a link to'] = 'A friend sent you a link to';
-
-?>
+$_LANGMAIL['Your wishlist\\\'s link'] = 'Your wishlist\'s link';
+$_LANGMAIL['Message from %1$s %2$s'] = 'Message from %1$s %2$s';
+$_LANGMAIL['Your order has been changed'] = 'Your order has been changed';
+$_LANGMAIL['Welcome!'] = 'Welcome!';
+$_LANGMAIL['New credit slip regarding your order'] = 'New credit slip regarding your order';
+$_LANGMAIL['Fwd: Customer message'] = 'Fwd: Customer message';
+$_LANGMAIL['Package in transit'] = 'Package in transit';
+$_LANGMAIL['Order confirmation'] = 'Order confirmation';
+$_LANGMAIL['Message from a customer'] = 'Message from a customer';
+$_LANGMAIL['New message regarding your order'] = 'New message regarding your order';
+$_LANGMAIL['Process the payment of your order'] = 'Process the payment of your order';
+$_LANGMAIL['Log: You have a new alert from your shop'] = 'Log: You have a new alert from your shop';
+$_LANGMAIL['Your new password'] = 'Your new password';
+$_LANGMAIL['An answer to your message is available #ct%1$s #tc%2$s'] = 'An answer to your message is available #ct%1$s #tc%2$s';
+$_LANGMAIL['%1$s sent you a link to %2$s'] = '%1$s sent you a link to %2$s';
