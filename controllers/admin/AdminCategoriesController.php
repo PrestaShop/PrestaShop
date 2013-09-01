cb70937f31aa04b940774e86d222f0f11ff5e117
Gabriel Schwardy <info@caleydon.com>
2013-09-01 23:08:17 +0200

    Update AdminCategoriesController.php
    Fix for categories reordering problem reported here:
    http://forge.prestashop.com/browse/PSCFV-10268

--- controllers/admin/AdminCategoriesController.php
+++ controllers/admin/AdminCategoriesController.php
@@ -213,7 +213,7 @@ class AdminCategoriesControllerCore extends AdminController
 
 	public function getList($id_lang, $order_by = null, $order_way = null, $start = 0, $limit = null, $id_lang_shop = false)
 	{
-		parent::getList($id_lang, $order_by, $order_way, $start, $limit, Context::getContext()->shop->id);
+		parent::getList($id_lang, 'sa.position', $order_way, $start, $limit, Context::getContext()>shop>id);
 		// Check each row to see if there are combinations and get the correct action in consequence
 
 		$nb_items = count($this->_list);
