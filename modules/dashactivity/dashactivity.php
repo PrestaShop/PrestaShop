10e720b93d52afd72dd956401769198590864448
Vincent Augagneur <vincent.augagneur@prestashop.com>
2013-09-02 09:40:24 +0200

    //added new modules for dashboard

---
+++ modules/dashactivity/dashactivity.php
@@ -0,0 +1,53 @@
+<?php
+/*
+* 2007-2013 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author PrestaShop SA <contact@prestashop.com>
+*  @copyright  2007-2013 PrestaShop SA
+*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
+if (!defined('_PS_VERSION_'))
+	exit;
+
+class Dashactivity extends Module
+{
+	public function __construct()
+	{
+		$this->name = 'dashactivity';
+		$this->tab = '';
+		$this->version = '0.1';
+		$this->author = 'PrestaShop';
+		
+		parent::__construct();
+	}
+
+	public function install()
+	{
+		if (!parent::install() || !$this->registerHook('dashboardZoneOne'))
+			return false;
+		return true;
+	}
+
+	public function hookDashboardZoneOne($params)
+	{
+		return $this->display(__FILE__, 'dashboard_zone_one.tpl');
+	}
+}
