7338bbb59c31a8e999cbfc25778ade44142aa1c2
indesign47 <transfertpcmac@neuf.fr>
2013-09-03 16:33:40 +0200

    test
    test

--- admin-dev/import/index.php
+++ admin-dev/import/index.php
@@ -22,6 +22,7 @@
 *  @copyright  2007-2013 PrestaShop SA
 *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
+* test
 */
 
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
