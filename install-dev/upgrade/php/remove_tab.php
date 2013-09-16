dbb5e77c4c2c140e95b5552e3919c76ba4381a19
gRoussac <gregory.roussac@prestashop.com>
2013-09-16 15:02:12 +0200

    [-] IN : Could not remove some tabs

--- install-dev/upgrade/php/remove_tab.php
+++ install-dev/upgrade/php/remove_tab.php
@@ -29,6 +29,6 @@ function remove_tab($tabname)
 	Db::getInstance()->execute('
 	DELETE t, l
 	FROM `'._DB_PREFIX_.'tab` t LEFT JOIN `'._DB_PREFIX_.'tab_lang` l ON (t.id_tab = l.id_tab)
-	WHERE t.`class_name` = '.pSQL($tabname));
+	WHERE t.`class_name` = "'.pSQL($tabname).'"');
 }
 
