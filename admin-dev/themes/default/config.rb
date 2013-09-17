23deb950deb52fe7c74b8a27bc9060bf3a1ed07c
Kevin Granger <shipowlata@gmail.com>
2013-09-17 10:00:33 +0200

    // fix

--- admin-dev/themes/default/config.rb
+++ admin-dev/themes/default/config.rb
@@ -5,12 +5,12 @@ sass_dir = "css"
 images_dir = "img"
 javascripts_dir = "js"
 fonts_dir = "font"
-output_style = :compressed
-environment = :production
+output_style = :extended
+environment = :development
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
-line_comments = false
+line_comments = true
 color_output = false
 preferred_syntax = :sass
