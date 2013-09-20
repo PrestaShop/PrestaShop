7b54b38900fafe17f7b8cf4bcce0826e7d77c21c
Damien Metzger <damien.metzger@prestashop.com>
2013-09-20 18:27:35 +0200

    [*] BO : removed deprecated chart engines

--- install-dev/upgrade/sql/1.6.0.1.sql
+++ install-dev/upgrade/sql/1.6.0.1.sql
@@ -1,6 +1,7 @@
 SET NAMES 'utf8';
 
 INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VALUES('PS_DASHBOARD_USE_PUSH', '0', NOW(), NOW());
+UPDATE `PREFIX_configuration` SET `value` = 'graphnvd3' WHERE `name` = 'PS_STATS_RENDER';
 
 ALTER TABLE `PREFIX_employee` CHANGE `bo_show_screencast` `bo_menu` TINYINT(1) NOT NULL DEFAULT '0';
 UPDATE `PREFIX_employee` SET bo_menu = 0;
