7e30d7bf4dd2427d94336dc3110d9a4e4c232aaf
smartdatasoft <m_arifdu@yahoo.com>
2013-09-24 11:50:06 +0000

    [+] LO :  Added Bengali installer

---
+++ install-dev/langs/bn/install.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+	'informations' => array(
+		'phone' => '+1 (888) 947-6543',
+		'documentation' => 'http://doc.prestashop.com/',
+		'documentation_upgrade' => 'http://docs.prestashop.com/display/PS15/Updating+PrestaShop',
+		'forum' => 'http://www.prestashop.com/forums/',
+		'blog' => 'http://www.prestashop.com/blog/',
+		'support' => 'https://www.prestashop.com/en/support',
+	),
+	'translations' => array(
+		'menu_welcome' => 'Choose your language',
+		'menu_license' => 'License agreements',
+		'menu_system' => 'System Compatibility',
+		'menu_database' => 'System Configuration',
+		'menu_configure' => 'Store information',
+		'menu_process' => 'Store installation',
+	),
+);
