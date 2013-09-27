628ba41a8e567fa96459cee69bd0083908b8c179
Jonadabe <jonadabe@hotmail.com>
2013-09-27 17:48:57 +0100

    Update login.js
    // some typos

--- js/login.js
+++ js/login.js
@@ -1,33 +1,32 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	// Focus on email address field
 	$('#email').focus();
 
 	// Initialize events
-	$('#login_form').submit(function(e) {
+	$('#login_form').submit(function (e) {
 		// Kill default behaviour
 		e.preventDefault();
 		doAjaxLogin($('#redirect').val());
 	});
 
-	$('#forgot_password_form').submit(function(e) {
+	$('#forgot_password_form').submit(function (e) {
 		// Kill default behaviour
 		e.preventDefault();
 		doAjaxForgot();
 	});
 
-	$('.show-forgot-password').click(function(e) {
+	$('.show-forgot-password').click(function (e) {
 		// Kill default behaviour
 		e.preventDefault();
 		displayForgotPassword();
 	});
 
-	$('.show-login-form').click(function(e) {
+	$('.show-login-form').click(function (e) {
 		// Kill default behaviour
 		e.preventDefault();
 		displayLogin();
 	});
 });
-
 
 function displayForgotPassword() {
 	$('#error').hide();
@@ -58,29 +57,32 @@ function displayLogin() {
  */
 function doAjaxLogin(redirect) {
 	$('#error').hide();
-	$('#login_form .ajax-loader').fadeIn('slow', function() {
+	$('#login_form .ajax-loader').fadeIn('slow', function () {
 		$.ajax({
-			type: "POST",
-			headers: { "cache-control": "no-cache" },
-			url: "ajax-tab.php" + '?rand=' + new Date().getTime(),
-			async: true,
-			dataType: "json",
-			data: {
-				ajax: "1",
-				token: "",
-				controller: "AdminLogin",
-				submitLogin: "1",
-				passwd: $('#passwd').val(),
-				email: $('#email').val(),
-				redirect: redirect
+			type : "POST",
+			headers : {
+				"cache-control" : "no-cache"
 			},
-			success: function(jsonData) {
-				if (jsonData.hasErrors)
+			url : "ajax-tab.php" + '?rand=' + new Date().getTime(),
+			async : true,
+			dataType : "json",
+			data : {
+				ajax : "1",
+				token : "",
+				controller : "AdminLogin",
+				submitLogin : "1",
+				passwd : $('#passwd').val(),
+				email : $('#email').val(),
+				redirect : redirect
+			},
+			success : function (jsonData) {
+				if (jsonData.hasErrors) {
 					displayErrors(jsonData.errors);
-				else
+				} else {
 					window.location.assign(jsonData.redirect);
+				}
 			},
-			error: function(XMLHttpRequest, textStatus, errorThrown) {
+			error : function (XMLHttpRequest, textStatus, errorThrown) {
 				$('#error').html('<h3>TECHNICAL ERROR:</h3><p>Details: Error thrown: ' + XMLHttpRequest + '</p><p>Text status: ' + textStatus + '</p>').show();
 				$('#login_form .ajax-loader').fadeOut('slow');
 			}
@@ -89,31 +91,32 @@ function doAjaxLogin(redirect) {
 }
 function doAjaxForgot() {
 	$('#error').hide();
-	$('#forgot_password_form .ajax-loader').fadeIn('slow', function() {
+	$('#forgot_password_form .ajax-loader').fadeIn('slow', function () {
 		$.ajax({
-			type: "POST",
-			headers: { "cache-control": "no-cache" },
-			url: "ajax-tab.php" + '?rand=' + new Date().getTime(),
-			async: true,
-			dataType: "json",
-			data: {
-				ajax: "1",
-				token: "",
-				controller: "AdminLogin",
-				submitForgot: "1",
-				email_forgot: $('#email_forgot').val()
+			type : "POST",
+			headers : {
+				"cache-control" : "no-cache"
 			},
-			success: function(jsonData) {
-				if (jsonData.hasErrors)
+			url : "ajax-tab.php" + '?rand=' + new Date().getTime(),
+			async : true,
+			dataType : "json",
+			data : {
+				ajax : "1",
+				token : "",
+				controller : "AdminLogin",
+				submitForgot : "1",
+				email_forgot : $('#email_forgot').val()
+			},
+			success : function (jsonData) {
+				if (jsonData.hasErrors) {
 					displayErrors(jsonData.errors);
-				else
-				{
+				} else {
 					alert(jsonData.confirm);
 					$('#forgot_password_form .ajax-loader').hide();
 					displayLogin();
 				}
 			},
-			error: function(XMLHttpRequest, textStatus, errorThrown) {
+			error : function (XMLHttpRequest, textStatus, errorThrown) {
 				$('#error').html('<h3>TECHNICAL ERROR:</h3><p>Details: Error thrown: ' + XMLHttpRequest + '</p><p>Text status: ' + textStatus + '</p>').show();
 				$('#forgot_password_form .ajax-loader').fadeOut('slow');
 			}
@@ -123,10 +126,14 @@ function doAjaxForgot() {
 function displayErrors(errors) {
 	str_errors = '<h3>' + (errors.length > 1 ? there_are : there_is) + ' ' + errors.length + ' ' + (errors.length > 1 ? label_errors : label_error) + '</h3><ol>';
 	for (var error in errors) //IE6 bug fix
-		if (error != 'indexOf') str_errors += '<li>' + errors[error] + '</li>';
+	{
+		if (error !== 'indexOf') {
+			str_errors += '<li>' + errors[error] + '</li>';
+		}
+	}
 	$('.ajax-loader').hide();
 	$('#error').html(str_errors + '</ol>').fadeIn('slow');
 	$("#login").effect("shake", {
-		times: 4
+		times : 4
 	}, 100);
 }
