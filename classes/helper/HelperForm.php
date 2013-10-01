f2a760c245596cce18fccb84c6eab5aa0e2f7b39
Vincent Augagneur <vincent.augagneur@prestashop.com>
2013-10-01 12:31:07 +0200

    //small fix

--- classes/helper/HelperForm.php
+++ classes/helper/HelperForm.php
@@ -181,6 +181,7 @@ class HelperFormCore extends Helper
 			'required_fields' => $this->getFieldsRequired(),
 			'vat_number' => file_exists(_PS_MODULE_DIR_.'vatnumber/ajax.php'),
 			'module_dir' => _MODULE_DIR_,
+			'base_url' => $this->context->shop->getBaseURL(),
 			'contains_states' => (isset($this->fields_value['id_country']) && isset($this->fields_value['id_state'])) ? Country::containsStates($this->fields_value['id_country']) : null,
 		));
 		return parent::generate();
