f38de51a47bab9d94a528c55f92bbcf479a6bdd5
gRoussac <gregory.roussac@prestashop.com>
2013-10-03 16:08:53 +0200

    [-] BO : Fix bug #PSCFV-10064, could not import deature in multilang, thanks @E.Bigfoot

--- classes/FeatureValue.php
+++ classes/FeatureValue.php
@@ -128,15 +128,32 @@ class FeatureValueCore extends ObjectModel
 				return $tab['value'];
 	}
 
-	public static function addFeatureValueImport($id_feature, $value)
+	public static function addFeatureValueImport($id_feature, $value, $id_product = null, $id_lang = null)
 	{
-		$id_feature_value = Db::getInstance()->getValue('
-		SELECT fv.`id_feature_value`
-		FROM '._DB_PREFIX_.'feature_value fv
-		LEFT JOIN '._DB_PREFIX_.'feature_value_lang fvl ON (fvl.`id_feature_value` = fv.`id_feature_value`)
-		WHERE `value` = \''.pSQL($value).'\'
-		AND fv.`id_feature` = '.(int)$id_feature.'
-		GROUP BY fv.`id_feature_value`');
+		if (!is_null($id_product) && $id_product)
+		{
+			$id_feature_value = Db::getInstance()->getValue('
+				SELECT `id_feature_value`
+				FROM '._DB_PREFIX_.'feature_product
+				WHERE `id_feature` = '.(int)$id_feature.'
+				AND `id_product` = '.(int)$id_product);
+
+			if ($id_feature_value && !is_null($id_lang) && $id_lang)
+				Db::getInstance()->execute('
+				UPDATE '._DB_PREFIX_.'feature_value_lang 
+				SET `value` = \''.pSQL($value).'\' 
+				WHERE `id_feature_value` = '.(int)$id_feature_value.' 
+				AND `id_lang` = '.(int)$id_lang);
+		}
+		else
+			$id_feature_value = Db::getInstance()->getValue('
+				SELECT fv.`id_feature_value`
+				FROM '._DB_PREFIX_.'feature_value fv
+				LEFT JOIN '._DB_PREFIX_.'feature_value_lang fvl ON (fvl.`id_feature_value` = fv.`id_feature_value`)
+				WHERE `value` = \''.pSQL($value).'\'
+				AND fv.`id_feature` = '.(int)$id_feature.'
+				GROUP BY fv.`id_feature_value`');
+
 		if ($id_feature_value)
 			return (int)$id_feature_value;
 
