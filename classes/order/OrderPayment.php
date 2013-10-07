1ac7bb6018be9182f2bf51cb96ac47f6801027eb
gRoussac <gregory.roussac@prestashop.com>
2013-10-07 12:08:39 +0200

    // Report from https://github.com/Lupul/PrestaShop/commit/ebf09792e95598c30d8ac2617c4eabdd571be475

--- classes/order/OrderPayment.php
+++ classes/order/OrderPayment.php
@@ -49,7 +49,7 @@ class OrderPaymentCore extends ObjectModel
 			'id_currency' => 		array('type' => self::TYPE_INT, 'validate' => 'isUnsignedId', 'required' => true),
 			'amount' => 			array('type' => self::TYPE_FLOAT, 'validate' => 'isNegativePrice', 'required' => true),
 			'payment_method' => 	array('type' => self::TYPE_STRING, 'validate' => 'isGenericName'),
-			'conversion_rate' => 	array('type' => self::TYPE_INT, 'validate' => 'isFloat'),
+			'conversion_rate' => 	array('type' => self::TYPE_FLOAT, 'validate' => 'isFloat'),
 			'transaction_id' => 	array('type' => self::TYPE_STRING, 'validate' => 'isAnything', 'size' => 254),
 			'card_number' => 		array('type' => self::TYPE_STRING, 'validate' => 'isAnything', 'size' => 254),
 			'card_brand' => 		array('type' => self::TYPE_STRING, 'validate' => 'isAnything', 'size' => 254),
