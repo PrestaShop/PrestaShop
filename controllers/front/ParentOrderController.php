d125ca0a360ea66da8f50fe171bd675ccfe9d202
gRoussac <gregory.roussac@prestashop.com>
2013-10-07 12:02:47 +0200

    [-] FO : isVirtualCart was forced to false

--- controllers/front/ParentOrderController.php
+++ controllers/front/ParentOrderController.php
@@ -56,8 +56,6 @@ class ParentOrderControllerCore extends FrontController
 		header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 
 		$this->nbProducts = $this->context->cart->nbProducts();
-
-		global $isVirtualCart;
 		
 		if (!$this->context->customer->isLogged(true) && $this->context->getMobileDevice() && Tools::getValue('step'))
 			Tools::redirect($this->context->link->getPageLink('authentication', true, (int)$this->context->language->id, $params));
