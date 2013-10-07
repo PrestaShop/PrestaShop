7d6b0a22d5add56269fb85b15a369267dc384624
gRoussac <gregory.roussac@prestashop.com>
2013-10-07 11:24:42 +0200

    [-] BO : Retro compat, PS_ADMIN_DIR not defined //bad commit sorry

--- admin-dev/index.php
+++ admin-dev/index.php
@@ -28,7 +28,7 @@ $timer_start = microtime(true);
 define('_PS_ADMIN_DIR_', getcwd());
 
 if (!defined('PS_ADMIN_DIR'))
-	define('PS_ADMIN_DIR', PS_ADMIN_DIR);
+	define('PS_ADMIN_DIR', _PS_ADMIN_DIR_);
 
 require(_PS_ADMIN_DIR_.'/../config/config.inc.php');
 require(_PS_ADMIN_DIR_.'/functions.php');
