6ebc36d05dd988b7e21a8036d92ceb806d1ccbe4
gRoussac <gregory.roussac@prestashop.com>
2013-10-11 15:51:02 +0200

    [-] Bo :  Could not update mailalert 2.5 with id_lang missing

--- modules/mailalerts/upgrade/install-2.5.php
+++ modules/mailalerts/upgrade/install-2.5.php
@@ -6,24 +6,9 @@ if (!defined('_PS_VERSION_'))
 function upgrade_module_2_5($object)
 {
 	return Db::getInstance()->execute('
-		ALTER TABLE '._DB_PREFIX_.'mailalert_customer_oos 
-		ADD '.mailalerts_stripslashes_field('id_lang').' INT( 10 ) UNSIGNED NOT NULL , 
+		ALTER TABLE `'._DB_PREFIX_.'mailalert_customer_oos` 
+		ADD `id_lang` INT( 10 ) UNSIGNED NOT NULL , 
 		DROP PRIMARY KEY , 
-		ADD PRIMARY KEY ( 
-			'.mailalerts_stripslashes_field('id_customer').' , 
-			'.mailalerts_stripslashes_field('customer_email').' , 
-			'.mailalerts_stripslashes_field('id_product').' , 
-			'.mailalerts_stripslashes_field('id_product_attribute').' , 
-			'.mailalerts_stripslashes_field('id_shop').' 
-		)'
-	);
-}
-
-function mailalerts_stripslashes_field($field)
-{
-	$quotes = array('"\\\'"', '"\'"');
-	$dquotes = array('\'\\\\"\'', '\'"\'');
-	$backslashes = array('"\\\\\\\\"', '"\\\\"');
-
-	return '`'.bqSQL($field).'` = replace(replace(replace(`'.bqSQL($field).'`, '.$quotes[0].', '.$quotes[1].'), '.$dquotes[0].', '.$dquotes[1].'), '.$backslashes[0].', '.$backslashes[1].')';
+		ADD PRIMARY KEY (`id_customer` , `customer_email` , `id_product` , `id_product_attribute` , `id_shop`)
+	');
 }
