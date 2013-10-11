c64bc50f1e0b4007d0ab48751d75086b53c67c74
Jerome Nadaud <jerome.nadaud@prestashop.com>
2013-10-11 17:17:58 +0200

    // Adding update for customer message

--- install-dev/upgrade/sql/1.6.0.1.sql
+++ install-dev/upgrade/sql/1.6.0.1.sql
@@ -62,3 +62,5 @@ INSERT INTO `PREFIX_hook_module` (`id_module`, `id_shop`, `id_hook`, `position`)
 INSERT INTO `PREFIX_hook_module` (`id_module`, `id_shop`, `id_hook`, `position`) (
 	SELECT m.id_module, s.id_shop, h.id_hook, 2 FROM `PREFIX_module` m, `PREFIX_shop` s, `PREFIX_hook` h WHERE m.name IN ('dashproducts') AND h.name IN ('dashboardZoneTwo')
 );
+
+/* PHP:update_order_messages(); */;
