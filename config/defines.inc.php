640e7dbad440e15718bd00fa4a4b4dffd42758d1
Jerome Nadaud <jerome.nadaud@prestashop.com>
2013-10-18 17:40:49 +0200

    // Define Order state dir constante

--- config/defines.inc.php
+++ config/defines.inc.php
@@ -75,6 +75,7 @@ define('_PS_SCENE_IMG_DIR_',        _PS_IMG_DIR_.'scenes/');
 define('_PS_SCENE_THUMB_IMG_DIR_',  _PS_IMG_DIR_.'scenes/thumbs/');
 define('_PS_MANU_IMG_DIR_',         _PS_IMG_DIR_.'m/');
 define('_PS_SHIP_IMG_DIR_',         _PS_IMG_DIR_.'s/');
+define('_PS_ORDER_STATE_IMG_DIR_',  _PS_IMG_DIR_.'os/');
 define('_PS_SUPP_IMG_DIR_',         _PS_IMG_DIR_.'su/');
 define('_PS_COL_IMG_DIR_',			_PS_IMG_DIR_.'co/');
 define('_PS_TMP_IMG_DIR_',          _PS_IMG_DIR_.'tmp/');
