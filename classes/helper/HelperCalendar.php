5f75439a6b240dbdd26ce959fec2a6adbc43181b
Jerome Nadaud <jerome.nadaud@prestashop.com>
2013-10-21 17:07:57 +0200

    // Date range picker improvements

--- classes/helper/HelperCalendar.php
+++ classes/helper/HelperCalendar.php
@@ -212,13 +212,19 @@ class HelperCalendarCore extends Helper
 			$bo_theme = 'default';
 
 		if ($context->controller->ajax)
+		{
 			$html = '<script type="text/javascript" src="'.__PS_BASE_URI__.$admin_webpath
 				.'/themes/'.$bo_theme.'/js/date-range-picker.js"></script>';
+			$html .= '<script type="text/javascript" src="'.__PS_BASE_URI__.$admin_webpath
+				.'/themes/'.$bo_theme.'/js/calendar.js"></script>';
+		}
 		else
 		{
 			$html = '';
 			$context->controller->addJs(__PS_BASE_URI__.$admin_webpath
 				.'/themes/'.$bo_theme.'/js/date-range-picker.js');
+			$context->controller->addJs(__PS_BASE_URI__.$admin_webpath
+				.'/themes/'.$bo_theme.'/js/calendar.js');
 		}
 
 		$this->tpl = $this->createTemplate($this->base_tpl);
