2b2c0f62b8db16f03dfda17e0fd4a427fb571df7
Damien Metzger <damien.metzger@prestashop.com>
2013-10-24 19:29:22 +0200

    // Fixed boulette

---
+++ modules/mailalerts/mails/en/src/productcoverage.php
@@ -0,0 +1,23 @@
+<?php include ('header.php'); ?>
+
+<tr>
+	<td align="center">
+		<span class="title"><?php echo t('Hi,'); ?></span>
+	</td>
+</tr>
+<tr>
+	<td>&nbsp;</td>
+</tr>
+<tr>
+	<td class="box">
+		<p>
+			{product} <?php echo t('is almost out of stock.'); ?>
+		</p>
+		<span>
+			<?php echo t('The stock cover is now less than the specified minimum of'); ?> <strong><span>{warning_coverage}.</span></strong><br /><br /> 
+			<strong><span><?php echo t('Current stock cover:'); ?></span></strong> {current_coverage}
+		</span>
+	</td>
+</tr>
+
+<?php include ('footer.php'); ?>
