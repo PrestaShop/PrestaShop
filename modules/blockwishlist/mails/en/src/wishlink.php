1a7f9e6c114b9c7cfe08a941e170035d46357da3
Sarah Lorenzini <sarah.lorenzini@prestashop.com>
2013-10-24 17:35:37 +0200

    // new design for blockwishlist mails template

---
+++ modules/blockwishlist/mails/en/src/wishlink.php
@@ -0,0 +1,23 @@
+<?php include ('header.php'); ?>
+
+<tr>
+	<td align="center">
+		<span class="title"><?php echo t('Hi,'); ?></span>
+	</td>
+</tr>
+<tr>
+	<td>&nbsp;</td>
+</tr>
+<tr>
+	<td class="box">
+		<p>
+			<?php echo t('Message from {shop_name}'); ?>
+		</p>
+		<span>
+			<span><strong>{shop_name}</strong></span> <?php echo t('invites you to send this link to your friends, so they can see your wishlist:'); ?> <span><strong>{wishlist}</strong></span><br /><br />
+			<a title="WishList" href="{message}">{message}</a>
+		</span>
+	</td>
+</tr>
+
+<?php include ('footer.php'); ?>
