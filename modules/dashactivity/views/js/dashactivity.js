6a2e8479709aa08bdb69f4d026a3541929d44c7b
Damien Metzger <damien.metzger@prestashop.com>
2013-10-24 15:04:06 +0200

    // dashboard improvements

--- modules/dashactivity/views/js/dashactivity.js
+++ modules/dashactivity/views/js/dashactivity.js
@@ -8,11 +8,14 @@ function pie_chart_trends(widget_name, chart_details)
 			.donut(true)
 			.showLabels(false)
 			.showLegend(false);
-		  d3.select("#dash_traffic_chart2 svg")
+
+		d3.select("#dash_traffic_chart2 svg")
 			.datum(chart_details.data)
 			.transition().duration(1200)
 			.call(chart);
+			
 		nv.utils.windowResize(chart.update);
+
 		return chart;
 	});	
 }
