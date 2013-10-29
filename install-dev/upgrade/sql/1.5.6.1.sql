f8ea147a7b54f51d853456485c7b9255b486d3fd
gRoussac <gregory.roussac@prestashop.com>
2013-10-29 19:05:21 +0100

    [*] CORE : Manual merge of https://github.com/PrestaShop/PrestaShop/ thanks @kpodemski

--- install-dev/upgrade/sql/1.5.6.1.sql
+++ install-dev/upgrade/sql/1.5.6.1.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `PREFIX_customer_message` CHANGE `ip_address` `ip_address` VARCHAR( 15 ) NULL DEFAULT NULL;
 
 UPDATE `PREFIX_orders` SET conversion_rate = 1 WHERE conversion_rate = 0;
+
+ALTER TABLE `PREFIX_cms` ADD `indexation` tinyint(1) UNSIGNED NULL DEFAULT '1' AFTER `active`;
