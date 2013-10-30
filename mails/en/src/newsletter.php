2857eff1610946fe50867fcf137c3c5e3bfade08
Rémi Gaillard <remi.gaillard@prestashop.com>
2013-10-30 10:25:30 +0100

    //

---
+++ mails/en/src/newsletter.php
@@ -0,0 +1,14 @@
+<?php include ('header.php'); ?>
+
+<tr>
+	<td align="center">
+		<span class="title"><?php echo t('Hi {firstname} {lastname},'); ?></span>
+	</td>
+</tr>
+<tr>
+	<td>
+		{message}
+	</td>
+</tr>
+
+<?php include ('footer.php'); ?>
