f919cc899d07dc608f71557ef3b7d3ef2bf03fd3
gRoussac <gregory.roussac@prestashop.com>
2013-10-30 20:06:47 +0100

    [-] IN : Set conversion rate to 1 if equal 0 in base

--- install-dev/upgrade/sql/1.5.6.1.sql
+++ install-dev/upgrade/sql/1.5.6.1.sql
@@ -1,5 +1,7 @@
 ALTER TABLE `PREFIX_customer_message` CHANGE `ip_address` `ip_address` VARCHAR( 15 ) NULL DEFAULT NULL;
 
+UPDATE `PREFIX_currency` SET `conversion_rate` = 1 WHERE `conversion_rate` = 0;
+
 ALTER TABLE `PREFIX_currency` CHANGE `conversion_rate` `conversion_rate` DECIMAL( 13, 6 ) NOT NULL DEFAULT '1';
 
 UPDATE `PREFIX_orders` SET conversion_rate = 1 WHERE conversion_rate = 0;
