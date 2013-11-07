e6f9f806bf199320e26795abb901f67f4e409d32
gRoussac <gregory.roussac@prestashop.com>
2013-11-07 12:03:35 +0100

    [-] FO : Fix bug #PSCFV-10892, getCMSPages by id_shop thnaks @jd440

--- classes/CMS.php
+++ classes/CMS.php
@@ -190,13 +190,16 @@ class CMSCore extends ObjectModel
 		return (Db::getInstance()->getValue($sql));
 	}
 
-	public static function getCMSPages($id_lang = null, $id_cms_category = null, $active = true)
+	public static function getCMSPages($id_lang = null, $id_cms_category = null, $active = true, $id_shop = null)
 	{
 		$sql = new DbQuery();
 		$sql->select('*');
 		$sql->from('cms', 'c');
 		if ($id_lang)
 			$sql->innerJoin('cms_lang', 'l', 'c.id_cms = l.id_cms AND l.id_lang = '.(int)$id_lang);
+
+		if ($id_shop)
+			$sql->innerJoin('cms_shop', 'cs', 'c.id_cms = cs.id_cms AND cs.id_shop = '.(int)$id_shop); 
 
 		if ($active)
 			$sql->where('c.active = 1');
