16a72fec2c2407b6861c4a5dc65e56c22ba2a2e7
Damien Metzger <damien.metzger@prestashop.com>
2013-11-19 14:44:27 +0100

    Update best-sales.php

--- best-sales.php
+++ best-sales.php
@@ -12,7 +12,7 @@
 * obtain it through the world-wide-web, please send an email
 * to license@prestashop.com so we can send you a copy immediately.
 *
-* DISCLAIMER
+* DISCLAIMERpoiuytr hi
 *
 * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
 * versions in the future. If you wish to customize PrestaShop for your
