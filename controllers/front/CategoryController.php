2b210e0732dfc8310209465cbd8a993acf31f92c
gRoussac <gregory.roussac@prestashop.com>
2013-11-27 16:20:20 +0100

    [*] FO : Merge of https://github.com/PrestaShop/PrestaShop/pull/854

--- controllers/front/CategoryController.php
+++ controllers/front/CategoryController.php
@@ -129,7 +129,8 @@ class CategoryControllerCore extends FrontController
 			'homeSize' => Image::getSize(ImageType::getFormatedName('home')),
 			'allow_oosp' => (int)Configuration::get('PS_ORDER_OUT_OF_STOCK'),
 			'comparator_max_item' => (int)Configuration::get('PS_COMPARATOR_MAX_ITEM'),
-			'suppliers' => Supplier::getSuppliers()
+			'suppliers' => Supplier::getSuppliers(),
+			'body_classes' => array($this->php_self.'-'.$this->category->id, $this->php_self.'-'.$this->category->link_rewrite)
 		));
 	}
 
