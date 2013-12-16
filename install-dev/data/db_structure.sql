f883a4c47ad05d10d15d46561f384a981573960a
emilien-puget <puget.e@gmail.com>
2013-12-16 17:00:41 +0100

    [*] BO : informations about the number of column by page added

--- install-dev/data/db_structure.sql
+++ install-dev/data/db_structure.sql
@@ -2008,8 +2008,19 @@ CREATE TABLE IF NOT EXISTS `PREFIX_theme` (
   `id_theme` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(64) NOT NULL,
   `directory` varchar(64) NOT NULL,
+  `responsive` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_theme`)
 ) ENGINE=ENGINE_TYPE  DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `PREFIX_theme_meta` (
+  `id_theme` int(11) NOT NULL,
+  `id_meta` int(10) unsigned NOT NULL,
+  `left_column` tinyint(1) NOT NULL DEFAULT '0',
+  `right_column` tinyint(1) NOT NULL DEFAULT '0',
+  UNIQUE KEY `id_theme_2` (`id_theme`,`id_meta`),
+  KEY `id_theme` (`id_theme`),
+  KEY `id_meta` (`id_meta`)
+) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS `PREFIX_theme_specific` (
   `id_theme` int(11) unsigned NOT NULL,
