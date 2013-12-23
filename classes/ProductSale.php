9d9c1c99a06c8b3913b5d21b99b820caa54302c2
gRoussac <gregory.roussac@prestashop.com>
2013-12-23 20:07:18 +0100

    // small fix

--- classes/ProductSale.php
+++ classes/ProductSale.php
@@ -78,7 +78,7 @@ class ProductSaleCore
 		//Subquery: get product ids in a separate query to (greatly!) improve performances and RAM usage
 		$sql = 'SELECT cp.`id_product`
 				FROM `'._DB_PREFIX_.'category_group` cg
-				LEFT JOIN `'._DB_PREFIX_.'category_product` cp ON (cp.`id_category` = cg.`id_category`)
+				INNER JOIN `'._DB_PREFIX_.'category_product` cp ON (cp.`id_category` = cg.`id_category`)
 				WHERE cp.`id_product` IS NOT NULL AND cg.`id_group` '.$sql_groups;
 		$products = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($sql);
 		$ids = array();
