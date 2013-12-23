bb2115e7022a51b5a604dcafd9b53dd9db4a1f95
gRoussac <gregory.roussac@prestashop.com>
2013-12-23 20:06:38 +0100

    // small fix

--- classes/ProductSale.php
+++ classes/ProductSale.php
@@ -84,7 +84,7 @@ class ProductSaleCore
 		$products = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS('
 		SELECT cp.`id_product`
 		FROM `'._DB_PREFIX_.'category_group` cg
-		LEFT JOIN `'._DB_PREFIX_.'category_product` cp ON (cp.`id_category` = cg.`id_category`)
+		INNER JOIN `'._DB_PREFIX_.'category_product` cp ON (cp.`id_category` = cg.`id_category`)
 		'.$sql_groups);
 	
 		$ids = array();
