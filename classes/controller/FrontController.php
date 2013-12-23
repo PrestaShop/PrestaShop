0fd2e644b2129ad8a81377fec290098d7ad8a33e
gRoussac <gregory.roussac@prestashop.com>
2013-12-23 19:50:32 +0100

    // report of https://github.com/PrestaShop/PrestaShop/commit/d716fdaa9755c091c392b8b0f2bd7a5298b463d2

--- classes/controller/FrontController.php
+++ classes/controller/FrontController.php
@@ -995,10 +995,13 @@ class FrontControllerCore extends Controller
 				$customer = new Customer((int)$cart->id_customer);
 				if (Validate::isLoadedObject($customer))
 				{
+					$customer->logged = 1;
+					$this->context->customer = $customer;
 					$this->context->cookie->id_customer = (int)$customer->id;
 					$this->context->cookie->customer_lastname = $customer->lastname;
 					$this->context->cookie->customer_firstname = $customer->firstname;
 					$this->context->cookie->logged = 1;
+					$this->context->cookie->check_cgv = 1;
 					$this->context->cookie->is_guest = $customer->isGuest();
 					$this->context->cookie->passwd = $customer->passwd;
 					$this->context->cookie->email = $customer->email;
