921fe271acb4a9ab28e75e44fac7117efcb1f3d4
emilien-puget <puget.e@gmail.com>
2013-12-23 11:44:48 +0100

    // PREFIX missing

--- install-dev/upgrade/sql/1.6.0.3.sql
+++ install-dev/upgrade/sql/1.6.0.3.sql
@@ -4,4 +4,4 @@ ALTER TABLE `PREFIX_theme` ADD `default_left_column` TINYINT( 1 ) NOT NULL DEFAU
 
 ALTER TABLE `PREFIX_theme` ADD `default_right_column` TINYINT( 1 ) NOT NULL DEFAULT '0';
 
-ALTER TABLE `ps_theme` ADD `product_per_page` INT UNSIGNED NOT NULL;
+ALTER TABLE `PREFIX_theme` ADD `product_per_page` INT UNSIGNED NOT NULL;
