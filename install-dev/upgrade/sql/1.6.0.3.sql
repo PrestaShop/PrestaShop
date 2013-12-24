9a4a212f2f7c344cec67216faa0e27a1d3d8b33b
emilien-puget <puget.e@gmail.com>
2013-12-24 10:50:42 +0100

    // missing column

--- install-dev/upgrade/sql/1.6.0.3.sql
+++ install-dev/upgrade/sql/1.6.0.3.sql
@@ -4,4 +4,6 @@ ALTER TABLE `PREFIX_theme` ADD `default_left_column` TINYINT( 1 ) NOT NULL DEFAU
 
 ALTER TABLE `PREFIX_theme` ADD `default_right_column` TINYINT( 1 ) NOT NULL DEFAULT '0';
 
+ALTER TABLE `PREFIX_theme` ADD `responsive` TINYINT( 1 ) NOT NULL DEFAULT '0';
+
 ALTER TABLE `PREFIX_theme` ADD `product_per_page` INT UNSIGNED NOT NULL;
