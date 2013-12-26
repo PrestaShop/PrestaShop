665a8e639a722b18cbc9361a7576a6eee7d771fe
gRoussac <gregory.roussac@prestashop.com>
2013-12-26 14:22:42 +0100

    [-] FO : Fix bug #PSCFV-11267, bad cache on carrier choice in normal checkout

--- controllers/front/ParentOrderController.php
+++ controllers/front/ParentOrderController.php
@@ -477,7 +477,6 @@ class ParentOrderControllerCore extends FrontController
 	{	
 		$address = new Address($this->context->cart->id_address_delivery);
 		$id_zone = Address::getZoneById($address->id);
-		$bad_delivery = false;
 		$carriers = $this->context->cart->simulateCarriersOutput();
 		$checked = $this->context->cart->simulateCarrierSelectedOutput();
 		$delivery_option_list = $this->context->cart->getDeliveryOptionList();
