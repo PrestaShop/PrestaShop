fa6428abcca2734e0c9023864742c3b512819b19
Damien Metzger <damien.metzger@prestashop.com>
2013-12-30 16:31:35 +0100

    // Merged productTabsManager into admin-product (the only place where it is used)

--- controllers/admin/AdminProductsController.php
+++ controllers/admin/AdminProductsController.php
@@ -1341,7 +1341,6 @@ class AdminProductsControllerCore extends AdminController
 			));
 
 			$this->addJS(array(
-				_PS_JS_DIR_.'productTabsManager.js',
 				_PS_JS_DIR_.'admin-products.js',
 				_PS_JS_DIR_.'attributesBack.js',
 				_PS_JS_DIR_.'price.js',
