8955ed38b300fdda1a7e0efa9d496fd3980c7066
gRoussac <gregory.roussac@prestashop.com>
2013-12-31 14:42:06 +0100

    // follow up https://github.com/PrestaShop/PrestaShop/commit/ca44205b7536619c93da0b337d169e7ed19f746d

--- controllers/admin/AdminPerformanceController.php
+++ controllers/admin/AdminPerformanceController.php
@@ -729,6 +729,7 @@ class AdminPerformanceControllerCore extends AdminController
 					else
 						Configuration::updateValue('PS_MEDIA_SERVERS', 0);
 					rewriteSettingsFile($base_urls, null, null);
+					Tools::clearSmartyCache();
 					Media::clearCache();
 					Tools::generateHtaccess(null, null, null, '', null, array($base_urls['_MEDIA_SERVER_1_'], $base_urls['_MEDIA_SERVER_2_'], $base_urls['_MEDIA_SERVER_3_']));
 					unset($this->_fieldsGeneral['_MEDIA_SERVER_1_']);
