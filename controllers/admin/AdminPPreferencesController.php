6b545ef1b07346e619c7ba738edd147c451ed839
Xavier <xavier.borderie@prestashop.com>
2014-01-02 17:12:59 +0100

    [*] BO: clarifying discount option hint (2). Fixes PSCSX-289.

--- controllers/admin/AdminPPreferencesController.php
+++ controllers/admin/AdminPPreferencesController.php
@@ -200,7 +200,7 @@ class AdminPPreferencesControllerCore extends AdminController
 					),
 					'PS_DISPLAY_DISCOUNT_PRICE' => array(
 						'title' => $this->l('Display discounted price'),
-						'desc' => $this->l('"In the volume discounts board, display the new price with the applied discount instead of showing the discount (ie. "-5%").'),
+						'desc' => $this->l('In the volume discounts board, display the new price with the applied discount instead of showing the discount (ie. "-5%").'),
 						'validation' => 'isBool',
 						'cast' => 'intval',
 						'required' => false,
