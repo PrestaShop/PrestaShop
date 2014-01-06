f168e2d31b30320622fd72422226a77c1546f490
Damien Metzger <damien.metzger@prestashop.com>
2014-01-06 18:53:16 +0100

    // Fixed contextual help link

--- classes/controller/AdminController.php
+++ classes/controller/AdminController.php
@@ -1217,7 +1217,7 @@ class AdminControllerCore extends Controller
 			$this->page_header_toolbar_title = array_pop($this->toolbar_title);
 		$this->addPageHeaderToolBarModulesListButton();
 
-		$this->context->smarty->assign('help_link', 'http://help.prestashop.com/'.$this->context->language->iso_code.'/doc/'.str_replace('.php', '', basename(__FILE__)).'?version='._PS_VERSION_.'&country='.$this->context->country->iso_code);
+		$this->context->smarty->assign('help_link', 'http://help.prestashop.com/'.$this->context->language->iso_code.'/doc/'.Tools::getValue('controller').'?version='._PS_VERSION_.'&country='.$this->context->country->iso_code);
 	}
 
 	/**
