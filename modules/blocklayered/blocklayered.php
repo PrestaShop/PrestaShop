79d9831869b57b5d8bf9b6419a7aacf91d4e7f11
Samy Rabih <srabih@o2sources.com>
2014-01-07 12:48:39 +0100

    Filter on current shop ID for price index

--- modules/blocklayered/blocklayered.php
+++ modules/blocklayered/blocklayered.php
@@ -2787,13 +2787,13 @@ class BlockLayered extends Module
 		{
 			$price_filter_query = '
 			INNER JOIN `'._DB_PREFIX_.'layered_price_index` psi ON (psi.id_product = p.id_product AND psi.id_currency = '.(int)$id_currency.'
-			AND psi.price_min <= '.(int)$filter_value[1].' AND psi.price_max >= '.(int)$filter_value[0].') ';
+			AND psi.price_min <= '.(int)$filter_value[1].' AND psi.price_max >= '.(int)$filter_value[0].' AND psi.id_shop='.(int)Context::getContext()->shop->id.') ';
 		}
 		else
 		{
 			$price_filter_query = '
 			INNER JOIN `'._DB_PREFIX_.'layered_price_index` psi 
-			ON (psi.id_product = p.id_product AND psi.id_currency = '.(int)$id_currency.') ';
+			ON (psi.id_product = p.id_product AND psi.id_currency = '.(int)$id_currency.' AND psi.id_shop='.(int)Context::getContext()->shop->id).') ';
 		}
 		
 		return array('join' => $price_filter_query, 'select' => ', psi.price_min, psi.price_max');
