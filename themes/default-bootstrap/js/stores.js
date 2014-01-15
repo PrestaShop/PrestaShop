498f595f5926af7d8e0b4e9bdda67fb4766649e7
gRoussac <gregory.roussac@prestashop.com>
2014-01-15 14:06:04 +0100

    // norms

--- themes/default-bootstrap/js/stores.js
+++ themes/default-bootstrap/js/stores.js
@@ -22,6 +22,32 @@
 *  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
+$(document).ready(function()
+{
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: new google.maps.LatLng(defaultLat, defaultLong),
+		zoom: 10,
+		mapTypeId: 'roadmap',
+		mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU}
+	});
+	infoWindow = new google.maps.InfoWindow();
+
+	locationSelect = document.getElementById('locationSelect');
+		locationSelect.onchange = function() {
+		var markerNum = locationSelect.options[locationSelect.selectedIndex].value;
+		if (markerNum !== 'none')
+		google.maps.event.trigger(markers[markerNum], 'click');
+	};
+	
+	$('#addressInput').keypress(function(e) {
+		code = e.keyCode ? e.keyCode : e.which;
+		if(code.toString() === 13)
+			searchLocations();
+	});
+
+
+	initMarkers();
+});
 
 function initMarkers()
 {
@@ -196,30 +222,3 @@ function parseXml(str)
 }
 
 function doNothing() {}
-
-$(document).ready(function()
-{
-	map = new google.maps.Map(document.getElementById('map'), {
-		center: new google.maps.LatLng(defaultLat, defaultLong),
-		zoom: 10,
-		mapTypeId: 'roadmap',
-		mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU}
-	});
-	infoWindow = new google.maps.InfoWindow();
-
-	locationSelect = document.getElementById('locationSelect');
-		locationSelect.onchange = function() {
-		var markerNum = locationSelect.options[locationSelect.selectedIndex].value;
-		if (markerNum !== 'none')
-		google.maps.event.trigger(markers[markerNum], 'click');
-	};
-	
-	$('#addressInput').keypress(function(e) {
-		code = e.keyCode ? e.keyCode : e.which;
-		if(code.toString() === 13)
-			searchLocations();
-	});
-
-
-	initMarkers();
-});
