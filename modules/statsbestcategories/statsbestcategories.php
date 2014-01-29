1b567d8a08c037455e82f1ba0a07e85cafb6504e
emilien-puget <puget.e@gmail.com>
2014-01-29 10:30:30 +0100

    // statsbestcategories  :  norm

--- modules/statsbestcategories/statsbestcategories.php
+++ modules/statsbestcategories/statsbestcategories.php
@@ -29,13 +29,13 @@ if (!defined('_PS_VERSION_'))
 
 class StatsBestCategories extends ModuleGrid
 {
-	private $_html;
-	private $_query;
-	private $_columns;
-	private $_defaultSortColumn;
-	private $_defaultSortDirection;
-	private $_emptyMessage;
-	private $_pagingMessage;
+	private $html;
+	private $query;
+	private $columns;
+	private $default_sort_column;
+	private $default_sort_direction;
+	private $empty_message;
+	private $paging_message;
 
 	public function __construct()
 	{
@@ -44,15 +44,15 @@ class StatsBestCategories extends ModuleGrid
 		$this->version = 1.0;
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
-		
-		parent::__construct();
-		
-		$this->_defaultSortColumn = 'totalPriceSold';
-		$this->_defaultSortDirection = 'DESC';
-		$this->_emptyMessage = $this->l('Empty recordset returned');
-		$this->_pagingMessage = sprintf($this->l('Displaying %1$s of %2$s'), '{0} - {1}', '{2}');
 
-		$this->_columns = array(
+		parent::__construct();
+
+		$this->default_sort_column = 'totalPriceSold';
+		$this->default_sort_direction = 'DESC';
+		$this->empty_message = $this->l('Empty recordset returned');
+		$this->paging_message = sprintf($this->l('Displaying %1$s of %2$s'), '{0} - {1}', '{2}');
+
+		$this->columns = array(
 			array(
 				'id' => 'name',
 				'header' => $this->l('Name'),
@@ -90,34 +90,35 @@ class StatsBestCategories extends ModuleGrid
 
 	public function hookAdminStatsModules($params)
 	{
-		$engineParams = array(
+		$engine_params = array(
 			'id' => 'id_category',
 			'title' => $this->displayName,
-			'columns' => $this->_columns,
-			'defaultSortColumn' => $this->_defaultSortColumn,
-			'defaultSortDirection' => $this->_defaultSortDirection,
-			'emptyMessage' => $this->_emptyMessage,
-			'pagingMessage' => $this->_pagingMessage
+			'columns' => $this->columns,
+			'defaultSortColumn' => $this->default_sort_column,
+			'defaultSortDirection' => $this->default_sort_direction,
+			'emptyMessage' => $this->empty_message,
+			'pagingMessage' => $this->paging_message
 		);
 
 		if (Tools::getValue('export'))
-			$this->csvExport($engineParams);
+			$this->csvExport($engine_params);
 
-		$this->_html = '
+		$this->html = '
 			<div class="panel-heading">
 				<i class="icon-sitemap"></i> '.$this->displayName.'
 			</div>
-			'.$this->engine($engineParams).'
+			'.$this->engine($engine_params).'
 			<a class="btn btn-default export-csv" href="'.htmlentities($_SERVER['REQUEST_URI']).'&export=1">
 				<i class="icon-cloud-upload"></i> '.$this->l('CSV Export').'
 			</a>';
-		return $this->_html;
+
+		return $this->html;
 	}
 
 	public function getData()
 	{
 		$currency = new Currency(Configuration::get('PS_CURRENCY_DEFAULT'));
-		$dateBetween = $this->getDate();
+		$date_between = $this->getDate();
 		$id_lang = $this->getLang();
 
 		// If a shop is selected, get all children categories for the shop
@@ -133,15 +134,15 @@ class StatsBestCategories extends ModuleGrid
 					)';
 			if ($result = Db::getInstance()->executeS($sql))
 			{
-				$ntreeRestriction = array();
+				$ntree_restriction = array();
 				foreach ($result as $row)
-					$ntreeRestriction[] = '(nleft >= '.$row['nleft'].' AND nright <= '.$row['nright'].')';
-				
-				if ($ntreeRestriction)
+					$ntree_restriction[] = '(nleft >= '.$row['nleft'].' AND nright <= '.$row['nright'].')';
+
+				if ($ntree_restriction)
 				{
 					$sql = 'SELECT id_category
 							FROM '._DB_PREFIX_.'category
-							WHERE '.implode(' OR ', $ntreeRestriction);
+							WHERE '.implode(' OR ', $ntree_restriction);
 					if ($result = Db::getInstance()->executeS($sql))
 					{
 						foreach ($result as $row)
@@ -152,7 +153,7 @@ class StatsBestCategories extends ModuleGrid
 		}
 
 		// Get best categories
-		$this->_query = '
+		$this->query = '
 		SELECT SQL_CALC_FOUND_ROWS ca.`id_category`, CONCAT(parent.name, \' > \', calang.`name`) as name,
 			IFNULL(SUM(t.`totalQuantitySold`), 0) AS totalQuantitySold,
 			ROUND(IFNULL(SUM(t.`totalPriceSold`), 0), 2) AS totalPriceSold,
@@ -165,8 +166,8 @@ class StatsBestCategories extends ModuleGrid
 				LEFT JOIN `'._DB_PREFIX_.'category_product` capr2 ON capr2.`id_product` = pr.`id_product`
 				WHERE capr.`id_category` = capr2.`id_category`
 				AND p.`id_page_type` = 1
-				AND dr.`time_start` BETWEEN '.$dateBetween.'
-				AND dr.`time_end` BETWEEN '.$dateBetween.'
+				AND dr.`time_start` BETWEEN '.$date_between.'
+				AND dr.`time_end` BETWEEN '.$date_between.'
 			) AS totalPageViewed
 		FROM `'._DB_PREFIX_.'category` ca
 		LEFT JOIN `'._DB_PREFIX_.'category_lang` calang ON (ca.`id_category` = calang.`id_category` AND calang.`id_lang` = '.(int)$id_lang.Shop::addSqlRestrictionOnLang('calang').')
@@ -183,7 +184,7 @@ class StatsBestCategories extends ModuleGrid
 				LEFT OUTER JOIN `'._DB_PREFIX_.'order_detail` cp ON pr.`id_product` = cp.`product_id`
 				LEFT JOIN `'._DB_PREFIX_.'orders` o ON o.`id_order` = cp.`id_order`
 				WHERE o.valid = 1
-				AND o.invoice_date BETWEEN '.$dateBetween.'
+				AND o.invoice_date BETWEEN '.$date_between.'
 				GROUP BY pr.`id_product`
 			) t ON t.`id_product` = pr.`id_product`
 		) t	ON t.`id_product` = capr.`id_product`
@@ -192,13 +193,13 @@ class StatsBestCategories extends ModuleGrid
 		HAVING ca.`id_category` != 1';
 		if (Validate::IsName($this->_sort))
 		{
-			$this->_query .= ' ORDER BY `'.$this->_sort.'`';
+			$this->query .= ' ORDER BY `'.$this->_sort.'`';
 			if (isset($this->_direction) && Validate::isSortDirection($this->_direction))
-				$this->_query .= ' '.$this->_direction;
+				$this->query .= ' '.$this->_direction;
 		}
 		if (($this->_start === 0 || Validate::IsUnsignedInt($this->_start)) && Validate::IsUnsignedInt($this->_limit))
-			$this->_query .= ' LIMIT '.$this->_start.', '.($this->_limit);
-		$values = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($this->_query);
+			$this->query .= ' LIMIT '.$this->_start.', '.($this->_limit);
+		$values = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($this->query);
 		foreach ($values as &$value)
 			$value['totalPriceSold'] = Tools::displayPrice($value['totalPriceSold'], $currency);
 		$this->_values = $values;
