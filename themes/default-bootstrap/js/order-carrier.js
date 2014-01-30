1448152be70109034ecaded035e5f61f9e088f35
gRoussac <gregory.roussac@prestashop.com>
2014-01-30 16:19:54 +0100

    // move onchange to order-carrier.js

--- themes/default-bootstrap/js/order-carrier.js
+++ themes/default-bootstrap/js/order-carrier.js
@@ -32,6 +32,15 @@ $(document).ready(function() {
 
 	if (typeof cart_gift != 'undefined' && cart_gift && $('input#gift').is(':checked'))
 		$('p#gift_div').show();
+
+	$('input[data-js=delivery_option_radio]').on('change', function(){
+		var key = $(this).data('key');
+		var id_address = parseInt($(this).data('id_address'));
+		if (orderProcess == 'order' && key && id_address)
+			updateExtraCarrier(key, id_address);
+		else if(orderProcess == 'order-opc' && typeof updateCarrierSelectionAndGift !== 'undefined')
+			updateCarrierSelectionAndGift();
+	});
 });
 
 function acceptCGV()
