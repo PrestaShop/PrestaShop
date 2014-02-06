83d300a48d3a5dc06a574391f335d8ef308952a5
gRoussac <gregory.roussac@prestashop.com>
2014-02-06 12:10:24 +0100

    // IN : add ignore if meta alreeady in base

--- install-dev/upgrade/sql/1.6.0.4.sql
+++ install-dev/upgrade/sql/1.6.0.4.sql
@@ -1,3 +1,3 @@
 SET NAMES 'utf8';
 
-INSERT INTO `PREFIX_meta` (`id_meta`, `page`, `configurable`) VALUES (NULL, 'product', '0'), (NULL, 'category', '0'), (NULL, 'cms', '0');
+INSERT IGNORE INTO `PREFIX_meta` (`id_meta`, `page`, `configurable`) VALUES (NULL, 'product', '0'), (NULL, 'category', '0'), (NULL, 'cms', '0');
