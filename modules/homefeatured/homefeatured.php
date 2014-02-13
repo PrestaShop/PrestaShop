93d50629ab1f4f3ee9c78e22ba200df78d0d5c63
emilien-puget <puget.e@gmail.com>
2014-02-13 10:21:19 +0100

    //homefeatured : norm

--- modules/homefeatured/homefeatured.php
+++ modules/homefeatured/homefeatured.php
@@ -29,25 +29,23 @@ if (!defined('_PS_VERSION_'))
 
 class HomeFeatured extends Module
 {
-	private $_html = '';
-	private $_postErrors = array();
 
-	function __construct()
+	public function __construct()
 	{
 		$this->name = 'homefeatured';
 		$this->tab = 'front_office_features';
-		$this->version = '1.2';
+		$this->version = '1.3';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
 		$this->bootstrap = true;
-		parent::__construct();	
+		parent::__construct();
 
 		$this->displayName = $this->l('Featured products on the homepage');
 		$this->description = $this->l('Displays featured products in the central column of your homepage.');
 	}
 
-	function install()
+	public function install()
 	{
 		$this->_clearCache('homefeatured.tpl');
 		Configuration::updateValue('HOME_FEATURED_NBR', 8);
@@ -61,31 +59,35 @@ class HomeFeatured extends Module
 			|| !$this->registerHook('displayHomeTabContent')
 		)
 			return false;
+
 		return true;
 	}
-	
+
 	public function uninstall()
 	{
 		$this->_clearCache('homefeatured.tpl');
 		$this->_clearCache('tab.tpl');
+
 		return parent::uninstall();
 	}
 
 	public function getContent()
 	{
 		$output = '';
+		$errors = array();
 		if (Tools::isSubmit('submitHomeFeatured'))
 		{
 			$nbr = (int)Tools::getValue('HOME_FEATURED_NBR');
-			if (!$nbr OR $nbr <= 0 OR !Validate::isInt($nbr))
+			if (!$nbr || $nbr <= 0 || !Validate::isInt($nbr))
 				$errors[] = $this->l('An invalid number of products has been specified.');
 			else
-				Configuration::updateValue('HOME_FEATURED_NBR', (int)($nbr));
-			if (isset($errors) AND sizeof($errors))
+				Configuration::updateValue('HOME_FEATURED_NBR', (int)$nbr);
+			if (isset($errors) && count($errors))
 				$output .= $this->displayError(implode('<br />', $errors));
 			else
 				$output .= $this->displayConfirmation($this->l('Your settings have been updated.'));
 		}
+
 		return $output.$this->renderForm();
 	}
 
@@ -110,17 +112,20 @@ class HomeFeatured extends Module
 		{
 			$category = new Category(Context::getContext()->shop->getCategory(), (int)Context::getContext()->language->id);
 			$nb = (int)Configuration::get('HOME_FEATURED_NBR');
-			$products = $category->getProducts((int)Context::getContext()->language->id, 1, ($nb ? $nb : 8), "position");
+			$products = $category->getProducts((int)Context::getContext()->language->id, 1, ($nb ? $nb : 8), 'position');
 
-			$this->smarty->assign(array(
-				'products' => $products,
-				'add_prod_display' => Configuration::get('PS_ATTRIBUTE_CATEGORY_DISPLAY'),
-				'homeSize' => Image::getSize(ImageType::getFormatedName('home')),
-			));
+			$this->smarty->assign(
+				array(
+					'products' => $products,
+					'add_prod_display' => Configuration::get('PS_ATTRIBUTE_CATEGORY_DISPLAY'),
+					'homeSize' => Image::getSize(ImageType::getFormatedName('home')),
+				)
+			);
 		}
+
 		return $this->display(__FILE__, 'homefeatured.tpl', $this->getCacheId());
 	}
-	
+
 	public function hookDisplayHomeTabContent($params)
 	{
 		return $this->hookDisplayHome($params);
@@ -140,7 +145,7 @@ class HomeFeatured extends Module
 	{
 		$this->_clearCache('homefeatured.tpl');
 	}
-	
+
 	public function renderForm()
 	{
 		$fields_form = array(
@@ -164,10 +169,10 @@ class HomeFeatured extends Module
 				)
 			),
 		);
-		
+
 		$helper = new HelperForm();
 		$helper->show_toolbar = false;
-		$helper->table =  $this->table;
+		$helper->table = $this->table;
 		$lang = new Language((int)Configuration::get('PS_LANG_DEFAULT'));
 		$helper->default_form_language = $lang->id;
 		$helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') ? Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG') : 0;
@@ -185,11 +190,11 @@ class HomeFeatured extends Module
 
 		return $helper->generateForm(array($fields_form));
 	}
-	
+
 	public function getConfigFieldsValues()
 	{
 		return array(
 			'HOME_FEATURED_NBR' => Tools::getValue('HOME_FEATURED_NBR', Configuration::get('HOME_FEATURED_NBR')),
-			);
+		);
 	}
 }
--- modules/homefeatured/views/templates/hook/homefeatured.tpl
+++ modules/homefeatured/views/templates/hook/homefeatured.tpl
@@ -33,7 +33,7 @@
 			{assign var='nbLi' value=$products|@count}
 			{math equation="nbLi/nbItemsPerLine" nbLi=$nbLi nbItemsPerLine=$nbItemsPerLine assign=nbLines}
 			{math equation="nbLines*liHeight" nbLines=$nbLines|ceil liHeight=$liHeight assign=ulHeight}
-			<ul style="height:{$ulHeight}px;">
+			<ul style="height:{$ulHeight|escape:'html'}px;">
 			{foreach from=$products item=product name=homeFeaturedProducts}
 				{math equation="(total%perLine)" total=$smarty.foreach.homeFeaturedProducts.total perLine=$nbItemsPerLine assign=totModulo}
 				{if $totModulo == 0}{assign var='totModulo' value=$nbItemsPerLine}{/if}
