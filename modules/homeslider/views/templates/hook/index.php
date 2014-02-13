2720e8b6bbe9916d0be813f2ac66f128b3fbcd65
emilien-puget <puget.e@gmail.com>
2014-02-13 12:01:53 +0100

    //homeslider : norm

--- modules/homeslider/views/templates/hook/index.php
+++ modules/homeslider/views/templates/hook/index.php
@@ -19,7 +19,7 @@
 * needs please refer to http://www.prestashop.com for more information.
 *
 *  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2014 PrestaShop SA
+*  @copyright  2007-2014 PrestaShop SA
 *  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
@@ -31,5 +31,5 @@ header('Cache-Control: no-store, no-cache, must-revalidate');
 header('Cache-Control: post-check=0, pre-check=0', false);
 header('Pragma: no-cache');
 
-header('Location: ../../../../../');
+header('Location: ../');
 exit;
