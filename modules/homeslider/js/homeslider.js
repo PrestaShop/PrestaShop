69dcf648abde514203627cd4cbc54e8440773fa9
gRoussac <gregory.roussac@prestashop.com>
2014-02-13 14:30:24 +0100

    // prevent bxslider undefined

--- modules/homeslider/js/homeslider.js
+++ modules/homeslider/js/homeslider.js
@@ -35,16 +35,21 @@ $(function(){
     if (typeof(homeslider_width) == 'undefined')
         homeslider_width = 779;
 
-	$('#homeslider').bxSlider({
-        maxSlides: 1,
-        slideWidth: homeslider_width,
-		infiniteLoop: homeslider_loop,
-		hideControlOnEnd: true,
-		pager: true,
-		autoHover: true,
-		auto: homeslider_loop,
-		speed: homeslider_speed,
-		pause: homeslider_pause,
-		controls: false
-	});
+	if (!!$.prototype.bxSlider)
+		$('#homeslider').bxSlider({
+	        maxSlides: 1,
+	        slideWidth: homeslider_width,
+			infiniteLoop: homeslider_loop,
+			hideControlOnEnd: true,
+			pager: true,
+			autoHover: true,
+			auto: homeslider_loop,
+			speed: homeslider_speed,
+			pause: homeslider_pause,
+			controls: false
+		});
+
+    $('.homeslider-description').click(function () {
+        window.location.href = $(this).prev('a').prop('href');
+    });
 });
