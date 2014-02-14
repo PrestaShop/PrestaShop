b7571bc428b01f0d70539e28361dd118884a73e8
emilien-puget <puget.e@gmail.com>
2014-02-14 15:54:30 +0100

    //smallfix

--- controllers/front/SupplierController.php
+++ controllers/front/SupplierController.php
@@ -93,21 +93,31 @@ class SupplierControllerCore extends FrontController
 	 */
 	protected function assignOne()
 	{
-		$this->supplier->description = Tools::nl2br(trim($this->supplier->description));
-		$nbProducts = $this->supplier->getProducts($this->supplier->id, null, null, null, $this->orderBy, $this->orderWay, true);
-		$this->pagination((int)$nbProducts);
+		if (Configuration::get('PS_DISPLAY_SUPPLIERS'))
+		{
+			$this->supplier->description = Tools::nl2br(trim($this->supplier->description));
+			$nbProducts = $this->supplier->getProducts($this->supplier->id, null, null, null, $this->orderBy, $this->orderWay, true);
+			$this->pagination((int)$nbProducts);
 
-		$products = $this->supplier->getProducts($this->supplier->id, $this->context->cookie->id_lang, (int)$this->p, (int)$this->n, $this->orderBy, $this->orderWay);
-		$this->addColorsToProductList($products);
+			$products = $this->supplier->getProducts($this->supplier->id, $this->context->cookie->id_lang, (int)$this->p, (int)$this->n, $this->orderBy, $this->orderWay);
+			$this->addColorsToProductList($products);
 
-		$this->context->smarty->assign(array(
-			'nb_products' => $nbProducts,
-			'products' => $products,
-			'path' => ($this->supplier->active ? Tools::safeOutput($this->supplier->name) : ''),
-			'supplier' => $this->supplier,
-			'comparator_max_item' => Configuration::get('PS_COMPARATOR_MAX_ITEM'),
-			'body_classes' => array($this->php_self.'-'.$this->supplier->id, $this->php_self.'-'.$this->supplier->link_rewrite)
-		));
+			$this->context->smarty->assign(
+				array(
+					'nb_products' => $nbProducts,
+					'products' => $products,
+					'path' => ($this->supplier->active ? Tools::safeOutput($this->supplier->name) : ''),
+					'supplier' => $this->supplier,
+					'comparator_max_item' => Configuration::get('PS_COMPARATOR_MAX_ITEM'),
+					'body_classes' => array(
+						$this->php_self.'-'.$this->supplier->id,
+						$this->php_self.'-'.$this->supplier->link_rewrite
+					)
+				)
+			);
+		}
+		else
+			Tools::redirect('index.php?controller=404');
 	}
 
 	/**
@@ -134,6 +144,6 @@ class SupplierControllerCore extends FrontController
 			));
 		}
 		else
-			$this->context->smarty->assign('nbSuppliers', 0);
+			Tools::redirect('index.php?controller=404');
 	}
 }
