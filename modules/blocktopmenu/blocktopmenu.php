941a06004c139cbdebbd5c82149c995d894f75fc
jeromenadaud <jerome.nadaud@prestashop.com>
2014-02-19 17:18:08 +0100

    [-] MO : Blocktopmenu - Improve queries number in categories display

--- modules/blocktopmenu/blocktopmenu.php
+++ modules/blocktopmenu/blocktopmenu.php
@@ -52,7 +52,7 @@ class Blocktopmenu extends Module
 	{
 		$this->name = 'blocktopmenu';
 		$this->tab = 'front_office_features';
-		$this->version = 1.9;
+		$this->version = 1.10;
 		$this->author = 'PrestaShop';
 
 		$this->bootstrap = true;
@@ -340,7 +340,7 @@ class Blocktopmenu extends Module
 			switch (substr($item, 0, strlen($value[1])))
 			{
 				case 'CAT':
-					$this->getCategory($id, $id_lang, $id_shop);
+					$this->_menu .= $this->generateCategoriesMenu(Category::getNestedCategories($id, $id_lang, true, $this->user_groups));
 					break;
 
 				case 'PRD':
@@ -462,53 +462,54 @@ class Blocktopmenu extends Module
 		return $html;
 	}
 
-	private function getCategory($id_category, $id_lang = false, $id_shop = false)
+	private function generateCategoriesMenu($categories)
 	{
-		$id_lang = $id_lang ? (int)$id_lang : (int)Context::getContext()->language->id;
-		$category = new Category((int)$id_category, (int)$id_lang);
+		$html = '';
 
-		if ($category->level_depth > 1)
-			$category_link = $category->getLink();
-		else
-			$category_link = $this->context->link->getPageLink('index');
-
-		if (is_null($category->id))
-			return;
-
-		$children = Category::getChildren((int)$id_category, (int)$id_lang, true, (int)$id_shop);
-		$selected = ($this->page_name == 'category' && ((int)Tools::getValue('id_category') == $id_category)) ? ' class="sfHoverForce"' : '';
-
-		$is_intersected = array_intersect($category->getGroups(), $this->user_groups);
-		// filter the categories that the user is allowed to see and browse
-		if (!empty($is_intersected))
+		foreach ($categories as $key => $category)
 		{
-			$this->_menu .= '<li '.$selected.'>';
-			$this->_menu .= '<a href="'.Tools::HtmlEntitiesUTF8($category_link).'" title="'.$category->name.'">'.$category->name.'</a>';
-
-			if (count($children))
+			if ($category['level_depth'] > 1)
 			{
-				$this->_menu .= '<ul>';
+				$cat = new Category($category['id_category']);
+				$link = Tools::HtmlEntitiesUTF8($cat->getLink());
+			}
+			else
+				$link = $this->context->link->getPageLink('index');
 
-				foreach ($children as $child)
-					$this->getCategory((int)$child['id_category'], (int)$id_lang, (int)$child['id_shop']);
+			$html .= '<li'.(($this->page_name == 'category'
+				&& (int)Tools::getValue('id_category') == $category['id_category']) ? ' class="sfHoverForce"' : '').'>';
+			$html .= '<a href="'.$link.'" title="'.$category['name'].'">'.$category['name'].'</a>';
 
-				if ($category->level_depth == 2)
+			if (isset($category['children']) && !empty($category['children']))
+			{
+				$html .= '<ul>';
+				$html .= $this->generateCategoriesMenu($category['children']);
+
+				if ($category['level_depth'] == 2)
 				{
 					$files = scandir(_PS_CAT_IMG_DIR_);
-					$this->_menu .= '<li id="category-thumbnail">';
 
-					foreach ($files as $file)
-						if (preg_match('/'.$category->id.'-([0-9])?_thumb.jpg/i', $file) === 1)
-							$this->_menu .= '<div><img src="'.$this->context->link->getMediaLink(_THEME_CAT_DIR_.$file).'" alt="'.Tools::SafeOutput($category->name).'" title="'.Tools::SafeOutput($category->name).'" class="imgm" /></div>';
+					if (count($files) > 0)
+					{
+						$html .= '<li id="category-thumbnail">';
 
-					$this->_menu .= '</li>';
+						foreach ($files as $file)
+							if (preg_match('/'.$category['id_category'].'-([0-9])?_thumb.jpg/i', $file) === 1)
+								$html .= '<div><img src="'.$this->context->link->getMediaLink(_THEME_CAT_DIR_.$file)
+								.'" alt="'.Tools::SafeOutput($category['name']).'" title="'
+								.Tools::SafeOutput($category['name']).'" class="imgm" /></div>';
+
+						$html .= '</li>';
+					}
 				}
 
-				$this->_menu .= '</ul>';
+				$html .= '</ul>';
 			}
 
-			$this->_menu .= '</li>';
+			$html .= '</li>';
 		}
+
+		return $html;
 	}
 
 	private function getCMSMenuItems($parent, $depth = 1, $id_lang = false)
