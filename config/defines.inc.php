a6865e721b835596c01fa23da42a1480437e5d32
Vincent Augagneur <vincent.augagneur@prestashop.com>
2014-02-19 18:15:56 +0100

    //removed _PS_DISPLAY_COMPATIBILITY_WARNING_ from _PS_MODE_DEV_

--- config/defines.inc.php
+++ config/defines.inc.php
@@ -26,20 +26,18 @@
 
 /* Debug only */
 define('_PS_MODE_DEV_', true);
+/* Compatibility warning */
+define('_PS_DISPLAY_COMPATIBILITY_WARNING_', true);
 if (_PS_MODE_DEV_)
 {
 	@ini_set('display_errors', 'on');
 	@error_reporting(E_ALL | E_STRICT);
 	define('_PS_DEBUG_SQL_', true);
-	/* Compatibility warning */
-	define('_PS_DISPLAY_COMPATIBILITY_WARNING_', true);
 }
 else
 {
 	@ini_set('display_errors', 'off');
 	define('_PS_DEBUG_SQL_', false);
-	/* Compatibility warning */
-	define('_PS_DISPLAY_COMPATIBILITY_WARNING_', false);
 }
 
 define('_PS_DEBUG_PROFILING_', false);
