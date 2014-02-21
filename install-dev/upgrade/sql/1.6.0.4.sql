4ef0be1c8c6f4bca32a7317d3d06045b46ba72e0
gRoussac <gregory.roussac@prestashop.com>
2014-02-21 18:25:04 +0100

    // follow up #PSCFV-10494, update orders before

--- install-dev/upgrade/sql/1.6.0.4.sql
+++ install-dev/upgrade/sql/1.6.0.4.sql
@@ -5,3 +5,5 @@ INSERT IGNORE INTO `PREFIX_meta` (`id_meta`, `page`, `configurable`) VALUES (NUL
 ALTER TABLE `PREFIX_employee` ADD `optin` tinyint(1) unsigned NOT NULL default '1' AFTER `active`;
 
 ALTER IGNORE TABLE `PREFIX_meta` ADD UNIQUE (`page`);
+
+UPDATE `PREFIX_orders` SET module = 'free_order' where total_paid = 0 AND module LIKE '';
