b4ff0f2f637febe68748f0c255c8434205143cee
emilien-puget <puget.e@gmail.com>
2014-02-26 10:18:07 +0100

    // tinymce menu modified

--- js/tinymce.inc.js
+++ js/tinymce.inc.js
@@ -21,7 +21,16 @@ function tinySetup(config)
 		language: iso,
 		skin: "prestashop",
 		resize: false,
-		extended_valid_elements : "em[class|name|id]"
+		extended_valid_elements : "em[class|name|id]",
+		menu: {
+			edit: {title: 'Edit', items: 'undo redo | cut copy paste | selectall'},
+			insert: {title: 'Insert', items: 'media image link | pagebreak'},
+			view: {title: 'View', items: 'visualaid'},
+			format: {title: 'Format', items: 'bold italic underline strikethrough superscript subscript | formats | removeformat'},
+			table: {title: 'Table', items: 'inserttable tableprops deletetable | cell row column'},
+			tools: {title: 'Tools', items: 'code'}
+		}
+
 	}
 
 	$.each(default_config, function(index, el)
