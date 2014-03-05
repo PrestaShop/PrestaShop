864cbb9d07cf989bb736c3dcbe4919fd1515baa9
Jerome Nadaud <jerome.nadaud@gmail.com>
2014-03-05 15:32:54 +0100

    // Fix some filemanager security problems

--- admin-dev/filemanager/upload.php
+++ admin-dev/filemanager/upload.php
@@ -11,10 +11,10 @@ $path_pos = strpos($storeFolder, $current_path);
 $thumb_pos = strpos($_POST['path_thumb'], $thumbs_base_path);
 if ($path_pos !== 0
 	|| $thumb_pos !== 0
-	|| strpos($storeFolderThumb, '../', strlen($thumbs_base_path)) !== false
-	|| strpos($storeFolderThumb, './', strlen($thumbs_base_path)) !== false
-	|| strpos($storeFolder, '../', strlen($current_path)) !== false
-	|| strpos($storeFolder, './', strlen($current_path)) !== false
+	|| strpos($storeFolderThumb, '..'.DIRECTORY_SEPARATOR, strlen($thumbs_base_path)) !== false
+	|| strpos($storeFolderThumb, '.'.DIRECTORY_SEPARATOR, strlen($thumbs_base_path)) !== false
+	|| strpos($storeFolder, '..'.DIRECTORY_SEPARATOR, strlen($current_path)) !== false
+	|| strpos($storeFolder, '.'.DIRECTORY_SEPARATOR, strlen($current_path)) !== false
 )
 	die('wrong path');
 
