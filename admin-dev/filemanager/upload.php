589606f8eff79a51d6b62decafa1f3407020bda9
Jerome Nadaud <jerome.nadaud@gmail.com>
2014-03-06 11:19:40 +0100

    // Filemanager smallfix

--- admin-dev/filemanager/upload.php
+++ admin-dev/filemanager/upload.php
@@ -7,8 +7,8 @@ include('include/utils.php');
 $storeFolder = $_POST['path'];
 $storeFolderThumb = $_POST['path_thumb'];
 
-$path_pos = strpos(realpath($storeFolder), realpath($current_path));
-$thumb_pos = strpos(realpath($_POST['path_thumb']), realpath($thumbs_base_path));
+$path_pos = strpos($storeFolder, $current_path);
+$thumb_pos = strpos($_POST['path_thumb'], $thumbs_base_path);
 
 if ($path_pos === false || $thumb_pos === false
 	|| preg_match('/\.{1,2}[\/|\\\]/', $_POST['path_thumb']) !== 0
