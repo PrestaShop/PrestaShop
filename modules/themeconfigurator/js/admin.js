3568d7a515030b96dcd91e9745697f6708c64bd5
Shipow <shipowlata@gmail.com>
2014-03-13 14:37:25 +0100

    [*] BO : Module Theme Configurator

--- modules/themeconfigurator/js/admin.js
+++ modules/themeconfigurator/js/admin.js
@@ -24,31 +24,41 @@
  */
 
 jQuery(document).ready(function() {
-	$('.button.new-item').click(function() {
-		var item_container = $(this).parent().parent('.new-item');
-		item_container.toggleClass('active').children('.item-container').slideToggle();
+
+	//show new item panel
+	$('.button-new-item').on('click', function() {
+		$('.new-item').find('.item-container').slideToggle();
 	});
-	$('.button-edit').click(function() {
-		var item_container = $(this).parents('.item');
-		item_container.toggleClass('active').children('.item-container').slideToggle();
+	// cancel new item
+	$('.button-new-item-cancel').on('click',function() {
+		$('.new-item').find('.item-container').slideToggle();
 	});
-	$('.button-close').click(function() {
-		var item_container = $(this).parents('.item');
-		item_container.toggleClass('active').children('.item-container').slideToggle();
-	});	
-	
-	$('.lang-flag').click(function() {
-		var lang_id = (this.id).substr(5);
-		$('.lang-flag').each(function () {
-			$(this).removeClass('active');
-		});
-		$(this).addClass('active');
-		$('.lang-content').each(function () {
-			$(this).hide();
-		});
-		$('#items-'+lang_id).show();
-	});	
-	$('.new-lang-flag').click(function() {
+	//show item content edit panel
+	$('.button-edit').on('click', function(e) {
+		e.preventDefault();
+		var $item_container = $(this).closest('.item');
+		$item_container.find('.item-container').slideToggle();
+		$(this).find('.button-edit-edit').toggleClass('hide');
+		$(this).find('.button-edit-close').toggleClass('hide');
+	});
+	//cancel item edit
+	$('.button-item-edit-cancel').on('click',function(){
+		$(this).closest('form').find('.button-edit .button-edit-edit').toggleClass('hide');
+		$(this).closest('form').find('.button-edit .button-edit-close').toggleClass('hide');
+		$(this).closest('form').find('.item-container').slideToggle();
+	})
+
+	//delete item
+	$('.link-item-delete').on('click',function(e){
+		e.preventDefault();
+		var $form = $(this).closest('form');
+		var $hiddenField = $("<input type='hidden' name='removeItem'/>");
+		$hiddenField.appendTo($form);
+		$form.submit();
+	});
+
+	// set language for new item
+	$('.new-lang-flag').on('click', function() {
 		var lang_id = (this.id).substr(5);
 		$('.new-lang-flag').each(function () {
 			$(this).removeClass('active');
--- modules/themeconfigurator/themeconfigurator.php
+++ modules/themeconfigurator/themeconfigurator.php
@@ -567,7 +567,7 @@ class ThemeConfigurator extends Module
 			{
 				$module_instance = Module::getInstanceByName($module['name']);
 				if (Validate::isLoadedObject($module_instance) && method_exists($module_instance, 'getContent'))
-					$desc = '<a class="btn btn-link" href="'.$this->context->link->getAdminLink('AdminModules', true).'&configure='.urlencode($module_instance->name).'&tab_module='.$module_instance->tab.'&module_name='.urlencode($module_instance->name).'">'.$this->l('Configure').' <i class="icon-external-link"></i></a>';
+					$desc = '<a class="btn btn-default" href="'.$this->context->link->getAdminLink('AdminModules', true).'&configure='.urlencode($module_instance->name).'&tab_module='.$module_instance->tab.'&module_name='.urlencode($module_instance->name).'">'.$this->l('Configure').' <i class="icon-external-link"></i></a>';
 			}
 			if (!$desc && isset($module['desc']) && $module['desc'])
 				$desc = $module['desc'];
