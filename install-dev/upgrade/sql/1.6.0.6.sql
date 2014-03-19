e6f4720f42869e4e12e7b53175ba324a894fe529
Vincent Augagneur <vincent.augagneur@prestashop.com>
2014-03-19 08:41:52 +0100

    //added new field in tab to hide tab with host_mode

--- install-dev/upgrade/sql/1.6.0.6.sql
+++ install-dev/upgrade/sql/1.6.0.6.sql
@@ -15,3 +15,5 @@ INSERT IGNORE INTO `PREFIX_meta` (`id_meta`, `page`, `configurable`) VALUES
 INSERT IGNORE INTO `PREFIX_theme_meta` ( `id_theme` , `id_meta` , `left_column` , `right_column` )
   SELECT `PREFIX_theme`.`id_theme` , `PREFIX_meta`.`id_meta` , `default_left_column` , `default_right_column`
   FROM `PREFIX_theme` , `PREFIX_meta`;
+  
+ALTER TABLE `PREFIX_tab` ADD `hide_host_mode` tinyint(1) NOT NULL DEFAULT '0' AFTER  `active`;
