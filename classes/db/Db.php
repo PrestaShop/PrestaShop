bac6e94b1a13669eb8780bf547c789c94a030e6e
Vincent Augagneur <vincent.augagneur@prestashop.com>
2014-03-19 09:43:52 +0100

    //added new config file db_slave_server.inc.php

--- classes/db/Db.php
+++ classes/db/Db.php
@@ -181,11 +181,12 @@ abstract class DbCore
 		if (!self::$_servers)
 			self::$_servers = array(
 				array('server' => _DB_SERVER_, 'user' => _DB_USER_, 'password' => _DB_PASSWD_, 'database' => _DB_NAME_), /* MySQL Master server */
-				// Add here your slave(s) server(s)
-					// array('server' => '192.168.0.15', 'user' => 'rep', 'password' => '123456', 'database' => 'rep'),
-					// array('server' => '192.168.0.3', 'user' => 'myuser', 'password' => 'mypassword', 'database' => 'mydatabase'),
 			);
-
+		
+		// Add here your slave(s) server(s) in this file
+		if (file_exists(_PS_ROOT_DIR_.'/config/db_slave_server.inc.php'))
+			self::$_servers = array_merge(self::$_servers, include_once(_PS_ROOT_DIR_.'/config/db_slave_server.inc.php'));
+			
 		$total_servers = count(self::$_servers);
 		if ($master || $total_servers == 1)
 			$id_server = 0;
