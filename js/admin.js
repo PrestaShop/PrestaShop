fae8377e414d7ad9481b1a174f5acb6a9205e3ce
Shipow <shipowlata@gmail.com>
2014-03-26 15:21:50 +0100

    [*] BO : Order details - fix repay shipping costs when returning products - PSCSX-1383

--- js/admin.js
+++ js/admin.js
@@ -435,17 +435,17 @@ function selectCheckbox(obj)
 	$(obj).parent().parent().find('td.cancelCheck input[type=checkbox]').attr("checked", true);
 }
 
-function toggleShippingCost(obj)
+function toggleShippingCost()
 {
-	generateDiscount = $(obj).parent().find('#generateDiscount').attr("checked");
-	generateCreditSlip = $(obj).parent().find('#generateCreditSlip').attr("checked");
+	generateDiscount = $('#generateDiscount').attr("checked");
+	generateCreditSlip = $('#generateCreditSlip').attr("checked");
 	if (generateDiscount != 'checked' && generateCreditSlip != 'checked')
 	{
-		$(obj).parent().find('#spanShippingBack input[type=checkbox]').attr("checked", false);
-		$(obj).parent().find('#spanShippingBack').css('display', 'none');
+		$('#spanShippingBack input[type=checkbox]').attr("checked", false);
+		$('#spanShippingBack').css('display', 'none');
 	}
 	else
-		$(obj).parent().find('#spanShippingBack').css('display', 'block');
+		$('#spanShippingBack').css('display', 'block');
 }
 
 function orderOverwriteMessage(sl, text)
