4a3dd5ac99f8c77575a4a903745e30ff03dd4e0c
gRoussac <gregory.roussac@prestashop.com>
2014-04-03 15:02:03 +0200

    // small fix follow up https://github.com/PrestaShop/PrestaShop/pull/1627

--- controllers/admin/AdminShopController.php
+++ controllers/admin/AdminShopController.php
@@ -251,8 +251,8 @@ class AdminShopControllerCore extends AdminController
 
 		$result = parent::postProcess();
 
-		if ($result !== false && (Tools::isSubmit('submitAddshopAndStay') || Tools::isSubmit('submitAddshop')) && $result->id_category != Configuration::get('PS_HOME_CATEGORY', null, $result->id_shop_group, $result->id))
-			Configuration::updateValue('PS_HOME_CATEGORY', (int)$result->id_category, false, $result->id_shop_group, $result->id);
+		if ($result !== false && (Tools::isSubmit('submitAddshopAndStay') || Tools::isSubmit('submitAddshop')) && (int)$result->id_category != (int)Configuration::get('PS_HOME_CATEGORY', null, null, (int)$result->id))
+			Configuration::updateValue('PS_HOME_CATEGORY', (int)$result->id_category, false, null, (int)$result->id);
 
 		if ($this->redirect_after)
 			$this->redirect_after .= '&id_shop_group='.$this->id_shop_group;
