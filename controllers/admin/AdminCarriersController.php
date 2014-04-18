520d9d9ee25bc6f5b46a52f0fdb50d458c92e688
gRoussac <gregory.roussac@prestashop.com>
2014-04-18 15:26:44 +0200

    // norms

--- controllers/admin/AdminCarriersController.php
+++ controllers/admin/AdminCarriersController.php
@@ -131,11 +131,7 @@ class AdminCarriersControllerCore extends AdminController
 	public function renderList()
 	{
 		$this->_select = 'b.*';
-		$this->_join = 'LEFT JOIN `'._DB_PREFIX_.'carrier_lang` b ON a.id_carrier = b.id_carrier'.Shop::addSqlRestrictionOnLang('b').' AND b.id_lang = '.$this->context->language->id.'
-							LEFT JOIN `'._DB_PREFIX_.'carrier_tax_rules_group_shop` ctrgs ON (a.`id_carrier` = ctrgs.`id_carrier`
-								AND ctrgs.id_shop='.(int)$this->context->shop->id.')';
-		//$this->_where = ';
-
+		$this->_join = 'LEFT JOIN `'._DB_PREFIX_.'carrier_lang` b ON a.id_carrier = b.id_carrier'.Shop::addSqlRestrictionOnLang('b').' AND b.id_lang = '.$this->context->language->id.' LEFT JOIN `'._DB_PREFIX_.'carrier_tax_rules_group_shop` ctrgs ON (a.`id_carrier` = ctrgs.`id_carrier` AND ctrgs.id_shop='.(int)$this->context->shop->id.')';
 		return parent::renderList();
 	}
 
