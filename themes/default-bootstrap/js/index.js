f4de33652032c2628c54e79e499606377ddd3b67
gRoussac <gregory.roussac@prestashop.com>
2014-04-28 19:18:03 +0200

    // slight change

--- themes/default-bootstrap/js/index.js
+++ themes/default-bootstrap/js/index.js
@@ -24,6 +24,5 @@
 */
 
 $(document).ready(function(){
-	$('#home-page-tabs li:first').addClass('active');
-	$('#index .tab-content ul:first').addClass('active');
+	$('#home-page-tabs li:first, #index .tab-content ul:first').addClass('active');
 });
