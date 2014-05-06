07041d1676d5cf91864c2df9cd1172cce23d4ba6
gRoussac <gregory.roussac@prestashop.com>
2014-05-06 11:27:56 +0200

    [-] FO : Fix bug #PSCFV-11922, bad carrier default selection

--- controllers/front/ParentOrderController.php
+++ controllers/front/ParentOrderController.php
@@ -485,7 +485,7 @@ class ParentOrderControllerCore extends FrontController
 		$address = new Address($this->context->cart->id_address_delivery);
 		$id_zone = Address::getZoneById($address->id);
 		$carriers = $this->context->cart->simulateCarriersOutput(null, true);
-		$checked = $this->context->cart->simulateCarrierSelectedOutput();
+		$checked = $this->context->cart->simulateCarrierSelectedOutput(false);
 		$delivery_option_list = $this->context->cart->getDeliveryOptionList();
 		$delivery_option = $this->context->cart->getDeliveryOption(null, false);
 		$this->setDefaultCarrierSelection($delivery_option_list);
