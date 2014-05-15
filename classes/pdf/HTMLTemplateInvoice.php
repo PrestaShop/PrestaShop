36d011c9a437a4dfb965bc316c1f8274040259d1
fram <fm.de.jouvencel@gmail.com>
2014-05-15 18:28:34 +0200

    // more debug for testing

--- classes/pdf/HTMLTemplateInvoice.php
+++ classes/pdf/HTMLTemplateInvoice.php
@@ -90,25 +90,32 @@ class HTMLTemplateInvoiceCore extends HTMLTemplate
 	 */
 	public function getTaxTabContent()
 	{
-			$address = new Address((int)$this->order->{Configuration::get('PS_TAX_ADDRESS_TYPE')});
-			$tax_exempt = Configuration::get('VATNUMBER_MANAGEMENT')
-								&& !empty($address->vat_number)
-								&& $address->id_country != Configuration::get('VATNUMBER_COUNTRY');
-			$carrier = new Carrier($this->order->id_carrier);
-			
-			$this->smarty->assign(array(
-				'tax_exempt' => $tax_exempt,
-				'use_one_after_another_method' => $this->order_invoice->useOneAfterAnotherTaxComputationMethod(),
-				'product_tax_breakdown' => $this->order_invoice->getProductTaxesBreakdown(),
-				'shipping_tax_breakdown' => $this->order_invoice->getShippingTaxesBreakdown($this->order),
-				'ecotax_tax_breakdown' => $this->order_invoice->getEcoTaxTaxesBreakdown(),
-				'wrapping_tax_breakdown' => $this->order_invoice->getWrappingTaxesBreakdown(),
-				'order' => $this->order,
-				'order_invoice' => $this->order_invoice,
-				'carrier' => $carrier
-			));
+		$debug = Tools::getValue('debug');
 
-			return $this->smarty->fetch($this->getTemplate('invoice.tax-tab'));
+		$address = new Address((int)$this->order->{Configuration::get('PS_TAX_ADDRESS_TYPE')});
+		$tax_exempt = Configuration::get('VATNUMBER_MANAGEMENT')
+							&& !empty($address->vat_number)
+							&& $address->id_country != Configuration::get('VATNUMBER_COUNTRY');
+		$carrier = new Carrier($this->order->id_carrier);
+			
+		$data = array(
+			'tax_exempt' => $tax_exempt,
+			'use_one_after_another_method' => $this->order_invoice->useOneAfterAnotherTaxComputationMethod(),
+			'product_tax_breakdown' => $this->order_invoice->getProductTaxesBreakdown(),
+			'shipping_tax_breakdown' => $this->order_invoice->getShippingTaxesBreakdown($this->order),
+			'ecotax_tax_breakdown' => $this->order_invoice->getEcoTaxTaxesBreakdown(),
+			'wrapping_tax_breakdown' => $this->order_invoice->getWrappingTaxesBreakdown(),
+			'order' => $debug ? null : $this->order,
+			'order_invoice' => $debug ? null : $this->order_invoice,
+			'carrier' => $debug ? null : $carrier
+		);
+
+		if ($debug)
+			return $data;
+
+		$this->smarty->assign($data);
+
+		return $this->smarty->fetch($this->getTemplate('invoice.tax-tab'));
 	}
 
 	/**
