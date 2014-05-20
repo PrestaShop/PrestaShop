59ac732cb8c5ef63736a4c28c1564e3f418bba96
gRoussac <gregory.roussac@prestashop.com>
2014-05-20 10:05:42 +0200

    // warning

--- classes/Tools.php
+++ classes/Tools.php
@@ -2324,7 +2324,9 @@ exit;
 
 	public static function str_replace_once($needle, $replace, $haystack)
 	{
-		$pos = strpos($haystack, $needle);
+		$pos = false;
+		if ($needle)
+			$pos = strpos($haystack, $needle);
 		if ($pos === false)
 			return $haystack;
 		return substr_replace($haystack, $replace, $pos, strlen($needle));
