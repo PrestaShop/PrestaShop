a11cbe1da05e6c87a9692d8cb00ea2440d32ea98
gRoussac <gregory.roussac@prestashop.com>
2014-05-27 10:48:10 +0200

    // small change

--- install-dev/upgrade/sql/1.6.0.8.sql
+++ install-dev/upgrade/sql/1.6.0.8.sql
@@ -1,6 +1,6 @@
 SET NAMES 'utf8';
-INSERT INTO `PREFIX_hook` (`id_hook` , `name` , `title` , `description` , `position` , `live_edit`) VALUES 
-(NULL , 'displayAdminOrderTabOrder', 'Display new elements in the Back Office, AdminOrder panel Order', 'This hook launches modules when the AdminOrder tab is displayed in the Back Office and extends / override Order panel tabs', '1', '0'),
-(NULL , 'displayAdminOrderContentOrder', 'Display new elements in the Back Office, AdminOrder panel Order', 'This hook launches modules when the AdminOrder tab is displayed in the Back Office and extends / override Order panel content', '1', '0'),
-(NULL , 'displayAdminOrderTabShip', 'Display new elements in the Back Office, AdminOrder Shipping', 'This hook launches modules when the AdminOrder tab is displayed in the Back Office and extends / override Shipping panel tabs', '1', '0'),
-(NULL , 'displayAdminOrderContentShip', 'Display new elements in the Back Office, AdminOrder Shipping', 'This hook launches modules when the AdminOrder tab is displayed in the Back Office and extends / override Shipping panel content', '1', '0');
+INSERT INTO `PREFIX_hook` ( `name` , `title` , `description` , `position` , `live_edit`) VALUES 
+('displayAdminOrderTabOrder', 'Display new elements in the Back Office, AdminOrder panel Order', 'This hook launches modules when the AdminOrder tab is displayed in the Back Office and extends / override Order panel tabs', '1', '0'),
+('displayAdminOrderContentOrder', 'Display new elements in the Back Office, AdminOrder panel Order', 'This hook launches modules when the AdminOrder tab is displayed in the Back Office and extends / override Order panel content', '1', '0'),
+('displayAdminOrderTabShip', 'Display new elements in the Back Office, AdminOrder Shipping', 'This hook launches modules when the AdminOrder tab is displayed in the Back Office and extends / override Shipping panel tabs', '1', '0'),
+('displayAdminOrderContentShip', 'Display new elements in the Back Office, AdminOrder Shipping', 'This hook launches modules when the AdminOrder tab is displayed in the Back Office and extends / override Shipping panel content', '1', '0');
