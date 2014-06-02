599d28cb3787b958c378ac591da129b3a1f111af
Shipow <shipowlata@gmail.com>
2014-06-02 12:23:32 +0200

    [*] BO : help

--- classes/controller/AdminController.php
+++ classes/controller/AdminController.php
@@ -1926,6 +1926,7 @@ class AdminControllerCore extends Controller
 			'check_openssl' => (extension_loaded('openssl') ? 'ok' : 'ko'),
 			'add_permission' => 1,
 			'addons_register_link' => "//addons.prestashop.com/".$language->iso_code."/login?email=".urlencode($this->context->employee->email)."&firstname=".urlencode($this->context->employee->firstname)."&lastname=".urlencode($this->context->employee->lastname)."&website=".urlencode($this->context->shop->getBaseURL())."&utm_source=back-office&utm_medium=connect-to-addons&utm_campaign=back-office-".$iso_code_caps."#createnow",
+			'addons_forgot_password_link' => "//addons.prestashop.com/".$language->iso_code."/forgot-your-password"
 		));
 		
 		$this->modals[] = array(
