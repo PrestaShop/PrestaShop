ab4cb257cb017375d188529bdd37e119b7cc2499
Julien Bourdeau <julien@sigerr.org>
2014-06-09 15:29:18 +0200

    // Display error if theme exists

--- controllers/admin/AdminThemesController.php
+++ controllers/admin/AdminThemesController.php
@@ -1616,6 +1616,11 @@ class AdminThemesControllerCore extends AdminController
 	 */
 	protected function importThemeXmlConfig(SimpleXMLElement $xml, $theme_dir = false)
 	{
+		$attr = $xml->attributes();
+		$th_name = (string)$attr->name;
+		if ($this->isThemeInstalled($th_name))
+			return array(sprintf($this->l('Theme %s already installed.'), $th_name));
+
 		$new_theme_array = array();
 		foreach ($xml->variations->variation as $variation)
 		{
