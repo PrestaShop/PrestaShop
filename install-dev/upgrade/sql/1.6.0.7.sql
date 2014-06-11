7ee6935267084698f1bd3b26a1ffbc83b42392ef
gRoussac <gregory.roussac@prestashop.com>
2014-06-11 11:52:31 +0200

    [-] IN : Fix bug #PSCSX-2367, duplicates in ps_module
    Conflicts:
    
    	install-dev/upgrade/sql/1.6.0.7.sql

--- install-dev/upgrade/sql/1.6.0.7.sql
+++ install-dev/upgrade/sql/1.6.0.7.sql
@@ -19,3 +19,4 @@ ALTER TABLE `PREFIX_orders` ADD INDEX (`current_state`);
 INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VALUES('PS_USE_HTMLPURIFIER', '1', NOW(), NOW());
 
 /* PHP:ps1607_language_code_update(); */;
+/* PHP:drop_module_non_unique_index(); */;
