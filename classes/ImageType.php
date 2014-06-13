2db214d8f75f7b6c208beaa4ec48e8c71cb6d2b2
Rémi Gaillard <remi.gaillard@prestashop.com>
2014-06-13 12:17:07 +0200

    // typo

--- classes/ImageType.php
+++ classes/ImageType.php
@@ -97,7 +97,7 @@ class ImageTypeCore extends ObjectModel
 			if (!empty($type))
 				$where .= ' AND `'.bqSQL($type).'` = 1 ';
 
-			$query = 'SELECT * FROM `'._DB_PREFIX_.'image_type`'.$where.' ORDER BY `name` ASC';
+			$query = 'SELECT * FROM `'._DB_PREFIX_.'image_type` '.$where.' ORDER BY `name` ASC';
 			self::$images_types_cache[$type] = Db::getInstance()->executeS($query);
 		}
 		return self::$images_types_cache[$type];
