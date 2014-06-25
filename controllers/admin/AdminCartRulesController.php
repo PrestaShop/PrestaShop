eb9655270b92dff44524369281501aed6171a6b7
AlexEven <AlexEven@users.noreply.github.com>
2014-06-25 16:02:24 +0200

    // AdminCartRulesController: wording
    Validity end date --> Expiration date

--- controllers/admin/AdminCartRulesController.php
+++ controllers/admin/AdminCartRulesController.php
@@ -44,7 +44,7 @@ class AdminCartRulesControllerCore extends AdminController
 			'priority' => array('title' => $this->l('Priority'), 'align' => 'center', 'class' => 'fixed-width-xs'),
 			'code' => array('title' => $this->l('Code'), 'class' => 'fixed-width-sm'),
 			'quantity' => array('title' => $this->l('Quantity'), 'align' => 'center', 'class' => 'fixed-width-xs'),
-			'date_to' => array('title' => $this->l('Validity end date'), 'type' => 'datetime'),
+			'date_to' => array('title' => $this->l('Expiration date'), 'type' => 'datetime'),
 			'active' => array('title' => $this->l('Status'), 'active' => 'status', 'type' => 'bool', 'orderby' => false),
 		);
 
