f262b80247836922f62c92c0342e63d3d15853df
AlexEven <AlexEven@users.noreply.github.com>
2014-06-27 10:11:09 +0200

    // AdminCustomersController: wording

--- controllers/admin/AdminCustomersController.php
+++ controllers/admin/AdminCustomersController.php
@@ -375,7 +375,7 @@ class AdminCustomersControllerCore extends AdminController
 				),
 				array(
 					'type' => 'switch',
-					'label' => $this->l('Status'),
+					'label' => $this->l('Enabled'),
 					'name' => 'active',
 					'required' => false,
 					'class' => 't',
