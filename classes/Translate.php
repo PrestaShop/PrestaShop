83f3e3ad97f715cc15ddaa5d84046a76b6096d5e
shaffe-fr <shaffe.fr@gmail.com>
2014-07-07 12:30:13 +0200

    Add arguments to get expected modules translations

--- classes/Translate.php
+++ classes/Translate.php
@@ -64,7 +64,7 @@ class TranslateCore
 			if (class_exists($class_name_controller) && Module::getModuleNameFromClass($class_name_controller))
 			{
 				$string = str_replace('\'', '\\\'', $string);
-				return Translate::getModuleTranslation(Module::$classInModule[$class_name_controller], $string, $class_name_controller);
+				return Translate::getModuleTranslation(Module::$classInModule[$class_name_controller], $string, $class_name_controller, $sprintf, $addslashes);
 			}
 		}
 
