86e907abc57ecb37d1cb4cd4e641100ba2e7fc0e
PrestaEdit <j.danse@prestaedit.com>
2014-08-09 22:50:03 +0200

    // use context

--- classes/controller/AdminController.php
+++ classes/controller/AdminController.php
@@ -1931,15 +1931,14 @@ class AdminControllerCore extends Controller
 		}
 
 		// Iso needed to generate Addons login
-		$language = new Language($this->context->employee->id_lang);
-		$iso_code_caps = strtoupper($language->iso_code);
+		$iso_code_caps = strtoupper($this->context->language->iso_code);
 
 		$this->context->smarty->assign(array(
 			'check_url_fopen' => (ini_get('allow_url_fopen') ? 'ok' : 'ko'),
 			'check_openssl' => (extension_loaded('openssl') ? 'ok' : 'ko'),
 			'add_permission' => 1,
-			'addons_register_link' => "//addons.prestashop.com/".$language->iso_code."/login?email=".urlencode($this->context->employee->email)."&firstname=".urlencode($this->context->employee->firstname)."&lastname=".urlencode($this->context->employee->lastname)."&website=".urlencode($this->context->shop->getBaseURL())."&utm_source=back-office&utm_medium=connect-to-addons&utm_campaign=back-office-".$iso_code_caps."#createnow",
-			'addons_forgot_password_link' => "//addons.prestashop.com/".$language->iso_code."/forgot-your-password"
+			'addons_register_link' => "//addons.prestashop.com/".$this->context->language->iso_code."/login?email=".urlencode($this->context->employee->email)."&firstname=".urlencode($this->context->employee->firstname)."&lastname=".urlencode($this->context->employee->lastname)."&website=".urlencode($this->context->shop->getBaseURL())."&utm_source=back-office&utm_medium=connect-to-addons&utm_campaign=back-office-".$iso_code_caps."#createnow",
+			'addons_forgot_password_link' => "//addons.prestashop.com/".$this->context->language->iso_code."/forgot-your-password"
 		));
 
 		$this->modals[] = array(
