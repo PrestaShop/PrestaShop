9a0118f4abfd53ecaf124baf2c7080c8ced1a495
gRoussac <gregory.roussac@prestashop.com>
2014-09-15 17:12:05 +0200

    // follow up https://github.com/PrestaShop/PrestaShop/pull/2102

--- classes/Cookie.php
+++ classes/Cookie.php
@@ -65,13 +65,13 @@ class CookieCore
 		$this->_content = array();
 		$this->_standalone = $standalone;
 		$this->_expire = is_null($expire) ? time() + 1728000 : (int)$expire;
-		$this->_name = 'PrestaShop-'.md5(($this->_standalone ? '' : _PS_VERSION_).$name);
 		$this->_path = trim(($this->_standalone ? '' : Context::getContext()->shop->physical_uri).$path, '/\\').'/';
 		if ($this->_path{0} != '/') $this->_path = '/'.$this->_path;
 		$this->_path = rawurlencode($this->_path);
 		$this->_path = str_replace('%2F', '/', $this->_path);
 		$this->_path = str_replace('%7E', '~', $this->_path);
 		$this->_domain = $this->getDomain($shared_urls);
+		$this->_name = 'PrestaShop-'.md5(($this->_standalone ? '' : _PS_VERSION_).$name.$this->_domain);
 		$this->_allow_writing = true;
 		$this->_salt = $this->_standalone ? str_pad('', 8, md5('ps'.__FILE__)) : _COOKIE_IV_;
 		if ($this->_standalone)
