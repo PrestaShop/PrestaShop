4e787a4f005744b93f1c30f10792f02f26639ff0
gRoussac <gregory.roussac@prestashop.com>
2014-09-16 17:17:48 +0200

    [-] FO : Fix bug #PSCSX-3304, jqzoom bad image on loading when ipa in url

--- themes/default-bootstrap/js/product.js
+++ themes/default-bootstrap/js/product.js
@@ -149,6 +149,11 @@ $(document).ready(function()
 
 	//set jqZoom parameters if needed
 	if (typeof(jqZoomEnabled) != 'undefined' && jqZoomEnabled)
+	{
+		var new_src = $('#thumbs_list .shown img').attr('src').replace('cart_', 'large_');
+		if ($('.jqzoom img').attr('src')!= new_src)
+			$('.jqzoom img').attr('src', new_src).parent().attr('href', new_src);
+
 		$('.jqzoom').jqzoom({
 			zoomType: 'innerzoom', //innerzoom/standard/reverse/drag
 			zoomWidth: 458, //zooming div default width(default width value is 200)
@@ -158,6 +163,7 @@ $(document).ready(function()
 			title: false
 		});
 
+	}
 	if (typeof(contentOnly) != 'undefined' && !contentOnly)
 	{
 		if (!!$.prototype.fancybox)
@@ -740,12 +746,12 @@ function displayImage(domAAroundImgThumb, no_animation)
 {
 	if (typeof(no_animation) == 'undefined')
 		no_animation = false;
-	if (domAAroundImgThumb.prop('href'))
+	if (domAAroundImgThumb.attr('href'))
 	{
 		var new_src = domAAroundImgThumb.attr('href').replace('thickbox', 'large');
 		var new_title = domAAroundImgThumb.attr('title');
 		var new_href = domAAroundImgThumb.attr('href');
-		if ($('#bigpic').prop('src') != new_src)
+		if ($('#bigpic').attr('src') != new_src)
 		{
 			$('#bigpic').attr({
 				'src' : new_src,
