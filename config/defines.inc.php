7472321f664f12e47f1ed02f63deaf0ff55f7979
gRoussac <gregory.roussac@prestashop.com>
2014-09-17 15:50:58 +0200

    // small change

--- config/defines.inc.php
+++ config/defines.inc.php
@@ -29,7 +29,7 @@ if (!defined('_PS_MODE_DEV_'))
 	define('_PS_MODE_DEV_', true);
 /* Compatibility warning */
 define('_PS_DISPLAY_COMPATIBILITY_WARNING_', true);
-if (_PS_MODE_DEV_)
+if (_PS_MODE_DEV_ === true)
 {
 	@ini_set('display_errors', 'on');
 	@error_reporting(E_ALL | E_STRICT);
