ea8f1c4e58f63e8b1f530aec1faa2c6bb0f6b2af
Jerome Nadaud <jerome.nadaud@gmail.com>
2014-10-07 11:26:39 +0200

    // Update filemanager for PSOD

--- admin-dev/filemanager/config/config.php
+++ admin-dev/filemanager/config/config.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (!defined('_PS_ADMIN_DIR_'))
-		define('_PS_ADMIN_DIR_', getcwd().'/../');
+		define('_PS_ADMIN_DIR_',dirname(__FILE__).'/../../');
 
 require_once(_PS_ADMIN_DIR_.'/../config/config.inc.php');
 require_once(_PS_ADMIN_DIR_.'/init.php');
