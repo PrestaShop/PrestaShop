231f63d07c82ea261b9618c3110fa3abb647d3c3
Jerome Nadaud <jerome.nadaud@gmail.com>
2014-10-24 15:50:01 +0200

    // Small fix

--- js/tinymce.inc.js
+++ js/tinymce.inc.js
@@ -4,16 +4,14 @@ function tinySetup(config)
 		config = {};
 
 	//var editor_selector = 'rte';
-	//if (typeof config['editor_selector'] !== 'undefined')
-		//var editor_selector = config['editor_selector'];
-	if (typeof config['editor_selector'] != 'undefined')
-		config['selector'] = '.'+config['editor_selector'];
 
-//    safari,pagebreak,style,table,advimage,advlink,inlinepopups,media,contextmenu,paste,fullscreen,xhtmlxtras,preview
+	if (typeof config.editor_selector != 'undefined')
+		config.selector = '.'+config.editor_selector;
+
 	default_config = {
 		selector: ".rte" ,
 		plugins : "colorpicker link image paste pagebreak table contextmenu filemanager table code media autoresize textcolor anchor",
-		toolbar1 : "code,|,bold,italic,underline,strikethrough,|,alignleft,aligncenter,alignright,alignfull,formatselect,|,blockquote,colorpicker,pasteword,|,bullist,numlist,|,outdent,indent,|,link,unlink,|,anchor,|,image",
+		toolbar1 : "code,|,bold,italic,underline,strikethrough,|,alignleft,aligncenter,alignright,alignfull,formatselect,|,blockquote,colorpicker,pasteword,|,bullist,numlist,|,outdent,indent,|,link,unlink,|,anchor,|,media,image",
 		toolbar2: "",
 		external_filemanager_path: ad+"/filemanager/",
 		filemanager_title: "File manager" ,
@@ -27,14 +25,13 @@ function tinySetup(config)
 		extended_valid_elements : "em[class|name|id]",
 		menu: {
 			edit: {title: 'Edit', items: 'undo redo | cut copy paste | selectall'},
-			insert: {title: 'Insert', items: 'image link | pagebreak'},
+			insert: {title: 'Insert', items: 'media image link | pagebreak'},
 			view: {title: 'View', items: 'visualaid'},
 			format: {title: 'Format', items: 'bold italic underline strikethrough superscript subscript | formats | removeformat'},
 			table: {title: 'Table', items: 'inserttable tableprops deletetable | cell row column'},
 			tools: {title: 'Tools', items: 'code'}
 		}
-
-	}
+	};
 
 	$.each(default_config, function(index, el)
 	{
@@ -43,5 +40,4 @@ function tinySetup(config)
 	});
 
 	tinyMCE.init(config);
-
-};
+}
