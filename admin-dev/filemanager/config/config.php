940cf780eb8ce90612ca0e0826b4d05493a4b760
Jerome Nadaud <jerome.nadaud@gmail.com>
2014-10-24 10:21:41 +0200

    [-] BO : FixBug #PSCSX-2580 - Allow pdf files in CMS

--- admin-dev/filemanager/config/config.php
+++ admin-dev/filemanager/config/config.php
@@ -100,10 +100,10 @@ $duplicate_files=true;
 //Allowed extensions (lowercase insert)
 //**********************
 $ext_img = array('jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'svg'); //Images
-$ext_file = array('doc', 'docx','rtf', 'pdf', 'xls', 'xlsx', 'txt', 'csv','html','xhtml','psd','sql','log','fla','xml','ade','adp','mdb','accdb','ppt','pptx','odt','ots','ott','odb','odg','otp','otg','odf','ods','odp','css','ai'); //Files
-$ext_video = array('mov', 'mpeg', 'mp4', 'avi', 'mpg','wma',"flv","webm"); //Video
-$ext_music = array('mp3', 'm4a', 'ac3', 'aiff', 'mid','ogg','wav'); //Audio
-$ext_misc = array('zip', 'rar','gz','tar','iso','dmg'); //Archives
+$ext_file = array('pdf'); //array('doc', 'docx','rtf', 'pdf', 'xls', 'xlsx', 'txt', 'csv','html','xhtml','psd','sql','log','fla','xml','ade','adp','mdb','accdb','ppt','pptx','odt','ots','ott','odb','odg','otp','otg','odf','ods','odp','css','ai'); //Files
+$ext_video = array();//array('mov', 'mpeg', 'mp4', 'avi', 'mpg','wma',"flv","webm"); //Video
+$ext_music = array();//array('mp3', 'm4a', 'ac3', 'aiff', 'mid','ogg','wav'); //Audio
+$ext_misc = array();// array('zip', 'rar','gz','tar','iso','dmg'); //Archives
 
 $ext=array_merge($ext_img, $ext_file, $ext_misc, $ext_video,$ext_music); //allowed extensions
 
