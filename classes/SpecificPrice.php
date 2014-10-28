bc3a2cc99ec8c5c11ac915ee9efae2ad4187c338
Julien Bourdeau <julien@sigerr.org>
2014-10-28 15:25:03 +0100

    // Add SpecificPrice::$reduction_tax default value

--- classes/SpecificPrice.php
+++ classes/SpecificPrice.php
@@ -39,7 +39,7 @@ class SpecificPriceCore extends ObjectModel
 	public	$price;
 	public	$from_quantity;
 	public	$reduction;
-	public	$reduction_tax;
+	public	$reduction_tax = 1;
 	public	$reduction_type;
 	public	$from;
 	public	$to;
@@ -230,14 +230,14 @@ class SpecificPriceCore extends ObjectModel
 					AND
 					(`to` = \'0000-00-00 00:00:00\' OR \''.$now.'\' <= `to`)
 				)
-				AND id_cart IN (0, '.(int)$id_cart.') 
+				AND id_cart IN (0, '.(int)$id_cart.')
 				AND IF(`from_quantity` > 1, `from_quantity`, 0) <= ';
 
-			$query .= (Configuration::get('PS_QTY_DISCOUNT_ON_COMBINATION') || !$id_cart || !$real_quantity) ? (int)$quantity : max(1, (int)$real_quantity);			
+			$query .= (Configuration::get('PS_QTY_DISCOUNT_ON_COMBINATION') || !$id_cart || !$real_quantity) ? (int)$quantity : max(1, (int)$real_quantity);
 			$query .= ' ORDER BY `id_product_attribute` DESC, `from_quantity` DESC, `id_specific_price_rule` ASC, `score` DESC';
-			
+
 			SpecificPrice::$_specificPriceCache[$key] = Db::getInstance(_PS_USE_SQL_SLAVE_)->getRow($query);
-				
+
 		}
 		return SpecificPrice::$_specificPriceCache[$key];
 	}
@@ -408,7 +408,7 @@ class SpecificPriceCore extends ObjectModel
 			$feature_active = Configuration::get('PS_SPECIFIC_PRICE_FEATURE_ACTIVE');
 		return $feature_active;
 	}
-	
+
 	public static function exists($id_product, $id_product_attribute, $id_shop, $id_group, $id_country, $id_currency, $id_customer, $from_quantity, $from, $to, $rule = false)
 	{
 		$rule = ' AND `id_specific_price_rule`'.(!$rule ? '=0' : '!=0');
