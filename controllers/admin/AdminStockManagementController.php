7803a43955a8a82838ec5d219d7a40662db24169
Sacha Froment <sacha.froment@prestashop.com>
2014-10-29 11:36:51 +0100

    // need to create wpl before synch stock for new product

--- controllers/admin/AdminStockManagementController.php
+++ controllers/admin/AdminStockManagementController.php
@@ -634,8 +634,6 @@ class AdminStockManagementControllerCore extends AdminController
 
 				if ($stock_manager->addProduct($id_product, $id_product_attribute, $warehouse, $quantity, $id_stock_mvt_reason, $price, $usable))
 				{
-					StockAvailable::synchronize($id_product);
-
 					// Create warehouse_product_location entry if we add stock to a new warehouse
 					$id_wpl = (int)WarehouseProductLocation::getIdByProductAndWarehouse($id_product, $id_product_attribute, $id_warehouse);
 					if(!$id_wpl)
@@ -646,6 +644,8 @@ class AdminStockManagementControllerCore extends AdminController
 						$wpl->id_warehouse = (int)$id_warehouse;
 						$wpl->save();
 					}
+
+					StockAvailable::synchronize($id_product);
 
 					if (Tools::isSubmit('addstockAndStay'))
 					{
@@ -971,7 +971,7 @@ class AdminStockManagementControllerCore extends AdminController
 			$this->warnings[md5('PS_ADVANCED_STOCK_MANAGEMENT')] = $this->l('You need to activate the Advanced Stock Management feature prior to using this feature.');
 			return false;
 		}
-		
+
 		// Manage the add stock form
 		if ($this->display == 'addstock' || $this->display == 'removestock' || $this->display == 'transferstock')
 		{
@@ -1091,7 +1091,7 @@ class AdminStockManagementControllerCore extends AdminController
 					$this->content .= $helper->generateForm($this->fields_form);
 
 					$this->context->smarty->assign(array(
-						'content' => $this->content,						
+						'content' => $this->content,
 						'show_page_header_toolbar' => $this->show_page_header_toolbar,
 						'page_header_toolbar_title' => $this->page_header_toolbar_title,
 						'page_header_toolbar_btn' => $this->page_header_toolbar_btn
@@ -1169,7 +1169,7 @@ class AdminStockManagementControllerCore extends AdminController
 
         return $this->context->smarty->fetch('helpers/list/list_action_transferstock.tpl');
     }
-	
+
 	public function initProcess()
 	{
 		if (!Configuration::get('PS_ADVANCED_STOCK_MANAGEMENT'))
@@ -1188,6 +1188,6 @@ class AdminStockManagementControllerCore extends AdminController
 		else
 			$this->list_id = 'product';
 
-		parent::initProcess();	
-	}    
+		parent::initProcess();
+	}
 }
