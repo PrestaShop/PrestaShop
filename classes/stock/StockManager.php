f605edaf21b66662e79121c7d8633bf30c6449fa
Sacha Froment <sacha.froment@prestashop.com>
2014-11-18 15:32:33 +0100

    // follow up 0e15ddb11230f15b1d56c2078f37232675402b00

--- classes/stock/StockManager.php
+++ classes/stock/StockManager.php
@@ -50,7 +50,8 @@ class StockManagerCore implements StockManagerInterface
 							   $id_stock_mvt_reason,
 							   $price_te,
 							   $is_usable = true,
-							   $id_supply_order = null)
+							   $id_supply_order = null,
+							   $id_employee = null)
 	{
 		if (!Validate::isLoadedObject($warehouse) || !$price_te || !$quantity || !$id_product)
 			return false;
@@ -70,7 +71,7 @@ class StockManagerCore implements StockManagerInterface
 			'price_te' => $price_te,
 			'last_wa' => null,
 			'current_wa' => null,
-			'id_employee' => $context->employee->id,
+			'id_employee' => (int)$context->employee->id ? (int)$context->employee->id : $id_employee,
 			'employee_firstname' => $context->employee->firstname,
 			'employee_lastname' => $context->employee->lastname,
 			'sign' => 1
