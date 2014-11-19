b8e5b691aa95cde76ce86b86d4949fbc601368ea
gRoussac <gregory.roussac@prestashop.com>
2014-11-19 18:32:11 +0100

    // follow up https://github.com/PrestaShop/PrestaShop/pull/2041

--- classes/PaymentModule.php
+++ classes/PaymentModule.php
@@ -940,7 +940,8 @@ abstract class PaymentModuleCore extends Module
 		return Db::getInstance()->executeS('
 		SELECT DISTINCT m.`id_module`, h.`id_hook`, m.`name`, hm.`position`
 		FROM `'._DB_PREFIX_.'module` m
-		LEFT JOIN `'._DB_PREFIX_.'hook_module` hm ON hm.`id_module` = m.`id_module`
+		LEFT JOIN `'._DB_PREFIX_.'hook_module` hm ON hm.`id_module` = m.`id_module`'
+		. Shop::addSqlRestriction(false, 'hm').'
 		LEFT JOIN `'._DB_PREFIX_.'hook` h ON hm.`id_hook` = h.`id_hook`
 		INNER JOIN `'._DB_PREFIX_.'module_shop` ms ON (m.`id_module` = ms.`id_module` AND ms.id_shop='.(int)Context::getContext()->shop->id.')
 		WHERE h.`name` = \''.pSQL($hook_payment).'\'');
