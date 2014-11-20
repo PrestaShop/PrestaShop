1c7db43de79d18fbeebfc897e9d7010f09385076
Sacha Froment <sacha.froment@prestashop.com>
2014-11-20 17:46:23 +0100

    // follow up #1852

--- js/tinymce.inc.js
+++ js/tinymce.inc.js
@@ -11,6 +11,7 @@ function tinySetup(config)
 	default_config = {
 		selector: ".rte" ,
 		plugins : "colorpicker link image paste pagebreak table contextmenu filemanager table code media autoresize textcolor anchor",
+		browser_spellcheck : true,
 		toolbar1 : "code,|,bold,italic,underline,strikethrough,|,alignleft,aligncenter,alignright,alignfull,formatselect,|,blockquote,colorpicker,pasteword,|,bullist,numlist,|,outdent,indent,|,link,unlink,|,anchor,|,media,image",
 		toolbar2: "",
 		external_filemanager_path: ad+"/filemanager/",
