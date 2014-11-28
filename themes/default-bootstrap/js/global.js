22cad9ae31ebafaf9fe098a32ac5f89084be074c
gRoussac <gregory.roussac@prestashop.com>
2014-11-28 18:48:56 +0100

    [-] FO : Fix bug #SCSX-3986, could not use blockmanufacturer select on index

--- themes/default-bootstrap/js/global.js
+++ themes/default-bootstrap/js/global.js
@@ -55,15 +55,15 @@ $(document).ready(function(){
 			$(this.form).submit();
 		});
 
-		$(document).on('change', 'select[name="manufacturer_list"], select[name="supplier_list"]', function(){
-			if (this.value != '')
-				location.href = this.value;
-		});
-
 		$(document).on('change', 'select[name="currency_payement"]', function(){
 			setCurrency($(this).val());
 		});
 	}
+
+	$(document).on('change', 'select[name="manufacturer_list"], select[name="supplier_list"]', function(){
+		if (this.value != '')
+			location.href = this.value;
+	});
 
 	$(document).on('click', '.back', function(e){
 		e.preventDefault();
