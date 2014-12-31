93c2272869f8885b322ade533bd4994c94577c1d
Samir Shah <solaris.smoke@gmail.com>
2014-12-31 07:41:39 +0300

    Check if search results were found before looping through them

--- controllers/front/SearchController.php
+++ controllers/front/SearchController.php
@@ -96,8 +96,9 @@ class SearchControllerCore extends FrontController
 			$original_query = $query;
 			$query = Tools::replaceAccentedChars(urldecode($query));
 			$search = Search::find($this->context->language->id, $query, $this->p, $this->n, $this->orderBy, $this->orderWay);
-			foreach ((array)$search['result'] as &$product)
-				$product['link'] .= (strpos($product['link'], '?') === false ? '?' : '&').'search_query='.urlencode($query).'&results='.(int)$search['total'];
+			if(is_array($search['result']))
+				foreach ($search['result'] as &$product)
+					$product['link'] .= (strpos($product['link'], '?') === false ? '?' : '&').'search_query='.urlencode($query).'&results='.(int)$search['total'];
 			Hook::exec('actionSearch', array('expr' => $query, 'total' => $search['total']));
 			$nbProducts = $search['total'];
 			$this->pagination($nbProducts);
