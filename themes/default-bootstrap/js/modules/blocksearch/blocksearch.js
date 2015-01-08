3cfdd388c97ee55f6171515f8923bd899bb6e0ce
Gytis Škėma <gytis@reservationpartner.lt>
2015-01-08 22:36:07 +0200

    [-] FO : BlockSearch JS: fix keyup event binding

--- themes/default-bootstrap/js/modules/blocksearch/blocksearch.js
+++ themes/default-bootstrap/js/modules/blocksearch/blocksearch.js
@@ -31,6 +31,38 @@ $(document).ready(function()
 
 	var $input = $("#search_query_" + blocksearch_type);
 
+	var width_ac_results = 	$input.parent('form').width();
+	if (typeof ajaxsearch != 'undefined' && ajaxsearch) {
+		$input.autocomplete(
+			search_url,
+			{
+				minChars: 3,
+				max: 10,
+				width: (width_ac_results > 0 ? width_ac_results : 500),
+				selectFirst: false,
+				scroll: false,
+				dataType: "json",
+				formatItem: function(data, i, max, value, term) {
+					return value;
+				},
+				parse: function(data) {
+					var mytab = [];
+					for (var i = 0; i < data.length; i++)
+						mytab[mytab.length] = { data: data[i], value: data[i].cname + ' > ' + data[i].pname };
+					return mytab;
+				},
+				extraParams: {
+					ajaxSearch: 1,
+					id_lang: id_lang
+				}
+			}
+		)
+			.result(function(event, data, formatted) {
+				$('#search_query_' + blocksearch_type).val(data.pname);
+				document.location.href = data.product_link;
+			});
+	}
+
 	if (typeof instantsearch != 'undefined' && instantsearch) {
 		$input.on('keyup', function(){
 			if($(this).val().length > 4)
@@ -69,38 +101,6 @@ $(document).ready(function()
 			}
 			else
 				tryToCloseInstantSearch();
-		});
-	}
-
-	var width_ac_results = 	$input.parent('form').width();
-	if (typeof ajaxsearch != 'undefined' && ajaxsearch) {
-		$input.autocomplete(
-			search_url,
-			{
-				minChars: 3,
-				max: 10,
-				width: (width_ac_results > 0 ? width_ac_results : 500),
-				selectFirst: false,
-				scroll: false,
-				dataType: "json",
-				formatItem: function(data, i, max, value, term) {
-					return value;
-				},
-				parse: function(data) {
-					var mytab = [];
-					for (var i = 0; i < data.length; i++)
-						mytab[mytab.length] = { data: data[i], value: data[i].cname + ' > ' + data[i].pname };
-					return mytab;
-				},
-				extraParams: {
-					ajaxSearch: 1,
-					id_lang: id_lang
-				}
-			}
-		)
-		.result(function(event, data, formatted) {
-			$('#search_query_' + blocksearch_type).val(data.pname);
-			document.location.href = data.product_link;
 		});
 	}
 });
