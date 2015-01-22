f55d74330335aac764cc483087b56eb35c8abd85
joce <jocelyn.Fournier@gmail.com>
2015-01-22 17:18:48 +0100

    fix wrong variable name (cherry picked from commit 4cba116)

--- classes/Pack.php
+++ classes/Pack.php
@@ -258,16 +258,18 @@ class PackCore extends Product
 	}
 
 	/**
-	* Add an item to the pack
-	*
-	* @param integer $id_product
-	* @param integer $id_item
-	* @param integer $qty
-	* @return boolean true if everything was fine
-	*/
+	 * Add an item to the pack
+	 *
+	 * @param integer $id_product
+	 * @param integer $id_item
+	 * @param integer $qty
+	 * @param int     $id_attribute_item
+	 * @return bool true if everything was fine
+	 * @throws PrestaShopDatabaseException
+	 */
 	public static function addItem($id_product, $id_item, $qty, $id_attribute_item = 0)
 	{
-		$id_product_attribute = (int)$id_product_attribute ? (int)$id_product_attribute : Product::getDefaultAttribute((int)$id_item);
+		$id_attribute_item = (int)$id_attribute_item ? (int)$id_attribute_item : Product::getDefaultAttribute((int)$id_item);
 		return Db::getInstance()->update('product', array('cache_is_pack' => 1), 'id_product = '.(int)$id_product) &&
 			Db::getInstance()->insert('pack', array('id_product_pack' => (int)$id_product, 'id_product_item' => (int)$id_item, 'id_product_attribute_item' => (int)$id_attribute_item,'quantity' => (int)$qty)) &&
 			Configuration::updateGlobalValue('PS_PACK_FEATURE_ACTIVE', '1');
