2dd2aa56eb6d49ff7288e91a4cab33b6ee304e97
fram <fm.de.jouvencel@gmail.com>
2015-01-23 14:02:05 +0100

    // (bool)(-1) === true, again

--- classes/order/Order.php
+++ classes/order/Order.php
@@ -2146,7 +2146,7 @@ class OrderCore extends ObjectModel
 			}
 
 			$cart_rule = new CartRule($order_cart_rule['id_cart_rule']);
-			if ($cart_rule->reduction_product)
+			if ($cart_rule->reduction_product > 0)
 			{
 				if (empty($product_specific_discounts[$cart_rule->reduction_product]))
 				{
