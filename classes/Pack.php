b69bc1f79dcb9cb59749ce5a219f7a88744f472a
Gregory Roussac <gregory.roussac@prestashop.com>
2015-01-28 12:08:09 +0100

    // follow up https://github.com/jocel1/PrestaShop/commit/f55d74330335aac764cc483087b56eb35c8abd85

--- classes/Pack.php
+++ classes/Pack.php
@@ -267,7 +267,7 @@ class PackCore extends Product
 	*/
 	public static function addItem($id_product, $id_item, $qty, $id_attribute_item = 0)
 	{
-		$id_product_attribute = (int)$id_product_attribute ? (int)$id_product_attribute : Product::getDefaultAttribute((int)$id_item);
+		$id_attribute_item = (int)$id_attribute_item ? (int)$id_attribute_item : Product::getDefaultAttribute((int)$id_item);
 		return Db::getInstance()->update('product', array('cache_is_pack' => 1), 'id_product = '.(int)$id_product) &&
 			Db::getInstance()->insert('pack', array('id_product_pack' => (int)$id_product, 'id_product_item' => (int)$id_item, 'id_product_attribute_item' => (int)$id_attribute_item,'quantity' => (int)$qty)) &&
 			Configuration::updateGlobalValue('PS_PACK_FEATURE_ACTIVE', '1');
