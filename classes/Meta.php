b2a0d3dbcda1c545880a826a045afb98c383c504
Sacha Froment <sacha.froment@prestashop.com>
2015-02-02 09:48:46 +0100

    // follow up #2340

--- classes/Meta.php
+++ classes/Meta.php
@@ -372,16 +372,11 @@ class MetaCore extends ObjectModel
 	{
 		$sql = 'SELECT `meta_title`, `meta_description`, `meta_keywords`
 				FROM `'._DB_PREFIX_.'cms_lang`
-				WHERE 
-					id_lang = '.(int)$id_lang.'
+				WHERE id_lang = '.(int)$id_lang.'
 					AND id_cms = '.(int)$id_cms.
-					(
-						(int)Context::getContext()->shop->id
-						
-							? ' AND id_shop = '.(int)Context::getContext()->shop->id 
-							: ''
-					);
-					
+					((int)Context::getContext()->shop->id ?
+						' AND id_shop = '.(int)Context::getContext()->shop->id : '' );
+
 		if ($row = Db::getInstance(_PS_USE_SQL_SLAVE_)->getRow($sql))
 		{
 			$row['meta_title'] = $row['meta_title'].' - '.Configuration::get('PS_SHOP_NAME');
@@ -406,8 +401,8 @@ class MetaCore extends ObjectModel
 				FROM `'._DB_PREFIX_.'cms_category_lang`
 				WHERE id_lang = '.(int)$id_lang.'
 					AND id_cms_category = '.(int)$id_cms_category.
-					(int)Context::getContext()->shop->id ? '
-					AND id_shop = '.(int)Context::getContext()->shop->id : '';
+					((int)Context::getContext()->shop->id ?
+						' AND id_shop = '.(int)Context::getContext()->shop->id : '');
 		if ($row = Db::getInstance(_PS_USE_SQL_SLAVE_)->getRow($sql))
 		{
 			$row['meta_title'] = $row['meta_title'].' - '.Configuration::get('PS_SHOP_NAME');
