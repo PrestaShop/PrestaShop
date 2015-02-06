8c48fb0a3706cc580dfa1bd64ad6a38a95cb5323
Jerome Nadaud <jerome.nadaud@prestashop.com>
2015-02-06 14:00:34 +0100

    // Change install version

--- install-dev/data/db_structure.sql
+++ install-dev/data/db_structure.sql
@@ -1847,6 +1847,7 @@ CREATE TABLE `PREFIX_tag_count` (
   `id_group` int(10) unsigned NOT NULL DEFAULT 0,
   `id_tag` int(10) unsigned NOT NULL DEFAULT 0,
   `id_lang` int(10) unsigned NOT NULL DEFAULT 0,
+  `id_shop` int(11) unsigned NOT NULL DEFAULT 0,
   `counter` int(10) unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`id_group`, `id_tag`),
   KEY (`id_group`, `id_lang`, `id_shop`, `counter`)
