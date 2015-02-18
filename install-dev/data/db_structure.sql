90d6930a289346862858378a1e01ab5072e26a2f
joce <jocelyn.Fournier@gmail.com>
2015-02-19 00:50:41 +0100

    Shorter varchar (MySQL < 5.0.3 compat)

--- install-dev/data/db_structure.sql
+++ install-dev/data/db_structure.sql
@@ -2606,7 +2606,7 @@ CREATE TABLE `PREFIX_smarty_lazy_cache` (
   `template_hash` varchar(32) NOT NULL DEFAULT '',
   `cache_id` varchar(32) NOT NULL DEFAULT '',
   `compile_id` varchar(32) NOT NULL DEFAULT '',
-  `filepath` varchar(2048) NOT NULL DEFAULT '',
+  `filepath` varchar(255) NOT NULL DEFAULT '',
   `last_update` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`template_hash`, `cache_id`, `compile_id`)
 ) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
