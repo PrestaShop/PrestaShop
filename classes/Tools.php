cd42216bc1753f382d11636a2f2f349581c62495
Sergio Quiñonez <sqs_sqs@hotmail.com>
2015-02-20 13:19:21 -0700

    Update Tools.php
    Hello Team,
    Here contributing with another improves, the geolocation of PrestaShop does not work at all in shops with CloudFlare cache, this part of code is very important to get the real IP, I have a geolocation module and works like a charm with this modification.
    
    Regards,
    Sergio Quiñonez

--- classes/Tools.php
+++ classes/Tools.php
@@ -245,6 +245,9 @@ class ToolsCore
 	*/
 	public static function getRemoteAddr()
 	{
+		// Add compatibility with CloudFlare cache
+		$_SERVER['REMOTE_ADDR'] = isset($_SERVER["HTTP_CF_CONNECTING_IP"]) ? $_SERVER["HTTP_CF_CONNECTING_IP"] : $_SERVER["REMOTE_ADDR"];
+		
 		// This condition is necessary when using CDN, don't remove it.
 		if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR'] && (!isset($_SERVER['REMOTE_ADDR'])
 			|| preg_match('/^127\..*/i', trim($_SERVER['REMOTE_ADDR'])) || preg_match('/^172\.16.*/i', trim($_SERVER['REMOTE_ADDR']))
