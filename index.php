c66c2ac0f2fe60d894b794e791a2eea356a294ec
tchauviere <thibaud.chauviere@prestashop.com>
2015-03-11 18:26:53 +0100

    // Test fork

--- index.php
+++ index.php
@@ -23,6 +23,6 @@
 *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
-
+// Test
 require(dirname(__FILE__).'/config/config.inc.php');
 Dispatcher::getInstance()->dispatch();
