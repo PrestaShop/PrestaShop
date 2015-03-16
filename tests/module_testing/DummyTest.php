f761ca0c2dcf46c5a1aae0b45375379bf7ca44d3
mdomenjoud <mdomenjoud@octo.com>
2015-03-16 10:21:54 +0100

    Add tool init_module_unit_testing.sh to initiate Unit tests inside a module

---
+++ tests/module_testing/DummyTest.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace PrestaShop\PrestaShop\Tests\Unit;
+
+
+use PHPUnit_Framework_TestCase;
+
+class DummyTest extends PHPUnit_Framework_TestCase
+{
+
+    public function test_Dummy()
+    {
+        $this->assertTrue(true, "Everything works fine");
+    }
+}
