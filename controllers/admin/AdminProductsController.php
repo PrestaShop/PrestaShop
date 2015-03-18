deaffea343066f8466836f4c23ba67591e89c5c1
joce <jocelyn.Fournier@gmail.com>
2015-03-18 16:56:14 +0100

    [-] BO : Fix tree category issue

--- controllers/admin/AdminProductsController.php
+++ controllers/admin/AdminProductsController.php
@@ -2434,6 +2434,7 @@ class AdminProductsControllerCore extends AdminController
 			$tree = new HelperTreeCategories('categories-tree', $this->l('Filter by category'));
 			$tree->setAttribute('is_category_filter', (bool)$this->id_current_category)
 				->setAttribute('base_url', preg_replace('#&id_category=[0-9]*#', '', self::$currentIndex).'&token='.$this->token)
+				->setInputName('id-category')
 				->setRootCategory(Configuration::get('PS_ROOT_CATEGORY'))
 				->setSelectedCategories(array((int)$id_category));
 			$this->tpl_list_vars['category_tree'] = $tree->render();
