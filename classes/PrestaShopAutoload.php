2f6bfa31acd18c8b12c1dc47a49dc67d91e278f2
tchauviere <thibaud.chauviere@prestashop.com>
2015-03-23 10:42:40 +0100

    [*] CORE : Autoloader now take into account Core folder (legacy issue otherwise)

--- classes/PrestaShopAutoload.php
+++ classes/PrestaShopAutoload.php
@@ -188,6 +188,7 @@ class PrestaShopAutoload
 					$namespacePattern = '[\\a-z0-9_]*[\\]';
 					$pattern = '#\W((abstract\s+)?class|interface)\s+(?P<classname>'.basename($file, '.php').'(?:Core)?)'
 								.'(?:\s+extends\s+'.$namespacePattern.'[a-z][a-z0-9_]*)?(?:\s+implements\s+'.$namespacePattern.'[a-z][\\a-z0-9_]*(?:\s*,\s*'.$namespacePattern.'[a-z][\\a-z0-9_]*)*)?\s*\{#i';
+
 					if (preg_match($pattern, $content, $m))
 					{
 						$classes[$m['classname']] = array(
