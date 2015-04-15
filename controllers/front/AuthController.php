168117196db3ebf64f27be92e33e4aa97095bc06
Neo-NMO <Neo.NMO@gmail.com>
2015-04-15 10:39:29 -0400

    Update AuthController.php
    The newsletter module should be installed and enabled in the preferences.

--- controllers/front/AuthController.php
+++ controllers/front/AuthController.php
@@ -91,7 +91,7 @@ class AuthControllerCore extends FrontController
 
 		$this->assignCountries();
 
-		$newsletter = Configuration::get('PS_CUSTOMER_NWSL') || (Module::isInstalled('blocknewsletter') && Module::getInstanceByName('blocknewsletter')->active);
+		$newsletter = Configuration::get('PS_CUSTOMER_NWSL') && (Module::isInstalled('blocknewsletter') && Module::getInstanceByName('blocknewsletter')->active);
 		$this->context->smarty->assign('newsletter', $newsletter);
 		$this->context->smarty->assign('optin', (bool)Configuration::get('PS_CUSTOMER_OPTIN'));
 
