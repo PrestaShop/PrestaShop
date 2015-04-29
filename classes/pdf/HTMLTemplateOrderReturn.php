3a05a423af66925513a442367afdb626c378ed49
Jerome Nadaud <jerome.nadaud@gmail.com>
2015-04-29 10:31:09 +0200

    // PHPDOC + wording

--- classes/pdf/HTMLTemplateOrderReturn.php
+++ classes/pdf/HTMLTemplateOrderReturn.php
@@ -1,28 +1,28 @@
 <?php
-/*
-* 2007-2015 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Open Software License (OSL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/osl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2015 PrestaShop SA
-*  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*/
+/**
+ * 2007-2015 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ *  @author 	PrestaShop SA <contact@prestashop.com>
+ *  @copyright  2007-2015 PrestaShop SA
+ *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ *  International Registered Trademark & Property of PrestaShop SA
+ */
 
 /**
  * @since 1.5
@@ -34,7 +34,7 @@ class HTMLTemplateOrderReturnCore extends HTMLTemplate
 
 	/**
 	 * @param OrderReturn $order_return
-	 * @param Smarty $smarty
+	 * @param $smarty
 	 * @throws PrestaShopException
 	 */
 	public function __construct(OrderReturn $order_return, $smarty)
@@ -54,6 +54,7 @@ class HTMLTemplateOrderReturnCore extends HTMLTemplate
 
 	/**
 	 * Returns the template's HTML content
+	 *
 	 * @return string HTML content
 	 */
 	public function getContent()
@@ -81,6 +82,7 @@ class HTMLTemplateOrderReturnCore extends HTMLTemplate
 
 	/**
 	 * Returns the template filename
+	 *
 	 * @return string filename
 	 */
 	public function getFilename()
@@ -90,6 +92,7 @@ class HTMLTemplateOrderReturnCore extends HTMLTemplate
 
 	/**
 	 * Returns the template filename when using bulk rendering
+	 *
 	 * @return string filename
 	 */
 	public function getBulkFilename()
