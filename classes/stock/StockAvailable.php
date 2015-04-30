337a5e94f4a366bce828d948a3647e203cb7a5c2
joce <jocelyn.Fournier@gmail.com>
2015-04-30 16:57:01 +0200

    // add ON DUPLICATE KEY UPDATE support + small opts

--- classes/stock/StockAvailable.php
+++ classes/stock/StockAvailable.php
@@ -324,28 +324,14 @@ class StockAvailableCore extends ObjectModel
 		if (!Validate::isUnsignedId($id_product))
 			return false;
 
-		$existing_id = StockAvailable::getStockAvailableIdByProductId((int)$id_product, (int)$id_product_attribute, $id_shop);
-		if ($existing_id > 0)
-		{
-			Db::getInstance()->update(
-				'stock_available',
-				array('out_of_stock' => (int)$out_of_stock),
-				'id_product = '.(int)$id_product.
-				(($id_product_attribute) ? ' AND id_product_attribute = '.(int)$id_product_attribute : '').
-				StockAvailable::addSqlShopRestriction(null, $id_shop)
-			);
-		}
-		else
-		{
-			$params = array(
-				'out_of_stock' => (int)$out_of_stock,
-				'id_product' => (int)$id_product,
-				'id_product_attribute' => (int)$id_product_attribute
-			);
+		$params = array(
+			'out_of_stock' => (int)$out_of_stock,
+			'id_product' => (int)$id_product,
+			'id_product_attribute' => (int)$id_product_attribute
+		);
 
-			StockAvailable::addSqlShopParams($params, $id_shop);
-			Db::getInstance()->insert('stock_available', $params);
-		}
+		StockAvailable::addSqlShopParams($params, $id_shop);
+		Db::getInstance()->insert('stock_available', $params, false, true, Db::ON_DUPLICATE_KEY);
 	}
 
 	/**
