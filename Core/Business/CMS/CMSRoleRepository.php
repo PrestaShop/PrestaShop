34cb4020a16e9ed995738a7a40e3eae544b79062
djfm <fm.de.jouvencel@gmail.com>
2015-05-06 11:02:01 +0200

    // removed incompatible methods before rewrite

--- Core/Business/CMS/CMSRoleRepository.php
+++ Core/Business/CMS/CMSRoleRepository.php
@@ -28,95 +28,18 @@ class CMSRoleRepository extends Core_Foundation_Database_EntityRepository
 {
 	protected $entityClass = 'CMSRoleEntity';
 
-	/**
-	 * Get CMS Role by its given reference name
-	 * @param $name
-	 * @return array|false
-	 * @throws PrestaShopDatabaseException
-	 */
-	public function getRoleByName($name)
+	public function findByName(array $names)
 	{
-		$sql = '
-		SELECT *
-		FROM `'._DB_PREFIX_.CMSRoleEntity::$definition['table'].'`
-		WHERE `name` = "'.pSQL($name).'"';
+		$safe_names = [];
 
-		return Db::getInstance()->executeS($sql);
+		foreach ($names as $name) {
+			$safe_names[] = '\'' . $this->db->escape($name) . '\'';
+		}
+
+		$safe_names = implode(', ', $safe_names);
+
+		$sql = 'SELECT * FROM ' . $this->getTableNameWithPrefix() . ' WHERE name IN (' . $safe_names . ')';
+
+		return $this->hydrateMany($this->db->select($sql));
 	}
-
-	/**
-	 * Get CMS Roles by their Associated id_cms
-	 * @param $id_cms
-	 * @return array|false
-	 * @throws PrestaShopDatabaseException
-	 */
-	public function getRoleByIdCms($id_cms)
-	{
-		$sql = '
-		SELECT *
-		FROM `'._DB_PREFIX_.CMSRoleEntity::$definition['table'].'`
-		WHERE `id_cms` ='.(int)$id_cms;
-
-		return Db::getInstance()->executeS($sql);
-	}
-
-	/**
-	 * Get CMS Roles by their reference names
-	 * @param $names_array
-	 * @return array|false
-	 * @throws PrestaShopDatabaseException
-	 * @throws PrestaShopExceptionCore
-	 */
-	public function getCMSRolesWhereNamesIn($names_array)
-	{
-		if (!is_array($names_array))
-			throw new PrestaShopExceptionCore('Expected array given '.gettype($names_array));
-
-		$names_exploded = implode('","', $names_array);
-
-		$sql = '
-		SELECT *
-		FROM `'._DB_PREFIX_.CMSRoleEntity::$definition['table'].'`
-		WHERE `name` IN ("'.$names_exploded.'")';
-
-		return Db::getInstance()->executeS($sql);
-	}
-
-	/**
-	 * Get associated CMS id from CMS Role reference name
-	 * @param $reference_name
-	 * @return array|false
-	 */
-	public function getCMSIdAssociatedFromName($reference_name)
-	{
-		$sql = '
-		SELECT `id_cms`
-		FROM `'._DB_PREFIX_.CMSRoleEntity::$definition['table'].'`
-		WHERE `name` = "'.pSQL($reference_name).'"';
-
-		return Db::getInstance()->getRow($sql);
-	}
-
-
-	/**
-	 * Return all CMS roles already associated to a CMS Page
-	 * @return array|false
-	 * @throws PrestaShopDatabaseException
-	 */
-	public function getCMSRolesAssociated()
-	{
-		$sql = '
-			SELECT *
-			FROM `'._DB_PREFIX_.CMSRoleEntity::$definition['table'].'`
-			WHERE `id_cms` != 0
-		';
-
-		$result = Db::getInstance()->executeS($sql);
-
-		if ($result === false || $result === null)
-			$result = array();
-
-		return $result;
-	}
-
 }
