da971032a6101548bb2290a0ed8a1802063d1a24
Xavier Gouley <xavier.gouley@prestashop.com>
2015-06-12 14:36:06 +0200

    [*] Security : Add "change password" screen accessible from a "forgot password" query sent by mail. The site will not generate a random password anymore but will ask the user to type a new one from a secured unique URL. Part of #PSCSX-5770 for front only. (cherry picked from commit 0fc208ee5e454ae39fae0023b0f71809a361e300)
    (cherry picked from commit 863a2f264e461d03d7a230c50ed46e73ac015345)

--- install-dev/upgrade/sql/1.7.0.0.sql
+++ install-dev/upgrade/sql/1.7.0.0.sql
@@ -23,3 +23,19 @@ ALTER TABLE `PREFIX_customer` CHANGE COLUMN `passwd` `passwd` varchar(255) NOT N
 INSERT INTO  `PREFIX_configuration` (`id_configuration` ,`id_shop_group` ,`id_shop` ,`name` ,`value` ,`date_add` ,`date_upd`) VALUES (NULL , NULL , NULL ,  'PS_ACTIVE_CRONJOB_EXCHANGE_RATE',  '0',  '0000-00-00 00:00:00',  '0000-00-00 00:00:00');
 
 /* PHP:ps1700_stores(); */;
+
+ALTER TABLE  `PREFIX_currency` DROP  `iso_code_num` ,DROP  `sign` ,DROP  `blank` ,DROP  `format` ,DROP  `decimals` ;
+
+/* Password reset token for new "Forgot my password" screen */
+CREATE TABLE IF NOT EXISTS `PREFIX_reset_token` (
+  `id_reset_token` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_customer` int(10) unsigned NULL DEFAULT 0,
+  `secure_key` varchar(32) DEFAULT NULL,
+  `id_employee` int(10) unsigned NULL DEFAULT 0,
+  `unique_token` varchar(40) DEFAULT NULL,
+  `last_token_gen` datetime NOT NULL,
+  `validity_date` datetime NOT NULL,
+  PRIMARY KEY (`id_reset_token`),
+  KEY (`id_customer`),
+  KEY (`id_customer`)
+) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
