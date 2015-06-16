6b0c4dd030fe3b37c89ba00f554be09ccb8ae3ac
Luc Vandesype <luc.vandesype@gmail.com>
2015-06-16 18:00:29 +0200

    // psr2 cleanup

--- Core/Business/Cldr/Core_Business_Cldr_CldrLocalize.php
+++ Core/Business/Cldr/Core_Business_Cldr_CldrLocalize.php
@@ -26,190 +26,189 @@
 
 class Core_Business_Cldr_CldrLocalize
 {
-	const DEFAULT_LOCALE = 'en';
+    const DEFAULT_LOCALE = 'en';
 
-	protected static $_filters = array(
-		'language' => array('filter' => 'strtolower'),
-		'script' => array('filter' => array('strtolower', 'ucfirst')),
-		'territory' => array('filter' => 'strtoupper'),
-		'variant' => array('filter' => 'strtoupper')
-	);
+    protected static $_filters = array(
+        'language' => array('filter' => 'strtolower'),
+        'script' => array('filter' => array('strtolower', 'ucfirst')),
+        'territory' => array('filter' => 'strtoupper'),
+        'variant' => array('filter' => 'strtoupper')
+    );
 
-	private static $_browserLocales;
-	private static $_environmentLocale;
-	private static $_locale;
+    private static $_browserLocales;
+    private static $_environmentLocale;
+    private static $_locale;
 
-	public function __toString()
-	{
-		return self::toString();
-	}
+    public function __toString()
+    {
+        return self::toString();
+    }
 
-	public static function getBrowserLocales()
-	{
-		if (self::$_browserLocales !== null) {
-			return self::$_browserLocales;
-		}
+    public static function getBrowserLocales()
+    {
+        if (self::$_browserLocales !== null) {
+            return self::$_browserLocales;
+        }
 
-		$regex  = '(?P<locale>[\w\-]+)+(?:;q=(?P<quality>[0-9]+\.[0-9]+))?';
-		$result = array();
+        $regex  = '(?P<locale>[\w\-]+)+(?:;q=(?P<quality>[0-9]+\.[0-9]+))?';
+        $result = array();
 
-		$httpLanguages = getenv('HTTP_ACCEPT_LANGUAGE');
+        $httpLanguages = getenv('HTTP_ACCEPT_LANGUAGE');
 
-		if (empty($httpLanguages)) {
-			if (array_key_exists('HTTP_ACCEPT_LANGUAGE', $_SERVER)) {
-				$httpLanguages = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
-			} else {
-				return $result;
-			}
-		}
+        if (empty($httpLanguages)) {
+            if (array_key_exists('HTTP_ACCEPT_LANGUAGE', $_SERVER)) {
+                $httpLanguages = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+            } else {
+                return $result;
+            }
+        }
 
-		foreach (explode(',', $httpLanguages) as $language) {
-			if (preg_match("/{$regex}/", $language, $matches)) {
-				$quality = isset($matches['quality']) ? $matches['quality'] : 1;
-				$result[self::_canonicalize($matches['locale'])] = $quality;
-			}
-		}
+        foreach (explode(',', $httpLanguages) as $language) {
+            if (preg_match("/{$regex}/", $language, $matches)) {
+                $quality = isset($matches['quality']) ? $matches['quality'] : 1;
+                $result[self::_canonicalize($matches['locale'])] = $quality;
+            }
+        }
 
-		arsort($result);
-		$result = array_keys($result);
-		self::$_browserLocales = $result;
-		return $result;
-	}
+        arsort($result);
+        $result = array_keys($result);
+        self::$_browserLocales = $result;
+        return $result;
+    }
 
-	public static function getEnvironmentLocale()
-	{
-		if (self::$_environmentLocale !== null) {
-			return self::$_environmentLocale;
-		}
+    public static function getEnvironmentLocale()
+    {
+        if (self::$_environmentLocale !== null) {
+            return self::$_environmentLocale;
+        }
 
-		$regex = '(?P<locale>[\w\_]+)(\.|@|$)+';
-		$result = array();
+        $regex = '(?P<locale>[\w\_]+)(\.|@|$)+';
+        $result = array();
 
-		$value = setlocale(LC_ALL, 0);
+        $value = setlocale(LC_ALL, 0);
 
-		if ($value != 'C' && $value != 'POSIX' && preg_match("/{$regex}/", $value, $matches)) {
-			$result = (array) $matches['locale'];
+        if ($value != 'C' && $value != 'POSIX' && preg_match("/{$regex}/", $value, $matches)) {
+            $result = (array) $matches['locale'];
 
-			// TODO: Add region handle
-		}
+        // TODO: Add region handle
+        }
 
-		self::$_environmentLocale = $result;
-		return $result;
-	}
+        self::$_environmentLocale = $result;
+        return $result;
+    }
 
-	public static function getLanguage($locale = null)
-	{
-		if (!isset($locale)) {
-			$locale = self::getLocale();
-		}
+    public static function getLanguage($locale = null)
+    {
+        if (!isset($locale)) {
+            $locale = self::getLocale();
+        }
 
-		$locale = explode('_', $locale);
-		return $locale[0];
-	}
+        $locale = explode('_', $locale);
+        return $locale[0];
+    }
 
-	public static function setLocale($value)
-	{
-		if (!is_string($value)) {
-			throw new \InvalidArgumentException(
-				'Invalid type for setLocale function');
-		}
+    public static function setLocale($value)
+    {
+        if (!is_string($value)) {
+            throw new \InvalidArgumentException('Invalid type for setLocale function');
+        }
 
-		self::$_locale = self::_canonicalize($value);
-	}
+        self::$_locale = self::_canonicalize($value);
+    }
 
-	public static function getLocale()
-	{
-		if (!isset(self::$_locale)) {
-			self::$_locale = self::getPreferedLocale();
-		}
+    public static function getLocale()
+    {
+        if (!isset(self::$_locale)) {
+            self::$_locale = self::getPreferedLocale();
+        }
 
-		return self::$_locale;
-	}
+        return self::$_locale;
+    }
 
-	public static function getPreferedLocale($locale = null)
-	{
-		if ($locale instanceof self) {
-			$locale = $locale->toString();
-		}
+    public static function getPreferedLocale($locale = null)
+    {
+        if ($locale instanceof self) {
+            $locale = $locale->toString();
+        }
 
-		if ($locale === 'browser') {
-			$locale = self::getBrowserLocales();
-		}
+        if ($locale === 'browser') {
+            $locale = self::getBrowserLocales();
+        }
 
-		if ($locale === 'environment') {
-			$locale = self::getEnvironmentLocale();
-		}
+        if ($locale === 'environment') {
+            $locale = self::getEnvironmentLocale();
+        }
 
-		if (($locale === 'auto') or ($locale === null)) {
-			$locale = self::getBrowserLocales();
-			$locale += self::getEnvironmentLocale();
-		}
+        if (($locale === 'auto') or ($locale === null)) {
+            $locale = self::getBrowserLocales();
+            $locale += self::getEnvironmentLocale();
+        }
 
-		if (is_array($locale) === true) {
-			reset($locale);
-			$locale = current($locale);
-		}
+        if (is_array($locale) === true) {
+            reset($locale);
+            $locale = current($locale);
+        }
 
-		if ($locale === null || trim($locale) == '') {
-			$locale = self::DEFAULT_LOCALE;
-		}
+        if ($locale === null || trim($locale) == '') {
+            $locale = self::DEFAULT_LOCALE;
+        }
 
-		$locale = self::_canonicalize($locale);
-		return (string)$locale;
-	}
+        $locale = self::_canonicalize($locale);
+        return (string)$locale;
+    }
 
-	public static function getRegion($locale = null)
-	{
-		if (!isset($locale)) {
-			$locale = self::getLocale();
-		}
+    public static function getRegion($locale = null)
+    {
+        if (!isset($locale)) {
+            $locale = self::getLocale();
+        }
 
-		$locale = explode('_', strtoupper($locale));
+        $locale = explode('_', strtoupper($locale));
 
-		if (isset($locale[1]) === true) {
-			return $locale[1];
-		}
+        if (isset($locale[1]) === true) {
+            return $locale[1];
+        }
 
-		return $locale[0];
-	}
+        return $locale[0];
+    }
 
-	public static function toString()
-	{
-		return (string)self::getLocale();
-	}
+    public static function toString()
+    {
+        return (string)self::getLocale();
+    }
 
-	private static function _canonicalize($locale)
-	{
-		if (empty($locale) || $locale == '') {
-			return null;
-		}
+    private static function _canonicalize($locale)
+    {
+        if (empty($locale) || $locale == '') {
+            return null;
+        }
 
-		$regex  = '(?P<language>[a-z]{2,3})(?:[_-](?P<script>[a-z]{4}))?(?:[_-](?P<territory>[a-z]{2}))?(?:[_-](?P<variant>[a-z]{5,}))?';
+        $regex  = '(?P<language>[a-z]{2,3})(?:[_-](?P<script>[a-z]{4}))?(?:[_-](?P<territory>[a-z]{2}))?(?:[_-](?P<variant>[a-z]{5,}))?';
 
-		if (!preg_match("/^{$regex}$/i", $locale, $matches)) {
-			throw new \InvalidArgumentException('Locale "'.$locale.'" could not be parsed');
-		}
+        if (!preg_match("/^{$regex}$/i", $locale, $matches)) {
+            throw new \InvalidArgumentException('Locale "'.$locale.'" could not be parsed');
+        }
 
-		$tags = array_filter(array_intersect_key($matches, static::$_filters));
+        $tags = array_filter(array_intersect_key($matches, static::$_filters));
 
-		foreach ($tags as $name => &$tag) {
-			foreach ((array)static::$_filters[$name]['filter'] as $filter) {
-				$tag = $filter($tag);
-			}
-		}
+        foreach ($tags as $name => &$tag) {
+            foreach ((array)static::$_filters[$name]['filter'] as $filter) {
+                $tag = $filter($tag);
+            }
+        }
 
-		$result = array();
+        $result = array();
 
-		foreach (static::$_filters as $name => $value) {
-			if (isset($tags[$name])) {
-				$result[] = $tags[$name];
-			}
-		}
+        foreach (static::$_filters as $name => $value) {
+            if (isset($tags[$name])) {
+                $result[] = $tags[$name];
+            }
+        }
 
-		if ($result) {
-			return implode('_', $result);
-		}
+        if ($result) {
+            return implode('_', $result);
+        }
 
-		return $result;
-	}
+        return $result;
+    }
 }
--- Core/Business/Cldr/Core_Business_Cldr_CldrRepository.php
+++ Core/Business/Cldr/Core_Business_Cldr_CldrRepository.php
@@ -33,200 +33,205 @@ use ICanBoogie\CLDR\NumberFormatter;
 
 class Core_Business_Cldr_CldrRepository
 {
-	protected $cldrCacheFolder;
-	protected $repository;
-	protected $localeRepository;
-	protected $region;
-	protected $locale;
+    protected $cldrCacheFolder;
+    protected $repository;
+    protected $localeRepository;
+    protected $region;
+    protected $locale;
 
-	public function __construct()
-	{
-		$this->cldrCacheFolder = _PS_CACHE_DIR_.'cldr';
+    public function __construct()
+    {
+        $this->cldrCacheFolder = _PS_CACHE_DIR_.'cldr';
 
-		if(!is_dir($this->cldrCacheFolder)){
-			try {
-				mkdir($this->cldrCacheFolder.DIRECTORY_SEPARATOR.'tmp', 0777, true);
-			} catch (Exception $e) {
-				throw new Exception('Cldr cache folder can\'t be created');
-			}
-		}
+        if (!is_dir($this->cldrCacheFolder)) {
+            try {
+                mkdir($this->cldrCacheFolder.DIRECTORY_SEPARATOR.'tmp', 0777, true);
+            } catch (Exception $e) {
+                throw new Exception('Cldr cache folder can\'t be created');
+            }
+        }
 
-		$provider = new RunTimeProvider
-		(
-			new FileProvider
-			(
-				new WebProvider, $this->cldrCacheFolder
-			)
-		);
+        $provider = new RunTimeProvider(
+            new FileProvider(new WebProvider, $this->cldrCacheFolder)
+        );
 
-		$locale = new Core_Business_Cldr_CldrLocalize();
-		$this->locale = $locale->getLanguage();
-		$this->region = $locale->getRegion();
+        $locale = new Core_Business_Cldr_CldrLocalize();
+        $this->locale = $locale->getLanguage();
+        $this->region = $locale->getRegion();
 
-		$this->repository = new Repository($provider);
-		$this->localeRepository = $this->repository->locales[$this->locale];
-	}
+        $this->repository = new Repository($provider);
+        $this->localeRepository = $this->repository->locales[$this->locale];
+    }
 
-	/**
-	 * set a locale
-	 *
-	 * @param string $locale Locale
-	 */
-	public function setLocale($locale){
-		$this->locale = $locale;
-		$this->localeRepository = $this->repository->locales[$this->locale];
-	}
+    /**
+     * set a locale
+     *
+     * @param string $locale Locale
+     */
+    public function setLocale($locale)
+    {
+        $this->locale = $locale;
+        $this->localeRepository = $this->repository->locales[$this->locale];
+    }
 
-	/**
-	 * set a region
-	 *
-	 * @param string $region Region
-	 */
-	public function setRegion($region){
-		$this->region = $region;
-	}
+    /**
+     * set a region
+     *
+     * @param string $region Region
+     */
+    public function setRegion($region)
+    {
+        $this->region = $region;
+    }
 
-	/**
-	 * Return current cldr repository
-	 *
-	 * @return object cldr repository
-	 */
-	public function getRepository()
-	{
-		return $this->repository;
-	}
+    /**
+     * Return current cldr repository
+     *
+     * @return object cldr repository
+     */
+    public function getRepository()
+    {
+        return $this->repository;
+    }
 
-	/**
-	 * get all available currencies
-	 *
-	 * @return array currencies
-	 */
-	public function getAllCurrencies(){
-		$currencies = $this->repository->supplemental['codeMappings'];
-		$datas = array();
-		foreach($currencies as $k=>$v){
-			if($k === 'XTS' ||  strlen($k)!==3){
-				continue;
-			}
-			$currency = $this->getCurrency($k);
-			$datas[] = array( 'name' => ucfirst($currency['name']).' ('.$k.')', 'code' => $k, 'iso_code' => $currency['iso_code']);
-		}
+    /**
+     * get all available currencies
+     *
+     * @return array currencies
+     */
+    public function getAllCurrencies()
+    {
+        $currencies = $this->repository->supplemental['codeMappings'];
+        $datas = array();
+        foreach ($currencies as $k => $v) {
+            if ($k === 'XTS' ||  strlen($k)!==3) {
+                continue;
+            }
+            $currency = $this->getCurrency($k);
+            $datas[] = array(
+                'name' => ucfirst($currency['name']).' ('.$k.')',
+                'code' => $k,
+                'iso_code' => $currency['iso_code']
+            );
+        }
 
-		//sort array naturally
-		uasort($datas, function($x, $y) {
-			return strnatcmp($x["name"], $y["name"]);
-		});
+        //sort array naturally
+        uasort($datas, function ($x, $y) {
+            return strnatcmp($x["name"], $y["name"]);
+        });
 
-		return $datas;
-	}
+        return $datas;
+    }
 
-	/**
-	 * Return a iso code num currency by iso code
-	 *
-	 * @param string $code currency iso code
-	 *
-	 * @return int|null iso code num
-	 */
-	public function getCurrencyIsoCodeNum($code){
-		$currencies = $this->repository->supplemental['codeMappings'];
+    /**
+     * Return a iso code num currency by iso code
+     *
+     * @param string $code currency iso code
+     *
+     * @return int|null iso code num
+     */
+    public function getCurrencyIsoCodeNum($code)
+    {
+        $currencies = $this->repository->supplemental['codeMappings'];
 
-		if(!empty($currencies[$code]) && !empty($currencies[$code]['_numeric'])){
-			return $currencies[$code]['_numeric'];
-		}
+        if (!empty($currencies[$code]) && !empty($currencies[$code]['_numeric'])) {
+            return $currencies[$code]['_numeric'];
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	/**
-	 * Return a currency
-	 *
-	 * @param string $code currency iso code
-	 *
-	 * @return array currency
-	 */
-	public function getCurrency($code = null){
-		if(!$code) {
-			$territory = $this->repository->territories[$this->region];
-			$code = (string)$territory->currency;
-		} elseif(!$this->isCurrencyValid($code)){
-			return array(
-				'name' => $code,
-				'symbol' => '',
-				'code' => $code,
-				'iso_code' => ''
-			);
-		}
+    /**
+     * Return a currency
+     *
+     * @param string $code currency iso code
+     *
+     * @return array currency
+     */
+    public function getCurrency($code = null)
+    {
+        if (!$code) {
+            $territory = $this->repository->territories[$this->region];
+            $code = (string)$territory->currency;
+        } elseif (!$this->isCurrencyValid($code)) {
+            return array(
+                'name' => $code,
+                'symbol' => '',
+                'code' => $code,
+                'iso_code' => ''
+            );
+        }
 
-		$currency = new Currency($this->repository, $code);
-		$localized_currency = $currency->localize($this->locale);
+        $currency = new Currency($this->repository, $code);
+        $localized_currency = $currency->localize($this->locale);
 
-		return array(
-			'name' => $localized_currency->name,
-			'symbol' => $this->getCurrencySymbol($code), //!empty($currency['symbol-alt-narrow']) ? $currency['symbol-alt-narrow'] : $currency['symbol'],
-			'code' => $code,
-			'iso_code' => $this->getCurrencyIsoCodeNum($code)
-		);
-	}
+        return array(
+            'name' => $localized_currency->name,
+            'symbol' => $this->getCurrencySymbol($code),
+            'code' => $code,
+            'iso_code' => $this->getCurrencyIsoCodeNum($code)
+        );
+    }
 
-	/**
-	 * Return a currency symbol
-	 *
-	 * @param string $code currency iso code
-	 *
-	 * @return string symbol
-	 */
-	public function getCurrencySymbol($code){
-		$locale = $this->repository->locales[$this->locale];
-		$currency = $locale['currencies'][$code];
+    /**
+     * Return a currency symbol
+     *
+     * @param string $code currency iso code
+     *
+     * @return string symbol
+     */
+    public function getCurrencySymbol($code)
+    {
+        $locale = $this->repository->locales[$this->locale];
+        $currency = $locale['currencies'][$code];
 
-		return !empty($currency['symbol-alt-narrow']) ? $currency['symbol-alt-narrow'] : $currency['symbol'];
-	}
+        return !empty($currency['symbol-alt-narrow']) ? $currency['symbol-alt-narrow'] : $currency['symbol'];
+    }
 
-	/**
-	 * Return a formatted price
-	 *
-	 * @param float $price
-	 * @param string $code iso code
-	 *
-	 * @return string well formatted price
-	 */
-	public function getPrice($price, $code)
-	{
-		$currency = new Currency($this->repository, $code);
-		$localized_currency = $currency->localize($this->locale);
+    /**
+     * Return a formatted price
+     *
+     * @param float $price
+     * @param string $code iso code
+     *
+     * @return string well formatted price
+     */
+    public function getPrice($price, $code)
+    {
+        $currency = new Currency($this->repository, $code);
+        $localized_currency = $currency->localize($this->locale);
 
-		return $localized_currency->format($price);
-	}
+        return $localized_currency->format($price);
+    }
 
-	/**
-	 * Return a formatted number
-	 *
-	 * @param float $number
-	 *
-	 * @return string well formatted number
-	 */
-	public function getNumber($number)
-	{
-		$formatter = new NumberFormatter($this->repository);
-		$localized_formatter = $formatter->localize($this->locale);
+    /**
+     * Return a formatted number
+     *
+     * @param float $number
+     *
+     * @return string well formatted number
+     */
+    public function getNumber($number)
+    {
+        $formatter = new NumberFormatter($this->repository);
+        $localized_formatter = $formatter->localize($this->locale);
 
-		return $localized_formatter($number);
-	}
+        return $localized_formatter($number);
+    }
 
-	/**
-	 * Check if a curency iso code is valid
-	 *
-	 * @param string $str iso code
-	 *
-	 * @return bool
-	 */
-	private function isCurrencyValid($str)
-	{
-		if($str === 'XTS' ||  strlen($str)!==3 || empty($this->repository->supplemental['codeMappings'][$str])){
-			return false;
-		}
+    /**
+     * Check if a curency iso code is valid
+     *
+     * @param string $str iso code
+     *
+     * @return bool
+     */
+    private function isCurrencyValid($str)
+    {
+        if ($str === 'XTS' ||  strlen($str)!==3 || empty($this->repository->supplemental['codeMappings'][$str])) {
+            return false;
+        }
 
-		return true;
-	}
-
+        return true;
+    }
 }
