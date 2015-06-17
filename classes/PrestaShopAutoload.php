214dcd316395525d6c65d34c5ea32e5c4e29e39c
Luc Vandesype <luc.vandesype@gmail.com>
2015-06-17 10:51:07 +0200

    remove namespaced classes from prestashop autoload. This classes are loaded from composer

--- classes/PrestaShopAutoload.php
+++ classes/PrestaShopAutoload.php
@@ -192,7 +192,8 @@ class PrestaShopAutoload
                     $pattern = '#\W((abstract\s+)?class|interface)\s+(?P<classname>'.basename($file, '.php').'(?:Core)?)'
                                 .'(?:\s+extends\s+'.$namespacePattern.'[a-z][a-z0-9_]*)?(?:\s+implements\s+'.$namespacePattern.'[a-z][\\a-z0-9_]*(?:\s*,\s*'.$namespacePattern.'[a-z][\\a-z0-9_]*)*)?\s*\{#i';
 
-                    if (preg_match($pattern, $content, $m)) {
+                    //DONT LOAD CLASS WITH NAMESPACE - PSR4 autoloaded from composer
+                    if (false === strstr($content, 'namespace ') && preg_match($pattern, $content, $m)) {
                         $classes[$m['classname']] = array(
                             'path' => $path.$file,
                             'type' => trim($m[1]),
