e2e0b2952ef06494b1f9596533d1da68c79179be
Jérôme Nadaud <jerome.nadaud@gmail.com>
2015-06-17 21:52:06 +0200

    // Fix double customization in cart

--- classes/Cart.php
+++ classes/Cart.php
@@ -531,14 +531,15 @@ class CartCore extends ObjectModel
 		// Build ORDER BY
 		$sql->orderBy('cp.`date_add`, cp.`id_product`, cp.`id_product_attribute` ASC');
 
-		/*if (Customization::isFeatureActive())
+		if (Customization::isFeatureActive())
 		{
 			$sql->select('cu.`id_customization`, cu.`quantity` AS customization_quantity');
 			$sql->leftJoin('customization', 'cu',
 				'p.`id_product` = cu.`id_product` AND cp.`id_product_attribute` = cu.`id_product_attribute` AND cu.`id_cart` = '.(int)$this->id);
+			$sql->groupBy('cp.`id_product_attribute`, cp.`id_product`, cp.`id_shop`');
 		}
 		else
-			$sql->select('NULL AS customization_quantity, NULL AS id_customization');*/
+			$sql->select('NULL AS customization_quantity, NULL AS id_customization');
 
 		if (Combination::isFeatureActive())
 		{
@@ -564,6 +565,7 @@ class CartCore extends ObjectModel
 				'p.`reference` AS reference, p.`ean13`,
 				p.`upc` AS upc, product_shop.`minimal_quantity` AS minimal_quantity, product_shop.`wholesale_price` wholesale_price'
 			);
+
 		$result = Db::getInstance()->executeS($sql);
 
 		// Reset the cache before the following return, or else an empty cart will add dozens of queries
