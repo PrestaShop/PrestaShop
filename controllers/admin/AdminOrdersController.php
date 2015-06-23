6b43b45820f64ceb6382a049ee85399158638a92
PrestaEdit <j.danse@prestaedit.com>
2015-06-23 22:29:45 +0200

    [-] submitState

--- controllers/admin/AdminOrdersController.php
+++ controllers/admin/AdminOrdersController.php
@@ -454,55 +454,7 @@ class AdminOrdersControllerCore extends AdminController
 		/* Change order status, add a new entry in order history and send an e-mail to the customer if needed */
 		elseif (Tools::isSubmit('submitState') && isset($order))
 		{
-			if ($this->tabAccess['edit'] === '1')
-			{
-				$order_state = new OrderState(Tools::getValue('id_order_state'));
-
-				if (!Validate::isLoadedObject($order_state))
-					$this->errors[] = Tools::displayError('The new order status is invalid.');
-				else
-				{
-					$current_order_state = $order->getCurrentOrderState();
-					if ($current_order_state->id != $order_state->id)
-					{
-						// Create new OrderHistory
-						$history = new OrderHistory();
-						$history->id_order = $order->id;
-						$history->id_employee = (int)$this->context->employee->id;
-
-						$use_existings_payment = false;
-						if (!$order->hasInvoice())
-							$use_existings_payment = true;
-						$history->changeIdOrderState((int)$order_state->id, $order, $use_existings_payment);
-
-						$carrier = new Carrier($order->id_carrier, $order->id_lang);
-						$templateVars = array();
-						if ($history->id_order_state == Configuration::get('PS_OS_SHIPPING') && $order->shipping_number)
-							$templateVars = array('{followup}' => str_replace('@', $order->shipping_number, $carrier->url));
-
-						// Save all changes
-						if ($history->addWithemail(true, $templateVars))
-						{
-							// synchronizes quantities if needed..
-							if (Configuration::get('PS_ADVANCED_STOCK_MANAGEMENT'))
-							{
-								foreach ($order->getProducts() as $product)
-								{
-									if (StockAvailable::dependsOnStock($product['product_id']))
-										StockAvailable::synchronize($product['product_id'], (int)$product['id_shop']);
-								}
-							}
-
-							Tools::redirectAdmin(self::$currentIndex.'&id_order='.(int)$order->id.'&vieworder&token='.$this->token);
-						}
-						$this->errors[] = Tools::displayError('An error occurred while changing order status, or we were unable to send an email to the customer.');
-					}
-					else
-						$this->errors[] = Tools::displayError('The order has already been assigned this status.');
-				}
-			}
-			else
-				$this->errors[] = Tools::displayError('You do not have permission to edit this.');
+			$this->processSubmitState($order);
 		}
 
 		/* Add a new message for the current order and send an e-mail to the customer if needed */
@@ -1545,6 +1497,59 @@ class AdminOrdersControllerCore extends AdminController
 				}
 				else
 					$this->errors[] = Tools::displayError('The order carrier cannot be updated.');
+			}
+		}
+		else
+			$this->errors[] = Tools::displayError('You do not have permission to edit this.');
+	}
+
+	protected function processSubmitState($order)
+	{
+		if ($this->tabAccess['edit'] === '1')
+		{
+			$order_state = new OrderState(Tools::getValue('id_order_state'));
+
+			if (!Validate::isLoadedObject($order_state))
+				$this->errors[] = Tools::displayError('The new order status is invalid.');
+			else
+			{
+				$current_order_state = $order->getCurrentOrderState();
+				if ($current_order_state->id != $order_state->id)
+				{
+					// Create new OrderHistory
+					$history = new OrderHistory();
+					$history->id_order = $order->id;
+					$history->id_employee = (int)$this->context->employee->id;
+
+					$use_existings_payment = false;
+					if (!$order->hasInvoice())
+						$use_existings_payment = true;
+					$history->changeIdOrderState((int)$order_state->id, $order, $use_existings_payment);
+
+					$carrier = new Carrier($order->id_carrier, $order->id_lang);
+					$templateVars = array();
+					if ($history->id_order_state == Configuration::get('PS_OS_SHIPPING') && $order->shipping_number)
+						$templateVars = array('{followup}' => str_replace('@', $order->shipping_number, $carrier->url));
+
+					// Save all changes
+					if ($history->addWithemail(true, $templateVars))
+					{
+						// synchronizes quantities if needed..
+						if (Configuration::get('PS_ADVANCED_STOCK_MANAGEMENT'))
+						{
+							foreach ($order->getProducts() as $product)
+							{
+								if (StockAvailable::dependsOnStock($product['product_id']))
+									StockAvailable::synchronize($product['product_id'], (int)$product['id_shop']);
+							}
+						}
+
+						Tools::redirectAdmin(self::$currentIndex.'&id_order='.(int)$order->id.'&vieworder&token='.$this->token);
+					}
+					$this->errors[] = Tools::displayError('An error occurred while changing order status, or we were unable to send an email to the customer.');
+				}
+				else
+					$this->errors[] = Tools::displayError('The order has already been assigned this status.');
 			}
 		}
 		else
