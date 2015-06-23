ffac44abf14b5819351346ca579a328482d61b85
PrestaEdit <j.danse@prestaedit.com>
2015-06-23 22:51:01 +0200

    [-] submitEditNote

--- controllers/admin/AdminOrdersController.php
+++ controllers/admin/AdminOrdersController.php
@@ -482,23 +482,7 @@ class AdminOrdersControllerCore extends AdminController
 		}
 		elseif (Tools::isSubmit('submitEditNote'))
 		{
-			$note = Tools::getValue('note');
-			$order_invoice = new OrderInvoice((int)Tools::getValue('id_order_invoice'));
-			if (Validate::isLoadedObject($order_invoice) && Validate::isCleanHtml($note))
-			{
-				if ($this->tabAccess['edit'] === '1')
-				{
-					$order_invoice->note = $note;
-					if ($order_invoice->save())
-						Tools::redirectAdmin(self::$currentIndex.'&id_order='.$order_invoice->id_order.'&vieworder&conf=4&token='.$this->token);
-					else
-						$this->errors[] = Tools::displayError('The invoice note was not saved.');
-				}
-				else
-					$this->errors[] = Tools::displayError('You do not have permission to edit this.');
-			}
-			else
-				$this->errors[] = Tools::displayError('The invoice for edit note was unable to load. ');
+			$this->processSubmitEditNote();
 		}
 		elseif (Tools::isSubmit('submitAddOrder') && ($id_cart = Tools::getValue('id_cart')) &&
 			($module_name = Tools::getValue('payment_module_name')) &&
@@ -1203,6 +1187,27 @@ class AdminOrdersControllerCore extends AdminController
 		}
 		else
 			$this->errors[] = Tools::displayError('You do not have permission to edit this.');
+	}
+
+	protected function processSubmitEditNote()
+	{
+		$note = Tools::getValue('note');
+		$order_invoice = new OrderInvoice((int)Tools::getValue('id_order_invoice'));
+		if (Validate::isLoadedObject($order_invoice) && Validate::isCleanHtml($note))
+		{
+			if ($this->tabAccess['edit'] === '1')
+			{
+				$order_invoice->note = $note;
+				if ($order_invoice->save())
+					Tools::redirectAdmin(self::$currentIndex.'&id_order='.$order_invoice->id_order.'&vieworder&conf=4&token='.$this->token);
+				else
+					$this->errors[] = Tools::displayError('The invoice note was not saved.');
+			}
+			else
+				$this->errors[] = Tools::displayError('You do not have permission to edit this.');
+		}
+		else
+			$this->errors[] = Tools::displayError('The invoice for edit note was unable to load. ');
 	}
 
 	protected function processSubmitAddressShipping($order)
