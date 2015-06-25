a2ba49b67f2cb94d5ea9f7fdc71ceb1c69ffb9ee
gRoussac <gregory.roussac@prestashop.com>
2015-06-25 14:51:42 +0200

    [-] : CORE array_replace_recursive > PHP 5.3

--- classes/Tools.php
+++ classes/Tools.php
@@ -3557,6 +3557,42 @@ exit;
 		}
 		unset($row);
 	}
+
+	/**
+	 * Replaces elements from passed arrays into the first array recursively
+	 * @param array $base The array in which elements are replaced.
+	 * @param array $replacements The array from which elements will be extracted.
+	*/
+	public static function arrayReplaceRecursive($base, $replacements)
+	{
+		if (function_exists('array_replace_recursive'))
+			return array_replace_recursive($base, $replacements);
+
+		foreach (array_slice(func_get_args(), 1) as $replacements)
+		{
+			$bref_stack = array(&$base);
+			$head_stack = array($replacements);
+
+			do
+			{
+				end($bref_stack);
+
+				$bref = &$bref_stack[key($bref_stack)];
+				$head = array_pop($head_stack);
+				unset($bref_stack[key($bref_stack)]);
+				foreach (array_keys($head) as $key)
+					if (isset($key, $bref) && is_array($bref[$key]) && is_array($head[$key]))
+					{
+						$bref_stack[] = &$bref[$key];
+						$head_stack[] = $head[$key];
+					}
+					else
+						$bref[$key] = $head[$key];
+			}
+			while (count($head_stack));
+		}
+		return $base;
+	}
 }
 
 /**
