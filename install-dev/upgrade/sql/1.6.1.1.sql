f1a83442f516afa216a1ebc0d22f24594e9c6526
Conner Burnett <conner@ksrsales.com>
2015-08-04 15:24:00 -0500

    [+] Installer : actionFrontControllerSetMedia in DB
    Hook was executed in FrontController setMedia method, but was not
    available in the hook table.
    This is a useful alternative for modules to use when adding CSS and JS
    files instead of relying on hookTop and hookHeader

--- install-dev/upgrade/sql/1.6.1.1.sql
+++ install-dev/upgrade/sql/1.6.1.1.sql
@@ -6,3 +6,5 @@ UPDATE `PREFIX_tax_rules_group` SET `date_add` = NOW(), `date_upd` = NOW() WHERE
 /* PHP:ps1611_fill_old_order_invoice_shop_addresses(); */;
 
 ALTER TABLE  `PREFIX_order_detail` ADD  `original_wholesale_price` DECIMAL( 20, 6 ) NOT NULL DEFAULT  '0.000000';
+
+INSERT INTO `PREFIX_ps_hook` (`id_hook`, `name`, `title`, `description`, `position`, `live_edit`) VALUES (NULL, 'actionFrontControllerSetMedia', 'actionFrontControllerSetMedia', 'Execute Hook FrontController SetMedia', '0', '0');
