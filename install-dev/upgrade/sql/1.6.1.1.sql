669b521111eeade8780949c8f9a77924891a7eef
Conner <conner@ksrsales.com>
2015-08-04 15:35:14 -0500

    Removed ps prefix from DB call

--- install-dev/upgrade/sql/1.6.1.1.sql
+++ install-dev/upgrade/sql/1.6.1.1.sql
@@ -7,4 +7,4 @@ UPDATE `PREFIX_tax_rules_group` SET `date_add` = NOW(), `date_upd` = NOW() WHERE
 
 ALTER TABLE  `PREFIX_order_detail` ADD  `original_wholesale_price` DECIMAL( 20, 6 ) NOT NULL DEFAULT  '0.000000';
 
-INSERT INTO `PREFIX_ps_hook` (`id_hook`, `name`, `title`, `description`, `position`, `live_edit`) VALUES (NULL, 'actionFrontControllerSetMedia', 'actionFrontControllerSetMedia', 'Execute Hook FrontController SetMedia', '0', '0');
+INSERT INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `position`, `live_edit`) VALUES (NULL, 'actionFrontControllerSetMedia', 'actionFrontControllerSetMedia', 'Execute Hook FrontController SetMedia', '0', '0');
