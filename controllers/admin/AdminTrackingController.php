66f82ea17b80376b3bf856bd60f84a5cfa6b395a
Luc Vandesype <luc.vandesype@gmail.com>
2015-08-12 12:06:54 +0200

    [*] BO : #PSCSX-6463 add tracking for product without photo, price and description

--- controllers/admin/AdminTrackingController.php
+++ controllers/admin/AdminTrackingController.php
@@ -240,6 +240,116 @@ class AdminTrackingControllerCore extends AdminController
         return $this->renderList();
     }
 
+    public function getCustomListProductsWithoutPhoto()
+    {
+        $this->table = 'product';
+        $this->list_id = 'products_without_photo';
+        $this->lang = true;
+        $this->identifier = 'id_product';
+        $this->_orderBy = 'id_product';
+        $this->_orderWay = 'DESC';
+        $this->className = 'Product';
+        $this->_list_index = 'index.php?controller=AdminProducts';
+        $this->_list_token = Tools::getAdminTokenLite('AdminProducts');
+        $this->show_toolbar = false;
+
+        $this->addRowAction('edit');
+        $this->addRowAction('delete');
+
+        $this->fields_list = array(
+            'id_product' => array('title' => $this->l('ID'), 'class' => 'fixed-width-xs', 'align' => 'center'),
+            'reference' => array('title' => $this->l('Reference')),
+            'name' => array('title' => $this->l('Name'), 'filter_key' => 'b!name'),
+            'active' => array('title' => $this->l('Status'), 'type' => 'bool', 'active' => 'status', 'align' => 'center', 'class' => 'fixed-width-xs')
+        );
+
+        $this->clearFilters();
+
+        $this->_join = Shop::addSqlAssociation('product', 'a');
+        $this->_filter = 'AND NOT EXISTS (
+			SELECT 1
+			FROM `'._DB_PREFIX_.'image` i
+			WHERE a.id_product = i.id_product
+		)';
+        $this->toolbar_title = $this->l('List of products without images:');
+        return $this->renderList();
+    }
+
+    public function getCustomListProductsWithoutDescription()
+    {
+        $this->table = 'product';
+        $this->list_id = 'products_without_description';
+        $this->lang = true;
+        $this->identifier = 'id_product';
+        $this->_orderBy = 'id_product';
+        $this->_orderWay = 'DESC';
+        $this->className = 'Product';
+        $this->_list_index = 'index.php?controller=AdminProducts';
+        $this->_list_token = Tools::getAdminTokenLite('AdminProducts');
+        $this->show_toolbar = false;
+
+        $this->addRowAction('edit');
+        $this->addRowAction('delete');
+
+        $this->fields_list = array(
+            'id_product' => array('title' => $this->l('ID'), 'class' => 'fixed-width-xs', 'align' => 'center'),
+            'reference' => array('title' => $this->l('Reference')),
+            'name' => array('title' => $this->l('Name'), 'filter_key' => 'b!name'),
+            'active' => array('title' => $this->l('Status'), 'type' => 'bool', 'active' => 'status', 'align' => 'center', 'class' => 'fixed-width-xs')
+        );
+
+        $this->clearFilters();
+
+        $defaultLanguage = new Language(Configuration::get('PS_LANG_DEFAULT'));
+
+        $this->_join = Shop::addSqlAssociation('product', 'a');
+        $this->_filter = 'AND EXISTS (
+			SELECT 1
+			FROM `'._DB_PREFIX_.'product_lang` pl
+			WHERE
+			a.id_product = pl.id_product AND
+			pl.id_lang = '.(int)$defaultLanguage->id.' AND
+			pl.id_shop = '.(int)$this->context->shop->id.' AND
+			description = "" AND description_short = ""
+		)';
+        $this->toolbar_title = $this->l('List of products without description:');
+        return $this->renderList();
+    }
+
+    public function getCustomListProductsWithoutPrice()
+    {
+        $this->table = 'product';
+        $this->list_id = 'products_without_price';
+        $this->lang = true;
+        $this->identifier = 'id_product';
+        $this->_orderBy = 'id_product';
+        $this->_orderWay = 'DESC';
+        $this->className = 'Product';
+        $this->_list_index = 'index.php?controller=AdminProducts';
+        $this->_list_token = Tools::getAdminTokenLite('AdminProducts');
+        $this->show_toolbar = false;
+
+        $this->addRowAction('edit');
+        $this->addRowAction('delete');
+
+        $this->fields_list = array(
+            'id_product' => array('title' => $this->l('ID'), 'class' => 'fixed-width-xs', 'align' => 'center'),
+            'reference' => array('title' => $this->l('Reference')),
+            'name' => array('title' => $this->l('Name'), 'filter_key' => 'b!name'),
+            'active' => array('title' => $this->l('Status'), 'type' => 'bool', 'active' => 'status', 'align' => 'center', 'class' => 'fixed-width-xs')
+        );
+
+        $this->clearFilters();
+
+        $this->_join = Shop::addSqlAssociation('product', 'a');
+        $this->_filter = ' AND a.price = 0.000000 AND a.wholesale_price = 0.000000 AND NOT EXISTS (
+			SELECT 1
+			FROM `'._DB_PREFIX_.'specific_price` sp
+			WHERE a.id_product = sp.id_product
+		)';
+        $this->toolbar_title = $this->l('List of product without price:');
+        return $this->renderList();
+    }
 
     public function renderList()
     {
@@ -317,6 +427,18 @@ class AdminTrackingControllerCore extends AdminController
 
         if (Tools::isSubmit('submitResetdisabled_products')) {
             $this->processResetFilters('disabled_products');
+        }
+
+        if (Tools::isSubmit('submitResetproducts_without_photo')) {
+            $this->processResetFilters('products_without_photo');
+        }
+
+        if (Tools::isSubmit('submitResetproducts_without_description')) {
+            $this->processResetFilters('products_without_description');
+        }
+
+        if (Tools::isSubmit('submitResetproducts_without_price')) {
+            $this->processResetFilters('products_without_price');
         }
     }
 
