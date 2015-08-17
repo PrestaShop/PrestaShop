1103d599ca5eb2cd943321141c0136d15c5e1149
Julien Martin <martin.julien82@gmail.com>
2015-08-17 18:40:41 +0200

    // MySql upgrade moved to 1.7.0.0 file

--- install-dev/upgrade/sql/1.6.2.0.sql
+++
@@ -1,4 +0,0 @@
-SET NAMES 'utf8';
-
-ALTER TABLE `PREFIX_product` ADD `show_condition` TINYINT(1) NOT NULL DEFAULT '1' AFTER `available_date`;
-ALTER TABLE `PREFIX_product_shop` ADD `show_condition` TINYINT(1) NOT NULL DEFAULT '1' AFTER `available_date`;
