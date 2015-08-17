c91168e54424b18eac8cc6c1d0aaa759262ccdc5
cedricfontaine <cfontaine@spidmail.net>
2015-08-17 09:22:13 -0400

    Update OrderController.php

--- controllers/front/OrderController.php
+++ controllers/front/OrderController.php
@@ -57,8 +57,9 @@ class OrderControllerCore extends ParentOrderController
         // If some products have disappear
         if (is_array($products)) {
             $this->step = 0;
-            foreach ($products as $product)
+            foreach ($products as $product) {
                 $this->errors[] = sprintf(Tools::displayError('An item (%s) in your cart is no longer available in this quantity (%s available). You cannot proceed with your order until the quantity is adjusted.'), $product['name'], $product['stock_quantity']);
+            }
         }
 
         // Check minimal amount
