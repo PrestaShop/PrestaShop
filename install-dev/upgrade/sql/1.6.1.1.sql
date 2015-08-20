6f17d52f75450bef42a7730986bc4fcc31619e1a
joce <jocelyn.Fournier@gmail.com>
2015-08-20 13:34:45 +0200

    [-] BO : Fix issue with cover image (PSCSX-6473)

--- install-dev/upgrade/sql/1.6.1.1.sql
+++ install-dev/upgrade/sql/1.6.1.1.sql
@@ -8,3 +8,5 @@ ALTER TABLE  `PREFIX_order_detail` ADD  `original_wholesale_price` DECIMAL( 20, 
 ALTER TABLE `PREFIX_specific_price` DROP KEY `id_product_2`;
 
 ALTER IGNORE TABLE `PREFIX_specific_price` ADD UNIQUE KEY `id_product_2` (`id_cart`, `id_product`,`id_shop`,`id_shop_group`,`id_currency`,`id_country`,`id_group`,`id_customer`,`id_product_attribute`,`from_quantity`,`id_specific_price_rule`,`from`,`to`);
+
+UPDATE `PREFIX_image_shop` ish, `PREFIX_image` i SET ish.id_product = i.id_product WHERE i.id_image=ish.id_image;
