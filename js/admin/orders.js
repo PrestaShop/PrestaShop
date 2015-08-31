92eb1852deb80bd816ec85435dc3f6fbc9f2834f
David-Julian BUCH <buchdj@gmail.com>
2015-09-01 00:10:35 +0200

    Product price can be 0 in case of a gift for example

--- js/admin/orders.js
+++ js/admin/orders.js
@@ -495,7 +495,7 @@ function init()
 			go = false;
 		}
 
-		if ($('input#add_product_product_price_excl').val() == 0)
+		if ($('input#add_product_product_price_excl').val() < 0)
 		{
 			jAlert(txt_add_product_no_product_price);
 			go = false;
