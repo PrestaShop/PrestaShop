4955b1020e59f4a937466104191a7c8219106d71
Julien Bourdeau <julien@sigerr.org>
2015-09-02 18:58:40 +0200

    Remove specific cms.css and cms.js

--- controllers/front/CmsController.php
+++ controllers/front/CmsController.php
@@ -84,17 +84,6 @@ class CmsControllerCore extends FrontController
         }
     }
 
-    public function setMedia()
-    {
-        parent::setMedia();
-
-        if ($this->assignCase == 1) {
-            $this->addJS(_THEME_JS_DIR_.'cms.js');
-        }
-
-        $this->addCSS(_THEME_CSS_DIR_.'cms.css');
-    }
-
     /**
      * Assign template vars related to page content
      * @see FrontController::initContent()
