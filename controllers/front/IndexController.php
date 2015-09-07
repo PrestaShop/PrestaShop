3649266fe4be4f1f8dfcf2b135b124579c58b966
djfm <fm.de.jouvencel@gmail.com>
2015-09-07 15:00:11 +0200

    [*] StarterTheme : Home now renders

--- controllers/front/IndexController.php
+++ controllers/front/IndexController.php
@@ -35,11 +35,8 @@ class IndexControllerCore extends FrontController
     public function initContent()
     {
         parent::initContent();
-        $this->addJS(_THEME_JS_DIR_.'index.js');
-
-        $this->context->smarty->assign(array('HOOK_HOME' => Hook::exec('displayHome'),
-            'HOOK_HOME_TAB' => Hook::exec('displayHomeTab'),
-            'HOOK_HOME_TAB_CONTENT' => Hook::exec('displayHomeTabContent')
+        $this->context->smarty->assign(array(
+            'HOOK_HOME' => Hook::exec('displayHome'),
         ));
         $this->setTemplate('index.tpl');
     }
