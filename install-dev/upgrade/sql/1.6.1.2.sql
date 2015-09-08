2fdd210be3720bf5c43639482c1098a9dab096b8
joce <jocelyn.Fournier@gmail.com>
2015-09-08 10:02:18 +0200

    [-] BO : Fix performance issue with tags

--- install-dev/upgrade/sql/1.6.1.2.sql
+++ install-dev/upgrade/sql/1.6.1.2.sql
@@ -12,13 +12,13 @@ REPLACE INTO `PREFIX_tag_count` (id_group, id_tag, id_lang, id_shop, counter)
 				AND EXISTS(SELECT 1 FROM `PREFIX_category_product` cp
 								LEFT JOIN `PREFIX_category_group` cgo ON (cp.`id_category` = cgo.`id_category`)
 								WHERE cgo.`id_group` = cg.id_group AND product_shop.`id_product` = cp.`id_product`)
-				GROUP BY pt.id_tag, pt.id_lang, cg.id_group, id_shop;
+				GROUP BY pt.id_tag, pt.id_lang, cg.id_group, id_shop ORDER BY NULL;
 REPLACE INTO `PREFIX_tag_count` (id_group, id_tag, id_lang, id_shop, counter)
 			SELECT 0, pt.id_tag, pt.id_lang, id_shop, COUNT(pt.id_tag) AS times
 				FROM `PREFIX_product_tag` pt
 				INNER JOIN `PREFIX_product_shop` product_shop
 					USING (id_product)
 				WHERE product_shop.`active` = 1
-				GROUP BY pt.id_tag, pt.id_lang, id_shop;
+				GROUP BY pt.id_tag, pt.id_lang, id_shop ORDER BY NULL;
 
 TRUNCATE TABLE `PREFIX_smarty_last_flush`;
