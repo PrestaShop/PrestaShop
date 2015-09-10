f04e4a9b237369b2ec893787c0f5013c2d86f25b
Amazzing <Amazzing@users.noreply.github.com>
2015-09-11 00:22:37 +0300

    [-] Fixed currency convertion
    When using the identity operator (===), object variables are identical if and only if they refer to the same instance of the same class.
    In some cases $currency_from and $currency_to are same, but they refer don't refer to the same instance
    
    See DiscountController.php: 
    $discount['value'] = Tools::convertPriceFull(
        $discount['value'],
        new Currency((int)$discount['reduction_currency']),
        new Currency((int)$this->context->cart->id_currency)
    );

--- classes/Tools.php
+++ classes/Tools.php
@@ -629,7 +629,7 @@ class ToolsCore
 	 */
 	public static function convertPriceFull($amount, Currency $currency_from = null, Currency $currency_to = null)
 	{
-		if ($currency_from === $currency_to)
+		if ($currency_from == $currency_to)
 			return $amount;
 
 		if ($currency_from === null)
