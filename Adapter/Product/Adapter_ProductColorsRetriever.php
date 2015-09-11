349bbed90e9d044fb4897bb8199a9565a10c1c8b
Maxime Biloé <maxime.biloe@prestashop.com>
2015-09-11 17:29:11 +0200

    //Rework ProductController in progress

--- Adapter/Product/Adapter_ProductColorsRetriever.php
+++ Adapter/Product/Adapter_ProductColorsRetriever.php
@@ -4,6 +4,6 @@ class Adapter_ProductColorsRetriever
 {
     public function getColoredVariants($id_product)
     {
-        return current(Product::getAttributesColorList([$id_product]));
+        return (is_array(Product::getAttributesColorList([$id_product]))) ? current(Product::getAttributesColorList([$id_product])) : null;
     }
 }
