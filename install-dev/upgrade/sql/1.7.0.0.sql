8593720e4a69ea0671e87e9cffc7fb77d84748ac
Julien Bourdeau <julien@sigerr.org>
2015-09-11 17:27:06 +0200

    // Move sql upgrade from 1.6.2.0.sql to 1.7.0.0.sql

--- install-dev/upgrade/sql/1.7.0.0.sql
+++ install-dev/upgrade/sql/1.7.0.0.sql
@@ -1,5 +1,7 @@
 SET NAMES 'utf8';
 
+INSERT INTO `PREFIX_configuration` (`name` , `value` , `date_add` , `date_upd`) VALUES ('PS_SMARTY_LOCAL', '0', NOW(), NOW());
+
 INSERT INTO `PREFIX_hook_alias` (`name`, `alias`) VALUES ('actionCartUpdateQuantityBefore', 'actionBeforeCartUpdateQty');
 INSERT INTO `PREFIX_hook_alias` (`name`, `alias`) VALUES ('actionAjaxDieBefore', 'actionBeforeAjaxDie');
 INSERT INTO `PREFIX_hook_alias` (`name`, `alias`) VALUES ('actionAuthenticationBefore', 'actionBeforeAuthentication');
@@ -15,3 +17,5 @@ ALTER TABLE PREFIX_employee ADD `reset_password_token` varchar(40) DEFAULT NULL;
 ALTER TABLE PREFIX_employee ADD `reset_password_validity` datetime DEFAULT NULL;
 
 INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VALUES ('PS_PASSWD_RESET_VALIDITY', '1440', NOW(), NOW());
+
+ALTER TABLE `PREFIX_customer` CHANGE COLUMN `passwd` `passwd` varchar(255) NOT NULL;
