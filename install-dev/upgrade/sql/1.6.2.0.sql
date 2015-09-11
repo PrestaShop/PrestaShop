e8196832402e80c9a8b2159f3fc580902f49deb5
Julien Bourdeau <julien@sigerr.org>
2015-09-11 17:27:06 +0200

    // Move sql upgrade from 1.6.2.0.sql to 1.7.0.0.sql
    (cherry picked from commit 8593720e4a69ea0671e87e9cffc7fb77d84748ac)

--- install-dev/upgrade/sql/1.6.2.0.sql
+++
@@ -1,4 +0,0 @@
-SET NAMES 'utf8';
-
-INSERT INTO `PREFIX_configuration` (`name` , `value` , `date_add` , `date_upd`) VALUES ('PS_SMARTY_LOCAL', '0', NOW(), NOW());
-ALTER TABLE `PREFIX_customer` CHANGE COLUMN `passwd` `passwd` varchar(255) NOT NULL;
