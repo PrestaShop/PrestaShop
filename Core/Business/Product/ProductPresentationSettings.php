baa38da10ee5b1e8cce38d3f65a2008bd6a29a92
djfm <fm.de.jouvencel@gmail.com>
2015-09-13 16:25:02 +0200

    [*] Core : Introducing ProductPresenter for the front-end

---
+++ Core/Business/Product/ProductPresentationSettings.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PrestaShop\PrestaShop\Core\Business\Product;
+
+class ProductPresentationSettings
+{
+    public $catalog_mode;
+    public $restricted_country_mode;
+    public $include_taxes;
+}
