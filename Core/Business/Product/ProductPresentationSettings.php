95c164ede79a58a2f6b07a470e040283b8460f56
djfm <fm.de.jouvencel@gmail.com>
2015-09-14 18:35:50 +0200

    [*] Starter Theme Category : in stock labels

--- Core/Business/Product/ProductPresentationSettings.php
+++ Core/Business/Product/ProductPresentationSettings.php
@@ -8,4 +8,5 @@ class ProductPresentationSettings
     public $restricted_country_mode;
     public $include_taxes;
     public $allow_add_variant_to_cart_from_listing;
+    public $stock_management_enabled;
 }
