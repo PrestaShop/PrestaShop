b519a855de343caf7cc3ea8efa9af6e747d233a2
Luc Vandesype <luc.vandesype@gmail.com>
2015-09-15 10:59:22 +0200

    [-] : fix obsolete upgrade sql file

--- install-dev/upgrade/sql/1.7.0.0.sql
+++ install-dev/upgrade/sql/1.7.0.0.sql
@@ -19,3 +19,5 @@ ALTER TABLE PREFIX_employee ADD `reset_password_validity` datetime DEFAULT NULL;
 INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VALUES ('PS_PASSWD_RESET_VALIDITY', '1440', NOW(), NOW());
 
 ALTER TABLE `PREFIX_customer` CHANGE COLUMN `passwd` `passwd` varchar(255) NOT NULL;
+
+INSERT INTO  `PREFIX_configuration` (`id_configuration` ,`id_shop_group` ,`id_shop` ,`name` ,`value` ,`date_add` ,`date_upd`) VALUES (NULL , NULL , NULL ,  'PS_ACTIVE_CRONJOB_EXCHANGE_RATE',  '0',  '0000-00-00 00:00:00',  '0000-00-00 00:00:00');
