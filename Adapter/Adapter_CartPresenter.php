cf6cd3efc204ce438456974f6363bed96e8f86a6
djfm <fm.de.jouvencel@gmail.com>
2015-09-19 15:56:51 +0200

    [*] Introducing CartPresenter for templates related to the cart

---
+++ Adapter/Adapter_CartPresenter.php
@@ -0,0 +1,43 @@
+<?php
+
+class Adapter_CartPresenter
+{
+    private $pricePresenter;
+
+    public function __construct()
+    {
+        $this->pricePresenter = new Adapter_PricePresenter;
+    }
+
+    private function includeTaxes()
+    {
+        return !Product::getTaxCalculationMethod(Context::getContext()->cookie->id_customer);
+    }
+
+    protected function presentProduct(array $rawProduct)
+    {
+        $product['name'] = $rawProduct['name'];
+        $product['price'] = $this->pricePresenter->format(
+            $this->pricePresenter->convertAmount(
+                $this->includeTaxes() ?
+                $rawProduct['total_wt'] :
+                $rawProduct['total']
+            )
+        );
+
+        $product['quantity'] = $rawProduct['quantity'];
+
+        return $product;
+    }
+
+    public function present(Cart $cart)
+    {
+        $rawProducts = $cart->getProducts(true);
+
+        $products = array_map([$this, 'presentProduct'], $rawProducts);
+
+        return [
+            'products' => $products
+        ];
+    }
+}
