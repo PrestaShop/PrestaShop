2b561c9d11bc42a384f5c06a20266c123314b032
djfm <fm.de.jouvencel@gmail.com>
2015-09-19 18:17:02 +0200

    Display cart totals in a generic way

--- Adapter/Adapter_CartPresenter.php
+++ Adapter/Adapter_CartPresenter.php
@@ -4,16 +4,23 @@ class Adapter_CartPresenter
 {
     private $pricePresenter;
     private $link;
+    private $translator;
 
     public function __construct()
     {
         $this->pricePresenter = new Adapter_PricePresenter;
         $this->link = Context::getContext()->link;
+        $this->translator = new Adapter_Translator;
     }
 
     private function includeTaxes()
     {
         return !Product::getTaxCalculationMethod(Context::getContext()->cookie->id_customer);
+    }
+
+    private function shouldShowTaxLine()
+    {
+        return Configuration::get('PS_TAX_DISPLAY');
     }
 
     private function getAddToCartURL(array $product)
@@ -49,11 +56,32 @@ class Adapter_CartPresenter
 
         $products = array_map([$this, 'presentProduct'], $rawProducts);
 
+        $totals = [];
+
+        $total_excluding_tax = $cart->getOrderTotal(false);
+        $total_including_tax = $cart->getOrderTotal(true);
+
+        $total = $this->includeTaxes() ? $total_including_tax : $total_excluding_tax;
+
+        if ($this->shouldShowTaxLine()) {
+            $totals['tax'] = [
+                'type'   => 'tax',
+                'label'  => $this->translator->l('Tax', 'Cart'),
+                'amount' => $this->pricePresenter->convertAndFormat(
+                    $total_including_tax - $total_excluding_tax
+                )
+            ];
+        }
+
+        $totals['total'] = [
+            'type'   => 'total',
+            'label'  => $this->translator->l('Total', 'Cart'),
+            'amount' => $this->pricePresenter->convertAndFormat($total)
+        ];
+
         return [
             'products' => $products,
-            'total'    => $this->pricePresenter->convertAndFormat(
-                $cart->getOrderTotal($this->includeTaxes())
-            )
+            'totals'   => $totals
         ];
     }
 }
