0da3ee5f3bb62ae48a8d38b99692d8a12b2b03b5
djfm <fm.de.jouvencel@gmail.com>
2015-09-24 10:40:21 +0200

    [*] StarterTheme : Added Customizations Handling to CartPresenter

--- Adapter/Adapter_CartPresenter.php
+++ Adapter/Adapter_CartPresenter.php
@@ -5,12 +5,14 @@ class Adapter_CartPresenter
     private $pricePresenter;
     private $link;
     private $translator;
+    private $imageRetriever;
 
     public function __construct()
     {
         $this->pricePresenter = new Adapter_PricePresenter;
         $this->link = Context::getContext()->link;
         $this->translator = new Adapter_Translator;
+        $this->imageRetriever = new Adapter_ImageRetriever($this->link);
     }
 
     private function includeTaxes()
@@ -53,13 +55,64 @@ class Adapter_CartPresenter
         return $product;
     }
 
+    protected function addCustomizedData(array $products, Cart $cart)
+    {
+        $data = Product::getAllCustomizedDatas($cart->id);
+
+        return array_map(function (array $product) use ($data) {
+
+            $product['customizations'] = [];
+
+            $id_product = (int)$product['id_product'];
+            $id_product_attribute = (int)$product['id_product_attribute'];
+            if (array_key_exists($id_product, $data)) {
+                if (array_key_exists($id_product_attribute, $data[$id_product])) {
+                    foreach ($data[$id_product] as $byAddress) {
+                        foreach ($byAddress as $customizations) {
+                            foreach ($customizations as $customization) {
+                                $presentedCustomization = [
+                                    'quantity' => $customization['quantity'],
+                                    'fields'   => []
+                                ];
+                                foreach ($customization['datas'] as $byType) {
+                                    $field = [];
+                                    foreach ($byType as $data) {
+                                        switch ($data['type']) {
+                                            case Product::CUSTOMIZE_FILE:
+                                                $field['type'] = 'image';
+                                                $field['image'] = $this->imageRetriever->getCustomizationImage(
+                                                    $data['value']
+                                                );
+                                                break;
+                                            case Product::CUSTOMIZE_TEXTFIELD:
+                                                $field['type'] = 'text';
+                                                $field['text'] = $data['value'];
+                                                break;
+                                            default:
+                                                $field['type'] = null;
+                                        }
+                                        $field['label'] = $data['name'];
+                                    }
+                                    $presentedCustomization['fields'][] = $field;
+                                }
+                                $product['customizations'][] = $presentedCustomization;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return $product;
+        }, $products);
+    }
+
     public function present(Cart $cart)
     {
         $rawProducts = $cart->getProducts(true);
 
         $products = array_map([$this, 'presentProduct'], $rawProducts);
 
-
+        $products = $this->addCustomizedData($products, $cart);
         $totals = [];
 
         $total_excluding_tax = $cart->getOrderTotal(false);
