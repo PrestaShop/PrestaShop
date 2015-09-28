a1a902c770e5cd93b98b89fef65539b824ed7043
djfm <fm.de.jouvencel@gmail.com>
2015-09-28 12:24:39 +0200

    [+] StarterTheme Tests : Changing variants from the product sheet

--- tests/StarterTheme/fixtures.js
+++ tests/StarterTheme/fixtures.js
@@ -1,5 +1,14 @@
 module.exports = {
     aProductWithVariants: {
-        id: 5
+        id: 5,
+        // Format is: key = id of attribute group, value = id of attribute value
+        defaultVariant: {
+            '1': 1,
+            '3': 16
+        },
+        anotherVariant: {
+            '1': 3,
+            '3': 14
+        }
     }
 };
--- tests/StarterTheme/specs/product-page.js
+++ tests/StarterTheme/specs/product-page.js
@@ -1,6 +1,41 @@
 /* global describe, it, browser */
 
 var fixtures = require('../fixtures');
+var _ = require('underscore');
+var q = require('q');
+
+function checkGroupHasValue (valueId, groupId) {
+  var groupSelector = '#group_' + groupId;
+  return browser.getAttribute(
+    groupSelector + ' [selected], ' + groupSelector + ' [checked]',
+    'value'
+  ).then(function (selectedValueId) {
+    selectedValueId.should.equal(valueId.toString());
+  });
+}
+
+function checkVariantIsSelected (variant) {
+  return q.all(_.map(variant, checkGroupHasValue));
+}
+
+function chooseValueInGroup (valueId, groupId) {
+  var groupSelector = '#group_' + groupId;
+  return browser.getTagName(groupSelector)
+    .then(function (tagName) {
+      if (tagName === 'select') {
+        return browser.selectByValue(groupSelector, valueId);
+      } else {
+        return browser.click(groupSelector + ' [value="' + valueId + '"]');
+      }
+    })
+  ;
+}
+
+function chooseVariant (variant) {
+  return q.all(_.map(variant, chooseValueInGroup)).then(function () {
+    return browser.click('.product-variants .product-refresh');
+  });
+}
 
 describe('The product page', function () {
   describe('of a product with variants', function () {
@@ -10,6 +45,28 @@ describe('The product page', function () {
         .then(function () {
           return browser.element('.product-variants');
         })
+      ;
+    });
+
+    it('should show the default variant by default', function () {
+      return browser
+        .productPage(fixtures.aProductWithVariants.id)
+        .then(checkVariantIsSelected.bind(
+          undefined,
+          fixtures.aProductWithVariants.defaultVariant
+        ))
+      ;
+    });
+
+    it('should allow selecting another variant', function () {
+      var variant = fixtures.aProductWithVariants.anotherVariant;
+
+      return browser
+        .productPage(fixtures.aProductWithVariants.id)
+        .then(function () {
+          return chooseVariant(variant);
+        })
+        .then(checkVariantIsSelected.bind(undefined, variant))
       ;
     });
   });
