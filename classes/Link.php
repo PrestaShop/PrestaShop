b0ea6808fc2d5fa8686fec13c1860de0ded387ed
Julien Bourdeau <julien@sigerr.org>
2015-09-29 19:33:32 +0200

    // Improve {url} for Category, Module, CMS

--- classes/Link.php
+++ classes/Link.php
@@ -796,11 +796,65 @@ class LinkCore
             $params['params'] = array_merge($id, $params['params']);
         }
 
+        $default = [
+            'id_lang' => $context->language->id,
+            'id_shop' => null,
+            'alias' => null,
+            'ssl' => null,
+            'relative_protocol' => false,
+        ];
+        $params = array_merge($default, $params);
+
         $url_parameters = http_build_query($params['params']);
 
         switch ($params['entity']) {
+            case "category":
+                $params = array_merge(['selected_filters' => null], $params);
+                $link = $context->link->getCategoryLink(
+                    new Category($params['id'], $params['id_lang']),
+                    $params['alias'],
+                    $params['id_lang'],
+                    $params['selected_filters'],
+                    $params['id_shop'],
+                    $params['relative_protocol']
+                );
+                break;
+            case "cms":
+                $link = $context->link->getCMSLink(
+                    new CMS($params['id'], $params['id_lang']),
+                    $params['alias'],
+                    $params['ssl'],
+                    $params['id_lang'],
+                    $params['id_shop'],
+                    $params['relative_protocol']
+                );
+                break;
+            case "module":
+                $params = array_merge([
+                    'selected_filters' => null,
+                    'params' => array(),
+                    'controller' => 'default',
+                ], $params);
+                $link = $context->link->getModuleLink(
+                    $params['name'],
+                    $params['controller'],
+                    $params['params'],
+                    $params['ssl'],
+                    $params['id_lang'],
+                    $params['id_shop'],
+                    $params['relative_protocol']
+                );
+                break;
             default:
-                $link = $context->link->getPageLink($params['entity'], true, null, $url_parameters);
+                $link = $context->link->getPageLink(
+                    $params['entity'],
+                    $params['ssl'],
+                    $params['id_lang'],
+                    $url_parameters,
+                    false,
+                    $params['id_shop'],
+                    $params['relative_protocol']
+                );
                 break;
         }
 
