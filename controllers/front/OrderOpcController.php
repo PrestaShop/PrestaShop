e371c80df1d1eb6df2eaf310db8ef500c4d048b5
Julien Bourdeau <julien@sigerr.org>
2015-09-30 11:27:33 +0200

    [+] StarterTheme : Introduce rivets.js

--- controllers/front/OrderOpcController.php
+++ controllers/front/OrderOpcController.php
@@ -326,21 +326,9 @@ class OrderOpcControllerCore extends ParentOrderController
     {
         parent::setMedia();
 
-        if (!$this->useMobileTheme()) {
-            // Adding CSS style sheet
-            $this->addCSS(_THEME_CSS_DIR_.'order-opc.css');
-            // Adding JS files
-            $this->addJS(_THEME_JS_DIR_.'order-opc.js');
-            $this->addJqueryPlugin('scrollTo');
-        } else {
-            $this->addJS(_THEME_MOBILE_JS_DIR_.'opc.js');
-        }
-
         $this->addJS(array(
-            _THEME_JS_DIR_.'tools/vatManagement.js',
-            _THEME_JS_DIR_.'tools/statesManagement.js',
-            _THEME_JS_DIR_.'order-carrier.js',
-            _PS_JS_DIR_.'validate.js'
+            _THEME_JS_DIR_.'vendor/rivets.bundled.min.js',
+            _THEME_JS_DIR_.'theme.js'
         ));
     }
 
