4c169a230ef6eb9d50b7ccbdd489134e738cb8f4
djfm <fm.de.jouvencel@gmail.com>
2015-10-01 14:21:34 +0200

    [*] StarterTheme OPC Payment : AdvancedEU with and without JS

--- themes/StarterTheme/_dev/js/checkout.js
+++ themes/StarterTheme/_dev/js/checkout.js
@@ -1,8 +1,8 @@
 import $ from 'jquery';
 
-$(document).ready(function listenForTermsAndConditionsApprovalChange () {
+function setupRegularCheckout () {
   function hideSubmitButton () {
-    $('#conditions-to-approve input[type="submit"]').hide();
+    $('#conditions-to-approve button').hide();
   }
 
   function refreshPaymentOptions () {
@@ -15,5 +15,49 @@ $(document).ready(function listenForTermsAndConditionsApprovalChange () {
 
   hideSubmitButton();
   $('body').on('change', '#conditions-to-approve input[type="checkbox"]', refreshPaymentOptions);
+}
 
+function setupAdvancedCheckout () {
+  function getSelectedPaymentOption () {
+    return $('#payment-options input[name="advanced-payment-option"]:checked').attr('id');
+  }
+
+  function enableOrDisableOrderButton () {
+    var show = true;
+    $('#conditions-to-approve input[type="checkbox"]').each((_, checkbox) => {
+      if (!checkbox.checked) {
+        show = false;
+      }
+    });
+
+    if (!getSelectedPaymentOption()) {
+      show = false;
+    }
+
+    $('#payment-confirmation button').attr('disabled', !show);
+  }
+
+  function confirmPayment () {
+    var option = getSelectedPaymentOption();
+    if (option) {
+      $('#pay-with-' + option + '-form').submit();
+    }
+  }
+
+  $('#payment-confirmation button').on('click', function () {
+    confirmPayment();
+  });
+
+
+  $('#payment-options input[type="checkbox"][disabled]').attr('disabled', false);
+  $('body').on('change', '#conditions-to-approve input[type="checkbox"]', enableOrDisableOrderButton);
+  $('body').on('change', 'input[name="advanced-payment-option"]', enableOrDisableOrderButton);
+}
+
+$(document).ready(function setupCheckoutScripts () {
+  if ($('#payment-options').data('uses-advanced-payment-api')) {
+    setupAdvancedCheckout();
+  } else {
+    setupRegularCheckout();
+  }
 });
