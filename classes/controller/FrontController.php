d322cf1120f22b14112f3daaf534856456eea4c9
Julien Bourdeau <julien@sigerr.org>
2015-10-01 14:34:07 +0200

    // StarterTheme: Remove assets autoload
    (cherry picked from commit c0d4f5634648eb58272dd88ef72a3281ea98a2b4)

--- classes/controller/FrontController.php
+++ classes/controller/FrontController.php
@@ -827,23 +827,6 @@ class FrontControllerCore extends Controller
             _THEME_JS_DIR_.'theme.js',
         ));
 
-        // Automatically add js files from js/autoload directory in the template
-        if (@filemtime($this->getThemeDir().'js/autoload/')) {
-            foreach (scandir($this->getThemeDir().'js/autoload/', 0) as $file) {
-                if (preg_match('/^[^.].*\.js$/', $file)) {
-                    $this->addJS($this->getThemeDir().'js/autoload/'.$file);
-                }
-            }
-        }
-        // Automatically add css files from css/autoload directory in the template
-        if (@filemtime($this->getThemeDir().'css/autoload/')) {
-            foreach (scandir($this->getThemeDir().'css/autoload', 0) as $file) {
-                if (preg_match('/^[^.].*\.css$/', $file)) {
-                    $this->addCSS($this->getThemeDir().'css/autoload/'.$file);
-                }
-            }
-        }
-
         if (Tools::isSubmit('live_edit') && Tools::getValue('ad') && Tools::getAdminToken('AdminModulesPositions'.(int)Tab::getIdFromClassName('AdminModulesPositions').(int)Tools::getValue('id_employee'))) {
             $this->addJqueryUI('ui.sortable');
             $this->addjqueryPlugin('fancybox');
