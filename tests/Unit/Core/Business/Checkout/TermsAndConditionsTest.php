a90d42ae337cb1560c3fb4d0c67200d04ca796e3
djfm <fm.de.jouvencel@gmail.com>
2015-10-02 14:17:11 +0200

    [*] Core : Introducting the 'termsAndConditions' hook

---
+++ tests/Unit/Core/Business/Checkout/TermsAndConditionsTest.php
@@ -0,0 +1,55 @@
+<?php
+namespace PrestaShop\PrestaShop\Tests\Core\Business\Checkout;
+
+use PrestaShop\PrestaShop\Tests\TestCase\UnitTestCase;
+use PrestaShop\PrestaShop\Core\Business\Checkout\TermsAndConditions;
+
+class TermsAndConditionsTest extends UnitTestCase
+{
+    private $terms;
+
+    public function setup()
+    {
+        $this->terms = new TermsAndConditions;
+    }
+
+    public function test_SetText_InsertsLinks()
+    {
+        $this->assertEquals(
+            'hello <a target="_blank" href="http://www.world.com">world</a>',
+            $this->terms->setText('hello [world]', "http://www.world.com")->format()
+        );
+    }
+
+    public function test_SetText_InsertsSeveralLinks()
+    {
+        $this->assertEquals(
+            'hello <a target="_blank" href="http://www.world.com">world</a> <a target="_blank" href="http://yay.com">yay</a>',
+            $this->terms->setText('hello [world] [yay]', "http://www.world.com", "http://yay.com")->format()
+        );
+    }
+
+    public function test_SetText_JustDoesntAddLinksWhenMissing()
+    {
+        $this->assertEquals(
+            'hello world',
+            $this->terms->setText('hello [world]')->format()
+        );
+    }
+
+    public function test_FormatForTemplate_Overrides_Conditions()
+    {
+        $a = new TermsAndConditions;
+        $b = new TermsAndConditions;
+        $newA = new TermsAndConditions;
+
+        $a->setIdentifier('a')->setText('a');
+        $b->setIdentifier('b')->setText('b');
+        $newA->setIdentifier('a')->setText('newA');
+
+        $this->assertEquals(
+            ['a' => 'newA', 'b' => 'b'],
+            TermsAndConditions::formatForTemplate([[$a, $b], $newA])
+        );
+    }
+}
