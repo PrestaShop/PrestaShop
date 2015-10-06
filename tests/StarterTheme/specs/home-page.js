88cc1f2c20fc9d0ddbc4369c8522f13b23de9b4f
djfm <fm.de.jouvencel@gmail.com>
2015-10-06 11:14:33 +0200

    [*] StarterTheme : Improved Tests Documentation

--- tests/StarterTheme/specs/home-page.js
+++ tests/StarterTheme/specs/home-page.js
@@ -4,9 +4,7 @@ describe('The home page', function () {
   it('should contain a link with the logo', function () {
     return browser
       .url('/')
-      .then(function () {
-        return browser.element('a.logo img');
-      })
+      .element('a.logo img')
     ;
   });
 });
