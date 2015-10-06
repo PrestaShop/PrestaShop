e58e0c5a21a0a374d12d5fad486d4fd34ef081d8
djfm <fm.de.jouvencel@gmail.com>
2015-10-06 10:47:54 +0200

    [*] StarterTheme : Introducing application events in JS

--- themes/StarterTheme/_dev/js/theme.js
+++ themes/StarterTheme/_dev/js/theme.js
@@ -1,7 +1,14 @@
+import $ from 'jquery';
 import './setup-rivets';
 import './checkout';
 
-import $ from 'jquery';
+import prestashop from 'prestashop';
+import EventEmitter from 'events';
+
+// "inherit" EventEmitter
+for (var i in EventEmitter.prototype) {
+    prestashop[i] = EventEmitter.prototype[i];
+}
 
 $(document).ready(() => {
     $('.ps-shown-by-js').show();
