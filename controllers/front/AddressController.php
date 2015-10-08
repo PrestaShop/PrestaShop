0a77c269cc36203cd9668ee1c31ab34a81c982ce
Julien Bourdeau <julien@sigerr.org>
2015-10-08 14:39:08 +0200

    // StarterTheme: Move address form validation to Adapter

--- controllers/front/AddressController.php
+++ controllers/front/AddressController.php
@@ -40,6 +40,7 @@ class AddressControllerCore extends FrontController
 
     protected $address_form;
     protected $address_formatter;
+    protected $address_fields = [];
 
     public $form_errors = [];
 
@@ -51,40 +52,26 @@ class AddressControllerCore extends FrontController
     {
         parent::init();
 
-        // Get address ID
-        $id_address = 0;
-        if ($this->ajax && Tools::isSubmit('type')) {
-            if (Tools::getValue('type') == 'delivery' && isset($this->context->cart->id_address_delivery)) {
-                $id_address = (int)$this->context->cart->id_address_delivery;
-            } elseif (Tools::getValue('type') == 'invoice' && isset($this->context->cart->id_address_invoice)
-                        && $this->context->cart->id_address_invoice != $this->context->cart->id_address_delivery) {
-                $id_address = (int)$this->context->cart->id_address_invoice;
-            }
-        } else {
-            $id_address = (int)Tools::getValue('id_address', 0);
+        $id_address = (int)Tools::getValue('id_address', 0);
+        $this->_address = new Address($id_address);
+
+        if (!Validate::isLoadedObject($this->_address) || !Customer::customerHasAddress($this->context->customer->id, $id_address)) {
+            Tools::redirect('index.php?controller=addresses');
         }
 
-        // Initialize address
-        if ($id_address) {
-            $this->_address = new Address($id_address);
-            if (Validate::isLoadedObject($this->_address) && Customer::customerHasAddress($this->context->customer->id, $id_address)) {
-                if (Tools::isSubmit('delete')) {
-                    if ($this->_address->delete()) {
-                        if ($this->context->cart->id_address_invoice == $this->_address->id) {
-                            unset($this->context->cart->id_address_invoice);
-                        }
-                        if ($this->context->cart->id_address_delivery == $this->_address->id) {
-                            unset($this->context->cart->id_address_delivery);
-                            $this->context->cart->updateAddressId($this->_address->id, (int)Address::getFirstCustomerAddressId(Context::getContext()->customer->id));
-                        }
-                        Tools::redirect('index.php?controller=addresses');
-                    }
-                    $this->errors[] = Tools::displayError('This address cannot be deleted.');
+        if (Tools::isSubmit('delete')) {
+            if ($this->_address->delete()) {
+                if ($this->context->cart->id_address_invoice == $this->_address->id) {
+                    unset($this->context->cart->id_address_invoice);
                 }
-            } elseif ($this->ajax) {
-                exit;
-            } else {
+                if ($this->context->cart->id_address_delivery == $this->_address->id) {
+                    unset($this->context->cart->id_address_delivery);
+                    $this->context->cart->updateAddressId($this->_address->id, (int)Address::getFirstCustomerAddressId(Context::getContext()->customer->id));
+                }
+
                 Tools::redirect('index.php?controller=addresses');
+            } else {
+                $this->errors[] = Tools::displayError('This address cannot be deleted.');
             }
         }
 
@@ -117,139 +104,42 @@ class AddressControllerCore extends FrontController
      */
     protected function processSubmitAddress()
     {
-        $address = new Address();
-        $address->validateController();
-        $this->validateFormController();
-        $address->id_customer = (int)$this->context->customer->id;
-
         // Check page token
         if ($this->context->customer->isLogged() && !$this->isTokenValid()) {
             $this->errors[] = Tools::displayError('Invalid token.');
+            return false;
         }
 
-        // Check phone
-        if (Configuration::get('PS_ONE_PHONE_AT_LEAST') && !Tools::getValue('phone') && !Tools::getValue('phone_mobile')) {
-            $this->form_errors['phone'][] = $this->form_errors['phone_mobile'][] = $this->l('You must register at least one phone number.');
+        if ($this->address_form->hasErrors()) {
+            $this->address_fields = $this->address_form->getAddressFormatWithErrors();
+            return false;
         }
-        if ($address->id_country) {
-            // Check country
-            if (!($country = new Country($address->id_country)) || !Validate::isLoadedObject($country)) {
-                throw new PrestaShopException('Country cannot be loaded with address->id_country');
-            }
 
-            if ((int)$country->contains_states && !(int)$address->id_state) {
-                $this->form_errors['state'][] = $this->l('This country requires you to chose a State.');
-            }
+        //  StarterTheme: Save data
+        $errors = $this->_address->validateController();
+        $this->errors = array_merge($this->errors, $errors);
 
-            if (!$country->active) {
-                $this->form_errors['country'][] = $this->l('This country is not active.');
-            }
+        if (empty($this->errors)) {
+            $saved = $this->_address->save();
 
-            $postcode = Tools::getValue('postcode');
-            /* Check zip code format */
-            if ($country->zip_code_format && !$country->checkZipCode($postcode)) {
-                $this->form_errors['postcode'][] = sprintf($this->l('The Zip/Postal code you\'ve entered is invalid. It must follow this format: %s'), str_replace('C', $country->iso_code, str_replace('N', '0', str_replace('L', 'A', $country->zip_code_format))));
-            } elseif (empty($postcode) && $country->need_zip_code) {
-                $this->form_errors['postcode'][] = $this->l('A Zip/Postal code is required.');
-            } elseif ($postcode && !Validate::isPostCode($postcode)) {
-                $this->form_errors['postcode'][] = $this->l('The Zip/Postal code is invalid.');
+            if (!$saved) {
+                $this->errors[] = $this->l('An error occurred while updating your address.');
+                return false;
             }
+        }
 
-            // Check country DNI
-            if ($country->isNeedDni() && (!Tools::getValue('dni') || !Validate::isDniLite(Tools::getValue('dni')))) {
-                $this->form_errors['dni'][] = $this->l('The identification number is incorrect or has already been used.');
-            } elseif (!$country->isNeedDni()) {
-                $address->dni = null;
+        // StarterTheme: Handle ajax for address validation !
+
+        // Redirect to old page or current page
+        if ($back = Tools::getValue('back')) {
+            if ($back == Tools::secureReferrer(Tools::getValue('back'))) {
+                Tools::redirect(html_entity_decode($back));
             }
+            $mod = Tools::getValue('mod');
+            Tools::redirect('index.php?controller='.$back.($mod ? '&back='.$mod : ''));
         } else {
-            $this->form_errors['country'][] = $this->l('This information is required.');
+            Tools::redirect('index.php?controller=addresses');
         }
-
-        // Check if the alias exists
-        if (!$this->context->customer->is_guest && !empty($_POST['alias']) && (int)$this->context->customer->id > 0) {
-            $id_address = Tools::getValue('id_address');
-            if (Configuration::get('PS_ORDER_PROCESS_TYPE') && (int)Tools::getValue('opc_id_address_'.Tools::getValue('type')) > 0) {
-                $id_address = Tools::getValue('opc_id_address_'.Tools::getValue('type'));
-            }
-
-            if (Address::aliasExist(Tools::getValue('alias'), (int)$id_address, (int)$this->context->customer->id)) {
-                $this->form_errors['alias'][] = sprintf($this->l('The alias "%s" has already been used. Please select another one.'), Tools::safeOutput(Tools::getValue('alias')));
-            }
-        }
-
-        // Check the requires fields which are settings in the BO
-        $this->form_errors = array_merge($this->form_errors, $address->validateFieldsRequiredDatabase());
-
-        // Don't continue this process if we have errors !
-        if (($this->form_errors || $this->errors) && !$this->ajax) {
-            return;
-        }
-
-        // If we edit this address, delete old address and create a new one
-        if (Validate::isLoadedObject($this->_address)) {
-            if (Validate::isLoadedObject($country) && !$country->contains_states) {
-                $address->id_state = 0;
-            }
-            $address_old = $this->_address;
-            if (Customer::customerHasAddress($this->context->customer->id, (int)$address_old->id)) {
-                if ($address_old->isUsed()) {
-                    $address_old->delete();
-                } else {
-                    $address->id = (int)$address_old->id;
-                    $address->date_add = $address_old->date_add;
-                }
-            }
-        }
-
-        if ($this->ajax && Configuration::get('PS_ORDER_PROCESS_TYPE')) {
-            $this->errors = array_unique(array_merge($this->errors, $address->validateController()));
-            if (count($this->errors)) {
-                $return = array(
-                    'hasError' => (bool)$this->errors,
-                    'errors' => $this->errors
-                );
-                $this->ajaxDie(json_encode($return));
-            }
-        }
-
-        // Save address
-        if ($result = $address->save()) {
-            // Update id address of the current cart if necessary
-            if (isset($address_old) && $address_old->isUsed()) {
-                $this->context->cart->updateAddressId($address_old->id, $address->id);
-            } else { // Update cart address
-                $this->context->cart->autosetProductAddress();
-            }
-
-            if ((bool)Tools::getValue('select_address', false) == true || (Tools::getValue('type') == 'invoice' && Configuration::get('PS_ORDER_PROCESS_TYPE'))) {
-                $this->context->cart->id_address_invoice = (int)$address->id;
-            } elseif (Configuration::get('PS_ORDER_PROCESS_TYPE')) {
-                $this->context->cart->id_address_invoice = (int)$this->context->cart->id_address_delivery;
-            }
-            $this->context->cart->update();
-
-            if ($this->ajax) {
-                $return = array(
-                    'hasError' => (bool)$this->errors,
-                    'errors' => $this->errors,
-                    'id_address_delivery' => (int)$this->context->cart->id_address_delivery,
-                    'id_address_invoice' => (int)$this->context->cart->id_address_invoice
-                );
-                $this->ajaxDie(json_encode($return));
-            }
-
-            // Redirect to old page or current page
-            if ($back = Tools::getValue('back')) {
-                if ($back == Tools::secureReferrer(Tools::getValue('back'))) {
-                    Tools::redirect(html_entity_decode($back));
-                }
-                $mod = Tools::getValue('mod');
-                Tools::redirect('index.php?controller='.$back.($mod ? '&back='.$mod : ''));
-            } else {
-                Tools::redirect('index.php?controller=addresses');
-            }
-        }
-        $this->errors[] = Tools::displayError('An error occurred while updating your address.');
     }
 
     /**
@@ -275,16 +165,17 @@ class AddressControllerCore extends FrontController
         $back = Tools::getValue('back');
         $mod = Tools::getValue('mod');
 
+        if (empty($this->address_fields)) {
+            $this->address_fields = $this->address_form->getAddressFormat();
+        }
+
         $this->context->smarty->assign(array(
-            'form_validation' => Address::$definition['fields'],
-            'errors' => $this->errors,
             'one_phone_at_least' => (int)Configuration::get('PS_ONE_PHONE_AT_LEAST'),
-            'ajaxurl' => _MODULE_DIR_,
             'token' => Tools::getToken(false),
             'select_address' => (int)Tools::getValue('select_address'),
             'address' => $address,
             'countries' => $countries,
-            'address_fields' => $this->address_form->getAddressFormat(),
+            'address_fields' => $this->address_fields,
             'back' => Tools::safeOutput($back),
             'mod' => Tools::safeOutput($mod),
         ));
@@ -345,17 +236,6 @@ class AddressControllerCore extends FrontController
                 'errors' => $this->errors
             );
             $this->ajaxDie(json_encode($return));
-        }
-    }
-
-    public function validateFormController()
-    {
-        $requireFormFieldsList = AddressFormat::getFieldsRequired();
-
-        foreach ($_POST as $field_name => $val) {
-            if (in_array($field_name, $requireFormFieldsList) && empty($val)) {
-                $this->form_errors[$field_name][] = $this->l('This information is required.');
-            }
         }
     }
 }
