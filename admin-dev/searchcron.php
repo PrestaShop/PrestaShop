4a4e5e6316a1b1205fcf3e80a55c243e17178c3b
gRoussac <gregory.roussac@prestashop.com>
2015-10-08 18:43:38 +0200

    [-] CORE : fix #PSCSX-7007, notice in searchcron

--- admin-dev/searchcron.php
+++ admin-dev/searchcron.php
@@ -41,6 +41,6 @@ if (substr(_COOKIE_KEY_, 34, 8) != Tools::getValue('token')) {
 
 ini_set('max_execution_time', 7200);
 Search::indexation(Tools::getValue('full'));
-if (Tools::getValue('redirect')) {
+if (Tools::getValue('redirect') && isset($_SERVER['HTTP_REFERER'])) {
     Tools::redirectAdmin($_SERVER['HTTP_REFERER'].'&conf=4');
 }
