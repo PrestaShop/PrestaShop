9e6f598d5cf2a98d56e083fd92acb1270010e448
gRoussac <gregory.roussac@prestashop.com>
2015-10-13 16:49:48 +0200

    [-] Bo : Fix pagination, followup https://github.com/PrestaShop/PrestaShop/commit/dd3cf42354fc1a7cd08b6b9efe202b185bdf3239

--- classes/controller/AdminController.php
+++ classes/controller/AdminController.php
@@ -889,8 +889,8 @@ class AdminControllerCore extends Controller
                     $this->processFilter();
                 }
 
-                if ((int)Tools::getValue('submitFilter'.$this->list_id) || Tools::isSubmit('submitReset'.$this->list_id)) {
-                    $this->setRedirectAfter(self::$currentIndex.'&token='.$this->token);
+                if (isset($_POST) && count($_POST) && (int)Tools::getValue('submitFilter'.$this->list_id) || Tools::isSubmit('submitReset'.$this->list_id)) {
+                    $this->setRedirectAfter(self::$currentIndex.'&token='.$this->token.(Tools::isSubmit('submitFilter'.$this->list_id) ? '&submitFilter'.$this->list_id.'='.(int)Tools::getValue('submitFilter'.$this->list_id) : ''));
                 }
 
                 // If the method named after the action exists, call "before" hooks, then call action method, then call "after" hooks
