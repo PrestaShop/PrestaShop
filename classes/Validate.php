aed2563c3cafbbe37f5c7893b10098ce490d3415
BOUALLOUS Marwein <mbouallo@student.42.fr>
2015-10-18 15:41:43 +0200

    Correcting isBirthay
    adding the Year only validation to correct a bug with birthdate validation

--- classes/Validate.php
+++ classes/Validate.php
@@ -510,24 +510,18 @@ class ValidateCore
      */
     public static function isBirthDate($date)
     {
-        if (empty($date) || $date == '0000-00-00') {
-            return true;
-        }
-        if (preg_match('/^([0-9]{4})-((?:0?[1-9])|(?:1[0-2]))-((?:0?[1-9])|(?:[1-2][0-9])|(?:3[01]))([0-9]{2}:[0-9]{2}:[0-9]{2})?$/', $date, $birth_date))
-		{
-			if ($birth_date[1] <= date('Y'))
-			{
-				if ($birth_date[2] <= date('m'))
-				{
-					if($birth_date[3] <= date('d'))
-					    return true;
-					return false;
-				}
-				return false;
-			}
-			return false;
-		}
-		return false;
+        if (empty($date) || $date == '0000-00-00')
+        return false;
+	if (preg_match('/^([0-9]{4})-((?:0?[1-9])|(?:1[0-2]))-((?:0?[1-9])|(?:[1-2][0-9])|(?:3[01]))([0-9]{2}:[0-9]{2}:[0-9]{2})?$/', $date, $birth_date))
+	{
+	     if ($birth_date[1] > date('Y') && $birth_date[2] > date('m') && $birth_date[3] > date('d')
+	        || $birth_date[1] == date('Y') && $birth_date[2] == date('m') && $birth_date[3] > date('d')
+	        || $birth_date[1] == date('Y') && $birth_date[2] > date('m')
+	        || $birth_date[1] > date('Y'))
+	        return false;
+	     return true;
+	}
+	return false;
     }
 
     /**
