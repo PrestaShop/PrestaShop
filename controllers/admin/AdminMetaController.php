5f037158a51e44868b9d2461a234f64bcfa24054
gskema <gytis@reservationpartner.lt>
2015-10-19 16:28:38 +0300

    [-] FO: Allow bots to crawl module images. PSCSX-6909

--- controllers/admin/AdminMetaController.php
+++ controllers/admin/AdminMetaController.php
@@ -763,7 +763,12 @@ class AdminMetaControllerCore extends AdminController
         $tab = array();
 
         // Special allow directives
-        $tab['Allow'] = array('*/modules/*.css', '*/modules/*.js');
+        $tab['Allow'] = array(
+            '*/modules/*.css',
+            '*/modules/*.js',
+            '*/modules/*.png',
+            '*/modules/*.jpg',
+        );
 
         // Directories
         $tab['Directories'] = array('classes/', 'config/', 'download/', 'mails/', 'modules/', 'translations/', 'tools/');
