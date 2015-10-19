eabd0d9de98e75b7b5446f64ac3d3fb0b2ee14d2
djfm <fm.de.jouvencel@gmail.com>
2015-10-19 11:14:25 +0200

    // fixed the tests

--- tests/StarterTheme/specs/customer/my-account.js
+++ tests/StarterTheme/specs/customer/my-account.js
@@ -1,17 +1,15 @@
-/* global describe, it, browser, before */
+/* global describe, it, browser, before, after */
 
 var fixtures = require('../../fixtures');
-var q        = require('q');
 
-describe('Customer account: My account page', function () {
+describe('My account page', function () {
 
   before(function () {
-    return browser
-            .url(fixtures.urls.login)
-            .setValue('.login-form input[name=email]', fixtures.customer.email)
-            .setValue('.login-form input[name=passwd]', fixtures.customer.password)
-            .submitForm('.login-form form')
-            .pause(500);
+    return browser.loginDefaultCustomer();
+  });
+
+  after(function () {
+    return browser.logout();
   });
 
   describe('The customer account has basic feature', function () {
@@ -22,32 +20,30 @@ describe('Customer account: My account page', function () {
 
     it('should display addresses link', function () {
       return browser
-              .elements('.page-content a#addresses-link, .page-content a#address-link')
-              .then(function (elements) {
-                elements.value.length.should.be.greaterThan(0);
-              });
+        .elements('.page-content a#addresses-link, .page-content a#address-link')
+        .then(function (elements) {
+          elements.value.length.should.be.greaterThan(0);
+        })
+      ;
     });
 
     it('should display identity link', function () {
       return browser
-              .isExisting('.page-content a#identity-link')
-              .then(function (isExisting) {
-                isExisting.should.be.true;
-              });
+        .isExisting('.page-content a#identity-link')
+        .then(function (isExisting) {
+          isExisting.should.be.true;
+        })
+      ;
     });
 
     it('should display history link', function () {
       return browser
-              .isExisting('.page-content a#history-link')
-              .then(function (isExisting) {
-                isExisting.should.be.true;
-              });
+        .isExisting('.page-content a#history-link')
+        .then(function (isExisting) {
+          isExisting.should.be.true;
+        })
+      ;
     });
 
   });
-
-  after(function () {
-    return browser.url(fixtures.urls.logout).pause(500);
-  })
-
 });
