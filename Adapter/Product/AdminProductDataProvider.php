ff4d13dd97a3179d67ea492098ef7fa75414b403
Xavier Gouley <xavier.gouley@prestashop.com>
2015-10-26 15:43:58 +0100

    // Hooks and parameters for displayAdminProductsExtra (id_product)

--- Adapter/Product/AdminProductDataProvider.php
+++ Adapter/Product/AdminProductDataProvider.php
@@ -271,6 +271,16 @@ class AdminProductDataProvider extends AbstractAdminQueryBuilder implements Prod
             $sqlOrder = array('id_product ASC');
         }
 
+        // exec legacy hook but with different parameters (retro-compat < 1.7 is broken here)
+        \Hook::exec('actionAdminProductsListingFieldsModifier', array(
+            '_ps_version' => _PS_VERSION_,
+            'sql_select' => &$sqlSelect,
+            'sql_table' => &$sqlTable,
+            'sql_where' => &$sqlWhere,
+            'sql_order' => &$sqlOrder,
+            'sql_limit' => &$sqlLimit
+        ));
+
         $sql = $this->compileSqlQuery($sqlSelect, $sqlTable, $sqlWhere, $sqlOrder, $sqlLimit);
         $products = \Db::getInstance()->executeS($sql, true, false);
         $total = \Db::getInstance()->executeS('SELECT FOUND_ROWS();', true, false);
--- src/PrestaShopBundle/Controller/Admin/ProductController.php
+++ src/PrestaShopBundle/Controller/Admin/ProductController.php
@@ -320,6 +320,7 @@ class ProductController extends FrameworkBundleAdminController
 
         return array(
             'form' => $form->createView(),
+            'id_product' => $id,
         );
     }
 
