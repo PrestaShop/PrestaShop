ae1c3507bbe89abe166e7cbeee8ad6796502726f
Guisardo <missgatasch@gmail.com>
2015-10-29 09:48:26 -0300

    // CORE: Fix mime types for Woff
    The official mime type is application/font-...
    http://zduck.com/2013/google-chrome-and-woff-font-mime-type-warnings/
    http://www.w3.org/TR/WOFF2/#IMT

--- classes/Tools.php
+++ classes/Tools.php
@@ -2440,7 +2440,8 @@ class ToolsCore
         fwrite($write_fd, "AddType application/vnd.ms-fontobject .eot\n");
         fwrite($write_fd, "AddType font/ttf .ttf\n");
         fwrite($write_fd, "AddType font/otf .otf\n");
-        fwrite($write_fd, "AddType application/x-font-woff .woff\n");
+        fwrite($write_fd, "AddType application/font-woff .woff\n");
+        fwrite($write_fd, "AddType application/font-woff2 .woff2\n");
         fwrite($write_fd, "<IfModule mod_headers.c>
     <FilesMatch \"\.(ttf|ttc|otf|eot|woff|woff2|svg)$\">
         Header add Access-Control-Allow-Origin \"*\"
@@ -2462,6 +2463,7 @@ class ToolsCore
     ExpiresByType image/svg+xml \"access plus 1 year\"
     ExpiresByType image/vnd.microsoft.icon \"access plus 1 year\"
     ExpiresByType application/font-woff \"access plus 1 year\"
+    ExpiresByType application/font-woff2 \"access plus 1 year\"
     ExpiresByType application/x-font-woff \"access plus 1 year\"
     ExpiresByType application/vnd.ms-fontobject \"access plus 1 year\"
     ExpiresByType font/opentype \"access plus 1 year\"
