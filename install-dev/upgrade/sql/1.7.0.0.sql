c8bdf3908638b578e2c1f56bae4dfff8f1381999
djfm <fm.de.jouvencel@gmail.com>
2015-11-05 17:17:22 +0100

    // seems I messed up a conflict resolution

--- install-dev/upgrade/sql/1.7.0.0.sql
+++ install-dev/upgrade/sql/1.7.0.0.sql
@@ -40,13 +40,6 @@ UPDATE `PREFIX_access` SET `view` = '0', `add` = '0', `edit` = '0', `delete` = '
 ALTER TABLE  `PREFIX_product_lang` ADD  `social_sharing_title` VARCHAR( 255 ) NOT NULL;
 ALTER TABLE  `PREFIX_product_lang` ADD  `social_sharing_description` VARCHAR( 255 ) NOT NULL;
 
-/* PHP:ps1700_stores(); */;
-
-
-/* Password reset token for new "Forgot my password screen */
-ALTER TABLE PREFIX_customer ADD `reset_password_token` varchar(40) DEFAULT NULL;
-ALTER TABLE PREFIX_customer ADD `reset_password_validity` datetime DEFAULT NULL;
-ALTER TABLE PREFIX_employee ADD `reset_password_token` varchar(40) DEFAULT NULL;
-ALTER TABLE PREFIX_employee ADD `reset_password_validity` datetime DEFAULT NULL;
+/* PHP:ps1700_stores(); */
 
 INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VALUES ('PS_PASSWD_RESET_VALIDITY', '1440', NOW(), NOW());
