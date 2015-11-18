0319b03f66506797e0cc524a5e9135941c0b4f4a
djfm <fm.de.jouvencel@gmail.com>
2015-11-18 11:27:54 +0100

    // WIP, getFacetsFromFilterBlock

---
+++ Core/Business/Product/Search/Filter.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace PrestaShop\PrestaShop\Core\Business\Product\Search;
+
+class Filter
+{
+    private $label;
+    private $type;
+    private $active;
+    private $available;
+    private $properties = [];
+    private $value;
+
+    public function setLabel($label)
+    {
+        $this->label = $label;
+        return $this;
+    }
+
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function setProperty($name, $value)
+    {
+        $this->properties[$name] = $value;
+        return $this;
+    }
+
+    public function getProperty($name)
+    {
+        return $this->properties[$name];
+    }
+
+    public function setValue($value)
+    {
+        $this->value = $value;
+        return $this;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
