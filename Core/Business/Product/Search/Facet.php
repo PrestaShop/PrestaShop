8e3f19f02877a4253d78041594664a55cc5ea9fd
djfm <fm.de.jouvencel@gmail.com>
2015-11-18 11:27:54 +0100

    // WIP, getFacetsFromFilterBlock

---
+++ Core/Business/Product/Search/Facet.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace PrestaShop\PrestaShop\Core\Business\Product\Search;
+
+class Facet
+{
+    private $label;
+    private $type;
+    private $properties = [];
+    private $filters = [];
+
+    public function setLabel($label)
+    {
+        $this->label = $label;
+        return $this;
+    }
+
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    public function setProperty($name, $value)
+    {
+        $this->properties[$name] = $value;
+        return $this;
+    }
+
+    public function getProperty($name)
+    {
+        return $this->properties[$name];
+    }
+}
