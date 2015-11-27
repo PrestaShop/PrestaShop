26158d93201a3451c326d2e0ded3999a9914e8ac
djfm <fm.de.jouvencel@gmail.com>
2015-11-27 15:23:44 +0100

    // cleanup

---
+++ Adapter/Category/CategoryProductSearchProvider.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace PrestaShop\PrestaShop\Adapter\Category;
+
+use PrestaShop\PrestaShop\Core\Business\Product\Search\ProductSearchProviderInterface;
+use PrestaShop\PrestaShop\Core\Business\Product\Search\ProductSearchContext;
+use PrestaShop\PrestaShop\Core\Business\Product\Search\ProductSearchQuery;
+use PrestaShop\PrestaShop\Core\Business\Product\Search\ProductSearchResult;
+use PrestaShop\PrestaShop\Core\Business\Product\Search\PaginationResult;
+use PrestaShop\PrestaShop\Core\Business\Product\Search\SortOrderFactory;
+use PrestaShop\PrestaShop\Adapter\Translator;
+use Category;
+
+class CategoryProductSearchProvider implements ProductSearchProviderInterface
+{
+    private $translator;
+    private $category;
+    private $sortOrderFactory;
+
+    public function __construct(
+        Translator $translator,
+        Category $category
+    ) {
+        $this->translator = $translator;
+        $this->category = $category;
+        $this->sortOrderFactory = new SortOrderFactory($this->translator);
+    }
+
+    private function getProductsOrCount(
+        ProductSearchContext $context,
+        ProductSearchQuery $query,
+        $type = 'products'
+    ) {
+        return $this->category->getProducts(
+            $context->getIdLang(),
+            $query->getPage(),
+            $query->getResultsPerPage(),
+            $query->getSortOrder()->toLegacyOrderBy(),
+            $query->getSortOrder()->toLegacyOrderWay(),
+            $type !== 'products'
+        );
+    }
+
+    public function runQuery(
+        ProductSearchContext $context,
+        ProductSearchQuery $query
+    ) {
+        $products = $this->getProductsOrCount($context, $query, 'products');
+        $count = $this->getProductsOrCount($context, $query, 'count');
+
+        $result = new ProductSearchResult;
+        $result->setProducts($products);
+        $result->setNextQuery($query);
+
+        $pagination = new PaginationResult;
+        $pagination
+            ->setTotalResultsCount($count)
+            ->setPage($query->getPage())
+            ->setResultsCount(count($products))
+            ->setPagesCount(ceil($count / $query->getResultsPerPage()))
+        ;
+        $result->setPaginationResult($pagination);
+
+        $result->setAvailableSortOrders(
+            $this->sortOrderFactory->getDefaultSortOrders()
+        );
+
+        return $result;
+    }
+
+    public function addFacetsToQuery(
+        ProductSearchContext $context,
+        $encodedFacets,
+        ProductSearchQuery $query
+    ) {
+        // Nothing to do here.
+    }
+}
