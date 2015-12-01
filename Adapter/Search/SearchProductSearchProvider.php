94a459140139388622618b8545a5bf8139232d65
djfm <fm.de.jouvencel@gmail.com>
2015-12-01 16:10:09 +0100

    // basic search implementation

---
+++ Adapter/Search/SearchProductSearchProvider.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace PrestaShop\PrestaShop\Adapter\Search;
+
+use PrestaShop\PrestaShop\Core\Business\Product\Search\ProductSearchProviderInterface;
+use PrestaShop\PrestaShop\Core\Business\Product\Search\ProductSearchContext;
+use PrestaShop\PrestaShop\Core\Business\Product\Search\ProductSearchQuery;
+use PrestaShop\PrestaShop\Core\Business\Product\Search\ProductSearchResult;
+use PrestaShop\PrestaShop\Core\Business\Product\Search\PaginationResult;
+use PrestaShop\PrestaShop\Core\Business\Product\Search\SortOrderFactory;
+use PrestaShop\PrestaShop\Adapter\Translator;
+use Search;
+use Tools;
+
+class SearchProductSearchProvider implements ProductSearchProviderInterface
+{
+    private $translator;
+    private $category;
+    private $sortOrderFactory;
+
+    public function __construct(
+        Translator $translator
+    ) {
+        $this->translator = $translator;
+        $this->sortOrderFactory = new SortOrderFactory($this->translator);
+    }
+
+    public function runQuery(
+        ProductSearchContext $context,
+        ProductSearchQuery $query
+    ) {
+        $products = [];
+        $count    = 0;
+
+        if (($string = $query->getSearchString())) {
+            $result = Search::find(
+                $context->getIdLang(),
+                Tools::replaceAccentedChars(urldecode($string)),
+                $query->getPage(),
+                $query->getResultsPerPage(),
+                $query->getSortOrder()->toLegacyOrderBy(),
+                $query->getSortOrder()->toLegacyOrderWay(),
+                false, // ajax, what's the link?
+                false, // $use_cookie, ignored anyway
+                null
+            );
+            $products = $result['result'];
+            $count    = $result['total'];
+        } elseif (($tag = $query->getSearchTag())) {
+            $products = Search::searchTag(
+                $context->getIdLang(),
+                urldecode($tag),
+                false,
+                $query->getPage(),
+                $query->getResultsPerPage(),
+                $query->getSortOrder()->toLegacyOrderBy(true),
+                $query->getSortOrder()->toLegacyOrderWay(),
+                false,
+                null
+            );
+
+            $count = Search::searchTag(
+                $context->getIdLang(),
+                urldecode($tag),
+                true,
+                $query->getPage(),
+                $query->getResultsPerPage(),
+                $query->getSortOrder()->toLegacyOrderBy(true),
+                $query->getSortOrder()->toLegacyOrderWay(),
+                false,
+                null
+            );
+        }
+
+        $result = new ProductSearchResult;
+        $result->setProducts($products);
+        $result->setNextQuery($query);
+
+        $pagination = new PaginationResult;
+        $pagination
+            ->setTotalResultsCount($count)
+            ->setPage($query->getPage())
+            ->setResultsCount(count($products))
+            ->setPagesCount(ceil($count / $query->getResultsPerPage()))
+        ;
+        $result->setPaginationResult($pagination);
+
+        $result->setAvailableSortOrders(
+            $this->sortOrderFactory->getDefaultSortOrders()
+        );
+
+        return $result;
+    }
+
+    public function addFacetsToQuery(
+        ProductSearchContext $context,
+        $encodedFacets,
+        ProductSearchQuery $query
+    ) {
+        // Nothing to do here.
+    }
+}
