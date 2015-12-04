c1fb7e2e1407df30fc8ae2795be213083e824afe
djfm <fm.de.jouvencel@gmail.com>
2015-12-04 15:12:18 +0100

    // fixed subtle bug with duplicate tab on chrome

--- themes/StarterTheme/_dev/js/facets.js
+++ themes/StarterTheme/_dev/js/facets.js
@@ -29,8 +29,22 @@ function makeQuery (url) {
     if (pendingQuery) {
         // wait for current results
     } else {
+
+        // We need to add a parameter to the URL
+        // to make it different from the one we're on,
+        // otherwise when you do "duplicate tab" under chrome
+        // it mixes up the cache between the AJAX request (that
+        // returns JSON) and the non-AJAX request (that returns
+        // HTML) and you just get a mess of JSON on the duplicated tab.
+
+        const slightlyDifferentURL = [
+            url,
+            url.indexOf('?') >= 0 ? '&' : '?',
+            'from-xhr'
+        ].join('');
+
         $
-          .get(url, null, null, 'json')
+          .get(slightlyDifferentURL, null, null, 'json')
           .then(updateResults)
           .fail(handleError)
         ;
