34ad2f5f45e8b02ce7c3174d94644d6114399746
rav88 <r.derlatka@gmail.com>
2015-12-06 23:56:10 +0100

    MySQL 5.7 SQL Incection FIX
    Fixes Error with MySQL 5.7, where is not possible to inject SELECT with alias into the UPDATE statement that way.
    (Error Code: 1093. You can't specify target table 'ps_order_invoice' for update in FROM clause	0.015 sec)

--- classes/order/Order.php
+++ classes/order/Order.php
@@ -1190,9 +1190,12 @@ class OrderCore extends ObjectModel
         if ($number) {
             $sql .= (int)$number;
         } else {
-            $sql .= '(SELECT new_number FROM (SELECT (MAX(`number`) + 1) AS new_number
-            FROM `'._DB_PREFIX_.'order_invoice`'.(Configuration::get('PS_INVOICE_RESET') ?
+            $getNumberSql = '(SELECT new_number FROM (SELECT (MAX(`number`) + 1) AS new_number
+			FROM `'._DB_PREFIX_.'order_invoice`'.(Configuration::get('PS_INVOICE_RESET') ?
                 ' WHERE DATE_FORMAT(`date_add`, "%Y") = '.(int)date('Y') : '').') AS result)';
+			$getNumberSqlRow = Db::getInstance()->getRow($getNumberSql);
+			$newInvoiceNumber = $getNumberSqlRow['new_number'];
+			$sql .= $newInvoiceNumber;
         }
 
         $sql .= ' WHERE `id_order_invoice` = '.(int)$order_invoice_id;
