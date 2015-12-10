a5a09009aa2399d8ed22ca945c3f563b7b44dd16
djfm <fm.de.jouvencel@gmail.com>
2015-12-10 10:16:40 +0100

    // renamed PaginationResult => Pagination

--- Adapter/Supplier/SupplierProductSearchProvider.php
+++ Adapter/Supplier/SupplierProductSearchProvider.php
@@ -6,7 +6,7 @@ use PrestaShop\PrestaShop\Core\Business\Product\Search\ProductSearchProviderInte
 use PrestaShop\PrestaShop\Core\Business\Product\Search\ProductSearchContext;
 use PrestaShop\PrestaShop\Core\Business\Product\Search\ProductSearchQuery;
 use PrestaShop\PrestaShop\Core\Business\Product\Search\ProductSearchResult;
-use PrestaShop\PrestaShop\Core\Business\Product\Search\PaginationResult;
+use PrestaShop\PrestaShop\Core\Business\Product\Search\Pagination;
 use PrestaShop\PrestaShop\Core\Business\Product\Search\SortOrderFactory;
 use PrestaShop\PrestaShop\Adapter\Translator;
 use Supplier;
@@ -52,12 +52,12 @@ class SupplierProductSearchProvider implements ProductSearchProviderInterface
         $result = new ProductSearchResult;
         $result->setProducts($products);
 
-        $pagination = new PaginationResult;
+        $pagination = new Pagination;
         $pagination
             ->setTotalResultsCount($count)
             ->setResultsCount(count($products))
         ;
-        $result->setPaginationResult($pagination);
+        $result->setPagination($pagination);
 
         $result->setAvailableSortOrders(
             $this->sortOrderFactory->getDefaultSortOrders()
