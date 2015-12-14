2077ddb7f04d8b5dd1898391d7395bb9fbc087e7
Maxime Biloé <maxime.biloe@prestashop.com>
2015-12-14 15:52:14 +0100

    [-]CORE:Fix convertPriceFull which must return float and not string

--- classes/Tools.php
+++ classes/Tools.php
@@ -772,7 +772,7 @@ class ToolsCore
             $amount *= $currency_to->conversion_rate;
         }
 
-        return Tools::ps_round(self::displayPrice($amount, $currency_to), _PS_PRICE_COMPUTE_PRECISION_) ;
+        return Tools::ps_round($amount, _PS_PRICE_COMPUTE_PRECISION_) ;
     }
 
     /**
