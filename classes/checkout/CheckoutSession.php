b805cf2a205aec892dae7eb319949a82e07643aa
djfm <fm.de.jouvencel@gmail.com>
2015-12-17 15:20:02 +0100

    // checkout reorganization, first steps

---
+++ classes/checkout/CheckoutSession.php
@@ -0,0 +1,29 @@
+<?php
+
+class CheckoutSessionCore
+{
+    private $context;
+    private $guest;
+
+    public function setContext(Context $context)
+    {
+        $this->context = $context;
+        return $this;
+    }
+
+    public function setGuest($guest)
+    {
+        $this->guest = (bool)$guest;
+        return $this;
+    }
+
+    public function isGuest()
+    {
+        return $this->guest;
+    }
+
+    public function customerHasLoggedIn()
+    {
+        return $this->context->customer->isLogged();
+    }
+}
---
+++ themes/StarterTheme/templates/checkout/checkout-step.tpl
@@ -0,0 +1,4 @@
+<section>
+  <h1>{$title}</h1>
+  <div>{block "step_content"}{/block}</div>
+</section>
