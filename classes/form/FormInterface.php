a0c5bbae39b8eb0e665816e81663c7cae37c6911
djfm <fm.de.jouvencel@gmail.com>
2015-12-17 15:33:43 +0100

    // Hello FormInterface

---
+++ classes/form/FormInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+interface FormInterface
+{
+    public function setAction($action);
+    public function fillWith(array $params = []);
+    public function handleRequest(array $params = []);
+    public function wasSubmitted();
+    public function submit();
+    public function getErrors();
+    public function hasErrors();
+    public function render();
+}
