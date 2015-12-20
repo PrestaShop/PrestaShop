d65939e69683c49f7e17463891decff6d028475c
djfm <fm.de.jouvencel@gmail.com>
2015-12-20 17:34:15 +0100

    // [+] Addresses Step

--- themes/StarterTheme/_dev/js/checkout.js
+++ themes/StarterTheme/_dev/js/checkout.js
@@ -82,7 +82,7 @@ function setupCheckoutScripts () {
   $('body').on('change', 'input[name="advanced-payment-option"]', enableOrDisableOrderButton);
   $('body').on('change', 'input[type="checkbox"][data-action="hideOrShow"]', hideOrShow);
   $('body').on('change', '.js-address-selector input', selectAddress);
-  $('body').on('click', '.checkout-step.-reachable', function (event) {
+  $('body').on('click', '.checkout-step.-reachable h1', function (event) {
     $(event.target).closest('.checkout-step').toggleClass('-js-current');
   });
 
