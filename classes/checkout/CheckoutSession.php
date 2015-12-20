8a4a7da8a975243f27879b4adc9721ecd3047211
djfm <fm.de.jouvencel@gmail.com>
2015-12-20 17:16:26 +0100

    // set delivery, invoice addresses

--- classes/checkout/CheckoutSession.php
+++ classes/checkout/CheckoutSession.php
@@ -19,4 +19,33 @@ class CheckoutSessionCore
     {
         return $this->context->customer;
     }
+
+    public function getCustomerAddressesCount()
+    {
+        return count($this->getCustomer()->getSimpleAddresses());
+    }
+
+    public function setIdAddressDelivery($id_address)
+    {
+        $this->context->cart->id_address_delivery = $id_address;
+        $this->context->cart->save();
+        return $this;
+    }
+
+    public function setIdAddressInvoice($id_address)
+    {
+        $this->context->cart->id_address_invoice = $id_address;
+        $this->context->cart->save();
+        return $this;
+    }
+
+    public function getIdAddressDelivery()
+    {
+        return $this->context->cart->id_address_delivery;
+    }
+
+    public function getIdAddressInvoice()
+    {
+        return $this->context->cart->id_address_invoice;
+    }
 }
