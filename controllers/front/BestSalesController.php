aba649111b44c8a21ea6777304cdbf80f4bf4116
Maxime Biloé <maxime.biloe@prestashop.com>
2015-12-21 17:14:44 +0100

    // Rework BestSales controller

--- controllers/front/BestSalesController.php
+++ controllers/front/BestSalesController.php
@@ -24,37 +24,48 @@
  * International Registered Trademark & Property of PrestaShop SA
  */
 
-class BestSalesControllerCore extends FrontController
+use PrestaShop\PrestaShop\Core\Business\Product\Search\ProductSearchQuery;
+use PrestaShop\PrestaShop\Core\Business\Product\Search\SortOrder;
+use PrestaShop\PrestaShop\Adapter\BestSales\BestSalesProductSearchProvider;
+use PrestaShop\PrestaShop\Adapter\Translator;
+use PrestaShop\PrestaShop\Adapter\LegacyContext;
+
+class BestSalesControllerCore extends ProductListingFrontController
 {
     public $php_self = 'best-sales';
 
-    public function initContent()
+    /**
+     * Initializes controller
+     *
+     * @see FrontController::init()
+     * @throws PrestaShopException
+     */
+    public function init()
     {
         if (Configuration::get('PS_DISPLAY_BEST_SELLERS')) {
-            parent::initContent();
+            parent::init();
 
-            $this->productSort();
-            $nb_products = (int)ProductSale::getNbSales();
-            $this->pagination($nb_products);
-
-            if (!Tools::getValue('orderby')) {
-                $this->orderBy = 'sales';
-            }
-
-            $products = ProductSale::getBestSales($this->context->language->id, $this->p - 1, $this->n, $this->orderBy, $this->orderWay);
-            $this->addColorsToProductList($products);
-
-            $this->context->smarty->assign(array(
-                'products' => $products,
-                'add_prod_display' => Configuration::get('PS_ATTRIBUTE_CATEGORY_DISPLAY'),
-                'nbProducts' => $nb_products,
-                'homeSize' => Image::getSize(ImageType::getFormatedName('home')),
-                'comparator_max_item' => Configuration::get('PS_COMPARATOR_MAX_ITEM')
-            ));
-
-            $this->setTemplate(_PS_THEME_DIR_.'best-sales.tpl');
+            $this->doProductSearch('catalog/best-sales.tpl');
         } else {
             Tools::redirect('index.php?controller=404');
         }
+    }
+
+    protected function getProductSearchQuery()
+    {
+        $query = new ProductSearchQuery;
+        $query
+            ->setQueryType('best-sales')
+            ->setSortOrder(new SortOrder('product', 'name', 'asc'))
+        ;
+        return $query;
+    }
+
+    protected function getDefaultProductSearchProvider()
+    {
+        $translator = new Translator(new LegacyContext);
+        return new BestSalesProductSearchProvider(
+            $translator
+        );
     }
 }
