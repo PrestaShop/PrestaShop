bcbec89864562bcd839d479d5684c966bf90b0f5
Thomas Nabord <thomas.nabord@prestashop.com>
2015-12-23 14:18:05 +0100

    [*] Project : Improve PHP 7 compatibility
    [*] Project : Improve PHP 7 compatibility
    
    [-] CORE : ObjectModel - check if validate value is set
    
    (cherry picked from commit a043e190daae207bdda1a6a181aee931c3edb4a7)
    
    # Conflicts:
    #	classes/controller/AdminController.php

--- classes/controller/AdminController.php
+++ classes/controller/AdminController.php
@@ -1380,13 +1380,15 @@ class AdminControllerCore extends Controller
                 if (isset($values['type']) && $values['type'] == 'textLang') {
                     foreach ($languages as $language) {
                         if (Tools::getValue($field.'_'.$language['id_lang']) && isset($values['validation'])) {
-                            if (!Validate::$values['validation'](Tools::getValue($field.'_'.$language['id_lang']))) {
+                            $values_validation = $values['validation'];
+                            if (!Validate::$values_validation(Tools::getValue($field.'_'.$language['id_lang']))) {
                                 $this->errors[] = sprintf(Tools::displayError('field %s is invalid.'), $values['title']);
                             }
                         }
                     }
                 } elseif (Tools::getValue($field) && isset($values['validation'])) {
-                    if (!Validate::$values['validation'](Tools::getValue($field))) {
+                    $values_validation = $values['validation'];
+                    if (!Validate::$values_validation(Tools::getValue($field))) {
                         $this->errors[] = sprintf(Tools::displayError('field %s is invalid.'), $values['title']);
                     }
                 }
@@ -3631,7 +3633,8 @@ class AdminControllerCore extends Controller
         if (isset($field['validation'])) {
             $valid_method_exists = method_exists('Validate', $field['validation']);
             if ((!isset($field['empty']) || !$field['empty'] || (isset($field['empty']) && $field['empty'] && $value)) && $valid_method_exists) {
-                if (!Validate::$field['validation']($value)) {
+                $field_validation = $field['validation'];
+                if (!Validate::$field_validation($value)) {
                     $this->errors[] = Tools::displayError($field['title'].' : Incorrect value');
                     return false;
                 }
