311fcbf477ac45058e8d2622a8141d17215cbe34
djfm <fm.de.jouvencel@gmail.com>
2015-12-23 10:56:03 +0100

    // basic payment step

--- classes/checkout/CheckoutPaymentStep.php
+++ classes/checkout/CheckoutPaymentStep.php
@@ -1,9 +1,28 @@
 <?php
 
+use Symfony\Component\Translation\TranslatorInterface;
+
 class CheckoutPaymentStepCore extends AbstractCheckoutStep
 {
+    private $selected_payment_option;
+
+    public function __construct(
+        Smarty $smarty,
+        TranslatorInterface $translator,
+        PaymentOptionsFinder $paymentOptionsFinder,
+        ConditionsToApproveFinder $conditionsToApproveFinder
+    ) {
+        parent::__construct($smarty, $translator);
+        $this->paymentOptionsFinder = $paymentOptionsFinder;
+        $this->conditionsToApproveFinder = $conditionsToApproveFinder;
+    }
+
     public function handleRequest(array $requestParams = [])
     {
+        if (isset($requestParams['select_payment_option'])) {
+            $this->selected_payment_option = $requestParams['select_payment_option'];
+        }
+
         $this->setTitle(
             $this->getTranslator()->trans(
                 'Payment',
@@ -17,7 +36,13 @@ class CheckoutPaymentStepCore extends AbstractCheckoutStep
     {
         return $this->renderTemplate(
             'checkout/payment-step.tpl', [
-
+                'payment_options' => $this
+                    ->paymentOptionsFinder
+                    ->getPaymentOptionsForTemplate(),
+                'conditions_to_approve'   => $this
+                    ->conditionsToApproveFinder
+                    ->getConditionsToApproveForTemplate(),
+                'selected_payment_option' => $this->selected_payment_option
             ]
         );
     }
