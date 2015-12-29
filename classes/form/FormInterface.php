76db50ec83e11cc195fb4a9c9d0962dc00ea7731
djfm <fm.de.jouvencel@gmail.com>
2015-12-29 18:18:15 +0100

    // removed strange methods from FormInterface

--- classes/form/FormInterface.php
+++ classes/form/FormInterface.php
@@ -4,8 +4,6 @@ interface FormInterface
 {
     public function setAction($action);
     public function fillWith(array $params = []);
-    public function handleRequest(array $params = []);
-    public function wasSubmitted();
     public function submit();
     public function getErrors();
     public function hasErrors();
