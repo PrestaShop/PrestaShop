b96ee98652e1050350a31b0ca633656360d9b6bb
djfm <fm.de.jouvencel@gmail.com>
2015-12-29 17:05:57 +0100

    // this belongs to the implementation

--- classes/checkout/CheckoutStepInterface.php
+++ classes/checkout/CheckoutStepInterface.php
@@ -6,7 +6,6 @@ interface CheckoutStepInterface
     public function getTitle();
     public function handleRequest(array $requestParameters = []);
     public function setCheckoutProcess(CheckoutProcess $checkoutProcess);
-    public function getCheckoutProcess();
     public function isReachable();
     public function isComplete();
     public function isCurrent();
