c3df838fc7ffe79ca9443f9837a74e914fd3cc16
djfm <fm.de.jouvencel@gmail.com>
2016-01-07 13:33:36 +0100

    // [*] Harmonization of forms
    This makes the 3 forms (Address, Login, Customer) that
    are used all over the place use the same structure.
    
    Yes, the wheel was re-invented, but better re-invent
    the wheel once than with each form and each place
    it is displayed on.

---
+++ classes/form/CustomerLoginFormatter.php
@@ -0,0 +1,38 @@
+<?php
+
+use Symfony\Component\Translation\TranslatorInterface;
+
+class CustomerLoginFormatterCore implements FormFormatterInterface
+{
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    public function getFormat()
+    {
+        return [
+            'back' => (new FormField)
+                ->setName('back')
+                ->setType('hidden'),
+            'email' => (new FormField)
+                ->setName('email')
+                ->setType('email')
+                ->setRequired(true)
+                ->setLabel($this->translator->trans(
+                    'Email', [], 'Customer'
+                ))
+                ->addConstraint('isEmail'),
+            'password' => (new FormField)
+                ->setName('password')
+                ->setType('password')
+                ->setRequired(true)
+                ->setLabel($this->translator->trans(
+                    'Password', [], 'Customer'
+                ))
+                ->addConstraint('isPasswd'),
+        ];
+    }
+}
