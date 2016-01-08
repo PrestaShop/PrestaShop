2d82854ca7865be98a607dd4aaa0a8bd6214ddcf
Nicolas Martin <nihco2@gmail.com>
2016-01-08 17:05:56 +0100

    // Add middle header hook

--- tests/StarterTheme/prepare-shop.php
+++ tests/StarterTheme/prepare-shop.php
@@ -79,6 +79,7 @@ hookModule('blocktopmenu', 'displayTop');
 hookModule('blocklanguages', 'displayNav');
 hookModule('blockcurrencies', 'displayNav');
 hookModule('blockuserinfo', 'displayNav');
+hookModule('blocksearch', 'displayHeaderMiddle');
 
 // We need a customizable product: we add a single required text field to the product with id 1.
 
