b158f34d54afea80fb3bab5e61151229b1d5757f
Nicolas Martin <nihco2@gmail.com>
2016-01-11 17:44:36 +0100

    // [+] classic : Add drop-down  modules

--- themes/classic/_dev/js/theme.js
+++ themes/classic/_dev/js/theme.js
@@ -6,17 +6,24 @@ window.jQuery = $;
 import '../css/theme';
 import './checkout';
 import './facets';
-import './product';
+
+import DropDown from './components/drop-down';
+import TopMenu from './components/top-menu';
 
 import prestashop from 'prestashop';
 import EventEmitter from 'events';
-import {psShowHide} from './common';
+import {
+  psShowHide
+}
+from './common';
 
 // "inherit" EventEmitter
 for (var i in EventEmitter.prototype) {
-    prestashop[i] = EventEmitter.prototype[i];
+  prestashop[i] = EventEmitter.prototype[i];
 }
 
 $(document).ready(() => {
+  let dropDown = new DropDown();
+  let topMenu = new TopMenu();
   psShowHide();
 });
