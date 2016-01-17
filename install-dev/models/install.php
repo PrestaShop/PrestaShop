cc4934534cc4f68a8078c1929a4c6bab8d3e3aaf
Steven Sulley <steven@sdf.me.uk>
2016-01-17 22:48:13 +0000

    [-] Security : Fixes PSCSX-4841 Usage of ECB mode
    Use CBC mode instead for added security.

--- install-dev/models/install.php
+++ install-dev/models/install.php
@@ -88,8 +88,8 @@ class InstallModelInstall extends InstallAbstractModel
 
         // If mcrypt is activated, add Rijndael 128 configuration
         if (function_exists('mcrypt_encrypt')) {
-            $settings_constants['_RIJNDAEL_KEY_'] = Tools::passwdGen(mcrypt_get_key_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_ECB));
-            $settings_constants['_RIJNDAEL_IV_'] = base64_encode(mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_ECB), MCRYPT_RAND));
+            $settings_constants['_RIJNDAEL_KEY_'] = Tools::passwdGen(mcrypt_get_key_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC));
+            $settings_constants['_RIJNDAEL_IV_'] = base64_encode(mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC), MCRYPT_RAND));
         }
 
         $settings_content = "<?php\n";
