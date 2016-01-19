ca9c0627c0f7d73c3026aac02850f4016dcdc928
Mickaël Andrieu <andrieu.travail@gmail.com>
2016-01-19 10:52:13 +0100

    // Removed useless file

--- composer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$TRAVIS_SECURE_ENV_VARS" = "true" ];
-then
-mkdir ~/.composer/
-echo '{ "config": {"github-oauth":{"github.com": ' > ~/.composer/config.json
-echo "\"$GITHUB_COMPOSER_AUTH\"" >> ~/.composer/config.json
-echo '}}}' >> ~/.composer/config.json
-fi
