2b70b2a2743c2d4d9a812d963a89d233e70d2570
Nicolas Martin <nihco2@gmail.com>
2016-01-25 11:28:42 +0100

    [*] Improve miniatures

---
+++ themes/classic/_dev/js/components/product-miniature.js
@@ -0,0 +1,19 @@
+export default class ProductMinitature {
+  init(){
+    $('.js-product-miniature').each((index, element) => {
+      if($(element).find('.discount-percentage').length){
+        $(element).find('.new').remove();
+      }
+      if($(element).find('.color').length > 5){
+        let count = 0;
+        $(element).find('.color').each((index, element) =>{
+          if(index>4){
+            $(element).hide();
+            count ++;
+          }
+        });
+        $('.js-count').append(`+${count}`);
+      }
+    });
+  }
+}
