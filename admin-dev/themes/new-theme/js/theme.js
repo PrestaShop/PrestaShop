d74181dd5d64c95d5a2e4dd7ad9c775409996d75
Julien Martin <martin.julien82@gmail.com>
2016-02-04 15:01:09 +0100

    [+]BO: PrestaKit added

--- admin-dev/themes/new-theme/js/theme.js
+++ admin-dev/themes/new-theme/js/theme.js
@@ -1,4 +1,5 @@
 import $ from 'expose?$!expose?jQuery!jquery';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/PrestaKit/dist/css/bootstrap-prestakit.css';
 import '../scss/theme.scss';
