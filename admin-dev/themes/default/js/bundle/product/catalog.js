7cd2ab02a7179500b6505347387da258f3812925
Julien Martin <martin.julien82@gmail.com>
2016-02-09 18:34:36 +0100

    [*]BO: Catalog page started

--- admin-dev/themes/default/js/bundle/product/catalog.js
+++ admin-dev/themes/default/js/bundle/product/catalog.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
 	/*
 	 * Tree behavior: collapse/expand system and radio button change event.
 	 */
-	$('div#product_catalog_category_tree_filter div.form-wrapper').categorytree();
+	$('div#product_catalog_category_tree_filter').categorytree();
 	$('div#product_catalog_category_tree_filter div.radio > label > input:radio').change(function() {
 		if ($(this).is(':checked')) {
 			$('form#product_catalog_list input[name="filter_category"]').val($(this).val());
