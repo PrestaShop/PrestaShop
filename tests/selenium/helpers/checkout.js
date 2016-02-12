9198e1fd7dcb74ca76aced4d1a994d0c03413748
Maxime Biloé <maxime.biloe@gmail.com>
2016-02-12 16:27:44 +0100

    //Fix tests because of markup change

--- tests/selenium/helpers/checkout.js
+++ tests/selenium/helpers/checkout.js
@@ -6,7 +6,7 @@ export function addSomeProductToCart () {
     return browser
         .url(fixtures.urls.aCategoryWithProducts)
         .click('.product-miniature:nth-of-type(2)')
-        .click('[name=add]')
+        .click('[data-button-action="add-to-cart"]')
         .waitForVisible('#blockcart-modal')
     ;
 };
