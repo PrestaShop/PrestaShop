0f708de514de929b81dbf5d89a9590028299ea4b
Thibaud Chauviere <thibaud.chauviere@prestashop.com>
2016-02-17 18:31:02 +0100

    // Added new tab system with yaml for SF2 architecture

--- src/PrestaShopBundle/Controller/Admin/ModuleController.php
+++ src/PrestaShopBundle/Controller/Admin/ModuleController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-class ModuleController extends Controller
+class ModuleController extends FrameworkBundleAdminController
 {
     /**
      * Controller responsible for displaying "Catalog" section of Module management pages
