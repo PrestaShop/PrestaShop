f66c8adfa96f60a3044198d0a583132074028a3b
Julien Martin <martin.julien82@gmail.com>
2016-02-19 16:43:30 +0100

    // Composer install / uninstall action created
    With php to avoid bash scripts

---
+++ tools/contrib/install.php
@@ -0,0 +1,12 @@
+<?php
+
+if (file_exists('.git/hooks/pre-commit')) {
+    copy('.git/hooks/pre-commit', '.git/hooks/pre-commit.back');
+    echo "A Git precommit file was found, a backup was created.\n";
+}
+
+if (file_exists('tools/contrib/pre-commit')) {
+    copy('tools/contrib/pre-commit', '.git/hooks/pre-commit');
+    chmod('.git/hooks/pre-commit', 0750);
+    echo "\e[32mPre-commit Hook has been installed.\e[0;m\n";
+}
