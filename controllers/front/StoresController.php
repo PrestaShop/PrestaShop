62aaf29e2236eb98e01049b8f98d803aae7a85c7
Geoffray <gwarnants@users.noreply.github.com>
2016-03-08 15:33:37 +0100

    Removed sensor parameter from Google Maps JS API
    The sensor parameter is no longer required for the Google Maps JavaScript API.
    
    https://developers.google.com/maps/documentation/javascript/error-messages#sensor-not-required

--- controllers/front/StoresController.php
+++ controllers/front/StoresController.php
@@ -286,7 +286,7 @@ class StoresControllerCore extends FrontController
 
         if (!Configuration::get('PS_STORES_SIMPLIFIED')) {
             $default_country = new Country((int)Tools::getCountry());
-            $this->addJS('http'.((Configuration::get('PS_SSL_ENABLED') && Configuration::get('PS_SSL_ENABLED_EVERYWHERE')) ? 's' : '').'://maps.google.com/maps/api/js?sensor=true&region='.substr($default_country->iso_code, 0, 2));
+            $this->addJS('http'.((Configuration::get('PS_SSL_ENABLED') && Configuration::get('PS_SSL_ENABLED_EVERYWHERE')) ? 's' : '').'://maps.google.com/maps/api/js?region='.substr($default_country->iso_code, 0, 2));
             $this->addJS(_THEME_JS_DIR_.'stores.js');
         }
     }
