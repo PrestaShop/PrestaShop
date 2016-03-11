95ca2f0a8a18699128c2e9c38c24d544fa0e4a07
Michael Dekker <git@michaeldekker.nl>
2016-03-11 22:58:21 +0100

    [-] Installer : Fix install DB query

--- install-dev/data/db_structure.sql
+++ install-dev/data/db_structure.sql
@@ -311,9 +311,9 @@ CREATE TABLE `PREFIX_cart_rule_shop` (
 CREATE TABLE `PREFIX_cart_product` (
   `id_cart` int(10) unsigned NOT NULL,
   `id_product` int(10) unsigned NOT NULL,
-  `id_address_delivery` int(10) UNSIGNED DEFAULT '0',
+  `id_address_delivery` int(10) unsigned NOT NULL DEFAULT '0',
   `id_shop` int(10) unsigned NOT NULL DEFAULT '1',
-  `id_product_attribute` int(10) unsigned NOT NULL,
+  `id_product_attribute` int(10) unsigned NOT NULL DEFAULT '0',
   `quantity` int(10) unsigned NOT NULL DEFAULT '0',
   `date_add` datetime NOT NULL,
   PRIMARY KEY (`id_cart`,`id_product`,`id_product_attribute`,`id_address_delivery`),
