2f8d6d1f664698875583071f59b025281e1cbb8c
Mickaël Andrieu <andrieu.travail@gmail.com>
2016-04-04 15:44:02 +0200

    // moved product category tags to his own module

--- admin-dev/themes/default/js/bundle/product/form.js
+++ admin-dev/themes/default/js/bundle/product/form.js
@@ -48,9 +48,9 @@ $(document).ready(function() {
 	displayFieldsManager.init();
 	seo.init();
 	tags.init();
-	productCategoriesTags.init();
 	rightSidebar.init();
 	recommendedModules.init();
+	BOEvent.emitEvent("Product Categories Management started", "CustomEvent");
 
 	/** Type product fields display management */
 	$('#form_step1_type_product').change(function(){
@@ -1997,7 +1997,6 @@ var seo = (function() {
 	};
 })();
 
-
 /**
  * Tags management
  */
@@ -2007,159 +2006,6 @@ var tags = (function() {
 			$('#form_step6_tags .tokenfield').tokenfield();
 		}
 	};
-})();
-
-/**
- * Product categories Tags management
- */
-var productCategoriesTags;
-productCategoriesTags = (function () {
-  return {
-    'init': function () {
-      selectedCategories = this.getTags();
-      selectedCategories.forEach(this.createTag);
-
-      // add tags management
-      this.manageTagsOnInput();
-      this.manageTagsOnTags();
-
-      // add search box
-      this.initSearchBox();
-    },
-    'removeTag': function (categoryId) {
-      $('span[data-id^="' + categoryId + '"]').parent().remove();
-
-      return true;
-    },
-    'getTags': function () {
-      var categoriesForm = $('#form_step1_categories');
-      var inputs = categoriesForm.find('label > input:checked').toArray();
-
-      var tags = [];
-      var that = this;
-      inputs.forEach(function getLabels(input) {
-        var tree = that.getTree();
-        var tag = {
-          'name': input.parentNode.innerText,
-          'id': input.value,
-        };
-        tree.forEach(function getCategories(_category) {
-          if (_category.id == tag.id) {
-            tag.breadcrumb = _category.breadcrumb;
-          }
-        });
-
-        tags.push(tag);
-      });
-
-      return tags;
-    },
-    'manageTagsOnInput': function () {
-      var categoriesForm = $('#form_step1_categories');
-      var that = this;
-      categoriesForm.on('click', 'input', function () {
-        var input = $(this);
-        if (input.prop('checked') === false) {
-          that.removeTag($(this).val());
-        } else {
-          var tag = {
-            'name': input.parent().text(),
-            'id': input.val(),
-            'breadcrumb': ''
-          };
-
-          that.createTag(tag);
-        }
-      });
-
-      return true;
-    },
-    'manageTagsOnTags': function () {
-      var categoriesForm = $('#form_step1_categories');
-      var tagsContainer = $('#ps_categoryTags');
-      var that = this;
-
-      tagsContainer.on('click', 'a.pstaggerClosingCross', function (event) {
-        event.preventDefault();
-        var id = $(this).data('id');
-        that.removeTag(id);
-        categoriesForm.find('input[value="' + id + '"]').prop('checked', false);
-        tagsContainer.focus();
-      });
-
-      return true;
-    },
-    'getTree': function () {
-      var tree = JSON.parse($('#ps_categoryTree').html());
-
-      return tree;
-    },
-    'createTag': function (category) {
-
-      if (category.breadcrumb == '') {
-        var tree = this.getTree();
-        tree.forEach(function getCategories(_category) {
-          if (_category.id == category.id) {
-            category.breadcrumb = _category.breadcrumb;
-          }
-        });
-      }
-
-      $('#ps_categoryTags').append('<span class="pstaggerTag">' +
-        '<span data-id="' + category.id + '" title="' + category.breadcrumb + '">' + category.name + '</span>' +
-        '<a class="pstaggerClosingCross" href="#" data-id="' + category.id + '">x</a>' +
-        '</span>');
-
-      return true;
-    },
-    'getNameFromBreadcrumb': function (name) {
-
-      if (name.indexOf('&gt;') !== -1) {
-        return name.substring(name.lastIndexOf('&gt') + 4); // remove "&gt; "
-      }
-
-      return name;
-    },
-    'initSearchBox': function () {
-      var searchBox = $('#ps-select-product-category');
-      var tree = this.getTree();
-      var tags = [];
-      var that = this;
-      tree.forEach(function buildTags(tagObject){
-        tags.push({
-          label: tagObject.breadcrumb,
-          value: tagObject.id
-        });
-      });
-
-      searchBox.autocomplete({
-        source: tags,
-        minChars: 2,
-        autoFill: true,
-        max:20,
-        matchContains: true,
-        mustMatch:false,
-        scroll:false,
-        select: function(event, ui) {
-          event.preventDefault();
-          var label = ui.item.label;
-          var categoryName = that.getNameFromBreadcrumb(label);
-          var categoryId = ui.item.value;
-          that.createTag({
-            'name': categoryName,
-            'id': categoryId
-          });
-
-          $(this).val('');
-        }
-      }).data('ui-autocomplete')._renderItem = function(ul, item) {
-        return $('<li>')
-          .data('ui-autocomplete-item', item)
-          .append('<a>'+item.label+'</a>')
-          .appendTo(ul);
-      };
-    }
-  };
 })();
 
 var recommendedModules = (function() {
