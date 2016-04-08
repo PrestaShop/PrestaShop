81d6f5bd41e1e63de216e4a5e02aafa2bf397c22
Gytis Škėma <gytis@ymail.com>
2016-04-08 13:59:10 +0300

    [*] BO: Bring back email pattern value in case someone uses it

--- controllers/admin/AdminTranslationsController.php
+++ controllers/admin/AdminTranslationsController.php
@@ -1569,6 +1569,17 @@ class AdminTranslationsControllerCore extends AdminController
     protected function getMailPattern()
     {
         Tools::displayAsDeprecated('Email pattern is no longer used, emails are always saved like they are.');
+        // Let the indentation like it.
+        return '<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/1999/REC-html401-19991224/strict.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>#title</title>
+</head>
+<body>
+    #content
+</body>
+</html>';
     }
 
     /**
