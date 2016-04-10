8472860274c5f81b8923d81626030e2d66350643
Michael Dekker <git@michaeldekker.nl>
2016-04-10 23:04:57 +0200

    [*] CORE : Toggle birthdate field with one click

--- install-dev/upgrade/sql/1.7.0.0.sql
+++ install-dev/upgrade/sql/1.7.0.0.sql
@@ -25,6 +25,8 @@ ALTER TABLE `PREFIX_customer` CHANGE COLUMN `passwd` `passwd` varchar(255) NOT N
 
 INSERT INTO `PREFIX_configuration` (`id_configuration` ,`id_shop_group` ,`id_shop` ,`name` ,`value` ,`date_add` ,`date_upd`) VALUES (NULL , NULL , NULL , 'PS_ACTIVE_CRONJOB_EXCHANGE_RATE', '0', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
+INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VALUES ('PS_CUSTOMER_BIRTHDATE', 1, NOW(), NOW());
+
 ALTER TABLE `PREFIX_customer` CHANGE COLUMN `firstname` `firstname` varchar(255) NOT NULL;
 ALTER TABLE `PREFIX_customer` CHANGE COLUMN `lastname` `lastname` varchar(255) NOT NULL;
 
