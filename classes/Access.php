30401530ad05a271dd3a8702a7c2c63cc1c05f99
Jérémie Tabet <contact@jeremietabet.com>
2016-04-11 11:09:09 +0200

    [-] Security : Added addAccess and removeAccess for retrocompatibility

--- classes/Access.php
+++ classes/Access.php
@@ -63,4 +63,37 @@ class AccessCore extends ObjectModel
     {
         return sprintf('ROLE_MOD_MODULE_%s_%s', strtoupper($module['name'], $authorization));
     }
+    
+    public function addAccess($idProfile, $idTab, $authorization)
+    {
+        $slug = self::findSlugByIdTab($idTab).$authorization;
+        $result = Db::getInstance()->getRow('
+            SELECT `id_authorization_role`
+            FROM `'._DB_PREFIX_.'authorization_role` t
+            WHERE `slug` = "'.$slug.'"
+        ');
+        $sql = '
+            INSERT INTO `'._DB_PREFIX_.'access` (`id_profile`, `id_authorization_role`)
+            VALUES ('.$idProfile.','.$result['id_authorization_role'].')
+        ';
+        
+        return Db::getInstance()->execute($sql) ? 'ok' : 'error';
+    }
+    
+    public function removeAccess($idProfile, $idTab, $authorization)
+    {
+        $slug = self::findSlugByIdTab($idTab).$authorization;
+        $result = Db::getInstance()->getRow('
+            SELECT `id_authorization_role`
+            FROM `'._DB_PREFIX_.'authorization_role` t
+            WHERE `slug` = "'.$slug.'"
+        ');
+        $sql = '
+            DELETE FROM `'._DB_PREFIX_.'authorization_role` t
+            WHERE `id_profile` = "'.$idProfile.'"
+            AND `id_authorization_role` = "'.$result['id_authorization_role'].'"
+        ';
+        
+        return Db::getInstance()->execute($sql) ? 'ok' : 'error';
+    }
 }
