6c0d58df3dd050e50fbb76a501d076af3bc9cd04
Jérémie Tabet <contact@jeremietabet.com>
2016-04-11 09:50:18 +0200

    [+] Security : Added class for Access with helper functions

---
+++ classes/Access.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * 2007-2015 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2015 PrestaShop SA
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+class AccessCore extends ObjectModel
+{
+    public static function findIdTabByAuthSlug($authSlug)
+    {
+        preg_match(
+            '/ROLE_MOD_[A-Z]+_(?P<classname>[A-Z]+)_(?P<auth>[A-Z]+)/',
+            $authSlug,
+            $matches
+        );
+
+        $result = Db::getInstance()->getRow('
+            SELECT `id_tab`
+            FROM `'._DB_PREFIX_.'tab` t
+            WHERE UCASE(`class_name`) = "'.$matches['classname'].'"
+        ');
+        
+        return $result['id_tab'];
+    }
+    
+    public static function findSlugByIdTab($idTab)
+    {
+        $result = Db::getInstance()->getRow('
+            SELECT `class_name`
+            FROM `'._DB_PREFIX_.'tab` t
+            WHERE `id_tab` = "'.$idTab.'"
+        ');
+        
+        return self::sluggifyTab($result['class_name']);
+    }
+    
+    public static function sluggifyTab($tab, $authorization = '')
+    {
+        return sprintf('ROLE_MOD_TAB_%s_%s', strtoupper($tab['classname'], $authorization));
+    }
+    
+    public static function sluggifyModule($module, $authorization = '')
+    {
+        return sprintf('ROLE_MOD_MODULE_%s_%s', strtoupper($module['name'], $authorization));
+    }
+}
