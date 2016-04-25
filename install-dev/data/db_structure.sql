a1ce85c01924c84604566fdf02bff9bda72519e1
Julien Bourdeau <julien@sigerr.org>
2016-04-25 14:33:04 +0200

    [+] IN: All key must not be nullable

--- install-dev/data/db_structure.sql
+++ install-dev/data/db_structure.sql
@@ -312,7 +312,7 @@ CREATE TABLE `PREFIX_cart_product` (
   `id_product` int(10) unsigned NOT NULL,
   `id_address_delivery` int(10) UNSIGNED DEFAULT '0',
   `id_shop` int(10) unsigned NOT NULL DEFAULT '1',
-  `id_product_attribute` int(10) unsigned DEFAULT '0',
+  `id_product_attribute` int(10) unsigned NOT NULL DEFAULT '0',
   `quantity` int(10) unsigned NOT NULL DEFAULT '0',
   `date_add` datetime NOT NULL,
   PRIMARY KEY (`id_cart`,`id_product`,`id_product_attribute`,`id_address_delivery`),
