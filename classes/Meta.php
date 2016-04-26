3e8b02fb978aee1cbb46aae92f8dc45665cbafc7
ZiZuu.com <ZiZuu-store@users.noreply.github.com>
2016-04-26 18:36:56 +0200

    [*] CORE: simplify logic expression
    ! (!A && !B) is a simple A || B

--- classes/Meta.php
+++ classes/Meta.php
@@ -211,8 +211,8 @@ class MetaCore extends ObjectModel
      */
     public static function getMetaTags($id_lang, $page_name, $title = '')
     {
-        if (!(!Configuration::get('PS_SHOP_ENABLE')
-            && !in_array(Tools::getRemoteAddr(), explode(',', Configuration::get('PS_MAINTENANCE_IP'))))) {
+        if (Configuration::get('PS_SHOP_ENABLE')
+            || in_array(Tools::getRemoteAddr(), explode(',', Configuration::get('PS_MAINTENANCE_IP')))) {
             if ($page_name == 'product' && ($id_product = Tools::getValue('id_product'))) {
                 return Meta::getProductMetas($id_product, $id_lang, $page_name);
             } elseif ($page_name == 'category' && ($id_category = Tools::getValue('id_category'))) {
