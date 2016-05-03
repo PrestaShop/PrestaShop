440287fc78328a76624ec05cf1525e1b12c4da28
Maxime Biloé <maxime.biloe@gmail.com>
2016-05-03 13:59:25 +0200

    BO: Notifications JS refacto

--- admin-dev/themes/new-theme/js/notifications.js
+++ admin-dev/themes/new-theme/js/notifications.js
@@ -10,42 +10,14 @@
     success: function(json) {
       if (json) {
 
-        let orderTpl = jQuery("#order-notification-template").html();
-        let customerTpl = jQuery("#customer-notification-template").html();
-        let messageTpl = jQuery("#message-notification-template").html();
-
         let nbOrders = parseInt(json.order.total);
         let nbCustomers = parseInt(json.customer.total);
         let nbCustomerMessages = parseInt(json.customer_message.total);
         let notifications_total = nbOrders + nbCustomers + nbCustomerMessages;
 
-        jQuery.each(json.order.results, function(property, value) {
-          jQuery("#orders-notifications").append(
-            orderTpl.replace(/_id_order_/g, parseInt(value.id_order))
-              .replace(/_customer_name_/g, value.customer_name)
-              .replace(/_iso_code_/g, value.iso_code)
-              .replace(/_carrier_/g, (value.carrier !== "" ? " - " + value.carrier : ""))
-              .replace(/_total_paid_/g, value.total_paid)
-          );
-        });
-
-        jQuery.each(json.customer.results, function(property, value) {
-          jQuery("#customers-notifications").append(
-            customerTpl.replace(/_id_customer_/g, parseInt(value.id_customer))
-              .replace(/_customer_name_/g, value.customer_name)
-              .replace(/_company_/g, (value.company !== "" ? " (" + value.company + ") " : ""))
-              .replace(/_date_add_/g, value.date_add)
-          );
-        });
-
-        jQuery.each(json.customer_message.results, function(property, value) {
-          jQuery("#messages-notifications").append(
-            messageTpl.replace(/_status_/g, value.status)
-              .replace(/_customer_name_/g, value.customer_name)
-              .replace(/_company_/g, (value.company !== "" ? " (" + value.company + ") " : ""))
-              .replace(/_date_add_/g, value.date_add)
-          );
-        });
+        fillTpl(json.order.results, jQuery("#orders-notifications"), jQuery("#order-notification-template").html());
+        fillTpl(json.customer.results, jQuery("#customers-notifications"), jQuery("#customer-notification-template").html());
+        fillTpl(json.customer_message.results, jQuery("#messages-notifications"), jQuery("#message-notification-template").html());
 
         setNotificationsNumber(jQuery('.notifications #orders-tab'), "_nb_new_orders_", nbOrders);
         setNotificationsNumber(jQuery('.notifications #customers-tab'), "_nb_new_customers_", nbCustomers);
@@ -54,6 +26,22 @@
       }
     }
   });
+
+  let fillTpl = function (results, eltAppendTo, tpl) {
+    jQuery.each(results, function(property, value) {
+      eltAppendTo.append(
+        tpl.replace(/_id_order_/g, parseInt(value.id_order))
+          .replace(/_customer_name_/g, value.customer_name)
+          .replace(/_iso_code_/g, value.iso_code)
+          .replace(/_carrier_/g, (value.carrier !== "" ? " - " + value.carrier : ""))
+          .replace(/_total_paid_/g, value.total_paid)
+          .replace(/_id_customer_/g, parseInt(value.id_customer))
+          .replace(/_company_/g, (value.company !== "" ? " (" + value.company + ") " : ""))
+          .replace(/_date_add_/g, value.date_add)
+          .replace(/_status_/g, value.status)
+      );
+    });
+  }
 
   let setNotificationsNumber = function (elt, id, number) {
     if (number > 0) {
