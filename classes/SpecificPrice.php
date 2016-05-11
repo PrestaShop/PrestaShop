af593e3718897a72c4cfb3583db5d27d159a1255
Nigel Helliwell <nigel.helliwell@gmail.com>
2016-05-11 17:02:47 -0500

    BO: Removed new filter on id_cart: http://forge.prestashop.com/browse/PSCSX-7849

--- classes/SpecificPrice.php
+++ classes/SpecificPrice.php
@@ -337,9 +337,7 @@ class SpecificPriceCore extends ObjectModel
             $query_extra .= self::filterOutField('id_product_attribute', $id_product_attribute);
         }
 
-        if ($id_cart !== null) {
-            $query_extra .= self::filterOutField('id_cart', $id_cart);
-        }
+        $query_extra .= self::filterOutField('id_cart', $id_cart);
 
         if ($ending == $now && $beginning == $now) {
             $key = __FUNCTION__.'-'.$first_date.'-'.$last_date;
