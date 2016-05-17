4bd4061a607be8eb5fc9663c5693c13f7a0cf9d9
Thomas Nabord <thomas.nabord@prestashop.com>
2016-05-17 15:55:05 +0200

    CO: Select proper autoload.php file for travis
    (cherry picked from commit 041aa560a93465c2c7c577d1c5ad67e78a67831c)

--- config/autoload.php
+++ config/autoload.php
@@ -30,4 +30,4 @@ require_once(_PS_CONFIG_DIR_.'alias.php');
 require_once(_PS_CLASS_DIR_.'PrestaShopAutoload.php');
 spl_autoload_register(array(PrestaShopAutoload::getInstance(), 'load'));
 
-require(_PS_VENDOR_DIR_.'autoload.php');
+require(__DIR__.'/../app/autoload.php');
