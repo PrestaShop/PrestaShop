3efce7c8643d511ea2298d65dd979ad9d1415ed4
Jorge Vargas <jorgevargaslarrota@hotmail.com>
2016-05-30 20:35:22 -0500

    B2B with internationalization
    SIRET and APE is not a feature with international use, then must be and abstract method to allow all countries to use it only by overriding this class. Then I suggest you to delete this field in Customer class and implements as a module and an apart class in future versions.

--- classes/Validate.php
+++ classes/Validate.php
@@ -1056,18 +1056,7 @@ class ValidateCore
      */
     public static function isSiret($siret)
     {
-        if (Tools::strlen($siret) != 14) {
-            return false;
-        }
-        $sum = 0;
-        for ($i = 0; $i != 14; $i++) {
-            $tmp = ((($i + 1) % 2) + 1) * intval($siret[$i]);
-            if ($tmp >= 10) {
-                $tmp -= 9;
-            }
-            $sum += $tmp;
-        }
-        return ($sum % 10 === 0);
+        return ($siret ? true : false);
     }
 
     /**
@@ -1078,7 +1067,7 @@ class ValidateCore
      */
     public static function isApe($ape)
     {
-        return (bool)preg_match('/^[0-9]{3,4}[a-zA-Z]{1}$/s', $ape);
+        return ($ape ? true : false);
     }
 
     public static function isControllerName($name)
