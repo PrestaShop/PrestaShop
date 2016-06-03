6d00f2d9ee1a36af0032ac52e90a1d58a6be2d88
Maxime Biloé <maxime.biloe@gmail.com>
2016-06-03 16:00:58 +0200

    FO: Moce some JS from Classic to Core

--- themes/_core/js/cart.js
+++ themes/_core/js/cart.js
@@ -24,4 +24,54 @@ $(document).ready(() => {
       prestashop.emit('cart dom updated');
     });
   });
+
+  $('body').on(
+    'click',
+    '[data-button-action="add-to-cart"]',
+    function(event) {
+      event.preventDefault();
+      var $form = $($(event.target).closest('form'));
+      var query = $form.serialize() + '&add=1&action=update';
+      var actionURL = $form.attr('action');
+
+      $.post(actionURL, query, null, 'json').then(function(resp) {
+        prestashop.emit('cart updated', {
+          reason: {
+            idProduct: resp.id_product,
+            idProductAttribute: resp.id_product_attribute,
+            linkAction: 'add-to-cart'
+          }
+        });
+      });
+    }
+  );
+
+  $('body').on(
+    'submit',
+    '[data-link-action="add-voucher"]',
+    function(event) {
+      event.preventDefault();
+
+      $(this).append($('<input>')
+        .attr('type', 'hidden')
+        .attr('name', 'ajax').val('1')
+      );
+      $(this).append($('<input>')
+        .attr('type', 'hidden')
+        .attr('name', 'action').val('update')
+      );
+
+      // First perform the action using AJAX
+      var actionURL = $(this).attr('action');
+
+      $.post(actionURL, $(this).serialize(), null, 'json').then(function() {
+        // If succesful, refresh cart preview
+        prestashop.emit('cart updated', {
+          reason: event.target.dataset
+        });
+      });
+    }
+  );
 });
+
+
