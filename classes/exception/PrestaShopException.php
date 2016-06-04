0b0a6df241224ce01807d4671dff151991f9b5a5
fojtcz <m.fojtik@sezmam.cz>
2016-06-04 15:02:05 +0200

    CO: Fix error log path

--- classes/exception/PrestaShopException.php
+++ classes/exception/PrestaShopException.php
@@ -145,7 +145,7 @@ class PrestaShopExceptionCore extends Exception
     protected function logError()
     {
         $logger = new FileLogger();
-        $logger->setFilename(_PS_ROOT_DIR_.'/log/'.date('Ymd').'_exception.log');
+        $logger->setFilename(_PS_ROOT_DIR_.'/app/logs/'.date('Ymd').'_exception.log');
         $logger->logError($this->getExtendedMessage(false));
     }
 
