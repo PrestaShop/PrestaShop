b0103c824a3d4b7371a6ca1bbe95fb029a67abd4
Maxime Biloé <maxime.biloe@gmail.com>
2016-06-24 17:37:40 +0200

    BO: Compiled new theme assets

--- admin-dev/themes/new-theme/public/bundle.js
+++ admin-dev/themes/new-theme/public/bundle.js
@@ -73420,9 +73420,6 @@
 	var queueIndex = -1;
 
 	function cleanUpNextTick() {
-	    if (!draining || !currentQueue) {
-	        return;
-	    }
 	    draining = false;
 	    if (currentQueue.length) {
 	        queue = currentQueue.concat(queue);
