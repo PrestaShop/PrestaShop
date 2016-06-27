01d96c6fbd97aa6f46d82a1e2412f29c7c29350f
Jérémie Tabet <contact@jeremietabet.com>
2016-06-27 12:24:34 +0200

    CO: Added support for %parameter% in translation

--- src/PrestaShopBundle/Translation/SprintfTranslatorTrait.php
+++ src/PrestaShopBundle/Translation/SprintfTranslatorTrait.php
@@ -34,13 +34,11 @@ trait SprintfTranslatorTrait
      */
     public function trans($id, array $parameters = array(), $domain = null, $locale = null)
     {
-        $translation = parent::trans($id, array(), $domain, $locale);
-
-        if (!empty($parameters)) {
-            return $this->checkAndReplaceArgs($translation, $parameters);
+        if (!$this->isSprintfString($id) || empty($parameters)) {
+            return parent::trans($id, $parameters, $domain, $locale);
         }
 
-        return $translation;
+        return vsprintf(parent::trans($id, array(), $domain, $locale), $parameters);
     }
 
     /**
@@ -48,28 +46,21 @@ trait SprintfTranslatorTrait
      */
     public function transChoice($id, $number, array $parameters = array(), $domain = null, $locale = null)
     {
-        $translation = parent::transChoice($id, $number, array(), $domain, $locale);
-
-        if (!empty($parameters)) {
-            return $this->checkAndReplaceArgs($translation, $parameters);
+        if (!$this->isSprintfString($id)) {
+            return parent::transChoice($id, $number, $parameters, $domain, $locale);
         }
 
-        return $translation;
+        return vsprintf(parent::transChoice($id, $number, array(), $domain, $locale), $parameters);
     }
 
     /**
-     * Check if string use a specif syntax for sprintf and replace arguments if use it
      *
-     * @param string $string
-     * @param mixed $args
-     * @return string
+     * @param  string  $string
+     * @return boolean
      */
-    private final function checkAndReplaceArgs($string, $args)
+    private final function isSprintfString($string)
     {
-        if (preg_match_all('#(?:%%|%(?:[0-9]+\$)?[+-]?(?:[ 0]|\'.)?-?[0-9]*(?:\.[0-9]+)?[bcdeufFosxX])#', $string, $matches)) {
-            return vsprintf($string, (array) $args);
-        }
-
-        return $string;
+        return (bool) preg_match_all('#(?:%%|%(?:[0-9]+\$)?[+-]?(?:[ 0]|\'.)?-?[0-9]*(?:\.[0-9]+)?[bcdeufFosxX])#', $string)
+            && !(bool) preg_match_all('/%\w+%/', $string);
     }
 }
