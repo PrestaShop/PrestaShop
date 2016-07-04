f911152ec8c228c1c7cae8425c7889fcb9ba545c
Maxime Biloé <maxime.biloe@gmail.com>
2016-07-04 16:11:00 +0200

    BO: Should raise an exception or log it when sprintf is not an array

--- config/smartyadmin.config.inc.php
+++ config/smartyadmin.config.inc.php
@@ -57,10 +57,27 @@ function smartyTranslate($params, &$smarty)
     $htmlentities = !isset($params['js']);
     $pdf = isset($params['pdf']);
     $addslashes = (isset($params['slashes']) || isset($params['js']));
-    $sprintf = isset($params['sprintf']) ? $params['sprintf'] : null;
+    $sprintf = isset($params['sprintf']) ? $params['sprintf'] : array();
 
     if (!empty($params['d'])) {
-        return Context::getContext()->getTranslator()->trans($params['s'], (array) $sprintf, $params['d']);
+        if (!is_array($sprintf)) {
+            $backTrace = debug_backtrace();
+
+            $errorMessage = sprintf(
+                'Unable to translate "%s" in %s. sprintf() parameter should be an array.',
+                $params['s'],
+                $backTrace[0]['args'][1]->template_resource
+            );
+
+            if (_PS_MODE_DEV_) {
+                throw new Exception($errorMessage);
+            } else {
+                PrestaShopLogger::addLog($errorMessage);
+                return $params['s'];
+            }
+        }
+
+        return Context::getContext()->getTranslator()->trans($params['s'], $sprintf, $params['d']);
     }
 
     if ($pdf) {
