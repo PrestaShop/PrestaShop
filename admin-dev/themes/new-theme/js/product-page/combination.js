4a7024ab147e4c0a6847f8e7ab1fe34dad19d3d7
Maxime Biloé <maxime.biloe@gmail.com>
2016-07-11 10:57:45 +0200

    BO: Make Ajax request to load combination form

--- admin-dev/themes/new-theme/js/product-page/combination.js
+++ admin-dev/themes/new-theme/js/product-page/combination.js
@@ -2,6 +2,24 @@ import $ from 'jquery';
 
 export default function() {
   $(document).ready(function() {
+    let $jsCombinationsList = $('.js-combinations-list');
+    let idsProductAttribute = $jsCombinationsList.data('ids-product-attribute').split(',');
+    let idsCount = idsProductAttribute.length;
+    let currentCount = 0;
+    let step = 5;
 
+    let combinationUrl = $jsCombinationsList.data('combinations-url') + '/' + idsProductAttribute.slice(currentCount, currentCount+step).join('-');
+
+    let getCombinations = () => {
+      $.get(combinationUrl).then(function (resp) {
+        $jsCombinationsList.append(resp);
+        currentCount += step;
+        combinationUrl = $jsCombinationsList.data('combinations-url') + '/' + idsProductAttribute.slice(currentCount, currentCount+step).join('-');
+        if (currentCount <= idsCount) {
+          getCombinations();
+        }
+      });
+    };
+    getCombinations();
   });
 }
