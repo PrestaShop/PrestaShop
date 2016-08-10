125666ceb5cc922ba70507215924531ce9c89aa6
Michael Dekker <git@michaeldekker.nl>
2016-08-11 01:11:58 +0200

    CO: Document Attachment class (#5991)

--- classes/Attachment.php
+++ classes/Attachment.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 2007-2015 PrestaShop
+ * 2007-2016 PrestaShop
  *
  * NOTICE OF LICENSE
  *
@@ -19,11 +19,14 @@
  * needs please refer to http://www.prestashop.com for more information.
  *
  * @author    PrestaShop SA <contact@prestashop.com>
- * @copyright 2007-2015 PrestaShop SA
+ * @copyright 2007-2016 PrestaShop SA
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
 
+/**
+ * Class AttachmentCore
+ */
 class AttachmentCore extends ObjectModel
 {
     public $file;
@@ -33,7 +36,7 @@ class AttachmentCore extends ObjectModel
     public $mime;
     public $description;
 
-    /** @var int position */
+    /** @var int position Position */
     public $position;
 
     /**
@@ -55,18 +58,27 @@ class AttachmentCore extends ObjectModel
         ),
     );
 
+    /**
+     * @see ObjectModel::add()
+     */
     public function add($autodate = true, $null_values = false)
     {
         $this->file_size = filesize(_PS_DOWNLOAD_DIR_.$this->file);
         return parent::add($autodate, $null_values);
     }
 
+    /**
+     * @see ObjectModel::update()
+     */
     public function update($null_values = false)
     {
         $this->file_size = filesize(_PS_DOWNLOAD_DIR_.$this->file);
         return parent::update($null_values);
     }
 
+    /**
+     * @see ObjectModel::delete()
+     */
     public function delete()
     {
         @unlink(_PS_DOWNLOAD_DIR_.$this->file);
@@ -85,6 +97,14 @@ class AttachmentCore extends ObjectModel
         return parent::delete();
     }
 
+    /**
+     * Delete selection of attachments
+     *
+     * @param array $attachments Attachments
+     *
+     * @return bool|int Whether the selection has been successfully deleted
+     * @todo: Find out if $return can be initialized with true. (breaking change)
+     */
     public function deleteSelection($attachments)
     {
         $return = 1;
@@ -92,9 +112,19 @@ class AttachmentCore extends ObjectModel
             $attachment = new Attachment((int)$id_attachment);
             $return &= $attachment->delete();
         }
+
         return $return;
     }
 
+    /**
+     * Get attachments
+     *
+     * @param int  $id_lang    Language ID
+     * @param int  $id_product Product ID
+     * @param bool $include    Whether the attachments are included or excluded from the Product ID
+     *
+     * @return array|false|mysqli_result|null|PDOStatement|resource Database query result
+     */
     public static function getAttachments($id_lang, $id_product, $include = true)
     {
         return Db::getInstance()->executeS('
@@ -111,9 +141,10 @@ class AttachmentCore extends ObjectModel
     }
 
     /**
-     * Unassociate $id_product from the current object
+     * Delete Product attachments for the given Product ID
      *
-     * @param $id_product int
+     * @param int $id_product Product ID
+     *
      * @return bool
      */
     public static function deleteProductAttachments($id_product)
@@ -128,10 +159,11 @@ class AttachmentCore extends ObjectModel
     }
 
     /**
-     * associate $id_product to the current object.
+     * Associate $id_product to the current object.
      *
      * @param int $id_product id of the product to associate
-     * @return bool true if succed
+     *
+     * @return bool true if success
      */
     public function attachProduct($id_product)
     {
@@ -149,9 +181,10 @@ class AttachmentCore extends ObjectModel
      * Associate an array of id_attachment $array to the product $id_product
      * and remove eventual previous association
      *
-     * @param $id_product
-     * @param $array
-     * @return bool
+     * @param int   $id_product Product ID
+     * @param array $array      Attachment IDs
+     *
+     * @return bool Whether the attachments have been successfully associated with the Product
      */
     public static function attachToProduct($id_product, $array)
     {
@@ -178,24 +211,32 @@ class AttachmentCore extends ObjectModel
         return $result1;
     }
 
-    public static function getProductAttached($id_lang, $list)
+    /**
+     * Get Attachment IDs for the given Product within the given range of attachment IDs
+     *
+     * @param int $idLang Language ID
+     * @param array $list List of attachment IDs in which to search
+     *
+     * @return array|bool List of attachment IDs found. False if nothing found.
+     */
+    public static function getProductAttached($idLang, $list)
     {
-        $ids_attachements = array();
+        $idsAttachments = array();
         if (is_array($list)) {
-            foreach ($list as $attachement) {
-                $ids_attachements[] = $attachement['id_attachment'];
+            foreach ($list as $attachment) {
+                $idsAttachments[] = $attachment['id_attachment'];
             }
 
             $sql = 'SELECT * FROM `'._DB_PREFIX_.'product_attachment` pa
 					LEFT JOIN `'._DB_PREFIX_.'product_lang` pl ON (pa.`id_product` = pl.`id_product`'.Shop::addSqlRestrictionOnLang('pl').')
-					WHERE `id_attachment` IN ('.implode(',', array_map('intval', $ids_attachements)).')
-						AND pl.`id_lang` = '.(int)$id_lang;
+					WHERE `id_attachment` IN ('.implode(',', array_map('intval', $idsAttachments)).')
+						AND pl.`id_lang` = '.(int) $idLang;
             $tmp = Db::getInstance()->executeS($sql);
-            $product_attachements = array();
+            $productAttachments = array();
             foreach ($tmp as $t) {
-                $product_attachements[$t['id_attachment']][] = $t['name'];
+                $productAttachments[$t['id_attachment']][] = $t['name'];
             }
-            return $product_attachements;
+            return $productAttachments;
         } else {
             return false;
         }
