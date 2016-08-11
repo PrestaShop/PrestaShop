5ae52a15f5a1b736ebacd40e34178bb9e0b8d556
Martin Fojtík <m.fojtik@seznam.cz>
2016-08-11 12:07:13 +0200

    IN: Check if settings.inc.php has default values (#5958)

--- src/PrestaShopBundle/Utils/Migrate.php
+++ src/PrestaShopBundle/Utils/Migrate.php
@@ -28,43 +28,45 @@ class Migrate
 
         if (file_exists($root_dir.'/'.self::SETTINGS_FILE)) {
             $tmp_settings = file_get_contents($root_dir.'/'.self::SETTINGS_FILE);
-            $tmp_settings = preg_replace('/(\'|")\_/', '$1_LEGACY_', $tmp_settings);
-            file_put_contents($root_dir.'/'.self::SETTINGS_FILE, $tmp_settings);
-            include $root_dir.'/'.self::SETTINGS_FILE;
+            if (strpos($tmp_settings, '_DB_SERVER_') !== false) {
+                $tmp_settings = preg_replace('/(\'|")\_/', '$1_LEGACY_', $tmp_settings);
+                file_put_contents($root_dir.'/'.self::SETTINGS_FILE, $tmp_settings);
+                include $root_dir.'/'.self::SETTINGS_FILE;
 
-            $factory   = new RandomLib\Factory;
-            $generator = $factory->getLowStrengthGenerator();
-            $secret    = $generator->generateString(56);
+                $factory   = new RandomLib\Factory;
+                $generator = $factory->getLowStrengthGenerator();
+                $secret    = $generator->generateString(56);
 
-            $default_parameters = Yaml::parse($root_dir.'/app/config/parameters.yml.dist');
+                $default_parameters = Yaml::parse($root_dir.'/app/config/parameters.yml.dist');
 
-            $parameters = array(
-                'parameters' => array(
-                    'database_host'     => _LEGACY_DB_SERVER_,
-                    'database_port'     => '~',
-                    'database_user'     => _LEGACY_DB_USER_,
-                    'database_password' => _LEGACY_DB_PASSWD_,
-                    'database_name'     => _LEGACY_DB_NAME_,
-                    'database_prefix'   => _LEGACY_DB_PREFIX_,
-                    'database_engine'   => _LEGACY_MYSQL_ENGINE_,
-                    'cookie_key'        => _LEGACY_COOKIE_KEY_,
-                    'cookie_iv'         => _LEGACY_COOKIE_IV_,
-                    'ps_caching'        => _LEGACY_PS_CACHING_SYSTEM_,
-                    'ps_cache_enable'   => _LEGACY_PS_CACHE_ENABLED_,
-                    'ps_creation_date'  => _LEGACY_PS_CREATION_DATE_,
-                    'secret'            => $secret,
-                    'mailer_transport'  => 'smtp',
-                    'mailer_host'       => '127.0.0.1',
-                    'mailer_user'       => '~',
-                    'mailer_password'   => '~',
-                ) + $default_parameters['parameters']
-            );
+                $parameters = array(
+                    'parameters' => array(
+                        'database_host'     => _LEGACY_DB_SERVER_,
+                        'database_port'     => '~',
+                        'database_user'     => _LEGACY_DB_USER_,
+                        'database_password' => _LEGACY_DB_PASSWD_,
+                        'database_name'     => _LEGACY_DB_NAME_,
+                        'database_prefix'   => _LEGACY_DB_PREFIX_,
+                        'database_engine'   => _LEGACY_MYSQL_ENGINE_,
+                        'cookie_key'        => _LEGACY_COOKIE_KEY_,
+                        'cookie_iv'         => _LEGACY_COOKIE_IV_,
+                        'ps_caching'        => _LEGACY_PS_CACHING_SYSTEM_,
+                        'ps_cache_enable'   => _LEGACY_PS_CACHE_ENABLED_,
+                        'ps_creation_date'  => _LEGACY_PS_CREATION_DATE_,
+                        'secret'            => $secret,
+                        'mailer_transport'  => 'smtp',
+                        'mailer_host'       => '127.0.0.1',
+                        'mailer_user'       => '~',
+                        'mailer_password'   => '~',
+                    ) + $default_parameters['parameters']
+                );
 
-            if (file_put_contents($root_dir.'/app/config/parameters.yml', Yaml::dump($parameters))) {
-                $settings_content = "<?php\n";
-                $settings_content .= "//@deprecated 1.7";
+                if (file_put_contents($root_dir.'/app/config/parameters.yml', Yaml::dump($parameters))) {
+                    $settings_content = "<?php\n";
+                    $settings_content .= "//@deprecated 1.7";
 
-                file_put_contents($root_dir.'/'.self::SETTINGS_FILE, $settings_content);
+                    file_put_contents($root_dir.'/'.self::SETTINGS_FILE, $settings_content);
+                }
             }
         }
         if ($event !== null) {
