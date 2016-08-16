bf3acb83679c763dd56610e112cd5e4a213a5769
Julien Bourdeau <julien@sigerr.org>
2016-08-16 11:56:44 +0200

    FO: Fix $title var for 404 page

--- classes/controller/FrontController.php
+++ classes/controller/FrontController.php
@@ -1421,7 +1421,7 @@ class FrontControllerCore extends Controller
         $page_name = $this->getPageName();
         $meta_tags = Meta::getMetaTags($this->context->language->id, $page_name);
 
-        $my_account_controllers = [
+        $my_account_controllers = array(
             'address',
             'authentication',
             'discount',
@@ -1431,23 +1431,24 @@ class FrontControllerCore extends Controller
             'order-slip',
             'password',
             'guest-tracking',
-        ];
+        );
 
-        $body_classes = [
+        $body_classes = array(
             'lang-'.$this->context->language->iso_code => true,
-            'lang-rtl' => (bool)$this->context->language->is_rtl,
+            'lang-rtl' => (bool) $this->context->language->is_rtl,
             'country-'.$this->context->country->iso_code => true,
             'currency-'.$this->context->currency->iso_code => true,
             $this->context->shop->theme->getLayoutNameForPage($this->php_self) => true,
             'page-'.$this->php_self => true,
             'tax-display-'.($this->getDisplayTaxesLabel() ? 'enabled' : 'disabled') => true,
-        ];
+        );
 
         if (in_array($this->php_self, $my_account_controllers)) {
             $body_classes['page-customer-account'] = true;
         }
 
-        $page = [
+        $page = array(
+            'title' => '',
             'canonical' => $this->getCanonicalURL(),
             'meta' => array(
                 'title' => $meta_tags['meta_title'],
@@ -1457,7 +1458,7 @@ class FrontControllerCore extends Controller
             'page_name' => $page_name,
             'body_classes' => $body_classes,
             'admin_notifications' => [],
-        ];
+        );
 
         return $page;
     }
