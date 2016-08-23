21e50472a895ff79a5b901c706f7a8475e331ed8
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2016-08-23 10:10:52 +0200

    BO: Reformat code

--- admin-dev/themes/new-theme/js/translation-page/messages-search.js
+++ admin-dev/themes/new-theme/js/translation-page/messages-search.js
@@ -1,45 +1,45 @@
 import Jets from 'jets/jets';
 import $ from 'jquery';
 
-export default function() {
-    $(() => {
-        const searchSelector = '.search-translation';
-        $(searchSelector + ' form').submit(function (event) {
-            $('#jetsContent form').addClass('hide');
+export default function () {
+  $(() => {
+    const searchSelector = '.search-translation';
+    $(searchSelector + ' form').submit(function (event) {
+      $('#jetsContent form').addClass('hide');
 
-            const keywords = $('#jetsSearch').val().toLowerCase();
-            const jetsSelector = '#jetsContent > [data-jets*="' + keywords + '"]';
+      const keywords = $('#jetsSearch').val().toLowerCase();
+      const jetsSelector = '#jetsContent > [data-jets*="' + keywords + '"]';
 
-            if (0 === $(jetsSelector).length) {
-                var notificationElement = $(searchSelector + '> .alert')[0];
-                $(notificationElement).removeClass('hide');
-                setTimeout(function () {
-                    $(notificationElement).addClass('hide');
-                }, 2000);
-            } else {
-                $(jetsSelector).removeClass('hide');
-            }
+      if (0 === $(jetsSelector).length) {
+        var notificationElement = $(searchSelector + '> .alert')[0];
+        $(notificationElement).removeClass('hide');
+        setTimeout(function () {
+          $(notificationElement).addClass('hide');
+        }, 2000);
+      } else {
+        $(jetsSelector).removeClass('hide');
+      }
 
-            event.preventDefault();
+      event.preventDefault();
 
-            return false;
-        });
-
-        $(searchSelector + ' input[type=reset]').click(function () {
-            $('#jetsSearch').val('');
-            $('#jetsContent form').addClass('hide');
-        })
+      return false;
     });
 
-    if ($('#jetsSearch').length > 0) {
-        return new Jets({
-            searchTag: '#jetsSearch',
-            contentTag: '#jetsContent',
-            callSearchManually: true,
-            manualContentHandling: function (tag) {
-                // Search for translation keys and translation values
-                return $(tag).find('verbatim')[0].innerText + $(tag).find('textarea')[0].value;
-            }
-        });
-    }
+    $(searchSelector + ' input[type=reset]').click(function () {
+      $('#jetsSearch').val('');
+      $('#jetsContent form').addClass('hide');
+    })
+  });
+
+  if ($('#jetsSearch').length > 0) {
+    return new Jets({
+      searchTag: '#jetsSearch',
+      contentTag: '#jetsContent',
+      callSearchManually: true,
+      manualContentHandling: function (tag) {
+        // Search for translation keys and translation values
+        return $(tag).find('verbatim')[0].innerText + $(tag).find('textarea')[0].value;
+      }
+    });
+  }
 }
