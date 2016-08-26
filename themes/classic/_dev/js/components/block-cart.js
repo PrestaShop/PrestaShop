f360c89a08493fcc8608d916d2f0016edc6430f2
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2016-08-26 11:18:22 +0200

    FO: Rename block cart event

--- themes/classic/_dev/js/components/block-cart.js
+++ themes/classic/_dev/js/components/block-cart.js
@@ -2,15 +2,24 @@ import prestashop from 'prestashop';
 import $ from 'jquery';
 
 prestashop.blockcart = prestashop.blockcart || {};
-prestashop.blockcart.showModal = html => {
-  let $modal = $('#blockcart-modal');
-  if($modal.length){
-    $modal.remove();
+
+prestashop.blockcart.showModal = (html) => {
+  function getBlockCartModal() {
+    return $('#blockcart-modal');
   }
+
+  let $blockCartModal = getBlockCartModal();
+  if ($blockCartModal.length){
+    $blockCartModal.remove();
+  }
+
   $('body').append(html);
-  $('#blockcart-modal').modal('show').on('hidden.bs.modal', function (e) {
-    prestashop.emit('product updated', {
-      reason: e.currentTarget.dataset
+
+  $blockCartModal = getBlockCartModal();
+  $blockCartModal.modal('show').on('hidden.bs.modal', (event) => {
+    prestashop.emit('updateProduct', {
+      reason: event.currentTarget.dataset
     });
   });
 };
+
