ac143166fd0ac5f898f2587a602e2969e168f44c
Michael Dekker <git@michaeldekker.nl>
2016-08-27 00:18:08 +0200

    CO: Remove contrib folder

--- .github/contrib/install.php
+++ .github/contrib/install.php
@@ -5,8 +5,8 @@ if (file_exists('.git/hooks/pre-commit')) {
     echo "A Git precommit file was found, a backup was created.\n";
 }
 
-if (file_exists('tools/contrib/pre-commit')) {
-    copy('tools/contrib/pre-commit', '.git/hooks/pre-commit');
+if (file_exists('pre-commit')) {
+    copy('pre-commit', '.git/hooks/pre-commit');
     chmod('.git/hooks/pre-commit', 0750);
     echo "\e[32mPre-commit Hook has been installed.\e[0;m\n";
 }
