bb2c03094f01e64c43e4aeb81289606bbecd28ce
Michael Dekker <git@michaeldekker.nl>
2016-08-27 00:35:56 +0200

    CO: Remove SwiftMailerBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -12,7 +12,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             // PrestaShop Core bundle
