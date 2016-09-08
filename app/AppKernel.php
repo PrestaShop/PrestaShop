a668ae2a09d833c0374608207c5acd689887d184
Mickaël Andrieu <andrieu.travail@gmail.com>
2016-09-08 10:34:58 +0200

    BO: fixed debug mode
    (cherry picked from commit ff8517aa882df493214256733e4ff5cd311eab9e)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -27,7 +27,6 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
 
         if (extension_loaded('apc')) {
