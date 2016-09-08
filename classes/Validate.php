7d0f9ffb0bf5e229a85cdbb7113ab7e83fb07060
Alex Sampaio <alex.sampaio1991@gmail.com>
2016-09-08 17:15:54 +0200

    BO: Improve validator birthday date

--- classes/Validate.php
+++ classes/Validate.php
@@ -524,9 +524,11 @@ class ValidateCore
             return true;
         }
         if (preg_match('/^([0-9]{4})-((?:0?[1-9])|(?:1[0-2]))-((?:0?[1-9])|(?:[1-2][0-9])|(?:3[01]))([0-9]{2}:[0-9]{2}:[0-9]{2})?$/', $date, $birth_date)) {
-            if ($birth_date[1] > date('Y') && $birth_date[2] > date('m') && $birth_date[3] > date('d')
-                || $birth_date[1] == date('Y') && $birth_date[2] == date('m') && $birth_date[3] > date('d')
-                || $birth_date[1] == date('Y') && $birth_date[2] > date('m')) {
+            if ( $birth_date[1] > date('Y')
+                || ($birth_date[1] > date('Y') && $birth_date[2] > date('m'))
+                || ($birth_date[1] > date('Y') && $birth_date[2] > date('m') && $birth_date[3] > date('d'))
+                || ($birth_date[1] == date('Y') && $birth_date[2] == date('m') && $birth_date[3] > date('d'))
+                || ($birth_date[1] == date('Y') && $birth_date[2] > date('m')) ) {
                 return false;
             }
             return true;
