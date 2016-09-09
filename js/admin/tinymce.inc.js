1d6dc1af70f8623e7f6ef2560ff9df0a482208e1
fatma.bouchekoua <fatma.bouchekoua@prestashop.com>
2016-09-09 11:00:13 +0200

    BO: Add new plugins and options to tinymce

--- js/admin/tinymce.inc.js
+++ js/admin/tinymce.inc.js
@@ -1,22 +1,32 @@
-function getTinyMaterialIconsAssoc()
-{
-    return {
-        'mce-i-code': '<i class="material-icons">code</i>',
-        'mce-i-none': '<i class="material-icons">format_color_text</i>',
-        'mce-i-bold': '<i class="material-icons">format_bold</i>',
-        'mce-i-italic': '<i class="material-icons">format_italic</i>',
-        'mce-i-underline': '<i class="material-icons">format_underlined</i>',
-        'mce-i-strikethrough': '<i class="material-icons">format_strikethrough</i>',
-        'mce-i-blockquote': '<i class="material-icons">format_quote</i>',
-        'mce-i-link': '<i class="material-icons">link</i>',
-        'mce-i-alignleft': '<i class="material-icons">format_align_left</i>',
-        'mce-i-aligncenter': '<i class="material-icons">format_align_center</i>',
-        'mce-i-alignright': '<i class="material-icons">format_align_right</i>',
-        'mce-i-alignjustify': '<i class="material-icons">format_align_justify</i>',
-        'mce-i-bullist': '<i class="material-icons">format_list_bulleted</i>',
-        'mce-i-numlist': '<i class="material-icons">format_list_numbered</i>',
-        'mce-i-image': '<i class="material-icons">image</i>'
-    }
+/**
+ * Change default icons to marerial icons
+ */
+function changeToMaterial() {
+  var materialIconAssoc = {
+    'mce-i-code': '<i class="material-icons">code</i>',
+    'mce-i-none': '<i class="material-icons">format_color_text</i>',
+    'mce-i-bold': '<i class="material-icons">format_bold</i>',
+    'mce-i-italic': '<i class="material-icons">format_italic</i>',
+    'mce-i-underline': '<i class="material-icons">format_underlined</i>',
+    'mce-i-strikethrough': '<i class="material-icons">format_strikethrough</i>',
+    'mce-i-blockquote': '<i class="material-icons">format_quote</i>',
+    'mce-i-link': '<i class="material-icons">link</i>',
+    'mce-i-alignleft': '<i class="material-icons">format_align_left</i>',
+    'mce-i-aligncenter': '<i class="material-icons">format_align_center</i>',
+    'mce-i-alignright': '<i class="material-icons">format_align_right</i>',
+    'mce-i-alignjustify': '<i class="material-icons">format_align_justify</i>',
+    'mce-i-bullist': '<i class="material-icons">format_list_bulleted</i>',
+    'mce-i-numlist': '<i class="material-icons">format_list_numbered</i>',
+    'mce-i-image': '<i class="material-icons">image</i>',
+    'mce-i-table': '<i class="material-icons">grid_on</i>',
+    'mce-i-media': '<i class="material-icons">video_library</i>',
+    'mce-i-browse': '<i class="material-icons">attachment</i>',
+    'mce-i-checkbox': '<i class="mce-ico mce-i-checkbox"></i>',
+  };
+
+  $.each(materialIconAssoc, function(index, value){
+    $('.' + index).replaceWith(value);
+  });
 }
 
 function tinySetup(config)
@@ -33,16 +43,16 @@ function tinySetup(config)
 
 	var default_config = {
 		selector: ".rte" ,
-		plugins : "colorpicker link image filemanager table media placeholder",
+		plugins : "align colorpicker link image filemanager table media placeholder advlist code table media",
 		browser_spellcheck : true,
-		toolbar1 : "colorpicker,bold,italic,underline,strikethrough,blockquote,link,alignleft,aligncenter,alignright,alignjustify,bullist,numlist,image",
+		toolbar1 : "code,colorpicker,bold,italic,underline,strikethrough,blockquote,link,align,bullist,numlist,table,image,media",
 		toolbar2: "",
 		external_filemanager_path: baseAdminDir+"filemanager/",
 		filemanager_title: "File manager" ,
 		external_plugins: { "filemanager" : baseAdminDir+"filemanager/plugin.min.js"},
 		language: iso_user,
 		skin: "prestashop",
-        menubar:false,
+    menubar:false,
 		statusbar: false,
 		relative_urls : false,
 		convert_urls: false,
@@ -50,32 +60,7 @@ function tinySetup(config)
 		extended_valid_elements : "em[class|name|id],@[role|data-*|aria-*]",
 		valid_children : "+*[*]",
 		valid_elements:"*[*]",
-
-        init_instance_callback : function(editor) {
-            var editorDom = editor.editorContainer;
-
-            if (editorDom) {
-                var jQButtonSelector = '.mce-toolbar:not(.mce-menubar) > .mce-container-body > .mce-container > div > .mce-widget > button > i';
-                var editorButtonsIcons = $(editorDom).find(jQButtonSelector);
-
-                if (editorButtonsIcons) {
-
-                    var materialIconAssoc = getTinyMaterialIconsAssoc();
-
-                    $.each(editorButtonsIcons, function(index, value){
-                        // Clean extra class on object to keep the only one we need
-                        $(this).removeClass('mce-ico');
-                        var tinyIcoClass = $(this).attr('class');
-
-                        if (typeof materialIconAssoc[tinyIcoClass] != 'undefined') {
-                            $(this).replaceWith(materialIconAssoc[tinyIcoClass]);
-                        }
-
-                    });
-
-                }
-            }
-        }
+    init_instance_callback : "changeToMaterial"
 	};
 
 	$.each(default_config, function(index, el)
@@ -84,5 +69,10 @@ function tinySetup(config)
 			config[index] = el;
 	});
 
-	tinyMCE.init(config);
+  // Change icons in popups
+  $('body').on('click', '.mce-btn, .mce-open, .mce-menu-item', function() {
+    changeToMaterial();
+  });
+
+  tinyMCE.init(config);
 }
