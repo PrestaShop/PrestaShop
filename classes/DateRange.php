d072fa5412a48e976f6d9cbd89a83ed246bfd19a
Michael Dekker <git@michaeldekker.nl>
2016-09-12 00:46:45 +0200

    CO: Doc + cleanup DateRange class

--- classes/DateRange.php
+++ classes/DateRange.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 2007-2015 PrestaShop
+ * 2007-2016 PrestaShop
  *
  * NOTICE OF LICENSE
  *
@@ -19,14 +19,20 @@
  * needs please refer to http://www.prestashop.com for more information.
  *
  * @author    PrestaShop SA <contact@prestashop.com>
- * @copyright 2007-2015 PrestaShop SA
+ * @copyright 2007-2016 PrestaShop SA
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
 
+/**
+ * Class DateRangeCore
+ */
 class DateRangeCore extends ObjectModel
 {
+    /** @var string $time_start */
     public $time_start;
+
+    /** @var string $time_end */
     public $time_end;
 
     /**
@@ -37,10 +43,15 @@ class DateRangeCore extends ObjectModel
         'primary' => 'id_date_range',
         'fields' => array(
             'time_start' => array('type' => self::TYPE_DATE, 'validate' => 'isDate', 'required' => true),
-            'time_end' =>    array('type' => self::TYPE_DATE, 'validate' => 'isDate', 'required' => true),
+            'time_end' => array('type' => self::TYPE_DATE, 'validate' => 'isDate', 'required' => true),
         ),
     );
 
+    /**
+     * Get current range
+     *
+     * @return mixed
+     */
     public static function getCurrentRange()
     {
         $result = Db::getInstance()->getRow('
@@ -54,8 +65,10 @@ class DateRangeCore extends ObjectModel
             $dateRange->time_start = date('Y-m-d');
             $dateRange->time_end = strftime('%Y-%m-%d %H:%M:%S', strtotime($dateRange->time_start) + $rangeSize);
             $dateRange->add();
+
             return $dateRange->id;
         }
+
         return $result['id_date_range'];
     }
 }
