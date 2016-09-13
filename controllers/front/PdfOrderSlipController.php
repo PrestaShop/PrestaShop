f498e5886816cf0ddab01f14ab806051af87ae4a
Julien Bourdeau <julien@sigerr.org>
2016-09-13 17:10:39 +0200

    Revert FO: Restore smarty legacy function for PDF
    This reverts commit b0b8f5b4ca4c4712292700ce63cd221071dfddbd.

--- controllers/front/PdfOrderSlipController.php
+++ controllers/front/PdfOrderSlipController.php
@@ -23,7 +23,7 @@
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
-class PdfOrderSlipControllerCore extends PdfFrontController
+class PdfOrderSlipControllerCore extends FrontController
 {
     public $php_self = 'pdf-order-slip';
     protected $display_header = false;
