06301985e46dffea1f2018ea4da5ba362f3149e3
Julien Bourdeau <julien@sigerr.org>
2016-09-26 11:41:16 +0200

    FO: Fix smarty module resources paths arg

--- classes/Smarty/SmartyResourceModule.php
+++ classes/Smarty/SmartyResourceModule.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 2007-2015 PrestaShop
+ * 2007-2015 PrestaShop.
  *
  * NOTICE OF LICENSE
  *
@@ -25,37 +25,34 @@
  */
 
 /**
- * Override module templates easily
+ * Override module templates easily.
  *
  * @since 1.7.0.0
  */
 class SmartyResourceModuleCore extends Smarty_Resource_Custom
 {
-    private $shop;
-
     public function __construct(array $paths)
     {
         $this->paths = $paths;
     }
 
     /**
-     * Fetch a template
+     * Fetch a template.
      *
-     * @param string $name template name
+     * @param string $name   template name
      * @param string $source template source
-     * @param integer $mtime template modification timestamp (epoch)
-     * @return void
+     * @param int    $mtime  template modification timestamp (epoch)
      */
     protected function fetch($name, &$source, &$mtime)
     {
         foreach ($this->paths as $path) {
             if (Tools::file_exists_cache($file = $path.$name)) {
                 if (_PS_MODE_DEV_) {
-                    $source = implode('', [
+                    $source = implode('', array(
                         '<!-- begin '.$file.' -->',
                         file_get_contents($file),
-                        '<!-- end '.$file.' -->'
-                    ]);
+                        '<!-- end '.$file.' -->',
+                    ));
                 } else {
                     $source = file_get_contents($file);
                 }
