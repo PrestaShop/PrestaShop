0fcfb44e3fa9100056d583feeb9368ac70ea2a62
Turtle <robin.rozo@gmail.com>
2016-09-28 15:17:48 +0200

    BO: Fixed rendering condition of default groups form in AdminGroupsController

--- controllers/admin/AdminGroupsController.php
+++ controllers/admin/AdminGroupsController.php
@@ -101,7 +101,7 @@ class AdminGroupsControllerCore extends AdminController
         $this->_use_found_rows = false;
 
         $groups = Group::getGroups(Context::getContext()->language->id, true);
-        if (Shop::isFeatureActive()) {
+        if (Group::isFeatureActive()) {
             $this->fields_options = array(
                 'general' => array(
                     'title' =>    $this->l('Default groups options'),
