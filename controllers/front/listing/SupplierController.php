2910f55f5641281e55ca5c4deec0046fe4343d78
Alex Even <alexandra.even@prestashop.com>
2016-09-29 18:30:53 +0200

    FO: fix some wording on FO

--- controllers/front/listing/SupplierController.php
+++ controllers/front/listing/SupplierController.php
@@ -147,8 +147,8 @@ class SupplierControllerCore extends ProductListingFrontController
             $suppliers_for_display[$supplier['id_supplier']]['image'] = $this->context->link->getSupplierImageLink($supplier['id_supplier'], 'small_default');
             $suppliers_for_display[$supplier['id_supplier']]['url'] = $this->context->link->getsupplierLink($supplier['id_supplier']);
             $suppliers_for_display[$supplier['id_supplier']]['nb_products'] = $supplier['nb_products'] > 1
-                ? $this->trans('%N% products', array('%N%' => $supplier['nb_products']), 'Shop.Theme.Catalog')
-                : $this->trans('%N% product', array('%N%' => $supplier['nb_products']), 'Shop.Theme.Catalog');
+                ? $this->trans('%number% products', array('%number%' => $supplier['nb_products']), 'Shop.Theme.Catalog')
+                : $this->trans('%number% product', array('%number%' => $supplier['nb_products']), 'Shop.Theme.Catalog');
         }
 
         return $suppliers_for_display;
