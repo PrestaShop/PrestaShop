7f1189d9b9b1c7f20c08b1dda8993deece15040f
Michael Dekker <git@michaeldekker.nl>
2016-10-03 03:24:00 +0200

    CO: Doc + update TranslatedConfiguration class

--- classes/TranslatedConfiguration.php
+++ classes/TranslatedConfiguration.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 2007-2015 PrestaShop
+ * 2007-2016 PrestaShop
  *
  * NOTICE OF LICENSE
  *
@@ -19,11 +19,14 @@
  * needs please refer to http://www.prestashop.com for more information.
  *
  * @author    PrestaShop SA <contact@prestashop.com>
- * @copyright 2007-2015 PrestaShop SA
+ * @copyright 2007-2016 PrestaShop SA
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
 
+/**
+ * Class TranslatedConfigurationCore
+ */
 class TranslatedConfigurationCore extends Configuration
 {
     protected $webserviceParameters = array(
@@ -41,72 +44,98 @@ class TranslatedConfigurationCore extends Configuration
         'primary' => 'id_configuration',
         'multilang' => true,
         'fields' => array(
-            'name' =>            array('type' => self::TYPE_STRING, 'validate' => 'isConfigName', 'required' => true, 'size' => 32),
-            'id_shop_group' =>    array('type' => self::TYPE_NOTHING, 'validate' => 'isUnsignedId'),
-            'id_shop' =>        array('type' => self::TYPE_NOTHING, 'validate' => 'isUnsignedId'),
-            'value' =>            array('type' => self::TYPE_STRING, 'lang' => true),
-            'date_add' =>        array('type' => self::TYPE_DATE, 'validate' => 'isDate'),
-            'date_upd' =>        array('type' => self::TYPE_DATE, 'validate' => 'isDate'),
+            'name' => array('type' => self::TYPE_STRING, 'validate' => 'isConfigName', 'required' => true, 'size' => 32),
+            'id_shop_group' => array('type' => self::TYPE_NOTHING, 'validate' => 'isUnsignedId'),
+            'id_shop' => array('type' => self::TYPE_NOTHING, 'validate' => 'isUnsignedId'),
+            'value' => array('type' => self::TYPE_STRING, 'lang' => true),
+            'date_add' => array('type' => self::TYPE_DATE, 'validate' => 'isDate'),
+            'date_upd' => array('type' => self::TYPE_DATE, 'validate' => 'isDate'),
         ),
     );
 
-    public function __construct($id = null, $id_lang = null)
+    /**
+     * TranslatedConfigurationCore constructor.
+     *
+     * @param null $id
+     * @param null $idLang
+     */
+    public function __construct($id = null, $idLang = null)
     {
         $this->def = ObjectModel::getDefinition($this);
         // Check if the id configuration is set in the configuration_lang table.
         // Otherwise configuration is not set as translated configuration.
         if ($id !== null) {
-            $id_translated = Db::getInstance()->executeS('				SELECT `'.bqSQL($this->def['primary']).'`
+            $idTranslated = Db::getInstance()->executeS('				SELECT `'.bqSQL($this->def['primary']).'`
 				FROM `'.bqSQL(_DB_PREFIX_.$this->def['table']).'_lang`
 				WHERE `'.bqSQL($this->def['primary']).'`='.(int)$id.' LIMIT 0,1
 			');
 
-            if (empty($id_translated)) {
+            if (empty($idTranslated)) {
                 $id = null;
             }
         }
-        parent::__construct($id, $id_lang);
+        parent::__construct($id, $idLang);
     }
 
-    public function add($autodate = true, $nullValues = false)
+    /**
+     * @param bool $autoDate
+     * @param bool $nullValues
+     *
+     * @return bool
+     */
+    public function add($autoDate = true, $nullValues = false)
     {
         return $this->update($nullValues);
     }
 
+    /**
+     * @param bool $nullValues
+     *
+     * @return bool
+     */
     public function update($nullValues = false)
     {
         $ishtml = false;
-        foreach ($this->value as $i18n_value) {
-            if (Validate::isCleanHtml($i18n_value)) {
+        foreach ($this->value as $i18NValue) {
+            if (Validate::isCleanHtml($i18NValue)) {
                 $ishtml = true;
                 break;
             }
         }
         Configuration::updateValue($this->name, $this->value, $ishtml);
 
-        $last_insert = Db::getInstance()->getRow('
+        $lastInsert = Db::getInstance()->getRow('
 			SELECT `id_configuration` AS id
 			FROM `'._DB_PREFIX_.'configuration`
 			WHERE `name` = \''.pSQL($this->name).'\'');
-        if ($last_insert) {
-            $this->id = $last_insert['id'];
+        if ($lastInsert) {
+            $this->id = $lastInsert['id'];
         }
 
         return true;
     }
 
-    public function getWebserviceObjectList($sql_join, $sql_filter, $sql_sort, $sql_limit)
+    /**
+     * @param string $sqlJoin
+     * @param string $sqlFilter
+     * @param string $sqlSort
+     * @param string $sqlLimit
+     *
+     * @return array|false|mysqli_result|null|PDOStatement|resource
+     */
+    public function getWebserviceObjectList($sqlJoin, $sqlFilter, $sqlSort, $sqlLimit)
     {
         $query = '
 		SELECT DISTINCT main.`'.$this->def['primary'].'` FROM `'._DB_PREFIX_.$this->def['table'].'` main
-		'.$sql_join.'
+		'.$sqlJoin.'
 		WHERE id_configuration IN
 		(	SELECT id_configuration
 			FROM '._DB_PREFIX_.$this->def['table'].'_lang
-		) '.$sql_filter.'
-		'.($sql_sort != '' ? $sql_sort : '').'
-		'.($sql_limit != '' ? $sql_limit : '').'
+		) '.$sqlFilter.'
+		'.($sqlSort != '' ? $sqlSort : '').'
+		'.($sqlLimit != '' ? $sqlLimit : '').'
 		';
+
         return Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($query);
     }
 }
