84fe4702b808a81cd132c3d12d76bd6019be8624
Alex Sampaio <alex.sampaio1991@gmail.com>
2016-10-07 13:25:39 +0200

    BO: trad for ajaxProcessDeleteProductAttribute

--- src/PrestaShopBundle/Controller/Admin/AttributeController.php
+++ src/PrestaShopBundle/Controller/Admin/AttributeController.php
@@ -188,7 +188,6 @@ class AttributeController extends FrameworkBundleAdminController
      */
     public function deleteAttributeAction($idProduct, Request $request)
     {
-        $translator = $this->container->get('translator');
         $response = new JsonResponse();
 
         if (!$request->isXmlHttpRequest()) {
@@ -206,7 +205,7 @@ class AttributeController extends FrameworkBundleAdminController
                 $response->setStatusCode(400);
             }
 
-            $response->setData(['message' => $translator->trans($legacyResponse['message'], [], 'AdminProducts')]);
+            $response->setData(['message' => $legacyResponse['message']]);
         }
 
 
@@ -223,7 +222,6 @@ class AttributeController extends FrameworkBundleAdminController
      */
     public function deleteAllAttributeAction($idProduct, Request $request)
     {
-        $translator = $this->container->get('translator');
         $attributeAdapter = $this->container->get('prestashop.adapter.data_provider.attribute');
         $response = new JsonResponse();
 
@@ -244,7 +242,7 @@ class AttributeController extends FrameworkBundleAdminController
             }
         }
 
-        $response->setData(['message' => $translator->trans($res['message'], [], 'AdminProducts')]);
+        $response->setData(['message' => $res['message']]);
 
         return $response;
     }
