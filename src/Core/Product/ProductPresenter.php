fbb7cd8754d7e78a4e796b12d41d3111d0fa8724
Maxime Biloé <maxime.biloe@gmail.com>
2016-10-21 10:54:41 +0200

    FO: Properly translate isbn, ean and upc

--- src/Core/Product/ProductPresenter.php
+++ src/Core/Product/ProductPresenter.php
@@ -479,10 +479,10 @@ class ProductPresenter
         //if the attribute's references doesn't exist then get the product's references or unset it
         foreach ($presentedProduct['specific_references'] as $key => $value) {
             if (empty($value)) {
+                $translatedKey = $this->getTranslatedKey($key);
+                unset($presentedProduct['specific_references'][$key]);
                 if (!empty($product[$key])) {
-                    $presentedProduct['specific_references'][$key] = $product[$key];
-                } else {
-                    unset($presentedProduct['specific_references'][$key]);
+                    $presentedProduct['specific_references'][$translatedKey] = $product[$key];
                 }
             }
         }
@@ -595,5 +595,19 @@ class ProductPresenter
             );
         }
         return $presentedProduct;
+    }
+
+    private function getTranslatedKey($key)
+    {
+        switch ($key) {
+            case 'ean13':
+                return $this->translator->trans('ean13', array(), 'Shop.Theme.Catalog');
+            case 'isbn':
+                return $this->translator->trans('isbn', array(), 'Shop.Theme.Catalog');
+            case 'upc':
+                return $this->translator->trans('upc', array(), 'Shop.Theme.Catalog');
+        }
+
+        return $key;
     }
 }
