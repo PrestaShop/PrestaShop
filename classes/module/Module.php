ea37d7ed47d27e8bc99e07d193a104a317cab878
Mickaël Andrieu <andrieu.travail@gmail.com>
2016-11-24 18:16:15 +0100

    BO: fixed XSS issue on translation module page

--- classes/module/Module.php
+++ classes/module/Module.php
@@ -3118,6 +3118,7 @@ abstract class ModuleCore
 
     protected function trans($id, array $parameters = array(), $domain = null, $locale = null)
     {
+        $parameters['legacy'] = 'htmlspecialchars';
         return $this->getTranslator()->trans($id, $parameters, $domain, $locale);
     }
 }
