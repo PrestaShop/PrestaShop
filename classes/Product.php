1194d8505d36b1570ad3fda19e26c9e342f94ae6
Maxime Biloé <maxime.biloe@prestashop.com>
2016-12-14 10:07:31 +0100

    CO: Remove unset alias

--- classes/Product.php
+++ classes/Product.php
@@ -4112,7 +4112,7 @@ class ProductCore extends ObjectModel
         if (($customization_labels = Db::getInstance()->executeS('
 			SELECT `id_customization_field`, `id_lang`, `id_shop`, `name`
 			FROM `'._DB_PREFIX_.'customization_field_lang`
-			WHERE `id_customization_field` IN ('.implode(', ', $customization_field_ids).')'.($id_shop ? ' AND cfl.`id_shop` = '.$id_shop : '').'
+			WHERE `id_customization_field` IN ('.implode(', ', $customization_field_ids).')'.($id_shop ? ' AND `id_shop` = '.$id_shop : '').'
 			ORDER BY `id_customization_field`')) === false) {
             return false;
         }
