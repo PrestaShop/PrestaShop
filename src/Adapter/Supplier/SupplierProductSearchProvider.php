630ce4b29c7f6f9e9998ff33ea4c914c95905908
Alex Sampaio <alex.sampaio1991@gmail.com>
2016-12-27 17:53:59 +0100

    CO: Fix error page when supplier/manufacturer doesn t have product

--- src/Adapter/Supplier/SupplierProductSearchProvider.php
+++ src/Adapter/Supplier/SupplierProductSearchProvider.php
@@ -74,14 +74,16 @@ class SupplierProductSearchProvider implements ProductSearchProviderInterface
         $count = $this->getProductsOrCount($context, $query, 'count');
 
         $result = new ProductSearchResult();
-        $result
-            ->setProducts($products)
-            ->setTotalProductsCount($count)
-        ;
 
-        $result->setAvailableSortOrders(
-            $this->sortOrderFactory->getDefaultSortOrders()
-        );
+        if (!empty($products)) {
+            $result
+                ->setProducts($products)
+                ->setTotalProductsCount($count);
+
+            $result->setAvailableSortOrders(
+                $this->sortOrderFactory->getDefaultSortOrders()
+            );
+        }
 
         return $result;
     }
