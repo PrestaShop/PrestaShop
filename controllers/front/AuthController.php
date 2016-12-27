cee1f0a04fb96ee7251c06ed7bf89258def984b3
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2016-12-27 13:26:33 +0200

    FO: fixed bug when logged in customer can access login/registration pages

--- controllers/front/AuthController.php
+++ controllers/front/AuthController.php
@@ -30,6 +30,16 @@ class AuthControllerCore extends FrontController
     public $php_self = 'authentication';
     public $auth = false;
 
+    public function checkAccess()
+    {
+        if ($this->context->customer->isLogged() && !$this->ajax) {
+            $this->redirect_after = ($this->authRedirection) ? urlencode($this->authRedirection) : 'my-account';
+            $this->redirect();
+        }
+
+        return parent::checkAccess();
+    }
+
     public function initContent()
     {
         parent::initContent();
