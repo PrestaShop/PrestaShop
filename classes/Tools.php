7a329e47c09807517e393a9862534caa1b65a7f5
mahdi <ramtin2025@yahoo.com>
2016-12-27 03:43:15 +0330

    Add ping function

--- classes/Tools.php
+++ classes/Tools.php
@@ -3852,3 +3852,10 @@ function cmpPriceDesc($a, $b)
     }
     return 0;
 }
+
+function ping($host,$port=80,$timeout=1)
+{
+    if ( ! @fsockopen($host, $port, $errno, $errstr, $timeout) )
+        return false;
+    return true;
+}
