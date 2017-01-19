91de9aff7465296e1cbb88b57a6f6a6d363c88fe
Alex Sampaio <alex.sampaio1991@gmail.com>
2017-01-19 10:32:03 +0100

    BO: update configurationLang

--- classes/lang/ConfigurationLang.php
+++ classes/lang/ConfigurationLang.php
@@ -36,11 +36,19 @@ class ConfigurationLangCore extends DataLangCore
     {
         $this->fieldNames = array(
             'value' => array(
-                md5("Dear Customer,\n\nRegards,\nCustomer service")
+                md5("Dear Customer,\n\nRegards,\nCustomer service") // PS_CUSTOMER_SERVICE_SIGNATURE
                     => $this->translator->trans("Dear Customer,\n\nRegards,\nCustomer service", array(), 'Admin.OrdersCustomers.Feature', $this->locale),
 
-                md5('We are currently updating our shop and will be back really soon.Thanks for your patience.')
-                    => $this->translator->trans('We are currently updating our shop and will be back really soon.Thanks for your patience.', array(), 'Admin.OrdersCustomers.Feature', $this->locale),
+                md5('We are currently updating our shop and will be back really soon.Thanks for your patience.') // PS_MAINTENANCE_TEXT
+                    => $this->translator->trans('We are currently updating our shop and will be back really soon.&lt;br&gt;Thanks for your patience.', array(), 'Admin.OrdersCustomers.Feature', $this->locale),
+
+                md5('#IN') => $this->translator->trans('#IN', array(), 'Admin.OrdersCustomers.Feature', $this->locale), // PS_INVOICE_PREFIX
+                md5('#DE') => $this->translator->trans('#DE', array(), 'Admin.OrdersCustomers.Feature', $this->locale), // PS_DELIVERY_PREFIX
+                md5('#RE') => $this->translator->trans('#RE', array(), 'Admin.OrdersCustomers.Feature', $this->locale), // PS_RETURN_PREFIX
+
+                // PS_RETURN_PREFIX - No translate word per word but adapting for your language
+                md5('a|about|above|after|again|against|all|am|an|and|any|are|aren|as|at|be|because|been|before|being|below|between|both|but|by|can|cannot|could|couldn|did|didn|do|does|doesn|doing|don|down|during|each|few|for|from|further|had|hadn|has|hasn|have|haven|having|he|ll|her|here|hers|herself|him|himself|his|how|ve|if|in|into|is|isn|it|its|itself|let|me|more|most|mustn|my|myself|no|nor|not|of|off|on|once|only|or|other|ought|our|ours|ourselves|out|over|own|same|shan|she|should|shouldn|so|some|such|than|that|the|their|theirs|them|themselves|then|there|these|they|re|this|those|through|to|too|under|until|up|very|was|wasn|we|were|weren|what|when|where|which|while|who|whom|why|with|won|would|wouldn|you|your|yours|yourself|yourselves')
+                    => $this->translator->trans('a|about|above|after|again|against|all|am|an|and|any|are|aren|as|at|be|because|been|before|being|below|between|both|but|by|can|cannot|could|couldn|did|didn|do|does|doesn|doing|don|down|during|each|few|for|from|further|had|hadn|has|hasn|have|haven|having|he|ll|her|here|hers|herself|him|himself|his|how|ve|if|in|into|is|isn|it|its|itself|let|me|more|most|mustn|my|myself|no|nor|not|of|off|on|once|only|or|other|ought|our|ours|ourselves|out|over|own|same|shan|she|should|shouldn|so|some|such|than|that|the|their|theirs|them|themselves|then|there|these|they|re|this|those|through|to|too|under|until|up|very|was|wasn|we|were|weren|what|when|where|which|while|who|whom|why|with|won|would|wouldn|you|your|yours|yourself|yourselves', array(), 'Admin.ShopParameters.Feature', $this->locale),
             ),
         );
     }
