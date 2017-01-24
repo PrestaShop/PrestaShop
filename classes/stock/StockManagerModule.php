81f5893836f62c6859cb07c06e75c31ca9cadbb1
Michel ANTOINE <michel.br.antoine@gmail.com>
2017-01-24 10:23:34 +0100

    CO: deprecate displayError in StockManageerModuleCore

--- classes/stock/StockManagerModule.php
+++ classes/stock/StockManagerModule.php
@@ -41,13 +41,13 @@ abstract class StockManagerModuleCore extends Module
         $class_file = _PS_MODULE_DIR_.'/'.$this->name.'/'.$this->stock_manager_class.'.php';
 
         if (!isset($this->stock_manager_class) || !file_exists($class_file)) {
-            die(sprintf(Tools::displayError('Incorrect Stock Manager class [%s]'), $this->stock_manager_class));
+            die($this->trans('Incorrect Stock Manager class [%s]', array($this->stock_manager_class), 'Admin.Catalog.Notification'));
         }
 
         require_once($class_file);
 
         if (!class_exists($this->stock_manager_class)) {
-            die(sprintf(Tools::displayError('Stock Manager class not found [%s]'), $this->stock_manager_class));
+            die($this->trans('Stock Manager class not found [%s]', array($this->stock_manager_class), 'Admin.Catalog.Notification'));
         }
 
         $class = $this->stock_manager_class;
