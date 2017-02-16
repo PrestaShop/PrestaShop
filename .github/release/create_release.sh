6c014004d29c84b526f84648c56630520a54f6c1
Julien Bourdeau <julien@sigerr.org>
2017-02-16 13:52:36 +0100

    CO: release - Remove files and directories

--- .github/release/create_release.sh
+++ .github/release/create_release.sh
@@ -17,3 +17,26 @@ cd $rootPath; composer install;
 cd $rootPath; cd themes/classic/_dev; npm update; npm run build;
 cd $rootPath; cd admin-dev/themes/new-theme; npm update; npm run build;
 cd $rootPath; cd admin-dev/themes/default; npm update; npm run build;
+
+
+cd $rootPath;
+
+find $rootPath -type d -name "node_modules" -exec rm -rf {} \;
+find $rootPath -type d -name ".svn" -exec rm -rf {} \;
+find $rootPath -type d -name "tests" -exec rm -rf {} \;
+
+find $rootPath -type f -name ".DS_Store" -exec rm -rf {} \;
+find $rootPath -type f -name "*.map" -exec rm -rf {} \;
+
+rm -f .gitignore
+rm -f .gitmodules
+rm -f .travis.yml
+
+rm -rf app/cache; mkdir app/cache;
+rm -rf app/logs; mkdir app/logs;
+
+read -p "Do you want to delete the .git directory? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    rm -rf .git
+fi
