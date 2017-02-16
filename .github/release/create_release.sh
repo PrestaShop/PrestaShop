de09e53f0bbb9fc7d5dbaa7ddb7fec13d0f5f8e3
Julien Bourdeau <julien@sigerr.org>
2017-02-16 12:58:26 +0100

    CO: release - Rebuild CSS and JS

--- .github/release/create_release.sh
+++ .github/release/create_release.sh
@@ -13,3 +13,7 @@ node download_cldr.js
 rootPath=$(realpath $(pwd)/../..)
 
 cd $rootPath; composer install;
+
+cd $rootPath; cd themes/classic/_dev; npm update; npm run build;
+cd $rootPath; cd admin-dev/themes/new-theme; npm update; npm run build;
+cd $rootPath; cd admin-dev/themes/default; npm update; npm run build;
