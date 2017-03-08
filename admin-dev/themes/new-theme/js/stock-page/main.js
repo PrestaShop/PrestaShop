1706f3543f7f05ad3dafa088c28f45e81bf7765d
nihco2 <nicolas.martin@prestashop.com>
2017-03-08 14:42:42 +0100

    BO: Display simple product name list

--- admin-dev/themes/new-theme/js/stock-page/main.js
+++ admin-dev/themes/new-theme/js/stock-page/main.js
@@ -1,10 +1,27 @@
 import Vue from 'vue';
+import VueResource from 'vue-resource';
 import app from './components/app';
-import store from './store'
+import store from './store/'
+
+Vue.use(VueResource);
 
 const stockApp = new Vue({
   store,
   el: '#stock-app',
   template: '<app/>',
-  components: { app }
+  components: { app },
+  methods:{
+     getStock: function(){
+       this.$http.get(data.apiUrl).then(function(response){
+         if(response.status == 200) {
+           this.$store.commit('addProducts', response.body);
+         }
+       }, function(error){
+           console.log(error.statusText);
+       });
+     }
+   },
+   mounted: function () {
+     this.getStock();
+   }
 });
