aa3ed4302245d39ef79170f91e30d4cd384a2451
nihco2 <nicolas.martin@prestashop.com>
2017-03-14 17:45:07 +0100

    BO: Update webpack babel-loader for vue

--- admin-dev/themes/new-theme/webpack.config.js
+++ admin-dev/themes/new-theme/webpack.config.js
@@ -92,6 +92,7 @@ let config = {
       },
       {
         test: /\.vue$/,
+<<<<<<< c04e25e36a419f686c55cdafb38fbd96a2e81b0e
         loader: 'vue-loader',
         options: {
           loaders: {
@@ -99,6 +100,16 @@ let config = {
           },
           postcss: [require('postcss-cssnext')()]
         }
+||||||| merged common ancestors
+        loader: 'vue-loader'
+=======
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            js: 'babel-loader?presets[]=es2015&presets[]=stage-2'
+          }
+        }
+>>>>>>> BO: Update webpack babel-loader for vue
       },
       {
         test: /\.css$/,
