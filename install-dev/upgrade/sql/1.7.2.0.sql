8825aa3388b709cdec668e6306a74c228a228b5b
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2017-03-15 11:43:05 +0100

    CO: Fix SQL migration for available stock

--- install-dev/upgrade/sql/1.7.2.0.sql
+++ install-dev/upgrade/sql/1.7.2.0.sql
@@ -1,5 +1,3 @@
-SET SESSION sql_mode = '';
-SET NAMES 'utf8';
 
-ALTER TABLE  `PREFIX_available` ADD  `physical_quantity` INT NOT NULL DEFAULT  '0' AFTER  `quantity`;
-ALTER TABLE  `PREFIX_available` ADD  `reserved_quantity` INT NOT NULL DEFAULT  '0' AFTER  `quantity`;
+ALTER TABLE  `PREFIX_stock_available` ADD  `physical_quantity` INT NOT NULL DEFAULT  '0' AFTER  `quantity`;
+ALTER TABLE  `PREFIX_stock_available` ADD  `reserved_quantity` INT NOT NULL DEFAULT  '0' AFTER  `quantity`;
