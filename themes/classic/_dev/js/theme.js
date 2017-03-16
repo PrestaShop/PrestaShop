52551e2bfbed093a7325c0b04d75f96c9744bcd6
Maxime Biloé <maxime.biloe@gmail.com>
2017-03-16 17:44:10 +0100

    FO: Upgrade Classic to Webpack 2

--- themes/classic/_dev/js/theme.js
+++ themes/classic/_dev/js/theme.js
@@ -22,12 +22,11 @@
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
-import 'expose?Tether!tether';
+import 'expose-loader?Tether!tether';
 import 'bootstrap/dist/js/bootstrap.min';
 import 'flexibility';
 import 'bootstrap-touchspin';
 
-import '../css/theme';
 import './responsive';
 import './checkout';
 import './customer';
