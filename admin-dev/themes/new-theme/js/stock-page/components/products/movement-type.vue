373f27349c6484ddd80b245b3b9eb4447c2a4a0c
nihco2 <nicolas.martin@prestashop.com>
2017-03-16 18:03:36 +0100

    BO: update stock quantity from stock app

--- admin-dev/themes/new-theme/js/stock-page/components/products/movement-type.vue
+++ admin-dev/themes/new-theme/js/stock-page/components/products/movement-type.vue
@@ -2,7 +2,7 @@
   <div class="col-md-4">
     <label>Movement type</label>
     <div class="movements">
-      <select data-toggle="select2" class="form-control m-r-1">
+      <select class="form-control m-r-1">
         <option selected>None</option>
         <option value="1">Supply Order</option>
         <option value="2">Inventory adjustment</option>
@@ -10,18 +10,32 @@
         <option value="4">Transfer</option>
         <option value="5">Product return</option>
       </select>
-      <button type="button" class="btn update-qty"><i class="material-icons">edit</i>APPLY NEW QUANTITY</button>
+      <button type="button" class="btn update-qty" :class="classObject" :disabled="disabled"><i class="material-icons">edit</i>APPLY NEW QUANTITY</button>
     </div>
   </div>
 </template>
 
+<script>
+  export default {
+    computed: {
+      disabled() {
+        return !this.$store.getters.hasQty;
+      },
+      classObject() {
+        return {
+          'btn-primary': !this.disabled
+        }
+      }
+    }
+  }
+</script>
 <style lang="sass?outputStyle=expanded" scoped>
   @import "~PrestaKit/scss/custom/_variables.scss";
   .movements {
     display: flex;
   }
   .update-qty {
-    background: $gray-light;
-    color: $white;
+    color: white;
+    transition: background-color 0.2s ease;
   }
 </style>
--- admin-dev/themes/new-theme/js/stock-page/components/products/product-line.vue
+++ admin-dev/themes/new-theme/js/stock-page/components/products/product-line.vue
@@ -12,26 +12,30 @@
       {{ product.supplier_name }}
     </td>
     <td class="text-xs-center p-r-1">
-      {{ product.product_available_quantity + product.product_reserved_quantity }}
+      {{ physical }}
+      <span class="qty-update" v-if="hasQty">
+        <i class="material-icons">trending_flat</i>
+        {{physicalQtyUpdated}}
+      </span>
     </td>
     <td class="text-xs-center">
       {{ product.product_reserved_quantity }}
     </td>
     <td class="text-xs-center">
       {{ product.product_available_quantity }}
+      <span class="qty-update" v-if="hasQty">
+        <i class="material-icons">trending_flat</i>
+        {{availableQtyUpdated}}
+      </span>
     </td>
-    <td class="text-xs-center">
-      <div class="qty" :class="classObject" >
-        <Spinner :productId="product.product_id" v-on:focusIn="toggleCheck" v-on:focusOut="toggleCheck" v-on:enabled="toggleEnabled"/>
-        <button class="check-button" v-on:click="sendQty(product)"><i class="material-icons">check</i></button>
-      </div>
+    <td>
+      <Spinner :product="product" v-on:valueChanged="onValueChanged" />
     </td>
   </tr>
 </template>
 
 <script>
   import Spinner from './spinner';
-  import { mapActions } from 'vuex';
 
   export default {
     props: ['product'],
@@ -39,48 +43,31 @@
       imagePath() {
         return `${data.baseUrl}/${this.product.image_thumbnail_path}`;
       },
-      classObject() {
-        return {
-          active: this.isActive,
-          disabled: !this.isEnabled
-        }
+      hasQty() {
+        return (this.value !== 0);
+      },
+      physicalQtyUpdated () {
+        return this.physical + this.value;
+      },
+      availableQtyUpdated() {
+        return this.product.product_available_quantity + this.value;
+      },
+      physical() {
+        return this.product.product_available_quantity + this.product.product_reserved_quantity;
+      }
+    },
+    data() {
+      return {
+        value: 0
+      }
+    },
+    methods: {
+      onValueChanged(val) {
+        this.value = val;
       }
     },
     components: {
       Spinner
-    },
-    data() {
-      return {
-        isActive: false,
-        isEnabled: false
-      }
-    },
-    methods: {
-      ...mapActions([
-        'updateQtyByProductId'
-      ]),
-      sendQty(product) {
-        let apiRootUrl = data.apiRootUrl.replace(/\?.*/,'');
-        let apiEditProductsUrl = `${apiRootUrl}/product/${product.product_id}`
-        let apiEditCombinationsUrl = `${apiRootUrl}/product/${product.product_id}/combination/${product.product_attribute_id}`;
-        let postUrl = product.product_attribute_id ? apiEditCombinationsUrl : apiEditProductsUrl;
-
-        // POST when qty !=0
-
-        if(this.isEnabled) {
-          this.$store.dispatch('updateQtyByProductId', {
-            http: this.$http,
-            url: postUrl,
-            qty: product.qty
-          });
-        }
-      },
-      toggleCheck(val) {
-        this.isActive = !this.isActive
-      },
-      toggleEnabled(val) {
-        this.isEnabled = (val !==0);
-      }
     }
   }
 </script>
@@ -90,35 +77,10 @@
   .thumbnail {
       border: $gray-light 1px solid;
   }
-  .qty {
-      position: relative;
-      .check-button {
-        outline:none;
-        display: none;
-        position: absolute;
-        top: 3px;
-        right: 27px;
-        border: none;
-        height: 29px;
-        width: 40px;
-        background: $brand-primary;
-        z-index: 2;
-        border-left: 10px solid white;
-        .material-icons {
-          color: white;
-        }
-      }
-  }
-  .qty.active {
-    .check-button {
-      display: block;
-    }
-  }
-  .qty.disabled {
-    .check-button {
-      display: block;
-      background: $gray-light;
-      cursor: default;
+  .qty-update {
+    color: $brand-primary;
+    .material-icons {
+      vertical-align: middle;
     }
   }
 </style>
