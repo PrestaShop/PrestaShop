7366b223ce8d590d3fc5bb019de52546e833969d
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2017-03-16 14:28:14 +0100

    CO: Revise SQL migration

--- install-dev/upgrade/sql/1.7.2.0.sql
+++ install-dev/upgrade/sql/1.7.2.0.sql
@@ -27,4 +27,7 @@ ALTER TABLE `PREFIX_stock` ADD `isbn` VARCHAR(32) NULL DEFAULT NULL;
 ALTER TABLE `PREFIX_supply_order_detail` ADD `isbn` VARCHAR(32) NULL DEFAULT NULL;
 
 ALTER TABLE  `PREFIX_stock_available` ADD  `physical_quantity` INT NOT NULL DEFAULT  '0' AFTER  `quantity`;
-ALTER TABLE  `PREFIX_stock_available` ADD  `reserved_quantity` INT NOT NULL DEFAULT  '0' AFTER  `quantity`;
+ALTER TABLE  `PREFIX_stock_available` ADD  `reserved_quantity` INT NOT NULL DEFAULT  '0' AFTER  `physical_quantity`;
+
+INSERT INTO `PREFIX_tab` (`id_tab`, `id_parent`, `position`, `module`, `class_name`, `active`, `hide_host_mode`, `icon`) VALUES
+(null, 9, 7, NULL, 'AdminStockManagement', 1, 0, '');
