af50e7ab5c747b435e87ced695c894ce25736792
nihco2 <nicolas.martin@prestashop.com>
2017-03-20 17:27:00 +0100

    BO: refactor stock app store

---
+++ admin-dev/themes/new-theme/js/stock-page/store/actions.js
@@ -0,0 +1,53 @@
+import Vue from 'vue';
+import VueResource from 'vue-resource';
+import * as types from './mutation-types';
+
+Vue.use(VueResource);
+
+export const sort = ({ commit, state }, payload) => {
+  let url = payload.url,
+      order = payload.order;
+
+  Vue.http.get(url, {
+    params: {
+      order
+    }
+  }).then((res) => {
+    commit(types.ADD_PRODUCTS, res.body);
+  }, function(error) {
+      return showGrowl('error', error.statusText);
+  });
+};
+
+export const updateQtyByProductId = ({ commit, state }, payload) => {
+  let url = payload.url,
+      delta = payload.delta;
+
+  Vue.http.post(url, {
+    delta
+  }).then((res) => {
+      commit(types.UPDATE_PRODUCTS, res.body);
+      return showGrowl('notice', 'Stock successfully updated');
+  }, function(error) {
+      return showGrowl('error', error.statusText);
+  });
+};
+
+export const getStock = ({ commit, state }, payload) => {
+  Vue.http.get(payload.url).then(function(response){
+    if(response.status === 200) {
+      commit(types.ADD_PRODUCTS, response.body);
+    }
+  }, function(error){
+      return showGrowl('error', error.statusText);
+  });
+};
+
+const showGrowl = (type, message) => {
+  window.$.growl[type]({
+    title:'',
+    size: "large",
+    message: message,
+    duration: 1000
+  });
+};
