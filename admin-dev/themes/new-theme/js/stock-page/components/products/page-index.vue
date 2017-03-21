bacad02ff9cf95f75f4e2fa09893a0c3189d1e03
nihco2 <nicolas.martin@prestashop.com>
2017-03-21 13:55:17 +0100

    BO: Improve stock pagination

---
+++ admin-dev/themes/new-theme/js/stock-page/components/products/page-index.vue
@@ -0,0 +1,22 @@
+<template>
+  <a v-on:click="changePage($event, index)" class="page-link" :class="classObject">{{ index }}</a>
+</template>
+
+<script>
+  export default {
+    props: ['index','current'],
+    computed: {
+      classObject() {
+        return {
+          current: (this.current === this.index)
+        }
+      }
+    },
+    methods: {
+      changePage(event, pageIndex) {
+        event.preventDefault();
+        this.$emit('pageChanged', pageIndex);
+      }
+    }
+  }
+</script>
