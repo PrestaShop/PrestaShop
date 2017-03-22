18e817bb56429e3cbb595bef88332615a04d1d1a
nihco2 <nicolas.martin@prestashop.com>
2017-03-22 12:12:59 +0100

    BO: Update bulk action behaviour

--- admin-dev/themes/new-theme/js/stock-page/store/mutations.js
+++ admin-dev/themes/new-theme/js/stock-page/store/mutations.js
@@ -2,6 +2,9 @@ import * as types from './mutation-types';
 
 export default {
   [types.ADD_PRODUCTS] (state, products) {
+    window._.forEach(products, (product) => {
+      product.qty = 0;
+    });
     state.products = products;
   },
   [types.UPDATE_ORDER] (state, order) {
@@ -12,6 +15,7 @@ export default {
       'product_id': updatedProduct.product_id,
       'combination_id': updatedProduct.combination_id
     });
+    updatedProduct.qty = 0;
     state.products.splice(index, 1, updatedProduct);
   },
   [types.UPDATE_PRODUCTS] (state, updatedProducts) {
@@ -21,6 +25,7 @@ export default {
         'product_id': product.product_id,
         'combination_id': product.combination_id
       });
+      product.qty = 0;
       state.products.splice(index, 1, product);
     });
     state.hasQty = false;
@@ -28,7 +33,18 @@ export default {
   [types.UPDATE_PRODUCT_QTY] (state, updatedProduct) {
     let hasQty = false;
 
+    let index = window._.findIndex(state.productsToUpdate, {
+      'product_id': updatedProduct.product_id,
+      'combination_id': updatedProduct.combination_id
+    });
+
+    let productToUpdate = window._.find(state.products, {
+      'product_id': updatedProduct.product_id,
+      'combination_id': updatedProduct.combination_id
+    });
+
     window._.forEach(state.products, (product) => {
+      productToUpdate.qty = updatedProduct.delta;
       if(product.qty) {
         hasQty = true;
       }
@@ -36,10 +52,6 @@ export default {
 
     state.hasQty = hasQty;
 
-    let index = window._.findIndex(state.productsToUpdate, {
-      'product_id': updatedProduct.product_id,
-      'combination_id': updatedProduct.combination_id
-    });
     if(index !== -1) {
       return state.productsToUpdate.splice(index, 1, updatedProduct);
     }
