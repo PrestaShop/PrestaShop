b58b2e3827154e44e6f478aaf83265263ccbee88
Nicolas Martin <nicolas.martin@prestashop.com>
2017-03-29 18:31:55 +0200

    Merge pull request #27 from nihco2/revert-26-filter-by-keywords
    Revert "Allow filtering stock products by keywords"

---
+++ admin-dev/themes/new-theme/js/stock-page/components/header/filter-component.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="filter-container">
+    <SearchFilter :placeholder="placeholder" />
+    <ul class="m-t-1">
+      <li v-for="n in 10">
+        <input type="checkbox" class="m-r-1 checkbox">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import SearchFilter from './search-filter';
+
+  export default {
+    props: ['placeholder','getData'],
+    mounted() {
+      this.$store.dispatch(this.getData);
+    },
+    components: {
+      SearchFilter
+    }
+  }
+</script>
+
+<style lang="sass?outputStyle=expanded" scoped>
+  @import "~PrestaKit/scss/custom/_variables.scss";
+  .filter-container {
+    border: $gray-light 1px solid;
+    padding: 10px;
+  }
+  ul {
+    list-style: none;
+    padding-left: 0;
+  }
+</style>
