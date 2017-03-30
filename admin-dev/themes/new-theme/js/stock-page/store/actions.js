ae03c14134a4a1a1b606b8bb03073216cb34f46a
nihco2 <nicolas.martin@prestashop.com>
2017-03-30 16:57:04 +0200

    BO: Refactor stock with vuex modules

--- admin-dev/themes/new-theme/js/stock-page/store/actions.js
+++ admin-dev/themes/new-theme/js/stock-page/store/actions.js
@@ -1,34 +1,9 @@
 import Vue from 'vue';
 import VueResource from 'vue-resource';
 import * as types from './mutation-types';
+import { showGrowl } from './utils/growl';
 
 Vue.use(VueResource);
-
-export const updateQtyByProductId = ({ commit, state }, payload) => {
-  let url = payload.url,
-      delta = payload.delta;
-
-  Vue.http.post(url, {
-    delta
-  }).then((res) => {
-      commit(types.UPDATE_PRODUCT, res.body);
-      return showGrowl('notice', 'Stock successfully updated');
-  }, function(error) {
-      return showGrowl('error', error.statusText);
-  });
-};
-
-export const updateQtyByProductsId = ({ commit, state }, payload) => {
-  let url = payload.url,
-      productsQty = state.productsToUpdate;
-
-  Vue.http.post(url, productsQty).then((res) => {
-      commit(types.UPDATE_PRODUCTS, res.body);
-      return showGrowl('notice', 'Stock successfully updated');
-  }, function(error) {
-      return showGrowl('error', error.statusText);
-  });
-};
 
 export const getStock = ({ commit, state }, payload) => {
   Vue.http.get(payload.url, {
@@ -38,20 +13,11 @@ export const getStock = ({ commit, state }, payload) => {
       page_index: payload.page_index
     }
   }).then(function(response) {
-      commit(types.SET_PAGE_INDEX, payload.page_index);
-      commit(types.UPDATE_ORDER, payload.order);
-      commit(types.SET_TOTAL_PAGES, response.headers.get('Total-Pages'));
-      commit(types.ADD_PRODUCTS, response.body);
-  }, function(error){
-      return showGrowl('error', error.statusText);
-  });
-};
-
-const showGrowl = (type, message) => {
-  window.$.growl[type]({
-    title:'',
-    size: "large",
-    message: message,
-    duration: 1000
+    commit(types.SET_PAGE_INDEX, payload.page_index);
+    commit(types.UPDATE_ORDER, payload.order);
+    commit(types.SET_TOTAL_PAGES, response.headers.get('Total-Pages'));
+    commit(types.ADD_PRODUCTS, response.body);
+  }, function(error) {
+    return showGrowl('error', error.statusText);
   });
 };
