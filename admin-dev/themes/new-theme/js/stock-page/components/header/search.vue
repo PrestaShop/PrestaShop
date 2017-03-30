8294b914c168ed7ee90236b16d564718ee50bae9
nihco2 <nicolas.martin@prestashop.com>
2017-03-30 16:56:26 +0200

    BO: Add tags on search bar

--- admin-dev/themes/new-theme/js/stock-page/components/header/search.vue
+++ admin-dev/themes/new-theme/js/stock-page/components/header/search.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="row">
+  <div id="search" class="row">
     <div class="col-md-8">
-      <div class="autocomplete m-b-2">
-        <label>Search products (search by name,reference,ID,supplier)</label>
-        <input type="text" class="form-control search" autocomplete="off">
+      <div class="m-b-2">
+        <form @keyup.enter="onKeyEnter" class="search-form" @submit.prevent="onKeyEnter">
+          <label>Search products (search by name,reference,supplier)</label>
+          <Tags :tags="tags" @tags-change="handleChange"  />
+          <button type="button" class="btn btn-primary search-button" @click="onSubmit">
+            <i class="material-icons">search</i>Search
+          </button>
+        </form>
       </div>
       <SearchFilter />
     </div>
@@ -12,10 +17,69 @@
 
 <script>
   import SearchFilter from './search-filter';
+  import Tags from 'vue-tagsinput';
 
   export default {
     components: {
-      SearchFilter
+      SearchFilter,
+      Tags
+    },
+    data() {
+      return {
+        tags: []
+      }
+    },
+    methods: {
+      onSubmit() {
+        if(this.tags.length) {
+          let apiRootUrl = data.apiRootUrl.replace(/\?.*/,'');
+          this.$store.dispatch('searchByKeywords', {
+            url: apiRootUrl,
+            keywords: this.tags
+          });
+        }
+      },
+      onKeyEnter() {
+        $(this.$el).find('.input').blur();
+      },
+      handleChange(index, text) {
+        if (text) {
+          this.tags.splice(index, 0, text);
+        } else {
+          this.tags.splice(index, 1);
+        }
+      }
     }
   }
 </script>
+<style lang="sass?outputStyle=expanded">
+  @import "~PrestaKit/scss/custom/_variables.scss";
+  #search .tags-input {
+    background: white;
+    .tag {
+      background: $brand-primary;
+      color: white;
+      padding: 2px 4px;
+      border-radius: 0;
+      font-weight: lighter;
+      .hl-click {
+        height: 100%;
+        width: 15px;
+      }
+    }
+    input.input {
+      font-family: Open Sans, sans-serif;
+    }
+  }
+  .search-form {
+    position: relative;
+    .search-button {
+      position: absolute;
+      right: 0;
+      top: 0;
+      border-radius: 0;
+      margin-top: 28px;
+      height:29px;
+    }
+  }
+</style>
