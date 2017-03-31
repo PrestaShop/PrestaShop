45c32bf7d7123c826ebe98caf19f6695efc326ac
nihco2 <nicolas.martin@prestashop.com>
2017-03-31 13:56:53 +0200

    BO: Add alert when no products

---
+++ admin-dev/themes/new-theme/js/stock-page/components/products/alert.vue
@@ -0,0 +1,26 @@
+<template>
+  <tr>
+    <td colspan="7">
+      <div class="alert alert-info" role="alert">
+        <i class="material-icons">info_outline</i>
+        <p class="alert-text">No product matches your search. Try changing search terms.</p>
+      </div>
+    </td>
+  </tr>
+</template>
+
+<style lang="sass?outputStyle=expanded" scoped>
+  @import "~PrestaKit/scss/custom/_variables.scss";
+
+  .alert {
+    border-radius: 0;
+    border-width: 2px;
+    padding: 0;
+    background: $notice;
+    .alert-text {
+      padding: 15px 0 15px 15px;
+      margin-left: 55px;
+      background: white;
+    }
+  }
+</style>
