d5265f5f679980216e2e8153ca51a69b4881ce0d
Alex Sampaio <alex.sampaio1991@gmail.com>
2017-04-04 17:03:02 +0200

    CO: refacto of Stocks Repositories

--- src/PrestaShopBundle/Entity/Repository/StockRepository.php
+++ src/PrestaShopBundle/Entity/Repository/StockRepository.php
@@ -29,7 +29,6 @@ namespace PrestaShopBundle\Entity\Repository;
 use Configuration;
 use Doctrine\DBAL\Driver\Connection;
 use Doctrine\DBAL\Driver\Statement;
-use Employee;
 use PDO;
 use PrestaShop\PrestaShop\Adapter\ImageManager;
 use PrestaShop\PrestaShop\Adapter\LegacyContext as ContextAdapter;
@@ -44,44 +43,12 @@ use Product;
 use RuntimeException;
 use Shop;
 
-class StockRepository
+class StockRepository extends StockManagementRepository
 {
-    use NormalizeFieldTrait;
-
-    /**
-     * @var Connection
-     */
-    private $connection;
-
-    /**
-     * @var ImageManager
-     */
-    private $imageManager;
-
-    /**
-     * @var string
-     */
-    private $tablePrefix;
-
-    /**
-     * @var int
-     */
-    private $languageId;
-
-    /**
-     * @var int
-     */
-    private $shopId;
-
     /**
      * @var StockManager
      */
     private $stockManager;
-
-    /**
-     * @var ContextAdapter
-     */
-    private $contextAdapter;
 
     /**
      * @var array
@@ -104,35 +71,17 @@ class StockRepository
         $tablePrefix
     )
     {
-        $this->connection = $connection;
-        $this->imageManager = $imageManager;
+        parent::__construct(
+            $connection,
+            $contextAdapter,
+            $imageManager,
+            $tablePrefix
+        );
+
         $this->stockManager = $stockManager;
-
-        $this->tablePrefix = $tablePrefix;
-
-        $this->contextAdapter = $contextAdapter;
-        $context = $contextAdapter->getContext();
-
-        if (!$context->employee instanceof Employee) {
-            throw new RuntimeException('Determining the active language requires a contextual employee instance.');
-        }
-
-        $languageId = $context->employee->id_lang;
-        $this->languageId = (int)$languageId;
-
-        if (!$context->shop instanceof Shop) {
-            throw new RuntimeException('Determining the active shop requires a contextual shop instance.');
-        }
-
-        $shop = $context->shop;
-        if ($shop->getContextType() !== $shop::CONTEXT_SHOP) {
-            throw new NotImplementedException('Shop context types other than "single shop" are not supported');
-        }
 
         $this->orderStates['error'] = (int)Configuration::get('PS_OS_ERROR');
         $this->orderStates['cancellation'] = (int)Configuration::get('PS_OS_CANCELED');
-
-        $this->shopId = $shop->getContextualShopId();
     }
 
     /**
@@ -172,17 +121,43 @@ class StockRepository
         $statement->bindValue('delta', $delta, PDO::PARAM_INT);
 
         if ($statement->execute()) {
-            // @TODO: call with container
-            $stockMovement = new StockMovementRepository(
-                $this->connection,
-                $this->contextAdapter,
-                $this->imageManager,
-                $this->tablePrefix
-            );
-            $stockMovement->saveFromMovement($movement);
+            $idStock = $this->getStockBy($productIdentity);
+            if (!empty($idStock)) {
+                $movement->setIdStock((int)$idStock);
+
+                // @TODO: call with container
+                $stockMovement = new StockMovementRepository(
+                    $this->connection,
+                    $this->contextAdapter,
+                    $this->imageManager,
+                    $this->tablePrefix
+                );
+                $stockMovement->saveFromMovement($movement);
+            }
         }
 
         return $this->selectStockBy($productIdentity);
+    }
+
+    private function getStockBy(ProductIdentity $productIdentity)
+    {
+        $query = '
+                SELECT id_stock_available
+                FROM {table_prefix}stock_available
+                WHERE id_product = :product_id
+                AND id_product_attribute = :combination_id
+            ';
+
+        $query = str_replace('{table_prefix}', $this->tablePrefix, $query);
+
+        $statement = $this->connection->prepare($query);
+
+        $statement->bindValue('product_id', $productIdentity->getProductId(), PDO::PARAM_INT);
+        $statement->bindValue('combination_id', $productIdentity->getCombinationId(), PDO::PARAM_INT);
+
+        $statement->execute();
+
+        return $statement->fetchColumn();
     }
 
     /**
@@ -194,10 +169,10 @@ class StockRepository
         $andWhereClause = '
             AND p.id_product = :product_id
             AND COALESCE(pa.id_product_attribute, 0) = :combination_id';
-        $query = $this->selectStock($andWhereClause);
+        $query = $this->selectSql($andWhereClause);
 
         $statement = $this->connection->prepare($query);
-        $this->bindStockValues($statement, null, $productIdentity);
+        $this->bindStockManagementValues($statement, null, $productIdentity);
 
         $statement->execute();
         $rows = $statement->fetchAll();
@@ -221,7 +196,7 @@ class StockRepository
      * @param QueryParamsCollection $queryParams
      * @return mixed
      */
-    public function getStock(QueryParamsCollection $queryParams)
+    public function getData(QueryParamsCollection $queryParams)
     {
         $this->stockManager->updatePhysicalProductQuantity(
             $this->shopId,
@@ -229,67 +204,9 @@ class StockRepository
             $this->orderStates['cancellation']
         );
 
-        $query = $this->selectStock(
-                $this->andWhere($queryParams),
-                $this->having($queryParams),
-                $this->orderBy($queryParams)
-            ) . $this->paginate();
-
-        $statement = $this->connection->prepare($query);
-        $this->bindStockValues($statement, $queryParams);
-
-        $statement->execute();
-        $rows = $statement->fetchAll();
-
-        $rows = $this->addImageThumbnailPaths($rows);
-
-        return $this->castNumericToInt($rows);
+        return parent::getData($queryParams);
     }
 
-    /**
-     * @param QueryParamsCollection $queryParams
-     * @return bool|string
-     */
-    public function countStockPages(QueryParamsCollection $queryParams)
-    {
-        $query = sprintf(
-            'SELECT CEIL(FOUND_ROWS() / :%s) as total_pages',
-            QueryParamsCollection::SQL_PARAM_MAX_RESULTS
-        );
-
-        $statement = $this->connection->prepare($query);
-        $this->bindMaxResultsValue($statement, $queryParams);
-
-        $statement->execute();
-
-        return (int)$statement->fetchColumn();
-    }
-
-    /**
-     * @param array $rows
-     * @return array
-     */
-    private function addImageThumbnailPaths(array $rows)
-    {
-        array_walk($rows, function (&$row) {
-            $row['product_thumbnail'] = 'N/A';
-            $row['combination_thumbnail'] = 'N/A';
-
-            if ((int)$row['product_cover_id'] > 0) {
-                $row['product_thumbnail'] = $this->imageManager->getThumbnailPath(
-                    $row['product_cover_id']
-                );
-            }
-
-            if ((int)$row['combination_cover_id'] > 0) {
-                $row['combination_thumbnail'] = $this->imageManager->getThumbnailPath(
-                    $row['combination_cover_id']
-                );
-            }
-        });
-
-        return $rows;
-    }
 
     /**
      * @param string $andWhereClause
@@ -297,7 +214,7 @@ class StockRepository
      * @param null $orderByClause
      * @return mixed
      */
-    private function selectStock(
+    protected function selectSql(
         $andWhereClause = '',
         $having = '',
         $orderByClause = null
@@ -335,6 +252,7 @@ class StockRepository
                 IF (LENGTH(TRIM(p.reference)) > 0, p.reference, "N/A"),
                 CONCAT(p.reference, " ", pa.reference)
             ) AS product_reference,
+            pl.name AS product_name,
             IF (
                 COALESCE(pa.id_product_attribute, 0) > 0,
                 GROUP_CONCAT(
@@ -344,10 +262,9 @@ class StockRepository
                 "N/A"
             ) AS combination_name,
             p.id_supplier AS supplier_id,
+            COALESCE(s.name, "N/A") AS supplier_name,
             COALESCE(ic.id_image, 0) AS product_cover_id,
             COALESCE(i.id_image, 0) as combination_cover_id,
-            COALESCE(s.name, "N/A") AS supplier_name,
-            pl.name AS product_name,
             sa.quantity as product_available_quantity,
             sa.physical_quantity as product_physical_quantity,
             sa.reserved_quantity as product_reserved_quantity,
@@ -359,11 +276,15 @@ class StockRepository
                 p.id_product = pl.id_product AND
                 pl.id_lang = :language_id
             )
-            LEFT JOIN {table_prefix}product_shop ps ON (
+            INNER JOIN {table_prefix}product_shop ps ON (
                 p.id_product = ps.id_product AND
                 ps.id_shop = :shop_id
             )
-            LEFT JOIN {table_prefix}stock_available sa ON (p.id_product = sa.id_product)
+            LEFT JOIN {table_prefix}stock_available sa ON (
+              p.id_product = sa.id_product AND
+              sa.id_shop = :shop_id AND
+              sa.id_product_attribute = COALESCE(pa.id_product_attribute, 0)
+            )
             LEFT JOIN {table_prefix}image ic ON (
                 p.id_product = ic.id_product AND
                 ic.cover = 1
@@ -371,6 +292,7 @@ class StockRepository
             LEFT JOIN {table_prefix}image_shop ims ON (
                 p.id_product = ims.id_product AND
                 ic.id_image  = ims.id_image AND
+                ims.id_shop = :shop_id AND
                 ims.cover = 1
             )
             LEFT JOIN (
@@ -458,16 +380,21 @@ class StockRepository
             )
             {left_join}
             WHERE
-            ps.id_shop = :shop_id AND
-            sa.id_shop = :shop_id AND
-            ims.id_shop = :shop_id AND
-            sa.id_product_attribute = COALESCE(pa.id_product_attribute, 0) AND
             p.state = :state
             {and_where}
             GROUP BY p.id_product, COALESCE(pa.id_product_attribute, 0)
             HAVING 1 {having}
             {order_by}
         ');
+    }
+
+    /**
+     * @param QueryParamsCollection $queryParams
+     * @return string
+     */
+    protected function andWhere(QueryParamsCollection $queryParams)
+    {
+        return $this->andWhereLimitingCombinationsPerProduct() . parent::andWhere($queryParams);
     }
 
     /**
@@ -495,142 +422,10 @@ class StockRepository
     }
 
     /**
-     * @param Statement $statement
-     * @param QueryParamsCollection|null $queryParams
-     * @param ProductIdentity|null $productIdentity
-     */
-    private function bindStockValues(
-        Statement $statement,
-        QueryParamsCollection $queryParams = null,
-        ProductIdentity $productIdentity = null
-    )
-    {
-        $statement->bindValue('shop_id', $this->shopId, PDO::PARAM_INT);
-        $statement->bindValue('language_id', $this->languageId, PDO::PARAM_INT);
-        $statement->bindValue('state', Product::STATE_SAVED, PDO::PARAM_INT);
-
-        if ($queryParams) {
-            $this->bindValuesInStatement($statement, $queryParams);
-        }
-
-        if ($productIdentity) {
-            $statement->bindValue('product_id', $productIdentity->getProductId(), PDO::PARAM_INT);
-            $statement->bindValue('combination_id', $productIdentity->getCombinationId(), PDO::PARAM_INT);
-        }
-    }
-
-    /**
      * @return string
      */
     private function orderByProductIds()
     {
         return 'ORDER BY p.id_product DESC, COALESCE(pa.id_product_attribute, 0)';
     }
-
-    /**
-     * @param QueryParamsCollection $queryParams
-     * @return string
-     */
-    private function andWhere(QueryParamsCollection $queryParams)
-    {
-        $filters = $queryParams->getSqlFilters();
-        $filters = strtr($filters[$queryParams::SQL_CLAUSE_WHERE], array(
-            '{product_id}' => 'p.id_product',
-            '{supplier_id}' => 'p.id_supplier',
-            '{category_id}' => 'cp.id_category',
-            '{attributes}' => 'product_attributes.attributes',
-            '{features}' => 'product_features.features',
-        ));
-
-        return $this->andWhereLimitingCombinationsPerProduct() . $filters;
-    }
-
-    /**
-     * @param QueryParamsCollection $queryParams
-     * @return string
-     */
-    private function having(QueryParamsCollection $queryParams)
-    {
-        $filters = $queryParams->getSqlFilters();
-
-        if (!array_key_exists($queryParams::SQL_CLAUSE_HAVING, $filters)) {
-            return '';
-        }
-
-        return strtr($filters['having'], array(
-            '{combination_name}' => 'combination_name',
-            '{product_reference}' => 'product_reference',
-            '{supplier_name}' => 'supplier_name',
-            '{product_name}' => 'product_name'
-        ));
-    }
-
-    /**
-     * @param QueryParamsCollection $queryParams
-     * @return string
-     */
-    private function orderBy(QueryParamsCollection $queryParams)
-    {
-        $orderByClause = $queryParams->getSqlOrder();
-
-        $descendingOrder = false !== strpos($orderByClause, ' DESC');
-
-        $productColumns = 'product_id, combination_id';
-        if ($descendingOrder) {
-            $productColumns = 'product_id DESC, combination_id ASC';
-        }
-
-        return strtr($orderByClause, array(
-            '{product} DESC' => $productColumns,
-            '{product}' => $productColumns,
-            '{reference}' => 'product_reference',
-            '{supplier}' => 'supplier_name',
-            '{available_quantity}' => 'product_available_quantity',
-            '{physical_quantity}' => 'product_physical_quantity'
-        ));
-    }
-
-    /**
-     * @return string
-     */
-    private function paginate()
-    {
-        return sprintf(
-            'LIMIT :%s,:%s',
-            QueryParamsCollection::SQL_PARAM_FIRST_RESULT,
-            QueryParamsCollection::SQL_PARAM_MAX_RESULTS
-        );
-    }
-
-    /**
-     * @param Statement $statement
-     * @param QueryParamsCollection $queryParams
-     */
-    private function bindValuesInStatement(Statement $statement, QueryParamsCollection $queryParams)
-    {
-        $sqlParams = $queryParams->getSqlParams();
-
-        foreach ($sqlParams as $name => $value) {
-            if (is_int($value)) {
-                $statement->bindValue($name, $value, PDO::PARAM_INT);
-            } else {
-                $statement->bindValue($name, $value, PDO::PARAM_STR);
-            }
-        }
-    }
-
-    /**
-     * @param Statement $statement
-     * @param QueryParamsCollection $queryParams
-     */
-    private function bindMaxResultsValue(Statement $statement, QueryParamsCollection $queryParams)
-    {
-        $paginationParams = $queryParams->getSqlPaginationParams();
-        $statement->bindValue(
-            QueryParamsCollection::SQL_PARAM_MAX_RESULTS,
-            $paginationParams[QueryParamsCollection::SQL_PARAM_MAX_RESULTS],
-            PDO::PARAM_INT
-        );
-    }
-
 }
