811fdd1aa323a5042b610e9ae13a644174ddc19b
Alex Sampaio <alex.sampaio1991@gmail.com>
2017-04-04 17:03:02 +0200

    CO: refacto of Stocks Repositories

--- src/PrestaShopBundle/Entity/Repository/StockMovementRepository.php
+++ src/PrestaShopBundle/Entity/Repository/StockMovementRepository.php
@@ -44,40 +44,8 @@ use Product;
 use RuntimeException;
 use Shop;
 
-class StockMovementRepository
+class StockMovementRepository extends StockManagementRepository
 {
-    use NormalizeFieldTrait;
-
-    /**
-     * @var Connection
-     */
-    private $connection;
-
-    /**
-     * @var ImageManager
-     */
-    private $imageManager;
-
-    /**
-     * @var string
-     */
-    private $tablePrefix;
-
-    /**
-     * @var int
-     */
-    private $languageId;
-
-    /**
-     * @var int
-     */
-    private $shopId;
-
-    /**
-     * @var \Context
-     */
-    private $context;
-
     /**
      * @param Connection $connection
      * @param ContextAdapter $contextAdapter
@@ -92,177 +60,74 @@ class StockMovementRepository
         $tablePrefix
     )
     {
-        $this->connection = $connection;
-        $this->imageManager = $imageManager;
-
-        $this->tablePrefix = $tablePrefix;
-
-        $this->context = $contextAdapter->getContext();
-
-        if (!$this->context->employee instanceof Employee) {
-            throw new RuntimeException('Determining the active language requires a contextual employee instance.');
-        }
-
-        $languageId = $this->context->employee->id_lang;
-        $this->languageId = (int)$languageId;
-
-        if (!$this->context->shop instanceof Shop) {
-            throw new RuntimeException('Determining the active shop requires a contextual shop instance.');
-        }
-
-        $shop = $this->context->shop;
-        if ($shop->getContextType() !== $shop::CONTEXT_SHOP) {
-            throw new NotImplementedException('Shop context types other than "single shop" are not supported');
-        }
-
-        $this->shopId = $shop->getContextualShopId();
+        parent::__construct(
+            $connection,
+            $contextAdapter,
+            $imageManager,
+            $tablePrefix
+        );
     }
 
+    /**
+     * Register a Movement
+     *
+     * @param Movement $movement
+     * @return bool
+     */
     public function saveFromMovement(Movement $movement)
     {
-        $delta = $movement->getDelta();
+        $idStock = $movement->getIdStock();
 
-        // @TODO: good data
-        $mvt_params = array(
-            'id_stock' => 0,
-            'id_order' => 0,
-            'id_supply_order' => 0,
-            'id_stock_mvt_reason' => $delta >= 1 ? 1 : 2,
-            'id_employee' => (int)$this->context->employee->id,
-            'employee_firstname' => $this->context->employee->firstname,
-            'employee_lastname' => $this->context->employee->lastname,
-            'physical_quantity' => abs($delta),
-            'date_add' => date('Y-m-d H:i:s'),
-            'sign' => $delta >= 1 ? 1 : -1,
-            'price_te' => 0,
-            'last_wa' => 0,
-            'current_wa' => 0,
-            'referer' => null,
-        );
+        if (!empty($idStock)) {
+            $delta = $movement->getDelta();
 
-        $query = 'INSERT INTO {table_prefix}stock_mvt SET ';
+            // @TODO: good data
+            $mvt_params = array(
+                'id_stock' => $idStock,
+                'id_order' => $movement->getIdOrder(),
+                'id_supply_order' => $movement->getIdSupplyOrder(),
 
-        foreach ($mvt_params as $k => $value) {
-            if (null !== $value) {
-                $query .= '`'.$k.'`' . ' = :' . $k . ' ,';
-            } else {
-                unset($mvt_params[$k]);
+                'id_stock_mvt_reason' => $delta >= 1 ? 1 : 2,
+                'physical_quantity' => abs($delta),
+                'sign' => $delta >= 1 ? 1 : -1,
+
+                'id_employee' => (int)$this->context->employee->id,
+                'employee_firstname' => $this->context->employee->firstname,
+                'employee_lastname' => $this->context->employee->lastname,
+                'date_add' => date('Y-m-d H:i:s'),
+
+                'price_te' => 0,
+                'last_wa' => 0,
+                'current_wa' => 0,
+                'referer' => null,
+            );
+
+            $query = 'INSERT INTO {table_prefix}stock_mvt SET ';
+
+            foreach ($mvt_params as $k => $value) {
+                if (null !== $value) {
+                    $query .= '`' . $k . '`' . ' = :' . $k . ' ,';
+                } else {
+                    unset($mvt_params[$k]);
+                }
             }
+
+            $query = rtrim(str_replace('{table_prefix}', $this->tablePrefix, $query), ',');
+
+            $statement = $this->connection->prepare($query);
+
+            foreach ($mvt_params as $k => $value) {
+                if (is_int($value)) {
+                    $statement->bindValue($k, $value, PDO::PARAM_INT);
+                } else {
+                    $statement->bindValue($k, $value, PDO::PARAM_STR);
+                }
+            }
+
+            return $statement->execute();
         }
 
-        $query = rtrim(str_replace('{table_prefix}', $this->tablePrefix, $query), ',');
-
-        $statement = $this->connection->prepare($query);
-
-        foreach ($mvt_params as $k => $value) {
-            if (is_int($value)) {
-                $statement->bindValue($k, $value, PDO::PARAM_INT);
-            } else {
-                $statement->bindValue($k, $value, PDO::PARAM_STR);
-            }
-        }
-
-        return $statement->execute();
-    }
-
-    /**
-     * @param ProductIdentity $productIdentity
-     * @return mixed
-     */
-//    private function selectMovementBy(ProductIdentity $productIdentity)
-//    {
-//        $andWhereClause = '
-//            AND p.id_product = :product_id
-//            AND COALESCE(pa.id_product_attribute, 0) = :combination_id';
-//        $query = $this->selectStock($andWhereClause);
-//
-//        $statement = $this->connection->prepare($query);
-//        $this->bindStockValues($statement, null, $productIdentity);
-//
-//        $statement->execute();
-//        $rows = $statement->fetchAll();
-//
-//        if (count($rows) === 0) {
-//            throw new ProductNotFoundException(
-//                sprintf(
-//                    'Product with id %d and combination id %d can not be found',
-//                    $productIdentity->getProductId(),
-//                    $productIdentity->getCombinationId()
-//                )
-//            );
-//        }
-//
-//        $rows = $this->addImageThumbnailPaths($rows);
-//
-//        return $this->castNumericToInt($rows)[0];
-//    }
-
-    /**
-     * @param QueryParamsCollection $queryParams
-     * @return mixed
-     */
-    public function getMovements(QueryParamsCollection $queryParams)
-    {
-        $query = $this->selectMovements(
-                $this->andWhere($queryParams),
-                $this->having($queryParams),
-                $this->orderBy($queryParams)
-            ) . $this->paginate();
-
-        $statement = $this->connection->prepare($query);
-        $this->bindMovementsValues($statement, $queryParams);
-
-        $statement->execute();
-        $rows = $statement->fetchAll();
-
-        $rows = $this->addImageThumbnailPaths($rows);
-
-        return $this->castNumericToInt($rows);
-    }
-
-    /**
-     * @param QueryParamsCollection $queryParams
-     * @return bool|string
-     */
-    public function countMovementPages(QueryParamsCollection $queryParams)
-    {
-        $query = sprintf(
-            'SELECT CEIL(FOUND_ROWS() / :%s) as total_pages',
-            QueryParamsCollection::SQL_PARAM_MAX_RESULTS
-        );
-
-        $statement = $this->connection->prepare($query);
-        $this->bindMaxResultsValue($statement, $queryParams);
-
-        $statement->execute();
-
-        return (int)$statement->fetchColumn();
-    }
-
-    /**
-     * @param array $rows
-     * @return array
-     */
-    private function addImageThumbnailPaths(array $rows)
-    {
-        array_walk($rows, function (&$row) {
-            $row['product_thumbnail'] = 'N/A';
-            $row['combination_thumbnail'] = 'N/A';
-
-            if ((int)$row['product_cover_id'] > 0) {
-                $row['product_thumbnail'] = $this->imageManager->getThumbnailPath(
-                    $row['product_cover_id']
-                );
-            }
-
-            if ((int)$row['combination_cover_id'] > 0) {
-                $row['combination_thumbnail'] = $this->imageManager->getThumbnailPath(
-                    $row['combination_cover_id']
-                );
-            }
-        });
-
-        return $rows;
+        return false;
     }
 
     /**
@@ -271,7 +136,7 @@ class StockMovementRepository
      * @param null $orderByClause
      * @return mixed
      */
-    private function selectMovements(
+    protected function selectSql(
         $andWhereClause = '',
         $having = '',
         $orderByClause = null
@@ -306,6 +171,7 @@ class StockMovementRepository
                 IF (LENGTH(TRIM(p.reference)) > 0, p.reference, "N/A"),
                 CONCAT(p.reference, " ", pa.reference)
             ) AS product_reference,
+            pl.name AS product_name,
             IF (
                 COALESCE(pa.id_product_attribute, 0) > 0,
                 GROUP_CONCAT(
@@ -315,10 +181,9 @@ class StockMovementRepository
                 "N/A"
             ) AS combination_name,
             p.id_supplier AS supplier_id,
+            COALESCE(s.name, "N/A") AS supplier_name,
             COALESCE(ic.id_image, 0) AS product_cover_id,
             COALESCE(i.id_image, 0) as combination_cover_id,
-            COALESCE(s.name, "N/A") AS supplier_name,
-            pl.name AS product_name,
             COALESCE(product_attributes.attributes, "") AS product_attributes,
             COALESCE(product_features.features, "") AS product_features
             FROM {table_prefix}stock_mvt sm
@@ -326,13 +191,13 @@ class StockMovementRepository
               smrl.id_stock_mvt_reason = sm.id_stock_mvt_reason
               AND smrl.id_lang = :language_id)
             INNER JOIN {table_prefix}stock_available sa ON (sa.id_stock_available = sm.id_stock)
-            LEFT JOIN {table_prefix}product p ON (p.id_product = sa.id_product)
+            INNER JOIN {table_prefix}product p ON (p.id_product = sa.id_product)
             LEFT JOIN {table_prefix}product_attribute pa ON (pa.id_product_attribute = sa.id_product_attribute)
             LEFT JOIN {table_prefix}product_lang pl ON (
                 p.id_product = pl.id_product AND
                 pl.id_lang = :language_id
             )
-            LEFT JOIN {table_prefix}product_shop ps ON (
+            INNER JOIN {table_prefix}product_shop ps ON (
                 p.id_product = ps.id_product AND
                 ps.id_shop = :shop_id
             )
@@ -343,6 +208,7 @@ class StockMovementRepository
             LEFT JOIN {table_prefix}image_shop ims ON (
                 p.id_product = ims.id_product AND
                 ic.id_image  = ims.id_image AND
+                ims.id_shop = :shop_id AND
                 ims.cover = 1
             )
             LEFT JOIN (
@@ -429,41 +295,13 @@ class StockMovementRepository
                 product_features.id_product = p.id_product
             )
             WHERE
-            ps.id_shop = :shop_id AND
             sa.id_shop = :shop_id AND
-            ims.id_shop = :shop_id AND
-            sa.id_product_attribute = COALESCE(pa.id_product_attribute, 0) AND
-            p.state = :state
+            sa.id_product_attribute = COALESCE(pa.id_product_attribute, 0)
             {and_where}
-            GROUP BY p.id_product, COALESCE(pa.id_product_attribute, 0)
+            GROUP BY sm.id_stock_mvt
             HAVING 1 {having}
             {order_by}
         ');
-    }
-
-    /**
-     * @param Statement $statement
-     * @param QueryParamsCollection|null $queryParams
-     * @param ProductIdentity|null $productIdentity
-     */
-    private function bindMovementsValues(
-        Statement $statement,
-        QueryParamsCollection $queryParams = null,
-        ProductIdentity $productIdentity = null
-    )
-    {
-        $statement->bindValue('shop_id', $this->shopId, PDO::PARAM_INT);
-        $statement->bindValue('language_id', $this->languageId, PDO::PARAM_INT);
-        $statement->bindValue('state', Product::STATE_SAVED, PDO::PARAM_INT);
-
-        if ($queryParams) {
-            $this->bindValuesInStatement($statement, $queryParams);
-        }
-
-        if ($productIdentity) {
-            $statement->bindValue('product_id', $productIdentity->getProductId(), PDO::PARAM_INT);
-            $statement->bindValue('combination_id', $productIdentity->getCombinationId(), PDO::PARAM_INT);
-        }
     }
 
     /**
@@ -471,113 +309,6 @@ class StockMovementRepository
      */
     private function orderByMovementsIds()
     {
-        return 'ORDER BY sm.date_add DESC';
+        return 'ORDER BY sm.id_stock_mvt DESC';
     }
-
-    /**
-     * @param QueryParamsCollection $queryParams
-     * @return string
-     */
-    private function andWhere(QueryParamsCollection $queryParams)
-    {
-        $filters = $queryParams->getSqlFilters();
-        $filters = strtr($filters[$queryParams::SQL_CLAUSE_WHERE], array(
-            '{product_id}' => 'p.id_product',
-            '{supplier_id}' => 'p.id_supplier',
-            '{category_id}' => 'cp.id_category',
-            '{attributes}' => 'product_attributes.attributes',
-            '{features}' => 'product_features.features',
-        ));
-
-        return $filters;
-    }
-
-    /**
-     * @param QueryParamsCollection $queryParams
-     * @return string
-     */
-    private function having(QueryParamsCollection $queryParams)
-    {
-        $filters = $queryParams->getSqlFilters();
-
-        if (!array_key_exists($queryParams::SQL_CLAUSE_HAVING, $filters)) {
-            return '';
-        }
-
-        return strtr($filters['having'], array(
-            '{combination_name}' => 'combination_name',
-            '{product_reference}' => 'product_reference',
-            '{supplier_name}' => 'supplier_name',
-            '{product_name}' => 'product_name'
-        ));
-    }
-
-    /**
-     * @param QueryParamsCollection $queryParams
-     * @return string
-     */
-    private function orderBy(QueryParamsCollection $queryParams)
-    {
-        $orderByClause = $queryParams->getSqlOrder();
-
-        $descendingOrder = false !== strpos($orderByClause, ' DESC');
-
-        $productColumns = 'product_id, combination_id';
-        if ($descendingOrder) {
-            $productColumns = 'product_id DESC, combination_id ASC';
-        }
-
-        return strtr($orderByClause, array(
-            '{product} DESC' => $productColumns,
-            '{product}' => $productColumns,
-            '{reference}' => 'product_reference',
-            '{supplier}' => 'supplier_name',
-            '{available_quantity}' => 'product_available_quantity',
-            '{physical_quantity}' => 'product_physical_quantity'
-        ));
-    }
-
-    /**
-     * @return string
-     */
-    private function paginate()
-    {
-        return sprintf(
-            'LIMIT :%s,:%s',
-            QueryParamsCollection::SQL_PARAM_FIRST_RESULT,
-            QueryParamsCollection::SQL_PARAM_MAX_RESULTS
-        );
-    }
-
-    /**
-     * @param Statement $statement
-     * @param QueryParamsCollection $queryParams
-     */
-    private function bindValuesInStatement(Statement $statement, QueryParamsCollection $queryParams)
-    {
-        $sqlParams = $queryParams->getSqlParams();
-
-        foreach ($sqlParams as $name => $value) {
-            if (is_int($value)) {
-                $statement->bindValue($name, $value, PDO::PARAM_INT);
-            } else {
-                $statement->bindValue($name, $value, PDO::PARAM_STR);
-            }
-        }
-    }
-
-    /**
-     * @param Statement $statement
-     * @param QueryParamsCollection $queryParams
-     */
-    private function bindMaxResultsValue(Statement $statement, QueryParamsCollection $queryParams)
-    {
-        $paginationParams = $queryParams->getSqlPaginationParams();
-        $statement->bindValue(
-            QueryParamsCollection::SQL_PARAM_MAX_RESULTS,
-            $paginationParams[QueryParamsCollection::SQL_PARAM_MAX_RESULTS],
-            PDO::PARAM_INT
-        );
-    }
-
 }
