e1fad41c2c75445b48b60eeea5e73a99ee448a19
nihco2 <nicolas.martin@prestashop.com>
2017-04-14 17:27:30 +0200

    BO: Add tree item component

---
+++ admin-dev/themes/new-theme/js/stock-page/components/utils/ps-tree-item.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Checkbox :id="computedId" :item="item" @checked="onCheck"/>
+    <span class="m-l-1">{{label}}</span>
+  </div>
+</template>
+
+<script>
+  import Checkbox from './checkbox';
+
+  export default {
+    props:['id','item','label'],
+    computed:{
+      computedId() {
+        var randLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+        return randLetter + this.id;
+      }
+    },
+    methods: {
+      onCheck(obj) {
+        this.$emit('checked', obj);
+      }
+    },
+    components: {
+      Checkbox
+    }
+  }
+</script>
