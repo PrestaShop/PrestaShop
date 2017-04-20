8528bce3e8cb9c83f6f37a5e868465a3c9a07775
nihco2 <nicolas.martin@prestashop.com>
2017-04-20 18:01:53 +0200

    BO: Add translation mixin

--- admin-dev/themes/new-theme/js/app/pages/stock/main.js
+++ admin-dev/themes/new-theme/js/app/pages/stock/main.js
@@ -1,13 +1,20 @@
 import Vue from 'vue';
 import app from './components/app';
 import store from './store/';
+import router from './router';
+import Translation from './mixins/translate';
 
-const stockApp = new Vue({
+Vue.mixin(Translation);
+
+new Vue({
+  router,
   store,
   el: '#stock-app',
   template: '<app />',
   components: { app },
   mounted() {
+    this.$store.dispatch('getTranslations');
+
     this.$store.dispatch('getStock', {
       order: this.$store.state.order,
       page_size: this.$store.state.productsPerPage,
