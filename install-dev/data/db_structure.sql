1d96873e264010e2a8f391ba55620ce9c214620e
Jáchym Toušek <enumag@gmail.com>
2017-04-22 20:43:29 +0200

    CO: Increase carrier delay length

--- install-dev/data/db_structure.sql
+++ install-dev/data/db_structure.sql
@@ -115,7 +115,7 @@ CREATE TABLE `PREFIX_carrier_lang` (
   `id_carrier` int(10) unsigned NOT NULL,
   `id_shop` int(11) unsigned NOT NULL DEFAULT '1',
   `id_lang` int(10) unsigned NOT NULL,
-  `delay` varchar(128) DEFAULT NULL,
+  `delay` varchar(512) DEFAULT NULL,
   PRIMARY KEY (`id_lang`,`id_shop`, `id_carrier`)
 ) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8 COLLATION;
 
