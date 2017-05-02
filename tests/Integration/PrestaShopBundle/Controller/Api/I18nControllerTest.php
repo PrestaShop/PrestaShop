7f37d29648a6d89fcc984b0c8a217a6af9341a7e
Alex Sampaio <alex.sampaio1991@gmail.com>
2017-05-02 11:19:23 +0200

    CO: add test to translation list api page

---
+++ tests/Integration/PrestaShopBundle/Controller/Api/I18nControllerTest.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * 2007-2017 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2017 PrestaShop SA
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+namespace PrestaShop\PrestaShop\Tests\Integration\PrestaShopBundle\Controller\Api;
+
+/**
+ * @group api
+ * @group supplier
+ */
+class I18nControllerTest extends ApiTestCase
+{
+    /**
+     * @dataProvider getBadListTranslations
+     * @test
+     *
+     * @param $params
+     */
+    public function it_should_return_bad_response_when_requesting_list_of_translations($params)
+    {
+        $route = $this->router->generate('api_i18n_translations_list', $params);
+        $this->client->request('GET', $route);
+
+        /** @var \Symfony\Component\HttpFoundation\Response $response */
+        $response = $this->client->getResponse();
+        $this->assertEquals(400, $response->getStatusCode(), 'It should return a response with "Bad Request" Status.');
+    }
+
+    /**
+     * @dataProvider getGoodListTranslations
+     * @test
+     *
+     * @param $params
+     */
+    public function it_should_return_ok_response_when_requesting_list_of_translations($params)
+    {
+        $route = $this->router->generate('api_i18n_translations_list', $params);
+        $this->client->request('GET', $route);
+
+        /** @var \Symfony\Component\HttpFoundation\Response $response */
+        $response = $this->client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode(), 'It should return a response with "OK" Status.');
+    }
+
+    /**
+     * @return array
+     */
+    public function getBadListTranslations()
+    {
+        return array(
+            array(
+                array('page' => 'internationnal'), // syntax error wanted
+            ),
+            array(
+                array('page' => 'stockk'), // syntax error wanted
+            ),
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function getGoodListTranslations()
+    {
+        return array(
+            array(
+                array('page' => 'international'),
+            ),
+            array(
+                array('page' => 'stock'),
+            ),
+        );
+    }
+}
