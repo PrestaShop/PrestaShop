c585f717358829b57535604c8bc38744c0a6c07e
Maxime Biloé <maxime.biloe@gmail.com>
2017-05-03 15:00:54 +0200

    BO: PSTree with or without checkbox
    (cherry picked from commit b1b8632a097450869df6042c940bc072dd5941b4)

--- admin-dev/themes/new-theme/js/app/widgets/ps-tree/ps-tree-item.vue
+++ admin-dev/themes/new-theme/js/app/widgets/ps-tree/ps-tree-item.vue
@@ -1,7 +1,25 @@
 <template>
   <div :class="className">
-    <PSCheckbox :ref="model.name" :id="id" :model="model" @checked="onCheck"/>
-    <span class="tree-label">{{model.name}}</span>
+    <div class="flex">
+      <div class="flex" :class="chevron" @click="toggle">
+        <i class="material-icons" v-if="open">keyboard_arrow_down</i>
+        <i class="material-icons" v-else>chevron_right</i>
+      </div>
+      <PSCheckbox :ref="model.name" :id="id" :model="model" @checked="onCheck" v-if="hasCheckbox"/>
+      <span class="tree-label">{{model.name}}</span>
+    </div>
+    <ul v-show="open" v-if="isFolder">
+      <li v-for="(element, index) in model.children">
+        <PSTreeItem
+          ref="item"
+          :class="className"
+          :hasCheckbox="hasCheckbox"
+          :model="element"
+          :label="element.name"
+          @checked="onCheck"
+        />
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -10,19 +28,38 @@
   import { EventBus } from 'app/utils/event-bus';
 
   export default {
-    props:['model','className'],
+    name: 'PSTreeItem',
+    props:['model','className', 'hasCheckbox'],
     computed: {
       id() {
         return this.model.id;
       },
+      isFolder() {
+        return this.model.children && this.model.children.length;
+      },
+      chevron() {
+        if(!this.isFolder) {
+          return 'hidden';
+        }
+      }
     },
     methods: {
+      toggle() {
+        if (this.isFolder) {
+          this.open = !this.open;
+        }
+      },
       onCheck(obj) {
         this.$emit('checked', obj);
       }
     },
     components: {
       PSCheckbox
+    },
+    data() {
+      return {
+        open: false
+      }
     },
     mounted() {
       EventBus.$on('toggleCheckbox', (tag) => {
@@ -36,8 +73,25 @@
 </script>
 
 <style lang="sass" scoped>
+  .tree {
+    .material-icons {
+      vertical-align: middle;
+      font-size: 20px;
+      cursor: pointer;
+    }
+  }
   .tree-label {
     margin-left: 5px;
     font-size: 12px;
+  }
+  .hidden {
+    visibility: hidden;
+  }
+  ul {
+    padding: 0 0 0 20px;
+  }
+  li {
+    margin: 5px 0;
+    list-style-type: none;
   }
 </style>
