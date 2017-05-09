14ef88cc265b2b04f90fa3b9c4d789b467e296ba
nihco2 <nicolas.martin@prestashop.com>
2017-05-09 11:20:39 +0200

    BO: Add datepicker

---
+++ admin-dev/themes/new-theme/js/app/widgets/ps-datepicker.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="input-group date">
+    <input type='text' class="form-control" />
+    <span class="input-group-addon">
+      <i class="material-icons">event</i>
+    </span>
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+       $(this.$el).datetimepicker().on('dp.change', (infos) =>{
+         this.$emit('dpChange', infos);
+       });
+    }
+  }
+</script>
