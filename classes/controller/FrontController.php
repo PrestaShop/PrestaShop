c09b15fd0e2846c3e73b657eb8815a060e26bb70
irder <service@tianex.com>
2017-05-10 14:58:13 +0800

    Update FrontController.php

--- classes/controller/FrontController.php
+++ classes/controller/FrontController.php
@@ -1651,10 +1651,10 @@ class FrontControllerCore extends Controller
         if (count($products_need_cache)) {
             $colors = Product::getAttributesColorList($products_need_cache);
         }
-
-        Tools::enableCache();
+        
         foreach ($products as &$product) {
             if (in_array($product['id_product'], $products_need_cache)) { 
+                Tools::enableCache();
                 $tpl = $this->context->smarty->createTemplate(_PS_THEME_DIR_.'product-list-colors.tpl', $this->getColorsListCacheId($product['id_product']));
                 if (isset($colors[$product['id_product']])) {
                     $tpl->assign(array(
@@ -1669,12 +1669,13 @@ class FrontControllerCore extends Controller
                 } else {
                     $product['color_list'] = '';
                 }
+                Tools::restoreCacheSettings();
             } else {
                 $tpl = $this->context->smarty->createTemplate(_PS_THEME_DIR_.'product-list-colors.tpl', $this->getColorsListCacheId($product['id_product']));
                 $product['color_list'] = $tpl->fetch(_PS_THEME_DIR_.'product-list-colors.tpl', $this->getColorsListCacheId($product['id_product']));
             }
         }
-        Tools::restoreCacheSettings();
+        
     }
 
     /**
