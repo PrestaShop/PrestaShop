7d456ddccac0adcd316739a4e46247c6bd4bba26
fatmaBouchekoua <fatma.bouchekoua@prestashop.com>
2017-06-19 14:40:12 +0100

    BO: Translate stores schedules, name and adress from BO

--- install-dev/upgrade/sql/1.7.3.0.sql
+++ install-dev/upgrade/sql/1.7.3.0.sql
@@ -13,3 +13,16 @@ ALTER TABLE `PREFIX_product_shop` ADD `low_stock_threshold` INT(10) NULL DEFAULT
 
 ALTER TABLE `PREFIX_product_attribute` ADD `low_stock_threshold` INT(10) NULL DEFAULT NULL AFTER `minimal_quantity`;
 ALTER TABLE `PREFIX_product_attribute_shop` ADD `low_stock_threshold` INT(10) NULL DEFAULT NULL AFTER `minimal_quantity`;
+
+CREATE TABLE IF NOT EXISTS `PREFIX_store_lang` (
+  `id_store` int(11) unsigned NOT NULL,
+  `id_lang` int(11) unsigned NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `address1` varchar(128) NOT NULL,
+  `address2` varchar(128) DEFAULT NULL,
+  `hours` text,
+  `note` text,
+  PRIMARY KEY (`id_store`, `id_lang`)
+) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
+
+/* PHP:migrate_data_from_store_to_store_lang_and_clean_store();
