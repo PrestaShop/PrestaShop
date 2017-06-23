fc5efdacb59504593c73f9a6e5a39ab2c7b52f2e
Alex Sampaio <alex.sampaio1991@gmail.com>
2017-06-23 14:07:56 +0200

    CO: clean db_structure

--- install-dev/data/db_structure.sql
+++ install-dev/data/db_structure.sql
@@ -1,15 +1,9 @@
 SET SESSION sql_mode = '';
 SET NAMES 'utf8';
 
+-- Can move to Entity, IDX_index created with doctrine..
 CREATE TABLE `PREFIX_accessory` (
   `id_product_1` int(10) unsigned NOT NULL,
   `id_product_2` int(10) unsigned NOT NULL,
   KEY `accessory_product` (`id_product_1`,`id_product_2`)
 ) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8 COLLATION;
-
-CREATE TABLE `PREFIX_group_shop` (
-`id_group` INT( 11 ) UNSIGNED NOT NULL,
-`id_shop` INT( 11 ) UNSIGNED NOT NULL,
-	PRIMARY KEY (`id_group`, `id_shop`),
-	KEY `id_shop` (`id_shop`)
-) ENGINE=ENGINE_TYPE  DEFAULT CHARSET=utf8 COLLATION;
