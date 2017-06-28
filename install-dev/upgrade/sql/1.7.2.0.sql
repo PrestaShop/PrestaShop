95b364018f43f572c8682e612f7e1fc01a7916f0
Alex Sampaio <alex.sampaio1991@gmail.com>
2017-06-28 10:13:05 +0200

    IN: fix update sql

--- install-dev/upgrade/sql/1.7.2.0.sql
+++ install-dev/upgrade/sql/1.7.2.0.sql
@@ -27,9 +27,9 @@ ALTER TABLE `PREFIX_product_attribute` ADD `isbn` VARCHAR(32) NULL DEFAULT NULL;
 ALTER TABLE `PREFIX_stock` ADD `isbn` VARCHAR(32) NULL DEFAULT NULL;
 ALTER TABLE `PREFIX_supply_order_detail` ADD `isbn` VARCHAR(32) NULL DEFAULT NULL;
 
-ALTER TABLE  `PREFIX_stock_available` ADD  `physical_quantity` INT NOT NULL DEFAULT  '0' AFTER  `quantity`;
-ALTER TABLE  `PREFIX_stock_available` ADD  `reserved_quantity` INT NOT NULL DEFAULT  '0' AFTER  `physical_quantity`;
-ALTER TABLE `ps_stock_mvt` CHANGE `id_stock` `id_stock` INT(11) UNSIGNED NOT NULL COMMENT 'since ps 1.7 corresponding to id_stock_available';
+ALTER TABLE `PREFIX_stock_available` ADD  `physical_quantity` INT NOT NULL DEFAULT  '0' AFTER  `quantity`;
+ALTER TABLE `PREFIX_stock_available` ADD  `reserved_quantity` INT NOT NULL DEFAULT  '0' AFTER  `physical_quantity`;
+ALTER TABLE `PREFIX_stock_mvt` CHANGE `id_stock` `id_stock` INT(11) UNSIGNED NOT NULL COMMENT 'since ps 1.7 corresponding to id_stock_available';
 
 UPDATE `PREFIX_configuration` SET `value` = 0 WHERE `name` = "PS_ADVANCED_STOCK_MANAGEMENT";
 /* PHP:add_new_status_stock(); */;
