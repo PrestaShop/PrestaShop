2f8535effa6454d999f797836ccc34f1b238f548
hibatallahAouadni <hibatallah.aouadni@sifast.com>
2017-06-30 12:14:37 +0100

    BO: Add unicode prime symbol in PDF

--- classes/pdf/PDFGenerator.php
+++ classes/pdf/PDFGenerator.php
@@ -52,6 +52,7 @@ class PDFGeneratorCore extends TCPDF
         'pl' => 'dejavusans',
         'ar' => 'dejavusans',
         'fa' => 'dejavusans',
+        'fr' => 'dejavusans',
         'ur' => 'dejavusans',
         'az' => 'dejavusans',
         'ca' => 'dejavusans',
@@ -147,7 +148,7 @@ class PDFGeneratorCore extends TCPDF
         if (array_key_exists($iso_lang, $this->font_by_lang)) {
             $this->font = $this->font_by_lang[$iso_lang];
         }else {
-            $this->font = self::DEFAULT_FONT;
+            $this->font = 'dejavusans';
         }
 
         $this->setHeaderFont(array($this->font, '', PDF_FONT_SIZE_MAIN, '', false));
