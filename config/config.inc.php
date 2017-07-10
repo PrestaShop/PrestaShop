c5767357e484b81fa00b40aa37d4dac248db8b0e
Presta Module <support@presta-module.com>
2017-07-10 10:54:17 +0200

    CO: remove duplicate require_once

--- config/config.inc.php
+++ config/config.inc.php
@@ -54,7 +54,6 @@ if (!file_exists(_PS_ROOT_DIR_.'/app/config/parameters.yml') && !file_exists(_PS
 
 /* include settings file only if we are not in multi-tenancy mode */
 require_once(_PS_ROOT_DIR_.'/config/settings.inc.php');
-require_once(_PS_CONFIG_DIR_.'autoload.php');
 require_once $currentDir . DIRECTORY_SEPARATOR . 'bootstrap.php';
 
 /* Improve PHP configuration on Windows */
