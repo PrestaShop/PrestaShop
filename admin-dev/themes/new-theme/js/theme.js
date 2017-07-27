4125a85a3ddd2c89d2da13e019de55c30878ae68
Maxime Biloé <maxime.biloe@gmail.com>
2017-07-27 15:12:11 +0200

    BO: Use the new UI Kit

--- admin-dev/themes/new-theme/js/theme.js
+++ admin-dev/themes/new-theme/js/theme.js
@@ -29,8 +29,6 @@ import 'dropzone/dist/min/dropzone.min.css';
 
 import 'magnific-popup/dist/magnific-popup.css';
 import 'PrestaKit/dist/css/bootstrap-prestashop-ui-kit.css';
-import 'PrestaKit/dist/css/jquery.growl.css';
-import 'PrestaKit/dist/css/bootstrap-switch.min.css';
 
 // Theme SCSS
 
