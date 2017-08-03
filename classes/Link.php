a6d8e5bba98f88790eb8432c36acb2624bf0cf92
Alex Sampaio <alex.sampaio1991@gmail.com>
2017-08-03 10:39:21 +0200

    CO: refactoring Link.php with kernel

--- classes/Link.php
+++ classes/Link.php
@@ -686,15 +686,15 @@ class LinkCore
 
         // Even if URL rewriting is not enabled, the page handled by Symfony must work !
         // For that, we add an 'index.php' in the URL before the route
-        global $kernel; // sf kernel
-        if ($kernel instanceof Symfony\Component\HttpKernel\HttpKernelInterface) {
-            $sfRouter = $kernel->getContainer()->get('router');
+        $sfContainer = SymfonyContainer::getSfContainer();
+        if ($sfContainer) {
+            $sfRouter = $sfContainer->get('router');
         }
 
         switch ($controller) {
             case 'AdminProducts':
                 // New architecture modification: temporary behavior to switch between old and new controllers.
-                $pagePreference = $kernel->getContainer()->get('prestashop.core.admin.page_preference_interface');
+                $pagePreference = $sfContainer->getContainer()->get('prestashop.core.admin.page_preference_interface');
                 $redirectLegacy = $pagePreference->getTemporaryShouldUseLegacyPage('product');
                 if (!$redirectLegacy) {
                     if (array_key_exists('id_product', $sfRouteParams)) {
@@ -1306,9 +1306,9 @@ class LinkCore
                 if (!array_key_exists('route', $params)) {
                     throw new \InvalidArgumentException('You need to setup a `route` attribute.');
                 }
-                global $kernel; // sf kernel
-                if ($kernel instanceof Symfony\Component\HttpKernel\HttpKernelInterface) {
-                    $sfRouter = $kernel->getContainer()->get('router');
+                $sfContainer = SymfonyContainer::getSfContainer();
+                if ($sfContainer) {
+                    $sfRouter = $sfContainer->get('router');
 
                     if (array_key_exists('sf-params', $params)) {
                         return $sfRouter->generate($params['route'], $params['sf-params'], UrlGeneratorInterface::ABSOLUTE_URL);
