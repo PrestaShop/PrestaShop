8f1265d40d55ecb3b9f9cb9011c2b274c00bdc97
nihco2 <nicolas.martin@prestashop.com>
2017-08-03 10:49:49 +0200

    BO: compiled assets

--- admin-dev/themes/new-theme/public/main.bundle.js
+++ admin-dev/themes/new-theme/public/main.bundle.js
@@ -59,7 +59,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "50611892a31ae9de3257"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "39df9d4daa260ab5f475"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
