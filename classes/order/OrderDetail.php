6ca0ad5a20469f328ec97ced1541c0f154b08b86
mounir.boukhris <mounir.boukhris@sifast.com>
2017-08-08 16:42:27 +0200

    BO: fix stock available after add product in order

--- classes/order/OrderDetail.php
+++ classes/order/OrderDetail.php
@@ -468,10 +468,7 @@ class OrderDetailCore extends ObjectModel
     protected function checkProductStock($product, $id_order_state)
     {
         if ($id_order_state != Configuration::get('PS_OS_CANCELED') && $id_order_state != Configuration::get('PS_OS_ERROR')) {
-            $update_quantity = true;
-            if (!StockAvailable::dependsOnStock($product['id_product'])) {
-                $update_quantity = StockAvailable::updateQuantity($product['id_product'], $product['id_product_attribute'], -(int)$product['cart_quantity']);
-            }
+            $update_quantity = StockAvailable::updateQuantity($product['id_product'], $product['id_product_attribute'], -(int)$product['cart_quantity']);
 
             if ($update_quantity) {
                 $product['stock_quantity'] -= $product['cart_quantity'];
