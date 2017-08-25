77f98e831312c3d58558037c6bd53bccff7f1466
Thomas Nabord <thomas.nabord@prestashop.com>
2017-08-25 09:40:13 +0100

    CO: Remove deprecated calls to create_function()

--- classes/Tools.php
+++ classes/Tools.php
@@ -2055,7 +2055,7 @@ class ToolsCore
 
     private static function toCamelCaseCallback(array $matches)
     {
-      return strtoupper($matches[1]);
+        return strtoupper($matches[1]);
     }
 
     /**
@@ -3828,7 +3828,7 @@ exit;
             return;
         }
 
-        $sort_function = create_function('$a, $b', "return \$b['$column'] > \$a['$column'] ? 1 : -1;");
+        $sort_function = function($a, $b) use ($column) { return $b[$column] > $a[$column] ? 1 : -1; };
 
         uasort($rows, $sort_function);
 
