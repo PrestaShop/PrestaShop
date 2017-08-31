d363fff6a6062565b3d94a6b84476738d3fdbf75
Thomas Nabord <thomas.nabord@prestashop.com>
2017-08-31 10:49:36 +0100

    TE: Add functional tests in travis script

---
+++ travis-scripts/run-functional-tests.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ $EXTRA_TESTS != *"functional"* ]]; then
+  exit 0
+fi
+
+bash $TRAVIS_BUILD_DIR/travis-scripts/install-prestashop
+
+echo "* Installing functional tests ...";
+
+cd $TRAVIS_BUILD_DIR && echo "<?php\n\ndefine('_PS_MODE_DEV_', false);" > config/defines_custom.inc.php # Disable DEV MODE
+
+git clone https://github.com/PrestaShop/PSFunctionalTests.git
+cd $TRAVIS_BUILD_DIR/PSFunctionalTests && npm install && cd test/itg/1.6
+
+echo "* Running functional tests ...";
+$TRAVIS_BUILD_DIR/PSFunctionalTests/node_modules/mocha/bin/mocha index.webdriverio.js -c --URL=localhost -t 100000
+exit $?
