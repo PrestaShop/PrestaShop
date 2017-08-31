2a0616b68f6033911fba9ba47aa201453e483340
Thomas Nabord <thomas.nabord@prestashop.com>
2017-08-31 14:46:08 +0100

    TE: Switch back to precise travis build and install chrome manually

--- travis-scripts/run-functional-tests.sh
+++ travis-scripts/run-functional-tests.sh
@@ -4,13 +4,13 @@ if [[ $EXTRA_TESTS != *"functional"* ]]; then
   exit 0
 fi
 
-echo "* Installing functional tests ...";
+echo "<?php define('_PS_MODE_DEV_', false);" > config/defines_custom.inc.php
 
-cd $TRAVIS_BUILD_DIR && echo "<?php\n\ndefine('_PS_MODE_DEV_', false);" > config/defines_custom.inc.php # Disable DEV MODE
+echo "* Installing functional tests ...";
 
 git clone https://github.com/PrestaShop/PSFunctionalTests.git
 cd $TRAVIS_BUILD_DIR/PSFunctionalTests && npm install && cd test/itg/1.6
 
 echo "* Running functional tests ...";
-$TRAVIS_BUILD_DIR/PSFunctionalTests/node_modules/mocha/bin/mocha index.webdriverio.js -c --URL=localhost -t 100000
+$TRAVIS_BUILD_DIR/PSFunctionalTests/node_modules/mocha/bin/mocha index.webdriverio.js -c --URL=localhost  --MODULE=None --SAUCELABS=None -t 200000
 exit $?
