7502a68223ed820e419415a981273fca3d58c6a3
Alex Sampaio <alex.sampaio1991@gmail.com>
2017-09-05 14:03:13 +0200

    BO: upgrade sql quick access

--- install-dev/upgrade/sql/1.7.3.0.sql
+++ install-dev/upgrade/sql/1.7.3.0.sql
@@ -4,4 +4,6 @@ SET NAMES 'utf8';
 UPDATE `PREFIX_tab` SET `position` = 0 WHERE `class_name` = 'AdminZones' AND `position` = '1';
 UPDATE `PREFIX_tab` SET `position` = 1 WHERE `class_name` = 'AdminCountries' AND `position` = '0';
 
+/* PHP:ps_1730_add_quick_access_evaluation_catalog(); */;
+
 /* PHP:ps_1730_move_some_aeuc_configuration_to_core(); */;
