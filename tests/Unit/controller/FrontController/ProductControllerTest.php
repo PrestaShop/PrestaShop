03e3fa767a39b5752826a52e3da7f68a6378eaa5
Thomas LEVIANDIER <thomas.leviandier@prestashop.com>
2017-09-05 12:02:45 +0200

    fix long array syntax / factorize some code

--- tests/Unit/controller/FrontController/ProductControllerTest.php
+++ tests/Unit/controller/FrontController/ProductControllerTest.php
@@ -63,7 +63,7 @@ class ProductControllerTest extends IntegrationTestCase
      *
      * @return mixed Method return.
      */
-    public function invokeMethod($object, $methodName, array $parameters = [])
+    public function invokeMethod($object, $methodName, array $parameters = array())
     {
         $reflection = new \ReflectionClass(get_class($object));
         $method     = $reflection->getMethod($methodName);
@@ -121,187 +121,109 @@ class ProductControllerTest extends IntegrationTestCase
 
     public function specificPricesProvider()
     {
-        return [
+
+        $specificPrices = array(
+            0 => array(
+                'id_specific_price'      => '9',
+                'id_specific_price_rule' => '0',
+                'id_cart'                => '0',
+                'id_product'             => '10',
+                'id_shop'                => '1',
+                'id_shop_group'          => '0',
+                'id_currency'            => '0',
+                'id_country'             => '0',
+                'id_group'               => '0',
+                'id_customer'            => '0',
+                'id_product_attribute'   => '0',
+                'price'                  => '15.000000',
+                'from_quantity'          => '15',
+                'reduction'              => 0,
+                'reduction_tax'          => '1',
+                'reduction_type'         => 'amount',
+                'from'                   => '0000-00-00 00:00:00',
+                'to'                     => '0000-00-00 00:00:00',
+                'score'                  => '48',
+                'quantity'               => '15',
+                'reduction_with_tax'     => 0,
+                'nextQuantity'           => -1,
+            ),
+        );
+        $currencyEur = array(
+            'conversion_rate' => 1.0,
+            'sign'            => '€',
+            'code'            => 'EUR',
+        );
+        $currencyDol = array(
+            'conversion_rate' => 1.3,
+            'sign'            => '$',
+            'code'            => 'USD',
+        );
+
+        return array(
             // EUR to USD, without ecotax
-            [
-                [
+            array(
+                array(
                     'price'           => 31.2,
                     'tax_rate'        => 20,
                     'ecotax_amount'   => 0,
-                    'currency'        => [
-                        'conversion_rate' => 1.3,
-                        'sign'            => '$',
-                        'code'            => 'USD',
-                    ],
-                    'specific_prices' => [
-                        0 => [
-                            'id_specific_price'      => '9',
-                            'id_specific_price_rule' => '0',
-                            'id_cart'                => '0',
-                            'id_product'             => '10',
-                            'id_shop'                => '1',
-                            'id_shop_group'          => '0',
-                            'id_currency'            => '0',
-                            'id_country'             => '0',
-                            'id_group'               => '0',
-                            'id_customer'            => '0',
-                            'id_product_attribute'   => '0',
-                            'price'                  => '15.000000',
-                            'from_quantity'          => '15',
-                            'reduction'              => 0,
-                            'reduction_tax'          => '1',
-                            'reduction_type'         => 'amount',
-                            'from'                   => '0000-00-00 00:00:00',
-                            'to'                     => '0000-00-00 00:00:00',
-                            'score'                  => '48',
-                            'quantity'               => '15',
-                            'reduction_with_tax'     => 0,
-                            'nextQuantity'           => -1,
-                        ],
-                    ],
-                    'expected'        => [
-                        [
+                    'currency'        => $currencyDol,
+                    'specific_prices' => $specificPrices,
+                    'expected'        => array(
+                        array(
                             'discount' => 7.80,
                             'save'     => 117.00,
-                        ],
-                    ],
-                ],
-            ],
+                        ),
+                    ),
+                ),
+            ),
             // EUR to EUR, without ecotax
-            [
-                [
+            array(
+                array(
                     'price'           => 24,
                     'tax_rate'        => 20,
                     'ecotax_amount'   => 0,
-                    'currency'        => [
-                        'conversion_rate' => 1.0,
-                        'sign'            => '€',
-                        'code'            => 'EUR',
-                    ],
-                    'specific_prices' => [
-                        0 => [
-                            'id_specific_price'      => '9',
-                            'id_specific_price_rule' => '0',
-                            'id_cart'                => '0',
-                            'id_product'             => '10',
-                            'id_shop'                => '1',
-                            'id_shop_group'          => '0',
-                            'id_currency'            => '0',
-                            'id_country'             => '0',
-                            'id_group'               => '0',
-                            'id_customer'            => '0',
-                            'id_product_attribute'   => '0',
-                            'price'                  => '15.000000',
-                            'from_quantity'          => '15',
-                            'reduction'              => 0,
-                            'reduction_tax'          => '1',
-                            'reduction_type'         => 'amount',
-                            'from'                   => '0000-00-00 00:00:00',
-                            'to'                     => '0000-00-00 00:00:00',
-                            'score'                  => '48',
-                            'quantity'               => '15',
-                            'reduction_with_tax'     => 0,
-                            'nextQuantity'           => -1,
-                        ],
-                    ],
-                    'expected'        => [
-                        [
+                    'currency'        => $currencyEur,
+                    'specific_prices' => $specificPrices,
+                    'expected'        => array(
+                        array(
                             'discount' => 6.00,
                             'save'     => 90.00,
-                        ],
-                    ],
-                ],
-            ],
+                        ),
+                    ),
+                ),
+            ),
             // EUR to USD, with ecotax
-            [
-                [
+            array(
+                array(
                     'price'           => 31.2,
                     'tax_rate'        => 20,
                     'ecotax_amount'   => 0.9,
-                    'currency'        => [
-                        'conversion_rate' => 1.3,
-                        'sign'            => '$',
-                        'code'            => 'USD',
-                    ],
-                    'specific_prices' => [
-                        0 => [
-                            'id_specific_price'      => '9',
-                            'id_specific_price_rule' => '0',
-                            'id_cart'                => '0',
-                            'id_product'             => '10',
-                            'id_shop'                => '1',
-                            'id_shop_group'          => '0',
-                            'id_currency'            => '0',
-                            'id_country'             => '0',
-                            'id_group'               => '0',
-                            'id_customer'            => '0',
-                            'id_product_attribute'   => '0',
-                            'price'                  => '15.000000',
-                            'from_quantity'          => '15',
-                            'reduction'              => 0,
-                            'reduction_tax'          => '1',
-                            'reduction_type'         => 'amount',
-                            'from'                   => '0000-00-00 00:00:00',
-                            'to'                     => '0000-00-00 00:00:00',
-                            'score'                  => '48',
-                            'quantity'               => '15',
-                            'reduction_with_tax'     => 0,
-                            'nextQuantity'           => -1,
-                        ],
-                    ],
-                    'expected'        => [
-                        [
+                    'currency'        => $currencyDol,
+                    'specific_prices' => $specificPrices,
+                    'expected'        => array(
+                        array(
                             'discount' => 6.63,
                             'save'     => 99.45,
-                        ],
-                    ],
-                ],
-            ],
+                        ),
+                    ),
+                ),
+            ),
             // EUR to EUR, with ecotax
-            [
-                [
+            array(
+                array(
                     'price'           => 24,
                     'tax_rate'        => 20,
                     'ecotax_amount'   => 0.9,
-                    'currency'        => [
-                        'conversion_rate' => 1.0,
-                        'sign'            => '€',
-                        'code'            => 'EUR',
-                    ],
-                    'specific_prices' => [
-                        0 => [
-                            'id_specific_price'      => '9',
-                            'id_specific_price_rule' => '0',
-                            'id_cart'                => '0',
-                            'id_product'             => '10',
-                            'id_shop'                => '1',
-                            'id_shop_group'          => '0',
-                            'id_currency'            => '0',
-                            'id_country'             => '0',
-                            'id_group'               => '0',
-                            'id_customer'            => '0',
-                            'id_product_attribute'   => '0',
-                            'price'                  => '15.000000',
-                            'from_quantity'          => '15',
-                            'reduction'              => 0,
-                            'reduction_tax'          => '1',
-                            'reduction_type'         => 'amount',
-                            'from'                   => '0000-00-00 00:00:00',
-                            'to'                     => '0000-00-00 00:00:00',
-                            'score'                  => '48',
-                            'quantity'               => '15',
-                            'reduction_with_tax'     => 0,
-                            'nextQuantity'           => -1,
-                        ],
-                    ],
-                    'expected'        => [
-                        [
+                    'currency'        => $currencyEur,
+                    'specific_prices' => $specificPrices,
+                    'expected'        => array(
+                        array(
                             'discount' => 5.10,
                             'save'     => 76.50,
-                        ],
-                    ],
-                ],
-            ],
-        ];
+                        ),
+                    ),
+                ),
+            ),
+        );
     }
 }
