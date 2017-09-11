d06b9073b2e64138958514b2eeea68c6c07c59a9
Yisrael Dov Lebow <lebow@lebowtech.com>
2017-09-11 21:01:04 +0300

    Removing RTL.js it breaks image uploads
    rtl.js breaks image uploads among other things and makes the backend not usable in hebrew.

--- classes/controller/AdminController.php
+++ classes/controller/AdminController.php
@@ -2269,7 +2269,6 @@ class AdminControllerCore extends Controller
         //rtl.js overrides inline styles
         //iso_code.css overrides default fonts for every language (optional)
         if ($this->context->language->is_rtl) {
-            $this->addJS(_PS_JS_DIR_.'rtl.js');
             $this->addCSS(__PS_BASE_URI__.$this->admin_webpath.'/themes/'.$this->bo_theme.'/css/'.$this->context->language->iso_code.'.css', 'all', false);
         }
 
