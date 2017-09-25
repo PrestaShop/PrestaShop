c2500e3fa45bcf7211ecb714137d864cc95f3327
Mickaël Andrieu <andrieu.travail@gmail.com>
2017-09-25 23:51:12 +0200

    BO: started Administration page migration

---
+++ src/PrestaShopBundle/Form/Admin/AdvancedParameters/Administration/GeneralForm.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * 2007-2017 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2017 PrestaShop SA
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+namespace PrestaShopBundle\Form\Admin\AdvancedParameters\Administration;
+
+use PrestaShopBundle\Form\Admin\Type\TranslatorAwareType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GeneralForm extends TranslatorAwareType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('check_modules_update', 'Symfony\Component\Form\Extension\Core\Type\ChoiceType', array(
+                'choices'  => array(
+                    false => 'No',
+                    true => 'Yes',
+                ),
+                'choice_translation_domain' => 'Admin.Global',
+                'required' => true,
+            ))
+            ->add('check_ip_address', 'Symfony\Component\Form\Extension\Core\Type\ChoiceType', array(
+                'choices'  => array(
+                    false => 'No',
+                    true => 'Yes',
+                ),
+                'choice_translation_domain' => 'Admin.Global',
+                'required' => true,
+            ))
+            ->add('front_cookie_lifetime', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
+                'required' => true,
+            ))
+            ->add('back_cookie_lifetime', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
+                'required' => true,
+            ))
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'translation_domain' => 'Admin.Advparameters.Feature',
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'administration_general_block';
+    }
+}
