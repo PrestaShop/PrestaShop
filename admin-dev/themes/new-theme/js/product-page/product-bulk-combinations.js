fb6b7b0cc8599da740796aa5a1dd39c1936fce1f
Thomas LEVIANDIER <thomas.leviandier@prestashop.com>
2017-10-10 10:30:29 +0200

    BO: CR/QA fixes

--- admin-dev/themes/new-theme/js/product-page/product-bulk-combinations.js
+++ admin-dev/themes/new-theme/js/product-page/product-bulk-combinations.js
@@ -208,7 +208,12 @@ class Combination {
   updateForm(values) {
     values.forEach((valueObject) => {
       var valueId = valueObject.id.substr(this.inputBulkPattern.length);
-      $('#'+this.convertInput(valueId)).val(valueObject.value);
+      var $field = $('#'+this.convertInput(valueId));
+      if ($field.is(':checkbox')) {
+          $field.prop("checked", !!valueObject.value);
+      } else {
+          $field.val(valueObject.value);
+      }
     });
     return this.form;
   }
