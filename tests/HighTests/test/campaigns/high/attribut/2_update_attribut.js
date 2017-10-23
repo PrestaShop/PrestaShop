ad5063f87220abc3f5dacaeb238665e72609617a
fouratachour <fourat.ach@gmail.com>
2017-10-23 15:13:02 +0100

    Delete attribute/attribute value - high test

--- tests/HighTests/test/campaigns/high/attribut/2_update_attribut.js
+++ tests/HighTests/test/campaigns/high/attribut/2_update_attribut.js
@@ -1,12 +1,11 @@
-scenario('Create attribut', client => {
+scenario('Update attribut', client => {
   test('should open the browser', () => client.open());
   test('should sign in BO', () => client.signinBO());
   test('should go to attribut list', () => client.goToAttributList());
   test('should search for the created attribut', () => client.searchAttribut());
-  test('should select the created attribut', () => client.selectAttribut());
-  test('should add value to the created attribut', () => client.addValueToAttribut());
-
-  test('should check attribut success panel', () => client.SuccessPanel('Création réussie.'));
+  test('should update attribut name', () => client.updateAttributName());
+  test('should update attribut value', () => client.updateAttributValue());
+  test('should check attribut success panel', () => client.SuccessPanel('Mise à jour réussie.'));
   test('should sign out BO', () => client.signoutBO());
 }, 'attribut', true);
 
@@ -15,6 +14,7 @@ scenario('The Check of the Product attribute in Front Office', client => {
   test('should open the browser', () => client.open());
   test('should sign in BO', () => client.signinFO());
   test('should search for the product', () => client.searchForProduct());
-  test('should Check the Product attribute in Front Office', () => client.checkForProductAttributFO('create'));
-
+  test('should Check updated attribute in Front Office', () => client.checkForProductAttributFO());
 }, 'attribut', true);
+
+
