fd5af915b7a30e54791fb5aa077fc97690657f1d
fouratachour <fourat.ach@gmail.com>
2017-10-27 12:03:46 +0100

    Code refactoring

--- tests/HighTests/test/campaigns/high/catalogbulkaction/catalogbulkaction.js
+++ tests/HighTests/test/campaigns/high/catalogbulkaction/catalogbulkaction.js
@@ -6,28 +6,28 @@ scenario('Catalog bulk action', () => {
     test('should go to catalog list', () => client.goToCatalog());
   }, 'catalogbulkaction');
 
-  scenario('Deactivate the product list', client => {
+  scenario('Disable the product list', client => {
     test('should select all product', () => client.selectAllProduct());
-    test('should Disable the product list', () => client.disableProductlist());
-    test('should check deactivated product ', () => client.checkProductListMsg('Product(s) successfully deactivated.','clear'));
+    test('should disable all products', () => client.disableProductlist());
+    test('should check that all products have been disabled ', () => client.checkProductListMsg('Product(s) successfully deactivated.', 'clear'));
   }, 'catalogbulkaction');
 
-  scenario('Activate the product list', client => {
+  scenario('Enable the product list', client => {
     test('should select all product', () => client.selectAllProduct());
     test('should enable the product list', () => client.enableProductlist());
-    test('should check activated product ', () => client.checkProductListMsg('Product(s) successfully activated.', 'check'));
+    test('should check that all products have been enabled ', () => client.checkProductListMsg('Product(s) successfully activated.', 'check'));
   }, 'catalogbulkaction');
 
   scenario('Duplicate the product list', client => {
     test('should select all product', () => client.selectAllProduct());
     test('should duplicate the product list', () => client.duplicateProductlist());
-    test('should check duplicated product ', () => client.checkProductListMsg('Product(s) successfully duplicated.', 'clear'));
+    test('should check that all products have been duplicated ', () => client.checkProductListMsg('Product(s) successfully duplicated.', 'clear'));
   }, 'catalogbulkaction');
 
-  scenario('Activate the product list', client => {
+  scenario('Enable the product list', client => {
     test('should select all product', () => client.selectAllProduct());
     test('should enable the product list', () => client.enableProductlist());
-    test('should check activated product ', () => client.checkProductListMsg('Product(s) successfully activated.', 'check'));
+    test('should check that all products have been enabled', () => client.checkProductListMsg('Product(s) successfully activated.', 'check'));
   }, 'catalogbulkaction');
 
 }, 'catalogbulkaction', true);
