065cb194dabf8149d1027a1b187aaa2fd813b7a0
fouratachour <fourat.ach@gmail.com>
2017-11-01 10:20:36 +0100

    Change the quantity for a single product

--- tests/HighTests/test/clients/quantity.js
+++ tests/HighTests/test/clients/quantity.js
@@ -34,21 +34,67 @@ class ModifyQuantity extends PrestashopClient {
       .then((text) => expect(text.substring(14)).to.be.equal((Number(global.productQuantity) + 50).toString()));
   }
 
-  modifyThirdProductQuantity() {
+  saveGroupProduct() {
+    return this.client
+      .waitForExist(selector.BO.CatalogPage.StockSubmenu.group_apply_button, 90000)
+      .click(selector.BO.CatalogPage.StockSubmenu.group_apply_button)
+      .pause(1000)
+      .then(() => this.client.getText(selector.BO.CatalogPage.StockSubmenu.success_panel))
+      .then((text) => expect(text.substring(2)).to.be.equal('Stock successfully updated'))
+  }
+
+  getThirdProductQuantity() {
     return this.client
       .waitForExist(selector.BO.CatalogPage.StockSubmenu.third_product_quantity, 90000)
       .then(() => this.client.getText(selector.BO.CatalogPage.StockSubmenu.third_product_quantity))
       .then((text) => global.productQuantity = text)
-
-      .click(selector.BO.CatalogPage.StockSubmenu.submenu)
   }
 
-  save() {
+  modifyThirdProductQuantity() {
     return this.client
-      .waitForExist(selector.BO.CatalogPage.StockSubmenu.save_button, 90000)
-      .click(selector.BO.CatalogPage.StockSubmenu.save_button)
+      .pause(4000)
+      .click(selector.BO.CatalogPage.StockSubmenu.third_product_quantity_input)
+      .click(selector.BO.CatalogPage.StockSubmenu.add_quantity_button)
+      .click(selector.BO.CatalogPage.StockSubmenu.add_quantity_button)
+      .click(selector.BO.CatalogPage.StockSubmenu.add_quantity_button)
+      .then(() => this.client.getText(selector.BO.CatalogPage.StockSubmenu.third_product_quantity_modified))
+      .then((text) => expect(text.substring(14)).to.be.equal((Number(global.productQuantity) + 3).toString()))
   }
 
+  saveThirdProduct() {
+    return this.client
+      .waitForExist(selector.BO.CatalogPage.StockSubmenu.save_third_product_quantity_button, 90000)
+      .click(selector.BO.CatalogPage.StockSubmenu.save_third_product_quantity_button)
+      .pause(1000)
+      .then(() => this.client.getText(selector.BO.CatalogPage.StockSubmenu.success_panel))
+      .then((text) => expect(text.substring(2)).to.be.equal('Stock successfully updated'))
+  }
+
+  getFourthProductQuantity() {
+    return this.client
+      .waitForExist(selector.BO.CatalogPage.StockSubmenu.fourth_product_quantity, 90000)
+      .then(() => this.client.getText(selector.BO.CatalogPage.StockSubmenu.fourth_product_quantity))
+      .then((text) => global.productQuantity = text)
+  }
+
+  modifyFourthProductQuantity() {
+    return this.client
+      .pause(4000)
+      .click(selector.BO.CatalogPage.StockSubmenu.fourth_product_quantity_input)
+      .waitForExist(selector.BO.CatalogPage.StockSubmenu.fourth_product_quantity_input, 90000)
+      .setValue(selector.BO.CatalogPage.StockSubmenu.fourth_product_quantity_input, '-50')
+      .then(() => this.client.getText(selector.BO.CatalogPage.StockSubmenu.fourth_product_quantity_modified))
+      .then((text) => expect(text.substring(14)).to.be.equal((Number(global.productQuantity) - 50).toString()))
+  }
+
+  saveFourthProduct() {
+    return this.client
+      .waitForExist(selector.BO.CatalogPage.StockSubmenu.save_fourth_product_quantity_button, 90000)
+      .click(selector.BO.CatalogPage.StockSubmenu.save_fourth_product_quantity_button)
+      .pause(1000)
+      .then(() => this.client.getText(selector.BO.CatalogPage.StockSubmenu.success_panel))
+      .then((text) => expect(text.substring(2)).to.be.equal('Stock successfully updated'))
+  }
 }
 
 module.exports = ModifyQuantity;
