5e3273ab6ba83767012f5f49e4c8c7ff17aeb153
fouratachour <fourat.ach@gmail.com>
2017-11-01 10:20:36 +0100

    Change the quantity for a single product

--- tests/HighTests/test/campaigns/high/modify_quantity/modify_quantity.js
+++ tests/HighTests/test/campaigns/high/modify_quantity/modify_quantity.js
@@ -2,20 +2,23 @@ scenario('Check modify quantity', () => {
 
   scenario('Log in back office and go to catalog stock', client => {
     test('should open the browser', () => client.open());
-    test('should sign in BO', () => client.signinBO());
+    test('should sign in Back Office', () => client.signinBO());
     test('should go to stock list', () => client.goToCatalogStock());
   }, 'quantity');
 
-  scenario('Change the quantity of group of product', client => {
-    test('should modify first product quantity', () => client.modifyFirstProductQuantity());
-    test('should modify third product quantity', () => client.modifySecondProductQuantity());
-    test('should save the quantity of the product ', () => client.save());
+  scenario('Change group of product quantity', client => {
+    test('should change the quantity of the first product', () => client.modifyFirstProductQuantity());
+    test('should change the quantity of the second product', () => client.modifySecondProductQuantity());
+    test('should save the quantity of the product and check the success message', () => client.saveGroupProduct());
   }, 'quantity');
 
   scenario('Change the quantity of single product ', client => {
-    test('should modify first product quantity', () => client.modifyThirdProductQuantity());
-
-    test('should save the quantity of the product ', () => client.save());
+    test('should get the quantity of the third product', () => client.getThirdProductQuantity());
+    test('should change the quantity of the third product', () => client.modifyThirdProductQuantity());
+    test('should save the quantity of the third product', () => client.saveThirdProduct());
+    test('should get the quantity of the fourth product', () => client.getFourthProductQuantity());
+    test('should change the quantity of the fourth product', () => client.modifyFourthProductQuantity());
+    test('should save the quantity of the fourth product', () => client.saveFourthProduct());
   }, 'quantity');
 
 }, 'quantity',true);
