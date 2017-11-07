aa5c78b53304b71d3ec138a942a79a3ccceddaf6
comxd <direction@comexpertise.com>
2017-11-07 19:46:40 +0100

    requested changes by @LittleBigDev

--- install-dev/upgrade/sql/1.7.2.3.sql
+++
@@ -1,5 +0,0 @@
-SET SESSION sql_mode = '';
-SET NAMES 'utf8';
-
-INSERT IGNORE INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `position`) VALUES
-  (NULL, 'displayAdminCustomersAddressesItemAction', 'Display new elements in the Back Office, tab AdminCustomers, Addresses actions', 'This hook launches modules when the Addresses list into the AdminCustomers tab is displayed in the Back Office', '1');
