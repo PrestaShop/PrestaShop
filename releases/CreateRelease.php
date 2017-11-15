78e74075426492e60b1a3ba038c1d630cdb07e2a
Alban L <alban.legout@gmail.com>
2017-11-15 16:50:17 +0100

    CO: Fix issues, code refacto.

--- releases/CreateRelease.php
+++ releases/CreateRelease.php
@@ -2,6 +2,7 @@
 
 require_once 'Libraries/ReleaseCreator.php';
 require_once 'Libraries/functions.php';
+require_once 'Exception/BuildException.php';
 
 $options = getopt('', ['version:']);
 
@@ -9,7 +10,7 @@ if (empty($options['version'])) {
     echo "\e[31mERROR:\n";
     echo "'version' option missing.\n";
     echo "\e[32mExample:\n";
-    echo "'php build/CreateZip.php --version='1.7.2'\e[0m\n";
+    echo "'php CreateRelease.php --version='1.7.2.4'\e[0m\n";
     exit(1);
 }
 
@@ -18,7 +19,7 @@ try {
 } catch (Exception $e) {
     echo "\e[31mERROR:\n";
     echo "Can not create the release.\n";
-    echo "$e->getMessage()\e[0m\n";
+    echo "{$e->getMessage()}\e[0m\n";
     exit(1);
 }
 exit(0);
