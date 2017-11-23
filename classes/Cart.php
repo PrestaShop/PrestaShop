59e7723ec55c69764dcd9bb694c273f9aaedb794
Mickaël Andrieu <andrieu.travail@gmail.com>
2017-11-23 15:30:28 +0100

    Fixed indentation

--- classes/Cart.php
+++ classes/Cart.php
@@ -1357,7 +1357,7 @@ class CartCore extends ObjectModel
         $context = Context::getContext()->cloneContext();
         $context->cart = $this;
         Cache::clean('getContextualValue_*');
-		CartRule::autoRemoveFromCart();
+        CartRule::autoRemoveFromCart();
         if ($auto_add_cart_rule) {
             CartRule::autoAddToCart($context);
         }
