1c6fe275396913ecce4985c8790b5a219b575bde
Maxime Morel-Bailly <maxime@upilot.com>
2017-11-24 21:15:44 +0400

    CO: Add missing Phpdoc in SpecificPrice for method exists

--- classes/SpecificPrice.php
+++ classes/SpecificPrice.php
@@ -702,6 +702,23 @@ class SpecificPriceCore extends ObjectModel
         return $feature_active;
     }
 
+    /**
+     * Check if a specific price exists based on given parameters and return the specific rule id.
+     * 
+     * @param  int  $id_product
+     * @param  int  $id_product_attribute Set at 0 when the specific price was set for all attributes
+     * @param  int  $id_shop              Set at 0 when the specific price was set for all shops
+     * @param  int  $id_group             Set at 0 when the specific price was set for all groups
+     * @param  int  $id_country           Set at 0 when the specific price was set for all countries
+     * @param  int  $id_currency          Set at 0 when the specific price was set for all currencies
+     * @param  int  $id_customer          Set at 0 when the specific price was set for all customers
+     * @param  int  $from_quantity        The starting quantity for which the specific price is applied
+     * @param  string $from               Date from which the specific price start. 0000-00-00 00:00:00 if no starting date
+     * @param  string $to                 Date from which the specific price end. 0000-00-00 00:00:00 if no ending date
+     * @param  boolean $rule              If a specific price rule (from specific_price_rule) was set or not.
+     * 
+     * @return int                        The specific rule id, 0 if no corresponding rule found
+     */
     public static function exists($id_product, $id_product_attribute, $id_shop, $id_group, $id_country, $id_currency, $id_customer, $from_quantity, $from, $to, $rule = false)
     {
         $rule = ' AND `id_specific_price_rule`'.(!$rule ? '=0' : '!=0');
