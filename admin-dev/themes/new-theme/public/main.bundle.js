65e2c208dc910a8776595a5611eeed8edce37a0f
Pablo Borowicz <eternoendless@gmail.com>
2017-11-29 12:09:18 +0100

    Build assets

--- admin-dev/themes/new-theme/public/main.bundle.js
+++ admin-dev/themes/new-theme/public/main.bundle.js
@@ -59,7 +59,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "ed4f51df808ea75fdf71"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "3241e29731411dc5f9ba"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
