7037d3fce72fd5657932166348ba22ad68f4b7e4
neoteknic <neoteknic@gmail.com>
2017-12-05 12:19:53 +0100

    [CORE] Update toCamelCase
    Fix php 7.2 warning (create_function deprecated), hudge performances improvement too :
    Benchmark (php 7.1) :
    Old function : 0.836s (100k iterations)
    New function : 0.066s (100k iterations)
    
    Quick test : http://sandbox.onlinephpfunctions.com/code/a4e0f966c5bb23c935cafb08fc74be53587ed429

--- classes/Tools.php
+++ classes/Tools.php
@@ -2049,10 +2049,11 @@ class ToolsCore
     public static function toCamelCase($str, $catapitalise_first_char = false)
     {
         $str = Tools::strtolower($str);
-        if ($catapitalise_first_char) {
-            $str = Tools::ucfirst($str);
+        $str = str_replace(' ', '', ucwords(str_replace(['-', '_'], ' ', $str)) );
+        if (!$catapitalise_first_char) {
+            $str = lcfirst($str);
         }
-        return preg_replace_callback('/_+([a-z])/', create_function('$c', 'return strtoupper($c[1]);'), $str);
+        return $str;
     }
 
     /**
