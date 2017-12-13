c43ca814b9075199ad6bf6124b68cef7472297ca
Thomas LEVIANDIER <thomas.leviandier@gmail.com>
2017-12-13 14:09:40 +0100

    fix namespace

--- tests/Unit/Core/Cart/Calculation/Carrier/CarrierTest.php
+++ tests/Unit/Core/Cart/Calculation/Carrier/CarrierTest.php
@@ -24,7 +24,7 @@
  * International Registered Trademark & Property of PrestaShop SA
  */
 
-namespace Tests\Unit\Core\Cart\Calculation\Products;
+namespace Tests\Unit\Core\Cart\Calculation\Carrier;
 
 use Address;
 use Carrier;
