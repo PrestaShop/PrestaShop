ae9ee87496c370f3aa04f0ab1b5b6bc7d06115c9
hadrich-hatem <hatem-hadrich.sifast@prestashop.com>
2017-12-25 16:46:14 +0100

    Refactoring code of category

--- tests/E2E/test/selectors/FO/access_page.js
+++ tests/E2E/test/selectors/FO/access_page.js
@@ -1,11 +1,12 @@
 module.exports = {
- AccessPageFO:{
-  sign_in_button: 'div.user-info > a',
-  login_input: '//*[@id="login-form"]/section/div[1]/div[1]/input',
-  password_inputFO: '//*[@id="login-form"]/section/div[2]/div[1]/div/input',
-  login_button: '//*[@id="login-form"]/footer/button',
-  sign_out_button: '//*[@id="_desktop_user_info"]/div/a[1]',
-  logo_home_page: '.logo.img-responsive',
-  product_list_button: '//*[@id="content"]/section/a'
- }
+    AccessPageFO: {
+        sign_in_button: '//*[@id="_desktop_user_info"]/div/a',
+        login_input: '//*[@id="login-form"]/section/div[1]/div[1]/input',
+        password_inputFO: '//*[@id="login-form"]/section/div[2]/div[1]/div/input',
+        login_button: '//*[@id="login-form"]/footer/button',
+        sign_out_button: '//*[@id="_desktop_user_info"]/div/a[1]',
+        logo_home_page: '//*[@id="_desktop_logo"]/a',
+        product_list_button: '//*[@id="content"]/section/a',
+        categories_list: '//*[@id="left-column"]/div[1]/ul/li[2]/ul'
+    }
 };
