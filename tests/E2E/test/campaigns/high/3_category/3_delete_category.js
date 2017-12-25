a31541cb7b76c01eee0f0668383b5f034a3e4a37
hadrich-hatem <hatem-hadrich.sifast@prestashop.com>
2017-12-25 16:46:14 +0100

    Refactoring code of category

--- tests/E2E/test/campaigns/high/3_category/3_delete_category.js
+++ tests/E2E/test/campaigns/high/3_category/3_delete_category.js
@@ -1,36 +1,66 @@
 const {AccessPageBO} = require('../../../selectors/BO/access_page');
-const {AccessPageFO} = require('../../../selectors/FO/access_page');
+const {CatalogPage} = require('../../../selectors/BO/catalogpage/index');
+const {CategorySubMenu} = require('../../../selectors/BO/catalogpage/category_submenu');
 
-scenario('Delete category', client => {
-  test('should open the browser', () => client.open());
-  test('should log in successfully in BO', () => client.signInBO(AccessPageBO));
-  test('should go to category', () => client.goToCategoryList());
-  test('should search for the category', () => client.searchCategoryBO());
-  test('should delete category', () => client.deleteCategory());
-  test('should delete category success panel', () => client.successPanel('Successful deletion.'));
+scenario('Delete "Category"', () => {
+    scenario('Login in the Back Office', client => {
+        test('should open the browser', () => client.open());
+        test('should login successfully in the Back Office', () => client.signInBO(AccessPageBO));
+    }, 'category');
+    scenario('Delete the created "Category"', client => {
+        test('should go to "Category" page', () => client.goToSubtabMenuPage(CatalogPage.menu_button, CategorySubMenu.submenu));
+        test('should search for category ', () => client.searchByValue(CategorySubMenu.search_input, CategorySubMenu.search_button, 'category' + date_time));
+        test('should click on "Delete" action', () => client.clickOnAction(CategorySubMenu.delete_button, CategorySubMenu.action_button, 'delete'));
+        test('should delete category', () => client.waitForExistAndClick(CategorySubMenu.second_delete_button));
+        test('should verify the appearance of the green validation', () => client.checkTextValue(CatalogPage.success_panel, '×\nSuccessful deletion.'));
+    }, 'category');
+    scenario('Logout from the Back Office', client => {
+        test('should logout successfully from the Back Office', () => client.signOutBO());
+    }, 'category');
 }, 'category', true);
 
-scenario('Create category', client => {
-  test('should open the browser', () => client.open());
-  test('should log in successfully in BO', () => client.signInBO(AccessPageBO));
-  test('should go to category', () => client.goToCategoryList());
-  test('should create new category', () => client.createCategory());
-  test('should add category name', () => client.addCategoryName());
-  test('should add category image', () => client.addCategoryImage());
-  test('should add category thumb', () => client.addCategoryThumb());
-  test('should add category title', () => client.addCategoryTitle());
-  test('should add category meta desciption', () => client.addCategoryMetaDescription());
-  test('should add category meta keys words', () => client.addCategoryMetakeyswords());
-  test('should add category simplify url', () => client.addCategorySimplifyUrl());
-  test('should add category save', () => client.SaveCategory());
-  test('should check category success panel', () => client.successPanel('Successful creation.'));
+scenario('Create "Category"', () => {
+    scenario('Login in the Back Office', client => {
+        test('should open the browser', () => client.open());
+        test('should login successfully in the Back Office', () => client.signInBO(AccessPageBO));
+    }, 'category');
+    scenario('Create a new "Category"', client => {
+        test('should go to "Category" page', () => client.goToSubtabMenuPage(CatalogPage.menu_button, CategorySubMenu.submenu));
+        test('should click on "Add new category" button', () => client.waitForExistAndClick(CategorySubMenu.new_category_button));
+        test('should set the "Name" input', () => client.waitAndSetValue(CategorySubMenu.name_input, 'category' + date_time));
+        test('should upload the picture', () => client.uploadPicture('category_image.png', CategorySubMenu.picture, 'image'));
+        test('should upload the thumb picture', () => client.uploadPicture('category_miniature.png', CategorySubMenu.thumb_picture, 'image'));
+        test('should set the "Title" input', () => client.waitAndSetValue(CategorySubMenu.title, 'test category'));
+        test('should set the "Meta desciption" input', () => client.waitAndSetValue(CategorySubMenu.meta_description, 'this is the meta description'));
+        test('should set the "Meta keywords" input', () => client.waitAndSetValue(CategorySubMenu.keyswords, 'keyswords'));
+        test('should set the "Simplify url" input', () => client.waitAndSetValue(CategorySubMenu.simplify_URL_input, 'category' + date_time));
+        test('should click on "Save" button', () => {
+            let promise = Promise.resolve();
+            return promise
+                .then(() => client.scrollWaitForExistAndClick(CategorySubMenu.save_button, 50))
+                .then(() => client.getTextInVar(CategorySubMenu.category_number_span, "number_category"))
+        });
+        test('should verify the appearance of the green validation', () => client.checkTextValue(CatalogPage.success_panel, '×\nSuccessful creation.'));
+    }, 'category');
+    scenario('Logout from the Back Office', client => {
+        test('should logout successfully from Back Office', () => client.signOutBO());
+    }, 'category');
 }, 'category', true);
 
-scenario('Delete category with action group', client => {
-  test('should open the browser', () => client.open());
-  test('should log in successfully in BO', () => client.signInBO(AccessPageBO));
-  test('should go to category', () => client.goToCategoryList());
-  test('should search for the category', () => client.searchCategoryBO());
-  test('should delete category with action group', () => client.deleteCategoryWithActionGroup());
-  test('should check delete category success panel', () => client.successPanel('The selection has been successfully deleted.'));
+scenario('Delete "Category"', () => {
+    scenario('Login in the Back Office', client => {
+        test('should open the browser', () => client.open());
+        test('should login successfully in the Back Office', () => client.signInBO(AccessPageBO));
+    }, 'category');
+    scenario('Delete category with action group', client => {
+        test('should go to "Category" page', () => client.goToSubtabMenuPage(CatalogPage.menu_button, CategorySubMenu.submenu));
+        test('should search for category ', () => client.searchByValue(CategorySubMenu.search_input, CategorySubMenu.search_button, 'category' + date_time));
+        test('should select the category that we will deleted', () => client.waitForExistAndClick(CategorySubMenu.select_category));
+        test('should click on "Delete selected" action', () => client.clickOnAction(CategorySubMenu.delete_action_group_button, CategorySubMenu.action_group_button, 'delete', true));
+        test('should click on "Delete" button', () => client.waitForExistAndClick(CategorySubMenu.second_delete_button));
+        test('should verify the appearance of the green validation', () => client.checkTextValue(CatalogPage.success_panel, '×\nThe selection has been successfully deleted.'));
+    }, 'category');
+    scenario('Logout from the Back Office', client => {
+        test('should logout successfully from Back Office', () => client.signOutBO());
+    }, 'category');
 }, 'category', true);
--- tests/E2E/test/clients/category.js
+++ tests/E2E/test/clients/category.js
@@ -1,180 +1,50 @@
 var CommonClient = require('./common_client');
-var {selector} = require('../globals.webdriverio.js');
 
-global.categoryNameEntry = 'category' + new Date().getTime();
+global.checkCategoryName = [];
 
 class Category extends CommonClient {
 
-  goToCategoryList() {
-    return this.client
-      .waitForExist(selector.CatalogPage.menu_button, 90000)
-      .moveToObject(selector.CatalogPage.menu_button)
-      .waitForExist(selector.CategorySubMenu.submenu, 90000)
-      .click(selector.CategorySubMenu.submenu)
-  }
+    checkImage(selector) {
+        return this.client
+            .waitForExist(selector)
+            .pause(2000)
+            .then(() => this.client.isExisting(selector))
+            .then((text) => expect(text).to.be.equal(true));
 
-  createCategory() {
-    return this.client
-      .waitForExist(selector.CategorySubMenu.new_category_button, 90000)
-      .click(selector.CategorySubMenu.new_category_button)
-  }
+    }
 
-  addCategoryName() {
-    return this.client
-      .waitForExist(selector.CategorySubMenu.name_input, 90000)
-      .setValue(selector.CategorySubMenu.name_input, global.categoryNameEntry)
-  }
+    clickOnAction(actionSelector, groupActionSelector = '', action = 'edit', alert = false) {
+        if (action === 'delete') {
+            if (alert) {
+                return this.client
+                    .waitForExistAndClick(groupActionSelector)
+                    .waitForExistAndClick(actionSelector)
+                    .alertAccept()
+            } else {
+                return this.client
+                    .waitForExistAndClick(groupActionSelector)
+                    .waitForExistAndClick(actionSelector)
+            }
+        } else {
+            return this.client
+                .pause(2000)
+                .waitForExistAndClick(actionSelector)
+        }
+    }
 
-  addCategoryImage() {
-    return this.client
-      .execute(function () {
-        document.getElementById("image").style = "";
-      })
-      .chooseFile(selector.CategorySubMenu.picture, global.categoryImage)
-  }
+    getCategoriesName(categories_list, i) {
+        return this.client.getText(categories_list + '/li[' + i + ']/a').then(function (name) {
+            checkCategoryName[i] = name;
+        });
+    }
 
-  addCategoryThumb() {
-    return this.client
-      .execute(function () {
-        document.getElementById("image").style = "";
-      })
-      .chooseFile(selector.CategorySubMenu.thumb_picture, global.categoryThumb)
-  }
-
-  addCategoryTitle() {
-    return this.client
-      .waitForExist(selector.CategorySubMenu.title, 90000)
-      .setValue(selector.CategorySubMenu.title, 'test category')
-  }
-
-  addCategoryMetaDescription() {
-    return this.client
-      .waitForExist(selector.CategorySubMenu.meta_description, 90000)
-      .setValue(selector.CategorySubMenu.meta_description, 'this is the meta description')
-  }
-
-  addCategoryMetakeyswords() {
-    return this.client
-      .waitForExist(selector.CategorySubMenu.keyswords, 90000)
-      .setValue(selector.CategorySubMenu.keyswords, 'keyswords')
-  }
-
-  addCategorySimplifyUrl() {
-    return this.client
-      .waitForExist(selector.CategorySubMenu.simplify_URL_input, 90000)
-      .setValue(selector.CategorySubMenu.simplify_URL_input, global.categoryNameEntry)
-  }
-
-  SaveCategory() {
-    return this.client
-      .waitForExist(selector.CategorySubMenu.save_button, 90000)
-      .click(selector.CategorySubMenu.save_button)
-  }
-
-  goToCategoryBO() {
-    return this.client
-      .waitForExist(selector.CatalogPage.menu_button, 90000)
-      .moveToObject(selector.CatalogPage.menu_button)
-      .waitForExist(selector.CategorySubMenu.submenu, 90000)
-      .click(selector.CategorySubMenu.submenu)
-  }
-
-  searchCategoryBO() {
-    return this.client
-      .waitForExist(selector.CategorySubMenu.name_search_input, 90000)
-      .setValue(selector.CategorySubMenu.name_search_input, global.categoryNameEntry)
-      .waitForExist(selector.CategorySubMenu.search_button, 90000)
-      .click(selector.CategorySubMenu.search_button)
-  }
-
-  checkCategoryImage() {
-    return this.client
-      .waitForExist(selector.CategorySubMenu.update_button, 90000)
-      .click(selector.CategorySubMenu.update_button)
-      .pause(2000)
-      .then(() => this.client.isExisting(selector.CategorySubMenu.image_link))
-      .then((text) => expect(text).to.be.equal(true));
-
-  }
-
-  checkCategoryImageThumb() {
-    return this.client
-      .waitForExist(selector.CategorySubMenu.thumb_link, 90000)
-      .then(() => this.client.isExisting(selector.CategorySubMenu.thumb_link))
-      .then((text) => expect(text).to.be.equal(true));
-  }
-
-  checkCategoryTitle() {
-    return this.client
-      .waitForExist(selector.CategorySubMenu.title, 90000)
-      .then(() => this.client.getAttribute(selector.CategorySubMenu.title, "value"))
-      .then((text) => expect(text).to.be.equal("test category"));
-  }
-
-  checkCategoryMetaDescription() {
-    return this.client
-      .waitForExist(selector.CategorySubMenu.meta_description, 90000)
-      .then(() => this.client.getAttribute(selector.CategorySubMenu.meta_description, "value"))
-      .then((text) => expect(text).to.be.equal("this is the meta description"));
-  }
-
-  checkCategorySimplifyURL() {
-    return this.client
-      .waitForExist(selector.CategorySubMenu.simplify_URL_input, 90000)
-      .then(() => this.client.getAttribute(selector.CategorySubMenu.simplify_URL_input, "value"))
-      .then((text) => expect(text).to.be.equal(global.categoryNameEntry));
-  }
-
-  openProductList() {
-    return this.client
-      .waitForExist(selector.AccessPageFO.product_list_button, 90000)
-      .click(selector.AccessPageFO.product_list_button)
-  }
-
-  checkCategoryExistenceFO() {
-    return this.client
-      .waitForExist(selector.SearchProductPage.second_category_name, 90000)
-      .then(() => this.client.getText(selector.SearchProductPage.second_category_name))
-      .then((text) => expect(text).to.be.equal(global.categoryNameEntry));
-  }
-
-  updateCategory() {
-    global.categoryNameEntry = global.categoryNameEntry + 'update';
-    return this.client
-      .moveToObject(selector.CategorySubMenu.update_button, 90000)
-      .click(selector.CategorySubMenu.update_button)
-      .waitForExist(selector.CategorySubMenu.name_input, 90000)
-      .setValue(selector.CategorySubMenu.name_input, global.categoryNameEntry)
-      .waitForExist(selector.CategorySubMenu.simplify_URL_input, 90000)
-      .setValue(selector.CategorySubMenu.simplify_URL_input, global.categoryNameEntry)
-      .waitForExist(selector.CategorySubMenu.save_button, 90000)
-      .click(selector.CategorySubMenu.save_button)
-  }
-
-  deleteCategory() {
-    return this.client
-      .waitForExist(selector.CategorySubMenu.action_button, 90000)
-      .click(selector.CategorySubMenu.action_button)
-      .waitForExist(selector.CategorySubMenu.delete_button, 90000)
-      .click(selector.CategorySubMenu.delete_button)
-      .waitForExist(selector.CategorySubMenu.second_delete_button, 90000)
-      .click(selector.CategorySubMenu.second_delete_button)
-  }
-
-  deleteCategoryWithActionGroup() {
-    return this.client
-      .waitForExist(selector.CategorySubMenu.select_category, 90000)
-      .click(selector.CategorySubMenu.select_category)
-      .waitForExist(selector.CategorySubMenu.action_group_button, 90000)
-      .click(selector.CategorySubMenu.action_group_button)
-      .waitForExist(selector.CategorySubMenu.delete_action_group_button, 90000)
-      .click(selector.CategorySubMenu.delete_action_group_button)
-      .alertAccept()
-      .waitForExist(selector.CategorySubMenu.second_delete_button, 90000)
-      .click(selector.CategorySubMenu.second_delete_button)
-      .pause(5000)
-  }
-
+    checkCategory(selector, category_name) {
+        return this.client
+            .waitForExist(selector)
+            .then(() => {
+                expect(checkCategoryName).to.be.an('array').that.does.include(category_name)
+            });
+    }
 }
 
 module.exports = Category;
