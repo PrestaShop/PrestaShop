0f20b57f5aa4a22f737e99ed2f31f9960e73a5bb
Pablo Borowicz <eternoendless@gmail.com>
2018-01-08 14:51:38 +0100

    Explicit $ import

--- admin-dev/themes/new-theme/js/maintenance-page/ip-input.js
+++ admin-dev/themes/new-theme/js/maintenance-page/ip-input.js
@@ -22,6 +22,7 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
+const $ = window.$;
 const IpInput = {};
 
 IpInput.addRemoteAddr = (event) => {
