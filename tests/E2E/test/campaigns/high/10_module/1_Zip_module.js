727491555fb4902e154759cd1572227fae1062bd
fouratachour <fourat.ach@gmail.com>
2018-01-09 15:59:37 +0100

    Adding module partnaire

--- tests/E2E/test/campaigns/high/10_module/1_Zip_module.js
+++ tests/E2E/test/campaigns/high/10_module/1_Zip_module.js
@@ -1,43 +1,35 @@
 const {AccessPageBO} = require('../../../selectors/BO/access_page');
 const {ModulePage} = require('../../../selectors/BO/module_page');
 const {AddProductPage} = require('../../../selectors/BO/add_product_page');
+const module_common_scenarios = require('./module');
 let promise = Promise.resolve();
-scenario('Install "PrestaShop Security Module"', () => {
-  scenario('Login in the Back Office', client => {
-    test('should open the browser', () => client.open());
-    test('should login successfully in the Back Office', () => client.signInBO(AccessPageBO));
-  }, 'common_client');
-  scenario('Install " PrestaShop Security Module " with ZIP file', client => {
-    test('should click on "Module" button', () => client.goToSubtabMenuPage(ModulePage.modules_subtab, ModulePage.modules_subtab));
-    test('should click on "Upload a module" button', () => client.waitForExistAndClick(ModulePage.upload_button));
-    test('should add zip file', () => client.addFile(ModulePage.zip_file_input,"v1.1.7-prestafraud.zip"));
-    test('should verify that the module is installed', () => {
-      return promise
-        .then(() => client.waitForVisible(ModulePage.success_install_message))
-        .then(() => client.checkTextValue(ModulePage.installed_message, "Module installed!"))
-    });
-    test('should click on close modal button', () => client.waitForExistAndClick(ModulePage.close_modal_button));
-    test('should click on "Installed Modules"', () => client.waitForExistAndClick(ModulePage.installed_modules_tabs, 1000));
-    test('should set the name of the module in the search input', () => client.waitAndSetValue(ModulePage.modules_search_input, "prestafraud"));
-    test('should click on "Search" button', () => client.waitForExistAndClick(ModulePage.modules_search_button));
-    test('should check if the module "prestafraud" was installed', () => client.checkTextValue(ModulePage.built_in_module, "1", "contain"));
-  }, 'common_client');
-  scenario('Check Configuration page of "PrestaShop Security Module"', client => {
-    test('should click on "Configure" button', () => client.waitForExistAndClick(ModulePage.action_module_built_button));
-    test('should check the configuration page', () => client.checkTextValue(ModulePage.config_legend, "PrestaShop Security configuration"));
-  }, 'common_client');
-  scenario('Uninstall "PrestaShop Security Module"', client => {
-    test('should click on "Module" button', () => client.goToSubtabMenuPage(ModulePage.modules_subtab, ModulePage.modules_subtab));
-    test('should click on "Installed Modules"', () => client.waitForExistAndClick(ModulePage.installed_modules_tabs, 1000));
-    test('should set the name of the module in the search input', () => client.waitAndSetValue(ModulePage.modules_search_input, "prestafraud"));
-    test('should click on "Search" button', () => client.waitForExistAndClick(ModulePage.modules_search_button));
-    test('should click on option button', () => client.waitForVisibleAndClick(ModulePage.option_button));
-    test('should click on "Uninstall" button', () => client.waitForExistAndClick(ModulePage.uninstall_button));
-    test('should click on "Yes, uninstall it" button', () => client.waitForVisibleAndClick(ModulePage.uninstall_confirmation));
-    test('should check that the success alert message is well displayed', () => client.waitForExistAndClick(AddProductPage.close_validation_button));
-    test('should check if the module "prestafraud" was installed', () => client.checkTextValue(ModulePage.built_in_module, "0", "contain"));
-  }, 'common_client');
-  scenario('Logout from the Back Office', client => {
-    test('should logout successfully from the Back Office', () => client.signOutBO());
-  }, 'common_client');
+scenario('Install "PrestaShop Security" module', () => {
+    scenario('Login in the Back Office', client => {
+        test('should open the browser', () => client.open());
+        test('should login successfully in the Back Office', () => client.signInBO(AccessPageBO));
+    }, 'common_client');
+    scenario('Install "PrestaShop Security" module by uploading a ZIP file', client => {
+        test('should go to "Module" page', () => client.goToSubtabMenuPage(ModulePage.modules_subtab, ModulePage.modules_subtab));
+        test('should click on "Upload a module" button', () => client.waitForExistAndClick(ModulePage.upload_button));
+        test('should add zip file', () => client.addFile(ModulePage.zip_file_input, "v1.1.7-prestafraud.zip"));
+        test('should verify that the module is installed', () => {
+            return promise
+                .then(() => client.waitForVisible(ModulePage.success_install_message))
+                .then(() => client.checkTextValue(ModulePage.module_import_success, "Module installed!"))
+        });
+        test('should click on close modal button', () => client.waitForExistAndClick(ModulePage.close_modal_button));
+        test('should click on "Installed Modules"', () => client.waitForExistAndClick(ModulePage.installed_modules_tabs, 1000));
+        test('should search for "PrestaShop Security" module in the installed module tab', () => client.waitAndSetValue(ModulePage.modules_search_input, "prestafraud"));
+        test('should click on "Search" button', () => client.waitForExistAndClick(ModulePage.modules_search_button));
+        test('should check if the module "prestafraud" was installed', () => client.checkTextValue(ModulePage.built_in_module, "1", "contain"));
+    }, 'common_client');
+    scenario('Check Configuration page of "PrestaShop Security" module', client => {
+        module_common_scenarios.checkConfigPage(client, ModulePage, "PrestaShop Security");
+    }, 'common_client');
+    scenario('Uninstall "PrestaShop Security" module', client => {
+        module_common_scenarios.uninstallModule(client, ModulePage, AddProductPage, "PrestaShop Security", "prestafraud");
+    }, 'common_client');
+    scenario('Logout from the Back Office', client => {
+        test('should logout successfully from the Back Office', () => client.signOutBO());
+    }, 'common_client');
 }, 'common_client', true);
