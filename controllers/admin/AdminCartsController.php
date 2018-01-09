75f62bb7a6af06a8dcc20cb5b9fe18319fe0c372
Pablo Borowicz <eternoendless@gmail.com>
2018-01-09 16:42:56 +0100

    Removed unused variable

--- controllers/admin/AdminCartsController.php
+++ controllers/admin/AdminCartsController.php
@@ -428,7 +428,7 @@ class AdminCartsControllerCore extends AdminController
                     }
                 }
             }
-            $this->setMedia($isNewTheme = false);
+            $this->setMedia(false);
             $this->initFooter();
             $this->context->smarty->assign(array('customization_errors' => implode('<br />', $errors),
                                                             'css_files' => $this->css_files));
