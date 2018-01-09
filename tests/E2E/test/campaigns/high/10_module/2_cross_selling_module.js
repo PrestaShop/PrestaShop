c1d5728a54f98d1e5c208a8d0479c199c83f1f3c
fouratachour <fourat.ach@gmail.com>
2018-01-09 15:59:37 +0100

    Adding module partnaire

--- tests/E2E/test/campaigns/high/10_module/2_cross_selling_module.js
+++ tests/E2E/test/campaigns/high/10_module/2_cross_selling_module.js
@@ -1,38 +1,40 @@
 const {AccessPageBO} = require('../../../selectors/BO/access_page');
 const {ModulePage} = require('../../../selectors/BO/module_page');
 const {AddProductPage} = require('../../../selectors/BO/add_product_page');
-scenario('Install "Google AdWords Module"', () => {
-  scenario('Login in the Back Office', client => {
-    test('should open the browser', () => client.open());
-    test('should login successfully in the Back Office', () => client.signInBO(AccessPageBO));
-  }, 'common_client');
-  scenario('Install "Google AdWords Module " From Cross selling', client => {
-    test('should click on "Module" button', () => client.goToSubtabMenuPage(ModulePage.modules_subtab, ModulePage.modules_subtab));
-    test('should set the name of the module in the search input', () => client.waitAndSetValue(ModulePage.module_selection_input, "gadwords"));
-    test('should click on "Search" button', () => client.waitForExistAndClick(ModulePage.selection_search_button));
-    test('should click on "Install" button', () => client.waitForExistAndClick(ModulePage.install_button));
-    test('should check that the success alert message is well displayed', () => client.waitForExistAndClick(AddProductPage.close_validation_button));
-    test('should click on "Installed Modules"', () => client.waitForVisibleAndClick(ModulePage.installed_modules_tabs));
-    test('should set the name of the module in the search input', () => client.waitAndSetValue(ModulePage.modules_search_input, "gadwords"));
-    test('should click on "Search" button', () => client.waitForExistAndClick(ModulePage.modules_search_button));
-    test('should check if the module "prestafraud" was installed', () => client.checkTextValue(ModulePage.built_in_module, "1", "contain"));
-  }, 'common_client');
-  scenario('Check Configuration page of "Google AdWords Module"', client => {
-    test('should click on "Configure" button', () => client.waitForExistAndClick(ModulePage.action_module_built_button));
-    test('should check the configuration page', () => client.checkTextValue(ModulePage.config_legend_adwords, "Google AdWords"));
-  }, 'common_client');
-  scenario('Uninstall "Google AdWords Module"', client => {
-    test('should click on "Module" button', () => client.goToSubtabMenuPage(ModulePage.modules_subtab, ModulePage.modules_subtab));
-    test('should click on "Installed Modules"', () => client.waitForVisibleAndClick(ModulePage.installed_modules_tabs));
-    test('should set the name of the module in the search input', () => client.waitAndSetValue(ModulePage.modules_search_input, "gadwords"));
-    test('should click on "Search" button', () => client.waitForExistAndClick(ModulePage.modules_search_button));
-    test('should click on option button', () => client.waitForVisibleAndClick(ModulePage.option_button));
-    test('should click on "Uninstall" button', () => client.waitForExistAndClick(ModulePage.uninstall_button));
-    test('should click on "Yes, uninstall it" button', () => client.waitForVisibleAndClick(ModulePage.uninstall_adwords_module));
-    test('should check that the success alert message is well displayed', () => client.waitForExistAndClick(AddProductPage.close_validation_button));
-    test('should check if the module "prestafraud" was installed', () => client.checkTextValue(ModulePage.built_in_module, "0", "contain"));
-  }, 'common_client');
-  scenario('Logout from the Back Office', client => {
-    test('should logout successfully from the Back Office', () => client.signOutBO());
-  }, 'common_client');
+const module_common_scenarios = require('./module');
+scenario('Install and Uninstall "Google AdWords" Module', () => {
+    scenario('Login in the Back Office', client => {
+        test('should open the browser', () => client.open());
+        test('should login successfully in the Back Office', () => client.signInBO(AccessPageBO));
+    }, 'common_client');
+    scenario('Install "Google AdWords Module " From Cross selling', client => {
+        module_common_scenarios.installModule(client, ModulePage, AddProductPage, "Google AdWords", "gadwords");
+    }, 'common_client');
+    scenario('Check Configuration page of "Google AdWords Module"', client => {
+        module_common_scenarios.checkConfigPage(client, ModulePage, "Google AdWords");
+    }, 'common_client');
+    scenario('Uninstall "Google AdWords Module"', client => {
+        module_common_scenarios.uninstallModule(client, ModulePage, AddProductPage, "Google AdWords", "gadwords");
+    }, 'common_client');
+}, 'common_client');
+
+scenario('Install,disable,enable and Uninstall "PayPal" module', () => {
+    scenario('Install "PayPal" module From Cross selling', client => {
+        module_common_scenarios.installModule(client, ModulePage, AddProductPage, "PayPal", "PayPal's");
+    }, 'common_client');
+    scenario('Check Configuration page of "PayPal" module', client => {
+        module_common_scenarios.checkConfigPage(client, ModulePage, "PayPal");
+    }, 'common_client');
+    scenario('Disable Module', client => {
+        module_common_scenarios.disableModule(client, ModulePage, AddProductPage, "PayPal", "PayPal");
+    }, 'common_client');
+    scenario('Disable Module', client => {
+        module_common_scenarios.enableModule(client, ModulePage, AddProductPage, "PayPal", "PayPal");
+    }, 'common_client');
+    scenario('Uninstall "PayPal" module', client => {
+        module_common_scenarios.uninstallModule(client, ModulePage, AddProductPage, "PayPal", "PayPal");
+    }, 'common_client');
+    scenario('Logout from the Back Office', client => {
+        test('should logout successfully from the Back Office', () => client.signOutBO());
+    }, 'common_client');
 }, 'common_client', true);
