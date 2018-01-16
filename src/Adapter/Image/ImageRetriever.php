7df1c74667c95125060268a524dcafab34d13d1a
123monsite-regis <you@example.com>
2018-01-16 18:30:41 +0100

    FO: Displaying the No Image for Products on theme

--- src/Adapter/Image/ImageRetriever.php
+++ src/Adapter/Image/ImageRetriever.php
@@ -211,4 +211,44 @@ class ImageRetriever
             'legend' => ''
         ];
     }
+
+    public function getNoPictureImage(Language $language)
+    {
+        $urls  = [];
+        $type = 'products';
+        $image_types = ImageType::getImagesTypes($type, true);
+        
+        foreach ($image_types as $image_type) {
+            
+            $url = $this->link->getImageLink(
+                '',
+                $language->iso_code.'-default',
+                $image_type['name']
+            );
+
+            $urls[$image_type['name']] = [
+                'url'      => $url,
+                'width'     => (int)$image_type['width'],
+                'height'    => (int)$image_type['height'],
+            ];
+        }
+        
+        uasort($urls, function (array $a, array $b) {
+            return $a['width'] * $a['height'] > $b['width'] * $b['height'] ? 1 : -1;
+        });
+
+        $keys = array_keys($urls);
+
+        $small  = $urls[$keys[0]];
+        $large  = end($urls);
+        $medium = $urls[$keys[ceil((count($keys) - 1) / 2)]];
+        
+        return array(
+            'bySize' => $urls,
+            'small'  => $small,
+            'medium' => $medium,
+            'large'  => $large,
+            'legend' => ''
+        );
+    }
 }
