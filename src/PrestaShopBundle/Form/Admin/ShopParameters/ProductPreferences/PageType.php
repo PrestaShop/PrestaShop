ed7bdbe7f9575a3456ca8b80fd4592727438607e
Šarūnas Jonušas <sarunas.jonusas@invertus.lt>
2018-01-17 16:05:05 +0200

    add missing class docs for form types

--- src/PrestaShopBundle/Form/Admin/ShopParameters/ProductPreferences/PageType.php
+++ src/PrestaShopBundle/Form/Admin/ShopParameters/ProductPreferences/PageType.php
@@ -8,6 +8,10 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+/**
+ * Class generates "Product page" form
+ * in "Configure > Shop Parameters > Product Settings" page.
+ */
 class PageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
--- src/PrestaShopBundle/Form/Admin/ShopParameters/ProductPreferences/PaginationType.php
+++ src/PrestaShopBundle/Form/Admin/ShopParameters/ProductPreferences/PaginationType.php
@@ -8,6 +8,10 @@ use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+/**
+ * Class generates "Pagination" form
+ * in "Configure > Shop Parameters > Product Settings" page.
+ */
 class PaginationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
