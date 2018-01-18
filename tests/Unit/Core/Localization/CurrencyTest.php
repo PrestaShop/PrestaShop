73fdf43fa968dff21281d3734463693eb04fbcce
Bastien Bieri <bastien.bieri@prestashop.com>
2018-01-18 11:43:53 +0100

    Moved CurrencyTest according to Currency entity new location

--- tests/Unit/Core/Localization/CurrencyTest.php
+++ tests/Unit/Core/Localization/CurrencyTest.php
@@ -24,7 +24,7 @@
  * International Registered Trademark & Property of PrestaShop SA
  */
 
-namespace Tests\Unit\Core\Localization\Currency;
+namespace Tests\Unit\Core\Localization;
 
 use PHPUnit\Framework\TestCase;
 use PrestaShop\PrestaShop\Core\Localization\Currency;
