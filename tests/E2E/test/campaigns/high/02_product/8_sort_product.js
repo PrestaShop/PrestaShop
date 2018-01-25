f872bcc4b5149e07ce641ec6135d44f681943278
hadrich-hatem <hatem-hadrich.sifast@prestashop.com>
2018-01-25 18:20:35 +0100

    Sort product

---
+++ tests/E2E/test/campaigns/high/02_product/8_sort_product.js
@@ -0,0 +1,19 @@
+const {AddProductPage} = require('../../../selectors/BO/add_product_page');
+const {ProductList} = require('../../../selectors/BO/add_product_page');
+const {AccessPageBO} = require('../../../selectors/BO/access_page');
+const common_scenarios = require('../02_product/product');
+let promise = Promise.resolve();
+
+scenario('Check the sort of products in the Back Office', client => {
+  test('should open browser', () => client.open());
+  test('should log in successfully in BO', () => client.signInBO(AccessPageBO));
+  test('should go to "Catalog" page', () => {
+    return promise
+      .then(() => client.waitForExistAndClick(AddProductPage.products_subtab, 2000))
+      .then(() => client.getProductsNumber('product_catalog_list'));
+  });
+
+  common_scenarios.sortProduct(ProductList.product_id, 'id_product');
+  common_scenarios.sortProduct(ProductList.product_name, 'name');
+  common_scenarios.sortProduct(ProductList.product_reference, 'reference');
+}, 'product/product', true);
