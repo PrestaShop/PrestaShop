8bc53dfcb62fb68c9f1c8a990ee69938b14f7407
fouratachour <fourat.ach@gmail.com>
2018-01-26 18:14:15 +0100

    Sort module by name, Increasing price, Decreasing price, popularity

--- tests/E2E/test/clients/module.js
+++ tests/E2E/test/clients/module.js
@@ -10,6 +10,8 @@ global.moduleObject = {
   "data-type": ''
 };
 global.moduleLists = {};
+global.moduleInfo = [];
+global.moduleSort = [];
 
 class Module extends CommonClient {
 
@@ -37,6 +39,51 @@ class Module extends CommonClient {
       })
   }
 
+  getModuleAttr(selector, attr, index) {
+    return this.client
+      .getAttribute(selector.replace('%I', index + 1), attr)
+      .then((name) => {
+        moduleSort[index] = name.toLowerCase();
+        moduleInfo[index] = name.toLowerCase();
+      })
+  }
+
+  checkSortByName(length) {
+    return this.client
+      .pause(1000)
+      .then(() => {
+        this.client
+          .waitUntil(function () {
+            expect(moduleInfo.sort()).to.deep.equal(moduleSort)
+          }, 1000 * length)
+      });
+  }
+
+  checkSortByIncPrice(length) {
+    return this.client
+      .pause(1000)
+      .then(() => {
+        this.client
+          .waitUntil(function () {
+            expect(moduleInfo.sort(function (a, b) {
+              return a - b;
+            })).to.deep.equal(moduleSort)
+          }, 1000 * length)
+      });
+  }
+
+  checkSortDesc(length) {
+    return this.client
+      .pause(1000)
+      .then(() => {
+        this.client
+          .waitUntil(function () {
+            expect(moduleInfo.sort(function (a, b) {
+              return a - b;
+            }).reverse()).to.deep.equal(moduleSort)
+          }, 1000 * length)
+      });
+  }
 }
 
 module.exports = Module;
