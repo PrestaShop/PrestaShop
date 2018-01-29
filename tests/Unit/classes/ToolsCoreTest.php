42094d4bd8fc03d03fe23bcfbcb51b123bf09f0a
Maxime Morel-Bailly <maxime@upilot.com>
2018-01-29 19:25:44 +0400

    Add test case for Tools::strReplaceFirst

--- tests/Unit/classes/ToolsCoreTest.php
+++ tests/Unit/classes/ToolsCoreTest.php
@@ -305,4 +305,21 @@ class ToolsCoreTest extends PHPUnit_Framework_TestCase
         $_POST = array();
         $_GET = array();
     }
+
+    /**
+     * @dataProvider testStrReplaceFirstProvider
+     */
+    public function testStrReplaceFirst($search, $replace, $subject, $cur, $expected) {
+        $this->assertEquals($expected, Tools::StrReplaceFirst($search, $replace, $subject, $cur));
+    }
+
+    public function testStrReplaceFirstProvider() {
+        return [
+            ['s', 'f', 'seed', 0, 'feed'],
+            ['s', 'f', 'seed', 1, 'seed'],
+            ['e', 'o', 'feed', 0, 'foed'],
+            ['e', 'o', 'feed', 1, 'foed'],
+            ['e', 'o', 'feed', 2, 'feod'],
+        ];
+    }
 }
