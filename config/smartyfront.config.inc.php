e1d2888203d01c77415940c3e76d69db5bbea1be
Jérôme H <jherry22@gmail.com>
2018-02-07 21:48:34 +0100

    CO: prefer Hook::coreRenderWidget
    We should call Hook::coreRenderWidget method to allow developpers of modules to override this behavior. It exists so it must be called here.

--- config/smartyfront.config.inc.php
+++ config/smartyfront.config.inc.php
@@ -79,7 +79,7 @@ function withWidget($params, callable $cb)
 function smartyWidget($params, &$smarty)
 {
     return withWidget($params, function ($widget, $params) {
-        return $widget->renderWidget(null, $params);
+        return Hook::coreRenderWidget($widget, null, $params);
     });
 }
 
