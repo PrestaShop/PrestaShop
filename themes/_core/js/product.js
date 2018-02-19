61a6ace28e302741ef268a9bb69e91c5c9abb133
Alban L <alban.legout@gmail.com>
2018-02-19 16:20:50 +0100

    themes assets updated

--- themes/_core/js/product.js
+++ themes/_core/js/product.js
@@ -27,125 +27,156 @@ import prestashop from 'prestashop';
 import {psGetRequestParameter} from './common';
 
 $(document).ready(function () {
-  $('body').on('change', '.product-variants [data-product-attribute]', function () {
-    $("input[name$='refresh']").click();
-  });
+    // Used to be able to abort request if user modify something
+    var currentRequest = null;
 
-  $('body').on(
-    'click',
-    '.product-refresh',
-    function (event, extraParameters) {
-      var $productRefresh = $(this);
-      event.preventDefault();
+    // Used to clearTimeout if user flood the product quantity input
+    var currentRequestDelayedId = null;
 
-      let eventType = 'updatedProductCombination';
-      if (typeof extraParameters !== 'undefined' && extraParameters.eventType) {
-        eventType = extraParameters.eventType;
-      }
-
-      var preview = psGetRequestParameter('preview');
-      if (preview !== null) {
-        preview = '&preview=' + preview;
-      } else {
-        preview = '';
-      }
-
-      var query = $(event.target.form).serialize() + '&ajax=1&action=productrefresh' + preview;
-      var actionURL = $(event.target.form).attr('action');
-
-      $.post(actionURL, query, null, 'json').then(function(resp) {
-        prestashop.emit('updateProduct', {
-          reason: {
-            productUrl: resp.productUrl
-          },
-          refreshUrl: $productRefresh.data('url-update'),
-          eventType: eventType,
-          resp: resp
-        });
-      });
-    }
-  );
-
-  prestashop.on('updateProduct', function (event) {
-    if (typeof event.refreshUrl == "undefined") {
-        event.refreshUrl = true;
-    }
-
-    var eventType = event.eventType;
-
-    let replaceAddToCartSections = ((addCartHtml) => {
-      let $addToCartSnippet = $(addCartHtml);
-      let $addProductToCart = $('.product-add-to-cart');
-
-      function replaceAddToCartSection(replacement) {
-        let replace = replacement.$addToCartSnippet.find(replacement.targetSelector);
-
-        if ($(replacement.$targetParent.find(replacement.targetSelector)).length > 0) {
-          if (replace.length > 0) {
-            $(replacement.$targetParent.find(replacement.targetSelector)).replaceWith(replace[0].outerHTML);
-          } else {
-            $(replacement.$targetParent.find(replacement.targetSelector)).html('');
-          }
+    // Listen on all form elements + those who have a data-product-attribute
+    $('body').on(
+        'change touchspin.on.startspin',
+        '.product-variants *[name]',
+        function(e) {
+            prestashop.emit('updateProduct', {
+                eventType: 'updatedProductCombination',
+                event: e
+            });
         }
-      }
+    );
 
-      const productAvailabilitySelector = '.add';
-      replaceAddToCartSection({
-        $addToCartSnippet: $addToCartSnippet,
-        $targetParent: $addProductToCart,
-        targetSelector: productAvailabilitySelector
-      });
+    // Refresh all the product content
+    prestashop.on('updateProduct', function (args) {
+        const eventType = args.eventType;
+        const event = args.event;
+        const $productAttribute = $(event.currentTarget);
+        const $productActions = $productAttribute.parents('.product-actions');
+        const $quantityWantedInput = $productActions.find('#quantity_wanted:first');
+        const updateUrl = $quantityWantedInput.data('update-url');
+        const preview = psGetRequestParameter('preview');
 
-      const productAvailabilityMessageSelector = '#product-availability';
-      replaceAddToCartSection({
-        $addToCartSnippet: $addToCartSnippet,
-        $targetParent: $addProductToCart,
-        targetSelector: productAvailabilityMessageSelector
-      });
+        if (currentRequestDelayedId) {
+            clearTimeout(currentRequestDelayedId);
+        }
 
-      const productMinimalQuantitySelector = '.product-minimal-quantity';
-      replaceAddToCartSection({
-        $addToCartSnippet: $addToCartSnippet,
-        $targetParent: $addProductToCart,
-        targetSelector: productMinimalQuantitySelector
-      });
+        currentRequestDelayedId = setTimeout(function() {
+            currentRequest = $.ajax({
+                url: updateUrl + '?' +
+                    $productActions.find('form:first').serialize() +
+                    '&preview=' + preview,
+                method: 'POST',
+                data: {
+                    ajax: 1,
+                    action: 'refresh',
+                    quantity_wanted: $quantityWantedInput.val()
+                },
+                dataType: 'json',
+                beforeSend: function() {
+                    if (currentRequest != null) {
+                        currentRequest.abort();
+                    }
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    if (textStatus !== 'abort'
+                        && $('section#main > .ajax-error').length === 0
+                    ) {
+                        showError($('#product-availability'), 'An error occurred while processing your request');
+                    }
+                },
+                success: function(data, textStatus, errorThrown) {
+                    // Avoid image to blink each time we modify the product quantity
+                    // Can not compare directly cause of HTML comments in data.
+                    const $newImagesContainer = $('<div>').append(data.product_cover_thumbnails);
+
+                    // Used to avoid image blinking if same image = epileptic friendly
+                    if ($('.images-container').html() !== $newImagesContainer.find('.images-container').html()) {
+                        $('.images-container').replaceWith(data.product_cover_thumbnails);
+                    }
+                    $('.product-prices').replaceWith(data.product_prices);
+                    $('.product-customization').replaceWith(data.product_customization);
+                    $('.product-variants').replaceWith(data.product_variants);
+                    $('.product-discounts').replaceWith(data.product_discounts);
+                    $('.product-additional-info').replaceWith(data.product_additional_info);
+                    $('#product-details').replaceWith(data.product_details);
+                    replaceAddToCartSections(data);
+                    const minimalProductQuantity = parseInt(data.product_minimal_quantity, 10);
+
+                    // Prevent quantity input from blinking with classic theme.
+                    if (!isNaN(minimalProductQuantity)
+                        && $quantityWantedInput.val() < minimalProductQuantity
+                        && eventType !== 'updatedProductQuantity'
+                    ) {
+                        $quantityWantedInput.attr('min', minimalProductQuantity);
+                        $quantityWantedInput.val(minimalProductQuantity);
+                    }
+                    prestashop.emit('updatedProduct', data);
+                },
+                complete: function(jqXHR, textStatus) {
+                    currentRequest = null;
+                    currentRequestDelayedId = null;
+                }
+            });
+        }.bind(currentRequest, currentRequestDelayedId), 250);
     });
-
-    $.post(event.reason.productUrl, {ajax: '1', action: 'refresh'}, null, 'json').then(function(resp) {
-      $('.product-prices').replaceWith(resp.product_prices);
-      $('.product-customization').replaceWith(resp.product_customization);
-      $('.product-variants').replaceWith(resp.product_variants);
-      $('.product-discounts').replaceWith(resp.product_discounts);
-      $('.images-container').replaceWith(resp.product_cover_thumbnails);
-      $('.product-additional-info').replaceWith(resp.product_additional_info);
-      $('#product-details').replaceWith(resp.product_details);
-
-      // Replace all "add to cart" sections but the quantity input in order to keep quantity field intact i.e.
-      // Prevent quantity input from blinking with classic theme.
-      let $productAddToCart;
-      $(resp.product_add_to_cart).each(function(index, value) {
-          if ($(value).hasClass('product-add-to-cart')) {
-            $productAddToCart = $(value);
-          }
-      });
-      replaceAddToCartSections($productAddToCart);
-
-      const minimalProductQuantity = parseInt(resp.product_minimal_quantity, 10);
-      const quantityInputSelector = '#quantity_wanted';
-      let quantityInput = $(quantityInputSelector);
-      const quantity_wanted = quantityInput.val();
-
-      if (!isNaN(minimalProductQuantity) && quantity_wanted < minimalProductQuantity && eventType !== 'updatedProductQuantity') {
-        quantityInput.attr('min', minimalProductQuantity);
-        quantityInput.val(minimalProductQuantity);
-      }
-
-      if (event.refreshUrl) {
-        window.history.pushState({id_product_attribute: resp.id_product_attribute}, undefined, resp.product_url);
-      }
-
-      prestashop.emit('updatedProduct', resp);
-    });
-  });
 });
 
+// Replace all "add to cart" sections but the quantity input
+// in order to keep quantity field intact i.e.
+function replaceAddToCartSections(data)
+{
+    let $productAddToCart = null;
+
+    $(data.product_add_to_cart).each(function(index, value) {
+        if ($(value).hasClass('product-add-to-cart')) {
+            $productAddToCart = $(value);
+
+            return false;
+        }
+    });
+
+    if ($productAddToCart === null) {
+        showError($('#product-availability'), 'An error occurred while processing your request');
+    }
+    const $addProductToCart = $('.product-add-to-cart');
+    const productAvailabilitySelector = '.add';
+    const productAvailabilityMessageSelector = '#product-availability';
+    const productMinimalQuantitySelector = '.product-minimal-quantity';
+
+    replaceAddToCartSection({
+        $addToCartSnippet: $productAddToCart,
+        $targetParent: $addProductToCart,
+        targetSelector: productAvailabilitySelector
+    });
+
+    replaceAddToCartSection({
+        $addToCartSnippet: $productAddToCart,
+        $targetParent: $addProductToCart,
+        targetSelector: productAvailabilityMessageSelector
+    });
+
+    replaceAddToCartSection({
+        $addToCartSnippet: $productAddToCart,
+        $targetParent: $addProductToCart,
+        targetSelector: productMinimalQuantitySelector
+    });
+}
+
+function replaceAddToCartSection(replacement)
+{
+    if ($(replacement.$targetParent.find(replacement.targetSelector)).length <= 0) {
+        return;
+    }
+    const replace = replacement.$addToCartSnippet.find(replacement.targetSelector);
+
+    if (replace.length > 0) {
+        $(replacement.$targetParent.find(replacement.targetSelector)).replaceWith(replace[0].outerHTML);
+    } else {
+        $(replacement.$targetParent.find(replacement.targetSelector)).html('');
+    }
+}
+
+function showError($container, textError)
+{
+    const $error = $(`<div class="alert alert-danger ajax-error" role="alert">${textError}</div>`);
+    $container.replaceWith($error);
+}
