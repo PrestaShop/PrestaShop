c9100ec59480f2ccf923cdad34d75a1bdcfa5c31
Pablo Borowicz <eternoendless@gmail.com>
2018-02-19 14:46:03 +0100

    Add documentation

--- src/PrestaShopBundle/DependencyInjection/Compiler/OverrideTranslatorServiceCompilerPass.php
+++ src/PrestaShopBundle/DependencyInjection/Compiler/OverrideTranslatorServiceCompilerPass.php
@@ -29,6 +29,11 @@ namespace PrestaShopBundle\DependencyInjection\Compiler;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
+/**
+ * On Symfony 3.x, the parameters like `translator.class` are not used anymore and cannot override the original services.
+ * This made the translations unavailable in prod mode, and the module page was crashing.
+ * This class replaces the symfony translator with PrestaShop's extended one when in prod mode.
+ */
 class OverrideTranslatorServiceCompilerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
