9d89955df4f09194030169a5cc30574b011fd730
Alban L <alban.legout@gmail.com>
2018-03-05 18:34:11 +0100

    Clean code and fix issues

--- src/Core/Product/ProductPresenter.php
+++ src/Core/Product/ProductPresenter.php
@@ -688,7 +688,6 @@ class ProductPresenter
         if (isset($presentedProduct['features'])) {
             $presentedProduct['grouped_features'] = $this->buildGroupedFeatures($presentedProduct['features']);
         }
-        $presentedProduct['product_url'] = $this->getProductURL($presentedProduct, $language, true);
 
         return $presentedProduct;
     }
