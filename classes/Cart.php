e08f88218070524131d893e731adb9c2f742bffc
Axome <dev@axome.com>
2018-03-19 18:09:56 +0100

    Fix bug Cart Product weight Multishop

--- classes/Cart.php
+++ classes/Cart.php
@@ -557,7 +557,7 @@ class CartCore extends ObjectModel
             $sql->select('
 				product_attribute_shop.`price` AS price_attribute, product_attribute_shop.`ecotax` AS ecotax_attr,
 				IF (IFNULL(pa.`reference`, \'\') = \'\', p.`reference`, pa.`reference`) AS reference,
-				(p.`weight`+ pa.`weight`) weight_attribute,
+				(p.`weight`+ product_attribute_shop.`weight`) weight_attribute,
 				IF (IFNULL(pa.`ean13`, \'\') = \'\', p.`ean13`, pa.`ean13`) AS ean13,
 				IF (IFNULL(pa.`upc`, \'\') = \'\', p.`upc`, pa.`upc`) AS upc,
 				IFNULL(product_attribute_shop.`minimal_quantity`, product_shop.`minimal_quantity`) as minimal_quantity,
