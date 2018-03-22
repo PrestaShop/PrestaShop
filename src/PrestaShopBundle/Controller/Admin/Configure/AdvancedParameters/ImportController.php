23dcd2ce4c7485410bf7d95b553ed42cb4e6dbe8
Šarūnas Jonušas <sarunas.jonusas@invertus.lt>
2018-03-22 15:03:55 +0200

    start working on import migration

---
+++ src/PrestaShopBundle/Controller/Admin/Configure/AdvancedParameters/ImportController.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * 2007-2018 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2018 PrestaShop SA
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+namespace PrestaShopBundle\Controller\Admin\Configure\AdvancedParameters;
+
+use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
+use Symfony\Component\HttpFoundation\Request;
+
+class ImportController extends FrameworkBundleAdminController
+{
+    public function importAction(Request $request)
+    {
+        $legacyController = $request->attributes->get('_legacy_controller');
+
+        $params = [
+            'layoutHeaderToolbarBtn' => [],
+            'layoutTitle' => $this->get('translator')->trans('Import', [], 'Admin.Navigation.Menu'),
+            'requireAddonsSearch' => true,
+            'requireBulkActions' => false,
+            'showContentHeader' => true,
+            'enableSidebar' => true,
+            'help_link' => $this->generateSidebarLink($legacyController),
+            'servers' => $this->get('prestashop.adapter.memcache_server.manager')->getServers(),
+        ];
+
+        return $this->render('@AdvancedParameters/import.html.twig', $params);
+    }
+}
