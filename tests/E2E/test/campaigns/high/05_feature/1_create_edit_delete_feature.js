364961952d6750e5b3e0064abe87dbd12bc35615
fatmaBouchekoua <fatma.bouchekoua@prestashop.com>
2018-03-22 16:42:51 +0100

    TE: Refactor create, edit and delete feature script

---
+++ tests/E2E/test/campaigns/high/05_feature/1_create_edit_delete_feature.js
@@ -0,0 +1,70 @@
+const {AccessPageBO} = require('../../../selectors/BO/access_page');
+const {AccessPageFO} = require('../../../selectors/FO/access_page');
+const {AddProductPage} = require('../../../selectors/BO/add_product_page');
+const commonProduct = require('../02_product/product');
+const commonFeature = require('./feature');
+
+let productData = {
+  name: 'Feat',
+  quantity: "10",
+  price: '5',
+  image_name: 'image_test.jpg',
+  reference: 'feat',
+  feature: {
+    name: 'Feature',
+    value: 'Feature Value'
+  }
+};
+
+let featureData = {
+  name: 'Feature',
+  values: {
+    1: 'Feature Value'
+  }
+};
+
+scenario('Create "Feature"', () => {
+  /* Create feature */
+  scenario('Login in the Back Office', client => {
+    test('should open the browser', () => client.open());
+    test('should login successfully in the Back Office', () => client.signInBO(AccessPageBO));
+  }, 'attribute_and_feature');
+  commonFeature.createFeature(featureData);
+  commonProduct.createProduct(AddProductPage, productData);
+
+  /* Check the created feature */
+  scenario('Login in the Front Office', client => {
+    test('should login successfully in the Front Office', () => client.signInFO(AccessPageFO));
+  }, 'attribute_and_feature');
+  commonFeature.checkFeatureInFO(productData.name, featureData);
+
+  /* Update the created feature */
+  scenario('Go back to the Back Office', client => {
+    test('should go back to the Back Office', () => client.accessToBO(AccessPageBO));
+  }, 'attribute_and_feature');
+  commonFeature.updateFeature(featureData);
+
+  /* Check the updated feature */
+  scenario('Go back to the Front Office', client => {
+    test('should go back to the Front Office', () => client.accessToFO(AccessPageFO));
+  }, 'attribute_and_feature');
+  commonFeature.checkFeatureInFO(productData.name, featureData);
+
+  /* Delete feature */
+  scenario('Go back to the Back Office', client => {
+    test('should go back to the Back Office', () => client.accessToBO(AccessPageBO));
+  }, 'attribute_and_feature');
+  commonFeature.deleteFeature(featureData);
+  scenario('Logout from the Back Office', client => {
+    test('should logout successfully from the Back Office', () => client.signOutBO());
+  }, 'attribute_and_feature');
+
+  /* Check the deleted feature */
+  scenario('Go back to the Front Office', client => {
+    test('should go back to the Front Office', () => client.accessToFO(AccessPageFO));
+  }, 'attribute_and_feature');
+  commonFeature.checkDeletedFeatureInFO(productData.name);
+  scenario('Logout from the Front Office', client => {
+    test('should logout successfully from the Front Office', () => client.signOutFO(AccessPageFO));
+  }, 'attribute_and_feature');
+}, 'attribute_and_feature', true);
---
+++ tests/E2E/test/campaigns/high/05_feature/feature.js
@@ -0,0 +1,81 @@
+const {CatalogPage} = require('../../../selectors/BO/catalogpage/index');
+const {FeatureSubMenu} = require('../../../selectors/BO/catalogpage/feature_submenu');
+const {Menu} = require('../../../selectors/BO/menu.js');
+const {SearchProductPage} = require('../../../selectors/FO/search_product_page');
+let promise = Promise.resolve();
+
+/**** Example of feature data (all these properties are required) ****
+ * let featureData = {
+ *  name: 'Feature name',
+ *  values: {
+ *    1: 'Feature Value'
+ *  }
+ * };
+ *
+ */
+module.exports = {
+  createFeature(data) {
+    scenario('Create a new "Feature"', client => {
+      test('Should go to "Attributes & Features" page', () => client.goToSubtabMenuPage(Menu.Sell.Catalog.catalog_menu, Menu.Sell.Catalog.attributes_features_submenu));
+      test('should click on "Feature" subtab', () => client.waitForExistAndClick(FeatureSubMenu.tabmenu));
+      test('should click on "Add new feature" button', () => client.waitForExistAndClick(FeatureSubMenu.add_new_feature));
+      test('should set the "Name" input', () => client.waitAndSetValue(FeatureSubMenu.name_input, data.name + date_time));
+      test('should click on "Save" button', () => client.waitForExistAndClick(FeatureSubMenu.save_button));
+      test('should verify the appearance of the green validation', () => client.checkTextValue(CatalogPage.success_panel, '×\nSuccessful creation.'));
+      test('should search for the created feature', () => client.searchByValue(FeatureSubMenu.search_input.replace('%SEARCHBY', 'name'), FeatureSubMenu.search_button, data.name + date_time));
+      test('should select the created feature', () => client.waitForExistAndClick(FeatureSubMenu.selected_feature));
+      test('should click on "Add new feature value" button', () => client.waitForExistAndClick(FeatureSubMenu.add_value_button));
+      test('should set the "Value" input', () => client.waitAndSetValue(FeatureSubMenu.value_input, data.values[1]));
+      test('should click on "Save" button', () => client.waitForExistAndClick(FeatureSubMenu.save_value_button));
+      test('should verify the appearance of the green validation', () => client.checkTextValue(CatalogPage.success_panel, '×\nSuccessful creation.'));
+    }, 'attribute_and_feature');
+  },
+  checkFeatureInFO(productName, data) {
+    scenario('Check that the feature is well created/updated in the Front Office', client => {
+      test('should set the shop language to "English"', () => client.changeLanguage('english'));
+      test('should search for the product', () => client.searchByValue(SearchProductPage.search_input, SearchProductPage.search_button, productName + date_time));
+      test('should go to the product page', () => client.waitForExistAndClick(SearchProductPage.product_result_name));
+      test('should check the feature name', () => client.checkTextValue(SearchProductPage.feature_name, data.name + date_time));
+      test('should check the feature value', () => client.checkTextValue(SearchProductPage.feature_value, data.values[1]));
+    }, 'attribute_and_feature');
+  },
+  updateFeature(data) {
+    scenario('Update the created "Feature"', client => {
+      test('Should go to "Attributes & Features" page', () => client.goToSubtabMenuPage(Menu.Sell.Catalog.catalog_menu, Menu.Sell.Catalog.attributes_features_submenu));
+      test('should click on "Feature" subtab', () => client.waitForExistAndClick(FeatureSubMenu.tabmenu));
+      test('should search for the created feature', () => client.searchByValue(FeatureSubMenu.search_input.replace('%SEARCHBY', 'name'), FeatureSubMenu.search_button, data.name + date_time));
+      test('should click on "Edit" action', () => {
+        return promise
+          .then(() => client.clickOnAction(FeatureSubMenu.select_option, FeatureSubMenu.update_feature_button))
+          .then(() => client.editObjectData(data));
+      });
+      test('should set the "Name" input', () => client.waitAndSetValue(FeatureSubMenu.name_input, data.name + date_time));
+      test('should click on "Save" button', () => client.waitForExistAndClick(FeatureSubMenu.save_button));
+      test('should verify the appearance of the green validation', () => client.checkTextValue(CatalogPage.success_panel, '×\nSuccessful update.'));
+      test('should click on "Reset" button', () => client.waitForExistAndClick(FeatureSubMenu.reset_button));
+      test('should search for the updated feature', () => client.searchByValue(FeatureSubMenu.search_input.replace('%SEARCHBY', 'name'), FeatureSubMenu.search_button, data.name + date_time));
+      test('should select the feature', () => client.waitForExistAndClick(FeatureSubMenu.selected_feature));
+      test('should click on "Edit" action', () => client.waitForExistAndClick(FeatureSubMenu.update_feature_value_button));
+      test('should set the "Value" input', () => client.waitAndSetValue(FeatureSubMenu.value_input, data.values[1]));
+      test('should click on "Save" button', () => client.waitForExistAndClick(FeatureSubMenu.save_value_button));
+      test('should verify the appearance of the green validation', () => client.checkTextValue(CatalogPage.success_panel, '×\nSuccessful update.'));
+    }, 'attribute_and_feature');
+  },
+  deleteFeature(data) {
+    scenario('Delete the created "Feature"', client => {
+      test('Should go to "Attributes & Features" page', () => client.goToSubtabMenuPage(Menu.Sell.Catalog.catalog_menu, Menu.Sell.Catalog.attributes_features_submenu));
+      test('should click on "Feature" subtab', () => client.waitForExistAndClick(FeatureSubMenu.tabmenu));
+      test('should search for the created feature', () => client.searchByValue(FeatureSubMenu.search_input.replace('%SEARCHBY', 'name'), FeatureSubMenu.search_button, data.name + date_time));
+      test('should delete the created feature', () => client.clickOnAction(FeatureSubMenu.select_option, FeatureSubMenu.delete_feature, 'delete'));
+      test('should verify the appearance of the green validation', () => client.checkTextValue(CatalogPage.success_panel, '×\nSuccessful deletion.'));
+    }, 'attribute_and_feature');
+  },
+  checkDeletedFeatureInFO(productName) {
+    scenario('Check that the feature does not exist in the Front Office', client => {
+      test('should set the shop language to "English"', () => client.changeLanguage('english'));
+      test('should search for the product', () => client.searchByValue(SearchProductPage.search_input, SearchProductPage.search_button, productName + date_time));
+      test('should go to the product page', () => client.waitForExistAndClick(SearchProductPage.product_result_name));
+      test('should check that the feature has been deleted in the Front Office', () => client.checkDeleted(SearchProductPage.feature_name));
+    }, 'attribute_and_feature');
+  }
+};
