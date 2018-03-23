1e72e78a3a47b0862338aeaf88e8b0e4a1f18d50
Rokas <rokas.zygmantas@invertus.lt>
2018-03-23 14:53:15 +0200

    Migrate AdminPreferencesController to Symfony

---
+++ src/PrestaShopBundle/Controller/Admin/Configure/ShopParameters/PreferencesController.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * 2007-2018 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2018 PrestaShop SA
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+namespace PrestaShopBundle\Controller\Admin\Configure\ShopParameters;
+
+use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
+use PrestaShopBundle\Security\Voter\PageVoter;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Responsible of "Configure > Shop Parameters > General" page
+ */
+class PreferencesController extends FrameworkBundleAdminController
+{
+    const CONTROLLER_NAME = 'AdminPreferences';
+
+    /**
+     * @param Request $request
+     * @param FormInterface|null $form
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \LogicException
+     */
+    public function indexAction(Request $request, FormInterface $form = null)
+    {
+        if (is_null($form)) {
+            $form = $this->get('prestashop.adapter.preferences.form_handler')->getForm();
+        }
+
+        $twigValues = array(
+            'layoutHeaderToolbarBtn' => array(),
+            'layoutTitle' => $this->get('translator')->trans('Preferences', array(), 'Admin.Navigation.Menu'),
+            'requireAddonsSearch' => true,
+            'requireBulkActions' => false,
+            'showContentHeader' => true,
+            'enableSidebar' => true,
+            'help_link' => $this->generateSidebarLink('AdminPreferences'),
+            'requireFilterStatus' => false,
+            'form' => $form->createView(),
+        );
+
+        return $this->render('@ShopParameters/preferences.html.twig', $twigValues);
+    }
+
+    /**
+     * Process the form
+     *
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     *
+     * @throws \LogicException
+     */
+    public function processFormAction(Request $request)
+    {
+        if ($this->isDemoModeEnabled()) {
+            $this->addFlash('error', $this->getDemoModeErrorMessage());
+
+            return $this->redirectToRoute('admin_preferences');
+        }
+
+        if (!in_array(
+            $this->authorizationLevel($this::CONTROLLER_NAME),
+            array(
+                PageVoter::LEVEL_READ,
+                PageVoter::LEVEL_UPDATE,
+                PageVoter::LEVEL_CREATE,
+                PageVoter::LEVEL_DELETE,
+            )
+        )) {
+            $this->addFlash('error', $this->trans('You do not have permission to update this.', 'Admin.Notifications.Error'));
+
+            return $this->redirectToRoute('admin_preferences');
+        }
+
+        $this->dispatchHook('actionAdminPreferencesControllerPostProcessBefore', array('controller' => $this));
+
+        /** @var FormInterface $form */
+        $form = $this->get('prestashop.adapter.preferences.form_handler')->getForm();
+        $form->handleRequest($request);
+
+        if (!$form->isSubmitted()) {
+            return $this->redirectToRoute('admin_preferences');
+        }
+
+        $data = $form->getData();
+        $saveErrors = $this->get('prestashop.adapter.preferences.form_handler')->save($data);
+
+        if (0 === count($saveErrors)) {
+            $this->addFlash('success', $this->trans('Successful update.', 'Admin.Notifications.Success'));
+
+            return $this->redirectToRoute('admin_preferences');
+        }
+
+        $this->flashErrors($saveErrors);
+        return $this->redirectToRoute('admin_preferences');
+    }
+}
