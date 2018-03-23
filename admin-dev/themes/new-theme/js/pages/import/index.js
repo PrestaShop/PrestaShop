01378478605a2fd49062ca2557312fc16f4d9bb6
Šarūnas Jonušas <sarunas.jonusas@invertus.lt>
2018-03-23 10:34:17 +0200

    added entity alert toggling

---
+++ admin-dev/themes/new-theme/js/pages/import/index.js
@@ -0,0 +1,44 @@
+/**
+ * 2007-2018 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2018 PrestaShop SA
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+const $ = window.$;
+
+$(() => {
+    const entity_categories = 0;
+    const entity_products = 1;
+
+    $('.js-entity-select').on('change', toggleEntityAlert);
+
+    function toggleEntityAlert() {
+        let $alert = $('.js-entity-alert');
+        let selectedEntity = $(this).find('option:selected').val();
+
+        if ([entity_categories, entity_products].includes(parseInt(selectedEntity))) {
+            $alert.show()
+        } else {
+            $alert.hide();
+        }
+    }
+});
