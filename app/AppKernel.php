fd9e288224e2aa00e47829dcf6d826d159a7782e
Mickaël Andrieu <andrieu.travail@gmail.com>
2018-03-28 13:45:06 +0100

    Address @alegout comments

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -72,13 +72,6 @@ class AppKernel extends Kernel
             $_SERVER['SYMFONY__CACHE__DRIVER'] = 'array';
         }
 
-        /* Will not work until PrestaShop is installed */
-        if ($this->parametersFileExists()) {
-            try {
-                $this->enableComposerAutoloaderOnModules($this->getActiveModules());
-            } catch (\Exception $e) {}
-        }
-
         return $bundles;
     }
 
@@ -210,22 +203,5 @@ class AppKernel extends Kernel
             'charset' => 'utf8',
             'driver' => 'pdo_mysql',
         ));
-    }
-
-    /**
-     * Enable auto loading of module Composer autoloader if needed.
-     * Need to be done as earlier as possible in application lifecycle.
-     *
-     * @param array $modules the list of modules
-     */
-    private function enableComposerAutoloaderOnModules($modules)
-    {
-        foreach ($modules as $module) {
-            $autoloader = __DIR__.'/../modules/'.$module.'/vendor/autoload.php';
-
-            if (file_exists($autoloader)) {
-                include_once $autoloader;
-            }
-        }
     }
 }
