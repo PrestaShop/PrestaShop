776641468a12b65893b972fb30aa4c2f0c044a43
Šarūnas Jonušas <sarunas.jonusas@invertus.lt>
2018-03-29 10:44:26 +0300

    add dynamic history files number update

--- admin-dev/themes/new-theme/js/pages/import/FormFieldToggle.js
+++ admin-dev/themes/new-theme/js/pages/import/FormFieldToggle.js
@@ -147,7 +147,6 @@ export default class FormFieldToggle {
       $availableFields.html(fields);
       $availableFields.find('[data-toggle="popover"]').popover();
     }).catch(error => {
-
     });
   }
 }
