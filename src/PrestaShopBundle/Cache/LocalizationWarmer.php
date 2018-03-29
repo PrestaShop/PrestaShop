a36f04de2b27a0092c3d1b258f959bb46bf08f84
Bastien Bieri <bastien.bieri@prestashop.com>
2018-03-29 18:14:50 +0200

    CLDR language pack data should not be downloaded when in test.

--- src/PrestaShopBundle/Cache/LocalizationWarmer.php
+++ src/PrestaShopBundle/Cache/LocalizationWarmer.php
@@ -54,20 +54,28 @@ class LocalizationWarmer implements CacheWarmerInterface
             }
         }
 
-        $path_cache_file = $cacheDir.$this->version.$this->country.'.xml';
+        $path_cache_file = $cacheDir . $this->version . $this->country . '.xml';
 
         if (is_file($path_cache_file)) {
             $localization_file_content = file_get_contents($path_cache_file);
         } else {
-            $localization_file_content = @Tools::file_get_contents('http://api.prestashop.com/localization/'.$this->version.'/'.$this->country.'.xml');
-            if (!@simplexml_load_string($localization_file_content)) {
+            if (!_PS_IN_TEST_) {
+                $localization_file_content = @Tools::file_get_contents(
+                    'http://api.prestashop.com/localization/' . $this->version . '/' . $this->country . '.xml'
+                );
+
+                if (!@simplexml_load_string($localization_file_content)) {
+                    $localization_file_content = false;
+                }
+            } else {
                 $localization_file_content = false;
             }
-            if (!$localization_file_content) {
-                $localization_file = _PS_ROOT_DIR_.'/localization/default.xml';
 
-                if (file_exists(_PS_ROOT_DIR_.'/localization/'.$this->country.'.xml')) {
-                    $localization_file = _PS_ROOT_DIR_.'/localization/'.$this->country.'.xml';
+            if (!$localization_file_content) {
+                $localization_file = _PS_ROOT_DIR_ . '/localization/default.xml';
+
+                if (file_exists(_PS_ROOT_DIR_ . '/localization/' . $this->country . '.xml')) {
+                    $localization_file = _PS_ROOT_DIR_ . '/localization/' . $this->country . '.xml';
                 }
 
                 $localization_file_content = file_get_contents($localization_file);
