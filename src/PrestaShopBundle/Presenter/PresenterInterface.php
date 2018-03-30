d51573ba7883ced5d04af65c4e3bb390ccb2ca47
jocelyn fournier <jocelyn.fournier@softizy.com>
2018-03-30 23:53:11 +0200

    add new presenter implementation optimized for performances (lazy loading)

--- src/PrestaShopBundle/Presenter/PresenterInterface.php
+++ src/PrestaShopBundle/Presenter/PresenterInterface.php
@@ -23,10 +23,9 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
+namespace PrestaShopBundle\Presenter;
 
-namespace PrestaShop\PrestaShop\Adapter\Order;
-
-class OrderReturnPresenter extends \PrestaShopBundle\Presenter\Order\OrderReturnPresenter
+interface PresenterInterface
 {
-
+    public function present($object); // must return an array or an AbstractLazyPresenter
 }
