cbe4cc0d79a18265d38b2588f05f65e18230a0a7
jocelyn fournier <jocelyn.fournier@softizy.com>
2018-04-02 13:27:57 +0200

    remove @package annotations

--- src/Adapter/ObjectPresenter.php
+++ src/Adapter/ObjectPresenter.php
@@ -30,8 +30,6 @@ namespace PrestaShop\PrestaShop\Adapter;
  * @deprecated since 1.7.4.0
  * @see \PrestaShop\PrestaShop\Adapter\Presenter\Object\ObjectPresenter
  *
- * Class ObjectPresenter
- * @package PrestaShop\PrestaShop\Adapter
  */
 class ObjectPresenter extends \PrestaShop\PrestaShop\Adapter\Presenter\Object\ObjectPresenter
 {
