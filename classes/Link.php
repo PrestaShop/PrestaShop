bd12bf38e505f008b7c87735ece381eded93316b
Mickaël Andrieu <andrieu.travail@gmail.com>
2018-04-09 15:49:35 +0200

    Disable new Logs page

--- classes/Link.php
+++ classes/Link.php
@@ -737,7 +737,7 @@ class LinkCore
                     'AdminTranslationSf' => 'admin_international_translation_overview',
                     'AdminInformation' => 'admin_system_information',
                     'AdminAddonsCatalog' => 'admin_module_addons_store',
-                    'AdminLogs' => 'admin_logs',
+                    // 'AdminLogs' => 'admin_logs', @todo: to uncomment when Logs Page is done
                     'AdminPerformance' => 'admin_performance',
                     'AdminAdminPreferences' => 'admin_administration',
                     'AdminMaintenance' => 'admin_maintenance',
