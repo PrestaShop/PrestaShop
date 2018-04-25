0a05d3392588e0ad0bc315c57cf2d5ac27626195
Šarūnas Jonušas <sarunas.jonusas@invertus.lt>
2018-04-25 18:43:05 +0300

    WIP add data provider & definition factory for logs grid

---
+++ src/Core/Grid/DataProvider/LogGridDataProvider.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * 2007-2018 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2018 PrestaShop SA
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+namespace PrestaShop\PrestaShop\Core\Grid\DataProvider;
+
+use PrestaShopBundle\Entity\Repository\LogRepository;
+use PrestaShopBundle\Service\Hook\HookDispatcher;
+
+/**
+ * Class LogsGridDataProvider is responsible for providing data for Logs grid
+ */
+final class LogGridDataProvider implements GridDataProviderInterface
+{
+    /**
+     * @var LogRepository
+     */
+    private $logRepository;
+
+    /**
+     * @var HookDispatcher
+     */
+    private $hookDispatcher;
+
+    /**
+     * @param LogRepository $logRepository
+     * @param HookDispatcher $hookDispatcher
+     */
+    public function __construct(
+        LogRepository $logRepository,
+        HookDispatcher $hookDispatcher
+    ) {
+        $this->logRepository = $logRepository;
+        $this->hookDispatcher = $hookDispatcher;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRows(array $filters)
+    {
+        $logQuery = $this->logRepository->getAllWithEmployeeInformationQuery($filters);
+
+        $this->hookDispatcher->dispatchForParameters('modifyLogGridQuery', [
+            'query' => $logQuery,
+        ]);
+
+        $logs = $logQuery->execute()->fetchAll(\PDO::FETCH_ASSOC);
+
+        return $logs;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRowsTotal()
+    {
+        return count($this->logRepository->findAll());
+    }
+}
