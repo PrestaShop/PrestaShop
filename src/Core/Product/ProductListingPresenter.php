92a3a84f77b711ddf93358dba7cf14915af04fb8
Matthieu Mota <matthieumota@gmail.com>
2018-04-30 13:46:52 +0200

    Fix add to cart url in product listing appear or not

--- src/Core/Product/ProductListingPresenter.php
+++ src/Core/Product/ProductListingPresenter.php
@@ -34,4 +34,42 @@ namespace PrestaShop\PrestaShop\Core\Product;
  */
 class ProductListingPresenter extends \PrestaShop\PrestaShop\Adapter\Presenter\Product\ProductListingPresenter
 {
+    public function present(
+        ProductPresentationSettings $settings,
+        array $product,
+        \Language $language
+    ) {
+        $presentedProduct = parent::present(
+            $settings,
+            $product,
+            $language
+        );
+
+        if ($product['id_product_attribute'] != 0 && !$settings->allow_add_variant_to_cart_from_listing) {
+            $presentedProduct['add_to_cart_url'] = null;
+        }
+
+        if ($product['customizable'] == 2 || !empty($product['customization_required'])) {
+            $presentedProduct['add_to_cart_url'] = null;
+        }
+
+        if (!$this->shouldEnableAddToCartButton($product, $settings)) {
+            $presentedProduct['add_to_cart_url'] = null;
+        }
+
+        return $presentedProduct;
+    }
+
+    protected function shouldEnableAddToCartButton(array $product, ProductPresentationSettings $settings)
+    {
+        if ($settings->stock_management_enabled && !$product['allow_oosp'] && $product['quantity'] <= 0) {
+            return false;
+        }
+
+        if (isset($product['attributes']) && count($product['attributes']) > 0 && !$settings->allow_add_variant_to_cart_from_listing) {
+            return false;
+        }
+
+        return parent::shouldEnableAddToCartButton($product, $settings);
+    }
 }
