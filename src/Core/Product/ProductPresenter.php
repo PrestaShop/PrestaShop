f37cd5744d59de7f9d9ad9ca1c5b63eeeb31ced0
rdy4ever <rdy4ever@users.noreply.github.com>
2018-05-03 21:48:46 +0300

    Did some cleanup

--- src/Core/Product/ProductPresenter.php
+++ src/Core/Product/ProductPresenter.php
@@ -688,16 +688,16 @@ class ProductPresenter
         if (isset($presentedProduct['features'])) {
             $presentedProduct['grouped_features'] = $this->buildGroupedFeatures($presentedProduct['features']);
         }
-        
+
         //microdata availability
         $presentedProduct['seo_availability'] = 'https://schema.org/';
-        if($product['quantity'] > 0){
+        if ($product['quantity'] > 0) {
             $presentedProduct['seo_availability'] .= 'InStock';
         }
-        elseif ($product['quantity'] <= 0 && $product['allow_oosp']){
+        elseif ($product['quantity'] <= 0 && $product['allow_oosp']) {
             $presentedProduct['seo_availability'] .= 'PreOrder';
         }
-        else{
+        else {
             $presentedProduct['seo_availability'] .= 'OutOfStock';
         }
 
