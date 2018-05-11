6403910d58e349bb8f25b8aa4794266c3131d2f6
Aurélien Pelletier <aurelien.pelletier@gmail.com>
2018-05-11 13:55:09 +0200

    Formatting and removing unsused code

--- docs/docker/prestashopCli.sh
+++ docs/docker/prestashopCli.sh
@@ -19,31 +19,19 @@ PRESTASHOP_DEST="$CURRENT_DIR/nginx_fpm/prestashop"
 PRESTASHOP_SOURCE="$CURRENT_DIR/prestashop-source"
 
 function clonePrestaShop() {
-	echo "Cloning PrestaShop"
-	#git clone git@github.com:PrestaShop/PrestaShop.git $PRESTASHOP_SOURCE
-	cd $PRESTASHOP_SOURCE
-	git checkout $PS_BRANCH
+    echo "Cloning PrestaShop"
+    git clone git@github.com:PrestaShop/PrestaShop.git $PRESTASHOP_SOURCE
+    cd $PRESTASHOP_SOURCE
+    git checkout $PS_BRANCH
 }
 
 function buildRelease() {
-	echo "Building new Release: $PS_VERSION from branch  $PS_BRANCH"
-	cd $PRESTASHOP_SOURCE
-	php tools/build/CreateRelease.php --version="$PS_VERSION" --no-zip --destination-dir=$PRESTASHOP_DEST
-	mv $PRESTASHOP_DEST/prestashop/* $PRESTASHOP_DEST/
-	rm -Rf $PRESTASHOP_DEST/prestashop
-}
-
-
-function installPrestaShop() {
-	echo "Installating PrestaShop"
-	if ! php $PRESTASHOP_DEST/install/index_cli.php  --language=fr  --country=fr  --domain=$SHOPNAME.localtest.me  --db_server=$DB_SERVER  --db_user=$DB_USER  --db_name=$SHOPNAME  --db_password=$DB_PASSWORD  --firstname=John  --lastname=doe  --email=test@test.com  --password=azerty  --newsletter=0  --db_create=1
-	then
-		echo 'Installation failed!'
-		exit 1
-	fi
-	rm -Rf $PRESTASHOP_DEST/prestashop/install/
+    echo "Building new Release: $PS_VERSION from branch  $PS_BRANCH"
+    cd $PRESTASHOP_SOURCE
+    php tools/build/CreateRelease.php --version="$PS_VERSION" --no-zip --destination-dir=$PRESTASHOP_DEST
+    mv $PRESTASHOP_DEST/prestashop/* $PRESTASHOP_DEST/
+    rm -Rf $PRESTASHOP_DEST/prestashop
 }
 
 clonePrestaShop
 buildRelease
-#installPrestaShop
