8f63c03ef25fc4000889c3de00354f9b64153f2b
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2018-05-16 19:49:17 +0200

    Boy scout

--- tests/Integration/PrestaShopBundle/Controller/Api/TranslationControllerTest.php
+++ tests/Integration/PrestaShopBundle/Controller/Api/TranslationControllerTest.php
@@ -68,7 +68,7 @@ class TranslationControllerTest extends ApiTestCase
      */
     public function it_should_return_ok_response_when_requesting_domain($params)
     {
-        $this->assetOkRequest('api_translation_domain_catalog', $params);
+        $this->assertOkRequest('api_translation_domain_catalog', $params);
     }
 
     /**
@@ -120,7 +120,7 @@ class TranslationControllerTest extends ApiTestCase
      */
     public function it_should_return_ok_response_when_requesting_domain_catalog($params)
     {
-        $this->assetOkRequest('api_translation_domains_tree', $params);
+        $this->assertOkRequest('api_translation_domains_tree', $params);
     }
 
     /**
