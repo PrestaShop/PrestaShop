335851e5523b19aac49a65e5173414cfbc7df083
Pablo Borowicz <eternoendless@gmail.com>
2018-06-27 12:57:59 +0200

    // Changelog 1.7.3.4

--- config/autoload.php
+++ config/autoload.php
@@ -24,7 +24,7 @@
  * International Registered Trademark & Property of PrestaShop SA
  */
 
-define('_PS_VERSION_', '1.7.3.3');
+define('_PS_VERSION_', '1.7.3.4');
 
 require_once(_PS_CONFIG_DIR_.'alias.php');
 require_once(_PS_CLASS_DIR_.'PrestaShopAutoload.php');
