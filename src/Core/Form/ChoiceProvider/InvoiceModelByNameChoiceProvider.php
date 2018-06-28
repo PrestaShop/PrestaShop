0b3c7091f5bd41370a6fb25473069080d681b4d5
Rokas Zygmantas <rokas.zygmantas@invertus.eu>
2018-06-28 17:46:36 +0300

    Add choice providers for invoice options

---
+++ src/Core/Form/ChoiceProvider/InvoiceModelByNameChoiceProvider.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * 2007-2018 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2018 PrestaShop SA
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+namespace PrestaShop\PrestaShop\Core\Form\ChoiceProvider;
+
+use PrestaShop\PrestaShop\Adapter\Country\CountryDataProvider;
+use PrestaShop\PrestaShop\Adapter\OrderState\OrderStateDataProvider;
+use PrestaShop\PrestaShop\Core\File\InvoiceModelFinder;
+use PrestaShop\PrestaShop\Core\Form\FormChoiceProviderInterface;
+
+/**
+ * Class InvoiceModelByNameChoiceProvider provides invoice model choices with name values
+ */
+final class InvoiceModelByNameChoiceProvider implements FormChoiceProviderInterface
+{
+    /**
+     * @var InvoiceModelFinder
+     */
+    private $invoiceModelFinder;
+
+    public function __construct(InvoiceModelFinder $invoiceModelFinder)
+    {
+        $this->invoiceModelFinder = $invoiceModelFinder;
+    }
+
+    /**
+     * Get invoice model choices
+     *
+     * @return array
+     */
+    public function getChoices()
+    {
+        $choices = [
+            'invoice' => 'invoice',
+        ];
+
+        $invoiceModels = $this->invoiceModelFinder->find();
+
+        foreach ($invoiceModels as $invoiceModel) {
+            $modelName = basename($invoiceModel, '.tpl');
+            $choices[$modelName] = $modelName;
+        }
+
+        return $choices;
+    }
+}
