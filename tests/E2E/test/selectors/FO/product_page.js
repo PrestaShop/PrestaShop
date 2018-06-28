bfcc05e39cf7794c22a86e944b49561663182287
fouratachour <fourat.ach@gmail.com>
2018-06-28 10:48:36 +0100

    TE: Edit and delete widget

--- tests/E2E/test/selectors/FO/product_page.js
+++ tests/E2E/test/selectors/FO/product_page.js
@@ -31,10 +31,12 @@ module.exports = {
     product_description: '//*[@id="description"]',
     product_detail_tab: '//*[@role="tablist"]//li[2]',
     attachments_tab: '//*[@id="main"]//div[@class="product-information"]//a[@aria-controls="attachments"]',
-    widget_after_product_thumbs: '//*[@id="content"]//p[contains(text(),"%NAME")]',
-    second_widget_after_product_thumbs:'//*[@id="content"]//div[2]/p',
     product_footer_linkwidget:'//*[@id="main"]/div[2]//p[contains(text(),"%DISPLAYFOOTERPRODUCT")]',
+    filename_link: '(//*[@id="attachments"]//a)[1]',
     second_product_footer_linkwidget:'//*[@id="main"]/div[2]/div/div[2]/p',
-    filename_link: '(//*[@id="attachments"]//a)[1]'
+    widget_after_product_thumbs: '//*[@id="content"]//div[contains(@class,"links")]//p[contains(text(),"%NAME")]',
+    second_widget_after_product_thumbs:'//*[@id="content"]//div[contains(@class,"links")]//div[2]/p',
+    display_footer_product_linkwidget:'//*[@id="main"]/div[2]/div//p[contains(text(),"%DISPLAYFOOTERPRODUCT")]',
+    display_second_footer_product_linkwidget:'//*[@id="main"]/div[2]/div/div[2]/p'
   }
 };
