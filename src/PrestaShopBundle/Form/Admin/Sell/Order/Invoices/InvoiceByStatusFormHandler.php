b96754714b37f4bfec14e83d0f6cbadf2d401d09
Rokas <rokas.zygmantas@gmail.com>
2018-06-30 19:33:43 +0300

    Move business logic out of InvoicesController to related form handlers

---
+++ src/PrestaShopBundle/Form/Admin/Sell/Order/Invoices/InvoiceByStatusFormHandler.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * 2007-2018 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2018 PrestaShop SA
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+namespace PrestaShopBundle\Form\Admin\Sell\Order\Invoices;
+
+use PrestaShop\PrestaShop\Core\DataProvider\OrderInvoiceDataProviderInterface;
+use PrestaShop\PrestaShop\Core\Form\FormDataProviderInterface;
+use PrestaShop\PrestaShop\Core\Form\FormHandler;
+use PrestaShop\PrestaShop\Core\PDF\InvoicePDFGeneratorInterface;
+use PrestaShopBundle\Service\Hook\HookDispatcher;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Class InvoiceByStatusFormHandler manages the data manipulated using "By status" form
+ * in "Sell > Orders > Invoices" page
+ */
+class InvoiceByStatusFormHandler extends FormHandler
+{
+    /**
+     * @var OrderInvoiceDataProviderInterface
+     */
+    private $orderInvoiceDataProvider;
+
+    /**
+     * @var InvoicePDFGeneratorInterface
+     */
+    private $invoicePDFGenerator;
+
+    public function __construct(
+        FormBuilderInterface $formBuilder,
+        HookDispatcher $hookDispatcher,
+        FormDataProviderInterface $formDataProvider,
+        array $formTypes,
+        $hookName,
+        OrderInvoiceDataProviderInterface $orderInvoiceDataProvider,
+        InvoicePDFGeneratorInterface $invoicePDFGenerator
+    ) {
+        parent::__construct($formBuilder, $hookDispatcher, $formDataProvider, $formTypes, $hookName);
+        $this->orderInvoiceDataProvider = $orderInvoiceDataProvider;
+        $this->invoicePDFGenerator = $invoicePDFGenerator;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function save(array $data)
+    {
+        if ($errors = parent::save($data)) {
+            return $errors;
+        }
+
+        $invoiceCollection = [];
+
+        foreach ($data['generate_by_status']['order_states'] as $orderStateId) {
+            // Put invoices for each selected status into one collection
+            $invoiceCollection = array_merge(
+                $invoiceCollection,
+                $this->orderInvoiceDataProvider->getByStatus($orderStateId)
+            );
+        }
+
+        if (empty($invoiceCollection)) {
+            $errors[] = [
+                'key' => 'No invoice has been found for this status.',
+                'domain' => 'Admin.Orderscustomers.Notification',
+                'parameters' => [],
+            ];
+        } else {
+            // Generate PDF out of found invoices
+            $this->invoicePDFGenerator->generateInvoicesPDF($invoiceCollection);
+        }
+
+        return $errors;
+    }
+}
