ee46c4ecaa40578f83618cd300cdc33d7ff6e237
Thomas Nabord <thomas.nabord@prestashop.com>
2018-07-03 10:49:30 +0100

    Use AddonsCollection for module presenter

--- src/Adapter/Presenter/Module/ModulePresenter.php
+++ src/Adapter/Presenter/Module/ModulePresenter.php
@@ -29,6 +29,7 @@ use Currency;
 use PrestaShop\PrestaShop\Adapter\Module\Module;
 use PrestaShop\PrestaShop\Adapter\Product\PriceFormatter;
 use PrestaShop\PrestaShop\Adapter\Presenter\PresenterInterface;
+use PrestaShop\PrestaShop\Core\Addon\AddonsCollection;
 use Exception;
 
 class ModulePresenter implements PresenterInterface
@@ -85,10 +86,10 @@ class ModulePresenter implements PresenterInterface
     /**
      * Transform a collection of addons as a simple array of data.
      *
-     * @param array $modules
+     * @param AddonsCollection $modules
      * @return array
      */
-    public function presentCollection(array $modules)
+    public function presentCollection(AddonsCollection $modules)
     {
         $presentedProducts = array();
         foreach ($modules as $name => $product) {
