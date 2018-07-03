78e247b04740be0b752371791e5964b2374e2792
Šarūnas Jonušas <sarunas.jonusas@invertus.lt>
2018-07-03 17:42:22 +0300

    update row action naming

--- src/PrestaShopBundle/Form/Admin/Configure/AdvancedParameters/RequestSql/RequestSqlType.php
+++ src/PrestaShopBundle/Form/Admin/Configure/AdvancedParameters/RequestSql/RequestSqlType.php
@@ -30,8 +30,10 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
+/**
+ * Class RequestSqlType defines RequestSql entity form type
+ */
 class RequestSqlType extends AbstractType
 {
     /**
@@ -43,23 +45,5 @@ class RequestSqlType extends AbstractType
             ->add('name', TextType::class)
             ->add('sql', TextareaType::class)
         ;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'translation_domain' => '',
-        ]);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getBlockPrefix()
-    {
-        return 'request_sql_block';
     }
 }
