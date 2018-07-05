8503bd6373afea949248389f9ef42f7a4ec32415
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2018-07-05 15:40:23 +0200

    Use php_self for Symfony Front Controllers

--- classes/Hook.php
+++ classes/Hook.php
@@ -811,7 +811,9 @@ class HookCore extends ObjectModel
                 $exceptions = Module::getExceptionsStatic($array['id_module'], $array['id_hook']);
 
                 $controller_obj = Context::getContext()->controller;
-                $controller = $controller_obj === null ? null : $controller_obj->controller_name;
+                $controller = isset($controller_obj->controller_name) ?
+                            $controller_obj->controller_name :
+                            $controller_obj->php_self;
 
                 //check if current controller is a module controller
                 if (isset($controller_obj->module) && Validate::isLoadedObject($controller_obj->module)) {
