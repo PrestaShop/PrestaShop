ea29a301739f1cb747753ddc27d3d02404e082bb
Presta Module <support@presta-module.com>
2018-07-09 17:46:46 +0200

    CO: Fix Cart::isVirtualCart() method when cart is empty

--- classes/Cart.php
+++ classes/Cart.php
@@ -3888,7 +3888,12 @@ class CartCore extends ObjectModel
         }
 
         if (!isset(self::$_isVirtualCart[$this->id])) {
-            $isVirtual = !$this->hasRealProducts();
+            $products = $this->getProducts();
+            if (!count($products)) {
+                $isVirtual = false;
+            } else {
+                $isVirtual = !$this->hasRealProducts();
+            }
 
             self::$_isVirtualCart[$this->id] = $isVirtual;
         }
