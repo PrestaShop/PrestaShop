0c0ce064f3406bc0ecb7400ce38934d0fe568443
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2018-07-10 05:24:42 +0200

    Useless lines

--- src/PrestaShopBundle/Form/Admin/Improve/Design/PositionsType.php
+++ src/PrestaShopBundle/Form/Admin/Improve/Design/PositionsType.php
@@ -54,6 +54,7 @@ class PositionsType extends TranslatorAwareType
                 FormType\ChoiceType::class,
                 [
                     'required' => false,
+                    'choices' => $this->getTransplantTo(),
                 ]
             )
             ->add(
@@ -78,7 +79,7 @@ class PositionsType extends TranslatorAwareType
                     'attr' => [
                         'size' => 25,
                     ],
-                    'choices' => $this->formatExceptionsData([]),
+                    'choices' => $this->getExceptionsData(),
                     'choice_attr' => function ($value, $key, $index) {
                         return $value === null ? ['disabled' => 'disabled'] : [];
                     },
@@ -101,7 +102,7 @@ class PositionsType extends TranslatorAwareType
         $hooks = $moduleInstance->getPossibleHooksList();
     }
 
-    protected function formatExceptionsData(array $fileList)
+    protected function getExceptionsData()
     {
         $data = [
             $this->trans('___________ CUSTOM ___________', 'Admin.Design.Feature') => null
@@ -109,12 +110,6 @@ class PositionsType extends TranslatorAwareType
 
         $controllers = Dispatcher::getControllers(_PS_FRONT_CONTROLLER_DIR_);
         ksort($controllers);
-
-        foreach ($fileList as $k => $v) {
-            if (!isset($controllers[$v])) {
-                $data[$v] = false;
-            }
-        }
 
         $data[$this->trans('____________ CORE ____________', 'Admin.Design.Feature')] = null;
         foreach ($controllers as $k => $v) {
