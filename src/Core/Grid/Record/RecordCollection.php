ac89252032856dd18d3c06d1a3a59db53140b0ab
Šarūnas Jonušas <sarunas.jonusas@invertus.lt>
2018-07-17 09:59:43 +0300

    added rows modifier

--- src/Core/Grid/Record/RecordCollection.php
+++ src/Core/Grid/Record/RecordCollection.php
@@ -48,4 +48,16 @@ final class RecordCollection extends AbstractCollection implements RecordCollect
     {
         return $this->items;
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function applyModification(callable $modifier)
+    {
+        foreach ($this->items as $key => $row) {
+            $this->items[$key] = $modifier($row);
+        }
+
+        return $this;
+    }
 }
