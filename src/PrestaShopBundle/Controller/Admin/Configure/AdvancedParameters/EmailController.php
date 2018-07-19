a592ec60f6a0173999236989f0acbaae90bcff89
Šarūnas Jonušas <sarunas.jonusas@invertus.lt>
2018-07-19 17:15:44 +0300

    implement email configuration form saving

--- src/PrestaShopBundle/Controller/Admin/Configure/AdvancedParameters/EmailController.php
+++ src/PrestaShopBundle/Controller/Admin/Configure/AdvancedParameters/EmailController.php
@@ -27,20 +27,66 @@
 namespace PrestaShopBundle\Controller\Admin\Configure\AdvancedParameters;
 
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * Class EmailController is responsible for handling "Configure > Advanced Parameters > E-mail" page
+ */
 class EmailController extends FrameworkBundleAdminController
 {
+    /**
+     * Show email configuration page
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
     public function indexAction(Request $request)
     {
-        $emailConfigurationFormHandler = $this->get('prestashop.admin.email_configuration.form_handler');
-        $form = $emailConfigurationFormHandler->getForm();
-
+        $emailConfigurationForm = $this->getEmailConfigurationFormHandler()->getForm();
         $extensionChecker = $this->get('prestashop.core.configuration.php_extension_checker');
 
         return $this->render('@PrestaShop/Admin/Configure/AdvancedParameters/Email/email.html.twig', [
-            'emailConfigurationForm' => $form->createView(),
+            'emailConfigurationForm' => $emailConfigurationForm->createView(),
             'isOpenSslExtensionLoaded' => $extensionChecker->loaded('openssl'),
+            'enableSidebar' => true,
+            'help_link' => $this->generateSidebarLink($request->attributes->get('_legacy_controller')),
         ]);
+    }
+
+    /**
+     * Process email configuration saving
+     *
+     * @param Request $request
+     *
+     * @return RedirectResponse
+     */
+    public function processFormAction(Request $request)
+    {
+        $formHandler = $this->getEmailConfigurationFormHandler();
+        $emailConfigurationForm = $formHandler->getForm();
+        $emailConfigurationForm->handleRequest($request);
+
+        if ($emailConfigurationForm->isSubmitted()) {
+            $errors = $formHandler->save($emailConfigurationForm->getData());
+
+            if (empty($errors)) {
+                $this->addFlash('success', $this->trans('Successful update.', 'Admin.Notifications.Success'));
+            } else {
+                $this->flashErrors($errors);
+            }
+        }
+
+        return $this->redirectToRoute('admin_email');
+    }
+
+    /**
+     * Get email configuration form handler
+     */
+    protected function getEmailConfigurationFormHandler()
+    {
+        return $this->get('prestashop.admin.email_configuration.form_handler');
     }
 }
