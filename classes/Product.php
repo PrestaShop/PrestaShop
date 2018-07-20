2da46cd6a707d8d0780f6a0cdcbd662f7726bf58
Lathanao <tanguy.salmon@gmail.com>
2018-07-20 13:20:09 +0700

    CO: Add new variable in product.php, to be able to sort features in front
    | Questions     | Answers
    | ------------- | -------------------------------------------------------
    | Branch?       | 1.7.4.x
    | Description?  | This commit allows to be able to sort features in front. By a range system, it's possible to add, e.g. title for sorted features.
    It's useful for product with a lot of features, like computers for example.
    | Type?         | improvement
    | Category?     | CO
    | BC breaks?    | No
    | Deprecations? | No
    | Fixed ticket? | No
    | How to test?  | Just add a small range snippet in templates\catalog\_partials\product-details.tpl . I can provide snippet.

--- classes/Product.php
+++ classes/Product.php
@@ -4942,7 +4942,7 @@ class ProductCore extends ObjectModel
         }
         if (!array_key_exists($id_product . '-' . $id_lang, self::$_frontFeaturesCache)) {
             self::$_frontFeaturesCache[$id_product . '-' . $id_lang] = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS('
-				SELECT name, value, pf.id_feature
+				SELECT name, value, pf.id_feature, f.position
 				FROM ' . _DB_PREFIX_ . 'feature_product pf
 				LEFT JOIN ' . _DB_PREFIX_ . 'feature_lang fl ON (fl.id_feature = pf.id_feature AND fl.id_lang = ' . (int) $id_lang . ')
 				LEFT JOIN ' . _DB_PREFIX_ . 'feature_value_lang fvl ON (fvl.id_feature_value = pf.id_feature_value AND fvl.id_lang = ' . (int) $id_lang . ')
