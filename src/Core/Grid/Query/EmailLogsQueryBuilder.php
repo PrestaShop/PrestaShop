f9ade0a05478de0b327268c1ec3e90ffd70c0431
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2018-07-23 22:58:22 +0300

    add email logs grid query builder

---
+++ src/Core/Grid/Query/EmailLogsQueryBuilder.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace PrestaShop\PrestaShop\Core\Grid\Query;
+
+use Doctrine\DBAL\Query\QueryBuilder;
+use PrestaShop\PrestaShop\Core\Grid\Search\SearchCriteriaInterface;
+
+/**
+ * Class EmailLogsQueryBuilder is responsible for building queries for email logs grid data
+ */
+final class EmailLogsQueryBuilder extends AbstractDoctrineQueryBuilder
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getSearchQueryBuilder(SearchCriteriaInterface $searchCriteria = null)
+    {
+        $qb = $this->getQueryBuilder($searchCriteria->getFilters());
+        $qb->select('m.*, l.name AS language')
+            ->orderBy(
+                $searchCriteria->getOrderBy(),
+                $searchCriteria->getOrderWay()
+            )
+            ->setFirstResult($searchCriteria->getOffset())
+            ->setMaxResults($searchCriteria->getLimit());
+
+        return $qb;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCountQueryBuilder(SearchCriteriaInterface $searchCriteria = null)
+    {
+        $qb = $this->getQueryBuilder($searchCriteria->getFilters());
+        $qb->select('COUNT(m.id_mail)');
+
+        return $qb;
+    }
+
+    /**
+     * Get generic query builder
+     *
+     * @param array $filters
+     *
+     * @return QueryBuilder
+     */
+    private function getQueryBuilder(array $filters)
+    {
+        $qb = $this->connection
+            ->createQueryBuilder()
+            ->from($this->dbPrefix.'mail', 'm')
+            ->leftJoin('m', $this->dbPrefix.'lang', 'l', 'm.id_lang = l.id_lang');
+
+        foreach ($filters as $name => $value) {
+            $qb->andWhere("$name LIKE :$name");
+            $qb->setParameter($name, '%'.$value.'%');
+        }
+
+        return $qb;
+    }
+}
