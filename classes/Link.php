dae06f5074572595c5978d027bc067d54091d39c
Sébastien Bareyre <sebastien@ohmyweb.fr>
2018-07-23 16:06:59 +0200

    fix missing space

--- classes/Link.php
+++ classes/Link.php
@@ -960,7 +960,7 @@ class LinkCore
 
         $uriPath = Dispatcher::getInstance()->createUrl($controller, $idLang, $request, false, '', $idShop);
 
-        if(substr($uriPath, 0, 10) === 'index.php?') {
+        if (substr($uriPath, 0, 10) === 'index.php?') {
             return $this->getBaseLink($idShop, $ssl, $relativeProtocol).ltrim($uriPath, '/');
         } else {
             return $this->getBaseLink($idShop, $ssl, $relativeProtocol).$this->getLangLink($idLang, null, $idShop).ltrim($uriPath, '/');
