6efe1bbcbba9612fb3bc745683365ff08dfd64d6
Šarūnas Jonušas <sarunas.jonusas@invertus.lt>
2018-07-24 10:47:33 +0300

    add actions grid column

--- src/Core/Grid/Definition/Factory/EmailLogsDefinitionFactory.php
+++ src/Core/Grid/Definition/Factory/EmailLogsDefinitionFactory.php
@@ -3,13 +3,38 @@
 namespace PrestaShop\PrestaShop\Core\Grid\Definition\Factory;
 
 use PrestaShop\PrestaShop\Core\Grid\Column\ColumnCollection;
+use PrestaShop\PrestaShop\Core\Grid\Column\ColumnFilterOption;
+use PrestaShop\PrestaShop\Core\Grid\Column\Type\Common\ActionColumn;
 use PrestaShop\PrestaShop\Core\Grid\Column\Type\DataColumn;
+use PrestaShopBundle\Form\Admin\Type\DateRangeType;
+use PrestaShopBundle\Form\Admin\Type\SearchAndResetFormType;
 
 /**
  * Class EmailLogsDefinitionFactory is responsible for creating email logs definition
  */
 final class EmailLogsDefinitionFactory extends AbstractGridDefinitionFactory
 {
+    /**
+     * @var string the URL to reset Grid filters.
+     */
+    private $resetActionUrl;
+
+    /**
+     * @var string the URL for redirection.
+     */
+    private $redirectionUrl;
+
+    /**
+     * LogGridDefinitionFactory constructor.
+     * @param string $resetActionUrl
+     * @param string $redirectionUrl
+     */
+    public function __construct($resetActionUrl, $redirectionUrl)
+    {
+        $this->resetActionUrl = $resetActionUrl;
+        $this->redirectionUrl = $redirectionUrl;
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -66,6 +91,20 @@ final class EmailLogsDefinitionFactory extends AbstractGridDefinitionFactory
                 ->setName($this->trans('Sent', [], 'Admin.Advparameters.Feature'))
                 ->setOptions([
                     'field' => 'date_add',
+                    'filter' => new ColumnFilterOption(DateRangeType::class, [
+                        'required' => false,
+                    ]),
+                ])
+            )
+            ->add((new ActionColumn('actions'))
+                ->setName($this->trans('Actions', [], 'Admin.Global'))
+                ->setOptions([
+                    'filter' => new ColumnFilterOption(SearchAndResetFormType::class, [
+                        'attr' => [
+                            'data-url' => $this->resetActionUrl,
+                            'data-redirect' => $this->redirectionUrl,
+                        ],
+                    ]),
                 ])
             )
         ;
