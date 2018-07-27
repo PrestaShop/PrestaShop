f8bbbc717f5fc722f935f37d920e18147c41ac9b
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2018-07-27 10:21:29 +0200

    Ignore mbo

--- tests/E2E/test/selectors/BO/menu.js
+++ tests/E2E/test/selectors/BO/menu.js
@@ -45,10 +45,10 @@ module.exports = {
       Modules: {
         modules_menu: '//*[@id="subtab-AdminParentModulesSf"]/a',
         modules_services_submenu: '#subtab-AdminModulesSf',
-        installed_modules_tabs: '#subtab-AdminModulesManage',
-        notifications_tabs: '#subtab-AdminModulesNotifications',
-        selection_tab: '#subtab-AdminPsMboModule',
-        modules_catalog_submenu: '#subtab-AdminAddonsCatalog'
+        installed_modules_tabs: '//*[@id="subtab-AdminModulesManage"]',
+        notifications_tabs: '//*[@id="subtab-AdminModulesNotifications"]',
+        selection_tab: '//*[@id="subtab-AdminModulesCatalog"]',
+        modules_catalog_submenu: '#subtab-AdminAddonsCatalog',
       },
       Design: {
         design_menu: '//*[@id="subtab-AdminParentThemes"]/a',
