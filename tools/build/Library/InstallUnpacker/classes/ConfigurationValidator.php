3d48baa31c625d56642f6bbf42c4565d81604d99
Mathieu Ferment <mathieu.ferment@gmail.com>
2018-08-01 17:44:20 +0200

    Add form at the beginning of install process (unzip) to allow users to install last PS version

---
+++ tools/build/Library/InstallUnpacker/classes/ConfigurationValidator.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * 2007-2018 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2018 PrestaShop SA
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+class ConfigurationValidator
+{
+    /**
+     * Check whether download, unzip & replace of latest PS version can be carried out.
+     *
+     * @return string[] array of issues. Empty if download can be performed.
+     */
+    public function testSystemCanPerformDownloadUnzipAndReplace()
+    {
+        $errors = [];
+
+        if (!$this->testCurl() && !$this->testFopen()) {
+            $errors[] = 'You need allow_url_fopen or cURL enabled for automatic download to work.';
+        }
+
+        $systemErrors = $this->testFilesystemCapabilities();
+        if (false === empty($systemErrors)) {
+            $errors[] = sprintf('Cannot manipulate file on this system: %s', implode('; ', $systemErrors));
+        }
+
+        if (false === class_exists('ZipArchive')) {
+            $errors[] = 'Cannot unzip files as php class ZipArchive is not available.';
+        }
+
+        // @todo: be able to use fallback util directories
+        $downloadDirPath = __DIR__.DIRECTORY_SEPARATOR.'download';
+        if (is_dir($downloadDirPath) || file_exists($downloadDirPath)) {
+            $errors[] = "Directory 'download' already exists.";
+        }
+        $downloadDirPath = __DIR__.DIRECTORY_SEPARATOR.'extracted';
+        if (is_dir($downloadDirPath) || file_exists($downloadDirPath)) {
+            $errors[] = "Directory 'extracted' already exists.";
+        }
+
+        return $errors;
+    }
+
+    /**
+     * @return bool
+     */
+    public function testFopen()
+    {
+        return in_array(ini_get('allow_url_fopen'), array('On', 'on', '1'));
+    }
+
+    /**
+     * @return bool
+     */
+    public function testCurl()
+    {
+        return extension_loaded('curl');
+    }
+
+    /**
+     * Test whether files and directories can be manipulated by php on given system
+     * by performing multiple file operations
+     *
+     * @return string[]
+     */
+    public function testFilesystemCapabilities()
+    {
+        // choose a random available directory name
+        $dirPath = $this->getRandomDirectoryPath();
+
+        $randomNameIsActuallyAlreadyUsed = (is_dir($dirPath) || file_exists($dirPath));
+        if ($randomNameIsActuallyAlreadyUsed) {
+            // retry another random directory name
+            $dirPath = $this->getRandomDirectoryPath();
+
+            $randomNameIsActuallyAlreadyUsed = (is_dir($dirPath) || file_exists($dirPath));
+            if ($randomNameIsActuallyAlreadyUsed) {
+                throw new \RuntimeException('Failed to find available directory name');
+            }
+        }
+
+        // create directory
+        $createDirectoryResult = @mkdir($dirPath);
+        if (false === $createDirectoryResult) {
+            return ['Cannot create directories'];
+        }
+
+        // create file in it
+        $fileCreationTestPath = $dirPath . DIRECTORY_SEPARATOR . 'test-file.php';
+        $createFileResult = @file_put_contents($fileCreationTestPath, "<?php echo 'Hello world !';");
+        if (false === $createFileResult) {
+            return ['Cannot write files'];
+        }
+
+        // download a file from github in directory
+        $downloadTestPath = $dirPath . DIRECTORY_SEPARATOR . 'test-download.txt';
+        // @todo: use another file from the network ?
+        $target = 'https://raw.githubusercontent.com/PrestaShop/PrestaShop/develop/robots.txt';
+        $downloadFileResult = @file_put_contents($downloadTestPath, Download::fileGetContents($target));
+        if (false === $downloadFileResult) {
+            return ['Cannot download files from network'];
+        }
+
+        // move a file from test directory into root directory
+        $fileMoveTestPath = __DIR__ . DIRECTORY_SEPARATOR . 'test-move.php';
+        $moveResult = @rename($fileCreationTestPath, $fileMoveTestPath);
+        if (false === $moveResult) {
+            return ['Cannot move files into prestashop root directory'];
+        }
+
+        // delete test file in root directory
+        $deleteFileResult = unlink($fileMoveTestPath);
+        if (false === $deleteFileResult) {
+            return ['Cannot delete files in prestashop root directory'];
+        }
+
+        // delete test directory
+        $deleteDirectoryContentResult = array_map('unlink', glob($dirPath . DIRECTORY_SEPARATOR . "*.*"));
+        $deleteDirectoryResult = @rmdir($dirPath);
+        if ((false === $deleteDirectoryContentResult) || (false === $deleteDirectoryResult)) {
+            return ['Cannot delete directories in prestashop root directory'];
+        }
+    }
+
+    /**
+     * @return string
+     */
+    private function getRandomDirectoryPath()
+    {
+        $randomDirectoryName = 'test-' . rand(0, 999);
+        $dirPath = __DIR__ . DIRECTORY_SEPARATOR . $randomDirectoryName;
+
+        return $dirPath;
+    }
+}
--- tools/build/Library/InstallUnpacker/index_template.php
+++ tools/build/Library/InstallUnpacker/index_template.php
@@ -31,24 +31,46 @@ define('_PS_INSTALL_MINIMUM_PHP_VERSION_', '5.6');
 define('_PS_VERSION_', '%ps-version-placeholder%');
 
 define('ZIP_NAME', 'prestashop.zip');
-define('TARGET_FOLDER', __DIR__.'/');
+define('TARGET_FOLDER', __DIR__ . '/');
 define('BATCH_SIZE', 500);
 
 // bust cache, or else it won't load the installer after the extraction is done
 header("Cache-Control: no-cache, no-store, must-revalidate");
 
 if (!defined('PHP_VERSION_ID') || PHP_VERSION_ID < _PS_INSTALL_MINIMUM_PHP_VERSION_ID_) {
-    die('You need at least PHP '._PS_INSTALL_MINIMUM_PHP_VERSION_.' to install PrestaShop. Your current PHP version is '.PHP_VERSION);
+    die('You need at least PHP ' . _PS_INSTALL_MINIMUM_PHP_VERSION_ . ' to install PrestaShop. Your current PHP version is ' . PHP_VERSION);
 }
 
-function getFileContent($fileOrContent, $debug) {
+// --------------------------------------------------------------------------------
+/** COMPUTED INLINE CLASSES **/
+
+// --------------------------------------------------------------------------------
+
+$installManager = new InstallManager();
+
+function isThisTheLatestAvailableVersion()
+{
+    $latestVersionAvailable = getLatestStableAvailableVersion();
+
+    return (_PS_VERSION_ === $latestVersionAvailable);
+}
+
+function getLatestStableAvailableVersion()
+{
+    global $installManager;
+    return $installManager->getLatestStableAvailableVersion();
+}
+
+function getFileContent($fileOrContent, $debug)
+{
     if ($debug) {
-        return file_get_contents('content/'.$fileOrContent);
+        return file_get_contents('content/' . $fileOrContent);
     }
     return base64_decode($fileOrContent);
 }
 
-function getZipErrorMessage($errorCode) {
+function getZipErrorMessage($errorCode)
+{
     $errors = [
         ZipArchive::ER_EXISTS => "File already exists.",
         ZipArchive::ER_INCONS => "Zip archive inconsistent or corrupted. Double check your uploaded files.",
@@ -70,27 +92,51 @@ function getZipErrorMessage($errorCode) {
 
 $selfUri = basename(__FILE__);
 
-$startId = (isset($_POST['startId'])) ? (int) $_POST['startId'] : 0;
+$userHasChosenToDownloadLatestPSVersion = ((isset($_POST['downloadLatest'])) && ($_POST['downloadLatest'] === 'true'));
+if ($userHasChosenToDownloadLatestPSVersion) {
+    $issues = $installManager->testDownloadCapabilities();
+
+    if (empty($issues)) {
+        try {
+            $installManager->downloadUnzipAndReplaceLatestPSVersion();
+            die(json_encode([
+                'success' => true,
+            ]));
+        } catch (\Exception $e) {
+            die(json_encode([
+                'error' => true,
+                'message' => $e->getMessage(),
+            ]));
+        }
+    } else {
+        die(json_encode([
+            'warning' => true,
+            'issues' => $issues,
+        ]));
+    }
+}
+
+$startId = (isset($_POST['startId'])) ? (int)$_POST['startId'] : 0;
 
 if (isset($_POST['extract'])) {
     if (!extension_loaded('zip')) {
         die(json_encode([
-            'error'   => true,
+            'error' => true,
             'message' => 'You must install PHP zip extension first',
         ]));
     }
 
     $zip = new ZipArchive();
-    if (true !== $error = $zip->open(__DIR__.'/'.ZIP_NAME)) {
+    if (true !== $error = $zip->open(__DIR__ . '/' . ZIP_NAME)) {
         die(json_encode([
-            'error'   => true,
+            'error' => true,
             'message' => getZipErrorMessage($error),
         ]));
     }
 
     if (!is_writable(TARGET_FOLDER)) {
         die(json_encode([
-            'error'   => true,
+            'error' => true,
             'message' => "You need to grant write permissions for PHP on the following directory: "
                 . realpath(TARGET_FOLDER),
         ]));
@@ -104,7 +150,7 @@ if (isset($_POST['extract'])) {
         $currentFile = $zip->getNameIndex($id);
         if (in_array($currentFile, ['/index.php', 'index.php'])) {
             $indexContent = $zip->getFromIndex($id);
-            if (!file_put_contents(getcwd().'/index.php.temp', $indexContent)) {
+            if (!file_put_contents(getcwd() . '/index.php.temp', $indexContent)) {
                 die(json_encode([
                     'error' => true,
                     'message' => "Unable to write to file " . getcwd() . '/index.php.temp'
@@ -118,12 +164,12 @@ if (isset($_POST['extract'])) {
     foreach ($fileList as $currentFile) {
         if ($zip->extractTo(TARGET_FOLDER, $currentFile) === false) {
             die(json_encode([
-                'error'    => true,
-                'message'  => 'Extraction error - '.$zip->getStatusString(),
-                'file'     => $currentFile,
+                'error' => true,
+                'message' => 'Extraction error - ' . $zip->getStatusString(),
+                'file' => $currentFile,
                 'numFiles' => $numFiles,
-                'lastId'   => $lastId,
-                'files'    => $fileList,
+                'lastId' => $lastId,
+                'files' => $fileList,
             ]));
         }
     }
@@ -137,17 +183,18 @@ if (isset($_POST['extract'])) {
     $zip->close();
 
     if ($lastId >= $numFiles) {
-        unlink(getcwd().'/index.php');
-        unlink(getcwd().'/prestashop.zip');
-        rename(getcwd().'/index.php.temp', getcwd().'/index.php');
+        unlink(getcwd() . '/index.php');
+        unlink(getcwd() . '/prestashop.zip');
+        rename(getcwd() . '/index.php.temp', getcwd() . '/index.php');
     }
 
     die(json_encode([
-        'error'    => false,
+        'error' => false,
         'numFiles' => $numFiles,
-        'lastId'   => $lastId,
+        'lastId' => $lastId,
     ]));
 }
+
 
 if (isset($_GET['element'])) {
     switch ($_GET['element']) {
@@ -171,93 +218,188 @@ if (isset($_GET['element'])) {
     exit;
 }
 
-?>
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>PrestaShop installation</title>
-  <link rel="stylesheet" type="text/css" href="<?= $selfUri ?>?element=css">
-</head>
-<body>
-<div id="content">
-  <div>
-    <img id="spinner" src="<?= $selfUri ?>?element=gif"/>
-    <div id="progressContainer">
-      <div class="progressNumber">0 %</div>
-      <div class="progress">
-        <div class="current">
-        </div>
-      </div>
-    </div>
-    <div id="error"></div>
-  </div>
-</div>
-<script type="text/javascript" src="<?= $selfUri ?>?element=jquery"></script>
-<script type="text/javascript">
-
-  function extractFiles(startId) {
-
-    if (typeof startId === 'undefined') {
-      startId = 0;
+$thereIsAMoreRecentPSVersion = false;
+$userHasChosenToIgnoreLatestPSVersion = ((isset($_POST['submit'])) && ($_POST['submit'] === 'Skip Latest'));
+if (false === $userHasChosenToIgnoreLatestPSVersion) {
+    try {
+        if (false === isThisTheLatestAvailableVersion()) {
+            $latestPrestaShopAvailableVersion = getLatestStableAvailableVersion();
+            $thereIsAMoreRecentPSVersion = true;
+        }
+    } catch (\Exception $e) {
+        // do nothing, this is an optional feature
     }
+}
 
-    var request = $.ajax({
-      method: "POST",
-      url: "<?= $selfUri ?>",
-      data: {
-        extract: true,
-        startId: startId,
-      }
-    });
+$showFormToDownloadLatestPSVersion = ($thereIsAMoreRecentPSVersion && !$userHasChosenToIgnoreLatestPSVersion);
 
-    request.done(function(msg) {
-      try {
-        msg = JSON.parse(msg);
-      } catch (e) {
-        if (String(msg).match("<tittle>PrestaShop")) {
-          msg = "Invalid server response";
+if ($showFormToDownloadLatestPSVersion):
+    ?>
+    <div id="download-latest-version-form">
+
+        This is not the latest version of Prestashop. Do you want to get the latest ?
+
+        <form method="post">
+            <input id="latest" type="button" name="submit" value="Download Latest">
+            <input id="skip" type="submit" name="submit" value="Skip Latest">
+            <div id="waiting"></div>
+            <div id="error"></div>
+            <div id="fallback-after-error" style="display:none;">Cannot download latest Prestashop version. Please click
+                on 'Skip latest' to resume standard installation.
+            </div>
+            <script type="text/javascript" src="<?= $selfUri ?>?element=jquery"></script>
+            <script type="text/javascript">
+                $(document).ready(function () {
+
+                    $("#latest").on("click", function (event) {
+
+                        $.ajax({
+                            url: "<?= $selfUri ?>",
+                            method: "POST",
+                            data: {'downloadLatest': true},
+                            beforeSend: function () {
+                                $('#latest').attr('disabled', 'disabled');
+                                $('#waiting').html('Downloading latest version ...');
+                            },
+                            success: function (msg) {
+                                try {
+                                    msg = JSON.parse(msg);
+                                } catch (e) {
+                                    if (String(msg).match("<tittle>PrestaShop")) {
+                                        msg = "Invalid server response";
+                                    }
+                                    msg = {
+                                        message: msg
+                                    };
+                                }
+
+                                if (msg.error) {
+                                    $('#error').html('An error has occured: <br />' + msg.message);
+                                    $('#waiting').remove();
+                                    $('#fallback-after-error').show();
+                                } else {
+                                    if (msg.success == true) {
+                                        location.reload();
+                                    } else {
+                                        if (msg.warning == true) {
+                                            var issuesList = 'An error has occured: <br /><ul>';
+
+                                            $.each(msg.issues, function (key, issue) {
+                                                issuesList = issuesList + '<li>' + issue + '</li>';
+                                            });
+
+                                            var issuesList = issuesList + '</ul>';
+
+                                            $('#error').html(issuesList);
+                                            $('#waiting').remove();
+                                            $('#fallback-after-error').show();
+                                        } else {
+                                            $('#error').html('An error has occured: <br />' + msg.message);
+                                            $('#waiting').remove();
+                                            $('#fallback-after-error').show();
+                                        }
+                                    }
+                                }
+                            },
+                            fail: function (jqXHR, textStatus, errorThrown) {
+                                $('#error').html('We are sorry, an error has occurred' + textStatus);
+                                $('#waiting').remove();
+                            }
+                        });
+                    });
+                });
+            </script>
+        </form>
+    </div>
+<?php else: ?>
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>PrestaShop installation</title>
+        <link rel="stylesheet" type="text/css" href="<?= $selfUri ?>?element=css">
+    </head>
+    <body>
+    <div id="content">
+        <div>
+            <img id="spinner" src="<?= $selfUri ?>?element=gif"/>
+            <div id="progressContainer">
+                <div class="progressNumber">0 %</div>
+                <div class="progress">
+                    <div class="current">
+                    </div>
+                </div>
+            </div>
+            <div id="error">
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript" src="<?= $selfUri ?>?element=jquery"></script>
+    <script type="text/javascript">
+
+        function extractFiles(startId) {
+
+            if (typeof startId === 'undefined') {
+                startId = 0;
+            }
+
+            var request = $.ajax({
+                method: "POST",
+                url: "<?= $selfUri ?>",
+                data: {
+                    extract: true,
+                    startId: startId,
+                }
+            });
+
+            request.done(function (msg) {
+                try {
+                    msg = JSON.parse(msg);
+                } catch (e) {
+                    if (String(msg).match("<tittle>PrestaShop")) {
+                        msg = "Invalid server response";
+                    }
+                    msg = {
+                        message: msg
+                    };
+                }
+
+                if (
+                    msg.error
+                    || typeof msg.lastId === 'undefined'
+                    || typeof msg.numFiles === 'undefined'
+                ) {
+                    $('#error').html('An error has occured: <br />' + msg.message);
+                    $('#spinner').remove();
+                } else {
+                    if (msg.lastId > msg.numFiles) {
+                        // end
+                        window.location.href = 'install/';
+                    } else {
+                        $("#progressContainer")
+                            .find(".current")
+                            .width((msg.lastId / msg.numFiles * 100) + '%');
+
+                        $("#progressContainer")
+                            .find(".progressNumber")
+                            .css({left: Math.round((msg.lastId / msg.numFiles * 100)) + '%'})
+                            .html(Math.round((msg.lastId / msg.numFiles * 100)) + '%');
+
+                        extractFiles(msg.lastId);
+                    }
+                }
+            });
+
+            request.fail(function (jqXHR, textStatus, errorThrown) {
+                $('#error').html('An error has occurred' + textStatus);
+                $('#spinner').remove();
+            });
         }
-        msg = {
-          message: msg
-        };
-      }
 
-      if (
-        msg.error
-        || typeof msg.lastId === 'undefined'
-        || typeof msg.numFiles === 'undefined'
-      ) {
-        $('#error').html('An error has occured: <br />' + msg.message);
-        $('#spinner').remove();
-      } else {
-        if (msg.lastId > msg.numFiles) {
-          // end
-          window.location.href = 'install/';
-        } else {
-          $("#progressContainer")
-            .find(".current")
-            .width((msg.lastId / msg.numFiles * 100)+'%');
-
-          $("#progressContainer")
-            .find(".progressNumber")
-            .css({left: Math.round((msg.lastId / msg.numFiles * 100))+'%'})
-            .html(Math.round((msg.lastId / msg.numFiles * 100))+'%');
-
-          extractFiles(msg.lastId);
-        }
-      }
-    });
-
-    request.fail(function(jqXHR, textStatus, errorThrown) {
-      $('#error').html('An error has occurred' + textStatus);
-      $('#spinner').remove();
-    });
-  }
-
-  $(function() {
-    extractFiles();
-  });
-</script>
-</body>
-</html>
+        $(function () {
+            extractFiles();
+        });
+    </script>
+    </body>
+    </html>
+<?php endif; ?>
