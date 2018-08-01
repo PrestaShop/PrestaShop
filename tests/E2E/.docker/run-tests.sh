d8dcd28315ef1f8a184ce0b7942d26349b332a73
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2018-08-01 22:10:47 +0200

    Run tests under docker

--- tests/E2E/.docker/run-tests.sh
+++ tests/E2E/.docker/run-tests.sh
@@ -5,8 +5,6 @@ URL="${URL:-prestashop}"
 MODULE="${MODULE:-}"
 SAUCELABS="${SAUCELABS:-}"
 SCRIPT="${SCRIPT:-index.webdriverio.js}"
-SELENIUM=""${SELENIUM_URL:-}""
-
 COMMAND="npm run test -- --URL=$URL --HEADLESS"
 
 if [ ! -z "$MODULE" ]; then
@@ -17,10 +15,19 @@ if [ ! -z "$SAUCELABS" ]; then
     COMMAND="$COMMAND --SAUCELABS"
 fi
 
-if [ ! -z "$SELENIUM" ]; then
-    COMMAND="$COMMAND --SELENIUM=$SELENIUM"
+if [ ! -z "$SELENIUM_PROTOCOL" ]; then
+    COMMAND="$COMMAND --SELENIUM_PROTOCOL=$SELENIUM_PROTOCOL"
+fi
+
+if [ ! -z "$SELENIUM_HOST" ]; then
+    COMMAND="$COMMAND --SELENIUM_HOST=$SELENIUM_HOST"
+fi
+
+if [ ! -z "$SELENIUM_PORT" ]; then
+    COMMAND="$COMMAND --SELENIUM_PORT=$SELENIUM_PORT"
 fi
 
 pushd /var/www/html/tests/E2E/
+npm install
 echo $COMMAND
 $COMMAND
