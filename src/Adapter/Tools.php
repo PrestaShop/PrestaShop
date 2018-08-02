33bdb3babb791c592d7b7621394e6ccf7100eb49
Tomas Ilginis <tomas.ilginis@invertus.lt>
2018-08-02 19:04:35 +0300

    adds languages to cache and clears the previous cache after language update action

--- src/Adapter/Tools.php
+++ src/Adapter/Tools.php
@@ -143,4 +143,12 @@ class Tools
     {
         return LegacyTools::getShopDomainSsl($http, $entities);
     }
+
+    /**
+     * Clear both Smarty and Symfony cache
+     */
+    public function clearAllCache()
+    {
+        LegacyTools::clearAllCache();
+    }
 }
