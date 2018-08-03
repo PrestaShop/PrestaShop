9511dc488a59a536cc180e5d84bcfbc52e2361cd
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2018-08-03 11:44:29 +0200

    Add missing rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,35 @@
 // http://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-    env: {
-        browser: true,
-        node: true,
-        es6: true,
-    },
-    globals: {
-        google: true,
-        document: true,
-        navigator: false,
-        window: true,
-    },
-    root: true,
-    extends: 'airbnb',
-    plugins: [
-        'import',
-        'html',
-    ],
-    rules: {
-        'indent': ['error', 4, {'SwitchCase': 1}],
-        'import/no-unresolved': 0,
-        'function-paren-newline': ['off', 'never'],
-        'object-curly-spacing': ['error', 'never'],
-        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-        'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
-        'import/extensions': ['off', 'never'],
-    }
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  globals: {
+    google: true,
+    document: true,
+    navigator: false,
+    window: true,
+  },
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module"
+  },
+  root: true,
+  extends: 'airbnb',
+  plugins: [
+    'import',
+    'html',
+  ],
+  rules: {
+    'indent': ['error', 4, {'SwitchCase': 1}],
+    'import/no-unresolved': 0,
+    'no-use-before-define': 0,
+    'function-paren-newline': ['off', 'never'],
+    'object-curly-spacing': ['error', 'never'],
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'import/extensions': ['off', 'never'],
+  }
 };
