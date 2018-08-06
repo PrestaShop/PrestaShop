40319656d242283796b77461e71cd8b1a8b32582
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2018-08-06 17:43:47 +0300

    get export sql name from page breadcrumb

--- admin-dev/themes/new-theme/js/components/grid/extension/export-to-sql-manager-extension.js
+++ admin-dev/themes/new-theme/js/components/grid/extension/export-to-sql-manager-extension.js
@@ -47,14 +47,13 @@ export default class ExportToSqlManagerExtension {
    * @private
    */
   _onShowSqlQueryClick(grid) {
-    const query = grid.getContainer().find('.js-grid-table').data('query');
-
     const $sqlManagerForm = $('#' + grid.getId() + '_common_show_query_modal_form');
-    $sqlManagerForm.find('textarea[name="sql"]').val(query);
+    this._fillExportForm($sqlManagerForm, grid);
 
     const $modal = $('#' + grid.getId() + '_grid_common_show_query_modal');
     $modal.modal('show');
 
+    console.log(this._getNameFromBreadcrumb());return;
     $modal.on('click', '.btn-sql-submit', () => $sqlManagerForm.submit());
   }
 
@@ -66,10 +65,53 @@ export default class ExportToSqlManagerExtension {
    * @private
    */
   _onExportSqlManagerClick(grid) {
+    const $sqlManagerForm = $('#' + grid.getId() + '_common_show_query_modal_form');
+
+    this._fillExportForm($sqlManagerForm, grid);
+
+    $sqlManagerForm.submit();
+  }
+
+  /**
+   * Fill export form with SQL and it's name
+   *
+   * @param {jQuery} $sqlManagerForm
+   * @param {Grid} grid
+   *
+   * @private
+   */
+  _fillExportForm($sqlManagerForm, grid) {
     const query = grid.getContainer().find('.js-grid-table').data('query');
 
-    const $sqlManagerForm = $('#' + grid.getId() + '_common_show_query_modal_form');
     $sqlManagerForm.find('textarea[name="sql"]').val(query);
-    $sqlManagerForm.submit();
+    $sqlManagerForm.find('input[name="name"]').val(this._getNameFromBreadcrumb());
+  }
+
+  /**
+   * Get export name from page's breadcrumb
+   *
+   * @return {String}
+   *
+   * @private
+   */
+  _getNameFromBreadcrumb() {
+    const $breadcrumbs = $('.header-toolbar').find('.breadcrumb-item');
+    let name = '';
+
+    $breadcrumbs.each((i, item) => {
+      const $breadcrumb = $(item);
+
+      const breadcrumbTitle = 0 < $breadcrumb.find('a').length ?
+        $breadcrumb.find('a').text() :
+        $breadcrumb.text();
+
+      if (0 < name.length) {
+        name = name.concat(' > ');
+      }
+
+      name = name.concat(breadcrumbTitle);
+    });
+
+    return name;
   }
 }
