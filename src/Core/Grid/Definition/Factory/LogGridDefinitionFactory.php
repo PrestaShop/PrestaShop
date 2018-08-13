8bff715bf5c85284c92168cf5174062075c56637
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2018-08-13 22:21:04 +0300

    update Logs grid definition

--- src/Core/Grid/Definition/Factory/LogGridDefinitionFactory.php
+++ src/Core/Grid/Definition/Factory/LogGridDefinitionFactory.php
@@ -30,15 +30,17 @@ use PrestaShop\PrestaShop\Core\Grid\Action\GridActionCollection;
 use PrestaShop\PrestaShop\Core\Grid\Action\Type\SimpleGridAction;
 use PrestaShop\PrestaShop\Core\Grid\Action\Type\SubmitGridAction;
 use PrestaShop\PrestaShop\Core\Grid\Column\ColumnCollection;
-use PrestaShop\PrestaShop\Core\Grid\Column\ColumnFilterOption;
 use PrestaShop\PrestaShop\Core\Grid\Column\Type\Common\ActionColumn;
 use PrestaShop\PrestaShop\Core\Grid\Column\Type\Common\BulkActionColumn;
 use PrestaShop\PrestaShop\Core\Grid\Column\Type\Common\DateTimeColumn;
 use PrestaShop\PrestaShop\Core\Grid\Column\Type\Employee\EmployeeNameWithAvatarColumn;
 use PrestaShop\PrestaShop\Core\Grid\Column\Type\DataColumn;
 use PrestaShop\PrestaShop\Core\Grid\Column\Type\Status\SeverityLevelColumn;
+use PrestaShop\PrestaShop\Core\Grid\Filter\Filter;
+use PrestaShop\PrestaShop\Core\Grid\Filter\FilterCollection;
 use PrestaShopBundle\Form\Admin\Type\DateRangeType;
 use PrestaShopBundle\Form\Admin\Type\SearchAndResetType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 /**
  * Class LogGridDefinitionFactory is responsible for creating new instance of Log grid definition
@@ -88,80 +90,129 @@ final class LogGridDefinitionFactory extends AbstractGridDefinitionFactory
     protected function getColumns()
     {
         return (new ColumnCollection())
-            ->add(
-                (new BulkActionColumn('bulk_action'))
+            ->add((new BulkActionColumn('bulk_action'))
                 ->setOptions([
                     'bulk_field' => 'id_log',
                 ])
             )
-            ->add(
-                (new DataColumn('id_log'))
+            ->add((new DataColumn('id_log'))
                 ->setName($this->trans('ID', [], 'Global.Actions'))
                 ->setOptions([
                     'field' => 'id_log',
                 ])
             )
-            ->add(
-                (new EmployeeNameWithAvatarColumn('employee'))
+            ->add((new EmployeeNameWithAvatarColumn('employee'))
                 ->setName($this->trans('Employee', [], 'Admin.Global'))
                 ->setOptions([
                     'field' => 'employee',
                 ])
             )
-            ->add(
-                (new SeverityLevelColumn('severity'))
+            ->add((new SeverityLevelColumn('severity'))
                 ->setName($this->trans('Severity (1-4)', [], 'Admin.Advparameters.Feature'))
                 ->setOptions([
                     'with_message' => true,
                     'field' => 'severity',
                 ])
             )
-            ->add(
-                (new DataColumn('message'))
+            ->add((new DataColumn('message'))
                 ->setName($this->trans('Message', [], 'Admin.Global'))
                 ->setOptions([
                     'field' => 'message',
                 ])
             )
-            ->add(
-                (new DataColumn('object_type'))
+            ->add((new DataColumn('object_type'))
                 ->setName($this->trans('Object type', [], 'Admin.Advparameters.Feature'))
                 ->setOptions([
                     'field' => 'object_type',
                 ])
             )
-            ->add(
-                (new DataColumn('object_id'))
+            ->add((new DataColumn('object_id'))
                 ->setName($this->trans('Object ID', [], 'Admin.Advparameters.Feature'))
                 ->setOptions([
                     'field' => 'object_id',
                 ])
             )
-            ->add(
-                (new DataColumn('error_code'))
+            ->add((new DataColumn('error_code'))
                 ->setName($this->trans('Error code', [], 'Admin.Advparameters.Feature'))
                 ->setOptions([
                     'field' => 'error_code',
                 ])
             )
-            ->add(
-                (new DateTimeColumn('date_add'))
+            ->add((new DateTimeColumn('date_add'))
                 ->setName($this->trans('Date', [], 'Admin.Global'))
                 ->setOptions([
                     'format' => 'Y-m-d H:i',
-                    'filter' => new ColumnFilterOption(DateRangeType::class),
                     'field' => 'date_add',
                 ])
             )
             ->add((new ActionColumn('actions'))
-                ->setOptions([
-                    'filter' => new ColumnFilterOption(SearchAndResetType::class, [
-                        'attr' => [
-                            'data-url' => $this->resetActionUrl,
-                            'data-redirect' => $this->redirectionUrl,
-                        ],
-                    ]),
+                ->setName($this->trans('Actions', [], 'Admin.Global'))
+            )
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return (new FilterCollection())
+            ->add((new Filter('id_log', TextType::class))
+                ->setTypeOptions([
+                    'required' => false,
                 ])
+                ->setAssociatedColumn('id_log')
+            )
+            ->add((new Filter('employee', TextType::class))
+                ->setTypeOptions([
+                    'required' => false,
+                ])
+                ->setAssociatedColumn('employee')
+            )
+            ->add((new Filter('severity', TextType::class))
+                ->setTypeOptions([
+                    'required' => false,
+                ])
+                ->setAssociatedColumn('severity')
+            )
+            ->add((new Filter('message', TextType::class))
+                ->setTypeOptions([
+                    'required' => false,
+                ])
+                ->setAssociatedColumn('message')
+            )
+            ->add((new Filter('object_type', TextType::class))
+                ->setTypeOptions([
+                    'required' => false,
+                ])
+                ->setAssociatedColumn('object_type')
+            )
+            ->add((new Filter('object_id', TextType::class))
+                ->setTypeOptions([
+                    'required' => false,
+                ])
+                ->setAssociatedColumn('object_id')
+            )
+            ->add((new Filter('error_code', TextType::class))
+                ->setTypeOptions([
+                    'required' => false,
+                ])
+                ->setAssociatedColumn('error_code')
+            )
+            ->add((new Filter('date_add', DateRangeType::class))
+                ->setTypeOptions([
+                    'required' => false,
+                ])
+                ->setAssociatedColumn('date_add')
+            )
+            ->add((new Filter('actions', SearchAndResetType::class))
+                ->setTypeOptions([
+                    'attr' => [
+                        'data-url' => $this->resetActionUrl,
+                        'data-redirect' => $this->redirectionUrl,
+                    ],
+                ])
+                ->setAssociatedColumn('actions')
             )
         ;
     }
