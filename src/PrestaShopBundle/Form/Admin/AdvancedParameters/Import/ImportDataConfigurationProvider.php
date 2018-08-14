76971dd3ff30c525cae6a98041bed89cf224a594
Tomas Ilginis <tomas.ilginis@invertus.lt>
2018-08-14 13:27:31 +0300

    uses form handler and specific provider to display the form

---
+++ src/PrestaShopBundle/Form/Admin/AdvancedParameters/Import/ImportDataConfigurationProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace PrestaShopBundle\Form\Admin\AdvancedParameters\Import;
+
+use PrestaShop\PrestaShop\Core\Form\FormDataProviderInterface;
+
+/**
+ * todo: insert comment
+ */
+class ImportDataConfigurationProvider implements FormDataProviderInterface
+{
+    public function getData()
+    {
+        // TODO: Implement getData() method.
+    }
+
+    public function setData(array $data)
+    {
+        // TODO: Implement setData() method.
+    }
+}
