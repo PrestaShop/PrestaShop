0c61a1daa1f6b8cb6cfa8d1071cbaffb7909204f
Tomas Ilginis <tomas.ilginis@invertus.lt>
2018-08-14 15:10:13 +0300

    adds validation for import data configuration service

--- src/PrestaShopBundle/Form/Admin/AdvancedParameters/Import/ImportDataConfigurationProvider.php
+++ src/PrestaShopBundle/Form/Admin/AdvancedParameters/Import/ImportDataConfigurationProvider.php
@@ -2,6 +2,7 @@
 
 namespace PrestaShopBundle\Form\Admin\AdvancedParameters\Import;
 
+use PrestaShop\PrestaShop\Core\Configuration\DataConfigurationInterface;
 use PrestaShop\PrestaShop\Core\Form\FormDataProviderInterface;
 
 /**
@@ -9,13 +10,25 @@ use PrestaShop\PrestaShop\Core\Form\FormDataProviderInterface;
  */
 class ImportDataConfigurationProvider implements FormDataProviderInterface
 {
+    /**
+     * @var DataConfigurationInterface
+     */
+    private $dataConfiguration;
+
+    public function __construct(DataConfigurationInterface $dataConfiguration)
+    {
+        $this->dataConfiguration = $dataConfiguration;
+    }
+
     public function getData()
     {
-        // TODO: Implement getData() method.
+        return [
+            'import_data_configuration' => $this->dataConfiguration->getConfiguration()
+        ];
     }
 
     public function setData(array $data)
     {
-        // TODO: Implement setData() method.
+        return $this->dataConfiguration->updateConfiguration($data);
     }
 }
