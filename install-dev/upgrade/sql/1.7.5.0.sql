78e0fa0de09cb02f9b53fd3a611e42448e593266
Mreker <mathias@reker.dk>
2018-08-22 11:56:17 +0200

    add new optional head_seo_title in CMS pages

--- install-dev/upgrade/sql/1.7.5.0.sql
+++ install-dev/upgrade/sql/1.7.5.0.sql
@@ -2,3 +2,6 @@ SET SESSION sql_mode = '';
 SET NAMES 'utf8';
 
 /* PHP:add_supplier_manufacturer_routes(); */;
+
+ALTER TABLE `PREFIX_cms_lang`
+	ADD `head_seo_title` varchar(255) DEFAULT NULL AFTER `meta_title`;
