7033e739abe2bb7c5a0c9c6ffd25ac208b03c9b8
Šarūnas Jonušas <sarunas.jonusas@invertus.lt>
2018-08-22 11:15:34 +0300

    add SqlRequest delete using command

--- src/Adapter/SqlManager/CommandHandler/DeleteSqlRequestHandler.php
+++ src/Adapter/SqlManager/CommandHandler/DeleteSqlRequestHandler.php
@@ -26,66 +26,50 @@
 
 namespace PrestaShop\PrestaShop\Adapter\SqlManager\CommandHandler;
 
-use PrestaShop\PrestaShop\Core\Domain\SqlManagement\Command\EditSqlRequestCommand;
-use PrestaShop\PrestaShop\Core\Domain\SqlManagement\CommandHandler\EditSqlRequestHandlerInterface;
-use PrestaShop\PrestaShop\Core\Domain\SqlManagement\Exception\CannotEditSqlRequestException;
+use PrestaShop\PrestaShop\Core\Domain\SqlManagement\Command\DeleteSqlRequestCommand;
+use PrestaShop\PrestaShop\Core\Domain\SqlManagement\CommandHandler\DeleteSqlRequestHandlerInterface;
+use PrestaShop\PrestaShop\Core\Domain\SqlManagement\Exception\CannotDeleteSqlRequestException;
 use PrestaShop\PrestaShop\Core\Domain\SqlManagement\Exception\SqlRequestException;
 use PrestaShop\PrestaShop\Core\Domain\SqlManagement\Exception\SqlRequestNotFoundException;
 use PrestaShopException;
 use RequestSql;
 
 /**
- * Class EditSqlRequestHandler is responsible for updating RequestSql
+ * Class DeleteSqlRequestHandler
  *
  * @internal
  */
-final class EditSqlRequestHandler implements EditSqlRequestHandlerInterface
+final class DeleteSqlRequestHandler implements DeleteSqlRequestHandlerInterface
 {
     /**
      * {@inheritdoc}
      *
-     * @param EditSqlRequestCommand $command
-     *
-     * @throws CannotEditSqlRequestException
+     * @throws CannotDeleteSqlRequestException
      * @throws SqlRequestException
-     * @throws SqlRequestNotFoundException
      */
-    public function handle(EditSqlRequestCommand $command)
+    public function handle(DeleteSqlRequestCommand $command)
     {
+        $entityId = $command->getSqlRequestId()->getValue();
+
         try {
-            $entity = new RequestSql($command->getSqlRequestId()->getValue());
+            $entity = new RequestSql($entityId);
 
             if (0 >= $entity->id) {
                 throw new SqlRequestNotFoundException(
-                    sprintf(
-                        'SqlRequest with id "%s" was not found for edit',
-                        $command->getSqlRequestId()->getValue()
-                    )
+                    sprintf('SqlRequest with id "%s" was not found for edit', var_export($entityId, true))
                 );
             }
 
-            if (null !== $command->getName()) {
-                $entity->name = $command->getName();
-            }
-
-            if (null !== $command->getSql()) {
-                $entity->sql = $command->getSql();
-            }
-
-            if (false === $entity->update()) {
-                throw new CannotEditSqlRequestException(
-                    sprintf(
-                        'Error occurred when updating RequestSql with id "%s"',
-                        $command->getSqlRequestId()->getValue()
-                    )
+            if (false === $entity->delete()) {
+                throw new CannotDeleteSqlRequestException(
+                    sprintf('Could not delete SqlRequest with id %s', var_export($entityId))
                 );
             }
         } catch (PrestaShopException $e) {
             throw new SqlRequestException(
-                sprintf(
-                    'Error occurred when updating RequestSql with id "%s"',
-                    $command->getSqlRequestId()->getValue()
-                )
+                sprintf('Unexpected error occurred when deleting SqlRequest with id %s', var_export($entityId, true)),
+                0,
+                $e
             );
         }
     }
