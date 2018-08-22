727d482118a93725075977873468832522e2f6a0
Tomas <tilginis@gmail.com>
2018-08-22 19:59:54 +0300

    includes inactive language configuration value display using multi language configuration provider

--- src/Adapter/Product/StockConfiguration.php
+++ src/Adapter/Product/StockConfiguration.php
@@ -28,6 +28,7 @@ namespace PrestaShop\PrestaShop\Adapter\Product;
 
 use PrestaShop\PrestaShop\Adapter\Configuration;
 use PrestaShop\PrestaShop\Core\Configuration\DataConfigurationInterface;
+use PrestaShop\PrestaShop\Core\Configuration\MultiLangConfigurationInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
@@ -40,9 +41,15 @@ class StockConfiguration implements DataConfigurationInterface
      */
     private $configuration;
 
-    public function __construct(Configuration $configuration)
+    /**
+     * @var MultiLangConfigurationInterface
+     */
+    private $langConfiguration;
+
+    public function __construct(Configuration $configuration, MultiLangConfigurationInterface $langConfiguration)
     {
         $this->configuration = $configuration;
+        $this->langConfiguration = $langConfiguration;
     }
 
     /**
@@ -53,11 +60,16 @@ class StockConfiguration implements DataConfigurationInterface
         return [
             'allow_ordering_oos' => $this->configuration->getBoolean('PS_ORDER_OUT_OF_STOCK'),
             'stock_management' => $this->configuration->getBoolean('PS_STOCK_MANAGEMENT'),
-            'in_stock_label' => $this->configuration->get('PS_LABEL_IN_STOCK_PRODUCTS'),
-            'oos_allowed_backorders' => $this->configuration->get('PS_LABEL_OOS_PRODUCTS_BOA'),
-            'oos_denied_backorders' => $this->configuration->get('PS_LABEL_OOS_PRODUCTS_BOD'),
-            'delivery_time' => (array) $this->configuration->get('PS_LABEL_DELIVERY_TIME_AVAILABLE'),
-            'oos_delivery_time' => (array) $this->configuration->get('PS_LABEL_DELIVERY_TIME_OOSBOA'),
+            'in_stock_label' =>
+                $this->langConfiguration->getIncludingInactiveLocales('PS_LABEL_IN_STOCK_PRODUCTS'),
+            'oos_allowed_backorders' =>
+                $this->langConfiguration->getIncludingInactiveLocales('PS_LABEL_OOS_PRODUCTS_BOA'),
+            'oos_denied_backorders' =>
+                $this->langConfiguration->getIncludingInactiveLocales('PS_LABEL_OOS_PRODUCTS_BOD'),
+            'delivery_time' =>
+                $this->langConfiguration->getIncludingInactiveLocales('PS_LABEL_DELIVERY_TIME_AVAILABLE'),
+            'oos_delivery_time' =>
+                $this->langConfiguration->getIncludingInactiveLocales('PS_LABEL_DELIVERY_TIME_OOSBOA'),
             'pack_stock_management' => $this->configuration->get('PS_PACK_STOCK_TYPE'),
         ];
     }
