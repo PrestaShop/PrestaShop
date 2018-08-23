c1d2117b4bc5837d11eb8ea1c6714447c7a8a4cd
Thomas Nabord <thomas.nabord@prestashop.com>
2018-08-23 16:08:52 +0100

    Moving ajax call of html email content

--- controllers/admin/AdminTranslationsController.php
+++ controllers/admin/AdminTranslationsController.php
@@ -3313,4 +3313,14 @@ class AdminTranslationsControllerCore extends AdminController
 
         return $email_html;
     }
+
+    /**
+     * Display the HTML content of an email
+     */
+    public function displayAjaxEmailHTML()
+    {
+        $email = Tools::getValue('email');
+        $this->ajaxRender(
+            AdminTranslationsController::getEmailHTML($email));
+    }
 }
