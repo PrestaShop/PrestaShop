06b0b504a21ba4fc5bf68759d7b8775c334f84fa
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2018-08-27 22:47:20 +0300

    update webpack config with categories entry

--- admin-dev/themes/new-theme/webpack.config.js
+++ admin-dev/themes/new-theme/webpack.config.js
@@ -92,6 +92,9 @@ const config = {
     backup: [
       './js/pages/backup'
     ],
+    categories: [
+      './js/pages/categories'
+    ],
     module_card: [
       './js/app/pages/module-card'
     ]
