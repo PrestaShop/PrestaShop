9ab65055d8c9e1c37671920a20429ba2bf520517
Thomas Nabord <thomas.nabord@prestashop.com>
2018-08-29 10:59:33 +0100

    Deprecate file get-file-admin.php

--- src/PrestaShopBundle/Model/Product/AdminModelAdapter.php
+++ src/PrestaShopBundle/Model/Product/AdminModelAdapter.php
@@ -41,6 +41,7 @@ use PrestaShop\PrestaShop\Adapter\Tools;
 use PrestaShop\PrestaShop\Adapter\Warehouse\WarehouseDataProvider;
 use PrestaShop\PrestaShop\Core\Product\ProductInterface;
 use PrestaShopBundle\Utils\FloatParser;
+use Symfony\Component\Routing\Router;
 use Product;
 use ProductDownload;
 use Tools as ToolsLegacy;
@@ -82,6 +83,8 @@ class AdminModelAdapter extends \PrestaShopBundle\Model\AdminModelAdapter
     private $productPricePriority;
     /** @var WarehouseDataProvider */
     private $warehouseAdapter;
+    /** @var Router */
+    private $router;
     /** @var array */
     private $multiShopKeys = array(
         'category_box',
@@ -193,7 +196,8 @@ class AdminModelAdapter extends \PrestaShopBundle\Model\AdminModelAdapter
         FeatureDataProvider $featureDataProvider,
         PackDataProvider $packDataProvider,
         ShopContext $shopContext,
-        TaxRuleDataProvider $taxRuleDataProvider
+        TaxRuleDataProvider $taxRuleDataProvider,
+        Router $router
     ) {
         $this->context = $legacyContext;
         $this->contextShop = $this->context->getContext();
@@ -210,6 +214,7 @@ class AdminModelAdapter extends \PrestaShopBundle\Model\AdminModelAdapter
         $this->configuration = new Configuration();
         $this->shopContext = $shopContext;
         $this->taxRuleDataProvider = $taxRuleDataProvider;
+        $this->router = $router;
     }
 
     /**
@@ -713,7 +718,10 @@ class AdminModelAdapter extends \PrestaShopBundle\Model\AdminModelAdapter
 
             if ($download->filename) {
                 $res['filename'] = $download->filename;
-                $res['file_download_link'] = $this->context->getAdminBaseUrl() . $download->getTextLink(true);
+                $res['file_download_link'] = $this->router->generate(
+                    'admin_product_virtual_download_file_action',
+                    ['idProduct' => $download->id_product]
+                );
             }
 
             return $res;
