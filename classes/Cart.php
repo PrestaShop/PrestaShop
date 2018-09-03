435f1f8f5f7b19e9adfe361f9b1eedf8962ecd8a
Guillaume Durand <guillaume@kadolis.com>
2018-09-03 16:56:02 +0200

    Fix notice (clean code)

--- classes/Cart.php
+++ classes/Cart.php
@@ -3652,7 +3652,7 @@ class CartCore extends ObjectModel
             $this->updateProductWeight($this->id);
         }
 
-        return isset(self::$_totalWeight[$this->id]) ? self::$_totalWeight[$this->id] : null;
+        return self::$_totalWeight[(int) $this->id];
     }
 
     /**
