0173759b0ae6317eb93e3936237d11036cebef4d
sadlyblue <sadb55@gmail.com>
2018-09-04 13:17:18 +0100

    Propose improvement
    First, get baselink only once, instead of each time -> could have a very slight speed improvement.
    Second, the if(strpos($quickAccess[$index]['link'], 'http') !== 0 or strpos($quickAccess[$index]['link'], $baselink) === 0) is used to allow other links, like the https://google.com, etc... It only adds the baselink and token to the link that do not start with http (or https of course) or to those that start with http, but it's the same as the current used link. If it's another link for the same shop, it will ask for login any way.
    Third, moved the check to add the admin token to inside the else.  This way it doesn't add to store url.
    Fourth, moved the clean url, to be inside the else to generate the shop front office url when link is '../'.

--- classes/QuickAccess.php
+++ classes/QuickAccess.php
@@ -81,34 +81,37 @@ class QuickAccessCore extends ObjectModel
             return false;
         }
 
+        $baselink = Context::getContext()->link->getBaseLink();
         foreach ($quickAccess as $index => $quick) {
-            // first, clean url to have a real quickLink
-            $quick['link'] = Context::getContext()->link->getQuickLink($quick['link']);
-            $tokenString = $idEmployee;
-
-            if ('../' === $quick['link'] && Shop::getContext() == Shop::CONTEXT_SHOP) {
-                $url = Context::getContext()->shop->getBaseURL();
-                if (!$url) {
-                    unset($quickAccess[$index]);
-                    continue;
-                }
-                $quickAccess[$index]['link'] = $url;
-            } else {
-                preg_match('/controller=(.+)(&.+)?$/', $quick['link'], $admin_tab);
-                if (isset($admin_tab[1])) {
-                    if (strpos($admin_tab[1], '&')) {
-                        $admin_tab[1] = substr($admin_tab[1], 0, strpos($admin_tab[1], '&'));
+            if(strpos($quickAccess[$index]['link'], 'http') !== 0 or strpos($quickAccess[$index]['link'], $baselink) === 0){
+                if ('../' === $quick['link'] && Shop::getContext() == Shop::CONTEXT_SHOP) {
+                    $url = Context::getContext()->shop->getBaseURL();
+                    if (!$url) {
+                        unset($quickAccess[$index]);
+                        continue;
                     }
-                    $quick_access[$index]['target'] = $admin_tab[1];
+                    $quickAccess[$index]['link'] = $url;
+                } else{
+                    // first, clean url to have a real quickLink
+                    $quick['link'] = Context::getContext()->link->getQuickLink($quick['link']);
+                    $tokenString = $idEmployee;
+                    
+                    preg_match('/controller=(.+)(&.+)?$/', $quick['link'], $admin_tab);
+                    if (isset($admin_tab[1])) {
+                        if (strpos($admin_tab[1], '&')) {
+                            $admin_tab[1] = substr($admin_tab[1], 0, strpos($admin_tab[1], '&'));
+                        }
+                        $quick_access[$index]['target'] = $admin_tab[1];
 
-                    $tokenString = $admin_tab[1] . (int) Tab::getIdFromClassName($admin_tab[1]) . $idEmployee;
+                        $tokenString = $admin_tab[1].(int)Tab::getIdFromClassName($admin_tab[1]).$idEmployee;
+                    }
+                    $quickAccess[$index]['link'] = $baselink.basename(_PS_ADMIN_DIR_).'/'.$quick['link'];
+                    if (false === strpos($quickAccess[$index]['link'], 'token')) {
+                        $separator = strpos($quickAccess[$index]['link'], '?') ? '&' : '?';
+                        $quickAccess[$index]['link'] .= $separator.'token='.Tools::getAdminToken($tokenString);
+                    }
+
                 }
-                $quickAccess[$index]['link'] = Context::getContext()->link->getBaseLink() . basename(_PS_ADMIN_DIR_) . '/' . $quick['link'];
-            }
-
-            if (false === strpos($quickAccess[$index]['link'], 'token')) {
-                $separator = strpos($quickAccess[$index]['link'], '?') ? '&' : '?';
-                $quickAccess[$index]['link'] .= $separator . 'token=' . Tools::getAdminToken($tokenString);
             }
         }
 
