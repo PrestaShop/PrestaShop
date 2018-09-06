4afa39bfdbfe01c50c30181caae11c638a8bf909
Mathieu Ferment <mathieu.ferment@gmail.com>
2018-09-06 11:23:56 +0200

    Optimize upgrade file 1.7.5.0.sql SQL commands

--- install-dev/upgrade/sql/1.7.5.0.sql
+++ install-dev/upgrade/sql/1.7.5.0.sql
@@ -5,5 +5,5 @@ SET NAMES 'utf8';
 
 /* PHP:ps_1750_update_module_tabs(); */;
 
-ALTER TABLE `PREFIX_product` CHANGE `location` `location` VARCHAR(255) NULL DEFAULT NULL;
-ALTER TABLE `PREFIX_product_attribute` CHANGE `location` `location` VARCHAR(255) NULL DEFAULT NULL;
+ALTER TABLE `PREFIX_product` MODIFY `location` VARCHAR(255) NULL DEFAULT NULL;
+ALTER TABLE `PREFIX_product_attribute` MODIFY `location` VARCHAR(255) NULL DEFAULT NULL;
