7aad1c8b14d68911fa999b18f9db2fec3de57e88
Rokas Zygmantas <rokas.zygmantas@invertus.eu>
2018-09-07 15:17:46 +0300

    Change step 1 redirect to step 2 to use GET

--- src/PrestaShopBundle/Controller/Admin/Configure/AdvancedParameters/ImportController.php
+++ src/PrestaShopBundle/Controller/Admin/Configure/AdvancedParameters/ImportController.php
@@ -133,13 +133,7 @@ class ImportController extends FrameworkBundleAdminController
             if (!$errors = $formHandler->save($data)) {
                 //todo: remove legacy request to get the new request available
 //                return $this->fowardRequestToLegacyResponse($request);
-                return $this->redirectToRoute(
-                    'admin_import_show_data',
-                    [
-                        'request' => $request,
-                    ],
-                    Response::HTTP_TEMPORARY_REDIRECT
-                );
+                return $this->redirectToRoute('admin_import_show_data');
             }
 
             $this->flashErrors($errors);
