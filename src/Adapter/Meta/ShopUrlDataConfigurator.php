eccf39b82ca8dfd6a1bdab0dc873179dd68d2884
Tomas <tilginis@gmail.com>
2018-09-08 11:01:03 +0300

    adds shop url configuration validation and saving logic

--- src/Adapter/Meta/ShopUrlDataConfigurator.php
+++ src/Adapter/Meta/ShopUrlDataConfigurator.php
@@ -26,7 +26,14 @@
 
 namespace PrestaShop\PrestaShop\Adapter\Meta;
 
+use InvalidArgumentException;
+use PrestaShop\PrestaShop\Adapter\Validate;
 use PrestaShop\PrestaShop\Core\Configuration\DataConfigurationInterface;
+use PrestaShop\PrestaShop\Core\ConfigurationInterface;
+use PrestaShopDatabaseException;
+use PrestaShopException;
+use ShopUrl;
+use Symfony\Component\Translation\TranslatorInterface;
 
 /**
  * Class ShopUrlDataConfigurator is responsible for updating and getting data from shop_url table.
@@ -34,18 +41,42 @@ use PrestaShop\PrestaShop\Core\Configuration\DataConfigurationInterface;
 final class ShopUrlDataConfigurator implements DataConfigurationInterface
 {
     /**
-     * @var array
+     * @var ShopUrl
      */
-    private $mainShopUrlData;
+    private $mainShopUrl;
+
+    /**
+     * @var ConfigurationInterface
+     */
+    private $configuration;
+
+    /**
+     * @var Validate
+     */
+    private $validate;
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
 
     /**
      * ShopUrlsDataConfigurator constructor.
      *
-     * @param array $mainShopUrlData
+     * @param ShopUrl $mainShopUrl
+     * @param ConfigurationInterface $configuration
+     * @param Validate $validate
+     * @param TranslatorInterface $translator
      */
-    public function __construct(array $mainShopUrlData)
-    {
-        $this->mainShopUrlData = $mainShopUrlData;
+    public function __construct(
+        ShopUrl $mainShopUrl,
+        ConfigurationInterface $configuration,
+        Validate $validate,
+        TranslatorInterface $translator
+    ) {
+        $this->mainShopUrl = $mainShopUrl;
+        $this->configuration = $configuration;
+        $this->validate = $validate;
+        $this->translator = $translator;
     }
 
     /**
@@ -54,9 +85,9 @@ final class ShopUrlDataConfigurator implements DataConfigurationInterface
     public function getConfiguration()
     {
         return [
-            'domain' => $this->mainShopUrlData['domain'],
-            'domain_ssl' => $this->mainShopUrlData['domain_ssl'],
-            'base_uri' => $this->mainShopUrlData['physical_uri'],
+            'domain' => $this->mainShopUrl->domain,
+            'domain_ssl' => $this->mainShopUrl->domain_ssl,
+            'physical_uri' => $this->mainShopUrl->physical_uri,
         ];
     }
 
@@ -65,14 +96,54 @@ final class ShopUrlDataConfigurator implements DataConfigurationInterface
      */
     public function updateConfiguration(array $configuration)
     {
-        // TODO: Implement updateConfiguration() method.
+        $errors = [];
+        try {
+            if ($this->validateConfiguration($configuration)) {
+                $this->mainShopUrl->domain = $configuration['domain'];
+                $this->mainShopUrl->domain_ssl = $configuration['domain_ssl'];
+                $this->mainShopUrl->physical_uri = (string) $configuration['physical_uri'];
+                $this->mainShopUrl->update();
+
+                $this->configuration->set('PS_SHOP_DOMAIN', $configuration['domain']);
+                $this->configuration->set('PS_SHOP_DOMAIN_SSL', $configuration['domain_ssl']);
+            }
+        } catch (InvalidArgumentException $exception) {
+            $errors[] = $exception->getMessage();
+        } catch (PrestaShopException $exception) {
+            $errors[] = $exception->getMessage();
+        }
+
+        return $errors;
     }
 
     /**
      * {@inheritdoc}
+     *
+     * @throws InvalidArgumentException
      */
     public function validateConfiguration(array $configuration)
     {
+        $issetRequiredVars = isset(
+            $configuration['domain'],
+            $configuration['domain_ssl']
+        );
 
+        if (!$issetRequiredVars) {
+            return false;
+        }
+
+        if (!$this->validate->isCleanHtml($configuration['domain'])) {
+            throw new InvalidArgumentException(
+                $this->translator->trans('This domain is not valid.', [], 'Admin.Notifications.Error')
+            );
+        }
+
+        if (!$this->validate->isCleanHtml($configuration['domain_ssl'])) {
+            throw new InvalidArgumentException(
+                $this->translator->trans('The SSL domain is not valid.', [], 'Admin.Notifications.Error')
+            );
+        }
+
+        return true;
     }
 }
