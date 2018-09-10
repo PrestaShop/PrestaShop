bc1a428c7b681039fe6f76e5cddb92b750244367
Šarūnas Jonušas <sarunas.jonusas@invertus.lt>
2018-09-10 14:25:15 +0300

    fix missing numeric value parser

--- src/Core/Grid/Query/DoctrineQueryParser.php
+++ src/Core/Grid/Query/DoctrineQueryParser.php
@@ -92,6 +92,16 @@ final class DoctrineQueryParser implements QueryParserInterface
     }
 
     /**
+     * @param int|float $value
+     *
+     * @return string
+     */
+    private function parseNumericParameter($value)
+    {
+        return "'".$value."'";
+    }
+
+    /**
      * @param array $value
      *
      * @return string
