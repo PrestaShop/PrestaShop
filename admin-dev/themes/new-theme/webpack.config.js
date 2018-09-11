44cfbb01aaabb59f58e413668afd8a157fde301f
Šarūnas Jonušas <sarunas.jonusas@invertus.lt>
2018-09-11 16:58:40 +0300

    update webpack

--- admin-dev/themes/new-theme/webpack.config.js
+++ admin-dev/themes/new-theme/webpack.config.js
@@ -111,6 +111,9 @@ const config = {
     meta: [
       './js/pages/meta'
     ],
+    categories: [
+      './js/pages/categories'
+    ],
   },
   output: {
     path: path.resolve(__dirname, 'public'),
