e5acc7a3a372aeb489221bde62164aa3971d79f2
Šarūnas Jonušas <sarunas.jonusas@invertus.lt>
2018-09-12 12:24:05 +0300

    add material choice tree

---
+++ src/PrestaShopBundle/Form/Admin/Type/Material/MaterialChoiceTreeType.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * 2007-2018 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2018 PrestaShop SA
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+namespace PrestaShopBundle\Form\Admin\Type\Material;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MaterialChoiceTreeType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['multiple'] = $options['multiple'];
+        $view->vars['choices_tree'] = $options['choices_tree'];
+        $view->vars['choice_label'] = $options['choice_label'];
+        $view->vars['choice_value'] = $options['choice_value'];
+        $view->vars['choice_children'] = $options['choice_children'];
+        $view->vars['choice_name'] = $options['choice_name'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver
+            ->setRequired([
+                'choice_label',
+                'choice_value',
+            ])
+            ->setDefaults([
+                'choices_tree' => [],
+                'choice_children' => 'children',
+                'choice_name' => 'tree',
+                'multiple' => true,
+            ])
+            ->setAllowedTypes('choices_tree', 'array')
+            ->setAllowedTypes('multiple', 'bool')
+            ->setAllowedTypes('choice_value', 'string')
+            ->setAllowedTypes('choice_label', 'string')
+            ->setAllowedTypes('choice_children', 'string')
+            ->setAllowedTypes('choice_name', 'string')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'material_choice_tree';
+    }
+}
