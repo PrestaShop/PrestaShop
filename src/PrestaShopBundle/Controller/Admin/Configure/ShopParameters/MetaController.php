5b8fd19d4f4c42febf508303fcf16245080f9201
Tomas Ilginis <tomas.ilginis@invertus.lt>
2018-09-20 12:42:07 +0300

    uses learn more link, creates card, copies styles to new theme form default theme, updates learn more button style to target class name

--- src/PrestaShopBundle/Controller/Admin/Configure/ShopParameters/MetaController.php
+++ src/PrestaShopBundle/Controller/Admin/Configure/ShopParameters/MetaController.php
@@ -78,6 +78,8 @@ class MetaController extends FrameworkBundleAdminController
         $isShopContext = $context->isShopContext();
         $isShopFeatureActive = $this->get('prestashop.adapter.multistore_feature')->isActive();
 
+        $helperBlockLinkProvider = $this->get('prestashop.core.helper_doc.meta_page_link_provider');
+
         return [
             'layoutHeaderToolbarBtn' => [
                 'add' => [
@@ -98,6 +100,7 @@ class MetaController extends FrameworkBundleAdminController
             'isHostMode' => $hostingInformation->isHostMode(),
             'enableSidebar' => true,
             'help_link' => $this->generateSidebarLink('AdminMeta'),
+            'helperDocLink' => $helperBlockLinkProvider->getLink(),
         ];
     }
 
