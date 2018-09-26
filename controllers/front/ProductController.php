0057daaa8d93f1f91542c36843c0ef18d074c078
jancinert <jan.cinert@gmail.com>
2018-09-26 02:38:34 +0200

    Product detail - a specific price with quantity_from = 1 -> wrong prices displayed upon a quantity input changes
    Fixes overlap code changes in not merged PR https://github.com/PrestaShop/PrestaShop/pull/8099
    
    Fixes #10710

--- controllers/front/ProductController.php
+++ controllers/front/ProductController.php
@@ -338,7 +338,7 @@ class ProductControllerCore extends FrontController
                 $quantity_discount['base_price'] = 0;
             }
             if ($quantity_discount['id_product_attribute']) {
-                $quantity_discount['base_price'] = $this->product->getPrice(Product::$_taxCalculationMethod == PS_TAX_INC, $quantity_discount['id_product_attribute']);
+                $quantity_discount['base_price'] = $this->product->getPrice(Product::$_taxCalculationMethod == PS_TAX_INC, $quantity_discount['id_product_attribute'], 6, null, false, false, $quantity_discount['from_quantity']);
 
                 $combination = new Combination((int)$quantity_discount['id_product_attribute']);
                 $attributes = $combination->getAttributesName((int)$this->context->language->id);
