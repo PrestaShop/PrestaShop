afd997e61bf4cd700eb9846983a35d94701c4b01
Tomas <tilginis@gmail.com>
2018-09-29 16:40:22 +0300

    implements filtering by status

--- src/PrestaShopBundle/Controller/Admin/Improve/International/CurrencyController.php
+++ src/PrestaShopBundle/Controller/Admin/Improve/International/CurrencyController.php
@@ -60,6 +60,23 @@ class CurrencyController extends FrameworkBundleAdminController
         ];
     }
 
+    public function searchAction(Request $request)
+    {
+        $definitionFactory = $this->get('prestashop.core.grid.definition.factory.currency');
+        $definitionFactory = $definitionFactory->getDefinition();
+
+        $gridFilterFormFactory = $this->get('prestashop.core.grid.filter.form_factory');
+        $searchParametersForm = $gridFilterFormFactory->create($definitionFactory);
+        $searchParametersForm->handleRequest($request);
+
+        $filters = [];
+        if ($searchParametersForm->isSubmitted()) {
+            $filters = $searchParametersForm->getData();
+        }
+
+        return $this->redirectToRoute('admin_currency_index', ['filters' => $filters]);
+    }
+
     public function toggleStatusAction()
     {
         //todo: implement
