305f4f648ba408509893eabbf952a84393bcfa25
Rokas <rokas.zygmantas@gmail.com>
2018-09-30 13:52:36 +0300

    Render contacts list, without javascript actions yet

--- src/Core/Search/Filters/ContactFilters.php
+++ src/Core/Search/Filters/ContactFilters.php
@@ -1,5 +1,6 @@
-{#**
- * 2007-2018 PrestaShop
+<?php
+/**
+ * 2007-2018 PrestaShop.
  *
  * NOTICE OF LICENSE
  *
@@ -21,14 +22,28 @@
  * @copyright 2007-2018 PrestaShop SA
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
- *#}
+ */
 
-{% extends '@PrestaShop/Admin/layout.html.twig' %}
+namespace PrestaShop\PrestaShop\Core\Search\Filters;
 
-{% block content %}
-  <div class="row">
-    <div class="col">
-      {% include '@PrestaShop/Admin/Common/Grid/grid_panel.html.twig' with {'grid': grid} %}
-    </div>
-  </div>
-{% endblock %}
+use PrestaShop\PrestaShop\Core\Search\Filters;
+
+/**
+ * Class ContactFilters is responsible for providing default filter values for Contacts list.
+ */
+final class ContactFilters extends Filters
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDefaults()
+    {
+        return [
+            'limit' => 10,
+            'offset' => 0,
+            'orderBy' => 'id_contact',
+            'sortOrder' => 'asc',
+            'filters' => [],
+        ];
+    }
+}
