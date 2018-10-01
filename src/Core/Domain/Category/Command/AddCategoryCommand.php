e5cb994c75576bd02c48e1b8fb42008af5787e7f
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2018-10-01 09:38:58 +0300

    update commands doc blocks

--- src/Core/Domain/Category/Command/AddCategoryCommand.php
+++ src/Core/Domain/Category/Command/AddCategoryCommand.php
@@ -31,7 +31,7 @@ use PrestaShop\PrestaShop\Core\Domain\Category\Exception\CategoryConstraintExcep
 /**
  * Class AddCategoryCommand adds new category.
  */
-class AddCategoryCommand extends AbstractRootCategoryCommand
+class AddCategoryCommand extends AbstractCategoryCommand
 {
     /**
      * @var int
