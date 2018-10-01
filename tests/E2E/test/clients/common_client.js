4ecb8de204a0e5dffab1fec2fdf3d5162c9d11e0
ansar.mallouli <ansar.mallouli@sifast.com>
2018-10-01 10:15:54 +0100

    TE: localization: Local units

--- tests/E2E/test/clients/common_client.js
+++ tests/E2E/test/clients/common_client.js
@@ -332,7 +332,7 @@ class CommonClient {
    * @returns {*}
    */
   checkFile(folderPath, fileName, pause = 0) {
-    fs.stat(folderPath + fileName, function(err, stats) {
+    fs.stat(folderPath + fileName, function (err, stats) {
       err === null && stats.isFile() ? global.existingFile = true : global.existingFile = false;
     });
 
