3c813fd576e0071b3c321c358f99e08de4dc19d8
Captain Youz <anas.mammeri@gmail.com>
2018-10-08 11:03:44 +0200

    feat: split prod and dev conf into separate files

---
+++ admin-dev/themes/new-theme/.webpack/dev.js
@@ -0,0 +1,39 @@
+const common = require('./common.js');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
+
+function devConfig(hostname) {
+  if (!hostname) console.log('Default host for livereload is set to `localhost`. If you want to use a custom one, just launch the command with hostname option. Ex: `npm run dev -- --hostname="yourUrl"`');
+  console.log('Livereload available on:', hostname);
+
+  let dev = Object.assign(common, {
+    devtool: 'inline-source-map'
+  });
+
+  dev.module.rules.push({
+    test:/\.(s*)css$/,
+    use: [
+      'css-loader',
+      'postcss-loader',
+      'sass-loader'
+    ]
+  });
+
+  // @toDo: remove sass support in vue files
+  dev.module.rules.push({
+    test:/\.sass$/,
+    use: [
+      'css-loader',
+      'postcss-loader',
+      'sass-loader'
+    ]
+  });
+
+  dev.plugins.push(new LiveReloadPlugin({
+    appendScriptTag: true,
+    hostname: hostname || 'localhost'
+  }));
+
+  return dev;
+}
+
+module.exports = devConfig;
