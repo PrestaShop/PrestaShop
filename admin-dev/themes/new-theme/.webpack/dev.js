b6a22df003c80e2fb5d26519d5c8f7b4a93f4a70
Captain Youz <anas.mammeri@gmail.com>
2018-10-08 11:37:50 +0200

    refactor: refactor and docs

--- admin-dev/themes/new-theme/.webpack/dev.js
+++ admin-dev/themes/new-theme/.webpack/dev.js
@@ -1,13 +1,20 @@
 const common = require('./common.js');
 const LiveReloadPlugin = require('webpack-livereload-plugin');
 
+/**
+ * devConfig function return the development webpack config,
+ * by merging development specific configuration with the common one.
+ *
+ * @param {String} hostname Represtent the hostname paramater. Default to localhost
+ */
 function devConfig(hostname) {
   if (!hostname) console.log('Default host for livereload is set to `localhost`. If you want to use a custom one, just launch the command with hostname option. Ex: `npm run dev -- --hostname="yourUrl"`');
+
+  hostname = hostname || 'localhost';
+
   console.log('Livereload available on:', hostname);
 
-  let dev = Object.assign(common, {
-    devtool: 'inline-source-map'
-  });
+  let dev = Object.assign(common, { devtool: 'inline-source-map' });
 
   dev.module.rules.push({
     test:/\.(s*)css$/,
@@ -21,7 +28,7 @@ function devConfig(hostname) {
 
   dev.plugins.push(new LiveReloadPlugin({
     appendScriptTag: true,
-    hostname: hostname || 'localhost'
+    hostname: hostname
   }));
 
   return dev;
--- admin-dev/themes/new-theme/.webpack/prod.js
+++ admin-dev/themes/new-theme/.webpack/prod.js
@@ -1,6 +1,12 @@
 const common = require('./common.js');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+/**
+ * prodConfig function return the production webpack config,
+ * by merging production specific configuration with the common one.
+ *
+ * @param {Boolean} analyze With analye to true, bundle analyze plugin will launch
+ */
 function prodConfig(analyze) {
   let prod = Object.assign(common, {
     stats: 'minimal',
@@ -23,18 +29,16 @@ function prodConfig(analyze) {
   prod.module.rules.push({
     test:/\.(s*)css$/,
     use: [
-      MiniCssExtractPlugin.loader,  // extract CSS to theme.css in prod, style-loader in dev
+      MiniCssExtractPlugin.loader,
       'css-loader',
       'postcss-loader',
       'sass-loader'
     ]
   });
 
-  prod.plugins.push(
-    new MiniCssExtractPlugin({
-      filename: '[name].css'
-    })
-  );
+  prod.plugins.push(new MiniCssExtractPlugin({
+    filename: '[name].css'
+  }));
 
   if (analyze) {
     const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
