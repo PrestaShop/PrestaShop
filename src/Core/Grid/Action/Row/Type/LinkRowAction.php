37074d7f270e70cb006bf1f26bd86f76e21f4214
Tomas Ilginis <tomas.ilginis@invertus.lt>
2018-10-11 12:38:38 +0300

    adds extra route param fields which can be passed to generate urls with more then one variable

--- src/Core/Grid/Action/Row/Type/LinkRowAction.php
+++ src/Core/Grid/Action/Row/Type/LinkRowAction.php
@@ -54,12 +54,15 @@ final class LinkRowAction extends AbstractRowAction
             ->setDefaults([
                 'confirm_message' => '',
                 'accessibility_checker' => null,
+                'route_param_extra_fields' => [],
             ])
             ->setAllowedTypes('route', 'string')
             ->setAllowedTypes('route_param_name', 'string')
             ->setAllowedTypes('route_param_field', 'string')
             ->setAllowedTypes('confirm_message', 'string')
-            ->setAllowedTypes('accessibility_checker', [AccessibilityCheckerInterface::class, 'callable', 'null']);
+            ->setAllowedTypes('accessibility_checker', [AccessibilityCheckerInterface::class, 'callable', 'null'])
+            ->setAllowedTypes('route_param_extra_fields', 'array')
+        ;
     }
 
     /**
