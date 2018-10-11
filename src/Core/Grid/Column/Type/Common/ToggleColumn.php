3edd3e664b583e01c354e7cb502327c0e73bd61f
Tomas Ilginis <tomas.ilginis@invertus.lt>
2018-10-11 17:57:39 +0300

    updates positions so it has available routeParamNames to set

--- src/Core/Grid/Column/Type/Common/ToggleColumn.php
+++ src/Core/Grid/Column/Type/Common/ToggleColumn.php
@@ -60,14 +60,14 @@ final class ToggleColumn extends AbstractColumn
                 'route_param_name',
             ])
             ->setDefaults([
-                'route_param_extra_fields' => [],
+                'route_params_extra' => [],
             ])
             ->setAllowedTypes('field', 'string')
             ->setAllowedTypes('primary_field', 'string')
             ->setAllowedTypes('route', 'string')
             ->setAllowedTypes('route_param_name', 'string')
             ->setAllowedTypes('sortable', 'bool')
-            ->setAllowedTypes('route_param_extra_fields', 'array')
+            ->setAllowedTypes('route_params_extra', 'array')
         ;
     }
 }
