da3ca10d60d300c57bb8768ba167c32834971f11
Tomas Ilginis <tomas.ilginis@invertus.lt>
2018-10-11 14:11:09 +0300

    adds cms page category deletion command

---
+++ src/Adapter/CMS/PageCategory/CommandHandler/DeleteCmsPageCategoryHandler.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * 2007-2018 PrestaShop.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2018 PrestaShop SA
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+namespace PrestaShop\PrestaShop\Adapter\CMS\PageCategory\CommandHandler;
+
+use CMSCategory;
+use PrestaShop\PrestaShop\Core\Domain\CmsPageCategory\Command\DeleteCmsPageCategoryCommand;
+use PrestaShop\PrestaShop\Core\Domain\CmsPageCategory\CommandHandler\DeleteCmsPageCategoryHandlerInterface;
+use PrestaShop\PrestaShop\Core\Domain\CmsPageCategory\Exception\CannotDeleteCmsPageCategoryException;
+use PrestaShop\PrestaShop\Core\Domain\CmsPageCategory\Exception\CmsPageCategoryException;
+use PrestaShop\PrestaShop\Core\Domain\CmsPageCategory\Exception\CmsPageCategoryNotFoundException;
+use PrestaShopException;
+
+/**
+ * Class DeleteCmsPageCategoryHandler
+ */
+class DeleteCmsPageCategoryHandler implements DeleteCmsPageCategoryHandlerInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @param DeleteCmsPageCategoryCommand $command
+     *
+     * @throws CannotDeleteCmsPageCategoryException
+     * @throws CmsPageCategoryException
+     * @throws CmsPageCategoryNotFoundException
+     */
+    public function handle(DeleteCmsPageCategoryCommand $command)
+    {
+        try {
+            $entity = new CMSCategory($command->getCmsPageCategoryId()->getValue());
+        } catch (PrestaShopException $exception) {
+            throw new CmsPageCategoryException(
+                sprintf(
+                    'An error occurred when finding cms category object with id "%s"',
+                    $command->getCmsPageCategoryId()->getValue()
+                ),
+                0,
+                $exception
+            );
+        }
+
+        if (0 >= $entity->id) {
+            throw new CmsPageCategoryNotFoundException(
+                sprintf(
+                    'Cms category object with id "%s" has not been found for deletion.',
+                    $command->getCmsPageCategoryId()->getValue()
+                )
+            );
+        }
+
+        try {
+            if (false === $entity->delete()) {
+                throw new CannotDeleteCmsPageCategoryException(
+                    sprintf(
+                        'Unable to delete  cms category object with id "%s"',
+                        $command->getCmsPageCategoryId()->getValue()
+                    )
+                );
+            }
+        } catch (PrestaShopException $exception) {
+            throw new CmsPageCategoryException(
+                sprintf(
+                    'An error occurred when  deleting  cms category object with id "%s"',
+                    $command->getCmsPageCategoryId()->getValue()
+                ),
+                0,
+                $exception
+            );
+        }
+    }
+}
