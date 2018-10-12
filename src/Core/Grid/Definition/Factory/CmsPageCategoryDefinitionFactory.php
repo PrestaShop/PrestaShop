22af67415700f1e9db21c1c8aeb0c310f37435a8
Tomas Ilginis <tomas.ilginis@invertus.lt>
2018-10-12 13:47:13 +0300

    implements disable status command usag

--- src/Core/Grid/Definition/Factory/CmsPageCategoryDefinitionFactory.php
+++ src/Core/Grid/Definition/Factory/CmsPageCategoryDefinitionFactory.php
@@ -255,15 +255,15 @@ final class CmsPageCategoryDefinitionFactory extends AbstractGridDefinitionFacto
                     ],
                 ])
             )
-//            ->add((new SubmitBulkAction('disable_selection'))
-//                ->setName($this->trans('Disable selection', [], 'Admin.Actions'))
-//                ->setOptions([
-//                    'submit_route' => 'admin_cms_pages_bulk_status_disable',
-//                    'route_params_extra' => [
-//                        'cmsCategoryParentId' => $this->cmsCategoryParentId,
-//                    ],
-//                ])
-//            )
+            ->add((new SubmitBulkAction('disable_selection'))
+                ->setName($this->trans('Disable selection', [], 'Admin.Actions'))
+                ->setOptions([
+                    'submit_route' => 'admin_cms_pages_bulk_status_disable',
+                    'route_params_extra' => [
+                        'cmsCategoryParentId' => $this->cmsCategoryParentId,
+                    ],
+                ])
+            )
             ->add((new SubmitBulkAction('delete_bulk'))
                 ->setName($this->trans('Delete selected', [], 'Admin.Actions'))
                 ->setOptions([
