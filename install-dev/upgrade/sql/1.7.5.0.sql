ec34e77e16b6d2181f6f4edf191caa6d70eb76cf
Mreker <mathias@reker.dk>
2018-10-15 15:45:49 +0200

    Removed dublicates and changed varchar from 2048 to 255 for passwords

--- install-dev/upgrade/sql/1.7.5.0.sql
+++ install-dev/upgrade/sql/1.7.5.0.sql
@@ -6,108 +6,66 @@ SET NAMES 'utf8';
 /* PHP:ps_1750_update_module_tabs(); */;
 
 ALTER TABLE `PREFIX_cms_lang`
-	ADD `head_seo_title` varchar(255) DEFAULT NULL AFTER `meta_title`;
+	ADD `head_seo_title` varchar(255) DEFAULT NULL AFTER `meta_title`,
+	CHANGE `meta_title` `meta_title` VARCHAR(255) NOT NULL,
+	CHANGE `meta_description` `meta_description` VARCHAR(512) DEFAULT NULL;
 
 ALTER TABLE `PREFIX_stock_available`
   ADD `location` VARCHAR(255) NOT NULL DEFAULT '' AFTER `out_of_stock`;
 
-ALTER TABLE `prefix_store`
+ALTER TABLE `PREFIX_store`
   CHANGE `email` `email` VARCHAR(255) DEFAULT NULL;
 
-ALTER TABLE `prefix_contact`
+ALTER TABLE `PREFIX_contact`
   CHANGE `email` `email` VARCHAR(255) NOT NULL;
 
-ALTER TABLE `prefix_category_lang`
-  CHANGE `meta_title` `meta_title` VARCHAR(255) DEFAULT NULL;
-
-ALTER TABLE `prefix_cms_lang`
-  CHANGE `meta_title` `meta_title` VARCHAR(255) NOT NULL;
-
-ALTER TABLE `prefix_cms_category_lang`
-  CHANGE `meta_title` `meta_title` VARCHAR(255) DEFAULT NULL;
-
-ALTER TABLE `prefix_contact`
-  CHANGE `email` `email` VARCHAR(255) NOT NULL;
-
-ALTER TABLE `prefix_customer`
-  CHANGE `company` `company` VARCHAR(255);
-
-ALTER TABLE `prefix_customer`
-  CHANGE `email` `email` VARCHAR(255) NOT NULL;
-
-ALTER TABLE `prefix_customer`
-  CHANGE `passwd` `passwd` VARCHAR(255) NOT NULL;
-
-ALTER TABLE `prefix_manufacturer_lang`
-  CHANGE `meta_title` `meta_title` VARCHAR(255) NOT NULL;
-
-ALTER TABLE `prefix_supplier_lang`
-  CHANGE `meta_title` `meta_title` VARCHAR(255) DEFAULT NULL;
-
-ALTER TABLE `prefix_employee`
-  CHANGE `lastname` `lastname` VARCHAR(255) NOT NULL;
-
-ALTER TABLE `prefix_employee`
-  CHANGE `firstname` `firstname` VARCHAR(255) NOT NULL;
-
-ALTER TABLE `prefix_employee`
-  CHANGE `email` `email` VARCHAR(255) NOT NULL;
-
-ALTER TABLE `prefix_employee`
-  CHANGE `passwd` `passwd` VARCHAR(255) NOT NULL;
-
-ALTER TABLE `prefix_referrer`
-  CHANGE `passwd` `passwd` VARCHAR(255) DEFAULT NULL;
-
-ALTER TABLE `prefix_supply_order_history`
-  CHANGE `employee_lastname` `employee_lastname` VARCHAR(255) DEFAULT '';
-
-ALTER TABLE `prefix_supply_order_history`
-  CHANGE `employee_firstname` `employee_firstname` VARCHAR(255) DEFAULT '';
-
-ALTER TABLE `prefix_supply_order_receipt_history`
-  CHANGE `employee_firstname` `employee_firstname` VARCHAR(255) DEFAULT '';
-
-ALTER TABLE `prefix_supply_order_receipt_history`
-  CHANGE `employee_lastname` `employee_firstname` VARCHAR(255) DEFAULT '';
-
-ALTER TABLE `prefix_category_lang`
+ALTER TABLE `PREFIX_category_lang`
+  CHANGE `meta_title` `meta_title` VARCHAR(255) DEFAULT NULL,
   CHANGE `meta_description` `meta_description` VARCHAR(512) DEFAULT NULL;
 
-ALTER TABLE `prefix_cms_lang`
+ALTER TABLE `PREFIX_cms_category_lang`
+  CHANGE `meta_title` `meta_title` VARCHAR(255) DEFAULT NULL,
   CHANGE `meta_description` `meta_description` VARCHAR(512) DEFAULT NULL;
-
-ALTER TABLE `prefix_cms_category_lang`
-  CHANGE `meta_description` `meta_description` VARCHAR(512) DEFAULT NULL;
-
-ALTER TABLE `prefix_manufacturer_lang`
-  CHANGE `meta_description` `meta_description` VARCHAR(512) DEFAULT NULL;
-
-ALTER TABLE `prefix_supplier_lang`
-  CHANGE `meta_description` `meta_description` VARCHAR(512) DEFAULT NULL; 
 
 ALTER TABLE `PREFIX_customer`
-  CHANGE `passwd` `passwd` varchar(2048) NOT NULL;
+  CHANGE `company` `company` VARCHAR(255),
+  CHANGE `email` `email` VARCHAR(255) NOT NULL,
+  CHANGE `passwd` `passwd` VARCHAR(255) NOT NULL;
+
+ALTER TABLE `PREFIX_manufacturer_lang`
+  CHANGE `meta_title` `meta_title` VARCHAR(255) NOT NULL,
+  CHANGE `meta_description` `meta_description` VARCHAR(512) DEFAULT NULL;
 
 ALTER TABLE `PREFIX_employee`
-  CHANGE `passwd` `passwd` varchar(2048) NOT NULL;
+  CHANGE `firstname` `firstname` VARCHAR(255) NOT NULL,
+  CHANGE `email` `email` VARCHAR(255) NOT NULL,
+  CHANGE `passwd` `passwd` VARCHAR(255) NOT NULL,
+  CHANGE `lastname` `lastname` VARCHAR(255) NOT NULL;
 
-ALTER TABLE `PREFIX_customer`
-  CHANGE `passwd` `passwd` varchar(2048) NOT NULL;
+ALTER TABLE `PREFIX_referrer`
+  CHANGE `passwd` `passwd` VARCHAR(255) DEFAULT NULL;
+
+ALTER TABLE `PREFIX_supply_order_history`
+  CHANGE `employee_lastname` `employee_lastname` VARCHAR(255) DEFAULT '',
+  CHANGE `employee_firstname` `employee_firstname` VARCHAR(255) DEFAULT '';
+
+ALTER TABLE `PREFIX_supply_order_receipt_history`
+  CHANGE `employee_firstname` `employee_firstname` VARCHAR(255) DEFAULT '',
+  CHANGE `employee_lastname` `employee_firstname` VARCHAR(255) DEFAULT '';
+
+ALTER TABLE `PREFIX_supplier_lang`
+  CHANGE `meta_description` `meta_description` VARCHAR(512) DEFAULT NULL,
+  CHANGE `meta_title` `meta_title` VARCHAR(255) DEFAULT NULL;
 
 ALTER TABLE `PREFIX_order_detail`
   CHANGE `product_reference` `product_reference` varchar(64) DEFAULT NULL;
 
 ALTER TABLE `PREFIX_product`
-  CHANGE `reference` `supplier_reference` varchar(64) DEFAULT NULL;
-
-ALTER TABLE `PREFIX_product`
+  CHANGE `reference` `supplier_reference` varchar(64) DEFAULT NULL,
   CHANGE `supplier_reference` `supplier_reference` varchar(64) DEFAULT NULL;
 
 ALTER TABLE `PREFIX_product_attribute`
-  CHANGE `reference` `reference` varchar(64) DEFAULT NULL;
-
-ALTER TABLE `PREFIX_product_attribute`
+  CHANGE `reference` `reference` varchar(64) DEFAULT NULL,
   CHANGE `supplier_reference` `supplier_reference` varchar(64) DEFAULT NULL;
 
 ALTER TABLE `PREFIX_warehouse`
@@ -117,10 +75,8 @@ ALTER TABLE `PREFIX_stock`
   CHANGE `reference` `reference` varchar(64) DEFAULT NULL;
 
 ALTER TABLE `PREFIX_supply_order_detail`
-  CHANGE `reference` `reference` varchar(64) NOT NULL; 
-
-ALTER TABLE `PREFIX_supply_order_detail`
-  CHANGE `supplier_reference` `supplier_reference` varchar(64) NOT NULL; 
+  CHANGE `reference` `reference` varchar(64) NOT NULL,
+  CHANGE `supplier_reference` `supplier_reference` varchar(64) NOT NULL;
 
 ALTER TABLE `PREFIX_product_supplier`
-  CHANGE `product_supplier_reference` `product_supplier_reference` varchar(64) DEFAULT NULL; 
+  CHANGE `product_supplier_reference` `product_supplier_reference` varchar(64) DEFAULT NULL;
