c32c1df16b77fd28506871468c7ec11728a92759
khouloud.belguith <khouloud.belguiith@gmail.com>
2018-10-18 10:49:13 +0200

    Apply quotes around the column names would make all the SQL requests

--- controllers/admin/AdminRequestSqlController.php
+++ controllers/admin/AdminRequestSqlController.php
@@ -50,10 +50,7 @@ class AdminRequestSqlControllerCore extends AdminController
         $this->fields_list = array(
             'id_request_sql' => array('title' => $this->trans('ID', array(), 'Admin.Global'), 'class' => 'fixed-width-xs'),
             'name' => array('title' => $this->trans('SQL query Name', array(), 'Admin.Advparameters.Feature')),
-            'sql' => array(
-                'title' => $this->trans('SQL query', array(), 'Admin.Advparameters.Feature'),
-                'filter_key' => 'a!sql',
-            ),
+            'sql' => array('title' => $this->trans('SQL query', array(), 'Admin.Advparameters.Feature')),
         );
 
         $this->fields_options = array(
