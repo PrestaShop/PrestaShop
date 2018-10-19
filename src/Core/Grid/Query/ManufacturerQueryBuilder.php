8bb6e559dd0fb11df5468d6e2f54ba73fd1e29a0
Tomas Ilginis <tomas.ilginis@invertus.lt>
2018-10-19 16:37:46 +0300

    displays the list of brands with reduced amount of data

---
+++ src/Core/Grid/Query/ManufacturerQueryBuilder.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * 2007-2018 PrestaShop.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2018 PrestaShop SA
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+namespace PrestaShop\PrestaShop\Core\Grid\Query;
+
+use Doctrine\DBAL\Query\QueryBuilder;
+use Doctrine\DBAL\Connection;
+use PrestaShop\PrestaShop\Core\Grid\Search\SearchCriteriaInterface;
+
+/**
+ * Class ManufacturerQueryBuilder is responsible for building queries for manufacturers grid data.
+ */
+final class ManufacturerQueryBuilder extends AbstractDoctrineQueryBuilder
+{
+    /**
+     * @var int
+     */
+    private $contextLangId;
+
+    /**
+     * @param Connection $connection
+     * @param string $dbPrefix
+     * @param int $contextLangId
+     */
+    public function __construct(
+        Connection $connection,
+        $dbPrefix,
+        $contextLangId
+    ) {
+        parent::__construct($connection, $dbPrefix);
+        //todo: single shop context only?
+        $this->contextLangId = $contextLangId;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSearchQueryBuilder(SearchCriteriaInterface $searchCriteria)
+    {
+        $qb = $this->getQueryBuilder($searchCriteria->getFilters());
+        $qb->select('m.`id_manufacturer`, m.`name`, m.`active`');
+
+        return $qb;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCountQueryBuilder(SearchCriteriaInterface $searchCriteria)
+    {
+        $qb = $this->getQueryBuilder($searchCriteria->getFilters());
+        $qb->select('COUNT(DISTINCT m.`id_manufacturer`)');
+
+        return $qb;
+    }
+
+    /**
+     * Get generic query builder.
+     *
+     * @param array $filters
+     *
+     * @return QueryBuilder
+     */
+    private function getQueryBuilder(array $filters)
+    {
+        $qb = $this->connection
+            ->createQueryBuilder()
+            ->from($this->dbPrefix . 'manufacturer', 'm')
+        ;
+
+        return $qb;
+    }
+}
