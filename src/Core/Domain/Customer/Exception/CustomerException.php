fba365255685605da2f0099307711438d397b327
Tomas <tilginis@gmail.com>
2018-10-22 18:39:01 +0300

    fix CustomerException parent
    Co-Authored-By: sarjon <jonusas.sarunas@gmail.com>

--- src/Core/Domain/Customer/Exception/CustomerException.php
+++ src/Core/Domain/Customer/Exception/CustomerException.php
@@ -29,6 +29,6 @@ namespace PrestaShop\PrestaShop\Core\Domain\Customer\Exception;
 /**
  * Class CustomerException is base "Customer" context exception.
  */
-class CustomerException extends \Exception
+class CustomerException extends DomainException
 {
 }
