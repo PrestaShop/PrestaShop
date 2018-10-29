e9d1ac828564f442399c6fd958920c7aeced0665
Thomas Nabord <thomas.nabord@prestashop.com>
2018-10-29 14:54:53 +0000

    Update selector for develop branch with previous changes

--- tests/E2E/test/selectors/FO/add_account_page.js
+++ tests/E2E/test/selectors/FO/add_account_page.js
@@ -42,6 +42,9 @@ module.exports = {
     email_sent_message: '//*[@id="content-hook_order_confirmation"]//p',
     save_notification: '//*[@id="notifications"]//li',
     danger_alert: '//*[@id="customer-form"]//li[contains(@class,"alert-danger")]',
-    add_new_address: '//*[@id="checkout-addresses-step"]//p[@class="add-address"]/a'
+    add_new_address: '//*[@id="checkout-addresses-step"]//p[@class="add-address"]/a',
+    //---------------------- address management -------------------------//
+    address_firstname_input: '//*[@class="js-address-form"]//input[@name="firstname"]',
+    address_lastname_input: '//*[@class="js-address-form"]//input[@name="lastname"]'
   }
 };
