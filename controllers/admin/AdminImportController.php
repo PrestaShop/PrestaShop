d56e21863cbadcbeb6bfc0e9d032af0f60618c0e
Martin Finkenflügel <1516225+MartinFinkenflugel@users.noreply.github.com>
2018-11-02 17:32:27 +0100

    Update AdminImportController.php

--- controllers/admin/AdminImportController.php
+++ controllers/admin/AdminImportController.php
@@ -4284,7 +4284,7 @@ class AdminImportControllerCore extends AdminController
                 $reader_excel->setReadDataOnly(true);
                 $excel_file = $reader_excel->load($csv_folder . $filename);
 
-                $csv_writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($excel_file, 'CSV');
+                $csv_writer = \PhpOffice\PhpSpreadsheet\IOFactory::createWriter($excel_file, 'Csv');
 
                 $csv_writer->setSheetIndex(0);
                 $csv_writer->setDelimiter(';');
