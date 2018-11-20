e3182dc6910370f67a873433472866e962d313bf
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2018-11-20 09:55:02 +0200

    fix customers filtering by registration date

--- src/Core/Grid/Query/Builder/CustomerQueryBuilder.php
+++ src/Core/Grid/Query/Builder/CustomerQueryBuilder.php
@@ -201,6 +201,16 @@ final class CustomerQueryBuilder extends AbstractDoctrineQueryBuilder
                 $qb->setParameter('date_from', sprintf('%s 0:0:0', $filterValue['from']));
                 $qb->setParameter('date_to', sprintf('%s 23:59:59', $filterValue['to']));
 
+                if (isset($filterValue['from'])) {
+                    $qb->andWhere('c.date_add >= :date_from');
+                    $qb->setParameter('date_from', sprintf('%s 0:0:0', $filterValue['from']));
+                }
+
+                if (isset($filterValue['to'])) {
+                    $qb->andWhere('c.date_add <= :date_to');
+                    $qb->setParameter('date_to', sprintf('%s 23:59:59', $filterValue['to']));
+                }
+
                 continue;
             }
 
