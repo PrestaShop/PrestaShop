7cfb436a5170c804b597b23af7fe0f3c2f6dfad3
Mathieu Ferment <mathieu.ferment@prestashop.com>
2018-11-27 16:12:32 +0100

    Fix new unit tests bootstrap and scripts

--- tests/check_unit.sh
+++ tests/check_unit.sh
@@ -3,10 +3,10 @@
 composer run-script unit-tests --timeout=0;
 UNIT=$?
 
-if [[ "UNIT" == "0" ]]; then
-  echo -e "\e[92mPHPUNIT TESTS OK"
+if [[ "$UNIT" == "0" ]]; then
+  echo -e "\e[UNIT TESTS OK"
   exit 0;
 else
-  echo -e "\e[91mPHPUNIT TESTS FAILED"
+  echo -e "\e[91mUNIT TESTS FAILED"
   exit 255;
 fi
