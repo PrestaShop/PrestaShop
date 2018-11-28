0ea626e3723c602fdc99a1682e13de73a8737faf
Mathieu Ferment <mathieu.ferment@prestashop.com>
2018-11-28 15:40:59 +0100

    Fix bad namespace LegacyLegacy

--- tests-legacy/Unit/Core/Webservice/ServerRequirementsCheckerTest.php
+++ tests-legacy/Unit/Core/Webservice/ServerRequirementsCheckerTest.php
@@ -24,7 +24,7 @@
  * International Registered Trademark & Property of PrestaShop SA
  */
 
-namespace LegacyLegacyTests\Unit\Core\Webservice;
+namespace LegacyTests\Unit\Core\Webservice;
 
 use PHPUnit\Framework\TestCase;
 use PrestaShop\PrestaShop\Adapter\Configuration;
