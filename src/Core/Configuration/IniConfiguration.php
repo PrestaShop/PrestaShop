289e9284ea7bddd2ef2d28c94db5fe1b01efc25c
Rokas <rokas.zygmantas@gmail.com>
2018-11-29 11:40:56 +0200

    Make import respect server limitations

--- src/Core/Configuration/IniConfiguration.php
+++ src/Core/Configuration/IniConfiguration.php
@@ -38,17 +38,41 @@ class IniConfiguration
      */
     public function getPostMaxSizeInBytes()
     {
-        $postMaxSize = ini_get('post_max_size');
-        $bytes = (int) trim($postMaxSize);
-        $last = strtolower($postMaxSize[strlen($postMaxSize) - 1]);
+        return $this->convertToBytes(ini_get('post_max_size'));
+    }
+
+    /**
+     * Get maximum upload size allowed by the server in bytes.
+     *
+     * @return int
+     */
+    public function getUploadMaxSizeInBytes()
+    {
+        return min(
+            $this->convertToBytes(ini_get('upload_max_filesize')),
+            $this->getPostMaxSizeInBytes()
+        );
+    }
+
+    /**
+     * Convert a numeric value to bytes.
+     *
+     * @param int $value
+     *
+     * @return int
+     */
+    private function convertToBytes($value)
+    {
+        $bytes = (int) trim($value);
+        $last = strtolower($value[strlen($value) - 1]);
 
         switch ($last) {
             case 'g':
                 $bytes *= 1024;
-                // no break to fall-through
+            // no break to fall-through
             case 'm':
                 $bytes *= 1024;
-                // no break to fall-through
+            // no break to fall-through
             case 'k':
                 $bytes *= 1024;
         }
