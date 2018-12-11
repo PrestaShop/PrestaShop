a2bb5fb05c2e4c88d8b5e9a621ed004f3305b8c8
MathiasReker <mathias@reker.dk>
2018-12-11 10:24:44 +0100

    Fix use of alternative syntax

--- install-dev/theme/views/system.php
+++ install-dev/theme/views/system.php
@@ -29,24 +29,24 @@
 
 <p><?php echo $this->translator->trans('If you have any questions, please visit our <a href="%doc%" target="_blank">documentation</a> and <a href="%forum%" target="_blank">community forum</a>.', array('%doc%' => $this->getDocumentationLink(), '%forum%' => $this->getForumLink()), 'Install'); ?></p>
 
-<?php if ($this->tests['required']['success']): ?>
+<?php if ($this->tests['required']['success']) { ?>
 	<h3 class="okBlock"><?php echo $this->translator->trans('PrestaShop compatibility with your system environment has been verified!', array(), 'Install'); ?></h3>
-<?php else: ?>
+<?php } else { ?>
 	<h3 class="errorBlock"><?php echo $this->translator->trans('Oops! Please correct the item(s) below, and then click "Refresh information" to test the compatibility of your new system.', array(), 'Install'); ?></h3>
-<?php endif; ?>
+<?php } ?>
 <!-- Display tests results -->
-<?php foreach ($this->tests_render as $type => $categories): ?>
+<?php foreach ($this->tests_render as $type => $categories) { ?>
 	<ul id="<?php echo $type ?>">
-	<?php foreach ($categories as $category): ?>
-		<li class="title <?php if ($category['success'] == 1): ?>ok<?php endif;?>"><?php echo $category['title'] ?></li>
-		<?php $i = 0; foreach ($category['checks'] as $id => $lang): ?>
-			<li class="required <?php if ($i == 0): ?>first<?php endif;?> <?php echo isset($this->tests[$type]['checks'][$id]) ? $this->tests[$type]['checks'][$id] : 'fail' ?>">
+	<?php foreach ($categories as $category) { ?>
+		<li class="title <?php if ($category['success'] == 1) { ?>ok<?php }?>"><?php echo $category['title'] ?></li>
+		<?php $i = 0; foreach ($category['checks'] as $id => $lang) { ?>
+			<li class="required <?php if ($i == 0) { ?>first<?php }?> <?php echo isset($this->tests[$type]['checks'][$id]) ? $this->tests[$type]['checks'][$id] : 'fail' ?>">
 				<?php echo $lang ?>
 			</li>
-		<?php $i++; endforeach; ?>
-	<?php endforeach; ?>
+		<?php $i++; } ?>
+	<?php } ?>
 	</ul>
-<?php endforeach; ?>
+<?php } ?>
 
 <p><input class="button" value="<?php echo $this->translator->trans('Refresh', array(), 'Install'); ?> " type="submit" id="req_bt_refresh" /></p>
 
