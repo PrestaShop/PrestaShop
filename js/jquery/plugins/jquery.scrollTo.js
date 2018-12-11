948fc11707fdfc43d6aa82b0744c81e6e32329f0
MathiasReker <mathias@reker.dk>
2018-12-11 14:59:56 +0100

    Fix

--- js/jquery/plugins/jquery.scrollTo.js
+++ js/jquery/plugins/jquery.scrollTo.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2007 Ariel Flesler - aflesler ○ gmail • com | https://github.com/flesler
+ * Copyright (c) 2007-2015 Ariel Flesler - aflesler<a>gmail<d>com | http://flesler.blogspot.com
  * Licensed under MIT
  * @author Ariel Flesler
  * @version 2.1.2
