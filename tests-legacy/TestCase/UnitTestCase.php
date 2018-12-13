110eb86ec7fabcb57f7d5b95aed51fe6f07ed919
MathiasReker <mathias@reker.dk>
2018-12-13 19:05:40 +0100

    no alias tag

--- tests-legacy/TestCase/UnitTestCase.php
+++ tests-legacy/TestCase/UnitTestCase.php
@@ -231,7 +231,7 @@ class UnitTestCase extends \PHPUnit\Framework\TestCase
      * @param array  $parameters Array of parameters to pass into method.
      *
      * @return mixed Method return.
-     * @link https://jtreminio.com/2013/03/unit-testing-tutorial-part-3-testing-protected-private-methods-coverage-reports-and-crap/
+     * @see https://jtreminio.com/2013/03/unit-testing-tutorial-part-3-testing-protected-private-methods-coverage-reports-and-crap/
      */
     protected function invokeMethod(&$object, $methodName, array $parameters = array())
     {
