86b917bb4661f2123bc2ab158c791c2edb0ae3f1
MathiasReker <mathias@reker.dk>
2018-12-14 13:39:44 +0100

    Single blank line before namespace

--- tests-legacy/PrestaShopBundle/Model/Product/AdminModelAdapterTest.php
+++ tests-legacy/PrestaShopBundle/Model/Product/AdminModelAdapterTest.php
@@ -23,6 +23,7 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
+
 namespace LegacyTests\PrestaShopBundle\Model\Product;
 
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
