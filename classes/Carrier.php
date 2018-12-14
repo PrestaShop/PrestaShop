1bbd73234e3d833030669970ebb993aa25bc332c
jolelievre <jo.lelievre@gmail.com>
2018-12-14 11:17:07 +0100

    Update classes/Carrier.php
    Co-Authored-By: MathiasReker <mathias@reker.dk>

--- classes/Carrier.php
+++ classes/Carrier.php
@@ -515,7 +515,7 @@ class CarrierCore extends ObjectModel
      *                             - ALL_CARRIERS
      * @param bool $active Returns only active carriers when true
      * @param bool $delete
-     * @param mixed $id_zone
+     * @param int|false $id_zone
      * @param null|mixed $ids_group
      *
      * @return array Carriers
