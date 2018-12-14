37fb8293c79d915bf710d1b236a839ebaa9927aa
Pierre RAMBAUD <pierre.rambaud@prestashop.com>
2018-12-14 10:11:56 +0100

    Change reports directory

--- tests/E2E/scripts/run-nightly-tests.sh
+++ tests/E2E/scripts/run-nightly-tests.sh
@@ -3,7 +3,7 @@
 DIR_PATH=$(mktemp -d)
 REPORT_PATH=$(mktemp -p)
 BRANCH=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/TRAVIS_BRANCH -H "Metadata-Flavor: Google")
-OUTPUT_DIR="$(date +%Y-%m-%d)-reports-${BRANCH}"
+OUTPUT_NAME="$(date +%Y-%m-%d)-${BRANCH}"
 
 if [ -d $DIR_PATH ]; then
   rm -rf $DIR_PATH
@@ -13,7 +13,7 @@ git clone https://github.com/PrestaShop/PrestaShop.git $DIR_PATH
 
 cd $DIR_PATH
 git checkout $BRANCH
-mkdir -p "${DIR_PATH}/reports"
+mkdir -p "${DIR_PATH}/campaigns"
 
 cd "${DIR_PATH}/tests/E2E"
 for directory in test/campaigns/full/* ; do
@@ -29,14 +29,14 @@ for directory in test/campaigns/full/* ; do
     TEST_PATH=${directory/test\/campaigns\//}
     docker-compose exec -e TEST_PATH=$TEST_PATH tests /tmp/wait-for-it.sh --timeout=720 --strict prestashop-web:80 -- bash /tmp/run-tests.sh
 
-    cp mochawesome-report/mochawesome.json "${REPORT_PATH}/reports/${TEST_PATH//\//-}.json"
+    cp mochawesome-report/mochawesome.json "${REPORT_PATH}/campaigns/${TEST_PATH//\//-}.json"
 
     docker-compose down
   fi
 done
 
-./scripts/combine-reports.py "${REPORT_PATH}/reports" "${REPORT_PATH}/results.json"
-./node_modules/mochawesome-report-generator/bin/cli.js "${REPORT_PATH}/results.json" -o "${REPORT_PATH}/${OUTPUT_DIR}"
-gsutil cp -r "${REPORT_PATH}/${OUTPUT_DIR}" gs://prestashop-core-nightly
+./scripts/combine-reports.py "${REPORT_PATH}/campaigns" "${REPORT_PATH}/${OUTPUT_NAME}.json"
+./node_modules/mochawesome-report-generator/bin/cli.js "${REPORT_PATH}/${OUTPUT_NAME}.json" -o "${REPORT_PATH}/reports"
+gsutil cp -r "${REPORT_PATH}/reports" gs://prestashop-core-nightly
 
 sudo halt -p
