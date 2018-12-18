f147c0a3cf30376d8c019c3173f3ddfcd0d499a2
Pablo Borowicz <pablo.borowicz@prestashop.com>
2018-12-18 18:43:49 +0100

    Bump version to 1.7.5.1

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -32,11 +32,11 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class AppKernel extends Kernel
 {
-    const VERSION = '1.7.5.0';
+    const VERSION = '1.7.5.1';
     const MAJOR_VERSION_STRING = '1.7';
     const MAJOR_VERSION = 17;
     const MINOR_VERSION = 5;
-    const RELEASE_VERSION = 0;
+    const RELEASE_VERSION = 1;
 
     /**
      * @{inheritdoc}
