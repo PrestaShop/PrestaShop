fd4500445e8a2772de06dae7e179cb9ac887661c
Thomas LEVIANDIER <thomas.leviandier@gmail.com>
2018-12-20 11:06:57 +0100

    fix tests folder

--- tests-legacy/Unit/Core/Localization/CLDR/DataLayer/LocaleReferenceTest.php
+++ tests-legacy/Unit/Core/Localization/CLDR/DataLayer/LocaleReferenceTest.php
@@ -24,7 +24,7 @@
  * International Registered Trademark & Property of PrestaShop SA
  */
 
-namespace Tests\Unit\Core\Localization\CLDR\DataLayer;
+namespace LegacyTests\Unit\Core\Localization\CLDR\DataLayer;
 
 use PHPUnit\Framework\TestCase;
 use PrestaShop\PrestaShop\Core\Localization\CLDR\LocaleData as CldrLocaleData;
