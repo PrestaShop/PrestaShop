e21c06c51cd75de1751a00c8b599256dd860148b
Pierre RAMBAUD <pierre.rambaud@prestashop.com>
2018-12-28 16:21:41 +0100

    Remove live reload and fix missing theme.css file

--- admin-dev/themes/new-theme/.webpack/dev.js
+++ admin-dev/themes/new-theme/.webpack/dev.js
@@ -8,28 +8,12 @@ const LiveReloadPlugin = require('webpack-livereload-plugin');
  * @param {String} hostname Development host name, sent as a parameter to webpack. Defaults to localhost
  */
 function devConfig(hostname) {
-  if (!hostname) console.log('Default host for live reload is set to `localhost`. If you want to use a custom one, just launch the command with hostname option. Ex: `npm run dev -- --hostname="yourUrl"`');
-
-  hostname = hostname || 'localhost';
-
-  console.log('Live reload available on:', hostname);
-
-  let dev = Object.assign(common, { devtool: 'inline-source-map' });
-
-  dev.module.rules.push({
-    test:/\.(s*)css$/,
-    use: [
-      'style-loader',
-      'css-loader',
-      'postcss-loader',
-      'sass-loader'
-    ]
-  });
-
-  dev.plugins.push(new LiveReloadPlugin({
-    appendScriptTag: true,
-    hostname: hostname
-  }));
+  let dev = Object.assign(
+    common,
+    {
+      devtool: 'inline-source-map',
+    }
+  );
 
   return dev;
 }
