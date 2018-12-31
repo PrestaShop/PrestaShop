0b9bcb39c5a49abec511c203891bcd53e09cde28
Mathieu Ferment <mathieu.ferment@prestashop.com>
2018-12-31 14:02:42 +0100

    Fix tests by using ps_banner instead of ps_featuredproducts

--- tests-legacy/Integration/PrestaShopBundle/Controller/Api/Improve/Design/PositionsControllerTest.php
+++ tests-legacy/Integration/PrestaShopBundle/Controller/Api/Improve/Design/PositionsControllerTest.php
@@ -51,16 +51,17 @@ class PositionsControllerTest extends WebTestCase
 
         /** @var ModuleManager */
         $moduleManager = self::$kernel->getContainer()->get('prestashop.module.manager');
+
+        if (!$moduleManager->isInstalled('ps_banner')) {
+            $moduleManager->install('ps_banner');
+        }
+
         if (!$moduleManager->isInstalled('bankwire')) {
             $moduleManager->install('bankwire');
         }
 
-        if (!$moduleManager->isInstalled('ps_featuredproducts')) {
-            $moduleManager->install('ps_featuredproducts');
-        }
-
         $this->moduleId = $moduleManager->getModuleIdByName('bankwire');
-        $this->otherModuleId = $moduleManager->getModuleIdByName('ps_featuredproducts');
+        $this->otherModuleId = $moduleManager->getModuleIdByName('ps_banner');
         $this->hookId = Hook::getIdByName('displayHome');
     }
 
