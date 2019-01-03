e2652c77c3ed2df9b18c6b0a7c23daff3c98087c
Thomas Nabord <thomas.nabord@prestashop.com>
2019-01-03 11:34:09 +0100

    Update license of PHP files

--- tests/Selenium/commands/init.js
+++ tests/Selenium/commands/init.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 module.exports = function initializePrestaShopBrowserCommands(browser) {
 
   /**
- * 2007-2018 PrestaShop
+ * 2007-2019 PrestaShop and Contributors
  *
  * NOTICE OF LICENSE
  *
@@ -23,7 +23,7 @@ module.exports = function initializePrestaShopBrowserCommands(browser) {
  * needs please refer to http://www.prestashop.com for more information.
  *
  * @author    PrestaShop SA <contact@prestashop.com>
- * @copyright 2007-2018 PrestaShop SA
+ * @copyright 2007-2019 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
