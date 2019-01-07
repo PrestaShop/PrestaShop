0ad2074b32e2f8a3cce9aafcddf1ec1b91fe977d
Tomas Ilginis <tilginis@gmail.com>
2019-01-07 23:46:37 +0200

    modifies tests so they would work  with new iso code validatoer string passed

--- tests/Unit/Core/ConstraintValidator/IsUrlRewriteValidatorTest.php
+++ tests/Unit/Core/ConstraintValidator/IsUrlRewriteValidatorTest.php
@@ -74,7 +74,7 @@ class IsUrlRewriteValidatorTest extends ConstraintValidatorTestCase
         $this->validator->validate($incorrectRewriteUrl, new IsUrlRewrite());
 
         $this->buildViolation((new IsUrlRewrite())->message)
-            ->setParameter('%s', $incorrectRewriteUrl)
+            ->setParameter('%s', '"' . $incorrectRewriteUrl . '"')
             ->assertRaised()
         ;
     }
