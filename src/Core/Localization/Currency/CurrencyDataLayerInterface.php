68dd0064eb905f98fe2ed617bef14cffd2d57cc0
Thomas LEVIANDIER <thomas.leviandier@gmail.com>
2019-01-09 17:55:44 +0100

    fix interface

--- src/Core/Localization/Currency/CurrencyDataLayerInterface.php
+++ src/Core/Localization/Currency/CurrencyDataLayerInterface.php
@@ -56,7 +56,7 @@ interface CurrencyDataLayerInterface
      * @return CurrencyData
      *                      The currency data to be written by the upper data layer
      */
-    public function write($currencyCode, CurrencyData $currencyData);
+    public function write($currencyCode, $currencyData);
 
     /**
      * Set the lower layer.
