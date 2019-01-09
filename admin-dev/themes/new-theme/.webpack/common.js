d80c3617e58111204536259a2eef56ac91bb1024
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2019-01-09 20:45:49 +0200

    add "Go to previous page" functionality

--- admin-dev/themes/new-theme/.webpack/common.js
+++ admin-dev/themes/new-theme/.webpack/common.js
@@ -52,6 +52,7 @@ module.exports = {
     translation_settings: './js/pages/translation-settings',
     translations: './js/app/pages/translations',
     webservice: './js/pages/webservice',
+    error: './js/pages/error',
   },
   output: {
     path: path.resolve(__dirname, '../public'),
