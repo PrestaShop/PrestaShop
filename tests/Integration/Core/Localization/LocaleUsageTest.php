cd12ed079c8a1b36181aac00ef3abff13feea488
Thomas LEVIANDIER <thomas.leviandier@gmail.com>
2019-01-09 16:31:11 +0100

    cs-fixer

--- tests/Integration/Core/Localization/LocaleUsageTest.php
+++ tests/Integration/Core/Localization/LocaleUsageTest.php
@@ -36,7 +36,7 @@ use Tests\TestCase\SymfonyIntegrationTestCase;
 
 class LocaleUsageTest extends SymfonyIntegrationTestCase
 {
-    const SERVICE_LOCALE_REPOSITORY  = 'prestashop.core.localization.locale.repository';
+    const SERVICE_LOCALE_REPOSITORY = 'prestashop.core.localization.locale.repository';
     const SERVICE_CLDR_CACHE_ADAPTER = 'prestashop.core.localization.cldr.cache.adapter';
 
     /**
@@ -100,95 +100,95 @@ class LocaleUsageTest extends SymfonyIntegrationTestCase
     public function provideLocalizedNumbers()
     {
         return [
-            'United States'    => [
-                'localeCode'      => 'en-US',
-                'rawNumber'       => 1234568.12345,
+            'United States' => [
+                'localeCode' => 'en-US',
+                'rawNumber' => 1234568.12345,
                 'formattedNumber' => '1,234,568.123',
             ],
-            'Japan'            => [
-                'localeCode'      => 'ja-JP',
-                'rawNumber'       => 1234568.12345,
+            'Japan' => [
+                'localeCode' => 'ja-JP',
+                'rawNumber' => 1234568.12345,
                 'formattedNumber' => '1,234,568.123',
             ],
-            'United Kingdom'   => [
-                'localeCode'      => 'en-GB',
-                'rawNumber'       => 1234568.12345,
+            'United Kingdom' => [
+                'localeCode' => 'en-GB',
+                'rawNumber' => 1234568.12345,
                 'formattedNumber' => '1,234,568.123',
             ],
-            'Germany'          => [
-                'localeCode'      => 'de-DE',
-                'rawNumber'       => 1234568.12345,
+            'Germany' => [
+                'localeCode' => 'de-DE',
+                'rawNumber' => 1234568.12345,
                 'formattedNumber' => '1.234.568,123',
             ],
-            'France'           => [
-                'localeCode'      => 'fr-FR',
-                'rawNumber'       => 1234568.12345,
-                'formattedNumber' => '1 234 568,123',
+            'France' => [
+                'localeCode' => 'fr-FR',
+                'rawNumber' => 1234568.12345,
+                'formattedNumber' => '1 234 568,123',
             ],
-            'India (Hindi)'    => [
-                'localeCode'      => 'hi-IN',
-                'rawNumber'       => 1234568.12345,
+            'India (Hindi)' => [
+                'localeCode' => 'hi-IN',
+                'rawNumber' => 1234568.12345,
                 'formattedNumber' => '12,34,568.123',
             ],
-            'India (English)'  => [
-                'localeCode'      => 'en-IN',
-                'rawNumber'       => 1234568.12345,
+            'India (English)' => [
+                'localeCode' => 'en-IN',
+                'rawNumber' => 1234568.12345,
                 'formattedNumber' => '12,34,568.123',
             ],
-            'India (Bengali)'  => [
-                'localeCode'      => 'bn-IN',
-                'rawNumber'       => 1234568.12345,
+            'India (Bengali)' => [
+                'localeCode' => 'bn-IN',
+                'rawNumber' => 1234568.12345,
                 'formattedNumber' => '12,34,568.123',
             ],
-            'Spain'            => [
-                'localeCode'      => 'es-ES',
-                'rawNumber'       => 1234568.12345,
+            'Spain' => [
+                'localeCode' => 'es-ES',
+                'rawNumber' => 1234568.12345,
                 'formattedNumber' => '1.234.568,123',
             ],
-            'Canada (French)'  => [
-                'localeCode'      => 'fr-CA',
-                'rawNumber'       => 1234568.12345,
-                'formattedNumber' => '1 234 568,123',
+            'Canada (French)' => [
+                'localeCode' => 'fr-CA',
+                'rawNumber' => 1234568.12345,
+                'formattedNumber' => '1 234 568,123',
             ],
             'Canada (English)' => [
-                'localeCode'      => 'en-CA',
-                'rawNumber'       => 1234568.12345,
+                'localeCode' => 'en-CA',
+                'rawNumber' => 1234568.12345,
                 'formattedNumber' => '1,234,568.123',
             ],
-            'China'            => [
-                'localeCode'      => 'zh-CN',
-                'rawNumber'       => 1234568.12345,
+            'China' => [
+                'localeCode' => 'zh-CN',
+                'rawNumber' => 1234568.12345,
                 'formattedNumber' => '1,234,568.123',
             ],
-            'Australia'        => [
-                'localeCode'      => 'en-AU',
-                'rawNumber'       => 1234568.12345,
+            'Australia' => [
+                'localeCode' => 'en-AU',
+                'rawNumber' => 1234568.12345,
                 'formattedNumber' => '1,234,568.123',
             ],
-            'Brazil'           => [
-                'localeCode'      => 'pt-BR',
-                'rawNumber'       => 1234568.12345,
+            'Brazil' => [
+                'localeCode' => 'pt-BR',
+                'rawNumber' => 1234568.12345,
                 'formattedNumber' => '1.234.568,123',
             ],
-            'Mexico'           => [
-                'localeCode'      => 'es-MX',
-                'rawNumber'       => 1234568.12345,
+            'Mexico' => [
+                'localeCode' => 'es-MX',
+                'rawNumber' => 1234568.12345,
                 'formattedNumber' => '1,234,568.123',
             ],
-            'Russia'           => [
-                'localeCode'      => 'ru-RU',
-                'rawNumber'       => 1234568.12345,
-                'formattedNumber' => '1 234 568,123',
+            'Russia' => [
+                'localeCode' => 'ru-RU',
+                'rawNumber' => 1234568.12345,
+                'formattedNumber' => '1 234 568,123',
             ],
-            'Italy'            => [
-                'localeCode'      => 'it-IT',
-                'rawNumber'       => 1234568.12345,
+            'Italy' => [
+                'localeCode' => 'it-IT',
+                'rawNumber' => 1234568.12345,
                 'formattedNumber' => '1.234.568,123',
             ],
-            'Poland'           => [
-                'localeCode'      => 'pl-PL',
-                'rawNumber'       => 1234568.12345,
-                'formattedNumber' => '1 234 568,123',
+            'Poland' => [
+                'localeCode' => 'pl-PL',
+                'rawNumber' => 1234568.12345,
+                'formattedNumber' => '1 234 568,123',
             ],
         ];
     }
@@ -229,127 +229,127 @@ class LocaleUsageTest extends SymfonyIntegrationTestCase
             'it',
             'pl',
         ];
-        $cacheDir  = _PS_CACHE_DIR_ . 'sandbox' . DIRECTORY_SEPARATOR;
+        $cacheDir = _PS_CACHE_DIR_ . 'sandbox' . DIRECTORY_SEPARATOR;
 
         foreach ($countries as $country) {
             $xmlContent = (new LocalizationWarmer(_PS_VERSION_, $country))
                 ->warmUp($cacheDir);
 
-            (new LocalizationPack)->loadLocalisationPack($xmlContent, false, true);
+            (new LocalizationPack())->loadLocalisationPack($xmlContent, false, true);
         }
     }
 
     public function provideFormattedPrices()
     {
         return [
-            'United States'    => [
-                'localeCode'      => 'en-US',
-                'rawNumber'       => 1234568.12345,
-                'currencyCode'    => 'USD',
+            'United States' => [
+                'localeCode' => 'en-US',
+                'rawNumber' => 1234568.12345,
+                'currencyCode' => 'USD',
                 'formattedPrice' => '$1,234,568.12',
             ],
-            'Japan'            => [
-                'localeCode'      => 'ja-JP',
-                'rawNumber'       => 1234568.12345,
-                'currencyCode'    => 'JPY',
+            'Japan' => [
+                'localeCode' => 'ja-JP',
+                'rawNumber' => 1234568.12345,
+                'currencyCode' => 'JPY',
                 'formattedPrice' => '¥1,234,568',
             ],
             'United Kingdom' => [
-                'localeCode'     => 'en-GB',
-                'rawNumber'      => 1234568.12345,
-                'currencyCode'   => 'GBP',
+                'localeCode' => 'en-GB',
+                'rawNumber' => 1234568.12345,
+                'currencyCode' => 'GBP',
                 'formattedPrice' => '£1,234,568.12',
             ],
-            'Germany'          => [
-                'localeCode'      => 'de-DE',
-                'rawNumber'       => 1234568.12345,
-                'currencyCode'    => 'EUR',
-                'formattedPrice' => '1.234.568,12 €',
+            'Germany' => [
+                'localeCode' => 'de-DE',
+                'rawNumber' => 1234568.12345,
+                'currencyCode' => 'EUR',
+                'formattedPrice' => '1.234.568,12 €',
             ],
-            'France'           => [
-                'localeCode'      => 'fr-FR',
-                'rawNumber'       => 1234568.12345,
-                'currencyCode'    => 'EUR',
-                'formattedPrice' => '1 234 568,12 €',
+            'France' => [
+                'localeCode' => 'fr-FR',
+                'rawNumber' => 1234568.12345,
+                'currencyCode' => 'EUR',
+                'formattedPrice' => '1 234 568,12 €',
             ],
-            'India (Hindi)'    => [
-                'localeCode'      => 'hi-IN',
-                'rawNumber'       => 1234568.12345,
-                'currencyCode'    => 'INR',
+            'India (Hindi)' => [
+                'localeCode' => 'hi-IN',
+                'rawNumber' => 1234568.12345,
+                'currencyCode' => 'INR',
                 'formattedPrice' => '₹12,34,568.12',
             ],
-            'India (English)'  => [
-                'localeCode'      => 'en-IN',
-                'rawNumber'       => 1234568.12345,
-                'currencyCode'    => 'INR',
-                'formattedPrice' => '₹ 12,34,568.12',
+            'India (English)' => [
+                'localeCode' => 'en-IN',
+                'rawNumber' => 1234568.12345,
+                'currencyCode' => 'INR',
+                'formattedPrice' => '₹ 12,34,568.12',
             ],
-            'India (Bengali)'  => [
-                'localeCode'      => 'bn-IN',
-                'rawNumber'       => 1234568.12345,
-                'currencyCode'    => 'INR',
+            'India (Bengali)' => [
+                'localeCode' => 'bn-IN',
+                'rawNumber' => 1234568.12345,
+                'currencyCode' => 'INR',
                 'formattedPrice' => '12,34,568.12₹',
             ],
-            'Spain'            => [
-                'localeCode'      => 'es-ES',
-                'rawNumber'       => 1234568.12345,
-                'currencyCode'    => 'EUR',
-                'formattedPrice' => '1.234.568,12 €',
+            'Spain' => [
+                'localeCode' => 'es-ES',
+                'rawNumber' => 1234568.12345,
+                'currencyCode' => 'EUR',
+                'formattedPrice' => '1.234.568,12 €',
             ],
-            'Canada (French)'  => [
-                'localeCode'      => 'fr-CA',
-                'rawNumber'       => 1234568.12345,
-                'currencyCode'    => 'CAD',
-                'formattedPrice' => '1 234 568,12 $',
+            'Canada (French)' => [
+                'localeCode' => 'fr-CA',
+                'rawNumber' => 1234568.12345,
+                'currencyCode' => 'CAD',
+                'formattedPrice' => '1 234 568,12 $',
             ],
             'Canada (English)' => [
-                'localeCode'      => 'en-CA',
-                'rawNumber'       => 1234568.12345,
-                'currencyCode'    => 'CAD',
+                'localeCode' => 'en-CA',
+                'rawNumber' => 1234568.12345,
+                'currencyCode' => 'CAD',
                 'formattedPrice' => '$1,234,568.12',
             ],
-            'China'            => [
-                'localeCode'      => 'zh-CN',
-                'rawNumber'       => 1234568.12345,
-                'currencyCode'    => 'CNY',
+            'China' => [
+                'localeCode' => 'zh-CN',
+                'rawNumber' => 1234568.12345,
+                'currencyCode' => 'CNY',
                 'formattedPrice' => '¥1,234,568.12',
             ],
-            'Australia'        => [
-                'localeCode'      => 'en-AU',
-                'rawNumber'       => 1234568.12345,
-                'currencyCode'    => 'AUD',
+            'Australia' => [
+                'localeCode' => 'en-AU',
+                'rawNumber' => 1234568.12345,
+                'currencyCode' => 'AUD',
                 'formattedPrice' => '$1,234,568.12',
             ],
-            'Brazil'           => [
-                'localeCode'      => 'pt-BR',
-                'rawNumber'       => 1234568.12345,
-                'currencyCode'    => 'BRL',
-                'formattedPrice' => 'R$ 1.234.568,12',
+            'Brazil' => [
+                'localeCode' => 'pt-BR',
+                'rawNumber' => 1234568.12345,
+                'currencyCode' => 'BRL',
+                'formattedPrice' => 'R$ 1.234.568,12',
             ],
-            'Mexico'           => [
-                'localeCode'      => 'es-MX',
-                'rawNumber'       => 1234568.12345,
-                'currencyCode'    => 'MXN',
+            'Mexico' => [
+                'localeCode' => 'es-MX',
+                'rawNumber' => 1234568.12345,
+                'currencyCode' => 'MXN',
                 'formattedPrice' => '$1,234,568.12',
             ],
-            'Russia'           => [
-                'localeCode'      => 'ru-RU',
-                'rawNumber'       => 1234568.12345,
-                'currencyCode'    => 'RUB',
-                'formattedPrice' => '1 234 568,12 RUB', // Expectation with legacy ICanBoogie system
-                // 'formattedPrice' => '1 234 568,12 ₽', // Expectation with NEW CLDR system
+            'Russia' => [
+                'localeCode' => 'ru-RU',
+                'rawNumber' => 1234568.12345,
+                'currencyCode' => 'RUB',
+                'formattedPrice' => '1 234 568,12 RUB', // Expectation with legacy ICanBoogie system
+                // 'formattedPrice' => '1 234 568,12 ₽', // Expectation with NEW CLDR system
             ],
-            'Italy'            => [
-                'localeCode'      => 'it-IT',
-                'rawNumber'       => 1234568.12345,
-                'currencyCode'    => 'EUR',
-                'formattedPrice' => '1.234.568,12 €',
+            'Italy' => [
+                'localeCode' => 'it-IT',
+                'rawNumber' => 1234568.12345,
+                'currencyCode' => 'EUR',
+                'formattedPrice' => '1.234.568,12 €',
             ],
-            'Poland'           => [
-                'localeCode'      => 'pl-PL',
-                'rawNumber'       => 1234568.12345,
-                'currencyCode'    => 'PLN',
-                'formattedPrice' => '1 234 568,12 zł',
+            'Poland' => [
+                'localeCode' => 'pl-PL',
+                'rawNumber' => 1234568.12345,
+                'currencyCode' => 'PLN',
+                'formattedPrice' => '1 234 568,12 zł',
             ],
         ];
     }
