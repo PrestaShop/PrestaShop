e731426ec7f18848836e0e94774990e5cfbdcfe6
Tomas Ilginis <tilginis@gmail.com>
2019-01-15 15:38:56 +0200

    displays contact page multi-lang fields and its functionality, adds contact query handler, adds contact id as value object with its validation

--- src/Core/Domain/Contact/Exception/ContactNotFoundException.php
+++ src/Core/Domain/Contact/Exception/ContactNotFoundException.php
@@ -1,5 +1,6 @@
-{#**
- * 2007-2018 PrestaShop
+<?php
+/**
+ * 2007-2018 PrestaShop.
  *
  * NOTICE OF LICENSE
  *
@@ -21,20 +22,13 @@
  * @copyright 2007-2018 PrestaShop SA
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
- *#}
+ */
 
-{% extends 'PrestaShopBundle:Admin:layout.html.twig' %}
+namespace PrestaShop\PrestaShop\Core\Domain\Contact\Exception;
 
-{% block content %}
-  <div class="row justify-content-center">
-    <div class="col">
-      {% include '@PrestaShop/Admin/Configure/ShopParameters/Contact/Contacts/Blocks/form.html.twig' %}
-    </div>
-  </div>
-{% endblock %}
-
-{% block javascripts %}
-  {{ parent() }}
-
-  <script src="{{ asset('themes/new-theme/public/contacts.bundle.js') }}"></script>
-{% endblock %}
+/**
+ * Class ContactNotFoundException
+ */
+class ContactNotFoundException extends ContactException
+{
+}
--- src/Core/Domain/Contact/QueryHandler/GetContactForEditingHandlerInterface.php
+++ src/Core/Domain/Contact/QueryHandler/GetContactForEditingHandlerInterface.php
@@ -1,5 +1,6 @@
-{#**
- * 2007-2018 PrestaShop
+<?php
+/**
+ * 2007-2018 PrestaShop.
  *
  * NOTICE OF LICENSE
  *
@@ -21,20 +22,22 @@
  * @copyright 2007-2018 PrestaShop SA
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
- *#}
+ */
 
-{% extends 'PrestaShopBundle:Admin:layout.html.twig' %}
+namespace PrestaShop\PrestaShop\Core\Domain\Contact\QueryHandler;
 
-{% block content %}
-  <div class="row justify-content-center">
-    <div class="col">
-      {% include '@PrestaShop/Admin/Configure/ShopParameters/Contact/Contacts/Blocks/form.html.twig' %}
-    </div>
-  </div>
-{% endblock %}
+use PrestaShop\PrestaShop\Core\Domain\Contact\DTO\EditableContact;
+use PrestaShop\PrestaShop\Core\Domain\Contact\Query\GetContactForEditing;
 
-{% block javascripts %}
-  {{ parent() }}
-
-  <script src="{{ asset('themes/new-theme/public/contacts.bundle.js') }}"></script>
-{% endblock %}
+/**
+ * Interface GetContactForEditingHandlerInterface defines contract for GetContactForEditingHandler
+ */
+interface GetContactForEditingHandlerInterface
+{
+    /**
+     * @param GetContactForEditing $query
+     *
+     * @return EditableContact
+     */
+    public function handle(GetContactForEditing $query);
+}
