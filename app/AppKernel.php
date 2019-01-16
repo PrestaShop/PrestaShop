30c8a73b6832741ca9d0b2ce6025c6e2f4a4e0d4
Jonathan Lelievre <jo.lelievre@gmail.com>
2019-01-16 02:48:33 +0100

    Revert PS version, finish integrating MailThemeCollectionInterface

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -32,7 +32,7 @@ use Symfony\Component\HttpKernel\Kernel;
 
 class AppKernel extends Kernel
 {
-    const VERSION = '1.7.6.0';
+    const VERSION = '1.7.5.0';
     const MAJOR_VERSION_STRING = '1.7';
     const MAJOR_VERSION = 17;
     const MINOR_VERSION = 5;
