433f0fec932cf06f2f868ebf6684b1450da990b0
Rokas Zygmantas <rokas.zygmantas@invertus.eu>
2019-01-18 15:49:58 +0200

    WIP employee form

---
+++ src/Core/Domain/Employee/QueryResult/EditableEmployee.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * 2007-2018 PrestaShop
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2018 PrestaShop SA
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+namespace PrestaShop\PrestaShop\Core\Domain\Employee\QueryResult;
+
+use PrestaShop\PrestaShop\Core\Domain\Employee\ValueObject\EmployeeId;
+use PrestaShop\PrestaShop\Core\Domain\Employee\ValueObject\FirstName;
+use PrestaShop\PrestaShop\Core\Domain\Employee\ValueObject\LastName;
+
+/**
+ * Stores editable data of an employee.
+ */
+class EditableEmployee
+{
+    /**
+     * @var EmployeeId
+     */
+    private $employeeId;
+
+    /**
+     * @var FirstName
+     */
+    private $firstName;
+
+    /**
+     * @var LastName
+     */
+    private $lastName;
+
+    /**
+     * @param EmployeeId $employeeId
+     * @param FirstName $firstName
+     * @param LastName $lastName
+     */
+    public function __construct(
+        EmployeeId $employeeId,
+        FirstName $firstName,
+        LastName $lastName
+    ) {
+        $this->employeeId = $employeeId;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return EmployeeId
+     */
+    public function getEmployeeId()
+    {
+        return $this->employeeId;
+    }
+
+    /**
+     * @return FirstName
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @return LastName
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+}
