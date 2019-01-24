3fd345742e4eb21787d708ee4198600de97bcd63
Rokas <rokas.zygmantas@gmail.com>
2019-01-24 18:54:58 +0200

    WIP employee form fields

---
+++ src/PrestaShopBundle/Form/Admin/Type/ClickableAvatarType.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * 2007-2018 PrestaShop.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2018 PrestaShop SA
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+namespace PrestaShopBundle\Form\Admin\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Class ClickableAvatarType displays an avatar image with a clickable link.
+ */
+class ClickableAvatarType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        $view->vars['click_url'] = $options['click_url'];
+        $view->vars['image_url'] = $options['image_url'];
+        $view->vars['image_alt'] = $options['image_alt'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver
+            ->setDefaults([
+                'click_url' => 'http://www.prestashop.com/forums/index.php?app=core&module=usercp',
+                'image_url' => '',
+                'image_alt' => '',
+            ])
+            ->setAllowedTypes('click_url', 'string')
+            ->setAllowedTypes('image_url', 'string')
+            ->setAllowedTypes('image_alt', 'string')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'clickable_avatar';
+    }
+}
