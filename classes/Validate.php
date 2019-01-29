1788c6d026701bb8b1bece8cee2e87f1d0d53381
Samuel <samuel@148.fr>
2019-01-29 17:39:35 +0100

    Revert "Use DateTime instead of regex for Birth date validation"
    This reverts commit b0b885530ca49dde421a2d340a9b6e8fb6ad7001.

--- classes/Validate.php
+++ classes/Validate.php
@@ -601,12 +601,11 @@ class ValidateCore
         if (empty($date) || $date == '0000-00-00') {
             return true;
         }
-        $dateTime = \DateTime::createFromFormat('Y-m-d', $date);
-        $dateTime->setTime(0, 0);
-        if (false === $dateTime) {
-            return false;
+        if (preg_match('/^([0-9]{4})-((?:0?[1-9])|(?:1[0-2]))-((?:0?[1-9])|(?:[1-2][0-9])|(?:3[01]))([0-9]{2}:[0-9]{2}:[0-9]{2})?$/', $date, $birth_date)) {
+            return strtotime($date) < time();
         }
-        return $dateTime->getTimestamp() < time();
+
+        return false;
     }
 
     /**
