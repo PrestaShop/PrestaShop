fa3bd1d39b035f541fb93c54916d8a19706deff4
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2019-01-30 16:43:51 +0200

    update permissions data retrieving

--- src/Core/Domain/Profile/Exception/ProfileConstraintException.php
+++ src/Core/Domain/Profile/Exception/ProfileConstraintException.php
@@ -1,12 +1,20 @@
 <?php
 /**
+<<<<<<< HEAD
  * Copyright since 2007 PrestaShop SA and Contributors
  * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+=======
+ * 2007-2018 PrestaShop
+>>>>>>> update permissions data retrieving
  *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Open Software License (OSL 3.0)
+<<<<<<< HEAD
  * that is bundled with this package in the file LICENSE.md.
+=======
+ * that is bundled with this package in the file LICENSE.txt.
+>>>>>>> update permissions data retrieving
  * It is also available through the world-wide-web at this URL:
  * https://opensource.org/licenses/OSL-3.0
  * If you did not receive a copy of the license and are unable to
@@ -17,15 +25,25 @@
  *
  * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
  * versions in the future. If you wish to customize PrestaShop for your
+<<<<<<< HEAD
  * needs please refer to https://devdocs.prestashop.com/ for more information.
  *
  * @author    PrestaShop SA and Contributors <contact@prestashop.com>
  * @copyright Since 2007 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+=======
+ * needs please refer to http://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2018 PrestaShop SA
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+>>>>>>> update permissions data retrieving
  */
 
 namespace PrestaShop\PrestaShop\Core\Domain\Profile\Exception;
 
+<<<<<<< HEAD
 /**
  * Is thrown when some constraint is violated in Profile subdomain
  */
@@ -35,4 +53,8 @@ class ProfileConstraintException extends ProfileException
      * @var string Code is used when invalid profile name is encountered
      */
     const INVALID_NAME = 1;
+=======
+class ProfileConstraintException extends ProfileException
+{
+>>>>>>> update permissions data retrieving
 }
