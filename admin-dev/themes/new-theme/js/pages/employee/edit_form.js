bca634efb54dae9891e99b03d781871c1b78d200
Rokas Zygmantas <rokas.zygmantas@invertus.eu>
2019-01-31 15:49:51 +0200

    Add employee form JS

--- admin-dev/themes/new-theme/js/pages/employee/edit_form.js
+++ admin-dev/themes/new-theme/js/pages/employee/edit_form.js
@@ -1,5 +1,5 @@
-{#**
- * 2007-2018 PrestaShop
+/**
+ * 2007-2019 PrestaShop and Contributors
  *
  * NOTICE OF LICENSE
  *
@@ -15,36 +15,15 @@
  *
  * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
  * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to http://www.prestashop.com for more information.
+ * needs please refer to https://www.prestashop.com for more information.
  *
  * @author    PrestaShop SA <contact@prestashop.com>
- * @copyright 2007-2018 PrestaShop SA
+ * @copyright 2007-2019 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
- *#}
+ */
+import ChoiceTree from "../../components/form/choice-tree";
 
-{% set enableSidebar = true %}
-
-{% extends '@PrestaShop/Admin/layout.html.twig' %}
-
-{% block content %}
-  {% if employeeForm.vars.errors is not empty %}
-    <div class="alert alert-danger">
-      {% for error in employeeForm.vars.errors %}
-        <div class="alert-text">{{ error.message }}</div>
-      {% endfor %}
-    </div>
-  {% endif %}
-
-  <div class="row justify-content-center">
-    <div class="col">
-      {% include '@PrestaShop/Admin/Configure/AdvancedParameters/Employee/Blocks/form.html.twig' %}
-    </div>
-  </div>
-{% endblock %}
-
-{% block javascripts %}
-  {{ parent() }}
-
-  <script src="{{ asset('themes/new-theme/public/employee_form.bundle.js') }}"></script>
-{% endblock %}
+$(() => {
+  new ChoiceTree('#employee_shop_association').enableAutoCheckChildren();
+});
