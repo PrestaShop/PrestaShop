8eedc0fcf0e37ced7ae1e9dc6ffdbfa296e67f64
Jonathan Lelievre <jo.lelievre@gmail.com>
2019-01-31 17:51:56 +0100

    Build assets for product preferences page

--- admin-dev/themes/new-theme/public/product_preferences.bundle.js
+++ admin-dev/themes/new-theme/public/product_preferences.bundle.js
@@ -1,5 +1,5 @@
 /******/!function(n){// webpackBootstrap
-/******/var o={};function r(e){if(o[e])return o[e].exports;var t=o[e]={i:e,l:!1,exports:{}};return n[e].call(t.exports,t,t.exports,r),t.l=!0,t.exports}r.m=n,r.c=o,r.d=function(e,t,n){r.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},r.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r.t=function(t,e){if(1&e&&(t=r(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var n=Object.create(null);if(r.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var o in t)r.d(n,o,function(e){return t[e]}.bind(null,o));return n},r.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return r.d(t,"a",t),t},r.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},r.p="",r(r.s=273)}({22:function(e,t,n){"use strict";function r(e,t){for(var n=0;n<t.length;n++){var o=t[n];o.enumerable=o.enumerable||!1,o.configurable=!0,"value"in o&&(o.writable=!0),Object.defineProperty(e,o.key,o)}}
+/******/var o={};function a(e){if(o[e])return o[e].exports;var t=o[e]={i:e,l:!1,exports:{}};return n[e].call(t.exports,t,t.exports,a),t.l=!0,t.exports}a.m=n,a.c=o,a.d=function(e,t,n){a.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},a.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},a.t=function(t,e){if(1&e&&(t=a(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var n=Object.create(null);if(a.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var o in t)a.d(n,o,function(e){return t[e]}.bind(null,o));return n},a.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return a.d(t,"a",t),t},a.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},a.p="",a(a.s=268)}({21:function(e,t,n){"use strict";function a(e,t){for(var n=0;n<t.length;n++){var o=t[n];o.enumerable=o.enumerable||!1,o.configurable=!0,"value"in o&&(o.writable=!0),Object.defineProperty(e,o.key,o)}}
 /**
  * 2007-2019 PrestaShop and Contributors
  *
@@ -24,7 +24,7 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
-var a=window.$,o=function(){function t(e){!function(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}(this,t),e=e||{},this.localeItemSelector=e.localeItemSelector||".js-locale-item",this.localeButtonSelector=e.localeButtonSelector||".js-locale-btn",this.localeInputSelector=e.localeInputSelector||".js-locale-input",a("body").on("click",this.localeItemSelector,this.toggleInputs.bind(this))}var e,n,o;return e=t,(n=[{key:"toggleInputs",value:function(e){var t=a(e.target),n=t.closest("form"),o=t.data("locale");n.find(this.localeButtonSelector).text(o),n.find(this.localeInputSelector).addClass("d-none"),n.find(this.localeInputSelector+".js-locale-"+o).removeClass("d-none")}}])&&r(e.prototype,n),o&&r(e,o),t}();t.a=o},273:function(e,t,n){"use strict";n.r(t);var o=n(22);function r(e,t){for(var n=0;n<t.length;n++){var o=t[n];o.enumerable=o.enumerable||!1,o.configurable=!0,"value"in o&&(o.writable=!0),Object.defineProperty(e,o.key,o)}}
+var r=window.$,o=function(){function t(e){!function(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}(this,t),e=e||{},this.localeItemSelector=e.localeItemSelector||".js-locale-item",this.localeButtonSelector=e.localeButtonSelector||".js-locale-btn",this.localeInputSelector=e.localeInputSelector||".js-locale-input",r("body").on("click",this.localeItemSelector,this.toggleInputs.bind(this))}var e,n,o;return e=t,(n=[{key:"toggleInputs",value:function(e){var t=r(e.target),n=t.closest("form"),o=t.data("locale");n.find(this.localeButtonSelector).text(o),n.find(this.localeInputSelector).addClass("d-none"),n.find(this.localeInputSelector+".js-locale-"+o).removeClass("d-none")}}])&&a(e.prototype,n),o&&a(e,o),t}();t.a=o},268:function(e,t,n){"use strict";n.r(t);var o=n(21);function a(e,t){for(var n=0;n<t.length;n++){var o=t[n];o.enumerable=o.enumerable||!1,o.configurable=!0,"value"in o&&(o.writable=!0),Object.defineProperty(e,o.key,o)}}
 /**
  * 2007-2019 PrestaShop and Contributors
  *
@@ -49,4 +49,29 @@ var a=window.$,o=function(){function t(e){!function(e,t){if(!(e instanceof t))th
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
-var a=window.$,l=function(){function t(){var e=this;!function(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}(this,t),this.handle(),a('input[name="form[stock][stock_management]"]').on("change",function(){return e.handle()})}var e,n,o;return e=t,(n=[{key:"handle",value:function(){var e=a('input[name="form[stock][stock_management]"]:checked').val(),t=parseInt(e);this.handleAllowOrderingOutOfStockOption(t),this.handleDisplayAvailableQuantitiesOption(t)}},{key:"handleAllowOrderingOutOfStockOption",value:function(e){var t=a('input[name="form[stock][allow_ordering_oos]"]');e?t.removeAttr("disabled"):(t.val([1]),t.attr("disabled","disabled"))}},{key:"handleDisplayAvailableQuantitiesOption",value:function(e){var t=a('input[name="form[page][display_quantities]"]');e?t.removeAttr("disabled"):(t.val([0]),t.attr("disabled","disabled"))}}])&&r(e.prototype,n),o&&r(e,o),t}();(0,window.$)(function(){new o.a,new l})}});
+var r=window.$,l=function(){function t(){var e=this;!function(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}(this,t),this.handle(),r('input[name="form[stock][stock_management]"]').on("change",function(){return e.handle()})}var e,n,o;return e=t,(n=[{key:"handle",value:function(){var e=r('input[name="form[stock][stock_management]"]:checked').val(),t=parseInt(e);this.handleAllowOrderingOutOfStockOption(t),this.handleDisplayAvailableQuantitiesOption(t)}},{key:"handleAllowOrderingOutOfStockOption",value:function(e){var t=r('input[name="form[stock][allow_ordering_oos]"]');e?t.removeAttr("disabled"):(t.val([1]),t.attr("disabled","disabled"))}},{key:"handleDisplayAvailableQuantitiesOption",value:function(e){var t=r('input[name="form[page][display_quantities]"]');e?t.removeAttr("disabled"):(t.val([0]),t.attr("disabled","disabled"))}}])&&a(e.prototype,n),o&&a(e,o),t}();function i(e,t){for(var n=0;n<t.length;n++){var o=t[n];o.enumerable=o.enumerable||!1,o.configurable=!0,"value"in o&&(o.writable=!0),Object.defineProperty(e,o.key,o)}}
+/**
+ * 2007-2019 PrestaShop and Contributors
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2019 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+var c=window.$,u=function(){function t(){var e=this;!function(e,t){if(!(e instanceof t))throw new TypeError("Cannot call a class as a function")}(this,t),this.handle(0),c('input[name="form[general][catalog_mode]"]').on("change",function(){return e.handle(600)})}var e,n,o;return e=t,(n=[{key:"handle",value:function(e){var t=c('input[name="form[general][catalog_mode]"]:checked').val(),n=parseInt(t),o=c(".catalog-mode-option");n?o.show(e):o.hide(e/2)}}])&&i(e.prototype,n),o&&i(e,o),t}();(0,window.$)(function(){new o.a,new l,new u})}});
