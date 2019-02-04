b5d287e9a8b2060bba82342f2a574d70d1bf2edc
Mathieu Ferment <mathieu.ferment@prestashop.com>
2019-02-04 12:04:48 +0100

    Fix lint for tests/Integration/Survival/bootstrap-survival.php

--- tests/Integration/Survival/bootstrap-survival.php
+++ tests/Integration/Survival/bootstrap-survival.php
@@ -27,9 +27,8 @@
 /**
  * This bootstrap file is dedicated to start a PrestaShop running shop and use its back-office
  */
-
 define('_PS_ROOT_DIR_', __DIR__ . '/../../..');
-define('_PS_MODULE_DIR_', _PS_ROOT_DIR_.'/tests-legacy/resources/modules/');
-require_once dirname(__FILE__).'/../../../admin-dev/bootstrap.php';
-require_once _PS_CONFIG_DIR_.'autoload.php';
-require_once dirname(__FILE__).'/../../../config/bootstrap.php';
+define('_PS_MODULE_DIR_', _PS_ROOT_DIR_ . '/tests-legacy/resources/modules/');
+require_once dirname(__FILE__) . '/../../../admin-dev/bootstrap.php';
+require_once _PS_CONFIG_DIR_ . 'autoload.php';
+require_once dirname(__FILE__) . '/../../../config/bootstrap.php';
