07dcd7e02731009c94467addc41af51cc27f742e
Julius Zukauskas <julius.zukauskas@invertus.eu>
2019-02-04 13:09:25 +0200

    Added placeholders to taxes list filters

--- src/Core/Grid/Definition/Factory/TaxGridDefinitionFactory.php
+++ src/Core/Grid/Definition/Factory/TaxGridDefinitionFactory.php
@@ -1,10 +1,6 @@
 <?php
 /**
-<<<<<<< HEAD
  * 2007-2019 PrestaShop and Contributors
-=======
- * 2007-2018 PrestaShop.
->>>>>>> 0be34fb913... Migrated taxes list
  *
  * NOTICE OF LICENSE
  *
@@ -20,15 +16,10 @@
  *
  * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
  * versions in the future. If you wish to customize PrestaShop for your
-<<<<<<< HEAD
  * needs please refer to https://www.prestashop.com for more information.
  *
  * @author    PrestaShop SA <contact@prestashop.com>
  * @copyright 2007-2019 PrestaShop SA and Contributors
-=======
- * needs please refer to http://www.prestashop.com for more information.
- * @author    PrestaShop SA <contact@prestashop.com>
- * @copyright 2007-2018 PrestaShop SA
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
