1e761ebaff2950b93aabd2ff0822ceb05ddcab83
Rolige eCommerce Solutions <info@rolige.com>
2019-02-05 12:04:43 -0600

    Fix carrier choice in the cart rule controller

--- controllers/admin/AdminCartRulesController.php
+++ controllers/admin/AdminCartRulesController.php
@@ -640,9 +640,10 @@ class AdminCartRulesControllerCore extends AdminController
         $groups = $current_object->getAssociatedRestrictions('group', false, true);
         $shops = $current_object->getAssociatedRestrictions('shop', false, false);
         $cart_rules = $current_object->getAssociatedRestrictions('cart_rule', false, true, 0, $limit);
-        $carriers = $current_object->getAssociatedRestrictions('carrier', true, false);
+        $carriers = $current_object->getAssociatedRestrictions('carrier', true, true);
         foreach ($carriers as &$carriers2) {
             foreach ($carriers2 as &$carrier) {
+                $carrier['name'] = $carrier['id_carrier'].' - '.$carrier['name'].' ('.$carrier['delay'].')';
                 foreach ($carrier as $field => &$value) {
                     if ($field == 'name' && $value == '0') {
                         $value = Configuration::get('PS_SHOP_NAME');
