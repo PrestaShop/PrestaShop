1a76d2e6f805fd36c8eff950173c4134d00a094e
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2019-02-08 10:10:12 +0200

    remove unused class

--- src/PrestaShopBundle/Controller/Admin/Configure/ShopParameters/ContactsController.php
+++ src/PrestaShopBundle/Controller/Admin/Configure/ShopParameters/ContactsController.php
@@ -30,7 +30,6 @@ use PrestaShop\PrestaShop\Core\Search\Filters\ContactFilters;
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
 use PrestaShopBundle\Security\Annotation\AdminSecurity;
 use PrestaShopBundle\Security\Annotation\DemoRestricted;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
