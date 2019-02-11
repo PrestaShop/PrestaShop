90faeff7cf3e96a6fa8280b8c64c363d21066b3d
Jonathan Lelievre <jo.lelievre@gmail.com>
2019-02-11 14:54:20 +0100

    Update tests scripts messages

--- tests/check_integration.sh
+++ tests/check_integration.sh
@@ -4,8 +4,8 @@ composer run-script integration-tests --timeout=0;
 INTEGRATION=$?
 
 if [[ "$INTEGRATION" == "0" ]]; then
-  echo -e "\e[INTEGRATION TESTS OK"
+  echo -e "\e[92mINTEGRATION TESTS OK\e[0m"
 else
-  echo -e "\e[91mINTEGRATION TESTS FAILED"
+  echo -e "\e[91mINTEGRATION TESTS FAILED\e[0m"
 fi
 exit $INTEGRATION;
