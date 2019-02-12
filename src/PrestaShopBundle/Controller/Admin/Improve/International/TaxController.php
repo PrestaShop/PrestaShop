dfc00e98e5cf7a7a6e9f2573b5e1ec254b919585
Julius Zukauskas <julius.zukauskas@invertus.eu>
2019-02-12 18:24:06 +0200

    Minor fixes regarding commenting and code optimization

--- src/PrestaShopBundle/Controller/Admin/Improve/International/TaxController.php
+++ src/PrestaShopBundle/Controller/Admin/Improve/International/TaxController.php
@@ -33,7 +33,6 @@ use PrestaShop\PrestaShop\Core\Domain\Tax\Command\BulkToggleTaxStatusCommand;
 use PrestaShop\PrestaShop\Core\Domain\Tax\Command\DeleteTaxCommand;
 use PrestaShop\PrestaShop\Core\Domain\Tax\Command\ToggleTaxStatusCommand;
 use PrestaShop\PrestaShop\Core\Domain\Tax\Exception\DeleteTaxException;
-use PrestaShop\PrestaShop\Core\Domain\Tax\Exception\TaxException;
 use PrestaShop\PrestaShop\Core\Domain\Tax\Exception\TaxNotFoundException;
 use PrestaShop\PrestaShop\Core\Domain\Tax\Exception\UpdateTaxException;
 use PrestaShop\PrestaShop\Core\Domain\Tax\Query\GetTaxForEditing;
@@ -169,7 +168,7 @@ class TaxController extends FrameworkBundleAdminController
                 'success',
                 $this->trans('Successful deletion.', 'Admin.Notifications.Success')
             );
-        } catch (TaxException $e) {
+        } catch (DomainException $e) {
             $this->addFlash('error', $this->getErrorMessageForException($e, $this->getErrorMessages()));
         }
 
@@ -290,7 +289,7 @@ class TaxController extends FrameworkBundleAdminController
                 'success',
                 $this->trans('Successful deletion.', 'Admin.Notifications.Success')
             );
-        } catch (TaxException $e) {
+        } catch (DomainException $e) {
             $this->addFlash('error', $this->getErrorMessageForException($e, $this->getErrorMessages()));
         }
 
@@ -306,7 +305,7 @@ class TaxController extends FrameworkBundleAdminController
     }
 
     /**
-     * Gets error by exception type.
+     * Gets error messages for exceptions
      *
      * @return array
      */
