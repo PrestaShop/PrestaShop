ef7dde8cd28317b29dda9fc929f31c408d397089
Julius Zukauskas <julius.zukauskas@invertus.eu>
2019-02-12 18:24:06 +0200

    Minor fixes regarding commenting and code optimization

--- src/PrestaShopBundle/Controller/Admin/Improve/International/TaxController.php
+++ src/PrestaShopBundle/Controller/Admin/Improve/International/TaxController.php
@@ -32,7 +32,6 @@ use PrestaShop\PrestaShop\Core\Domain\Tax\Command\BulkToggleTaxStatusCommand;
 use PrestaShop\PrestaShop\Core\Domain\Tax\Command\DeleteTaxCommand;
 use PrestaShop\PrestaShop\Core\Domain\Tax\Command\ToggleTaxStatusCommand;
 use PrestaShop\PrestaShop\Core\Domain\Tax\Exception\DeleteTaxException;
-use PrestaShop\PrestaShop\Core\Domain\Tax\Exception\TaxException;
 use PrestaShop\PrestaShop\Core\Domain\Tax\Exception\TaxNotFoundException;
 use PrestaShop\PrestaShop\Core\Domain\Tax\Exception\UpdateTaxException;
 use PrestaShop\PrestaShop\Core\Domain\Tax\Query\GetTaxForEditing;
@@ -271,7 +270,7 @@ class TaxController extends FrameworkBundleAdminController
                 'success',
                 $this->trans('Successful deletion.', 'Admin.Notifications.Success')
             );
-        } catch (TaxException $e) {
+        } catch (DomainException $e) {
             $this->addFlash('error', $this->getErrorMessageForException($e, $this->getErrorMessages()));
         }
 
