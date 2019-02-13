5d1f8bfa51b9aafd152d57bdbc4842539ef3a4b7
Mikael Kermorgant <mikael.kermorgant@gmail.com>
2019-02-13 10:14:38 +0200

    load doctrine entity mappings from modules
    This is a quick attempt at loading doctrine entity mappings from
    module configuration files. Mainly for initiating a discussion about
    this topic

--- src/PrestaShopBundle/PrestaShopBundle.php
+++ src/PrestaShopBundle/PrestaShopBundle.php
@@ -26,6 +26,7 @@
 
 namespace PrestaShopBundle;
 
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
 use PrestaShopBundle\DependencyInjection\Compiler\DynamicRolePass;
 use PrestaShopBundle\DependencyInjection\Compiler\LoadServicesFromModulesPass;
 use PrestaShopBundle\DependencyInjection\Compiler\OverrideTranslatorServiceCompilerPass;
@@ -36,7 +37,9 @@ use PrestaShopBundle\DependencyInjection\Compiler\RouterPass;
 use PrestaShopBundle\DependencyInjection\PrestaShopExtension;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\Yaml\Yaml;
 
 class PrestaShopBundle extends Bundle
 {
@@ -60,5 +63,61 @@ class PrestaShopBundle extends Bundle
         $container->addCompilerPass(new RouterPass(), PassConfig::TYPE_AFTER_REMOVING);
         $container->addCompilerPass(new OverrideTranslatorServiceCompilerPass());
         $container->addCompilerPass(new OverrideTwigServiceCompilerPass());
+        $this->addModuleEntityMappingsPass($container);
+    }
+
+    /**
+     * @param ContainerBuilder $container
+     */
+    private function addModuleEntityMappingsPass(ContainerBuilder $container)
+    {
+        $installedModules  = $container->getParameter('kernel.active_modules');
+
+        foreach ($this->getModulesPaths() as $modulePath) {
+            if (in_array($modulePath->getFilename(), $installedModules)
+                && file_exists($modulePath . '/config/doctrine.yml')
+            ) {
+                $config = Yaml::parse(file_get_contents($modulePath . '/config/doctrine.yml'));
+
+                foreach ($config['doctrine']['orm']['mappings'] as $key => $mapping) {
+                    $container->addCompilerPass($this->buildDoctrineOrmMappingPass($modulePath, $mapping));
+                }
+            }
+        }
+    }
+
+    /**
+     * @return \Iterator
+     */
+    private function getModulesPaths()
+    {
+        return Finder::create()->directories()->in(__DIR__ . '/../../modules')->depth(0);
+    }
+
+    /**
+     * @param array $mapping
+     *
+     * @return DoctrineOrmMappingsPass
+     */
+    private function buildDoctrineOrmMappingPass($path, $mapping)
+    {
+        [$namespaces, $managerParameters, $enabledParameter, $aliasMap] = [
+            [$path.'/'.$mapping['dir'] => $mapping['prefix']],
+            [],
+            false,
+            [$mapping['alias'] => $mapping['prefix']]
+        ];
+
+        if ($mapping['type'] === 'xml') {
+            return DoctrineOrmMappingsPass::createXmlMappingDriver(
+                $namespaces, $managerParameters, $enabledParameter, $aliasMap
+            );
+        } elseif ($mapping['type'] === 'yml') {
+            return DoctrineOrmMappingsPass::createYamlMappingDriver(
+                $namespaces, $managerParameters, $enabledParameter, $aliasMap
+            );
+        }
+
+        throw new \Exception(sprintf('mapping of type %s not supported for modules', $type));
     }
 }
