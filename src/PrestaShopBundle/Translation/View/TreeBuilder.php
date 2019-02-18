4b33a56adf196c749a5dbdc6b6a7e64027fb9996
Mickaël Andrieu <mickael.andrieu@prestashop.com>
2019-02-18 18:36:31 +0100

    Ease the detection of translations files in the modules

--- src/PrestaShopBundle/Translation/View/TreeBuilder.php
+++ src/PrestaShopBundle/Translation/View/TreeBuilder.php
@@ -71,16 +71,13 @@ class TreeBuilder
             $domainDatabase = str_replace('.' . $provider->getLocale(), '', $domain);
 
             foreach (array_keys($messages) as $translationKey) {
-                if (array_key_exists($translationKey, $xliffCatalog[$domain])) {
-                }
-
                 $data = array(
-                    'xlf' => (array_key_exists($domain, $xliffCatalog) &&
+                    'xlf' => array_key_exists($domain, $xliffCatalog) &&
                     array_key_exists($translationKey, $xliffCatalog[$domain]) ?
-                        $xliffCatalog[$domain][$translationKey] : null),
-                    'db' => (array_key_exists($domainDatabase, $databaseCatalogue) &&
+                        $xliffCatalog[$domain][$translationKey] : null,
+                    'db' => array_key_exists($domainDatabase, $databaseCatalogue) &&
                     array_key_exists($translationKey, $databaseCatalogue[$domainDatabase]) ?
-                        $databaseCatalogue[$domainDatabase][$translationKey] : null),
+                        $databaseCatalogue[$domainDatabase][$translationKey] : null,
                 );
 
                 // if search is empty or is in catalog default|xlf|database
