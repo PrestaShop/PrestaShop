ddb91718624d898c1f4cbed5246d1dec88f4bc6a
Jonathan Lelievre <jo.lelievre@gmail.com>
2019-02-22 16:26:45 +0100

    Test that doctrine mapping works correctly

--- tests/Resources/modules/ps_banner/src/Entity/Banner.php
+++ tests/Resources/modules/ps_banner/src/Entity/Banner.php
@@ -26,6 +26,28 @@
 
 namespace PrestaShop\Module\Banner\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table()
+ * @ORM\Entity()
+ */
 class Banner
 {
+    /**
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\Column(name="id_product_comment", type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
 }
