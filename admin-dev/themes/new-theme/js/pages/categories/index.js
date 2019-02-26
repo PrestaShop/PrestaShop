1a861b38a009bcfbc0151fb0b7512af15f20eb18
Tomas Ilginis <tilginis@gmail.com>
2019-02-26 14:22:53 +0200

    adds root category access

--- admin-dev/themes/new-theme/js/pages/categories/index.js
+++ admin-dev/themes/new-theme/js/pages/categories/index.js
@@ -39,7 +39,7 @@ import DeleteCategoriesBulkActionExtension from "../../components/grid/extension
 import TranslatableInput from "../../components/translatable-input";
 import ChoiceTable from "../../components/choice-table";
 import TextWithLengthCounter from "../../components/form/text-with-length-counter";
-import ElementToLinkRewriteCopier from "./element-to-link-rewrite-copier";
+import ElementToLinkRewriteCopier from "../../components/element-to-link-rewrite-copier";
 import ChoiceTree from "../../components/form/choice-tree";
 import FormSubmitButton from "../../components/form-submit-button";
 
@@ -64,10 +64,17 @@ $(() => {
   new TranslatableInput();
   new ChoiceTable();
   new TextWithLengthCounter();
+
   new ElementToLinkRewriteCopier({
     targetElementSelector: 'input[name^="category[name]"]',
     destinationElementSelector: 'input[name^="category[link_rewrite]"]',
   });
+
+  new ElementToLinkRewriteCopier({
+    targetElementSelector: 'input[name^="root_category[name]"]',
+    destinationElementSelector: 'input[name^="root_category[link_rewrite]"]',
+  });
+
   new FormSubmitButton();
 
   new ChoiceTree('#category_id_parent');
