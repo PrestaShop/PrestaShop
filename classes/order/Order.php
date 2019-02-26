71a83c371dc15d76c8de5bf94ec8161888043bea
Connie <connie@diacalc.org>
2019-02-26 13:05:34 +0400

    To fix issue #9694 (when the shipping cost gets free if the quantity is more than a half specifiend in BO) there should be passed the default values to updateQty but the last one parameter - skipAvailabilityCheckOutOfStock, which should be set to true;

--- classes/order/Order.php
+++ classes/order/Order.php
@@ -2545,7 +2545,16 @@ class OrderCore extends ObjectModel
 
         // add real order products
         foreach ($this->getProducts() as $product) {
-            $new_cart->updateQty($product['product_quantity'], (int) $product['product_id']);
+            $new_cart->updateQty(
+                    $product['product_quantity'], 
+                    (int) $product['product_id'],
+                    null, //pass default values
+                    false, 
+                    'up', 
+                    0, 
+                    null, 
+                    true,
+                    true ); // - skipAvailabilityCheckOutOfStock
         }
 
         // get new shipping cost
