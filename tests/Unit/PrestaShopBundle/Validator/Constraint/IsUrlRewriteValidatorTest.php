e8b36c79e2cd9d170236063e71e4b52a6301c069
Julius Zukauskas <julius.zukauskas@invertus.eu>
2019-02-26 14:49:37 +0200

    Changed tests namespace

--- tests/Unit/PrestaShopBundle/Validator/Constraint/IsUrlRewriteValidatorTest.php
+++ tests/Unit/PrestaShopBundle/Validator/Constraint/IsUrlRewriteValidatorTest.php
@@ -24,7 +24,7 @@
  * International Registered Trademark & Property of PrestaShop SA
  */
 
-namespace Tests\Unit\Core\ConstraintValidator;
+namespace Tests\Unit\PrestaShopBundle\Validator\Constraint;
 
 use PrestaShop\PrestaShop\PrestaShopBundle\Validator\Constraint\IsUrlRewrite;
 use PrestaShop\PrestaShop\PrestaShopBundle\Validator\Constraint\IsUrlRewriteValidator;
