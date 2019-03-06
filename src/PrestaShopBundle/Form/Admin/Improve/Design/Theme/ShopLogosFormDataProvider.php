c0584cd2308820225966c261a3c5f7b9c238281e
Tomas Ilginis <tilginis@gmail.com>
2019-03-06 13:08:35 +0200

    on page loads keeps the checkboxes checked

--- src/PrestaShopBundle/Form/Admin/Improve/Design/Theme/ShopLogosFormDataProvider.php
+++ src/PrestaShopBundle/Form/Admin/Improve/Design/Theme/ShopLogosFormDataProvider.php
@@ -30,6 +30,8 @@ use PrestaShop\PrestaShop\Core\CommandBus\CommandBusInterface;
 use PrestaShop\PrestaShop\Core\Domain\Shop\Command\UploadLogosCommand;
 use PrestaShop\PrestaShop\Core\Domain\Shop\Exception\ShopException;
 use PrestaShop\PrestaShop\Core\Form\FormDataProviderInterface;
+use PrestaShop\PrestaShop\Core\Form\MultiStoreSettingsFormDataProviderInterface;
+use PrestaShop\PrestaShop\Core\Form\ValueObject\ShopRestriction;
 
 final class ShopLogosFormDataProvider implements FormDataProviderInterface
 {
@@ -39,11 +41,20 @@ final class ShopLogosFormDataProvider implements FormDataProviderInterface
     private $commandBus;
 
     /**
-     * @param CommandBusInterface $commandBus
+     * @var MultiStoreSettingsFormDataProviderInterface
      */
-    public function __construct(CommandBusInterface $commandBus)
-    {
+    private $themeMultiStoreSettingsFormDataProvider;
+
+    /**
+     * @param CommandBusInterface $commandBus
+     * @param MultiStoreSettingsFormDataProviderInterface $themeMultiStoreSettingsFormDataProvider
+     */
+    public function __construct(
+        CommandBusInterface $commandBus,
+        MultiStoreSettingsFormDataProviderInterface $themeMultiStoreSettingsFormDataProvider
+    ) {
         $this->commandBus = $commandBus;
+        $this->themeMultiStoreSettingsFormDataProvider = $themeMultiStoreSettingsFormDataProvider;
     }
 
     /**
@@ -52,8 +63,7 @@ final class ShopLogosFormDataProvider implements FormDataProviderInterface
     public function getData()
     {
         return [
-            'options' => [
-            ],
+            'options' => $this->themeMultiStoreSettingsFormDataProvider->getData(),
         ];
     }
 
@@ -64,28 +74,58 @@ final class ShopLogosFormDataProvider implements FormDataProviderInterface
      */
     public function setData(array $data)
     {
+        $data = $this->geFilteredFieldsByShopRestriction($data);
+
         $command = new UploadLogosCommand();
 
-        if ($data['header_logo']) {
+        if (!empty($data['header_logo'])) {
             $command->setUploadedHeaderLogo($data['header_logo']);
         }
 
-        if ($data['mail_logo']) {
+        if (!empty($data['mail_logo'])) {
             $command->setUploadedMailLogo($data['mail_logo']);
         }
 
-        if ($data['invoice_logo']) {
+        if (!empty($data['invoice_logo'])) {
             $command->setUploadedInvoiceLogo($data['invoice_logo']);
         }
 
-        if ($data['favicon']) {
+        if (!empty($data['favicon'])) {
             $command->setUploadedFavicon($data['favicon']);
         }
 
-        if (isset($data['shop_restriction'])) {
-            $command->setShopRestriction($data['shop_restriction']);
+        $this->commandBus->handle($command);
+    }
+
+    /**
+     * If shop_restriction argument exists in the post this means that certain shop restrictions are applied.
+     * It filters and drops the values which are not being selected for editing for specific shop.
+     *
+     * @param array $data - form data.
+     *
+     * @return array
+     */
+    private function geFilteredFieldsByShopRestriction(array $data)
+    {
+        if (!isset($data['shop_restriction'])) {
+
+            return $data;
         }
 
-        $this->commandBus->handle($command);
+        /** @var ShopRestriction $shopRestriction */
+        $shopRestriction = $data['shop_restriction'];
+
+        $shopRestrictionFields = $shopRestriction->getShopRestrictionFields();
+
+        foreach ($shopRestrictionFields as $shopRestrictionField) {
+            $doesValueExistsAndNotRestrictedToShop = isset($data[$shopRestrictionField->getFieldName()]) &&
+                !$shopRestrictionField->isRestrictedToContextShop();
+
+            if ($doesValueExistsAndNotRestrictedToShop) {
+                unset($data[$shopRestrictionField->getFieldName()]);
+            }
+        }
+
+        return $data;
     }
 }
