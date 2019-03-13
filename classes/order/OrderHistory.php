73314f359c3f3ec5af868a7d0b7a3239f382dd2d
Jean-François Viguier <16720275+jf-viguier@users.noreply.github.com>
2019-03-13 11:40:14 +0100

    Add carrier validation

--- classes/order/OrderHistory.php
+++ classes/order/OrderHistory.php
@@ -486,13 +486,16 @@ class OrderHistoryCore extends ObjectModel
             ShopUrl::cacheMainDomainForShop($order->id_shop);
 
             $topic = $result['osname'];
-            $carrier = new Carrier((int) $order->id_carrier, $order->id_lang);
+            $carrierUrl = '';
+            if (Validate::isLoadedObject($carrier = new Carrier((int) $order->id_carrier, $order->id_lang))) {
+                $carrierUrl = $carrier->url;
+            }
             $data = array(
                 '{lastname}' => $result['lastname'],
                 '{firstname}' => $result['firstname'],
                 '{id_order}' => (int) $this->id_order,
                 '{order_name}' => $order->getUniqReference(),
-                '{followup}' => str_replace('@', $order->getWsShippingNumber(), $carrier->url),
+                '{followup}' => str_replace('@', $order->getWsShippingNumber(), $carrierUrl),
                 '{shipping_number}' => $order->getWsShippingNumber(),
             );
 
