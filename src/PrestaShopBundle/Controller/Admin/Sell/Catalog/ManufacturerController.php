943f165be59bbdd3d668a5538784ff6d68e3a2b5
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2019-03-14 14:59:15 +0200

    fix naming conventions

--- src/PrestaShopBundle/Controller/Admin/Sell/Catalog/ManufacturerController.php
+++ src/PrestaShopBundle/Controller/Admin/Sell/Catalog/ManufacturerController.php
@@ -73,7 +73,7 @@ class ManufacturerController extends FrameworkBundleAdminController
             $this->addFlash('error', $this->getErrorMessageForException($e, $this->getErrorMessages($e)));
         }
 
-        return $this->render('@PrestaShop/Admin/Sell/Catalog/Manufacturer/Address/add.html.twig', [
+        return $this->render('@PrestaShop/Admin/Sell/Catalog/Manufacturer/Address/create.html.twig', [
             'enableSidebar' => true,
             'layoutTitle' => $this->trans('Add new address', 'Admin.Orderscustomers.Feature'),
             'addressForm' => $addressForm->createView(),
