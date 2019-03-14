e72c9479f4ab057cff40399556dea31de20cd468
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2019-03-14 11:15:47 +0200

    extract states toggler into reusable JS component

--- admin-dev/themes/new-theme/js/pages/manufacturer/address/form.js
+++ admin-dev/themes/new-theme/js/pages/manufacturer/address/form.js
@@ -23,10 +23,14 @@
  * International Registered Trademark & Property of PrestaShop SA
  */
 
-import CountryStateOptionHandler from './country-state-option-handler';
+import CountryStateSelectionToggler from '../../../components/country-state-selection-toggler';
+import ManufacturerAddressMap from './manufacturer-address-map';
 
 const $ = window.$;
 
 $(document).ready(() => {
-  new CountryStateOptionHandler();
+  new CountryStateSelectionToggler(
+    ManufacturerAddressMap.manufacturerAddressCountrySelect,
+    ManufacturerAddressMap.manufacturerAddressStateBlock
+  );
 });
