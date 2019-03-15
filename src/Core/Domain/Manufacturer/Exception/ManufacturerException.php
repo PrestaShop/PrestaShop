646f434a5b59415592b9de0292391792a7285d64
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2019-03-15 16:20:58 +0200

    throw exception when manufacturer is not found

---
+++ src/Core/Domain/Manufacturer/Exception/ManufacturerException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sarjon
+ * Date: 19.3.15
+ * Time: 16.18
+ */
+
+namespace PrestaShop\PrestaShop\Core\Domain\Manufacturer\Exception;
+
+use PrestaShop\PrestaShop\Core\Domain\Exception\DomainException;
+
+/**
+ * Base exception for Manufacturer subdomain
+ */
+class ManufacturerException extends DomainException
+{
+}
---
+++ src/Core/Domain/Manufacturer/Exception/ManufacturerNotFoundException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sarjon
+ * Date: 19.3.15
+ * Time: 16.19
+ */
+
+namespace PrestaShop\PrestaShop\Core\Domain\Manufacturer\Exception;
+
+/**
+ * Is thrown when manufacturer is not found in Manufacturer subdomain
+ */
+class ManufacturerNotFoundException extends ManufacturerException
+{
+}
