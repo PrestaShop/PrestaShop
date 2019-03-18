4e6d4fed7e046b78fca0dee163c8fbc0dcc39ef0
Tomas Ilginis <tilginis@gmail.com>
2019-03-18 15:26:30 +0200

    adds constraint validations directly in the form

--- src/PrestaShopBundle/Form/Admin/Improve/Design/Pages/CmsPageCategoryType.php
+++ src/PrestaShopBundle/Form/Admin/Improve/Design/Pages/CmsPageCategoryType.php
@@ -9,6 +9,9 @@
 namespace PrestaShopBundle\Form\Admin\Improve\Design\Pages;
 
 
+use PrestaShop\PrestaShop\Core\ConstraintValidator\Constraints\CleanHtml;
+use PrestaShop\PrestaShop\Core\ConstraintValidator\Constraints\DefaultLanguage;
+use PrestaShop\PrestaShop\Core\ConstraintValidator\Constraints\IsUrlRewrite;
 use PrestaShopBundle\Form\Admin\Type\Material\MaterialChoiceTreeType;
 use PrestaShopBundle\Form\Admin\Type\ShopChoiceTreeType;
 use PrestaShopBundle\Form\Admin\Type\SwitchType;
@@ -18,6 +21,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class CmsPageCategoryType extends AbstractType
 {
@@ -48,8 +52,28 @@ class CmsPageCategoryType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $categoryNameValidationPattern = '/^[^<>;=#{}]*$/u';
+        $genericNameValidationPattern = '/^[^<>={}]*$/u';
+
         $builder
-            ->add('name', TranslatableType::class)
+            ->add('name', TranslatableType::class, [
+                'error_bubbling' => false,
+                'constraints' => [
+                    new DefaultLanguage()
+                ],
+                'options' => [
+                    'constraints' => [
+                        new Regex([
+                            'pattern' => $categoryNameValidationPattern,
+                            'message' =>  $this->trans(
+                                '%s is invalid.',
+                                [],
+                                'Admin.Notifications.Error'
+                            ),
+                        ])
+                    ],
+                ],
+            ])
             ->add('is_displayed', SwitchType::class, [
                 'required' => false,
             ])
@@ -61,17 +85,68 @@ class CmsPageCategoryType extends AbstractType
             ->add('description', TranslatableType::class, [
                 'required' => false,
                 'type' => TextareaType::class,
+                'options' => [
+                    'constraints' => [
+                        new CleanHtml()
+                    ],
+                ],
             ])
             ->add('meta_title', TranslatableType::class, [
                 'required' => false,
+                'options' => [
+                    'constraints' => [
+                        new Regex([
+                            'pattern' => $genericNameValidationPattern,
+                            'message' =>  $this->trans(
+                                '%s is invalid.',
+                                [],
+                                'Admin.Notifications.Error'
+                            ),
+                        ])
+                    ],
+                ],
             ])
             ->add('meta_description', TranslatableType::class, [
                 'required' => false,
+                'options' => [
+                    'constraints' => [
+                        new Regex([
+                            'pattern' => $genericNameValidationPattern,
+                            'message' =>  $this->trans(
+                                '%s is invalid.',
+                                [],
+                                'Admin.Notifications.Error'
+                            ),
+                        ])
+                    ],
+                ],
             ])
             ->add('meta_keywords', TranslatableType::class, [
                 'required' => false,
+                'options' => [
+                    'constraints' => [
+                        new Regex([
+                            'pattern' => $genericNameValidationPattern,
+                            'message' =>  $this->trans(
+                                '%s is invalid.',
+                                [],
+                                'Admin.Notifications.Error'
+                            ),
+                        ])
+                    ],
+                ],
             ])
-            ->add('friendly_url', TranslatableType::class)
+            ->add('friendly_url', TranslatableType::class, [
+                'error_bubbling' => false,
+                'constraints' => [
+                    new DefaultLanguage(),
+                ],
+                'options' => [
+                    'constraints' => [
+                        new IsUrlRewrite()
+                    ],
+                ],
+            ])
         ;
 
         if ($this->isShopFeatureEnabled) {
