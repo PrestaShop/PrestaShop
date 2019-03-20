6350e497f65f6ac8ee1c797e1c98c4ed7351d8f7
Tomas Ilginis <tilginis@gmail.com>
2019-03-20 19:05:24 +0200

    adds bulk enabling functionality

--- src/Adapter/CMS/Page/CommandHandler/BulkEnableCmsPageHandler.php
+++ src/Adapter/CMS/Page/CommandHandler/BulkEnableCmsPageHandler.php
@@ -26,8 +26,57 @@
 
 namespace PrestaShop\PrestaShop\Adapter\CMS\Page\CommandHandler;
 
+use PrestaShop\PrestaShop\Core\Domain\CmsPage\Command\BulkEnableCmsPageCommand;
+use PrestaShop\PrestaShop\Core\Domain\CmsPage\CommandHandler\BulkEnableCmsPageHandlerInterface;
+use PrestaShop\PrestaShop\Core\Domain\CmsPage\Exception\CannotEnableCmsPageException;
+use PrestaShop\PrestaShop\Core\Domain\CmsPage\Exception\CmsPageException;
+use PrestaShop\PrestaShop\Core\Domain\CmsPage\Exception\CmsPageNotFoundException;
+use PrestaShopException;
 
-class BulkEnableCmsPageHandler
+/**
+ * Enables multiple cms pages.
+ */
+final class BulkEnableCmsPageHandler extends AbstractCmsPageHandler implements BulkEnableCmsPageHandlerInterface
 {
+    /**
+     * {@inheritdoc}
+     *
+     * @throws CmsPageException
+     */
+    public function handle(BulkEnableCmsPageCommand $command)
+    {
+        try {
+            $this->enableCmsPages($command);
+        } catch (PrestaShopException $exception) {
+            throw new CmsPageException(
+                'An error occurred when bulk enabling the cms pages',
+                0,
+                $exception
+            );
+        }
+    }
 
+    /**
+     * @param BulkEnableCmsPageCommand $command
+     *
+     * @throws CannotEnableCmsPageException
+     * @throws CmsPageException
+     * @throws PrestaShopException
+     * @throws \PrestaShopDatabaseException
+     * @throws CmsPageNotFoundException
+     */
+    private function enableCmsPages(BulkEnableCmsPageCommand $command)
+    {
+        foreach ($command->getCmsPages() as $cmsPage) {
+            $cms = $this->getCmsPageIfExistsById($cmsPage->getValue());
+
+            $cms->active = true;
+
+            if (false === $cms->update()) {
+                throw new CannotEnableCmsPageException(
+                    sprintf('Failed to enable cms page with id %s', $cmsPage->getValue())
+                );
+            }
+        }
+    }
 }
