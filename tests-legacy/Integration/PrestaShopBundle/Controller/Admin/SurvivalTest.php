4cba22d1cbbeb53f0a12876f5562358ef001c759
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2019-03-27 17:18:33 +0200

    add survival tests

--- tests-legacy/Integration/PrestaShopBundle/Controller/Admin/SurvivalTest.php
+++ tests-legacy/Integration/PrestaShopBundle/Controller/Admin/SurvivalTest.php
@@ -154,6 +154,8 @@ class SurvivalTest extends WebTestCase
             'admin_employees_create' => ['Add new employee', 'admin_employees_create'],
             'admin_cms_pages_index' => ['Cms page', 'admin_cms_pages_index'],
             'admin_cms_pages_category_create' => ['Add new cms category page', 'admin_cms_pages_category_create'],
+            'admin_profiles_index' => ['Profiles', 'admin_profiles_index'],
+            'admin_profiles_create' => ['Add new profile', 'admin_profiles_create'],
         ];
     }
 
