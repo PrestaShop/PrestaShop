5badce30d3ffc5c32a38f9750d87abf916da84b4
fgschaefer <fop42@gmx.net>
2019-03-27 15:19:55 +0100

    fix old bug PSCSX-8515 (group for not logged in user)
    in getCurrentCustomerGroups function, if user is not logged in (no customer present), return default value instead of empty list. fixes multishop search if shops have different 'visitor' (and 'guest'?) groups and categories that are only accessible for one of these groups.

--- classes/controller/FrontController.php
+++ classes/controller/FrontController.php
@@ -1201,7 +1201,7 @@ class FrontControllerCore extends Controller
 
         $context = Context::getContext();
         if (!isset($context->customer) || !$context->customer->id) {
-            return array();
+            return array((int)Group::getCurrent()->id);
         }
 
         if (!is_array(self::$currentCustomerGroups)) {
