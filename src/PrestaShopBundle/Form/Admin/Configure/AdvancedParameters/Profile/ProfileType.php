63848eb8f27a670605079997591157c5e90dae1a
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2019-03-27 16:48:50 +0200

    add missing docs for ProfileType

--- src/PrestaShopBundle/Form/Admin/Configure/AdvancedParameters/Profile/ProfileType.php
+++ src/PrestaShopBundle/Form/Admin/Configure/AdvancedParameters/Profile/ProfileType.php
@@ -31,8 +31,14 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
+/**
+ * Builds form for Profile
+ */
 class ProfileType extends AbstractType
 {
+    /**
+     * {@inheritdoc}
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
