e67e5facf52348f053b596ebfb8fabfbd289004d
Julius Zukauskas <julius.zukauskas@invertus.eu>
2019-03-29 13:46:55 +0200

    Moved some params getter from view to controller

--- src/PrestaShopBundle/Controller/Admin/Improve/Design/CmsPageController.php
+++ src/PrestaShopBundle/Controller/Admin/Improve/Design/CmsPageController.php
@@ -179,6 +179,7 @@ class CmsPageController extends FrameworkBundleAdminController
 
         return $this->render('PrestaShopBundle:Admin/Improve/Design/Cms:add.html.twig', [
             'cmsPageForm' => $form->createView(),
+            'cmsCategoryParentId' => $request->get('id_cms_category'),
             'enableSidebar' => true,
             'help_link' => $this->generateSidebarLink($request->attributes->get('_legacy_controller')),
         ]);
@@ -218,6 +219,7 @@ class CmsPageController extends FrameworkBundleAdminController
 
         return $this->render('@PrestaShop/Admin/Improve/Design/Cms/edit.html.twig', [
             'cmsPageForm' => $form->createView(),
+            'cmsCategoryParentId' => $request->get('id_cms_category'),
             'enableSidebar' => true,
             'help_link' => $this->generateSidebarLink($request->attributes->get('_legacy_controller')),
         ]);
