80388fbce715c25d0ad24520d890da1ab512b6ac
Jonathan Lelievre <jo.lelievre@gmail.com>
2019-03-29 13:20:37 +0100

    Move preview actions to MailThemeController

--- src/PrestaShopBundle/Controller/Admin/Improve/Design/MailThemeController.php
+++ src/PrestaShopBundle/Controller/Admin/Improve/Design/MailThemeController.php
@@ -27,7 +27,16 @@
 namespace PrestaShopBundle\Controller\Admin\Improve\Design;
 
 use PrestaShop\PrestaShop\Core\Exception\CoreException;
+use PrestaShop\PrestaShop\Core\Exception\FileNotFoundException;
+use PrestaShop\PrestaShop\Core\Exception\InvalidArgumentException;
 use PrestaShop\PrestaShop\Core\Form\FormHandlerInterface;
+use PrestaShop\PrestaShop\Core\Language\LanguageInterface;
+use PrestaShop\PrestaShop\Core\Language\LanguageRepositoryInterface;
+use PrestaShop\PrestaShop\Core\MailTemplate\Layout\LayoutInterface;
+use PrestaShop\PrestaShop\Core\MailTemplate\MailTemplateInterface;
+use PrestaShop\PrestaShop\Core\MailTemplate\MailTemplateRendererInterface;
+use PrestaShop\PrestaShop\Core\MailTemplate\ThemeCatalogInterface;
+use PrestaShop\PrestaShop\Core\MailTemplate\ThemeInterface;
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
 use PrestaShopBundle\Form\Admin\Improve\Design\MailTheme\GenerateMailsType;
 use PrestaShopBundle\Security\Annotation\AdminSecurity;
@@ -35,6 +44,7 @@ use PrestaShopBundle\Service\MailTemplate\GenerateMailTemplatesService;
 use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
  * Class MailThemeController manages mail theme generation, you can define the shop
@@ -146,6 +156,8 @@ class MailThemeController extends FrameworkBundleAdminController
      * @param Request $request
      *
      * @return Response
+     *
+     * @throws \Exception
      */
     public function saveConfigurationAction(Request $request)
     {
@@ -176,6 +188,131 @@ class MailThemeController extends FrameworkBundleAdminController
         }
 
         return $this->redirectToRoute('admin_mail_theme_index');
+    }
+
+    /**
+     * Preview a mail layout from a defined theme
+     *
+     * @AdminSecurity("is_granted('read', request.get('_legacy_controller'))")
+     *
+     * @param string $theme
+     * @param string $layout
+     * @param string $type
+     * @param string $locale
+     * @param string $module
+     *
+     * @return Response
+     *
+     * @throws FileNotFoundException
+     * @throws InvalidArgumentException
+     */
+    public function previewLayoutAction($theme, $layout, $type, $locale, $module = '')
+    {
+        $renderedLayout = $this->renderLayout($theme, $layout, $type, $locale, $module);
+
+        return new Response($renderedLayout);
+    }
+
+    /**
+     * Display the raw source of a theme layout (mainly useful for developers/integrators)
+     *
+     * @AdminSecurity("is_granted('read', request.get('_legacy_controller'))")
+     *
+     * @param string $theme
+     * @param string $layout
+     * @param string $type
+     * @param string $locale
+     * @param string $module
+     *
+     * @return Response
+     *
+     * @throws FileNotFoundException
+     * @throws InvalidArgumentException
+     */
+    public function rawLayoutAction($theme, $layout, $type, $locale, $module = '')
+    {
+        $renderedLayout = $this->renderLayout($theme, $layout, $type, $locale, $module);
+
+        $response = new Response($renderedLayout, 200, [
+            'Content-Type' => 'text/plain',
+        ]);
+
+        return $response;
+    }
+
+    /**
+     * @param string $themeName
+     * @param string $layoutName
+     * @param string $type
+     * @param string $locale
+     * @param string $module
+     *
+     * @return string
+     *
+     * @throws FileNotFoundException
+     * @throws InvalidArgumentException
+     */
+    private function renderLayout($themeName, $layoutName, $type, $locale = '', $module = '')
+    {
+        /** @var ThemeCatalogInterface $themeCatalog */
+        $themeCatalog = $this->get('prestashop.core.mail_template.theme_catalog');
+        /** @var ThemeInterface $theme */
+        $theme = $themeCatalog->getByName($themeName);
+
+        /** @var LayoutInterface $layout */
+        $layout = null;
+        /* @var LayoutInterface $layoutInterface */
+        foreach ($theme->getLayouts() as $layoutInterface) {
+            if ($layoutInterface->getName() == $layoutName
+                && $layoutInterface->getModuleName() == $module
+            ) {
+                $layout = $layoutInterface;
+                break;
+            }
+        }
+
+        if (null === $layout) {
+            throw new FileNotFoundException(sprintf(
+                'Could not find layout %s%s in theme %s',
+                empty($module) ? '' : $module . ':',
+                $layoutName,
+                $themeName
+            ));
+        }
+
+        /** @var LanguageRepositoryInterface $languageRepository */
+        $languageRepository = $this->get('prestashop.core.admin.lang.repository');
+        if (empty($locale)) {
+            $locale = $this->getContext()->language->locale;
+        }
+        /** @var LanguageInterface $language */
+        $language = $languageRepository->getByLocaleOrIsoCode($locale);
+        if (null === $language) {
+            throw new InvalidArgumentException(sprintf('Could not find Language with locale or isoCode %s', $locale));
+        }
+
+        /** @var MailTemplateRendererInterface $renderer */
+        $renderer = $this->get('prestashop.core.mail_template.mail_template_renderer');
+        //Special case for preview, we fill the mail variables
+        $renderer->addTransformation($this->get('prestashop.core.mail_template.transformation.mail_variables'));
+
+        switch ($type) {
+            case MailTemplateInterface::HTML_TYPE:
+                $renderedLayout = $renderer->renderHtml($layout, $language);
+                break;
+            case MailTemplateInterface::TXT_TYPE:
+                $renderedLayout = $renderer->renderTxt($layout, $language);
+                break;
+            default:
+                throw new NotFoundHttpException(sprintf(
+                    'Requested type %s is not managed, please use one of these: %s',
+                    $type,
+                    implode(',', [MailTemplateInterface::HTML_TYPE, MailTemplateInterface::TXT_TYPE])
+                ));
+                break;
+        }
+
+        return $renderedLayout;
     }
 
     /**
