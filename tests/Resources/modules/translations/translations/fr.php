e5e803b068d632b2faa9af6fe36ec2c624f28fff
Mickaël Andrieu <mickael.andrieu@prestashop.com>
2019-04-01 20:03:04 +0200

    Now works on controllers

--- tests/Resources/modules/translations/translations/fr.php
+++ tests/Resources/modules/translations/translations/fr.php
@@ -2,5 +2,5 @@
 
 global $_MODULE;
 $_MODULE = array();
-$_MODULE['<{translations}prestashop>prestashop_b10a8db164e0754105b7a99be72e3fe5'] = 'Bonjour le monde';
-$_MODULE['<{translations}prestashop>prestashop_9e8be49b9cfd2252504e0a48ddb1c9df'] = 'Contrôleur moderne';
+$_MODULE['<{translations}prestashop>translations_b10a8db164e0754105b7a99be72e3fe5'] = 'Bonjour le monde';
+$_MODULE['<{translations}prestashop>translations_9e8be49b9cfd2252504e0a48ddb1c9df'] = 'Contrôleur moderne';
