c977c33643c206cad94673527106088556d8c670
Mickaël Andrieu <mickael.andrieu@prestashop.com>
2019-04-01 20:03:04 +0200

    Now works on controllers

--- tests/Resources/modules/translations/translations/es.php
+++ tests/Resources/modules/translations/translations/es.php
@@ -2,4 +2,4 @@
 
 global $_MODULE;
 $_MODULE = array();
-$_MODULE['<{translations}prestashop>prestashop_b10a8db164e0754105b7a99be72e3fe5'] = 'Hola el mundo';
+$_MODULE['<{translations}prestashop>translations_b10a8db164e0754105b7a99be72e3fe5'] = 'Hola el mundo';
