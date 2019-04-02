ceba575757be3774e21cfae710c3c6323e282832
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2019-04-02 17:32:56 +0300

    remove unused attribute from category type

--- src/PrestaShopBundle/Form/Admin/Catalog/Category/AbstractCategoryType.php
+++ src/PrestaShopBundle/Form/Admin/Catalog/Category/AbstractCategoryType.php
@@ -125,9 +125,6 @@ abstract class AbstractCategoryType extends TranslatorAwareType
                 'type' => TextWithLengthCounterType::class,
                 'required' => false,
                 'options' => [
-                    'attr' => [
-                        'class' => 'serp-watched-title',
-                    ],
                     'max_length' => 255,
                     'constraints' => [
                         new Regex([
