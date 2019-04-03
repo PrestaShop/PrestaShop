ed3b48b844c757a8ef5041eef3bb0b047b78ff1b
Rodrigo Laurindo <rblaurin@gmail.com>
2019-04-02 21:06:42 -0300

    Update 1.7.6.0.sql

--- install-dev/upgrade/sql/1.7.6.0.sql
+++ install-dev/upgrade/sql/1.7.6.0.sql
@@ -16,6 +16,18 @@ CREATE TABLE `PREFIX_currency_lang` (
 
 /* PHP:ps_1760_copy_data_from_currency_to_currency_lang(); */;
 
+ALTER TABLE `PREFIX_admin_filter`
+	ADD `filter_id` VARCHAR (255) DEFAULT '' NOT NULL AFTER `shop`,
+  DROP INDEX IF EXISTS `admin_filter_search_idx`,
+  DROP INDEX IF EXISTS `search_idx`,
+	ADD UNIQUE INDEX `admin_filter_search_id_idx` (`employee`, `shop`, `controller`, `action`, `filter_id`)
+;
+
+/* Module Manager tab should be the first tab in Modules Tab */
+UPDATE `PREFIX_tab` SET `position` = 0 WHERE `class_name` = 'AdminModulesSf' AND `position`= 1;
+UPDATE `PREFIX_tab` SET `position` = 1 WHERE `class_name` = 'AdminParentModulesCatalog' AND `position`= 0;
+
+/* Add field MPN to tables */
 ALTER TABLE `PREFIX_order_detail` ADD `product_mpn` VARCHAR(32) NULL AFTER `product_upc`;
 ALTER TABLE `PREFIX_supply_order_detail` ADD `mpn` VARCHAR(32) NULL AFTER `upc`;
 ALTER TABLE `PREFIX_stock` ADD `mpn` VARCHAR(32) NULL AFTER `upc`;
