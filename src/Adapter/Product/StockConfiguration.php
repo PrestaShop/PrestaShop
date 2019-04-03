2b15d78affb67103d2f10d75c0bab1a67a01d50c
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2019-04-03 17:01:34 +0300

    fix multilanguage configuration for Stock preferences

--- src/Adapter/Product/StockConfiguration.php
+++ src/Adapter/Product/StockConfiguration.php
@@ -28,6 +28,7 @@ namespace PrestaShop\PrestaShop\Adapter\Product;
 
 use PrestaShop\PrestaShop\Adapter\Configuration;
 use PrestaShop\PrestaShop\Core\Configuration\DataConfigurationInterface;
+use PrestaShop\PrestaShop\Core\Configuration\LocalizedConfigurationInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
@@ -40,9 +41,15 @@ class StockConfiguration implements DataConfigurationInterface
      */
     private $configuration;
 
-    public function __construct(Configuration $configuration)
+    /**
+     * @var LocalizedConfigurationInterface
+     */
+    private $localizedConfiguration;
+
+    public function __construct(Configuration $configuration, LocalizedConfigurationInterface $localizedConfiguration)
     {
         $this->configuration = $configuration;
+        $this->localizedConfiguration = $localizedConfiguration;
     }
 
     /**
@@ -53,11 +60,11 @@ class StockConfiguration implements DataConfigurationInterface
         return [
             'allow_ordering_oos' => $this->configuration->getBoolean('PS_ORDER_OUT_OF_STOCK'),
             'stock_management' => $this->configuration->getBoolean('PS_STOCK_MANAGEMENT'),
-            'in_stock_label' => $this->configuration->get('PS_LABEL_IN_STOCK_PRODUCTS'),
-            'oos_allowed_backorders' => $this->configuration->get('PS_LABEL_OOS_PRODUCTS_BOA'),
-            'oos_denied_backorders' => $this->configuration->get('PS_LABEL_OOS_PRODUCTS_BOD'),
-            'delivery_time' => (array) $this->configuration->get('PS_LABEL_DELIVERY_TIME_AVAILABLE'),
-            'oos_delivery_time' => (array) $this->configuration->get('PS_LABEL_DELIVERY_TIME_OOSBOA'),
+            'in_stock_label' => $this->localizedConfiguration->get('PS_LABEL_IN_STOCK_PRODUCTS'),
+            'oos_allowed_backorders' => $this->localizedConfiguration->get('PS_LABEL_OOS_PRODUCTS_BOA'),
+            'oos_denied_backorders' => $this->localizedConfiguration->get('PS_LABEL_OOS_PRODUCTS_BOD'),
+            'delivery_time' => (array) $this->localizedConfiguration->get('PS_LABEL_DELIVERY_TIME_AVAILABLE'),
+            'oos_delivery_time' => (array) $this->localizedConfiguration->get('PS_LABEL_DELIVERY_TIME_OOSBOA'),
             'pack_stock_management' => $this->configuration->get('PS_PACK_STOCK_TYPE'),
         ];
     }
