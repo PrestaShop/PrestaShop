6a2d59d2d4c548c8595c9a90b0285e87875c08d0
jonskapel <michjohan@gmail.com>
2019-04-05 09:11:33 +0200

    WS, call hook actionUpdateQuantity when updating
    It turned out, that actionUpdateQuantity is not called, when one puts
    (HTTP PUT) new quantity of a product using webservice request to the
    resource. It happened only for products with id_product_attribute = 0.
    This commit add a call of this hook to updateWS method.

--- classes/stock/StockAvailable.php
+++ classes/stock/StockAvailable.php
@@ -104,7 +104,26 @@ class StockAvailableCore extends ObjectModel
         if ($this->depends_on_stock) {
             return WebserviceRequest::getInstance()->setError(500, $this->trans('You cannot update the available stock when it depends on stock.', array(), 'Admin.Catalog.Notification'), 133);
         }
-        return $this->update();
+        $result =  $this->update();
+		if($this->id_product_attribute == 0){
+			$id_shop = (Shop::getContext() != Shop::CONTEXT_GROUP && $this->id_shop ? $this->id_shop : null);
+			$quantity = (int)Db::getInstance(_PS_USE_SQL_SLAVE_)->getValue('
+			SELECT quantity as quantity
+			FROM '._DB_PREFIX_.'stock_available
+			WHERE id_product = '.(int)$this->id_product.'
+			AND id_product_attribute = 0 '.
+					StockAvailable::addSqlShopRestriction(null, $id_shop)
+					);
+			Hook::exec('actionUpdateQuantity',
+					array(
+							'id_product' => $this->id_product,
+							'id_product_attribute' => 0,
+							'quantity' => (int)$quantity
+					)
+					);
+			Cache::clean('StockAvailable::getQuantityAvailableByProduct_'.(int)$this->id_product.'*');
+		}
+		return $result ;
     }
 
     public static function getStockAvailableIdByProductId($id_product, $id_product_attribute = null, $id_shop = null)
