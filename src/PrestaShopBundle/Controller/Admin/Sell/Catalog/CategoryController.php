f29b885a75fb32f3313d39698ba5c56404a0aba9
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2019-04-08 16:52:15 +0300

    Applies CS fixer

--- src/PrestaShopBundle/Controller/Admin/Sell/Catalog/CategoryController.php
+++ src/PrestaShopBundle/Controller/Admin/Sell/Catalog/CategoryController.php
@@ -50,7 +50,6 @@ use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
 use PrestaShopBundle\Form\Admin\Sell\Category\DeleteCategoriesType;
 use PrestaShopBundle\Security\Annotation\AdminSecurity;
 use PrestaShopBundle\Security\Annotation\DemoRestricted;
-use PrestaShopBundle\Security\Voter\PageVoter;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
