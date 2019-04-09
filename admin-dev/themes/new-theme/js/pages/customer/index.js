84bac8e2c9670e947571d2189a01cb00f66e84fc
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2019-04-09 11:00:31 +0300

    Uses single quote insted of double quote

--- admin-dev/themes/new-theme/js/pages/customer/index.js
+++ admin-dev/themes/new-theme/js/pages/customer/index.js
@@ -32,15 +32,15 @@ import BulkActionCheckboxExtension from '../../components/grid/extension/bulk-ac
 import SubmitBulkExtension from '../../components/grid/extension/submit-bulk-action-extension';
 import SubmitGridExtension from '../../components/grid/extension/submit-grid-action-extension';
 import LinkRowActionExtension from '../../components/grid/extension/link-row-action-extension';
-import LinkableItem from "../../components/linkable-item";
-import ChoiceTable from "../../components/choice-table";
-import ColumnTogglingExtension from "../../components/grid/extension/column-toggling-extension";
+import LinkableItem from '../../components/linkable-item';
+import ChoiceTable from '../../components/choice-table';
+import ColumnTogglingExtension from '../../components/grid/extension/column-toggling-extension';
 import DeleteCustomersBulkActionExtension
-  from "../../components/grid/extension/action/bulk/customer/delete-customers-bulk-action-extension";
+  from '../../components/grid/extension/action/bulk/customer/delete-customers-bulk-action-extension';
 import DeleteCustomerRowActionExtension
-  from "../../components/grid/extension/action/row/customer/delete-customer-row-action-extension";
-import ShowcaseCard from "../../components/showcase-card/showcase-card";
-import ShowcaseCardCloseExtension from "../../components/showcase-card/extension/showcase-card-close-extension";
+  from '../../components/grid/extension/action/row/customer/delete-customer-row-action-extension';
+import ShowcaseCard from '../../components/showcase-card/showcase-card';
+import ShowcaseCardCloseExtension from '../../components/showcase-card/extension/showcase-card-close-extension';
 
 const $ = window.$;
 
