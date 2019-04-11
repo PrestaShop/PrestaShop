ff672ce5e4ac66fe615d05f41727a5c28b26cd43
Mickaël Andrieu <andrieu.travail@gmail.com>
2019-04-11 15:49:02 +0200

    First POC

--- install-dev/controllers/console/process.php
+++ install-dev/controllers/console/process.php
@@ -350,7 +350,7 @@ class InstallControllerConsoleProcess extends InstallControllerConsole implement
         }
         global $kernel;
         $kernel = new AppKernel($env, _PS_MODE_DEV_);
-        $kernel->loadClassCache();
+        $kernel->();
         $kernel->boot();
     }
 }
