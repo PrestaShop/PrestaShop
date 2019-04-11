c3ea5be61c2a62591a94d4c70a548e06a71946a8
Rokas Zygmantas <rokas.zygmantas@invertus.eu>
2019-04-11 18:53:34 +0300

    Add missing security annotations

--- src/PrestaShopBundle/Controller/Admin/TranslationsController.php
+++ src/PrestaShopBundle/Controller/Admin/TranslationsController.php
@@ -49,6 +49,8 @@ class TranslationsController extends FrameworkBundleAdminController
     const controller_name = self::CONTROLLER_NAME;
 
     /**
+     * @AdminSecurity("is_granted(['read'], request.get('_legacy_controller'))")
+     *
      * @Template("@PrestaShop/Admin/Translations/overview.html.twig")
      */
     public function overviewAction()
