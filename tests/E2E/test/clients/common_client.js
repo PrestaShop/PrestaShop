3409cef355fc5bfb82697a3a0be56fb43e082c08
Boubker BRIBRI <boubker.bribri@prestashop.com>
2019-04-19 16:49:47 +0200

    Add Javascript perform click function

--- tests/E2E/test/clients/common_client.js
+++ tests/E2E/test/clients/common_client.js
@@ -824,6 +824,21 @@ class CommonClient {
         .pause(pause)
         .url();
   }
+
+  /**
+   * perform a javascript click
+   * @param selector, xpath of the element
+   * @param pause
+   * @param timeout
+   * @return true, if click works, false otherwise
+   */
+  waitForExistAndClickJs(selector, pause = 0, timeout = 90000) {
+    return this.client
+      .pause(pause)
+      .execute(function (selector) {
+        return document.evaluate(selector,document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click();
+      }, selector);
+  }
 }
 
 module.exports = CommonClient;
