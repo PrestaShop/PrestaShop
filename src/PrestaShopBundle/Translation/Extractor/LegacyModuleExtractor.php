b622cee5ed71b672aad90bd52903d269c064983f
Pablo Borowicz <pablo.borowicz@prestashop.com>
2019-04-19 10:57:23 +0200

    Update LegacyModuleExtractor so that the default domain name is the inferred module domain name instead of "messages"

--- src/PrestaShopBundle/Translation/Extractor/LegacyModuleExtractor.php
+++ src/PrestaShopBundle/Translation/Extractor/LegacyModuleExtractor.php
@@ -27,8 +27,8 @@
 
 namespace PrestaShopBundle\Translation\Extractor;
 
+use PrestaShop\TranslationToolsBundle\Translation\Helper\DomainHelper;
 use Symfony\Component\Translation\Extractor\ExtractorInterface;
-use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\Translation\MessageCatalogue;
 
 /**
@@ -77,14 +77,51 @@ final class LegacyModuleExtractor implements LegacyModuleExtractorInterface
 
     /**
      * {@inheritdoc}
+     *
+     * WARNING: The domains in the returned catalogue are dot-separated
      */
     public function extract($moduleName, $locale)
     {
-        $catalogueForExtraction = new MessageCatalogue($locale);
-        $this->phpExtractor->extract($this->modulesDirectory . '/' . $moduleName, $catalogueForExtraction);
-        $this->smartyExtractor->extract($this->modulesDirectory . '/' . $moduleName, $catalogueForExtraction);
-        $this->twigExtractor->extract($this->modulesDirectory . '/' . $moduleName, $catalogueForExtraction);
+        $extractedCatalogue = new MessageCatalogue($locale);
 
-        return $catalogueForExtraction;
+        $this->phpExtractor->extract($this->modulesDirectory . '/' . $moduleName, $extractedCatalogue);
+        $extractedCatalogue = $this->postprocessPhpCatalogue($extractedCatalogue, $moduleName);
+
+        $this->smartyExtractor->extract($this->modulesDirectory . '/' . $moduleName, $extractedCatalogue);
+        $this->twigExtractor->extract($this->modulesDirectory . '/' . $moduleName, $extractedCatalogue);
+
+        return $extractedCatalogue;
+    }
+
+    /**
+     * Modules usually don't use domain names when calling the l() function in PHP files.
+     * Therefore, the PHP extractor will stores those calls in the default domain named "messages".
+     * This process moves all wordings in the "messages" domain to the inferred module domain.
+     *
+     * @param MessageCatalogue $extractedCatalogue
+     * @param string $moduleName
+     *
+     * @return MessageCatalogue
+     */
+    private function postprocessPhpCatalogue(MessageCatalogue $extractedCatalogue, $moduleName)
+    {
+        $defaultDomain = 'messages';
+
+        if (!in_array($defaultDomain, $extractedCatalogue->getDomains())) {
+            return $extractedCatalogue;
+        }
+
+        $newDomain = DomainHelper::buildModuleDomainFromLegacySource($moduleName, '');
+
+        $allWordings = $extractedCatalogue->all();
+
+        // move default domain into the new domain (avoiding to overwrite existing translations)
+        $allWordings[$newDomain] = (isset($allWordings[$newDomain]))
+            ? array_merge($allWordings[$newDomain], $allWordings[$defaultDomain])
+            : $allWordings[$defaultDomain];
+
+        unset($allWordings[$defaultDomain]);
+
+        return new MessageCatalogue($extractedCatalogue->getLocale(), $allWordings);
     }
 }
