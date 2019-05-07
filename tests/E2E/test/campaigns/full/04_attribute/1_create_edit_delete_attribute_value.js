e1c7052041e24f738863ea1fd1d15a1bd0ef4695
Simon Garny <simon.garny@prestashop.com>
2019-05-07 15:25:45 +0200

    move test 01 from the test suite 04 from high to full

--- tests/E2E/test/campaigns/full/04_attribute/1_create_edit_delete_attribute_value.js
+++ tests/E2E/test/campaigns/full/04_attribute/1_create_edit_delete_attribute_value.js
@@ -11,8 +11,9 @@ const commonAttribute = require('../../common_scenarios/attribute');
 const commonScenarios = require('../../common_scenarios/product');
 const welcomeScenarios = require('../../common_scenarios/welcome');
 
+
 let productData = {
-  name: 'Att2',
+  name: 'Att',
   quantity: "10",
   price: '5',
   image_name: 'image_test.jpg',
@@ -42,7 +43,96 @@ let attributeData = {
   }
 };
 
-require('../../high/04_attribute/1_create_edit_delete_attribute_value');
+let productData2 = {
+  name: 'Att2',
+  quantity: "10",
+  price: '5',
+  image_name: 'image_test.jpg',
+  reference: 'att',
+  attribute: {
+    1: {
+      name: 'attribute',
+      variation_quantity: '10'
+    }
+  }
+};
+
+let attributeData2 = {
+  name: 'attribute',
+  public_name: 'attribute',
+  type: 'radio',
+  values: {
+    1: {
+      value: '10'
+    },
+    2: {
+      value: '20'
+    },
+    3: {
+      value: '30'
+    }
+  }
+};
+
+scenario('Create, edit and delete "Attribute"', () => {
+  /* Create attribute */
+  scenario('Login in the Back Office', client => {
+    test('should open the browser', () => client.open());
+    test('should login successfully in the Back Office', () => client.signInBO(AccessPageBO));
+  }, 'attribute_and_feature');
+  welcomeScenarios.findAndCloseWelcomeModal();
+  commonAttribute.createAttribute(attributeData);
+  commonScenarios.createProduct(AddProductPage, productData);
+
+  /* Check the created attribute */
+  scenario('Go to the Front Office', client => {
+    test('should go to the Front Office', () => client.accessToFO(AccessPageFO));
+  }, 'attribute_and_feature');
+  commonAttribute.checkAttributeInFO(productData.name, attributeData);
+
+  /* Update the created attribute */
+  scenario('Go back to the Back Office', client => {
+    test('should login successfully in the Back Office', () => client.accessToBO(AccessPageBO));
+  }, 'attribute_and_feature');
+  commonAttribute.updateAttribute(attributeData);
+
+  /* Check the updated attribute */
+  scenario('Go back to the Front Office', client => {
+    test('should go back to the Front Office', () => client.accessToFO(AccessPageFO));
+  }, 'attribute_and_feature');
+  commonAttribute.checkAttributeInFO(productData.name, attributeData);
+
+  /* Delete attribute value */
+  scenario('Go back to the Back Office', client => {
+    test('should login successfully in the Back Office', () => client.accessToBO(AccessPageBO));
+  }, 'attribute_and_feature');
+  commonAttribute.deleteAttributeValue(attributeData);
+
+  /* Check the deleted attribute value */
+  scenario('Go back to the Front Office', client => {
+    test('should go back to the Front Office', () => client.accessToFO(AccessPageFO));
+  }, 'attribute_and_feature');
+  commonAttribute.checkAttributeInFO(productData.name, attributeData);
+
+  /* Delete the created attribute */
+  scenario('Go back to the Back Office', client => {
+    test('should login successfully in the Back Office', () => client.accessToBO(AccessPageBO));
+  }, 'attribute_and_feature');
+  commonAttribute.deleteAttribute(attributeData);
+  scenario('Logout from the Back Office', client => {
+    test('should logout successfully from the Back Office', () => client.signOutBO());
+  }, 'attribute_and_feature');
+
+  /* Check the deleted attribute */
+  scenario('Go back to the Front Office', client => {
+    test('should go back to the Front Office', () => client.accessToFO(AccessPageFO));
+  }, 'attribute_and_feature');
+  commonAttribute.checkDeletedAttributeInFO(productData.name);
+  scenario('Logout from the Front Office', client => {
+    test('should logout successfully from the Front Office', () => client.signOutFO(AccessPageFO));
+  }, 'attribute_and_feature');
+}, 'attribute_and_feature', true);
+
 
 scenario('Delete "Attribute" with bulk actions', () => {
 
@@ -51,9 +141,9 @@ scenario('Delete "Attribute" with bulk actions', () => {
     test('should login successfully in the Back Office', () => client.signInBO(AccessPageBO));
   }, 'attribute_and_feature');
   welcomeScenarios.findAndCloseWelcomeModal();
-  commonAttribute.createAttribute(attributeData);
-  commonScenarios.createProduct(AddProductPage, productData);
-  commonAttribute.attributeBulkActions(attributeData, 'delete');
+  commonAttribute.createAttribute(attributeData2);
+  commonScenarios.createProduct(AddProductPage, productData2);
+  commonAttribute.attributeBulkActions(attributeData2, 'delete');
 
   scenario('Logout from the Back Office', client => {
     test('should logout successfully from the Back Office', () => client.signOutBO());
@@ -62,6 +152,6 @@ scenario('Delete "Attribute" with bulk actions', () => {
   scenario('Go back to the Front Office', client => {
     test('should go back to the Front Office', () => client.accessToFO(AccessPageFO));
   }, 'attribute_and_feature');
-  commonAttribute.checkDeletedAttributeInFO(productData.name);
+  commonAttribute.checkDeletedAttributeInFO(productData2.name);
 
 }, 'attribute_and_feature', true);
