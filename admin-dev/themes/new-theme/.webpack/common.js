eab51c78c75a465ec9d1e9a410e8d77a1974e8a5
Julius Zukauskas <zuk3975@gmail.com>
2019-05-09 17:59:06 +0300

    Enables javascript for create form

--- admin-dev/themes/new-theme/.webpack/common.js
+++ admin-dev/themes/new-theme/.webpack/common.js
@@ -50,6 +50,7 @@ module.exports = {
     form_popover_error: './js/components/form/form-popover-error',
     manufacturer: './js/pages/manufacturer',
     manufacturer_address_form: './js/pages/manufacturer/manufacturer_address_form.js',
+    catalog_price_rule_form: './js/pages/catalog-price-rule/form',
   },
   output: {
     path: path.resolve(__dirname, '../public'),
