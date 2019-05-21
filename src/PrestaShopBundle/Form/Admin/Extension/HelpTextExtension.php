1a00518d90136db707ad1da7ca24ed11e4c7e7b7
Tomas <tilginis@gmail.com>
2019-05-21 13:13:48 +0300

    Update src/PrestaShopBundle/Form/Admin/Extension/HelpTextExtension.php
    adds isset to options param
    
    Co-Authored-By: GoT <PierreRambaud@users.noreply.github.com>

--- src/PrestaShopBundle/Form/Admin/Extension/HelpTextExtension.php
+++ src/PrestaShopBundle/Form/Admin/Extension/HelpTextExtension.php
@@ -56,7 +56,7 @@ class HelpTextExtension extends AbstractTypeExtension
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
         if (isset($options['help'])) {
-            $view->vars['help'] = $options['help'];
+            $view->vars['help'] = isset($options['help']) ? $options['help'] : null;
         }
     }
 
