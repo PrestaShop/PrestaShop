bafe5d157ba4abbf73b6d5f5fbb0f61a8c526d29
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2019-05-22 14:05:49 +0300

    Fixes saving private note about customer from order page

--- src/PrestaShopBundle/Controller/Admin/Sell/Customer/CustomerController.php
+++ src/PrestaShopBundle/Controller/Admin/Sell/Customer/CustomerController.php
@@ -263,7 +263,7 @@ class CustomerController extends AbstractAdminController
      * @param int $customerId
      * @param Request $request
      *
-     * @return RedirectResponse
+     * @return Response
      */
     public function savePrivateNoteAction($customerId, Request $request)
     {
@@ -278,6 +278,13 @@ class CustomerController extends AbstractAdminController
                     (int) $customerId,
                     $data['note']
                 ));
+
+                if ($request->isXmlHttpRequest()) {
+                    return $this->json([
+                        'success' => true,
+                        'message' => $this->trans('Successful update.', 'Admin.Notifications.Success'),
+                    ]);
+                }
 
                 $this->addFlash('success', $this->trans('Successful update.', 'Admin.Notifications.Success'));
             } catch (CustomerException $e) {
