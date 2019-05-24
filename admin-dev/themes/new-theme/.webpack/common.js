0609a2185f64c4a5b9df436c16182e71a2dac574
Rokas Zygmantas <rokas.zygmantas@invertus.eu>
2019-05-24 16:07:38 +0300

    Rename JS entry

--- admin-dev/themes/new-theme/.webpack/common.js
+++ admin-dev/themes/new-theme/.webpack/common.js
@@ -30,6 +30,7 @@ module.exports = {
     invoices: './js/pages/invoices',
     language: './js/pages/language',
     localization: './js/pages/localization',
+    login: './js/pages/login/index',
     logs: './js/pages/logs',
     main: './js/theme.js',
     maintenance: './js/pages/maintenance',
@@ -54,7 +55,6 @@ module.exports = {
     webservice: './js/pages/webservice',
     error: './js/pages/error',
     monitoring: './js/pages/monitoring',
-    login_page: './js/pages/login/index',
   },
   output: {
     path: path.resolve(__dirname, '../public'),
