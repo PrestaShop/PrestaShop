fcf2a417b24388f9f91960729788be165531c485
Pablo Borowicz <pablo.borowicz@prestashop.com>
2019-06-03 17:15:01 +0200

    Fix test failing because trying to mock a final class

--- tests-legacy/Unit/Core/Localization/DataLayer/CurrencyReferenceTest.php
+++ tests-legacy/Unit/Core/Localization/DataLayer/CurrencyReferenceTest.php
@@ -29,11 +29,11 @@ namespace LegacyTests\Unit\Core\Localization\DataLayer;
 use PHPUnit\Framework\TestCase;
 use PrestaShop\PrestaShop\Core\Localization\CLDR\Currency as CldrCurrency;
 use PrestaShop\PrestaShop\Core\Localization\CLDR\CurrencyData as CldrCurrencyData;
-use PrestaShop\PrestaShop\Core\Localization\CLDR\Locale as CldrLocale;
 use PrestaShop\PrestaShop\Core\Localization\CLDR\LocaleRepository as CldrLocaleRepository;
 use PrestaShop\PrestaShop\Core\Localization\Currency\CurrencyData;
 use PrestaShop\PrestaShop\Core\Localization\Currency\LocalizedCurrencyId;
 use PrestaShop\PrestaShop\Core\Localization\Currency\DataLayer\CurrencyReference as CurrencyReferenceDataLayer;
+use PrestaShop\PrestaShop\Core\Localization\CLDR\LocaleInterface as CldrLocaleInterface;
 
 class CurrencyReferenceTest extends TestCase
 {
@@ -42,7 +42,7 @@ class CurrencyReferenceTest extends TestCase
      *
      * @var CurrencyReferenceDataLayer
      */
-    protected $layer;
+    protected $currencyReference;
 
     protected function setUp()
     {
@@ -50,8 +50,9 @@ class CurrencyReferenceTest extends TestCase
         $stubCurrencyData->setIsoCode('PCE');
         $stubCldrCurrency          = new CldrCurrency($stubCurrencyData);
 
-        $stubLocale = $this->createMock(CldrLocale::class);
-        $stubLocale->method('getCurrency')
+        $stubLocale = $this->createMock(CldrLocaleInterface::class);
+        $stubLocale
+            ->method('getCurrency')
             ->willReturnMap([
                 ['PCE', $stubCldrCurrency],
                 ['unknown', null],
@@ -67,7 +68,7 @@ class CurrencyReferenceTest extends TestCase
             ]);
 
         /** @var CldrLocaleRepository $cldrLocaleRepo */
-        $this->layer = new CurrencyReferenceDataLayer($cldrLocaleRepo, 'fr-FR');
+        $this->currencyReference = new CurrencyReferenceDataLayer($cldrLocaleRepo);
     }
 
     /**
@@ -77,25 +78,21 @@ class CurrencyReferenceTest extends TestCase
      */
     public function testRead()
     {
-        /** @noinspection PhpUnhandledExceptionInspection */
-        $currencyData = $this->layer->read(new LocalizedCurrencyId('PCE', 'fr-FR'));
-        /** @noinspection end */
+        /** @var CurrencyData $currencyData */
+        $currencyData = $this->currencyReference->read(new LocalizedCurrencyId('PCE', 'fr-FR'));
 
         $this->assertInstanceOf(
             CurrencyData::class,
             $currencyData
         );
 
-        /** @var CurrencyData $currencyData */
         $this->assertSame(
             'PCE',
             $currencyData->getIsoCode()
         );
 
         // Same test with unknown cache key
-        /** @noinspection PhpUnhandledExceptionInspection */
-        $currencyData = $this->layer->read(new LocalizedCurrencyId('unknown', 'unknown'));
-        /** @noinspection end */
+        $currencyData = $this->currencyReference->read(new LocalizedCurrencyId('unknown', 'unknown'));
 
         $this->assertNull($currencyData);
     }
