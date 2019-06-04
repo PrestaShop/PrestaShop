f83b81f823d3a0e79a7d5e0572acf00dcc51c3f2
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2019-06-04 10:34:56 +0300

    Handles not cart not found error

--- src/PrestaShopBundle/Controller/Admin/Sell/Order/CartController.php
+++ src/PrestaShopBundle/Controller/Admin/Sell/Order/CartController.php
@@ -26,6 +26,7 @@
 
 namespace PrestaShopBundle\Controller\Admin\Sell\Order;
 
+use PrestaShop\PrestaShop\Core\Domain\Cart\Exception\CartNotFoundException;
 use PrestaShop\PrestaShop\Core\Domain\Cart\Query\GetCartForViewing;
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
 use PrestaShopBundle\Security\Annotation\AdminSecurity;
@@ -34,6 +35,14 @@ use Symfony\Component\HttpFoundation\Response;
 
 class CartController extends FrameworkBundleAdminController
 {
+    /**
+     * @return Response
+     */
+    public function indexAction()
+    {
+        return $this->render('@PrestaShop/Admin/Sell/Order/Cart/index.html.twig');
+    }
+
     /**
      * @AdminSecurity("is_granted('read', request.get('_legacy_controller'))")
      *
@@ -44,7 +53,14 @@ class CartController extends FrameworkBundleAdminController
      */
     public function viewAction(Request $request, $cartId)
     {
-        $cartView = $this->getQueryBus()->handle(new GetCartForViewing((int) $cartId));
+        try {
+            $cartView = $this->getQueryBus()->handle(new GetCartForViewing((int) $cartId));
+        } catch (CartNotFoundException $e) {
+            $this->addFlash('error', $this->getErrorMessageForException($e, $this->getErrorMessages()));
+
+            return $this->redirectToRoute('admin_carts_index');
+        }
+
         $cartKpi = $this->get('prestashop.core.kpi_row.factory.cart')->build([
             'cart_id' => $cartId,
         ]);
@@ -56,5 +72,16 @@ class CartController extends FrameworkBundleAdminController
             'help_link' => $this->generateSidebarLink($request->attributes->get('_legacy_controller')),
             'cartKpi' => $cartKpi,
         ]);
+    }
+
+    /**
+     * @return array
+     */
+    private function getErrorMessages()
+    {
+        return [
+            CartNotFoundException::class =>
+                $this->trans('The object cannot be loaded (or found)', 'Admin.Notifications.Error'),
+        ];
     }
 }
