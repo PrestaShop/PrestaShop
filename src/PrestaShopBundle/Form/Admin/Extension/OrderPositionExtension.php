792e0a98268dad7dd2253a539bd4dabb1acb68d3
Tomas Ilginis <tilginis@gmail.com>
2019-06-06 10:56:14 +0300

    adds fake order position extension

---
+++ src/PrestaShopBundle/Form/Admin/Extension/OrderPositionExtension.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace PrestaShopBundle\Form\Admin\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrderPositionExtension extends AbstractTypeExtension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver
+            ->setDefaults([
+                'order' => null, // cant thing of better name
+            ])
+            ->setAllowedTypes('order', ['null', 'int'])
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function finishView(FormView $view, FormInterface $form, array $options)
+    {
+        if ($form->isRoot()) {
+//            here I'll reiterate over form elements and set the order to form element view.
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExtendedType()
+    {
+        return FormType::class;
+    }
+}
