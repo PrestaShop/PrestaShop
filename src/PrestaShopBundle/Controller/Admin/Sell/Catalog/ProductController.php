9f9be18db54e97935151e08822bd995e045a2e57
Tomas Ilginis <tilginis@gmail.com>
2019-06-06 16:04:54 +0300

    adds product list grid definition

---
+++ src/PrestaShopBundle/Controller/Admin/Sell/Catalog/ProductController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PrestaShopBundle\Controller\Admin\Sell\Catalog;
+
+use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
+
+/**
+ * Manages "Sell -> Catalog -> Products" page actions.
+ */
+class ProductController extends FrameworkBundleAdminController
+{
+
+}
