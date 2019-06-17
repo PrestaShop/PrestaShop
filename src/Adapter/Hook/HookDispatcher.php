0a8dd7aa0fb3ad6b4c372478dba3e8d3140f70f9
Tomas Ilginis <tilginis@gmail.com>
2019-06-17 15:57:29 +0300

    adds lower case support when dispatching a hook in any form

--- src/Adapter/Hook/HookDispatcher.php
+++ src/Adapter/Hook/HookDispatcher.php
@@ -26,6 +26,7 @@
 
 namespace PrestaShop\PrestaShop\Adapter\Hook;
 
+use PrestaShop\PrestaShop\Core\Foundation\IoC\Container;
 use PrestaShop\PrestaShop\Core\Hook\Hook;
 use PrestaShop\PrestaShop\Core\Hook\HookDispatcherInterface;
 use PrestaShop\PrestaShop\Core\Hook\HookInterface;
@@ -66,11 +67,16 @@ class HookDispatcher extends EventDispatcher implements HookDispatcherInterface
         if ($event === null) {
             $event = new HookEvent();
         }
+
         if (!$event instanceof HookEvent) {
             throw new \Exception('HookDispatcher must dispatch a HookEvent subclass only. ' . get_class($event) . ' given.');
         }
 
-        return parent::dispatch($eventName, $event);
+        if ($listeners = $this->getListeners(strtolower($eventName))) {
+            $this->doDispatch($listeners, $eventName, $event);
+        }
+
+        return $event;
     }
 
     /**
