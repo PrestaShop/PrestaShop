80285b3ea19fa9c5cd2466ab67b040d36e56270d
Jonathan Lelievre <jo.lelievre@gmail.com>
2019-06-20 11:50:08 +0200

    Fix carrier display in order confirmation email

--- classes/PaymentModule.php
+++ classes/PaymentModule.php
@@ -569,6 +569,7 @@ abstract class PaymentModuleCore extends Module
                         $delivery = new Address((int) $order->id_address_delivery);
                         $delivery_state = $delivery->id_state ? new State((int) $delivery->id_state) : false;
                         $invoice_state = $invoice->id_state ? new State((int) $invoice->id_state) : false;
+                        $carrier = $order->id_carrier ? new Carrier($order->id_carrier) : false;
 
                         $data = array(
                             '{firstname}' => $this->context->customer->firstname,
