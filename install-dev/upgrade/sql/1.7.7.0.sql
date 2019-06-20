287f83326cfdb2a5abbd80318b0ca093ece8cf19
Rokas Zygmantas <rokas.zygmantas@invertus.eu>
2019-06-20 17:01:05 +0300

    Restore removed installation sql queries and put them into 1.7.7.0 migration

--- install-dev/upgrade/sql/1.7.7.0.sql
+++ install-dev/upgrade/sql/1.7.7.0.sql
@@ -2,3 +2,5 @@ SET SESSION sql_mode='';
 SET NAMES 'utf8';
 
 ALTER TABLE `PREFIX_search_word` MODIFY `word` VARCHAR(30) NOT NULL;
+DELETE FROM `PREFIX_tab` WHERE `class_name` = 'AdminLogin';
+DELETE FROM `PREFIX_hook` WHERE `name` = 'actionAdminLoginControllerSetMedia';
