4c779c9bd7aaa94b527d9b8af0e67c4ecc8b83f7
Tomas Ilginis <tilginis@gmail.com>
2019-06-25 16:47:32 +0300

    WIP -writting tests for product actions

---
+++ tests/Integration/Behaviour/Features/Context/Domain/ProductFeatureContext.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Integration\Behaviour\Features\Context\Domain;
+
+use Behat\Behat\Tester\Exception\PendingException;
+use PrestaShop\PrestaShop\Core\Domain\Product\Command\ToggleProductStatusCommand;
+use Product;
+use RuntimeException;
+use Tests\Integration\Behaviour\Features\Context\SharedStorage;
+
+class ProductFeatureContext extends AbstractDomainFeatureContext
+{
+
+    /**
+     * @Given /^product "([^"]*)" with id product "([^"]*)" exists$/
+     */
+    public function assertProductWithIdProductExists($storageReference, $productId)
+    {
+        $product = new Product($productId);
+
+        if (0 >= $product->id) {
+            throw new RuntimeException(
+                sprintf(
+                    'Product with id %s does not exist',
+                    $productId
+                )
+            );
+        }
+
+        SharedStorage::getStorage()->set($storageReference, $product);
+    }
+
+    /**
+     * @When /^I toggle status of product "([^"]*)"$/
+     */
+    public function toggleStatus($storageReference)
+    {
+        /** @var Product $currency */
+        $product = SharedStorage::getStorage()->get($storageReference);
+
+        $this->getCommandBus(new ToggleProductStatusCommand((int) $product->id));
+    }
+
+    /**
+     * @Then /^product "([^"]*)" should have status "([^"]*)"$/
+     */
+    public function assertProductShouldHaveStatus($storageReference, $expectedStatus)
+    {
+        /** @var Product $currency */
+        $product = SharedStorage::getStorage()->get($storageReference);
+
+        if ((int) $product->active !== (int) $expectedStatus) {
+            throw new RuntimeException(
+                sprintf(
+                    'Expected status %s but received %s',
+                    (int) $expectedStatus,
+                    (int) $product->active
+                )
+            );
+        }
+    }
+}
