06a0d1e437b0b48f54c7b52f45565565544e8a81
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2019-06-27 13:56:19 +0300

    Configured webpack for view page

--- admin-dev/themes/new-theme/.webpack/common.js
+++ admin-dev/themes/new-theme/.webpack/common.js
@@ -52,6 +52,7 @@ module.exports = {
     translation_settings: './js/pages/translation-settings',
     translations: './js/app/pages/translations',
     webservice: './js/pages/webservice',
+    customer_thread_view: './js/pages/customer-thread/view.js'
   },
   output: {
     path: path.resolve(__dirname, '../public'),
