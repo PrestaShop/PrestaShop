d12a79295fa8f4a56eed267bddf7f414fc2c7337
Rokas Zygmantas <rokas.zygmantas@invertus.eu>
2019-06-27 16:11:37 +0300

    Implement feature creation form and handling

---
+++ src/PrestaShopBundle/Controller/Admin/Sell/Catalog/FeatureController.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * 2007-2019 PrestaShop and Contributors
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2019 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+namespace PrestaShopBundle\Controller\Admin\Sell\Catalog;
+
+use Exception;
+use PrestaShop\PrestaShop\Core\Domain\Feature\Exception\FeatureConstraintException;
+use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Controller responsible for "Sell > Catalog > Attributes & Features > Features" page
+ */
+class FeatureController extends FrameworkBundleAdminController
+{
+    /**
+     * Create feature action.
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function createAction(Request $request)
+    {
+        $featureFormBuilder = $this->get('prestashop.core.form.identifiable_object.builder.feature_form_builder');
+        $featureFormHandler = $this->get('prestashop.core.form.identifiable_object.handler.feature_form_handler');
+
+        $featureForm = $featureFormBuilder->getForm();
+        $featureForm->handleRequest($request);
+
+        try {
+            $handlerResult = $featureFormHandler->handle($featureForm);
+
+            if (null !== $handlerResult->getIdentifiableObjectId()) {
+                $this->addFlash('success', $this->trans('Successful creation.', 'Admin.Notifications.Success'));
+
+                //@todo change route to index when it's migrated
+                return $this->redirectToRoute('admin_features_create');
+            }
+        } catch (Exception $e) {
+            $this->addFlash('error', $this->getErrorMessageForException($e, $this->getErrorMessages()));
+        }
+
+        return $this->render('@PrestaShop/Admin/Sell/Catalog/Features/create.html.twig', [
+            'featureForm' => $featureForm->createView(),
+        ]);
+    }
+
+
+    /**
+     * Get translated error messages for feature exceptions
+     *
+     * @return array
+     */
+    private function getErrorMessages()
+    {
+        return [
+            FeatureConstraintException::class => [
+                FeatureConstraintException::EMPTY_NAME => $this->trans('', 'Admin.Notifications.Error'),
+            ],
+        ];
+    }
+}
