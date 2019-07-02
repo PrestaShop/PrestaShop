12573bdba06e630f1a45809d1504d6aa53876a70
Tomas Ilginis <tilginis@gmail.com>
2019-07-02 14:40:56 +0300

    adds some behat tests for configuration turn on and off functionality

--- tests/Integration/Behaviour/Features/Context/ProductFeatureContext.php
+++ tests/Integration/Behaviour/Features/Context/ProductFeatureContext.php
@@ -2,6 +2,11 @@
 
 namespace Tests\Integration\Behaviour\Features\Context;
 
+use Behat\Behat\Tester\Exception\PendingException;
+use PrestaShop\PrestaShop\Core\Grid\Definition\Factory\GridDefinitionFactoryInterface;
+use PrestaShop\PrestaShop\Core\Grid\Definition\Factory\ProductGridDefinitionFactory;
+use RuntimeException;
+
 class ProductFeatureContext extends AbstractPrestaShopFeatureContext
 {
     /**
@@ -19,6 +24,54 @@ class ProductFeatureContext extends AbstractPrestaShopFeatureContext
 
         if (!$filesystem->exists($purifierCacheDirectory)) {
             $filesystem->mkdir($purifierCacheDirectory);
+        }
+    }
+
+    /**
+     * @Then /^grid definition "([^"]*)" should contain column with id "([^"]*)"$/
+     */
+    public function assertGridDefinitionShouldContainColumnWithId($gridDefinitionId, $columnId)
+    {
+        /** @var GridDefinitionFactoryInterface $gridDefinition */
+        $gridDefinition = CommonFeatureContext::getContainer()->get($gridDefinitionId);
+
+        /** @var array $columns */
+        $columns = $gridDefinition->getDefinition()->getColumns()->toArray();
+        $columnIds = array_column($columns, 'id');
+
+        if (!in_array($columnId, $columnIds, true)) {
+            throw new RuntimeException(
+                sprintf(
+                    'For grid definition "%s" missing columnId "%s".',
+                    $gridDefinitionId,
+                    $columnId
+                )
+            );
+        }
+    }
+
+    /**
+     * @todo: ask why this part does not work.
+     *
+     * @Then /^grid definition "([^"]*)" should not contain column with id "([^"]*)"$/
+     */
+    public function assertGridDefinitionShouldNotContainColumnWithId($gridDefinitionId, $columnId)
+    {
+        /** @var ProductGridDefinitionFactory $gridDefinition */
+        $gridDefinition = CommonFeatureContext::getContainer()->get($gridDefinitionId);
+
+        /** @var array $columns */
+        $columns = $gridDefinition->getDefinition()->getColumns()->toArray();
+        $columnIds = array_column($columns, 'id');
+
+        if (in_array($columnId, $columnIds, true)) {
+            throw new RuntimeException(
+                sprintf(
+                    'For grid definition "%s"  columnId "%s" exists',
+                    $gridDefinitionId,
+                    $columnId
+                )
+            );
         }
     }
 }
