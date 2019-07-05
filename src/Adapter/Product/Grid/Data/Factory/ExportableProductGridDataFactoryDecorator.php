7e85d1176523ec5f02a4cc30f87cbfb8e6c8cacf
Tomas Ilginis <tilginis@gmail.com>
2019-07-05 14:06:22 +0300

    creates exportable data factory decorator which changes image

--- src/Adapter/Product/Grid/Data/Factory/ExportableProductGridDataFactoryDecorator.php
+++ src/Adapter/Product/Grid/Data/Factory/ExportableProductGridDataFactoryDecorator.php
@@ -2,7 +2,10 @@
 
 namespace PrestaShop\PrestaShop\Adapter\Product\Grid\Data\Factory;
 
+use Link;
 use PrestaShop\PrestaShop\Core\Grid\Data\Factory\GridDataFactoryInterface;
+use PrestaShop\PrestaShop\Core\Grid\Data\GridData;
+use PrestaShop\PrestaShop\Core\Grid\Record\RecordCollection;
 use PrestaShop\PrestaShop\Core\Grid\Search\SearchCriteriaInterface;
 
 /**
@@ -11,10 +14,58 @@ use PrestaShop\PrestaShop\Core\Grid\Search\SearchCriteriaInterface;
 final class ExportableProductGridDataFactoryDecorator implements GridDataFactoryInterface
 {
     /**
+     * @var GridDataFactoryInterface
+     */
+    private $productGridDataFactory;
+
+    /**
+     * @var Link
+     */
+    private $link;
+
+    /**
+     * @param GridDataFactoryInterface $productGridDataFactory
+     * @param Link $link
+     */
+    public function __construct(
+        GridDataFactoryInterface $productGridDataFactory,
+        Link $link
+    ) {
+        $this->productGridDataFactory = $productGridDataFactory;
+        $this->link = $link;
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function getData(SearchCriteriaInterface $searchCriteria)
     {
-        // TODO: Implement getData() method.
+        $productData = $this->productGridDataFactory->getData($searchCriteria);
+
+        $modifiedRecords = $this->applyModification(
+            $productData->getRecords()->all()
+        );
+
+        return new GridData(
+            new RecordCollection($modifiedRecords),
+            $productData->getRecordsTotal(),
+            $productData->getQuery()
+        );
+    }
+
+    /**
+     * Applies modifications.
+     *
+     * @param array $products
+     *
+     * @return array
+     */
+    private function applyModification(array $products)
+    {
+        foreach ($products as $i => $product) {
+            $products[$i]['image'] = $this->link->getImageLink($product['link_rewrite'], $product['id_image']);
+        }
+
+        return $products;
     }
 }
