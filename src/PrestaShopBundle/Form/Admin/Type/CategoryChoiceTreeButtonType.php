14dddbdc0d455d40b5854083ec7a7676c87c59ab
Tomas Ilginis <tilginis@gmail.com>
2019-07-09 17:49:39 +0300

    adds template, adds type for category tree button

---
+++ src/PrestaShopBundle/Form/Admin/Type/CategoryChoiceTreeButtonType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace PrestaShopBundle\Form\Admin\Type;
+
+use PrestaShopBundle\Form\Admin\Type\Material\MaterialChoiceTreeType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Displays button which has category tree pre-rendered.
+ */
+class CategoryChoiceTreeButtonType extends AbstractType
+{
+    /**
+     * @var array
+     */
+    private $categoryTreeChoices;
+
+    /**
+     * @param array $categoryTreeChoices
+     */
+    public function __construct(array $categoryTreeChoices)
+    {
+        $this->categoryTreeChoices = $categoryTreeChoices;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'choices_tree' => $this->categoryTreeChoices,
+            'choice_label' => 'name',
+            'choice_value' => 'id_category',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParent()
+    {
+        return MaterialChoiceTreeType::class;
+    }
+}
