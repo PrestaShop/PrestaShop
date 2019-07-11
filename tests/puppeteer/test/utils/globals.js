c1ca2868b209916ac741974ed7994174a9bcfe5c
Simon Garny <simon.garny@prestashop.com>
2019-07-11 16:02:32 +0200

    added configuration for mocha

---
+++ tests/puppeteer/test/utils/globals.js
@@ -0,0 +1,7 @@
+global.URL_FO = process.env.URL_FO || 'http://localhost/prestashop/';
+global.URL_BO = process.env.URL_BO || URL_FO + 'admin-dev/';
+global.EMAIL = process.env.LOGIN || 'demo@prestashop.com';
+global.PASSWD = process.env.PASSWD || 'prestashop_demo';
+global.HEADLESS = process.env.HEADLESS || true;
+global.browser = null;
+global.page = null;
