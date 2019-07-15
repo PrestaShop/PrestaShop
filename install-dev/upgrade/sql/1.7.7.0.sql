ecaa7a9029a082d233d3a1a3e29e20e91c0f5134
Pierre RAMBAUD <pierre.rambaud@prestashop.com>
2019-07-15 12:02:39 +0200

    Use utf8mb4 for database

--- install-dev/upgrade/sql/1.7.7.0.sql
+++ install-dev/upgrade/sql/1.7.7.0.sql
@@ -1,4 +1,14 @@
+<<<<<<< HEAD
 SET SESSION sql_mode='';
 SET NAMES 'utf8';
 
 ALTER TABLE `PREFIX_search_word` MODIFY `word` VARCHAR(30) NOT NULL;
+=======
+SET SESSION sql_mode = '';
+SET NAMES 'utf8';
+
+ALTER DATABASE
+    DB_NAME
+    CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
+>>>>>>> Use utf8mb4 for database
