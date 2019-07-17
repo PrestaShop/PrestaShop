9541c98c711cfa07824514b3e6980a8320384bce
Simon Garny <simon.garny@prestashop.com>
2019-07-17 17:10:04 +0200

    add window size and linter recommandations

--- tests/puppeteer/test/utils/helpers.js
+++ tests/puppeteer/test/utils/helpers.js
@@ -1,30 +1,32 @@
 require('./globals');
 const chai = require('chai');
 chai.use(require('chai-string'));
+
 global.expect = chai.expect;
 
 const puppeteer = require('puppeteer');
 
-global.test = (name, instructions) => it(name, () => {
-  return instructions().catch();
-});
+global.test = (name, instructions) => it(name, () => instructions().catch());
 
-global.scenario = (name, tests, init, close = false) =>
-  describe(name, async () => {
-    before(async () => {
-      global.browser = await puppeteer.launch({
-        headless: global.HEADLESS,
-        timeout: 0,
-        args: ['--start-maximized', '--window-size=1920,1040', '--no-sandbox']
-      });
-      await init();
+global.scenario = (name, tests, init, close = false) => describe(name, async () => {
+  before(async () => {
+    global.browser = await puppeteer.launch({
+      headless: global.HEADLESS,
+      timeout: 0,
+      args: ['--start-maximized', '--no-sandbox'],
+      defaultViewport: {
+        width: 1270,
+        height: 899
+      },
     });
-
-    await tests();
-
-    if (close) {
-      await after(async () => {
-        await global.browser.close();
-      });
-    }
+    await init();
   });
+
+  await tests();
+
+  if (close) {
+    await after(async () => {
+      await global.browser.close();
+    });
+  }
+});
