28f7a6b4189d7a91318c283a714415a701224b9b
Boubker BRIBRI <boubker.bribri@prestashop.com>
2019-07-19 10:50:44 +0200

    Adding common functions needed

--- tests/puppeteer/pages/commonPage.js
+++ tests/puppeteer/pages/commonPage.js
@@ -10,4 +10,51 @@ module.exports = class CommonPage {
   async goTo(URL) {
     await this.page.goto(URL);
   }
+
+  /**
+   * Get Text from element
+   * @param selector, from where to get text
+   * @return textContent
+   */
+  async getTextContent(selector) {
+    return this.page.$eval(selector, el => el.textContent);
+  }
+
+  /**
+   * Is checkBox have checked status
+   * @param selector, checkbox to check
+   * @return boolean, true if checked, false if not
+   */
+  async elementChecked(selector) {
+    return this.page.$eval(selector, el => el.checked);
+  }
+
+  /**
+   * Is element visible
+   * @param selector, element to check
+   * @return boolean, true if visible, false if not
+   */
+  async elementVisible(selector) {
+    try {
+      await this.page.waitForSelector(selector, {visible: true, timeout: 10});
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  /**
+   * Open link in new Tab and get opened Page
+   * @param currentPage, current page where to click on selector
+   * @param selector, where to click
+   * @return newPage, what was opened by the browser
+   */
+  async openLinkWithTargetBlank(currentPage, selector) {
+    const pageTarget = await currentPage.target();
+    await currentPage.click(selector);
+    const newTarget = await global.browser.waitForTarget(target => target.opener() === pageTarget);
+    this.page = await newTarget.page();
+    await this.page.waitForSelector('body');
+    return this.page;
+  }
 };
