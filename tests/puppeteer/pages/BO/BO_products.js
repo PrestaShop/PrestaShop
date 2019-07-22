305680711c728f79ab25c49ccef18c0e5092b16e
Simon Garny <simon.garny@prestashop.com>
2019-07-22 17:38:44 +0200

    first version of the BO crawling smoke test

---
+++ tests/puppeteer/pages/BO/BO_products.js
@@ -0,0 +1,13 @@
+const CommonPage = require('../commonPage');
+
+module.exports = class BO_PRODUCTS extends CommonPage {
+  constructor(page) {
+    super(page);
+
+    this.pageTitle = 'Products •';
+  }
+
+  /*
+  Methods
+   */
+};
