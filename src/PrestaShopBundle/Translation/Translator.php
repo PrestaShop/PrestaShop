7887326ca764499ee1183d6c66c88159620036b3
Pablo Borowicz <pablo.borowicz@prestashop.com>
2019-07-24 13:38:06 +0200

    Explain the two translators

--- src/PrestaShopBundle/Translation/Translator.php
+++ src/PrestaShopBundle/Translation/Translator.php
@@ -29,6 +29,9 @@ namespace PrestaShopBundle\Translation;
 
 use Symfony\Bundle\FrameworkBundle\Translation\Translator as BaseTranslator;
 
+/**
+ * Replaces the original Symfony FrameworkBundle translator
+ */
 class Translator extends BaseTranslator
 {
     use PrestaShopTranslatorTrait;
