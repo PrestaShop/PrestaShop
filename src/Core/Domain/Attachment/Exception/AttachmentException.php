2805edb4bcf006559ef5edbb1f036d68d74fd845
Raimondas <raimondas.sapola@invertus.eu>
2019-07-24 17:59:45 +0300

    Delete single row and dynamic delete confirmation message and refactoring File -> Attachment

--- src/Core/Domain/Attachment/Exception/AttachmentException.php
+++ src/Core/Domain/Attachment/Exception/AttachmentException.php
@@ -1,4 +1,5 @@
-{#**
+<?php
+/**
  * 2007-2019 PrestaShop and Contributors
  *
  * NOTICE OF LICENSE
@@ -21,23 +22,15 @@
  * @copyright 2007-2019 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
- *#}
+ */
 
-{% extends 'PrestaShopBundle:Admin:layout.html.twig' %}
+namespace PrestaShop\PrestaShop\Core\Domain\Attachment\Exception;
 
-{% block content %}
-  {% block attachments_listing %}
-    <div class="row">
-      <div class="col">
-        {% include '@PrestaShop/Admin/Common/Grid/grid_panel.html.twig' with {'grid': attachmentGrid} %}
-      </div>
-    </div>
-  {% endblock %}
-{% endblock %}
+use PrestaShop\PrestaShop\Core\Domain\Exception\DomainException;
 
-{% block javascripts %}
-  {{ parent() }}
-
-  <script src="{{ asset('themes/default/js/bundle/pagination.js') }}"></script>
-  <script src="{{ asset('themes/new-theme/public/attachment.bundle.js') }}"></script>
-{% endblock %}
+/**
+ * Class AttachmentException base exception for Attachment subdomain
+ */
+class AttachmentException extends DomainException
+{
+}
