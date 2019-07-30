7052306290136810ffe8554fe3d967076564a0de
Raimondas <raimondas.sapola@invertus.eu>
2019-07-30 15:15:45 +0300

    Build assets

--- admin-dev/themes/new-theme/.webpack/common.js
+++ admin-dev/themes/new-theme/.webpack/common.js
@@ -54,6 +54,7 @@ module.exports = {
     webservice: './js/pages/webservice',
     error: './js/pages/error',
     monitoring: './js/pages/monitoring',
+    attachment: './js/pages/attachment',
   },
   output: {
     path: path.resolve(__dirname, '../public'),
