e8d67acecebeef370d5dfc74f3b2f074457ff66e
Raimondas <raimondas.sapola@invertus.eu>
2019-08-06 14:52:42 +0300

    added missing type hints

--- src/Core/Domain/Attachment/ValueObject/AttachmentId.php
+++ src/Core/Domain/Attachment/ValueObject/AttachmentId.php
@@ -40,37 +40,17 @@ class AttachmentId
 
     /**
      * @param int $id
-     *
-     * @throws AttachmentConstraintException
      */
-    public function __construct($id)
+    public function __construct(int $id)
     {
-        $this->assertIsIntegerGreaterThanZero($id);
         $this->id = $id;
     }
 
     /**
      * @return int
      */
-    public function getValue()
+    public function getValue(): int
     {
         return $this->id;
-    }
-
-    /**
-     * Validates that the value is integer and is greater than zero
-     *
-     * @param $value
-     *
-     * @throws AttachmentConstraintException
-     */
-    private function assertIsIntegerGreaterThanZero($value)
-    {
-        if (!is_int($value) || 0 >= $value) {
-            throw new AttachmentConstraintException(
-                sprintf('Invalid attachment id "%s".', var_export($value, true)),
-                AttachmentConstraintException::INVALID_ID
-            );
-        }
     }
 }
