2a8516cae487501a12d950cc935af120dac1861e
Pablo Borowicz <pablo.borowicz@prestashop.com>
2019-08-09 10:44:56 +0200

    Update invalid hook names

--- install-dev/upgrade/sql/1.7.7.0.sql
+++ install-dev/upgrade/sql/1.7.7.0.sql
@@ -2,3 +2,8 @@ SET SESSION sql_mode='';
 SET NAMES 'utf8';
 
 ALTER TABLE `PREFIX_search_word` MODIFY `word` VARCHAR(30) NOT NULL;
+
+/* Update wrong hook names */
+UPDATE `PREFIX_hook` SET `name` = 'actionAdministrationPageSave' WHERE `name` = 'actionAdministrationPageFormSave';
+UPDATE `PREFIX_hook` SET `name` = 'actionMaintenancePageSave' WHERE `name` = 'actionMaintenancePageFormSave';
+UPDATE `PREFIX_hook` SET `name` = 'actionPerformancePageSave' WHERE `name` = 'actionPerformancePageFormSave';
