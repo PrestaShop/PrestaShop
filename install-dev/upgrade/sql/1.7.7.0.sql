5c47f32e316af9ef03f94a43060dc4167ed4db35
matthieu rolland <matthieu.rolland@prestashop.com>
2019-08-21 16:52:03 +0200

    fix update sql

--- install-dev/upgrade/sql/1.7.7.0.sql
+++ install-dev/upgrade/sql/1.7.7.0.sql
@@ -1,6 +1,6 @@
 SET SESSION sql_mode = '';
 SET NAMES 'utf8';
 
-ALTER TABLE `PREFIX_search_word` MODIFY `word` VARCHAR(30) NOT NULL;
-
 /* PHP:ps_1770_preset_tab_enabled(); */;
+ALTER TABLE `PREFIX_search_word` MODIFY `word` VARCHAR(30) NOT NULL;
+DELETE `PREFIX_configuration` WHERE name = 'PS_PRICE_DISPLAY_PRECISION';
