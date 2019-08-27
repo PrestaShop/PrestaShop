89c8547184fc84ede10b29dd72accaf3fd25373c
Raimondas <raimondas.sapola@invertus.eu>
2019-08-27 17:03:52 +0300

    WIP editing form assets

---
+++ admin-dev/themes/new-theme/js/pages/address/address-form-map.js
@@ -0,0 +1,37 @@
+/**
+ * 2007-2019 PrestaShop and Contributors
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2019 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+/**
+ * Defines all selectors that are used in customers address add/edit form.
+ */
+export default {
+  firstName: '#customer_address_first_name',
+  lastName: '#customer_address_last_name',
+  company: '#customer_address_company',
+  countrySelect: '#customer_address_id_country',
+  stateSelect: '#customer_address_id_state',
+  customerEmail: '#customer_address_customer_email',
+  stateFormRowSelect: '.js-address-state-select',
+}
