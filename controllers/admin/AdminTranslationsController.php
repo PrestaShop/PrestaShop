6c6a9a9fa879eb7c6cc0bec0737d8b9263c7818b
Bruno Desprez <bruno.desprez@gmail.com>
2019-08-30 16:59:13 +0200

    When an email subject is modify in the translations page for the specific selection, display it ...

--- controllers/admin/AdminTranslationsController.php
+++ controllers/admin/AdminTranslationsController.php
@@ -86,8 +86,8 @@ class AdminTranslationsControllerCore extends AdminController
         $this->link_lang_pack = str_replace('%ps_version%', _PS_VERSION_, $this->link_lang_pack);
 
         $this->themes = (new ThemeManagerBuilder($this->context, Db::getInstance()))
-                            ->buildRepository()
-                            ->getList();
+            ->buildRepository()
+            ->getList();
     }
 
     /*
@@ -134,13 +134,13 @@ class AdminTranslationsControllerCore extends AdminController
         // Create a title for each translation page
         $title = $this->trans(
             '%1$s (Language: %2$s, Theme: %3$s)',
-                array(
-                    '%1$s' => (empty($this->translations_informations[$this->type_selected]['name']) ? false : $this->translations_informations[$this->type_selected]['name']),
-                    '%2$s' => $this->lang_selected->name,
-                    '%3$s' => $this->theme_selected ? $this->theme_selected : $this->trans('None', array(), 'Admin.Global'),
-                ),
-                'Admin.International.Feature'
-            );
+            array(
+                '%1$s' => (empty($this->translations_informations[$this->type_selected]['name']) ? false : $this->translations_informations[$this->type_selected]['name']),
+                '%2$s' => $this->lang_selected->name,
+                '%3$s' => $this->theme_selected ? $this->theme_selected : $this->trans('None', array(), 'Admin.Global'),
+            ),
+            'Admin.International.Feature'
+        );
 
         // Set vars for all forms
         $this->tpl_view_vars = array(
@@ -187,15 +187,15 @@ class AdminTranslationsControllerCore extends AdminController
     public function initMain()
     {
         if (
-            !in_array(
-                $this->authorizationLevel(),
-                array(
-                    AdminController::LEVEL_VIEW,
-                    AdminController::LEVEL_EDIT,
-                    AdminController::LEVEL_ADD,
-                    AdminController::LEVEL_DELETE,
-                )
+        !in_array(
+            $this->authorizationLevel(),
+            array(
+                AdminController::LEVEL_VIEW,
+                AdminController::LEVEL_EDIT,
+                AdminController::LEVEL_ADD,
+                AdminController::LEVEL_DELETE,
             )
+        )
         ) {
             Tools::redirectAdmin(Context::getContext()->link->getAdminLink('AdminDashboard'));
         }
@@ -2582,8 +2582,8 @@ class AdminTranslationsControllerCore extends AdminController
             $str_return .= '<p class="error">
                 ' . $this->trans('There was a problem getting the mail files.', array(), 'Admin.International.Notification') . '<br>
                 ' . $this->trans('English language files must exist in %folder% folder', array(
-                '%folder%' => '<em>' . preg_replace('@/[a-z]{2}(/?)$@', '/en$1', $mails['directory']) . '</em>',
-            ), 'Admin.International.Notification') . '
+                    '%folder%' => '<em>' . preg_replace('@/[a-z]{2}(/?)$@', '/en$1', $mails['directory']) . '</em>',
+                ), 'Admin.International.Notification') . '
             </p>';
         }
 
@@ -2773,6 +2773,39 @@ class AdminTranslationsControllerCore extends AdminController
     }
 
     /**
+     * Getting the translated values of each passed subject email
+     * @param array $subject_mail
+     * @param $i18n_dir
+     * @return array
+     */
+    protected function getSubjectMailContent($subject_mail, $i18n_dir)
+    {
+        // Getting the translations depending on theme
+        global $_LANGMAIL;
+        $fileTheme = "$i18n_dir/lang.php";
+        if (Tools::file_exists_cache($fileTheme)) {
+            include $fileTheme;
+        }
+        $subject_mail_content = array();
+        foreach ($subject_mail as $templateSubjects) {
+            foreach ($templateSubjects as $subject) {
+                $key = str_replace('\'', '\\\'', $subject);
+                $trad = str_replace(
+                    '"',
+                    '&quot;',
+                    Tools::stripslashes((array_key_exists($key, $_LANGMAIL) && !empty($_LANGMAIL[$key])) ? $_LANGMAIL[$key] : $subject)
+                );
+                if ($trad !== $subject) {
+                    $subject_mail_content[$subject]['trad'] = $trad;
+                    // I think it's useless … The method Mail::l does not treat sprintf …
+                    $subject_mail_content[$subject]['use_sprintf'] = $this->checkIfKeyUseSprintf($subject);
+                }
+            }
+        }
+        return $subject_mail_content;
+    }
+
+    /**
      * This method generate the form for mails translations.
      *
      * @param bool $no_display
@@ -2817,6 +2850,8 @@ class AdminTranslationsControllerCore extends AdminController
         foreach ($modules_has_mails as $module_name => $module_path) {
             $module_path = rtrim($module_path, '/');
             $module_mails[$module_name] = $this->getMailFiles($module_path . '/mails/' . $this->lang_selected->iso_code . '/', 'module_mail');
+            // If the "subject" key is not set, the subjects translation isn't visible …
+            $module_mails[$module_name]['subject'] = $this->getSubjectMailContent($subject_mail, $i18n_dir);
             $module_mails[$module_name]['display'] = $this->displayMailContent($module_mails[$module_name], $subject_mail, $this->lang_selected, Tools::strtolower($module_name), $module_name, $module_name);
         }
 
