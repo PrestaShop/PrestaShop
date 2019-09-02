ebb778254a64b982b8e91a939b7b266c9cb93133
Boubker BRIBRI <boubker.bribri@prestashop.com>
2019-09-02 12:39:59 +0200

    Replace quantity by quatity_wanted in product data FO

--- tests/puppeteer/campaigns/data/FO/product.js
+++ tests/puppeteer/campaigns/data/FO/product.js
@@ -2,7 +2,7 @@ module.exports = {
 
   firstProductData: {
     name: 'T-SHIRT IMPRIMÉ COLIBRI',
-    quantity: '1',
+    quantity_wanted: '1',
     price: '22.94',
     description: 'Symbole de légèreté et de délicatesse',
   },
