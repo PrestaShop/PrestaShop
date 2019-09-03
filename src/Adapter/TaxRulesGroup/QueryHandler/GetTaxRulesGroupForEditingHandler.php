cd94d29d99c9f0caac6ef540db184989f9ad1f81
Raimondas <raimondas.sapola@invertus.eu>
2019-09-03 09:38:30 +0300

    Minor cs fixes

--- src/Adapter/TaxRulesGroup/QueryHandler/GetTaxRulesGroupForEditingHandler.php
+++ src/Adapter/TaxRulesGroup/QueryHandler/GetTaxRulesGroupForEditingHandler.php
@@ -47,9 +47,6 @@ final class GetTaxRulesGroupForEditingHandler extends AbstractTaxRulesGroupHandl
         $taxRulesGroupId = $query->getTaxRulesGroupId();
         $taxRulesGroup = $this->getTaxRulesGroup($taxRulesGroupId);
 
-        return new EditableTaxRulesGroup(
-            $taxRulesGroupId,
-            (bool) $taxRulesGroup->active
-        );
+        return new EditableTaxRulesGroup($taxRulesGroupId, (bool) $taxRulesGroup->active);
     }
 }
