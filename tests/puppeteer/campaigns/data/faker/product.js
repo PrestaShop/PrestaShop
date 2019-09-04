a0ed2f9fcceeb354e6ef22fd288fccafd5d8884c
Boubker BRIBRI <boubker.bribri@prestashop.com>
2019-09-04 15:08:30 +0200

    Update faker to add combinations

--- tests/puppeteer/campaigns/data/faker/product.js
+++ tests/puppeteer/campaigns/data/faker/product.js
@@ -1,8 +1,8 @@
 const faker = require('faker');
 
 module.exports = class Product {
-  constructor(productType = 'Simple product', quantity = false, price = false, productHasCombinations = false,
-    wantedQuantity = '1') {
+  constructor(productType = 'Standard product', quantity = false, wantedQuantity = '1', price = false,
+    productHasCombinations = false, combinations = false) {
     this.name = faker.commerce.productName().toUpperCase();
     this.description = faker.lorem.sentence();
     this.reference = faker.random.alphaNumeric(7);
@@ -11,5 +11,9 @@ module.exports = class Product {
     this.price = price || faker.random.number({min: 10, max: 20}).toString();
     this.type = productType;
     this.withCombination = productHasCombinations;
+    this.combinations = combinations || {
+      Color: ['White', 'Black'],
+      Size: ['S', 'M'],
+    };
   }
 };
