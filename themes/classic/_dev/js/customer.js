1bfdc4b0f3a2406d4fdc9ca3e0a5026580882ad9
rdy4ever <rdy4ever@users.noreply.github.com>
2019-09-13 20:40:52 +0300

    Trim unnecesary white spaces from customer name
    Remove whitespace from both sides of the customer name when input loses focus.

--- themes/classic/_dev/js/customer.js
+++ themes/classic/_dev/js/customer.js
@@ -40,3 +40,8 @@ function setupCustomerScripts() {
 }
 
 $(document).ready(setupCustomerScripts);
+
+//trim white spaces from customer name
+$("input[name='firstname'], input[name='lastname']").on('blur', function() {
+  this.value = this.value.trim();
+});
