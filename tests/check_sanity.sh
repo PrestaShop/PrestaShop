1c1da7a05909d852c92afb534adfd91242b1389d
Boubker BRIBRI <boubker.bribri@prestashop.com>
2019-09-13 16:14:41 +0200

    Add sanity to travis

---
+++ tests/check_sanity.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+bash travis-scripts/run-sanity-tests;
+SANITY=$?
+
+
+if [[ "$SANITY" == "0"]]; then
+  echo -e "\e[92mPUPPETEER SANITY TESTS OK"
+  exit 0;
+else
+  echo -e "\e[91mPUPPETEER SANITY TESTS FAILED"
+  exit 255;
+fi
