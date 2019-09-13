1d625c38e903cb41df7246d3e06aba5014ca61f1
rdy4ever <rdy4ever@users.noreply.github.com>
2019-09-13 20:45:03 +0300

    Trim unnecessary white spaces from customer name

--- themes/classic/_dev/js/customer.js
+++ themes/classic/_dev/js/customer.js
@@ -40,3 +40,8 @@ function setupCustomerScripts() {
 }
 
 $(document).ready(setupCustomerScripts);
+
+//trim unnecessary white spaces from customer name
+$("input[name='firstname'], input[name='lastname']").on('blur', function() {
+  this.value = this.value.trim();
+});
