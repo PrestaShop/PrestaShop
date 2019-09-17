f955a8acdc9899c3c6ed9cab1a67f9a0ec34329d
boubkerbribri <48441421+boubkerbribri@users.noreply.github.com>
2019-09-17 12:55:39 +0200

    Update check_sanity.sh
    Co-Authored-By: GoT <PierreRambaud@users.noreply.github.com>

--- tests/check_sanity.sh
+++ tests/check_sanity.sh
@@ -8,6 +8,6 @@ if [[ "$SANITY" == "0" ]]; then
   echo -e "\e[92mPUPPETEER SANITY TESTS OK\e[0m"
   exit 0;
 else
-  echo -e "\e[91mPUPPETEER SANITY TESTS FAILED"
+  echo -e "\e[91mPUPPETEER SANITY TESTS FAILED\e[0m"
   exit 255;
 fi
