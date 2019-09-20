2e1272d34248f2e05a57334649da2049a590e557
Boubker BRIBRI <boubker.bribri@prestashop.com>
2019-09-20 16:04:08 +0200

    Add login test on upgrade scenario

--- tests/puppeteer/campaigns/upgrade/upgradeShop.js
+++ tests/puppeteer/campaigns/upgrade/upgradeShop.js
@@ -1,6 +1,7 @@
+// Using chai
 const {expect} = require('chai');
 const helper = require('../utils/helpers');
-// Using chai
+
 // importing pages
 const LoginPage = require('../../pages/BO/login');
 const DashboardPage = require('../../pages/BO/dashboard');
@@ -11,121 +12,111 @@ const ShopParamsGeneralPage = require('../../pages/BO/shopParamsGeneral');
 const ShopParamsMaintenancePage = require('../../pages/BO/shopParamsMaintenance');
 const AutoUpgradePage = require('../../pages/BO/modulesPages/autoUpgrade');
 const HomePage = require('../../pages/FO/home');
+const loginCommon = require('../commonTests/loginBO');
 
 let browser;
 let page;
-let loginPage;
-let dashboardPage;
-let bOBasePage;
-let moduleCatalogPage;
-let moduleManagerPage;
-let shopParamsGeneralPage;
-let shopParamsMaintenancePage;
-let autoUpgradePage;
-let homePage;
-
 
 // creating pages objects in a function
 const init = async function () {
-  loginPage = await (new LoginPage(page));
-  dashboardPage = await (new DashboardPage(page));
-  bOBasePage = await (new BOBasePage(page));
-  moduleCatalogPage = await (new ModuleCatalogPage(page));
-  moduleManagerPage = await (new ModuleManagerPage(page));
-  shopParamsGeneralPage = await (new ShopParamsGeneralPage(page));
-  shopParamsMaintenancePage = await (new ShopParamsMaintenancePage(page));
-  autoUpgradePage = await (new AutoUpgradePage(page));
-  homePage = await (new HomePage(page));
+  return {
+    loginPage: new LoginPage(page),
+    dashboardPage: new DashboardPage(page),
+    boBasePage: new BOBasePage(page),
+    moduleCatalogPage: new ModuleCatalogPage(page),
+    moduleManagerPage: new ModuleManagerPage(page),
+    shopParamsGeneralPage: new ShopParamsGeneralPage(page),
+    shopParamsMaintenancePage: new ShopParamsMaintenancePage(page),
+    autoUpgradePage: new AutoUpgradePage(page),
+    homePage: new HomePage(page),
+  };
 };
 
 // Upgrade shop from a version to the last stable one
 describe('Upgrade Prestashop to last Stable', async () => {
   // before and after functions
-  before(async () => {
+  before(async function () {
     browser = await helper.createBrowser();
-    page = await browser.newPage();
+    page = await helper.newTab(browser);
     await page.setExtraHTTPHeaders({
       'Accept-Language': 'en-GB',
     });
-    await init();
+    this.pageObjects = await init();
   });
   after(async () => {
-    await browser.close();
+    await helper.closeBrowser(browser);
   });
   // Steps
-  it('should login into BO', async () => {
-    await loginPage.goTo(global.URL_BO);
-    await loginPage.login(global.EMAIL, global.PASSWD);
-    const pageTitle = await dashboardPage.getPageTitle();
-    await expect(pageTitle).to.contains(dashboardPage.pageTitle);
-    await bOBasePage.closeOnboardingModal();
+  loginCommon.loginBO();
+  it('should go the Module Catalog page', async function () {
+    await this.pageObjects.boBasePage.goToSubMenu(this.pageObjects.boBasePage.modulesParentLink,
+      this.pageObjects.boBasePage.moduleCatalogueLink);
+    const pageTitle = await this.pageObjects.moduleCatalogPage.getPageTitle();
+    await expect(pageTitle).to.contains(this.pageObjects.moduleCatalogPage.pageTitle);
   });
 
-  it('should go the Module Catalog page', async () => {
-    await bOBasePage.goToSubMenu(bOBasePage.modulesParentLink, bOBasePage.moduleCatalogueLink);
-    const pageTitle = await moduleCatalogPage.getPageTitle();
-    await expect(pageTitle).to.contains(moduleCatalogPage.pageTitle);
-  });
-
-  it('should Install module \'1-Click Upgrade\'', async () => {
-    await moduleCatalogPage.searchModule('autoupgrade', '1-Click Upgrade');
-    const installResultMessage = await moduleCatalogPage.installModule('1-Click Upgrade');
+  it('should Install module \'1-Click Upgrade\'', async function () {
+    await this.pageObjects.moduleCatalogPage.searchModule('autoupgrade', '1-Click Upgrade');
+    const installResultMessage = await this.pageObjects.moduleCatalogPage.installModule('1-Click Upgrade');
     await expect(installResultMessage)
-      .to.equal(moduleCatalogPage.installMessageSuccessful.replace('%MODULETAG', 'autoupgrade'));
+      .to.equal(this.pageObjects.moduleCatalogPage.installMessageSuccessful.replace('%MODULETAG', 'autoupgrade'));
   });
 
-  it('should disable Shop', async () => {
-    await bOBasePage.goToSubMenu(bOBasePage.shopParametersParentLink, bOBasePage.shopParametersGeneralLink);
-    let pageTitle = await shopParamsGeneralPage.getPageTitle();
-    await expect(pageTitle).to.contains(shopParamsGeneralPage.pageTitle);
-    await shopParamsGeneralPage.goToSubTabMaintenance();
-    pageTitle = await shopParamsMaintenancePage.getPageTitle();
-    await expect(pageTitle).to.contains(shopParamsMaintenancePage.pageTitle);
-    await shopParamsMaintenancePage.changeShopStatus(false);
+  it('should disable Shop', async function () {
+    await this.pageObjects.boBasePage.goToSubMenu(this.pageObjects.boBasePage.shopParametersParentLink,
+      this.pageObjects.boBasePage.shopParametersGeneralLink);
+    let pageTitle = await this.pageObjects.shopParamsGeneralPage.getPageTitle();
+    await expect(pageTitle).to.contains(this.pageObjects.shopParamsGeneralPage.pageTitle);
+    await this.pageObjects.shopParamsGeneralPage.goToSubTabMaintenance();
+    pageTitle = await this.pageObjects.shopParamsMaintenancePage.getPageTitle();
+    await expect(pageTitle).to.contains(this.pageObjects.shopParamsMaintenancePage.pageTitle);
+    await this.pageObjects.shopParamsMaintenancePage.changeShopStatus(false);
   });
 
-  it('should Go to configuration module Page', async () => {
-    await bOBasePage.goToSubMenu(bOBasePage.modulesParentLink, bOBasePage.moduleManagerLink);
-    let pageTitle = await moduleManagerPage.getPageTitle();
-    await expect(pageTitle).to.contains(moduleManagerPage.pageTitle);
-    await moduleManagerPage.searchModule('autoupgrade', '1-Click Upgrade');
-    await moduleManagerPage.goToConfigurationPage('1-Click Upgrade');
-    pageTitle = await autoUpgradePage.getPageTitle();
-    await expect(pageTitle).to.contains(autoUpgradePage.pageTitle);
+  it('should Go to configuration module Page', async function () {
+    await this.pageObjects.boBasePage.goToSubMenu(this.pageObjects.boBasePage.modulesParentLink,
+      this.pageObjects.boBasePage.moduleManagerLink);
+    let pageTitle = await this.pageObjects.moduleManagerPage.getPageTitle();
+    await expect(pageTitle).to.contains(this.pageObjects.moduleManagerPage.pageTitle);
+    await this.pageObjects.moduleManagerPage.searchModule('autoupgrade', '1-Click Upgrade');
+    await this.pageObjects.moduleManagerPage.goToConfigurationPage('1-Click Upgrade');
+    pageTitle = await this.pageObjects.autoUpgradePage.getPageTitle();
+    await expect(pageTitle).to.contains(this.pageObjects.autoUpgradePage.pageTitle);
   });
 
-  it('should upgrade Prestashop', async () => {
-    await autoUpgradePage.upgradePrestashop('major');
+  it('should upgrade Prestashop', async function () {
+    await this.pageObjects.autoUpgradePage.upgradePrestashop('major');
   });
 
-  it('should reload and check that user was automatically logged out', async () => {
-    await bOBasePage.reloadPage();
-    const pageTitle = await loginPage.getPageTitle();
-    await expect(pageTitle).to.contains(loginPage.pageTitle);
+  it('should reload and check that user was automatically logged out', async function () {
+    await this.pageObjects.boBasePage.reloadPage();
+    const pageTitle = await this.pageObjects.loginPage.getPageTitle();
+    await expect(pageTitle).to.contains(this.pageObjects.loginPage.pageTitle);
   });
 
-  it('should login and verify version in BO', async () => {
-    await loginPage.goTo(global.URL_BO);
-    await loginPage.login(global.EMAIL, global.PASSWD);
-    const pageTitle = await dashboardPage.getPageTitle();
-    await expect(pageTitle).to.contains(dashboardPage.pageTitle);
-    const version = await bOBasePage.getTextContent(bOBasePage.shopVersionBloc);
+  it('should login and verify version in BO', async function () {
+    await this.pageObjects.loginPage.goTo(global.URL_BO);
+    await this.pageObjects.loginPage.login(global.EMAIL, global.PASSWD);
+    const pageTitle = await this.pageObjects.dashboardPage.getPageTitle();
+    await expect(pageTitle).to.contains(this.pageObjects.dashboardPage.pageTitle);
+    const version = await this.pageObjects.boBasePage.getTextContent(this.pageObjects.boBasePage.shopVersionBloc);
     expect(version).to.be.equal(global.PS_VERSION);
   });
 
-  it('should enable Shop', async () => {
-    await bOBasePage.goToSubMenu(bOBasePage.shopParametersParentLink, bOBasePage.shopParametersGeneralLink);
-    let pageTitle = await shopParamsGeneralPage.getPageTitle();
-    await expect(pageTitle).to.contains(shopParamsGeneralPage.pageTitle);
-    await shopParamsGeneralPage.goToSubTabMaintenance();
-    pageTitle = await shopParamsMaintenancePage.getPageTitle();
-    await expect(pageTitle).to.contains(shopParamsMaintenancePage.pageTitle);
-    await shopParamsMaintenancePage.changeShopStatus(true);
+  it('should enable Shop', async function () {
+    await this.pageObjects.boBasePage.goToSubMenu(this.pageObjects.boBasePage.shopParametersParentLink,
+      this.pageObjects.boBasePage.shopParametersGeneralLink);
+    let pageTitle = await this.pageObjects.shopParamsGeneralPage.getPageTitle();
+    await expect(pageTitle).to.contains(this.pageObjects.shopParamsGeneralPage.pageTitle);
+    await this.pageObjects.shopParamsGeneralPage.goToSubTabMaintenance();
+    pageTitle = await this.pageObjects.shopParamsMaintenancePage.getPageTitle();
+    await expect(pageTitle).to.contains(this.pageObjects.shopParamsMaintenancePage.pageTitle);
+    await this.pageObjects.shopParamsMaintenancePage.changeShopStatus(true);
   });
 
-  it('should verify FO', async () => {
-    page = await bOBasePage.viewMyShop();
-    await page.waitForSelector(homePage.userInfoLink, {visible: true});
-    await page.waitForSelector(homePage.contactLink, {visible: true});
+  it('should verify FO', async function () {
+    page = await this.pageObjects.boBasePage.viewMyShop();
+    await page.waitForSelector(this.pageObjects.homePage.userInfoLink, {visible: true});
+    await page.waitForSelector(this.pageObjects.homePage.contactLink, {visible: true});
   });
 });
