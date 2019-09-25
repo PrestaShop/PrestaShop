0545b436b28269e0a0a6d54c7d436eae9e497d65
matthieu rolland <matthieu.rolland@prestashop.com>
2019-09-25 14:31:22 +0200

    cs fixer

--- classes/Context.php
+++ classes/Context.php
@@ -23,7 +23,6 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
-
 use PrestaShop\PrestaShop\Adapter\SymfonyContainer;
 use PrestaShop\PrestaShop\Core\Localization\CLDR\ComputingPrecision;
 use PrestaShop\PrestaShop\Core\Localization\Locale;
