7a929c64eec49f1f5d55b422419a7b8d157854d0
202 ecommerce <202-ecommerce@users.noreply.github.com>
2019-09-25 12:00:51 +0200

    update error message

--- controllers/front/CartController.php
+++ controllers/front/CartController.php
@@ -520,7 +520,7 @@ class CartControllerCore extends FrontController
                 } elseif ($this->shouldAvailabilityErrorBeRaised($product, $qty_to_check)) {
                     // check quantity after cart quantity update
                     $this->{$ErrorKey}[] = $this->trans(
-                        'The item %product% in your cart is no longer available in this quantity. You cannot proceed with your order until the quantity is adjusted.',
+                        'The item %product% in your cart is now a product with attributes. Please delete it and choose one of its combinations to proceed with your order.',
                         array('%product%' => $product->name),
                         'Shop.Notifications.Error'
                     );
