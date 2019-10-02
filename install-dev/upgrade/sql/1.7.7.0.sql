edffb231468a72190ccbc2a8bb7a222e84cddcd4
matthieu rolland <matthieu.rolland@prestashop.com>
2019-10-02 18:17:46 +0200

    remove column from employee table

--- install-dev/upgrade/sql/1.7.7.0.sql
+++ install-dev/upgrade/sql/1.7.7.0.sql
@@ -15,3 +15,6 @@ ALTER TABLE `PREFIX_product` ADD `mpn` VARCHAR(40) NULL AFTER `upc`;
 
 /* Delete price display precision configuration */
 DELETE `PREFIX_configuration` WHERE name = 'PS_PRICE_DISPLAY_PRECISION';
+
+/* Delete optin field from employee table */
+ALTER TABLE `PREFIX_employee` DROP `optin`;
