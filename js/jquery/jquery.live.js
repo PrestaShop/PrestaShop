a0b96d18df8f5a87135256dc9e4080c5c511dd78
matthieu rolland <matthieu.rolland@prestashop.com>
2019-10-10 14:57:38 +0200

    minify jquery.live

--- js/jquery/jquery.live.js
+++ js/jquery/jquery.live.js
@@ -1,9 +1,1 @@
-jQuery.fn.extend({
-    live: function (event, callback) {
-        if (this.selector) {
-            jQuery(document).on(event, this.selector, callback);
-        }
-        console.warn('jQuery.live() is deprecated since Prestashop 1.7.7, it will not work in future versions, please use jQuery.on() instead.');
-        return this;
-    }
-});
+jQuery.fn.extend({live:function(e,n){return this.selector&&jQuery(document).on(e,this.selector,n),console.warn("jQuery.live() is deprecated since Prestashop 1.7.7, it will not work in future versions, please use jQuery.on() instead."),this}});
