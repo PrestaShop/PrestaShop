66883c00679ddb69653e57a06122d6436fb59c6a
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2019-10-10 11:15:33 +0300

    Add help text for discounts of type amount

--- admin-dev/themes/new-theme/js/pages/order/view.js
+++ admin-dev/themes/new-theme/js/pages/order/view.js
@@ -57,6 +57,7 @@ $(() => {
   function initAddCartRuleFormHandler() {
     const $modal = $(OrderViewPageMap.addCartRuleModal);
     const $form = $modal.find('form');
+    const $valueHelp = $modal.find(OrderViewPageMap.cartRuleHelpText);
     const $invoiceSelect = $modal.find(OrderViewPageMap.addCartRuleInvoiceIdSelect);
     const $valueInput = $form.find(OrderViewPageMap.addCartRuleValueInput);
     const $valueFormGroup = $valueInput.closest('.form-group');
@@ -69,6 +70,12 @@ $(() => {
 
     $form.find(OrderViewPageMap.addCartRuleTypeSelect).on('change', (event) => {
       const selectedCartRuleType = $(event.currentTarget).val();
+
+      if (selectedCartRuleType === 'amount') {
+        $valueHelp.removeClass('d-none');
+      } else {
+        $valueHelp.addClass('d-none');
+      }
 
       if (selectedCartRuleType === 'free_shipping') {
         $valueFormGroup.addClass('d-none');
