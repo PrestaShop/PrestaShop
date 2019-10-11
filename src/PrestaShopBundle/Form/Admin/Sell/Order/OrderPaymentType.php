c06629d71a5cd6e6577a3458f76277c98bad76e0
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2019-10-11 16:42:30 +0300

    Apply CS fixer

--- src/PrestaShopBundle/Form/Admin/Sell/Order/OrderPaymentType.php
+++ src/PrestaShopBundle/Form/Admin/Sell/Order/OrderPaymentType.php
@@ -31,7 +31,6 @@ use PrestaShop\PrestaShop\Core\Form\FormChoiceProviderInterface;
 use PrestaShopBundle\Form\Admin\Type\DatePickerType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
