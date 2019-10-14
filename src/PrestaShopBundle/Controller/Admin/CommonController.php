8c695a430939e035125e1bccdfa8969171da3026
Justinas <justinasu@gmail.com>
2019-10-14 23:13:42 +0300

    fixed BC break with php 5.6 syntax

--- src/PrestaShopBundle/Controller/Admin/CommonController.php
+++ src/PrestaShopBundle/Controller/Admin/CommonController.php
@@ -118,7 +118,9 @@ class CommonController extends FrameworkBundleAdminController
                 unset($callerParameters[$k]);
             }
         }
-        $routeName = $request->attributes->get('caller_route', ($callerParameters + array('_route' => false))['_route']);
+        
+        $callerParameters += array('_route' => false);
+        $routeName = $request->attributes->get('caller_route', $callerParameters['_route']);
         $nextPageUrl = (!$routeName || ($offset + $limit >= $total)) ? false : $this->generateUrl($routeName, array_merge(
             $callerParameters,
             array(
