f1c7a0d4ff2ea47d8825552852ef30f336ccf836
Thomas Baccelli <thomas.baccelli@prestashop.com>
2019-10-16 16:55:09 +0200

    Implements CQRS on Notifications

--- src/PrestaShopBundle/Controller/Admin/CommonController.php
+++ src/PrestaShopBundle/Controller/Admin/CommonController.php
@@ -26,9 +26,14 @@
 
 namespace PrestaShopBundle\Controller\Admin;
 
+use Context;
 use PrestaShop\PrestaShop\Adapter\Module\AdminModuleDataProvider;
 use PrestaShop\PrestaShop\Core\Addon\AddonsCollection;
 use PrestaShop\PrestaShop\Core\Addon\Module\ModuleManagerBuilder;
+use PrestaShop\PrestaShop\Core\Domain\Notification\Command\UpdateEmployeeNotificationLastElementCommand;
+use PrestaShop\PrestaShop\Core\Domain\Notification\Exception\TypeException;
+use PrestaShop\PrestaShop\Core\Domain\Notification\Query\GetNotificationLastElements;
+use PrestaShop\PrestaShop\Core\Domain\Notification\QueryResult\NotificationsResults;
 use PrestaShop\PrestaShop\Core\Grid\Definition\Factory\GridDefinitionFactoryInterface;
 use PrestaShop\PrestaShop\Core\Grid\Definition\GridDefinitionInterface;
 use PrestaShop\PrestaShop\Core\Kpi\Row\KpiRowInterface;
@@ -56,20 +61,52 @@ class CommonController extends FrameworkBundleAdminController
      */
     public function notificationsAction()
     {
-        // TODO: Use CQRS
-        return new JsonResponse((new \Notification())->getLastElements());
+        $employeeId = Context::getContext()->employee->id;
+        /** @var NotificationsResults $elements */
+        $elements = $this->getQueryBus()->handle(new GetNotificationLastElements($employeeId));
+
+        $response = [];
+
+        foreach ($elements->getNotificationsResults() as $element) {
+            $notifications = [];
+            foreach ($element->getNotifications() as $notification) {
+                $notifications[] = [
+                    'id_order' => $notification->getOrderId(),
+                    'id_customer' => $notification->getCustomerId(),
+                    'id_customer_message' => $notification->getCustomerMessageId(),
+                    'id_customer_thread' => $notification->getCustomerThreadId(),
+                    'total_paid' => $notification->getTotalPaid(),
+                    'carrier' => $notification->getCarrier(),
+                    'iso_code' => $notification->getIsoCode(),
+                    'company' => $notification->getCompany(),
+                    'status' => $notification->getStatus(),
+                    'customer_name' => $notification->getCustomerName(),
+                    'date_add' => $notification->getDateAdd(),
+                    'customer_view_url' => $notification->getCustomerViewUrl(),
+                ];
+            }
+            $response[($element->getType()->getValue())] = [
+                'total' => $element->getTotal(),
+                'results' => $notifications,
+            ];
+        }
+
+        return new JsonResponse($response);
     }
 
     /**
      * Update the last time a notification type has been seen.
      *
      * @param Request $request
+     *
+     * @throws TypeException
      */
     public function notificationsAckAction(Request $request)
     {
         $type = $request->request->get('type');
-        // TODO: Use CQRS
-        return new JsonResponse((new \Notification())->updateEmployeeLastElement($type));
+        $this->getCommandBus()->handle(new UpdateEmployeeNotificationLastElementCommand($type));
+
+        return new JsonResponse(true);
     }
 
     /**
