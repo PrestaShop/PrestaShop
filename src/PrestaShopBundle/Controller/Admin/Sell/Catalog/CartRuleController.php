843a5484d6e1f179b357bea2cb2382a34886a30e
Julius Zukauskas <zuk3975@gmail.com>
2019-10-16 18:51:19 +0300

    Adds SearchCartRules command

---
+++ src/PrestaShopBundle/Controller/Admin/Sell/Catalog/CartRuleController.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * 2007-2019 PrestaShop SA and Contributors
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2019 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+namespace PrestaShopBundle\Controller\Admin\Sell\Catalog;
+
+use PrestaShop\PrestaShop\Core\Domain\CartRule\Query\SearchCartRules;
+use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Responsible for Cart rules (a.k.a cart discounts/vouchers) actions in Back Office
+ */
+class CartRuleController extends FrameworkBundleAdminController
+{
+    //@todo: isnt it confusing with list filtering action, which is called 'search' in other controllers ?
+    public function searchAction(Request $request): JsonResponse
+    {
+        $searchPhrase = $request->query->get('search_phrase');
+        $cartRules = [];
+
+        if ($searchPhrase) {
+            $cartRules = $this->getQueryBus()->handle(new SearchCartRules($searchPhrase));
+        }
+
+        return $this->json([
+            'cart_rules' => $cartRules,
+        ]);
+    }
+}
