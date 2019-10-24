4cb7fc0a700cf139166bd4283f0de6eca0209eb9
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2019-10-24 12:26:14 +0300

    Update order status colors

--- install-dev/upgrade/sql/1.7.7.0.sql
+++ install-dev/upgrade/sql/1.7.7.0.sql
@@ -5,6 +5,7 @@ ALTER TABLE `PREFIX_search_word` MODIFY `word` VARCHAR(30) NOT NULL;
 INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VALUES ('PS_DISPLAY_MANUFACTURERS', '1', NOW(), NOW());
 
 /* PHP:ps_1770_preset_tab_enabled(); */;
+/* PHP:ps_1770_update_order_status_colors(); */;
 
 /* Add field MPN to tables */
 ALTER TABLE `PREFIX_order_detail` ADD `product_mpn` VARCHAR(40) NULL AFTER `product_upc`;
