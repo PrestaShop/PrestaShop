e5bc041a47dc9122c2a507932ed80ef18b963d5a
Progi1984 <franck.lefevre@prestashop.com>
2019-10-24 16:26:03 +0200

    Checked that ShippingCostTax values are not null before being formatted

--- src/Adapter/Order/QueryHandler/GetOrderForViewingHandler.php
+++ src/Adapter/Order/QueryHandler/GetOrderForViewingHandler.php
@@ -584,9 +584,9 @@ final class GetOrderForViewingHandler implements GetOrderForViewingHandlerInterf
 
         foreach ($shipping as $item) {
             if ($order->getTaxCalculationMethod() == PS_TAX_INC) {
-                $price = $this->locale->formatPrice($item['shipping_cost_tax_incl'], $currency->iso_code);
+                $price = !empty($item['shipping_cost_tax_incl']) ? $this->locale->formatPrice($item['shipping_cost_tax_incl'], $currency->iso_code) : '';
             } else {
-                $price = $this->locale->formatPrice($item['shipping_cost_tax_excl'], $currency->iso_code);
+                $price = !empty($item['shipping_cost_tax_excl']) ? $this->locale->formatPrice($item['shipping_cost_tax_excl'], $currency->iso_code) : '';
             }
 
             $trackingUrl = null;
