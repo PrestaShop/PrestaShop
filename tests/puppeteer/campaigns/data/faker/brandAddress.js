ee2cbe41bb6f0cb4f5ab8675500f2e572887982c
Boubker BRIBRI <boubker.bribri@prestashop.com>
2019-10-25 15:35:11 +0200

    Add faker For Brand and Address

---
+++ tests/puppeteer/campaigns/data/faker/brandAddress.js
@@ -0,0 +1,19 @@
+const faker = require('faker');
+
+const countries = ['Italy', 'France', 'United States', 'Netherlands', 'United Kingdom', 'Germany'];
+
+module.exports = class branAddress {
+  constructor(brandAddressToCreate = {}) {
+    this.brandName = brandAddressToCreate.brandName || '--';
+    this.firstName = brandAddressToCreate.firstName || faker.name.firstName();
+    this.lastName = brandAddressToCreate.lastName || faker.name.lastName();
+    this.address = brandAddressToCreate.address || faker.address.streetAddress();
+    this.secondaryAddress = brandAddressToCreate.secondaryAddress || faker.address.secondaryAddress();
+    this.postalCode = brandAddressToCreate.postalCode || faker.address.zipCode();
+    this.city = brandAddressToCreate.city || faker.address.city();
+    this.country = brandAddressToCreate.country || faker.random.arrayElement(countries);
+    this.homePhone = brandAddressToCreate.homePhone || faker.phone.phoneNumber('01########');
+    this.mobilePhone = brandAddressToCreate.mobilePhone || faker.phone.phoneNumber('06########');
+    this.other = brandAddressToCreate.other || '';
+  }
+};
