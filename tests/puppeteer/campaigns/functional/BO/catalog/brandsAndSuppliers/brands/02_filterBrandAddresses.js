8903248aa905012f5861a46e3e9436b36978fbab
Boubker BRIBRI <boubker.bribri@prestashop.com>
2019-10-25 15:43:22 +0200

    Add functional tests for brands and addresses

---
+++ tests/puppeteer/campaigns/functional/BO/catalog/brandsAndSuppliers/brands/02_filterBrandAddresses.js
@@ -0,0 +1,204 @@
+require('module-alias/register');
+// Using chai
+const {expect} = require('chai');
+const helper = require('@utils/helpers');
+const loginCommon = require('@commonTests/loginBO');
+const {demoAddresses} = require('@data/demo/brands');
+// Importing pages
+const BOBasePage = require('@pages/BO/BObasePage');
+const LoginPage = require('@pages/BO/login');
+const DashboardPage = require('@pages/BO/dashboard');
+const BrandsPage = require('@pages/BO/brands');
+
+let browser;
+let page;
+let numberOfBrandsAddresses = 0;
+
+// Init objects needed
+const init = async function () {
+  return {
+    boBasePage: new BOBasePage(page),
+    loginPage: new LoginPage(page),
+    dashboardPage: new DashboardPage(page),
+    brandsPage: new BrandsPage(page),
+  };
+};
+
+// Filter And Quick Edit Addresses
+describe('Filter And Quick Edit Addresses', async () => {
+  // before and after functions
+  before(async function () {
+    browser = await helper.createBrowser();
+    page = await helper.newTab(browser);
+    this.pageObjects = await init();
+  });
+  after(async () => {
+    await helper.closeBrowser(browser);
+  });
+  // Login into BO and go to brands page
+  loginCommon.loginBO();
+  it('should go to brands page', async function () {
+    await this.pageObjects.boBasePage.goToSubMenu(
+      this.pageObjects.boBasePage.productsParentLink,
+      this.pageObjects.boBasePage.brandsAndSuppliersLink,
+    );
+    await this.pageObjects.boBasePage.closeSfToolBar();
+    const pageTitle = await this.pageObjects.brandsPage.getPageTitle();
+    await expect(pageTitle).to.contains(this.pageObjects.brandsPage.pageTitle);
+  });
+  it('should reset all filters and get Number of brands in BO', async function () {
+    await this.pageObjects.brandsPage.resetFilters('manufacturer_address');
+    numberOfBrandsAddresses = await this.pageObjects.brandsPage.getNumberFromText(
+      this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer_address'),
+    );
+    await expect(numberOfBrandsAddresses).to.be.above(0);
+  });
+  // 1 : Filter brands
+  describe('Filter brands addresses', async () => {
+    it('should filter by Id', async function () {
+      await this.pageObjects.brandsPage.filterAddresses('input', 'id_address', demoAddresses.first.id);
+      const numberOfBrandsAddressesAfterFilter = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer_address'),
+      );
+      await expect(numberOfBrandsAddressesAfterFilter).to.be.at.most(numberOfBrandsAddresses);
+      const textColumn = await this.pageObjects.brandsPage.getTextContent(
+        this.pageObjects.brandsPage.tableColumn.replace('%TABLE', 'manufacturer_address')
+          .replace('%ROW', 1).replace('%COLUMN', 'id_address'),
+      );
+      await expect(textColumn).to.contains(demoAddresses.first.id);
+    });
+    it('should reset all filters', async function () {
+      await this.pageObjects.brandsPage.resetFilters('manufacturer_address');
+      const numberOfBrandsAddressesAfterReset = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer_address'),
+      );
+      await expect(numberOfBrandsAddressesAfterReset).to.equal(numberOfBrandsAddresses);
+    });
+    it('should filter by brand name', async function () {
+      await this.pageObjects.brandsPage.filterAddresses('input', 'name', demoAddresses.first.brand);
+      const numberOfBrandsAddressesAfterFilter = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer_address'),
+      );
+      await expect(numberOfBrandsAddressesAfterFilter).to.be.at.most(numberOfBrandsAddresses);
+      const textColumn = await this.pageObjects.brandsPage.getTextContent(
+        this.pageObjects.brandsPage.tableColumn
+          .replace('%TABLE', 'manufacturer_address')
+          .replace('%ROW', 1)
+          .replace('%COLUMN', 'name'),
+      );
+      await expect(textColumn).to.contains(demoAddresses.first.brand);
+    });
+    it('should reset all filters', async function () {
+      await this.pageObjects.brandsPage.resetFilters('manufacturer_address');
+      const numberOfBrandsAddressesAfterReset = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer_address'),
+      );
+      await expect(numberOfBrandsAddressesAfterReset).to.equal(numberOfBrandsAddresses);
+    });
+    it('should filter by Manufacturer firstname', async function () {
+      await this.pageObjects.brandsPage.filterAddresses('input', 'firstname', demoAddresses.first.firstName);
+      const numberOfBrandsAddressesAfterFilter = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer_address'),
+      );
+      await expect(numberOfBrandsAddressesAfterFilter).to.be.at.most(numberOfBrandsAddresses);
+      const textColumn = await this.pageObjects.brandsPage.getTextContent(
+        this.pageObjects.brandsPage.tableColumn
+          .replace('%TABLE', 'manufacturer_address')
+          .replace('%ROW', 1)
+          .replace('%COLUMN', 'firstname'),
+      );
+      await expect(textColumn).to.contains(demoAddresses.first.firstName);
+    });
+    it('should reset all filters', async function () {
+      await this.pageObjects.brandsPage.resetFilters('manufacturer_address');
+      const numberOfBrandsAddressesAfterReset = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer_address'),
+      );
+      await expect(numberOfBrandsAddressesAfterReset).to.equal(numberOfBrandsAddresses);
+    });
+    it('should filter by Manufacturer lastname', async function () {
+      await this.pageObjects.brandsPage.filterAddresses('input', 'lastname', demoAddresses.first.lastName);
+      const numberOfBrandsAddressesAfterFilter = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer_address'),
+      );
+      await expect(numberOfBrandsAddressesAfterFilter).to.be.at.most(numberOfBrandsAddresses);
+      const textColumn = await this.pageObjects.brandsPage.getTextContent(
+        this.pageObjects.brandsPage.tableColumn
+          .replace('%TABLE', 'manufacturer_address')
+          .replace('%ROW', 1)
+          .replace('%COLUMN', 'lastname'),
+      );
+      await expect(textColumn).to.contains(demoAddresses.first.lastName);
+    });
+    it('should reset all filters', async function () {
+      await this.pageObjects.brandsPage.resetFilters('manufacturer_address');
+      const numberOfBrandsAddressesAfterReset = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer_address'),
+      );
+      await expect(numberOfBrandsAddressesAfterReset).to.equal(numberOfBrandsAddresses);
+    });
+    it('should filter by Address postal code', async function () {
+      await this.pageObjects.brandsPage.filterAddresses('input', 'postcode', demoAddresses.first.postalCode);
+      const numberOfBrandsAddressesAfterFilter = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer_address'),
+      );
+      await expect(numberOfBrandsAddressesAfterFilter).to.be.at.most(numberOfBrandsAddresses);
+      const textColumn = await this.pageObjects.brandsPage.getTextContent(
+        this.pageObjects.brandsPage.tableColumn
+          .replace('%TABLE', 'manufacturer_address')
+          .replace('%ROW', 1)
+          .replace('%COLUMN', 'postcode'),
+      );
+      await expect(textColumn).to.contains(demoAddresses.first.postalCode);
+    });
+    it('should reset all filters', async function () {
+      await this.pageObjects.brandsPage.resetFilters('manufacturer_address');
+      const numberOfBrandsAddressesAfterReset = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer_address'),
+      );
+      await expect(numberOfBrandsAddressesAfterReset).to.equal(numberOfBrandsAddresses);
+    });
+    it('should filter by City', async function () {
+      await this.pageObjects.brandsPage.filterAddresses('input', 'city', demoAddresses.first.city);
+      const numberOfBrandsAddressesAfterFilter = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer_address'),
+      );
+      await expect(numberOfBrandsAddressesAfterFilter).to.be.at.most(numberOfBrandsAddresses);
+      const textColumn = await this.pageObjects.brandsPage.getTextContent(
+        this.pageObjects.brandsPage.tableColumn
+          .replace('%TABLE', 'manufacturer_address')
+          .replace('%ROW', 1)
+          .replace('%COLUMN', 'city'),
+      );
+      await expect(textColumn).to.contains(demoAddresses.first.city);
+    });
+    it('should reset all filters', async function () {
+      await this.pageObjects.brandsPage.resetFilters('manufacturer_address');
+      const numberOfBrandsAddressesAfterReset = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer_address'),
+      );
+      await expect(numberOfBrandsAddressesAfterReset).to.equal(numberOfBrandsAddresses);
+    });
+    it('should filter by Country', async function () {
+      await this.pageObjects.brandsPage.filterAddresses('select', 'country', demoAddresses.first.country);
+      const numberOfBrandsAddressesAfterFilter = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer_address'),
+      );
+      await expect(numberOfBrandsAddressesAfterFilter).to.be.at.most(numberOfBrandsAddresses);
+      const textColumn = await this.pageObjects.brandsPage.getTextContent(
+        this.pageObjects.brandsPage.tableColumn
+          .replace('%TABLE', 'manufacturer_address')
+          .replace('%ROW', 1)
+          .replace('%COLUMN', 'country'),
+      );
+      await expect(textColumn).to.contains(demoAddresses.first.country);
+    });
+    it('should reset all filters', async function () {
+      await this.pageObjects.brandsPage.resetFilters('manufacturer_address');
+      const numberOfBrandsAddressesAfterReset = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer_address'),
+      );
+      await expect(numberOfBrandsAddressesAfterReset).to.equal(numberOfBrandsAddresses);
+    });
+  });
+});
---
+++ tests/puppeteer/campaigns/functional/BO/catalog/brandsAndSuppliers/brands/04_BulkActionsBrands.js
@@ -0,0 +1,193 @@
+require('module-alias/register');
+const {expect} = require('chai');
+const helper = require('@utils/helpers');
+const files = require('@utils/files');
+const BrandFaker = require('@data/faker/brand');
+const loginCommon = require('@commonTests/loginBO');
+// Importing pages
+const BOBasePage = require('@pages/BO/BObasePage');
+const LoginPage = require('@pages/BO/login');
+const DashboardPage = require('@pages/BO/dashboard');
+const BrandsPage = require('@pages/BO/brands');
+const AddBrandPage = require('@pages/BO/addBrand');
+
+let browser;
+let page;
+let numberOfBrands = 0;
+let firstBrandData;
+let secondBrandData;
+
+// Init objects needed
+const init = async function () {
+  return {
+    boBasePage: new BOBasePage(page),
+    loginPage: new LoginPage(page),
+    dashboardPage: new DashboardPage(page),
+    brandsPage: new BrandsPage(page),
+    addBrandPage: new AddBrandPage(page),
+  };
+};
+
+// Create 2 brands, Enable, disable and delete with bulk actions
+describe('Create 2 brands, Enable, disable and delete with bulk actions', async () => {
+  // before and after functions
+  before(async function () {
+    browser = await helper.createBrowser();
+    page = await helper.newTab(browser);
+    this.pageObjects = await init();
+    firstBrandData = await (new BrandFaker({name: 'BrandToDelete'}));
+    secondBrandData = await (new BrandFaker({name: 'BrandToDelete2'}));
+  });
+  after(async () => {
+    await helper.closeBrowser(browser);
+    await Promise.all([
+      files.deleteFile(firstBrandData.logo),
+      files.deleteFile(secondBrandData.logo),
+    ]);
+  });
+  // Login into BO and go to brands page
+  loginCommon.loginBO();
+  it('should go to brands page', async function () {
+    await this.pageObjects.boBasePage.goToSubMenu(
+      this.pageObjects.boBasePage.productsParentLink,
+      this.pageObjects.boBasePage.brandsAndSuppliersLink,
+    );
+    await this.pageObjects.boBasePage.closeSfToolBar();
+    const pageTitle = await this.pageObjects.brandsPage.getPageTitle();
+    await expect(pageTitle).to.contains(this.pageObjects.brandsPage.pageTitle);
+  });
+  it('should reset all Brands filters', async function () {
+    await this.pageObjects.brandsPage.resetFilters('manufacturer');
+    numberOfBrands = await this.pageObjects.brandsPage.getNumberFromText(
+      this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer'),
+    );
+    await expect(numberOfBrands).to.be.above(0);
+  });
+  // 1: Create 2 Brands
+  describe('Create 2 Brands', async () => {
+    it('should go to new brand page', async function () {
+      await this.pageObjects.brandsPage.goToAddNewBrandPage();
+      const pageTitle = await this.pageObjects.addBrandPage.getPageTitle();
+      await expect(pageTitle).to.contains(this.pageObjects.addBrandPage.pageTitle);
+    });
+    it('should create first brand', async function () {
+      const result = await this.pageObjects.addBrandPage.createEditBrand(firstBrandData);
+      await expect(result).to.equal(this.pageObjects.brandsPage.successfulCreationMessage);
+      const numberOfBrandsAfterCreation = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer'),
+      );
+      await expect(numberOfBrandsAfterCreation).to.be.equal(numberOfBrands + 1);
+    });
+    it('should go to new brand page', async function () {
+      await this.pageObjects.brandsPage.goToAddNewBrandPage();
+      const pageTitle = await this.pageObjects.addBrandPage.getPageTitle();
+      await expect(pageTitle).to.contains(this.pageObjects.addBrandPage.pageTitle);
+    });
+    it('should create second brand', async function () {
+      const result = await this.pageObjects.addBrandPage.createEditBrand(secondBrandData);
+      await expect(result).to.equal(this.pageObjects.brandsPage.successfulCreationMessage);
+      const numberOfBrandsAfterCreation = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer'),
+      );
+      await expect(numberOfBrandsAfterCreation).to.be.equal(numberOfBrands + 2);
+    });
+  });
+  // 2 : Disable, enable Brands
+  describe('Disable, enable created Brands', async () => {
+    it('should filter Brand list by name of brand created', async function () {
+      await this.pageObjects.brandsPage.filterBrands('input', 'name', 'BrandToDelete');
+      const numberOfBrandsAfterFilter = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer'),
+      );
+      await expect(numberOfBrandsAfterFilter).to.be.at.most(numberOfBrands);
+      /* eslint-disable no-await-in-loop */
+      for (let i = 1; i <= numberOfBrandsAfterFilter; i++) {
+        const textColumn = await this.pageObjects.brandsPage.getTextContent(
+          this.pageObjects.brandsPage.tableColumn
+            .replace('%TABLE', 'manufacturer')
+            .replace('%ROW', 1)
+            .replace('%COLUMN', 'name'),
+        );
+        await expect(textColumn).to.contains('BrandToDelete');
+      }
+      /* eslint-enable no-await-in-loop */
+    });
+    it('should disable brands', async function () {
+      const disableTextResult = await this.pageObjects.brandsPage.changeBrandsEnabledColumnBulkActions(false);
+      await expect(disableTextResult).to.be.equal(this.pageObjects.brandsPage.successfulUpdateStatusMessage);
+      const numberOfBrandsInGrid = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer'),
+      );
+      await expect(numberOfBrandsInGrid).to.be.at.most(numberOfBrands);
+      /* eslint-disable no-await-in-loop */
+      for (let i = 1; i <= numberOfBrandsInGrid; i++) {
+        const textColumn = await this.pageObjects.brandsPage.getTextContent(
+          this.pageObjects.brandsPage.tableColumn
+            .replace('%TABLE', 'manufacturer')
+            .replace('%ROW', 1)
+            .replace('%COLUMN', 'active'),
+        );
+        await expect(textColumn).to.contains('clear');
+      }
+      /* eslint-enable no-await-in-loop */
+    });
+    it('should enable brands', async function () {
+      const disableTextResult = await this.pageObjects.brandsPage.changeBrandsEnabledColumnBulkActions(true);
+      await expect(disableTextResult).to.be.equal(this.pageObjects.brandsPage.successfulUpdateStatusMessage);
+      const numberOfBrandsInGrid = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer'),
+      );
+      await expect(numberOfBrandsInGrid).to.be.at.most(numberOfBrands);
+      /* eslint-disable no-await-in-loop */
+      for (let i = 1; i <= numberOfBrandsInGrid; i++) {
+        const textColumn = await this.pageObjects.brandsPage.getTextContent(
+          this.pageObjects.brandsPage.tableColumn
+            .replace('%TABLE', 'manufacturer')
+            .replace('%ROW', 1)
+            .replace('%COLUMN', 'active'),
+        );
+        await expect(textColumn).to.contains('check');
+      }
+      /* eslint-enable no-await-in-loop */
+    });
+    it('should reset Brand filters', async function () {
+      await this.pageObjects.brandsPage.resetFilters('manufacturer');
+      const numberOfBrandsAfterReset = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer'),
+      );
+      await expect(numberOfBrandsAfterReset).to.be.equal(numberOfBrands + 2);
+    });
+  });
+  // 3 : Delete Brands created with bulk actions
+  describe('Delete Brands with Bulk Actions', async () => {
+    it('should filter Brand list by name of brand created', async function () {
+      await this.pageObjects.brandsPage.filterBrands('input', 'name', 'BrandToDelete');
+      const numberOfBrandsAfterFilter = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer'),
+      );
+      await expect(numberOfBrandsAfterFilter).to.be.at.most(numberOfBrands);
+      /* eslint-disable no-await-in-loop */
+      for (let i = 1; i <= numberOfBrandsAfterFilter; i++) {
+        const textColumn = await this.pageObjects.brandsPage.getTextContent(
+          this.pageObjects.brandsPage.tableColumn
+            .replace('%TABLE', 'manufacturer')
+            .replace('%ROW', 1)
+            .replace('%COLUMN', 'name'),
+        );
+        await expect(textColumn).to.contains('BrandToDelete');
+      }
+      /* eslint-enable no-await-in-loop */
+    });
+    it('should delete Brands with Bulk Actions and check Result', async function () {
+      const deleteTextResult = await this.pageObjects.brandsPage.deleteWithBulkActions('manufacturer');
+      await expect(deleteTextResult).to.be.equal(this.pageObjects.brandsPage.successfulDeleteMessage);
+    });
+    it('should reset Brand filters', async function () {
+      await this.pageObjects.brandsPage.resetFilters('manufacturer');
+      const numberOfBrandsAfterReset = await this.pageObjects.brandsPage.getNumberFromText(
+        this.pageObjects.brandsPage.gridHeaderTitle.replace('%TABLE', 'manufacturer'),
+      );
+      await expect(numberOfBrandsAfterReset).to.be.equal(numberOfBrands);
+    });
+  });
+});
