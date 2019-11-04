c99dec0b8f14fbba594fe7f57811643ca4ab25ec
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2019-11-04 17:34:50 +0100

    Filter profiles

---
+++ tests/puppeteer/campaigns/data/demo/profile.js
@@ -0,0 +1,8 @@
+module.exports = {
+  Profiles: {
+    superAdmin: {id: 1, name: 'SuperAdmin'},
+    logistician: {id: 2, name: 'Logistician'},
+    translator: {id: 3, name: 'Translator'},
+    salesman: {id: 4, name: 'Salesman'},
+  },
+};
