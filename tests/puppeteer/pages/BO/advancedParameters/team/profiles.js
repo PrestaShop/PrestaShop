10cd93b847ddf0b8161c59e701a583e64f51ece4
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2019-11-04 17:15:54 +0100

    CRUD profile

--- tests/puppeteer/pages/BO/advancedParameters/team/profiles.js
+++ tests/puppeteer/pages/BO/advancedParameters/team/profiles.js
@@ -1,41 +1,40 @@
 require('module-alias/register');
 const BOBasePage = require('@pages/BO/BObasePage');
 
-module.exports = class Employees extends BOBasePage {
+module.exports = class Profiles extends BOBasePage {
   constructor(page) {
     super(page);
 
-    this.pageTitle = 'Employees';
+    this.pageTitle = 'Profiles';
     this.successfulUpdateStatusMessage = 'The status has been successfully updated.';
 
     // Selectors
     // Header links
-    this.addNewEmployeeLink = '#page-header-desc-configuration-add[title=\'Add new employee\']';
-    this.profilesTab = '#subtab-AdminProfiles';
-    // List of employees
-    this.employeeGridPanel = '#employee_grid_panel';
-    this.employeeGridTitle = `${this.employeeGridPanel} h3.card-header-title`;
-    this.employeesListForm = '#employee_grid';
-    this.employeesListTableRow = `${this.employeesListForm} tbody tr:nth-child(%ROW)`;
-    this.employeesListTableColumn = `${this.employeesListTableRow} td.column-%COLUMN`;
-    this.employeesListTableToggleDropDown = `${this.employeesListTableColumn.replace('%COLUMN', 'actions')} 
+    this.addNewProfileLink = '#page-header-desc-configuration-add[title=\'Add new profile\']';
+    // List of profiles
+    this.profileGridPanel = '#profile_grid_panel';
+    this.profileGridTitle = `${this.profileGridPanel} h3.card-header-title`;
+    this.profilesListForm = '#profile_grid';
+    this.profilesListTableRow = `${this.profilesListForm} tbody tr:nth-child(%ROW)`;
+    this.profilesListTableColumn = `${this.profilesListTableRow} td.column-%COLUMN`;
+    this.profilesListTableToggleDropDown = `${this.profilesListTableColumn.replace('%COLUMN', 'actions')} 
     a[data-toggle='dropdown']`;
-    this.employeesListTableDeleteLink = `${this.employeesListTableColumn.replace('%COLUMN', 'actions')} a[data-url]`;
-    this.employeesListTableEditLink = `${this.employeesListTableColumn.replace('%COLUMN', 'actions')} a[href*='edit']`;
-    this.employeesListColumnValidIcon = `${this.employeesListTableColumn.replace('%COLUMN', 'active')} 
+    this.profilesListTableDeleteLink = `${this.profilesListTableColumn.replace('%COLUMN', 'actions')} a[data-url]`;
+    this.profilesListTableEditLink = `${this.profilesListTableColumn.replace('%COLUMN', 'actions')} a[href*='edit']`;
+    this.profilesListColumnValidIcon = `${this.profilesListTableColumn.replace('%COLUMN', 'active')} 
     i.grid-toggler-icon-valid`;
-    this.employeesListColumnNotValidIcon = `${this.employeesListTableColumn.replace('%COLUMN', 'active')} 
+    this.profilesListColumnNotValidIcon = `${this.profilesListTableColumn.replace('%COLUMN', 'active')} 
     i.grid-toggler-icon-not-valid`;
     // Filters
-    this.employeeFilterInput = `${this.employeesListForm} #employee_%FILTERBY`;
-    this.filterSearchButton = `${this.employeesListForm} button[name='employee[actions][search]']`;
-    this.filterResetButton = `${this.employeesListForm} button[name='employee[actions][reset]']`;
+    this.profileFilterInput = `${this.profilesListForm} #profile_%FILTERBY`;
+    this.filterSearchButton = `${this.profilesListForm} button[name='profile[actions][search]']`;
+    this.filterResetButton = `${this.profilesListForm} button[name='profile[actions][reset]']`;
     // Bulk Actions
-    this.selectAllRowsLabel = `${this.employeesListForm} .md-checkbox label`;
-    this.bulkActionsToggleButton = `${this.employeesListForm} button.dropdown-toggle`;
-    this.bulkActionsEnableButton = `${this.employeesListForm} #employee_grid_bulk_action_enable_selection`;
-    this.bulkActionsDisableButton = `${this.employeesListForm} #employee_grid_bulk_action_disable_selection`;
-    this.bulkActionsDeleteButton = `${this.employeesListForm} #employee_grid_bulk_action_delete_selection`;
+    this.selectAllRowsLabel = `${this.profilesListForm} .md-checkbox label`;
+    this.bulkActionsToggleButton = `${this.profilesListForm} button.dropdown-toggle`;
+    this.bulkActionsEnableButton = `${this.profilesListForm} #profile_grid_bulk_action_enable_selection`;
+    this.bulkActionsDisableButton = `${this.profilesListForm} #profile_grid_bulk_action_disable_selection`;
+    this.bulkActionsDeleteButton = `${this.profilesListForm} #profile_grid_bulk_action_delete_selection`;
   }
 
   /*
@@ -43,11 +42,11 @@ module.exports = class Employees extends BOBasePage {
    */
 
   /**
-   * Go to new Page Employee page
+   * Go to new Page profile page
    * @return {Promise<void>}
    */
-  async goToAddNewEmployeePage() {
-    await this.clickAndWaitForNavigation(this.addNewEmployeeLink);
+  async goToAddNewProfilePage() {
+    await this.clickAndWaitForNavigation(this.addNewProfileLink);
   }
 
   /**
@@ -58,36 +57,36 @@ module.exports = class Employees extends BOBasePage {
     if (await this.elementVisible(this.filterResetButton, 2000)) {
       await this.clickAndWaitForNavigation(this.filterResetButton);
     }
-    return this.getNumberFromText(this.employeeGridTitle);
+    return this.getNumberFromText(this.profileGridTitle);
   }
 
   /**
-   * Go to Edit employee page
+   * Go to Edit profile page
    * @param row, row in table
    * @return {Promise<void>}
    */
-  async goToEditEmployeePage(row) {
+  async goToEditProfilePage(row) {
     // Click on edit
     await Promise.all([
-      this.page.click(this.employeesListTableEditLink.replace('%ROW', row)),
+      this.page.click(this.profilesListTableEditLink.replace('%ROW', row)),
       this.page.waitForNavigation({waitUntil: 'networkidle0'}),
     ]);
   }
 
   /**
-   * Filter list of employees
+   * Filter list of profiles
    * @param filterType, input or select to choose method of filter
    * @param filterBy, column to filter
    * @param value, value to filter with
    * @return {Promise<void>}
    */
-  async filterEmployees(filterType, filterBy, value = '') {
+  async filterProfiles(filterType, filterBy, value = '') {
     switch (filterType) {
       case 'input':
-        await this.setValue(this.employeeFilterInput.replace('%FILTERBY', filterBy), value.toString());
+        await this.setValue(this.profileFilterInput.replace('%FILTERBY', filterBy), value.toString());
         break;
       case 'select':
-        await this.selectByVisibleText(this.employeeFilterInput.replace('%FILTERBY', filterBy), value ? 'Yes' : 'No');
+        await this.selectByVisibleText(this.profileFilterInput.replace('%FILTERBY', filterBy), value ? 'Yes' : 'No');
         break;
       default:
       // Do nothing
@@ -106,7 +105,7 @@ module.exports = class Employees extends BOBasePage {
    */
   async getToggleColumnValue(row) {
     if (await this.elementVisible(
-      this.employeesListColumnValidIcon.replace('%ROW', row), 100)) return true;
+      this.profilesListColumnValidIcon.replace('%ROW', row), 100)) return true;
     return false;
   }
 
@@ -118,12 +117,12 @@ module.exports = class Employees extends BOBasePage {
    */
   async updateToggleColumnValue(row, valueWanted = true) {
     if (await this.getToggleColumnValue(row) !== valueWanted) {
-      this.page.click(this.employeesListTableColumn.replace('%ROW', row).replace('%COLUMN', 'active'));
+      this.page.click(this.profilesListTableColumn.replace('%ROW', row).replace('%COLUMN', 'active'));
       if (valueWanted) {
-        await this.page.waitForSelector(this.employeesListColumnValidIcon.replace('%ROW', row));
+        await this.page.waitForSelector(this.profilesListColumnValidIcon.replace('%ROW', row));
       } else {
         await this.page.waitForSelector(
-          this.employeesListColumnNotValidIcon.replace('%ROW', row));
+          this.profilesListColumnNotValidIcon.replace('%ROW', row));
       }
       return true;
     }
@@ -131,28 +130,28 @@ module.exports = class Employees extends BOBasePage {
   }
 
   /**
-   * Delete employee
+   * Delete profile
    * @param row, row in table
    * @return {Promise<textContent>}
    */
-  async deleteEmployee(row) {
+  async deleteProfile(row) {
     this.dialogListener();
     // Click on dropDown
     await Promise.all([
-      this.page.click(this.employeesListTableToggleDropDown.replace('%ROW', row)),
+      this.page.click(this.profilesListTableToggleDropDown.replace('%ROW', row)),
       this.page.waitForSelector(
-        `${this.employeesListTableToggleDropDown.replace('%ROW', row)}[aria-expanded='true']`, {visible: true}),
+        `${this.profilesListTableToggleDropDown.replace('%ROW', row)}[aria-expanded='true']`, {visible: true}),
     ]);
     // Click on delete
     await Promise.all([
-      this.page.click(this.employeesListTableDeleteLink.replace('%ROW', row)),
+      this.page.click(this.profilesListTableDeleteLink.replace('%ROW', row)),
       this.page.waitForSelector(this.alertSuccessBlockParagraph),
     ]);
     return this.getTextContent(this.alertSuccessBlockParagraph);
   }
 
   /**
-   * Enable / disable employees by Bulk Actions
+   * Enable / disable profiles by Bulk Actions
    * @param enable
    * @return {Promise<textContent>}
    */
@@ -176,7 +175,7 @@ module.exports = class Employees extends BOBasePage {
   }
 
   /**
-   * Delete all employees with Bulk Actions
+   * Delete all profiles with Bulk Actions
    * @return {Promise<textContent>}
    */
   async deleteBulkActions() {
@@ -197,9 +196,5 @@ module.exports = class Employees extends BOBasePage {
       this.page.waitForSelector(this.alertSuccessBlockParagraph),
     ]);
     return this.getTextContent(this.alertSuccessBlockParagraph);
-  }
-
-  async goToProfilesPage() {
-    await this.clickAndWaitForNavigation(this.profilesTab);
   }
 };
