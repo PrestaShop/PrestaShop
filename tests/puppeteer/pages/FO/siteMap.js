aa96edbe693b1040f589869364cf7537a88cab34
Boubker BRIBRI <boubker.bribri@prestashop.com>
2019-11-12 13:50:59 +0100

    Adding new functions for pages categories Bo and siteMap FO

--- tests/puppeteer/pages/FO/siteMap.js
+++ tests/puppeteer/pages/FO/siteMap.js
@@ -10,4 +10,16 @@ module.exports = class SiteMap extends FOBasePage {
     // Selectors
     this.categoryNameSelect = '#category-page-%ID';
   }
+
+  /*
+  Methods
+   */
+  /**
+   *
+   * @param categoryID
+   * @return {Promise<void>}
+   */
+  async getCategoryName(categoryID) {
+    return this.getTextContent(this.categoryNameSelect.replace('%ID', categoryID));
+  }
 };
