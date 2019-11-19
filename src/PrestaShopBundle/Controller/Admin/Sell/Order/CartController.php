3ed99a4d75a1ca19dbab134f45615ac4a1909c4b
Julius Zukauskas <zuk3975@gmail.com>
2019-11-19 17:26:24 +0200

    adds commented legacy link. Removes Unused Cart index action

--- src/PrestaShopBundle/Controller/Admin/Sell/Order/CartController.php
+++ src/PrestaShopBundle/Controller/Admin/Sell/Order/CartController.php
@@ -61,14 +61,6 @@ use Symfony\Component\HttpFoundation\Response;
 class CartController extends FrameworkBundleAdminController
 {
     /**
-     * @return Response
-     */
-    public function indexAction()
-    {
-        return $this->render('@PrestaShop/Admin/Sell/Order/Cart/index.html.twig');
-    }
-
-    /**
      * @AdminSecurity("is_granted('read', request.get('_legacy_controller'))")
      *
      * @param Request $request
