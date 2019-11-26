fb6f71e6ac793c438b27948eb5bedda803885dbc
Tadas Davidsonas <tadas.davidsonas@invertus.eu>
2019-11-26 17:14:55 +0200

    order integration-tests with behat
    Refactored all the behat tests I've created up untill now to use the Domain classes avoiding the legacy code.
    
    started creating:
    Feature: Adding products to an existing Order

--- tests/Integration/Behaviour/Features/Context/Domain/OrderFeatureContext.php
+++ tests/Integration/Behaviour/Features/Context/Domain/OrderFeatureContext.php
@@ -26,8 +26,9 @@
 
 namespace Tests\Integration\Behaviour\Features\Context\Domain;
 
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Gherkin\Node\TableNode;
 use Order;
-use OrderCarrier;
 use OrderState;
 use PrestaShop\PrestaShop\Core\Domain\Order\Command\AddOrderFromBackOfficeCommand;
 use PrestaShop\PrestaShop\Core\Domain\Order\Command\BulkChangeOrderStatusCommand;
@@ -35,10 +36,10 @@ use PrestaShop\PrestaShop\Core\Domain\Order\Command\UpdateOrderShippingDetailsCo
 use PrestaShop\PrestaShop\Core\Domain\Order\Command\UpdateOrderStatusCommand;
 use PrestaShop\PrestaShop\Core\Domain\Order\Exception\OrderException;
 use PrestaShop\PrestaShop\Core\Domain\Order\Product\Command\AddProductToOrderCommand;
+use PrestaShop\PrestaShop\Core\Domain\Order\Query\GetOrderForViewing;
+use PrestaShop\PrestaShop\Core\Domain\Order\QueryResult\OrderCarrierForViewing;
+use PrestaShop\PrestaShop\Core\Domain\Order\QueryResult\OrderForViewing;
 use PrestaShop\PrestaShop\Core\Domain\Order\ValueObject\OrderId;
-use PrestaShopCollection;
-use PrestaShopDatabaseException;
-use PrestaShopException;
 use RuntimeException;
 use Tests\Integration\Behaviour\Features\Context\SharedStorage;
 use PrestaShop\PrestaShop\Core\Domain\Order\Invoice\Command\GenerateInvoiceCommand;
@@ -63,7 +64,9 @@ class OrderFeatureContext extends AbstractDomainFeatureContext
     public function before()
     {
         // needed because if no controller defined then CONTEXT_ALL is selected and exception is thrown
-        Context::getContext()->controller = 'test';
+        $adminControllerTestDouble = new \stdClass();
+        $adminControllerTestDouble->controller_type = 'admin';
+        Context::getContext()->controller = $adminControllerTestDouble;
     }
 
     /**
@@ -128,6 +131,8 @@ class OrderFeatureContext extends AbstractDomainFeatureContext
 
     /**
      * @When I generate invoice for :invoiceReference order
+     *
+     * @param $orderReference
      */
     public function generateOrderInvoice($orderReference)
     {
@@ -143,21 +148,21 @@ class OrderFeatureContext extends AbstractDomainFeatureContext
     /**
      * @When I update orders :references to status :status
      *
+     * @param string $orderIdsString
+     * @param string $status
+     *
      * @throws OrderException
      */
-    public function iUpdateOrdersToStatus(string $references, string $status)
+    public function iUpdateOrdersToStatus(string $orderIdsString, string $status)
     {
-        /** @var string[] $references */
-        $references = explode(',', $references);
+        /** @var string[] $orderIdsString */
+        $orderIdsString = explode(',', $orderIdsString);
         $ordersIds = [];
-        foreach ($references as $orderReference) {
-            /** @var Order $order */
-            $order = $this->getOrderByReference($orderReference);
-            $orderId = (int) $order->id;
-            $ordersIds[] = $orderId;
+        foreach ($orderIdsString as $orderIdString) {
+            $ordersIds[] = (int) $orderIdString;
         }
 
-        $statusId = $this->getOrderStatusId($status);
+        $statusId = $this->getOrderStatusIdFromMap($status);
         $this->getCommandBus()->handle(
             new BulkChangeOrderStatusCommand(
                 $ordersIds, $statusId
@@ -166,17 +171,18 @@ class OrderFeatureContext extends AbstractDomainFeatureContext
     }
 
     /**
-     * @Then order :reference has status :status
+     * @Then order :orderId has status :status
+     *
+     * @param int $orderId
+     * @param string $status
      */
-    public function orderHasStatus(string $reference, string $status)
+    public function orderHasStatus(int $orderId, string $status)
     {
-        /** @var PrestaShopCollection|Order[] $orderDetails */
-        $orders = Order::getByReference($reference);
-        /** @var Order $order */
-        $order = $orders->getFirst();
+        /** @var OrderForViewing $orderForViewing */
+        $orderForViewing = $this->getQueryBus()->handle(new GetOrderForViewing($orderId));
         /** @var OrderState $currentOrderState */
-        $currentOrderStateId = (int) $order->getCurrentState();
-        $statusId = $this->getOrderStatusId($status);
+        $currentOrderStateId = $orderForViewing->getHistory()->getCurrentOrderStatusId();
+        $statusId = $this->getOrderStatusIdFromMap($status);
         if ($currentOrderStateId !== $statusId) {
             throw new RuntimeException(
                 'After changing order status id should be [' . $statusId . '] but received [' . $currentOrderStateId . ']'
@@ -185,30 +191,24 @@ class OrderFeatureContext extends AbstractDomainFeatureContext
     }
 
     /**
-     * @Given there is existing order with reference :reference
+     * @Given there is existing order with id :orderId
      *
-     * @param string $reference
+     * @param int $orderId
      */
-    public function thereIsExistingOrderWithReference(string $reference)
+    public function thereIsExistingOrderWithId(int $orderId)
     {
-        /** @var PrestaShopCollection $orders */
-        $orders = Order::getByReference($reference);
-        if ($orders->count() === 0) {
-            throw new RuntimeException(
-                'There is no order with reference [' . $reference . ']'
-            );
-        }
+        $this->getQueryBus()->handle(new GetOrderForViewing($orderId));
     }
 
     /**
-     * @When I update order :reference to status :status
+     * @When I update order :orderId to status :status
+     *
+     * @param int $orderId
+     * @param string $status
      */
-    public function iUpdateOrderToStatus(string $reference, string $status)
+    public function iUpdateOrderToStatus(int $orderId, string $status)
     {
-        $statusId = $this->getOrderStatusId($status);
-        /** @var Order $order */
-        $order = $this->getOrderByReference($reference);
-        $orderId = (int) $order->id;
+        $statusId = $this->getOrderStatusIdFromMap($status);
         $this->getCommandBus()->handle(
             new UpdateOrderStatusCommand(
                 $orderId,
@@ -220,17 +220,15 @@ class OrderFeatureContext extends AbstractDomainFeatureContext
     /**
      * @When I update order :reference Tracking number to :trackingNumber and Carrier to :carrier
      *
-     * @param string $reference
+     * @param string $orderId
      * @param string $trackingNumber
      * @param string $carrier
      */
-    public function iUpdateOrderTrackingNumberToAndCarrierTo(string $reference, string $trackingNumber, string $carrier)
+    public function iUpdateOrderTrackingNumberToAndCarrierTo(string $orderId, string $trackingNumber, string $carrier)
     {
-        /** @var Order $order */
-        $order = $this->getOrderByReference($reference);
-        $oldOrderCarrierId = $order->getIdOrderCarrier();
-        $orderId = (int) $order->id;
-        $newCarrierId = $this->getCarrierId($carrier);
+        $oldOrderCarrierId = $this->getCarrierIdFromMap($carrier);
+        $this->getQueryBus()->handle(new GetOrderForViewing($orderId));
+        $newCarrierId = $this->getCarrierIdFromMap($carrier);
 
         $this->getCommandBus()->handle(
             new UpdateOrderShippingDetailsCommand(
@@ -243,65 +241,41 @@ class OrderFeatureContext extends AbstractDomainFeatureContext
     }
 
     /**
-     * @Then order :reference has Tracking number :trackingNumber
+     * @Then order :orderId has Tracking number :trackingNumber
      *
-     * @param string $orderReference
+     * @param int $orderId
      * @param string $trackingNumber
      */
-    public function orderHasTrackingNumber(string $orderReference, string $trackingNumber)
+    public function orderHasTrackingNumber(int $orderId, string $trackingNumber)
     {
-        /** @var Order $order */
-        $order = $this->getOrderByReference($orderReference);
-        /** @var string $trackingNumberFromDb */
-        $trackingNumberFromDb = $order->getWsShippingNumber();
-        if ($trackingNumber !== $trackingNumberFromDb) {
-            $msg = 'Order [' . $orderReference . '] tracking number is not equal to [' . $trackingNumber . '] ';
-            $msg .= 'Received [' . $trackingNumberFromDb . '] ';
+        $orderCarriersForViewing = $this->getOrderCarriersForViewing($orderId);
+        $orderTrackingNumberFromDb = $orderCarriersForViewing[0]->getTrackingNumber();
+
+        if ($trackingNumber !== $orderTrackingNumberFromDb) {
+            $msg = 'Order [' . $orderId . '] tracking number is not equal to [' . $trackingNumber . '] ';
+            $msg .= 'Received [' . $orderTrackingNumberFromDb . '] ';
             throw new RuntimeException($msg);
         }
     }
 
     /**
-     * @Then order :orderReference has Carrier :carrier
+     * @Then order :orderId has Carrier :carrier
      *
-     * @param string $orderReference
+     * @param string $orderId
      * @param string $carrier
-     *
-     * @throws PrestaShopDatabaseException
-     * @throws PrestaShopException
      */
-    public function orderHasCarrier(string $orderReference, string $carrier)
+    public function orderHasCarrier(string $orderId, string $carrier)
     {
-        /** @var Order $order */
-        $order = $this->getOrderByReference($orderReference);
-
-        $carrierId = $this->getCarrierId($carrier);
-        $orderCarrierIdFromDb = (int) $order->getIdOrderCarrier();
-        $orderCarrier = new OrderCarrier($orderCarrierIdFromDb);
-        $carrierIdFromDb = (int) $orderCarrier->id_carrier;
+        $carrierId = $this->getCarrierIdFromMap($carrier);
+        /** @var OrderCarrierForViewing[] $orderCarriersForViewing */
+        $orderCarriersForViewing = $this->getOrderCarriersForViewing($orderId);
+        $carrierIdFromDb = $orderCarriersForViewing[0]->getCarrierId();
 
         if ($carrierId !== $carrierIdFromDb) {
-            $msg = 'Order [' . $orderReference . '] carrier id is not equal to [' . $carrierId . '] ';
+            $msg = 'Order [' . $orderId . '] carrier id is not equal to [' . $carrierId . '] ';
             $msg .= 'Received [' . $carrierIdFromDb . '] ';
             throw new RuntimeException($msg);
         }
-    }
-
-    /**
-     * @param string $reference
-     *
-     * @return Order
-     */
-    private function getOrderByReference(string $reference)
-    {
-        /** @var PrestaShopCollection $ordersCollection */
-        $ordersCollection = Order::getByReference($reference);
-        /** @var Order $order */
-        $order = $ordersCollection->getFirst();
-        if ($order) {
-            return $order;
-        }
-        throw new RuntimeException('Order with reference [' . $reference . '] does not exist');
     }
 
     /**
@@ -309,7 +283,7 @@ class OrderFeatureContext extends AbstractDomainFeatureContext
      *
      * @return int
      */
-    private function getOrderStatusId(string $status)
+    private function getOrderStatusIdFromMap(string $status)
     {
         $orderStatusMapFlipped = array_flip(self::ORDER_STATUS_MAP);
         if (isset($orderStatusMapFlipped[$status])) {
@@ -326,7 +300,7 @@ class OrderFeatureContext extends AbstractDomainFeatureContext
      *
      * @return int
      */
-    private function getCarrierId(string $carrier)
+    private function getCarrierIdFromMap(string $carrier)
     {
         $carrierMapFlipped = array_flip(self::CARRIER_MAP);
         if (isset($carrierMapFlipped[$carrier])) {
@@ -336,5 +310,41 @@ class OrderFeatureContext extends AbstractDomainFeatureContext
             return $carrierId;
         }
         throw new RuntimeException('Invalid carrier [' . $carrier . ']');
+    }
+
+    /**
+     * @When I add payment to order with id :arg1 with the following properties:
+     */
+    public function iAddPaymentToOrderWithIdWithTheFollowingProperties($arg1, TableNode $table)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then if I query order with id :arg1 payments I should get an Order with properties:
+     */
+    public function ifIQueryOrderWithIdPaymentsIShouldGetAnOrderWithProperties($arg1, TableNode $table)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @param int $orderId
+     *
+     * @return array|OrderCarrierForViewing[]
+     */
+    private function getOrderCarriersForViewing(int $orderId)
+    {
+        /** @var OrderForViewing $orderForViewing */
+        $orderForViewing = $this->getQueryBus()->handle(new GetOrderForViewing($orderId));
+        /** @var OrderCarrierForViewing[] $orderCarriers */
+        $orderCarriersForViewing = $orderForViewing->getShipping()->getCarriers();
+
+        if (count($orderCarriersForViewing) == 0) {
+            $msg = 'Order [' . $orderId . '] has no carriers';
+            throw new RuntimeException($msg);
+        }
+
+        return $orderCarriersForViewing;
     }
 }
