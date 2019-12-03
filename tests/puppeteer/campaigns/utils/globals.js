2a5abf8345f288ee8266df7898903832d9ad14cc
Boubker BRIBRI <boubker.bribri@prestashop.com>
2019-12-03 10:46:00 +0100

    update downloads directory

--- tests/puppeteer/campaigns/utils/globals.js
+++ tests/puppeteer/campaigns/utils/globals.js
@@ -9,7 +9,7 @@ global.BO = {
   PASSWD: process.env.PASSWD || DefaultAccount.password,
   FIRSTNAME: process.env.FIRSTNAME || DefaultAccount.firstName,
   LASTNAME: process.env.LASTNAME || DefaultAccount.lastName,
-  DOWNLOAD_PATH: process.env.DOWNLOAD_PATH || '/home/pptruser/Downloads',
+  DOWNLOAD_PATH: process.env.DOWNLOAD_PATH || '/downloads',
 };
 global.INSTALL = {
   URL: process.env.URL_INSTALL || `${global.FO.URL}install-dev/`,
