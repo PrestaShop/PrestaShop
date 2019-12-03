318373692730c980e3cca95efcf0a9ed0eed4418
Progi1984 <franck.lefevre@prestashop.com>
2019-12-03 14:04:50 +0100

    Feedback from @jolelievre

--- src/PrestaShopBundle/Controller/Admin/Sell/Order/OrderController.php
+++ src/PrestaShopBundle/Controller/Admin/Sell/Order/OrderController.php
@@ -323,6 +323,8 @@ class OrderController extends FrameworkBundleAdminController
     }
 
     /**
+     * @AdminSecurity("is_granted(['create'], request.get('_legacy_controller'))", redirectRoute="admin_orders_index")
+     *
      * @param int $orderId
      * @param Request $request
      *
@@ -347,9 +349,8 @@ class OrderController extends FrameworkBundleAdminController
 
         return $this->render('@PrestaShop/Admin/Sell/Order/Order/Blocks/View/product.html.twig', [
             'orderForViewing' => $orderForViewing,
-            'product' => end($products),
-            ]
-        );
+            'product' => $products[array_key_last($products)],
+        ]);
     }
 
     /**
@@ -722,7 +723,7 @@ class OrderController extends FrameworkBundleAdminController
                 new DeleteProductFromOrderCommand($orderId, $orderDetailId)
             );
 
-            return $this->json(null);
+            return $this->json(null, Response::HTTP_NO_CONTENT);
         } catch (Exception $e) {
             return $this->json(
                 ['message' => $this->getErrorMessageForException($e, $this->getErrorMessages($e))],
