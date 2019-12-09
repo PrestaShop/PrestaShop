b1f5ba43986aeec232c5eba8eface1739bc29d44
Tadas Davidsonas <tadas.davidsonas@invertus.eu>
2019-12-09 15:23:49 +0200

    Behat test feature for DebugModeHandler

---
+++ tests/Integration/Behaviour/Features/Context/Domain/DebugModeFeatureContext.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Integration\Behaviour\Features\Context\Domain;
+
+use PHPUnit_Framework_AssertionFailedError;
+use PrestaShop\PrestaShop\Adapter\Debug\DebugMode;
+use PrestaShop\PrestaShop\Core\Domain\Configuration\Command\SwitchDebugModeCommand;
+
+class DebugModeFeatureContext extends AbstractDomainFeatureContext
+{
+    /**
+     * @When I enable debug mode
+     */
+    public function iEnableDebugMode()
+    {
+        $this->getCommandBus()->handle(new SwitchDebugModeCommand(true));
+    }
+
+    /**
+     * @When I disable debug mode
+     */
+    public function iDisableDebugMode()
+    {
+        $this->getCommandBus()->handle(new SwitchDebugModeCommand(false));
+    }
+
+    /**
+     * @Then debug mode should be enabled in the configuration
+     *
+     * @throws PHPUnit_Framework_AssertionFailedError
+     */
+    public function debugModeShouldBeEnabledInTheConfiguration()
+    {
+        /** @var DebugMode $configuration */
+        $configuration = $this->getContainer()->get('prestashop.adapter.debug_mode');
+        $isDebugModeEnabled = $configuration->isDebugModeEnabled();
+        assertTrue($isDebugModeEnabled, 'Debug mode is not enabled');
+    }
+
+    /**
+     * @Then debug mode should be disabled in the configuration
+     *
+     * @throws PHPUnit_Framework_AssertionFailedError
+     */
+    public function debugModeShouldBeDisabledInTheConfiguration()
+    {
+        /** @var DebugMode $configuration */
+        $configuration = $this->getContainer()->get('prestashop.adapter.debug_mode');
+        $isDebugModeEnabled = $configuration->isDebugModeEnabled();
+        assertFalse($isDebugModeEnabled, 'Debug mode is enabled');
+    }
+}
--- tests/Integration/Behaviour/Features/Scenario/Misc/switch_debug_mode.feature
+++
@@ -1,7 +0,0 @@
-@reset-database-before-feature
-Feature: Switch debug mode
-  In order to see or hide what exactly is causing the error
-  As a BO user
-  I need to be able to enable or disable debug mode
-
-  Scenario:
