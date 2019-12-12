387c3a2c260a53af0c7af1b6ebba0a4f9eb0ec21
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2019-12-12 16:55:20 +0100

    Complete the test

--- tests/puppeteer/campaigns/data/faker/invoice.js
+++ tests/puppeteer/campaigns/data/faker/invoice.js
@@ -2,7 +2,7 @@ const faker = require('faker');
 
 module.exports = class Invoice {
   constructor(invoiceOptions = {}) {
-    this.invoiceNumber = invoiceOptions.invoiceNumber || faker.random.number;
+    this.invoiceNumber = invoiceOptions.invoiceNumber || faker.random.number({min: 1, max: 200}).toString();
     this.legalFreeText = invoiceOptions.legalFreeText || faker.lorem.sentence();
     this.footerText = invoiceOptions.footerText || faker.lorem.word();
   }
