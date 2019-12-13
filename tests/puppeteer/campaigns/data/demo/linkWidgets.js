45ddd9591dfae39c670f9fa5e2276ee33157873f
Boubker BRIBRI <boubker.bribri@prestashop.com>
2019-12-13 10:44:54 +0100

    Add link widgets demo data

---
+++ tests/puppeteer/campaigns/data/demo/linkWidgets.js
@@ -0,0 +1,13 @@
+module.exports = {
+  LinkWidgets: {
+    demo_1: {
+      name: 'Test footer',
+      frName: 'Test footer',
+      hook: 'displayFooter',
+      contentPages: [],
+      productsPages: [],
+      staticPages: [],
+      customPages: [{name: 'Home in footer', url: global.FO.URL}],
+    },
+  },
+};
