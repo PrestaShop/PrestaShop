848cacae06c1852392eb4250c4c54b95f016e478
Boubker BRIBRI <boubker.bribri@prestashop.com>
2019-12-16 15:06:40 +0100

    Update suppliers URL

--- tests/puppeteer/tools/urls.js
+++ tests/puppeteer/tools/urls.js
@@ -44,7 +44,7 @@ module.exports = [
       {name: 'BO_brands', url: 'index.php/sell/catalog/brands/'},
       {name: 'BO_add_brand', url: 'index.php/sell/catalog/brands/new'},
       {name: 'BO_add_brand_address', url: 'index.php/sell/catalog/brands/addresses/new'},
-      {name: 'BO_suppliers', url: 'index.php?controller=AdminSuppliers'},
+      {name: 'BO_suppliers', url: 'index.php/sell/catalog/suppliers'},
       {name: 'BO_add_supplier', url: 'index.php/sell/catalog/suppliers/new'},
       {name: 'BO_files', url: 'index.php?controller=AdminAttachments'},
       {name: 'BO_add_file', url: 'index.php?controller=AdminAttachments&addattachment'},
