b22c05eb4914cdf5fb3d5cdebddf6a2d652c42d3
Tadas Davidsonas <tadas.davidsonas@invertus.eu>
2019-12-17 13:13:07 +0200

    AddEmployeeHandler test case created

--- tests/Integration/Behaviour/Features/Context/Domain/ShowcaseCardFeatureContext.php
+++ tests/Integration/Behaviour/Features/Context/Domain/ShowcaseCardFeatureContext.php
@@ -5,28 +5,32 @@ namespace Tests\Integration\Behaviour\Features\Context\Domain;
 use PHPUnit_Framework_Assert;
 use PrestaShop\PrestaShop\Core\Domain\ShowcaseCard\Command\CloseShowcaseCardCommand;
 use PrestaShop\PrestaShop\Core\Domain\ShowcaseCard\Query\GetShowcaseCardIsClosed;
+use Tests\Integration\Behaviour\Features\Context\SharedStorage;
 
 class ShowcaseCardFeatureContext extends AbstractDomainFeatureContext
 {
     /**
-     * @When I close :cardName showcase card as employee :employeeId
+     * @When employee :employee closes showcase card :cardName
      *
      * @param string $cardName
-     * @param int $employeeId
+     * @param string $employeeReference
      */
-    public function closeShowcaseCardAsEmployee(string $cardName, int $employeeId)
+    public function employeeClosesShowcaseCard(string $cardName, string $employeeReference)
     {
+        $employeeId = SharedStorage::getStorage()->get($employeeReference);
         $this->getCommandBus()->handle(new CloseShowcaseCardCommand($employeeId, $cardName));
     }
 
     /**
-     * @Then showcase card :cardName for employee :employeeId should be closed
+     * @Then employee :employeeReference should not see showcase card :cardName
      *
+     * @param string $employeeReference
      * @param string $cardName
-     * @param int $employeeId
      */
-    public function showcaseCardForEmployeeShouldBeClosed(string $cardName, int $employeeId)
+    public function employeeShouldNotSeeShowcaseCard(string $employeeReference, string $cardName)
     {
+        $employeeId = SharedStorage::getStorage()->get($employeeReference);
+
         $showcaseCardIsClosed = $this->getQueryBus()->handle(
             new GetShowcaseCardIsClosed((int) $employeeId, $cardName)
         );
