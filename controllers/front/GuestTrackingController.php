27cd627618d5f229d5c9c3c1f4bb3c0efc45d0b0
Daniel Hlavacek <daniel.hlavacek@hotmail.cz>
2019-12-19 16:29:51 +0100

    Improvement

--- controllers/front/GuestTrackingController.php
+++ controllers/front/GuestTrackingController.php
@@ -151,8 +151,15 @@ class GuestTrackingControllerCore extends FrontController
 
         $breadcrumbLinks['links'][] = array(
             'title' => $this->getTranslator()->trans('Guest order tracking', array(), 'Shop.Theme.Checkout'),
-            'url' => '#',
+            'url' => $this->context->link->getPageLink('guest-tracking'),
         );
+
+        if (Validate::isLoadedObject($this->order)) {
+            $breadcrumbLinks['links'][] = array(
+                'title' => $this->order->reference,
+                'url' => '#',
+            );
+        }
 
         return $breadcrumbLinks;
     }
