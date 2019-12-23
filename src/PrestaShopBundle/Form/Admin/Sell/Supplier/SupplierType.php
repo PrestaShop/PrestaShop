7929c33d03d4406745973f0f3b7448bbd2eca11b
Tadas Davidsonas <tadas.davidsonas@invertus.eu>
2019-12-23 17:12:41 +0200

    suppliers form rendering simplified - WIP

--- src/PrestaShopBundle/Form/Admin/Sell/Supplier/SupplierType.php
+++ src/PrestaShopBundle/Form/Admin/Sell/Supplier/SupplierType.php
@@ -166,21 +166,28 @@ class SupplierType extends TranslatorAwareType
                 ],
             ])
             ->add('phone', TextType::class, [
+                'label' => $this->trans('Home phone', 'Admin.Global'),
+                'help' => $this->trans('Phone number for this supplier', 'Admin.Catalog.Help'),
                 'required' => false,
                 'constraints' => $this->getPhoneCommonConstraints(),
             ])
             ->add('mobile_phone', TextType::class, [
+                'label' => $this->trans('Mobile phone', 'Admin.Global'),
+                'help' => $this->trans('Mobile phone number for this supplier.', 'Admin.Catalog.Help'),
                 'required' => false,
                 'constraints' => $this->getPhoneCommonConstraints(),
             ])
             ->add('address', TextType::class, [
+                'label' => $this->trans('Address', 'Admin.Global'),
                 'constraints' => $this->getAddressCommonConstraints(),
             ])
             ->add('address2', TextType::class, [
+                'label' => $this->trans('Address (2)', 'Admin.Global'),
                 'required' => false,
                 'constraints' => $this->getAddressCommonConstraints(),
             ])
             ->add('post_code', TextType::class, [
+                'label' => $this->trans('Zip/postal code', 'Admin.Global'),
                 'required' => false,
                 'constraints' => [
                     new TypedRegex([
@@ -197,6 +204,7 @@ class SupplierType extends TranslatorAwareType
                 ],
             ])
             ->add('city', TextType::class, [
+                'label' => $this->trans('City', 'Admin.Global'),
                 'constraints' => [
                     new NotBlank([
                         'message' => $this->trans(
@@ -217,6 +225,8 @@ class SupplierType extends TranslatorAwareType
                 ],
             ])
             ->add('id_country', CountryChoiceType::class, [
+            ->add('id_country', ChoiceType::class, [
+                'label' => $this->trans('Country', 'Admin.Global'),
                 'required' => true,
                 'withDniAttr' => true,
                 'constraints' => [
