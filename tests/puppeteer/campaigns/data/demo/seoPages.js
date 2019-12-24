df8f43d0475cdece8d7292c081a95da6d4dc025f
Boubker BRIBRI <boubker.bribri@prestashop.com>
2019-12-24 11:53:24 +0100

    Add demo seo pages

---
+++ tests/puppeteer/campaigns/data/demo/seoPages.js
@@ -0,0 +1,20 @@
+module.exports = {
+  pageNotFound: {
+    id: 1,
+    page: 'pagenotfound',
+    title: '404 error',
+    friendlyUrl: 'page-not-found',
+  },
+  best_sales: {
+    id: 2,
+    page: 'best-sales',
+    title: 'Best sales',
+    friendlyUrl: 'best-sales',
+  },
+  contact: {
+    id: 3,
+    page: 'contact',
+    title: 'Contact us',
+    friendlyUrl: 'contact-us',
+  },
+};
