89dfede7f48dc106be3c465503e5d373ac08faf1
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2019-12-25 15:44:19 +0100

    Add some fixes

--- tests/puppeteer/campaigns/data/faker/contact.js
+++ tests/puppeteer/campaigns/data/faker/contact.js
@@ -4,9 +4,9 @@ module.exports = class Contact {
   constructor(contactToCreate = {}) {
     this.firstName = contactToCreate.firstName || faker.name.firstName();
     this.lastName = contactToCreate.lastName || faker.name.lastName();
-    this.title = contactToCreate.lastName || `${this.firstName} ${this.lastName}`;
+    this.title = contactToCreate.title || `${this.firstName} ${this.lastName}`;
     this.email = contactToCreate.email || faker.internet.email(this.firstName, this.lastName, 'prestashop.com');
     this.saveMessage = contactToCreate.saveMessage === undefined ? true : contactToCreate.saveMessage;
-    this.description = faker.lorem.sentence();
+    this.description = contactToCreate.description || faker.lorem.sentence();
   }
 };
