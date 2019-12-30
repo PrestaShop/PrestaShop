8b24c326e0208f609b16536cf59cafc3bfac0617
Tadas Davidsonas <tadas.davidsonas@invertus.eu>
2019-12-30 15:05:41 +0200

    default value for PS_ECOTAX_TAX_RULES_GROUP_ID and upgrade

--- install-dev/upgrade/sql/1.7.7.0.sql
+++ install-dev/upgrade/sql/1.7.7.0.sql
@@ -541,6 +541,10 @@ INSERT IGNORE INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `po
 
 INSERT INTO `PREFIX_hook_alias` (`name`, `alias`) VALUES ('displayAdminOrderTop', 'displayInvoice');
 
+insert into prestashop.ps_configuration (`id_configuration`, `id_shop_group`, `id_shop`, `name`, `value`, `date_add`, `date_upd`)
+select NULL, NULL, NULL,'PS_ECOTAX_TAX_RULES_GROUP_ID', '0', NOW(), NOW() FROM prestashop.ps_configuration WHERE NOT EXISTS
+    (SELECT * FROM prestashop.ps_configuration WHERE `name`='PS_ECOTAX_TAX_RULES_GROUP_ID') limit 1;
+
 /* Add refund amount on order detail, and fill new columns via data in order_slip_detail table */
 ALTER TABLE `PREFIX_order_detail` ADD `total_refunded_tax_excl` DECIMAL(20, 6) NOT NULL AFTER `original_wholesale_price` DEFAULT '0.000000';
 ALTER TABLE `PREFIX_order_detail` ADD `total_refunded_tax_incl` DECIMAL(20, 6) NOT NULL AFTER `total_refunded_tax_excl` DEFAULT '0.000000';
