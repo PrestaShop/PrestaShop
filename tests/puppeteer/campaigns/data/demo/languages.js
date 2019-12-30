bf3cc0008701feca2204ee2f2db7c1c99b31ac38
Boubker BRIBRI <boubker.bribri@prestashop.com>
2019-12-30 17:02:44 +0100

    Update language demo file and its usage in employee faker

--- tests/puppeteer/campaigns/data/demo/languages.js
+++ tests/puppeteer/campaigns/data/demo/languages.js
@@ -1,3 +1,24 @@
 module.exports = {
-  Languages: ['English (English)', 'Français (French)'],
+  Languages: {
+    english: {
+      id: 1,
+      name: 'English (English)',
+      isoCode: 'en',
+      languageCode: 'en-us',
+      dateFormat: 'm/d/Y',
+      fullDateFormat: 'm/d/Y H:i:s',
+      isRtlLanguage: false,
+      enabled: true,
+    },
+    french: {
+      id: 2,
+      name: 'Français (French)',
+      isoCode: 'fr',
+      languageCode: 'fr',
+      dateFormat: 'd/m/Y',
+      fullDateFormat: 'd/m/Y H:i:s',
+      isRtlLanguage: false,
+      enabled: true,
+    },
+  },
 };
