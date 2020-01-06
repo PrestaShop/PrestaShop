fffefd7a8d357ec16b656ada6b1e3156450b3dc4
TechhDan <tech.dan2689@gmail.com>
2020-01-05 20:56:46 -0500

    Check create privilege with getBestEngine

--- src/PrestaShopBundle/Install/Database.php
+++ src/PrestaShopBundle/Install/Database.php
@@ -81,8 +81,8 @@ class Database extends AbstractInstall
                     if (!Db::checkAutoIncrement($server, $login, $password)) {
                         $errors[] = $this->translator->trans('The values of auto_increment increment and offset must be set to 1', [], 'Install');
                     }
-                    if (($create_error = Db::checkCreatePrivilege($server, $login, $password, $database, $prefix)) !== true) {
-                        $errors[] = $this->translator->trans('Your database login does not have the privileges to create table on the database "%s". Ask your hosting provider:', ['%database%' => $database], 'Install');
+                    if (($create_error = Db::checkCreatePrivilege($server, $login, $password, $database, $prefix, $this->getBestEngine($server, $database, $login, $password))) !== true) {
+                        $errors[] = $this->translator->trans('Your database login does not have the privileges to create table on the database "%s". Ask your hosting provider:', array('%database%' => $database), 'Install');
                         if ($create_error != false) {
                             $errors[] = $create_error;
                         }
