02ae5407c28637f47dd290ea597e09f410c847f8
Tadas Davidsonas <tadas.davidsonas@invertus.eu>
2020-01-06 14:25:02 +0200

    description made not required because of js errors

--- src/PrestaShopBundle/Form/Admin/Sell/Supplier/SupplierType.php
+++ src/PrestaShopBundle/Form/Admin/Sell/Supplier/SupplierType.php
@@ -166,7 +166,6 @@ class SupplierType extends TranslatorAwareType
             ])
             ->add('description', TranslateType::class, [
                 'label' => $this->trans('Description', 'Admin.Global'),
-                'required' => false,
                 'help' => sprintf(
                     '%s %s',
                     $this->trans('Will appear in the list of suppliers.', 'Admin.Catalog.Help'),
