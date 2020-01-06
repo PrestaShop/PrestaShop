37a6e4c76212d4767f0c48b133d7d8eb49d256c3
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2020-01-06 13:54:35 +0100

    Enable/Disable shop

--- tests/puppeteer/pages/FO/FObasePage.js
+++ tests/puppeteer/pages/FO/FObasePage.js
@@ -6,6 +6,7 @@ module.exports = class Home extends CommonPage {
     super(page);
 
     // Selectors for home page
+    this.content = '#content';
     this.desktopLogo = '#_desktop_logo';
     this.cartProductsCount = '#_desktop_cart span.cart-products-count';
     this.userInfoLink = '#_desktop_user_info';
@@ -132,5 +133,13 @@ module.exports = class Home extends CommonPage {
    */
   async getFooterLinksBlockTitle(position) {
     return this.getTextContent(this.wrapperTitle.replace('%POSITION', position));
+  }
+
+  /**
+   * Get Page content
+   * @return {Promise<textContent>}
+   */
+  getPageContent() {
+    return this.getTextContent(this.content);
   }
 };
