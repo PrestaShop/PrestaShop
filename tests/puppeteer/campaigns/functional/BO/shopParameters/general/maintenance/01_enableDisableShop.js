de3bd985a40c0850e4d0c68d80fe2e08a6d6f3a7
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2020-01-06 13:54:35 +0100

    Enable/Disable shop

---
+++ tests/puppeteer/campaigns/functional/BO/shopParameters/general/maintenance/01_enableDisableShop.js
@@ -0,0 +1,130 @@
+require('module-alias/register');
+// Using chai
+const {expect} = require('chai');
+const helper = require('@utils/helpers');
+const loginCommon = require('@commonTests/loginBO');
+// Importing pages
+const BOBasePage = require('@pages/BO/BObasePage');
+const LoginPage = require('@pages/BO/login');
+const DashboardPage = require('@pages/BO/dashboard');
+const GeneralPage = require('@pages/BO/shopParameters/general');
+const MaintenancePage = require('@pages/BO/shopParameters/general/maintenance');
+const HomePage = require('@pages/FO/home');
+
+let browser;
+let page;
+const maintenanceText = 'We are currently updating our shop and will be back really soon. Thanks for your patience.';
+const newMaintenanceText = 'Maintenance';
+
+// Init objects needed
+const init = async function () {
+  return {
+    boBasePage: new BOBasePage(page),
+    loginPage: new LoginPage(page),
+    dashboardPage: new DashboardPage(page),
+    generalPage: new GeneralPage(page),
+    maintenancePage: new MaintenancePage(page),
+    homePage: new HomePage(page),
+  };
+};
+
+/*
+Enable/Disable shop
+ */
+describe('Enable/Disable shop', async () => {
+  // before and after functions
+  before(async function () {
+    browser = await helper.createBrowser();
+    page = await helper.newTab(browser);
+    this.pageObjects = await init();
+  });
+  after(async () => {
+    await helper.closeBrowser(browser);
+  });
+
+  // Login into BO and go to maintenance page
+  loginCommon.loginBO();
+
+  it('should go to \'Shop parameters > General\' page', async function () {
+    await this.pageObjects.boBasePage.goToSubMenu(
+      this.pageObjects.boBasePage.shopParametersParentLink,
+      this.pageObjects.boBasePage.shopParametersGeneralLink,
+    );
+    await this.pageObjects.boBasePage.closeSfToolBar();
+    const pageTitle = await this.pageObjects.generalPage.getPageTitle();
+    await expect(pageTitle).to.contains(this.pageObjects.generalPage.pageTitle);
+  });
+
+  it('should go to \'Maintenance\' tab', async function () {
+    await this.pageObjects.generalPage.goToSubTabMaintenance();
+    const pageTitle = await this.pageObjects.maintenancePage.getPageTitle();
+    await expect(pageTitle).to.contains(this.pageObjects.maintenancePage.pageTitle);
+  });
+
+  it('should disable the shop', async function () {
+    const result = await this.pageObjects.maintenancePage.changeShopStatus(false);
+    await expect(result).to.contains(this.pageObjects.maintenancePage.successfulUpdateMessage);
+  });
+
+  it('should verify the existence of the maintenance text', async function () {
+    page = await this.pageObjects.boBasePage.viewMyShop();
+    this.pageObjects = await init();
+    const pageContent = await this.pageObjects.homePage.getPageContent();
+    await expect(pageContent).to.equal(maintenanceText);
+    page = await this.pageObjects.homePage.closePage(browser, 1);
+    this.pageObjects = await init();
+  });
+
+  it('should update the maintenance text', async function () {
+    const result = await this.pageObjects.maintenancePage.changeMaintenanceTextShopStatus(newMaintenanceText);
+    await expect(result).to.contains(this.pageObjects.maintenancePage.successfulUpdateMessage);
+  });
+
+  it('should verify the existence of the new maintenance text', async function () {
+    page = await this.pageObjects.boBasePage.viewMyShop();
+    this.pageObjects = await init();
+    const pageContent = await this.pageObjects.homePage.getPageContent();
+    await expect(pageContent).to.equal(newMaintenanceText);
+    page = await this.pageObjects.homePage.closePage(browser, 1);
+    this.pageObjects = await init();
+  });
+
+  it('should back to the default maintenance text', async function () {
+    const result = await this.pageObjects.maintenancePage.changeMaintenanceTextShopStatus(maintenanceText);
+    await expect(result).to.contains(this.pageObjects.maintenancePage.successfulUpdateMessage);
+  });
+
+  it('should add my IP address in Maintenance ip input', async function () {
+    const result = await this.pageObjects.maintenancePage.addMyIpAddress();
+    await expect(result).to.contains(this.pageObjects.maintenancePage.successfulUpdateMessage);
+  });
+
+  it('should verify that the Home page is displayed successfully', async function () {
+    page = await this.pageObjects.boBasePage.viewMyShop();
+    this.pageObjects = await init();
+    const pageContent = await this.pageObjects.homePage.getPageContent();
+    await expect(pageContent).to.not.equal(maintenanceText);
+    const result = await this.pageObjects.homePage.isHomePage();
+    await expect(result).to.be.true;
+    page = await this.pageObjects.homePage.closePage(browser, 1);
+    this.pageObjects = await init();
+  });
+
+  it('should delete the maintenance ip address and enable the shop', async function () {
+    let result = await this.pageObjects.maintenancePage.addMaintenanceIPAddress(' ');
+    await expect(result).to.contains(this.pageObjects.maintenancePage.successfulUpdateMessage);
+    result = await this.pageObjects.maintenancePage.changeShopStatus();
+    await expect(result).to.contains(this.pageObjects.maintenancePage.successfulUpdateMessage);
+  });
+
+  it('should verify that the shop is enabled', async function () {
+    page = await this.pageObjects.boBasePage.viewMyShop();
+    this.pageObjects = await init();
+    const pageContent = await this.pageObjects.homePage.getPageContent();
+    await expect(pageContent).to.not.equal(maintenanceText);
+    const result = await this.pageObjects.homePage.isHomePage();
+    await expect(result).to.be.true;
+    page = await this.pageObjects.homePage.closePage(browser, 1);
+    this.pageObjects = await init();
+  });
+});
--- tests/puppeteer/pages/BO/shopParameters/general/maintenance/index.js
+++ tests/puppeteer/pages/BO/shopParameters/general/maintenance/index.js
@@ -11,6 +11,9 @@ module.exports = class shopParamsMaintenance extends BOBasePage {
     this.generalNavItemLink = '#subtab-AdminPreferences';
     this.disableShopLabel = 'label[for=\'form_general_enable_shop_0\']';
     this.enableShopLabel = 'label[for=\'form_general_enable_shop_1\']';
+    this.maintenanceTextInputEN = '#form_general_maintenance_text_1_ifr';
+    this.addMyIPAddressButton = 'form .add_ip_button';
+    this.maintenanceIpInput = '#form_general_maintenance_ip';
     this.saveFormButton = 'form .card-footer button';
   }
 
@@ -29,12 +32,44 @@ module.exports = class shopParamsMaintenance extends BOBasePage {
   /**
    * Enable / disable shop
    * @param toEnable, true to enable and false to disable
-   * @return {Promise<void>}
+   * @return {Promise<textContent>}
    */
   async changeShopStatus(toEnable = true) {
     if (toEnable) await this.page.click(this.enableShopLabel);
     else await this.page.click(this.disableShopLabel);
     await this.page.click(this.saveFormButton);
-    await this.page.waitForSelector(this.alertSuccessBloc);
+    return this.getTextContent(this.alertSuccessBloc);
+  }
+
+  /**
+   * Update Maintenance text
+   * @param text
+   * @return {Promise<textContent>}
+   */
+  async changeMaintenanceTextShopStatus(text) {
+    await this.setValueOnTinymceInput(this.maintenanceTextInputEN, text);
+    await this.page.click(this.saveFormButton);
+    return this.getTextContent(this.alertSuccessBloc);
+  }
+
+  /**
+   * Add my IP address in maintenance IP input
+   * @return {Promise<textContent>}
+   */
+  async addMyIpAddress() {
+    await this.page.click(this.addMyIPAddressButton);
+    await this.page.click(this.saveFormButton);
+    return this.getTextContent(this.alertSuccessBloc);
+  }
+
+  /**
+   * Add maintenance IP address input
+   * @ipAddress
+   * @return {Promise<textContent>}
+   */
+  async addMaintenanceIPAddress(ipAddress) {
+    await this.setValue(this.maintenanceIpInput, ipAddress);
+    await this.page.click(this.saveFormButton);
+    return this.getTextContent(this.alertSuccessBloc);
   }
 };
