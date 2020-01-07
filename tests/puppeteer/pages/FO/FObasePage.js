dfba6377ca8a525c6e14b271256f3b3fcd1a99e7
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-01-07 18:37:04 +0100

    Delete duplicated selectors

--- tests/puppeteer/pages/FO/FObasePage.js
+++ tests/puppeteer/pages/FO/FObasePage.js
@@ -20,9 +20,6 @@ module.exports = class Home extends CommonPage {
 
     // footer
     this.siteMapLink = '#link-static-page-sitemap-2';
-    this.languageSelectorDiv = '#_desktop_language_selector';
-    this.languageSelectorExpandIcon = `${this.languageSelectorDiv} i.expand-more`;
-    this.languageSelectorMenuItemLink = `${this.languageSelectorDiv} ul li a[data-iso-code='%LANG']`;
     // footer links
     this.footerLinksDiv = '#footer div.links';
     this.wrapperDiv = `${this.footerLinksDiv}:nth-child(1) > div > div.wrapper:nth-child(%POSITION)`;
