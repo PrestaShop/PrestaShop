980426e00c1585dabb8c5d83c38809ea46fa022e
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2020-01-09 15:46:35 +0100

    Fix error

--- tests/puppeteer/campaigns/functional/BO/orders/invoices/03_invoiceOptions/01_enableDisableInvoices.js
+++ tests/puppeteer/campaigns/functional/BO/orders/invoices/03_invoiceOptions/01_enableDisableInvoices.js
@@ -11,6 +11,15 @@ const DashboardPage = require('@pages/BO/dashboard');
 const InvoicesPage = require('@pages/BO/orders/invoices/index');
 const OrdersPage = require('@pages/BO/orders/index');
 const ViewOrderPage = require('@pages/BO/orders/view');
+const FOBasePage = require('@pages/FO/FObasePage');
+const HomePage = require('@pages/FO/home');
+const ProductPage = require('@pages/FO/product');
+const CartPage = require('@pages/FO/cart');
+const CheckoutPage = require('@pages/FO/checkout');
+const OrderConfirmationPage = require('@pages/FO/orderConfirmation');
+// Importing data
+const {PaymentMethods} = require('@data/demo/orders');
+const {DefaultAccount} = require('@data/demo/customer');
 
 let browser;
 let page;
@@ -24,6 +33,12 @@ const init = async function () {
     invoicesPage: new InvoicesPage(page),
     ordersPage: new OrdersPage(page),
     viewOrderPage: new ViewOrderPage(page),
+    foBasePage: new FOBasePage(page),
+    homePage: new HomePage(page),
+    productPage: new ProductPage(page),
+    cartPage: new CartPage(page),
+    checkoutPage: new CheckoutPage(page),
+    orderConfirmationPage: new OrderConfirmationPage(page),
   };
 };
 
@@ -44,6 +59,39 @@ describe('Test disable/enable invoices', async () => {
   loginCommon.loginBO();
 
   describe('Disable invoices then check that there is no invoice created', async () => {
+    it('should go to FO and create an order', async function () {
+      // Click on view my shop
+      page = await this.pageObjects.boBasePage.viewMyShop();
+      this.pageObjects = await init();
+      await this.pageObjects.foBasePage.changeLanguage('en');
+      // Go to the first product page
+      await this.pageObjects.homePage.goToProductPage(1);
+      // Add the created product to the cart
+      await this.pageObjects.productPage.addProductToTheCart();
+      // Proceed to checkout the shopping cart
+      await this.pageObjects.cartPage.clickOnProceedToCheckout();
+      // Checkout the order
+      // Personal information step - Login
+      await this.pageObjects.checkoutPage.clickOnSignIn();
+      await this.pageObjects.checkoutPage.customerLogin(DefaultAccount);
+      // Address step - Go to delivery step
+      const isStepAddressComplete = await this.pageObjects.checkoutPage.goToDeliveryStep();
+      await expect(isStepAddressComplete, 'Step Address is not complete').to.be.true;
+      // Delivery step - Go to payment step
+      const isStepDeliveryComplete = await this.pageObjects.checkoutPage.goToPaymentStep();
+      await expect(isStepDeliveryComplete, 'Step Address is not complete').to.be.true;
+      // Payment step - Choose payment step
+      await this.pageObjects.checkoutPage.choosePaymentAndOrder(PaymentMethods.wirePayment.moduleName);
+      const cardTitle = await this.pageObjects.orderConfirmationPage
+        .getTextContent(this.pageObjects.orderConfirmationPage.orderConfirmationCardTitleH3);
+      // Check the confirmation message
+      await expect(cardTitle).to.contains(this.pageObjects.orderConfirmationPage.orderConfirmationCardTitle);
+      // Logout from FO
+      await this.pageObjects.foBasePage.logout();
+      page = await this.pageObjects.orderConfirmationPage.closePage(browser, 1);
+      this.pageObjects = await init();
+    });
+
     it('should go to invoices page', async function () {
       await this.pageObjects.boBasePage.goToSubMenu(
         this.pageObjects.boBasePage.ordersParentLink,
@@ -69,8 +117,8 @@ describe('Test disable/enable invoices', async () => {
       await expect(pageTitle).to.contains(this.pageObjects.ordersPage.pageTitle);
     });
 
-    it('should go to the fourth order page', async function () {
-      await this.pageObjects.ordersPage.goToOrder(4);
+    it('should go to the first order page', async function () {
+      await this.pageObjects.ordersPage.goToOrder(1);
       const pageTitle = await this.pageObjects.viewOrderPage.getPageTitle();
       await expect(pageTitle).to.contains(this.pageObjects.viewOrderPage.pageTitle);
     });
