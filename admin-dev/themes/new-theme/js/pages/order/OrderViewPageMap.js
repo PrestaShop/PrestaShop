ffa3c858c203b36e55932f71535e7a0805e1b88b
Progi1984 <franck.lefevre@prestashop.com>
2020-01-13 13:58:38 +0100

    Orders > Product : Display "Stock Location" column only if it is not empty

--- admin-dev/themes/new-theme/js/pages/order/OrderViewPageMap.js
+++ admin-dev/themes/new-theme/js/pages/order/OrderViewPageMap.js
@@ -71,6 +71,7 @@ export default {
   productsTablePaginationTemplate: '#orderProductsTablePagination .page-item.d-none',
   productsTableRow: productId => `#orderProduct_${productId}`,
   productsTableRowEdited: productId => `#editOrderProduct_${productId}`,
+  productsCellLocation: '.cell-product-location',
   productEditBtn: '.js-order-product-edit-btn',
   productAddBtn: '#addProductBtn',
   productActionBtn: '.js-product-action-btn',
