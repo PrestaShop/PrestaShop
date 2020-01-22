69469796674218841eff03ea3f56a7c8bfc597b5
idnovate.com <info@idnovate.com>
2020-01-22 11:43:36 +0100

    Fix https://github.com/PrestaShop/PrestaShop/issues/17290

--- controllers/front/CartController.php
+++ controllers/front/CartController.php
@@ -177,6 +177,7 @@ class CartControllerCore extends FrontController
             'cart_detailed' => $this->render('checkout/_partials/cart-detailed'),
             'cart_detailed_totals' => $this->render('checkout/_partials/cart-detailed-totals'),
             'cart_summary_items_subtotal' => $this->render('checkout/_partials/cart-summary-items-subtotal'),
+            'cart_summary_products' => $this->render('checkout/_partials/cart-summary-products'),
             'cart_summary_subtotals_container' => $this->render('checkout/_partials/cart-summary-subtotals'),
             'cart_summary_totals' => $this->render('checkout/_partials/cart-summary-totals'),
             'cart_detailed_actions' => $this->render('checkout/_partials/cart-detailed-actions'),
