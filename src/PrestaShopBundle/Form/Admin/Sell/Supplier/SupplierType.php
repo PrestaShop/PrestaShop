3e03a5daa20a00b26acf6fdef843acb48f08e289
Julius Zukauskas <zuk3975@gmail.com>
2020-01-23 10:50:54 +0200

    revert deleting DNI field

--- src/PrestaShopBundle/Form/Admin/Sell/Supplier/SupplierType.php
+++ src/PrestaShopBundle/Form/Admin/Sell/Supplier/SupplierType.php
@@ -26,6 +26,7 @@
 
 namespace PrestaShopBundle\Form\Admin\Sell\Supplier;
 
+use PrestaShop\PrestaShop\Core\ConstraintValidator\Constraints\AddressDniRequired;
 use PrestaShop\PrestaShop\Core\ConstraintValidator\Constraints\AddressStateRequired;
 use PrestaShop\PrestaShop\Core\ConstraintValidator\Constraints\CleanHtml;
 use PrestaShop\PrestaShop\Core\ConstraintValidator\Constraints\TypedRegex;
@@ -219,6 +220,27 @@ class SupplierType extends TranslatorAwareType
                 'constraints' => [
                     new AddressStateRequired([
                         'id_country' => $countryId,
+                    ]),
+                ],
+            ])
+            ->add('dni', TextType::class, [
+                'required' => false,
+                'empty_data' => '',
+                'constraints' => [
+                    new AddressDniRequired([
+                        'required' => false,
+                        'id_country' => $countryId,
+                    ]),
+                    new TypedRegex([
+                        'type' => 'dni_lite',
+                    ]),
+                    new Length([
+                        'max' => 16,
+                        'maxMessage' => $this->trans(
+                            'This field cannot be longer than %limit% characters',
+                            'Admin.Notifications.Error',
+                            ['%limit%' => 16]
+                        ),
                     ]),
                 ],
             ])
