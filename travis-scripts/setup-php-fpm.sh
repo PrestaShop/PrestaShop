15b4d089e29b07b3ec32b2901cca8e601e716b00
Thomas Baccelli <thomas.baccelli@prestashop.com>
2020-01-24 11:26:06 +0100

    Really clear cache for test environment and disable opcache for cli

--- travis-scripts/setup-php-fpm.sh
+++ travis-scripts/setup-php-fpm.sh
@@ -19,6 +19,8 @@ echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/con
 echo "always_populate_raw_post_data = -1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
 echo "error_log = /var/log/php-fpm.log" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
 echo "memory_limit = 2G" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
+# Disable opcache to prevent zend_mm_heap corrupted when running phpunit tests
+echo "opcache.enable_cli = 0" >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
 
 # Starting PHP FPM
 ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
