0649102bedd4870111d21a365fba5a8dcedd6315
PK André <pululuandre@hotmail.com>
2020-01-29 02:28:10 +0100

    [FIX] Can't explore a category when all subcats are disabled
    See https://github.com/PrestaShop/PrestaShop/blob/develop/classes/Category.php#L1163

--- src/Adapter/Grid/Action/Row/AccessibilityChecker/CategoryForViewAccessibilityChecker.php
+++ src/Adapter/Grid/Action/Row/AccessibilityChecker/CategoryForViewAccessibilityChecker.php
@@ -54,8 +54,8 @@ final class CategoryForViewAccessibilityChecker implements AccessibilityCheckerI
      */
     public function isGranted(array $category)
     {
-        $categoryChildren = Category::getChildren($category['id_category'], $this->contextLangId);
+        $CategoryHasChildren = Category::hasChildren($category['id_category'], $this->contextLangId, true);
 
-        return !empty($categoryChildren);
+        return $CategoryHasChildren;
     }
 }
