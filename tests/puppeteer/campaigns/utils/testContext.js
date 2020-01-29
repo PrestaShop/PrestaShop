48b904b6fdf0c43f76f65a0ea800b76051fd11a6
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-01-29 09:31:32 +0100

    Add context to common tests

---
+++ tests/puppeteer/campaigns/utils/testContext.js
@@ -0,0 +1,14 @@
+const addContext = require('mochawesome/addContext');
+
+module.exports = {
+  /**
+   *
+   * @param testObj, mocha step object
+   * @param title, context title
+   * @param value, value to add
+   * @return {Promise<void>}
+   */
+  async addContextItem(testObj, title, value) {
+    addContext(testObj, title);
+  },
+};
