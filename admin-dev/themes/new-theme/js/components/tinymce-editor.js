0fc05ec215bbdd384e7e90a449c733842d788239
Valentin Szczupak <valentin.szczupak@prestashop.com>
2020-01-30 16:53:35 +0100

    Change tinymce lang when changeLanguage event is fired
    Adding in the over side
    
    Assets
    
    Update to js string
    
    Co-Authored-By: Progi1984 <progi1984@gmail.com>
    
    Update to jstring
    
    Co-Authored-By: Progi1984 <progi1984@gmail.com>

--- admin-dev/themes/new-theme/js/components/tinymce-editor.js
+++ admin-dev/themes/new-theme/js/components/tinymce-editor.js
@@ -22,8 +22,9 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
+import {EventEmitter} from './event-emitter';
 
-const $ = window.$;
+const {$} = window;
 
 /**
  * This class init TinyMCE instances in the back-office. It is wildly inspired by
@@ -35,12 +36,12 @@ class TinyMCEEditor {
   constructor(options) {
     options = options || {};
     this.tinyMCELoaded = false;
-    if (typeof options.baseAdminUrl == 'undefined') {
-      if (typeof window.baseAdminDir != 'undefined') {
+    if (typeof options.baseAdminUrl === 'undefined') {
+      if (typeof window.baseAdminDir !== 'undefined') {
         options.baseAdminUrl = window.baseAdminDir;
       } else {
         const pathParts = window.location.pathname.split('/');
-        pathParts.every(function(pathPart) {
+        pathParts.every(pathPart => {
           if (pathPart !== '') {
             options.baseAdminUrl = `/${pathPart}/`;
 
@@ -51,8 +52,8 @@ class TinyMCEEditor {
         });
       }
     }
-    if (typeof options.langIsRtl == 'undefined') {
-      options.langIsRtl = typeof window.lang_is_rtl != 'undefined' ? window.lang_is_rtl === '1' : false;
+    if (typeof options.langIsRtl === 'undefined') {
+      options.langIsRtl = typeof window.lang_is_rtl !== 'undefined' ? window.lang_is_rtl === '1' : false;
     }
     this.setupTinyMCE(options);
   }
@@ -76,42 +77,49 @@ class TinyMCEEditor {
    * @param config
    */
   initTinyMCE(config) {
-    config = Object.assign({
-      selector: '.rte',
-      plugins: 'align colorpicker link image filemanager table media placeholder advlist code table autoresize',
-      browser_spellcheck: true,
-      toolbar1: 'code,colorpicker,bold,italic,underline,strikethrough,blockquote,link,align,bullist,numlist,table,image,media,formatselect',
-      toolbar2: '',
-      external_filemanager_path: config.baseAdminUrl + 'filemanager/',
-      filemanager_title: 'File manager',
-      external_plugins: {
-        'filemanager': config.baseAdminUrl + 'filemanager/plugin.min.js'
+    config = Object.assign(
+      {
+        selector: '.rte',
+        plugins: 'align colorpicker link image filemanager table media placeholder advlist code table autoresize',
+        browser_spellcheck: true,
+        toolbar1: 'code,colorpicker,bold,italic,underline,strikethrough,blockquote,link,align,bullist,numlist,table,image,media,formatselect',
+        toolbar2: '',
+        external_filemanager_path: `${config.baseAdminUrl}filemanager/`,
+        filemanager_title: 'File manager',
+        external_plugins: {
+          filemanager: `${config.baseAdminUrl}filemanager/plugin.min.js`
+        },
+        language: iso_user,
+        content_style: config.langIsRtl ? 'body {direction:rtl;}' : '',
+        skin: 'prestashop',
+        menubar: false,
+        statusbar: false,
+        relative_urls: false,
+        convert_urls: false,
+        entity_encoding: 'raw',
+        extended_valid_elements: 'em[class|name|id],@[role|data-*|aria-*]',
+        valid_children: '+*[*]',
+        valid_elements: '*[*]',
+        rel_list: [{title: 'nofollow', value: 'nofollow'}],
+        editor_selector: 'autoload_rte',
+        init_instance_callback: () => {
+          this.changeToMaterial();
+        },
+        setup: editor => {
+          this.setupEditor(editor);
+        }
       },
-      language: iso_user,
-      content_style : (config.langIsRtl ? 'body {direction:rtl;}' : ''),
-      skin: 'prestashop',
-      menubar: false,
-      statusbar: false,
-      relative_urls: false,
-      convert_urls: false,
-      entity_encoding: 'raw',
-      extended_valid_elements: 'em[class|name|id],@[role|data-*|aria-*]',
-      valid_children: '+*[*]',
-      valid_elements: '*[*]',
-      rel_list:[
-        { title: 'nofollow', value: 'nofollow' }
-      ],
-      editor_selector :'autoload_rte',
-      init_instance_callback: () => { this.changeToMaterial(); },
-      setup : (editor) => { this.setupEditor(editor); },
-    }, config);
+      config
+    );
 
-    if (typeof config.editor_selector != 'undefined') {
+    if (typeof config.editor_selector !== 'undefined') {
       config.selector = '.' + config.editor_selector;
     }
 
     // Change icons in popups
-    $('body').on('click', '.mce-btn, .mce-open, .mce-menu-item', () => { this.changeToMaterial(); });
+    $('body').on('click', '.mce-btn, .mce-open, .mce-menu-item', () => {
+      this.changeToMaterial();
+    });
 
     tinyMCE.init(config);
     this.watchTabChanges(config);
@@ -123,10 +131,10 @@ class TinyMCEEditor {
    * @param editor
    */
   setupEditor(editor) {
-    editor.on('loadContent', (event) => {
+    editor.on('loadContent', event => {
       this.handleCounterTiny(event.target.id);
     });
-    editor.on('change', (event) => {
+    editor.on('change', event => {
       tinyMCE.triggerSave();
       this.handleCounterTiny(event.target.id);
     });
@@ -149,16 +157,28 @@ class TinyMCEEditor {
 
       if (translatedField.length && tabContainer.length) {
         const textareaLocale = translatedField.data('locale');
-        const textareaLinkSelector = '.nav-item a[data-locale="'+textareaLocale+'"]';
+        const textareaLinkSelector = `.nav-item a[data-locale="${textareaLocale}"]`;
 
         $(textareaLinkSelector, tabContainer).on('shown.bs.tab', () => {
+          const form = $(textarea).closest('form');
           const editor = tinyMCE.get(textarea.id);
           if (editor) {
-            //Reset content to force refresh of editor
+            // Reset content to force refresh of editor
             editor.setContent(editor.getContent());
           }
+
+          EventEmitter.emit('languageSelected', {
+            selectedLocale: textareaLocale,
+            form
+          });
         });
       }
+    });
+
+    EventEmitter.on('languageSelected', data => {
+      const textareaLinkSelector = `.nav-item a[data-locale="${data.selectedLocale}"]`;
+
+      $(textareaLinkSelector).click();
     });
   }
 
@@ -174,19 +194,21 @@ class TinyMCEEditor {
 
     this.tinyMCELoaded = true;
     const pathArray = config.baseAdminUrl.split('/');
-    pathArray.splice((pathArray.length - 2), 2);
+    pathArray.splice(pathArray.length - 2, 2);
     const finalPath = pathArray.join('/');
     window.tinyMCEPreInit = {};
-    window.tinyMCEPreInit.base = finalPath+'/js/tiny_mce';
+    window.tinyMCEPreInit.base = `${finalPath}/js/tiny_mce`;
     window.tinyMCEPreInit.suffix = '.min';
-    $.getScript(`${finalPath}/js/tiny_mce/tinymce.min.js`, () => {this.setupTinyMCE(config)});
+    $.getScript(`${finalPath}/js/tiny_mce/tinymce.min.js`, () => {
+      this.setupTinyMCE(config);
+    });
   }
 
   /**
    * Replace initial TinyMCE icons with material icons
    */
   changeToMaterial() {
-    let materialIconAssoc = {
+    const materialIconAssoc = {
       'mce-i-code': '<i class="material-icons">code</i>',
       'mce-i-none': '<i class="material-icons">format_color_text</i>',
       'mce-i-bold': '<i class="material-icons">format_bold</i>',
@@ -205,10 +227,10 @@ class TinyMCEEditor {
       'mce-i-table': '<i class="material-icons">grid_on</i>',
       'mce-i-media': '<i class="material-icons">video_library</i>',
       'mce-i-browse': '<i class="material-icons">attachment</i>',
-      'mce-i-checkbox': '<i class="mce-ico mce-i-checkbox"></i>',
+      'mce-i-checkbox': '<i class="mce-ico mce-i-checkbox"></i>'
     };
 
-    $.each(materialIconAssoc, function (index, value) {
+    $.each(materialIconAssoc, (index, value) => {
       $(`.${index}`).replaceWith(value);
     });
   }
@@ -224,11 +246,20 @@ class TinyMCEEditor {
     const counterType = textarea.attr('counter_type');
     const max = tinyMCE.activeEditor.getBody().textContent.length;
 
-    textarea.parent().find('span.currentLength').text(max);
+    textarea
+      .parent()
+      .find('span.currentLength')
+      .text(max);
     if ('recommended' !== counterType && max > counter) {
-      textarea.parent().find('span.maxLength').addClass('text-danger');
+      textarea
+        .parent()
+        .find('span.maxLength')
+        .addClass('text-danger');
     } else {
-      textarea.parent().find('span.maxLength').removeClass('text-danger');
+      textarea
+        .parent()
+        .find('span.maxLength')
+        .removeClass('text-danger');
     }
   }
 }
