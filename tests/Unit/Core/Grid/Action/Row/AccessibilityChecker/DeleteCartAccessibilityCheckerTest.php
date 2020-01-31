b2fc931cd363d7cb97c77de987e1ad6b37502554
Tadas Davidsonas <tadas.davidsonas@invertus.eu>
2020-01-31 14:00:19 +0200

    more test cases added

--- tests/Unit/Core/Grid/Action/Row/AccessibilityChecker/DeleteCartAccessibilityCheckerTest.php
+++ tests/Unit/Core/Grid/Action/Row/AccessibilityChecker/DeleteCartAccessibilityCheckerTest.php
@@ -9,33 +9,7 @@ use PrestaShop\PrestaShop\Core\Grid\Action\Row\AccessibilityChecker\DeleteCartAc
 
 class DeleteCartAccessibilityCheckerTest extends TestCase
 {
-    private const CART_RECORD_STATUS_NOT_PLACED = [
-        'id_cart' => '15',
-        'date_add' => '2020-01-31 03:20:54',
-        'status' => 'Not placed',
-        'id_order' => null,
-        'customer_name' => 'J. DOE',
-        'carrier_name' => '--',
-        'id_guest' => '0',
-        'online' => 'No',
-        'cart_total' => '$0.00',
-        'is_order_placed' => false,
-        'shop_name' => 'testadas',
-    ];
-
-    private const CART_RECORD_STATUS_ORDER_ID = [
-        'id_cart' => '11',
-        'date_add' => '2020-01-22 10:21:43',
-        'status' => '8',
-        'id_order' => '8',
-        'customer_name' => 'T. Davidsonas',
-        'carrier_name' => '--',
-        'id_guest' => '0',
-        'online' => 'No',
-        'cart_total' => '$11.90',
-        'is_order_placed' => true,
-        'shop_name' => 'testadas',
-    ];
+    private const STATUS_PROPERTY = 'status';
 
     /** @var DeleteCartAccessibilityChecker */
     private $deleteCartAccessibilityChecker;
@@ -47,11 +21,17 @@ class DeleteCartAccessibilityCheckerTest extends TestCase
 
     public function testIsGrantedWhenRecordStatusIsNotPlaced()
     {
-        $this->assertTrue($this->deleteCartAccessibilityChecker->isGranted(self::CART_RECORD_STATUS_NOT_PLACED));
-    }
-
-    public function testIsGrantedWhenRecordStatusIsOrderId()
-    {
-        $this->assertFalse($this->deleteCartAccessibilityChecker->isGranted(self::CART_RECORD_STATUS_ORDER_ID));
+        $this->assertTrue($this->deleteCartAccessibilityChecker->isGranted([self::STATUS_PROPERTY => 'Not placed']));
+        $this->assertTrue($this->deleteCartAccessibilityChecker->isGranted([self::STATUS_PROPERTY => 'not placed']));
+        $this->assertTrue($this->deleteCartAccessibilityChecker->isGranted([self::STATUS_PROPERTY => 'not']));
+        // 12notready was unexpected to be true; 0 == $record['status'] gave false
+        $this->assertFalse($this->deleteCartAccessibilityChecker->isGranted([self::STATUS_PROPERTY => '12notready']));
+        $this->assertFalse($this->deleteCartAccessibilityChecker->isGranted([self::STATUS_PROPERTY => '8']));
+        $this->assertFalse($this->deleteCartAccessibilityChecker->isGranted([self::STATUS_PROPERTY => -10]));
+        $this->assertFalse($this->deleteCartAccessibilityChecker->isGranted([self::STATUS_PROPERTY => 2.1]));
+        $this->assertFalse($this->deleteCartAccessibilityChecker->isGranted([self::STATUS_PROPERTY => 2]));
+        $this->assertFalse($this->deleteCartAccessibilityChecker->isGranted([self::STATUS_PROPERTY => 6463165813]));
+        $this->assertFalse($this->deleteCartAccessibilityChecker->isGranted([self::STATUS_PROPERTY => '-12zef210']));
+        $this->assertFalse($this->deleteCartAccessibilityChecker->isGranted([self::STATUS_PROPERTY => '-12.02e1f']));
     }
 }
