79efe5c2258650278d02cf70d29d5d5ced144d64
Julius Zukauskas <zuk3975@gmail.com>
2020-02-06 12:06:12 +0200

    remove fields togglers that depends from country

--- admin-dev/themes/new-theme/js/pages/address/form.js
+++ admin-dev/themes/new-theme/js/pages/address/form.js
@@ -25,11 +25,9 @@
 
 import AutocompleteWithEmail from '@components/form/autocomplete-with-email';
 import CountryStateSelectionToggler from '@components/country-state-selection-toggler';
-import CountryDniRequiredToggler from '@components/country-dni-required-toggler';
-import CountryPostcodeRequiredToggler from '@components/country-postcode-required-toggler';
 import addressFormMap from './address-form-map';
 
-const $ = window.$;
+const {$} = window;
 
 $(document).ready(() => {
   new AutocompleteWithEmail(
@@ -38,21 +36,11 @@ $(document).ready(() => {
       firstName: addressFormMap.addressFirstnameInput,
       lastName: addressFormMap.addressLastnameInput,
       company: addressFormMap.addressCompanyInput,
-    }
+    },
   );
   new CountryStateSelectionToggler(
     addressFormMap.addressCountrySelect,
     addressFormMap.addressStateSelect,
-    addressFormMap.addressStateBlock
-  );
-  new CountryDniRequiredToggler(
-    addressFormMap.addressCountrySelect,
-    addressFormMap.addressDniInput,
-    addressFormMap.addressDniInputLabel
-  );
-  new CountryPostcodeRequiredToggler(
-    addressFormMap.addressCountrySelect,
-    addressFormMap.addressPostcodeInput,
-    addressFormMap.addressPostcodeInputLabel
+    addressFormMap.addressStateBlock,
   );
 });
