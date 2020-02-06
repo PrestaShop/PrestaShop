ce5fa236ccd9ea06632932a5d899c20e0ae9e7ac
Julius Zukauskas <zuk3975@gmail.com>
2020-02-06 12:04:31 +0200

    fix states provider to consider Country->contains_state

--- src/PrestaShopBundle/Form/Admin/Sell/Address/CustomerAddressType.php
+++ src/PrestaShopBundle/Form/Admin/Sell/Address/CustomerAddressType.php
@@ -91,6 +91,12 @@ class CustomerAddressType extends AbstractType
         $data = $builder->getData();
         $requiredFields = $data['required_fields'];
         $countryId = 0 !== $data['id_country'] ? $data['id_country'] : $this->contextCountryId;
+        $stateChoices = $this->stateChoiceProvider->getChoices(['id_country' => $countryId]);
+
+        $hideStates = false;
+        if (empty($stateChoices)) {
+            $hideStates = true;
+        }
 
         if (!isset($data['id_customer'])) {
             $builder->add('customer_email', EmailType::class, [
@@ -374,11 +380,14 @@ class CustomerAddressType extends AbstractType
         ])
         ->add('id_state', ChoiceType::class, [
             'required' => true,
-            'choices' => $this->stateChoiceProvider->getChoices(['id_country' => $countryId]),
+            'choices' => $stateChoices,
             'constraints' => [
                 new AddressStateRequired([
                     'id_country' => $countryId,
                 ]),
+            ],
+            'attr' => [
+                'class' => $hideStates ? 'd-none' : '',
             ],
         ])
         ->add('phone', TextType::class, [
