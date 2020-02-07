1760c1f0adaf7db8af4c2254dbf6c39a626a8cd0
Tadas Davidsonas <tadas.davidsonas@invertus.eu>
2020-02-07 12:59:37 +0200

    fixed failing phpunit test

--- tests/Unit/PrestaShopBundle/Routing/Linter/NamingConventionLinterTest.php
+++ tests/Unit/PrestaShopBundle/Routing/Linter/NamingConventionLinterTest.php
@@ -28,7 +28,6 @@ namespace Tests\Unit\PrestaShopBundle\Routing\Linter;
 
 use PHPUnit\Framework\TestCase;
 use PrestaShopBundle\PrestaShopBundle;
-use PrestaShopBundle\Routing\Linter\Exception\NamingConventionException;
 use PrestaShopBundle\Routing\Linter\NamingConventionLinter;
 use Symfony\Bundle\FrameworkBundle\Controller\ControllerNameParser;
 use Symfony\Component\HttpKernel\KernelInterface;
@@ -60,11 +59,12 @@ class NamingConventionLinterTest extends TestCase
     /**
      * @dataProvider getRoutesThatDoNotFollowNamingConventions
      */
-    public function testLinterThrowsExceptionWhenRouteAndControllerDoesNotFollowNamingConventions($routeName, Route $route)
-    {
-        $this->expectException(NamingConventionException::class);
-
-        $this->namingConventionLinter->lint($routeName, $route);
+    public function testLinterReturnsExpectedRouteNameWhenRouteAndControllerDoesNotFollowNamingConventions(
+        $routeName,
+        Route $route
+    ) {
+        $expectedRouteName = $this->namingConventionLinter->lint($routeName, $route);
+        $this->assertIsString($expectedRouteName);
     }
 
     public function getRoutesThatFollowNamingConventions()
