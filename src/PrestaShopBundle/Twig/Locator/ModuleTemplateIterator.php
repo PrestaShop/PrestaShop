5a808d674ee7f03860833b3839ec43a459b77a22
Pierre RAMBAUD <pierre.rambaud@prestashop.com>
2020-02-10 10:17:28 +0100

    Happy new year 2020
    Update license headers

--- src/PrestaShopBundle/Twig/Locator/ModuleTemplateIterator.php
+++ src/PrestaShopBundle/Twig/Locator/ModuleTemplateIterator.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * 2007-2019 PrestaShop SA and Contributors
+ * 2007-2020 PrestaShop SA and Contributors
  *
  * NOTICE OF LICENSE
  *
@@ -20,7 +20,7 @@
  * needs please refer to https://www.prestashop.com for more information.
  *
  * @author    PrestaShop SA <contact@prestashop.com>
- * @copyright 2007-2019 PrestaShop SA and Contributors
+ * @copyright 2007-2020 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
