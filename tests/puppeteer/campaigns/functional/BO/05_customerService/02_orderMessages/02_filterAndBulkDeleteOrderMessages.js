b29ecdf15f1a104661e999afe4ab75aaa5fd0f83
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-02-10 10:48:34 +0100

    Add context to order message tests

--- tests/puppeteer/campaigns/functional/BO/05_customerService/02_orderMessages/02_filterAndBulkDeleteOrderMessages.js
+++ tests/puppeteer/campaigns/functional/BO/05_customerService/02_orderMessages/02_filterAndBulkDeleteOrderMessages.js
@@ -9,6 +9,10 @@ const LoginPage = require('@pages/BO/login');
 const DashboardPage = require('@pages/BO/dashboard');
 const OrderMessagesPage = require('@pages/BO/customerService/orderMessages');
 const AddOrderMessagePage = require('@pages/BO/customerService/orderMessages/add');
+// Test context imports
+const testContext = require('@utils/testContext');
+
+const baseContext = 'functional_BO_customerService_orderMessages_filterAndBulkDeleteOrderMessages';
 
 let browser;
 let page;
@@ -47,6 +51,7 @@ describe('Create order messages, check filter results and delete them with bulk 
   loginCommon.loginBO();
 
   it('should go to order messages page', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'goToOrderMessagesPage', baseContext);
     await this.pageObjects.boBasePage.goToSubMenu(
       this.pageObjects.boBasePage.customerServiceParentLink,
       this.pageObjects.boBasePage.orderMessagesLink,
@@ -57,6 +62,7 @@ describe('Create order messages, check filter results and delete them with bulk 
   });
 
   it('should reset all filters', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'resetFirst', baseContext);
     numberOfOrderMessages = await this.pageObjects.orderMessagesPage.resetAndGetNumberOfLines();
     await expect(numberOfOrderMessages).to.be.above(0);
   });
@@ -69,12 +75,14 @@ describe('Create order messages, check filter results and delete them with bulk 
     ];
     tests.forEach((test, index) => {
       it('should go to new order message page', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', `goToNewOrderMessagePage${index + 1}`, baseContext);
         await this.pageObjects.orderMessagesPage.goToAddNewOrderMessagePage();
         const pageTitle = await this.pageObjects.addOrderMessagePage.getPageTitle();
         await expect(pageTitle).to.contains(this.pageObjects.addOrderMessagePage.pageTitle);
       });
 
       it('should create order message', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', `createOrderMessage${index + 1}`, baseContext);
         const result = await this.pageObjects.addOrderMessagePage.AddEditOrderMessage(test.args.orderMessageToCreate);
         await expect(result).to.equal(this.pageObjects.orderMessagesPage.successfulCreationMessage);
         const numberOfOrderMessagesAfterCreation = await this.pageObjects.orderMessagesPage.getNumberOfElementInGrid();
@@ -85,12 +93,13 @@ describe('Create order messages, check filter results and delete them with bulk 
   // 2: filter order Messages
   describe('Filter order Messages', async () => {
     const tests = [
-      {args: {filterBy: 'name', filterValue: secondOrderMessageData.name}},
-      {args: {filterBy: 'message', filterValue: secondOrderMessageData.message}},
+      {args: {testIdentifier: 'filterName', filterBy: 'name', filterValue: secondOrderMessageData.name}},
+      {args: {testIdentifier: 'filterMessage', filterBy: 'message', filterValue: secondOrderMessageData.message}},
     ];
 
     tests.forEach((test) => {
       it(`should filter by ${test.args.filterBy} '${test.args.filterValue}'`, async function () {
+        await testContext.addContextItem(this, 'testIdentifier', `${test.args.testIdentifier}`, baseContext);
         await this.pageObjects.orderMessagesPage.filterTable('name', test.args.filterValue);
         const numberOfOrderMessagesAfterFilter = await this.pageObjects.orderMessagesPage.getNumberOfElementInGrid();
         await expect(numberOfOrderMessagesAfterFilter).to.be.at.most(numberOfOrderMessages + 1);
@@ -99,6 +108,7 @@ describe('Create order messages, check filter results and delete them with bulk 
       });
 
       it('should reset filters and check number of order messages', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', `${test.args.testIdentifier}Reset`, baseContext);
         const numberOfOrderMessagesAfterReset = await this.pageObjects.orderMessagesPage.resetAndGetNumberOfLines();
         await expect(numberOfOrderMessagesAfterReset).to.be.equal(numberOfOrderMessages + 2);
       });
@@ -108,17 +118,20 @@ describe('Create order messages, check filter results and delete them with bulk 
   // 3: Delete order messages with bulk actions
   describe('Delete order messages with bulk actions', async () => {
     it('should filter by name of order message', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'filterToBulkDelete', baseContext);
       await this.pageObjects.orderMessagesPage.filterTable('name', firstOrderMessageData.name);
       const textColumn = await this.pageObjects.orderMessagesPage.getTextColumnFromTable(1, 'name');
       await expect(textColumn).to.contains(firstOrderMessageData.name);
     });
 
     it('should delete order messages', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'bulkDelete', baseContext);
       const result = await this.pageObjects.orderMessagesPage.deleteWithBulkActions();
       await expect(result).to.be.equal(this.pageObjects.orderMessagesPage.successfulMultiDeleteMessage);
     });
 
     it('should reset filters and check number of order messages', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'resetAfterBulkDelete', baseContext);
       const numberOfOrderMessagesAfterReset = await this.pageObjects.orderMessagesPage.resetAndGetNumberOfLines();
       await expect(numberOfOrderMessagesAfterReset).to.be.equal(numberOfOrderMessages);
     });
