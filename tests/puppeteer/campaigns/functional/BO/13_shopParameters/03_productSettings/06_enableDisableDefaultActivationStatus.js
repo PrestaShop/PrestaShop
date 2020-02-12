463cae0d50776ba3c60fc4f45a85014103dfd47a
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2020-02-12 09:27:16 +0100

    Delete closeSfToolBar()

--- tests/puppeteer/campaigns/functional/BO/13_shopParameters/03_productSettings/06_enableDisableDefaultActivationStatus.js
+++ tests/puppeteer/campaigns/functional/BO/13_shopParameters/03_productSettings/06_enableDisableDefaultActivationStatus.js
@@ -89,7 +89,6 @@ describe('Enable/Disable default activation status', async () => {
       this.pageObjects.boBasePage.shopParametersParentLink,
       this.pageObjects.boBasePage.productSettingsLink,
     );
-    await this.pageObjects.boBasePage.closeSfToolBar();
     const pageTitle = await this.pageObjects.productSettingsPage.getPageTitle();
     await expect(pageTitle).to.contains(this.pageObjects.productSettingsPage.pageTitle);
   });
