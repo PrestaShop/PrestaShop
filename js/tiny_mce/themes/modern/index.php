896caa67618a60876be946faae89b044fca746a7
Valentin Szczupak <valentin.szczupak@prestashop.com>
2020-02-13 14:54:11 +0100

    Restore index.php of modern theme

--- js/tiny_mce/themes/modern/index.php
+++ js/tiny_mce/themes/modern/index.php
@@ -1,6 +1,7 @@
 <?php
 /**
 <<<<<<< HEAD
+<<<<<<< HEAD
  * 2007-2020 PrestaShop SA and Contributors
 =======
  * 2007-2020 PrestaShop and Contributors
@@ -31,6 +32,31 @@
 >>>>>>> Update tinymce from 4.0.16 modified to 4.9.8
  * International Registered Trademark & Property of PrestaShop SA
  */
+=======
+ *  * 2007-2019 PrestaShop and Contributors
+ *   *
+ *    * NOTICE OF LICENSE
+ *     *
+ *      * This source file is subject to the Academic Free License (AFL 3.0)
+ *       * that is bundled with this package in the file LICENSE.txt.
+ *        * It is also available through the world-wide-web at this URL:
+ *         * http://opensource.org/licenses/afl-3.0.php
+ *          * If you did not receive a copy of the license and are unable to
+ *           * obtain it through the world-wide-web, please send an email
+ *            * to license@prestashop.com so we can send you a copy immediately.
+ *             *
+ *              * DISCLAIMER
+ *               *
+ *                * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ *                 * versions in the future. If you wish to customize PrestaShop for your
+ *                  * needs please refer to http://www.prestashop.com for more information.
+ *                   *
+ *                    * @author    PrestaShop SA <contact@prestashop.com>
+ *                     * @copyright 2007-2019 PrestaShop SA and Contributors
+ *                      * @license   http://opensource.org/licenses/afl-3.0.php Academic Free License (AFL 3.0)
+ *                       * International Registered Trademark & Property of PrestaShop SA
+ *                        */
+>>>>>>> Restore index.php of modern theme
 
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
@@ -40,4 +66,8 @@ header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
 header("Location: ../");
+<<<<<<< HEAD
 exit;
+=======
+exit;
+>>>>>>> Restore index.php of modern theme
