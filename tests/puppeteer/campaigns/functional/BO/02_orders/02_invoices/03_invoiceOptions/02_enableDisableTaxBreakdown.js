73c0d563794a908bf280e65b0f9e7155a0e11907
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-02-14 17:12:26 +0100

    Add context for invoices tests

--- tests/puppeteer/campaigns/functional/BO/02_orders/02_invoices/03_invoiceOptions/02_enableDisableTaxBreakdown.js
+++ tests/puppeteer/campaigns/functional/BO/02_orders/02_invoices/03_invoiceOptions/02_enableDisableTaxBreakdown.js
@@ -28,6 +28,10 @@ const ProductFaker = require('@data/faker/product');
 const {PaymentMethods} = require('@data/demo/orders');
 const {DefaultAccount} = require('@data/demo/customer');
 const {Statuses} = require('@data/demo/orders');
+// Test context imports
+const testContext = require('@utils/testContext');
+
+const baseContext = 'functional_BO_orders_invoices_invoiceOptions_enableDisableTaxBreakdown';
 
 let browser;
 let page;
@@ -106,6 +110,7 @@ describe('Test enable/disable tax breakdown', async () => {
   describe('Enable tax breakdown then check it in the invoice created', async () => {
     describe('Enable tax breakdown', async () => {
       it('should go to invoices page', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'goToInvoicesPageToEnableTaxBreakDown', baseContext);
         await this.pageObjects.boBasePage.goToSubMenu(
           this.pageObjects.boBasePage.ordersParentLink,
           this.pageObjects.boBasePage.invoicesLink,
@@ -116,6 +121,7 @@ describe('Test enable/disable tax breakdown', async () => {
       });
 
       it('should enable tax breakdown', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'enableTaxBreakDown', baseContext);
         await this.pageObjects.invoicesPage.enableTaxBreakdown(true);
         const textMessage = await this.pageObjects.invoicesPage.saveInvoiceOptions();
         await expect(textMessage).to.contains(this.pageObjects.invoicesPage.successfulUpdateMessage);
@@ -124,6 +130,7 @@ describe('Test enable/disable tax breakdown', async () => {
 
     describe('Create tax rule', async () => {
       it('should go to "Taxes" page', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'goToTaxesPage', baseContext);
         await this.pageObjects.boBasePage.goToSubMenu(
           this.pageObjects.boBasePage.internationalParentLink,
           this.pageObjects.boBasePage.taxesLink,
@@ -133,34 +140,40 @@ describe('Test enable/disable tax breakdown', async () => {
       });
 
       it('should go to "Tax Rules" page', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'goToTaxRulesPage', baseContext);
         await this.pageObjects.taxesPage.goToTaxRulesPage();
         const pageTitle = await this.pageObjects.taxRulesPage.getPageTitle();
         await expect(pageTitle).to.contains(this.pageObjects.taxRulesPage.pageTitle);
       });
 
       it('should go to Add new tax rules group page', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'goToAddTaxRulePage', baseContext);
         await this.pageObjects.taxRulesPage.goToAddNewTaxRulesGroupPage();
         const pageTitle = await this.pageObjects.addTaxRulesPage.getPageTitle();
         await expect(pageTitle).to.contains(this.pageObjects.addTaxRulesPage.pageTitleCreate);
       });
 
       it('should create new tax rule group', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'createTaxRuleGroup', baseContext);
         const textResult = await this.pageObjects.addTaxRulesPage.createEditTaxRulesGroup(taxRuleGroupToCreate);
         await expect(textResult).to.contains(this.pageObjects.addTaxRulesPage.successfulCreationMessage);
       });
 
       it('should create new tax rule', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'createFirstTaxRule', baseContext);
         const textResult = await this.pageObjects.addTaxRulesPage.createEditTaxRules(firstTaxRuleToCreate);
         await expect(textResult).to.contains(this.pageObjects.addTaxRulesPage.successfulUpdateMessage);
       });
 
       it('should click on Add new tax rule button', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'clickToCreateSecondTaxRule', baseContext);
         await this.pageObjects.addTaxRulesPage.clickOnAddNewTaxRule();
         const pageTitle = await this.pageObjects.addTaxRulesPage.getPageTitle();
         await expect(pageTitle).to.contains(this.pageObjects.addTaxRulesPage.pageTitleEdit);
       });
 
       it('should create new tax rule', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'createSecondTaxRule', baseContext);
         const textResult = await this.pageObjects.addTaxRulesPage.createEditTaxRules(secondTaxRuleToCreate);
         await expect(textResult).to.contains(this.pageObjects.addTaxRulesPage.successfulUpdateMessage);
       });
@@ -168,16 +181,17 @@ describe('Test enable/disable tax breakdown', async () => {
 
     describe('Create new product with the new tax rule', async () => {
       it('should go to Products page', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'goToProductPageToCreateProduct', baseContext);
         await this.pageObjects.boBasePage.goToSubMenu(
           this.pageObjects.boBasePage.catalogParentLink,
           this.pageObjects.boBasePage.productsLink,
         );
-        await this.pageObjects.boBasePage.closeSfToolBar();
         const pageTitle = await this.pageObjects.productsPage.getPageTitle();
         await expect(pageTitle).to.contains(this.pageObjects.productsPage.pageTitle);
       });
 
       it('should create Product', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'createProduct', baseContext);
         await this.pageObjects.productsPage.goToAddProductPage();
         const createProductMessage = await this.pageObjects.addProductPage.createEditProduct(productData);
         await expect(createProductMessage).to.equal(this.pageObjects.addProductPage.settingUpdatedMessage);
@@ -186,6 +200,7 @@ describe('Test enable/disable tax breakdown', async () => {
 
     describe('Create new order in FO with the created product', async () => {
       it('should go to FO and create an order', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'createOrderInFO', baseContext);
         // Click on preview button
         page = await this.pageObjects.addProductPage.previewProduct();
         this.pageObjects = await init();
@@ -217,6 +232,7 @@ describe('Test enable/disable tax breakdown', async () => {
 
     describe('Generate the invoice and check the tax breakdown', async () => {
       it('should go to orders page', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'goToOrdersPageTaxBreakdown', baseContext);
         await this.pageObjects.boBasePage.goToSubMenu(
           this.pageObjects.boBasePage.ordersParentLink,
           this.pageObjects.boBasePage.ordersLink,
@@ -226,17 +242,20 @@ describe('Test enable/disable tax breakdown', async () => {
       });
 
       it('should go to the first order page', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'goToFirstOrderPageTaxBreakdown', baseContext);
         await this.pageObjects.ordersPage.goToOrder(1);
         const pageTitle = await this.pageObjects.viewOrderPage.getPageTitle();
         await expect(pageTitle).to.contains(this.pageObjects.viewOrderPage.pageTitle);
       });
 
       it(`should change the order status to '${Statuses.paymentAccepted.status}' and check it`, async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'changeOrderStatusTaxBreakdown', baseContext);
         const result = await this.pageObjects.viewOrderPage.modifyOrderStatus(Statuses.paymentAccepted.status);
         await expect(result).to.be.true;
       });
 
       it('should download the invoice', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'downloadInvoiceTaxBreakdown', baseContext);
         firstInvoiceFileName = await this.pageObjects.viewOrderPage.getFileName();
         await this.pageObjects.viewOrderPage.downloadInvoice();
         const exist = await files.checkFileExistence(`${firstInvoiceFileName}.pdf`);
@@ -244,6 +263,7 @@ describe('Test enable/disable tax breakdown', async () => {
       });
 
       it('should check the tax breakdown', async () => {
+        await testContext.addContextItem(this, 'testIdentifier', 'checkTaxBreakdownInFile', baseContext);
         // Check the existence of the first tax
         let exist = await files.checkTextInPDF(`${firstInvoiceFileName}.pdf`, '10.000 %');
         await expect(exist).to.be.true;
@@ -259,16 +279,17 @@ describe('Test enable/disable tax breakdown', async () => {
   describe('Disable tax breakdown then check the invoice file', async () => {
     describe('Disable tax breakdown', async () => {
       it('should go to invoices page', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'goToInvoicesPageToDisableTaxBreakdown', baseContext);
         await this.pageObjects.boBasePage.goToSubMenu(
           this.pageObjects.boBasePage.ordersParentLink,
           this.pageObjects.boBasePage.invoicesLink,
         );
-        await this.pageObjects.boBasePage.closeSfToolBar();
         const pageTitle = await this.pageObjects.invoicesPage.getPageTitle();
         await expect(pageTitle).to.contains(this.pageObjects.invoicesPage.pageTitle);
       });
 
       it('should disable tax breakdown', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'disableTaxBreakdown', baseContext);
         await this.pageObjects.invoicesPage.enableTaxBreakdown(false);
         const textMessage = await this.pageObjects.invoicesPage.saveInvoiceOptions();
         await expect(textMessage).to.contains(this.pageObjects.invoicesPage.successfulUpdateMessage);
@@ -277,6 +298,7 @@ describe('Test enable/disable tax breakdown', async () => {
 
     describe('Generate the invoice and check that there is no tax breakdown', async () => {
       it('should go to the orders page', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'goToOrdersPageNoTaxBreakdown', baseContext);
         await this.pageObjects.boBasePage.goToSubMenu(
           this.pageObjects.boBasePage.ordersParentLink,
           this.pageObjects.boBasePage.ordersLink,
@@ -286,12 +308,14 @@ describe('Test enable/disable tax breakdown', async () => {
       });
 
       it('should go to the first order page', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'goToFirstOrderPageNoTaxBreakdown', baseContext);
         await this.pageObjects.ordersPage.goToOrder(1);
         const pageTitle = await this.pageObjects.viewOrderPage.getPageTitle();
         await expect(pageTitle).to.contains(this.pageObjects.viewOrderPage.pageTitle);
       });
 
       it('should download the invoice', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'downloadInvoiceNoTaxBreakdown', baseContext);
         secondInvoiceFileName = await this.pageObjects.viewOrderPage.getFileName();
         await this.pageObjects.viewOrderPage.downloadInvoice();
         const exist = await files.checkFileExistence(`${secondInvoiceFileName}.pdf`);
@@ -299,6 +323,7 @@ describe('Test enable/disable tax breakdown', async () => {
       });
 
       it('should check that there is no tax breakdown', async () => {
+        await testContext.addContextItem(this, 'testIdentifier', 'checkNoTaxBreakdownInFile', baseContext);
         let exist = await files.checkTextInPDF(`${secondInvoiceFileName}.pdf`, '10.000 %');
         await expect(exist).to.be.false;
         exist = await files.checkTextInPDF(`${secondInvoiceFileName}.pdf`, '20.000 %');
