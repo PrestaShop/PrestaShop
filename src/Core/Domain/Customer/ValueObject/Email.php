b847c696b1dca33d953fec5ecf5675255ae24d2e
Thomas Baccelli <thomas.baccelli@prestashop.com>
2020-02-14 15:35:09 +0100

    Add missing deprecation informations
    (cherry picked from commit 1a75f85a3468b22969799a1b91e4a7ca73d420fb)

--- src/Core/Domain/Customer/ValueObject/Email.php
+++ src/Core/Domain/Customer/ValueObject/Email.php
@@ -26,97 +26,16 @@
 
 namespace PrestaShop\PrestaShop\Core\Domain\Customer\ValueObject;
 
-use PrestaShop\PrestaShop\Core\Domain\Customer\Exception\CustomerConstraintException;
+use PrestaShop\PrestaShop\Core\Domain\ValueObject\Email as ValueObjectEmail;
 
 /**
- * Stores customer's email address
+ * @deprecated since version 1.7.6.4 use PrestaShop\PrestaShop\Core\Domain\ValueObject\Email instead
  */
-class Email
+class Email extends ValueObjectEmail
 {
-    /**
-     * @var int Maximum allowed length for customer email
-     */
-    const MAX_LENGTH = 255;
-
-    /**
-     * @var string
-     */
-    private $email;
-
-    /**
-     * @param string $email
-     */
     public function __construct($email)
     {
-        $this->assertEmailIsString($email);
-        $this->assertEmailDoesNotExceedAllowedLength($email);
-        $this->assertEmailIsValid($email);
-
-        $this->email = $email;
-    }
-
-    /**
-     * @return string
-     */
-    public function getValue()
-    {
-        return $this->email;
-    }
-
-    /**
-     * Check if given email is the same as current
-     *
-     * @param Email $email
-     *
-     * @return bool
-     */
-    public function isEqualTo(Email $email)
-    {
-        return strtolower($email->getValue()) === strtolower($this->getValue());
-    }
-
-    /**
-     * Assert that email is in valid format
-     *
-     * @param string $email
-     *
-     * @throws CustomerConstraintException
-     */
-    private function assertEmailIsValid($email)
-    {
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            throw new CustomerConstraintException(sprintf('Customer email %s is invalid.', var_export($email, true)), CustomerConstraintException::INVALID_EMAIL);
-        }
-    }
-
-    /**
-     * Assert that email length does not exceed allowed value
-     *
-     * @param string $email
-     *
-     * @throws CustomerConstraintException
-     */
-    private function assertEmailDoesNotExceedAllowedLength($email)
-    {
-        $email = html_entity_decode($email, ENT_COMPAT, 'UTF-8');
-
-        $length = function_exists('mb_strlen') ? mb_strlen($email, 'UTF-8') : strlen($email);
-        if (self::MAX_LENGTH < $length) {
-            throw new CustomerConstraintException(sprintf('Customer email is too long. Max allowed length is %s', self::MAX_LENGTH), CustomerConstraintException::INVALID_EMAIL);
-        }
-    }
-
-    /**
-     * Assert email is of type string
-     *
-     * @param string $email
-     *
-     * @throws CustomerConstraintException
-     */
-    private function assertEmailIsString($email)
-    {
-        if (!is_string($email)) {
-            throw new CustomerConstraintException('Customer email must be of type string', CustomerConstraintException::INVALID_EMAIL);
-        }
+        @trigger_error(self::class . ' is deprecated. Use ' . BaseEmail::class . ' instead', E_USER_DEPRECATED);
+        parent::__construct($email);
     }
 }
