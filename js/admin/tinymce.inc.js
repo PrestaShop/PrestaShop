167619ff4f144342a9dfe58c24b7ae80211f2341
Valentin Szczupak <valentin.szczupak@prestashop.com>
2020-02-18 10:44:48 +0100

    Disable weird line break

--- js/admin/tinymce.inc.js
+++ js/admin/tinymce.inc.js
@@ -47,11 +47,9 @@ function tinySetup(config) {
 
   var default_config = {
     selector: '.rte',
-    plugins:
-      'align colorpicker link image filemanager table media placeholder advlist code table autoresize',
+    plugins: 'align colorpicker link image filemanager table media placeholder advlist code table autoresize',
     browser_spellcheck: true,
-    toolbar1:
-      'code,colorpicker,bold,italic,underline,strikethrough,blockquote,link,align,bullist,numlist,table,image,media,formatselect',
+    toolbar1: 'code,colorpicker,bold,italic,underline,strikethrough,blockquote,link,align,bullist,numlist,table,image,media,formatselect',
     toolbar2: '',
     external_filemanager_path: baseAdminDir + 'filemanager/',
     filemanager_title: 'File manager',
