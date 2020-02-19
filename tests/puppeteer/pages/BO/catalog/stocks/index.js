4d91f2799de2516997c003087c2e4f134a894ea5
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-02-19 10:19:58 +0100

    Fix selector name

--- tests/puppeteer/pages/BO/catalog/stocks/index.js
+++ tests/puppeteer/pages/BO/catalog/stocks/index.js
@@ -9,7 +9,7 @@ module.exports = class Stocks extends BOBasePage {
     this.successfulUpdateMessage = 'Stock successfully updated';
 
     // Selectors
-    this.MovementsNavItemLink = '#head_tabs li:nth-child(2) > a';
+    this.movementsNavItemLink = '#head_tabs li:nth-child(2) > a';
     this.searchForm = 'form.search-form';
     this.searchInput = `${this.searchForm} input.input`;
     this.searchButton = `${this.searchForm} button.search-button`;
@@ -55,8 +55,8 @@ module.exports = class Stocks extends BOBasePage {
    * @return {Promise<void>}
    */
   async goToSubTabMovements() {
-    await this.page.click(this.MovementsNavItemLink);
-    await this.page.waitForSelector(`${this.MovementsNavItemLink}.active`, {visible: true});
+    await this.page.click(this.movementsNavItemLink);
+    await this.page.waitForSelector(`${this.movementsNavItemLink}.active`, {visible: true});
   }
 
   /**
