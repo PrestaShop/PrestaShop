71871da93d051593605acc14e1169d242b801edd
Tadas Davidsonas <tadas.davidsonas@invertus.eu>
2020-02-21 09:52:47 +0200

    changes after code review

--- src/Core/Grid/Definition/Factory/CartGridDefinitionFactory.php
+++ src/Core/Grid/Definition/Factory/CartGridDefinitionFactory.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 2007-2019 PrestaShop and Contributors
+ * 2007-2020 PrestaShop and Contributors
  *
  * NOTICE OF LICENSE
  *
@@ -19,7 +19,7 @@
  * needs please refer to https://www.prestashop.com for more information.
  *
  * @author    PrestaShop SA <contact@prestashop.com>
- * @copyright 2007-2019 PrestaShop SA and Contributors
+ * @copyright 2007-2020 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
@@ -220,7 +220,7 @@ final class CartGridDefinitionFactory extends AbstractGridDefinitionFactory
      */
     protected function getFilters()
     {
-        $filters = (new FilterCollection())
+        return (new FilterCollection())
             ->add((new Filter('id_cart', NumberType::class))
                 ->setTypeOptions([
                     'attr' => [
@@ -283,10 +283,7 @@ final class CartGridDefinitionFactory extends AbstractGridDefinitionFactory
                     'redirect_route' => 'admin_carts_index',
                 ])
                 ->setAssociatedColumn('actions')
-            )
-        ;
-
-        return $filters;
+            );
     }
 
     /**
