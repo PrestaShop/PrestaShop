f0db3e8c18e848194e7b6ffe7f2a01c1c77fe732
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2020-02-26 09:15:58 +0100

    check alert in suppliers page

--- tests/puppeteer/campaigns/functional/BO/13_shopParameters/01_general/general/08_enableDisableSuppliers.js
+++ tests/puppeteer/campaigns/functional/BO/13_shopParameters/01_general/general/08_enableDisableSuppliers.js
@@ -8,6 +8,8 @@ const BOBasePage = require('@pages/BO/BObasePage');
 const LoginPage = require('@pages/BO/login');
 const DashboardPage = require('@pages/BO/dashboard');
 const GeneralPage = require('@pages/BO/shopParameters/general');
+const BrandsPage = require('@pages/BO/catalog/brands');
+const SuppliersPage = require('@pages/BO/catalog/suppliers');
 const HomePage = require('@pages/FO/home');
 const SiteMapPage = require('@pages/FO/siteMap');
 // Test context imports
@@ -25,6 +27,8 @@ const init = async function () {
     loginPage: new LoginPage(page),
     dashboardPage: new DashboardPage(page),
     generalPage: new GeneralPage(page),
+    brandsPage: new BrandsPage(page),
+    suppliersPage: new SuppliersPage(page),
     homePage: new HomePage(page),
     siteMapPage: new SiteMapPage(page),
   };
@@ -44,26 +48,49 @@ describe('Enable/Disable display suppliers', async () => {
   // Login into BO and go to general page
   loginCommon.loginBO();
 
-  it('should go to \'Shop parameters > General\' page', async function () {
-    await testContext.addContextItem(this, 'testIdentifier', 'goToGeneralPage', baseContext);
-    await this.pageObjects.boBasePage.goToSubMenu(
-      this.pageObjects.boBasePage.shopParametersParentLink,
-      this.pageObjects.boBasePage.shopParametersGeneralLink,
-    );
-    await this.pageObjects.boBasePage.closeSfToolBar();
-    const pageTitle = await this.pageObjects.generalPage.getPageTitle();
-    await expect(pageTitle).to.contains(this.pageObjects.generalPage.pageTitle);
-  });
-
   const tests = [
     {args: {action: 'enable', exist: true}},
     {args: {action: 'disable', exist: false}},
   ];
-  tests.forEach((test) => {
+  tests.forEach((test, index) => {
+    it('should go to \'Shop parameters > General\' page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', `goToGeneralPage_${index}`, baseContext);
+      await this.pageObjects.boBasePage.goToSubMenu(
+        this.pageObjects.boBasePage.shopParametersParentLink,
+        this.pageObjects.boBasePage.shopParametersGeneralLink,
+      );
+      await this.pageObjects.boBasePage.closeSfToolBar();
+      const pageTitle = await this.pageObjects.generalPage.getPageTitle();
+      await expect(pageTitle).to.contains(this.pageObjects.generalPage.pageTitle);
+    });
+
     it(`should ${test.args.action} display suppliers`, async function () {
       await testContext.addContextItem(this, 'testIdentifier', `${test.args.action}DisplaySuppliers`, baseContext);
       const result = await this.pageObjects.generalPage.setDisplaySuppliers(test.args.exist);
       await expect(result).to.contains(this.pageObjects.generalPage.successfulUpdateMessage);
+    });
+
+    it('should go to Brands & Suppliers page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', `goToBrandsPage_${index}`, baseContext);
+      await this.pageObjects.boBasePage.goToSubMenu(
+        this.pageObjects.boBasePage.catalogParentLink,
+        this.pageObjects.boBasePage.brandsAndSuppliersLink,
+      );
+      const pageTitle = await this.pageObjects.brandsPage.getPageTitle();
+      await expect(pageTitle).to.contains(this.pageObjects.brandsPage.pageTitle);
+    });
+
+    it('should go to Suppliers tab', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', `goToSuppliersTab_${index}`, baseContext);
+      await this.pageObjects.brandsPage.goToSubTabSuppliers();
+      const pageTitle = await this.pageObjects.suppliersPage.getPageTitle();
+      await expect(pageTitle).to.contains(this.pageObjects.suppliersPage.pageTitle);
+    });
+
+    it(`should check that the message alert contains '${test.args.action}'`, async function () {
+      await testContext.addContextItem(this, 'testIdentifier', `checkAlertContains_${test.args.action}`, baseContext);
+      const text = await this.pageObjects.suppliersPage.getAlertTextMessage();
+      await expect(text).to.contains(test.args.action);
     });
 
     it('should verify the existence of the suppliers page link in FO', async function () {
