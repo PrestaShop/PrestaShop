25bc0e6376a2609970e92150c22ac68d8b10b3bb
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2020-02-26 18:42:34 +0100

    Meta forms

--- src/PrestaShopBundle/Form/Admin/Configure/ShopParameters/TrafficSeo/Meta/MetaSettingsUrlSchemaFormDataProvider.php
+++ src/PrestaShopBundle/Form/Admin/Configure/ShopParameters/TrafficSeo/Meta/MetaSettingsUrlSchemaFormDataProvider.php
@@ -37,27 +37,12 @@ use Symfony\Component\Translation\TranslatorInterface;
  * Class MetaSettingsFormDataProvider is responsible for providing configurations data and responsible for persisting data
  * in configuration database.
  */
-final class MetaSettingsFormDataProvider implements FormDataProviderInterface
+final class MetaSettingsUrlSchemaFormDataProvider implements FormDataProviderInterface
 {
     /**
      * @var DataConfigurationInterface
      */
-    private $setUpUrlDataConfiguration;
-
-    /**
-     * @var DataConfigurationInterface
-     */
-    private $shopUrlsDataConfiguration;
-
-    /**
-     * @var DataConfigurationInterface
-     */
     private $urlSchemaDataConfiguration;
-
-    /**
-     * @var DataConfigurationInterface
-     */
-    private $seoOptionsDataConfiguration;
 
     /**
      * @var RouteValidator
@@ -77,27 +62,18 @@ final class MetaSettingsFormDataProvider implements FormDataProviderInterface
     /**
      * MetaFormDataProvider constructor.
      *
-     * @param DataConfigurationInterface $setUpUrlDataConfiguration
-     * @param DataConfigurationInterface $shopUrlsDataConfiguration
      * @param DataConfigurationInterface $urlSchemaDataConfiguration
-     * @param DataConfigurationInterface $seoOptionsDataConfiguration
      * @param TranslatorInterface $translator
      * @param RouteValidator $routeValidator
      * @param Validate $validate
      */
     public function __construct(
-        DataConfigurationInterface $setUpUrlDataConfiguration,
-        DataConfigurationInterface $shopUrlsDataConfiguration,
         DataConfigurationInterface $urlSchemaDataConfiguration,
-        DataConfigurationInterface $seoOptionsDataConfiguration,
         TranslatorInterface $translator,
         RouteValidator $routeValidator,
         Validate $validate
     ) {
-        $this->setUpUrlDataConfiguration = $setUpUrlDataConfiguration;
-        $this->shopUrlsDataConfiguration = $shopUrlsDataConfiguration;
         $this->urlSchemaDataConfiguration = $urlSchemaDataConfiguration;
-        $this->seoOptionsDataConfiguration = $seoOptionsDataConfiguration;
         $this->routeValidator = $routeValidator;
         $this->translator = $translator;
         $this->validate = $validate;
@@ -108,12 +84,7 @@ final class MetaSettingsFormDataProvider implements FormDataProviderInterface
      */
     public function getData()
     {
-        return [
-            'set_up_urls' => $this->setUpUrlDataConfiguration->getConfiguration(),
-            'shop_urls' => $this->shopUrlsDataConfiguration->getConfiguration(),
-            'url_schema' => $this->urlSchemaDataConfiguration->getConfiguration(),
-            'seo_options' => $this->seoOptionsDataConfiguration->getConfiguration(),
-        ];
+        return $this->urlSchemaDataConfiguration->getConfiguration();
     }
 
     /**
@@ -127,12 +98,7 @@ final class MetaSettingsFormDataProvider implements FormDataProviderInterface
             return $errors;
         }
 
-        return array_merge(
-            $this->setUpUrlDataConfiguration->updateConfiguration($data['set_up_urls']),
-            $this->shopUrlsDataConfiguration->updateConfiguration($data['shop_urls']),
-            $this->urlSchemaDataConfiguration->updateConfiguration($data['url_schema']),
-            $this->seoOptionsDataConfiguration->updateConfiguration($data['seo_options'])
-        );
+        return $this->urlSchemaDataConfiguration->updateConfiguration($data);
     }
 
     /**
@@ -146,29 +112,9 @@ final class MetaSettingsFormDataProvider implements FormDataProviderInterface
      */
     private function validateData(array $data)
     {
-        $urlSchemaErrors = $this->validateUrlSchema($data['url_schema']);
-        $shopUrlErrors = $this->validateShopUrl($data['shop_urls']);
-
-        return array_merge(
-            $urlSchemaErrors,
-            $shopUrlErrors
-        );
-    }
-
-    /**
-     * Validates if configuration matches route pattern and if route has mandatory fields.
-     *
-     * @param array $configuration
-     *
-     * @return array
-     *
-     * @throws PrestaShopException
-     */
-    private function validateUrlSchema(array $configuration)
-    {
         $patternErrors = [];
         $requiredFieldErrors = [];
-        foreach ($configuration as $routeId => $rule) {
+        foreach ($data as $routeId => $rule) {
             if (!$this->routeValidator->isRoutePattern($rule)) {
                 $patternErrors[] = $this->translator->trans(
                   'The route %routeRule% is not valid',
@@ -201,34 +147,5 @@ final class MetaSettingsFormDataProvider implements FormDataProviderInterface
         }
 
         return $requiredFieldErrors;
-    }
-
-    /**
-     * Validates shop url form data.
-     *
-     * @param array $configuration
-     *
-     * @return array
-     */
-    private function validateShopUrl(array $configuration)
-    {
-        $errors = [];
-        if (!$this->validate->isCleanHtml($configuration['domain'])) {
-            $errors[] = $this->translator->trans(
-                'This domain is not valid.',
-                [],
-                'Admin.Notifications.Error'
-            );
-        }
-
-        if (!$this->validate->isCleanHtml($configuration['domain_ssl'])) {
-            $errors[] = $this->translator->trans(
-                'The SSL domain is not valid.',
-                [],
-                'Admin.Notifications.Error'
-            );
-        }
-
-        return $errors;
     }
 }
