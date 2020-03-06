341478ac0f1049a8a3d64d4569919e5a864a3ee6
Julius Zukauskas <zuk3975@gmail.com>
2020-03-06 17:04:37 +0200

    remove todo

--- src/Adapter/Order/CommandHandler/DeleteProductFromOrderHandler.php
+++ src/Adapter/Order/CommandHandler/DeleteProductFromOrderHandler.php
@@ -50,7 +50,6 @@ final class DeleteProductFromOrderHandler extends AbstractOrderCommandHandler im
      */
     public function handle(DeleteProductFromOrderCommand $command)
     {
-        //@todo: what if its the last product, should order stay with zero prices or be cancelled?
         $orderDetail = new OrderDetail($command->getOrderDetailId());
         $order = $this->getOrderObject($command->getOrderId());
         $this->assertProductCanBeDeleted($order, $orderDetail);
