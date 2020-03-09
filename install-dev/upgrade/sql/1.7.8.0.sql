bdd38a2be3fb5f03dc986608c0bdf4a6268e9f0e
Pablo Borowicz <pablo.borowicz@prestashop.com>
2020-03-09 12:48:12 +0100

    Add wording and wording_domain to Tab entity

--- install-dev/upgrade/sql/1.7.8.0.sql
+++ install-dev/upgrade/sql/1.7.8.0.sql
@@ -8,3 +8,6 @@ INSERT IGNORE INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `po
   (NULL, 'actionPresentProduct', 'Product Presenter', 'This hook is called before a product is presented', '1')
   (NULL, 'displayBanner', 'Display Banner', 'Use this hook for banners on top of every pages', '1')
 ;
+
+ALTER TABLE `PREFIX_tab` ADD `wording` VARCHAR(196) NOT NULL  DEFAULT '' AFTER `icon`;
+ALTER TABLE `PREFIX_tab` ADD `wording_domain` VARCHAR(196) NOT NULL DEFAULT '' AFTER `wording`;
