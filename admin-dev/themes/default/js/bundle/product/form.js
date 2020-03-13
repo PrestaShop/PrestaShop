b301afe217da0d92a774593ea8fecc464fbeb205
MarkALeonard <33283565+MarkALeonard@users.noreply.github.com>
2020-03-13 11:12:21 +0000

    Update admin-dev/themes/default/js/bundle/product/form.js
    Co-Authored-By: SZCZUPAK Valentin <valentin.szczupak@prestashop.com>

--- admin-dev/themes/default/js/bundle/product/form.js
+++ admin-dev/themes/default/js/bundle/product/form.js
@@ -936,7 +936,7 @@ var form = (function() {
             return false;
           }
           var orgLabel = e.attrs.label;
-          if (e.attrs.label == e.attrs.value) {
+          if (e.attrs.label === e.attrs.value) {
             engine.search(e.attrs.label, function(result) {
               if (result.length >= 1) {
                 e.attrs.label = result[0].label;
