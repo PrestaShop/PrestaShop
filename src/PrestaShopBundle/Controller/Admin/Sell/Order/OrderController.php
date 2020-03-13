0147947bd57a8670e97dd987d1a9ced0d8f0b9c3
Julius Zukauskas <zuk3975@gmail.com>
2020-03-13 11:07:36 +0200

    apply cs fixer

--- src/PrestaShopBundle/Controller/Admin/Sell/Order/OrderController.php
+++ src/PrestaShopBundle/Controller/Admin/Sell/Order/OrderController.php
@@ -75,7 +75,6 @@ use PrestaShop\PrestaShop\Core\Order\OrderSiblingProviderInterface;
 use PrestaShop\PrestaShop\Core\Search\Filters\OrderFilters;
 use PrestaShopBundle\Component\CsvResponse;
 use PrestaShopBundle\Controller\Admin\CommonController;
-use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
 use PrestaShopBundle\Form\Admin\Sell\Customer\PrivateNoteType;
 use PrestaShopBundle\Form\Admin\Sell\Order\AddOrderCartRuleType;
 use PrestaShopBundle\Form\Admin\Sell\Order\AddProductRowType;
