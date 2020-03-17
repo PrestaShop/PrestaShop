4713afa031621b8e1ac7256fbeeea3003ba6b9b0
matks <mathieu.ferment@prestashop.com>
2020-03-17 18:12:58 +0100

    Add phpdoc

--- classes/assets/CssMinifier.php
+++ classes/assets/CssMinifier.php
@@ -28,6 +28,12 @@ use MatthiasMullie\Minify\CSS;
 
 class CssMinifierCore
 {
+    /**
+     * @param string[] $files
+     * @param string $destination
+     *
+     * @return string minified data
+     */
     public static function minify(array $files, $destination)
     {
         $minifier = new CSS();
