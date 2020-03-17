8bfe70c73c3812d4aee2fc0e95d4e1832eb2a2cc
MarkALeonard <33283565+MarkALeonard@users.noreply.github.com>
2020-03-17 09:41:44 +0000

    Update admin-dev/themes/default/js/bundle/product/form.js
    Co-Authored-By: GoT <PierreRambaud@users.noreply.github.com>

--- admin-dev/themes/default/js/bundle/product/form.js
+++ admin-dev/themes/default/js/bundle/product/form.js
@@ -957,13 +957,11 @@ var form = (function() {
           }
         }
 
-        if(e.attrs.data){
-          if(filter([e.attrs]).length === 0){
-            $('#form_step3_attributes-tokenfield').val((i, value) => {
-              return value.replace(orgLabel,"");
-            });
-            return false;
-          };
+      if(e.attrs.data && filter([e.attrs]).length === 0){
+          $('#form_step3_attributes-tokenfield').val((i, value) => {
+            return value.replace(orgLabel,"");
+          });
+          return false;
         }
         
       });
