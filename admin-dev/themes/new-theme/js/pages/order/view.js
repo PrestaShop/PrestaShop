25fe2d2a273d2c832e0d7d138176b79257d1aa0a
Thomas Baccelli <thomas.baccelli@prestashop.com>
2020-03-17 21:13:57 +0100

    Refresh Documents tab when adding a product in order view

--- admin-dev/themes/new-theme/js/pages/order/view.js
+++ admin-dev/themes/new-theme/js/pages/order/view.js
@@ -25,7 +25,6 @@
 
 import OrderViewPageMap from '@pages/order/OrderViewPageMap';
 import OrderShippingManager from '@pages/order/order-shipping-manager';
-import InvoiceNoteManager from '@pages/order/invoice-note-manager';
 import OrderViewPage from '@pages/order/view/order-view-page';
 import OrderProductAutocomplete from '@pages/order/view/order-product-add-autocomplete';
 import OrderProductAdd from '@pages/order/view/order-product-add';
@@ -60,7 +59,6 @@ $(() => {
   handlePrivateNoteChange();
   handleUpdateOrderStatusButton();
 
-  new InvoiceNoteManager();
   const orderViewPageMessageHandler = new OrderViewPageMessagesHandler();
   orderViewPageMessageHandler.listenForPredefinedMessageSelection();
   orderViewPageMessageHandler.listenForFullMessagesOpen();
