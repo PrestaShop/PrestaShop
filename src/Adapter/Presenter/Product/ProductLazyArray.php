b7ae5a8c733d45d6cb866214b1fb304d18e68c7e
Jonas <jonas.burneika@gmail.com>
2020-03-19 16:11:24 +0200

    minor fix for availability messages in cart

--- src/Adapter/Presenter/Product/ProductLazyArray.php
+++ src/Adapter/Presenter/Product/ProductLazyArray.php
@@ -770,7 +770,7 @@ class ProductLazyArray extends AbstractLazyArray
         }
 
         if ($show_availability) {
-            if ($product['quantity'] - $product['quantity_wanted'] >= 0) {
+            if ($product['stock_quantity'] - $product['quantity_wanted'] >= 0) {
                 $this->product['availability_date'] = $product['available_date'];
 
                 if ($product['quantity'] < $settings->lastRemainingItems) {
