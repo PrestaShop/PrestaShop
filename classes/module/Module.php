d5da27628c7a9137abfc967459bba38c444e4b5a
matks <mathieu.ferment@prestashop.com>
2020-03-23 11:46:50 +0100

    Remove unused Exception use statement

--- classes/module/Module.php
+++ classes/module/Module.php
@@ -23,7 +23,6 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
-use Exception;
 use PrestaShop\PrestaShop\Adapter\LegacyLogger;
 use PrestaShop\PrestaShop\Adapter\Module\ModuleDataProvider;
 use PrestaShop\PrestaShop\Adapter\ServiceLocator;
