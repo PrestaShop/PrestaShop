48e03b04c315ea9b953d5e42de1591ce90e27d0c
Mickaël Andrieu <mickael.andrieu@prestashop.com>
2020-03-24 14:56:40 +0100

    Fixed deprecation on EntityMapper class

--- src/Adapter/EntityMapper.php
+++ src/Adapter/EntityMapper.php
@@ -82,7 +82,7 @@ class EntityMapper
                     if ($object_datas_lang = Db::getInstance()->executeS($sql)) {
                         foreach ($object_datas_lang as $row) {
                             foreach ($row as $key => $value) {
-                                if ($key != $entity_defs['primary'] && array_key_exists($key, $entity)) {
+                                if ($key != $entity_defs['primary'] && isset($entity, $key)) {
                                     if (!isset($object_datas[$key]) || !is_array($object_datas[$key])) {
                                         $object_datas[$key] = [];
                                     }
@@ -95,8 +95,7 @@ class EntityMapper
                 }
                 $entity->id = (int) $id;
                 foreach ($object_datas as $key => $value) {
-                    if (array_key_exists($key, $entity_defs['fields'])
-                        || array_key_exists($key, $entity)) {
+                    if (isset($entity_defs['fields'][$key]) || isset($entity, $key)) {
                         $entity->{$key} = $value;
                     } else {
                         unset($object_datas[$key]);
