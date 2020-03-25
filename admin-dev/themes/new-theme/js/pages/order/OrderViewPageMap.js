630c7d412b431ca64729ab0a54b4631666c40576
Progi1984 <franck.lefevre@prestashop.com>
2020-03-25 17:26:28 +0100

    [BO] Order > Products : The block discounts was not displayed after adding a product

--- admin-dev/themes/new-theme/js/pages/order/OrderViewPageMap.js
+++ admin-dev/themes/new-theme/js/pages/order/OrderViewPageMap.js
@@ -113,6 +113,10 @@ export default {
   productEditLocationText: '.editProductLocation',
   productEditAvailableText: '.editProductAvailable',
   productEditTotalPriceText: '.editProductTotalPrice',
+  // Product Discount List
+  productDiscountList: {
+    list: '.table.discountList',
+  },
   // Product Pack Modal
   productPackModal: {
     modal: '#product-pack-modal',
