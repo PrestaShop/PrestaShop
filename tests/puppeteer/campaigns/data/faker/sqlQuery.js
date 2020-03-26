c60e249c6ec67bd4cbf8165728b5525a8afbcdde
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2020-03-26 10:53:47 +0100

    Delete tables name and use the key

--- tests/puppeteer/campaigns/data/faker/sqlQuery.js
+++ tests/puppeteer/campaigns/data/faker/sqlQuery.js
@@ -5,7 +5,7 @@ module.exports = class Tax {
   constructor(sqlQueryToCreate = {}) {
     this.name = sqlQueryToCreate.name || faker.random.word();
     this.tableName = sqlQueryToCreate.tableName
-      || faker.random.arrayElement((Object.values(Tables).map(table => table.name)));
+      || faker.random.arrayElement((Object.values(Tables).map(table => table.key)));
     this.sqlQuery = sqlQueryToCreate.sqlQuery
       || `select * from ${this.tableName}`;
   }
