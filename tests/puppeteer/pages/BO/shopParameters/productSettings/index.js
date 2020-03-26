dd2b23535f7a666994b170d84709a84dbf9cd40c
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-03-26 11:36:17 +0100

    Add functions for test

--- tests/puppeteer/pages/BO/shopParameters/productSettings/index.js
+++ tests/puppeteer/pages/BO/shopParameters/productSettings/index.js
@@ -29,6 +29,10 @@ module.exports = class productSettings extends BOBasePage {
     // Products stock form
     this.productsStockForm = '#configuration_fieldset_stock';
     this.allowOrderingOosLabel = `${this.productsStockForm} label[for='form_stock_allow_ordering_oos_%TOGGLE']`;
+    // Pagination form
+    this.paginationFormBlock = '#configuration_fieldset_order_by_pagination';
+    this.productsPerPageInput = '#form_pagination_products_per_page';
+    this.savePaaginationFormButton = `${this.paginationFormBlock} .card-footer button`;
   }
 
   /*
@@ -164,6 +168,17 @@ module.exports = class productSettings extends BOBasePage {
   async setAllowOrderingOutOfStockStatus(toEnable = true) {
     await this.waitForSelectorAndClick(this.allowOrderingOosLabel.replace('%TOGGLE', toEnable ? 1 : 0));
     await this.clickAndWaitForNavigation(this.saveProductPageFormButton);
+    return this.getTextContent(this.alertSuccessBlock);
+  }
+
+  /**
+   * Set number of products displayed per page
+   * @param numberOfProducts
+   * @return {Promise<string>}
+   */
+  async setProductsDisplayedPerPage(numberOfProducts) {
+    await this.setValue(this.productsPerPageInput, numberOfProducts.toString());
+    await this.clickAndWaitForNavigation(this.savePaaginationFormButton);
     return this.getTextContent(this.alertSuccessBlock);
   }
 };
