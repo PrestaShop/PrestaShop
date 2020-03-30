bf1c20d157785a54eba74878a1fabf19bea24036
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2020-03-30 18:16:28 +0200

    Fix base context for allow ordering out of stock

--- tests/puppeteer/campaigns/functional/BO/13_shopParameters/03_productSettings/03_productsStock/01_allowOrderingOutOfStock.js
+++ tests/puppeteer/campaigns/functional/BO/13_shopParameters/03_productSettings/03_productsStock/01_allowOrderingOutOfStock.js
@@ -1,7 +1,7 @@
 require('module-alias/register');
 const testContext = require('@utils/testContext');
 
-const baseContext = 'functional_BO_productSettings_allowOrderingOutOfStock';
+const baseContext = 'functional_BO_productSettings_productsStock_allowOrderingOutOfStock';
 // Using chai
 const {expect} = require('chai');
 const helper = require('@utils/helpers');
