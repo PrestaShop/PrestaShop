49d876abb153d9ffeb7c19ff5de86c0571fec474
Adib Aroui <aroui.adib@gmail.com>
2020-03-31 17:17:14 +0100

    Code reuse fix

--- admin-dev/themes/new-theme/js/pages/order/create/product-manager.js
+++ admin-dev/themes/new-theme/js/pages/order/create/product-manager.js
@@ -363,18 +363,7 @@ export default class ProductManager {
           // Update the stock also for combination */
           this.products[key].combinations[combinationId].stock = Number(this.products[key].combinations[combinationId].stock) + removedQty;
         }
-        
-        // Update the stock counter if the removed product is the selected product
-        if (Number(this.selectedProduct.productId) === productId)
-        {
-          // Update the stock counter if the removed combination is the selected combination
-          if (combinationId && Number(this.selectedCombinationId) == combinationId)
-          {
-            this.productRenderer.renderStock(this.products[key].combinations[combinationId].stock);
-          } else {
-            this.productRenderer.renderStock(this.products[key].stock);
-          }
-        }
+        this._preRenderStock(productId, combinationId, key);
         break;
       }
     }
@@ -399,18 +388,26 @@ export default class ProductManager {
           // Update the stock also for combination */
           this.products[key].combinations[combinationId].stock = Number(this.products[key].combinations[combinationId].stock) - newQty + previousQty;
         }
-        // Update the stock counter if "the updated product" is the selected product
-        if (Number(this.selectedProduct.productId) === productId)
-        {
-          // Update the stock counter if "the updated product combination" is the selected combination
-          if (combinationId && Number(this.selectedCombinationId) === combinationId)
-          {
-            this.productRenderer.renderStock(this.products[key].combinations[combinationId].stock);
-          } else {
-            this.productRenderer.renderStock(this.products[key].stock);
-          }
-        }
+        this._preRenderStock(productId, combinationId, key);
         break;
+      }
+    }
+  }
+  /**
+   * Renders stock depending on product or combination
+   *
+   * @private
+   */
+  _preRenderStock(productId, combinationId, key){
+    // Update the stock counter if "the updated product" is the selected product
+    if (Number(this.selectedProduct.productId) === productId)
+    {
+      // Update the stock counter if "the updated product combination" is the selected combination
+      if (combinationId && Number(this.selectedCombinationId) === combinationId)
+      {
+        this.productRenderer.renderStock(this.products[key].combinations[combinationId].stock);
+      } else {
+        this.productRenderer.renderStock(this.products[key].stock);
       }
     }
   }
