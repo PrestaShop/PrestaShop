3cb5b1147e88bfd77e964362fe56de48c42f3e9f
Pablo Borowicz <pablo.borowicz@prestashop.com>
2020-04-02 16:24:05 +0200

    Refactor providers

--- src/PrestaShopBundle/Translation/Provider/UseDefaultCatalogueInterface.php
+++ src/PrestaShopBundle/Translation/Provider/UseDefaultCatalogueInterface.php
@@ -27,25 +27,11 @@
 
 namespace PrestaShopBundle\Translation\Provider;
 
-use Symfony\Component\Translation\MessageCatalogueInterface;
-
 /**
  * Defines what should be the default catalogue, contains all the translations keys.
+ *
+ * @deprecated Since 1.7.6.5, no longer used
  */
 interface UseDefaultCatalogueInterface
 {
-    /**
-     * Get the default (aka untranslated) catalogue
-     *
-     * @param bool $empty if true, empty the catalogue values (keep the keys)
-     *
-     * @return MessageCatalogueInterface Return a default catalogue with all keys
-     */
-    public function getDefaultCatalogue($empty = true);
-
-    /**
-     * @return string Path to the default directory
-     *                Most of the time, it's `app/Resources/translations/default/{locale}`
-     */
-    public function getDefaultResourceDirectory();
 }
