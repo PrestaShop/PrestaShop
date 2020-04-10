6ff2a80d7f45910ffd40fdb8e72ab4717af114cc
Julius Zukauskas <zuk3975@gmail.com>
2020-04-10 12:05:41 +0300

    initialize command for basic product

---
+++ src/Core/Domain/Product/Command/AddBasicProductCommand.php
@@ -0,0 +1,198 @@
+<?php
+/**
+ * 2007-2020 PrestaShop SA and Contributors
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2020 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\PrestaShop\Core\Domain\Product\Command;
+
+use PrestaShop\Decimal\Number;
+use PrestaShop\PrestaShop\Core\Domain\Category\ValueObject\CategoryId;
+use PrestaShop\PrestaShop\Core\Domain\Manufacturer\ValueObject\ManufacturerId;
+use PrestaShop\PrestaShop\Core\Domain\Product\ValueObject\ProductType;
+use PrestaShop\PrestaShop\Core\Domain\TaxRulesGroup\ValueObject\TaxRulesGroupId;
+
+class AddBasicProductCommand
+{
+    /**
+     * @var string[]
+     */
+    private $localizedNames;
+
+    /**
+     * @var ProductType
+     */
+    private $type;
+
+    /**
+     * @var Number
+     */
+    private $price;
+
+    /**
+     * @var int
+     */
+    private $quantity;
+
+    /**
+     * @var CategoryId[]
+     */
+    private $categoryIds;
+
+    /**
+     * @var string[]
+     */
+    private $localizedSummaries = [];
+
+    /**
+     * @var string[]
+     */
+    private $localizedDescriptions = [];
+
+    /**
+     * @var array
+     */
+    private $features = [];
+
+    /**
+     * @var string
+     */
+    private $reference = '';
+
+    /**
+     * @var ManufacturerId|null
+     */
+    private $manufacturerId;
+
+    /**
+     * @var TaxRulesGroupId|null
+     */
+    private $taxRulesGroupId;
+
+    /**
+     * @param array $localizedNames
+     * @param int $type
+     * @param string $price
+     * @param int $quantity
+     * @param array $categoryIds
+     */
+    public function __construct(
+        array $localizedNames,
+        int $type,
+        string $price,
+        int $quantity,
+        array $categoryIds
+    ) {
+        //@todo: validate fields
+        $this->localizedNames = $localizedNames;
+        $this->type = new ProductType($type);
+        $this->price = new Number($price);
+        $this->quantity = $quantity;
+        $this->setCategoryIds($categoryIds);
+    }
+
+    /**
+     * @param string[] $localizedSummaries
+     *
+     * @return AddBasicProductCommand
+     */
+    public function setLocalizedSummaries(array $localizedSummaries): AddBasicProductCommand
+    {
+        $this->localizedSummaries = $localizedSummaries;
+
+        return $this;
+    }
+
+    /**
+     * @param string[] $localizedDescriptions
+     *
+     * @return AddBasicProductCommand
+     */
+    public function setLocalizedDescriptions(array $localizedDescriptions): AddBasicProductCommand
+    {
+        $this->localizedDescriptions = $localizedDescriptions;
+
+        return $this;
+    }
+
+    /**
+     * @param ManufacturerId|null $manufacturerId
+     *
+     * @return AddBasicProductCommand
+     */
+    public function setManufacturerId(?ManufacturerId $manufacturerId): AddBasicProductCommand
+    {
+        $this->manufacturerId = $manufacturerId;
+
+        return $this;
+    }
+
+    /**
+     * @param array $features
+     */
+    public function setFeatures(array $features): void
+    {
+        //@todo: do we need features on basic product creation at all
+        // or should we just allow Adding features to existing product
+    }
+
+    /**
+     * @param string $reference
+     */
+    public function setReference(string $reference): void
+    {
+        $this->reference = $reference;
+    }
+
+    /**
+     * @param int $manufacturerId
+     */
+    public function setManufacturer(int $manufacturerId): void
+    {
+        $this->manufacturerId = new ManufacturerId($manufacturerId);
+    }
+
+    /**
+     * @param int $taxRulesGroupId
+     *
+     * @return AddBasicProductCommand
+     */
+    public function setTaxRulesGroupId(int $taxRulesGroupId): AddBasicProductCommand
+    {
+        $this->taxRulesGroupId = new TaxRulesGroupId($taxRulesGroupId);
+
+        return $this;
+    }
+
+    /**
+     * @param int[] $categoryIds
+     */
+    private function setCategoryIds(array $categoryIds): void
+    {
+        foreach ($categoryIds as $categoryId) {
+            $this->categoryIds[] = new CategoryId($categoryId);
+        }
+    }
+}
