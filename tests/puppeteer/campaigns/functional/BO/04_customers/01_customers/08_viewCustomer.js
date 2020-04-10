af2f296521c277a00be3c90e64c17e8302d9c74e
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2020-04-10 13:52:15 +0200

    add some tests

--- tests/puppeteer/campaigns/functional/BO/04_customers/01_customers/08_viewCustomer.js
+++ tests/puppeteer/campaigns/functional/BO/04_customers/01_customers/08_viewCustomer.js
@@ -10,9 +10,16 @@ const DashboardPage = require('@pages/BO/dashboard');
 const CustomersPage = require('@pages/BO/customers');
 const AddCustomerPage = require('@pages/BO/customers/add');
 const ViewCustomerPage = require('@pages/BO/customers/view');
-const FOLoginPage = require('@pages/FO/login');
 const FOBasePage = require('@pages/FO/FObasePage');
+const HomePage = require('@pages/FO/home');
+const ProductPage = require('@pages/FO/product');
+const CartPage = require('@pages/FO/cart');
+const CheckoutPage = require('@pages/FO/checkout');
+const OrderConfirmationPage = require('@pages/FO/orderConfirmation');
+// Importing data
+const {PaymentMethods} = require('@data/demo/paymentMethods');
 const CustomerFaker = require('@data/faker/customer');
+const AddressData = require('@data/faker/address');
 // Test context imports
 const testContext = require('@utils/testContext');
 
@@ -22,6 +29,9 @@ let browser;
 let page;
 let numberOfCustomers = 0;
 const createCustomerData = new CustomerFaker();
+const address = new AddressData({city: 'Paris', country: 'France'});
+const today = new Date();
+const todayDate = today.toISOString().slice(0, 10);
 
 // Init objects needed
 const init = async function () {
@@ -32,8 +42,12 @@ const init = async function () {
     customersPage: new CustomersPage(page),
     addCustomerPage: new AddCustomerPage(page),
     viewCustomerPage: new ViewCustomerPage(page),
-    foLoginPage: new FOLoginPage(page),
     foBasePage: new FOBasePage(page),
+    homePage: new HomePage(page),
+    productPage: new ProductPage(page),
+    cartPage: new CartPage(page),
+    checkoutPage: new CheckoutPage(page),
+    orderConfirmationPage: new OrderConfirmationPage(page),
   };
 };
 
@@ -84,8 +98,8 @@ describe('View customer', async () => {
       await expect(numberOfCustomersAfterCreation).to.be.equal(numberOfCustomers + 1);
     });
   });
-  // 2 : View Customer
-  describe('View Customer Created', async () => {
+  // 2 : View customer
+  describe('View customer created', async () => {
     it('should filter list by email', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'filterToViewCreatedCustomer', baseContext);
       await this.pageObjects.customersPage.resetFilter();
@@ -107,16 +121,151 @@ describe('View customer', async () => {
 
     it('should check customer personal information', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'checkCreatedCustomerInfo', baseContext);
-      const cardHeaderText = await this.pageObjects.viewCustomerPage.getTextFromCardHeader(1);
+      const cardHeaderText = await this.pageObjects.viewCustomerPage.getTextFromPersonnelInformationForm();
       await expect(cardHeaderText).to.contains(createCustomerData.firstName);
       await expect(cardHeaderText).to.contains(createCustomerData.lastName);
       await expect(cardHeaderText).to.contains(createCustomerData.email);
+      await expect(cardHeaderText).to.contains(createCustomerData.yearOfBirth);
+      await expect(cardHeaderText).to.contains('Never');
+      await expect(cardHeaderText).to.contains('English (English)');
+      await expect(cardHeaderText).to.contains('Newsletter');
+      await expect(cardHeaderText).to.contains('Partner offers');
+      await expect(cardHeaderText).to.contains('Active');
+    });
+
+    const tests = [
+      {args: {blockName: 'Orders', number: 0}},
+      {args: {blockName: 'Carts', number: 0}},
+      {args: {blockName: 'Messages', number: 0}},
+      {args: {blockName: 'Vouchers', number: 0}},
+      {args: {blockName: 'Groups', number: 3}},
+    ];
+    tests.forEach((test) => {
+      it(`should check ${test.args.blockName} number`, async function () {
+        const cardHeaderText = await this.pageObjects.viewCustomerPage.getNumberOfElementFromTitle(test.args.blockName);
+        await expect(cardHeaderText).to.contains(test.args.number);
+      });
     });
   });
-  // 3 : Delete Customer from BO
+  // 2 : Create order
+  describe('Create order in FO', async () => {
+    it('should go to FO and add the first product to the cart', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'addFirstProductToCart', baseContext);
+      // Click on view my shop
+      page = await this.pageObjects.boBasePage.viewMyShop();
+      this.pageObjects = await init();
+      await this.pageObjects.foBasePage.changeLanguage('en');
+      // Go to the first product page
+      await this.pageObjects.homePage.goToProductPage(1);
+      // Add the product to the cart
+      await this.pageObjects.productPage.addProductToTheCart();
+      // Proceed to checkout the shopping cart
+      await this.pageObjects.cartPage.clickOnProceedToCheckout();
+      const isCheckoutPage = await this.pageObjects.checkoutPage.isCheckoutPage();
+      await expect(isCheckoutPage).to.be.true;
+    });
+
+    it('should login and go to address step', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'loginToFO', baseContext);
+      await this.pageObjects.checkoutPage.clickOnSignIn();
+      const isStepLoginComplete = await this.pageObjects.checkoutPage.customerLogin(createCustomerData);
+      await expect(isStepLoginComplete, 'Step Personal information is not complete').to.be.true;
+    });
+
+    it('should create address then continue to delivery step', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'createAddress', baseContext);
+      const isStepAddressComplete = await this.pageObjects.checkoutPage.setAddress(address);
+      await expect(isStepAddressComplete, 'Step Address is not complete').to.be.true;
+    });
+
+    it('should continue to payment step', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToPaymentStep', baseContext);
+      const isStepDeliveryComplete = await this.pageObjects.checkoutPage.goToPaymentStep();
+      await expect(isStepDeliveryComplete, 'Step Address is not complete').to.be.true;
+    });
+
+    it('should choose the payment method and confirm the order', async function () {
+      await this.pageObjects.checkoutPage.choosePaymentAndOrder(PaymentMethods.wirePayment.moduleName);
+      const cardTitle = await this.pageObjects.orderConfirmationPage
+        .getTextContent(this.pageObjects.orderConfirmationPage.orderConfirmationCardTitleH3);
+      // Check the confirmation message
+      await expect(cardTitle).to.contains(this.pageObjects.orderConfirmationPage.orderConfirmationCardTitle);
+    });
+
+    it('should go back to BO', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goBackToBo', baseContext);
+      page = await this.pageObjects.foBasePage.closePage(browser, 1);
+      this.pageObjects = await init();
+      const pageTitle = await this.pageObjects.viewCustomerPage.getPageTitle();
+      await expect(pageTitle).to.contains(this.pageObjects.viewCustomerPage.pageTitle);
+    });
+  });
+  // 3 : View customer
+  describe('View customer created', async () => {
+    it('should go to customers page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToCustomersPageToDelete', baseContext);
+      await this.pageObjects.boBasePage.goToSubMenu(
+        this.pageObjects.boBasePage.customersParentLink,
+        this.pageObjects.boBasePage.customersLink,
+      );
+      const pageTitle = await this.pageObjects.customersPage.getPageTitle();
+      await expect(pageTitle).to.contains(this.pageObjects.customersPage.pageTitle);
+    });
+
+    it('should filter list by email', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'filterToViewCreatedCustomer', baseContext);
+      await this.pageObjects.customersPage.resetFilter();
+      await this.pageObjects.customersPage.filterCustomers(
+        'input',
+        'email',
+        createCustomerData.email,
+      );
+      const textEmail = await this.pageObjects.customersPage.getTextColumnFromTableCustomers(1, 'email');
+      await expect(textEmail).to.contains(createCustomerData.email);
+    });
+
+    it('should click on view customer', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToViewCustomerPage', baseContext);
+      await this.pageObjects.customersPage.goToViewCustomerPage('1');
+      const pageTitle = await this.pageObjects.viewCustomerPage.getPageTitle();
+      await expect(pageTitle).to.contains(this.pageObjects.viewCustomerPage.pageTitle);
+    });
+
+    it('should check customer personal information', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'checkCreatedCustomerInfo', baseContext);
+      const cardHeaderText = await this.pageObjects.viewCustomerPage.getTextFromPersonnelInformationForm();
+      await expect(cardHeaderText).to.contains(createCustomerData.firstName);
+      await expect(cardHeaderText).to.contains(createCustomerData.lastName);
+      await expect(cardHeaderText).to.contains(createCustomerData.email);
+      await expect(cardHeaderText).to.contains(createCustomerData.yearOfBirth);
+      await expect(cardHeaderText).to.contains(todayDate);
+      await expect(cardHeaderText).to.contains('English (English)');
+      await expect(cardHeaderText).to.contains('Newsletter');
+      await expect(cardHeaderText).to.contains('Partner offers');
+      await expect(cardHeaderText).to.contains('Active');
+    });
+
+    const tests = [
+      {args: {blockName: 'Orders', number: 1}},
+      {args: {blockName: 'Carts', number: 1}},
+      {args: {blockName: 'Viewed products', number: 1}},
+      {args: {blockName: 'Messages', number: 0}},
+      {args: {blockName: 'Vouchers', number: 0}},
+      {args: {blockName: 'Last emails', number: 1}},
+      {args: {blockName: 'Last connections', number: 1}},
+      {args: {blockName: 'Groups', number: 3}},
+    ];
+    tests.forEach((test) => {
+      it(`should check ${test.args.blockName} number`, async function () {
+        const cardHeaderText = await this.pageObjects.viewCustomerPage.getNumberOfElementFromTitle(test.args.blockName);
+        await expect(cardHeaderText).to.contains(test.args.number);
+      });
+    });
+  });
+  // 4 : Delete customer from BO
   describe('Delete Customer', async () => {
     it('should go to customers page', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'goTocustomersPageToDelete', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'goToCustomersPageToDelete', baseContext);
       await this.pageObjects.boBasePage.goToSubMenu(
         this.pageObjects.boBasePage.customersParentLink,
         this.pageObjects.boBasePage.customersLink,
--- tests/puppeteer/pages/BO/customers/view.js
+++ tests/puppeteer/pages/BO/customers/view.js
@@ -8,8 +8,41 @@ module.exports = class ViewCustomer extends BOBasePage {
     this.pageTitle = 'Information about customer';
 
     // Selectors
-    this.cardDiv = 'div.card:nth-child(%ID)';
-    this.cardHeaderTitle = `${this.cardDiv} h3.card-header`;
+    // Personnel information
+    this.personnalInformationDiv = '.customer-personal-informations-card';
+    this.socialTitleDiv = `${this.personnalInformationDiv} .customer-social-title`;
+    this.birthDayDiv = `${this.personnalInformationDiv} .customer-birthday`;
+    this.registrationDateDiv = `${this.personnalInformationDiv} .customer-registration-date`;
+    this.lastVisitDateDiv = `${this.personnalInformationDiv} .customer-last-visit-date`;
+    this.languageNameDiv = `${this.personnalInformationDiv} .customer-language-name`;
+    this.newsLetterSubscriptionSpan = `${this.personnalInformationDiv} .customer-newsletter-subscription-status`;
+    this.partnerOfferStatusSpan = `${this.personnalInformationDiv} .customer-partner-offers-status`;
+    this.lastUpdateDiv = `${this.personnalInformationDiv} .customer-latest-update`;
+    this.statusSpan = `${this.personnalInformationDiv} .customer-status`;
+    // Orders
+    this.ordersDiv = '.customer-orders-card';
+    this.ordersTitleDiv = `${this.ordersDiv} .card-header`;
+    // Carts
+    this.cartsDiv = '.customer-carts-card';
+    this.cartsTitleDiv = `${this.cartsDiv} .card-header`;
+    // Viewed products
+    this.viewedProductsDiv = '.customer-viewed-products-card';
+    this.viewedProductsTitleDiv = `${this.viewedProductsDiv} .card-header`;
+    // Messages
+    this.messagesDiv = '.customer-messages-card';
+    this.messagesTitleDiv = `${this.messagesDiv} .card-header`;
+    // Vouchers
+    this.vouchersDiv = '.customer-discounts-card';
+    this.vouchersTitleDiv = `${this.vouchersDiv} .card-header`;
+    // Last emails
+    this.lastEmailsDiv = '.customer-sent-emails-card';
+    this.lastEmailsTitleDiv = `${this.lastEmailsDiv} .card-header`;
+    // Last connections
+    this.lastConnectionsDiv = '.customer-last-connections-card';
+    this.lastConnectionsTitleDiv = `${this.lastConnectionsDiv} .card-header`;
+    // Groups
+    this.groupsDiv = '.customer-groups-card';
+    this.groupsTitleDiv = `${this.groupsDiv} .card-header`;
   }
 
   /*
@@ -18,10 +51,47 @@ module.exports = class ViewCustomer extends BOBasePage {
 
   /**
    * get text from card header
-   * @param cardHeaderID
    * @return {Promise<textContent>}
    */
-  async getTextFromCardHeader(cardHeaderID) {
-    return this.getTextContent(this.cardHeaderTitle.replace('%ID', cardHeaderID));
+  async getTextFromPersonnelInformationForm() {
+    return this.getTextContent(this.personnalInformationDiv);
+  }
+
+  /**
+   * Get number of element from title
+   * @param cardTitle
+   * @returns {Promise<string>}
+   */
+  getNumberOfElementFromTitle(cardTitle) {
+    let selector;
+    switch (cardTitle) {
+      case 'Orders':
+        selector = this.ordersTitleDiv;
+        break;
+      case 'Carts':
+        selector = this.cartsTitleDiv;
+        break;
+      case 'Viewed products':
+        selector = this.viewedProductsTitleDiv;
+        break;
+      case 'Messages':
+        selector = this.messagesTitleDiv;
+        break;
+      case 'Vouchers':
+        selector = this.vouchersTitleDiv;
+        break;
+      case 'LAst emails':
+        selector = this.lastEmailsTitleDiv;
+        break;
+      case 'Last connections':
+        selector = this.lastConnectionsTitleDiv;
+        break;
+      case 'Groups':
+        selector = this.groupsTitleDiv;
+        break;
+      default:
+        throw new Error(`${cardTitle} was not found`);
+    }
+    return this.getTextContent(`${selector} span`);
   }
 };
