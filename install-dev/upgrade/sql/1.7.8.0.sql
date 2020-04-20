3f6693789175fe2b27b03b01a1459ee373d11126
Mickaël Andrieu <mickael.andrieu@prestashop.com>
2020-04-20 17:53:10 +0200

    Registered the hook in the database for upgrade and in the XML file

--- install-dev/upgrade/sql/1.7.8.0.sql
+++ install-dev/upgrade/sql/1.7.8.0.sql
@@ -6,5 +6,6 @@ INSERT IGNORE INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `po
   (NULL, 'actionPresentOrder', 'Order footer', 'This hook is called before an order is presented', '1'),
   (NULL, 'actionPresentOrderReturn', 'Order Return Presenter', 'This hook is called before an order return is presented', '1'),
   (NULL, 'actionPresentProduct', 'Product Presenter', 'This hook is called before a product is presented', '1')
-  (NULL, 'displayBanner', 'Display Banner', 'Use this hook for banners on top of every pages', '1')
+  (NULL, 'displayBanner', 'Display Banner', 'Use this hook for banners on top of every pages', '1'),
+  (NULL, 'actionCheckoutProcess', 'Alter the Checkout Process', 'This hook allows the developers to add/remove steps in the Checkout Process in the Front Office', '1')
 ;
