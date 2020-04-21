762ad964c298e902c6b71796c55ef4c3be1687bb
Julius Zukauskas <zuk3975@gmail.com>
2020-04-21 11:48:13 +0300

    add onclick and reuse file upload

--- admin-dev/themes/new-theme/js/vue/pages/product/ProductImageUpload.vue
+++ admin-dev/themes/new-theme/js/vue/pages/product/ProductImageUpload.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
-    <div
-      style="width: 800px; height: 300px; border: 1px solid black"
-      @dragover.prevent
-      @drop.prevent="uploadImages"
-    />
+    <div>
+      <div
+        type="file"
+        style="width: 800px; height: 300px; border: 1px solid black"
+        @dragover.prevent
+        @drop.prevent="onDragUpload"
+        @click="triggerFileInput"
+      />
+      <input
+        type="file"
+        multiple
+        id="onclick-img-upload"
+        style="display:none"
+        @change="onClickUpload"
+      >
+    </div>
   </div>
 </template>
 
@@ -13,28 +24,44 @@
 
   const router = new Router();
 
-  export default {
+  const productImageUpload = {
     name: 'ProductImageUpload',
     methods: {
-      async uploadImages(event) {
-        const formData = new FormData();
-
-        Array.from(event.dataTransfer.files).forEach((file) => {
-          formData.append(`${file.name}${file.lastModified}`, file);
+      triggerFileInput() {
+        const fileInput = document.getElementById('onclick-img-upload');
+        fileInput.click();
+      },
+      onClickUpload(event) {
+        uploadImages(event.currentTarget.files).then((resp) => {
+          // @todo success.
+          console.log(resp);
         });
-
-        try {
-          const response = await fetch(router.generate('admin_products_v2_images'), {
-            method: 'POST',
-            body: formData,
-          }).then((resp) => resp.json());
-          // @todo hanlde success or intended error message
-          console.log(response);
-        } catch (e) {
-          // @todo: handle error
-          console.log(e);
-        }
+      },
+      onDragUpload(event) {
+        uploadImages(event.dataTransfer.files).then((resp) => {
+          // @todo success.
+          console.log(resp);
+        });
       },
     },
   };
+
+  async function uploadImages(fileList) {
+    return fetch(router.generate('admin_products_v2_images'), {
+      method: 'POST',
+      body: formatBody(fileList),
+    }).then((resp) => resp.json());
+  }
+
+  function formatBody(fileList) {
+    const formData = new FormData();
+
+    Array.from(fileList).forEach((file) => {
+      formData.append(`${file.name}${file.lastModified}`, file);
+    });
+
+    return formData;
+  }
+
+  export default productImageUpload;
 </script>
