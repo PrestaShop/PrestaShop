dd97f03d2f0948a961dd875bb9e053ed3516b60b
Pablo Borowicz <pablo.borowicz@prestashop.com>
2020-05-04 18:37:49 +0200

    Use 255 for field size since they are not indexed

--- install-dev/upgrade/sql/1.7.8.0.sql
+++ install-dev/upgrade/sql/1.7.8.0.sql
@@ -5,9 +5,9 @@ INSERT IGNORE INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `po
   (NULL, 'actionPresentCart', 'Cart Presenter', 'This hook is called before a cart is presented', '1'),
   (NULL, 'actionPresentOrder', 'Order footer', 'This hook is called before an order is presented', '1'),
   (NULL, 'actionPresentOrderReturn', 'Order Return Presenter', 'This hook is called before an order return is presented', '1'),
-  (NULL, 'actionPresentProduct', 'Product Presenter', 'This hook is called before a product is presented', '1')
+  (NULL, 'actionPresentProduct', 'Product Presenter', 'This hook is called before a product is presented', '1'),
   (NULL, 'displayBanner', 'Display Banner', 'Use this hook for banners on top of every pages', '1')
 ;
 
-ALTER TABLE `PREFIX_tab` ADD `wording` VARCHAR(196) DEFAULT NULL AFTER `icon`;
-ALTER TABLE `PREFIX_tab` ADD `wording_domain` VARCHAR(196) DEFAULT NULL AFTER `wording`;
+ALTER TABLE `PREFIX_tab` ADD `wording` VARCHAR(255) DEFAULT NULL AFTER `icon`;
+ALTER TABLE `PREFIX_tab` ADD `wording_domain` VARCHAR(255) DEFAULT NULL AFTER `wording`;
