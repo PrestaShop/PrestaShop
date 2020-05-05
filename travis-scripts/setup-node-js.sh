c3068792c56cea0de1ecdc32d41221f9b2e38fe5
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-05-05 11:07:44 +0200

    Debug commit : remove rm nvm

--- travis-scripts/setup-node-js.sh
+++ travis-scripts/setup-node-js.sh
@@ -2,5 +2,5 @@
 
 echo "Install node js";
 
-sudo rm -rf ~/.nvm - curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
