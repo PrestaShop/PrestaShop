86c509c1d695ebe4ef9578710b7d26f52719da90
matks <mathieu.ferment@prestashop.com>
2020-05-06 11:12:41 +0200

    Add hook in module uninstall process

--- install-dev/upgrade/sql/1.7.8.0.sql
+++ install-dev/upgrade/sql/1.7.8.0.sql
@@ -5,6 +5,8 @@ INSERT IGNORE INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `po
   (NULL, 'actionPresentCart', 'Cart Presenter', 'This hook is called before a cart is presented', '1'),
   (NULL, 'actionPresentOrder', 'Order footer', 'This hook is called before an order is presented', '1'),
   (NULL, 'actionPresentOrderReturn', 'Order Return Presenter', 'This hook is called before an order return is presented', '1'),
-  (NULL, 'actionPresentProduct', 'Product Presenter', 'This hook is called before a product is presented', '1')
-  (NULL, 'displayBanner', 'Display Banner', 'Use this hook for banners on top of every pages', '1')
+  (NULL, 'actionPresentProduct', 'Product Presenter', 'This hook is called before a product is presented', '1'),
+  (NULL, 'displayBanner', 'Display Banner', 'Use this hook for banners on top of every pages', '1'),
+  (NULL, 'actionModuleUninstallBefore', 'Module uninstall before', 'This hook is called before module uninstall process', '1'),
+  (NULL, 'actionModuleUninstallAfter', 'Module uninstall after', 'This hook is called at the end of module uninstall process', '1')
 ;
