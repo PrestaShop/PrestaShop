099546897c10ef17dc32849f8485f9827549e074
Jevgenij Visockij <jevgenij.visockij@invertus.eu>
2020-05-07 15:12:19 +0300

    Added cart rule delete action button

---
+++ src/Adapter/CartRule/AbstractCartRuleHandler.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * 2007-2020 PrestaShop SA and Contributors
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2020 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\PrestaShop\Adapter\CartRule;
+
+use CartRule;
+use PrestaShop\PrestaShop\Core\Domain\CartRule\Exception\CartRuleException;
+use PrestaShop\PrestaShop\Core\Domain\CartRule\Exception\CartRuleNotFoundException;
+use PrestaShop\PrestaShop\Core\Domain\CartRule\ValueObject\CartRuleId;
+use PrestaShopException;
+
+/**
+ * Provides reusable methods for CartRule handlers
+ */
+abstract class AbstractCartRuleHandler
+{
+    /**
+     * Gets legacy CartRule
+     *
+     * @param CartRuleId $cartRuleId
+     *
+     * @return CartRule
+     *
+     * @throws CartRuleException
+     * @throws CartRuleNotFoundException
+     */
+    protected function getCartRule(CartRuleId $cartRuleId): CartRule
+    {
+        try {
+            $cartRule = new CartRule($cartRuleId->getValue());
+        } catch (PrestaShopException $e) {
+            throw new CartRuleException('Failed to create new CartRule object', 0, $e);
+        }
+
+        if ($cartRule->id !== $cartRuleId->getValue()) {
+            throw new CartRuleNotFoundException(sprintf('CartRule with id "%s" was not found.', $cartRuleId->getValue()));
+        }
+
+        return $cartRule;
+    }
+
+    /**
+     * Deletes legacy CartRule
+     *
+     * @param CartRule $cartRule
+     *
+     * @return bool
+     *
+     * @throws CartRuleException
+     */
+    protected function deleteCartRule(CartRule $cartRule)
+    {
+        try {
+            return $cartRule->delete();
+        } catch (PrestaShopException $e) {
+            throw new CartRuleException(sprintf('An error occurred when deleting CartRule object with id "%s".', $cartRule->id));
+        }
+    }
+}
--- src/Core/Domain/CartRule/Exception/CartRuleNotFoundException.php
+++ src/Core/Domain/CartRule/Exception/CartRuleNotFoundException.php
@@ -1,12 +1,11 @@
 <?php
 /**
- * Copyright since 2007 PrestaShop SA and Contributors
- * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ * 2007-2020 PrestaShop SA and Contributors
  *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.md.
+ * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
  * https://opensource.org/licenses/OSL-3.0
  * If you did not receive a copy of the license and are unable to
@@ -17,24 +16,23 @@
  *
  * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
  * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to https://devdocs.prestashop.com/ for more information.
+ * needs please refer to https://www.prestashop.com for more information.
  *
- * @author    PrestaShop SA and Contributors <contact@prestashop.com>
- * @copyright Since 2007 PrestaShop SA and Contributors
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2020 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
  */
+
+declare(strict_types=1);
 
 namespace PrestaShop\PrestaShop\Core\Domain\CartRule\Exception;
 
-class CannotDeleteCartRuleException extends CartRuleException
-{
-    /**
-     * When fails to delete single cart rule
-     */
-    public const FAILED_DELETE = 10;
+use PrestaShop\PrestaShop\Core\Domain\Exception\DomainException;
 
-    /**
-     * When fails to delete cart rule in bulk action
-     */
-    public const FAILED_BULK_DELETE = 20;
+/**
+ * Thrown in cart rules context
+ */
+class CartRuleNotFoundException extends DomainException
+{
 }
