e05c3d3c9cf996b9375767a4c8f242b2bb846eb5
nprokopenko202 <51163204+nprokopenko202@users.noreply.github.com>
2020-05-18 19:49:14 +0200

    Update controllers/front/StoresController.php
    Co-authored-by: Matt75 <5262628+Matt75@users.noreply.github.com>

--- controllers/front/StoresController.php
+++ controllers/front/StoresController.php
@@ -240,7 +240,7 @@ class StoresControllerCore extends FrontController
 
         $breadcrumb['links'][] = [
             'title' => $this->trans('Our stores', [], 'Shop.Theme.Global'),
-            'url' => $this->context->link->getPageLink('stores', true)
+            'url' => $this->context->link->getPageLink('stores', true),
         ];
         
         return $breadcrumb;
