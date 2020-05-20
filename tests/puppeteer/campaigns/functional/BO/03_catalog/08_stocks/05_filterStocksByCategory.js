7351a49ff80c12f0e74d2e0ddc3f20615e823d1d
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-05-20 09:59:46 +0200

    Undo update of test stocks

--- tests/puppeteer/campaigns/functional/BO/03_catalog/08_stocks/05_filterStocksByCategory.js
+++ tests/puppeteer/campaigns/functional/BO/03_catalog/08_stocks/05_filterStocksByCategory.js
@@ -1,20 +1,16 @@
 require('module-alias/register');
-
+// Using chai
 const {expect} = require('chai');
-
-// Import utils
 const helper = require('@utils/helpers');
 const loginCommon = require('@commonTests/loginBO');
 
-// Import pages
+// importing pages
 const LoginPage = require('@pages/BO/login');
 const DashboardPage = require('@pages/BO/dashboard');
+const BOBasePage = require('@pages/BO/BObasePage');
 const StocksPage = require('@pages/BO/catalog/stocks');
-
-// Import data
 const {Products} = require('@data/demo/products');
-
-// Import test context
+// Test context imports
 const testContext = require('@utils/testContext');
 
 const baseContext = 'functional_BO_catalog_stocks_filterStocksByCategory';
@@ -28,6 +24,7 @@ const init = async function () {
   return {
     loginPage: new LoginPage(page),
     dashboardPage: new DashboardPage(page),
+    boBasePage: new BOBasePage(page),
     stocksPage: new StocksPage(page),
   };
 };
@@ -37,30 +34,23 @@ describe('Filter stocks by category', async () => {
   before(async function () {
     browser = await helper.createBrowser();
     page = await helper.newTab(browser);
-
     this.pageObjects = await init();
   });
-
   after(async () => {
     await helper.closeBrowser(browser);
   });
-
   // Steps
   loginCommon.loginBO();
 
   it('should go to stocks page', async function () {
     await testContext.addContextItem(this, 'testIdentifier', 'goToStocksPage', baseContext);
-
-    await this.pageObjects.dashboardPage.goToSubMenu(
-      this.pageObjects.dashboardPage.catalogParentLink,
-      this.pageObjects.dashboardPage.stocksLink,
+    await this.pageObjects.stocksPage.goToSubMenu(
+      this.pageObjects.stocksPage.catalogParentLink,
+      this.pageObjects.stocksPage.stocksLink,
     );
-
-    await this.pageObjects.stocksPage.closeSfToolBar();
-
+    await this.pageObjects.boBasePage.closeSfToolBar();
     const pageTitle = await this.pageObjects.stocksPage.getPageTitle();
     await expect(pageTitle).to.contains(this.pageObjects.stocksPage.pageTitle);
-
     numberOfProducts = await this.pageObjects.stocksPage.getTotalNumberOfProducts();
     await expect(numberOfProducts).to.be.above(0);
   });
@@ -70,14 +60,10 @@ describe('Filter stocks by category', async () => {
       `should filter by category '${product.category}' and check product '${product.name}' existence`,
       async function () {
         await testContext.addContextItem(this, 'testIdentifier', `filterByCategory${index + 1}`, baseContext);
-
         await this.pageObjects.stocksPage.filterByCategory(product.category);
-
         const numberOfProductsAfterFilter = await this.pageObjects.stocksPage.getNumberOfProductsFromList();
         await expect(numberOfProductsAfterFilter).to.be.below(numberOfProducts);
-
         const productsNamesAfterFilter = await this.pageObjects.stocksPage.getAllProductsName();
-
         await expect(
           productsNamesAfterFilter.some(x => x.includes(product.name)),
           `${product.name} was not found after filter by ${product.category}`,
@@ -86,7 +72,6 @@ describe('Filter stocks by category', async () => {
 
     it('should reset filter category', async function () {
       await testContext.addContextItem(this, 'testIdentifier', `resetFilter${index + 1}`, baseContext);
-
       const numberOfProductsAfterReset = await this.pageObjects.stocksPage.resetAndGetNumberOfProductsFromList();
       await expect(numberOfProductsAfterReset).to.be.equal(numberOfProducts);
     });
