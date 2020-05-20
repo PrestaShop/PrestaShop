fb6677e49e43e7c79c149e427568c1800aa445e0
Julius Zukauskas <zuk3975@gmail.com>
2020-05-20 11:48:22 +0300

    add missing docblocks

--- admin-dev/themes/new-theme/js/pages/product/price-refresher.js
+++ admin-dev/themes/new-theme/js/pages/product/price-refresher.js
@@ -39,17 +39,26 @@ export default class PriceRefresher {
     this.init();
   }
 
+  /**
+   * Initializes two way binding between DOM and state.
+   */
   init() {
     this.listenToDomChanges();
     this.listenToStateChanges();
   }
 
+  /**
+   * Listens to DOM elements changes and mutates the state accordingly.
+   */
   listenToDomChanges() {
     this.listenToPriceChangesInDom(this.taxIncludedInputs, true);
     this.listenToPriceChangesInDom(this.taxExcludedInputs, false);
     this.listenToTaxChangesInDom();
   }
 
+  /**
+   * Subscribes to state mutations and updates the DOM.
+   */
   listenToStateChanges() {
     this.store.subscribe((mutation) => {
       const subscribedMutationTypes = [
@@ -72,6 +81,12 @@ export default class PriceRefresher {
     });
   }
 
+  /**
+   * Listens for price tax included/excluded inputs changes
+   *
+   * @param selectors
+   * @param forTaxIncluded
+   */
   listenToPriceChangesInDom(selectors, forTaxIncluded) {
     selectors.forEach((selector) => {
       const el = document.querySelector(selector);
@@ -89,6 +104,9 @@ export default class PriceRefresher {
     });
   }
 
+  /**
+   * Listens to tax rule selection changes
+   */
   listenToTaxChangesInDom() {
     this.taxRuleInputs.forEach((selector) => {
       const el = document.querySelector(selector);
@@ -103,6 +121,12 @@ export default class PriceRefresher {
     });
   }
 
+  /**
+   * Updates price values in DOM
+   *
+   * @param selectors
+   * @param forTaxIncluded
+   */
   updatePricesInDom(selectors, forTaxIncluded) {
     selectors.forEach((selector) => {
       const el = document.querySelector(selector);
@@ -115,6 +139,11 @@ export default class PriceRefresher {
     });
   }
 
+  /**
+   * Updates tax rule values in DOM
+   *
+   * @param selectors
+   */
   updateTaxRulesInDom(selectors) {
     selectors.forEach((selector) => {
       const el = document.querySelector(selector);
