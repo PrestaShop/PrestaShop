4c12b9ed8cfd34e218bfcbe4393469ec6ca2e776
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-05-22 14:06:11 +0200

    Fix urls

--- tests/puppeteer/campaigns/tools/linkchecker.js
+++ tests/puppeteer/campaigns/tools/linkchecker.js
@@ -1,12 +1,13 @@
 require('module-alias/register');
-// Using chai
+
 const {expect} = require('chai');
+
+// Import utils
 const helper = require('@utils/helpers');
 const files = require('@utils/files');
-const urlsList = require('./urls.js');
+const urlsList = require('@tools/urls.js');
 
 const LOG_PASSED = process.env.LOG_PASSED || false;
-
 
 // Report dir and file
 const reportPath = 'campaigns/tools/reports';
@@ -17,6 +18,7 @@ const output = {
   endDate: null,
   pages: [],
 };
+
 let outputEntry = {
   name: '',
   url: '',
@@ -24,8 +26,10 @@ let outputEntry = {
   failed: [],
   jsError: [],
 };
+
 let requestError = false;
 let requestTextError = '';
+
 let javascriptError = false;
 let javascriptTextError = '';
 
@@ -37,35 +41,46 @@ describe('Crawl every page for defects and issues', async () => {
     await files.createDirectory(reportPath);
     // Create report dir
     filename = await files.generateReportFilename();
+
+    // Open browser
     browser = await helper.createBrowser();
     page = await helper.newTab(browser);
     await page.setExtraHTTPHeaders({
       'Accept-Language': 'fr-FR',
     });
 
+    // Intercepts requests
     await page.setRequestInterception(true);
-    // intercepts requests
     await page.on('request', (request) => {
       request.continue();
     });
-    // intercepts responses
+
+    // Intercepts responses
     await page.on('response', (response) => {
       checkResponseStatus(
         response.request().url(),
         response.status().toString(),
       );
+
     });
-    // intercepts JS errors
+
+    // Intercepts JS errors
     await page.on('pageerror', (pageerr) => {
       javascriptTextError = pageerr.toString();
       javascriptError = true;
+
       outputEntry.jsError.push({
         error: javascriptTextError,
       });
+
     });
+
   });
+
+
   after(async () => {
     await helper.closeBrowser(browser);
+
     output.endDate = new Date().toISOString();
     await files.createFile(reportPath, `${filename}.json`, JSON.stringify(output));
   });
@@ -74,11 +89,15 @@ describe('Crawl every page for defects and issues', async () => {
     describe(`${section.name} - ${section.description}`, async () => {
       section.urls.forEach((pageToCrawl, index) => {
         it(`Crawling ${pageToCrawl.name} (${index + 1}/${section.urls.length})`, async () => {
-          /* eslint-disable no-param-reassign */
-          pageToCrawl.url = `${section.urlPrefix}${pageToCrawl.url}`;
-          /* eslint-enable no-param-reassign */
-          await crawlPage(page, pageToCrawl);
+          const crawledPage = pageToCrawl;
+          crawledPage.url = `${section.urlPrefix}${pageToCrawl.url}`;
+
+          await crawlPage(page, crawledPage);
+
+          // Check no request error
           await expect(requestError, requestTextError).to.be.false;
+
+          // Check no javascript error
           await expect(javascriptError, javascriptTextError).to.be.false;
         });
       });
@@ -95,6 +114,7 @@ describe('Crawl every page for defects and issues', async () => {
 async function crawlPage(puppeteerPage, thisPageToCrawl) {
   requestError = false;
   javascriptError = false;
+
   outputEntry = {
     name: thisPageToCrawl.name,
     url: thisPageToCrawl.url,
@@ -102,11 +122,13 @@ async function crawlPage(puppeteerPage, thisPageToCrawl) {
     failed: [],
     jsError: [],
   };
+
   await puppeteerPage.goto(`${thisPageToCrawl.url}`, {waitUntil: 'networkidle0'});
 
   if (typeof (thisPageToCrawl.customAction) !== 'undefined') {
     await thisPageToCrawl.customAction(puppeteerPage);
   }
+
   output.pages.push(outputEntry);
 }
 
@@ -120,8 +142,11 @@ async function checkResponseStatus(url, status) {
   if (status.startsWith('4') || status.startsWith('5')) {
     requestError = true;
     requestTextError = `Request error : ${url} (${status}`;
+
     outputEntry.failed.push({url, status});
+
   } else if (JSON.parse(LOG_PASSED) === true) {
     outputEntry.passed.push({url, status});
   }
+
 }
