eb880e60d0592da9cb77aa39d24a454e14ab3ba9
matks <mathieu.ferment@prestashop.com>
2020-05-27 17:39:23 +0200

    Apply code review feedbacks

--- src/Adapter/LegacyContext.php
+++ src/Adapter/LegacyContext.php
@@ -244,7 +244,7 @@ class LegacyContext
         );
 
         if (null !== $previousContextController) {
-            $this->preserveCssAndJSAssets($previousContextController, $originCtrl);
+            $this->transferCssAndJSAssets($previousContextController, $originCtrl);
         }
 
         $originCtrl->run();
@@ -362,7 +362,7 @@ class LegacyContext
      * @param Controller $previousContextController
      * @param AdminLegacyLayoutControllerCore $originCtrl
      */
-    private function preserveCssAndJSAssets($previousContextController, AdminLegacyLayoutControllerCore $originCtrl): void
+    private function transferCssAndJSAssets($previousContextController, AdminLegacyLayoutControllerCore $originCtrl): void
     {
         if (!empty($previousContextController->js_files)) {
             array_map(function ($jsAsset) use ($originCtrl) {
