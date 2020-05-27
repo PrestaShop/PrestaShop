1a4707f5a7acd300cccd8a7e36beb6f6a9eabbe4
Mathieu Ferment <mathieu.ferment@prestashop.com>
2020-05-27 09:58:51 +0200

    Apply code review
    Co-authored-by: Progi1984 <progi1984@gmail.com>

--- classes/webservice/SQLUtils.php
+++ classes/webservice/SQLUtils.php
@@ -24,6 +24,8 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
+ declare(strict_types=1);
+ 
 class SQLUtils
 {
     /**
