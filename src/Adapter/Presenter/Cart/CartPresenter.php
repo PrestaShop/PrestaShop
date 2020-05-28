95c206940032a952d01cd76e13ed2af3b114e62f
Florian <flegars@202-ecommerce.com>
2020-05-28 16:37:17 +0200

    fix voucher display for single customer

--- src/Adapter/Presenter/Cart/CartPresenter.php
+++ src/Adapter/Presenter/Cart/CartPresenter.php
@@ -445,7 +445,14 @@ class CartPresenter implements PresenterInterface
             $vouchers['added']
         ));
 
-        $discounts = array_filter($discounts, function ($discount) use ($cartRulesIds) {
+        $discounts = array_filter($discounts, function ($discount) use ($cartRulesIds, $cart) {
+            $voucherCustomerId = (int) $discount['id_customer'];
+            $voucherIsRestrictedToASingleCustomer = ($voucherCustomerId !== 0);
+            $voucherIsEmptyCode = empty($discount['code']);
+            if ($voucherIsRestrictedToASingleCustomer && $cart->id_customer !== $voucherCustomerId && $voucherIsEmptyCode) {
+                return false;
+            }
+
             return !array_key_exists($discount['id_cart_rule'], $cartRulesIds);
         });
 
@@ -477,6 +484,7 @@ class CartPresenter implements PresenterInterface
         Hook::exec('actionPresentCart',
             ['presentedCart' => &$result]
         );
+
 
         return $result;
     }
