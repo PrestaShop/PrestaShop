8b850cd349d3b34e9192448b95cc8390f6671222
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-05-29 15:16:26 +0200

    Fix contacts tests

--- tests/puppeteer/pages/BO/shopParameters/contact/index.js
+++ tests/puppeteer/pages/BO/shopParameters/contact/index.js
@@ -148,8 +148,8 @@ module.exports = class Contacts extends BOBasePage {
     this.dialogListener();
     // Click on Select All
     await Promise.all([
-      this.page.click(this.selectAllRowsLabel),
-      this.waitForVisibleSelector(`${this.selectAllRowsLabel}:not([disabled])`),
+      this.page.$eval(this.selectAllRowsLabel, el => el.click()),
+      this.waitForVisibleSelector(`${this.bulkActionsToggleButton}:not([disabled])`),
     ]);
     // Click on Button Bulk actions
     await Promise.all([
