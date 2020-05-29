a24a6be2a7b2e0607cabe0724db37b044ba581be
Ibrahima SOW <ibrahima.sow@prestashop.com>
2020-05-29 10:08:40 +0000

    Create extractors, fix syntax and tests

---
+++ src/PrestaShopBundle/Translation/Provider/ExtractorInterface.php
@@ -0,0 +1,9 @@
+<?php
+namespace PrestaShopBundle\Translation\Provider;
+
+use Symfony\Component\Translation\MessageCatalogueInterface;
+
+interface ExtractorInterface
+{
+    public function extract(): MessageCatalogueInterface;
+}
