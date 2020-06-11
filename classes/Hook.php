7d0dfeae4b046d987d3ff6e585a9ec80318e0f45
Pierre RAMBAUD <pierre.rambaud@prestashop.com>
2020-06-11 14:22:50 +0200

    Fix cs

--- classes/Hook.php
+++ classes/Hook.php
@@ -565,7 +565,7 @@ class HookCore extends ObjectModel
         if (is_array($hook_name)) {
             $hook_names = $hook_name;
         } else {
-            $hook_names = array($hook_name);
+            $hook_names = [$hook_name];
         }
 
         $idShops = ($shop_list) ? ' AND `id_shop` IN(' . implode(', ', array_map('intval', $shop_list)) . ')' : '';
