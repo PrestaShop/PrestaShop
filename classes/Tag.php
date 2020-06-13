2f1db53ac8a6d1590a6134252548f8fdfc4dc8c2
Julius Zukauskas <zuk3975@gmail.com>
2020-06-13 12:20:55 +0300

    cast id_lang to int

--- classes/Tag.php
+++ classes/Tag.php
@@ -231,7 +231,7 @@ class TagCore extends ObjectModel
         }
         $result = [];
         foreach ($tmp as $tag) {
-            $result[$tag['id_lang']][] = $tag['name'];
+            $result[(int) $tag['id_lang']][] = $tag['name'];
         }
 
         return $result;
