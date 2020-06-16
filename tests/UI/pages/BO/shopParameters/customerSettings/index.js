e73cec1039ba1ae3cb5e7e5abbd3f6b1d16a46e4
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-06-16 18:02:35 +0200

    Fix form id for customer settings page

--- tests/UI/pages/BO/shopParameters/customerSettings/index.js
+++ tests/UI/pages/BO/shopParameters/customerSettings/index.js
@@ -10,14 +10,13 @@ module.exports = class customerSettings extends BOBasePage {
     this.successfulUpdateMessage = 'Update successful';
 
     // Selectors
-    this.generalForm = '#configuration_form';
-    this.redisplayCartAtLoginLabel = toggle => `label[for='form_general_redisplay_cart_at_login_${toggle}']`;
-    this.enablePartnerOfferLabel = toggle => `label[for='form_general_enable_offers_${toggle}']`;
+    this.redisplayCartAtLoginLabel = toggle => `label[for='form_redisplay_cart_at_login_${toggle}']`;
+    this.enablePartnerOfferLabel = toggle => `label[for='form_enable_offers_${toggle}']`;
     this.sendEmailAfterRegistrationLabel = toggle => 'label'
-      + `[for='form_general_send_email_after_registration_${toggle}']`;
-    this.askForBirthDateLabel = toggle => `label[for='form_general_ask_for_birthday_${toggle}']`;
-    this.enableB2BModeToggle = toggle => `label[for='form_general_enable_b2b_mode_${toggle}']`;
-    this.saveGeneralFormButton = `${this.generalForm} .card-footer button`;
+      + `[for='form_send_email_after_registration_${toggle}']`;
+    this.askForBirthDateLabel = toggle => `label[for='form_ask_for_birthday_${toggle}']`;
+    this.enableB2BModeToggle = toggle => `label[for='form_enable_b2b_mode_${toggle}']`;
+    this.saveGeneralFormButton = '#form-general-save-button';
   }
 
   /*
