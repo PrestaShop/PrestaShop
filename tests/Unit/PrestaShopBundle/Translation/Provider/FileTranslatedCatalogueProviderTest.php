e2d627a4dc26c2419798044ca6858ce9ae22f472
Ibrahima SOW <ibrahima.sow@prestashop.com>
2020-06-16 02:47:58 +0000

    Add tests for Default and FileTranslated providers

---
+++ tests/Unit/PrestaShopBundle/Translation/Provider/FileTranslatedCatalogueProviderTest.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * 2007-2020 PrestaShop SA and Contributors
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2020 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+namespace Tests\Unit\PrestaShopBundle\Translation\Provider;
+
+use PHPUnit\Framework\TestCase;
+use PrestaShopBundle\Translation\Provider\DefaultCatalogueProvider;
+use PrestaShopBundle\Translation\Provider\FileTranslatedCatalogueProvider;
+use Symfony\Component\Translation\Dumper\XliffFileDumper;
+use Symfony\Component\Translation\MessageCatalogue;
+
+class FileTranslatedCatalogueProviderTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    private static $tempDir;
+
+    public static function setUpBeforeClass()
+    {
+        self::$tempDir = implode(
+            DIRECTORY_SEPARATOR,
+            [sys_get_temp_dir(), 'FileTranslatedCatalogueProviderTest']
+        );
+        $wordings = [
+            'ShopSomeDomain' => [
+                'Some wording' => 'Some wording',
+                'Some other wording' => 'Some other wording',
+            ],
+            'ShopSomethingElse' => [
+                'Foo' => 'Foo',
+                'Bar' => 'Bar',
+            ],
+        ];
+        $catalogue = new MessageCatalogue(DefaultCatalogueProvider::DEFAULT_LOCALE);
+        foreach ($wordings as $domain => $messages) {
+            $catalogue->add($messages, $domain);
+        }
+        (new XliffFileDumper())->dump($catalogue, [
+            'path' => self::$tempDir . DIRECTORY_SEPARATOR . DefaultCatalogueProvider::DEFAULT_LOCALE,
+        ]);
+    }
+
+    public function testGetCatalogueFilters()
+    {
+        $catalogue = (new FileTranslatedCatalogueProvider(
+            DefaultCatalogueProvider::DEFAULT_LOCALE,
+            self::$tempDir,
+            ['#^Shop([A-Z]|\.|$)#']
+        ))
+            ->getCatalogue();
+
+        $domains = $catalogue->getDomains();
+        sort($domains);
+
+        $this->assertSame([
+            'ShopSomeDomain',
+            'ShopSomethingElse',
+        ], $domains);
+        $catalogue = (new FileTranslatedCatalogueProvider(
+            DefaultCatalogueProvider::DEFAULT_LOCALE,
+            self::$tempDir,
+            ['#^ShopSomething([A-Z]|\.|$)#']
+        ))
+            ->getCatalogue();
+
+        $domains = $catalogue->getDomains();
+        sort($domains);
+
+        $this->assertSame([
+            'ShopSomethingElse',
+        ], $domains);
+
+
+    }
+
+    public function testGetCatalogueMessages()
+    {
+        $expectedWordings = [
+            'ShopSomeDomain' => [
+                'Some wording' => 'Some wording',
+                'Some other wording' => 'Some other wording',
+            ],
+            'ShopSomethingElse' => [
+                'Foo' => 'Foo',
+                'Bar' => 'Bar',
+            ],
+        ];
+
+        $provider = new FileTranslatedCatalogueProvider(
+            DefaultCatalogueProvider::DEFAULT_LOCALE,
+            self::$tempDir,
+            ['#^Shop([A-Z]|\.|$)#']
+        );
+
+        $catalogue = $provider->getCatalogue();
+
+        $messages = $catalogue->all();
+        sort($messages);
+
+        $this->assertSame(array_values($expectedWordings), $messages);
+    }
+}
