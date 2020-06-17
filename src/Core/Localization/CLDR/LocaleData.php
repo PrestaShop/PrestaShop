98ff6b94b73597dcdb6854fb03de6f8ec3bce7cc
Progi1984 <franck.lefevre@prestashop.com>
2020-06-17 13:28:07 +0200

    Improved CLDR Reader for extracting territories

--- src/Core/Localization/CLDR/LocaleData.php
+++ src/Core/Localization/CLDR/LocaleData.php
@@ -44,6 +44,14 @@ class LocaleData
     protected $localeCode;
 
     /**
+     * List of available territories
+     * Array of strings (codes).
+     *
+     * @var string[]
+     */
+    protected $territories;
+
+    /**
      * List of available numbering systems
      * Array of strings (codes).
      *
@@ -174,6 +182,13 @@ class LocaleData
                 }
                 $this->currencies[$code]->overrideWith($currencyData);
             }
+        }
+
+        if (null !== $localeData->getTerritories()) {
+            if (null === $this->territories) {
+                $this->territories = [];
+            }
+            $this->territories = array_merge($this->territories, $localeData->getTerritories());
         }
 
         return $this;
@@ -371,6 +386,26 @@ class LocaleData
     public function setCurrencies($currencies)
     {
         $this->currencies = $currencies;
+
+        return $this;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getTerritories()
+    {
+        return $this->territories;
+    }
+
+    /**
+     * @param string[] $territories
+     *
+     * @return LocaleData
+     */
+    public function setTerritories($territories)
+    {
+        $this->territories = $territories;
 
         return $this;
     }
