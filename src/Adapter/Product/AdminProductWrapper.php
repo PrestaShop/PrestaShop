8f2ec477f8dd843f069d62ac87e793cdf1a4ccfc
Mathieu Ferment <mathieu.ferment@prestashop.com>
2020-06-18 21:59:39 +0200

    Use null coalescing operator for other specific prices attributes check
    Co-authored-by: GoT <PierreRambaud@users.noreply.github.com>

--- src/Adapter/Product/AdminProductWrapper.php
+++ src/Adapter/Product/AdminProductWrapper.php
@@ -269,9 +269,9 @@ class AdminProductWrapper
     {
         // ---- data formatting ----
         $id_product_attribute = $specificPriceValues['sp_id_product_attribute'] ?? 0;
-        $id_shop = $specificPriceValues['sp_id_shop'] ? $specificPriceValues['sp_id_shop'] : 0;
-        $id_currency = $specificPriceValues['sp_id_currency'] ? $specificPriceValues['sp_id_currency'] : 0;
-        $id_country = $specificPriceValues['sp_id_country'] ? $specificPriceValues['sp_id_country'] : 0;
+        $id_shop = $specificPriceValues['sp_id_shop'] ?? 0;
+        $id_currency = $specificPriceValues['sp_id_currency'] ?? 0;
+        $id_country = $specificPriceValues['sp_id_country'] ?? 0;
         $id_group = $specificPriceValues['sp_id_group'] ? $specificPriceValues['sp_id_group'] : 0;
         $id_customer = !empty($specificPriceValues['sp_id_customer']['data']) ? $specificPriceValues['sp_id_customer']['data'][0] : 0;
         $price = isset($specificPriceValues['leave_bprice']) ? '-1' : $this->floatParser->fromString($specificPriceValues['sp_price']);
