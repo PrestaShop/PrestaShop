9ede6d5ecc745b32ac24f50fd422734ebb50a188
Progi1984 <franck.lefevre@prestashop.com>
2020-06-26 15:59:25 +0200

    Removed assignation

--- install-dev/controllers/http/database.php
+++ install-dev/controllers/http/database.php
@@ -55,8 +55,6 @@ class InstallControllerHttpDatabase extends InstallControllerHttp implements Htt
         $this->session->database_password = trim(Tools::getValue('dbPassword'));
         $this->session->database_prefix = trim(Tools::getValue('db_prefix'));
         $this->session->database_clear = Tools::getValue('database_clear');
-
-        $this->session->enable_ssl = Tools::getValue('enable_ssl');
         $this->session->rewrite_engine = Tools::getValue('rewrite_engine');
     }
 
