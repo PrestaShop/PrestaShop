dd95e9667a1702a6c212dc5cd13db989c7dce078
Julius Zukauskas <zuk3975@gmail.com>
2020-06-29 11:23:23 +0300

    deleteCustomizationField handler

---
+++ src/Adapter/Product/CommandHandler/DeleteCustomizationFieldHandler.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * 2007-2020 PrestaShop SA and Contributors
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2020 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\PrestaShop\Adapter\Product\CommandHandler;
+
+use CustomizationField;
+use PrestaShop\PrestaShop\Adapter\Product\AbstractProductHandler;
+use PrestaShop\PrestaShop\Core\Domain\Product\Customization\Field\Command\DeleteCustomizationFieldCommand;
+use PrestaShop\PrestaShop\Core\Domain\Product\Customization\Field\CommandHandler\DeleteCustomizationFieldHandlerInterface;
+use PrestaShop\PrestaShop\Core\Domain\Product\Customization\Field\Exception\CannotDeleteCustomizationFieldException;
+use PrestaShop\PrestaShop\Core\Domain\Product\Customization\Field\Exception\CustomizationFieldException;
+use PrestaShop\PrestaShop\Core\Domain\Product\Customization\Field\Exception\CustomizationFieldNotFoundException;
+use PrestaShop\PrestaShop\Core\Domain\Product\ValueObject\ProductId;
+use PrestaShopException;
+
+/**
+ * Handles @var DeleteCustomizationFieldCommand using legacy object model
+ */
+final class DeleteCustomizationFieldHandler extends AbstractProductHandler implements DeleteCustomizationFieldHandlerInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function handle(DeleteCustomizationFieldCommand $command): void
+    {
+        $fieldIdValue = $command->getCustomizationFieldId()->getValue();
+        $customizationField = $this->loadCustomizationField($fieldIdValue);
+        $product = $this->getProduct(new ProductId($customizationField->id_product));
+        $usedFieldIds = array_map('intval', $product->getUsedCustomizationFieldsIds());
+
+        try {
+            if (in_array($fieldIdValue, $usedFieldIds)) {
+                $successfullyDeleted = $customizationField->softDelete();
+            } else {
+                $successfullyDeleted = $customizationField->delete();
+            }
+        } catch (PrestaShopException $e) {
+            throw new CustomizationFieldException(
+                sprintf(
+                    'Error occurred when trying to delete customization field #%d',
+                    $fieldIdValue
+                ),
+                0,
+                $e
+            );
+        }
+
+
+        if (!$successfullyDeleted) {
+            throw new CannotDeleteCustomizationFieldException(sprintf(
+                'Failed deleting customization field #%d',
+                    $fieldIdValue
+                )
+            );
+        }
+    }
+
+    /**
+     * @param int $fieldId
+     *
+     * @return CustomizationField
+     *
+     * @throws CustomizationFieldException
+     * @throws CustomizationFieldNotFoundException
+     */
+    private function loadCustomizationField(int $fieldId): CustomizationField
+    {
+        try {
+            $field = new CustomizationField($fieldId);
+
+            if ((int) $field->id !== $fieldId) {
+                throw new CustomizationFieldNotFoundException(sprintf(
+                    'Customization field #%d was not found',
+                    $fieldId
+                ));
+            }
+        } catch (PrestaShopException $e) {
+            throw new CustomizationFieldException(
+                sprintf('Error occurred when trying to get customization field #%d', $fieldId),
+                0,
+                $e
+            );
+        }
+
+        return $field;
+    }
+}
