43b99c7ff13d2c72d0f95b687977fded2c414440
matks <mathieu.ferment@prestashop.com>
2020-07-03 14:04:33 +0200

    Update version number to 1.7.6.7

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -31,11 +31,11 @@ use Symfony\Component\HttpKernel\Kernel;
 
 class AppKernel extends Kernel
 {
-    const VERSION = '1.7.6.6';
+    const VERSION = '1.7.6.7';
     const MAJOR_VERSION_STRING = '1.7';
     const MAJOR_VERSION = 17;
     const MINOR_VERSION = 6;
-    const RELEASE_VERSION = 6;
+    const RELEASE_VERSION = 7;
 
     /**
      * @{inheritdoc}
