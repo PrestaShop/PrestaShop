7c1347904b48aee27568ab13740df6a8fb87509b
matthieu rolland <matthieu.rolland@prestashop.com>
2020-07-08 15:48:16 +0200

    add new color field in ps_shop table

--- install-dev/upgrade/sql/1.7.8.0.sql
+++ install-dev/upgrade/sql/1.7.8.0.sql
@@ -11,3 +11,5 @@ INSERT IGNORE INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `po
   (NULL, 'actionModuleUninstallAfter', 'Module uninstall after', 'This hook is called at the end of module uninstall process', '1'),
   (NULL, 'actionPresentProductListing', 'Product Listing Presenter', 'This hook is called before a product listing is presented', '1')
 ;
+
+ALTER TABLE `PREFIX_shop` ADD `color` VARCHAR(50) NULL DEFAULT NULL AFTER `name`;
