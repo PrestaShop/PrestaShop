7859743d3cb79c40fe93f8e76037a565b88619de
Julius Zukauskas <zuk3975@gmail.com>
2020-07-08 16:58:45 +0300

    fill product with command values

--- src/Adapter/Product/CommandHandler/UpdateProductShippingHandler.php
+++ src/Adapter/Product/CommandHandler/UpdateProductShippingHandler.php
@@ -28,19 +28,75 @@ declare(strict_types=1);
 
 namespace PrestaShop\PrestaShop\Adapter\Product\CommandHandler;
 
+use PrestaShop\PrestaShop\Adapter\Product\AbstractProductHandler;
 use PrestaShop\PrestaShop\Core\Domain\Product\Command\UpdateProductShippingCommand;
 use PrestaShop\PrestaShop\Core\Domain\Product\CommandHandler\UpdateProductShippingHandlerInterface;
+use Product;
 
 /**
  * Handles @var UpdateProductShippingCommand using legacy object model
  */
-final class UpdateProductShippingHandler implements UpdateProductShippingHandlerInterface
+final class UpdateProductShippingHandler extends AbstractProductHandler implements UpdateProductShippingHandlerInterface
 {
     /**
      * {@inheritdoc}
      */
     public function handle(UpdateProductShippingCommand $command): void
     {
-        // TODO: Implement handle() method.
+        $product = $this->getProduct($command->getProductId());
+        $this->fillUpdatableFieldsWithCommandData($product, $command);
+
+        //@todo: performUpdate after rebase.
+    }
+
+    /**
+     * @param Product $product
+     * @param UpdateProductShippingCommand $command
+     */
+    private function fillUpdatableFieldsWithCommandData(Product $product, UpdateProductShippingCommand $command): void
+    {
+        if (null !== $command->getWidth()) {
+            $product->width = (string) $command->getWidth();
+            $this->fieldsToUpdate['width'] = true;
+        }
+
+        if (null !== $command->getHeight()) {
+            $product->height = (string) $command->getHeight();
+            $this->fieldsToUpdate['height'] = true;
+        }
+
+        if (null !== $command->getDepth()) {
+            $product->depth = (string) $command->getDepth();
+            $this->fieldsToUpdate['depth'] = true;
+        }
+
+        if (null !== $command->getWeight()) {
+            $product->weight = (string) $command->getWeight();
+            $this->fieldsToUpdate['weight'] = true;
+        }
+
+        if (null !== $command->getAdditionalShippingCost()) {
+            $product->additional_shipping_cost = (string) $command->getAdditionalShippingCost();
+            $this->fieldsToUpdate['additional_shipping_cost'] = true;
+        }
+
+        if (null !== $command->getCarrierReferences()) {
+            $product->setCarriers($command->getCarrierReferences());
+        }
+
+        if (null !== $command->getDeliveryTimeNotesType()) {
+            $product->additional_delivery_times = $command->getDeliveryTimeNotesType()->getValue();
+            $this->fieldsToUpdate['additional_delivery_times'] = true;
+        }
+
+        if (null !== $command->getLocalizedDeliveryTimeInStockNotes()) {
+            $product->delivery_in_stock = $command->getLocalizedDeliveryTimeInStockNotes();
+            $this->fieldsToUpdate['delivery_in_stock'] = true;
+        }
+
+        if (null !== $command->getLocalizedDeliveryTimeOutOfStockNotes()) {
+            $product->delivery_out_stock = $command->getLocalizedDeliveryTimeOutOfStockNotes();
+            $this->fieldsToUpdate['delivery_out_stock'] = true;
+        }
     }
 }
