8434f97a976a8302c707746291909c82564517d1
Jonathan Lelievre <jo.lelievre@gmail.com>
2020-07-09 15:20:50 +0200

    Fix session tables charset

--- install-dev/data/db_structure.sql
+++ install-dev/data/db_structure.sql
@@ -2847,11 +2847,11 @@ CREATE TABLE `PREFIX_employee_session` (
   `id_employee` int(10) unsigned DEFAULT NULL,
   `token` varchar(40) DEFAULT NULL,
   PRIMARY KEY `id_employee_session` (`id_employee_session`)
-) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8 COLLATION;
+) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8mb4 COLLATION;
 
 CREATE TABLE `PREFIX_customer_session` (
   `id_customer_session` int(11) unsigned NOT NULL auto_increment,
   `id_customer` int(10) unsigned DEFAULT NULL,
   `token` varchar(40) DEFAULT NULL,
   PRIMARY KEY `id_customer_session` (`id_customer_session`)
-) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8 COLLATION;
+) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8mb4 COLLATION;
