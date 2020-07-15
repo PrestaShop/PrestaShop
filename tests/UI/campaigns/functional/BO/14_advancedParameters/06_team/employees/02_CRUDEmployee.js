27a76a32b7da525bc0801301ca727ea62c70313f
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-07-15 14:10:39 +0200

    Add page to parameters for functional employee tests

--- tests/UI/campaigns/functional/BO/14_advancedParameters/06_team/employees/02_CRUDEmployee.js
+++ tests/UI/campaigns/functional/BO/14_advancedParameters/06_team/employees/02_CRUDEmployee.js
@@ -10,13 +10,12 @@ const loginCommon = require('@commonTests/loginBO');
 const EmployeeFaker = require('@data/faker/employee');
 
 // Import pages
-const LoginPage = require('@pages/BO/login/index');
-const DashboardPage = require('@pages/BO/dashboard/index');
-const EmployeesPage = require('@pages/BO/advancedParameters/team/index');
-const AddEmployeePage = require('@pages/BO/advancedParameters/team/add');
-const ProductsPage = require('@pages/BO/catalog/products/index');
-const OrdersPage = require('@pages/BO/orders/index');
-const FOBasePage = require('@pages/FO/FObasePage');
+const loginPage = require('@pages/BO/login/index');
+const dashboardPage = require('@pages/BO/dashboard/index');
+const employeesPage = require('@pages/BO/advancedParameters/team/index');
+const addEmployeePage = require('@pages/BO/advancedParameters/team/add');
+const productsPage = require('@pages/BO/catalog/products/index');
+const ordersPage = require('@pages/BO/orders/index');
 
 // Import test context
 const testContext = require('@utils/testContext');
@@ -47,54 +46,41 @@ const secondEditEmployeeData = new EmployeeFaker({
   active: false,
 });
 
-// Init objects needed
-const init = async function () {
-  return {
-    loginPage: new LoginPage(page),
-    dashboardPage: new DashboardPage(page),
-    employeesPage: new EmployeesPage(page),
-    addEmployeePage: new AddEmployeePage(page),
-    productsPage: new ProductsPage(page),
-    ordersPage: new OrdersPage(page),
-    foBasePage: new FOBasePage(page),
-  };
-};
-
 // Create, Read, Update and Delete Employee in BO
 describe('Create, Read, Update and Delete Employee in BO', async () => {
   // before and after functions
   before(async function () {
     browserContext = await helper.createBrowserContext(this.browser);
     page = await helper.newTab(browserContext);
-
-    // Init page objects
-    this.pageObjects = await init();
   });
 
   after(async () => {
     await helper.closeBrowserContext(browserContext);
   });
 
-  // Login from BO and go to "Advanced parameters>Team" page
-  loginCommon.loginBO();
+  it('should login in BO', async function () {
+    await loginCommon.loginBO(this, page);
+  });
 
   it('should go to "Advanced parameters>Team" page', async function () {
     await testContext.addContextItem(this, 'testIdentifier', 'goToAdvancedParamsPage', baseContext);
 
-    await this.pageObjects.dashboardPage.goToSubMenu(
-      this.pageObjects.dashboardPage.advancedParametersLink,
-      this.pageObjects.dashboardPage.teamLink,
+    await dashboardPage.goToSubMenu(
+      page,
+      dashboardPage.advancedParametersLink,
+      dashboardPage.teamLink,
     );
 
-    await this.pageObjects.employeesPage.closeSfToolBar();
+    await employeesPage.closeSfToolBar(page);
 
-    const pageTitle = await this.pageObjects.employeesPage.getPageTitle();
-    await expect(pageTitle).to.contains(this.pageObjects.employeesPage.pageTitle);
+    const pageTitle = await employeesPage.getPageTitle(page);
+    await expect(pageTitle).to.contains(employeesPage.pageTitle);
   });
 
   it('should reset all filters and get number of employees', async function () {
     await testContext.addContextItem(this, 'testIdentifier', 'resetFilterFirst', baseContext);
-    numberOfEmployees = await this.pageObjects.employeesPage.resetAndGetNumberOfLines();
+
+    numberOfEmployees = await employeesPage.resetAndGetNumberOfLines(page);
     await expect(numberOfEmployees).to.be.above(0);
   });
 
@@ -103,202 +89,217 @@ describe('Create, Read, Update and Delete Employee in BO', async () => {
     it('should go to add new employee page', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'goToNewEmployeePage', baseContext);
 
-      await this.pageObjects.employeesPage.goToAddNewEmployeePage();
-      const pageTitle = await this.pageObjects.addEmployeePage.getPageTitle();
-      await expect(pageTitle).to.contains(this.pageObjects.addEmployeePage.pageTitleCreate);
+      await employeesPage.goToAddNewEmployeePage(page);
+      const pageTitle = await addEmployeePage.getPageTitle(page);
+      await expect(pageTitle).to.contains(addEmployeePage.pageTitleCreate);
     });
 
     it('should create employee and check result', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'createEmployee', baseContext);
 
-      const textResult = await this.pageObjects.addEmployeePage.createEditEmployee(createEmployeeData);
-      await expect(textResult).to.equal(this.pageObjects.employeesPage.successfulCreationMessage);
+      const textResult = await addEmployeePage.createEditEmployee(page, createEmployeeData);
+      await expect(textResult).to.equal(employeesPage.successfulCreationMessage);
 
-      const numberOfEmployeesAfterCreation = await this.pageObjects.employeesPage.getNumberOfElementInGrid();
+      const numberOfEmployeesAfterCreation = await employeesPage.getNumberOfElementInGrid(page);
       await expect(numberOfEmployeesAfterCreation).to.be.equal(numberOfEmployees + 1);
     });
 
-    // Logout into BO
-    loginCommon.logoutBO();
+    it('should logout from BO', async function () {
+      await loginCommon.logoutBO(this, page);
+    });
 
     it('should sign in with new account and verify the default page', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'signInWithCreatedEmployee', baseContext);
 
-      await this.pageObjects.loginPage.login(createEmployeeData.email, createEmployeeData.password);
-      const pageTitle = await this.pageObjects.productsPage.getPageTitle();
-      await expect(pageTitle).to.contains(this.pageObjects.productsPage.pageTitle);
+      await loginPage.login(page, createEmployeeData.email, createEmployeeData.password);
+      const pageTitle = await productsPage.getPageTitle(page);
+      await expect(pageTitle).to.contains(productsPage.pageTitle);
     });
 
-    // Logout into BO
-    loginCommon.logoutBO();
+    it('should logout from BO', async function () {
+      await loginCommon.logoutBO(this, page);
+    });
   });
 
   // 2 : Update employee and check that employee can't sign in in BO (enabled = false)
   describe('Update the employee created', async () => {
-    // Login into BO
-    loginCommon.loginBO();
+    it('should login in BO', async function () {
+      await loginCommon.loginBO(this, page);
+    });
 
     describe('Update the password and the default page', async () => {
       it('should go to Employees page', async function () {
         await testContext.addContextItem(this, 'testIdentifier', 'goToEmployeePageForUpdate', baseContext);
 
-        await this.pageObjects.dashboardPage.goToSubMenu(
-          this.pageObjects.dashboardPage.advancedParametersLink,
-          this.pageObjects.dashboardPage.teamLink,
+        await dashboardPage.goToSubMenu(
+          page,
+          dashboardPage.advancedParametersLink,
+          dashboardPage.teamLink,
         );
 
-        const pageTitle = await this.pageObjects.employeesPage.getPageTitle();
-        await expect(pageTitle).to.contains(this.pageObjects.employeesPage.pageTitle);
+        const pageTitle = await employeesPage.getPageTitle(page);
+        await expect(pageTitle).to.contains(employeesPage.pageTitle);
       });
 
       it('should filter list by email', async function () {
         await testContext.addContextItem(this, 'testIdentifier', 'filterForUpdate', baseContext);
 
-        await this.pageObjects.employeesPage.filterEmployees(
+        await employeesPage.filterEmployees(
+          page,
           'input',
           'email',
           createEmployeeData.email,
         );
 
-        const textEmail = await this.pageObjects.employeesPage.getTextColumnFromTable(1, 'email');
+        const textEmail = await employeesPage.getTextColumnFromTable(page, 1, 'email');
         await expect(textEmail).to.contains(createEmployeeData.email);
       });
 
       it('should go to edit employee page', async function () {
         await testContext.addContextItem(this, 'testIdentifier', 'goToEditEmployeePage', baseContext);
 
-        await this.pageObjects.employeesPage.goToEditEmployeePage(1);
-        const pageTitle = await this.pageObjects.addEmployeePage.getPageTitle();
-        await expect(pageTitle).to.contains(this.pageObjects.addEmployeePage.pageTitleEdit);
+        await employeesPage.goToEditEmployeePage(page, 1);
+        const pageTitle = await addEmployeePage.getPageTitle(page);
+        await expect(pageTitle).to.contains(addEmployeePage.pageTitleEdit);
       });
 
       it('should update the employee account', async function () {
         await testContext.addContextItem(this, 'testIdentifier', 'updateEmployee', baseContext);
 
-        const textResult = await this.pageObjects.addEmployeePage.createEditEmployee(firstEditEmployeeData);
-        await expect(textResult).to.equal(this.pageObjects.addEmployeePage.successfulUpdateMessage);
+        const textResult = await addEmployeePage.createEditEmployee(page, firstEditEmployeeData);
+        await expect(textResult).to.equal(addEmployeePage.successfulUpdateMessage);
       });
 
       it('should click on cancel and verify the new employee\'s number', async function () {
         await testContext.addContextItem(this, 'testIdentifier', 'verifyNumberOfEmployeeAfterUpdate', baseContext);
 
-        await this.pageObjects.addEmployeePage.cancel();
+        await addEmployeePage.cancel(page);
 
-        const numberOfEmployeesAfterUpdate = await this.pageObjects.employeesPage.resetAndGetNumberOfLines();
+        const numberOfEmployeesAfterUpdate = await employeesPage.resetAndGetNumberOfLines(page);
         await expect(numberOfEmployeesAfterUpdate).to.be.equal(numberOfEmployees + 1);
       });
 
-      // Logout into BO
-      loginCommon.logoutBO();
+      it('should logout from BO', async function () {
+        await loginCommon.logoutBO(this, page);
+      });
 
       it('should sign in with edited account and verify the default page', async function () {
         await testContext.addContextItem(this, 'testIdentifier', 'signInWithUpdatedEmployee', baseContext);
-        await this.pageObjects.loginPage.login(firstEditEmployeeData.email, firstEditEmployeeData.password);
-        const pageTitle = await this.pageObjects.ordersPage.getPageTitle();
-        await expect(pageTitle).to.contains(this.pageObjects.ordersPage.pageTitle);
+
+        await loginPage.login(page, firstEditEmployeeData.email, firstEditEmployeeData.password);
+        const pageTitle = await ordersPage.getPageTitle(page);
+        await expect(pageTitle).to.contains(ordersPage.pageTitle);
       });
 
-      // Logout into BO
-      loginCommon.logoutBO();
+      it('should logout from BO', async function () {
+        await loginCommon.logoutBO(this, page);
+      });
     });
     describe('Disable the employee and check it', async () => {
-      // Login into BO
-      loginCommon.loginBO();
+      it('should login in BO', async function () {
+        await loginCommon.loginBO(this, page);
+      });
 
       it('should go to Employees page', async function () {
         await testContext.addContextItem(this, 'testIdentifier', 'goToEmployeesPageToDisable', baseContext);
 
-        await this.pageObjects.dashboardPage.goToSubMenu(
-          this.pageObjects.dashboardPage.advancedParametersLink,
-          this.pageObjects.dashboardPage.teamLink,
+        await dashboardPage.goToSubMenu(
+          page,
+          dashboardPage.advancedParametersLink,
+          dashboardPage.teamLink,
         );
 
-        const pageTitle = await this.pageObjects.employeesPage.getPageTitle();
-        await expect(pageTitle).to.contains(this.pageObjects.employeesPage.pageTitle);
+        const pageTitle = await employeesPage.getPageTitle(page);
+        await expect(pageTitle).to.contains(employeesPage.pageTitle);
       });
 
       it('should filter list by email', async function () {
         await testContext.addContextItem(this, 'testIdentifier', 'filterEmployeesToDisable', baseContext);
 
-        await this.pageObjects.employeesPage.filterEmployees(
+        await employeesPage.filterEmployees(
+          page,
           'input',
           'email',
           firstEditEmployeeData.email,
         );
 
-        const textEmail = await this.pageObjects.employeesPage.getTextColumnFromTable(1, 'email');
+        const textEmail = await employeesPage.getTextColumnFromTable(page, 1, 'email');
         await expect(textEmail).to.contains(firstEditEmployeeData.email);
       });
 
       it('should go to edit employee page', async function () {
         await testContext.addContextItem(this, 'testIdentifier', 'goToEditEmployeePageToDisable', baseContext);
 
-        await this.pageObjects.employeesPage.goToEditEmployeePage(1);
-        const pageTitle = await this.pageObjects.addEmployeePage.getPageTitle();
-        await expect(pageTitle).to.contains(this.pageObjects.addEmployeePage.pageTitleEdit);
+        await employeesPage.goToEditEmployeePage(page, 1);
+        const pageTitle = await addEmployeePage.getPageTitle(page);
+        await expect(pageTitle).to.contains(addEmployeePage.pageTitleEdit);
       });
 
       it('should disable the employee account', async function () {
         await testContext.addContextItem(this, 'testIdentifier', 'disableEmployee', baseContext);
 
-        const textResult = await this.pageObjects.addEmployeePage.createEditEmployee(secondEditEmployeeData);
-        await expect(textResult).to.equal(this.pageObjects.addEmployeePage.successfulUpdateMessage);
+        const textResult = await addEmployeePage.createEditEmployee(page, secondEditEmployeeData);
+        await expect(textResult).to.equal(addEmployeePage.successfulUpdateMessage);
       });
 
-      // Logout into BO
-      loginCommon.logoutBO();
+      it('should logout from BO', async function () {
+        await loginCommon.logoutBO(this, page);
+      });
 
       it('should test sign in with the disabled employee', async function () {
         await testContext.addContextItem(this, 'testIdentifier', 'signInWithDisabledEmployee', baseContext);
 
-        await this.pageObjects.loginPage.login(secondEditEmployeeData.email, secondEditEmployeeData.password, false);
-        const loginError = await this.pageObjects.loginPage.getLoginError();
-        await expect(loginError).to.contains(this.pageObjects.loginPage.loginErrorText);
+        await loginPage.login(page, secondEditEmployeeData.email, secondEditEmployeeData.password, false);
+        const loginError = await loginPage.getLoginError(page);
+        await expect(loginError).to.contains(loginPage.loginErrorText);
       });
     });
   });
 
   // 5 : Delete employee
   describe('Delete employee', async () => {
-    // Login into BO
-    loginCommon.loginBO();
+    it('should login in BO', async function () {
+      await loginCommon.loginBO(this, page);
+    });
 
     it('should go to Employees page', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'goToEmployeesPageToDelete', baseContext);
 
-      await this.pageObjects.dashboardPage.goToSubMenu(
-        this.pageObjects.dashboardPage.advancedParametersLink,
-        this.pageObjects.dashboardPage.teamLink,
+      await dashboardPage.goToSubMenu(
+        page,
+        dashboardPage.advancedParametersLink,
+        dashboardPage.teamLink,
       );
 
-      const pageTitle = await this.pageObjects.employeesPage.getPageTitle();
-      await expect(pageTitle).to.contains(this.pageObjects.employeesPage.pageTitle);
+      const pageTitle = await employeesPage.getPageTitle(page);
+      await expect(pageTitle).to.contains(employeesPage.pageTitle);
     });
 
     it('should filter list by email', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'filterEmployeesToDelete', baseContext);
 
-      await this.pageObjects.employeesPage.filterEmployees(
+      await employeesPage.filterEmployees(
+        page,
         'input',
         'email',
         secondEditEmployeeData.email,
       );
 
-      const textEmail = await this.pageObjects.employeesPage.getTextColumnFromTable(1, 'email');
+      const textEmail = await employeesPage.getTextColumnFromTable(page, 1, 'email');
       await expect(textEmail).to.contains(secondEditEmployeeData.email);
     });
 
     it('should delete employee', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'deleteEmployee', baseContext);
 
-      const textResult = await this.pageObjects.employeesPage.deleteEmployee(1);
-      await expect(textResult).to.equal(this.pageObjects.employeesPage.successfulDeleteMessage);
+      const textResult = await employeesPage.deleteEmployee(page, 1);
+      await expect(textResult).to.equal(employeesPage.successfulDeleteMessage);
     });
 
     it('should reset filter and check the number of employees', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'resetAfterDelete', baseContext);
 
-      const numberOfEmployeesAfterDelete = await this.pageObjects.employeesPage.resetAndGetNumberOfLines();
+      const numberOfEmployeesAfterDelete = await employeesPage.resetAndGetNumberOfLines(page);
       await expect(numberOfEmployeesAfterDelete).to.be.equal(numberOfEmployees);
     });
   });
