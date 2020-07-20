41fd0bdbc5c4eb03c26f9fb2ab1406af3ea51e38
Pierre RAMBAUD <pierre.rambaud@prestashop.com>
2020-07-20 11:07:34 +0200

    Remove useless ini_set

--- classes/Windows.php
+++ classes/Windows.php
@@ -32,7 +32,5 @@ class WindowsCore
 {
     public static function improveFilesytemPerformances()
     {
-        ini_set('realpath_cache_size', '1M');
-        ini_set('realpath_cache_ttl', '300');
     }
 }
