08632865e99b5e0386209b9cdaae34809a687e21
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-07-23 13:20:22 +0200

    Log in before to begin BO scenario

--- tests/UI/campaigns/functional/BO/10_payment/02_preferences/04_carrierRestrictions.js
+++ tests/UI/campaigns/functional/BO/10_payment/02_preferences/04_carrierRestrictions.js
@@ -15,6 +15,7 @@ const productPage = require('@pages/FO/product');
 const homePage = require('@pages/FO/home');
 const cartPage = require('@pages/FO/cart');
 const checkoutPage = require('@pages/FO/checkout');
+const foLoginPage = require('@pages/FO/login');
 
 // Import data
 const {DefaultAccount} = require('@data/demo/customer');
@@ -22,7 +23,7 @@ const {DefaultAccount} = require('@data/demo/customer');
 let browserContext;
 let page;
 
-describe('Configure carrier restrictions', async () => {
+describe('Configure carrier restrictions and check FO', async () => {
   // before and after functions
   before(async function () {
     browserContext = await helper.createBrowserContext(this.browser);
@@ -33,121 +34,139 @@ describe('Configure carrier restrictions', async () => {
     await helper.closeBrowserContext(browserContext);
   });
 
-  it('should login in BO', async function () {
-    await loginCommon.loginBO(this, page);
-  });
+  describe('Login into FO', async () => {
+    it('should go to FO page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToFO', baseContext);
 
-  it('should go to \'Payment > Preferences\' page', async function () {
-    await testContext.addContextItem(this, 'testIdentifier', 'goToPreferencesPage', baseContext);
-
-    await dashboardPage.goToSubMenu(
-      page,
-      dashboardPage.paymentParentLink,
-      dashboardPage.preferencesLink,
-    );
-
-    await preferencesPage.closeSfToolBar(page);
-
-    const pageTitle = await preferencesPage.getPageTitle(page);
-    await expect(pageTitle).to.contains(preferencesPage.pageTitle);
-  });
-
-  const tests = [
-    {args: {action: 'uncheck', paymentModule: 'ps_wirepayment', exist: false}},
-    {args: {action: 'check', paymentModule: 'ps_wirepayment', exist: true}},
-    {args: {action: 'uncheck', paymentModule: 'ps_checkpayment', exist: false}},
-    {args: {action: 'check', paymentModule: 'ps_checkpayment', exist: true}},
-  ];
-
-  tests.forEach((test, index) => {
-    it(`should ${test.args.action} free prestashop carrier for '${test.args.paymentModule}'`, async function () {
-      await testContext.addContextItem(this, 'testIdentifier', test.args.action + test.args.paymentModule, baseContext);
-
-      const result = await preferencesPage.setCarrierRestriction(
-        page,
-        0,
-        test.args.paymentModule,
-        test.args.exist,
-      );
-
-      await expect(result).to.contains(preferencesPage.successfulUpdateMessage);
-    });
-
-    it('should go to FO and add the first product to the cart', async function () {
-      await testContext.addContextItem(
-        this,
-        'testIdentifier',
-        `check_${test.args.paymentModule}_${test.args.exist}`,
-        baseContext,
-      );
-
-      // Click on view my shop
-      page = await preferencesPage.viewMyShop(page);
-
-      // Change language in FO
+      await homePage.goToFo(page);
       await homePage.changeLanguage(page, 'en');
 
-      // Go to the first product page
-      await homePage.goToProductPage(page, 1);
-
-      // Add the product to the cart
-      await productPage.addProductToTheCart(page);
-
-      // Proceed to checkout the shopping cart
-      await cartPage.clickOnProceedToCheckout(page);
-
-      const isCheckoutPage = await checkoutPage.isCheckoutPage(page);
-      await expect(isCheckoutPage).to.be.true;
+      const isHomePage = await homePage.isHomePage(page);
+      await expect(isHomePage, 'Fail to open FO home page').to.be.true;
     });
 
-    // Personal information step - Login
-    it('should login and go to address step', async function () {
-      await testContext.addContextItem(
-        this,
-        'testIdentifier',
-        `loginToFO${index}`,
-        baseContext,
-      );
+    it('should go to login page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToLoginPageFO', baseContext);
 
-      if (index === 0) {
-        // Personal information step - Login
-        await checkoutPage.clickOnSignIn(page);
-        const isStepLoginComplete = await checkoutPage.customerLogin(page, DefaultAccount);
-        await expect(isStepLoginComplete, 'Step Personal information is not complete').to.be.true;
-      }
+      await homePage.goToLoginPage(page);
+      const pageTitle = await foLoginPage.getPageTitle(page);
+      await expect(pageTitle, 'Fail to open FO login page').to.contains(foLoginPage.pageTitle);
     });
 
-    it('should continue to delivery step', async function () {
-      await testContext.addContextItem(
-        this,
-        'testIdentifier',
-        `goToDeliveryStep${index}`,
-        baseContext,
-      );
+    it('should sign in with default customer', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'sighInFO', baseContext);
 
-      // Address step - Go to delivery step
-      const isStepAddressComplete = await checkoutPage.goToDeliveryStep(page);
-      await expect(isStepAddressComplete, 'Step Address is not complete').to.be.true;
+      await foLoginPage.customerLogin(page, DefaultAccount);
+      const isCustomerConnected = await foLoginPage.isCustomerConnected(page);
+      await expect(isCustomerConnected, 'Customer is not connected').to.be.true;
+    });
+  });
+
+  describe('Configure carrier restrictions', async () => {
+    it('should login in BO', async function () {
+      await loginCommon.loginBO(this, page);
     });
 
-    it('should continue to payment step and check the existence of payment method', async function () {
-      await testContext.addContextItem(
-        this,
-        'testIdentifier',
-        `goToPaymentStep${index}`,
-        baseContext,
+    it('should go to \'Payment > Preferences\' page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToPreferencesPage', baseContext);
+
+      await dashboardPage.goToSubMenu(
+        page,
+        dashboardPage.paymentParentLink,
+        dashboardPage.preferencesLink,
       );
 
-      // Delivery step - Go to payment step
-      const isStepDeliveryComplete = await checkoutPage.goToPaymentStep(page);
-      await expect(isStepDeliveryComplete, 'Step Address is not complete').to.be.true;
+      await preferencesPage.closeSfToolBar(page);
 
-      // Payment step - Check payment method
-      const isVisible = await checkoutPage.isPaymentMethodExist(page, test.args.paymentModule);
-      await expect(isVisible).to.be.equal(test.args.exist);
+      const pageTitle = await preferencesPage.getPageTitle(page);
+      await expect(pageTitle).to.contains(preferencesPage.pageTitle);
+    });
 
-      // Go back to BO
-      page = await checkoutPage.closePage(browserContext, page, 0);
+    const tests = [
+      {args: {action: 'uncheck', paymentModule: 'ps_wirepayment', exist: false}},
+      {args: {action: 'check', paymentModule: 'ps_wirepayment', exist: true}},
+      {args: {action: 'uncheck', paymentModule: 'ps_checkpayment', exist: false}},
+      {args: {action: 'check', paymentModule: 'ps_checkpayment', exist: true}},
+    ];
+
+    tests.forEach((test, index) => {
+      it(`should ${test.args.action} free prestashop carrier for '${test.args.paymentModule}'`, async function () {
+        await testContext.addContextItem(
+          this,
+          'testIdentifier',
+          test.args.action + test.args.paymentModule,
+          baseContext,
+        );
+
+        const result = await preferencesPage.setCarrierRestriction(
+          page,
+          0,
+          test.args.paymentModule,
+          test.args.exist,
+        );
+
+        await expect(result).to.contains(preferencesPage.successfulUpdateMessage);
+      });
+
+      it('should go to FO and add the first product to the cart', async function () {
+        await testContext.addContextItem(
+          this,
+          'testIdentifier',
+          `check_${test.args.paymentModule}_${test.args.exist}`,
+          baseContext,
+        );
+
+        // Click on view my shop
+        page = await preferencesPage.viewMyShop(page);
+
+        // Change language in FO
+        await homePage.changeLanguage(page, 'en');
+
+        // Go to the first product page
+        await homePage.goToProductPage(page, 1);
+
+        // Add the product to the cart
+        await productPage.addProductToTheCart(page);
+
+        // Proceed to checkout the shopping cart
+        await cartPage.clickOnProceedToCheckout(page);
+
+        const isCheckoutPage = await checkoutPage.isCheckoutPage(page);
+        await expect(isCheckoutPage).to.be.true;
+      });
+
+      it('should continue to delivery step', async function () {
+        await testContext.addContextItem(
+          this,
+          'testIdentifier',
+          `goToDeliveryStep${index}`,
+          baseContext,
+        );
+
+        // Address step - Go to delivery step
+        const isStepAddressComplete = await checkoutPage.goToDeliveryStep(page);
+        await expect(isStepAddressComplete, 'Step Address is not complete').to.be.true;
+      });
+
+      it('should continue to payment step and check the existence of payment method', async function () {
+        await testContext.addContextItem(
+          this,
+          'testIdentifier',
+          `goToPaymentStep${index}`,
+          baseContext,
+        );
+
+        // Delivery step - Go to payment step
+        const isStepDeliveryComplete = await checkoutPage.goToPaymentStep(page);
+        await expect(isStepDeliveryComplete, 'Step Address is not complete').to.be.true;
+
+        // Payment step - Check payment method
+        const isVisible = await checkoutPage.isPaymentMethodExist(page, test.args.paymentModule);
+        await expect(isVisible).to.be.equal(test.args.exist);
+
+        // Go back to BO
+        page = await checkoutPage.closePage(browserContext, page, 0);
+      });
     });
   });
 });
