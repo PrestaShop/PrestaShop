c4682cf4848f6ee2d9c75ca390bb85b6fb655513
Karlis S <karlis@suvi.pri.ee>
2020-07-23 11:21:36 +0000

    Move outstanding files declare statement to very top of file

--- src/Core/Grid/Data/Factory/OutstandingGridDataFactory.php
+++ src/Core/Grid/Data/Factory/OutstandingGridDataFactory.php
@@ -24,9 +24,9 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  */
 
-namespace PrestaShop\PrestaShop\Core\Grid\Data\Factory;
-
 declare(strict_types=1);
+
+namespace PrestaShop\PrestaShop\Core\Grid\Data\Factory;
 
 use Customer;
 use PrestaShop\PrestaShop\Core\Grid\Data\GridData;
