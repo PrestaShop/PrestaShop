147e5fad6bbf3bae53627d065aba4c6243f27ded
matthieu rolland <matthieu.rolland@prestashop.com>
2020-07-28 10:54:30 +0200

    cs fixer

--- src/Adapter/Order/OrderAmountUpdater.php
+++ src/Adapter/Order/OrderAmountUpdater.php
@@ -350,6 +350,7 @@ class OrderAmountUpdater
      *
      * @param Order $order
      * @param int $precision
+     *
      * @throws PrestaShopDatabaseException
      * @throws PrestaShopException
      */
