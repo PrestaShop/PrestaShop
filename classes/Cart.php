ce3f7191a7a2b6805c57939d58dbfa551d3aed0c
Dmitry <dariusakafest@mail.ru>
2020-08-07 12:13:37 +0200

    Bug when cart empty

--- classes/Cart.php
+++ classes/Cart.php
@@ -3704,7 +3704,7 @@ class CartCore extends ObjectModel
             $this->updateProductWeight($this->id);
         }
 
-        return self::$_totalWeight[(int) $this->id];
+        return (isset(self::$_totalWeight[$this->id]) ? self::$_totalWeight[$this->id] : 0);
     }
 
     /**
