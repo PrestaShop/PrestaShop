b4a47e41d03efb4b62e6f2b3950dc8dd2a290d96
PululuK <pululuandre@hotmail.com>
2020-08-07 13:02:18 +0200

    In FO Product page, use ecotax excl. tax instead of tax incl.

--- controllers/front/ProductController.php
+++ controllers/front/ProductController.php
@@ -1027,7 +1027,8 @@ class ProductControllerCore extends ProductPresentingFrontControllerCore
         $product['minimal_quantity'] = $this->getProductMinimalQuantity($product);
         $product['quantity_wanted'] = $this->getRequiredQuantity($product);
         $product['extraContent'] = $extraContentFinder->addParams(['product' => $this->product])->present();
-        $product['ecotax'] = Tools::convertPrice((float) $product['ecotax'], $this->context->currency, true, $this->context);
+        $ecotax_rate = (float) Tax::getProductEcotaxRate();
+        $product['ecotax'] = Tools::ps_round((float) $product['ecotax'] * (1 + $ecotax_rate / 100), 6, null);
 
         $product_full = Product::getProductProperties($this->context->language->id, $product, $this->context);
 
