e83212710f472d71c8b02184d95ee6a0e404f710
Julius Zukauskas <zuk3975@gmail.com>
2020-08-11 14:29:01 +0300

    rename SetProductSuppliersCommand/Handler. Remove deletion ability from same command

--- tests/Integration/Behaviour/Features/Context/Domain/Product/UpdateProductSuppliersFeatureContext.php
+++ tests/Integration/Behaviour/Features/Context/Domain/Product/UpdateProductSuppliersFeatureContext.php
@@ -36,7 +36,7 @@ use PrestaShop\PrestaShop\Core\Domain\Product\Combination\ValueObject\Combinatio
 use PrestaShop\PrestaShop\Core\Domain\Product\Exception\CannotUpdateProductException;
 use PrestaShop\PrestaShop\Core\Domain\Product\Exception\ProductException;
 use PrestaShop\PrestaShop\Core\Domain\Product\QueryResult\ProductSupplierOptions;
-use PrestaShop\PrestaShop\Core\Domain\Product\Supplier\Command\UpdateProductSuppliersCommand;
+use PrestaShop\PrestaShop\Core\Domain\Product\Supplier\Command\SetProductSuppliersCommand;
 use PrestaShop\PrestaShop\Core\Domain\Product\Supplier\Exception\ProductSupplierException;
 use PrestaShop\PrestaShop\Core\Domain\Product\Supplier\Query\GetProductSupplierOptions;
 use PrestaShop\PrestaShop\Core\Domain\Product\Supplier\ValueObject\ProductSupplierId;
@@ -52,7 +52,7 @@ class UpdateProductSuppliersFeatureContext extends AbstractProductFeatureContext
     public function deleteAllProductSuppliers(string $productReference)
     {
         try {
-            $command = new UpdateProductSuppliersCommand($this->getSharedStorage()->get($productReference));
+            $command = new SetProductSuppliersCommand($this->getSharedStorage()->get($productReference));
             $command->setProductSuppliers([]);
             $this->getCommandBus()->handle($command);
         } catch (ProductException $e) {
@@ -96,7 +96,7 @@ class UpdateProductSuppliersFeatureContext extends AbstractProductFeatureContext
         }
 
         try {
-            $command = new UpdateProductSuppliersCommand($this->getSharedStorage()->get($productReference));
+            $command = new SetProductSuppliersCommand($this->getSharedStorage()->get($productReference));
             $command->setProductSuppliers($productSuppliers);
 
             $productSupplierIds = $this->getCommandBus()->handle($command);
@@ -127,7 +127,7 @@ class UpdateProductSuppliersFeatureContext extends AbstractProductFeatureContext
     public function updateProductDefaultSupplier(string $productReference, string $supplierReference)
     {
         try {
-            $command = new UpdateProductSuppliersCommand($this->getSharedStorage()->get($productReference));
+            $command = new SetProductSuppliersCommand($this->getSharedStorage()->get($productReference));
             $command->setDefaultSupplierId($this->getSharedStorage()->get($supplierReference));
 
             $this->getCommandBus()->handle($command);
