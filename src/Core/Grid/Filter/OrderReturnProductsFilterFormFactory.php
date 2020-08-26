1e145eab57db36712c62f298b557ba65c440eb57
Jevgenij Visockij <jevgenij.visockij@invertus.eu>
2020-08-26 15:15:56 +0300

    Changed all the merchandiseReturn in this PR to OrderReturn namespace

---
+++ src/Core/Grid/Filter/OrderReturnProductsFilterFormFactory.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * 2007-2020 PrestaShop SA and Contributors
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://www.prestashop.com for more information.
+ *
+ * @author    PrestaShop SA <contact@prestashop.com>
+ * @copyright 2007-2020 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * International Registered Trademark & Property of PrestaShop SA
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\PrestaShop\Core\Grid\Filter;
+
+use PrestaShop\PrestaShop\Core\Domain\OrderReturn\Exception\OrderReturnException;
+use PrestaShop\PrestaShop\Core\Grid\Definition\GridDefinitionInterface;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * Class CategoryFilterFormFactory decorates original filter factory to add custom submit action.
+ */
+final class OrderReturnProductsFilterFormFactory implements GridFilterFormFactoryInterface
+{
+    /**
+     * @var GridFilterFormFactoryInterface
+     */
+    private $formFactory;
+
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private $urlGenerator;
+
+    /**
+     * @var RequestStack
+     */
+    private $requestStack;
+
+    /**
+     * @param GridFilterFormFactoryInterface $formFactory
+     * @param UrlGeneratorInterface $urlGenerator
+     * @param RequestStack $requestStack
+     */
+    public function __construct(
+        GridFilterFormFactoryInterface $formFactory,
+        UrlGeneratorInterface $urlGenerator,
+        RequestStack $requestStack
+    ) {
+        $this->formFactory = $formFactory;
+        $this->urlGenerator = $urlGenerator;
+        $this->requestStack = $requestStack;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function create(GridDefinitionInterface $definition)
+    {
+        $orderReturnFilterForm = $this->formFactory->create($definition);
+
+        $newOrderReturnFormBuilder = $orderReturnFilterForm->getConfig()->getFormFactory()->createNamedBuilder(
+            $definition->getId(),
+            FormType::class
+        );
+
+        /* @var FormInterface $orderReturnFormItem */
+        foreach ($orderReturnFilterForm as $orderReturnFormItem) {
+            $newOrderReturnFormBuilder->add(
+                $orderReturnFormItem->getName(),
+                get_class($orderReturnFormItem->getConfig()->getType()->getInnerType()),
+                $orderReturnFormItem->getConfig()->getOptions()
+            );
+        }
+
+        $queryParams = [];
+
+        if (null === ($request = $this->requestStack->getCurrentRequest())
+            || !$request->attributes->has('orderReturnId')
+        ) {
+            throw new OrderReturnException('This page needs to have orderReturnId as a parameter');
+        }
+
+        $queryParams['orderReturnId'] = $request->attributes->get('orderReturnId');
+
+        $newOrderReturnFormBuilder->setAction(
+            $this->urlGenerator->generate('admin_order_returns_products_filter', $queryParams)
+        );
+
+        return $newOrderReturnFormBuilder->getForm();
+    }
+}
