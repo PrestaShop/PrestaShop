14e33f954be3b509d1dbe955ce2d5a7c2372a161
matthieu rolland <matthieu.rolland@prestashop.com>
2020-08-26 17:28:50 +0200

    mute jquery-migrate depreciation logs in BO when debug mode is on

--- classes/controller/AdminController.php
+++ classes/controller/AdminController.php
@@ -2660,6 +2660,7 @@ class AdminControllerCore extends Controller
 
             // add Jquery 3 and its migration script
             $this->addJs(_PS_JS_DIR_ . 'jquery/jquery-3.4.1.min.js');
+            $this->addJs(_PS_JS_DIR_ . 'jquery/bo-migrate-mute.min.js');
             $this->addJs(_PS_JS_DIR_ . 'jquery/jquery-migrate-3.1.0.min.js');
             // implement $.browser object and live method, that has been removed since jquery 1.9
             $this->addJs(_PS_JS_DIR_ . 'jquery/jquery.browser-0.1.0.min.js');
@@ -2729,6 +2730,8 @@ class AdminControllerCore extends Controller
                 'number_specifications' => $this->prepareNumberSpecifications($this->context),
             ]
         );
+
+        Media::addJsDef(['isDebugMode' => _PS_MODE_DEV_]);
 
         // Execute Hook AdminController SetMedia
         Hook::exec('actionAdminControllerSetMedia');
