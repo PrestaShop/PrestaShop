42efc84134694f82a656fab4fe04d47f63c3617b
ks129 <45097959+ks129@users.noreply.github.com>
2020-08-26 18:08:51 +0300

    Remove legacy controller from test and add survival test

--- tests-legacy/Integration/PrestaShopBundle/Controller/Admin/SurvivalTest.php
+++ tests-legacy/Integration/PrestaShopBundle/Controller/Admin/SurvivalTest.php
@@ -181,6 +181,7 @@ class SurvivalTest extends WebTestCase
             'admin_attachments_create' => ['Add new', 'admin_attachments_create'],
             'admin_orders_index' => ['Orders', 'admin_orders_index'],
             'admin_orders_create' => ['Add new', 'admin_orders_create'],
+            'admin_carriers_index' => ['Carriers', 'admin_carriers_index'],
         ];
     }
 
