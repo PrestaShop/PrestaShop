c14337e952677df9e8e99ce117b914eccdde9623
matthieu rolland <matthieu.rolland@prestashop.com>
2020-08-26 17:49:02 +0200

    lint

--- themes/_core/js/migrate-mute.js
+++ themes/_core/js/migrate-mute.js
@@ -25,7 +25,7 @@
 import jQuery from 'jquery';
 
 (function(){
-  if ( typeof jQuery.migrateMute === "undefined" ) {
+  if (typeof jQuery.migrateMute === "undefined") {
     jQuery.migrateMute = !window.prestashop.isDebugMode;
   }
 })();
