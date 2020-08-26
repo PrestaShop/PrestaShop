9fa585549d38387a244bf4d8b1b6cbe3afaeeed3
ks129 <45097959+ks129@users.noreply.github.com>
2020-08-26 16:49:25 +0300

    Add abstract carrier handler that contains carrier fetching function

---
+++ src/Adapter/Carrier/AbstractCarrierHandler.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\PrestaShop\Adapter\Carrier;
+
+use Carrier;
+use PrestaShop\PrestaShop\Core\Domain\Carrier\Exception\CarrierException;
+use PrestaShop\PrestaShop\Core\Domain\Carrier\Exception\CarrierNotFoundException;
+use PrestaShop\PrestaShop\Core\Domain\Carrier\ValueObject\CarrierId;
+use PrestaShopException;
+
+/**
+ * Provides reusable methods for carrier command/query handlers
+ */
+abstract class AbstractCarrierHandler
+{
+    protected function getCarrier(CarrierId $carrierId)
+    {
+        try {
+            $carrier = new Carrier($carrierId->getValue());
+        } catch (PrestaShopException $exception) {
+            throw new CarrierException('Failed to create new carrier', 0, $exception);
+        }
+
+        if ($carrier->id !== $carrierId->getValue()) {
+            throw new CarrierNotFoundException(sprintf('Carrier with id "%s" was not found', $carrierId->getValue()));
+        }
+
+        return $carrier;
+    }
+}
