51e2110ccf761896de15e4579652387388fc57b8
ks129 <45097959+ks129@users.noreply.github.com>
2020-08-26 17:12:13 +0300

    Create command, handler interface and handler for carrier deleting

--- src/Core/Domain/Carrier/CommandHandler/DeleteCarrierHandlerInterface.php
+++ src/Core/Domain/Carrier/CommandHandler/DeleteCarrierHandlerInterface.php
@@ -24,12 +24,19 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  */
 
-header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+declare(strict_types=1);
 
-header('Cache-Control: no-store, no-cache, must-revalidate');
-header('Cache-Control: post-check=0, pre-check=0', false);
-header('Pragma: no-cache');
+namespace PrestaShop\PrestaShop\Core\Domain\Carrier\CommandHandler;
 
-header('Location: ../../../../../../../../');
-exit;
+use PrestaShop\PrestaShop\Core\Domain\Carrier\Command\DeleteCarrierCommand;
+
+/**
+ * Defines contract for DeleteCarrierHandler
+ */
+interface DeleteCarrierHandlerInterface
+{
+    /**
+     * @param DeleteCarrierCommand $command
+     */
+    public function handle(DeleteCarrierCommand $command);
+}
