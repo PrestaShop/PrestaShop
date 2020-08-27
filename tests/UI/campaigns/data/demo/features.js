29e8e70de34455a689f52ecacf88ff04f0b865e9
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-08-27 12:04:15 +0200

    Add test 'Filter features'

---
+++ tests/UI/campaigns/data/demo/features.js
@@ -0,0 +1,16 @@
+module.exports = {
+  Features: {
+    composition: {
+      id: 1,
+      name: 'Composition',
+      values: {},
+      position: 1,
+    },
+    property: {
+      id: 2,
+      name: 'Property',
+      values: {},
+      position: 2,
+    },
+  },
+};
---
+++ tests/UI/pages/BO/catalog/features/index.js
@@ -0,0 +1,122 @@
+require('module-alias/register');
+const BOBasePage = require('@pages/BO/BObasePage');
+
+class Features extends BOBasePage {
+  constructor() {
+    super();
+
+    this.pageTitle = 'Features • ';
+
+    // Form selectors
+    this.gridForm = '#form-feature';
+    this.gridTableHeaderTitle = `${this.gridForm} .panel-heading`;
+    this.gridTableNumberOfTitlesSpan = `${this.gridTableHeaderTitle} span.badge`;
+
+    // Table selectors
+    this.gridTable = '#table-feature';
+
+    // Filter selectors
+    this.filterRow = `${this.gridTable} tr.filter`;
+    this.filterColumn = filterBy => `${this.filterRow} [name='featureFilter_${filterBy}']`;
+    this.filterSearchButton = '#submitFilterButtonfeature';
+    this.filterResetButton = 'button[name=\'submitResetfeature\']';
+
+    // Table body selectors
+    this.tableBody = `${this.gridTable} tbody`;
+    this.tableBodyRows = `${this.tableBody} tr`;
+    this.tableBodyRow = row => `${this.tableBodyRows}:nth-child(${row})`;
+    this.tableBodyColumn = row => `${this.tableBodyRow(row)} td`;
+
+    // Columns selectors
+    this.tableColumnSelectRowCheckbox = row => `${this.tableBodyColumn(row)} input[name='featureBox[]']`;
+    this.tableColumnId = row => `${this.tableBodyColumn(row)}:nth-child(2)`;
+    this.tableColumnName = row => `${this.tableBodyColumn(row)}:nth-child(3)`;
+    this.tableColumnValues = row => `${this.tableBodyColumn(row)}:nth-child(4)`;
+    this.tableColumnPosition = row => `${this.tableBodyColumn(row)}:nth-child(5)`;
+
+    // Row actions selectors
+    this.tableColumnActions = row => `${this.tableBodyColumn(row)} .btn-group-action`;
+    this.tableColumnActionsViewLink = row => `${this.tableColumnActions(row)} a[title='View']`;
+  }
+
+  /* Filter methods */
+  /**
+   * Reset all filters
+   * @param page
+   * @return {Promise<void>}
+   */
+  async resetFilter(page) {
+    if (!(await this.elementNotVisible(page, this.filterResetButton, 2000))) {
+      await this.clickAndWaitForNavigation(page, this.filterResetButton);
+    }
+    await this.waitForVisibleSelector(page, this.filterSearchButton, 2000);
+  }
+
+  /**
+   * Get Number of features
+   * @param page
+   * @return {Promise<number>}
+   */
+  getNumberOfElementInGrid(page) {
+    return this.getNumberFromText(page, this.gridTableNumberOfTitlesSpan);
+  }
+
+  /**
+   * Reset and get number of features
+   * @param page
+   * @return {Promise<number>}
+   */
+  async resetAndGetNumberOfLines(page) {
+    await this.resetFilter(page);
+    return this.getNumberOfElementInGrid(page);
+  }
+
+  /**
+   * Filter table
+   * @param page
+   * @param filterBy
+   * @param value
+   * @return {Promise<void>}
+   */
+  async filterTable(page, filterBy, value) {
+    await this.setValue(page, this.filterColumn(filterBy), value.toString());
+    await this.clickAndWaitForNavigation(page, this.filterSearchButton);
+  }
+
+  /* Column methods */
+  /**
+   * Get text column from table
+   * @param page
+   * @param row
+   * @param columnName
+   * @return {Promise<string>}
+   */
+  async getTextColumn(page, row, columnName) {
+    let columnSelector;
+
+    switch (columnName) {
+      case 'id_feature':
+        columnSelector = this.tableColumnId(row);
+        break;
+
+      case 'b!name':
+        columnSelector = this.tableColumnName(row);
+        break;
+
+      case 'values':
+        columnSelector = this.tableColumnValues(row);
+        break;
+
+      case 'a!position':
+        columnSelector = this.tableColumnPosition(row);
+        break;
+
+      default:
+        throw new Error(`Column ${columnName} was not found`);
+    }
+
+    return this.getTextContent(page, columnSelector);
+  }
+}
+
+module.exports = new Features();
