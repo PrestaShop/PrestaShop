5b1140b6101ead58d384b5e11b7e75ca2f3403e6
Jonathan Lelievre <jo.lelievre@gmail.com>
2020-08-27 18:08:16 +0200

    Improve PHPDoc
    Co-authored-by: Mathieu Ferment <mathieu.ferment@prestashop.com>

--- src/Core/Domain/Manufacturer/ValueObject/NoManufacturerId.php
+++ src/Core/Domain/Manufacturer/ValueObject/NoManufacturerId.php
@@ -29,7 +29,14 @@ declare(strict_types=1);
 namespace PrestaShop\PrestaShop\Core\Domain\Manufacturer\ValueObject;
 
 /**
- * This class is used to remove association with manufacturer
+ * Manufacturer Identifier is a ValueObject which represents a valid identifier of a manufacturer.
+ * It is being used in every class that must refer to a Manufacturer object or is linked to a Manufacturer object.
+ *
+ * However it is possible to decide to un-link the class from a Manufacturer. For example a product can be linked to a Manufacturer now and later this relationship is removed.
+ *
+ * This class NoManufacturerId carriers this intent, instead of using `null` which has another meaning (no modification).
+ *
+ * This picture might help understanding the situation: https://pbs.twimg.com/media/DusCOfyXcAA9_F7.jpg
  */
 class NoManufacturerId extends ManufacturerId
 {
