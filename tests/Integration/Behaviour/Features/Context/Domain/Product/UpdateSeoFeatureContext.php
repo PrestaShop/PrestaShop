2462ec4911565f32202327aa40f857c526b02637
Julius Zukauskas <zuk3975@gmail.com>
2020-08-31 13:18:23 +0300

    introduce ProductRedirectionSettings

--- tests/Integration/Behaviour/Features/Context/Domain/Product/UpdateSeoFeatureContext.php
+++ tests/Integration/Behaviour/Features/Context/Domain/Product/UpdateSeoFeatureContext.php
@@ -32,7 +32,7 @@ use Behat\Gherkin\Node\TableNode;
 use PHPUnit\Framework\Assert;
 use PrestaShop\PrestaShop\Core\Domain\Product\Command\UpdateProductSeoCommand;
 use PrestaShop\PrestaShop\Core\Domain\Product\Exception\ProductException;
-use PrestaShop\PrestaShop\Core\Domain\Product\ValueObject\ProductRedirectOption;
+use PrestaShop\PrestaShop\Core\Domain\Product\ProductRedirectionSettings;
 use RuntimeException;
 use Tests\Integration\Behaviour\Features\Context\Util\PrimitiveUtils;
 
@@ -108,7 +108,7 @@ class UpdateSeoFeatureContext extends AbstractProductFeatureContext
         $productForEditing = $this->getProductForEditing($productReference);
 
         Assert::assertEquals(
-            ProductRedirectOption::NO_TARGET_VALUE,
+            ProductRedirectionSettings::NO_TARGET_VALUE,
             $productForEditing->getProductSeoOptions()->getRedirectTargetId(),
             'Product "%s" expected to have no redirect target'
         );
