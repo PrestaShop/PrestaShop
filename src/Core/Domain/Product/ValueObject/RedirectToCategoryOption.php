b4ce8e1b4bd80a8a75e6e9c9d127a054b6e09d26
Julius Zukauskas <zuk3975@gmail.com>
2020-09-01 16:17:02 +0300

    poc separate objects for redirect option

---
+++ src/Core/Domain/Product/ValueObject/RedirectToCategoryOption.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\PrestaShop\Core\Domain\Product\ValueObject;
+
+use PrestaShop\PrestaShop\Core\Domain\Category\ValueObject\CategoryId;
+
+class RedirectToCategoryOption
+{
+    /**
+     * @var string
+     */
+    private $type;
+
+    /**
+     * @var CategoryId|null
+     */
+    private $categoryId;
+
+    /**
+     * @param string $type
+     * @param CategoryId|null $categoryId
+     */
+    public function __construct(string $type, ?CategoryId $categoryId)
+    {
+        $this->type = $type;
+        $this->categoryId = $categoryId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    /**
+     * @return CategoryId|null
+     */
+    public function getCategoryId(): ?CategoryId
+    {
+        return $this->categoryId;
+    }
+}
