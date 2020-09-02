43e486f326b24ba5b34233d534be642d53ebc239
Julius Zukauskas <zuk3975@gmail.com>
2020-09-02 16:08:58 +0300

    implement SetAssociatedProductAttachmentsHandler

--- src/Core/Domain/Product/Exception/CannotUpdateProductException.php
+++ src/Core/Domain/Product/Exception/CannotUpdateProductException.php
@@ -80,4 +80,9 @@ class CannotUpdateProductException extends ProductException
      * When product seo options update fails
      */
     const FAILED_UPDATE_SEO = 100;
+
+    /**
+     * When product attachments association update fails
+     */
+    const FAILED_UPDATE_ATTACHMENTS = 110;
 }
