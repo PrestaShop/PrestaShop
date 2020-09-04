59de5de991acbced9dcb97aca9e39baf87f51aab
PululuK <pululuandre@hotmail.com>
2020-09-04 20:00:58 +0200

    Fix JavaScript tests / ESLint

--- admin-dev/themes/new-theme/js/pages/improve/design_positions/hook-status-handler.js
+++ admin-dev/themes/new-theme/js/pages/improve/design_positions/hook-status-handler.js
@@ -22,57 +22,41 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  * International Registered Trademark & Property of PrestaShop SA
  */
-const {
-    $
-} = window;
+
+const {$} = window;
 
 class HookStatusHandler {
-    constructor() {
-        if ($('#position-filters').length === 0) {
-            return;
-        }
+  constructor() {
+    const self = this;
+    self.$hookStatus = $('.hook-switch-action');
+    self.$modulePositionsForm = $('#module-positions-form');
 
-        const self = this;
-        self.$hookStatus = $('.hook-switch-action');
-        self.$modulePositionsForm = $('#module-positions-form');
+    self.$hookStatus.on('change', function (e) {
+      e.stopImmediatePropagation();
+      self.toogleHookStatus($(this));
+    });
+  }
 
-        self.$hookStatus.on('change', function(e) {
-            e.stopImmediatePropagation();
-            self.toogleHookStatus($(this));
-        });
-    }
-
-    /**
+  /**
      * Toogle hooks status
      */
-    toogleHookStatus($hookElement) {
-        const self = this;
-
-        $.ajax({
-            type: 'POST',
-            headers: {
-                'cache-control': 'no-cache'
-            },
-            url: self.$modulePositionsForm.data('togglestatus-url'),
-            data: {
-                hookId: $hookElement.data('hook-id')
-            },
-            success: function(data) {
-                if (data.status) {
-                    window.showSuccessMessage(data.message);
-                    var $hook_modules_list = $hookElement.closest('.hook-panel').find('.module-list');
-                    console.log($hook_modules_list)
-                    if (data.hook_status) {
-                        $hook_modules_list.fadeTo(500, 1);
-                    } else {
-                        $hook_modules_list.fadeTo(500, 0.5);
-                    }
-                } else {
-                    window.showErrorMessage(data.message);
-                }
-            },
-        });
-    }
+  toogleHookStatus($hookElement) {
+    $.ajax({
+      type: 'POST',
+      headers: {'cache-control': 'no-cache'},
+      url: this.$modulePositionsForm.data('togglestatus-url'),
+      data: {hookId: $hookElement.data('hook-id')},
+      success(data) {
+        if (data.status) {
+          window.showSuccessMessage(data.message);
+          const $hookModulesList = $hookElement.closest('.hook-panel').find('.module-list');
+          $hookModulesList.fadeTo(500, data.hook_status ? 1 : 0.5);
+        } else {
+          window.showErrorMessage(data.message);
+        }
+      },
+    });
+  }
 }
 
 export default HookStatusHandler;
