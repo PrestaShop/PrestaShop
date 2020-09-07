58f5e4ddfd3bb2332fd0d15ef9e19eec310ec67f
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-09-07 14:41:32 +0200

    Add test 'Filter and quick edit zone'

--- tests/UI/pages/BO/international/locations/index.js
+++ tests/UI/pages/BO/international/locations/index.js
@@ -7,14 +7,39 @@ class Zones extends BOBasePage {
 
     this.pageTitle = 'Zones •';
 
-    // Selectors
-    // SubTab
+    // SubTab selectors
     this.countriesSubTab = '#subtab-AdminCountries';
+
+    // Form selectors
+    this.gridForm = '#form-zone';
+    this.gridTableHeaderTitle = `${this.gridForm} .panel-heading`;
+    this.gridTableNumberOfTitlesSpan = `${this.gridTableHeaderTitle} span.badge`;
+
+    // Table selectors
+    this.gridTable = '#table-zone';
+
+    // Filter selectors
+    this.filterRow = `${this.gridTable} tr.filter`;
+    this.filterColumn = filterBy => `${this.filterRow} [name='zoneFilter_${filterBy}']`;
+    this.filterSearchButton = '#submitFilterButtonzone';
+    this.filterResetButton = 'button[name=\'submitResetzone\']';
+
+    // Table body selectors
+    this.tableBody = `${this.gridTable} tbody`;
+    this.tableBodyRows = `${this.tableBody} tr`;
+    this.tableBodyRow = row => `${this.tableBodyRows}:nth-child(${row})`;
+    this.tableBodyColumn = row => `${this.tableBodyRow(row)} td`;
+
+    // Columns selectors
+    this.tableColumnSelectRowCheckbox = row => `${this.tableBodyColumn(row)} input[name='zoneBox[]']`;
+    this.tableColumnId = row => `${this.tableBodyColumn(row)}:nth-child(2)`;
+    this.tableColumnName = row => `${this.tableBodyColumn(row)}:nth-child(3)`;
+    this.tableColumnStatusLink = row => `${this.tableBodyColumn(row)}:nth-child(4) a`;
+    this.tableColumnStatusEnableLink = row => `${this.tableColumnStatusLink(row)}.action-enabled`;
+    this.tableColumnStatusDisableLink = row => `${this.tableColumnStatusLink(row)}.action-disabled`;
   }
 
-  /*
-  Methods
-   */
+  /* Header methods */
   /**
    * Go to sub tab countries
    * @param page
@@ -22,6 +47,116 @@ class Zones extends BOBasePage {
    */
   async goToSubTabCountries(page) {
     await this.clickAndWaitForNavigation(page, this.countriesSubTab);
+  }
+
+  /* Filter Methods */
+  /**
+   * Reset all filters
+   * @param page
+   * @return {Promise<void>}
+   */
+  async resetFilter(page) {
+    if (!(await this.elementNotVisible(page, this.filterResetButton, 2000))) {
+      await this.clickAndWaitForNavigation(page, this.filterResetButton);
+    }
+    await this.waitForVisibleSelector(page, this.filterSearchButton, 2000);
+  }
+
+  /**
+   * Get Number of zones
+   * @param page
+   * @return {Promise<number>}
+   */
+  getNumberOfElementInGrid(page) {
+    return this.getNumberFromText(page, this.gridTableNumberOfTitlesSpan);
+  }
+
+  /**
+   * Reset and get number of zones
+   * @param page
+   * @return {Promise<number>}
+   */
+  async resetAndGetNumberOfLines(page) {
+    await this.resetFilter(page);
+    return this.getNumberOfElementInGrid(page);
+  }
+
+  /**
+   * Filter zones
+   * @param page
+   * @param filterType
+   * @param filterBy
+   * @param value
+   * @return {Promise<void>}
+   */
+  async filterZones(page, filterType, filterBy, value) {
+    switch (filterType) {
+      case 'input':
+        await this.setValue(page, this.filterColumn(filterBy), value.toString());
+        await this.clickAndWaitForNavigation(page, this.filterSearchButton);
+        break;
+
+      case 'select':
+        await Promise.all([
+          page.waitForNavigation({waitUntil: 'networkidle'}),
+          this.selectByVisibleText(page, this.filterColumn(filterBy), value ? 'Yes' : 'No'),
+        ]);
+        break;
+
+      default:
+        throw new Error(`Filter ${filterBy} was not found`);
+    }
+  }
+
+  /* Column methods */
+
+  /**
+   * Get text from column in table
+   * @param page
+   * @param row
+   * @param columnName
+   * @return {Promise<string>}
+   */
+  async getTextColumn(page, row, columnName) {
+    let columnSelector;
+
+    switch (columnName) {
+      case 'id_zone':
+        columnSelector = this.tableColumnId(row);
+        break;
+
+      case 'name':
+        columnSelector = this.tableColumnName(row);
+        break;
+
+      default:
+        throw new Error(`Column ${columnName} was not found`);
+    }
+
+    return this.getTextContent(page, columnSelector);
+  }
+
+  /**
+   * Get zone status
+   * @param page
+   * @param row
+   * @return {Promise<boolean>}
+   */
+  getZoneStatus(page, row) {
+    return this.elementVisible(page, this.tableColumnStatusEnableLink(row), 1000);
+  }
+
+  /**
+   * Set zone status
+   * @param page
+   * @param row
+   * @param wantedStatus
+   * @return {Promise<void>}
+   */
+  async setZoneStatus(page, row, wantedStatus) {
+    if (wantedStatus !== await this.getZoneStatus(page, row)) {
+      await this.clickAndWaitForNavigation(page, this.tableColumnStatusLink(row));
+    }
   }
 }
 module.exports = new Zones();
