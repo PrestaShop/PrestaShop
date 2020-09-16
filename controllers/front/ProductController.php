6524c66f3c55d4f73a5146617134f81428d32497
Guisardo <missgatasch@gmail.com>
2020-09-16 17:30:18 -0300

    re lint

--- controllers/front/ProductController.php
+++ controllers/front/ProductController.php
@@ -1,4 +1,3 @@
-
 <?php
 /**
  * Copyright since 2007 PrestaShop SA and Contributors
@@ -677,13 +676,13 @@ class ProductControllerCore extends ProductPresentingFrontControllerCore
                             $id_product_attributes[] = $row['id_product_attribute'];
                         }
                     }
-                    $id_attributes = Db::getInstance()->executeS('SELECT pac2.`id_attribute` FROM `'._DB_PREFIX_.'product_attribute_combination` pac2'.
+                    $id_attributes = Db::getInstance()->executeS('SELECT pac2.`id_attribute` FROM `' . _DB_PREFIX_ . 'product_attribute_combination` pac2' .
                         ((!Product::isAvailableWhenOutOfStock($this->product->out_of_stock) && 0 == Configuration::get('PS_DISP_UNAVAILABLE_ATTR')) ?
-                        ' INNER JOIN `'._DB_PREFIX_.'stock_available` pa ON pa.id_product_attribute = pac2.id_product_attribute
+                        ' INNER JOIN `' . _DB_PREFIX_ . 'stock_available` pa ON pa.id_product_attribute = pac2.id_product_attribute
                         WHERE pa.quantity > 0 AND ' :
-                        ' WHERE ').
-                        'pac2.`id_product_attribute` IN ('.implode(',', array_map('intval', $id_product_attributes)).')
-                        AND pac2.id_attribute NOT IN ('.implode(',', array_map('intval', $current_selected_attributes)).')');
+                        ' WHERE ') .
+                        'pac2.`id_product_attribute` IN (' . implode(',', array_map('intval', $id_product_attributes)) . ')
+                        AND pac2.id_attribute NOT IN (' . implode(',', array_map('intval', $current_selected_attributes)) . ')');
                     foreach ($id_attributes as $k => $row) {
                         $id_attributes[$k] = (int) $row['id_attribute'];
                     }
