e367389bb54e25207125d942193145971ac43a08
Jonathan Lelievre <jo.lelievre@gmail.com>
2020-09-23 17:22:58 +0200

    Manually handle multi shop fallback

--- src/Adapter/Configuration.php
+++ src/Adapter/Configuration.php
@@ -139,8 +139,17 @@ class Configuration extends ParameterBag implements ConfigurationInterface, Shop
             return $this->getLocalized($key, $shopId, $shopGroupId);
         }
 
-        if (ConfigurationLegacy::hasKey($key, null, $shopGroupId, $shopId)) {
-            return ConfigurationLegacy::get($key, null, $shopGroupId, $shopId);
+        // Since has key doesn't check manage the fallback shop > shop group > global, we handle it manually
+        if (ConfigurationLegacy::hasKey($key, null, null, $shopId)) {
+            return ConfigurationLegacy::get($key, null, null, $shopId);
+        }
+
+        if (ConfigurationLegacy::hasKey($key, null, $shopGroupId)) {
+            return ConfigurationLegacy::get($key, null, $shopGroupId);
+        }
+
+        if (ConfigurationLegacy::hasKey($key)) {
+            return ConfigurationLegacy::get($key);
         }
 
         return $default;
