b366313d28e6dbf661708e48f929cf590fb70916
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-09-28 14:06:06 +0200

    Avoid creating new state with existing iso code

--- tests/UI/campaigns/data/faker/state.js
+++ tests/UI/campaigns/data/faker/state.js
@@ -5,11 +5,12 @@ const zones = Object.values(Zones).map(zone => zone.name);
 const countriesWithState = [
   'Argentina', 'Australia', 'Canada', 'India', 'Indonesia', 'Italy', 'Japan', 'Mexico', 'United States',
 ];
+const statesIsoCodes = ['IR', 'PK', 'RE', 'BV', 'ZM', 'HM', 'BS', 'SV'];
 
 module.exports = class State {
   constructor(stateToCreate = {}) {
     this.name = stateToCreate.name || `test ${faker.address.state()}`;
-    this.isoCode = stateToCreate.isoCode || faker.address.countryCode();
+    this.isoCode = stateToCreate.isoCode || faker.random.arrayElement(statesIsoCodes);
     this.country = stateToCreate.country || faker.random.arrayElement(countriesWithState);
     this.zone = stateToCreate.zone || faker.random.arrayElement(zones);
     this.active = stateToCreate.active === undefined ? false : stateToCreate.active;
