c1a00a88bc70cc3f9d82c7ba7001b6ef969576ea
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-09-28 17:34:03 +0200

    Refactoring create cart rule in UI tests

--- tests/UI/pages/BO/catalog/discounts/add.js
+++ tests/UI/pages/BO/catalog/discounts/add.js
@@ -10,25 +10,175 @@ class AddCartRule extends BOBasePage {
 
     // Selectors
     this.cartRuleForm = '#cart_rule_form';
+
     // Information tab
+    this.infomationsTabLink = '#cart_rule_link_informations';
+
     this.nameInput = ID => `#name_${ID}`;
     this.descriptionTextArea = `${this.cartRuleForm} textarea[name='description']`;
+
+    // Discount code selectors
     this.codeInput = '#code';
     this.generateButton = '#cart_rule_informations  a.btn-default';
+
+    // Toggle Selectors
+    this.highlightToggle = toggle => `${this.cartRuleForm} label[for='highlight_${toggle}']`;
+    this.partialUseToggle = toggle => `${this.cartRuleForm} label[for='partial_use_${toggle}']`;
+    this.priorityInput = `${this.cartRuleForm} input[name='priority']`;
+    this.statusToggle = toggle => `${this.cartRuleForm} label[for='active_${toggle}']`;
+
     // Conditions tab
     this.conditionsTabLink = '#cart_rule_link_conditions';
+
+    // Customer selectors
     this.singleCustomerInput = '#customerFilter';
     this.singleCustomerResultBlock = 'div.ac_results';
     this.singleCustomerResultItem = `${this.singleCustomerResultBlock} ul li`;
+
+    // Valid date selectors
+    this.dateFromInput = 'input[name=dateFrom]';
+    this.dateToInput = 'input[name=dateTo]';
+
+    // Minimum amount selectors
+    this.minimumAmountInput = 'input[name=minimum_amount]';
+    this.minimumAmountCurrencySelect = 'select[name=minimum_amount_currency]';
+    this.minimumAmountTaxSelect = 'select[name=minimum_amount_tax]';
+    this.minimumAmountShippingSelect = 'select[name=minimum_amount_shipping]';
+
+    // Quantity selectors
+    this.quantityInput = 'input[name=quantity]';
+    this.quantityPerUserInput = 'input[name=quantity_per_user]';
+
     // Actions tab
     this.actionsTabLink = '#cart_rule_link_actions';
-    this.freeShippingInput = TOGGLE => `${this.cartRuleForm} label[for='free_shipping_${TOGGLE}']`;
-    this.applyDiscountRadioButton = TOGGLE => `${this.cartRuleForm} label[for='apply_discount_${TOGGLE}']`;
-    this.reductionPercentInput = '#reduction_percent';
+    this.freeShippingInput = toggle => `${this.cartRuleForm} label[for='free_shipping_${toggle}']`;
+
+    // Discount percent selectors
+    this.applyDiscountRadioButton = toggle => `${this.cartRuleForm} label[for='apply_discount_${toggle}']`;
+    this.discountPercentRadioButton = this.applyDiscountRadioButton('percent');
+    this.discountPercentInput = '#reduction_percent';
+
+    // Discount amount selectors
+    this.discountAmountRadioButton = this.applyDiscountRadioButton('amount');
+    this.discountAmountInput = 'input[name=reduction_amount]';
+    this.discountAmountCurrencySelect = 'select[name=reduction_currency]';
+    this.discountAmountTaxSelect = 'select[name=reduction_tax]';
+
+    // Discount others selectors
+    this.discountOffRadioButton = this.applyDiscountRadioButton('off');
+
+    // Form footer selectors
     this.saveButton = '#desc-cart_rule-save';
   }
 
   /* Methods */
+  /**
+   * Fill form in information tab
+   * @param page
+   * @param cartRuleData
+   * @return {Promise<void>}
+   */
+  async fillInformationForm(page, cartRuleData) {
+    // Go to tab conditions
+    await page.click(this.infomationsTabLink);
+
+    // Fill information form
+    await this.setValue(page, this.nameInput(1), cartRuleData.name);
+    await this.setValue(page, this.descriptionTextArea, cartRuleData.description);
+
+    // Generate a discount code
+    if (cartRuleData.code) {
+      await this.setValue(page, this.codeInput, cartRuleData.code);
+    } else {
+      await page.click(this.generateButton);
+    }
+
+    // Set toggles
+    await page.click(this.highlightToggle(cartRuleData.highlight ? 'on' : 'off'));
+    await page.click(this.partialUseToggle(cartRuleData.partialUse ? 'on' : 'off'));
+
+    // Set priority
+    await this.setValue(page, this.priorityInput, cartRuleData.priority);
+
+    // Set status
+    await page.click(this.statusToggle(cartRuleData.status ? 'on' : 'off'));
+  }
+
+  /**
+   *
+   * @param page
+   * @param cartRuleData
+   * @return {Promise<void>}
+   */
+  async fillConditionsForm(page, cartRuleData) {
+    // Go to tab conditions
+    await page.click(this.conditionsTabLink);
+
+    // Set Customer
+    // Customer will not be set if we want to use the cart rule for any customer
+    if (cartRuleData.customer) {
+      await this.setValue(page, this.singleCustomerInput, cartRuleData.customer);
+      await this.waitForVisibleSelector(page, `${this.singleCustomerResultBlock}:not([style*='display: none;'])`);
+      await page.click(this.singleCustomerResultItem);
+    }
+
+    // Fill date from if its changed
+    if (cartRuleData.dateFrom) {
+      await this.setValue(page, this.dateFromInput, cartRuleData.dateFrom);
+    }
+
+    // Fill date to if its changed
+    if (cartRuleData.dateTo) {
+      await this.setValue(page, this.dateToInput, cartRuleData.dateTo);
+    }
+
+    // Fill minimum amount values
+    await this.setValue(page, this.minimumAmountInput, cartRuleData.minimumAmount.value.toString());
+    await this.selectByVisibleText(page, this.minimumAmountCurrencySelect, cartRuleData.minimumAmount.currency);
+    await this.selectByVisibleText(page, this.minimumAmountTaxSelect, cartRuleData.minimumAmount.tax);
+    await this.selectByVisibleText(page, this.minimumAmountShippingSelect, cartRuleData.minimumAmount.shipping);
+
+    // Fill quantities
+    await this.setValue(page, this.quantityInput, cartRuleData.quantity.toString());
+    await this.setValue(page, this.quantityPerUserInput, cartRuleData.quantityPerUser.toString());
+  }
+
+
+  /**
+   * Fill actions tab
+   * @param page
+   * @param cartRuleData
+   * @return {Promise<void>}
+   */
+  async fillActionsForm(page, cartRuleData) {
+    // Go to actions tab
+    await page.click(this.actionsTabLink);
+
+    // Set free shipping toggle
+    await page.click(this.freeShippingInput(cartRuleData.freeShipping));
+
+    switch (cartRuleData.discountType) {
+      case 'Percent':
+        await page.click(this.discountPercentRadioButton);
+        await this.setValue(page, this.discountPercentInput, cartRuleData.discountPercent.toString());
+        break;
+      case 'Amount':
+        await page.click(this.discountAmountRadioButton);
+        await this.setValue(page, this.discountAmountInput, cartRuleData.discountAmount.value.toString());
+        await this.selectByVisibleText(page, this.discountAmountCurrencySelect, cartRuleData.discountAmount.currency);
+        await this.selectByVisibleText(page, this.discountAmountTaxSelect, cartRuleData.discountAmount.tax);
+        break;
+      case 'None':
+        await page.click(this.discountOffRadioButton);
+        break;
+      default:
+        // Do nothing for this option
+    }
+
+    // Set free gift toggle
+  }
+
+
   /**
    * Create/edit cart rule
    * @param page
@@ -37,28 +187,15 @@ class AddCartRule extends BOBasePage {
    */
   async createEditCartRules(page, cartRuleData) {
     // Fill information form
-    await this.setValue(page, this.nameInput(1), cartRuleData.name);
-    await this.setValue(page, this.descriptionTextArea, cartRuleData.description);
+    await this.fillInformationForm(page, cartRuleData);
 
-    if (cartRuleData.code) {
-      await this.setValue(page, this.codeInput, cartRuleData.code);
-    } else {
-      await page.click(this.generateButton);
-    }
     // Fill conditions form
-    await page.click(this.conditionsTabLink);
-    await this.setValue(page, this.singleCustomerInput, cartRuleData.customer);
-    await this.waitForVisibleSelector(page, `${this.singleCustomerResultBlock}:not([style*='display: none;'])`);
-    await page.click(this.singleCustomerResultItem);
+    await this.fillConditionsForm(page, cartRuleData);
+
     // Fill actions form
-    await page.click(this.actionsTabLink);
-    await page.click(this.freeShippingInput(cartRuleData.freeShipping));
+    await this.fillActionsForm(page, cartRuleData);
 
-    if (cartRuleData.percent) {
-      await page.click(this.applyDiscountRadioButton('percent'));
-    }
-    await this.setValue(page, this.reductionPercentInput, cartRuleData.value.toString());
-
+    // Save and return successful message
     await this.clickAndWaitForNavigation(page, this.saveButton);
     return this.getTextContent(page, this.alertSuccessBlock);
   }
