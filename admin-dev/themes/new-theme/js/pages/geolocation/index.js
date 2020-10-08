9f64be213ff69ece1848903a833fc63282d07ffe
Jevgenij Visockij <jevgenij.visockij@invertus.eu>
2020-10-08 11:04:49 +0300

    Removed unneeded import of ChoicesTable

--- admin-dev/themes/new-theme/js/pages/geolocation/index.js
+++ admin-dev/themes/new-theme/js/pages/geolocation/index.js
@@ -23,8 +23,6 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  */
 
-import ChoiceTable from '@components/choice-table';
-
 const {$} = window;
 
 $(() => {
