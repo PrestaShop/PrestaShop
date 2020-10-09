1324ea158edbb7884f8d69fe9346a61bb5bb3c6a
Krystian Podemski <podemski.krystian@gmail.com>
2020-10-09 21:09:10 +0200

    revert field... shouldn't be deleted in the first place

--- install-dev/data/db_structure.sql
+++ install-dev/data/db_structure.sql
@@ -393,6 +393,7 @@ CREATE TABLE `PREFIX_category_product` (
 CREATE TABLE `PREFIX_cms` (
   `id_cms` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `id_cms_category` int(10) unsigned NOT NULL,
+  `position` int(10) unsigned NOT NULL DEFAULT '0',
   `active` tinyint(1) unsigned NOT NULL DEFAULT '0',
   `indexation` tinyint(1) unsigned NOT NULL DEFAULT '1',
   PRIMARY KEY (`id_cms`)
