d4df464886a5dad76626df284f071d4f48d4a742
Jonathan Lelievre <jo.lelievre@gmail.com>
2020-10-12 20:49:11 +0200

    Reintroduce basic filler methods

--- src/Adapter/AbstractObjectModelFiller.php
+++ src/Adapter/AbstractObjectModelFiller.php
@@ -29,18 +29,80 @@ declare(strict_types=1);
 namespace PrestaShop\PrestaShop\Adapter;
 
 use ObjectModel;
+use PrestaShop\PrestaShop\Core\Exception\CoreException;
 
 class AbstractObjectModelFiller
 {
-    protected function fillProperty(ObjectModel $objectModel, string $propertyName, array $propertiesToUpdate): void
+    /**
+     * @param ObjectModel $objectModel
+     * @param string $propertyName
+     * @param array $properties
+     */
+    protected function fillProperty(ObjectModel $objectModel, string $propertyName, array $properties): void
     {
-        if (!isset($propertiesToUpdate[$propertyName])) {
+        if (!array_key_exists($propertyName, $properties)) {
             return;
         }
 
-        if (property_exists(get_class($objectModel), $propertyName)) {
-            $objectModel->{$propertyName} = $propertiesToUpdate[$propertyName];
+        if (!property_exists($objectModel, $propertyName)) {
+            throw new CoreException(sprintf(
+                '%s object does not have a property %s',
+                get_class($objectModel),
+                $propertyName
+            ));
+        }
+
+        $objectModel->{$propertyName} = $properties[$propertyName];
+        if ($objectModel->id) {
             $objectModel->addFieldsToUpdate([$propertyName => true]);
         }
+    }
+
+    /**
+     * @param ObjectModel $objectModel
+     * @param string $propertyName
+     * @param array $properties
+     */
+    protected function fillLocalizedProperty(ObjectModel $objectModel, string $propertyName, array $properties)
+    {
+        if (!array_key_exists($propertyName, $properties)) {
+            return;
+        }
+
+        if (!property_exists($objectModel, $propertyName)) {
+            throw new CoreException(sprintf(
+                '%s object does not have a property %s',
+                get_class($objectModel),
+                $propertyName
+            ));
+        }
+
+        if (!is_array($properties[$propertyName])) {
+            throw new CoreException(sprintf(
+                'Localized object model property must be an array. "%s" given',
+                var_export($properties[$propertyName])
+            ));
+        }
+
+        $objectModel->{$propertyName} = $properties[$propertyName];
+
+        if ($objectModel->id) {
+            $this->addLocalizedPropertyToUpdate($objectModel, $propertyName, $properties[$propertyName]);
+        }
+    }
+
+    /**
+     * @param ObjectModel $objectModel
+     * @param string $propertyName
+     * @param array<int, string> $values
+     */
+    private function addLocalizedPropertyToUpdate(ObjectModel $objectModel, string $propertyName, array $values): void
+    {
+        $updateFieldValue = [];
+        foreach ($values as $langId => $value) {
+            $updateFieldValue[$langId] = true;
+        }
+
+        $objectModel->addFieldsToUpdate([$propertyName => $updateFieldValue]);
     }
 }
