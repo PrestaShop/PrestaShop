eb0e3fcd80a5a5d44faff722becdec3c2b5a1772
Valentin Szczupak <valentin.szczupak@prestashop.com>
2020-10-14 10:38:49 +0200

    Add comment to autocomplete var

--- classes/form/FormField.php
+++ classes/form/FormField.php
@@ -34,6 +34,9 @@ class FormFieldCore
     private $maxLength = null;
     private $errors = [];
     private $constraints = [];
+    /**
+     * @var string
+     */
     private $autocomplete = '';
 
     public function toArray()
