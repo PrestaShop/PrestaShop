59420b5ad4bc9fcecf01a98df4543e81be8e1dad
Julius Zukauskas <zuk3975@gmail.com>
2020-10-19 14:13:44 +0300

    prepare handler for AddProductImage

---
+++ src/Core/Domain/Product/Command/AddProductImageCommand.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\PrestaShop\Core\Domain\Product\Command;
+
+use PrestaShop\PrestaShop\Core\Domain\Product\ValueObject\ProductId;
+
+/**
+ * Adds new product image
+ */
+class AddProductImageCommand
+{
+    /**
+     * @var ProductId
+     */
+    private $productId;
+
+    /**
+     * @var string
+     */
+    private $originalName;
+
+    /**
+     * @var string
+     */
+    private $pathName;
+
+    /**
+     * @var int
+     */
+    private $fileSize;
+
+    /**
+     * @var string
+     */
+    private $mimeType;
+
+    /**
+     * @param ProductId $productId
+     * @param string $originalName
+     * @param string $pathName
+     * @param int $fileSize
+     * @param string $mimeType
+     */
+    public function __construct(
+        ProductId $productId,
+        string $originalName,
+        string $pathName,
+        int $fileSize,
+        string $mimeType
+    ) {
+        $this->productId = $productId;
+        $this->originalName = $originalName;
+        $this->pathName = $pathName;
+        $this->fileSize = $fileSize;
+        $this->mimeType = $mimeType;
+    }
+
+    /**
+     * @return ProductId
+     */
+    public function getProductId(): ProductId
+    {
+        return $this->productId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOriginalName(): string
+    {
+        return $this->originalName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPathName(): string
+    {
+        return $this->pathName;
+    }
+
+    /**
+     * @return int
+     */
+    public function getFileSize(): int
+    {
+        return $this->fileSize;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMimeType(): string
+    {
+        return $this->mimeType;
+    }
+}
