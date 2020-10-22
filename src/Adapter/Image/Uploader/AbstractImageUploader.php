6063874475a4966e73d894dfdc80466da3b114c9
Julius Zukauskas <zuk3975@gmail.com>
2020-10-22 13:09:08 +0300

    remove resovled todos

--- src/Adapter/Image/Uploader/AbstractImageUploader.php
+++ src/Adapter/Image/Uploader/AbstractImageUploader.php
@@ -106,7 +106,6 @@ abstract class AbstractImageUploader
             throw new ImageOptimizationException('An error occurred while uploading the image. Check your directory permissions.');
         }
 
-        //@todo: should we care about tempfile deletion? shouldn't we leave it for garbage collector?
         unlink($temporaryImageName);
     }
 
