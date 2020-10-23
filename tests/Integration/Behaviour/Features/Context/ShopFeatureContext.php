1bb5cea9a78744f87017e487a59b926f29cb928a
matthieu rolland <matthieu.rolland@prestashop.com>
2020-10-23 17:22:02 +0200

    add behat tests

--- tests/Integration/Behaviour/Features/Context/ShopFeatureContext.php
+++ tests/Integration/Behaviour/Features/Context/ShopFeatureContext.php
@@ -27,17 +27,24 @@
 namespace Tests\Integration\Behaviour\Features\Context;
 
 use Configuration;
+use Behat\Gherkin\Node\TableNode;
+use PHPUnit\Framework\Assert;
+use PrestaShop\PrestaShop\Core\Domain\Shop\Exception\ShopException;
+use PrestaShop\PrestaShop\Core\Domain\Shop\Query\SearchShops;
 use RuntimeException;
 use Shop;
+use Tests\Integration\Behaviour\Features\Context\Domain\AbstractDomainFeatureContext;
 
-class ShopFeatureContext extends AbstractPrestaShopFeatureContext
+class ShopFeatureContext extends AbstractDomainFeatureContext
 {
+    const LAST_FOUND_SHOPS_KEY = 'LAST_FOUND_SHOPS';
+
     /**
      * @Given single shop :shopReference context is loaded
      *
      * @param string $shopReference
      */
-    public function loadSingleShopContext(string $shopReference)
+    public function loadSingleShopContext(string $shopReference): void
     {
         /** @var Shop $shop */
         $shop = SharedStorage::getStorage()->get($shopReference);
@@ -51,7 +58,7 @@ class ShopFeatureContext extends AbstractPrestaShopFeatureContext
      * @param string $reference
      * @param string $shopName
      */
-    public function shopWithNameExists(string $reference, string $shopName)
+    public function shopWithNameExists(string $reference, string $shopName): void
     {
         $shopId = Shop::getIdByName($shopName);
 
@@ -65,7 +72,7 @@ class ShopFeatureContext extends AbstractPrestaShopFeatureContext
     /**
      * @Given single shop context is loaded
      */
-    public function singleShopContextIsLoaded()
+    public function singleShopContextIsLoaded(): void
     {
         Shop::setContext(Shop::CONTEXT_SHOP, Configuration::get('PS_SHOP_DEFAULT'));
     }
@@ -73,8 +80,124 @@ class ShopFeatureContext extends AbstractPrestaShopFeatureContext
     /**
      * @Given multiple shop context is loaded
      */
-    public function multipleShopContextIsLoaded()
+    public function multipleShopContextIsLoaded(): void
     {
         Shop::setContext(Shop::CONTEXT_ALL);
+    }
+
+    /**
+     * @Given I add a shop :reference with name :shopName
+     *
+     * @param string $reference
+     * @param string $shopName
+     */
+    public function addShop(string $reference, string $shopName): void
+    {
+        $shop = new Shop();
+        $shop->active = true;
+        $shop->id_shop_group = 1;
+        $shop->id_category = 2;
+        $shop->theme_name = _THEME_NAME_;
+        $shop->name = $shopName;
+        if (!$shop->add()) {
+            throw new RuntimeException(sprintf('Could not create shop: %s', Db::getInstance()->getMsgError()));
+        }
+        $shop->setTheme();
+
+        SharedStorage::getStorage()->set($reference, $shop);
+    }
+
+    /**
+     * @When I search for shops with the term :searchTerm
+     *
+     * @param string $searchTerm
+     */
+    public function searchShopsWithTerm(string $searchTerm): void
+    {
+        try {
+            $shops = $this->getQueryBus()->handle(new SearchShops($searchTerm));
+        } catch (ShopException $e) {
+            $this->setLastException($e);
+
+            return;
+        }
+
+        $this->setLastSearchShopsResult(!empty($shops) ? $shops['shops'] : []);
+    }
+
+    /**
+     * @Then I should get the following shop results:
+     *
+     * @param TableNode $table
+     */
+    public function assertFoundShops(TableNode $table): void
+    {
+        $expectedShops = $table->getColumnsHash();
+        $foundShops = $this->getLastSearchShopsResult();
+
+        foreach ($expectedShops as $key => $currentExpectedShop) {
+            $wasCurrentExpectedShopFound = false;
+            foreach ($foundShops as $currentFoundShop) {
+                if ($currentExpectedShop['id'] == $currentFoundShop['id']) {
+                    $wasCurrentExpectedShopFound = true;
+                    Assert::assertEquals(
+                        $currentExpectedShop['name'],
+                        $currentFoundShop['name'],
+                        sprintf(
+                            'Expected and found shops don\'t have the same name (%s and %s)',
+                            $currentExpectedShop['name'],
+                            $currentFoundShop['name']
+                        )
+                    );
+                    Assert::assertEquals(
+                        $currentExpectedShop['group_name'],
+                        $currentFoundShop['group_name'],
+                        sprintf(
+                            'Expected and found shops\'s groups don\'t match (%s and %s)',
+                            $currentExpectedShop['group_name'],
+                            $currentFoundShop['group_name']
+                        )
+                    );
+                    continue;
+                }
+            }
+            if (!$wasCurrentExpectedShopFound) {
+                throw new RuntimeException(sprintf(
+                    'Expected shop with name %s and id %s was not found',
+                    $currentExpectedShop['name'],
+                    $currentExpectedShop['id']
+                ));
+            }
+        }
+    }
+
+    /**
+     * Set the list of found shops in the last research
+     *
+     * @param array $shops
+     */
+    private function setLastSearchShopsResult(array $shops): void
+    {
+        $this->getSharedStorage()->set(self::LAST_FOUND_SHOPS_KEY, $shops);
+    }
+
+    /**
+     * Get a list of shop from the last research
+     *
+     * @return array
+     */
+    private function getLastSearchShopsResult(): array
+    {
+        return $this->getSharedStorage()->get(self::LAST_FOUND_SHOPS_KEY);
+    }
+
+    /**
+     * @Then I should get a ShopException
+     */
+    public function assertShopException(): void
+    {
+        $this->assertLastErrorIs(
+            ShopException::class
+        );
     }
 }
