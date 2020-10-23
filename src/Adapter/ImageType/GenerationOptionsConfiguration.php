76d142de6dab2775d7025a62f2100136714aa704
ks129 <45097959+ks129@users.noreply.github.com>
2020-10-23 08:41:26 +0300

    Create configuration object for image generation settings

--- src/Adapter/ImageType/GenerationOptionsConfiguration.php
+++ src/Adapter/ImageType/GenerationOptionsConfiguration.php
@@ -1,4 +1,28 @@
 <?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
 
 declare(strict_types=1);
 
@@ -38,7 +62,7 @@ class GenerationOptionsConfiguration implements DataConfigurationInterface
             'product_picture_max_size' => $this->configuration->getInt('PS_PRODUCT_PICTURE_MAX_SIZE'),
             'product_picture_width' => $this->configuration->getInt('PS_PRODUCT_PICTURE_WIDTH'),
             'product_picture_height' => $this->configuration->getInt('PS_PRODUCT_PICTURE_HEIGHT'),
-            'high_dpi' => $this->configuration->getBoolean('PS_HIGH_DPI'),
+            'high_dpi' => $this->configuration->getBoolean('PS_HIGHT_DPI'),
         ];
     }
 
@@ -51,11 +75,23 @@ class GenerationOptionsConfiguration implements DataConfigurationInterface
             $this->configuration->set('PS_IMAGE_QUALITY', $configuration['image_quality']);
             $this->configuration->set('PS_JPEG_QUALITY', $configuration['jpeg_quality']);
             $this->configuration->set('PS_PNG_QUALITY', $configuration['png_quality']);
-            $this->configuration->set('PS_IMAGE_REGENERATION_METHOD', $configuration['image_regeneration_method']);
-            $this->configuration->set('PS_PRODUCT_PICTURE_MAX_SIZE', $configuration['product_picture_max_size']);
-            $this->configuration->set('PS_PRODUCT_PICTURE_WIDTH', $configuration['product_picture_width']);
-            $this->configuration->set('PS_PRODUCT_PICTURE_HEIGHT', $configuration['product_picture_height']);
-            $this->configuration->set('PS_HIGH_DPI', $configuration['high_dpi']);
+
+            // Validate them is here as these may be not shown every time
+            if (isset($configuration['image_regeneration_method'])) {
+                $this->configuration->set('PS_IMAGE_REGENERATION_METHOD', $configuration['image_regeneration_method']);
+            }
+            if (isset($configuration['product_picture_max_size'])) {
+                $this->configuration->set('PS_PRODUCT_PICTURE_MAX_SIZE', $configuration['product_picture_max_size']);
+            }
+            if (isset($configuration['product_picture_width'])) {
+                $this->configuration->set('PS_PRODUCT_PICTURE_WIDTH', $configuration['product_picture_width']);
+            }
+            if (isset($configuration['product_picture_height'])) {
+                $this->configuration->set('PS_PRODUCT_PICTURE_HEIGHT', $configuration['product_picture_height']);
+            }
+            if (isset($configuration['hight_dpi'])) {
+                $this->configuration->set('PS_HIGHT_DPI', $configuration['hight_dpi']);
+            }
         }
 
         return [];
@@ -69,12 +105,7 @@ class GenerationOptionsConfiguration implements DataConfigurationInterface
         return isset(
             $configuration['image_quality'],
             $configuration['jpeg_quality'],
-            $configuration['png_quality'],
-            $configuration['image_regeneration_method'],
-            $configuration['product_picture_max_size'],
-            $configuration['product_picture_width'],
-            $configuration['product_picture_height'],
-            $configuration['high_dpi']
+            $configuration['png_quality']
         );
     }
 }
