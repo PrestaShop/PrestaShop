b65dc92ddc7a5869d265e2f5ea69cb3d11db7619
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-10-26 11:41:34 +0100

    Add restarting apache

--- travis-scripts/setup-composer.sh
+++ travis-scripts/setup-composer.sh
@@ -2,4 +2,7 @@
 
 echo "Install composer v1.10.16";
 
- curl -sS https://getcomposer.org/installer | php -- --install-dir=$COMPOSER_BIN_DIR --filename=composer --version=1.10.16
+ curl -sS https://getcomposer.org/installer | php -- --install-dir=vendor/bin/ --filename=composer --version=1.10.16
+
+# Restarting Apache
+sudo service apache2 restart
