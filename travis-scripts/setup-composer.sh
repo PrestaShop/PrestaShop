84a6f3c8aa1640a31a7c8c5877fbe3c9f686a68f
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-10-26 11:39:45 +0100

    Update composer install-dir

--- travis-scripts/setup-composer.sh
+++ travis-scripts/setup-composer.sh
@@ -2,4 +2,4 @@
 
 echo "Install composer v1.10.16";
 
- curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin --filename=composer --version=1.10.16
+ curl -sS https://getcomposer.org/installer | php -- --install-dir=$COMPOSER_BIN_DIR --filename=composer --version=1.10.16
