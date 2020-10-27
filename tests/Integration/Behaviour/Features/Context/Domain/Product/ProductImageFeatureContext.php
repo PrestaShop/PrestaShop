ae68abc838008ec2bc825dd54cf20c018cde0db2
Julius Zukauskas <zuk3975@gmail.com>
2020-10-27 18:03:38 +0200

    assert product image & its path

--- tests/Integration/Behaviour/Features/Context/Domain/Product/ProductImageFeatureContext.php
+++ tests/Integration/Behaviour/Features/Context/Domain/Product/ProductImageFeatureContext.php
@@ -33,25 +33,30 @@ use PHPUnit\Framework\Assert;
 use PrestaShop\PrestaShop\Core\Domain\Product\Command\AddProductImageCommand;
 use PrestaShop\PrestaShop\Core\Domain\Product\Query\GetProductImages;
 use PrestaShop\PrestaShop\Core\Domain\Product\QueryResult\ProductImage;
+use RuntimeException;
+use Tests\Integration\Behaviour\Features\Context\Util\PrimitiveUtils;
 use Tests\Resources\DummyFileUploader;
 
 class ProductImageFeatureContext extends AbstractProductFeatureContext
 {
     /**
-     * @When I add new product :productReference image :fileName
+     * @When I add new product :productReference image :imageReference named :fileName
      *
      * @param string $productReference
+     * @param string $imageReference
      * @param string $fileName
      */
-    public function uploadImage(string $productReference, string $fileName): void
+    public function uploadImage(string $productReference, string $imageReference, string $fileName): void
     {
         //@todo: behats database contains empty ImageType.
         $pathName = DummyFileUploader::upload($fileName);
 
-        $this->getCommandBus()->handle(new AddProductImageCommand(
+        $imageId = $this->getCommandBus()->handle(new AddProductImageCommand(
             $this->getSharedStorage()->get($productReference),
             $pathName
         ));
+
+        $this->getSharedStorage()->set($imageReference, $imageId->getValue());
     }
 
     /**
@@ -76,7 +81,39 @@ class ProductImageFeatureContext extends AbstractProductFeatureContext
     public function assertProductImages(string $productReference, TableNode $tableNode): void
     {
         $images = $this->getProductImages($productReference);
-        //@todo: how to assert images if uploaded ones doesn't have original name?
+        $dataRows = $tableNode->getColumnsHash();
+
+        Assert::assertEquals(
+            count($images),
+            count($dataRows),
+            'Expected and actual images count does not match'
+        );
+
+        foreach ($dataRows as $key => $dataRow) {
+            $actualImage = $images[$key];
+
+            Assert::assertEquals(
+                PrimitiveUtils::castStringBooleanIntoBoolean($dataRow['is cover']),
+                $actualImage->isCover(),
+                'Unexpected cover image'
+            );
+            Assert::assertEquals(
+                $this->parseLocalizedArray($dataRow['legend']),
+                $actualImage->getLocalizedLegends(),
+                'Unexpected image legend'
+            );
+            Assert::assertEquals(
+                PrimitiveUtils::castStringIntegerIntoInteger($dataRow['position']),
+                $actualImage->getPosition(),
+                'Unexpected image position'
+            );
+
+            $path = _PS_PROD_IMG_DIR_ . $actualImage->getPath();
+
+            if (!file_exists($path)) {
+                throw new RuntimeException(sprintf('File "%s" does not exist', $path));
+            }
+        }
     }
 
     /**
