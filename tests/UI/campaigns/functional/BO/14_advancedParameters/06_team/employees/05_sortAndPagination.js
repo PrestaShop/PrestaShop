26ba148c7eba8765e3b622c1483c4db4f8dfb3e8
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2020-10-27 15:10:38 +0100

    Create employees

---
+++ tests/UI/campaigns/functional/BO/14_advancedParameters/06_team/employees/05_sortAndPagination.js
@@ -0,0 +1,96 @@
+require('module-alias/register');
+
+const {expect} = require('chai');
+
+// Import utils
+const helper = require('@utils/helpers');
+const loginCommon = require('@commonTests/loginBO');
+
+// Import data
+const EmployeeFaker = require('@data/faker/employee');
+
+// Import pages
+const dashboardPage = require('@pages/BO/dashboard/index');
+const employeesPage = require('@pages/BO/advancedParameters/team/index');
+const addEmployeePage = require('@pages/BO/advancedParameters/team/add');
+
+// Import test context
+const testContext = require('@utils/testContext');
+
+const baseContext = 'functional_BO_advancedParams_team_employees_sortAndPagination';
+
+let browserContext;
+let page;
+let numberOfEmployees = 0;
+
+/*
+Create 20 employees
+Sort employee list
+Pagination
+Delete created employees
+ */
+describe('Sort and pagination employees', async () => {
+  // before and after functions
+  before(async function () {
+    browserContext = await helper.createBrowserContext(this.browser);
+    page = await helper.newTab(browserContext);
+  });
+
+  after(async () => {
+    await helper.closeBrowserContext(browserContext);
+  });
+
+  it('should login in BO', async function () {
+    await loginCommon.loginBO(this, page);
+  });
+
+  it('should go to "Advanced parameters > Team" page', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'goToAdvancedParamsPage', baseContext);
+
+    await dashboardPage.goToSubMenu(
+      page,
+      dashboardPage.advancedParametersLink,
+      dashboardPage.teamLink,
+    );
+
+    await employeesPage.closeSfToolBar(page);
+
+    const pageTitle = await employeesPage.getPageTitle(page);
+    await expect(pageTitle).to.contains(employeesPage.pageTitle);
+  });
+
+  it('should reset all filters and get number of employees', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'resetFilterFirst', baseContext);
+
+    numberOfEmployees = await employeesPage.resetAndGetNumberOfLines(page);
+    await expect(numberOfEmployees).to.be.above(0);
+  });
+
+  // 1 : Create 20 employees
+
+  const tests = new Array(20).fill(0, 0, 20);
+
+  tests.forEach((test, index) => {
+    const employeeData = new EmployeeFaker({firstName: `toDelete`});
+    describe(`Create employee n°${index + 1} in BO`, async () => {
+      it('should go to add new employee page', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', `goToNewEmployeePage${index + 1}`, baseContext);
+
+        await employeesPage.goToAddNewEmployeePage(page);
+
+        const pageTitle = await addEmployeePage.getPageTitle(page);
+        await expect(pageTitle).to.contains(addEmployeePage.pageTitleCreate);
+      });
+
+      it('should create employee', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', `createEmployee${index + 1}`, baseContext);
+
+        const textResult = await addEmployeePage.createEditEmployee(page, employeeData);
+        await expect(textResult).to.equal(employeesPage.successfulCreationMessage);
+
+        const numberOfEmployeesAfterCreation = await employeesPage.getNumberOfElementInGrid(page);
+        await expect(numberOfEmployeesAfterCreation).to.be.equal(numberOfEmployees + index + 1);
+      });
+    });
+  });
+});
