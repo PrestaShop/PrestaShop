dcce268749215a8658c78ed91f10bb4295222f37
Ibrahima SOW <ibrahima.sow@prestashop.com>
2020-11-03 18:09:11 +0000

    Refresh product list when a product is deleted + loading spinner + display new products at top of list

--- src/Adapter/Order/QueryHandler/GetOrderProductsForViewingHandler.php
+++ src/Adapter/Order/QueryHandler/GetOrderProductsForViewingHandler.php
@@ -169,7 +169,8 @@ final class GetOrderProductsForViewingHandler extends AbstractOrderHandler imple
 
         unset($product);
 
-        ksort($products);
+        // reorder products by order_detail_id DESC
+        krsort($products);
 
         $productsForViewing = [];
 
