d6cbc0372bf7594c5d53c09fdfbb9fec140cb17c
matthieu rolland <matthieu.rolland@prestashop.com>
2020-11-09 11:36:52 +0100

    take case sensitivity into account

--- tests/Integration/Behaviour/Features/Context/ShopFeatureContext.php
+++ tests/Integration/Behaviour/Features/Context/ShopFeatureContext.php
@@ -139,7 +139,7 @@ class ShopFeatureContext extends AbstractDomainFeatureContext
         foreach ($expectedShops as $key => $currentExpectedShop) {
             $wasCurrentExpectedShopFound = false;
             foreach ($foundShops as $currentFoundShop) {
-                if ($currentExpectedShop['name'] == $currentFoundShop['name']) {
+                if (strtolower($currentExpectedShop['name']) == strtolower($currentFoundShop['name'])) {
                     $wasCurrentExpectedShopFound = true;
                     Assert::assertEquals(
                         $currentExpectedShop['group_name'],
