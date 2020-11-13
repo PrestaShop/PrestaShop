453dd9d2a69d6bead6237c7b06c3738ef3675862
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2020-11-13 09:19:20 +0100

    Create 11 orders then delete by bulk actions

--- tests/UI/campaigns/functional/BO/14_advancedParameters/04_email/03_sortAndPagination.js
+++ tests/UI/campaigns/functional/BO/14_advancedParameters/04_email/03_sortAndPagination.js
@@ -9,6 +9,7 @@ const loginCommon = require('@commonTests/loginBO');
 // Import pages
 const dashboardPage = require('@pages/BO/dashboard');
 const emailPage = require('@pages/BO/advancedParameters/email');
+const foLoginPage = require('@pages/FO/login');
 const homePage = require('@pages/FO/home');
 const productPage = require('@pages/FO/product');
 const cartPage = require('@pages/FO/cart');
@@ -30,12 +31,11 @@ let page;
 let numberOfEmails = 0;
 
 /*
-Create an order to have 2 email logs in email table
-Filter email logs list
-Delete email log
-Delete email logs by bulk action
+Create 10 orders to have 20 emails
+Pagination
+Sort by Id, Recipient, Template, Language, Subject, Send
  */
-describe('Filter, delete and bulk actions email log', async () => {
+describe('Sort and pagination emails', async () => {
   // before and after functions
   before(async function () {
     browserContext = await helper.createBrowserContext(this.browser);
@@ -50,60 +50,105 @@ describe('Filter, delete and bulk actions email log', async () => {
     await loginCommon.loginBO(this, page);
   });
 
-  describe('Create order to have email logs', async () => {
-    it('should go to FO and create an order', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'createOrderInFO', baseContext);
+  it('should go to FO page', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'goToFO', baseContext);
 
-      // Click on view my shop
-      page = await dashboardPage.viewMyShop(page);
+    // Click on view my shop
+    page = await dashboardPage.viewMyShop(page);
 
-      // Change language in FO
-      await homePage.changeLanguage(page, 'en');
+    // Change language on FO
+    await homePage.changeLanguage(page, 'en');
 
-      // Go to the first product page
-      await homePage.goToProductPage(page, 1);
+    const isHomePage = await homePage.isHomePage(page);
+    await expect(isHomePage, 'Fail to open FO home page').to.be.true;
+  });
 
-      // Add the created product to the cart
-      await productPage.addProductToTheCart(page);
+  // 1 - Create 11 orders to have 22 emails in the list
+  const tests = new Array(11).fill(0, 0, 11);
 
-      // Proceed to checkout the shopping cart
-      await cartPage.clickOnProceedToCheckout(page);
+  tests.forEach((test, index) => {
+    describe(`Create order n°${index + 1} to have email logs`, async () => {
+      it('should go to login page', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'goToLoginFO', baseContext);
 
-      // Checkout the order
+        await homePage.goToLoginPage(page);
 
-      // Personal information step - Login
-      await checkoutPage.clickOnSignIn(page);
-      await checkoutPage.customerLogin(page, DefaultAccount);
+        const pageTitle = await foLoginPage.getPageTitle(page);
+        await expect(pageTitle, 'Fail to open FO login page').to.contains(foLoginPage.pageTitle);
+      });
 
-      // Address step - Go to delivery step
-      const isStepAddressComplete = await checkoutPage.goToDeliveryStep(page);
-      await expect(isStepAddressComplete, 'Step Address is not complete').to.be.true;
+      it('should sign in with default customer', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'sighInFO', baseContext);
 
-      // Delivery step - Go to payment step
-      const isStepDeliveryComplete = await checkoutPage.goToPaymentStep(page);
-      await expect(isStepDeliveryComplete, 'Step Address is not complete').to.be.true;
+        await foLoginPage.customerLogin(page, DefaultAccount);
 
-      // Payment step - Choose payment step
-      await checkoutPage.choosePaymentAndOrder(page, PaymentMethods.wirePayment.moduleName);
+        const isCustomerConnected = await foLoginPage.isCustomerConnected(page);
+        await expect(isCustomerConnected, 'Customer is not connected').to.be.true;
+      });
 
-      // Check the confirmation message
-      const cardTitle = await orderConfirmationPage.getOrderConfirmationCardTitle(page);
-      await expect(cardTitle).to.contains(orderConfirmationPage.orderConfirmationCardTitle);
+      it('should create an order', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'createOrder', baseContext);
 
-      // Logout from FO
-      await orderConfirmationPage.logout(page);
+        // Go to home page
+        await foLoginPage.goToHomePage(page);
 
-      // Go Back to BO
-      page = await orderConfirmationPage.closePage(browserContext, page, 0);
+        // Go to the first product page
+        await homePage.goToProductPage(page, 1);
+
+        // Add the created product to the cart
+        await productPage.addProductToTheCart(page);
+
+        // Proceed to checkout the shopping cart
+        await cartPage.clickOnProceedToCheckout(page);
+
+        // Address step - Go to delivery step
+        const isStepAddressComplete = await checkoutPage.goToDeliveryStep(page);
+        await expect(isStepAddressComplete, 'Step Address is not complete').to.be.true;
+
+        // Delivery step - Go to payment step
+        const isStepDeliveryComplete = await checkoutPage.goToPaymentStep(page);
+        await expect(isStepDeliveryComplete, 'Step Address is not complete').to.be.true;
+
+        // Payment step - Choose payment step
+        await checkoutPage.choosePaymentAndOrder(page, PaymentMethods.wirePayment.moduleName);
+
+        // Check the confirmation message
+        const cardTitle = await orderConfirmationPage.getOrderConfirmationCardTitle(page);
+        await expect(cardTitle).to.contains(orderConfirmationPage.orderConfirmationCardTitle);
+      });
+
+      it('should sign out from FO', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'sighOutFO', baseContext);
+
+        await orderConfirmationPage.logout(page);
+        const isCustomerConnected = await orderConfirmationPage.isCustomerConnected(page);
+        await expect(isCustomerConnected, 'Customer is connected').to.be.false;
+      });
     });
   });
 
-  /*describe('Delete email logs by bulk action', async () => {
-    it('should delete all email logs', async function () {
+  // 4 - Delete all emails
+  describe('Delete emails by bulk action', async () => {
+    it('should go to \'Advanced parameters > E-mail\' page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToEmailPage', baseContext);
+
+      page = await orderConfirmationPage.closePage(browserContext, page, 0);
+
+      await dashboardPage.goToSubMenu(
+        page,
+        dashboardPage.advancedParametersLink,
+        dashboardPage.emailLink,
+      );
+
+      const pageTitle = await emailPage.getPageTitle(page);
+      await expect(pageTitle).to.contains(emailPage.pageTitle);
+    });
+
+    it('should delete all emails', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'BulkDelete', baseContext);
 
       const deleteTextResult = await emailPage.deleteEmailLogsBulkActions(page);
       await expect(deleteTextResult).to.be.equal(emailPage.successfulMultiDeleteMessage);
     });
-  });*/
+  });
 });
