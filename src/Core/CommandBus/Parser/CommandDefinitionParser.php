528d975df98257d14eb1762fb8e4d900daabd91c
Julius Zukauskas <zuk3975@gmail.com>
2020-11-17 19:20:41 +0200

    revert removing CommandDefinition + parser and its tests

---
+++ src/Core/CommandBus/Parser/CommandDefinitionParser.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+namespace PrestaShop\PrestaShop\Core\CommandBus\Parser;
+
+use ReflectionClass;
+use ReflectionException;
+
+/**
+ * Parses commands and queries definitions
+ */
+final class CommandDefinitionParser
+{
+    /**
+     * @param string $commandName
+     *
+     * @return CommandDefinition
+     *
+     * @throws ReflectionException
+     */
+    public function parseDefinition($commandName)
+    {
+        return new CommandDefinition(
+            $commandName,
+            $this->parseType($commandName),
+            $this->parseDescription($commandName)
+        );
+    }
+
+    /**
+     * Checks whether the command is of type Query or Command by provided name
+     *
+     * @param string $commandName
+     *
+     * @return string
+     */
+    private function parseType($commandName)
+    {
+        if (strpos($commandName, '\Command\\')) {
+            return 'Command';
+        }
+
+        return 'Query';
+    }
+
+    /**
+     * @param string $commandName
+     *
+     * @return string
+     *
+     * @throws ReflectionException
+     */
+    private function parseDescription($commandName)
+    {
+        /**
+         * Removes comment symbols, annotations, and line breaks.
+         */
+        $commandName = preg_replace("/\/+\*\*|\*+\/|\*|@(\w+)\b(.*)|\n/",
+            '',
+            (new ReflectionClass($commandName))->getDocComment()
+        );
+
+        /**
+         * Replaces multiple spaces to single space
+         */
+        $commandName = preg_replace('/ +/', ' ', $commandName);
+
+        /*
+         * Strips whitespace from the beginning and end
+         */
+        return trim($commandName);
+    }
+}
