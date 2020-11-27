8acc2d7bd610546697933d7ff76fc22f59e01836
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2020-11-27 16:05:38 +0100

    Test delete button

--- tests/UI/campaigns/data/faker/shopGroup.js
+++ tests/UI/campaigns/data/faker/shopGroup.js
@@ -2,7 +2,7 @@ const faker = require('faker');
 
 module.exports = class ShopGroup {
   constructor(shopGroupToCreate) {
-    this.name = shopGroupToCreate.name || `shop_${faker.lorem.word()}`;
+    this.name = shopGroupToCreate.name || `shop_group_${faker.lorem.word()}`;
     this.shareCustomer = shopGroupToCreate.shareCustomer === undefined ? true : shopGroupToCreate.shareCustomer;
     this.shareAvailableQuantities = shopGroupToCreate.shareAvailableQuantities
     === undefined ? true : shopGroupToCreate.shareAvailableQuantities;
