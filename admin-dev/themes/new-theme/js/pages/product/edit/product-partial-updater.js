9d89201dd98ad5b6ee19ff32dffcba27255ebb44
Jonathan Lelievre <jo.lelievre@gmail.com>
2020-11-30 20:51:32 +0100

    Add ProductPartialUpdater which is able to compute updated data and only post the diff

---
+++ admin-dev/themes/new-theme/js/pages/product/edit/product-partial-updater.js
@@ -0,0 +1,93 @@
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+const {$} = window;
+
+export default class ProductPartialUpdater {
+  watch($productForm) {
+    this.$productForm = $productForm;
+    this.initialData = this.getFormDataAsObject();
+    this.$productForm.submit((e) => this.updatePartialForm(e));
+  }
+
+  updatePartialForm(event) {
+    event.stopImmediatePropagation();
+
+    const updatedData = this.getUpdatedFormData();
+    if (null !== updatedData) {
+      this.postUpdatedData(updatedData);
+    } else {
+      console.log('no update');
+    }
+
+    return false;
+  }
+
+  postUpdatedData(updatedData) {
+    const $updatedForm = this.$productForm.clone();
+    $updatedForm.empty();
+    Object.keys(updatedData).forEach((fieldName) => {
+      $("<input>").attr({
+        name: fieldName,
+        type: "hidden",
+        value: updatedData[fieldName]
+      }).appendTo($updatedForm);
+    });
+
+    $updatedForm.appendTo('body');
+    $updatedForm.submit();
+  }
+
+  getUpdatedFormData() {
+    let currentData = this.getFormDataAsObject();
+    // Loop through current form data and remove the one that did not change
+    // This way only updated AND new values remain
+    Object.keys(this.initialData).forEach((fieldName) => {
+      const fieldValue = this.initialData[fieldName];
+      if (currentData[fieldName] === fieldValue) {
+        delete currentData[fieldName];
+      }
+    });
+
+    if (Object.keys(currentData).length === 0) {
+      return null;
+    }
+
+    // We still need the token
+    currentData['product[_token]'] = this.initialData['product[_token]'];
+
+    return currentData;
+  }
+
+  getFormDataAsObject() {
+    const formArray = this.$productForm.serializeArray();
+    let serializedForm = {};
+    formArray.forEach((formField) => {
+      serializedForm[formField.name] = formField.value;
+    });
+
+    return serializedForm;
+  }
+}
