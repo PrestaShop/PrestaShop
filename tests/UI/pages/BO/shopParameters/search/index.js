465f5cf8896254087ccba4a4e6bb7a40ba3df8d9
Boubker BRIBRI <boubker.bribri@prestashop.com>
2020-11-30 10:21:30 +0100

    Add tests search and tag

---
+++ tests/UI/pages/BO/shopParameters/search/index.js
@@ -0,0 +1,200 @@
+require('module-alias/register');
+const BOBasePage = require('@pages/BO/BObasePage');
+
+class Search extends BOBasePage {
+  constructor() {
+    super();
+
+    this.pageTitle = 'Search •';
+    this.successfulCreationMessage = 'Creation successful';
+
+    // Selectors
+    // Header links
+    this.addNewAliasLink = '#page-header-desc-alias-new_alias';
+
+    // Tabs
+    this.tagsTabLink = '#subtab-AdminTags';
+
+    // Form selectors
+    this.gridForm = '#form-alias';
+    this.gridTableHeaderTitle = `${this.gridForm} .panel-heading`;
+    this.gridTableNumberOfTitlesSpan = `${this.gridTableHeaderTitle} span.badge`;
+
+    // Table selectors
+    this.gridTable = '#table-alias';
+
+    // Filter selectors
+    this.filterRow = `${this.gridTable} tr.filter`;
+    this.filterColumn = filterBy => `${this.filterRow} [name='aliasFilter_${filterBy}']`;
+    this.filterSearchButton = '#submitFilterButtonalias';
+    this.filterResetButton = 'button[name=\'submitResetalias\']';
+
+    // Table body selectors
+    this.tableBody = `${this.gridTable} tbody`;
+    this.tableBodyRows = `${this.tableBody} tr`;
+    this.tableBodyRow = row => `${this.tableBodyRows}:nth-child(${row})`;
+    this.tableBodyColumn = row => `${this.tableBodyRow(row)} td`;
+
+    // Row actions selectors
+    this.tableColumnActions = row => `${this.tableBodyColumn(row)} .btn-group-action`;
+    this.tableColumnActionsEditLink = row => `${this.tableColumnActions(row)} a.edit`;
+    this.tableColumnActionsToggleButton = row => `${this.tableColumnActions(row)} button.dropdown-toggle`;
+    this.tableColumnActionsDropdownMenu = row => `${this.tableColumnActions(row)} .dropdown-menu`;
+    this.tableColumnActionsDeleteLink = row => `${this.tableColumnActionsDropdownMenu(row)} a.delete`;
+
+    // Confirmation modal
+    this.deleteModalButtonYes = '#popup_ok';
+
+    // Columns selectors
+    this.tableColumnAliases = row => `${this.tableBodyColumn(row)}:nth-child(2)`;
+    this.tableColumnSearch = row => `${this.tableBodyColumn(row)}:nth-child(3)`;
+    this.tableColumnStatus = row => `${this.tableBodyColumn(row)}:nth-child(4)`;
+  }
+
+  /*
+  Methods
+   */
+
+  /* Header methods */
+  /**
+   * Go to add new alias page
+   * @param page
+   * @returns {Promise<void>}
+   */
+  async goToAddNewAliasPage(page) {
+    await this.clickAndWaitForNavigation(page, this.addNewAliasLink);
+  }
+
+  /**
+   * Go to tags page
+   * @param page
+   * @returns {Promise<void>}
+   */
+  async goToTagsPage(page) {
+    await this.clickAndWaitForNavigation(page, this.tagsTabLink);
+  }
+
+  /* Filter methods */
+  /**
+   * Get Number of lines
+   * @param page
+   * @return {Promise<number>}
+   */
+  getNumberOfElementInGrid(page) {
+    return this.getNumberFromText(page, this.gridTableNumberOfTitlesSpan);
+  }
+
+  /**
+   * Reset all filters
+   * @param page
+   * @return {Promise<void>}
+   */
+  async resetFilter(page) {
+    if (!(await this.elementNotVisible(page, this.filterResetButton, 2000))) {
+      await this.clickAndWaitForNavigation(page, this.filterResetButton);
+    }
+    await this.waitForVisibleSelector(page, this.filterSearchButton, 2000);
+  }
+
+  /**
+   * Reset and get number of lines
+   * @param page
+   * @return {Promise<number>}
+   */
+  async resetAndGetNumberOfLines(page) {
+    await this.resetFilter(page);
+    return this.getNumberOfElementInGrid(page);
+  }
+
+  /**
+   * Filter aliases
+   * @param page
+   * @param filterType
+   * @param filterBy
+   * @param value
+   * @return {Promise<void>}
+   */
+  async filterTable(page, filterType, filterBy, value) {
+    switch (filterType) {
+      case 'input':
+        await this.setValue(page, this.filterColumn(filterBy), value.toString());
+        await this.clickAndWaitForNavigation(page, this.filterSearchButton);
+        break;
+
+      case 'select':
+        await Promise.all([
+          page.waitForNavigation({waitUntil: 'networkidle'}),
+          this.selectByVisibleText(page, this.filterColumn(filterBy), value ? 'Yes' : 'No'),
+        ]);
+        break;
+
+      default:
+        throw new Error(`Filter ${filterBy} was not found`);
+    }
+  }
+
+  /* Column methods */
+  /**
+   * Go to edit page
+   * @param page
+   * @param row
+   * @return {Promise<void>}
+   */
+  async gotoEditAliasPage(page, row) {
+    await this.clickAndWaitForNavigation(page, this.tableColumnActionsEditLink(row));
+  }
+
+  /**
+   * Get text from column in table
+   * @param page
+   * @param row
+   * @param columnName
+   * @return {Promise<string>}
+   */
+  async getTextColumn(page, row, columnName) {
+    let columnSelector;
+
+    switch (columnName) {
+      case 'alias':
+        columnSelector = this.tableColumnAliases(row);
+        break;
+
+      case 'result':
+        columnSelector = this.tableColumnSearch(row);
+        break;
+
+      case 'active':
+        columnSelector = this.tableColumnStatus(row);
+        break;
+
+      default:
+        throw new Error(`Column ${columnName} was not found`);
+    }
+
+    return this.getTextContent(page, columnSelector);
+  }
+
+
+  /**
+   * Delete alias from row
+   * @param page
+   * @param row
+   * @return {Promise<string>}
+   */
+  async deleteAlias(page, row) {
+    await Promise.all([
+      page.click(this.tableColumnActionsToggleButton(row)),
+      this.waitForVisibleSelector(page, this.tableColumnActionsDeleteLink(row)),
+    ]);
+
+    await page.click(this.tableColumnActionsDeleteLink(row));
+
+    // Confirm delete action
+    await this.clickAndWaitForNavigation(page, this.deleteModalButtonYes);
+
+    // Get successful message
+    return this.getTextContent(page, this.alertSuccessBlock);
+  }
+}
+
+module.exports = new Search();
