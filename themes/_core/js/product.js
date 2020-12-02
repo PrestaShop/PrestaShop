484438932694494f89011d315d3f46bbe89d4185
Daniel Hlavacek <daniel.hlavacek@hotmail.cz>
2020-12-02 12:54:19 +0100

    Use a new selector

--- themes/_core/js/product.js
+++ themes/_core/js/product.js
@@ -220,7 +220,7 @@ function updateProduct(event, eventType, updateUrl) {
         $(prestashop.selectors.product.customization)
           .first()
           .replaceWith(data.product_customization);
-        $('.product-actions input[name=id_customization]').val(0);
+        $(prestashop.selectors.product.input_customization).val(0);
         $(prestashop.selectors.product.variantsUpdate)
           .first()
           .replaceWith(data.product_variants);
