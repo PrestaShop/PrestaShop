7f123bc5419924cc902673ad012e94296b70c856
matthieu-rolland <matthieu.rolland@prestashop.com>
2020-12-03 16:14:16 +0100

    move upgrade query from 1.7.7.0 to 1.7.7.1 sql file

--- install-dev/upgrade/sql/1.7.7.1.sql
+++ install-dev/upgrade/sql/1.7.7.1.sql
@@ -1,1 +1,9 @@
 /* PHP:ps_1771_update_customer_note(); */;
+
+SET SESSION sql_mode='';
+SET NAMES 'utf8';
+
+/* add id_order in cart */
+ALTER TABLE `PREFIX_cart`
+    ADD `id_order` INT(10) UNSIGNED NULL DEFAULT NULL,
+    ADD INDEX (id_order);
