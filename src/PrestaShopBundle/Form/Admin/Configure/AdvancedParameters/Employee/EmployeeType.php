3b348de6ebc9dd01f4b3b73bb6f70f8c86814dd1
JevgenijVisockij <47781757+JevgenijVisockij@users.noreply.github.com>
2020-12-09 17:18:24 +0200

    Update src/PrestaShopBundle/Form/Admin/Configure/AdvancedParameters/Employee/EmployeeType.php
    Removed unneeded full class namespace
    
    Co-authored-by: Progi1984 <progi1984@gmail.com>

--- src/PrestaShopBundle/Form/Admin/Configure/AdvancedParameters/Employee/EmployeeType.php
+++ src/PrestaShopBundle/Form/Admin/Configure/AdvancedParameters/Employee/EmployeeType.php
@@ -106,7 +106,7 @@ final class EmployeeType extends TranslatorAwareType
      * @param string $defaultAvatarUrl
      * @param bool $isAddonsConnected
      * @param int $superAdminProfileId
-     * @param \Symfony\Bundle\FrameworkBundle\Routing\Router $router
+     * @param Router $router
      */
     public function __construct(
         TranslatorInterface $translator,
