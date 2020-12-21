b18b7ad331e845680481407651087f762fbb9e98
PululuK <pululuandre@hotmail.com>
2020-12-21 13:01:37 +0100

    Update src/PrestaShopBundle/Entity/Repository/RequestSqlRepository.php
    Co-authored-by: Arman Hosseini <namra.1377@gmail.com>

--- src/PrestaShopBundle/Entity/Repository/RequestSqlRepository.php
+++ src/PrestaShopBundle/Entity/Repository/RequestSqlRepository.php
@@ -144,7 +144,7 @@ class RequestSqlRepository implements RepositoryInterface, DoctrineQueryBuilderI
      *
      * @param SearchCriteriaInterface $criteria
      *
-     * @deprecated deprecated since 1.7.8.0
+     * @deprecated since 1.7.8.0
      * @see RequestSqlQueryBuilder::buildQueryBySearchCriteria
      *
      * @return QueryBuilder
