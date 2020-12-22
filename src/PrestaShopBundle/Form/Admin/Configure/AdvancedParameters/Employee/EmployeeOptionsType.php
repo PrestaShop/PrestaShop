b953ca7d225bc6d0744e22cb7993ccbbb902c329
Jevgenij Visockij <jevgenij.visockij@invertus.eu>
2020-12-22 11:30:12 +0200

    Simplified team forms

--- src/PrestaShopBundle/Form/Admin/Configure/AdvancedParameters/Employee/EmployeeOptionsType.php
+++ src/PrestaShopBundle/Form/Admin/Configure/AdvancedParameters/Employee/EmployeeOptionsType.php
@@ -64,11 +64,15 @@ class EmployeeOptionsType extends TranslatorAwareType
     {
         $builder
             ->add('password_change_time', TextWithUnitType::class, [
+                'label' => $this->trans('Password regeneration', 'Admin.Advparameters.Feature'),
+                'help' => $this->trans('Security: Minimum time to wait between two password changes.', 'Admin.Advparameters.Feature'),
                 'required' => false,
                 'unit' => $this->trans('minutes', 'Admin.Advparameters.Feature'),
                 'disabled' => !$this->canOptionsBeChanged,
             ])
             ->add('allow_employee_specific_language', SwitchType::class, [
+                'label' => $this->trans('Memorize the language used in Admin panel forms', 'Admin.Advparameters.Feature'),
+                'help' => $this->trans('Allow employees to select a specific language for the Admin panel form.', 'Admin.Advparameters.Feature'),
                 'required' => false,
                 'disabled' => !$this->canOptionsBeChanged,
             ]);
