3c81c9729af05b1e507be0774b1b4f2a16361e6f
JevgenijVisockij <47781757+JevgenijVisockij@users.noreply.github.com>
2020-12-23 16:19:02 +0200

    Update src/PrestaShopBundle/Form/Admin/Improve/Design/Pages/CmsPageCategoryType.php
    Changed domain to an updated one.
    
    Co-authored-by: Julie Varisellaz <70583503+Julievrz@users.noreply.github.com>

--- src/PrestaShopBundle/Form/Admin/Improve/Design/Pages/CmsPageCategoryType.php
+++ src/PrestaShopBundle/Form/Admin/Improve/Design/Pages/CmsPageCategoryType.php
@@ -74,7 +74,7 @@ class CmsPageCategoryType extends TranslatorAwareType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $invalidCharactersForCatalogLabel = $this->trans('Invalid characters:', 'Admin.Notifications.Info') .  '<>;=#{}';
+        $invalidCharactersForCatalogLabel = $this->trans('Invalid characters:', 'Admin.Global') .  '<>;=#{}';
         $invalidCharactersForNameLabel = $this->trans('Invalid characters:', 'Admin.Notifications.Info') . '<>={}';
         $builder
             ->add('name', TranslatableType::class, [
