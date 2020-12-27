9e4693e58fed13ae2e1cf341a70a28a14d2d6a0f
Boubker Bribri <boubker.bribri@prestashop.com>
2020-12-27 18:43:36 +0100

    Migrate sanity tests to github actions
    Fix apache job name
    
    Moving sanity file
    
    Fix sudo call
    
    Delete sanity run from travis
    
    Update sanity job to add env vars
    
    Update sanity to job to add env vars
    
    Fix travis script
    
    Rename sanity fast fail command

---
+++ .github/workflows/sanity/setup-apache.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Get vars
+WORKSPACE=$1
+PHP_VERSION=$2
+
+# Install apache
+apt-get update && apt-get install apache2 libapache2-mod-php$PHP_VERSION
+
+# Enable rewrite mode
+a2enmod rewrite actions alias
+
+# Copy apache vhost and set Documentroot
+cp -f $WORKSPACE/.github/workflows/sanity/apache-vhost /etc/apache2/sites-available/000-default.conf
+sed -e "s?%BUILD_DIR%?$(echo $WORKSPACE)?g" --in-place /etc/apache2/sites-available/000-default.conf
+
+# Restart apache after giving permission
+chmod 777 -R $WORKSPACE
+service apache2 restart
