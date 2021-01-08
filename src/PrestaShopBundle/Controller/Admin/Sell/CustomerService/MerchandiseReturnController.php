0f593aee3c10d54b886f4496a96e2e05a66a8692
JevgenijVisockij <47781757+JevgenijVisockij@users.noreply.github.com>
2021-01-08 16:22:32 +0200

    Update src/PrestaShopBundle/Controller/Admin/Sell/CustomerService/MerchandiseReturnController.php
    Changed from comma to brackets
    
    Co-authored-by: Julie Varisellaz <70583503+Julievrz@users.noreply.github.com>

--- src/PrestaShopBundle/Controller/Admin/Sell/CustomerService/MerchandiseReturnController.php
+++ src/PrestaShopBundle/Controller/Admin/Sell/CustomerService/MerchandiseReturnController.php
@@ -298,7 +298,7 @@ class MerchandiseReturnController extends FrameworkBundleAdminController
                     'Admin.Notifications.Error'
                 ),
                 DeleteOrderReturnProductException::LAST_ORDER_RETURN_PRODUCT => $this->trans(
-                    'An error occurred while deleting this product, can\'t delete last product in order return.',
+                    'An error occurred while deleting this product (can\'t delete last product in order return).',
                     'Admin.Notifications.Error'
                 ),
                 DeleteOrderReturnProductException::UNEXPECTED_ERROR => $this->trans(
