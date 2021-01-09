e3eecc83b1a4402eb226f31892622a8f8ab12605
David Gonzalez <davidgg666@gmail.com>
2021-01-09 12:37:26 +0100

    Do not cast $nbDaysNewProduct

--- classes/Product.php
+++ classes/Product.php
@@ -1561,7 +1561,7 @@ class ProductCore extends ObjectModel
             FROM `' . _DB_PREFIX_ . 'product` p
             ' . Shop::addSqlAssociation('product', 'p') . '
             WHERE p.id_product = ' . (int) $this->id . '
-            AND DATEDIFF("' . date('Y-m-d') . ' 00:00:00", product_shop.`date_add`) < ' . (int) $nbDaysNewProduct;
+            AND DATEDIFF("' . date('Y-m-d') . ' 00:00:00", product_shop.`date_add`) < ' . $nbDaysNewProduct;
 
         return (bool) Db::getInstance()->getValue($query, true, false);
     }
