0f8e468b3260d86052d182d38e9af7e79a351e2e
Jonathan Lelievre <jo.lelievre@gmail.com>
2021-01-11 19:01:41 +0100

    refacto test class a little to include additional tests more easily, handle multi checbox weird management in tests

--- tests/Integration/PrestaShopBundle/Controller/Admin/Sell/Catalog/Product/ProductControllerTest.php
+++ tests/Integration/PrestaShopBundle/Controller/Admin/Sell/Catalog/Product/ProductControllerTest.php
@@ -2,9 +2,28 @@
 
 namespace Tests\Integration\PrestaShopBundle\Controller\Admin\Sell\Catalog\Product;
 
+use PrestaShop\PrestaShop\Core\Form\IdentifiableObject\CommandBuilder\Product\ProductCommandsBuilder;
+use PrestaShop\PrestaShop\Core\Form\IdentifiableObject\DataHandler\ProductFormDataHandler;
+use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\DomCrawler\Field\ChoiceFormField;
+use Symfony\Component\DomCrawler\Field\FormField;
+use Symfony\Component\DomCrawler\Form;
 
 /**
+ * This integration test is mostly used to ensure the partial update mechanism is correctly handled, the purpose is to check
+ * that with a set number of differences only partial data is provided to the ProductFormDataHandler so that it can then create
+ * only required CQRS commands.
+ *
+ * We don't check which commands are created it is the responsibility of the ProductFormDataHandler, we only check that the controller
+ * handles the request correctly so that only partial data is provided to the handler.
+ *
+ * For more details about CQRS commands partial generation:
+ *
+ * @see ProductFormDataHandler
+ * @see ProductCommandsBuilder
+ *
  * To run this test run this command from project root:
  * php -d date.timezone=UTC ./vendor/bin/phpunit -c tests/Integration/phpunit.xml tests/Integration/PrestaShopBundle/Controller/Admin/Sell/Catalog/Product/ProductControllerTest.php
  */
@@ -26,35 +45,26 @@ class ProductControllerTest extends WebTestCase
         $this->handleStrictPartialUpdate = false;
     }
 
-    public function testCreateProduct()
+    public function testCreateProduct(): int
     {
         $client = static::createClient();
         $router = $client->getKernel()->getContainer()->get('router');
         $createUrl = $router->generate('admin_products_v2_create');
         $crawler = $client->request('GET', $createUrl);
 
-        // Get button by id #product_save
-        $submitButton = $crawler->selectButton('product_save');
-        $productForm = $submitButton->form();
-
         // Only product name in default language should be required on creation
-        $productForm['product[basic][name][1]'] = 'Test Product';
-
+        $productForm = $this->fillProductForm($crawler, ['product[basic][name][1]' => 'Test Product']);
         $client->submit($productForm);
-        $response = $client->getResponse();
 
         // If creation happens correctly then we are redirect to the edition page
-        $this->assertTrue($response->isRedirection());
-        $redirectUrl = $response->headers->get('Location');
+        $createdProductId = $this->assertSuccessfulRedirection($client);
 
-        $parsedUrl = parse_url($redirectUrl);
-        $routerMatching = $router->match($parsedUrl['path']);
-        $this->assertArrayHasKey('_route', $routerMatching);
-        $this->assertEquals('admin_products_v2_edit', $routerMatching['_route']);
-        $this->assertArrayHasKey('productId', $routerMatching);
-        $this->assertGreaterThan(0, $routerMatching['productId']);
+        // Now we check that the correct data were use by the form handler
+        $dataChecker = $client->getContainer()->get('test.integration.core.form.identifiable_object.data_handler.product_form_data_handler_checker');
+        $createData = $dataChecker->getLastCreateData();
+        $this->assertHandlerData(['basic' => ['name' => [1 => 'Test Product']]], $createData);
 
-        return (int) $routerMatching['productId'];
+        return $createdProductId;
     }
 
     /**
@@ -72,36 +82,17 @@ class ProductControllerTest extends WebTestCase
         $createUrl = $router->generate('admin_products_v2_edit', ['productId' => $productId]);
         $crawler = $client->request('GET', $createUrl);
 
-        // Get button by id #product_save
-        $submitButton = $crawler->selectButton('product_save');
-        $productForm = $submitButton->form();
-        foreach ($formModifications as $formField => $formValue) {
-            $productForm[$formField] = 'Test Update Product';
-        }
-
+        $productForm = $this->fillProductForm($crawler, $formModifications);
         $client->submit($productForm);
-        $response = $client->getResponse();
 
         // If update happens correctly then we are redirect to the same edition page
-        $this->assertTrue($response->isRedirection());
-        $redirectUrl = $response->headers->get('Location');
-        $parsedUrl = parse_url($redirectUrl);
-        $routerMatching = $router->match($parsedUrl['path']);
-        $this->assertArrayHasKey('_route', $routerMatching);
-        $this->assertEquals('admin_products_v2_edit', $routerMatching['_route']);
-        $this->assertArrayHasKey('productId', $routerMatching);
-        $this->assertEquals($productId, $routerMatching['productId']);
+        $redirectionProductId = $this->assertSuccessfulRedirection($client);
+        $this->assertEquals($productId, $redirectionProductId);
 
         // Now we check that the correct data were use by the form handler
         $dataChecker = $client->getContainer()->get('test.integration.core.form.identifiable_object.data_handler.product_form_data_handler_checker');
         $updateData = $dataChecker->getLastUpdateData();
-        if ($this->handlePartialUpdate && !$this->handleStrictPartialUpdate) {
-            // This method is deprecated in PHPUnit for PHP 8.0 but we can't use more recent libraries that replace this
-            // because they require more recent version of PHP than ours, so for now we keep using this one
-            $this->assertArraySubset($expectedUpdateData, $updateData);
-        } else {
-            $this->assertEquals($expectedUpdateData, $updateData);
-        }
+        $this->assertHandlerData($expectedUpdateData, $updateData);
     }
 
     public function getProductEditionModifications()
@@ -120,5 +111,109 @@ class ProductControllerTest extends WebTestCase
                 ],
             ],
         ];
+
+        yield [
+            [
+                'product[shipping][carriers]' => [1, 2],
+            ],
+            [
+                'shipping' => [
+                    'carriers' => [1, 2],
+                ],
+            ],
+        ];
+
+        yield [
+            [
+                'product[shipping][carriers]' => [1, 3],
+            ],
+            [
+                'shipping' => [
+                    'carriers' => [1, 3],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @param array $expectedData
+     * @param array $handlerData
+     */
+    private function assertHandlerData(array $expectedData, array $handlerData): void
+    {
+        if ($this->handlePartialUpdate && !$this->handleStrictPartialUpdate) {
+            // This method is deprecated in PHPUnit for PHP 8.0 but we can't use more recent libraries that replace this
+            // because they require more recent version of PHP than ours, so for now we keep using this one
+            $this->assertArraySubset($expectedData, $handlerData);
+        } else {
+            $this->assertEquals($expectedData, $handlerData);
+        }
+    }
+
+    /**
+     * Check that request succeeded and therefore redirects to edition page
+     *
+     * @param Client $client
+     *
+     * @return int Returns the product ID from the redirection URL
+     */
+    private function assertSuccessfulRedirection(Client $client): int
+    {
+        $response = $client->getResponse();
+        $this->assertTrue($response->isRedirection());
+        $redirectUrl = $response->headers->get('Location');
+        $parsedUrl = parse_url($redirectUrl);
+
+        $router = $client->getContainer()->get('router');
+        $routerMatching = $router->match($parsedUrl['path']);
+        $this->assertArrayHasKey('_route', $routerMatching);
+        $this->assertEquals('admin_products_v2_edit', $routerMatching['_route']);
+        $this->assertArrayHasKey('productId', $routerMatching);
+        $this->assertGreaterThan(0, $routerMatching['productId']);
+
+        return (int) $routerMatching['productId'];
+    }
+
+    /**
+     * @param Crawler $crawler
+     * @param array $formModifications
+     *
+     * @return Form
+     */
+    private function fillProductForm(Crawler $crawler, array $formModifications): Form
+    {
+        // Get button by id #product_save
+        $submitButton = $crawler->selectButton('product_save');
+        $productForm = $submitButton->form();
+        foreach ($formModifications as $fieldName => $formValue) {
+            if (is_array($formValue)) {
+                // For multi select checkboxes or select inputs
+                /** @var ChoiceFormField[]|ChoiceFormField $formFields */
+                $formFields = $productForm->get($fieldName);
+                // Multiple checkboxes are returned as array
+                if (is_array($formFields)) {
+                    foreach ($formFields as $formField) {
+                        if ('checkbox' === $formField->getType()) {
+                            $optionValue = $formField->availableOptionValues()[0];
+                            if (in_array($optionValue, $formValue)) {
+                                $formField->tick();
+                            } else {
+                                $formField->untick();
+                            }
+                        } else {
+                            $formField->select($formValue);
+                        }
+                    }
+                } else {
+                    $formFields->select($formValue);
+                }
+            } else {
+                /** @var FormField $formField */
+                $formField = $productForm->get($fieldName);
+                $formField->setValue($formValue);
+            }
+        }
+
+        return $productForm;
     }
 }
