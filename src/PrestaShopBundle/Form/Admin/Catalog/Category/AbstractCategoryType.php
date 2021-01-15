97c67ffdbeccfc2ea6b6b9c5e2adb68682ff5a96
JevgenijVisockij <47781757+JevgenijVisockij@users.noreply.github.com>
2021-01-15 16:16:01 +0200

    Update src/PrestaShopBundle/Form/Admin/Catalog/Category/AbstractCategoryType.php
    Added dot to constraint message.
    
    Co-authored-by: Julie Varisellaz <70583503+Julievrz@users.noreply.github.com>

--- src/PrestaShopBundle/Form/Admin/Catalog/Category/AbstractCategoryType.php
+++ src/PrestaShopBundle/Form/Admin/Catalog/Category/AbstractCategoryType.php
@@ -283,7 +283,7 @@ abstract class AbstractCategoryType extends TranslatorAwareType
                         new Length([
                             'max' => SeoSettings::MAX_KEYWORDS_LENGTH,
                             'maxMessage' => $this->trans(
-                                'This field cannot be longer than %limit% characters',
+                                'This field cannot be longer than %limit% characters.',
                                 'Admin.Notifications.Error',
                                 [
                                     '%limit%' => SeoSettings::MAX_KEYWORDS_LENGTH,
