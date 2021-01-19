45aa31d27888dc9326048f633093673546df838f
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2021-01-19 12:12:25 +0100

    Add test for update tax excluded

--- tests/UI/campaigns/functional/BO/09_shipping/02_preferences/01_handling/01_handlingCharges.js
+++ tests/UI/campaigns/functional/BO/09_shipping/02_preferences/01_handling/01_handlingCharges.js
@@ -8,6 +8,9 @@ const loginCommon = require('@commonTests/loginBO');
 
 // Import pages
 const dashboardPage = require('@pages/BO/dashboard');
+const customerSettingsPage = require('@pages/BO/shopParameters/customerSettings');
+const groupsPage = require('@pages/BO/shopParameters/customerSettings/groups');
+const addGroupPage = require('@pages/BO/shopParameters/customerSettings/groups/add');
 const preferencesPage = require('@pages/BO/shipping/preferences');
 const foHomePage = require('@pages/FO/home');
 const foProductPage = require('@pages/FO/product');
@@ -15,8 +18,10 @@ const foCartPage = require('@pages/FO/cart');
 const foCheckoutPage = require('@pages/FO/checkout');
 
 // Import data
+const {groupAccess} = require('@data/demo/groupAccess');
 const {Carriers} = require('@data/demo/carriers');
 const {DefaultAccount} = require('@data/demo/customer');
+const GroupFaker = require('@data/faker/group');
 
 // Import test context
 const testContext = require('@utils/testContext');
@@ -29,8 +34,10 @@ const {expect} = require('chai');
 // Browser and tab
 let browserContext;
 let page;
+const editGroupData = new GroupFaker({name: 'Customer', frName: 'Client', discount: 0, priceDisplayMethod: 'Tax excluded', showPricesToggle: true});
 
 /*
+Choose Price display method : tax excluded
  */
 describe('Handling charges', async () => {
   // before and after functions
@@ -45,6 +52,57 @@ describe('Handling charges', async () => {
 
   it('should login in BO', async function () {
     await loginCommon.loginBO(this, page);
+  });
+
+  describe('Choose Price display method: tax excluded', async () => {
+    it('should go to \'Shop parameters > Customer Settings\' page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToCustomerSettingsPage', baseContext);
+
+      await dashboardPage.goToSubMenu(
+        page,
+        dashboardPage.shopParametersParentLink,
+        dashboardPage.customerSettingsLink,
+      );
+
+      await customerSettingsPage.closeSfToolBar(page);
+
+      const pageTitle = await customerSettingsPage.getPageTitle(page);
+      await expect(pageTitle).to.contains(customerSettingsPage.pageTitle);
+    });
+
+    it('should go to Groups page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToGroupsPage', baseContext);
+
+      await customerSettingsPage.goToGroupsPage(page);
+
+      const pageTitle = await groupsPage.getPageTitle(page);
+      await expect(pageTitle).to.contains(groupsPage.pageTitle);
+    });
+
+    it(`should filter by '${groupAccess.customer.name}'`, async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'filterByName', baseContext);
+
+      await groupsPage.filterTable(page, 'input', 'b!name', groupAccess.customer.name);
+
+      const textColumn = await groupsPage.getTextColumn(page, 1, 'b!name');
+      await expect(textColumn).to.contains(groupAccess.customer.name);
+    });
+
+    it('should go to edit group page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToEditGroupPage', baseContext);
+
+      await groupsPage.gotoEditGroupPage(page, 1);
+
+      const pageTitle = await addGroupPage.getPageTitle(page);
+      await expect(pageTitle).to.contains(addGroupPage.pageTitleEdit);
+    });
+
+    it('should update group by choosing \'Tax excluded\'', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'updateGroup', baseContext);
+
+      const textResult = await addGroupPage.createEditGroup(page, editGroupData);
+      await expect(textResult).to.contains(groupsPage.successfulUpdateMessage);
+    });
   });
 
   it('should go to \'Shipping > Preferences\' page', async function () {
