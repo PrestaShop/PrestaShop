71159f1e38f58b611a6bd90df91c6ee2842141ce
Pierre RAMBAUD <pierre.rambaud@prestashop.com>
2021-01-26 18:02:39 +0100

    Missing declare types and wordings

--- src/PrestaShopBundle/Controller/Admin/Configure/AdvancedParameters/SecurityController.php
+++ src/PrestaShopBundle/Controller/Admin/Configure/AdvancedParameters/SecurityController.php
@@ -141,7 +141,7 @@ class SecurityController extends FrameworkBundleAdminController
             '@PrestaShop/Admin/Configure/AdvancedParameters/Security/employees.html.twig',
             [
                 'enableSidebar' => true,
-                'layoutTitle' => $this->trans('Employees Sessions', 'Admin.Navigation.Menu'),
+                'layoutTitle' => $this->trans('Employees sessions', 'Admin.Navigation.Menu'),
                 'grid' => $this->presentGrid($sessionsEmployeesGridFactory->getGrid($filters)),
             ]
         );
@@ -164,7 +164,7 @@ class SecurityController extends FrameworkBundleAdminController
             '@PrestaShop/Admin/Configure/AdvancedParameters/Security/customers.html.twig',
             [
                 'enableSidebar' => true,
-                'layoutTitle' => $this->trans('Employees Sessions', 'Admin.Navigation.Menu'),
+                'layoutTitle' => $this->trans('Employees sessions', 'Admin.Navigation.Menu'),
                 'grid' => $this->presentGrid($sessionsCustomersGridFactory->getGrid($filters)),
             ]
         );
