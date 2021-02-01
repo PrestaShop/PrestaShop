86a8ccac3e2ef326ab66b83cd190d98248cc557d
Julius Zukauskas <julius.zukauskas@invertus.eu>
2021-02-01 15:43:05 +0200

    cs fix

--- src/Core/Form/IdentifiableObject/DataProvider/ProductFormDataProvider.php
+++ src/Core/Form/IdentifiableObject/DataProvider/ProductFormDataProvider.php
@@ -34,8 +34,8 @@ use PrestaShop\PrestaShop\Core\Domain\Product\QueryResult\LocalizedTags;
 use PrestaShop\PrestaShop\Core\Domain\Product\QueryResult\ProductForEditing;
 use PrestaShop\PrestaShop\Core\Domain\Product\QueryResult\ProductSupplierOptions;
 use PrestaShop\PrestaShop\Core\Domain\Product\QueryResult\ProductType;
-use PrestaShop\PrestaShop\Core\Util\DateTime\DateTime;
 use PrestaShop\PrestaShop\Core\Domain\Product\Supplier\Query\GetProductSupplierOptions;
+use PrestaShop\PrestaShop\Core\Util\DateTime\DateTime;
 
 /**
  * Provides the data that is used to prefill the Product form
