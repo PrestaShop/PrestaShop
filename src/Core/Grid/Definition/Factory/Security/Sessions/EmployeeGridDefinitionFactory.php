e100f7c8bb3a594bc727d3ea5394f113ac6eb581
Pierre RAMBAUD <pierre.rambaud@prestashop.com>
2021-02-01 15:29:26 +0100

    Unneeded class call

--- src/Core/Grid/Definition/Factory/Security/Sessions/EmployeeGridDefinitionFactory.php
+++ src/Core/Grid/Definition/Factory/Security/Sessions/EmployeeGridDefinitionFactory.php
@@ -33,7 +33,6 @@ use PrestaShop\PrestaShop\Core\Grid\Action\Bulk\BulkActionCollectionInterface;
 use PrestaShop\PrestaShop\Core\Grid\Action\Bulk\Type\SubmitBulkAction;
 use PrestaShop\PrestaShop\Core\Grid\Action\GridActionCollection;
 use PrestaShop\PrestaShop\Core\Grid\Action\GridActionCollectionInterface;
-use PrestaShop\PrestaShop\Core\Grid\Action\Row\AccessibilityChecker\AccessibilityCheckerInterface;
 use PrestaShop\PrestaShop\Core\Grid\Action\Row\RowActionCollection;
 use PrestaShop\PrestaShop\Core\Grid\Action\Type\SimpleGridAction;
 use PrestaShop\PrestaShop\Core\Grid\Column\ColumnCollection;
