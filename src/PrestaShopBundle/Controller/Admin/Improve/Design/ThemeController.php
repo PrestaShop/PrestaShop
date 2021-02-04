33b2e9cd6d3fade7055527aeaf9824d065e64a09
Jevgenij Visockij <jevgenij.visockij@invertus.eu>
2021-02-04 16:28:01 +0200

    Removed the extra text before error

--- src/PrestaShopBundle/Controller/Admin/Improve/Design/ThemeController.php
+++ src/PrestaShopBundle/Controller/Admin/Improve/Design/ThemeController.php
@@ -27,7 +27,6 @@
 namespace PrestaShopBundle\Controller\Admin\Improve\Design;
 
 use Exception;
-use PrestaShop\PrestaShop\Core\Addon\Theme\Exception\ThemeUploadException;
 use PrestaShop\PrestaShop\Core\Domain\Exception\DomainException;
 use PrestaShop\PrestaShop\Core\Domain\Exception\FileUploadException;
 use PrestaShop\PrestaShop\Core\Domain\Meta\Query\GetPagesForLayoutCustomization;
@@ -487,11 +486,6 @@ class ThemeController extends AbstractAdminController
      */
     private function handleImportThemeException(Exception $e)
     {
-        /**
-         * Will be the start for more specific error with the theme.
-         */
-        $couldntToUploadTheme = $this->trans('Couldn\'t upload the theme', 'Admin.Notifications.Error');
-
         return [
             ImportedThemeAlreadyExistsException::class => $this->trans(
                 'There is already a theme %theme_name% in your themes folder. Remove it if you want to continue.',
@@ -501,18 +495,18 @@ class ThemeController extends AbstractAdminController
                 ]
             ),
             ThemeConstraintException::class => [
-                ThemeConstraintException::RESTRICTED_ONLY_FOR_SINGLE_SHOP => $couldntToUploadTheme . ': ' . $this->trans(
+                ThemeConstraintException::RESTRICTED_ONLY_FOR_SINGLE_SHOP => $this->trans(
                         'Themes can be changed only in single shop context', 'Admin.Notifications.Error'
-                    ),
-                ThemeConstraintException::MISSING_CONFIGURATION_FILE => $couldntToUploadTheme . ': ' . $this->trans(
+                ),
+                ThemeConstraintException::MISSING_CONFIGURATION_FILE => $this->trans(
                         'Missing configuration file', 'Admin.Notifications.Error'
-                    ),
-                ThemeConstraintException::INVALID_CONFIGURATION => $couldntToUploadTheme . ': ' . $this->trans(
+                ),
+                ThemeConstraintException::INVALID_CONFIGURATION => $this->trans(
                         'Invalid configuration', 'Admin.Notifications.Error'
-                    ),
-                ThemeConstraintException::INVALID_DATA => $couldntToUploadTheme . ': ' . $this->trans(
+                ),
+                ThemeConstraintException::INVALID_DATA => $this->trans(
                         'Invalid data', 'Admin.Notifications.Error'
-                    ),
+                ),
             ],
         ];
     }
