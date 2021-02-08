7a555558d25247a723fa3820fd70b55fe66c1ee4
Julius Zukauskas <julius.zukauskas@invertus.eu>
2021-02-08 15:44:27 +0200

    move handler to Combination ns

--- src/Adapter/Product/Combination/CommandHandler/UpdateCombinationFromListingHandler.php
+++ src/Adapter/Product/Combination/CommandHandler/UpdateCombinationFromListingHandler.php
@@ -26,10 +26,10 @@
 
 declare(strict_types=1);
 
-namespace PrestaShop\PrestaShop\Adapter\Product\CommandHandler;
+namespace PrestaShop\PrestaShop\Adapter\Product\Combination\CommandHandler;
 
 use Combination;
-use PrestaShop\PrestaShop\Adapter\Product\Repository\CombinationRepository;
+use PrestaShop\PrestaShop\Adapter\Product\Combination\Repository\CombinationRepository;
 use PrestaShop\PrestaShop\Adapter\Product\Update\DefaultCombinationUpdater;
 use PrestaShop\PrestaShop\Core\Domain\Product\Combination\Command\UpdateCombinationFromListingCommand;
 use PrestaShop\PrestaShop\Core\Domain\Product\Combination\CommandHandler\UpdateCombinationFromListingHandlerInterface;
