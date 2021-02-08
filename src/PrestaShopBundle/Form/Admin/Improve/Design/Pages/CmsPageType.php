6ff4c05bba8701c6d95db3539b1ae3b60d7e1d11
Jevgenij Visockij <jevgenij.visockij@invertus.eu>
2021-02-08 17:34:55 +0200

    Removed accidentally added code

--- src/PrestaShopBundle/Form/Admin/Improve/Design/Pages/CmsPageType.php
+++ src/PrestaShopBundle/Form/Admin/Improve/Design/Pages/CmsPageType.php
@@ -121,6 +121,14 @@ class CmsPageType extends TranslatorAwareType
                         new TypedRegex([
                             'type' => 'generic_name',
                         ]),
+                        new Length([
+                            'max' => 255,
+                            'maxMessage' => $this->trans(
+                                'This field cannot be longer than %limit% characters',
+                                'Admin.Notifications.Error',
+                                ['%limit%' => 255]
+                            ),
+                        ]),
                     ],
                 ],
             ])
@@ -143,14 +151,6 @@ class CmsPageType extends TranslatorAwareType
                     'constraints' => [
                         new TypedRegex([
                             'type' => 'generic_name',
-                        ]),
-                        new Length([
-                            'max' => 255,
-                            'maxMessage' => $this->trans(
-                                'This field cannot be longer than %limit% characters',
-                                'Admin.Notifications.Error',
-                                ['%limit%' => 255]
-                            ),
                         ]),
                     ],
                 ],
