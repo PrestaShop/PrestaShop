2c14ff41e04d1cbedc0c60b465e7916ae4f729e3
Thomas BACCELLI <tbaccelli@gmail.com>
2021-02-09 15:50:34 +0100

    Use ->id_lang when loading entity

--- classes/ObjectModel.php
+++ classes/ObjectModel.php
@@ -262,7 +262,7 @@ abstract class ObjectModelCore implements \PrestaShop\PrestaShop\Core\Foundation
         if ($id) {
             /** @var \PrestaShop\PrestaShop\Adapter\EntityMapper $entity_mapper */
             $entity_mapper = ServiceLocator::get('\\PrestaShop\\PrestaShop\\Adapter\\EntityMapper');
-            $entity_mapper->load($id, $id_lang, $this, $this->def, $this->id_shop, self::$cache_objects);
+            $entity_mapper->load($id, $this->id_lang, $this, $this->def, $this->id_shop, self::$cache_objects);
         }
     }
 
