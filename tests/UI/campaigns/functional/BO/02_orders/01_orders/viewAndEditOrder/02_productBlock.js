b7cd2c93ed3a374881b5e39c8d1dfcdf45fbc238
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2021-02-15 15:58:42 +0100

    First commit

---
+++ tests/UI/campaigns/functional/BO/02_orders/01_orders/viewAndEditOrder/02_productBlock.js
@@ -0,0 +1,203 @@
+require('module-alias/register');
+
+// Helpers to open and close browser
+const helper = require('@utils/helpers');
+
+// Common tests login BO
+const loginCommon = require('@commonTests/loginBO');
+
+// Import pages
+const dashboardPage = require('@pages/BO/dashboard');
+const foHomePage = require('@pages/FO/home');
+const foProductPage = require('@pages/FO/product');
+const foCartPage = require('@pages/FO/cart');
+const foCheckoutPage = require('@pages/FO/checkout');
+const foOrderConfirmationPage = require('@pages/FO/checkout/orderConfirmation');
+const ordersPage = require('@pages/BO/orders');
+const viewOrderPage = require('@pages/BO/orders/view');
+const customersPage = require('@pages/BO/customers');
+
+// Import data
+const CustomerFaker = require('@data/faker/customer');
+const AddressFaker = require('@data/faker/address');
+const {PaymentMethods} = require('@data/demo/paymentMethods');
+
+// Import test context
+const testContext = require('@utils/testContext');
+
+const baseContext = 'functional_BO_orders_orders_viewAndEditOrder_productBlock';
+
+// Import expect from chai
+const {expect} = require('chai');
+
+let browserContext;
+let page;
+
+const customerData = new CustomerFaker({password: ''});
+const addressData = new AddressFaker({country: 'France'});
+
+/*
+Create order by guest in FO
+Go to orders page BO and view the created order page
+Check product block content
+*/
+describe('Check customer block in view order page', async () => {
+  // before and after functions
+  before(async function () {
+    browserContext = await helper.createBrowserContext(this.browser);
+    page = await helper.newTab(browserContext);
+  });
+
+  after(async () => {
+    await helper.closeBrowserContext(browserContext);
+  });
+
+  it('should go to FO page', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'goToFO', baseContext);
+
+    await foHomePage.goToFo(page);
+    await foHomePage.changeLanguage(page, 'en');
+
+    const isHomePage = await foHomePage.isHomePage(page);
+    await expect(isHomePage, 'Fail to open FO home page').to.be.true;
+  });
+
+  // 1 - create order
+  describe(`Create order by '${customerData.firstName} ${customerData.lastName}' in FO`, async () => {
+    it('should add product to cart and go to checkout page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'addProductToCart', baseContext);
+
+      await foHomePage.goToHomePage(page);
+
+      // Go to the fourth product page
+      await foHomePage.goToProductPage(page, 4);
+
+      // Add the created product to the cart
+      await foProductPage.addProductToTheCart(page);
+
+      // Proceed to checkout the shopping cart
+      await foCartPage.clickOnProceedToCheckout(page);
+
+      // Go to checkout page
+      const isCheckoutPage = await foCheckoutPage.isCheckoutPage(page);
+      await expect(isCheckoutPage).to.be.true;
+    });
+
+    it('should fill guest personal information', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'setPersonalInformation', baseContext);
+
+      const isStepPersonalInfoCompleted = await foCheckoutPage.setGuestPersonalInformation(page, customerData);
+      await expect(isStepPersonalInfoCompleted, 'Step personal information is not completed').to.be.true;
+    });
+
+    it('should fill address form and go to delivery step', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'setAddressStep', baseContext);
+
+      const isStepAddressComplete = await foCheckoutPage.setAddress(page, addressData);
+      await expect(isStepAddressComplete, 'Step Address is not complete').to.be.true;
+    });
+
+    it('should validate the order', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'validateOrder', baseContext);
+
+      // Delivery step - Go to payment step
+      const isStepDeliveryComplete = await foCheckoutPage.goToPaymentStep(page);
+      await expect(isStepDeliveryComplete, 'Step Address is not complete').to.be.true;
+
+      // Payment step - Choose payment step
+      await foCheckoutPage.choosePaymentAndOrder(page, PaymentMethods.wirePayment.moduleName);
+      const cardTitle = await foOrderConfirmationPage.getOrderConfirmationCardTitle(page);
+
+      // Check the confirmation message
+      await expect(cardTitle).to.contains(foOrderConfirmationPage.orderConfirmationCardTitle);
+    });
+  });
+
+  // 2 - Go to view order page
+  describe('View order page', async () => {
+    it('should login in BO', async function () {
+      await loginCommon.loginBO(this, page);
+    });
+
+    it('should go to Orders page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToOrdersPage', baseContext);
+
+      await dashboardPage.goToSubMenu(
+        page,
+        dashboardPage.ordersParentLink,
+        dashboardPage.ordersLink,
+      );
+
+      await ordersPage.closeSfToolBar(page);
+
+      const pageTitle = await ordersPage.getPageTitle(page);
+      await expect(pageTitle).to.contains(ordersPage.pageTitle);
+    });
+
+    it('should reset all filters', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'resetFiltersFirst', baseContext);
+
+      const numberOfOrders = await ordersPage.resetAndGetNumberOfLines(page);
+      await expect(numberOfOrders).to.be.above(0);
+    });
+
+    it(`should filter the Orders table by 'Customer: ${customerData.lastName}'`, async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'filterTable', baseContext);
+
+      await ordersPage.filterOrders(page, 'input', 'customer', customerData.lastName);
+
+      const textColumn = await ordersPage.getTextColumn(page, 'customer', 1);
+      await expect(textColumn).to.contains(customerData.lastName);
+    });
+
+    it('should view the order', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'viewOrderPage1', baseContext);
+
+      await ordersPage.goToOrder(page, 1);
+
+      const pageTitle = await viewOrderPage.getPageTitle(page);
+      await expect(pageTitle).to.contains(viewOrderPage.pageTitle);
+    });
+  });
+
+  // 4 - Delete the created customer
+  describe(`Delete the customer ${customerData.lastName}`, async () => {
+    it('should go customers page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToCustomersPage', baseContext);
+
+      await dashboardPage.goToSubMenu(
+        page,
+        dashboardPage.customersParentLink,
+        dashboardPage.customersLink,
+      );
+
+      await customersPage.closeSfToolBar(page);
+
+      const pageTitle = await customersPage.getPageTitle(page);
+      await expect(pageTitle).to.contains(customersPage.pageTitle);
+    });
+
+    it('should filter list by email', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'filterToDelete', baseContext);
+
+      await customersPage.filterCustomers(page, 'input', 'email', customerData.email);
+
+      const textResult = await customersPage.getTextColumnFromTableCustomers(page, 1, 'email');
+      await expect(textResult).to.contains(customerData.email);
+    });
+
+    it('should delete customer and check Result', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'deleteCustomer', baseContext);
+
+      const deleteTextResult = await customersPage.deleteCustomer(page, 1);
+      await expect(deleteTextResult).to.be.equal(customersPage.successfulDeleteMessage);
+    });
+
+    it('should reset all filters', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'resetAfterDelete', baseContext);
+
+      const numberOfCustomersAfterReset = await customersPage.resetAndGetNumberOfLines(page);
+      await expect(numberOfCustomersAfterReset).to.be.above(0);
+    });
+  });
+});
