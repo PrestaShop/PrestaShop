080dbd6780b1c08ebb137e7475d3e257d8708fa4
Progi1984 <franck.lefevre@prestashop.com>
2021-02-15 16:11:37 +0100

    Added unit tests

--- tests/Unit/Core/Domain/Order/QueryResult/OrderShippingAddressForViewingTest.php
+++ tests/Unit/Core/Domain/Order/QueryResult/OrderShippingAddressForViewingTest.php
@@ -2,10 +2,62 @@
 
 namespace Tests\Unit\Core\Domain\Order\QueryResult;
 
-use PrestaShop\PrestaShop\Core\Domain\Order\QueryResult\OrderInvoiceAddressForViewing;
 use PHPUnit\Framework\TestCase;
+use PrestaShop\PrestaShop\Core\Domain\Order\QueryResult\OrderShippingAddressForViewing;
 
-class OrderInvoiceAddressForViewingTest extends TestCase
+class OrderShippingAddressForViewingTest extends TestCase
 {
+    public function testConstruct(): void
+    {
+        $instance = new OrderShippingAddressForViewing(1, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k');
+        $this->assertEquals(1, $instance->getAddressId());
+        $this->assertEquals('a b', $instance->getFullName());
+        $this->assertEquals('c', $instance->getCompanyName());
+        $this->assertEquals('d', $instance->getAddress1());
+        $this->assertEquals('e', $instance->getAddress2());
+        $this->assertEquals('f', $instance->getStateName());
+        $this->assertEquals('g', $instance->getCityName());
+        $this->assertEquals('h', $instance->getCountryName());
+        $this->assertEquals('i', $instance->getPostCode());
+        $this->assertEquals('j', $instance->getPhoneNumber());
+        $this->assertEquals('k', $instance->getMobilePhoneNumber());
+        $this->assertNull($instance->getVatNumber());
+        $this->assertNull($instance->getDni());
+    }
 
+    public function testConstructWithVatNumber(): void
+    {
+        $instance = new OrderShippingAddressForViewing(1, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l');
+        $this->assertEquals(1, $instance->getAddressId());
+        $this->assertEquals('a b', $instance->getFullName());
+        $this->assertEquals('c', $instance->getCompanyName());
+        $this->assertEquals('d', $instance->getAddress1());
+        $this->assertEquals('e', $instance->getAddress2());
+        $this->assertEquals('f', $instance->getStateName());
+        $this->assertEquals('g', $instance->getCityName());
+        $this->assertEquals('h', $instance->getCountryName());
+        $this->assertEquals('i', $instance->getPostCode());
+        $this->assertEquals('j', $instance->getPhoneNumber());
+        $this->assertEquals('k', $instance->getMobilePhoneNumber());
+        $this->assertEquals('l', $instance->getVatNumber());
+        $this->assertNull($instance->getDni());
+    }
+
+    public function testConstructWithDNI(): void
+    {
+        $instance = new OrderShippingAddressForViewing(1, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm');
+        $this->assertEquals(1, $instance->getAddressId());
+        $this->assertEquals('a b', $instance->getFullName());
+        $this->assertEquals('c', $instance->getCompanyName());
+        $this->assertEquals('d', $instance->getAddress1());
+        $this->assertEquals('e', $instance->getAddress2());
+        $this->assertEquals('f', $instance->getStateName());
+        $this->assertEquals('g', $instance->getCityName());
+        $this->assertEquals('h', $instance->getCountryName());
+        $this->assertEquals('i', $instance->getPostCode());
+        $this->assertEquals('j', $instance->getPhoneNumber());
+        $this->assertEquals('k', $instance->getMobilePhoneNumber());
+        $this->assertEquals('l', $instance->getVatNumber());
+        $this->assertEquals('m', $instance->getDni());
+    }
 }
