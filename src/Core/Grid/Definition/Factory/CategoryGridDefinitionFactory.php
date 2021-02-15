078fa1db137050679a95bc5303df5dd1d57e254d
Jevgenij Visockij <jevgenij.visockij@invertus.eu>
2021-02-15 10:46:37 +0200

    CS fixer fix

--- src/Core/Grid/Definition/Factory/CategoryGridDefinitionFactory.php
+++ src/Core/Grid/Definition/Factory/CategoryGridDefinitionFactory.php
@@ -134,7 +134,7 @@ final class CategoryGridDefinitionFactory extends AbstractFilterableGridDefiniti
                     ->setOptions([
                         'field' => 'description',
                         'sortable' => false,
-                        'raw' => true
+                        'raw' => true,
                     ])
             )
             ->add(
