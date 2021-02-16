910f2ff5872fa7d7634d83f749f876a10c309d71
Pierre RAMBAUD <pierre.rambaud@prestashop.com>
2021-02-16 18:40:39 +0100

    Reorder classes use

--- tests/Unit/Adapter/Security/CommandHandler/DeleteEmployeeSessionHandlerTest.php
+++ tests/Unit/Adapter/Security/CommandHandler/DeleteEmployeeSessionHandlerTest.php
@@ -28,8 +28,8 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Adapter\Security\CommandHandler;
 
-use PrestaShop\PrestaShop\Adapter\Security\CommandHandler\DeleteEmployeeSessionHandler;
 use PHPUnit\Framework\TestCase;
+use PrestaShop\PrestaShop\Adapter\Security\CommandHandler\DeleteEmployeeSessionHandler;
 use PrestaShop\PrestaShop\Adapter\Session\Repository\EmployeeSessionRepository;
 use PrestaShop\PrestaShop\Core\Domain\Security\Command\DeleteEmployeeSessionCommand;
 
