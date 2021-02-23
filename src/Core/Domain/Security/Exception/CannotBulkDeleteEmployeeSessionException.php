58fb59b7e7b03d2a21cc11c6a2995e93e4209324
Pierre RAMBAUD <pierre.rambaud@prestashop.com>
2021-02-23 17:08:01 +0100

    Change exception extends call to use SessionException

--- src/Core/Domain/Security/Exception/CannotBulkDeleteEmployeeSessionException.php
+++ src/Core/Domain/Security/Exception/CannotBulkDeleteEmployeeSessionException.php
@@ -28,11 +28,9 @@ declare(strict_types=1);
 
 namespace PrestaShop\PrestaShop\Core\Domain\Security\Exception;
 
-use PrestaShop\PrestaShop\Core\Domain\Exception\DomainException;
-
 /**
  * Class CannotBulkDeleteEmployeeSessionException is a base exception for security sessions context.
  */
-class CannotBulkDeleteEmployeeSessionException extends DomainException
+class CannotBulkDeleteEmployeeSessionException extends SessionException
 {
 }
