bf2a77dc7d0b7892c6f58ecd8a2c6b8311b4e26b
Pierre RAMBAUD <pierre.rambaud@prestashop.com>
2021-02-23 17:08:01 +0100

    Change exception extends call to use SessionException

--- src/Core/Domain/Security/Exception/FailedToDeleteSessionException.php
+++ src/Core/Domain/Security/Exception/FailedToDeleteSessionException.php
@@ -28,11 +28,9 @@ declare(strict_types=1);
 
 namespace PrestaShop\PrestaShop\Core\Domain\Security\Exception;
 
-use PrestaShop\PrestaShop\Core\Domain\Exception\DomainException;
-
 /**
  * Class FailedToDeleteSessionException is a base exception for security sessions context.
  */
-class FailedToDeleteSessionException extends DomainException
+class FailedToDeleteSessionException extends SessionException
 {
 }
