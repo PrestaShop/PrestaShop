19c3eb179ecd3343d0e2f0000733c5124b2b0e1b
Jevgenij Visockij <jevgenij.visockij@invertus.eu>
2021-02-24 10:39:32 +0200

    Added proper exception when variable not found

--- tests/Integration/PrestaShopBundle/Controller/Exception/VariableNotFoundException.php
+++ tests/Integration/PrestaShopBundle/Controller/Exception/VariableNotFoundException.php
@@ -26,55 +26,10 @@
 
 declare(strict_types=1);
 
-namespace Tests\Integration\PrestaShopBundle\Controller;
+namespace Tests\Integration\PrestaShopBundle\Controller\Exception;
 
-use Tests\Integration\PrestaShopBundle\Controller\Exception\VariableNotFoundException;
+use Exception;
 
-class TestEntityDTO
+class VariableNotFoundException extends Exception
 {
-    /** @var ?int */
-    private $id;
-
-    /**
-     * @var array
-     */
-    private $variables;
-
-    /**
-     * Address constructor.
-     *
-     * @param int|null $id
-     * @param array $variables
-     */
-    public function __construct(
-        ?int $id,
-        array $variables
-    ) {
-        $this->id = $id;
-        $this->variables = $variables;
-    }
-
-    /**
-     * @return ?int
-     */
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param string $variableName
-     *
-     * @return mixed
-     *
-     * @throws VariableNotFoundException
-     */
-    public function __get(string $variableName)
-    {
-        if (!isset($this->variables[$variableName])) {
-            throw new VariableNotFoundException(sprintf('Variable %s not found in entity', $variableName));
-        }
-
-        return $this->variables[$variableName];
-    }
 }
