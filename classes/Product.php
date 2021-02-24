0e2aeddb1cf8e8b5e95a7b905e4f33df9dbb95ae
idnovate <info@idnovate.com>
2021-02-24 09:54:52 +0100

    BO: Remove unused variable

--- classes/Product.php
+++ classes/Product.php
@@ -4764,12 +4764,8 @@ class ProductCore extends ObjectModel
      *
      * @return array|false Matching products
      */
-    public static function searchByName($id_lang, $query, Context $context = null, $limit = null)
+    public static function searchByName($id_lang, $query, $limit = null)
     {
-        if (!$context) {
-            $context = Context::getContext();
-        }
-
         $sql = new DbQuery();
         $sql->select('p.`id_product`, pl.`name`, p.`ean13`, p.`isbn`, p.`upc`, p.`mpn`, p.`active`, p.`reference`, m.`name` AS manufacturer_name, stock.`quantity`, product_shop.advanced_stock_management, p.`customizable`');
         $sql->from('product', 'p');
