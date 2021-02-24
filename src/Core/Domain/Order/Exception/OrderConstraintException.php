b6bbb2d2dc69d09b248359e856198cb737951c8e
Pierre RAMBAUD <pierre.rambaud@prestashop.com>
2021-02-24 12:15:40 +0100

    INVALID_INTERNAL_NOTE and INVALID_PAYMENT_METHOD are needed

--- src/Core/Domain/Order/Exception/OrderConstraintException.php
+++ src/Core/Domain/Order/Exception/OrderConstraintException.php
@@ -39,5 +39,10 @@ class OrderConstraintException extends OrderException
     /**
      * @var int Is used when invalid (not string) internal note provided
      */
-    public const INVALID_PAYMENT_METHOD = 2;
+    public const INVALID_INTERNAL_NOTE = 2;
+
+    /**
+     * Used in add payment from BO when the payment method is invalid.
+     */
+    public const INVALID_PAYMENT_METHOD = 3;
 }
