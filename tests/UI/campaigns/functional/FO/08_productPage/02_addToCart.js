be99b4f0666dd9f862d25a4797cb7647c053a7e9
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2021-03-05 10:38:07 +0100

    Add test to check product informations

--- tests/UI/campaigns/functional/FO/08_productPage/02_addToCart.js
+++ tests/UI/campaigns/functional/FO/08_productPage/02_addToCart.js
@@ -24,11 +24,13 @@ const totalPrice = 137.66;
 const combination = {color: 'Black', size: 'M'};
 
 /*
+Change product quantity
+Choose combination( size, color)
 Add product to cart
 Check product details on the cart
  */
 
-describe('Product quick view', async () => {
+describe('Add product to cart', async () => {
   // before and after functions
   before(async function () {
     browserContext = await helper.createBrowserContext(this.browser);
@@ -48,42 +50,60 @@ describe('Product quick view', async () => {
     await expect(isHomePage).to.be.true;
   });
 
-  // 1 - Add product to cart
-  describe('Add product to cart', async () => {
-    it('should go to product page', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'goToProductPage', baseContext);
+  it('should go to product page', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'goToProductPage', baseContext);
 
-      await homePage.goToProductPage(page, 1);
+    await homePage.goToProductPage(page, 1);
 
-      const pageTitle = await productPage.getPageTitle(page);
-      await expect(pageTitle).to.contains(Products.demo_1.name);
-    });
+    const pageTitle = await productPage.getPageTitle(page);
+    await expect(pageTitle).to.contains(Products.demo_1.name);
+  });
 
-    it('should choose combination and add product to the cart', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'addProductToCart', baseContext);
+  it('should check product information', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'checkProductInformation', baseContext);
 
-      await productPage.addProductToTheCart(page, quantity, combination);
+    let result = await productPage.getProductInformation(page);
+    await Promise.all([
+      await expect(result.name).to.equal(Products.demo_1.name),
+      await expect(result.regularPrice).to.equal(Products.demo_1.regularPrice),
+      await expect(result.price).to.equal(Products.demo_1.finalPrice),
+      await expect(result.discountPercentage).to.contains(Products.demo_1.discount),
+      await expect(result.shortDescription).to.equal(Products.demo_1.shortDescription),
+      await expect(result.description).to.equal(Products.demo_1.description),
+      await expect(result.coverImage).to.contains(Products.demo_1.coverImage),
+    ]);
 
-      const pageTitle = await cartPage.getPageTitle(page);
-      await expect(pageTitle).to.equal(cartPage.pageTitle);
-    });
+    result = await productPage.getProductAttributes(page);
+    await Promise.all([
+      await expect(result.size).to.equal(Products.demo_1.combination.size),
+      await expect(result.color).to.equal(`Color ${Products.demo_1.combination.color}`),
+    ]);
+  });
 
-    it('should check the ordered product in cart page', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'checkOrderedProduct', baseContext);
+  it('should choose combination and add product to the cart', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'addProductToCart', baseContext);
 
-      let result = await cartPage.getProductDetail(page, 1);
-      await Promise.all([
-        expect(result.name).to.equal(Products.demo_1.name),
-        expect(result.price).to.equal(Products.demo_1.finalPrice),
-        expect(result.totalPrice).to.equal(totalPrice),
-        expect(result.quantity).to.equal(quantity),
-      ]);
+    await productPage.addProductToTheCart(page, quantity, combination);
 
-      result = await cartPage.getProductAttributes(page, 1);
-      await Promise.all([
-        expect(result.size).to.equal(combination.size),
-        expect(result.color).to.equal(combination.color),
-      ]);
-    });
+    const pageTitle = await cartPage.getPageTitle(page);
+    await expect(pageTitle).to.equal(cartPage.pageTitle);
+  });
+
+  it('should check the ordered product in cart page', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'checkOrderedProduct', baseContext);
+
+    let result = await cartPage.getProductDetail(page, 1);
+    await Promise.all([
+      expect(result.name).to.equal(Products.demo_1.name),
+      expect(result.price).to.equal(Products.demo_1.finalPrice),
+      expect(result.totalPrice).to.equal(totalPrice),
+      expect(result.quantity).to.equal(quantity),
+    ]);
+
+    result = await cartPage.getProductAttributes(page, 1);
+    await Promise.all([
+      expect(result.size).to.equal(combination.size),
+      expect(result.color).to.equal(combination.color),
+    ]);
   });
 });
