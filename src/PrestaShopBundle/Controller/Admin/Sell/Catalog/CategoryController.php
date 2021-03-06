10c058564b2fec0573659db0db69fe4ddcfdd17b
JevgenijVisockij <47781757+JevgenijVisockij@users.noreply.github.com>
2021-03-06 06:33:21 +0200

    Update src/PrestaShopBundle/Controller/Admin/Sell/Catalog/CategoryController.php
    Using decimal instead of string for sprintf
    
    Co-authored-by: GoT <PierreRambaud@users.noreply.github.com>

--- src/PrestaShopBundle/Controller/Admin/Sell/Catalog/CategoryController.php
+++ src/PrestaShopBundle/Controller/Admin/Sell/Catalog/CategoryController.php
@@ -901,7 +901,7 @@ class CategoryController extends FrameworkBundleAdminController
             ),
             UploadedImageConstraintException::class => [
                 UploadedImageConstraintException::EXCEEDED_SIZE => $this->trans(
-                    'Max file size allowed is "%s" bytes.', 'Admin.Notifications.Error',
+                    'Max file size allowed is "%d" bytes.', 'Admin.Notifications.Error',
                     [$iniConfig->getUploadMaxSizeInBytes()]
                 ),
                 UploadedImageConstraintException::UNRECOGNIZED_FORMAT => $this->trans(
