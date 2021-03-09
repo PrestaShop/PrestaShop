fc2c9c8e2f30b11d45510e0d5fd8015ac0250675
Jevgenij Visockij <jevgenij.visockij@invertus.eu>
2021-03-09 13:41:18 +0200

    Removed uneeded param in administration controller

--- src/PrestaShopBundle/Controller/Admin/Configure/AdvancedParameters/AdministrationController.php
+++ src/PrestaShopBundle/Controller/Admin/Configure/AdvancedParameters/AdministrationController.php
@@ -198,7 +198,12 @@ class AdministrationController extends FrameworkBundleAdminController
     }
 
     /**
+     * @return array
+     *
+     * @throws FieldNotFoundException
+     *
      * @var InvalidConfigurationDataErrorCollection
+     *
      */
     private function getErrorMessages(InvalidConfigurationDataErrorCollection $errors): array
     {
@@ -248,7 +253,6 @@ class AdministrationController extends FrameworkBundleAdminController
             'Admin.Notifications.Error',
             [
                 $this->getFieldLabel($error->getFieldName()),
-                GeneralDataProvider::MAX_COOKIE_VALUE,
             ]
         );
     }
