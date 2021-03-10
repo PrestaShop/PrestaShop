676f530a07c3608b8106a3fc0b3cca993f203957
Jevgenij Visockij <jevgenij.visockij@invertus.eu>
2021-03-10 09:26:27 +0200

    Added language to errors

--- src/PrestaShopBundle/Controller/Admin/Configure/AdvancedParameters/AdministrationController.php
+++ src/PrestaShopBundle/Controller/Admin/Configure/AdvancedParameters/AdministrationController.php
@@ -201,7 +201,6 @@ class AdministrationController extends FrameworkBundleAdminController
      * @throws FieldNotFoundException
      *
      * @var InvalidConfigurationDataErrorCollection
-     *
      */
     private function getErrorMessages(InvalidConfigurationDataErrorCollection $errors): array
     {
