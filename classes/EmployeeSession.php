f720487a27953881c50e2c3ff17cbe4de7dee0c3
Pierre RAMBAUD <pierre.rambaud@prestashop.com>
2021-03-15 10:47:06 +0100

    Add date_upd and date_add fields for employee and customer sessions

--- classes/EmployeeSession.php
+++ classes/EmployeeSession.php
@@ -35,6 +35,12 @@ class EmployeeSessionCore extends ObjectModel implements SessionInterface
     /** @var string Token */
     public $token;
 
+    /** @var string Object last modification date */
+    public $date_upd;
+
+    /** @var string Object creation date */
+    public $date_add;
+
     /**
      * @see ObjectModel::$definition
      */
@@ -44,6 +50,8 @@ class EmployeeSessionCore extends ObjectModel implements SessionInterface
         'fields' => [
             'id_employee' => ['type' => self::TYPE_INT, 'validate' => 'isUnsignedId', 'required' => true],
             'token' => ['type' => self::TYPE_STRING, 'validate' => 'isSha1', 'size' => 40, 'copy_post' => false],
+            'date_upd' => ['type' => self::TYPE_DATE, 'validate' => 'isDate', 'copy_post' => false],
+            'date_add' => ['type' => self::TYPE_DATE, 'validate' => 'isDate', 'copy_post' => false],
         ],
     ];
 
