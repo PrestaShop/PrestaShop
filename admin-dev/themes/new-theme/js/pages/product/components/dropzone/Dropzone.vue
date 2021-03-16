3b044feadb3a651844426d2b858117861f823b95
NeOMakinG <valentin.szczupak@prestashop.com>
2021-03-16 17:41:31 +0100

    Add remove image action to product page dropzone

--- admin-dev/themes/new-theme/js/pages/product/components/dropzone/Dropzone.vue
+++ admin-dev/themes/new-theme/js/pages/product/components/dropzone/Dropzone.vue
@@ -126,6 +126,7 @@
     saveImageInformations,
     replaceImage,
     saveImagePosition,
+    removeProductImage,
   } from '@pages/product/services/images';
   import ProductMap from '@pages/product/product-map';
   import ProductEventMap from '@pages/product/product-event-map';
@@ -323,14 +324,19 @@
        * Method to remove every selected files from the dropzone
        */
       removeSelection() {
-        this.selectedFiles.forEach((file) => {
-          this.dropzone.removeFile(file);
+        try {
+          this.selectedFiles.forEach(async (file) => {
+            await removeProductImage(this.productId);
+            this.dropzone.removeFile(file);
 
-          this.files = this.files.filter((e) => file !== e);
-        });
+            this.files = this.files.filter((e) => file !== e);
+          });
 
-        this.selectedFiles = [];
-        this.removeTooltips();
+          this.selectedFiles = [];
+          this.removeTooltips();
+        } catch (error) {
+          $.growl.error({message: error.message});
+        }
       },
       /**
        * Method to manage checkboxes of files mainly used on selectAll and unselectAll
