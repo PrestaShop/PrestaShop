a6daebf78432d4d59e7e3d702694281076145f7a
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2021-03-17 14:13:29 +0100

    Add tests for ProductTypeListener

--- src/PrestaShopBundle/Form/Admin/Sell/Product/EventListener/ProductTypeListener.php
+++ src/PrestaShopBundle/Form/Admin/Sell/Product/EventListener/ProductTypeListener.php
@@ -42,7 +42,7 @@ class ProductTypeListener implements EventSubscriberInterface
     /**
      * {@inheritDoc}
      */
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [
             FormEvents::PRE_SET_DATA => 'adaptProductForm',
