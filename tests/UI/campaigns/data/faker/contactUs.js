914557194a4c469cf785892452d776e9783115b9
Boubker Bribri <boubker.bribri@prestashop.com>
2021-03-18 18:21:30 +0100

    Add test 'Send message from contact us page with customer logged in'

--- tests/UI/campaigns/data/faker/contactUs.js
+++ tests/UI/campaigns/data/faker/contactUs.js
@@ -2,13 +2,15 @@ const faker = require('faker');
 
 const subject = ['Customer service', 'Webmaster'];
 
-module.exports = class Contact {
+module.exports = class ContactUs {
   constructor(messageToCreate = {}) {
     this.subject = messageToCreate.subject || faker.random.arrayElement(subject);
     this.firstName = messageToCreate.firstName || faker.name.firstName();
     this.lastName = messageToCreate.lastName || faker.name.lastName();
     this.emailAddress = messageToCreate.emailAddress
       || faker.internet.email(this.firstName, this.lastName, 'prestashop.com');
+
+    this.reference = messageToCreate.reference;
     this.fileName = faker.lorem.word();
     this.message = faker.lorem.sentence();
   }
