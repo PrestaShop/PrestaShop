7d303d9582b3072d331e6bed2b0343d4f65b841f
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2021-03-24 00:21:06 +0100

    Add upgrade scripts to update product type

--- install-dev/upgrade/php/ps_1780_update_product_type.php
+++ install-dev/upgrade/php/ps_1780_update_product_type.php
@@ -24,7 +24,24 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  */
 
-function ps_1780_update_product_type() {
-    // Update for each field the appropriate product_type based on
-    // dynamic associations (combinations, pack, virtual or standard)
+function ps_1780_update_product_type()
+{
+    // Update for each product the appropriate product_type based on dynamic associations
+    // (combinations, pack, virtual or standard) There are actually "tricks" since default
+    // combination or cached value are present in the table, this is more performant since
+    // we don't to compute the associations for each products in database
+
+    // First set all as standard
+    Db::getInstance()->execute(
+        'UPDATE `' . _DB_PREFIX_ . 'product` SET `product_type` = "standard"'
+    );
+    Db::getInstance()->execute(
+        'UPDATE `' . _DB_PREFIX_ . 'product` SET `product_type` = "combinations" WHERE `cache_default_attribute` != 0'
+    );
+    Db::getInstance()->execute(
+        'UPDATE `' . _DB_PREFIX_ . 'product` SET `product_type` = "pack" WHERE `cache_is_pack` = 1'
+    );
+    Db::getInstance()->execute(
+        'UPDATE `' . _DB_PREFIX_ . 'product` SET `product_type` = "virtual" WHERE `is_virtual` = 1'
+    );
 }
