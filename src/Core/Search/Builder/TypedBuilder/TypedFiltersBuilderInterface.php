ecd5a85998e44604988b1624068de5de9a9d7342
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2021-03-29 16:24:56 +0200

    Handle ClassFiltersBuilder building with tags

--- src/Core/Search/Builder/TypedBuilder/TypedFiltersBuilderInterface.php
+++ src/Core/Search/Builder/TypedBuilder/TypedFiltersBuilderInterface.php
@@ -26,8 +26,19 @@
 
 namespace PrestaShop\PrestaShop\Core\Search\Builder\TypedBuilder;
 
+use PrestaShop\PrestaShop\Core\Search\Builder\ClassFiltersBuilder;
 use PrestaShop\PrestaShop\Core\Search\Builder\FiltersBuilderInterface;
 
+/**
+ * The filters build process is based on multi layers among which we have one that adapts
+ * generic filters to specific classes, which allows overriding default values and filterId
+ * easily.
+ *
+ * But some Filters classes may need some specific way of being built so you can define a specific
+ * builder service that is handled by @see ClassFiltersBuilder which contains various specified
+ * builders, if the built type is identified by TypedFiltersBuilderInterface::supports method then
+ * this builder will be used instead of the generic construction.
+ */
 interface TypedFiltersBuilderInterface extends FiltersBuilderInterface
 {
     /**
