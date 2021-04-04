66a099e3b96fb78be547603d1d6acb645f392e85
Guisardo <missgatasch@gmail.com>
2021-04-04 14:02:34 -0300

    more cs

--- src/Adapter/PDF/DeliverySlipPdfGenerator.php
+++ src/Adapter/PDF/DeliverySlipPdfGenerator.php
@@ -72,7 +72,7 @@ final class DeliverySlipPdfGenerator implements PDFGeneratorInterface
             throw new RuntimeException($this->translator->trans('The order cannot be found within your database.', [], 'Admin.Orderscustomers.Notification'));
         }
 
-        if (Hook::exec('actionPDFDeliverySlipGenerator', array('order' => $order))) {
+        if (Hook::exec('actionPDFDeliverySlipGenerator', ['order' => $order])) {
             return;
         }
 
