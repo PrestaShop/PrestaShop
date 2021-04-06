846f8757331cabbf79b20197d05e9e37cf02b9f5
NeOMakinG <valentin.szczupak@prestashop.com>
2021-04-06 15:03:02 +0200

    Add typehead and some bugfixes

--- admin-dev/themes/new-theme/js/pages/product/product-map.js
+++ admin-dev/themes/new-theme/js/pages/product/product-map.js
@@ -136,4 +136,17 @@ export default {
     productSuppliers: '#product_suppliers',
     combinationSuppliers: '#combination_form_suppliers',
   },
+  categories: {
+    categoryTree: '.js-categories-tree',
+    overflow: '.js-category-tree-overflow',
+    actions: '.js-categories-tree-actions',
+    searchInput: '#ps-select-product-category',
+    fieldset: 'fieldset',
+    loader: '.categories-tree-loader',
+    currentAction: '.form-control-label:not(.hide)',
+    nextAction: '.hide',
+    childList: '.child-list',
+    everyItems: '.less, .more',
+    itemCheckbox: (id) => `.category[value="${id}"]`,
+  },
 };
