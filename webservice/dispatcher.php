8af5880561d42a7e0b22df282bfc0240a1de2274
Michael Voříšek <mvorisek@mvorisek.cz>
2021-04-08 22:57:40 +0200

    PHP CS - add webservice/ dir

--- webservice/dispatcher.php
+++ webservice/dispatcher.php
@@ -28,7 +28,7 @@ use PrestaShop\PrestaShop\Adapter\ContainerBuilder;
 
 ob_start();
 
-require_once dirname(__FILE__).'/../config/config.inc.php';
+require_once dirname(__FILE__) . '/../config/config.inc.php';
 
 // Cart is needed for some requests
 Context::getContext()->cart = new Cart();
@@ -55,7 +55,7 @@ if (isset($_SERVER['PHP_AUTH_USER'])) {
 } elseif (isset($_GET['ws_key'])) {
     $key = $_GET['ws_key'];
 } else {
-    header($_SERVER['SERVER_PROTOCOL'].' 401 Unauthorized');
+    header($_SERVER['SERVER_PROTOCOL'] . ' 401 Unauthorized');
     header('WWW-Authenticate: Basic realm="Welcome to PrestaShop Webservice, please enter the authentication key as the login. No password required."');
     die('401 Unauthorized');
 }
@@ -64,7 +64,7 @@ $input_xml = null;
 
 // if a XML is in PUT or in POST
 if (($_SERVER['REQUEST_METHOD'] == 'PUT') || ($_SERVER['REQUEST_METHOD'] == 'POST')) {
-    $putresource = fopen("php://input", "rb");
+    $putresource = fopen('php://input', 'rb');
     while ($putData = fread($putresource, 1024)) {
         $input_xml .= $putData;
     }
@@ -85,7 +85,7 @@ if (!class_exists($class_name)) {
 }
 // fetch the request
 WebserviceRequest::$ws_current_classname = $class_name;
-$request = call_user_func(array($class_name, 'getInstance'));
+$request = call_user_func([$class_name, 'getInstance']);
 
 $result = $request->fetch($key, $method, $_GET['url'], $params, $bad_class_name, $input_xml);
 
@@ -96,7 +96,7 @@ if (ob_get_length() != 0) {
 
 // Manage cache
 if (isset($_SERVER['HTTP_LOCAL_CONTENT_SHA1']) && $_SERVER['HTTP_LOCAL_CONTENT_SHA1'] == $result['content_sha1']) {
-    $result['status'] = $_SERVER['SERVER_PROTOCOL'].' 304 Not Modified';
+    $result['status'] = $_SERVER['SERVER_PROTOCOL'] . ' 304 Not Modified';
 }
 
 if (is_array($result['headers'])) {
