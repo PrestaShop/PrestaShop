4e9c0d72a992bcd3c07e4715e123337b53ac7952
Ibrahima SOW <ibrahima.sow@prestashop.com>
2021-04-09 10:44:16 +0000

    Display classic theme when translationType is EmailsBody + Modify selector label to fix e2e tests

--- admin-dev/themes/new-theme/js/pages/translation-settings/FormFieldToggle.js
+++ admin-dev/themes/new-theme/js/pages/translation-settings/FormFieldToggle.js
@@ -78,6 +78,7 @@ export default class FormFieldToggle {
     const $modulesFormGroup = $('.js-module-form-group');
     const $emailFormGroup = $('.js-email-form-group');
     const $themesFormGroup = $('.js-theme-form-group');
+    const $defaultThemeOption = $themesFormGroup.find('.js-default-theme');
 
     switch (selectedOption) {
       case back:
@@ -88,6 +89,7 @@ export default class FormFieldToggle {
       case themes:
         this.hide($modulesFormGroup, $emailFormGroup);
         this.show($themesFormGroup);
+        this.hide($defaultThemeOption);
         break;
 
       case modules:
@@ -118,12 +120,13 @@ export default class FormFieldToggle {
     const selectedEmailContentType = $('.js-email-form-group').find('select').val();
     const $themesFormGroup = $('.js-theme-form-group');
     const $noThemeOption = $themesFormGroup.find('.js-no-theme');
+    const $defaultThemeOption = $themesFormGroup.find('.js-default-theme');
 
     if (selectedEmailContentType === emailContentBody) {
       $noThemeOption.prop('selected', true);
-      this.show($noThemeOption, $themesFormGroup);
+      this.show($noThemeOption, $themesFormGroup, $defaultThemeOption);
     } else {
-      this.hide($noThemeOption, $themesFormGroup);
+      this.hide($noThemeOption, $themesFormGroup, $defaultThemeOption);
     }
   }
 
