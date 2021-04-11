6ebafb3db4e4c7ef96c0a97aa10f065c950953d6
Michael Voříšek <mvorisek@mvorisek.cz>
2021-04-11 23:53:59 +0200

    Delete obsoleted /images.inc.php file

--- admin-dev/functions.php
+++ admin-dev/functions.php
@@ -27,7 +27,6 @@
 if (!defined('_PS_ADMIN_DIR_')) {
     define('_PS_ADMIN_DIR_', __DIR__);
 }
-require_once _PS_ADMIN_DIR_.'/../images.inc.php';
 function bindDatepicker($id, $time)
 {
     if ($time) {
