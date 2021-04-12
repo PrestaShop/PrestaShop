6f9226e801ff7f14726c34a2104fdb82faab24c3
DuKe <30494301+josAlba@users.noreply.github.com>
2021-04-12 12:23:03 +0200

    Update classes/pdf/HTMLTemplateOrderSlip.php
    Co-authored-by: Progi1984 <progi1984@gmail.com>

--- classes/pdf/HTMLTemplateOrderSlip.php
+++ classes/pdf/HTMLTemplateOrderSlip.php
@@ -87,16 +87,16 @@ class HTMLTemplateOrderSlipCore extends HTMLTemplateInvoice
      */
     public function getNumberOrderSlip(int $id_order_slip): int
     {
-        $temporaryOrderSlip = Db::getInstance()->executeS('SELECT date_add FROM `' . _DB_PREFIX_ . 'order_slip` WHERE id_order_slip=' . $id_order_slip);
+        $row = Db::getInstance()->executeS('SELECT date_add FROM `' . _DB_PREFIX_ . 'order_slip` WHERE id_order_slip=' . $id_order_slip);
 
-        if (isset($temporaryOrderSlip[0]['date_add'])) {
-            $current_year = date('Y-01-01 00:00:00', strtotime($temporaryOrderSlip[0]['date_add']));
+        if (isset($row[0]['date_add'])) {
+            $current_year = date('Y-01-01 00:00:00', strtotime($row[0]['date_add']));
 
-            $numberOrderSlip = Db::getInstance()->executeS(
-                'SELECT count(*) as "n" FROM `' . _DB_PREFIX_ . 'order_slip` WHERE date_add > "' . $current_year . '" AND date_add < "' . $temporaryOrderSlip[0]['date_add'] . '"'
+            $rowCount = Db::getInstance()->executeS(
+                'SELECT count(*) as "n" FROM `' . _DB_PREFIX_ . 'order_slip` WHERE date_add > "' . $current_year . '" AND date_add < "' . $row[0]['date_add'] . '"'
             );
 
-            return intval($numberOrderSlip[0]['n']) + 1;
+            return intval($rowCount[0]['n']) + 1;
         }
 
         return 1;
