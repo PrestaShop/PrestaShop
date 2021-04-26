b2c28eeb8b3b2a1b3bf420ee90f8a041ad4cdf49
matthieu-rolland <matthieu.rolland@prestashop.com>
2021-04-26 11:19:20 +0200

    display dropdown in group context only if a child shop override this field

--- src/PrestaShopBundle/Service/Form/MultistoreCheckboxEnabler.php
+++ src/PrestaShopBundle/Service/Form/MultistoreCheckboxEnabler.php
@@ -165,10 +165,11 @@ class MultistoreCheckboxEnabler
         $options['attr']['disabled'] = !$this->multiStoreContext->isAllShopContext() && !$isOverriddenInCurrentContext;
 
         // add multistore dropdown in field option
-        if ($this->multiStoreContext->isAllShopContext()) {
+        if ($this->multiStoreContext->isAllShopContext() || $this->multiStoreContext->isGroupShopContext()) {
             $options['multistore_dropdown'] = $this->multistoreController->configurationDropdown(
                 $this->configuration,
-                $options['attr']['multistore_configuration_key']
+                $options['attr']['multistore_configuration_key'],
+                $this->multiStoreContext->isGroupShopContext() ? $this->multiStoreContext->getContextShopGroup()->id : null
             )->getContent();
         }
 
