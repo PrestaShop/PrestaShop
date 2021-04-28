38f667f8988fb086b7a9ef312070b9cb6e7d1358
jos <joseluisalba@joumma.com>
2021-04-28 12:11:10 +0200

    update

--- install-dev/upgrade/sql/1.7.9.0.sql
+++ install-dev/upgrade/sql/1.7.9.0.sql
@@ -2,4 +2,4 @@ SET SESSION sql_mode='';
 SET NAMES 'utf8';
 
 INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VALUES
-('PS_CREDIT_SLIP_PREFIX', '0', NOW(), NOW());
+('PS_CREDIT_SLIP_RESET', '0', NOW(), NOW());
