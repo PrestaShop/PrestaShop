eaa95c0d77b11691edcf2086d5aff615018e8b8f
Luc Vigato <luc.vigato@gmail.com>
2021-05-17 08:16:43 +0200

    Update classes/PaymentModule.php
    Co-authored-by: Mathieu Ferment <mathieu.ferment@prestashop.com>

--- classes/PaymentModule.php
+++ classes/PaymentModule.php
@@ -280,7 +280,7 @@ abstract class PaymentModuleCore extends Module
             $order_list = [];
             $order_detail_list = [];
 
-            if ($order_ref == null) {
+            if ($order_reference == null) {
                 do {
                     $reference = Order::generateReference();
                 } while (Order::getByReference($reference)->count());
