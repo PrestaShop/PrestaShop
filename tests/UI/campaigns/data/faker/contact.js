79aa70b9922dc1f2e5d8902c9432bd1ee7f0ede1
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2021-05-19 17:14:21 +0200

    Fic contact faker data

--- tests/UI/campaigns/data/faker/contact.js
+++ tests/UI/campaigns/data/faker/contact.js
@@ -4,6 +4,8 @@ module.exports = class Contact {
   constructor(contactToCreate = {}) {
     this.firstName = contactToCreate.firstName || faker.name.firstName();
     this.lastName = contactToCreate.lastName || faker.name.lastName();
+    this.title = contactToCreate.title || `${this.firstName} ${this.lastName}`;
+    this.email = contactToCreate.email || faker.internet.email(this.firstName, this.lastName, 'prestashop.com');
     this.saveMessage = contactToCreate.saveMessage === undefined ? true : contactToCreate.saveMessage;
     this.description = contactToCreate.description || faker.lorem.sentence();
   }
