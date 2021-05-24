95fa9629d2c63819305c17af23de5502dc3b5215
Okom3pom <contact@okom3pom.com>
2021-05-24 10:44:02 +0200

    Move error add service

--- src/PrestaShopBundle/Controller/Admin/Configure/AdvancedParameters/LogsController.php
+++ src/PrestaShopBundle/Controller/Admin/Configure/AdvancedParameters/LogsController.php
@@ -122,7 +122,7 @@ class LogsController extends FrameworkBundleAdminController
                 return $this->redirectToRoute('admin_logs_index');
             }
 
-            $this->addFlash('error', $this->trans('Invalid email address.', 'Admin.Notifications.Error') . ' ( ' . implode(',', $saveErrors) . ' ) ');
+            $this->flashErrors($saveErrors);
         }
 
         return $this->redirectToRoute('admin_logs_index');
