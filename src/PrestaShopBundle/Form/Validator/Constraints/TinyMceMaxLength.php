110387a3c50c7e8e3d020c94b1b4b1f303adad58
PululuK <pululuandre@hotmail.com>
2021-05-25 10:57:25 +0200

    Update src/PrestaShopBundle/Form/Validator/Constraints/TinyMceMaxLength.php
    Co-authored-by: Progi1984 <progi1984@gmail.com>

--- src/PrestaShopBundle/Form/Validator/Constraints/TinyMceMaxLength.php
+++ src/PrestaShopBundle/Form/Validator/Constraints/TinyMceMaxLength.php
@@ -34,6 +34,9 @@ class TinyMceMaxLength extends Constraint
     public const TOO_LONG_ERROR_CODE = 'TOO_LONG_ERROR_CODE';
 
     public $max;
+    /**
+     * @var string
+     */
     public $message;
 
     public function __construct($options = null)
