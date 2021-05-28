477214f1b569c1ce801862a60a3b30be70e6b4bc
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2021-05-28 17:36:02 +0200

    Add js doc for stats page

--- tests/UI/pages/BO/stats/index.js
+++ tests/UI/pages/BO/stats/index.js
@@ -1,7 +1,16 @@
 require('module-alias/register');
 const BOBasePage = require('@pages/BO/BObasePage');
 
+/**
+ * Stats page, contains functions that can be used on the page
+ * @class
+ * @extends BOBasePage
+ */
 class Stats extends BOBasePage {
+  /**
+   * @constructs
+   * Setting up texts and selectors to use on stats page
+   */
   constructor() {
     super();
 
