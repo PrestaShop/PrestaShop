6a64c5c0632b60fe7dfb5aab55a65d80bd34c92a
Julius Zukauskas <julius.zukauskas@invertus.eu>
2021-06-02 19:21:29 +0300

    wip attaachments-service.js and form theme

---
+++ admin-dev/themes/new-theme/js/pages/product/edit/attachments-manager.js
@@ -0,0 +1,72 @@
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+import ProductMap from '@pages/product/product-map';
+import AttachmentsService from "@pages/product/services/attachments-service";
+
+const {$} = window;
+
+export default class AttachmentsManager {
+  constructor(productId) {
+    this.attachmentsService = new AttachmentsService(productId);
+    this.$attachmentsContainer = $(ProductMap.attachments.attachmentsContainer);
+    this.$attachmentsCollection = $(ProductMap.attachments.attachmentsCollection);
+    this.prototypeTemplate = this.$attachmentsCollection.data('prototype');
+    this.prototypeName = this.$attachmentsCollection.data('prototypeName');
+
+    this.init();
+  }
+  /**
+   * @private
+   */
+  async init() {
+    const data = await this.attachmentsService.fetchAttachments();
+    this.$attachmentsCollection.empty();
+
+    let rowIndex = 0;
+    data.productAttachments.forEach((attachment) => {
+      //@todo; add other fields besides id so they can be filled with values
+      // const $row = $(this.getPrototypeRow(rowIndex));
+      // const $attachmentIdInput = $(ProductMap.attachments.collectionRow.attachmentIdInput(rowIndex), $row);
+      const $attachmentIdInput = $(this.getPrototypeRow(rowIndex));
+      $attachmentIdInput.val(attachment.id);
+      // this.$attachmentsCollection.append($row);
+      this.$attachmentsCollection.append($attachmentIdInput);
+
+      rowIndex += 1;
+    });
+  }
+
+  /**
+   * @param {Number} rowIndex
+   *
+   * @returns {String}
+   *
+   * @private
+   */
+  getPrototypeRow(rowIndex) {
+    return this.prototypeTemplate.replace(new RegExp(this.prototypeName, 'g'), rowIndex);
+  }
+}
