7d2c0acba552cf4581af727817d28a62368d4560
Krystian Podemski <podemski.krystian@gmail.com>
2021-06-03 20:06:03 +0200

    force disabled discounts (CartRules) to show
    separating customer CartRules methods
    
    refacto + tests
    
    fixes

---
+++ tests/Integration/Classes/CartRuleTest.php
@@ -0,0 +1,174 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Integration\Classes;
+
+use Tools;
+use CartRule;
+use Customer;
+use PHPUnit\Framework\TestCase;
+use PrestaShopBundle\Install\DatabaseDump;
+use PrestaShop\PrestaShop\Adapter\Configuration;
+
+class CartRuleTest extends TestCase
+{
+    /**
+     * @var Customer $dummyCustomer
+     */
+    public $dummyCustomer;
+
+    /**
+     * @var CartRule $dummyCartRule
+     */
+    public $dummyCartRule;
+
+    /**
+     * @var Configuration $configuration
+     */
+    public $configuration;
+
+    public static function setUpBeforeClass(): void
+    {
+        DatabaseDump::restoreDb();
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->dummyCustomer = $this->createDummyCustomer();
+        $this->configuration = new Configuration();
+    }
+
+    public function testGetCartRulesForCustomer(): void
+    {
+        $this->dummyCartRule = $this->createDummyCartRule(true, (int) $this->dummyCustomer->id);
+
+        $customerCartRules = CartRule::getCustomerCartRules(
+            $this->configuration->get('PS_LANG_DEFAULT'),
+            $this->dummyCustomer->id
+        );
+
+        $this->assertEquals(1, count($customerCartRules));
+    }
+
+    public function testGetAllCartRulesForCustomerEvenDisabled(): void
+    {
+        $this->dummyCartRule = $this->createDummyCartRule(false, (int) $this->dummyCustomer->id);
+
+        $customerCartRules = CartRule::getCustomerCartRules(
+            $this->configuration->get('PS_LANG_DEFAULT'),
+            $this->dummyCustomer->id,
+            true
+        );
+
+        // We assert 0 because 'active' flag does not work in "getCustomerCartRules"
+        // because of CartRule::isFeatureActive and one additional check
+        $this->assertEquals(0, count($customerCartRules));
+    }
+
+    public function testGetAllCartRulesForCustomerWithDedicatedMethod(): void
+    {
+        $this->dummyCartRule = $this->createDummyCartRule(true, (int) $this->dummyCustomer->id);
+
+        $customerCartRules = CartRule::getAllCustomerCartRules(
+            (int) $this->dummyCustomer->id
+        );
+
+        $this->assertEquals(1, count($customerCartRules));
+    }
+
+    public function testGetAllCartRulesForCustomerWithDedicatedMethodEvenDisabled(): void
+    {
+        $this->dummyCartRule = $this->createDummyCartRule(false, (int) $this->dummyCustomer->id);
+
+        $customerCartRules = CartRule::getAllCustomerCartRules(
+            (int) $this->dummyCustomer->id
+        );
+
+        $this->assertEquals(1, count($customerCartRules));
+    }
+
+    public function testGetAllCartRulesForCustomerWithDedicatedMethodBothEnabledAndDisabled()
+    {
+        $this->createDummyCartRule(false, (int) $this->dummyCustomer->id);
+        $this->createDummyCartRule(true, (int) $this->dummyCustomer->id);
+        $this->createDummyCartRule(true, (int) $this->dummyCustomer->id);
+
+        $customerCartRules = CartRule::getAllCustomerCartRules(
+            (int) $this->dummyCustomer->id
+        );
+
+        $this->assertEquals(3, count($customerCartRules));
+    }
+
+    /**
+     * @param bool $active
+     * @param int $customerId
+     * 
+     * @return CartRule
+     */
+    public function createDummyCartRule(
+        bool $active = true,
+        int $customerId
+    ) {
+        $randomNumber = rand(999, 9999);
+        $cart_rule = new CartRule();
+        $cart_rule->code = 'TEST_CART_RULE_'.$randomNumber;
+        $cart_rule->name = [
+            $this->configuration->get('PS_LANG_DEFAULT') => 'Test Cart Rule #'.$randomNumber
+        ];
+        $cart_rule->id_customer = (int) $customerId;
+        $cart_rule->free_shipping = true;
+        $cart_rule->quantity = 1;
+        $cart_rule->quantity_per_user = 1;
+        $cart_rule->minimum_amount_currency = $this->configuration->get('PS_CURRENCY_DEFAULT');
+        $cart_rule->reduction_currency = $this->configuration->get('PS_CURRENCY_DEFAULT');
+        $cart_rule->date_from = date('Y-m-d H:i:s', time());
+        $cart_rule->date_to = date('Y-m-d H:i:s', time() + 24 * 36000);
+        $cart_rule->active = $active;
+        $cart_rule->add();
+
+        return $cart_rule;
+    }
+
+    /**
+     * @return Customer
+     */
+    public function createDummyCustomer(): Customer
+    {
+        $customer = new Customer();
+        $customer->firstname = 'Jenna';
+        $customer->lastname = 'Doe';
+        $customer->email = 'pub+jenna@prestashop.com';
+        $customer->passwd = Tools::hash('prestashop');
+        $customer->save();
+
+        return $customer;
+    }
+}
