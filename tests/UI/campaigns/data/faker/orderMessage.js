e35923515e119edb2da676604b2c9f939afed535
Boubker Bribri <boubker.bribri@prestashop.com>
2021-06-03 15:09:34 +0200

    Add jsdoc for classes: imageType, invoice, language, orderMessage, orderReturnStatus, orderStatus, product, profile

--- tests/UI/campaigns/data/faker/orderMessage.js
+++ tests/UI/campaigns/data/faker/orderMessage.js
@@ -1,14 +1,26 @@
 const faker = require('faker');
+
 /**
- * Class representing order message data
+ * Create new order message to use on creation form on order message page on BO
  * @class
  */
-class OrderMessage {
+class OrderMessageData {
+  /**
+   * Constructor for class OrderMessage
+   * @param messageToCreate {Object} Could be used to force the value of some members
+   */
   constructor(messageToCreate = {}) {
+    /** @member {string} Name of the message */
     this.name = messageToCreate.name || faker.lorem.word();
-    this.message = messageToCreate.name || faker.lorem.sentence();
+
+    /** @member {string} The message to set */
+    this.message = messageToCreate.message || faker.lorem.sentence();
+
+    /** @member {string} French name of the message */
     this.frName = messageToCreate.frName || this.name;
-    this.frMessage = messageToCreate.frName || this.message;
+
+    /** @member {string} The french message to set */
+    this.frMessage = messageToCreate.frMessage || this.message;
   }
 }
-module.exports = OrderMessage;
+module.exports = OrderMessageData;
--- tests/UI/campaigns/data/faker/orderReturnStatus.js
+++ tests/UI/campaigns/data/faker/orderReturnStatus.js
@@ -1,8 +1,21 @@
 const faker = require('faker');
 
-module.exports = class OrderReturnStatus {
+/**
+ * Create new order return status to use on creation form on order return status page on BO
+ * @class
+ */
+class OrderReturnStatusData {
+  /**
+   * Constructor for class OrderReturnStatusData
+   * @param orderReturnStatusToCreate {Object} Could be used to force the value of some members
+   */
   constructor(orderReturnStatusToCreate = {}) {
+    /** @member {string} Name of the status */
     this.name = orderReturnStatusToCreate.name || `order_return_status_${faker.lorem.word()}`;
+
+    /** @member {string} Hexadecimal value for the status  */
     this.color = orderReturnStatusToCreate.color || faker.internet.color();
   }
-};
+}
+
+module.exports = OrderReturnStatusData;
