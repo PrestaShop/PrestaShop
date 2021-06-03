a1d7b37c8b0d9a04e81a344561650872ec964ec0
matthieu-rolland <matthieu.rolland@prestashop.com>
2021-06-03 15:54:43 +0200

    fix broken unit tests

--- tests/Unit/Adapter/Preferences/PreferencesConfigurationTest.php
+++ tests/Unit/Adapter/Preferences/PreferencesConfigurationTest.php
@@ -29,9 +29,12 @@ declare(strict_types=1);
 namespace Tests\Unit\Adapter\Preferences;
 
 use Cookie;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use PrestaShop\PrestaShop\Adapter\Configuration;
 use PrestaShop\PrestaShop\Adapter\Preferences\PreferencesConfiguration;
+use PrestaShop\PrestaShop\Adapter\Shop\Context as ShopContext;
+use PrestaShop\PrestaShop\Core\Feature\FeatureInterface;
 
 class PreferencesConfigurationTest extends TestCase
 {
@@ -45,6 +48,10 @@ class PreferencesConfigurationTest extends TestCase
      */
     private $mockConfiguration;
 
+    private $mockShopConfiguration;
+
+    private $mockMultistoreFeature;
+
     protected function setUp()
     {
         $this->mockConfiguration = $this->getMockBuilder(Configuration::class)
@@ -52,7 +59,10 @@ class PreferencesConfigurationTest extends TestCase
             ->disableOriginalConstructor()
             ->getMock();
 
-        $this->object = new PreferencesConfiguration($this->mockConfiguration);
+        $this->mockShopConfiguration = $this->getMockShopConfiguration();
+        $this->mockMultistoreFeature = $this->getMockMultistoreFeature();
+
+        $this->object = new PreferencesConfiguration($this->mockConfiguration, $this->mockShopConfiguration, $this->mockMultistoreFeature);
     }
 
     public function testGetConfiguration()
@@ -209,5 +219,23 @@ class PreferencesConfigurationTest extends TestCase
                 ]
             )
         );
+    }
+
+    /**
+     * @return MockObject
+     */
+    private function getMockShopConfiguration(): MockObject
+    {
+        return $this->getMockBuilder(ShopContext::class)
+            ->setMethods(['getContextShopGroup', 'getContextShopID', 'isAllShopContext'])
+            ->getMock();
+    }
+
+    /**
+     * @return MockObject
+     */
+    private function getMockMultistoreFeature(): MockObject
+    {
+        return $this->getMockForAbstractClass(FeatureInterface::class);
     }
 }
