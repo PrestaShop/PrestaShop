066bd9056d541b19e33d2c48d64d120422787bf2
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2021-06-11 16:36:37 +0200

    Add some fixes
    This reverts commit a2465c03ab7cade358d0aa7d63f70b10d5c6cd83.
    
    Revert "Refacto tests on view and edit order directory"
    
    This reverts commit f8516287e5c316857f4821db6bcf6ca84f7e89d6.

--- tests/UI/campaigns/functional/BO/02_orders/01_orders/viewAndEditOrder/02_productBlock.js
+++ tests/UI/campaigns/functional/BO/02_orders/01_orders/viewAndEditOrder/02_productBlock.js
@@ -115,7 +115,7 @@ Check product block content:
 - Pagination next and previous
 - Delete product
 */
-describe('BO - Orders - view and edit order : Check product block', async () => {
+describe('Check product block in view order page', async () => {
   // before and after functions
   before(async function () {
     browserContext = await helper.createBrowserContext(this.browser);
@@ -138,7 +138,7 @@ describe('BO - Orders - view and edit order : Check product block', async () => 
 
   // 1 - create order
   describe(`Create order by '${customerData.firstName} ${customerData.lastName}' in FO`, async () => {
-    it('should add product to cart', async function () {
+    it('should add product to cart and go to checkout page', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'addProductToCart', baseContext);
 
       await foHomePage.goToHomePage(page);
@@ -148,14 +148,6 @@ describe('BO - Orders - view and edit order : Check product block', async () => 
 
       // Add the created product to the cart
       await foProductPage.addProductToTheCart(page, productQuantity);
-
-      const notificationsNumber = await foCartPage.getCartNotificationsNumber(page);
-      await expect(notificationsNumber).to.be.equal(5);
-    });
-
-
-    it('should go to checkout step', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'goToCheckoutPage', baseContext);
 
       // Proceed to checkout the shopping cart
       await foCartPage.clickOnProceedToCheckout(page);
