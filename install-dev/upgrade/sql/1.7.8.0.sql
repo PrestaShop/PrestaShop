3179197670b8d594118e2b6106b9713c0bf7afbf
Lorenz Meyer <lm@bible.ch>
2021-06-14 09:28:13 +0200

    Shop and shop group have now a color property.

--- install-dev/upgrade/sql/1.7.8.0.sql
+++ install-dev/upgrade/sql/1.7.8.0.sql
@@ -76,6 +76,9 @@ UPDATE `PREFIX_product` SET `product_type` = "combinations" WHERE `cache_default
 UPDATE `PREFIX_product` SET `product_type` = "pack" WHERE `cache_is_pack` = 1;
 UPDATE `PREFIX_product` SET `product_type` = "virtual" WHERE `is_virtual` = 1;
 
+ALTER TABLE `ps_shop_group` ADD `color` VARCHAR(32) NULL AFTER `name`;
+ALTER TABLE `ps_shop` ADD `color` VARCHAR(32) NULL AFTER `name`;
+
 /* PHP:ps_1780_add_feature_flag_tab(); */;
 
 /* this table should be created by Doctrine but we need to perform INSERT and the 1.7.8.0.sql script is called
