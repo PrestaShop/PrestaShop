999387300dc3f46cc4bac7989fed555bdaffd582
Pierre RAMBAUD <pierre.rambaud@prestashop.com>
2021-06-14 15:32:43 +0200

    Restore the download part if module is not found for the current Catalog behavior

--- src/Core/Addon/Module/ModuleManager.php
+++ src/Core/Addon/Module/ModuleManager.php
@@ -35,6 +35,7 @@ use PrestaShop\PrestaShop\Adapter\Module\ModuleZipManager;
 use PrestaShop\PrestaShop\Core\Addon\AddonManagerInterface;
 use PrestaShop\PrestaShop\Core\Addon\AddonsCollection;
 use PrestaShop\PrestaShop\Core\Cache\Clearer\CacheClearerInterface;
+use PrestaShop\PrestaShop\Core\Domain\Theme\Exception\FailedToEnableThemeModuleException;
 use PrestaShopBundle\Event\ModuleManagementEvent;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\ParameterBag;
@@ -293,7 +294,16 @@ class ModuleManager implements AddonManagerInterface
         if (!empty($source)) {
             $this->moduleZipManager->storeInModulesFolder($source);
         } elseif (!$this->moduleProvider->isOnDisk($name)) {
-            return false;
+            if (!$this->moduleUpdater->setModuleOnDiskFromAddons($name)) {
+                throw new FailedToEnableThemeModuleException(
+                    $name,
+                    $this->translator->trans(
+                        'The module %name% could not be found on Addons.',
+                        ['%name%' => $name],
+                        'Admin.Modules.Notification'
+                    )
+                );
+            }
         }
 
         $module = $this->moduleRepository->getModule($name);
