11e55f8cd962c1e7022d190359fa3cbdd4671f07
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2021-06-29 11:27:27 +0200

    Add js doc for best sales page

--- tests/UI/pages/FO/bestSales/index.js
+++ tests/UI/pages/FO/bestSales/index.js
@@ -1,7 +1,16 @@
 require('module-alias/register');
 const FOBasePage = require('@pages/FO/FObasePage');
 
+/**
+ * Best sales page, contains functions that can be used on the page
+ * @class
+ * @extends BOBasePage
+ */
 class BestSales extends FOBasePage {
+  /**
+   * @constructs
+   * Setting up texts and selectors to use on best sales page
+   */
   constructor() {
     super();
 
