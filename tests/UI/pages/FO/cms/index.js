1572c9dcdfd0ad4d55c491bfc5c8041d0385a32b
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2021-06-29 12:12:10 +0200

    Update js doc for order cms page

--- tests/UI/pages/FO/cms/index.js
+++ tests/UI/pages/FO/cms/index.js
@@ -2,7 +2,16 @@
 require('module-alias/register');
 const FOBasePage = require('@pages/FO/FObasePage');
 
+/**
+ * CMS page, contains functions that can be used on the page
+ * @class
+ * @extends BOBasePage
+ */
 class CMS extends FOBasePage {
+  /**
+   * @constructs
+   * Setting up texts and selectors to use on cms page
+   */
   constructor() {
     super();
     this.pageNotFound = 'The page you are looking for was not found.';
