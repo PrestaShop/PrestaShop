3a02e89b4551853e6faeec6054c30ea842bc7a58
Boubker Bribri <boubker.bribri@prestashop.com>
2021-06-30 18:04:13 +0200

    Fix last data file

--- tests/UI/campaigns/data/faker/group.js
+++ tests/UI/campaigns/data/faker/group.js
@@ -12,19 +12,19 @@ class GroupData {
    * @param groupToCreate {Object} Could be used to force the value of some members
    */
   constructor(groupToCreate = {}) {
-    /** @member {string} Name of the group */
+    /** @type {string} Name of the group */
     this.name = groupToCreate.name || faker.name.jobType();
 
-    /** @member {string} French name of the group */
+    /** @type {string} French name of the group */
     this.frName = groupToCreate.frName || this.name;
 
-    /** @member {string} Basic discount for the group */
+    /** @type {string} Basic discount for the group */
     this.discount = groupToCreate.discount || 0;
 
-    /** @member {string} Price display method of the group */
+    /** @type {string} Price display method of the group */
     this.priceDisplayMethod = groupToCreate.priceDisplayMethod || faker.random.arrayElement(priceDisplayMethod);
 
-    /** @member {boolean} True to show prices for the group */
+    /** @type {boolean} True to show prices for the group */
     this.shownPrices = groupToCreate.shownPrices === undefined ? true : groupToCreate.shownPrices;
   }
 }
