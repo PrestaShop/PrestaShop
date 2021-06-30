e24153a454be070cf8c1b93e64061b9a78980dc5
okom3pom <contact@okom3pom.com>
2021-06-30 07:16:43 +0200

    Fix return for virtual product

--- controllers/front/OrderDetailController.php
+++ controllers/front/OrderDetailController.php
@@ -189,6 +189,7 @@ class OrderDetailControllerCore extends FrontController
 
                 $this->context->smarty->assign([
                     'order' => $this->order_to_display,
+                    'is_virtual' => $order->isVirtual(),
                     'HOOK_DISPLAYORDERDETAIL' => Hook::exec('displayOrderDetail', ['order' => $order]),
                 ]);
             } else {
