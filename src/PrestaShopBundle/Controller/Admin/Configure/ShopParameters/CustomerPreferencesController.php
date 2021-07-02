e125af926bfa161808145acd5c10349056a95828
Thomas BACCELLI <thomas.baccelli@prestashop.com>
2021-07-02 14:25:06 +0200

    Fix typo

--- src/PrestaShopBundle/Controller/Admin/Configure/ShopParameters/CustomerPreferencesController.php
+++ src/PrestaShopBundle/Controller/Admin/Configure/ShopParameters/CustomerPreferencesController.php
@@ -65,7 +65,7 @@ class CustomerPreferencesController extends FrameworkBundleAdminController
      * Process the Customer Preferences configuration form.
      *
      * @AdminSecurity(
-     *     "is_granted('update, request.get('_legacy_controller')) && is_granted('create', request.get('_legacy_controller')) && is_granted('delete', request.get('_legacy_controller'))",
+     *     "is_granted('update', request.get('_legacy_controller')) && is_granted('create', request.get('_legacy_controller')) && is_granted('delete', request.get('_legacy_controller'))",
      *     message="You do not have permission to update this.", redirectRoute="admin_customer_preferences"
      * )
      * @DemoRestricted(redirectRoute="admin_customer_preferences")
