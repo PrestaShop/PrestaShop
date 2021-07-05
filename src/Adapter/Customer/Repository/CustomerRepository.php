70d24274948116ae803b274957525f4c376d9062
Julius Zukauskas <julius.zukauskas@invertus.eu>
2021-07-05 11:48:31 +0300

    introduce CustomerRepository & assert customer existence

---
+++ src/Adapter/Customer/Repository/CustomerRepository.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+declare(strict_types=1);
+
+namespace PrestaShop\PrestaShop\Adapter\Customer\Repository;
+
+use ObjectModel;
+use PrestaShop\PrestaShop\Adapter\AbstractObjectModelRepository;
+use PrestaShop\PrestaShop\Core\Domain\Customer\Exception\CustomerNotFoundException;
+use PrestaShop\PrestaShop\Core\Domain\Customer\ValueObject\CustomerId;
+use PrestaShop\PrestaShop\Core\Exception\CoreException;
+use PrestaShopException;
+
+/**
+ * Provides methods to access Customer data storage
+ */
+class CustomerRepository extends AbstractObjectModelRepository
+{
+    /**
+     * @param CustomerId $customerId
+     *
+     * @throws CoreException
+     * @throws CustomerNotFoundException
+     */
+    public function assertCustomerExists(CustomerId $customerId): void
+    {
+        try {
+            if (!ObjectModel::existsInDatabase($customerId->getValue(), 'customer')) {
+                throw new CustomerNotFoundException(
+                    $customerId,
+                    sprintf('customer #%d does not exist', $customerId->getValue())
+                );
+            }
+        } catch (PrestaShopException $e) {
+            throw new CoreException(
+                sprintf(
+                    'Error occurred when trying to check if customer #%d exists [%s]',
+                    $customerId->getValue(),
+                    $e->getMessage()
+                ),
+                0,
+                $e
+            );
+        }
+    }
+}
