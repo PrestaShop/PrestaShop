9a9266be06b05703f7649311c985a2e862b6bcf0
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2021-07-06 13:06:32 +0200

    Refacto help card test

--- tests/UI/campaigns/functional/BO/03_catalog/04_attributesAndFeatures/01_attributes/attributes/04_helpCard.js
+++ tests/UI/campaigns/functional/BO/03_catalog/04_attributesAndFeatures/01_attributes/attributes/04_helpCard.js
@@ -1,9 +1,12 @@
 require('module-alias/register');
 
+// Import expect from chai
 const {expect} = require('chai');
 
 // Import utils
 const helper = require('@utils/helpers');
+
+// Common tests login BO
 const loginCommon = require('@commonTests/loginBO');
 
 // Import pages
@@ -23,7 +26,7 @@ Go to attributes page
 Open help card and check language
 Close help card
  */
-describe('Helper card on attributes page', async () => {
+describe('BO - Catalog - Attributes & Features : Help card on attributes page', async () => {
   // before and after functions
   before(async function () {
     browserContext = await helper.createBrowserContext(this.browser);
