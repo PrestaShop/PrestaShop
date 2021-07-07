49b1a4a1ca9e9b115dae8100a83ce5b22bd3a39c
Tom Macdonald <tm@thomas-macdonald.com>
2021-07-07 19:30:37 +0200

    Use jquery pseudo selector
    I'm not sure whether this is a general problem, but in the theme I am using, the country is in a `select`.
    This is not picked up by the current selector, meaning that the country is reset each time it is selected. This means clients cannot add addresses from their account page.
    This commit fixes the problem.

--- themes/_core/js/address.js
+++ themes/_core/js/address.js
@@ -14,7 +14,7 @@ function handleCountryChange(selectors) {
       id_address: $(`${selectors.address} form`).data('id-address'),
     };
     const getFormViewUrl = $(`${selectors.address} form`).data('refresh-url');
-    const formFieldsSelector = `${selectors.address} input`;
+    const formFieldsSelector = `${selectors.address} :input`;
 
     $.post(getFormViewUrl, requestData).then((resp) => {
       const inputs = [];
