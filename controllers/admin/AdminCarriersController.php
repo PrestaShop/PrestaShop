5d9f47d3e6d60e049d8916e823593ddba59c3e47
Progi1984 <franck.lefevre@prestashop.com>
2021-07-09 15:51:19 +0200

    Fixed sort for naming in Admin > Carriers list

--- controllers/admin/AdminCarriersController.php
+++ controllers/admin/AdminCarriersController.php
@@ -642,13 +642,13 @@ class AdminCarriersControllerCore extends AdminController
      */
     public function getList($id_lang, $order_by = null, $order_way = null, $start = 0, $limit = null, $id_lang_shop = false)
     {
-        parent::getList($id_lang, $order_by, $order_way, $start, $limit, $id_lang_shop);
+        // Replace "0" by the name of the shop directly in SQL query (allowing sort without errors)
+        $this->_select = 'CASE WHEN name = "0" '
+            . 'THEN "' . Carrier::getCarrierNameFromShopName() . '" '
+            . 'ELSE name '
+            . 'END AS name';
 
-        foreach ($this->_list as $key => $list) {
-            if ($list['name'] == '0') {
-                $this->_list[$key]['name'] = Carrier::getCarrierNameFromShopName();
-            }
-        }
+        parent::getList($id_lang, $order_by, $order_way, $start, $limit, $id_lang_shop);
     }
 
     public function ajaxProcessUpdatePositions()
