a62b4a8f9e6f6f9d32076b0cb98fa9a5317bb0d7
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2021-07-22 14:55:23 +0200

    Cs fix

--- src/Core/Domain/Product/Attachment/Command/SetAssociatedProductAttachmentsCommand.php
+++ src/Core/Domain/Product/Attachment/Command/SetAssociatedProductAttachmentsCommand.php
@@ -29,7 +29,6 @@ declare(strict_types=1);
 namespace PrestaShop\PrestaShop\Core\Domain\Product\Attachment\Command;
 
 use PrestaShop\PrestaShop\Core\Domain\Attachment\ValueObject\AttachmentId;
-use PrestaShop\PrestaShop\Core\Domain\Product\Attachment\Command\RemoveAllAssociatedProductAttachmentsCommand;
 use PrestaShop\PrestaShop\Core\Domain\Product\ValueObject\ProductId;
 use RuntimeException;
 
