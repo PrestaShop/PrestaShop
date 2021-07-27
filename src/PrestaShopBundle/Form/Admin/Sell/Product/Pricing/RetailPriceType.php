a4acdd1c28d983e2d17c3e85130fdea9b3b5a026
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2021-07-27 17:42:27 +0200

    Add extension that automatically adds checbox for fields that are overridable, the checbox is automatically added in the prestashop form theme

--- src/PrestaShopBundle/Form/Admin/Sell/Product/Pricing/RetailPriceType.php
+++ src/PrestaShopBundle/Form/Admin/Sell/Product/Pricing/RetailPriceType.php
@@ -83,6 +83,7 @@ class RetailPriceType extends TranslatorAwareType
                     new Type(['type' => 'float']),
                 ],
                 'default_empty_data' => 0.0,
+                'overridable_for_all' => true,
             ])
             ->add('price_tax_included', MoneyType::class, [
                 'required' => false,
