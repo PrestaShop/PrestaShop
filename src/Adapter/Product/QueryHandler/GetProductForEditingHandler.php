03969f3c2fc62ec0e5b6607fa423f86b1c26855e
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2021-07-29 13:14:55 +0200

    Handle shop constraint in GetProductForEditing query to get specific values per shop

--- src/Adapter/Product/QueryHandler/GetProductForEditingHandler.php
+++ src/Adapter/Product/QueryHandler/GetProductForEditingHandler.php
@@ -173,7 +173,7 @@ final class GetProductForEditingHandler implements GetProductForEditingHandlerIn
      */
     public function handle(GetProductForEditing $query): ProductForEditing
     {
-        $product = $this->productRepository->get($query->getProductId());
+        $product = $this->productRepository->get($query->getProductId(), $query->getShopConstraint());
 
         return new ProductForEditing(
             (int) $product->id,
