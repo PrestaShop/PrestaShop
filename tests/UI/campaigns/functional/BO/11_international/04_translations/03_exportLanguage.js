bed9ff2c567f765666a537740a1b2fe86ecdabd8
Boubker Bribri <boubker.bribri@prestashop.com>
2021-08-19 11:32:17 +0200

    Refacto translations tests

--- tests/UI/campaigns/functional/BO/11_international/04_translations/03_exportLanguage.js
+++ tests/UI/campaigns/functional/BO/11_international/04_translations/03_exportLanguage.js
@@ -20,7 +20,7 @@ const baseContext = 'functional_BO_international_localization_translations_expor
 let browserContext;
 let page;
 
-describe('Export languages in translations page', async () => {
+describe('BO - International - Translation : Export languages', async () => {
   // before and after functions
   before(async function () {
     browserContext = await helper.createBrowserContext(this.browser);
@@ -35,7 +35,7 @@ describe('Export languages in translations page', async () => {
     await loginCommon.loginBO(this, page);
   });
 
-  it('should go to translations page', async function () {
+  it('should go to \'International > Translations\' page', async function () {
     await testContext.addContextItem(this, 'testIdentifier', 'goToTranslationsPage', baseContext);
 
     await dashboardPage.goToSubMenu(
