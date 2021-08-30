8a01b37e773457e8d39be9433c1e67bd1f664f80
GoT <PierreRambaud@users.noreply.github.com>
2021-08-30 11:24:56 +0200

    Change condition for select statement
    Co-authored-by: Pablo Borowicz <pablo.borowicz@prestashop.com>

--- controllers/admin/AdminCarriersController.php
+++ controllers/admin/AdminCarriersController.php
@@ -643,7 +643,9 @@ class AdminCarriersControllerCore extends AdminController
     public function getList($id_lang, $order_by = null, $order_way = null, $start = 0, $limit = null, $id_lang_shop = false)
     {
         // Replace "0" by the name of the shop directly in SQL query (allowing sort without errors)
-        $this->_select = empty($this->_select) ? '' : ', ';
+        if (!empty($this->_select)) {
+            $this->_select .= ', ';
+        }
         $this->_select .= sprintf(
             'IF(name = "0", "%s", name) AS name',
             pSQL(Carrier::getCarrierNameFromShopName())
