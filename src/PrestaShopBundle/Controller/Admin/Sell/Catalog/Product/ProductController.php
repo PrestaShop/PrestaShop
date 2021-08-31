99a292bdf38fc4292635d1d2c5c321147eae71ba
Julius Zukauskas <julius.zukauskas@invertus.eu>
2021-08-31 13:59:15 +0300

    move SpecificPrice form out of product form
     remove ts-ignore from unrelated js file

--- src/PrestaShopBundle/Controller/Admin/Sell/Catalog/Product/ProductController.php
+++ src/PrestaShopBundle/Controller/Admin/Sell/Catalog/Product/ProductController.php
@@ -44,6 +44,7 @@ use PrestaShop\PrestaShop\Core\Form\IdentifiableObject\Handler\FormHandlerInterf
 use PrestaShop\PrestaShop\Core\Search\Filters\ProductFilters;
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
 use PrestaShopBundle\Entity\ProductDownload;
+use PrestaShopBundle\Form\Admin\Sell\Product\Pricing\SpecificPriceType;
 use PrestaShopBundle\Security\Annotation\AdminSecurity;
 use PrestaShopBundle\Security\Voter\PageVoter;
 use Symfony\Component\Form\FormInterface;
@@ -348,6 +349,8 @@ class ProductController extends FrameworkBundleAdminController
             'categoryTreeSelectorForm' => $categoryTreeFormBuilder->getForm()->createView(),
             'showContentHeader' => false,
             'productForm' => $productForm->createView(),
+            //@todo: form builder
+            'specificPriceForm' => $this->createForm(SpecificPriceType::class)->createView(),
             'statsLink' => $productId ? $this->getAdminLink('AdminStats', ['module' => 'statsproduct', 'id_product' => $productId]) : null,
             'helpLink' => $this->generateSidebarLink('AdminProducts'),
             'isMultiShopContext' => $isMultiShopContext,
