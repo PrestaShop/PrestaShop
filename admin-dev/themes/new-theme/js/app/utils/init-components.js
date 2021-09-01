f10a0c239a8941e41a8b9082ccaa5668adac803c
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2021-09-01 19:48:02 +0200

    Rename js component

--- admin-dev/themes/new-theme/js/app/utils/init-components.js
+++ admin-dev/themes/new-theme/js/app/utils/init-components.js
@@ -60,7 +60,7 @@ import SubmitBulkActionExtension from '@components/grid/extension/submit-bulk-ac
 import SubmitGridActionExtension from '@components/grid/extension/submit-grid-action-extension';
 import SubmitRowActionExtension from '@components/grid/extension/action/row/submit-row-action-extension';
 import DeltaQuantityInput from '@components/form/delta-quantity-input';
-import MultiStoreModifyAll from '@components/multi-store-modify-all';
+import ModifyAllStoresCheckbox from '@components/modify-all-stores-checkbox';
 
 const GridExtensions = {
   AsyncToggleColumnExtension,
@@ -142,7 +142,7 @@ const initPrestashopComponents = () => {
     Router,
     ColorPicker,
     DeltaQuantityInput,
-    MultiStoreModifyAll,
+    ModifyAllStoresCheckbox,
   };
 };
 export default initPrestashopComponents;
