0ba067b0e4fb2a5b0f26a9163a8ef1f54f82af56
Pierre RAMBAUD <pierre.rambaud@prestashop.com>
2021-09-06 14:05:05 +0200

    Remove user assistance, and refactor the js

--- install-dev/theme/views/footer.php
+++ install-dev/theme/views/footer.php
@@ -12,20 +12,3 @@
 
   <li>&copy; 2007-<?php echo date('Y'); ?></li>
 </ul>
-
-<script type="text/javascript">
-  if (typeof psuser_assistance != 'undefined')
-  {
-	var errors = new Array();
-
-	$.each($('li.fail'), function(i, item){
-	  errors.push($(this).text().trim());
-	});
-
-	psuser_assistance.setStep('install_<?php echo addslashes(self::$steps->current()->getName()); ?>', {'error': errors + ' || {"version": "' + ps_version + '"}'});
-
-	if (errors.length) {
-	  $('#iframe_help').attr('src', $('#iframe_help').attr('src') + '&errors=' + encodeURI(errors.join(', ')));
-    }
-  }
-</script>
