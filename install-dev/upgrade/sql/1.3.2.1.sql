0a08e25119b62bc41b30476207573fd823f5ae10
Pierre RAMBAUD <pierre.rambaud@prestashop.com>
2021-09-06 10:54:15 +0200

    Remove upgrade scripts in favor of autoupgrade process

--- install-dev/upgrade/sql/1.3.2.1.sql
+++
@@ -1,3 +0,0 @@
-SET NAMES 'utf8';
-
-
--- install-dev/upgrade/sql/1.6.1.5.sql
+++
@@ -1,3 +0,0 @@
-SET NAMES 'utf8';
-
-UPDATE `PREFIX_configuration` c SET `value` = '{"avoid":[]}' WHERE `name` IN ('PS_INVCE_INVOICE_ADDR_RULES', 'PS_INVCE_DELIVERY_ADDR_RULES');
