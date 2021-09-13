f754994c2634a45e29a00a6f50a8c3f0db958724
Thalassa Ingénierie Web <bdesprez@thalassa.fr>
2021-09-13 16:18:53 +0200

    Attribute is obviously bool
    Co-authored-by: GoT <PierreRambaud@users.noreply.github.com>

--- classes/checkout/CheckoutSession.php
+++ classes/checkout/CheckoutSession.php
@@ -199,7 +199,7 @@ class CheckoutSessionCore
 
     public function setSeparatePackagesAllowed(bool $separatePackagesAllowed): bool
     {
-        $this->context->cart->allow_separated_package = (bool) $separatePackagesAllowed;
+        $this->context->cart->allow_separated_package = $separatePackagesAllowed;
 
         return $this->context->cart->update();
     }
