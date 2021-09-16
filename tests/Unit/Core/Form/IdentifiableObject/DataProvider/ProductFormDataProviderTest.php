8a989cdd47357418f678ff5593cde0c8856783f0
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2021-09-16 09:56:15 +0200

    Fix unit test after rebase

--- tests/Unit/Core/Form/IdentifiableObject/DataProvider/ProductFormDataProviderTest.php
+++ tests/Unit/Core/Form/IdentifiableObject/DataProvider/ProductFormDataProviderTest.php
@@ -252,6 +252,7 @@ class ProductFormDataProviderTest extends TestCase
             false,
             42,
             self::HOME_CATEGORY_ID,
+            $this->mockCategoryDataProvider(),
             self::CONTEXT_LANG_ID,
             null
         );
@@ -266,6 +267,7 @@ class ProductFormDataProviderTest extends TestCase
             false,
             42,
             self::HOME_CATEGORY_ID,
+            $this->mockCategoryDataProvider(),
             self::CONTEXT_LANG_ID,
             $contextShopId
         );
