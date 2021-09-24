ffb955347df37465fcad966e74e60e31ef313488
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2021-09-24 11:41:23 +0200

    Refactor the grid controller to have more possibilities and control, and each action is now separated in a dedicated test function instead of being all done in the setup

--- tests/Integration/Core/Form/IdentifiableObject/Handler/FormHandlerChecker.php
+++ tests/Integration/Core/Form/IdentifiableObject/Handler/FormHandlerChecker.php
@@ -53,6 +53,9 @@ class FormHandlerChecker implements FormHandlerInterface
         $this->formHandler = $formHandler;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function handle(FormInterface $form)
     {
         $result = $this->formHandler->handle($form);
@@ -61,11 +64,17 @@ class FormHandlerChecker implements FormHandlerInterface
         return $result;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public function handleFor($id, FormInterface $form)
     {
-        return $this->formHandler->handleFor($form, $id);
+        return $this->formHandler->handleFor($id, $form);
     }
 
+    /**
+     * @return int
+     */
     public function getLastCreatedId(): int
     {
         return $this->lastCreatedId;
