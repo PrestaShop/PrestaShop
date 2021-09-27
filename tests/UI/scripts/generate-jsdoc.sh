36f5c30d3fa0cff48b581e035590fbcfbc4f1a21
Boubker Bribri <boubker.bribri@prestashop.com>
2021-09-27 16:14:23 +0200

    Add suggestions

--- tests/UI/scripts/generate-jsdoc.sh
+++ tests/UI/scripts/generate-jsdoc.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
 # To print only errors
-set +x
+set -x
+
+# Get tests/UI directory path
+PROJECT_PATH=$(cd "$( dirname "$0" )/../" && pwd)
+cd $PROJECT_PATH
 
 DOC_DIRECTORY=".doc"
 PAGES_DIRECTORY="pages"
 FAKER_DIRECTORY="campaigns/data/faker"
 
 # Create .doc directory
-mkdir -p $DOC_DIRECTORY
+mkdir -p $PROJECT_PATH/$DOC_DIRECTORY
 
 # Function to generate documentation
 function generate_doc() {
-  JS_DIRECTORY=$1
-  JS_DIRECTORIES=$(find $JS_DIRECTORY -type d)
-  JS_FILES=($(find $JS_DIRECTORY -type f -name \*.js))
+  local JS_DIRECTORY=$1
+  local JS_DIRECTORIES=$(find $JS_DIRECTORY -type d)
+  local JS_FILES=($(find $JS_DIRECTORY -type f -name \*.js))
 
   # 1 Create directories
-  cd $DOC_DIRECTORY
+  cd $PROJECT_PATH/$DOC_DIRECTORY
   mkdir -p $JS_DIRECTORIES
 
   # 2 Create documentation for each file
-  cd ..
+  cd $PROJECT_PATH
   for JS_FILE in "${JS_FILES[@]}"; do
-    jsdoc2md --no-gfm --files $JS_FILE > $DOC_DIRECTORY/$JS_FILE.md
+    jsdoc2md --no-gfm --files $PROJECT_PATH/$JS_FILE > $PROJECT_PATH/$DOC_DIRECTORY/$JS_FILE.md
   done
 
   echo "File generated for '$JS_DIRECTORY', Check errors if printed"
