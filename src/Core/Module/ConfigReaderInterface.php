981001af0639608a61194e4a09ea0dec45b03a5c
Pierre RAMBAUD <pierre.rambaud@prestashop.com>
2021-09-30 10:35:57 +0200

    Add capability to select modules and theme to install during the installation process

--- src/Core/Module/ConfigReaderInterface.php
+++ src/Core/Module/ConfigReaderInterface.php
@@ -24,33 +24,19 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  */
 
-/**
- * Step 2 : display license form
- */
-class InstallControllerHttpLicense extends InstallControllerHttp implements HttpConfigureInterface
+namespace PrestaShop\PrestaShop\Core\Module;
+
+use PrestaShop\PrestaShop\Core\Util\ArrayFinder;
+
+interface ConfigReaderInterface
 {
     /**
-     * {@inheritdoc}
+     * Read file properties
+     *
+     * @param string $name The module name
+     * @param string $isoCode The current iso code format fr_FR
+     *
+     * @return ArrayFinder|null
      */
-    public function processNextStep(): void
-    {
-        $this->session->licence_agrement = (bool) Tools::getValue('licence_agrement');
-        $this->session->configuration_agrement = Tools::getValue('configuration_agrement');
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function validate(): bool
-    {
-        return (bool) $this->session->licence_agrement;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function display(): void
-    {
-        $this->displayContent('license');
-    }
+    public function read(string $name, string $isoCode): ?ArrayFinder;
 }
