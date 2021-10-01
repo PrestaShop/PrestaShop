5fb89cce257beaa49ca6bd90509a3979228aab68
Daniel Hlavacek <daniel.hlavacek@hotmail.cz>
2021-10-01 13:24:19 +0200

    Remove unneeded check

--- classes/checkout/CheckoutPaymentStep.php
+++ classes/checkout/CheckoutPaymentStep.php
@@ -53,26 +53,6 @@ class CheckoutPaymentStepCore extends AbstractCheckoutStep
 
     public function handleRequest(array $requestParams = [])
     {
-        $cart = $this->getCheckoutSession()->getCart();
-        $allProductsInStock = $cart->isAllProductsInStock();
-        $allProductsExist = $cart->checkAllProductsAreStillAvailableInThisState();
-        $allProductsHaveMinimalQuantity = $cart->checkAllProductsHaveMinimalQuantities();
-
-        if ($allProductsInStock !== true || $allProductsExist !== true || $allProductsHaveMinimalQuantity !== true) {
-            $cartShowUrl = $this->context->link->getPageLink(
-                'cart',
-                null,
-                $this->context->language->id,
-                [
-                    'action' => 'show',
-                ],
-                false,
-                null,
-                false
-            );
-            Tools::redirect($cartShowUrl);
-        }
-
         if (isset($requestParams['select_payment_option'])) {
             $this->selected_payment_option = $requestParams['select_payment_option'];
         }
