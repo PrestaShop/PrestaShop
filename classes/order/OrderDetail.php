679541187333db7d89b72d1a294a8ef4ce0f453f
Daniel Hlavacek <daniel.hlavacek@hotmail.cz>
2021-10-04 11:51:19 +0200

    Do not use virtual cart for saving quantity in stock

--- classes/order/OrderDetail.php
+++ classes/order/OrderDetail.php
@@ -752,7 +752,7 @@ class OrderDetailCore extends ObjectModel
         $this->product_weight = $product['id_product_attribute'] ? (float) $product['weight_attribute'] : (float) $product['weight'];
         $this->id_warehouse = $id_warehouse;
 
-        $product_quantity = (int) Product::getQuantity($this->product_id, $this->product_attribute_id, null, $cart);
+        $product_quantity = (int) Product::getQuantity($this->product_id, $this->product_attribute_id);
         $this->product_quantity_in_stock = ($product_quantity - (int) $product['cart_quantity'] < 0) ?
             $product_quantity : (int) $product['cart_quantity'];
 
