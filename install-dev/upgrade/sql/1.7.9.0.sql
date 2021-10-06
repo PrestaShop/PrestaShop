7c634a8991912b590fc49436751e5870fa4f1703
Okom3pom <contact@okom3pom.com>
2021-10-06 20:51:06 +0200

    try

--- install-dev/upgrade/sql/1.7.9.0.sql
+++ install-dev/upgrade/sql/1.7.9.0.sql
@@ -11,4 +11,4 @@ INSERT IGNORE INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `po
   (NULL, 'actionValidateOrderAfter', 'New Order', 'This hook is called after validating an order by core', '1'),
   (NULL, 'actionAdminOrdersTrackingNumberUpdate', 'After setting the tracking number for the order', 'This hook allows you to execute code after the unique tracking number for the order was added', '1'),
   (NULL, 'displayBackOfficeEmployeeMenu', 'Administration menu', 'This hook is displayed in the employee menu', '1')
-;
+; 
