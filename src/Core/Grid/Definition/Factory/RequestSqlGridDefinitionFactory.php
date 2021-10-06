3b1d78ed21857ce49273c2c4dc2d7ea5ec72e20a
Boubker Bribri <boubker.bribri@prestashop.com>
2021-10-06 10:41:58 +0200

    Add wording suggestion

--- src/Core/Grid/Definition/Factory/RequestSqlGridDefinitionFactory.php
+++ src/Core/Grid/Definition/Factory/RequestSqlGridDefinitionFactory.php
@@ -107,7 +107,7 @@ final class RequestSqlGridDefinitionFactory extends AbstractGridDefinitionFactor
                         'actions' => (new RowActionCollection())
                             ->add(
                                 (new LinkRowAction('export'))
-                                    ->setName($this->trans('Export', [], 'Admin.Global'))
+                                    ->setName($this->trans('Export', [], 'Admin.Actions'))
                                     ->setIcon('cloud_download')
                                     ->setOptions([
                                         'route' => 'admin_sql_requests_export',
