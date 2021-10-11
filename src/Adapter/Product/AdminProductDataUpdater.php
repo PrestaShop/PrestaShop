1f33db205ece26d33a16ef3be95d6b4a0fdbd90a
Simone Gennari <43071148+gennaris@users.noreply.github.com>
2021-10-11 10:17:58 +0200

    Fixes PHPCSFixer errors.
    Co-authored-by: Progi1984 <progi1984@gmail.com>

--- src/Adapter/Product/AdminProductDataUpdater.php
+++ src/Adapter/Product/AdminProductDataUpdater.php
@@ -235,7 +235,6 @@ class AdminProductDataUpdater implements ProductInterface
                     Search::indexation(false, $product->id);
                 }
                 StockAvailable::setProductOutOfStock($product->id, StockAvailable::outOfStock($id_product_old));
-                
                 return $product->id;
             }
         } else {
