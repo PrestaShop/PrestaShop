cadb61457f85d8f2b36b3e994cec9343f783e328
Tatu Wikman <tatu.wikman@gmail.com>
2021-10-11 13:22:49 +0300

    Update src/Core/Domain/Configuration/ShopConfigurationInterface.php
    Co-authored-by: Progi1984 <progi1984@gmail.com>

--- src/Core/Domain/Configuration/ShopConfigurationInterface.php
+++ src/Core/Domain/Configuration/ShopConfigurationInterface.php
@@ -64,5 +64,5 @@ interface ShopConfigurationInterface extends ConfigurationInterface
      *
      * @return ShopConfigurationInterface
      */
-    public function remove($key);
+    public function remove(string $key): ShopConfigurationInterface;
 }
