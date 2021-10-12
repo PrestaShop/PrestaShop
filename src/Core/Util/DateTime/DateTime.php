db5c85f3452c39850018272dea1ca4efa6f2784f
Julius Zukauskas <julius.zukauskas@invertus.eu>
2021-10-12 20:30:45 +0300

    small cosmetic fixes

--- src/Core/Util/DateTime/DateTime.php
+++ src/Core/Util/DateTime/DateTime.php
@@ -27,6 +27,7 @@
 namespace PrestaShop\PrestaShop\Core\Util\DateTime;
 
 use DateTimeImmutable;
+use DateTimeInterface;
 use RuntimeException;
 
 /**
@@ -68,6 +69,8 @@ final class DateTime
     }
 
     /**
+     * @param DateTimeInterface|string|null $value
+     *
      * @return bool
      */
     public static function isNull($value): bool
