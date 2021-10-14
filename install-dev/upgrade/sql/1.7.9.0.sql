0a2ded3ca776065b741c28935041e8f0f86a8919
Jonathan Danse <j.danse@prestaedit.com>
2021-10-14 21:07:58 +0200

    Add actionPresentPaymentOptions hook

--- install-dev/upgrade/sql/1.7.9.0.sql
+++ install-dev/upgrade/sql/1.7.9.0.sql
@@ -10,5 +10,6 @@ INSERT INTO `PREFIX_configuration` (`name`, `value`, `date_add`, `date_upd`) VAL
 INSERT IGNORE INTO `PREFIX_hook` (`id_hook`, `name`, `title`, `description`, `position`) VALUES
   (NULL, 'actionValidateOrderAfter', 'New Order', 'This hook is called after validating an order by core', '1'),
   (NULL, 'actionAdminOrdersTrackingNumberUpdate', 'After setting the tracking number for the order', 'This hook allows you to execute code after the unique tracking number for the order was added', '1'),
-  (NULL, 'displayBackOfficeEmployeeMenu', 'Administration menu', 'This hook is displayed in the employee menu', '1')
+  (NULL, 'displayBackOfficeEmployeeMenu', 'Administration menu', 'This hook is displayed in the employee menu', '1'),
+  (NULL, 'actionPresentPaymentOptions', 'Payment options Presenter', 'This hook is called before payment options are presented', '1')
 ;
