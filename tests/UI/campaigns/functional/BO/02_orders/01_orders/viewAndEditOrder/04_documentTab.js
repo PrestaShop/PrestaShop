0133d3a703800be35a92003bcb3d2318f297f620
Nesrine ABDMOULEH <nesrine.abdmouleh.info@gmail.com>
2021-10-22 12:13:01 +0200

    Add pre-condition and post-condition tests

---
+++ tests/UI/campaigns/functional/BO/02_orders/01_orders/viewAndEditOrder/04_documentTab.js
@@ -0,0 +1,280 @@
+require('module-alias/register');
+
+const {expect} = require('chai');
+
+// Import utils
+const helper = require('@utils/helpers');
+const testContext = require('@utils/testContext');
+
+// Import login steps
+const loginCommon = require('@commonTests/loginBO');
+
+// Import BO pages
+const loginPage = require('@pages/BO/login/index');
+const dashboardPage = require('@pages/BO/dashboard');
+const customersPage = require('@pages/BO/customers');
+const ordersPage = require('@pages/BO/orders');
+const viewOrderPage = require('@pages/BO/orders/view');
+
+// Import FO pages
+const foLoginPage = require('@pages/FO/login');
+const foHomePage = require('@pages/FO/home');
+const foProductPage = require('@pages/FO/product');
+const foCartPage = require('@pages/FO/cart');
+const foCheckoutPage = require('@pages/FO/checkout');
+const foOrderConfirmationPage = require('@pages/FO/checkout/orderConfirmation');
+
+// Import demo data
+const {DefaultCustomer} = require('@data/demo/customer');
+const {PaymentMethods} = require('@data/demo/paymentMethods');
+const {Statuses} = require('@data/demo/orderStatuses');
+
+// Import faker data
+const AddressFaker = require('@data/faker/address');
+const CustomerFaker = require('@data/faker/customer');
+
+const baseContext = 'functional_BO_login_passwordReminder';
+
+let browserContext;
+let page;
+
+const addressData = new AddressFaker({country: 'France'});
+const customerData = new CustomerFaker({password: ''});
+
+/*
+Pre-condition :
+- Create order by guest
+- Create order by default customer
+Scenario :
+
+Post-condition :
+- Delete guest account
+ */
+
+describe('BO - Orders - View and edit order : Check order documents block', async () => {
+  // before and after functions
+  before(async function () {
+    browserContext = await helper.createBrowserContext(this.browser);
+    page = await helper.newTab(browserContext);
+  });
+
+  after(async () => {
+    await helper.closeBrowserContext(browserContext);
+  });
+
+  // Pre-condition - Create order by guest
+  describe('Create order by guest in FO', async () => {
+    it('should view my shop', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToFO', baseContext);
+
+      await foHomePage.goToFo(page);
+      await foHomePage.changeLanguage(page, 'en');
+
+      // Change FO language
+      await foHomePage.changeLanguage(page, 'en');
+
+      const isHomePage = await foHomePage.isHomePage(page);
+      await expect(isHomePage, 'Fail to open FO home page').to.be.true;
+    });
+
+    it('should add product to cart and proceed to checkout', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'addProductToCart', baseContext);
+
+      await foHomePage.goToHomePage(page);
+
+      // Go to the fourth product page
+      await foHomePage.goToProductPage(page, 4);
+
+      // Add the created product to the cart
+      await foProductPage.addProductToTheCart(page, 1);
+
+      // Proceed to checkout the shopping cart
+      await foCartPage.clickOnProceedToCheckout(page);
+
+      // Go to checkout page
+      const isCheckoutPage = await foCheckoutPage.isCheckoutPage(page);
+      await expect(isCheckoutPage).to.be.true;
+    });
+
+    it('should fill guest personal information', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'setPersonalInformation', baseContext);
+
+      const isStepPersonalInfoCompleted = await foCheckoutPage.setGuestPersonalInformation(page, customerData);
+      await expect(isStepPersonalInfoCompleted, 'Step personal information is not completed').to.be.true;
+    });
+
+    it('should fill address form and go to delivery step', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'setAddressStep', baseContext);
+
+      const isStepAddressComplete = await foCheckoutPage.setAddress(page, addressData);
+      await expect(isStepAddressComplete, 'Step Address is not complete').to.be.true;
+    });
+
+    it('should validate the order', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'validateOrder', baseContext);
+
+      // Delivery step - Go to payment step
+      const isStepDeliveryComplete = await foCheckoutPage.goToPaymentStep(page);
+      await expect(isStepDeliveryComplete, 'Step Address is not complete').to.be.true;
+
+      // Payment step - Choose payment step
+      await foCheckoutPage.choosePaymentAndOrder(page, PaymentMethods.wirePayment.moduleName);
+      const cardTitle = await foOrderConfirmationPage.getOrderConfirmationCardTitle(page);
+
+      // Check the confirmation message
+      await expect(cardTitle).to.contains(foOrderConfirmationPage.orderConfirmationCardTitle);
+    });
+  });
+
+  // Pre-condition - Create order by default customer
+  describe('Create order by default customer in FO', async () => {
+    it('should go to login page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToLoginFO', baseContext);
+
+      await foHomePage.goToHomePage(page);
+
+      await foHomePage.goToLoginPage(page);
+
+      const pageTitle = await foLoginPage.getPageTitle(page);
+      await expect(pageTitle, 'Fail to open FO login page').to.contains(foLoginPage.pageTitle);
+    });
+
+    it('should sign in with default customer', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'sighInFO', baseContext);
+
+      await foLoginPage.customerLogin(page, DefaultCustomer);
+
+      const isCustomerConnected = await foLoginPage.isCustomerConnected(page);
+      await expect(isCustomerConnected, 'Customer is not connected').to.be.true;
+    });
+
+    it('should add product to cart and proceed to checkout', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'addProductToCart2', baseContext);
+
+      await foHomePage.goToHomePage(page);
+
+      // Go to the first product page
+      await foHomePage.goToProductPage(page, 1);
+
+      // Add the product to the cart
+      await foProductPage.addProductToTheCart(page);
+
+      const notificationsNumber = await foCartPage.getCartNotificationsNumber(page);
+      await expect(notificationsNumber).to.be.equal(1);
+    });
+
+    it('should go to delivery step', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToDeliveryStep', baseContext);
+
+      // Proceed to checkout the shopping cart
+      await foCartPage.clickOnProceedToCheckout(page);
+
+      // Address step - Go to delivery step
+      const isStepAddressComplete = await foCheckoutPage.goToDeliveryStep(page);
+      await expect(isStepAddressComplete, 'Step Address is not complete').to.be.true;
+    });
+
+    it('should go to payment step', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToPaymentStep', baseContext);
+
+      // Delivery step - Go to payment step
+      const isStepDeliveryComplete = await foCheckoutPage.goToPaymentStep(page);
+      await expect(isStepDeliveryComplete, 'Step Address is not complete').to.be.true;
+    });
+
+    it('should choose payment method and confirm the order', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'confirmOrder', baseContext);
+
+      // Payment step - Choose payment step
+      await foCheckoutPage.choosePaymentAndOrder(page, PaymentMethods.wirePayment.moduleName);
+
+      // Check the confirmation message
+      const cardTitle = await foOrderConfirmationPage.getOrderConfirmationCardTitle(page);
+      await expect(cardTitle).to.contains(foOrderConfirmationPage.orderConfirmationCardTitle);
+    });
+  });
+
+  // 1 - Go to view order page
+  describe('Go to view order page', async () => {
+    it('should login in BO by default employee', async function () {
+      await loginCommon.loginBO(this, page);
+    });
+
+    it('should go to \'Orders > Orders\' page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToOrdersPage1', baseContext);
+
+      await dashboardPage.goToSubMenu(
+        page,
+        dashboardPage.ordersParentLink,
+        dashboardPage.ordersLink,
+      );
+
+      await ordersPage.closeSfToolBar(page);
+
+      const pageTitle = await ordersPage.getPageTitle(page);
+      await expect(pageTitle).to.contains(ordersPage.pageTitle);
+    });
+
+    it('should reset all filters', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'resetOrderTableFilters1', baseContext);
+
+      const numberOfOrders = await ordersPage.resetAndGetNumberOfLines(page);
+      await expect(numberOfOrders).to.be.above(0);
+    });
+
+    it(`should filter the Orders table by 'Customer: ${customerData.lastName}'`, async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'filterByCustomer1', baseContext);
+
+      await ordersPage.filterOrders(page, 'input', 'customer', customerData.lastName);
+
+      const textColumn = await ordersPage.getTextColumn(page, 'customer', 1);
+      await expect(textColumn).to.contains(customerData.lastName);
+    });
+
+    it('should view the order', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'viewOrderPage1', baseContext);
+
+      await ordersPage.goToOrder(page, 1);
+
+      const pageTitle = await viewOrderPage.getPageTitle(page);
+      await expect(pageTitle).to.contains(viewOrderPage.pageTitle);
+    });
+  });
+
+  // Post-condition - Delete guest account
+  describe('Delete the created guest account', async () => {
+    it('should go \'Customers >  Customers\' page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToCustomersPage', baseContext);
+
+      await dashboardPage.goToSubMenu(page, dashboardPage.customersParentLink, dashboardPage.customersLink);
+
+      await customersPage.closeSfToolBar(page);
+
+      const pageTitle = await customersPage.getPageTitle(page);
+      await expect(pageTitle).to.contains(customersPage.pageTitle);
+    });
+
+    it('should filter list by customer email', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'filterToDelete', baseContext);
+
+      await customersPage.filterCustomers(page, 'input', 'email', customerData.email);
+
+      const textResult = await customersPage.getTextColumnFromTableCustomers(page, 1, 'email');
+      await expect(textResult).to.contains(customerData.email);
+    });
+
+    it('should delete customer and check result', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'deleteCustomer', baseContext);
+
+      const deleteTextResult = await customersPage.deleteCustomer(page, 1);
+      await expect(deleteTextResult).to.be.equal(customersPage.successfulDeleteMessage);
+    });
+
+    it('should reset all filters', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'resetAfterDeleteCustomer', baseContext);
+
+      const numberOfCustomersAfterReset = await customersPage.resetAndGetNumberOfLines(page);
+      await expect(numberOfCustomersAfterReset).to.be.above(0);
+    });
+  });
+});
