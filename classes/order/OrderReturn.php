3a3305f79fb86ee82cf0372ca085fca1d35dc43f
Daniel Prestaworks <daniel@prestaworks.se>
2021-10-25 12:55:25 +0200

    Update OrderReturn.php

--- classes/order/OrderReturn.php
+++ classes/order/OrderReturn.php
@@ -67,9 +67,9 @@ class OrderReturnCore extends ObjectModel
         /* Classic product return */
         if ($order_detail_list) {
             foreach ($order_detail_list as $key => $order_detail) {
-                $orderdetail = new OrderDetail((int) $order_detail);
-                $id_customization = $orderdetail->id_customization;
                 if ($qty = (int) $product_qty_list[$key]) {
+                    $orderdetail = new OrderDetail((int) $order_detail);
+                    $id_customization = $orderdetail->id_customization;
                     Db::getInstance()->insert('order_return_detail', ['id_order_return' => (int) $this->id, 'id_order_detail' => (int) $order_detail, 'product_quantity' => $qty, 'id_customization' => (int) $id_customization]);
                 }
             }
