bb1c7331f3536dc966b0ef92c9af37e898201f1d
alexandrebrubeyonds <92941405+alexandrebrubeyonds@users.noreply.github.com>
2021-10-27 18:23:42 +0200

    Fix Stock movements page don't show time

--- admin-dev/themes/new-theme/js/app/pages/stock/components/movements/movement-line.vue
+++ admin-dev/themes/new-theme/js/app/pages/stock/components/movements/movement-line.vue
@@ -99,8 +99,9 @@
       },
       dateAdd() {
         const date = new Date(Date.parse(this.product.date_add));
+        const dateOptions = {hour: 'numeric', minute: 'numeric', second: 'numeric'};
 
-        return date.toLocaleDateString(window.data.locale, {});
+        return date.toLocaleDateString(window.data.locale, dateOptions);
       },
     },
     components: {
