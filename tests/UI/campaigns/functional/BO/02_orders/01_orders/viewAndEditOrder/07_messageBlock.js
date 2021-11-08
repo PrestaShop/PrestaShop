cad3428cd89879cd54480e41257efa42bdfc6d6c
Nesrine ABDMOULEH <nesrine.abdmouleh.info@gmail.com>
2021-11-08 16:26:10 +0100

    Fix double test identifiers

--- tests/UI/campaigns/functional/BO/02_orders/01_orders/viewAndEditOrder/07_messageBlock.js
+++ tests/UI/campaigns/functional/BO/02_orders/01_orders/viewAndEditOrder/07_messageBlock.js
@@ -151,7 +151,7 @@ describe('BO - Orders - View and edit order : Check message block', async () => 
     });
 
     it('should sign out from FO', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'signOutFO', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'signOutFO1', baseContext);
 
       await foOrderConfirmationPage.logout(page);
 
@@ -167,7 +167,7 @@ describe('BO - Orders - View and edit order : Check message block', async () => 
     });
 
     it('should go to \'Orders > Orders\' page', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'goToOrdersPage1', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'goToOrdersPage', baseContext);
 
       await dashboardPage.goToSubMenu(
         page,
@@ -269,7 +269,7 @@ describe('BO - Orders - View and edit order : Check message block', async () => 
     });
 
     it('should sign in with default customer', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'sighInFoToCheckStatus', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'signInFo1', baseContext);
 
       await foLoginPage.customerLogin(page, DefaultCustomer);
       const isCustomerConnected = await foLoginPage.isCustomerConnected(page);
@@ -277,7 +277,7 @@ describe('BO - Orders - View and edit order : Check message block', async () => 
     });
 
     it('should go to orders history page', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'goToOrderHistoryPage', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'goToOrderHistoryPage1', baseContext);
 
       await foHomePage.goToMyAccountPage(page);
 
@@ -288,7 +288,7 @@ describe('BO - Orders - View and edit order : Check message block', async () => 
     });
 
     it('should go to the first order in the list and check order message', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'checkOrderMessageBlock', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'checkOrderMessageBlock1', baseContext);
 
       await foOrderHistoryPage.goToDetailsPage(page, 1);
 
@@ -301,7 +301,7 @@ describe('BO - Orders - View and edit order : Check message block', async () => 
 
     // Issue https://github.com/PrestaShop/PrestaShop/issues/26532
     /*it('should check the message text', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'checkOrderMessageBlock', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'checkOrderMessageBlock2', baseContext);
 
       const message = await foOrderHistoryPage.getMessageRow(page);
       await expect(message)
@@ -311,7 +311,7 @@ describe('BO - Orders - View and edit order : Check message block', async () => 
     });*/
 
     it('should sign out from FO', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'signOutFO', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'signOutFO2', baseContext);
 
       await foOrderConfirmationPage.logout(page);
 
@@ -323,7 +323,7 @@ describe('BO - Orders - View and edit order : Check message block', async () => 
   // 4 - Send second message and uncheck display to customer
   describe('Uncheck display to customer and send message', async () => {
     it('should go back to BO', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'goBackToBo', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'goBackToBo1', baseContext);
 
       // Close page and init page objects
       page = await foOrderConfirmationPage.closePage(browserContext, page, 0);
@@ -333,7 +333,7 @@ describe('BO - Orders - View and edit order : Check message block', async () => 
     });
 
     it('should send second message', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'sendMessage', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'sendMessage1', baseContext);
 
       const textMessage = await viewOrderPage.sendMessage(page, secondMessageData);
       expect(textMessage).to.equal(viewOrderPage.commentSuccessfullMessage);
@@ -392,7 +392,7 @@ describe('BO - Orders - View and edit order : Check message block', async () => 
     });
 
     it('should sign in with default customer', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'sighInFoToCheckStatus', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'signInFo2', baseContext);
 
       await foLoginPage.customerLogin(page, DefaultCustomer);
       const isCustomerConnected = await foLoginPage.isCustomerConnected(page);
@@ -400,7 +400,7 @@ describe('BO - Orders - View and edit order : Check message block', async () => 
     });
 
     it('should go to orders history page', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'goToOrderHistoryPage', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'goToOrderHistoryPage1', baseContext);
 
       await foHomePage.goToMyAccountPage(page);
 
@@ -410,17 +410,10 @@ describe('BO - Orders - View and edit order : Check message block', async () => 
       await expect(pageTitle, 'Fail to open order history page').to.contains(foOrderHistoryPage.pageTitle);
     });
 
-    it('should go to the first order in the list and check order message', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'checkOrderMessageBlock', baseContext);
+    it('should go to the first order in the list and check that new message is not visible', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'checkOrderMessageBlock3', baseContext);
 
       await foOrderHistoryPage.goToDetailsPage(page, 1);
-
-      const isVisible = await foOrderHistoryPage.isBoxMessagesSectionVisible(page);
-      await expect(isVisible, 'Order status is not correct').to.be.true;
-    });
-
-    it('should check that the second message is not visible', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'checkOrderMessageBlock', baseContext);
 
       // New message is on the first row
       const message = await foOrderHistoryPage.getMessageRow(page, 1);
@@ -431,7 +424,7 @@ describe('BO - Orders - View and edit order : Check message block', async () => 
   // 6 - Send message from FO and check it on BO
   describe('Send message from FO and check it on BO', async () => {
     it('should send message', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'sendMessage', baseContext);
+      await testContext.addContextItem(this, 'testIdentifier', 'sendMessage2', baseContext);
 
       const alertMessage = await foOrderHistoryPage.sendMessage(page, messageToSendData);
       expect(alertMessage, 'Success message is not displayed!').to.equal(foOrderHistoryPage.messageSuccessSent);
