dfe5c92c2a5cd3e760fc05ec2486a234d2e391e7
Jevgenij Visockij <jevgenij.visockij@invertus.eu>
2021-11-09 17:42:42 +0200

    Added better validation to Invoices Options form
    # Conflicts:
    #	src/Core/ConstraintValidator/Constraints/NoTags.php
    #	src/Core/ConstraintValidator/NoTagsValidator.php
    
    # Conflicts:
    #	src/PrestaShopBundle/Controller/Admin/Sell/Order/InvoicesController.php
    #	src/PrestaShopBundle/Resources/config/services/bundle/form/form_data_provider.yml

--- controllers/admin/AdminLegacyLayoutController.php
+++ controllers/admin/AdminLegacyLayoutController.php
@@ -123,6 +123,10 @@ class AdminLegacyLayoutControllerCore extends AdminController
         $this->useRegularH1Structure = $useRegularH1Structure;
     }
 
+    public function postProcess()
+    {
+    }
+
     /**
      * This helps avoiding handling legacy processes when in Symfony Controllers.
      * Otherwise when using POST action to render form you sometimes get an exception.
