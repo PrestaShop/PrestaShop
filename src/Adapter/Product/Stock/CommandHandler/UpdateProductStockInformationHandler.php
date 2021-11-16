37de255140040a480882a997d77175f0307ec757
Julius Zukauskas <julius.zukauskas@invertus.eu>
2021-11-16 15:15:43 +0200

    introduce DeltaQuantity VO. BC break - movement reason arg added in handler; BC break deltaQuantity prop change in command

--- src/Adapter/Product/Stock/CommandHandler/UpdateProductStockInformationHandler.php
+++ src/Adapter/Product/Stock/CommandHandler/UpdateProductStockInformationHandler.php
@@ -28,8 +28,8 @@ declare(strict_types=1);
 
 namespace PrestaShop\PrestaShop\Adapter\Product\Stock\CommandHandler;
 
-use PrestaShop\PrestaShop\Adapter\Product\Stock\Update\ProductStockUpdater;
 use PrestaShop\PrestaShop\Adapter\Product\Stock\Update\ProductStockProperties;
+use PrestaShop\PrestaShop\Adapter\Product\Stock\Update\ProductStockUpdater;
 use PrestaShop\PrestaShop\Core\Domain\Product\Stock\Command\UpdateProductStockInformationCommand;
 use PrestaShop\PrestaShop\Core\Domain\Product\Stock\CommandHandler\UpdateProductStockInformationHandlerInterface;
 
