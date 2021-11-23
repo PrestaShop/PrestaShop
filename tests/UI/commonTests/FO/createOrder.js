df419b397ce3183d798575501f0a702298ab58ad
Nesrine ABDMOULEH <nesrine.abdmouleh.info@gmail.com>
2021-11-23 14:36:48 +0100

    Add test for change order status

---
+++ tests/UI/commonTests/FO/createOrder.js
@@ -0,0 +1,114 @@
+require('module-alias/register');
+// Import utils
+const helper = require('@utils/helpers');
+
+// Import FO pages
+const homePage = require('@pages/FO/home');
+const foLoginPage = require('@pages/FO/login');
+const productPage = require('@pages/FO/product');
+const cartPage = require('@pages/FO/cart');
+const orderConfirmationPage = require('@pages/FO/checkout/orderConfirmation');
+const checkoutPage = require('@pages/FO/checkout');
+
+// Import data
+const {DefaultCustomer} = require('@data/demo/customer');
+const {PaymentMethods} = require('@data/demo/paymentMethods');
+
+// Import test context
+const testContext = require('@utils/testContext');
+
+// Import expect from chai
+const {expect} = require('chai');
+
+let browserContext;
+let page;
+
+function createOrderFromFO(baseContext = 'commonTests-createOrderFO') {
+  describe('PRE-TEST: Create order in FO', async () => {
+    // before and after functions
+    before(async function () {
+      browserContext = await helper.createBrowserContext(this.browser);
+      page = await helper.newTab(browserContext);
+    });
+
+    after(async () => {
+      await helper.closeBrowserContext(browserContext);
+    });
+
+    it('should open FO page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'openFO', baseContext);
+
+      // Go to FO and change language
+      await homePage.goToFo(page);
+
+      await homePage.changeLanguage(page, 'en');
+
+      const isHomePage = await homePage.isHomePage(page);
+      await expect(isHomePage, 'Fail to open FO home page').to.be.true;
+    });
+
+    it('should go to login page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToLoginPageFO', baseContext);
+
+      await homePage.goToLoginPage(page);
+      const pageTitle = await foLoginPage.getPageTitle(page);
+      await expect(pageTitle, 'Fail to open FO login page').to.contains(foLoginPage.pageTitle);
+    });
+
+    it('should sign in with default customer', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'signInFO', baseContext);
+
+      await foLoginPage.customerLogin(page, DefaultCustomer);
+      const isCustomerConnected = await foLoginPage.isCustomerConnected(page);
+      await expect(isCustomerConnected, 'Customer is not connected').to.be.true;
+    });
+
+    it('should add product to cart', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'addProductToCart', baseContext);
+
+      // Go to home page
+      await foLoginPage.goToHomePage(page);
+
+      // Go to the first product page
+      await homePage.goToProductPage(page, 1);
+
+      // Add the product to the cart
+      await productPage.addProductToTheCart(page);
+
+      const notificationsNumber = await cartPage.getCartNotificationsNumber(page);
+      await expect(notificationsNumber).to.be.equal(1);
+    });
+
+    it('should go to delivery step', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToDeliveryStep', baseContext);
+
+      // Proceed to checkout the shopping cart
+      await cartPage.clickOnProceedToCheckout(page);
+
+      // Address step - Go to delivery step
+      const isStepAddressComplete = await checkoutPage.goToDeliveryStep(page);
+      await expect(isStepAddressComplete, 'Step Address is not complete').to.be.true;
+    });
+
+    it('should go to payment step', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToPaymentStep', baseContext);
+
+      // Delivery step - Go to payment step
+      const isStepDeliveryComplete = await checkoutPage.goToPaymentStep(page);
+      await expect(isStepDeliveryComplete, 'Step Address is not complete').to.be.true;
+    });
+
+    it('should choose payment method and confirm the order', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'confirmOrder', baseContext);
+
+      // Payment step - Choose payment step
+      await checkoutPage.choosePaymentAndOrder(page, PaymentMethods.wirePayment.moduleName);
+
+      // Check the confirmation message
+      const cardTitle = await orderConfirmationPage.getOrderConfirmationCardTitle(page);
+      await expect(cardTitle).to.contains(orderConfirmationPage.orderConfirmationCardTitle);
+    });
+  });
+}
+
+module.exports = {createOrderFromFO};
--- tests/UI/pages/BO/orders/view.js
+++ tests/UI/pages/BO/orders/view.js
@@ -26,6 +26,13 @@ class Order extends BOBasePage {
     this.noAvailableDocumentsMessage = 'There is no available document';
     this.updateSuccessfullMessage = 'Update successful';
 
+    // Order actions selectors
+    this.orderStatusesSelect = '#update_order_status_action_input';
+    this.updateStatusButton = '#update_order_status_action_btn';
+    this.viewInvoiceButton = 'form.order-actions-invoice a[data-role=\'view-invoice\']';
+    this.viewDeliverySlipButton = 'form.order-actions-delivery a[data-role=\'view-delivery-slip\']';
+    this.partialRefundButton = 'button.partial-refund-display';
+
     // Customer card
     this.customerInfoBlock = '#customerInfo';
     this.ViewAllDetailsLink = '#viewFullDetails a';
@@ -69,7 +76,6 @@ class Order extends BOBasePage {
     this.editProductQuantityInput = `${this.orderProductsEditRowTable} input.editProductQuantity`;
     this.editProductPriceInput = `${this.orderProductsEditRowTable} input.editProductPriceTaxIncl`;
     this.UpdateProductButton = `${this.orderProductsEditRowTable} button.productEditSaveBtn`;
-    this.partialRefundButton = 'button.partial-refund-display';
     this.orderTotalPriceSpan = '#orderTotal';
     this.returnProductsButton = '#order-view-page button.return-product-display';
     this.addProductTableRow = '#addProductTableRow';
@@ -91,9 +97,8 @@ class Order extends BOBasePage {
     this.paginationNextLink = '#orderProductsTablePaginationNext';
     this.paginationPreviousLink = '#orderProductsTablePaginationPrev';
 
-    // Status card
-    this.orderStatusesSelect = '#update_order_status_action_input';
-    this.updateStatusButton = '#update_order_status_action_btn';
+    // Status tab
+    this.historyTabContent = '#historyTabContent';
 
     // Documents tab
     this.documentTab = 'a#orderDocumentsTab';
@@ -163,6 +168,127 @@ class Order extends BOBasePage {
   Methods
    */
 
+  // Methods for order actions
+  /**
+   * Is update status button disabled
+   * @param page {Page} Browser tab
+   * @returns {Promise<boolean>}
+   */
+  isUpdateStatusButtonDisabled(page) {
+    return this.elementVisible(page, `${this.updateStatusButton},disabled`, 1000);
+  }
+
+  /**
+   * Is view invoice button visible
+   * @param page {Page} Browser tab
+   * @returns {Promise<boolean>}
+   */
+  isViewInvoiceButtonVisible(page) {
+    return this.elementVisible(page, this.viewInvoiceButton, 1000);
+  }
+
+  /**
+   * Is partial refund button visible
+   * @param page {Page} Browser tab
+   * @returns {Promise<boolean>}
+   */
+  isPartialRefundButtonVisible(page) {
+    return this.elementVisible(page, this.partialRefundButton, 1000);
+  }
+
+  /**
+   * Is delivery slip button visible
+   * @param page {Page} Browser tab
+   * @returns {Promise<boolean>}
+   */
+  isDeliverySlipButtonVisible(page) {
+    return this.elementVisible(page, this.viewDeliverySlipButton, 1000);
+  }
+
+  /**
+   * Select order status
+   * @param page {Page} Browser tab
+   * @param status {string} Status to edit
+   * @returns {Promise<void>}
+   */
+  async selectOrderStatus(page, status) {
+    await this.selectByVisibleText(page, this.orderStatusesSelect, status);
+  }
+
+  /**
+   * Modify the order status
+   * @param page {Page} Browser tab
+   * @param status {string} Status to edit
+   * @returns {Promise<string>}
+   */
+  async modifyOrderStatus(page, status) {
+    const actualStatus = await this.getOrderStatus(page);
+
+    if (status !== actualStatus) {
+      await this.selectByVisibleText(page, this.orderStatusesSelect, status);
+      await this.clickAndWaitForNavigation(page, this.updateStatusButton);
+      return this.getOrderStatus(page);
+    }
+    return actualStatus;
+  }
+
+  /**
+   * Get order status
+   * @param page {Page} Browser tab
+   * @returns {Promise<string>}
+   */
+  async getOrderStatus(page) {
+    return this.getTextContent(page, `${this.orderStatusesSelect} option[selected='selected']`, false);
+  }
+
+  /**
+   * Does status exist
+   * @param page {Page} Browser tab
+   * @param statusName {string} Status to check
+   * @returns {Promise<boolean>}
+   */
+  async doesStatusExist(page, statusName) {
+    let options = await page.$$eval(
+      `${this.orderStatusesSelect} option`,
+      all => all.map(
+        option => ({
+          textContent: option.textContent,
+          value: option.value,
+        })),
+    );
+
+    options = options.filter(option => statusName === option.textContent);
+    return options.length !== 0;
+  }
+
+  /**
+   * Get statuses number
+   * @param page {Page} Browser tab
+   * @returns {Promise<number>}
+   */
+  async getStatusesNumber(page) {
+    return this.getNumberFromText(page, this.historyTabContent);
+  }
+
+  /**
+   * Click on view invoice button to download the invoice
+   * @param page {Page} Browser tab
+   * @returns {Promise<void>}
+   */
+  async viewInvoice(page) {
+    return this.clickAndWaitForDownload(page, this.viewInvoiceButton);
+  }
+
+  /**
+   * Click on view delivery slip button to download the invoice
+   * @param page {Page} Browser tab
+   * @returns {Promise<void>}
+   */
+  async viewDeliverySlip(page) {
+    return this.clickAndWaitForDownload(page, this.viewDeliverySlipButton);
+  }
+
+  // Methods for customer block
   /**
    * Get shipping address from customer card
    * @param page {Page} Browser tab
@@ -249,52 +375,6 @@ class Order extends BOBasePage {
     await this.dialogListener(page);
     await this.waitForSelectorAndClick(page, this.deleteProductButton(row));
     return this.getGrowlMessageContent(page);
-  }
-
-  /**
-   * Modify the order status
-   * @param page {Page} Browser tab
-   * @param status {string} Status to edit
-   * @returns {Promise<string>}
-   */
-  async modifyOrderStatus(page, status) {
-    const actualStatus = await this.getOrderStatus(page);
-
-    if (status !== actualStatus) {
-      await this.selectByVisibleText(page, this.orderStatusesSelect, status);
-      await this.clickAndWaitForNavigation(page, this.updateStatusButton);
-      return this.getOrderStatus(page);
-    }
-    return actualStatus;
-  }
-
-  /**
-   * Get order status
-   * @param page {Page} Browser tab
-   * @returns {Promise<string>}
-   */
-  async getOrderStatus(page) {
-    return this.getTextContent(page, `${this.orderStatusesSelect} option[selected='selected']`, false);
-  }
-
-  /**
-   * Does status exist
-   * @param page {Page} Browser tab
-   * @param statusName {string} Status to check
-   * @returns {Promise<boolean>}
-   */
-  async doesStatusExist(page, statusName) {
-    let options = await page.$$eval(
-      `${this.orderStatusesSelect} option`,
-      all => all.map(
-        option => ({
-          textContent: option.textContent,
-          value: option.value,
-        })),
-    );
-
-    options = options.filter(option => statusName === option.textContent);
-    return options.length !== 0;
   }
 
   /**
