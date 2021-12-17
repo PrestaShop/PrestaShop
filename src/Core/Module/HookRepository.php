f4133ab0ff54003ea4d52fabdee62c2c5d288deb
Prestaworks <daniel@prestaworks.se>
2021-12-17 13:53:02 +0100

    Update HookRepository.php

--- src/Core/Module/HookRepository.php
+++ src/Core/Module/HookRepository.php
@@ -60,8 +60,23 @@ class HookRepository
         return (int) $id_hook;
     }
 
+    /**
+     * Creates a new hook if not already existing and returns the hook id.
+     *
+     * @param string $hook_name The name of the hook
+     * @param string $title The title for the hook
+     * @param string $description The description for the hook
+     * @param int $position if the modules in the hook can be positioned
+     *
+     * @return int Hook ID
+     */
     public function createHook($hook_name, $title = '', $description = '', $position = 1)
     {
+        $id_hook = (int) $this->getIdByName($hook_name);
+        if ($id_hook > 0) {
+            return $id_hook;
+        }
+
         $this->db->insert('hook', [
             'name' => $this->db->escape($hook_name),
             'title' => $this->db->escape($title),
