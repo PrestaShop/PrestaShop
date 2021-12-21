7cf7dcfb05c6305b21196d8aeb57dae42cf6f226
Julien Jakubowski <julien@jakubowski.fr>
2021-12-21 22:22:17 +0100

    Update src/Adapter/Image/ImageRetriever.php
    Co-authored-by: GoT <PierreRambaud@users.noreply.github.com>

--- src/Adapter/Image/ImageRetriever.php
+++ src/Adapter/Image/ImageRetriever.php
@@ -207,7 +207,7 @@ class ImageRetriever
             $url = $this->link->$getImageURL(
                 isset($object->link_rewrite) ? $object->link_rewrite : $object->name,
                 $id_image,
-                $image_type['name'] . ($generateHighDpiImages ? self::HIGH_RES_URL_SUFFIX : '')
+                $image_type['name'] . ($generateHighDpiImages ? static::HIGH_RES_URL_SUFFIX : '')
             );
 
             $urls[$image_type['name']] = [
