56d24435f5f200c070dc9164e6617cc1f9c1c07f
Daniel Hlavacek <daniel.hlavacek@hotmail.cz>
2021-12-21 16:20:22 +0100

    Update controllers/admin/AdminStoresController.php
    Co-authored-by: Julie Varisellaz <70583503+Julievrz@users.noreply.github.com>

--- controllers/admin/AdminStoresController.php
+++ controllers/admin/AdminStoresController.php
@@ -461,7 +461,7 @@ class AdminStoresControllerCore extends AdminController
             ],
             'PS_SHOP_DETAILS' => [
                 'title' => $this->trans('Company details', [], 'Admin.Shopparameters.Feature'),
-                'hint' => $this->trans('Shop registration information (e.g. DNI, VAT number, SIRET or RCS).', [], 'Admin.Shopparameters.Help'),
+                'hint' => $this->trans('Company's registration details (e.g. DNI, VAT, SIRET, RCS, etc.).', [], 'Admin.Shopparameters.Help'),
                 'validation' => 'isGenericName',
                 'type' => 'textarea',
                 'cols' => 30,
