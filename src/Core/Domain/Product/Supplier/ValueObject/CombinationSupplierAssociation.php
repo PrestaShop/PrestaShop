4cb224d77cd2ccb52386211e149eaec053659603
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2022-01-17 19:08:51 +0100

    Introduce interface for product supplier association which has two implementing classes one for product and one for combination

--- src/Core/Domain/Product/Supplier/ValueObject/CombinationSupplierAssociation.php
+++ src/Core/Domain/Product/Supplier/ValueObject/CombinationSupplierAssociation.php
@@ -30,26 +30,18 @@ namespace PrestaShop\PrestaShop\Core\Domain\Product\Supplier\ValueObject;
 
 use PrestaShop\PrestaShop\Core\Domain\Product\Combination\ValueObject\CombinationId;
 use PrestaShop\PrestaShop\Core\Domain\Product\Combination\ValueObject\CombinationIdInterface;
-use PrestaShop\PrestaShop\Core\Domain\Product\Combination\ValueObject\NoCombinationId;
 use PrestaShop\PrestaShop\Core\Domain\Product\ValueObject\ProductId;
 use PrestaShop\PrestaShop\Core\Domain\Supplier\ValueObject\SupplierId;
 
 /**
- * This value object identifies a specific association between a supplier and a product. It is based on three criteria
- * productId, combinationId and supplierId. For product without combinations combinationId is always 0.
- *
- * The productSupplierId is optional, it may be present when the association already exists in DB or absent if we
- * haven't created it yet but we can still use this VO based on the criteria.
+ * This value object identifies a specific association between a supplier and a combination. Usually the association is
+ * based on three criteria (productId, combinationId, supplierId) @see ProductSupplierAssociation. But in a context of
+ * search only the combinationId is mandatory. So this VO can be used to search a supplier association with a combination.
  */
-class ProductSupplierAssociation implements SupplierAssociationInterface
+class CombinationSupplierAssociation implements SupplierAssociationInterface
 {
     /**
-     * @var ProductId
-     */
-    private $productId;
-
-    /**
-     * @var CombinationIdInterface
+     * @var CombinationId
      */
     private $combinationId;
 
@@ -64,25 +56,15 @@ class ProductSupplierAssociation implements SupplierAssociationInterface
     private $productSupplierId;
 
     /**
-     * @param int $productId
      * @param int $combinationId
      * @param int $supplierId
      * @param int|null $productSupplierId
      */
-    public function __construct(int $productId, int $combinationId, int $supplierId, ?int $productSupplierId = null)
+    public function __construct(int $combinationId, int $supplierId, ?int $productSupplierId = null)
     {
-        $this->productId = new ProductId($productId);
-        $this->combinationId = $combinationId ? new CombinationId($combinationId) : new NoCombinationId();
+        $this->combinationId = new CombinationId($combinationId);
         $this->supplierId = new SupplierId($supplierId);
         $this->productSupplierId = null !== $productSupplierId ? new ProductSupplierId($productSupplierId) : null;
-    }
-
-    /**
-     * @return ProductId
-     */
-    public function getProductId(): ProductId
-    {
-        return $this->productId;
     }
 
     /**
@@ -109,11 +91,21 @@ class ProductSupplierAssociation implements SupplierAssociationInterface
         return $this->productSupplierId;
     }
 
+    /**
+     * No need for product ID to identify a supplier association for a designated combination, we can match it by
+     * combination ID only.
+     *
+     * @return ProductId|null
+     */
+    public function getProductId(): ?ProductId
+    {
+        return null;
+    }
+
     public function __toString()
     {
         return sprintf(
-            '[productId: %d, combinationId: %d, supplierId: %d]',
-            $this->getProductId()->getValue(),
+            '[productId: null, combinationId: %d, supplierId: %d]',
             $this->getCombinationId()->getValue(),
             $this->getSupplierId()->getValue()
         );
