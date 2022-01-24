4bf276a40b7c934686b5c398f47ebd4356dff9dc
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2022-01-24 03:04:21 +0100

    Integrate notion of NoSupplierId

--- src/Core/Domain/Supplier/ValueObject/NoSupplierId.php
+++ src/Core/Domain/Supplier/ValueObject/NoSupplierId.php
@@ -24,48 +24,16 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  */
 
+declare(strict_types=1);
+
 namespace PrestaShop\PrestaShop\Core\Domain\Supplier\ValueObject;
 
-use PrestaShop\PrestaShop\Core\Domain\Supplier\Exception\SupplierException;
-
-/**
- * Class SupplierId
- */
-class SupplierId implements SupplierIdInterface
+class NoSupplierId implements SupplierIdInterface
 {
-    /**
-     * @var int
-     */
-    private $value;
+    public const NO_SUPPLIER_ID = 0;
 
-    /**
-     * @param int $supplierId
-     *
-     * @throws SupplierException
-     */
-    public function __construct(int $supplierId)
-    {
-        $this->assertIsIntegerGreaterThanZero($supplierId);
-        $this->value = $supplierId;
-    }
-
-    /**
-     * @return int
-     */
     public function getValue(): int
     {
-        return $this->value;
-    }
-
-    /**
-     * @param int $supplierId
-     *
-     * @throws SupplierException
-     */
-    private function assertIsIntegerGreaterThanZero(int $supplierId)
-    {
-        if (0 >= $supplierId) {
-            throw new SupplierException(sprintf('Invalid Supplier id: %s', var_export($supplierId, true)));
-        }
+        return static::NO_SUPPLIER_ID;
     }
 }
