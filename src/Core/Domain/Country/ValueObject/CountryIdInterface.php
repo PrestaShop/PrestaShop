956dc7a8dfb7d7f7c74088f621f5e4139d8066cc
Amoifr <pascal.cescon@gmail.com>
2022-02-02 14:54:31 +0100

    Implement NoCountryId and CountryIdInterface

--- src/Core/Domain/Country/ValueObject/CountryIdInterface.php
+++ src/Core/Domain/Country/ValueObject/CountryIdInterface.php
@@ -26,46 +26,10 @@
 
 namespace PrestaShop\PrestaShop\Core\Domain\Country\ValueObject;
 
-use PrestaShop\PrestaShop\Core\Domain\Country\Exception\CountryConstraintException;
-
 /**
  * Provides country id value
  */
-class CountryId implements CountryIdInterface
+interface CountryIdInterface
 {
-    /**
-     * @var int
-     */
-    private $id;
-
-    /**
-     * @param int $id
-     *
-     * @throws CountryConstraintException
-     */
-    public function __construct(int $id)
-    {
-        $this->assertPositiveInt($id);
-        $this->id = $id;
-    }
-
-    /**
-     * @return int
-     */
-    public function getValue(): int
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param int $value
-     *
-     * @throws CountryConstraintException
-     */
-    private function assertPositiveInt(int $value)
-    {
-        if (0 >= $value) {
-            throw new CountryConstraintException(sprintf('Invalid country id "%s".', var_export($value, true)), CountryConstraintException::INVALID_ID);
-        }
-    }
+    public function getValue(): int;
 }
--- src/Core/Domain/Country/ValueObject/NoCountryId.php
+++ src/Core/Domain/Country/ValueObject/NoCountryId.php
@@ -26,46 +26,21 @@
 
 namespace PrestaShop\PrestaShop\Core\Domain\Country\ValueObject;
 
-use PrestaShop\PrestaShop\Core\Domain\Country\Exception\CountryConstraintException;
-
 /**
  * Provides country id value
  */
-class CountryId implements CountryIdInterface
+class NoCountryId implements CountryIdInterface
 {
     /**
      * @var int
      */
-    private $id;
-
-    /**
-     * @param int $id
-     *
-     * @throws CountryConstraintException
-     */
-    public function __construct(int $id)
-    {
-        $this->assertPositiveInt($id);
-        $this->id = $id;
-    }
+    public const NO_COUNTRY_ID_VALUE = 0;
 
     /**
      * @return int
      */
     public function getValue(): int
     {
-        return $this->id;
-    }
-
-    /**
-     * @param int $value
-     *
-     * @throws CountryConstraintException
-     */
-    private function assertPositiveInt(int $value)
-    {
-        if (0 >= $value) {
-            throw new CountryConstraintException(sprintf('Invalid country id "%s".', var_export($value, true)), CountryConstraintException::INVALID_ID);
-        }
+        return self::NO_COUNTRY_ID_VALUE;
     }
 }
