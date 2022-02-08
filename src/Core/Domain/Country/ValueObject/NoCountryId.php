21d0981ff46c422b6eb8d5b5724b9799085e8eed
Amoifr <pascal.cescon@gmail.com>
2022-02-08 16:06:40 +0100

    Implement NoCountryId and CountryIdInterface

--- src/Core/Domain/Country/ValueObject/NoCountryId.php
+++ src/Core/Domain/Country/ValueObject/NoCountryId.php
@@ -23,49 +23,25 @@
  * @copyright Since 2007 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  */
+declare(strict_types=1);
 
 namespace PrestaShop\PrestaShop\Core\Domain\Country\ValueObject;
-
-use PrestaShop\PrestaShop\Core\Domain\Country\Exception\CountryConstraintException;
 
 /**
  * Provides country id value
  */
-class CountryId implements CountryIdInterface
+class NoCountryId implements CountryIdInterface
 {
     /**
      * @var int
      */
-    private $id;
-
-    /**
-     * @param int $id
-     *
-     * @throws CountryConstraintException
-     */
-    public function __construct(int $id)
-    {
-        $this->assertPositiveInt($id);
-        $this->id = $id;
-    }
+    public const NO_COUNTRY_ID_VALUE = 0;
 
     /**
      * @return int
      */
     public function getValue(): int
     {
-        return $this->id;
-    }
-
-    /**
-     * @param int $value
-     *
-     * @throws CountryConstraintException
-     */
-    private function assertPositiveInt(int $value)
-    {
-        if (0 >= $value) {
-            throw new CountryConstraintException(sprintf('Invalid country id "%s".', var_export($value, true)), CountryConstraintException::INVALID_ID);
-        }
+        return self::NO_COUNTRY_ID_VALUE;
     }
 }
