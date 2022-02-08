41121c37369d38a301b74bc72b2e37f84f2dc91f
Amoifr <pascal.cescon@gmail.com>
2022-02-08 16:08:20 +0100

    Implement NoStateId and StateIdInterface

--- src/Core/Domain/State/ValueObject/StateIdInterface.php
+++ src/Core/Domain/State/ValueObject/StateIdInterface.php
@@ -24,48 +24,11 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  */
 
+declare(strict_types=1);
+
 namespace PrestaShop\PrestaShop\Core\Domain\State\ValueObject;
 
-use PrestaShop\PrestaShop\Core\Domain\State\Exception\StateConstraintException;
-
-/**
- * Provides state id
- */
-class StateId implements StateIdInterface
+interface StateIdInterface
 {
-    /**
-     * @var int
-     */
-    private $id;
-
-    /**
-     * @param int $id
-     *
-     * @throws StateConstraintException
-     */
-    public function __construct(int $id)
-    {
-        $this->assertPositiveInt($id);
-        $this->id = $id;
-    }
-
-    /**
-     * @return int
-     */
-    public function getValue(): int
-    {
-        return $this->id;
-    }
-
-    /**
-     * @param int $value
-     *
-     * @throws StateConstraintException
-     */
-    private function assertPositiveInt(int $value)
-    {
-        if (0 >= $value) {
-            throw new StateConstraintException(sprintf('Invalid state id "%s".', var_export($value, true)), StateConstraintException::INVALID_ID);
-        }
-    }
+    public function getValue(): int;
 }
