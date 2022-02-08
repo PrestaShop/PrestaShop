fb47b1382010e16d194120cf4f2c8ba19bbfe648
Mickaël Fernandez <mickael@creabilis.com>
2022-02-08 15:24:58 +0100

    Customer settings form : remove const from js file

--- admin-dev/themes/new-theme/js/pages/customer-preferences/index.ts
+++ admin-dev/themes/new-theme/js/pages/customer-preferences/index.ts
@@ -23,8 +23,6 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  */
 
-const {$} = window;
-
 $(() => {
   window.prestashop.component.initComponents(
     [
