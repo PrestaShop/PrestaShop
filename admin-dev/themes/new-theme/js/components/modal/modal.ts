4bb5a908fc41f1812dca0c8785c9079867c984e7
Julius Zukauskas <julius.zukauskas@invertus.eu>
2022-02-09 12:52:09 +0200

    off modal event after hiding

--- admin-dev/themes/new-theme/js/components/modal/modal.ts
+++ admin-dev/themes/new-theme/js/components/modal/modal.ts
@@ -222,7 +222,10 @@ export class Modal implements ModalType {
   hide(): void {
     this.$modal.modal('hide');
     // Sometimes modal animation is still in progress and hiding fails, so we attach event listener for that case.
-    this.$modal.on('shown.bs.modal', () => this.$modal.modal('hide'));
+    this.$modal.on('shown.bs.modal', () => {
+      this.$modal.modal('hide');
+      this.$modal.off('shown.bs.modal');
+    });
   }
 }
 
