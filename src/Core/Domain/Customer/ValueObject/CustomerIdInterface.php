8a6bf913645d71043197422d5769245577f97685
Amoifr <pascal.cescon@gmail.com>
2022-02-09 16:35:56 +0100

    Implement NoCustomerId and CustomerIdInterface

--- src/Core/Domain/Customer/ValueObject/CustomerIdInterface.php
+++ src/Core/Domain/Customer/ValueObject/CustomerIdInterface.php
@@ -24,49 +24,9 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  */
 
-declare(strict_types=1);
-
 namespace PrestaShop\PrestaShop\Core\Domain\Customer\ValueObject;
 
-use PrestaShop\PrestaShop\Core\Domain\Customer\Exception\CustomerConstraintException;
-
-/**
- * Defines Customer ID with it's constraints
- */
-class CustomerId implements CustomerIdInterface
+interface CustomerIdInterface
 {
-    /**
-     * @var int
-     */
-    private $customerId;
-
-    /**
-     * @param int $customerId
-     */
-    public function __construct(int $customerId)
-    {
-        $this->assertIntegerIsGreaterThanZero($customerId);
-        $this->customerId = $customerId;
-    }
-
-    /**
-     * @return int
-     */
-    public function getValue(): int
-    {
-        return $this->customerId;
-    }
-
-    /**
-     * @param int $customerId
-     */
-    private function assertIntegerIsGreaterThanZero(int $customerId): void
-    {
-        if (0 >= $customerId) {
-            throw new CustomerConstraintException(
-                sprintf('Customer id %s is invalid.', $customerId),
-                CustomerConstraintException::INVALID_ID
-            );
-        }
-    }
+    public function getValue(): int;
 }
