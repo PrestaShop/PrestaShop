5767bcc79d28c4820f7e5372cbcc5afea5ec5ba5
Progi1984 <franck.lefevre@prestashop.com>
2022-02-15 13:49:11 +0100

    Multistore - Catalog > Products > Add / edit - Update message when a group is selected

--- src/PrestaShopBundle/Controller/Admin/Sell/Catalog/Product/ProductController.php
+++ src/PrestaShopBundle/Controller/Admin/Sell/Catalog/Product/ProductController.php
@@ -513,12 +513,8 @@ class ProductController extends FrameworkBundleAdminController
     {
         return $this->render('@PrestaShop/Admin/Sell/Catalog/Product/disabled.html.twig', [
             'errorMessage' => $this->trans(
-                'This page is only compatible in a single store context. To access the page, please select a store or [1]disable the multistore feature[/1].',
-                'Admin.Notifications.Info',
-                [
-                    '[1]' => sprintf('<a href="%s">', $this->get('router')->generate('admin_preferences')),
-                    '[/1]' => '</a>',
-                ]
+                'Note that this page is available in a single shop or in all shops context. Switch context to work on it.',
+                'Admin.Notifications.Info'
             ),
             'standardPageUrl' => $this->generateUrl(
                 !empty($productId) ? 'admin_product_form' : 'admin_product_new',
