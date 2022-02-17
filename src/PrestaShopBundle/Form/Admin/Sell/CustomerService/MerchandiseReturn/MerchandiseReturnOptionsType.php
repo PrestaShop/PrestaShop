199d0a9758084d25d2fd37a6224b2fb3559b5cdb
Progi1984 <franck.lefevre@prestashop.com>
2022-02-17 21:47:00 +0100

    MultiStore - Sell > Customer Service > Merchandise Return - Add Checkbox & Simplify Form

--- src/PrestaShopBundle/Form/Admin/Sell/CustomerService/MerchandiseReturn/MerchandiseReturnOptionsType.php
+++ src/PrestaShopBundle/Form/Admin/Sell/CustomerService/MerchandiseReturn/MerchandiseReturnOptionsType.php
@@ -26,9 +26,10 @@
 
 namespace PrestaShopBundle\Form\Admin\Sell\CustomerService\MerchandiseReturn;
 
+use PrestaShopBundle\Form\Admin\Type\MultistoreConfigurationType;
 use PrestaShopBundle\Form\Admin\Type\SwitchType;
 use PrestaShopBundle\Form\Admin\Type\TranslatableType;
-use Symfony\Component\Form\AbstractType;
+use PrestaShopBundle\Form\Admin\Type\TranslatorAwareType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
@@ -36,26 +37,67 @@ use Symfony\Component\Validator\Constraints\GreaterThanOrEqual;
 /**
  * Form type for merchandise returns options
  */
-class MerchandiseReturnOptionsType extends AbstractType
+class MerchandiseReturnOptionsType extends TranslatorAwareType
 {
+    public const FIELD_ENABLE_ORDER_RETURN = 'enable_order_return';
+    public const FIELD_ORDER_RETURN_PERIOD_IN_DAYS = 'order_return_period_in_days';
+    public const FIELD_ORDER_RETURN_PREFIX = 'order_return_prefix';
+
     /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('enable_order_return', SwitchType::class, [
+            ->add(static::FIELD_ENABLE_ORDER_RETURN, SwitchType::class, [
+                'label' => $this->trans(
+                    'Enable returns',
+                    'Admin.Orderscustomers.Feature'
+                ),
+                'help' => $this->trans(
+                    'Would you like to allow merchandise returns in your shop?',
+                    'Admin.Orderscustomers.Help'
+                ),
                 'required' => false,
+                'multistore_configuration_key' => 'PS_ORDER_RETURN',
             ])
-            ->add('order_return_period_in_days', IntegerType::class, [
+            ->add(static::FIELD_ORDER_RETURN_PERIOD_IN_DAYS, IntegerType::class, [
+                'label' => $this->trans(
+                    'Time limit of validity',
+                    'Admin.Orderscustomers.Feature'
+                ),
+                'help' => $this->trans(
+                    'How many days after the delivery date does the customer have to return a product?',
+                    'Admin.Orderscustomers.Help'
+                ),
                 'required' => false,
                 'constraints' => new GreaterThanOrEqual([
                     'value' => 0,
                 ]),
+                'multistore_configuration_key' => 'PS_ORDER_RETURN_NB_DAYS',
             ])
-            ->add('order_return_prefix', TranslatableType::class, [
+            ->add(static::FIELD_ORDER_RETURN_PREFIX, TranslatableType::class, [
+                'label' => $this->trans(
+                    'Return prefix',
+                    'Admin.Orderscustomers.Feature'
+                ),
+                'help' => $this->trans(
+                    'Prefix used for merchandise returns (e.g. RE00001).',
+                    'Admin.Orderscustomers.Help'
+                ),
                 'required' => false,
+                'multistore_configuration_key' => 'PS_RETURN_PREFIX',
             ])
         ;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see MultistoreConfigurationTypeExtension
+     */
+    public function getParent(): string
+    {
+        return MultistoreConfigurationType::class;
     }
 }
