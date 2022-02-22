33b2289a2449927ee217aa67c38cda6d4f086fe1
Nesrine ABDMOULEH <nesrine.abdmouleh.info@gmail.com>
2022-02-22 14:43:45 +0100

    Add test to check view customer page

--- tests/UI/campaigns/functional/BO/02_orders/01_orders/createOrders/04_selectPreviousOrders.js
+++ tests/UI/campaigns/functional/BO/02_orders/01_orders/createOrders/04_selectPreviousOrders.js
@@ -72,20 +72,52 @@ describe('BO - Orders - Create order : Select previous order', async () => {
     await expect(isCartsTableVisible).to.be.true;
   });
 
-  it('should click on \'Details\' button from customer card', async function () {
-    await testContext.addContextItem(this, 'testIdentifier', 'clickOnDetailButton', baseContext);
+  describe('Check view customer Iframe', async () => {
+    it('should click on \'Details\' button from customer card', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'clickOnDetailButton', baseContext);
 
-    const isIframeVisible = await addOrderPage.clickOnDetailsButton(page);
-    await expect(isIframeVisible).to.be.true;
-  });
+      const isIframeVisible = await addOrderPage.clickOnDetailsButton(page);
+      await expect(isIframeVisible).to.be.true;
+    });
 
-  it('should check the existence of personal information block in the iframe', async function () {
-    await testContext.addContextItem(this, 'testIdentifier', 'checkIframeContent', baseContext);
+    it('should check the existence of personal information block in the iframe', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'checkPersonalInformation', baseContext);
 
-    const cardHeaderText = await addOrderPage.getPersonalInformationTitle(page, DefaultCustomer.id);
+      const cardHeaderText = await addOrderPage.getPersonalInformationTitle(page, DefaultCustomer.id);
 
-    await expect(cardHeaderText).to.contains(DefaultCustomer.firstName);
-    await expect(cardHeaderText).to.contains(DefaultCustomer.lastName);
-    await expect(cardHeaderText).to.contains(DefaultCustomer.email);
+      await expect(cardHeaderText).to.contains(DefaultCustomer.firstName);
+      await expect(cardHeaderText).to.contains(DefaultCustomer.lastName);
+      await expect(cardHeaderText).to.contains(DefaultCustomer.email);
+    });
+
+    [
+      {args: {blockName: 'Orders', number: 5}},
+      {args: {blockName: 'Carts', number: 6}},
+      {args: {blockName: 'Viewed products', number: 7}},
+      {args: {blockName: 'Messages', number: 0}},
+      {args: {blockName: 'Vouchers', number: 0}},
+      {args: {blockName: 'Last emails', number: 0}},
+      {args: {blockName: 'Last connections', number: 0}},
+      {args: {blockName: 'Groups', number: 1}},
+      {args: {blockName: 'Addresses', number: 2}},
+    ].forEach((test) => {
+      it(`should check the ${test.args.blockName} number`, async function () {
+        await testContext.addContextItem(this, 'testIdentifier', `check${test.args.blockName}Number`, baseContext);
+
+        const cardHeaderText = await addOrderPage.getNumberOfElementFromViewCustomerPage(
+          page,
+          DefaultCustomer.id,
+          test.args.blockName,
+        );
+        await expect(parseInt(cardHeaderText, 10)).to.be.at.least(test.args.number);
+      });
+    });
+
+    it('should check the existence of add private note card', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'checkAddPrivateNote', baseContext);
+
+      const isVisible = await addOrderPage.isPrivateNoteBlockVisible(page, DefaultCustomer.id);
+      await expect(isVisible).to.be.true;
+    });
   });
 });
