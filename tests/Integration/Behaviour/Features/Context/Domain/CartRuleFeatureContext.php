f8db85c57a75fd8f426d7d1ceb6ffa4a78ea4d71
Progi1984 <franck.lefevre@prestashop.com>
2022-02-25 12:00:33 +0100

    Fixed notice 'Undefined index: reduction_applies'

--- tests/Integration/Behaviour/Features/Context/Domain/CartRuleFeatureContext.php
+++ tests/Integration/Behaviour/Features/Context/Domain/CartRuleFeatureContext.php
@@ -103,7 +103,7 @@ class CartRuleFeatureContext extends AbstractDomainFeatureContext
 
     /**
      * @Given /^I specify (cart rule|its) "([^"]+)" as "([^"]+)"$/
-     * @Given /^I specify that (its) "([^"]+)" is "([^"]+)"$/
+     * @Given /^I specify that (its) "([^"]+)" is "([^"]*)"$/
      */
     public function specifyCartRuleProperty(array $properties, string $property, string $value)
     {
