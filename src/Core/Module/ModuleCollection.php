75d02a7e83a1f107eafb1734c8890233accc0f42
atomiix <thomas.baccelli@prestashop.com>
2022-03-03 17:42:11 +0100

    Rewrite ModuleCollection

--- src/Core/Module/ModuleCollection.php
+++ src/Core/Module/ModuleCollection.php
@@ -34,24 +34,13 @@ use PrestaShopException;
 use Traversable;
 
 /**
- * An ArrayCollection is a Collection implementation that wraps a regular PHP array.
+ * This class wrap an array of ModuleInterface
  */
 class ModuleCollection implements ArrayAccess, Countable, IteratorAggregate
 {
-    /**
-     * An array containing the modules of this collection.
-     *
-     * @var ModuleInterface[]
-     */
+    /** @var ModuleInterface[] */
     private $modules = [];
 
-    /**
-     * Initializes a new ModuleCollection.
-     *
-     * @param ModuleInterface[] $modules
-     *
-     * @throws PrestaShopException
-     */
     public function __construct(array $modules = [])
     {
         foreach ($modules as $module) {
@@ -65,28 +54,13 @@ class ModuleCollection implements ArrayAccess, Countable, IteratorAggregate
     }
 
     /**
-     * Creates a new instance from the specified elements.
+     * @param ModuleInterface[] $modules
      *
-     * This method is provided for derived classes to specify how a new
-     * instance should be created when constructor semantics have changed.
-     *
-     * @param array $modules elements
-     *
-     * @return static
+     * @return ModuleCollection
      */
     public static function createFrom(array $modules): ModuleCollection
     {
         return new static($modules);
-    }
-
-    /**
-     * Gets a native PHP array representation of the collection.
-     *
-     * @return ModuleInterface[]
-     */
-    public function toArray(): array
-    {
-        return $this->modules;
     }
 
     public function getIterator(): Traversable
@@ -94,188 +68,40 @@ class ModuleCollection implements ArrayAccess, Countable, IteratorAggregate
         return new ArrayIterator($this->modules);
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function offsetExists($offset): bool
-    {
-        return $this->containsKey($offset);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function offsetGet($offset): ?ModuleInterface
-    {
-        return $this->get($offset);
-    }
-
-    /**
-     * Required by ArrayAccess interface.
-     *
-     * {@inheritdoc}
-     */
-    public function offsetSet($offset, $module): void
-    {
-        if (!isset($offset)) {
-            $this->add($module);
-
-            return;
-        }
-
-        $this->set($offset, $module);
-    }
-
-    /**
-     * Required by interface ArrayAccess.
-     *
-     * {@inheritdoc}
-     */
-    public function offsetUnset($offset): void
-    {
-        $this->remove($offset);
-    }
-
-    /**
-     * Returns true if the key is found in the collection.
-     *
-     * @param mixed $key the key, can be integer or string
-     *
-     * @return bool
-     */
-    public function containsKey($key): bool
-    {
-        return isset($this->modules[$key]) || array_key_exists($key, $this->modules);
-    }
-
-    /**
-     * Returns true if the module is found in the collection.
-     *
-     * @param ModuleInterface $module the module
-     *
-     * @return bool
-     */
-    public function contains(ModuleInterface $module): bool
-    {
-        return in_array($module, $this->modules, true);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function indexOf(ModuleInterface $module)
-    {
-        return array_search($module, $this->modules, true);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function get($key): ?ModuleInterface
-    {
-        return $this->modules[$key] ?? null;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getKeys(): array
-    {
-        return array_keys($this->modules);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getValues(): array
-    {
-        return array_values($this->modules);
-    }
-
-    /**
-     * Add a module with a specified key in the collection.
-     *
-     * @param mixed $key the key
-     * @param ModuleInterface $module the specified module
-     */
-    public function set($key, ModuleInterface $module): void
-    {
-        $this->modules[$key] = $module;
-    }
-
-    /**
-     * Add a Module in the collection.
-     *
-     * @param ModuleInterface $module the specified module
-     *
-     * @return bool
-     */
-    public function add(ModuleInterface $module): bool
-    {
-        $this->modules[] = $module;
-
-        return true;
-    }
-
-    /**
-     * Remove a module from the collection by key.
-     *
-     * @param int|string $key
-     *
-     * @return ModuleInterface|null returns the removed module or null if not found
-     */
-    public function removeByKey($key): ?ModuleInterface
-    {
-        if (!isset($this->modules[$key])) {
-            return null;
-        }
-
-        $removed = $this->modules[$key];
-        unset($this->modules[$key]);
-
-        return $removed;
-    }
-
-    /**
-     * Remove a module from the collection.
-     *
-     * @param ModuleInterface $module the module to be removed
-     *
-     * @return bool true if the module has been found and removed
-     */
-    public function remove(ModuleInterface $module): bool
-    {
-        $key = array_search($module, $this->modules, true);
-
-        if ($key === false) {
-            return false;
-        }
-
-        unset($this->modules[$key]);
-
-        return true;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function isEmpty(): bool
-    {
-        return empty($this->modules);
-    }
-
-    /**
-     * Gets the sum of modules of the collection.
-     *
-     * @return int
-     */
     public function count(): int
     {
         return count($this->modules);
     }
 
+    public function offsetExists($offset): bool
+    {
+        return array_key_exists($offset, $this->modules);
+    }
+
+    /**
+     * @param mixed $offset
+     *
+     * @return ModuleInterface|null
+     */
+    public function offsetGet($offset)
+    {
+        return $this->modules[$offset] ?? null;
+    }
+
+    public function offsetSet($offset, $value): void
+    {
+        $this->modules[$offset] = $value;
+    }
+
+    public function offsetUnset($offset): void
+    {
+        if ($this->offsetExists($offset)) {
+            unset($this->modules[$offset]);
+        }
+    }
+
     public function filter(callable $callable): ModuleCollection
     {
-        return self::createFrom(array_filter($this->modules, $callable));
+        return static::createFrom(array_filter($this->modules, $callable));
     }
 }
