e2fa2270f4be2a8d7ebf970289d324d2daba8f1a
davidglezz <davidg@empathy.co>
2022-03-13 23:09:31 +0100

    Use AbstractMultistoreConfiguration in MediaServerConfiguration

--- src/Adapter/Media/MediaServerConfiguration.php
+++ src/Adapter/Media/MediaServerConfiguration.php
@@ -26,33 +26,27 @@
 
 namespace PrestaShop\PrestaShop\Adapter\Media;
 
-use PrestaShop\PrestaShop\Adapter\Configuration;
-use PrestaShop\PrestaShop\Core\Configuration\DataConfigurationInterface;
+use PrestaShop\PrestaShop\Core\Configuration\AbstractMultistoreConfiguration;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * This class will provide Media servers configuration for a Shop.
  */
-class MediaServerConfiguration implements DataConfigurationInterface
+class MediaServerConfiguration extends AbstractMultistoreConfiguration
 {
-    /**
-     * @var Configuration
-     */
-    private $configuration;
-
-    public function __construct(Configuration $configuration)
-    {
-        $this->configuration = $configuration;
-    }
+    private const CONFIGURATION_FIELDS = ['media_server_one', 'media_server_two', 'media_server_three'];
 
     /**
      * {@inheritdoc}
      */
     public function getConfiguration()
     {
+        $shopConstraint = $this->getShopConstraint();
+
         return [
-            'media_server_one' => $this->configuration->get('PS_MEDIA_SERVER_1'),
-            'media_server_two' => $this->configuration->get('PS_MEDIA_SERVER_2'),
-            'media_server_three' => $this->configuration->get('PS_MEDIA_SERVER_3'),
+            'media_server_one' => $this->configuration->get('PS_MEDIA_SERVER_1', null, $shopConstraint),
+            'media_server_two' => $this->configuration->get('PS_MEDIA_SERVER_2', null, $shopConstraint),
+            'media_server_three' => $this->configuration->get('PS_MEDIA_SERVER_3', null, $shopConstraint),
         ];
     }
 
@@ -61,76 +55,40 @@ class MediaServerConfiguration implements DataConfigurationInterface
      */
     public function updateConfiguration(array $configuration)
     {
-        $validateResult = $this->validateConfiguration($configuration);
-        if ($validateResult !== true) {
-            return $validateResult;
+        if ($this->validateConfiguration($configuration)) {
+            $shopConstraint = $this->getShopConstraint();
+            $this->updateConfigurationValue('PS_MEDIA_SERVER_1', 'media_server_one', $configuration, $shopConstraint);
+            $this->updateConfigurationValue('PS_MEDIA_SERVER_2', 'media_server_two', $configuration, $shopConstraint);
+            $this->updateConfigurationValue('PS_MEDIA_SERVER_3', 'media_server_three', $configuration, $shopConstraint);
+
+            $serverOne = $configuration['media_server_one'];
+            $serverTwo = $configuration['media_server_two'];
+            $serverThree = $configuration['media_server_three'];
+            $hasMediaServer = !empty($serverOne) || !empty($serverTwo) || !empty($serverThree);
+            $this->configuration->set('PS_MEDIA_SERVERS', $hasMediaServer ? 1 : 0);
         }
-
-        $serverOne = $configuration['media_server_one'];
-        $serverTwo = $configuration['media_server_two'];
-        $serverThree = $configuration['media_server_three'];
-
-        $this->configuration->set('PS_MEDIA_SERVER_1', $serverOne);
-        $this->configuration->set('PS_MEDIA_SERVER_2', $serverTwo);
-        $this->configuration->set('PS_MEDIA_SERVER_3', $serverThree);
-
-        $hasMediaServer = !empty($serverOne) || !empty($serverTwo) || !empty($serverThree);
-        $this->configuration->set('PS_MEDIA_SERVERS', $hasMediaServer ? 1 : 0);
 
         return [];
     }
 
     /**
-     * @param array $configuration
-     *
-     * @return array<int, array<string, array|string>>|bool
+     * @return OptionsResolver
      */
-    public function validateConfiguration(array $configuration)
+    protected function buildResolver(): OptionsResolver
     {
-        $errors = [];
-        $serverOne = $configuration['media_server_one'];
-        $serverTwo = $configuration['media_server_two'];
-        $serverThree = $configuration['media_server_three'];
+        $isValidDomain = function (string $domainName): bool {
+            return empty($domainName)
+                || false !== filter_var($domainName, FILTER_VALIDATE_DOMAIN)
+                && false !== filter_var(gethostbyname($domainName), FILTER_VALIDATE_IP);
+        };
 
-        if (!empty($serverOne) && !$this->isValidDomain($serverOne)) {
-            $errors[] = [
-                'key' => 'Media server #1 is invalid',
-                'domain' => 'Admin.Advparameters.Notification',
-                'parameters' => [],
-            ];
-        }
-
-        if (!empty($serverTwo) && !$this->isValidDomain($serverTwo)) {
-            $errors[] = [
-                'key' => 'Media server #2 is invalid',
-                'domain' => 'Admin.Advparameters.Notification',
-                'parameters' => [],
-            ];
-        }
-
-        if (!empty($serverThree) && !$this->isValidDomain($serverThree)) {
-            $errors[] = [
-                'key' => 'Media server #3 is invalid',
-                'domain' => 'Admin.Advparameters.Notification',
-                'parameters' => [],
-            ];
-        }
-
-        if (count($errors) > 0) {
-            return $errors;
-        }
-
-        return true;
-    }
-
-    /**
-     * @param string $domainName
-     *
-     * @return bool
-     */
-    private function isValidDomain(string $domainName): bool
-    {
-        return false !== filter_var($domainName, FILTER_VALIDATE_DOMAIN)
-            && false !== filter_var(gethostbyname($domainName), FILTER_VALIDATE_IP);
+        return (new OptionsResolver())
+            ->setDefined(self::CONFIGURATION_FIELDS)
+            ->setAllowedTypes('media_server_one', 'string')
+            ->setAllowedValues('media_server_one', $isValidDomain)
+            ->setAllowedTypes('media_server_two', 'string')
+            ->setAllowedValues('media_server_two', $isValidDomain)
+            ->setAllowedTypes('media_server_three', 'string')
+            ->setAllowedValues('media_server_three', $isValidDomain);
     }
 }
