28946c5470416b8581998adde2ca72f8483d0d26
Pablo Borowicz <pablo.borowicz@prestashop.com>
2022-03-15 14:27:08 +0100

    Remove deprecated searchcron.php

--- controllers/admin/AdminShopController.php
+++ controllers/admin/AdminShopController.php
@@ -707,7 +707,7 @@ class AdminShopControllerCore extends AdminController
         array_unshift($categories, Configuration::get('PS_ROOT_CATEGORY'));
         Category::updateFromShop($categories, $object->id);
         if (Tools::getValue('useImportData') && ($import_data = Tools::getValue('importData')) && is_array($import_data) && isset($import_data['product'])) {
-            ini_set('max_execution_time', '7200'); // like searchcron.php
+            ini_set('max_execution_time', '7200');
             Search::indexation(true);
         }
 
