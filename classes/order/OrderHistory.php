0d1aa34dd7d60f6cd7d82a4e4531c8e45183d77f
Hervé Hennes <nenes_fr@yahoo.fr>
2022-03-22 21:15:03 +0100

    Remove OrderHistory deprecated function

--- classes/order/OrderHistory.php
+++ classes/order/OrderHistory.php
@@ -426,34 +426,6 @@ class OrderHistoryCore extends ObjectModel
     }
 
     /**
-     * Returns the last order status.
-     *
-     * @param int $id_order
-     *
-     * @return OrderState|bool
-     *
-     * @deprecated 1.5.0.4
-     * @see Order->current_state
-     */
-    public static function getLastOrderState($id_order)
-    {
-        Tools::displayAsDeprecated();
-        $id_order_state = Db::getInstance()->getValue('
-        SELECT `id_order_state`
-        FROM `' . _DB_PREFIX_ . 'order_history`
-        WHERE `id_order` = ' . (int) $id_order . '
-        ORDER BY `date_add` DESC, `id_order_history` DESC');
-
-        // returns false if there is no state
-        if (!$id_order_state) {
-            return false;
-        }
-
-        // else, returns an OrderState object
-        return new OrderState((int) $id_order_state, (int) Configuration::get('PS_LANG_DEFAULT'));
-    }
-
-    /**
      * @param bool $autodate Optional
      * @param array|bool $template_vars Optional
      * @param Context|null $context Deprecated
