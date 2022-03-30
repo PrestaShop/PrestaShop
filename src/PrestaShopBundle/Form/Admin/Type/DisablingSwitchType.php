74d1b426fd606c537930996ebc32664d7388629d
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2022-03-30 18:59:18 +0200

    Refacto of the option to add disabling switch, rename of the feature and split the feature into two listeners, also improve integration in the form theme

--- src/PrestaShopBundle/Form/Admin/Type/DisablingSwitchType.php
+++ src/PrestaShopBundle/Form/Admin/Type/DisablingSwitchType.php
@@ -31,17 +31,8 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class DisablingToggleType extends SwitchType
+class DisablingSwitchType extends SwitchType
 {
-    public const DEFAULT_OPTIONS = [
-        'disable_on_match' => true,
-        'matching_value' => '0',
-        'label' => false,
-        'row_attr' => [
-            'class' => 'ps-disabling-toggle',
-        ],
-    ];
-
     /**
      * {@inheritDoc}
      */
@@ -50,9 +41,17 @@ class DisablingToggleType extends SwitchType
         parent::configureOptions($resolver);
 
         $resolver
-            ->setDefaults(self::DEFAULT_OPTIONS)
-            ->setAllowedTypes('disable_on_match', 'bool')
-            ->setAllowedTypes('matching_value', ['string'])
+            // The set default options from parent type
+            ->setDefaults([
+                'target_selector' => '',
+                'show_choices' => false,
+                'label' => false,
+                'required' => false,
+                'row_attr' => [
+                    'class' => 'ps-disabling-switch',
+                ],
+            ])
+            ->setAllowedTypes('target_selector', 'string')
         ;
     }
 
@@ -62,10 +61,8 @@ class DisablingToggleType extends SwitchType
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
         parent::buildView($view, $form, $options);
-        $view->vars['row_attr']['data-disabling-toggle-name'] = $form->getName();
-        $view->vars['row_attr']['data-disabling-toggle-disable-on-match'] = $options['disable_on_match'];
-        //@todo: if i try to assign boolean it doesnt work as e.g. with false the value is empty in data attr and js part fails
-        $view->vars['row_attr']['data-disabling-toggle-matching-value'] = (string) $options['matching_value'];
+        $view->vars['attr']['data-target-selector'] = $options['target_selector'];
+        $view->vars['attr']['data-matching-value'] = '0';
     }
 
     public function getParent(): string
