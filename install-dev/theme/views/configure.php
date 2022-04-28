d66ec82a8f1dea35b77dc1362fe243a0b2271d95
GoT <PierreRambaud@users.noreply.github.com>
2022-04-28 10:30:41 +0200

    Update install-dev/theme/views/configure.php
    Co-authored-by: Julie Varisellaz <70583503+Julievrz@users.noreply.github.com>

--- install-dev/theme/views/configure.php
+++ install-dev/theme/views/configure.php
@@ -172,7 +172,7 @@
     </div>
     <div class="password-strength-text"></div>
     <div class="password-requirements">
-      <p class="password-requirements-length" data-translation="<?php echo htmlspecialchars($this->translator->trans('Enter a password between %s and %s characters', [], 'Install')); ?>">
+      <p class="password-requirements-length" data-translation="<?php echo htmlspecialchars($this->translator->trans('Enter a password between %d and %d characters', [], 'Install')); ?>">
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0z" fill="none"/><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/></svg>
         <span></span>
       </p>
