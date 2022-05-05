5be17c60023cd341558b944f9a2b4d539db093b2
Julius Zukauskas <julius.zukauskas@invertus.eu>
2022-05-05 18:19:56 +0300

    getShopId from filters instead of context in list

--- src/PrestaShopBundle/Controller/Admin/Sell/Catalog/Product/CombinationController.php
+++ src/PrestaShopBundle/Controller/Admin/Sell/Catalog/Product/CombinationController.php
@@ -332,7 +332,7 @@ class CombinationController extends FrameworkBundleAdminController
         $combinationsList = $this->getQueryBus()->handle(new GetEditableCombinationsList(
             $productId,
             $this->getContextLangId(),
-            $this->getContextShopId(),
+            $combinationFilters->getShopId(),
             $combinationFilters->getLimit(),
             $combinationFilters->getOffset(),
             $combinationFilters->getOrderBy(),
