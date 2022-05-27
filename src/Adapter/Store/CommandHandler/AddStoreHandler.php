42b2d844b40ee2d42af728f583050a1eb6c6a9eb
Tom Combet <tom.combet@sfr.fr>
2022-05-27 11:28:56 +0200

    Simplify exception handling

--- src/Adapter/Store/CommandHandler/AddStoreHandler.php
+++ src/Adapter/Store/CommandHandler/AddStoreHandler.php
@@ -30,14 +30,12 @@ namespace PrestaShop\PrestaShop\Adapter\Store\CommandHandler;
 
 use Exception;
 use PrestaShop\PrestaShop\Adapter\Domain\AbstractObjectModelHandler;
-use PrestaShop\PrestaShop\Core\Domain\Contact\Exception\CannotAddContactException;
 use PrestaShop\PrestaShop\Core\Domain\Store\Command\AddStoreCommand;
 use PrestaShop\PrestaShop\Core\Domain\Store\CommandHandler\AddStoreHandlerInterface;
 use PrestaShop\PrestaShop\Core\Domain\Store\Exception\CannotAddStoreException;
 use PrestaShop\PrestaShop\Core\Domain\Store\Exception\StoreException;
 use PrestaShop\PrestaShop\Core\Domain\Store\Repository\StoreRepository;
 use PrestaShop\PrestaShop\Core\Domain\Store\ValueObject\StoreId;
-use PrestaShopException;
 use Store;
 
 /**
@@ -61,58 +59,58 @@ class AddStoreHandler extends AbstractObjectModelHandler implements AddStoreHand
      */
     public function handle(AddStoreCommand $command): StoreId
     {
-        try {
-            $entity = new Store();
+        $entity = new Store();
 
-            $entity->id_country = $command->getCountryId()->getValue();
-    
-            if (null !== $command->getStateId()) {
-                $entity->id_state = $command->getStateId()->getValue();
-            }
-    
-            $entity->postcode = $command->getPostcode()->getValue();
-            $entity->city = $command->getCity()->getValue();
-    
-            if (null !== $command->getLatitude()) {
-                $entity->latitude = $command->getLatitude()->getValue();
-            }
-    
-            if (null !== $command->getLongitude()) {
-                $entity->longitude = $command->getLongitude()->getValue();
-            }
-    
-            if (null !== $command->getPhone()) {
-                $entity->phone = $command->getPhone()->getValue();
-            }
-    
-            if (null !== $command->getFax()) {
-                $entity->fax = $command->getFax()->getValue();
-            }
-    
-            if (null !== $command->getEmail()) {
-                $entity->email = $command->getEmail()->getValue();
-            }
-    
-            foreach ($command->getLocalizedNames() as $langId => $name) {
-                $entity->name[$langId] = $name->getValue();
-            }
-    
-            foreach ($command->getLocalizedAddress1() as $langId => $address) {
-                $entity->address1[$langId] = $address->getValue();
-            }
-    
-            foreach ($command->getLocalizedAddress2() as $langId => $address) {
-                $entity->address2[$langId] = $address->getValue();
-            }
-    
-            foreach ($command->getLocalizedHours() as $langId => $hours) {
-                $entity->hours[$langId] = $hours->getValue();
-            }
-    
-            foreach ($command->getLocalizedNotes() as $langId => $note) {
-                $entity->note[$langId] = $note->getValue();
-            }
-            
+        $entity->id_country = $command->getCountryId()->getValue();
+
+        if (null !== $command->getStateId()) {
+            $entity->id_state = $command->getStateId()->getValue();
+        }
+
+        $entity->postcode = $command->getPostcode()->getValue();
+        $entity->city = $command->getCity()->getValue();
+
+        if (null !== $command->getLatitude()) {
+            $entity->latitude = $command->getLatitude()->getValue();
+        }
+
+        if (null !== $command->getLongitude()) {
+            $entity->longitude = $command->getLongitude()->getValue();
+        }
+
+        if (null !== $command->getPhone()) {
+            $entity->phone = $command->getPhone()->getValue();
+        }
+
+        if (null !== $command->getFax()) {
+            $entity->fax = $command->getFax()->getValue();
+        }
+
+        if (null !== $command->getEmail()) {
+            $entity->email = $command->getEmail()->getValue();
+        }
+
+        foreach ($command->getLocalizedNames() as $langId => $name) {
+            $entity->name[$langId] = $name->getValue();
+        }
+
+        foreach ($command->getLocalizedAddress1() as $langId => $address) {
+            $entity->address1[$langId] = $address->getValue();
+        }
+
+        foreach ($command->getLocalizedAddress2() as $langId => $address) {
+            $entity->address2[$langId] = $address->getValue();
+        }
+
+        foreach ($command->getLocalizedHours() as $langId => $hours) {
+            $entity->hours[$langId] = $hours->getValue();
+        }
+
+        foreach ($command->getLocalizedNotes() as $langId => $note) {
+            $entity->note[$langId] = $note->getValue();
+        }
+
+        try {
             $id = $this->storeRepository->add($entity);
 
             if (null !== $command->getShopAssociation()) {
