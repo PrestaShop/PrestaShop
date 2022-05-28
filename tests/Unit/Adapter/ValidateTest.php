4588e44898a3f55455bc02772b373ff33e495e33
PululuK <pululuandre@hotmail.com>
2022-05-28 19:05:17 +0200

    Fix tests

--- tests/Unit/Adapter/ValidateTest.php
+++ tests/Unit/Adapter/ValidateTest.php
@@ -127,7 +127,7 @@ class ValidateTest extends TestCase
 
     /**
      * @param bool $expected
-     * @param mixed $value
+     * @param mixed $objectClassName
      *
      * @dataProvider isValidObjectClassNameDataProvider
      */
@@ -145,8 +145,6 @@ class ValidateTest extends TestCase
             [true, '_MyClassName_'],
             [true, '__My__Class__Name__'],
             [false, ''],
-            [false, null],
-            [false, 666],
             [false, '666'],
             [true, '_666'],
             [true, '_6_6_6_'],
