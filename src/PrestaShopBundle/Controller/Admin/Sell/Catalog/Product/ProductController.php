b949de8efbdd3de4c81bb51889c2e889818e9faa
Matthieu Rolland <matthieu.rolland@prestashop.com>
2022-05-30 10:45:21 +0200

    Update src/PrestaShopBundle/Controller/Admin/Sell/Catalog/Product/ProductController.php
    Co-authored-by: Progi1984 <progi1984@gmail.com>

--- src/PrestaShopBundle/Controller/Admin/Sell/Catalog/Product/ProductController.php
+++ src/PrestaShopBundle/Controller/Admin/Sell/Catalog/Product/ProductController.php
@@ -506,7 +506,7 @@ class ProductController extends FrameworkBundleAdminController
     /**
      * @return bool
      */
-    private function shouldRedirectToV1(): bool
+    protected function shouldRedirectToV1(): bool
     {
         $multistoreFeature = $this->get('prestashop.adapter.multistore_feature');
 
