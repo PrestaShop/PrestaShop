0aa01fa6dd9ade3b65575791a4de2fae948bafa0
Jean-François Viguier <jf.viguier@gmail.com>
2022-06-09 17:45:05 +0200

    Fix carriers with same names don't show in Shipping > Preferences

--- src/Core/Form/ChoiceProvider/CarrierByReferenceChoiceProvider.php
+++ src/Core/Form/ChoiceProvider/CarrierByReferenceChoiceProvider.php
@@ -73,7 +73,7 @@ final class CarrierByReferenceChoiceProvider implements FormChoiceProviderInterf
 
         foreach ($carriers as $carrier) {
             $choiceId = $carrier['id_carrier'] . ' - ' . $carrier['name'];
-            if ($carrier['name']) {
+            if (isset($carrier['delay'])) {
                 $choiceId .= ' (' . $carrier['delay'] . ')';
             }
 
--- src/PrestaShopBundle/Form/Admin/Product/ProductShipping.php
+++ src/PrestaShopBundle/Form/Admin/Product/ProductShipping.php
@@ -95,7 +95,7 @@ class ProductShipping extends CommonAbstractType
         $this->carriersChoices = [];
         foreach ($carriers as $carrier) {
             $choiceId = $carrier['id_carrier'] . ' - ' . $carrier['name'];
-            if ($carrier['name']) {
+            if (isset($carrier['delay'])) {
                 $choiceId .= ' (' . $carrier['delay'] . ')';
             }
 
