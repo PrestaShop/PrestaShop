98dfff093c5b038f071809bc303c475b810b7763
Progi1984 <franck.lefevre@prestashop.com>
2022-06-15 16:59:12 +0200

    Target the table on the reload of the page after a sort

--- admin-dev/themes/new-theme/js/pages/customer/index.ts
+++ admin-dev/themes/new-theme/js/pages/customer/index.ts
@@ -86,4 +86,12 @@ $(() => {
   new LinkableItem();
 
   new FormSubmitButton();
+
+  // Scroll to the block
+  const documentURL = new URL(document.URL);
+  const documentHash = documentURL.hash.slice(1);
+
+  if (documentHash !== '') {
+    document.getElementById(documentHash)?.scrollIntoView();
+  }
 });
