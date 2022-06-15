5ced2094a9e90ab6e7595dd8d259c3a92ef83279
Khouloud BELGUITH <khouloud.belguith@prestashop.com>
2022-06-15 18:13:37 +0100

    Add Pagination to the outstanding page

--- tests/UI/pages/BO/customers/outstanding/index.js
+++ tests/UI/pages/BO/customers/outstanding/index.js
@@ -24,6 +24,12 @@ class Outstanding extends BOBasePage {
     this.tableRow = row => `${this.tableBody} tr:nth-child(${row})`;
     this.tableColumn = (row, column) => `${this.tableRow(row)} td.column-${column}`;
     this.tableColumnActionType = (row, column) => `${this.tableColumn(row, column)} a`;
+
+    this.cardHeaderTitle = '#outstanding_grid_panel .card-header-title';
+    this.paginationLabel = '#outstanding_grid .col-form-label';
+    this.paginationLimitSelect = '#paginator_select_page_limit';
+    this.paginationNextLink = '#pagination_next_url';
+    this.paginationPreviousLink = '#outstanding_grid_panel .pagination .previous a.page-link';
   }
 
   /* Methods */
@@ -70,6 +76,63 @@ class Outstanding extends BOBasePage {
   async viewInvoice(page, columnName, row = 1) {
     return this.clickAndWaitForDownload(page, this.tableColumnActionType(row, 'invoice'));
   }
+
+  /**
+   * Get the number of outstanding
+   * @param page
+   * @returns {Promise<number>}
+   */
+  async getNumberOutstanding(page) {
+    return this.getNumberFromText(page, this.cardHeaderTitle);
+  }
+
+  /* Pagination methods */
+  /**
+   * Get pagination label
+   * @param page {Page} Browser tab
+   * @return {Promise<string>}
+   */
+  getPaginationLabel(page) {
+    return this.getTextContent(page, this.paginationLabel);
+  }
+
+  /**
+   * Select pagination limit
+   * @param page {Page} Browser tab
+   * @param number {number} Value of pagination limit to select
+   * @returns {Promise<string>}
+   */
+  async selectPaginationLimit(page, number) {
+    await Promise.all([
+      this.selectByVisibleText(page, this.paginationLimitSelect, number),
+      page.waitForNavigation({waitUntil: 'networkidle'}),
+    ]);
+
+    return this.getPaginationLabel(page);
+  }
+
+  /**
+   * Click on next
+   * @param page {Page} Browser tab
+   * @returns {Promise<string>}
+   */
+  async paginationNext(page) {
+    await this.clickAndWaitForNavigation(page, this.paginationNextLink);
+
+    return this.getPaginationLabel(page);
+  }
+
+  /**
+   * Click on previous
+   * @param page {Page} Browser tab
+   * @returns {Promise<string>}
+   */
+  async paginationPrevious(page) {
+    await this.clickAndWaitForNavigation(page, this.paginationPreviousLink);
+
+    return this.getPaginationLabel(page);
+  }
+
 }
 
 module.exports = new Outstanding();
