38311b5588d867930bb4cef9facabd89e95a95db
Okom3pom <contact@okom3pom.com>
2022-06-18 09:06:38 +0200

    Allow to translate placeholder

--- classes/Tools.php
+++ classes/Tools.php
@@ -949,7 +949,11 @@ class ToolsCore
     {
         $format = Context::getContext()->language->date_format_lite;
         $search = ['d', 'm', 'Y'];
-        $replace = ['DD', 'MM', 'YYYY'];
+        $replace = [
+            Context::getContext()->getTranslator()->trans('DD', [], 'Shop.Forms.Help'),
+            Context::getContext()->getTranslator()->trans('MM', [], 'Shop.Forms.Help'),
+            Context::getContext()->getTranslator()->trans('YYYY', [], 'Shop.Forms.Help'),
+        ];
         $format = str_replace($search, $replace, $format);
 
         return $format;
