6748d01138ff88d9d133ca9a887a7cd4a38f1124
NeOMakinG <valentin.szczupak@prestashop.com>
2022-06-22 15:19:47 +0200

    Fix default theme compatibility

--- admin-dev/themes/default/webpack.config.js
+++ admin-dev/themes/default/webpack.config.js
@@ -24,9 +24,7 @@
  */
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const keepLicense = require('uglify-save-license');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const FontPreloadPlugin = require('webpack-font-preload-plugin');
@@ -48,53 +46,53 @@ module.exports = (env, argv) => {
       filename: '[name].bundle.js',
     },
     module: {
-      rules: [{
-        test: path.join(__dirname, 'js'),
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', {modules: false}],
-            ],
-          },
-        }],
-      }, {
-        test: /\.(scss|sass|css)$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: 'css-loader',
-          },
-          {
-            loader: 'postcss-loader',
-          },
-          {
-            loader: 'sass-loader',
-          },
-        ],
-      },
-      {
-        test: /\.(jpg|png|woff2?|eot|otf|ttf|svg|gif)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[hash].[ext]',
+      rules: [
+        {
+          test: path.join(__dirname, 'js'),
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: [['@babel/preset-env', {modules: false}]],
+              },
+            },
+          ],
         },
-        exclude: /MaterialIcons-Regular\.(woff2?|ttf)$/,
-      },
-      {
-        test: /MaterialIcons-Regular\.(woff2?|ttf)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[hash].preload.[ext]',
+        {
+          test: /\.(scss|sass|css)$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+            },
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'postcss-loader',
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
         },
-      },
+        {
+          test: /\.(jpg|png|woff2?|eot|otf|ttf|svg|gif)$/,
+          type: 'asset/resource',
+          generator: {
+            filename: '[hash][ext]',
+          },
+          exclude: /MaterialIcons-Regular\.(woff2?|ttf)$/,
+        },
+        {
+          test: /MaterialIcons-Regular\.(woff2?|ttf)$/,
+          type: 'asset/resource',
+          generator: {
+            filename: '[hash].preload[ext]',
+          },
+        },
       ],
     },
-    optimization: {
-
-    },
+    optimization: {},
     plugins: [
       new RemoveEmptyScriptsPlugin(),
       new CleanWebpackPlugin({
@@ -117,7 +115,8 @@ module.exports = (env, argv) => {
         extensions: ['woff2'],
         filter: /preload/,
         // eslint-disable-next-line
-        replaceCallback: ({indexSource, linksAsString}) => indexSource.replace('{{{preloadLinks}}}', linksAsString.replace(/href="auto/g, 'href="{"`$admin_dir`"}')),
+        replaceCallback: ({indexSource, linksAsString}) =>
+          indexSource.replace('{{{preloadLinks}}}', linksAsString.replace(/href="/g, 'href="{"`$admin_dir`"}')),
       }),
       new CssoWebpackPlugin({
         forceMediaMerge: true,
@@ -125,27 +124,23 @@ module.exports = (env, argv) => {
       new LicensePlugin({
         outputFilename: 'thirdPartyNotice.json',
         licenseOverrides: {
-          'vazirmatn@32.102.0': 'OFL-1.1'
+          'vazirmatn@32.102.0': 'OFL-1.1',
         },
         replenishDefaultLicenseTexts: true,
-      })
+      }),
     ],
   };
 
   if (!devMode) {
-    config.optimization.minimizer = [
-      new UglifyJsPlugin({
-        sourceMap: false,
-        uglifyOptions: {
-          compress: {
-            drop_console: true,
-          },
-          output: {
-            comments: keepLicense,
-          },
-        },
-      }),
-    ];
+    config.optimization = {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          parallel: true,
+          extractComments: false,
+        }),
+      ],
+    };
   }
 
   return config;
