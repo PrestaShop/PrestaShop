99389f359ed7365072a7d9a4ce4090435deafec6
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2022-06-24 14:27:10 +0200

    Add unit tests for Currency follback

--- src/Core/Localization/CLDR/Currency.php
+++ src/Core/Localization/CLDR/Currency.php
@@ -140,15 +140,11 @@ final class Currency implements CurrencyInterface
      */
     public function getDisplayName($countContext = CurrencyInterface::DISPLAY_NAME_COUNT_DEFAULT)
     {
-        if (empty($this->displayNames)) {
-            return $this->isoCode;
-        }
-
         if (!in_array($countContext, [CurrencyInterface::DISPLAY_NAME_COUNT_DEFAULT, CurrencyInterface::DISPLAY_NAME_COUNT_ONE, CurrencyInterface::DISPLAY_NAME_COUNT_OTHER])) {
             throw new LocalizationException(sprintf('Unknown display name: "%s"', print_r($countContext, true)));
         }
 
-        return $this->displayNames[$countContext];
+        return $this->displayNames[$countContext] ?? $this->isoCode;
     }
 
     /**
