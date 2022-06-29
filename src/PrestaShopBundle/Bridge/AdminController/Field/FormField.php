e0ef1f893c0942298c618e01af7a626190f3d053
Julius Zukauskas <julius.zukauskas@invertus.eu>
2022-06-29 17:50:26 +0300

    implementing FormFields and helper bridge. Saving progress

---
+++ src/PrestaShopBundle/Bridge/AdminController/Field/FormField.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShopBundle\Bridge\AdminController\Field;
+
+/**
+ * @todo: unsure if we need separate interface or no. Will see later
+ */
+class FormField implements FieldInterface
+{
+    /**
+     * @var string
+     */
+    private $label;
+
+    /**
+     * @var array
+     */
+    private $config;
+
+    /**
+     * @param string $label
+     * @param array<string, mixed>> $config
+     */
+    public function __construct(string $label, array $config = [])
+    {
+        $this->label = $label;
+        $this->config = $config;
+        //@todo: add options resolver
+    }
+
+    /**
+     * @return string
+     */
+    public function getLabel(): string
+    {
+        return $this->label;
+    }
+
+    /**
+     * @return array
+     */
+    public function getConfig(): array
+    {
+        return $this->config;
+    }
+}
