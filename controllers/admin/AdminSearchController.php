e27fc5c35278c96528e118590062b6554d8b992e
Okom3pom <contact@okom3pom.com>
2022-07-09 09:01:39 +0200

    Fix module link in search

--- controllers/admin/AdminSearchController.php
+++ controllers/admin/AdminSearchController.php
@@ -247,7 +247,7 @@ class AdminSearchControllerCore extends AdminController
                 || (isset($module->displayName) && stripos($module->displayName, $this->query) !== false)
                 || (isset($module->description) && stripos($module->description, $this->query) !== false)
             ) {
-                $module->linkto = 'index.php?tab=AdminModules&tab_module=' . $module->tab . '&module_name=' . $module->name . '&anchor=' . ucfirst($module->name) . '&token=' . Tools::getAdminTokenLite('AdminModules');
+                $module->linkto = Context::getContext()->link->getAdminLink('ADMINMODULESSF') . '&find=' . $module->name;
                 $this->_list['modules'][] = $module;
             }
         }
