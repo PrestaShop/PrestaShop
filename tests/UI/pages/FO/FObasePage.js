355bbe15b440971c414531a9807b55291db53c80
Nesrine ABDMOULEH <nesrine.abdmouleh.info@gmail.com>
2022-07-11 16:01:52 +0200

    Add bread crumb link selector

--- tests/UI/pages/FO/FObasePage.js
+++ tests/UI/pages/FO/FObasePage.js
@@ -19,6 +19,7 @@ class FOBasePage extends CommonPage {
     this.content = '#content';
     this.desktopLogo = '#_desktop_logo';
     this.desktopLogoLink = `${this.desktopLogo} a`;
+    this.breadCrumbLink = link => `#wrapper nav.breadcrumb a[href*=${link}]`;
     this.cartProductsCount = '#_desktop_cart .cart-products-count';
     this.cartLink = '#_desktop_cart a';
     this.userInfoLink = '#_desktop_user_info';
@@ -125,7 +126,7 @@ class FOBasePage extends CommonPage {
    * @returns {Promise<void>}
    */
   async clickOnBreadCrumbLink(page, link) {
-    await this.waitForSelectorAndClick(page, `#wrapper nav.breadcrumb a[href*=${link}]`);
+    await this.clickAndWaitForNavigation(page, this.breadCrumbLink(link));
   }
 
   /**
