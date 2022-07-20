722c024e7cfc69cf70457e8f17c6e97add7edab8
Progi1984 <franck.lefevre@prestashop.com>
2022-07-20 09:33:55 +0200

    Bump version to 8.1.0

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -32,10 +32,10 @@ use Symfony\Component\HttpKernel\Kernel;
 
 class AppKernel extends Kernel
 {
-    const VERSION = '8.0.0';
+    const VERSION = '8.1.0';
     const MAJOR_VERSION_STRING = '8';
     const MAJOR_VERSION = 8;
-    const MINOR_VERSION = 0;
+    const MINOR_VERSION = 1;
     const RELEASE_VERSION = 0;
 
     /**
