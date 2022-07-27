ad2b5819b9228174ecc93179ce4c490553774168
Obelisk-services <info@obelisk-services.com>
2022-07-27 13:57:41 +0200

    Fixed product_customization_id value lost after combination change

--- themes/_core/js/product.js
+++ themes/_core/js/product.js
@@ -228,7 +228,6 @@ function updateProduct(event, eventType, updateUrl) {
         $(prestashop.selectors.product.customization)
           .first()
           .replaceWith(data.product_customization);
-        $(prestashop.selectors.product.inputCustomization).val(0);
         $(prestashop.selectors.product.variantsUpdate)
           .first()
           .replaceWith(data.product_variants);
