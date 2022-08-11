fdecee97b85cf7a263e21efdce8119a9402d3e8e
Julius Zukauskas <julius.zukauskas@invertus.eu>
2022-08-11 11:43:18 +0300

    remove "disabled" from prrototype row, so input is submitted

--- src/PrestaShopBundle/Form/Admin/Sell/Customer/SearchedCustomerType.php
+++ src/PrestaShopBundle/Form/Admin/Sell/Customer/SearchedCustomerType.php
@@ -45,6 +45,7 @@ class SearchedCustomerType extends CommonAbstractType
         $builder
             ->add('id_customer', HiddenType::class, [
                 'label' => false,
+                'block_prefix' => 'searched_customer_id_customer',
             ])
             ->add('fullname_and_email', TextPreviewType::class, [
                 'label' => false,
