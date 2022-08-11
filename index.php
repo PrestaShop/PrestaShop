f3c49e52c207658be31193bec3645a9aa5b3d127
Fabien Papet <fabien.papet@gmail.com>
2022-08-11 14:42:49 +0200

    Add Symfony kernel into front-end

--- index.php
+++ index.php
@@ -24,5 +24,10 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  */
 
-require dirname(__FILE__).'/config/config.inc.php';
-Dispatcher::getInstance()->dispatch();
+use Symfony\Component\HttpFoundation\Request;
+
+require dirname(__FILE__) . '/config/config.inc.php';
+
+$kernel = new AppKernel(_PS_ENV_, true);
+$kernel->boot();
+Dispatcher::getInstance(Request::createFromGlobals())->dispatch();
