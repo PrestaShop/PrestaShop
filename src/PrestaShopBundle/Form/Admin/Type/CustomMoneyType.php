a969719d9f226cdb39e1cf12a258a542d8d09f50
Julius Zukauskas <julius.zukauskas@invertus.eu>
2022-08-16 16:36:28 +0300

    POC for price formatting in customMoneyType;

--- src/PrestaShopBundle/Form/Admin/Type/CustomMoneyType.php
+++ src/PrestaShopBundle/Form/Admin/Type/CustomMoneyType.php
@@ -26,13 +26,42 @@
 
 namespace PrestaShopBundle\Form\Admin\Type;
 
+use PrestaShop\Decimal\DecimalNumber;
+use PrestaShop\PrestaShop\Core\Localization\Locale;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CustomMoneyType extends AbstractTypeExtension
 {
     public const PRESTASHOP_DECIMALS = 6;
+
+    protected const PATTERN_MAP = [
+        "¤\u{a0}#,##0.00" => "¤\u{a0}{{ widget }}",
+        '¤#,##0.00' => '¤{{ widget }}',
+        "#,##0.00\u{a0}¤" => "{{ widget }}\u{a0}¤",
+        '#,##0.00¤' => '{{ widget }}¤',
+        "-¤\u{a0}#,##0.00" => "-¤\u{a0}{{ widget }}",
+        '-¤#,##0.00' => '-¤{{ widget }}',
+        "-#,##0.00\u{a0}¤" => "-{{ widget }}\u{a0}¤",
+        '-#,##0.00¤' => '-{{ widget }}¤',
+    ];
+
+    /**
+     * @var Locale
+     */
+    private $locale;
+
+    /**
+     * @param Locale $locale
+     */
+    public function __construct(
+        Locale $locale
+    ) {
+        $this->locale = $locale;
+    }
 
     public static function getExtendedTypes(): iterable
     {
@@ -54,5 +83,32 @@ class CustomMoneyType extends AbstractTypeExtension
         ]);
 
         $resolver->setAllowedTypes('scale', 'int');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        parent::buildView($view, $form, $options);
+        $currency = $options['currency'];
+        $moneyPattern = $view->vars['money_pattern'];
+
+        $priceSpecification = $this->locale->getPriceSpecification($currency);
+        $value = new DecimalNumber((string) (float) $view->vars['value']);
+        $pricePattern = $value->isPositive() ? $priceSpecification->getPositivePattern() : $priceSpecification->getNegativePattern();
+
+        // inspired by symfony MoneyType
+        preg_match('/^([^\s\xc2\xa0]*)[\s\xc2\xa0]*{{ widget }}(?:[,.]0+)?[\s\xc2\xa0]*([^\s\xc2\xa0]*)$/u', $moneyPattern, $matches);
+
+        if (!empty($matches[1])) {
+            $symbol = trim($matches[1]);
+        }
+
+        if (!isset($this::PATTERN_MAP[$pricePattern])) {
+            return;
+        }
+
+        $view->vars['money_pattern'] = str_replace('¤', $symbol ?? $currency, $this::PATTERN_MAP[$pricePattern]);
     }
 }
