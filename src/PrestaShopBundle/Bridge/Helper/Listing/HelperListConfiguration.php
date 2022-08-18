c17a8f0751efd84eec9ab63f81ff94491781e08f
Julius Zukauskas <julius.zukauskas@invertus.eu>
2022-08-18 09:05:06 +0300

    put resolvers in Configuration class

--- src/PrestaShopBundle/Bridge/Helper/Listing/HelperListConfiguration.php
+++ src/PrestaShopBundle/Bridge/Helper/Listing/HelperListConfiguration.php
@@ -28,6 +28,8 @@ declare(strict_types=1);
 
 namespace PrestaShopBundle\Bridge\Helper\Listing;
 
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
 /**
  * This object contains the configuration needed to generate a list using the helper list.
  */
@@ -500,7 +502,16 @@ class HelperListConfiguration
      */
     public function addToolbarAction(string $label, array $config): self
     {
-        $this->toolbarActions[$label] = $config;
+        $optionsResolver = new OptionsResolver();
+        $optionsResolver
+            ->setDefined(['href', 'desc', 'class'])
+            ->setDefaults(['class' => ''])
+            ->setAllowedTypes('class', ['string'])
+            ->setAllowedTypes('href', ['string'])
+            ->setAllowedTypes('desc', ['string'])
+        ;
+
+        $this->toolbarActions[$label] = $optionsResolver->resolve($config);
 
         return $this;
     }
@@ -513,6 +524,11 @@ class HelperListConfiguration
         return $this->toolbarActions;
     }
 
+    /**
+     * @param string $action
+     *
+     * @return $this
+     */
     public function addRowAction(string $action): self
     {
         $this->rowActions[] = $action;
@@ -528,9 +544,27 @@ class HelperListConfiguration
         return $this->rowActions;
     }
 
+    /**
+     * @param string $label
+     * @param array<string, mixed> $config
+     *
+     * @return $this
+     */
     public function addBulkAction(string $label, array $config): self
     {
-        $this->bulkActions[$label] = $config;
+        $optionsResolver = new OptionsResolver();
+        $optionsResolver
+            ->setDefaults([
+                'text' => '',
+                'icon' => null,
+                'confirm' => null,
+            ])
+            ->setAllowedTypes('text', ['string'])
+            ->setAllowedTypes('icon', ['string', 'null'])
+            ->setAllowedTypes('confirm', ['string', 'null'])
+        ;
+
+        $this->bulkActions[$label] = $optionsResolver->resolve($config);
 
         return $this;
     }
