aa41c0beb05b799650fa960671bb56879c7862bc
Nesrine ABDMOULEH <nesrine.abdmouleh.info@gmail.com>
2022-08-24 15:04:30 +0200

    Complete the steps of CRUD standard product

--- tests/UI/pages/BO/catalog/productsV2/add.js
+++ tests/UI/pages/BO/catalog/productsV2/add.js
@@ -19,8 +19,8 @@ class Products extends BOBasePage {
     this.pageTitle = 'Products';
 
     // Header selectors
-    this.productName = '#product_header_name_1';
-    this.productActive = '#product_header_active_1';
+    this.productNameInput = '#product_header_name_1';
+    this.productActiveSwitchButton = '#product_header_active_1';
 
     // Selectors in description tab
     this.descriptionTabLink = '#product_description-tab-nav';
@@ -29,16 +29,19 @@ class Products extends BOBasePage {
 
     // Selectors in details tab
     this.detailsTabLink = '#product_specifications-tab-nav';
-    this.productReference = '#product_specifications_references_reference';
+    this.productReferenceInput = '#product_specifications_references_reference';
 
     // Selectors in stocks tab
     this.stocksTabLink = '#product_stock-tab-nav';
-    this.productQuantity = '#product_stock_quantities_delta_quantity_delta';
-    this.productMinimumQuantityForSale = '#product_stock_quantities_minimal_quantity';
+    this.productQuantityInput = '#product_stock_quantities_delta_quantity_delta';
+    this.productMinimumQuantityInput = '#product_stock_quantities_minimal_quantity';
 
     // Selectors in pricing tab
     this.pricingTabLink = '#product_pricing-tab-nav';
-    this.retailPrice = '#product_pricing_retail_price_price_tax_excluded';
+    this.retailPriceInput = '#product_pricing_retail_price_price_tax_excluded';
+    this.taxRuleSpan = '#select2-product_pricing_retail_price_tax_rules_group_id-container';
+    this.taxRuleList = 'ul#select2-product_pricing_retail_price_tax_rules_group_id-results';
+    this.taxRuleSelect = taxRuleID => `${this.taxRuleList} li[id*='-${taxRuleID}']`;
 
     // Footer selectors
     this.previewProductButton = '#product_footer_preview';
@@ -72,6 +75,12 @@ class Products extends BOBasePage {
     await page.keyboard.type(value);
   }
 
+  /**
+   * Set product description
+   * @param page {Page} Browser tab
+   * @param productData {ProductData} Data to set in description form
+   * @returns {Promise<void>}
+   */
   async setProductDescription(page, productData) {
     await this.waitForSelectorAndClick(page, this.descriptionTabLink);
 
@@ -79,34 +88,74 @@ class Products extends BOBasePage {
     await this.setValueOnTinymceInput(page, this.productdescription, productData.description);
   }
 
+  /**
+   * Set product details
+   * @param page {Page} Browser tab
+   * @param productData {ProductData} Data to set in details form
+   * @returns {Promise<void>}
+   */
   async setProductDetails(page, productData) {
     await this.waitForSelectorAndClick(page, this.detailsTabLink);
-    await this.setValue(page, this.productReference, productData.reference);
+    await this.setValue(page, this.productReferenceInput, productData.reference);
   }
 
+  /**
+   * Set product stock
+   * @param page {Page} Browser tab
+   * @param productData {ProductData} Data to set in stock form
+   * @returns {Promise<void>}
+   */
   async setProductStock(page, productData) {
     await this.waitForSelectorAndClick(page, this.stocksTabLink);
-    await this.setValue(page, this.productQuantity, productData.quantity);
-    await this.setValue(page, this.productMinimumQuantityForSale, productData.minimumQuantity);
+    await this.setValue(page, this.productQuantityInput, productData.quantity);
+    await this.setValue(page, this.productMinimumQuantityInput, productData.minimumQuantity);
   }
 
+  /**
+   * Set product pricing
+   * @param page {Page} Browser tab
+   * @param productData {ProductData} Data to set in pricing form
+   * @returns {Promise<void>}
+   */
   async setProductPricing(page, productData) {
     await this.waitForSelectorAndClick(page, this.pricingTabLink);
-    await this.setValue(page, this.retailPrice, productData.price);
+    await this.setValue(page, this.retailPriceInput, productData.price);
+    // Select tax rule by ID
+    await Promise.all([
+      this.waitForSelectorAndClick(page, this.taxRuleSpan),
+      this.waitForVisibleSelector(page, this.taxRuleList),
+    ]);
+    await this.waitForSelectorAndClick(page, this.taxRuleSelect(productData.taxRuleID));
   }
 
+  /**
+   * Save product
+   * @param page {Page} Browser tab
+   * @returns {Promise<string>}
+   */
   async saveProduct(page) {
     await this.clickAndWaitForNavigation(page, this.saveProductButton);
 
     return this.getAlertSuccessBlockParagraphContent(page);
   }
 
+  /**
+   * Get save button name
+   * @param page {Page} Browser tab
+   * @returns {Promise<string>}
+   */
   async getSaveButtonName(page) {
     return this.getTextContent(page, this.saveProductButton);
   }
 
+  /**
+   * Set product
+   * @param page {Page} Browser tab
+   * @param productData {ProductData} Data to set in new product page
+   * @returns {Promise<string>}
+   */
   async setProduct(page, productData) {
-    await this.setValue(page, this.productName, productData.name);
+    await this.setValue(page, this.productNameInput, productData.name);
 
     await this.setProductDescription(page, productData);
 
@@ -116,7 +165,7 @@ class Products extends BOBasePage {
 
     await this.setProductPricing(page, productData);
 
-    await this.setChecked(page, this.productActive, productData.status);
+    await this.setChecked(page, this.productActiveSwitchButton, productData.status);
 
     return this.saveProduct(page);
   }
@@ -136,6 +185,11 @@ class Products extends BOBasePage {
     return newPage;
   }
 
+  /**
+   * Delete product
+   * @param page {Page} Browser tab
+   * @returns {Promise<string>}
+   */
   async deleteProduct(page) {
     await this.waitForSelectorAndClick(page, this.deleteProductButton);
     await this.waitForVisibleSelector(page, this.deleteProductFooterModal);
