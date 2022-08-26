f62db0828b12170e7115bccb4b10b1b1d27601da
Julius Zukauskas <julius.zukauskas@invertus.eu>
2022-08-26 12:27:18 +0300

    finish up documenting HelperListconfiguration props

--- src/PrestaShopBundle/Bridge/Helper/Listing/HelperListConfiguration.php
+++ src/PrestaShopBundle/Bridge/Helper/Listing/HelperListConfiguration.php
@@ -28,6 +28,8 @@ declare(strict_types=1);
 
 namespace PrestaShopBundle\Bridge\Helper\Listing;
 
+use AdminController;
+use HelperList;
 use PrestaShopBundle\Bridge\AdminController\ControllerConfiguration;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -39,12 +41,16 @@ class HelperListConfiguration
     /**
      * Sql SELECT clause
      *
+     * @see AdminController::$_select
+     *
      * @var string
      */
     public $select = '';
 
     /**
      * Sql WHERE clause
+     *
+     * @see AdminController::$_where
      *
      * @var string
      */
@@ -53,12 +59,16 @@ class HelperListConfiguration
     /**
      * Sql JOIN clause
      *
+     * @see AdminController::$_join
+     *
      * @var string
      */
     public $join = '';
 
     /**
      * Sql GROUP clause
+     *
+     * @see AdminController::$_group
      *
      * @var string
      */
@@ -67,12 +77,16 @@ class HelperListConfiguration
     /**
      * Sql ORDER BY clause
      *
+     * @see AdminController::$_orderBy
+     *
      * @var string
      */
     public $orderBy = '';
 
     /**
      * Sql value for ORDER BY clause argument e.g. ASC|DESC
+     *
+     * @see AdminController::$_orderWay
      *
      * @var string
      */
@@ -81,12 +95,16 @@ class HelperListConfiguration
     /**
      * Sql HAVING clause
      *
+     * @see AdminController::$_having
+     *
      * @var string
      */
     public $having = '';
 
     /**
      * Sql HAVING clause for filters
+     *
+     * @see AdminController::$_filterHaving
      *
      * @var string
      */
@@ -95,12 +113,16 @@ class HelperListConfiguration
     /**
      * Contains various sql clauses for filtering the list
      *
+     * @see AdminController::$filter
+     *
      * @var string
      */
     public $filter = '';
 
     /**
      * The definition of list fields
+     *
+     * @see HelperList::$fields_list
      *
      * @var array<string, array<string, mixed>>
      *
@@ -111,12 +133,16 @@ class HelperListConfiguration
     /**
      * List record rows
      *
+     * @see HelperList::$_list
+     *
      * @var array<int, array<string, mixed>>|null
      */
     public $list;
 
     /**
      * Total count of records found. Used for pagination.
+     *
+     * @see HelperList::$listTotal
      *
      * @var int
      */
@@ -131,6 +157,7 @@ class HelperListConfiguration
 
     /**
      * @see ControllerConfiguration::$tableName
+     * @see HelperList::$table
      *
      * @var string
      */
@@ -140,6 +167,8 @@ class HelperListConfiguration
      * Identifier of the list.
      * Based on this value the submit request key is built to allow identifying which list is performing certain action.
      * Most of the time it matches table name, but custom value can be provided as well.
+     *
+     * @see HelperList::$list_id
      *
      * @var string
      */
@@ -154,126 +183,222 @@ class HelperListConfiguration
 
     /**
      * @see ControllerConfiguration::$legacyControllerName
+     * @see HelperList::$controller_name
      *
      * @var string
      */
     private $legacyControllerName;
 
     /**
+     * Name of id field (e.g. id_feature)
+     *
+     * @see HelperList::$identifier
+     *
      * @var string
      */
-    private $identifier;
+    private $identifierKey;
 
     /**
+     * @see ControllerConfiguration::$token
+     *
      * @var string
      */
     private $token;
 
     /**
+     * Default value of sql ORDER BY clause
+     *
+     * @see HelperList::$_defaultOrderBy
+     *
      * @var string
      */
     private $defaultOrderBy;
 
     /**
+     * Default value for sql ORDER BY clause argument e.g. ASC|DESC
+     *
      * @var string
      */
     private $defaultOrderWay = 'ASC';
 
     /**
+     * Defines if language table should be joined when querying sql or not
+     *
+     * @see AdminController::$lang
+     *
      * @var bool
      */
     private $autoJoinLanguageTable;
 
     /**
+     * @see AdminController::$multishop_context
+     *
      * @var int
      */
     private $multiShopContext;
 
     /**
+     *  If set to true, then includes records flagged with "deleted=0" when fetching list query
+     * (yes, for some reason it seems to work backwards than the variable naming suggests)
+     *
+     * @see HelperList::$deleted
+     * @see AdminController::getWhereClause for original usage
+     *
      * @var bool
      */
     private $deleted;
 
     /**
+     * If set to true, then sql all columns select ("a.*") is not used.
+     *
+     * @see AdminController::$explicitSelect
+     * @see AdminController::getList() for original usage
+     *
      * @var bool
      */
     private $explicitSelect;
 
     /**
      * @var bool Use SQL_CALC_FOUND_ROWS / FOUND_ROWS to count the number of records
+     *
+     * @see AdminController::$_use_found_rows
      */
     private $useFoundRows;
 
     /**
+     * @see ControllerConfiguration::$positionIdentifierKey
+     * @see HelperList::$position_identifier
+     *
      * @var string
      */
-    private $positionIdentifier;
+    private $positionIdentifierKey;
 
     /**
+     * @see ControllerConfiguration::$bootstrap
+     * @see HelperList::$bootstrap
+     *
      * @var bool
      */
     private $bootstrap;
 
     /**
+     * @see ControllerConfiguration::$legacyCurrentIndex
+     * @see HelperList::$currentIndex
+     *
      * @var string|null
      */
     private $legacyCurrentIndex;
 
     /**
+     * Template variables passed to delete action button.
+     *
+     * @see HelperList::$tpl_delete_link_vars
+     *
      * @var array<string, mixed>
      */
     private $deleteLinkVars = [];
 
     /**
+     * @see AdminController::$shopLinkType
+     *
      * @var string
      */
     private $shopLinkType = '';
 
     /**
+     * Pagination limit options for list.
+     *
+     * @see AdminController::$_pagination
+     * @see HelperList::$_pagination
+     *
      * @var int[]
      */
     private $paginationLimits = [20, 50, 100, 300, 1000];
 
     /**
+     * Default pagination limit option for list.
+     *
+     * @see AdminController::$_default_pagination
+     * @see HelperList::$_default_pagination
+     *
      * @var int
      */
     private $defaultPaginationLimit = 50;
 
     /**
+     * Actions shown in list toolbar.
+     *
+     * @see HelperList::$toolbar_btn
+     * @see self::addToolbarAction() for array structure definition.
+     *
      * @var array<string, array<string, mixed>>
      */
     private $toolbarActions = [];
 
     /**
+     * Actions for every list row.
+     *
+     * @see HelperList::$actions
+     *
      * @var string[]
      */
     private $rowActions = [];
 
     /**
+     * Bulk actions for list.
+     *
+     * @see HelperList::$bulk_actions
+     * @see self::addBulkAction() for array structure definition.
+     *
      * @var array<string, array<string, mixed>>
      */
     private $bulkActions = [];
 
     /**
+     * Defines where list filters and other POST actions should be submitted.
+     * Without this property, HelperList would always submit back to legacy controller.
+     *
+     * @see HelperList::$postSubmitUrl
+     *
      * @var string|null
      */
     private $postSubmitUrl;
 
     /**
+     * If true - joins related shop tables when querying sql.
+     *
+     * @see AdminController::$shopShareDatas
+     *
      * @var bool
      */
     private $shopShareData = false;
 
     /**
      * @param int $tabId
+     * @param string $tableName
+     * @param string $listId
+     * @param string $objectModelClassName
+     * @param string $identifierKey
+     * @param string $positionIdentifierKey
+     * @param bool $isJoinLanguageTableAuto
+     * @param bool $deleted
+     * @param string $defaultOrderBy
+     * @param bool $explicitSelect
+     * @param bool $useFoundRows
+     * @param string $legacyControllerName
+     * @param string $token
+     * @param bool $bootstrap
+     * @param string $legacyCurrentIndex
+     * @param int $multiShopContext
+     * @param string $postSubmitUrl
      */
     public function __construct(
         int $tabId,
         string $tableName,
         string $listId,
         string $objectModelClassName,
-        string $identifier,
-        string $positionIdentifier,
+        string $identifierKey,
+        string $positionIdentifierKey,
         bool $isJoinLanguageTableAuto,
         bool $deleted,
         string $defaultOrderBy,
@@ -290,8 +415,8 @@ class HelperListConfiguration
         $this->tableName = $tableName;
         $this->listId = $listId;
         $this->objectModelClassName = $objectModelClassName;
-        $this->identifier = $identifier;
-        $this->positionIdentifier = $positionIdentifier;
+        $this->identifierKey = $identifierKey;
+        $this->positionIdentifierKey = $positionIdentifierKey;
         $this->autoJoinLanguageTable = $isJoinLanguageTableAuto;
         $this->deleted = $deleted;
         $this->defaultOrderBy = $defaultOrderBy;
@@ -348,9 +473,9 @@ class HelperListConfiguration
     /**
      * @return string
      */
-    public function getIdentifier(): string
+    public function getIdentifierKey(): string
     {
-        return $this->identifier;
+        return $this->identifierKey;
     }
 
     /**
@@ -412,9 +537,9 @@ class HelperListConfiguration
     /**
      * @return string
      */
-    public function getPositionIdentifier(): string
+    public function getPositionIdentifierKey(): string
     {
-        return $this->positionIdentifier;
+        return $this->positionIdentifierKey;
     }
 
     /**
@@ -537,7 +662,7 @@ class HelperListConfiguration
 
     /**
      * @param string $label
-     * @param array $config
+     * @param array{href?:string, desc?:string, class:string} $config
      *
      * @return $this
      */
@@ -587,7 +712,7 @@ class HelperListConfiguration
 
     /**
      * @param string $label
-     * @param array<string, mixed> $config
+     * @param array{text: string, icon?: string, confirm?: string} $config
      *
      * @return $this
      */
@@ -595,11 +720,6 @@ class HelperListConfiguration
     {
         $optionsResolver = new OptionsResolver();
         $optionsResolver
-            ->setDefaults([
-                'text' => '',
-                'icon' => null,
-                'confirm' => null,
-            ])
             ->setAllowedTypes('text', ['string'])
             ->setAllowedTypes('icon', ['string', 'null'])
             ->setAllowedTypes('confirm', ['string', 'null'])
@@ -644,6 +764,7 @@ class HelperListConfiguration
                 'icon',
                 'list',
             ])
+            ->addAllowedTypes('title', 'string')
             ->addAllowedTypes('orderby', 'boolean')
             ->addAllowedTypes('search', 'boolean')
             ->addAllowedTypes('havingFilter', 'boolean')
