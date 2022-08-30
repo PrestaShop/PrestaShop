0dfa1d345eed0f2ca89c5e6efcdf741846e4c630
leemyongpakvn <leemyongpakvn@gmail.com>
2022-08-30 07:34:28 +0700

    seperate renderedProducts and productSelectors
    so we are safe in case productSelectors is Undefined after the previous filter with No product

--- themes/classic/_dev/js/listing.js
+++ themes/classic/_dev/js/listing.js
@@ -229,11 +229,13 @@ function updateProductListDOM(data) {
   );
 
   const renderedProducts = $(data.rendered_products);
-  const productSelectors = $(prestashop.themeSelectors.listing.product, renderedProducts);
+  const productSelectors = $(prestashop.themeSelectors.listing.product);
 
   if (productSelectors.length > 0) {
-    productSelectors.removeClass().addClass($(prestashop.themeSelectors.listing.product).first().attr('class'));
-  }
+    productSelectors.removeClass().addClass(productSelectors.first().attr('class'));
+  } else {
+    productSelectors.removeClass().addClass(renderedProducts.first().attr('class'));
+  }  
 
   $(prestashop.themeSelectors.listing.list).replaceWith(renderedProducts);
 
