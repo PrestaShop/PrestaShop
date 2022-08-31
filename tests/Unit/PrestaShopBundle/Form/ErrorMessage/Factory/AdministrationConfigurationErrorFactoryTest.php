393b7c894f8a349522b80f5a25dcaaee49c472c6
Jevgenij Visockij <jevgenij.visockij@invertus.eu>
2022-08-31 17:45:21 +0300

    Adding tests

---
+++ tests/Unit/PrestaShopBundle/Form/ErrorMessage/Factory/AdministrationConfigurationErrorFactoryTest.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Unit\PrestaShopBundle\Form\ErrorMessage\Factory;
+
+use PHPUnit\Framework\TestCase;
+use PrestaShop\PrestaShop\Core\Form\ErrorMessage\AdministrationConfigurationError;
+use PrestaShopBundle\Entity\Lang;
+use PrestaShopBundle\Entity\Repository\LangRepository;
+use PrestaShopBundle\Form\Admin\Configure\AdvancedParameters\Administration\GeneralDataProvider;
+use PrestaShopBundle\Form\ErrorMessage\Factory\AdministrationConfigurationErrorFactory;
+use Symfony\Component\Translation\Translator;
+
+class AdministrationConfigurationErrorFactoryTest extends TestCase
+{
+    private const COOKIE_FIELD_NAME = 'Max cookie value';
+
+    public function testGetErrorMessageForConfigurationError(): void
+    {
+        $translatorMock = $this->getMockBuilder(Translator::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['trans'])
+            ->getMock();
+
+        $expectedTranslation = 'Expected translation';
+        $translatorMock->method('trans')->willReturnMap(
+            [
+                [
+                    '%s is invalid. Please enter an integer lower than %s.',
+                    [
+                        self::COOKIE_FIELD_NAME,
+                        GeneralDataProvider::MAX_COOKIE_VALUE,
+                    ],
+                    'Admin.Notifications.Error',
+                    null,
+                    $expectedTranslation,
+                ],
+            ]
+        );
+
+        $languageRepositoryMock = $this->getMockBuilder(LangRepository::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['findOneBy'])
+            ->getMock();
+
+        $language = new Lang();
+        $language->setName('English');
+        $languageRepositoryMock->method('findOneBy')->willReturn($language);
+        $administrationConfigurationErrorFactory = new AdministrationConfigurationErrorFactory($translatorMock, $languageRepositoryMock);
+
+        $error = new AdministrationConfigurationError(AdministrationConfigurationError::ERROR_COOKIE_LIFETIME_MAX_VALUE_EXCEEDED, 'field', 1);
+        $result = $administrationConfigurationErrorFactory->getErrorMessageForConfigurationError($error, self::COOKIE_FIELD_NAME);
+        self::assertEquals(
+            $expectedTranslation,
+            $result
+        );
+
+        $error = new AdministrationConfigurationError(35, 'field');
+        $result = $administrationConfigurationErrorFactory->getErrorMessageForConfigurationError($error, 'field');
+
+        self::assertNull($result);
+    }
+}
---
+++ tests/Unit/PrestaShopBundle/Form/ErrorMessage/Factory/CommonConfigurationErrorFactoryTest.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Unit\PrestaShopBundle\Form\ErrorMessage\Factory;
+
+use PHPUnit\Framework\TestCase;
+use PrestaShop\PrestaShop\Core\Form\ErrorMessage\CommonConfigurationError;
+use PrestaShop\PrestaShop\Core\Form\ErrorMessage\ConfigurationErrorInterface;
+use PrestaShop\PrestaShop\Core\Form\ErrorMessage\InvoiceConfigurationError;
+use PrestaShopBundle\Entity\Lang;
+use PrestaShopBundle\Entity\Repository\LangRepository;
+use PrestaShopBundle\Form\ErrorMessage\Factory\CommonConfigurationErrorFactory;
+use Symfony\Component\Translation\Translator;
+
+class CommonConfigurationErrorFactoryTest extends TestCase
+{
+    public function testGetErrorMessageForConfigurationError(): void
+    {
+        $translatorMock = $this->getMockBuilder(Translator::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['trans'])
+            ->getMock();
+
+        $translatorMock->method('trans')->willReturnMap(
+            [
+                [
+                    '%s is invalid. Please enter an integer greater than or equal to 0.',
+                    [
+                        'field',
+                    ],
+                    'Admin.Notifications.Error',
+                    null,
+                    'Field is invalid. Please enter an integer greater than or equal to 0.',
+                ],
+            ]
+        );
+
+        $languageRepositoryMock = $this->getMockBuilder(LangRepository::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['findOneBy'])
+            ->getMock();
+
+        $language = new Lang();
+        $language->setName('English');
+        $languageRepositoryMock->method('findOneBy')->willReturn($language);
+        $commonConfigurationErrorFactory = new CommonConfigurationErrorFactory($translatorMock, $languageRepositoryMock);
+
+        $error = new CommonConfigurationError(CommonConfigurationError::ERROR_NOT_NUMERIC_OR_LOWER_THAN_ZERO, 'field', 1);
+        $result = $commonConfigurationErrorFactory->getErrorMessageForConfigurationError($error, 'field');
+        self::assertEquals(
+            'Field is invalid. Please enter an integer greater than or equal to 0.',
+            $result
+        );
+
+        $error = new CommonConfigurationError(35, 'field');
+        $result = $commonConfigurationErrorFactory->getErrorMessageForConfigurationError($error, 'field');
+
+        self::assertNull($result);
+    }
+}
