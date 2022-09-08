aa50f115b4f480ff622ae821d21352b8a2bed2bc
margud <marius.gudauskis@invertus.eu>
2022-09-08 13:20:49 +0300

    removed not needed line

--- admin-dev/themes/new-theme/js/components/form/change-password-control.ts
+++ admin-dev/themes/new-theme/js/components/form/change-password-control.ts
@@ -249,7 +249,6 @@ export default class ChangePasswordControl {
     this.$submittableInputs.removeAttr('required');
     this.$inputsBlock.find('input').val('');
     this.$inputsBlock.find('.form-text').text('');
-    $('#employee_change_password_new_password_first').click();
   }
 
   /**
