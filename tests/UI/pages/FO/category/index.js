05adeef0ff3fda393428c68d24390cf5d8bc993d
cfarhani06 <chedly.farhani@prestashop.com>
2022-09-14 18:09:14 +0200

    add new tests to display all products

--- tests/UI/pages/FO/category/index.js
+++ tests/UI/pages/FO/category/index.js
@@ -17,9 +17,12 @@ class Category extends FOBasePage {
     // Selectors
     this.bodySelector = '#category';
     this.mainSection = '#main';
+    this.headerNamePage = '#js-product-list-header';
     this.productsSection = '#products';
+    this.productListTop = '#js-product-list-top';
     this.productListDiv = '#js-product-list';
     this.productItemListDiv = `${this.productListDiv} .products div.product`;
+    this.paginationText = `${this.productListDiv} .pagination div:nth-child(1)`;
     this.sortByDiv = `${this.productsSection} div.sort-by-row`;
     this.sortByButton = `${this.sortByDiv} button.select-title`;
   }
@@ -44,12 +47,48 @@ class Category extends FOBasePage {
   }
 
   /**
+  * Get number of all products
+  * @param page {Page}
+  * @returns {Promise<number>}
+  */
+  async getNumberOfProducts(page) {
+    return this.getNumberFromText(page, this.productListTop);
+  }
+
+  /**
+  * Get the header name of the page
+  * @param page {Page}
+  * @returns {Promise<string>}
+  */
+  async getHeaderPageName(page) {
+    return page.locator(this.headerNamePage).innerText().valueOf();
+  }
+
+  /**
    * Get sort by value from button
    * @param page {Page} Browser tab
    * @return {Promise<string>}
    */
   getSortByValue(page) {
     return this.getTextContent(page, this.sortByButton);
+  }
+
+  /**
+   * Is Sort By Button Visible
+   * @param page {Page} Browser tab
+   * @return {Promise<boolean>}
+   */
+  isSortButtonVisible(page) {
+    return this.elementVisible(page, this.sortByButton, 1000);
+  }
+
+  /**
+   * Get showing Items
+   * @param page {Page} Browser tab
+   * @return {Promise<string>}
+   */
+  getShowingItems(page) {
+    return this.getTextContent(page, this.paginationText, 1000);
   }
 }
 
