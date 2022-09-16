00933abf62a619483582faf98d41edcabd6b6369
Progi1984 <franck.lefevre@prestashop.com>
2022-09-16 17:14:01 +0200

    Refactored a small part of code in `classes/front/FrontController.php`

--- classes/controller/FrontController.php
+++ classes/controller/FrontController.php
@@ -1485,11 +1485,7 @@ class FrontControllerCore extends Controller
             }
 
             foreach ($assign_array as $assign_key => $assign_value) {
-                if ($this->ssl) {
-                    $urls[$assign_key] = $http . Tools::getMediaServer($assign_value) . $assign_value;
-                } else {
-                    $urls[$assign_key] = $assign_value;
-                }
+                $urls[$assign_key] = $http . Tools::getMediaServer($assign_value) . $assign_value;
             }
 
             $pages = [];
