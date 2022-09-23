ad6f9117c08af856fbfae5d80cfd5a9735c98e5c
margud <marius.gudauskis@invertus.eu>
2022-09-23 16:27:43 +0300

    improved code, fixed employer filter

--- src/Adapter/Product/Repository/ProductRepository.php
+++ src/Adapter/Product/Repository/ProductRepository.php
@@ -242,10 +242,10 @@ class ProductRepository extends AbstractObjectModelRepository
      * @param LanguageId $languageId
      *
      * @return array<array<string, string>>
-     *                                      e.g [
-     *                                      ['id_product' => '1', 'name' => 'Product name', 'reference' => 'demo15'],
-     *                                      ['id_product' => '2', 'name' => 'Product name2', 'reference' => 'demo16'],
-     *                                      ]
+     *                             e.g [
+     *                             ['id_product' => '1', 'name' => 'Product name', 'reference' => 'demo15'],
+     *                             ['id_product' => '2', 'name' => 'Product name2', 'reference' => 'demo16'],
+     *                             ]
      *
      * @throws CoreException
      */
