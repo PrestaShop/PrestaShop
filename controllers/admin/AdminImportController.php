a5b50ddc39d6b78441fd956a7327239a00bba811
Krystian Podemski <podemski.krystian@gmail.com>
2022-09-24 13:06:20 +0200

    Remove short_description field from the Suppliers importer

--- controllers/admin/AdminImportController.php
+++ controllers/admin/AdminImportController.php
@@ -480,6 +480,10 @@ class AdminImportControllerCore extends AdminController
                     ],
                 ];
 
+                if ($this->entities[$this->trans('Suppliers', [], 'Admin.Global')]) {
+                    unset($this->available_fields['short_description'], self::$validators['short_description']);
+                }
+
                 self::$default_values = [
                     'shop' => Shop::getGroupFromShop((int) Configuration::get('PS_SHOP_DEFAULT')),
                 ];
