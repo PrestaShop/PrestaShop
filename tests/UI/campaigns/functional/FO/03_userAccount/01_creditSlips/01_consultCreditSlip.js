09ab5238e88297c1c92d4b81b4e1d3b9c7f1e6a3
Progi1984 <franck.lefevre@prestashop.com>
2022-09-27 09:30:58 +0200

    Functional Tests : FO - Consult credit slip list & View PDF Credit slip & View order (Next)

--- tests/UI/campaigns/functional/FO/03_userAccount/01_creditSlips/01_consultCreditSlip.js
+++ tests/UI/campaigns/functional/FO/03_userAccount/01_creditSlips/01_consultCreditSlip.js
@@ -17,6 +17,8 @@ const loginCommon = require('@commonTests/BO/loginBO');
 const dashboardPage = require('@pages/BO/dashboard');
 const ordersPage = require('@pages/BO/orders');
 const viewOrderPage = require('@pages/BO/orders/view/viewOrderBasePage');
+const viewOrderProductsBlockPage = require('@pages/BO/orders/view/productsBlock');
+const viewOrderTabListBlock = require('@pages/BO/orders/view/tabListBlock');
 
 // Import FO pages
 const homePage = require('@pages/FO/home');
@@ -36,6 +38,9 @@ const baseContext = 'functional_FO_userAccount_creditSlips_consultCreditSlip';
 
 let browserContext;
 let page;
+let orderReference;
+let creditSlipID;
+let dateIssued;
 
 const customerData = new CustomerFaker();
 const addressData = new AddressFaker({
@@ -121,6 +126,10 @@ describe('FO - Consult credit slip list & View PDF Credit slip & View order', as
 
         const pageTitle = await creditSlipsPage.getPageTitle(page);
         await expect(pageTitle).to.equal(creditSlipsPage.pageTitle);
+      });
+
+      it('should check there no credit slips', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'checkNoCreditSlips', baseContext);
 
         const alertInfoMessage = await creditSlipsPage.getAlertInfoMessage(page);
         await expect(alertInfoMessage).to.equal(creditSlipsPage.noCreditSlipsInfoMessage);
@@ -171,16 +180,100 @@ describe('FO - Consult credit slip list & View PDF Credit slip & View order', as
         await expect(result).to.equal(Statuses.paymentAccepted.status);
       });
 
-      it('should check if the button \'Return products\' is visible', async function () {
-        await testContext.addContextItem(this, 'testIdentifier', 'checkReturnProductsButton', baseContext);
+      it('should check if the button \'Partial Refund\' is visible', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'checkPartialRefundButton', baseContext);
 
-        const result = await viewOrderPage.isReturnProductsButtonVisible(page);
+        const result = await viewOrderPage.isPartialRefundButtonVisible(page);
         await expect(result).to.be.true;
+      });
+
+      it('should create \'Partial refund\'', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'createPartialRefund', baseContext);
+
+        await viewOrderPage.clickOnPartialRefund(page);
+
+        const textMessage = await viewOrderProductsBlockPage.addPartialRefundProduct(page, 1, 1);
+        await expect(textMessage).to.contains(viewOrderProductsBlockPage.partialRefundValidationMessage);
+      });
+
+      it('should check if \'Credit slip\' document is created', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'checkCreditSlipDocument', baseContext);
+
+        // Get document name
+        const documentType = await viewOrderTabListBlock.getDocumentType(page, 3);
+        await expect(documentType).to.be.equal('Credit slip');
+      });
+
+      it('should get the order reference', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'getOrderReference', baseContext);
+
+        // Get document name
+        orderReference = await viewOrderPage.getOrderReference(page);
+        await expect(orderReference).is.not.equal('');
+      });
+
+      it('should get the identifier and the date issued of the credit slip', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'getIdentifierDateIssued', baseContext);
+
+        // Get Credit Slip ID
+        creditSlipID = await viewOrderTabListBlock.getFileName(page, 3);
+        await expect(creditSlipID).is.not.equal('');
+
+        // Get Date Issued
+        dateIssued = await viewOrderTabListBlock.getDocumentDate(page, 3);
+        await expect(dateIssued).is.not.equal('');
       });
     });
 
     describe('Check there are credit slips in FO', async () => {
+      it('should view my shop', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'viewMyShop_1', baseContext);
 
+        // View my shop and init pages
+        page = await viewOrderPage.viewMyShop(page);
+
+        await homePage.changeLanguage(page, 'en');
+        const isHomePage = await homePage.isHomePage(page);
+        await expect(isHomePage, 'Fail to open FO home page').to.be.true;
+      });
+
+      it('should go to my account page', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'goToMyAccountPage', baseContext);
+
+        await homePage.goToMyAccountPage(page);
+
+        const pageTitle = await myAccountPage.getPageTitle(page);
+        await expect(pageTitle).to.equal(myAccountPage.pageTitle);
+      });
+
+      it('should go credit slips page', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'goToCreditSlipsPage', baseContext);
+
+        await myAccountPage.goToCreditSlipsPage(page);
+
+        const pageTitle = await creditSlipsPage.getPageTitle(page);
+        await expect(pageTitle).to.equal(creditSlipsPage.pageTitle);
+      });
+
+      it('should check the number of credit slips', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'checkNumberCreditSlips', baseContext);
+
+        const numberCreditSlips = await creditSlipsPage.getNumberOfCreditSlips(page);
+        await expect(numberCreditSlips).to.equal(1);
+      });
+
+      it('should check that the \'Order reference, Credit Slip ID, Date Issued\' are correct', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'checkCreditSlipInfo', baseContext);
+
+        const creditSlipOrderReference = await creditSlipsPage.getOrderReference(page, 1);
+        await expect(creditSlipOrderReference).to.equal(orderReference);
+
+        const creditSlipOrderIdentifier = await creditSlipsPage.getCreditSlipID(page, 1);
+        await expect(parseInt(creditSlipOrderIdentifier.replace('#', ''), 10)).to.equal(parseInt(creditSlipID, 10));
+
+        const creditSlipDateIssued = await creditSlipsPage.getDateIssued(page, 1);
+        await expect(creditSlipDateIssued).to.equal(dateIssued);
+      });
     });
   });
 
