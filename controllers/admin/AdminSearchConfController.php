af0645c99f812f9e9f7e27474f1dae8e8e66547c
Hlavtox <daniel.hlavacek@hotmail.cz>
2022-09-27 08:07:57 +0200

    Remove french helptext

--- controllers/admin/AdminSearchConfController.php
+++ controllers/admin/AdminSearchConfController.php
@@ -125,8 +125,6 @@ class AdminSearchConfControllerCore extends AdminController
             'search' => [
                 'title' => $this->trans('Search', [], 'Admin.Shopparameters.Feature'),
                 'icon' => 'icon-search',
-                'info' => '<p><a href="https://github.com/PrestaShop/PrestaShop/issues/new?template=bug_report.md" target="_blank" class="btn-link"><i class="icon-external-link-sign"></i> Signaler un problème sur GitHub</a><br>'
-                    . '<a href="https://github.com/PrestaShop/PrestaShop/issues/new?template=feature_request.md" target="_blank"><i class="icon-external-link-sign"></i> Proposer une idée d\'amélioration sur GitHub</a>',
                 'fields' => [
                     'PS_SEARCH_START' => [
                         'title' => $this->trans('Search within word', [], 'Admin.Shopparameters.Feature'),
