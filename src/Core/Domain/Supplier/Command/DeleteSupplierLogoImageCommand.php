1aa6fad1047b5fd3041ecfab8fe2e3c855f40604
Progi1984 <franck.lefevre@prestashop.com>
2022-09-28 20:57:51 +0200

    Add a button to delete logo image on Supplier form

--- src/Core/Domain/Supplier/Command/DeleteSupplierLogoImageCommand.php
+++ src/Core/Domain/Supplier/Command/DeleteSupplierLogoImageCommand.php
@@ -1,4 +1,5 @@
-{#**
+<?php
+/**
  * Copyright since 2007 PrestaShop SA and Contributors
  * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
  *
@@ -21,19 +22,40 @@
  * @author    PrestaShop SA and Contributors <contact@prestashop.com>
  * @copyright Since 2007 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
- *#}
-<figure class="figure">
-  <img src="{{ logoImage.path }}" class="figure-img img-fluid img-thumbnail">
-  <figcaption class="figure-caption">
-    <p>{{ 'File size'|trans({}, 'Admin.Advparameters.Feature') }} {{ logoImage.size }}</p>
-    <button class="btn btn-outline-danger btn-sm js-form-submit-btn"
-            data-form-submit-url="{{ path('admin_manufacturers_delete_logo_image', {'manufacturerId': app.request.get('manufacturerId')}) }}"
-            data-form-csrf-token="{{ csrf_token('delete-logo-thumbnail') }}"
-    >
-      <i class="material-icons">
-        delete_forever
-      </i>
-      {{ 'Delete'|trans({}, 'Admin.Actions') }}
-    </button>
-  </figcaption>
-</figure>
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\PrestaShop\Core\Domain\Supplier\Command;
+
+use PrestaShop\PrestaShop\Core\Domain\Supplier\Exception\SupplierException;
+use PrestaShop\PrestaShop\Core\Domain\Supplier\ValueObject\SupplierId;
+
+/**
+ * Deletes supplier logo image
+ */
+class DeleteSupplierLogoImageCommand
+{
+    /**
+     * @var SupplierId
+     */
+    private $supplierId;
+
+    /**
+     * @param int $supplierId
+     *
+     * @throws SupplierException
+     */
+    public function __construct(int $supplierId)
+    {
+        $this->supplierId = new SupplierId($supplierId);
+    }
+
+    /**
+     * @return SupplierId
+     */
+    public function getSupplierId(): SupplierId
+    {
+        return $this->supplierId;
+    }
+}
