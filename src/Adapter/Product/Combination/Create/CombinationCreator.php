d71d4db5b6315c6f47672a11dc8e3b5db8683433
Julius Zukauskas <julius.zukauskas@invertus.eu>
2022-10-05 15:12:02 +0300

    fix default combination update in list

--- src/Adapter/Product/Combination/Create/CombinationCreator.php
+++ src/Adapter/Product/Combination/Create/CombinationCreator.php
@@ -32,7 +32,6 @@ use PrestaShop\PrestaShop\Adapter\Product\Combination\Repository\CombinationMult
 use PrestaShop\PrestaShop\Adapter\Product\Combination\Update\DefaultCombinationUpdater;
 use PrestaShop\PrestaShop\Adapter\Product\Repository\ProductMultiShopRepository;
 use PrestaShop\PrestaShop\Adapter\Product\Stock\Repository\StockAvailableMultiShopRepository;
-use PrestaShop\PrestaShop\Adapter\Product\Stock\Repository\StockAvailableRepository;
 use PrestaShop\PrestaShop\Core\Domain\Product\Combination\ValueObject\CombinationId;
 use PrestaShop\PrestaShop\Core\Domain\Product\Combination\ValueObject\GroupedAttributeIds;
 use PrestaShop\PrestaShop\Core\Domain\Product\Exception\InvalidProductTypeException;
