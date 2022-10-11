842b845b0cc8ad52fb2726599a2b249dde6ac295
Jean-François Viguier <jf.viguier@gmail.com>
2022-10-11 14:34:33 +0200

    cs fixer

--- classes/Customer.php
+++ classes/Customer.php
@@ -1351,7 +1351,7 @@ class CustomerCore extends ObjectModel
      * @param bool $htmlentities
      *
      * @return array
-     * 
+     *
      * @deprecated 8.1.0 The password check has been moved in controllers and this method is not called anywhere since 1.7.0
      */
     public function validateController($htmlentities = true)
