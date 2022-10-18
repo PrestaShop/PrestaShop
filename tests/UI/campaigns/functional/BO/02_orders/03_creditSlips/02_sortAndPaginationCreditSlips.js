8e7c5bf1d742324c41e8eaf3ef4c9b67d9c35746
cfarhani06 <chedly.farhani@prestashop.com>
2022-10-18 17:43:31 +0200

    creditSlips scenario : wip 181022

--- tests/UI/campaigns/functional/BO/02_orders/03_creditSlips/02_sortAndPaginationCreditSlips.js
+++ tests/UI/campaigns/functional/BO/02_orders/03_creditSlips/02_sortAndPaginationCreditSlips.js
@@ -19,8 +19,10 @@ const foAddAddressesPage = require('@pages/FO/myAccount/addAddress');
 // Import BO pages
 const dashboardPage = require('@pages/BO/dashboard');
 const ordersPage = require('@pages/BO/orders/index');
-const orderPageTabListBlock = require('@pages/BO/orders/view/tabListBlock');
-const orderPageProductsBlock = require('@pages/BO/orders/view/productsBlock');
+const orderPageTabListBlock = require('@pages/BO/orders/view/viewOrderBasePage');
+// const orderPageProductsBlock = require('@pages/BO/orders/view/productsBlock');
+const orderPageProductsBlock = require('@pages/BO/orders/view/tabListBlock');
+// /var/www/html/PrestaShop/tests/UI/pages/BO/orders/view/tabListBlock.js
 const creditSlipsPage = require('@pages/BO/orders/creditSlips/index');
 
 // Import data
@@ -39,7 +41,7 @@ const {expect} = require('chai');
 let browserContext;
 let page;
 
-let numberOfCreditSlips = 7;
+let numberOfCreditSlips = 1;
 const todayDate = getDateFormat('yyyy-mm-dd');
 const todayDateToCheck = getDateFormat('mm/dd/yyyy');
 const orderByCustomerData = {
@@ -52,6 +54,8 @@ const orderByCustomerData = {
 /*
 Pre-condition n°1:
 - Create 7 orders
+Pre-condition n°3:
+- Change the status of 11 orders and create partial refund
 Scenario:
 - Create 2 credit slips for the same order
 - Filter Credit slips table( by ID, Order ID, Date issued From and To)
@@ -60,9 +64,67 @@ Scenario:
 describe('BO - Orders - Credit Slips - Sort & Pagination Credit Slips', async () => {
   // Pre-condition: Create 7 order in FO
   for (let i = 0; i < numberOfCreditSlips; i++) {
-
     createOrderByCustomerTest(orderByCustomerData, baseContext);
-    console(i)
   }
+
+  // before and after functions
+  before(async function () {
+    browserContext = await helper.createBrowserContext(this.browser);
+    page = await helper.newTab(browserContext);
+  });
+
+  after(async () => {
+    await helper.closeBrowserContext(browserContext);
+  });
+
+  describe('Create 2 credit slips for the same order', async () => {
+    it('should login in BO', async function () {
+      await loginCommon.loginBO(this, page);
+    });
+
+    it('should go to \'Orders > Orders\' page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToOrdersPage', baseContext);
+
+      await dashboardPage.goToSubMenu(
+        page,
+        dashboardPage.ordersParentLink,
+        dashboardPage.ordersLink,
+      );
+
+      const pageTitle = await ordersPage.getPageTitle(page);
+      await expect(pageTitle).to.contains(ordersPage.pageTitle);
+    });
+
+    it('should go to the first order page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToCreatedOrderPage', baseContext);
+
+      await ordersPage.goToOrder(page, 1);
+
+      const pageTitle = await orderPageTabListBlock.getPageTitle(page);
+      await expect(pageTitle).to.contains(orderPageTabListBlock.pageTitle);
+    });
+
+    it(`should change the order status to '${Statuses.shipped.status}' and check it`, async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'updateCreatedOrderStatus', baseContext);
+      
+      // const result = await orderPageTabListBlock.modifyOrderStatus(page, test.args.status);
+      const result = await orderPageTabListBlock.modifyOrderStatus(page, Statuses.shipped.status);
+      await expect(result).to.equal(Statuses.shipped.status);
+    });
+
+    // it(`should create the partial refund n°`, async function () {
+    //   await testContext.addContextItem(this, 'testIdentifier', `addPartialRefund`, baseContext);
+
+    //   await orderPageTabListBlock.clickOnPartialRefund(page);
+
+    //   const textMessage = await orderPageProductsBlock.addPartialRefundProduct(
+    //     page,
+    //     test.args.productID,
+    //     test.args.quantity,
+    //   );
+    //   await expect(textMessage).to.contains(orderPageProductsBlock.partialRefundValidationMessage);
+    // });
+
+  });
 
 });
