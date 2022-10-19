ed16e6f5b1a5724b858eda93b175d1cefec04561
Jevgenij Visockij <jevgenij.visockij@invertus.eu>
2022-10-19 11:20:04 +0300

    CS Fixer fix

--- src/PrestaShopBundle/Form/Admin/Catalog/Category/AbstractCategoryType.php
+++ src/PrestaShopBundle/Form/Admin/Catalog/Category/AbstractCategoryType.php
@@ -26,7 +26,6 @@
 
 namespace PrestaShopBundle\Form\Admin\Catalog\Category;
 
-use PrestaShop\PrestaShop\Core\CommandBus\CommandBusInterface;
 use PrestaShop\PrestaShop\Core\ConfigurationInterface;
 use PrestaShop\PrestaShop\Core\ConstraintValidator\Constraints\CleanHtml;
 use PrestaShop\PrestaShop\Core\ConstraintValidator\Constraints\DefaultLanguage;
