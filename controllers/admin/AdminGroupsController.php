c9f1b2886a59543a4b36fab68992a674bb59896d
Karlis Suvi <45097959+ks129@users.noreply.github.com>
2022-10-28 15:52:40 +0000

    Disable access to the customer groups page when this feature is disabled

--- controllers/admin/AdminGroupsController.php
+++ controllers/admin/AdminGroupsController.php
@@ -161,7 +161,7 @@ class AdminGroupsControllerCore extends AdminController
 
     public function initPageHeaderToolbar()
     {
-        if (empty($this->display)) {
+        if (Group::isFeatureActive() && empty($this->display)) {
             $this->page_header_toolbar_btn['new_group'] = [
                 'href' => self::$currentIndex . '&addgroup&token=' . $this->token,
                 'desc' => $this->trans('Add new group', [], 'Admin.Shopparameters.Feature'),
@@ -199,6 +199,10 @@ class AdminGroupsControllerCore extends AdminController
 
     public function postProcess(): void
     {
+        if (!Group::isFeatureActive()) {
+            return;
+        }
+
         $tableCustomerGroup = 'customer_group';
         if (!empty($_POST[$tableCustomerGroup . 'Box'])
             && is_array($_POST[$tableCustomerGroup . 'Box'])
@@ -689,5 +693,25 @@ class AdminGroupsControllerCore extends AdminController
         ]);
 
         return $tpl->fetch();
+    }
+
+    /**
+     * AdminController::initContent() override.
+     *
+     * @see AdminController::initContent()
+     */
+    public function initContent() {
+        if (!Group::isFeatureActive()) {
+            $adminPerformanceUrl = $this->context->link->getAdminLink('AdminPerformance');
+            $url = '<a href="' . $adminPerformanceUrl . '#featuresDetachables">' . $this->trans('Performance', [], 'Admin.Global') . '</a>';
+            $this->displayWarning($this->trans('This feature has been disabled. You can activate it here: %url%.', ['%url%' => $url], 'Admin.Catalog.Notification'));
+
+            $this->context->smarty->assign([
+                'content' => $this->content,
+            ]);
+            return;
+        }
+
+        parent::initContent();
     }
 }
