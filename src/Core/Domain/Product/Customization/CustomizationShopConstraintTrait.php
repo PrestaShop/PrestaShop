6da74c33b07b015ff56e5f2d2fed485941c0a437
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2022-11-17 00:58:21 +0100

    Handle shopConstraint in customization fields queries and commands

--- src/Core/Domain/Product/Customization/CustomizationShopConstraintTrait.php
+++ src/Core/Domain/Product/Customization/CustomizationShopConstraintTrait.php
@@ -26,55 +26,33 @@
 
 declare(strict_types=1);
 
-namespace PrestaShop\PrestaShop\Core\Domain\Product\Customization\Query;
+namespace PrestaShop\PrestaShop\Core\Domain\Product\Customization;
 
-use PrestaShop\PrestaShop\Core\Domain\Product\Customization\CustomizationShopConstraintTrait;
-use PrestaShop\PrestaShop\Core\Domain\Product\ValueObject\ProductId;
+use PrestaShop\PrestaShop\Core\Domain\Shop\Exception\InvalidShopConstraintException;
 use PrestaShop\PrestaShop\Core\Domain\Shop\ValueObject\ShopConstraint;
 
 /**
- * Gets product customization fields
+ * The customization field commands/queries only handles single shop use case, we didn't implement the allShops use case because
+ * it was considered too complex to handle compared to the benefit. The "apply to all shops" boolean can't be assigned on the
+ * command itself but on each contained CustomizationField in the command. Besides the association of customization fields is not
+ * dependent to a specific shop they are common to all shops, and it only allows update the name field for a specific shop.
+ *
+ * A POC had been started in case the feature needs to evolve someday https://github.com/PrestaShop/PrestaShop/pull/27944
  */
-class GetProductCustomizationFields
+trait CustomizationShopConstraintTrait
 {
-    use CustomizationShopConstraintTrait;
-
     /**
-     * @var ProductId
-     */
-    private $productId;
-
-    /**
-     * @var ShopConstraint
-     */
-    private $shopConstraint;
-
-    /**
-     * @param int $productId
      * @param ShopConstraint $shopConstraint
+     *
+     * @throws InvalidShopConstraintException
      */
-    public function __construct(
-        int $productId,
-        ShopConstraint $shopConstraint
-    ) {
-        $this->productId = new ProductId($productId);
-        $this->checkShopConstraint($shopConstraint);
-        $this->shopConstraint = $shopConstraint;
-    }
-
-    /**
-     * @return ProductId
-     */
-    public function getProductId(): ProductId
+    protected function checkShopConstraint(ShopCOnstraint $shopConstraint): void
     {
-        return $this->productId;
-    }
-
-    /**
-     * @return ShopConstraint
-     */
-    public function getShopConstraint(): ShopConstraint
-    {
-        return $this->shopConstraint;
+        if ($shopConstraint->forAllShops() || $shopConstraint->getShopGroupId()) {
+            throw new InvalidShopConstraintException(sprintf(
+                '%s only handles single shop constraint.',
+                self::class
+            ));
+        }
     }
 }
