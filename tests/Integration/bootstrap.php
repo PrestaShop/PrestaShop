78d9d77a7df9ddf5600562b8d2e02a47d4300dd5
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2022-11-17 16:35:00 +0100

    Fix integration tests so that they do not rely on multi statement SQL queries

--- tests/Integration/bootstrap.php
+++ tests/Integration/bootstrap.php
@@ -26,7 +26,6 @@
 define('_PS_IN_TEST_', true);
 define('_PS_ROOT_DIR_', dirname(__DIR__, 2));
 define('_PS_MODULE_DIR_', _PS_ROOT_DIR_ . '/tests/Resources/modules/');
-define('_PS_ALLOW_MULTI_STATEMENTS_QUERIES_', true);
 require_once dirname(__DIR__, 2) . '/admin-dev/bootstrap.php';
 
 /*
