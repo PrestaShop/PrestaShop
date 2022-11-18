d8c9cf36dea04feb9f70fe5457c4e06732512769
cfarhani06 <chedly.farhani@prestashop.com>
2022-11-18 11:51:40 +0100

    mv commonTests/BO/loginBO from .js to .ts

--- tests/UI/campaigns/functional/BO/00_login/03_logout.ts
+++ tests/UI/campaigns/functional/BO/00_login/03_logout.ts
@@ -3,24 +3,23 @@
 import helper from '@utils/helpers';
 
 // Import test context
-import testContext from '/var/www/html/PrestaShop/tests/UI/utils/testContext';
+import testContext from '@utils/testContext';
 
 // Import common tests
-import loginCommon from '/var/www/html/PrestaShop/tests/UI/commonTests/BO/loginBO';
+import loginCommon from '@commonTests/BO/loginBO';
 
 // Import pages
-import loginPage from '/var/www/html/PrestaShop/tests/UI/pages/BO/login/index';
+import loginPage from '@pages/BO/login/index';
 
 const baseContext: string = 'functional_BO_login_logout';
 
-// Import expect from chai
-import {expect} from 'chai';
+import { expect } from 'chai';
 
 import type {BrowserContext, Page} from 'playwright';
 
 /*
 Pre-condition
-- Lognn to BO
+- Login to BO
 Scenario:
 - Logout from BO
  */
@@ -40,5 +39,14 @@ describe('BO - logout : log out from BO', async () => {
 
     it('should login in BO', async function () {
         await loginCommon.loginBO(this, page);
+    });
+
+    it('should logout from BO', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'logoutFromBOPage', baseContext);
+    
+        await loginCommon.logoutBO(this, page);
+    
+        const pageTitle = await loginPage.getPageTitle(page);
+        await expect(pageTitle).to.contains(loginPage.pageTitle);
     });
 });
--- tests/UI/commonTests/BO/loginBO.ts
+++ tests/UI/commonTests/BO/loginBO.ts
@@ -1,23 +1,24 @@
 import testContext from '@utils/testContext';
 
 require('module-alias/register');
-const {expect} = require('chai');
-const loginPage = require('@pages/BO/login');
-const dashboardPage = require('@pages/BO/dashboard');
+import {expect} from 'chai';
+import loginPage from '@pages/BO/login';
+import dashboardPage from '@pages/BO/dashboard';
+import {Page} from 'playwright';
 
-module.exports = {
-  async loginBO(mochaContext, page, email = global.BO.EMAIL, password = global.BO.PASSWD) {
+export default {
+  async loginBO(mochaContext: Mocha.Context, page: Page, email = global.BO.EMAIL, password = global.BO.PASSWD) {
     await testContext.addContextItem(mochaContext, 'testIdentifier', 'loginBO');
     await loginPage.goTo(page, global.BO.URL);
     await loginPage.successLogin(page, email, password);
     const pageTitle = await dashboardPage.getPageTitle(page);
-    await expect(pageTitle).to.contains(dashboardPage.pageTitle);
+    expect(pageTitle).to.contains(dashboardPage.pageTitle);
   },
 
-  async logoutBO(mochaContext, page) {
+  async logoutBO(mochaContext: Mocha.Context, page: Page) {
     await testContext.addContextItem(mochaContext, 'testIdentifier', 'logoutBO');
     await dashboardPage.logoutBO(page);
     const pageTitle = await loginPage.getPageTitle(page);
-    await expect(pageTitle).to.contains(loginPage.pageTitle);
+    expect(pageTitle).to.contains(loginPage.pageTitle);
   },
 };
