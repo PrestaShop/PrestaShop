279adc95ff0bca067e07d77e67e97e09b2785581
Fabien Papet <fabien.papet@gmail.com>
2022-11-18 18:15:00 +0100

    Migrate Tools::str2url to new code

--- classes/Tools.php
+++ classes/Tools.php
@@ -31,6 +31,7 @@ use PrestaShop\PrestaShop\Core\Localization\Exception\LocalizationException;
 use PrestaShop\PrestaShop\Core\Localization\Locale;
 use PrestaShop\PrestaShop\Core\Localization\Locale\Repository as LocaleRepository;
 use PrestaShop\PrestaShop\Core\Util\ColorBrightnessCalculator;
+use PrestaShop\PrestaShop\Core\Util\String\StringModifier;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -1400,45 +1401,7 @@ class ToolsCore
      */
     public static function str2url($str)
     {
-        static $array_str = [];
-        static $allow_accented_chars = null;
-
-        if (!is_string($str)) {
-            return false;
-        }
-
-        if (isset($array_str[$str])) {
-            return $array_str[$str];
-        }
-
-        if ($str == '') {
-            return '';
-        }
-
-        if ($allow_accented_chars === null) {
-            $allow_accented_chars = Configuration::get('PS_ALLOW_ACCENTED_CHARS_URL');
-        }
-
-        $return_str = trim($str);
-        $return_str = mb_strtolower($return_str, 'UTF-8');
-
-        if (!$allow_accented_chars) {
-            $return_str = Tools::replaceAccentedChars($return_str);
-        }
-
-        // Remove all non-whitelist chars.
-        if ($allow_accented_chars) {
-            $return_str = preg_replace('/[^a-zA-Z0-9\s\'\:\/\[\]\-\p{L}]/u', '', $return_str);
-        } else {
-            $return_str = preg_replace('/[^a-zA-Z0-9\s\'\:\/\[\]\-]/', '', $return_str);
-        }
-
-        $return_str = preg_replace('/[\s\'\:\/\[\]\-]+/', ' ', $return_str);
-        $return_str = str_replace([' ', '/'], '-', $return_str);
-
-        $array_str[$str] = $return_str;
-
-        return $return_str;
+        return (new StringModifier())->str2url((string) $str);
     }
 
     /**
@@ -1450,13 +1413,7 @@ class ToolsCore
      */
     public static function replaceAccentedChars($str)
     {
-        static $transliterator;
-
-        if (!$transliterator) {
-            $transliterator = Transliterator::create('Any-Latin; Latin-ASCII');
-        }
-
-        return $transliterator->transliterate($str);
+        return (new StringModifier())->replaceAccentedChars($str);
     }
 
     /**
