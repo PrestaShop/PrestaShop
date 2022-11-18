6b6ee222ae191c29a11004469e985ede787b4887
cfarhani06 <chedly.farhani@prestashop.com>
2022-11-18 15:11:36 +0100

    add Promise<void> to login and logout methods

--- tests/UI/commonTests/BO/loginBO.ts
+++ tests/UI/commonTests/BO/loginBO.ts
@@ -1,13 +1,13 @@
 import testContext from '@utils/testContext';
-
-require('module-alias/register');
 import {expect} from 'chai';
 import loginPage from '@pages/BO/login';
 import dashboardPage from '@pages/BO/dashboard';
 import {Page} from 'playwright';
 
+require('module-alias/register');
+
 export default {
-  async loginBO(mochaContext: Mocha.Context, page: Page, email = global.BO.EMAIL, password = global.BO.PASSWD) {
+  async loginBO(mochaContext: Mocha.Context, page: Page, email = global.BO.EMAIL, password = global.BO.PASSWD): Promise<void> {
     await testContext.addContextItem(mochaContext, 'testIdentifier', 'loginBO');
     await loginPage.goTo(page, global.BO.URL);
     await loginPage.successLogin(page, email, password);
@@ -15,7 +15,7 @@ export default {
     expect(pageTitle).to.contains(dashboardPage.pageTitle);
   },
 
-  async logoutBO(mochaContext: Mocha.Context, page: Page) {
+  async logoutBO(mochaContext: Mocha.Context, page: Page): Promise<void> {
     await testContext.addContextItem(mochaContext, 'testIdentifier', 'logoutBO');
     await dashboardPage.logoutBO(page);
     const pageTitle = await loginPage.getPageTitle(page);
