e67dc367cd8a3b83cb940951da1f3864c85550d3
Hlavtox <daniel.hlavacek@hotmail.cz>
2022-11-21 12:39:48 +0100

    Fix filemanager translations

--- admin-dev/filemanager/lang/bg.php
+++ admin-dev/filemanager/lang/bg.php
@@ -1,5 +1,7 @@
 <?php
-
+/*
+* Important - this file MUST implement all strings defined in base en.php file
+*/
 define('lang_Select', 'Избери');
 define('lang_Erase', 'Изтрий');
 define('lang_Open', 'Отвори');
