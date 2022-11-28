699eb7bd4ce35234fcffe431b660061baaddc95e
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2022-11-28 12:58:00 +0100

    Introduce PrestaShopLoggerInterface

---
+++ classes/log/PrestaShopLoggerInterface.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+declare(strict_types=1);
+
+interface PrestaShopLoggerInterface
+{
+    public const DEBUG = 0;
+    public const INFO = 1;
+    public const WARNING = 2;
+    public const ERROR = 3;
+
+    /**
+     * @param string $message
+     * @param int $level
+     *
+     * @return void
+     */
+    public function log($message, $level = self::DEBUG);
+
+    /**
+     * @param string $message
+     *
+     * @return void
+     */
+    public function logDebug($message);
+
+    /**
+     * @param string $message
+     *
+     * @return void
+     */
+    public function logInfo($message);
+
+    /**
+     * @param string $message
+     *
+     * @return void
+     */
+    public function logWarning($message);
+
+    /**
+     * @param string $message
+     *
+     * @return void
+     */
+    public function logError($message);
+}
