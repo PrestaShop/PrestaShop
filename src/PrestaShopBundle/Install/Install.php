47c5fe267f6281a238069176f6be07d2ac31b92d
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2022-11-28 12:59:04 +0100

    Install class can now configure its logger

--- src/PrestaShopBundle/Install/Install.php
+++ src/PrestaShopBundle/Install/Install.php
@@ -26,6 +26,7 @@
 
 namespace PrestaShopBundle\Install;
 
+use FileLogger as LegacyFileLogger;
 use Language as LanguageLegacy;
 use PhpEncryption;
 use PrestaShop\PrestaShop\Adapter\Entity\Cache;
@@ -38,7 +39,6 @@ use PrestaShop\PrestaShop\Adapter\Entity\Country;
 use PrestaShop\PrestaShop\Adapter\Entity\Currency;
 use PrestaShop\PrestaShop\Adapter\Entity\Db;
 use PrestaShop\PrestaShop\Adapter\Entity\Employee;
-use PrestaShop\PrestaShop\Adapter\Entity\FileLogger;
 use PrestaShop\PrestaShop\Adapter\Entity\Group;
 use PrestaShop\PrestaShop\Adapter\Entity\ImageManager;
 use PrestaShop\PrestaShop\Adapter\Entity\ImageType;
@@ -62,6 +62,7 @@ use PrestaShopBundle\Cache\LocalizationWarmer;
 use PrestaShopBundle\Service\Database\Upgrade as UpgradeDatabase;
 use PrestaShopException;
 use PrestashopInstallerException;
+use PrestaShopLoggerInterface;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Yaml\Yaml;
 
@@ -71,8 +72,6 @@ class Install extends AbstractInstall
     public const BOOTSTRAP_FILE = 'config/bootstrap.php';
 
     public const DEFAULT_THEME = 'classic';
-
-    protected $logger;
 
     /**
      * The path of the bootsrap file we want to use for the installation.
@@ -98,7 +97,12 @@ class Install extends AbstractInstall
      */
     protected $isDebug = null;
 
-    public function __construct($settingsFile = null, $bootstrapFile = null)
+    /**
+     * @param string|null $settingsFile
+     * @param string|null $bootstrapFile
+     * @param PrestaShopLoggerInterface $logger
+     */
+    public function __construct($settingsFile = null, $bootstrapFile = null, $logger = null)
     {
         if ($bootstrapFile === null) {
             $bootstrapFile = static::BOOTSTRAP_FILE;
@@ -111,21 +115,21 @@ class Install extends AbstractInstall
         $this->settingsFile = $settingsFile;
         $this->bootstrapFile = $bootstrapFile;
         $this->isDebug = _PS_MODE_DEV_;
+
+        if (null === $logger) {
+            $this->logger = new LegacyFileLogger();
+            $this->logger->setFilename(
+                _PS_ROOT_DIR_ . '/var/logs/' . _PS_ENV_ . '_' . @date('Ymd') . '_installation.log'
+            );
+        } else {
+            $this->setLogger($logger);
+        }
+
         parent::__construct();
     }
 
     public function setError($errors)
     {
-        static $logger = null;
-
-        if (null === $logger) {
-            $logger = new FileLogger();
-            $logger->setFilename(
-                _PS_ROOT_DIR_ . '/var/logs/' . _PS_ENV_ . '_' . @date('Ymd') . '_installation.log'
-            );
-            $this->logger = $logger;
-        }
-
         if (!is_array($errors)) {
             $errors = [$errors];
         }
@@ -133,7 +137,7 @@ class Install extends AbstractInstall
         parent::setError($errors);
 
         foreach ($errors as $error) {
-            $this->logger->logError($error);
+            $this->getLogger()->logError($error);
         }
     }
 
