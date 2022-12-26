a6fad57b4a7af953fce99da96c47256fcd026b23
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2022-12-26 17:17:16 +0100

    Improve shop list column and introduce ShopName and Empty columns that are used for toggling previews

--- src/Core/Grid/Column/Type/EmptyColumn.php
+++ src/Core/Grid/Column/Type/EmptyColumn.php
@@ -26,47 +26,21 @@
 
 declare(strict_types=1);
 
-namespace PrestaShop\PrestaShop\Core\Grid\Column\Type\Product;
+namespace PrestaShop\PrestaShop\Core\Grid\Column\Type;
 
 use PrestaShop\PrestaShop\Core\Grid\Column\AbstractColumn;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
- * Display a list of shops, the first shop is highlighted as bold and the rest are truncated if
- * max_displayed_characters is set.
+ * Empty column which displays nothing, mainly used when changing the content of a grid a replacing one column
+ * with empty content.
  */
-final class ShopListColumn extends AbstractColumn
+class EmptyColumn extends AbstractColumn
 {
     /**
      * {@inheritdoc}
      */
     public function getType()
     {
-        return 'shop_list';
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function configureOptions(OptionsResolver $resolver)
-    {
-        parent::configureOptions($resolver);
-
-        $resolver
-            ->setRequired([
-                'field',
-                'ids_field',
-                'product_id_field',
-            ])
-            ->setDefaults([
-                'sortable' => false,
-                'clickable' => false,
-                'max_displayed_characters' => 0,
-            ])
-            ->setAllowedTypes('field', 'string')
-            ->setAllowedTypes('ids_field', 'string')
-            ->setAllowedTypes('product_id_field', 'string')
-            ->setAllowedTypes('max_displayed_characters', 'int')
-        ;
+        return 'empty';
     }
 }
