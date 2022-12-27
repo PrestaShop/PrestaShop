ec4cd4106604d685f5f67cb339049cf17da13748
Nesrine ABDMOULEH <nesrine.abdmouleh.info@gmail.com>
2022-12-27 09:56:43 +0100

    Fix review

--- tests/UI/pages/BO/catalog/productsV2/add/index.js
+++ tests/UI/pages/BO/catalog/productsV2/add/index.js
@@ -5,7 +5,7 @@ const productsPage = require('@pages/BO/catalog/productsV2');
 const descriptionTab = require('@pages/BO/catalog/productsV2/add/descriptionTab');
 const detailsTab = require('@pages/BO/catalog/productsV2/add/detailsTab');
 const stocksTab = require('@pages/BO/catalog/productsV2/add/stocksTab');
-const virtualProductTab = require('@pages/BO/catalog/productsV2/add/virtualProductTab');
+import virtualProductTab from '@pages/BO/catalog/productsV2/add/virtualProductTab';
 const pricingTab = require('@pages/BO/catalog/productsV2/add/pricingTab');
 const packTab = require('@pages/BO/catalog/productsV2/add/packTab');
 
