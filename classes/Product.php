7f8ca5d82b9245ea375a7d2d75adcc220b5f7ad1
PululuK <pululuandre@hotmail.com>
2023-01-03 22:55:50 +0100

    Optmize `Product::hasCombinations`

--- classes/Product.php
+++ classes/Product.php
@@ -7482,7 +7482,7 @@ class ProductCore extends ObjectModel
         if (null === $this->id || 0 >= $this->id) {
             return false;
         }
-        $attributes = self::getAttributesInformationsByProduct($this->id);
+        $attributes = self::getProductAttributesIds($this->id, true);
 
         return !empty($attributes);
     }
