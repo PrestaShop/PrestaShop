1089a69ac91ca60406053dd8df78245f1d72756d
Nesrine ABDMOULEH <nesrine.abdmouleh.info@gmail.com>
2023-01-05 16:29:25 +0100

    Fix review

--- tests/UI/pages/FO/pricesDrop/index.ts
+++ tests/UI/pages/FO/pricesDrop/index.ts
@@ -1,13 +1,13 @@
 import FOBasePage from '@pages/FO/FObasePage';
 
-require('module-alias/register');
-
 /**
  * Prices drop page, contains functions that can be used on the page
  * @class
  * @extends FOBasePage
  */
 class PricesDrop extends FOBasePage {
+  public readonly pageTitle: string;
+
   /**
    * @constructs
    * Setting up texts and selectors to use on prices drop page
@@ -19,4 +19,4 @@ class PricesDrop extends FOBasePage {
   }
 }
 
-module.exports = new PricesDrop();
+export default new PricesDrop();
