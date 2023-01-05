2b7b9610d1ed9d04696c2cddd949d1adf6cf66b4
Nesrine ABDMOULEH <nesrine.abdmouleh.info@gmail.com>
2023-01-05 17:09:46 +0100

    Fix import FO pages in Orders scenarios

--- tests/UI/campaigns/functional/BO/02_orders/01_orders/viewAndEditOrder/07_messagesBlock.js
+++ tests/UI/campaigns/functional/BO/02_orders/01_orders/viewAndEditOrder/07_messagesBlock.js
@@ -9,6 +9,9 @@ import loginCommon from '@commonTests/BO/loginBO';
 // Import FO pages
 import foHomePage from '@pages/FO/home';
 import foLoginPage from '@pages/FO/login';
+import foOrderConfirmationPage from '@pages/FO/checkout/orderConfirmation';
+import foMyAccountPage from '@pages/FO/myAccount';
+import foOrderHistoryPage from '@pages/FO/myAccount/orderHistory';
 
 require('module-alias/register');
 
@@ -20,9 +23,6 @@ const dashboardPage = require('@pages/BO/dashboard');
 const ordersPage = require('@pages/BO/orders');
 const orderPageMessagesBlock = require('@pages/BO/orders/view/messagesBlock');
 const orderMessagesPage = require('@pages/BO/customerService/orderMessages');
-const foOrderConfirmationPage = require('@pages/FO/checkout/orderConfirmation');
-const foMyAccountPage = require('@pages/FO/myAccount');
-const foOrderHistoryPage = require('@pages/FO/myAccount/orderHistory');
 
 // Import demo data
 const {DefaultCustomer} = require('@data/demo/customer');
