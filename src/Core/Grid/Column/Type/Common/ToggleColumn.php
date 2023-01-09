6fd7132f0f930aa309c5a0f0d736eba8d9f45c0f
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2023-01-09 18:56:44 +0100

    Toggle column accepts extra parameters for route generation, the primary key used in the field is a composition of all parameters values

--- src/Core/Grid/Column/Type/Common/ToggleColumn.php
+++ src/Core/Grid/Column/Type/Common/ToggleColumn.php
@@ -57,6 +57,7 @@ final class ToggleColumn extends AbstractColumn
                 // @deprecated, use route_param_name option instead
                 'route_param_id' => '',
                 'route_param_name' => '',
+                'extra_route_params' => [],
             ])
             ->setRequired([
                 'field',
@@ -69,6 +70,7 @@ final class ToggleColumn extends AbstractColumn
             ->setAllowedTypes('route_param_name', 'string')
             ->setAllowedTypes('sortable', 'bool')
             ->setAllowedTypes('route_param_id', 'string')
+            ->setAllowedTypes('extra_route_params', 'array')
         ;
 
         $resolver->setNormalizer('route_param_name', static function (Options $options, $value) {
