5d2ac9c9530eaca7c84e7a73315023ac42c6a3aa
Progi1984 <franck.lefevre@prestashop.com>
2023-01-10 13:21:12 +0100

    Functional Tests : Migration TS (`@pages/BO/quickAccess/add`)

--- tests/UI/pages/BO/quickAccess/add.ts
+++ tests/UI/pages/BO/quickAccess/add.ts
@@ -1,5 +1,6 @@
-require('module-alias/register');
-const BOBasePage = require('@pages/BO/BObasePage');
+import BOBasePage from '@pages/BO/BObasePage';
+
+import type {Page} from 'playwright';
 
 /**
  * Add quick access page, contains functions that can be used on the page
@@ -7,6 +8,16 @@ const BOBasePage = require('@pages/BO/BObasePage');
  * @extends BOBasePage
  */
 class AddQuickAccess extends BOBasePage {
+  public readonly pageTitle: string;
+
+  private readonly nameInput: string;
+
+  private readonly urlInput: string;
+
+  private readonly newWindowToggle: (toggle: string) => string;
+
+  private readonly saveButton: string;
+
   /**
    * @constructs
    * Setting up texts and selectors to use on add quick access page
@@ -19,7 +30,7 @@ class AddQuickAccess extends BOBasePage {
     // Selectors
     this.nameInput = '#name_1';
     this.urlInput = '#link';
-    this.newWindowToggle = (toggle) => `#new_window_${toggle}`;
+    this.newWindowToggle = (toggle: string) => `#new_window_${toggle}`;
     this.saveButton = '#quick_access_form_submit_btn';
   }
 
@@ -33,7 +44,7 @@ class AddQuickAccess extends BOBasePage {
    * openNewWindow: boolean}} Data to set on new quick access form
    * @returns {Promise<string>}
    */
-  async setQuickAccessLink(page, quickAccessLinkData) {
+  async setQuickAccessLink(page: Page, quickAccessLinkData): Promise<string> {
     await this.setValue(page, this.nameInput, quickAccessLinkData.name);
     await this.setValue(page, this.urlInput, quickAccessLinkData.url);
     await this.setChecked(page, this.newWindowToggle(quickAccessLinkData.openNewWindow ? 'on' : 'off'));
@@ -44,4 +55,4 @@ class AddQuickAccess extends BOBasePage {
   }
 }
 
-module.exports = new AddQuickAccess();
+export default new AddQuickAccess();
