40c1535cff4d88877b44f54b41c9774470ee4cd0
Nesrine ABDMOULEH <nesrine.abdmouleh.info@gmail.com>
2023-01-10 14:51:27 +0100

    Add new test download invoice

--- tests/UI/pages/FO/myAccount/orderHistory.ts
+++ tests/UI/pages/FO/myAccount/orderHistory.ts
@@ -166,6 +166,15 @@ class OrderHistoryPage extends FOBasePage {
   }
 
   /**
+   * Download invoice
+   * @param page {Page} Browser tab
+   * @param row {number} Row number in orders table
+   */
+  async downloadInvoice(page: Page, row: number = 1): Promise<string | null> {
+    return this.clickAndWaitForDownload(page, this.orderTableColumnInvoice(row));
+  }
+
+  /**
    * Go to details page from order history page
    * @param page {Page} Browser tab
    * @param orderRow {Number} row in orders table
