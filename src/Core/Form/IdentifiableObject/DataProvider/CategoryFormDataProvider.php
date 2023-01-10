4607e956ec94b1deda5b5a72101f6159c2d5b8fb
Jevgenij Visockij <jevgenij.visockij@invertus.eu>
2023-01-10 10:07:15 +0200

    CS fixer fix

--- src/Core/Form/IdentifiableObject/DataProvider/CategoryFormDataProvider.php
+++ src/Core/Form/IdentifiableObject/DataProvider/CategoryFormDataProvider.php
@@ -27,10 +27,10 @@
 namespace PrestaShop\PrestaShop\Core\Form\IdentifiableObject\DataProvider;
 
 use PrestaShop\PrestaShop\Adapter\Group\GroupDataProvider;
-use PrestaShop\PrestaShop\Core\Shop\Url\CategoryProvider;
 use PrestaShop\PrestaShop\Core\CommandBus\CommandBusInterface;
 use PrestaShop\PrestaShop\Core\Domain\Category\Query\GetCategoryForEditing;
 use PrestaShop\PrestaShop\Core\Domain\Category\QueryResult\EditableCategory;
+use PrestaShop\PrestaShop\Core\Shop\Url\CategoryProvider;
 use PrestaShopBundle\Service\Routing\Router;
 
 /**
