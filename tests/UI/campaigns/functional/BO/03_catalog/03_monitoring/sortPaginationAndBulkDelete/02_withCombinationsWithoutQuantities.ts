80639f1d62e93680d5e05ee7c88c0547bf8d167d
Progi1984 <franck.lefevre@prestashop.com>
2023-01-13 12:10:17 +0100

    Functional Tests : Migration TS (`@commonTests/BO/advancedParameters/importFile`)

--- tests/UI/campaigns/functional/BO/03_catalog/03_monitoring/sortPaginationAndBulkDelete/02_withCombinationsWithoutQuantities.ts
+++ tests/UI/campaigns/functional/BO/03_catalog/03_monitoring/sortPaginationAndBulkDelete/02_withCombinationsWithoutQuantities.ts
@@ -5,7 +5,7 @@ import helper from '@utils/helpers';
 import testContext from '@utils/testContext';
 
 // Import common tests
-import {importFileTest} from '@commonTests/BO/advancedParameters/importFile';
+import importFileTest from '@commonTests/BO/advancedParameters/importFile';
 import bulkDeleteProductsTest from '@commonTests/BO/catalog/monitoring';
 import loginCommon from '@commonTests/BO/loginBO';
 
