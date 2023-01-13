4fd2b18128b6208aee7b8953c4425361e10e66cf
soledis_thomas_le_guillant <thomas.leguillant@groupe-soledis.com>
2023-01-13 12:24:10 +0100

    Modification of the "getShopDomainActive" method documentation

--- classes/Link.php
+++ classes/Link.php
@@ -1631,7 +1631,7 @@ class LinkCore
      * @param Shop $shop
      * @param int|null $idShop
      *
-     * @return array
+     * @return array<string>
      */
     private function getShopDomainActive(Shop $shop, ?int $idShop): array
     {
