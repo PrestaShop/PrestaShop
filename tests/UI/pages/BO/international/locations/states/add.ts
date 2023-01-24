e6fcfd431da1a8fa4e2ed670f7463b87f69cdd35
Progi1984 <franck.lefevre@prestashop.com>
2023-01-24 09:55:39 +0100

    Functional Tests : Migration TS (`@pages/BO/international/locations/states/add`)

--- tests/UI/pages/BO/international/locations/states/add.ts
+++ tests/UI/pages/BO/international/locations/states/add.ts
@@ -1,5 +1,8 @@
-require('module-alias/register');
-const BOBasePage = require('@pages/BO/BObasePage');
+import BOBasePage from '@pages/BO/BObasePage';
+
+import type StateData from '@data/faker/state';
+
+import type {Page} from 'playwright';
 
 /**
  * Add state page, contains functions that can be used on the page
@@ -7,6 +10,22 @@ const BOBasePage = require('@pages/BO/BObasePage');
  * @extends BOBasePage
  */
 class AddState extends BOBasePage {
+  public readonly pageTitleCreate: string;
+
+  public readonly pageTitleEdit: string;
+
+  private readonly nameInput: string;
+
+  private readonly isoCodeInput: string;
+
+  private readonly countrySelect: string;
+
+  private readonly zoneSelect: string;
+
+  private readonly statusToggle: (toggle: string) => string;
+
+  private readonly saveStateButton: string;
+
   /**
    * @constructs
    * Setting up texts and selectors to use on add state page
@@ -22,7 +41,7 @@ class AddState extends BOBasePage {
     this.isoCodeInput = '#iso_code';
     this.countrySelect = '#id_country';
     this.zoneSelect = '#id_zone';
-    this.statusToggle = (toggle) => `#active_${toggle}`;
+    this.statusToggle = (toggle: string) => `#active_${toggle}`;
     this.saveStateButton = '#state_form_submit_btn';
   }
 
@@ -35,7 +54,7 @@ class AddState extends BOBasePage {
    * @param stateData {StateData} Data to set on new/edit state form
    * @returns {Promise<string>}
    */
-  async createEditState(page, stateData) {
+  async createEditState(page: Page, stateData: StateData): Promise<string> {
     // Fill form
     await this.setValue(page, this.nameInput, stateData.name);
     await this.setValue(page, this.isoCodeInput, stateData.isoCode);
@@ -51,4 +70,4 @@ class AddState extends BOBasePage {
   }
 }
 
-module.exports = new AddState();
+export default new AddState();
