efe41759aa3052c78735fd47d777d0488aa79c8b
Progi1984 <franck.lefevre@prestashop.com>
2023-01-25 10:24:05 +0100

    Functional Tests : Migration TS (`@pages/BO/design/pages/pageCategory/add`)

--- tests/UI/pages/BO/design/pages/pageCategory/add.ts
+++ tests/UI/pages/BO/design/pages/pageCategory/add.ts
@@ -1,5 +1,8 @@
-require('module-alias/register');
-const BOBasePage = require('@pages/BO/BObasePage');
+import BOBasePage from '@pages/BO/BObasePage';
+
+import type CMSCategoryData from '@data/faker/CMScategory';
+
+import type {Page} from 'playwright';
 
 /**
  * Add page category page, contains functions that can be used on the page
@@ -7,6 +10,22 @@ const BOBasePage = require('@pages/BO/BObasePage');
  * @extends BOBasePage
  */
 class AddPageCategory extends BOBasePage {
+  public readonly pageTitleCreate: string;
+
+  private readonly nameInput: string;
+
+  private readonly displayedToggleInput: (toggle: number) => string;
+
+  private readonly descriptionIframe: string;
+
+  private readonly metaTitleInput: string;
+
+  private readonly metaDescriptionInput: string;
+
+  private readonly metaKeywordsInput: string;
+
+  private readonly saveCategoryButton: string;
+
   /**
    * @constructs
    * Setting up texts and selectors to use on add page category page
@@ -18,7 +37,7 @@ class AddPageCategory extends BOBasePage {
 
     // Selectors
     this.nameInput = '#cms_page_category_name_1';
-    this.displayedToggleInput = (toggle) => `#cms_page_category_is_displayed_${toggle}`;
+    this.displayedToggleInput = (toggle: number) => `#cms_page_category_is_displayed_${toggle}`;
     this.descriptionIframe = '#cms_page_category_description_1';
     this.metaTitleInput = '#cms_page_category_meta_title_1';
     this.metaDescriptionInput = '#cms_page_category_meta_description_1';
@@ -36,7 +55,7 @@ class AddPageCategory extends BOBasePage {
    * @param pageCategoryData {CMSCategoryData} Data to set on page category form
    * @returns {Promise<string>}
    */
-  async createEditPageCategory(page, pageCategoryData) {
+  async createEditPageCategory(page: Page, pageCategoryData: CMSCategoryData): Promise<string> {
     await this.setValue(page, this.nameInput, pageCategoryData.name);
     await this.setChecked(page, this.displayedToggleInput(pageCategoryData.displayed ? 1 : 0));
     await this.setValue(page, this.descriptionIframe, pageCategoryData.description);
@@ -49,4 +68,4 @@ class AddPageCategory extends BOBasePage {
   }
 }
 
-module.exports = new AddPageCategory();
+export default new AddPageCategory();
