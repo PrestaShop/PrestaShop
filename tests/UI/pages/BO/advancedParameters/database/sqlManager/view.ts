d6c1efa9eb0a04720dab2ddd70f14bf6857e67eb
Progi1984 <franck.lefevre@prestashop.com>
2023-01-26 11:49:52 +0100

    Functional Tests : Migration TS (`@pages/BO/advancedParameters/database/sqlManager/view`)

--- tests/UI/pages/BO/advancedParameters/database/sqlManager/view.ts
+++ tests/UI/pages/BO/advancedParameters/database/sqlManager/view.ts
@@ -1,5 +1,6 @@
-require('module-alias/register');
-const BOBasePage = require('@pages/BO/BObasePage');
+import BOBasePage from '@pages/BO/BObasePage';
+
+import type {Page} from 'playwright';
 
 /**
  * View sql query page, contains functions that can be used on the page
@@ -7,6 +8,22 @@ const BOBasePage = require('@pages/BO/BObasePage');
  * @extends BOBasePage
  */
 class ViewSQLQuery extends BOBasePage {
+  public readonly pageTitle: string;
+
+  private readonly sqlQueryResultTitle: string;
+
+  private readonly resultsTable: string;
+
+  private readonly tableColumnName: (column: number) => string;
+
+  private readonly tableBody: string;
+
+  private readonly tableRows: string;
+
+  private readonly tableRow: (row: number) => string;
+
+  private readonly tableColumn: (row: number, column: string) => string;
+
   /**
    * @constructs
    * Setting up texts and selectors to use on view sql query page
@@ -19,11 +36,11 @@ class ViewSQLQuery extends BOBasePage {
     // Selectors
     this.sqlQueryResultTitle = '#card-title';
     this.resultsTable = '#grid-table';
-    this.tableColumnName = (column) => `${this.resultsTable} th:nth-child(${column})`;
+    this.tableColumnName = (column: number) => `${this.resultsTable} th:nth-child(${column})`;
     this.tableBody = `${this.resultsTable} tbody`;
     this.tableRows = `${this.tableBody} tr`;
-    this.tableRow = (row) => `${this.tableRows}:nth-child(${row})`;
-    this.tableColumn = (row, column) => `${this.tableRow(row)} td.grid-${column}-value`;
+    this.tableRow = (row: number) => `${this.tableRows}:nth-child(${row})`;
+    this.tableColumn = (row: number, column: string) => `${this.tableRow(row)} td.grid-${column}-value`;
   }
 
   /*
@@ -34,7 +51,7 @@ class ViewSQLQuery extends BOBasePage {
    * @param page {Page} Browser tab
    * @returns {Promise<number>}
    */
-  getSQLQueryResultNumber(page) {
+  getSQLQueryResultNumber(page: Page): Promise<number> {
     return this.getNumberFromText(page, this.sqlQueryResultTitle);
   }
 
@@ -44,7 +61,7 @@ class ViewSQLQuery extends BOBasePage {
    * @param column {number} Id of column to get column name
    * @returns {Promise<string>}
    */
-  getColumnName(page, column = 1) {
+  getColumnName(page: Page, column: number = 1): Promise<string> {
     return this.getTextContent(page, this.tableColumnName(column));
   }
 
@@ -55,9 +72,9 @@ class ViewSQLQuery extends BOBasePage {
    * @param column {string} Name of the column
    * @return {Promise<string>}
    */
-  getTextColumn(page, row, column) {
+  getTextColumn(page: Page, row: number, column: string): Promise<string> {
     return this.getTextContent(page, this.tableColumn(row, column));
   }
 }
 
-module.exports = new ViewSQLQuery();
+export default new ViewSQLQuery();
