c20ede01e78f2384e0836f8f865543311c4793fb
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2023-01-27 15:03:17 +0100

    Fix Duplicate product test

--- tests/UI/campaigns/productV2/functional/07_duplicateProduct.ts
+++ tests/UI/campaigns/productV2/functional/07_duplicateProduct.ts
@@ -15,7 +15,7 @@ import productsPage from '@pages/BO/catalog/productsV2';
 import createProductsPage from '@pages/BO/catalog/productsV2/add';
 
 // Import data
-import {Products} from '@data/demo/products';
+import Products from '@data/demo/products';
 
 const baseContext: string = 'productV2_functional_duplicateProduct';
 
@@ -52,7 +52,7 @@ describe('BO - Catalog - Products : Duplicate product', async () => {
       );
       await productsPage.closeSfToolBar(page);
 
-      const pageTitle: string = await productsPage.getPageTitle(page);
+      const pageTitle = await productsPage.getPageTitle(page);
       await expect(pageTitle).to.contains(productsPage.pageTitle);
     });
 
@@ -68,28 +68,28 @@ describe('BO - Catalog - Products : Duplicate product', async () => {
 
       await productsPage.filterProducts(page, 'reference', Products.demo_14.reference);
 
-      const numberOfProductsAfterFilter: number = await productsPage.getNumberOfProductsFromList(page);
+      const numberOfProductsAfterFilter = await productsPage.getNumberOfProductsFromList(page);
       await expect(numberOfProductsAfterFilter).to.equal(1);
     });
 
     it('should click on duplicate button', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'clickOnDuplicateProduct', baseContext);
 
-      const isModalVisible: boolean = await productsPage.clickOnDuplicateProductButton(page);
+      const isModalVisible = await productsPage.clickOnDuplicateProductButton(page);
       await expect(isModalVisible).to.be.true;
     });
 
     it('should duplicate product', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'duplicateProduct', baseContext);
 
-      const textMessage: string = await productsPage.clickOnConfirmDialogButton(page);
+      const textMessage = await productsPage.clickOnConfirmDialogButton(page);
       await expect(textMessage).to.equal(createProductsPage.successfulDuplicateMessage);
     });
 
     it('should click on duplicate button from the footer of create product page', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'clickDuplicateFromFooterPage', baseContext);
 
-      const textMessage: string = await createProductsPage.duplicateProduct(page);
+      const textMessage = await createProductsPage.duplicateProduct(page);
       await expect(textMessage).to.equal(createProductsPage.successfulDuplicateMessage);
     });
 
@@ -98,14 +98,14 @@ describe('BO - Catalog - Products : Duplicate product', async () => {
 
       await createProductsPage.goToCatalogPage(page);
 
-      const pageTitle: string = await productsPage.getPageTitle(page);
+      const pageTitle = await productsPage.getPageTitle(page);
       await expect(pageTitle).to.contains(productsPage.pageTitle);
     });
 
     it('should check the number of duplicated products', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'checkNumberOfProducts', baseContext);
 
-      const numberOfProductsAfterDuplicate: number = await productsPage.getNumberOfProductsFromList(page);
+      const numberOfProductsAfterDuplicate = await productsPage.getNumberOfProductsFromList(page);
       await expect(numberOfProductsAfterDuplicate).to.equal(3);
     });
 
@@ -114,7 +114,7 @@ describe('BO - Catalog - Products : Duplicate product', async () => {
 
       await productsPage.filterProducts(page, 'product_name', 'copy of');
 
-      let textColumn: string | number | boolean = await productsPage.getTextColumn(page, 'product_name', 1);
+      let textColumn = await productsPage.getTextColumn(page, 'product_name', 1);
       await expect(textColumn).to.contain(`copy of copy of ${Products.demo_14.name}`);
 
       textColumn = await productsPage.getTextColumn(page, 'product_name', 2);
@@ -126,35 +126,35 @@ describe('BO - Catalog - Products : Duplicate product', async () => {
     it('should select the 2 products', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'clickOnDeleteProduct', baseContext);
 
-      const isBulkDeleteButtonEnabled: boolean = await productsPage.bulkSelectProducts(page);
+      const isBulkDeleteButtonEnabled = await productsPage.bulkSelectProducts(page);
       await expect(isBulkDeleteButtonEnabled).to.be.true;
     });
 
     it('should click on bulk actions button', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'clickOnBulkDeleteButton', baseContext);
 
-      const textMessage: string = await productsPage.clickOnBulkActionsProducts(page, 'delete');
+      const textMessage = await productsPage.clickOnBulkActionsProducts(page, 'delete');
       await expect(textMessage).to.equal('Deleting 2 products');
     });
 
     it('should bulk delete products', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'bulkDeleteProduct', baseContext);
 
-      const textMessage: string = await productsPage.bulkActionsProduct(page, 'delete');
+      const textMessage = await productsPage.bulkActionsProduct(page, 'delete');
       await expect(textMessage).to.equal('Deleting 2 / 2 products');
     });
 
     it('should close progress modal', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'closeProgressModal', baseContext);
 
-      const isModalVisible: boolean = await productsPage.closeBulkActionsProgressModal(page, 'delete');
+      const isModalVisible = await productsPage.closeBulkActionsProgressModal(page, 'delete');
       await expect(isModalVisible).to.be.true;
     });
 
     it('should reset filter', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'resetFilter', baseContext);
 
-      const numberOfProductsAfterReset: number = await productsPage.resetAndGetNumberOfLines(page);
+      const numberOfProductsAfterReset = await productsPage.resetAndGetNumberOfLines(page);
       await expect(numberOfProductsAfterReset).to.equal(numberOfProducts);
     });
   });
