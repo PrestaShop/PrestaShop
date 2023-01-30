43276d63be09b716b5fde1e900a3c42924d8e812
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2023-01-30 17:00:38 +0100

    Add new test to enable/disable demo mode

--- tests/UI/pages/BO/dashboard/index.ts
+++ tests/UI/pages/BO/dashboard/index.ts
@@ -1,4 +1,5 @@
 import BOBasePage from '@pages/BO/BObasePage';
+import {Page} from 'playwright';
 
 /**
  * Dashboard page, contains selectors and functions for the page
@@ -8,6 +9,12 @@ import BOBasePage from '@pages/BO/BObasePage';
 class Dashboard extends BOBasePage {
   public readonly pageTitle: string;
 
+  private readonly demoModeButton: string;
+
+  private readonly demoModeToggle: (toEnable: string) => string;
+
+  private readonly salesScore: string;
+
   /**
    * @constructs
    * Setting up titles and selectors to use on dashboard page
@@ -16,6 +23,38 @@ class Dashboard extends BOBasePage {
     super();
 
     this.pageTitle = 'Dashboard • ';
+
+    // selectors
+    this.demoModeButton = '#page-header-desc-configuration-switch_demo';
+    this.demoModeToggle = (toEnable: string) => `.process-icon-toggle-${toEnable}.switch_demo`;
+    this.salesScore = '#sales_score';
+  }
+
+  /* Methods */
+
+  /**
+   * Set demo mode
+   * @param page {Page} Browser tab
+   * @param toEnable {boolean} True if we need to enable demo mode, false if not
+   */
+  async setDemoMode(page: Page, toEnable: boolean): Promise<void> {
+    const isDemoModeOn = await this.elementVisible(page, this.demoModeToggle('on'), 2000);
+
+    if ((toEnable && !isDemoModeOn) || (!toEnable && isDemoModeOn)) {
+      await this.waitForSelectorAndClick(page, this.demoModeButton);
+      await page.waitForTimeout(2000);
+    }
+  }
+
+  /**
+   * Get sales score
+   * @param page {Page} Browser tab
+   */
+  async getSalesScore(page: Page): Promise<number> {
+    const text = await this.getTextContent(page, this.salesScore);
+    const salesScore = text.match(/\d+(\.\d+)?/g).toString().replace(',', '');
+
+    return (parseFloat(salesScore));
   }
 }
 
