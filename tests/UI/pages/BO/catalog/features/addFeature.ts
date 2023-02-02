919895e1d32dfdd900c58ca8300939dcfcae4183
Progi1984 <franck.lefevre@prestashop.com>
2023-02-02 11:26:32 +0100

    Functional Tests : Migration TS (`@pages/BO/catalog/features/addFeature`)

--- tests/UI/pages/BO/catalog/features/addFeature.ts
+++ tests/UI/pages/BO/catalog/features/addFeature.ts
@@ -1,5 +1,8 @@
-require('module-alias/register');
-const BOBasePage = require('@pages/BO/BObasePage');
+import BOBasePage from '@pages/BO/BObasePage';
+
+import type FeatureData from '@data/faker/feature';
+
+import type {Page} from 'playwright';
 
 /**
  * Add feature page, contains functions that can be used on add feature page
@@ -7,6 +10,18 @@ const BOBasePage = require('@pages/BO/BObasePage');
  * @extends BOBasePage
  */
 class AddFeature extends BOBasePage {
+  public readonly createPageTitle: string;
+
+  private readonly nameInput: string;
+
+  private readonly urlInput: string;
+
+  private readonly metaTitleInput: string;
+
+  private readonly indexableToggle: (toggle: string) => string;
+
+  private readonly saveButton: string;
+
   /**
    * @constructs
    * Setting up texts and selectors to use on add feature page
@@ -22,7 +37,7 @@ class AddFeature extends BOBasePage {
     this.nameInput = '#name_1';
     this.urlInput = 'input[name=\'url_name_1\']';
     this.metaTitleInput = 'input[name=\'meta_title_1\']';
-    this.indexableToggle = (toggle) => `#indexable_${toggle}`;
+    this.indexableToggle = (toggle: string) => `#indexable_${toggle}`;
     this.saveButton = '#feature_form_submit_btn';
   }
 
@@ -32,7 +47,7 @@ class AddFeature extends BOBasePage {
    * @param featureData {FeatureData} Values to set on add feature form inputs
    * @return {Promise<string>}
    */
-  async setFeature(page, featureData) {
+  async setFeature(page: Page, featureData: FeatureData): Promise<string> {
     // Set name
     await this.setValue(page, this.nameInput, featureData.name);
 
@@ -51,4 +66,4 @@ class AddFeature extends BOBasePage {
   }
 }
 
-module.exports = new AddFeature();
+export default new AddFeature();
