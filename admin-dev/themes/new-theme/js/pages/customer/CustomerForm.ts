d35ad90cf72440d18dc5a2067ea10de1660e31a3
Hlavtox <daniel.hlavacek@hotmail.cz>
2023-02-12 19:46:17 +0100

    Improve group selector

--- admin-dev/themes/new-theme/js/pages/customer/CustomerForm.ts
+++ admin-dev/themes/new-theme/js/pages/customer/CustomerForm.ts
@@ -31,9 +31,55 @@ import ChangePasswordHandler from '../../components/change-password-handler';
  */
 export default class CustomerForm {
   constructor() {
+    // Watch password field change and strength indicator
     const passwordHandler = new ChangePasswordHandler(
       customerFormMap.passwordStrengthFeedbackContainer,
     );
     passwordHandler.watchPasswordStrength($(customerFormMap.passwordInput));
+
+    // Watch customer group checkbox change and if it was unchecked, 
+    // update default group below if it's no longer in the list.
+    $('input[type="checkbox"][name="customer[group_ids][]"]').on('change', (event) => {
+      this.checkOrUpdateDefaultGroup($(event.currentTarget).is(':checked'));
+    });
+  }
+
+  private checkOrUpdateDefaultGroup(wasChecked: boolean): void {
+    // Get currently selected group ID
+    const currentDefaultGroup = parseInt($("#customer_default_group_id option:selected").val() as string);
+
+    // Get all checked groups in group access
+    const checkedGroups: number[] = [];
+    let firstGroupInList: number = 0;
+    $('input[type="checkbox"][name="customer[group_ids][]"]').each((index, value) => {
+      // We will keep track of all checked groups
+      if ($(value).is(":checked")) {
+        checkedGroups.push(parseInt($(value).val() as string))
+      }
+      // And store ID of the first group regardless of it's status
+      if (index === 0) {
+        firstGroupInList = parseInt($(value).val() as string);
+      }
+    });
+
+    // If no groups are selected, use a the first group in the list, no matter 
+    // if it's selected or not.
+    if (!checkedGroups.length) {
+      $("#customer_default_group_id").val(firstGroupInList).trigger('change');
+      return;
+    }
+
+    // If the last change was a newly added group and it's the only one in the list,
+    // we will set it as the default group.
+    if (wasChecked && checkedGroups.length === 1) {
+      $("#customer_default_group_id").val(checkedGroups[0]).trigger('change');
+      return;
+    }
+
+    // If the default group is not in the list anymore, select the first checked group.
+    if (!checkedGroups.includes(currentDefaultGroup)) {
+      $("#customer_default_group_id").val(checkedGroups[0]).trigger('change');
+      return;
+    }
   }
 }
