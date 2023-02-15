4ba9cc7dadf961e02956151a89bd9837fef16be9
Julius Zukauskas <julius.zukauskas@invertus.eu>
2023-02-15 15:23:09 +0200

    add handling visuals (class and label change) depending on stock

--- admin-dev/themes/new-theme/js/pages/product/edit/summary-quantity-updater.ts
+++ admin-dev/themes/new-theme/js/pages/product/edit/summary-quantity-updater.ts
@@ -35,18 +35,67 @@ export default class SummaryQuantityUpdater {
 
   shopId: number;
 
+  containerSelector: string;
+
   constructor(eventEmitter: typeof EventEmitter, productId: number, shopId: number) {
     this.eventEmitter = eventEmitter;
     this.productId = productId;
     this.shopId = shopId;
+    this.containerSelector = ProductMap.summaryTotalQuantityContainer;
 
     this.eventEmitter.on(ProductEventMap.combinations.refreshPage, () => this.refreshQuantity());
   }
 
   refreshQuantity(): void {
     getProductQuantity(this.productId, this.shopId).then((response: QuantityResult) => {
-      const totalQuantityElement = <HTMLElement> document.querySelector(ProductMap.summaryTotalQuantity);
-      totalQuantityElement.textContent = String(response.quantity);
+      const totalQuantityElement = this.getTotalQuantityElement();
+      const quantity = Number(response.quantity);
+      totalQuantityElement.textContent = String(quantity);
+
+      this.refreshAppearance(quantity);
     });
+  }
+
+  /**
+   * Refreshes the wording in related label and the color of the quantity wrapper.
+   *
+   * @param {number} quantity
+   *
+   * @private
+   */
+  private refreshAppearance(quantity: number) {
+    const container = <HTMLElement> document.querySelector(this.containerSelector);
+    const lowStockThreshold = Number(container.dataset.lowStockThreshold);
+
+    // one of these classes will be used to show correct color depending on stock level
+    const alertClassNames: string[] = ['success', 'warning', 'danger'];
+    let alertClass: string = 'success';
+    let label: string = <string> container.dataset.inStockLabel;
+
+    // determine class and label depending on quantity
+    if (quantity <= 0) {
+      alertClass = 'danger';
+      label = <string> container.dataset.outOfStockLabel;
+    } else if (lowStockThreshold > 0 && quantity <= lowStockThreshold) {
+      alertClass = 'warning';
+      label = <string> container.dataset.lowStockStockLabel;
+    }
+
+    const totalQuantityLabel = <HTMLElement> container.querySelector(ProductMap.summaryTotalQuantityLabel);
+    const totalQuantityElement = this.getTotalQuantityElement();
+    totalQuantityLabel.textContent = label;
+
+    // replace alert class if needed
+    alertClassNames.forEach((className: string) => {
+      if (className === alertClass) {
+        totalQuantityElement.classList.add(className);
+      } else {
+        totalQuantityElement.classList.remove(className);
+      }
+    });
+  }
+
+  private getTotalQuantityElement(): HTMLElement {
+    return <HTMLElement> document.querySelector(ProductMap.summaryTotalQuantity);
   }
 }
