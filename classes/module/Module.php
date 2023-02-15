7955d0e2e5fa9266a845155582dc231a7b146131
matthieu-rolland <matthieu.rolland@prestashop.com>
2023-02-15 14:33:40 +0100

    take shop id into account when disabling module

--- classes/module/Module.php
+++ classes/module/Module.php
@@ -984,7 +984,8 @@ abstract class ModuleCore implements ModuleInterface
         }
 
         // Disable module for all shops
-        $result &= Db::getInstance()->delete('module_shop', '`id_module` = ' . (int) $this->id);
+        $whereIdShop = $force_all ? '' : ' AND `id_shop` IN(' . implode(', ', Shop::getContextListShopID()) . ')';
+        $result &= Db::getInstance()->delete('module_shop', '`id_module` = ' . (int) $this->id . $whereIdShop);
 
         // if module has no more shop associations, set module.active = 0
         if (!$this->hasShopAssociations()) {
