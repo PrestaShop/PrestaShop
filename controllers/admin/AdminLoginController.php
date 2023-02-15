08970a88d1c453d621da90ab575641fc3ba175cc
Maxime Flasquin <maxime.flasquin@gmail.com>
2023-02-15 11:37:59 +0100

    Fix delete token before generate redirect route

--- controllers/admin/AdminLoginController.php
+++ controllers/admin/AdminLoginController.php
@@ -267,6 +267,8 @@ class AdminLoginControllerCore extends AdminController
 
                 $cookie->write();
 
+                $this->get(SessionRenewer::class)->renew();
+
                 // If there is a valid controller name submitted, redirect to it
                 if (isset($_POST['redirect']) && Validate::isControllerName($_POST['redirect'])) {
                     $url = $this->context->link->getAdminLink($_POST['redirect']);
@@ -274,8 +276,6 @@ class AdminLoginControllerCore extends AdminController
                     $tab = new Tab((int) $this->context->employee->default_tab);
                     $url = $this->context->link->getAdminLink($tab->class_name);
                 }
-
-                $this->get(SessionRenewer::class)->renew();
 
                 Hook::exec(
                     'actionAdminLoginControllerLoginAfter',
