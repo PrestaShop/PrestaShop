93cf97eb9960cd185571ad97c35dc10091f8fbf5
Nicolas Lœuillet <nicolas.loeuillet@prestashop.com>
2023-02-21 10:36:40 +0100

    Improved tests

--- tests/Resources/TestCase/ExtendedTestCaseMethodsTrait.php
+++ tests/Resources/TestCase/ExtendedTestCaseMethodsTrait.php
@@ -6,15 +6,33 @@ use PHPUnit\Framework\Assert;
 
 trait ExtendedTestCaseMethodsTrait
 {
-    public function assertEqualsWithEpsilon($expected, $actual, $message = '')
+    private function compareWithEpsilon($expected, $actual)
     {
         $success = false;
 
-        // @see https://github.com/sebastianbergmann/phpunit/issues/4966#issuecomment-1367081755 for `0.0000000001`
         if (abs($expected - $actual) < 0.0000000001) {
             $success = true;
         }
 
         Assert::assertTrue($success);
+    }
+
+    public function assertEqualsWithEpsilon($expected, $actual, $message = '')
+    {
+        if (!is_array($expected)) {
+            $expectedArray[] = $expected;
+        } else {
+            $expectedArray = $expected;
+        }
+
+        if (!is_array($actual)) {
+            $actualArray[] = $actual;
+        } else {
+            $actualArray = $actual;
+        }
+
+        foreach ($expectedArray as $key => $item) {
+            $this->compareWithEpsilon($item, $actualArray[$key]);
+        }
     }
 }
