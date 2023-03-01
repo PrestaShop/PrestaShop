c45b083475e3f55175721b17c48fe1f17b5323c2
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2023-03-01 12:20:46 +0100

    Improve adaptation of footer with Symfony debug toolbar, more dynamic based on the toolbar state

--- admin-dev/themes/new-theme/js/theme.ts
+++ admin-dev/themes/new-theme/js/theme.ts
@@ -54,6 +54,7 @@ import initInvalidFields from '@js/app/utils/fields';
 import initEmailFields from '@js/app/utils/email-idn';
 import initNumberCommaTransformer from '@js/app/utils/number-comma-transformer';
 import initPrestashopComponents from '@app/utils/init-components';
+import watchSymfonyDebugBar from '@app/utils/watch-symfony-debug-bar';
 import '@js/components/header/search-form';
 
 const {$} = window;
@@ -70,4 +71,5 @@ $(() => {
   initInvalidFields();
   initEmailFields('input[type="email"]');
   initNumberCommaTransformer('.js-comma-transformer');
+  watchSymfonyDebugBar();
 });
