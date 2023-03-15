fbbe83a7adfc1cbee4a07012198db9b8af619922
Progi1984 <franck.lefevre@prestashop.com>
2023-03-15 14:39:58 +0100

    Functional Tests : Fixed BO/05*/01*/01* test

--- tests/UI/campaigns/functional/BO/05_customerService/01_customerService/01_viewCustomerService.ts
+++ tests/UI/campaigns/functional/BO/05_customerService/01_customerService/01_viewCustomerService.ts
@@ -121,7 +121,7 @@ describe('BO - Customer Service : View messages', async () => {
     const text = await viewPage.getCustomerMessage(page);
     expect(text).to.contains(contactUsData.emailAddress);
     expect(text).to.contains(contactUsData.subject);
-    expect(text).to.contains(`${messageDateTime.substr(0, 10)} access_time - ${messageDateTime.substr(11, 5)}`);
+    expect(text).to.contains(`${messageDateTime.substr(0, 10)} - ${messageDateTime.substr(11, 5)}`);
     expect(text).to.contains('Attachment');
     expect(text).to.contains(contactUsData.message);
   });
@@ -143,7 +143,7 @@ describe('BO - Customer Service : View messages', async () => {
 
     const text = await viewPage.getOrdersAndMessagesTimeline(page);
     expect(text).to.contains('Orders and messages timeline');
-    expect(text).to.contains(`${messageDateTime.substr(0, 10)} access_time ${messageDateTime.substr(11, 5)}`);
+    expect(text).to.contains(`${messageDateTime.substr(0, 10)} - ${messageDateTime.substr(11, 5)}`);
     expect(text).to.contains(`Message to: ${contactUsData.subject}`);
     expect(text).to.contains(contactUsData.message);
   });
