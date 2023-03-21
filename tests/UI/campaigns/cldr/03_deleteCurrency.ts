fdd80a2fa7ba99cc66df36b7e96d4f3bbd7c6395
Progi1984 <franck.lefevre@prestashop.com>
2023-03-21 17:25:03 +0100

    Functional Tests : CLDR - Delete a currency

---
+++ tests/UI/campaigns/cldr/03_deleteCurrency.ts
@@ -0,0 +1,226 @@
+// Import utils
+import helper from '@utils/helpers';
+import testContext from '@utils/testContext';
+
+// Import commonTests
+import loginCommon from '@commonTests/BO/loginBO';
+
+// Import pages
+import dashboardPage from '@pages/BO/dashboard';
+import localizationPage from '@pages/BO/international/localization';
+import currenciesPage from '@pages/BO/international/currencies';
+import addCurrencyPage from '@pages/BO/international/currencies/add';
+
+// Import data
+import Currencies from '@data/demo/currencies';
+import CurrencyData from '@data/faker/currency';
+
+import {use, expect} from 'chai';
+import chaiString from 'chai-string';
+import type {BrowserContext, Page} from 'playwright';
+
+use(chaiString);
+
+const baseContext: string = 'cldr_deleteCurrency';
+
+describe('CLDR : Edit symbol / format currency', async () => {
+  let browserContext: BrowserContext;
+  let page: Page;
+  let numberOfCurrencies: number;
+
+  const currencies: CurrencyData[] = [
+    Currencies.gbp,
+    Currencies.jpy,
+    Currencies.usd,
+  ];
+
+  before(async function () {
+    browserContext = await helper.createBrowserContext(this.browser);
+    page = await helper.newTab(browserContext);
+  });
+
+  after(async () => {
+    await helper.closeBrowserContext(browserContext);
+  });
+
+  it('should login in BO', async function () {
+    await loginCommon.loginBO(this, page);
+  });
+
+  it('should go to \'International > Localization\' page', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'goToLocalizationPage', baseContext);
+
+    await dashboardPage.goToSubMenu(
+      page,
+      dashboardPage.internationalParentLink,
+      dashboardPage.localizationLink,
+    );
+    await localizationPage.closeSfToolBar(page);
+
+    const pageTitle = await localizationPage.getPageTitle(page);
+    await expect(pageTitle).to.contains(localizationPage.pageTitle);
+  });
+
+  it('should go to Currencies Tab', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'goToCurrenciesTab0', baseContext);
+
+    await localizationPage.goToSubTabCurrencies(page);
+
+    const pageTitle = await currenciesPage.getPageTitle(page);
+    await expect(pageTitle).to.contains(currenciesPage.pageTitle);
+  });
+
+  it('should reset all filters', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'resetFilter', baseContext);
+
+    numberOfCurrencies = await currenciesPage.resetAndGetNumberOfLines(page);
+    await expect(numberOfCurrencies).to.be.above(0);
+  });
+
+  currencies.forEach((currency: CurrencyData, index : number) => {
+    it('should go to create new currency page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', `goToAddNewCurrencyPage${currency.isoCode}`, baseContext);
+
+      await currenciesPage.goToAddNewCurrencyPage(page);
+
+      const pageTitle = await addCurrencyPage.getPageTitle(page);
+      await expect(pageTitle).to.contains(addCurrencyPage.pageTitle);
+    });
+
+    it(`should create the currency ${currency.isoCode}`, async function () {
+      await testContext.addContextItem(this, 'testIdentifier', `createOfficialCurrency${currency.isoCode}`, baseContext);
+
+      // Create and check successful message
+      const textResult = await addCurrencyPage.addOfficialCurrency(page, currency);
+      await expect(textResult).to.contains(currenciesPage.successfulCreationMessage);
+
+      // Check number of currencies after creation
+      const numberOfCurrenciesAfterCreation = await currenciesPage.getNumberOfElementInGrid(page);
+      await expect(numberOfCurrenciesAfterCreation).to.be.equal(numberOfCurrencies + index + 1);
+    });
+  });
+
+  it('should go to Localization Tab', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'goToLocalizationTab0', baseContext);
+
+    await localizationPage.goToSubTabLocalizations(page);
+
+    const pageTitle = await localizationPage.getPageTitle(page);
+    await expect(pageTitle).to.contains(localizationPage.pageTitle);
+  });
+
+  it(`should choose '${Currencies.usd.isoCode}' as default currency`, async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'setUSDAsDefaultCurrency', baseContext);
+
+    const textResult = await localizationPage.setDefaultCurrency(page, `${Currencies.usd.name} (${Currencies.usd.isoCode})`);
+    await expect(textResult).to.contains(localizationPage.successfulSettingsUpdateMessage);
+  });
+
+  it('should go to Currencies Tab', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'goToCurrenciesTab1', baseContext);
+
+    await localizationPage.goToSubTabCurrencies(page);
+
+    const pageTitle = await currenciesPage.getPageTitle(page);
+    await expect(pageTitle).to.contains(currenciesPage.pageTitle);
+  });
+
+  it(`should filter by iso code of currency '${Currencies.usd.isoCode}'`, async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'filterToUSDCurrency', baseContext);
+
+    // Filter
+    await currenciesPage.filterTable(page, 'input', 'iso_code', Currencies.usd.isoCode);
+
+    // Check number of currencies
+    const numberOfCurrenciesAfterFilter = await currenciesPage.getNumberOfElementInGrid(page);
+    await expect(numberOfCurrenciesAfterFilter).to.be.equal(1);
+
+    // Check currency
+    const textColumn = await currenciesPage.getTextColumnFromTableCurrency(page, 1, 'iso_code');
+    await expect(textColumn).to.contains(Currencies.usd.isoCode);
+  });
+
+  it('should delete currency', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'deleteCurrencyUSD', baseContext);
+
+    const result = await currenciesPage.deleteCurrency(page, 1);
+    await expect(result).to.be.equal(currenciesPage.cannotDeleteDefaultCurrencyMessage);
+  });
+
+  it('should go to Localization Tab', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'goToLocalizationTab1', baseContext);
+
+    await localizationPage.goToSubTabLocalizations(page);
+
+    const pageTitle = await localizationPage.getPageTitle(page);
+    await expect(pageTitle).to.contains(localizationPage.pageTitle);
+  });
+
+  it(`should choose '${Currencies.euro.isoCode}' as default currency`, async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'setEURAsDefaultCurrency', baseContext);
+
+    const textResult = await localizationPage.setDefaultCurrency(page, `${Currencies.euro.name} (${Currencies.euro.isoCode})`);
+    await expect(textResult).to.contains(localizationPage.successfulSettingsUpdateMessage);
+  });
+
+  it('should go to Currencies Tab', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'goToCurrenciesTab1', baseContext);
+
+    await localizationPage.goToSubTabCurrencies(page);
+
+    const pageTitle = await currenciesPage.getPageTitle(page);
+    await expect(pageTitle).to.contains(currenciesPage.pageTitle);
+  });
+
+  it(`should filter by iso code of currency '${Currencies.usd.isoCode}'`, async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'filterToUSDCurrency', baseContext);
+
+    // Filter
+    await currenciesPage.filterTable(page, 'input', 'iso_code', Currencies.usd.isoCode);
+
+    // Check number of currencies
+    const numberOfCurrenciesAfterFilter = await currenciesPage.getNumberOfElementInGrid(page);
+    await expect(numberOfCurrenciesAfterFilter).to.be.equal(1);
+
+    // Check currency
+    const textColumn = await currenciesPage.getTextColumnFromTableCurrency(page, 1, 'iso_code');
+    await expect(textColumn).to.contains(Currencies.usd.isoCode);
+  });
+
+  it('should delete currency', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'deleteCurrencyUSD', baseContext);
+
+    const result = await currenciesPage.deleteCurrency(page, 1);
+    await expect(result).to.be.equal(currenciesPage.successfulDeleteMessage);
+  });
+
+  it(`should select rows except '${Currencies.euro.isoCode}'`, async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'resetAndSelectRowsExceptEUR', baseContext);
+
+    const numberOfCurrenciesAfterDelete = await currenciesPage.resetAndGetNumberOfLines(page);
+    await expect(numberOfCurrenciesAfterDelete).to.be.eq(numberOfCurrencies + currencies.length - 1);
+
+    const isBulkActionsEnabledBeforeSelect = await currenciesPage.isBulkActionsEnabled(page);
+    await expect(isBulkActionsEnabledBeforeSelect).to.be.false;
+
+    for (let numRow = 1; numRow <= numberOfCurrenciesAfterDelete; numRow++) {
+      const textColumn = await currenciesPage.getTextColumnFromTableCurrency(page, numRow, 'iso_code');
+      if (textColumn !== Currencies.euro.isoCode) {
+        await currenciesPage.selectRow(page, numRow);
+      }
+    }
+
+    const isBulkActionsEnabledAfterSelect = await currenciesPage.isBulkActionsEnabled(page);
+    await expect(isBulkActionsEnabledAfterSelect).to.be.true;
+  });
+
+  it('should bulk delete currencies', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'bulkDeleteCurrencies', baseContext);
+
+    const result = await currenciesPage.bulkDeleteCurrencies(page);
+    expect(result).to.be.eq(currenciesPage.successfulMultiDeleteMessage);
+
+    const numberOfCurrenciesAfterBulkDeete = await currenciesPage.getNumberOfElementInGrid(page);
+    await expect(numberOfCurrenciesAfterBulkDeete).to.be.equal(numberOfCurrencies);
+  });
+});
