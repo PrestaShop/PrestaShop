03688ba332831f1ddd1e3a9bac50a4900cc6eb89
boherm <boris.hermans@prestashop.com>
2023-03-27 10:54:25 +0200

    Add new feature flag to enable/disable new Carts page in bo

--- src/Core/FeatureFlag/FeatureFlagSettings.php
+++ src/Core/FeatureFlag/FeatureFlagSettings.php
@@ -53,4 +53,5 @@ class FeatureFlagSettings
     public const FEATURE_FLAG_AUTHORIZATION_SERVER = 'authorization_server';
     public const FEATURE_FLAG_SYMFONY_LAYOUT = 'symfony_layout';
     public const FEATURE_FLAG_FRONT_CONTAINER_V2 = 'front_container_v2';
+    public const FEATURE_FLAG_CARTS_INDEX = 'carts_index';
 }
