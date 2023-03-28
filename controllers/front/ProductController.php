ac887d848649afced101f7652972eede548532b6
Nizel Shirogane Lynee <104589134+sudo-do@users.noreply.github.com>
2023-03-28 16:56:13 +0300

    Update ProductController.php

--- controllers/front/ProductController.php
+++ controllers/front/ProductController.php
@@ -513,9 +513,6 @@ class ProductControllerCore extends ProductPresentingFrontControllerCore
         header('Content-Type: application/json');
         $this->ajaxRender(json_encode([
             'product_prices' => $this->render('catalog/_partials/product-prices'),
-            'product_price_raw' => $product['rounded_display_price'],
-            'product_price_tax_exc_raw' => $product['price_tax_exc'],
-            'product_cover_raw' => $product['default_image']['large']['url'],
             'product_cover_thumbnails' => $this->render('catalog/_partials/product-cover-thumbnails'),
             'product_customization' => $this->render(
                 'catalog/_partials/product-customization',
@@ -549,6 +546,7 @@ class ProductControllerCore extends ProductPresentingFrontControllerCore
             'id_customization' => $product['id_customization'],
             'product_title' => $this->getTemplateVarPage()['meta']['title'],
             'is_quick_view' => $this->isQuickView(),
+            'product_page_head' => $this->render('_partials/head.tpl'),
         ]));
     }
 
