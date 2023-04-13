8ffc962ad9d3dcddcaf2cc333469a7df674e1f39
Jonathan Lelievre <jonathan.lelievre@prestashop.com>
2023-04-13 12:18:55 +0200

    Refacto CurrencySymbolUpdater so that getSymbol is reusable and we do not need to instanciate it several times

--- admin-dev/themes/new-theme/js/components/form/price-reduction-manager.ts
+++ admin-dev/themes/new-theme/js/components/form/price-reduction-manager.ts
@@ -40,6 +40,8 @@ export default class PriceReductionManager {
 
   private readonly reductionValueSymbolSelector: string;
 
+  private readonly currencySymbolUpdater: CurrencySymbolUpdater;
+
   constructor(
     reductionTypeSelector: string,
     taxInclusionInputs: string,
@@ -51,6 +53,17 @@ export default class PriceReductionManager {
     this.$taxInclusionInputs = $(taxInclusionInputs);
     this.currencySelect = currencySelect;
     this.reductionValueSymbolSelector = reductionValueSymbolSelector;
+    this.currencySymbolUpdater = new CurrencySymbolUpdater(
+      this.currencySelect,
+      ((symbol: string): void => {
+        if (symbol === '') {
+          return;
+        }
+
+        this.updateSymbol(symbol);
+      }),
+    );
+
     this.handle();
     this.$reductionTypeSelect.on('change', () => this.handle());
   }
@@ -59,51 +72,42 @@ export default class PriceReductionManager {
    * When source value is 'percentage', target field is shown, else hidden
    */
   private handle(): void {
-    this.initCurrencySymbolUpdater();
-
     if (this.$reductionTypeSelect.val() === 'percentage') {
       this.$taxInclusionInputs.fadeOut();
     } else {
       this.$taxInclusionInputs.fadeIn();
     }
+
+    this.updateSymbol(this.currencySymbolUpdater.getSymbol());
   }
 
-  private initCurrencySymbolUpdater(): void {
-    new CurrencySymbolUpdater(
-      this.currencySelect,
-      ((symbol: string): void => {
-        if (symbol === '') {
-          return;
+  private updateSymbol(symbol: string): void {
+    const reductionTypeSelect = <HTMLSelectElement> document.querySelector(this.reductionTypeSelector);
+
+    if (reductionTypeSelect) {
+      for (let i = 0; i < reductionTypeSelect.options.length; i += 1) {
+        const reductionOption = reductionTypeSelect.options[i];
+
+        if (reductionOption.value === 'amount') {
+          // Update reduction type choice "amount" symbol
+          reductionOption.innerHTML = symbol;
         }
+      }
 
-        const reductionTypeSelect = <HTMLSelectElement> document.querySelector(this.reductionTypeSelector);
+      const selectedReduction = <string> reductionTypeSelect.options[reductionTypeSelect.selectedIndex].value;
+      const reductionValueSymbols = <NodeListOf<HTMLSelectElement>> document.querySelectorAll(
+        this.reductionValueSymbolSelector,
+      );
 
-        if (reductionTypeSelect) {
-          for (let i = 0; i < reductionTypeSelect.options.length; i += 1) {
-            const reductionOption = reductionTypeSelect.options[i];
+      if (reductionValueSymbols.length === 0) {
+        return;
+      }
 
-            if (reductionOption.value === 'amount') {
-              // Update reduction type choice "amount" symbol
-              reductionOption.innerHTML = symbol;
-            }
-          }
-
-          const selectedReduction = <string> reductionTypeSelect.options[reductionTypeSelect.selectedIndex].value;
-          const reductionValueSymbols = <NodeListOf<HTMLSelectElement>> document.querySelectorAll(
-            this.reductionValueSymbolSelector,
-          );
-
-          if (reductionValueSymbols.length === 0) {
-            return;
-          }
-
-          // Update reduction value field symbol when "amount" type is selected
-          reductionValueSymbols.forEach((value: Element) => {
-            // eslint-disable-next-line no-param-reassign
-            value.innerHTML = selectedReduction === 'amount' ? symbol : '%';
-          });
-        }
-      }),
-    );
+      // Update reduction value field symbol when "amount" type is selected
+      reductionValueSymbols.forEach((value: Element) => {
+        // eslint-disable-next-line no-param-reassign
+        value.innerHTML = selectedReduction === 'amount' ? symbol : '%';
+      });
+    }
   }
 }
