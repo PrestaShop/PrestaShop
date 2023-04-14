6b7fbc7c7a2cff300ad51676f0e6ed1b3c9fb293
Progi1984 <franck.lefevre@prestashop.com>
2023-04-14 13:47:56 +0200

    Fixes PHP Lint errors

--- classes/Tools.php
+++ classes/Tools.php
@@ -36,7 +36,6 @@ use PrestaShop\PrestaShop\Core\Localization\Locale\Repository as LocaleRepositor
 use PrestaShop\PrestaShop\Core\Security\Hashing;
 use PrestaShop\PrestaShop\Core\Security\OpenSsl\OpenSSL;
 use PrestaShop\PrestaShop\Core\Security\PasswordGenerator;
-use PrestaShop\PrestaShop\Core\Util\ColorBrightnessCalculator;
 use PrestaShop\PrestaShop\Core\Util\String\StringModifier;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Request;
