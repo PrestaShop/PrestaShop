bf9e899dba5b5568db1f266626f5bd846d256607
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2023-05-10 09:53:33 +0200

    Fix lint errors

--- tests/UI/campaigns/functional/BO/07_modules/01_moduleManager/10_seeMore.ts
+++ tests/UI/campaigns/functional/BO/07_modules/01_moduleManager/10_seeMore.ts
@@ -7,11 +7,7 @@ import loginCommon from '@commonTests/BO/loginBO';
 
 // Import pages
 import dashboardPage from '@pages/BO/dashboard';
-import {moduleConfigurationPage} from '@pages/BO/modules/moduleConfiguration';
 import moduleManagerPage from '@pages/BO/modules/moduleManager';
-
-// Import data
-import Modules from '@data/demo/modules';
 
 import {expect} from 'chai';
 import type {BrowserContext, Page} from 'playwright';
