5e78aea5f67a80829684881ada7383eaba66d638
Darius Aleksiunas <darius.aleksiunas@invertus.eu>
2023-05-16 15:43:32 +0300

    implement bulk alias status handler

---
+++ src/Adapter/Alias/CommandHandler/BulkUpdateAliasStatusHandler.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShop\PrestaShop\Adapter\Alias\CommandHandler;
+
+use PrestaShop\PrestaShop\Adapter\Alias\Repository\AliasRepository;
+use PrestaShop\PrestaShop\Core\Domain\AbstractBulkCommandHandler;
+use PrestaShop\PrestaShop\Core\Domain\Alias\Command\BulkUpdateAliasStatusCommand;
+use PrestaShop\PrestaShop\Core\Domain\Alias\CommandHandler\BulkUpdateAliasStatusHandlerInterface;
+use PrestaShop\PrestaShop\Core\Domain\Alias\Exception\AliasException;
+use PrestaShop\PrestaShop\Core\Domain\Alias\Exception\CannotUpdateAliasException;
+use PrestaShop\PrestaShop\Core\Domain\Alias\ValueObject\AliasId;
+use PrestaShop\PrestaShop\Core\Domain\Exception\BulkCommandExceptionInterface;
+use PrestaShop\PrestaShop\Core\Domain\Alias\Exception\BulkAliasException;
+use PrestaShop\PrestaShop\Core\Domain\Feature\Exception\BulkFeatureException;
+
+class BulkUpdateAliasStatusHandler extends AbstractBulkCommandHandler implements BulkUpdateAliasStatusHandlerInterface
+{
+    /**
+     * @var AliasRepository
+     */
+    private $aliasRepository;
+
+    /**
+     * @var bool|null
+     */
+    private $enabled;
+
+    /**
+     * @param AliasRepository $aliasRepository
+     */
+    public function __construct(AliasRepository $aliasRepository)
+    {
+        $this->aliasRepository = $aliasRepository;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function handle(BulkUpdateAliasStatusCommand $command): void
+    {
+        $this->enabled = $command->isEnabled();
+        $this->handleBulkAction($command->getAliasIds(), AliasException::class);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function buildBulkException(array $caughtExceptions): BulkCommandExceptionInterface
+    {
+        return new BulkAliasException(
+            $caughtExceptions,
+            'Errors occurred during Alias bulk delete action',
+            BulkFeatureException::FAILED_BULK_DELETE
+        );
+    }
+
+    /**
+     * @param AliasId $id
+     * @return void
+     */
+    protected function handleSingleAction($id): void
+    {
+        $alias = $this->aliasRepository->get($id);
+        $alias->active = $this->enabled;
+        $this->aliasRepository->partialUpdate($alias, ['active'], CannotUpdateAliasException::class);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function supports($id): bool
+    {
+        return $id instanceof AliasId;
+    }
+}
