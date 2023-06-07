4bea9c613a9c4912189b896eb7e2870afc8e9feb
Lorenz Meyer <lm@bible.ch>
2023-06-07 14:02:35 +0200

    Caching locales
    Number formatting for a locale may only change when upgrading PrestaShop.
    Locales are stored in CLDR files and should be immutable.

--- src/Core/Localization/Locale/Repository.php
+++ src/Core/Localization/Locale/Repository.php
@@ -26,6 +26,7 @@
 
 namespace PrestaShop\PrestaShop\Core\Localization\Locale;
 
+use SymfonyCache;
 use PrestaShop\Decimal\Operation\Rounding;
 use PrestaShop\PrestaShop\Core\Localization\CLDR\LocaleRepository as CldrLocaleRepository;
 use PrestaShop\PrestaShop\Core\Localization\Currency\RepositoryInterface as CurrencyRepositoryInterface;
@@ -36,6 +37,7 @@ use PrestaShop\PrestaShop\Core\Localization\Specification\Factory as Specificati
 use PrestaShop\PrestaShop\Core\Localization\Specification\Number as NumberSpecification;
 use PrestaShop\PrestaShop\Core\Localization\Specification\NumberCollection as PriceSpecificationMap;
 use PrestaShop\PrestaShop\Core\Localization\Specification\Price as PriceSpecification;
+use Symfony\Contracts\Cache\ItemInterface;
 
 /**
  * Locale repository.
@@ -143,12 +145,14 @@ class Repository implements RepositoryInterface
     public function getLocale($localeCode)
     {
         if (!isset($this->locales[$localeCode])) {
-            $this->locales[$localeCode] = new Locale(
-                $localeCode,
-                $this->getNumberSpecification($localeCode),
-                $this->getPriceSpecifications($localeCode),
-                new NumberFormatter($this->roundingMode, $this->numberingSystem)
-            );
+            $this->locales[$localeCode] = SymfonyCache::getInstance()->get('locale_'. $localeCode, function (ItemInterface $item) use ($localeCode) {
+                return new Locale(
+                    $localeCode,
+                    $this->getNumberSpecification($localeCode),
+                    $this->getPriceSpecifications($localeCode),
+                    new NumberFormatter($this->roundingMode, $this->numberingSystem)
+                );
+            });
         }
 
         return $this->locales[$localeCode];
