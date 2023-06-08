5fa88c839e9e49cde42c4a64a99b256b84f67f4f
Lorenz Meyer <lm@bible.ch>
2023-06-08 08:15:12 +0200

    Use a ChainAdapter

--- classes/cache/SymfonyCache.php
+++ classes/cache/SymfonyCache.php
@@ -1,5 +1,7 @@
 <?php
 
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
+use Symfony\Component\Cache\Adapter\ChainAdapter;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 use Symfony\Component\Cache\Adapter\MemcachedAdapter;
 use Symfony\Component\Cache\Adapter\RedisAdapter;
@@ -12,6 +14,10 @@ class SymfonyCacheCore
      * Use a DSN as defined here https://symfony.com/doc/4.4/components/cache/adapters/memcached_adapter.html#configure-the-connection
      * and here https://symfony.com/doc/4.4/components/cache/adapters/redis_adapter.html#configure-the-connection
      *
+     * If more than one Prestashop instance uses the same cache mechanism, the configuration must differ:
+     * - for memcached, the DSN must include a key prefix (e.g. `memcached://localhost?prefix_key=my_key`)
+     * - for redis, the DSN must contain a dbindex (e.g. `redis://my.server.com:6379/20`)
+     *
      * @return TagAwareAdapter|null
      */
     public static function getInstance()
@@ -19,18 +25,25 @@ class SymfonyCacheCore
         static $instance = null;
         if (empty($instance)) {
             $dsn = (defined('_PS_CACHE_DSN_') ? _PS_CACHE_DSN_ : 'filesystem');
+            $ttl = (defined('_PS_CACHE_TTL_') ? _PS_CACHE_TL_ : 3600);
+            $prefix_key = (defined('_PS_CACHE_PREFIX_') ? _PS_CACHE_PREFIX_ : '');
             $type = substr($dsn, 0, strpos($dsn, ':'));
             switch ($type) {
                 case 'memcached':
-                    $adapter = new MemcachedAdapter(MemcachedAdapter::createConnection($dsn), 'init', 3600);
+                    $adapter = new MemcachedAdapter(MemcachedAdapter::createConnection($dsn), 'conf', $ttl);
                     break;
                 case 'redis':
-                    $adapter = new RedisAdapter(RedisAdapter::createConnection($dsn), 'init', 3600);
+                case 'rediss':
+                    $adapter = new RedisAdapter(RedisAdapter::createConnection($dsn), 'conf', $ttl);
                     break;
                 default:
-                    $adapter = new FilesystemAdapter('', 3600, _PS_CACHE_DIR_ . 'init');
+                    $adapter = new FilesystemAdapter('', $ttl, _PS_CACHE_DIR_ . 'conf');
             }
-            $instance = new TagAwareAdapter($adapter);
+            $instance = new TagAwareAdapter(
+                new ChainAdapter([
+                    new ArrayAdapter($ttl, false),
+                    $adapter,
+                ]));
         }
 
         return $instance;
