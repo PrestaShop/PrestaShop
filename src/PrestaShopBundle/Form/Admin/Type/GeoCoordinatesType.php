c6edd2e7174e06fdbb5052130d3570b87bdfc31d
Louis AUTHIE <65625876+LouisAUTHIE@users.noreply.github.com>
2023-06-19 16:59:26 +0200

    Apply suggestions from code review
    Co-authored-by: Fabien Papet <FabienPapet@users.noreply.github.com>

--- src/PrestaShopBundle/Form/Admin/Type/GeoCoordinatesType.php
+++ src/PrestaShopBundle/Form/Admin/Type/GeoCoordinatesType.php
@@ -42,15 +42,14 @@ class GeoCoordinatesType extends AbstractType
      * @param TranslatorInterface $translator
      */
     public function __construct(
-        TranslatorInterface $translator
+        private readonly TranslatorInterface $translator
     ) {
-        $this->translator = $translator;
     }
 
     /**
      * {@inheritdoc}
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('latitude', NumberType::class, [
@@ -75,7 +74,7 @@ class GeoCoordinatesType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'label_latitude' => $this->translator->trans('Latitude', [], 'Admin.Global'),
@@ -92,7 +91,7 @@ class GeoCoordinatesType extends AbstractType
     /**
      * {@inheritdoc}
      */
-    public function getBlockPrefix()
+    public function getBlockPrefix(): string
     {
         return 'geo_coordinates';
     }
