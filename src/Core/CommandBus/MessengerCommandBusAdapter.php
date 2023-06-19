9f82ad2f698166a37b0d9057bf3e752a5ffb2fdc
PICHAT Morgan <pichat.morgan@gmail.com>
2023-06-19 17:17:51 +0200

    Update commandBus adapter

--- src/Core/CommandBus/MessengerCommandBusAdapter.php
+++ src/Core/CommandBus/MessengerCommandBusAdapter.php
@@ -27,6 +27,7 @@
 namespace PrestaShop\PrestaShop\Core\CommandBus;
 
 use Symfony\Component\Messenger\Exception\HandlerFailedException;
+use Symfony\Component\Messenger\Exception\LogicException;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Stamp\HandledStamp;
 
@@ -45,12 +46,23 @@ final class MessengerCommandBusAdapter implements CommandBusInterface
     public function handle($command)
     {
         try {
-            $stamp = $this->commandBus
-                ->dispatch($command)
-                ->last(HandledStamp::class);
-            if ($stamp instanceof HandledStamp && $stamp->getResult() !== null) {
-                return $stamp->getResult();
+            $envelope = $this->commandBus->dispatch($command);
+            /** @var HandledStamp[] $handledStamps */
+            $handledStamps = $envelope->all(HandledStamp::class);
+
+            if (!$handledStamps) {
+                throw new LogicException(sprintf('Message of type "%s" was handled zero times. Exactly one handler is expected when using "%s::%s()".', get_debug_type($envelope->getMessage()), MessengerCommandBusAdapter::class, __FUNCTION__));
             }
+
+            if (\count($handledStamps) > 1) {
+                $handlers = implode(', ', array_map(function (HandledStamp $stamp): string {
+                    return sprintf('"%s"', $stamp->getHandlerName());
+                }, $handledStamps));
+
+                throw new LogicException(sprintf('Message of type "%s" was handled multiple times. Only one handler is expected when using "%s::%s()", got %d: %s.', get_debug_type($envelope->getMessage()), MessengerCommandBusAdapter::class, __FUNCTION__, \count($handledStamps), $handlers));
+            }
+
+            return $handledStamps[0]->getResult();
         } catch (HandlerFailedException $exception) {
             throw $exception->getPrevious();
         }
