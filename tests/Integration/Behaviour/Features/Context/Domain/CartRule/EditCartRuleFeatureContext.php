f52a337038ca2fe4524cd0c0566ced1add9ff2bd
Julius Zukauskas <julius.zukauskas@invertus.eu>
2023-06-21 18:20:15 +0300

    introduce dedicated context class for restrictions

--- tests/Integration/Behaviour/Features/Context/Domain/CartRule/EditCartRuleFeatureContext.php
+++ tests/Integration/Behaviour/Features/Context/Domain/CartRule/EditCartRuleFeatureContext.php
@@ -31,32 +31,12 @@ use Behat\Gherkin\Node\TableNode;
 use DateTimeImmutable;
 use PrestaShop\PrestaShop\Core\Domain\CartRule\Command\BulkToggleCartRuleStatusCommand;
 use PrestaShop\PrestaShop\Core\Domain\CartRule\Command\EditCartRuleCommand;
-use PrestaShop\PrestaShop\Core\Domain\CartRule\Command\SetCartRuleRestrictionsCommand;
 use PrestaShop\PrestaShop\Core\Domain\CartRule\Command\ToggleCartRuleStatusCommand;
 use PrestaShop\PrestaShop\Core\Domain\CartRule\Exception\CartRuleConstraintException;
-use PrestaShop\PrestaShop\Core\Domain\CartRule\Exception\CartRuleNotFoundException;
-use PrestaShop\PrestaShop\Core\Domain\CartRule\ValueObject\Restriction\RestrictionRule;
-use PrestaShop\PrestaShop\Core\Domain\CartRule\ValueObject\Restriction\RestrictionRuleGroup;
-use RuntimeException;
 use Tests\Integration\Behaviour\Features\Context\Util\PrimitiveUtils;
 
 class EditCartRuleFeatureContext extends AbstractCartRuleFeatureContext
 {
-    /**
-     * @var array<string, SetCartRuleRestrictionsCommand[]>
-     */
-    private array $restrictionCommandsByReference = [];
-
-    /**
-     * @var array<string, RestrictionRuleGroup[]>
-     */
-    private array $productRestrictionGroupsByReference = [];
-
-    /**
-     * This is just a random number which in theory should never be reached as cart rule id in tests
-     */
-    private const NON_EXISTING_CART_RULE_ID = 54440051;
-
     /**
      * @When /^I (enable|disable) cart rule with reference "(.+)"$/
      *
@@ -105,159 +85,6 @@ class EditCartRuleFeatureContext extends AbstractCartRuleFeatureContext
         } catch (CartRuleConstraintException $e) {
             $this->setLastException($e);
         }
-    }
-
-    /**
-     * @When I restrict following cart rules for cart rule :cartRuleReference:
-     *
-     * @param string $cartRuleReference
-     * @param TableNode $tableNode
-     *
-     * @return void
-     */
-    public function setRestrictedCartRules(string $cartRuleReference, TableNode $tableNode): void
-    {
-        $restrictedCartRuleIds = [];
-        foreach ($tableNode->getColumn(0) as $restrictedCartRuleReference) {
-            $restrictedCartRuleIds[] = $this->getSharedStorage()->get($restrictedCartRuleReference);
-        }
-
-        $command = $this->getRestrictionsCommand($cartRuleReference);
-
-        try {
-            $command->setRestrictedCartRuleIds($restrictedCartRuleIds);
-            $this->restrictionCommandsByReference[$cartRuleReference] = $command;
-        } catch (CartRuleConstraintException $e) {
-            $this->setLastException($e);
-        }
-    }
-
-    /**
-     * @When I add a restriction for cart rule :cartRuleReference, which requires at least :quantity product(s) in cart matching one of these rules:
-     * @When I add a restriction for cart rule :cartRuleReference, which requires any quantity of product(s) in cart matching one of these rules:
-     * @When I add a restriction for cart rule :cartRuleReference, which requires at least :quantity product(s), but I provide empty list of rules
-     *
-     * @param string $cartRuleReference
-     * @param int $quantity
-     * @param TableNode|null $table
-     *
-     * @return void
-     */
-    public function addRestrictionRule(string $cartRuleReference, int $quantity = 0, ?TableNode $table = null): void
-    {
-        $rules = [];
-
-        try {
-            if ($table) {
-                foreach ($table->getColumnsHash() as $row) {
-                    $rules[] = new RestrictionRule($row['type'], $this->referencesToIds($row['references']));
-                }
-            }
-
-            $restrictionGroups = [];
-            if (isset($this->productRestrictionGroupsByReference[$cartRuleReference])) {
-                $restrictionGroups = $this->productRestrictionGroupsByReference[$cartRuleReference];
-            }
-
-            $restrictionGroups[] = new RestrictionRuleGroup($quantity, $rules);
-            $this->productRestrictionGroupsByReference[$cartRuleReference] = $restrictionGroups;
-        } catch (CartRuleConstraintException $e) {
-            $this->setLastException($e);
-        }
-    }
-
-    /**
-     * @When I restrict cart rules for :cartRuleReference providing non-existing cart rules
-     *
-     * @param string $cartRuleReference
-     *
-     * @return void
-     */
-    public function restrictCartRulesProvidingNonExistingIds(string $cartRuleReference): void
-    {
-        $command = $this->getRestrictionsCommand($cartRuleReference);
-        $command->setRestrictedCartRuleIds([self::NON_EXISTING_CART_RULE_ID]);
-
-        try {
-            $this->getCommandBus()->handle($command);
-        } catch (CartRuleNotFoundException $e) {
-            $this->setLastException($e);
-        }
-    }
-
-    /**
-     * @When I clear all product restrictions for cart rule :cartRuleReference
-     *
-     * @param string $cartRuleReference
-     *
-     * @return void
-     */
-    public function clearProductRestrictionRules(string $cartRuleReference): void
-    {
-        $command = $this->getRestrictionsCommand($cartRuleReference);
-        $command->setProductRestrictionRuleGroups([]);
-
-        $this->getCommandBus()->handle($command);
-        $this->restrictionCommandsByReference[$cartRuleReference] = $command;
-        unset($this->productRestrictionGroupsByReference[$cartRuleReference]);
-    }
-
-    /**
-     * @When I save product restrictions for cart rule :cartRuleReference
-     * @When I save all the restrictions for cart rule :cartRuleReference
-     *
-     * @param string $cartRuleReference
-     *
-     * @return void
-     */
-    public function saveRestrictionRules(string $cartRuleReference): void
-    {
-        $command = $this->getRestrictionsCommand($cartRuleReference);
-
-        if (isset($this->productRestrictionGroupsByReference[$cartRuleReference])) {
-            // set the restrictions that (if) were added step by step before
-            $command->setProductRestrictionRuleGroups($this->productRestrictionGroupsByReference[$cartRuleReference]);
-            unset($this->productRestrictionGroupsByReference[$cartRuleReference]);
-        }
-
-        if ($command->isEmpty()) {
-            throw new RuntimeException(
-                sprintf(
-                    '%s is empty for cart rule referenced as "%s". Did you forget to fill the restrictions in other steps?',
-                    $command::class,
-                    $cartRuleReference
-                )
-            );
-        }
-
-        try {
-            $this->getCommandBus()->handle($command);
-        } catch (CartRuleConstraintException $e) {
-            $this->setLastException($e);
-        }
-    }
-
-    private function executeRestrictionsCommand(SetCartRuleRestrictionsCommand $command): void
-    {
-        try {
-            $this->getCommandBus()->handle($command);
-        } catch (CartRuleConstraintException $e) {
-            $this->setLastException($e);
-        }
-    }
-
-    private function getRestrictionsCommand(string $cartRuleReference): SetCartRuleRestrictionsCommand
-    {
-        if (isset($this->restrictionCommandsByReference[$cartRuleReference])) {
-            $command = $this->restrictionCommandsByReference[$cartRuleReference];
-            if (!($command instanceof SetCartRuleRestrictionsCommand)) {
-                throw new RuntimeException(sprintf('Expected "%s" got "%s', SetCartRuleRestrictionsCommand::class, var_export($command, true)));
-            }
-        } else {
-            $command = new SetCartRuleRestrictionsCommand($this->getSharedStorage()->get($cartRuleReference));
-        }
-
-        return $command;
     }
 
     /**
