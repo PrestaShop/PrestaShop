8792073639da77c6b663b3b698e212a48be6b856
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2023-06-23 17:27:39 +0200

    Fix review

--- tests/UI/campaigns/functional/FO/09_productPage/01_quickView.js
+++ tests/UI/campaigns/functional/FO/09_productPage/01_quickView.js
@@ -334,7 +334,7 @@ describe('Product quick view', async () => {
       await expect(pageTitle.toUpperCase()).to.contains(firstProductData.name);
     });
 
-    it('should check that the second product image is different than the second', async function () {
+    it('should check that the second product image is different than the first', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'getProductImage2', baseContext);
 
       const imageSecondColor = await productPage.getProductImageUrls(page);
