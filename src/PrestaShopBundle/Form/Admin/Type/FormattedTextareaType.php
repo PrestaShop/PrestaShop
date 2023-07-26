5833c29ed2ea979f36f46d1b56c9aa00fe4b683f
Daniel Hlavacek <daniel.hlavacek@hotmail.cz>
2023-07-26 20:14:16 +0200

    Add mysql column limits

--- src/PrestaShopBundle/Form/Admin/Type/FormattedTextareaType.php
+++ src/PrestaShopBundle/Form/Admin/Type/FormattedTextareaType.php
@@ -39,9 +39,12 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class FormattedTextareaType extends TranslatorAwareType
 {
     /**
-     * Max size of UTF-8 content in MySQL text column
+     * Max size of UTF-8 content in MySQL text columns
      */
+    public const LIMIT_TINYTEXT_UTF8 = 84;
     public const LIMIT_TEXT_UTF8 = 21844;
+    public const LIMIT_MEDIUMTEXT_UTF8 = 5592414;
+    public const LIMIT_LONGTEXT_UTF8 = 1431655764;
 
     /**
      * {@inheritdoc}
