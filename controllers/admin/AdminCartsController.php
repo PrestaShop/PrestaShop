8313ae29c001bc44420488473fec36396b4fc75b
Daniel Hlavacek <daniel.hlavacek@hotmail.cz>
2023-07-28 12:48:44 +0200

    Fix CS

--- controllers/admin/AdminCartsController.php
+++ controllers/admin/AdminCartsController.php
@@ -24,8 +24,6 @@
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  */
 
-use PrestaShop\PrestaShop\Core\Localization\Exception\LocalizationException;
-
 /**
  * @property Cart $object
  */
