f751bc9c12a155d8e84241284c7657fa98b309a6
PululuK <pululuandre@hotmail.com>
2023-09-21 17:56:11 +0200

    Add devices global tpl infos

--- classes/controller/FrontController.php
+++ classes/controller/FrontController.php
@@ -476,6 +476,7 @@ class FrontControllerCore extends Controller
             'static_token' => Tools::getToken(false),
             'token' => Tools::getToken(),
             'debug' => _PS_MODE_DEV_,
+            'device' => $this->getTemplateVarDevice(),
         ];
 
         // An array [module_name => module_output] will be returned
@@ -499,6 +500,15 @@ class FrontControllerCore extends Controller
         Media::addJsDef([
             'prestashop' => $this->buildFrontEndObject($templateVars),
         ]);
+    }
+
+    protected function getTemplateVarDevice(): array
+    {
+        return [
+            'is_mobile' => $this->context->isMobile(),
+            'is_tablet' => $this->context->isTablet(),
+            'type' => $this->context->getDevice(),
+        ];
     }
 
     /**
