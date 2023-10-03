7b5126f6d77c80022a739aaaf1db91fbfe4a76d0
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2023-10-03 16:06:35 +0200

    Fix random errors in orders campaign - part 2

--- tests/UI/campaigns/functional/BO/02_orders/05_shoppingCarts/02_sortAndPagination.ts
+++ tests/UI/campaigns/functional/BO/02_orders/05_shoppingCarts/02_sortAndPagination.ts
@@ -60,166 +60,168 @@ describe('BO - Orders - Shopping carts : Sort and pagination shopping carts', as
     });
   });
 
-  // before and after functions
-  before(async function () {
-    browserContext = await helper.createBrowserContext(this.browser);
-    page = await helper.newTab(browserContext);
-  });
+  describe('Pagination & sort shopping carts table', async () => {
+    // before and after functions
+    before(async function () {
+      browserContext = await helper.createBrowserContext(this.browser);
+      page = await helper.newTab(browserContext);
+    });
 
-  after(async () => {
-    await helper.closeBrowserContext(browserContext);
-  });
+    after(async () => {
+      await helper.closeBrowserContext(browserContext);
+    });
 
-  // 1 - Pagination
-  describe('Pagination next and previous', async () => {
     it('should login in BO', async function () {
       await loginCommon.loginBO(this, page);
     });
 
-    it('should go to \'Orders > Shopping carts\' page', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'goToShoppingCartsPage', baseContext);
+    // 1 - Pagination
+    describe('Pagination next and previous', async () => {
+      it('should go to \'Orders > Shopping carts\' page', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'goToShoppingCartsPage', baseContext);
 
-      await dashboardPage.goToSubMenu(
-        page,
-        dashboardPage.ordersParentLink,
-        dashboardPage.shoppingCartsLink,
-      );
+        await dashboardPage.goToSubMenu(
+          page,
+          dashboardPage.ordersParentLink,
+          dashboardPage.shoppingCartsLink,
+        );
 
-      const pageTitle = await shoppingCartsPage.getPageTitle(page);
-      expect(pageTitle).to.contains(shoppingCartsPage.pageTitle);
-    });
+        const pageTitle = await shoppingCartsPage.getPageTitle(page);
+        expect(pageTitle).to.contains(shoppingCartsPage.pageTitle);
+      });
 
-    it('should change the items number to 20 per page', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'changeItemNumberTo20', baseContext);
+      it('should change the items number to 20 per page', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'changeItemNumberTo20', baseContext);
 
-      const paginationNumber = await shoppingCartsPage.selectPaginationLimit(page, 20);
-      expect(paginationNumber).to.equal('1');
-    });
+        const paginationNumber = await shoppingCartsPage.selectPaginationLimit(page, 20);
+        expect(paginationNumber).to.equal('1');
+      });
 
-    it('should click on next', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'clickOnNext', baseContext);
+      it('should click on next', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'clickOnNext', baseContext);
 
-      const paginationNumber = await shoppingCartsPage.paginationNext(page);
-      expect(paginationNumber).to.equal('2');
-    });
+        const paginationNumber = await shoppingCartsPage.paginationNext(page);
+        expect(paginationNumber).to.equal('2');
+      });
 
-    it('should click on previous', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'clickOnPrevious', baseContext);
+      it('should click on previous', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'clickOnPrevious', baseContext);
 
-      const paginationNumber = await shoppingCartsPage.paginationPrevious(page);
-      expect(paginationNumber).to.equal('1');
-    });
+        const paginationNumber = await shoppingCartsPage.paginationPrevious(page);
+        expect(paginationNumber).to.equal('1');
+      });
 
-    it('should change the items number to 300 per page', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'changeItemNumberTo300', baseContext);
+      it('should change the items number to 300 per page', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'changeItemNumberTo300', baseContext);
 
-      const paginationNumber = await shoppingCartsPage.selectPaginationLimit(page, 300);
-      expect(paginationNumber).to.equal('1');
-    });
-  });
-
-  // 2 - Sort shopping cart table
-  describe('Sort shopping cart table', async () => {
-    it('should filter by customer lastName start by \'guest\'', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'filterToSort', baseContext);
-
-      await shoppingCartsPage.filterTable(page, 'input', 'c!lastname', 'guest');
-
-      const textColumn = await shoppingCartsPage.getTextColumn(page, 1, 'c!lastname');
-      expect(textColumn).to.contains(customerData.lastName);
-    });
-
-    const sortTests = [
-      {
-        args: {
-          testIdentifier: 'sortByIdDesc', sortBy: 'id_cart', sortDirection: 'down', isFloat: true,
-        },
-      },
-      {
-        args: {
-          testIdentifier: 'sortByOrderIDAsc', sortBy: 'status', sortDirection: 'up', isFloat: true,
-        },
-      },
-      {
-        args: {
-          testIdentifier: 'sortByOrderIDDesc', sortBy: 'status', sortDirection: 'down', isFloat: true,
-        },
-      },
-      {
-        args: {
-          testIdentifier: 'sortByCarrierAsc', sortBy: 'ca!name', sortDirection: 'up',
-        },
-      },
-      {
-        args: {
-          testIdentifier: 'sortByCarrierDesc', sortBy: 'ca!name', sortDirection: 'down',
-        },
-      },
-      {
-        args: {
-          testIdentifier: 'sortByDateAsc', sortBy: 'date', sortDirection: 'up',
-        },
-      },
-      {
-        args: {
-          testIdentifier: 'sortByDateDesc', sortBy: 'date', sortDirection: 'down',
-        },
-      },
-      {
-        args: {
-          testIdentifier: 'sortByOnlineAsc', sortBy: 'id_guest', sortDirection: 'up',
-        },
-      },
-      {
-        args: {
-          testIdentifier: 'sortByOnlineDesc', sortBy: 'id_guest', sortDirection: 'down',
-        },
-      },
-      {
-        args: {
-          testIdentifier: 'sortByIdAsc', sortBy: 'id_cart', sortDirection: 'up', isFloat: true,
-        },
-      },
-    ];
-
-    sortTests.forEach((test) => {
-      it(`should sort by '${test.args.sortBy}' '${test.args.sortDirection}' and check result`, async function () {
-        await testContext.addContextItem(this, 'testIdentifier', test.args.testIdentifier, baseContext);
-
-        const nonSortedTable = await shoppingCartsPage.getAllRowsColumnContent(page, test.args.sortBy);
-
-        await shoppingCartsPage.sortTable(page, test.args.sortBy, test.args.sortDirection);
-
-        const sortedTable = await shoppingCartsPage.getAllRowsColumnContent(page, test.args.sortBy);
-
-        if (test.args.isFloat) {
-          const nonSortedTableFloat: number[] = nonSortedTable.map((text:string): number => parseFloat(text));
-          const sortedTableFloat: number[] = sortedTable.map((text:string): number => parseFloat(text));
-
-          const expectedResult = await basicHelper.sortArrayNumber(nonSortedTableFloat);
-
-          if (test.args.sortDirection === 'up') {
-            expect(sortedTableFloat).to.deep.equal(expectedResult);
-          } else {
-            expect(sortedTableFloat).to.deep.equal(expectedResult.reverse());
-          }
-        } else {
-          const expectedResult: string[] = await basicHelper.sortArray(nonSortedTable);
-
-          if (test.args.sortDirection === 'up') {
-            expect(sortedTable).to.deep.equal(expectedResult);
-          } else {
-            expect(sortedTable).to.deep.equal(expectedResult.reverse());
-          }
-        }
+        const paginationNumber = await shoppingCartsPage.selectPaginationLimit(page, 300);
+        expect(paginationNumber).to.equal('1');
       });
     });
 
-    it('should reset all filters', async function () {
-      await testContext.addContextItem(this, 'testIdentifier', 'resetAfterSort', baseContext);
+    // 2 - Sort shopping cart table
+    describe('Sort shopping carts table', async () => {
+      it('should filter by customer lastName start by \'guest\'', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'filterToSort', baseContext);
 
-      const numberOfShoppingCartsAfterReset = await shoppingCartsPage.resetAndGetNumberOfLines(page);
-      expect(numberOfShoppingCartsAfterReset).to.be.above(1);
+        await shoppingCartsPage.filterTable(page, 'input', 'c!lastname', 'guest');
+
+        const textColumn = await shoppingCartsPage.getTextColumn(page, 1, 'c!lastname');
+        expect(textColumn).to.contains(customerData.lastName);
+      });
+
+      const sortTests = [
+        {
+          args: {
+            testIdentifier: 'sortByIdDesc', sortBy: 'id_cart', sortDirection: 'down', isFloat: true,
+          },
+        },
+        {
+          args: {
+            testIdentifier: 'sortByOrderIDAsc', sortBy: 'status', sortDirection: 'up', isFloat: true,
+          },
+        },
+        {
+          args: {
+            testIdentifier: 'sortByOrderIDDesc', sortBy: 'status', sortDirection: 'down', isFloat: true,
+          },
+        },
+        {
+          args: {
+            testIdentifier: 'sortByCarrierAsc', sortBy: 'ca!name', sortDirection: 'up',
+          },
+        },
+        {
+          args: {
+            testIdentifier: 'sortByCarrierDesc', sortBy: 'ca!name', sortDirection: 'down',
+          },
+        },
+        {
+          args: {
+            testIdentifier: 'sortByDateAsc', sortBy: 'date', sortDirection: 'up',
+          },
+        },
+        {
+          args: {
+            testIdentifier: 'sortByDateDesc', sortBy: 'date', sortDirection: 'down',
+          },
+        },
+        {
+          args: {
+            testIdentifier: 'sortByOnlineAsc', sortBy: 'id_guest', sortDirection: 'up',
+          },
+        },
+        {
+          args: {
+            testIdentifier: 'sortByOnlineDesc', sortBy: 'id_guest', sortDirection: 'down',
+          },
+        },
+        {
+          args: {
+            testIdentifier: 'sortByIdAsc', sortBy: 'id_cart', sortDirection: 'up', isFloat: true,
+          },
+        },
+      ];
+
+      sortTests.forEach((test) => {
+        it(`should sort by '${test.args.sortBy}' '${test.args.sortDirection}' and check result`, async function () {
+          await testContext.addContextItem(this, 'testIdentifier', test.args.testIdentifier, baseContext);
+
+          const nonSortedTable = await shoppingCartsPage.getAllRowsColumnContent(page, test.args.sortBy);
+
+          await shoppingCartsPage.sortTable(page, test.args.sortBy, test.args.sortDirection);
+
+          const sortedTable = await shoppingCartsPage.getAllRowsColumnContent(page, test.args.sortBy);
+
+          if (test.args.isFloat) {
+            const nonSortedTableFloat: number[] = nonSortedTable.map((text: string): number => parseFloat(text));
+            const sortedTableFloat: number[] = sortedTable.map((text: string): number => parseFloat(text));
+
+            const expectedResult = await basicHelper.sortArrayNumber(nonSortedTableFloat);
+
+            if (test.args.sortDirection === 'up') {
+              expect(sortedTableFloat).to.deep.equal(expectedResult);
+            } else {
+              expect(sortedTableFloat).to.deep.equal(expectedResult.reverse());
+            }
+          } else {
+            const expectedResult: string[] = await basicHelper.sortArray(nonSortedTable);
+
+            if (test.args.sortDirection === 'up') {
+              expect(sortedTable).to.deep.equal(expectedResult);
+            } else {
+              expect(sortedTable).to.deep.equal(expectedResult.reverse());
+            }
+          }
+        });
+      });
+
+      it('should reset all filters', async function () {
+        await testContext.addContextItem(this, 'testIdentifier', 'resetAfterSort', baseContext);
+
+        const numberOfShoppingCartsAfterReset = await shoppingCartsPage.resetAndGetNumberOfLines(page);
+        expect(numberOfShoppingCartsAfterReset).to.be.above(1);
+      });
     });
   });
 
