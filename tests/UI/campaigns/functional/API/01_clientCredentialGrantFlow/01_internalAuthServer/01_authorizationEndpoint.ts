31cd3f83fe96e7a278c0b1e80e2a0522cb1368eb
Progi1984 <franck.lefevre@prestashop.com>
2023-10-19 11:53:50 +0200

    Functional Tests : Link new issue for @todo

--- tests/UI/campaigns/functional/API/01_clientCredentialGrantFlow/01_internalAuthServer/01_authorizationEndpoint.ts
+++ tests/UI/campaigns/functional/API/01_clientCredentialGrantFlow/01_internalAuthServer/01_authorizationEndpoint.ts
@@ -70,7 +70,7 @@ describe('API : Internal Auth Server - Authorization Endpoint', async () => {
     it('should request the endpoint /admin-dev/api/oauth2/token with method POST with valid data', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'requestAuthWithMethodPOSTValidData', baseContext);
 
-      // @todo : https://github.com/PrestaShop/PrestaShop/issues/33710
+      // @todo : https://github.com/PrestaShop/PrestaShop/issues/34297
       const apiResponse = await apiContext.post('api/oauth2/token', {
         form: {
           client_id: 'my_client_id',
--- tests/UI/campaigns/functional/API/01_clientCredentialGrantFlow/01_internalAuthServer/02_resourceEndpoint.ts
+++ tests/UI/campaigns/functional/API/01_clientCredentialGrantFlow/01_internalAuthServer/02_resourceEndpoint.ts
@@ -20,7 +20,7 @@ describe('API : Internal Auth Server - Resource Endpoint', async () => {
     it('should request the authorization endpoint and fetch valid token', async function () {
       await testContext.addContextItem(this, 'testIdentifier', 'requestAuthorizationEndpoint', baseContext);
 
-      // @todo : https://github.com/PrestaShop/PrestaShop/issues/33710
+      // @todo : https://github.com/PrestaShop/PrestaShop/issues/34297
       const apiResponse = await apiContext.post('api/oauth2/token', {
         form: {
           client_id: 'my_client_id',
