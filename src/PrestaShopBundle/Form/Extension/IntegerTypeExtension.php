acf12d4b13c586d76eb36632897a4b80b396454f
boherm <boris.hermans@prestashop.com>
2023-10-20 17:35:09 +0200

    Fix some locales on IntegerType

---
+++ src/PrestaShopBundle/Form/Extension/IntegerTypeExtension.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+
+declare(strict_types=1);
+
+namespace PrestaShopBundle\Form\Extension;
+
+use PrestaShop\PrestaShop\Core\Localization\Number\LocaleNumberTransformer;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\DataTransformer\IntegerToLocalizedStringTransformer;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class IntegerTypeExtension extends AbstractTypeExtension
+{
+    /**
+     * @var LocaleNumberTransformer
+     */
+    private $localeNumberTransformer;
+
+    public function __construct(
+        LocaleNumberTransformer $localeNumberTransformer
+    ) {
+        $this->localeNumberTransformer = $localeNumberTransformer;
+    }
+
+    public static function getExtendedTypes(): iterable
+    {
+        return [IntegerType::class];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        // We only want to replace/adapt the IntegerToLocalizedStringTransformer, so we save the current transformers
+        // to restore them after replacing the new IntegerToLocalizedStringTransformer.
+        $viewTransformers = $builder->getViewTransformers();
+        $builder->resetViewTransformers();
+
+        foreach ($viewTransformers as $viewTransformer) {
+            if ($viewTransformer instanceof IntegerToLocalizedStringTransformer) {
+                $builder
+                    ->addViewTransformer(new IntegerToLocalizedStringTransformer(
+                        $options['grouping'],
+                        $options['rounding_mode'],
+                        !$options['grouping'] ? 'en' : $this->localeNumberTransformer->getLocaleForNumberInputs()
+                    ))
+                ;
+            } else {
+                $builder->addViewTransformer($viewTransformer);
+            }
+        }
+    }
+}
