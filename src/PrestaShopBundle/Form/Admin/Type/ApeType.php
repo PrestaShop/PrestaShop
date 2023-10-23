506f918eee2926735e84e9effa063773077687cd
Alexandre Bak <138218784+alexandrebak42@users.noreply.github.com>
2023-10-23 13:32:28 +0200

    delete indent

--- src/PrestaShopBundle/Form/Admin/Type/ApeType.php
+++ src/PrestaShopBundle/Form/Admin/Type/ApeType.php
@@ -23,7 +23,7 @@
  * @copyright Since 2007 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  */
- 
+
 declare(strict_types=1);
 
 namespace PrestaShopBundle\Form\Admin\Type;
