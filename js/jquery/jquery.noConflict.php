0c4da4788bab42a6146878378bde5e8bf1a0dacd
Dominik Ulrich <czshaimagal@gmail.com>
2023-11-01 15:49:10 +0100

    Check if version was passed

--- js/jquery/jquery.noConflict.php
+++ js/jquery/jquery.noConflict.php
@@ -1,6 +1,6 @@
 <?php
 
 header('content-type: application/x-javascript');
-if (preg_match('/^([0-9\.]+)$/Ui', $_GET['version'])) {
+if (isset($_GET['version']) && preg_match('/^([0-9\.]+)$/Ui', $_GET['version'])) {
     echo 'var $j'.str_replace('.', '', $_GET['version']).' = jQuery.noConflict(true);';
 }
