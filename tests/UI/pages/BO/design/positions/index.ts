2a1dd14d8d6bcc2a296bdb5b11d459e937ed37b8
Progi1984 <franck.lefevre@prestashop.com>
2023-11-10 10:39:12 +0100

    Functional Tests : API : GET /api/hook-status/{id}

--- tests/UI/pages/BO/design/positions/index.ts
+++ tests/UI/pages/BO/design/positions/index.ts
@@ -24,9 +24,15 @@ class Positions extends BOBasePage {
 
   private readonly modulePositionFormHookSection: string;
 
-  private readonly hookNameLink: (hookName: string) => string;
+  private readonly modulePositionFormHookSectionVisible: string;
 
-  private readonly modulesListInHookResults: (hookName: string) => string;
+  private readonly hookRowNth: (hookRow: number) => string;
+
+  private readonly hookStatusInput: (hookRow: number) => string;
+
+  private readonly hookRowName: (hookName: string) => string;
+
+  private readonly hookRowModulesList: (hookName: string) => string;
 
   private readonly hookNameSpan: (hookName: string) => string;
 
@@ -46,10 +52,14 @@ class Positions extends BOBasePage {
     this.filterModuleButton = '#position-filters span.select2';
     this.filterModuleInputValue = 'body span.select2-search.select2-search--dropdown input';
     this.filterModuleFirstResult = '#select2-show-modules-results li:nth-child(1)';
-    this.modulePositionFormHookSection = `${this.modulePositionForm} section[style='']`;
-    this.hookNameLink = (hookName: string) => `${this.modulePositionFormHookSection} a[name=${hookName}]`;
-    this.modulesListInHookResults = (hookName: string) => `${this.hookNameLink(hookName)} ~ section.module-list ul`;
-    this.hookNameSpan = (hookName: string) => `${this.hookNameLink(hookName)} + header span.hook-name`;
+    this.modulePositionFormHookSection = `${this.modulePositionForm} section`;
+    this.modulePositionFormHookSectionVisible = `${this.modulePositionFormHookSection}.hook-panel.hook-visible`;
+    this.hookRowNth = (hookRow: number) => `${this.modulePositionFormHookSection}:nth-child(${hookRow + 1
+    } of .hook-panel.hook-visible)`;
+    this.hookStatusInput = (hookRow: number) => `${this.hookRowNth(hookRow)} header span.hook-status input.hook-switch-action`;
+    this.hookRowName = (hookName: string) => `${this.modulePositionFormHookSection} a[name=${hookName}]`;
+    this.hookRowModulesList = (hookName: string) => `${this.hookRowName(hookName)} ~ section.module-list ul`;
+    this.hookNameSpan = (hookName: string) => `${this.hookRowName(hookName)} + header span.hook-name`;
   }
 
   /* Methods */
@@ -84,7 +94,7 @@ class Positions extends BOBasePage {
    * @returns {Promise<number>}
    */
   async getNumberOfHooks(page: Page): Promise<number> {
-    return (await page.$$(this.modulePositionFormHookSection)).length;
+    return (await page.$$(this.modulePositionFormHookSectionVisible)).length;
   }
 
   /**
@@ -94,7 +104,7 @@ class Positions extends BOBasePage {
    * @returns {Promise<string>}
    */
   async getModulesInHook(page: Page, hookName: string): Promise<string> {
-    return this.getTextContent(page, this.modulesListInHookResults(hookName));
+    return this.getTextContent(page, this.hookRowModulesList(hookName));
   }
 
   /**
@@ -105,6 +115,31 @@ class Positions extends BOBasePage {
    */
   async isHookVisible(page: Page, hookName: string): Promise<boolean> {
     return this.elementVisible(page, this.hookNameSpan(hookName), 1000);
+  }
+
+  /**
+   * Return the hookId
+   * @param page {Page} Browser tab
+   * @param hookRow {number} Hook Row
+   * @returns {Promise<boolean>}
+   */
+  async getHookId(page: Page, hookRow: number): Promise<number> {
+    const attribute = await this.getAttributeContent(page, this.hookStatusInput(hookRow), 'data-hook-id');
+
+    return parseInt(attribute, 10);
+  }
+
+  /**
+   * Return the hook status
+   * @param page {Page} Browser tab
+   * @param hookRow {number} Hook Row
+   * @returns {Promise<boolean>}
+   */
+  async getHookStatus(page: Page, hookRow: number): Promise<boolean> {
+    const inputValue = await this.getAttributeContent(page, `${this.hookStatusInput(hookRow)}:checked`, 'value');
+
+    // Return status=false if value='0' and true otherwise
+    return (inputValue !== '0');
   }
 }
 
