b3330161291f41315650b008cb4db26096902db4
Dominik Ulrich <czshaimagal@gmail.com>
2023-11-12 15:21:05 +0100

    Update ProductQuantity.php

--- src/PrestaShopBundle/Form/Admin/Product/ProductQuantity.php
+++ src/PrestaShopBundle/Form/Admin/Product/ProductQuantity.php
@@ -181,9 +181,11 @@ class ProductQuantity extends CommonAbstractType
                 FormType\NumberType::class,
                 [
                     'required' => true,
+                    'default_empty_data' => 1,
                     'label' => $this->translator->trans('Minimum quantity for sale', [], 'Admin.Catalog.Feature'),
                     'constraints' => [
                         new Assert\NotBlank(),
+                        new Assert\Positive(),
                         new Assert\Type(['type' => 'numeric']),
                     ],
                     'attr' => [
