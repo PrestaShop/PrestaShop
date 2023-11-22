d96ab10af34c304fdf1455e108be90cefd5e2091
Progi1984 <franck.lefevre@prestashop.com>
2023-11-22 11:27:53 +0100

    Functional Tests : Cash on delivery (COD) module - Place an order with it

---
+++ tests/UI/campaigns/modules/ps_cashondelivery/02_configuration/01_placeOrderWithIt.ts
@@ -0,0 +1,167 @@
+// Import utils
+import helper from '@utils/helpers';
+import testContext from '@utils/testContext';
+
+// Import commonTests
+import loginCommon from '@commonTests/BO/loginBO';
+
+// Import pages
+// Import BO pages
+import dashboardPage from '@pages/BO/dashboard';
+import ordersPage from '@pages/BO/orders';
+// Import FO pages
+import {cartPage} from '@pages/FO/cart';
+import checkoutPage from '@pages/FO/checkout';
+import orderConfirmationPage from '@pages/FO/checkout/orderConfirmation';
+import {homePage} from '@pages/FO/home';
+import {loginPage as foLoginPage} from '@pages/FO/login';
+
+// Import data
+import Customers from '@data/demo/customers';
+import OrderStatuses from '@data/demo/orderStatuses';
+import PaymentMethods from '@data/demo/paymentMethods';
+
+import {expect} from 'chai';
+import type {BrowserContext, Page} from 'playwright';
+
+const baseContext: string = 'modules_ps_cashondelivery_configuration_placeOrderWithIt';
+
+describe('Cash on delivery (COD) module - Place an order with it', async () => {
+  let browserContext: BrowserContext;
+  let page: Page;
+  let orderReference: string;
+
+  // before and after functions
+  before(async function () {
+    browserContext = await helper.createBrowserContext(this.browser);
+    page = await helper.newTab(browserContext);
+  });
+
+  after(async () => {
+    await helper.closeBrowserContext(browserContext);
+  });
+
+  describe('FO - Order a product with Cash on delivery payment', async () => {
+    it('should go to FO home page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToFO', baseContext);
+
+      await homePage.goToFo(page);
+
+      const isHomePage = await homePage.isHomePage(page);
+      expect(isHomePage).to.eq(true);
+    });
+
+    it('should go to login page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToLoginPageFO', baseContext);
+
+      await homePage.goToLoginPage(page);
+
+      const pageTitle = await foLoginPage.getPageTitle(page);
+      expect(pageTitle).to.contains(foLoginPage.pageTitle);
+    });
+
+    it('should sign in with default customer', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'sighInFO', baseContext);
+
+      await foLoginPage.customerLogin(page, Customers.johnDoe);
+
+      const isCustomerConnected = await foLoginPage.isCustomerConnected(page);
+      expect(isCustomerConnected).to.eq(true);
+    });
+
+    it('should add the first product to the cart', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'addProductToCart', baseContext);
+
+      await foLoginPage.goToHomePage(page);
+
+      // Add first product to cart by quick view
+      await homePage.addProductToCartByQuickView(page, 1);
+      await homePage.proceedToCheckout(page);
+
+      const pageTitle = await cartPage.getPageTitle(page);
+      expect(pageTitle).to.eq(cartPage.pageTitle);
+    });
+
+    it('should proceed to checkout and check Step Address', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'checkAddressStep', baseContext);
+
+      await cartPage.clickOnProceedToCheckout(page);
+
+      const isCheckoutPage = await checkoutPage.isCheckoutPage(page);
+      expect(isCheckoutPage).to.eq(true);
+
+      const isStepPersonalInformationComplete = await checkoutPage.isStepCompleted(
+        page,
+        checkoutPage.personalInformationStepForm,
+      );
+      expect(isStepPersonalInformationComplete).to.eq(true);
+    });
+
+    it('should validate Step Address and go to Delivery Step', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'checkDeliveryStep', baseContext);
+
+      const isStepAddressComplete = await checkoutPage.goToDeliveryStep(page);
+      expect(isStepAddressComplete).to.eq(true);
+    });
+
+    it('should go to payment step', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToPaymentStep', baseContext);
+
+      const isStepDeliveryComplete = await checkoutPage.goToPaymentStep(page);
+      expect(isStepDeliveryComplete, 'Step Address is not complete').to.eq(true);
+    });
+
+    it('should choose payment method and confirm the order', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'confirmOrder', baseContext);
+
+      // Payment step - Choose payment step
+      await checkoutPage.choosePaymentAndOrder(page, PaymentMethods.cashOnDelivery.moduleName);
+
+      // Check the confirmation message
+      const cardTitle = await orderConfirmationPage.getOrderConfirmationCardTitle(page);
+      expect(cardTitle).to.contains(orderConfirmationPage.orderConfirmationCardTitle);
+
+      orderReference = await orderConfirmationPage.getOrderReferenceValue(page);
+      expect(orderReference.length).to.be.gt(0);
+    });
+  });
+
+  describe('BO - Check the last order', async () => {
+    it('should login in BO', async function () {
+      await loginCommon.loginBO(this, page);
+    });
+
+    it('should go to \'Orders > Orders\' page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToOrdersPage', baseContext);
+
+      await dashboardPage.goToSubMenu(
+        page,
+        dashboardPage.ordersParentLink,
+        dashboardPage.ordersLink,
+      );
+
+      const pageTitle = await ordersPage.getPageTitle(page);
+      expect(pageTitle).to.contains(ordersPage.pageTitle);
+    });
+
+    it('should reset all filters and get number of orders', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'resetFiltersFirst', baseContext);
+
+      const numberOfOrders = await ordersPage.resetAndGetNumberOfLines(page);
+      expect(numberOfOrders).to.be.above(0);
+    });
+
+    it('should check the last order', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'checkLastOrder', baseContext);
+
+      const rowOrderReference = await ordersPage.getTextColumn(page, 'reference', 1);
+      expect(rowOrderReference).to.be.equal(orderReference);
+
+      const rowOrderPayment = await ordersPage.getTextColumn(page, 'payment', 1);
+      expect(rowOrderPayment).to.be.equal(PaymentMethods.cashOnDelivery.displayName);
+
+      const rowOrderStatus = await ordersPage.getTextColumn(page, 'osname', 1);
+      expect(rowOrderStatus).to.be.equal(OrderStatuses.awaitingCashOnDelivery.name);
+    });
+  });
+});
