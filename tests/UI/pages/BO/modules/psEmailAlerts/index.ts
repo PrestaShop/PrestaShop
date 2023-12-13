df83c56dc3c312edcb6db1e73eedbeb0ce28540b
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2023-12-13 14:45:26 +0100

    Rebase

--- tests/UI/pages/BO/modules/psEmailAlerts/index.ts
+++ tests/UI/pages/BO/modules/psEmailAlerts/index.ts
@@ -11,11 +11,8 @@ class PsEmailAlerts extends ModuleConfiguration {
   public readonly pageTitle: string;
 
   private readonly productAvailabilityCheckbox: (toEnable: boolean) => string;
-  private readonly orderEditToggle: (toEnable: string) => string;
 
-  private readonly saveModuleForm: string;
-
-  private readonly newOrderToggle: (toEnable: string) => string;
+  private readonly orderEditCheckbox: (toEnable: boolean) => string;
 
   private readonly submitCustomerNotifications: string;
 
@@ -43,17 +40,13 @@ class PsEmailAlerts extends ModuleConfiguration {
     // Selectors
     // Customer Notifications
     this.productAvailabilityCheckbox = (toEnable: boolean) => `#MA_CUSTOMER_QTY_${toEnable ? 'on' : 'off'}`;
+    this.orderEditCheckbox = (toEnable: boolean) => `#MA_ORDER_EDIT_${toEnable ? 'on' : 'off'}`;
     this.submitCustomerNotifications = 'button[name="submitMailAlert"]';
     // Merchant Notifications
     this.newOrderCheckbox = (toEnable: boolean) => `#MA_MERCHANT_ORDER_${toEnable ? 'on' : 'off'}`;
     this.addOrderEmailInput = '#fieldset_1_1 div.form-wrapper div:nth-child(2) > div > div > input';
     this.outOfStockCheckbox = (toEnable: boolean) => `#MA_MERCHANT_OOS_${toEnable ? 'on' : 'off'}`;
     this.returnsCheckbox = (toEnable: boolean) => `#MA_RETURN_SLIP_${toEnable ? 'on' : 'off'}`;
-    this.orderEditToggle = (toEnable: string) => `#MA_ORDER_EDIT_${toEnable}`;
-    this.saveModuleForm = '#module_form_submit_btn';
-    this.newOrderToggle = (toEnable: string) => `#MA_MERCHANT_ORDER_${toEnable}`;
-    this.outOfStockToggle = (toEnable: string) => `#MA_MERCHANT_OOS_${toEnable}`;
-    this.returnsToggle = (toEnable: string) => `#MA_RETURN_SLIP_${toEnable}`;
     this.returnEmailInput = '#fieldset_1_1 div.form-wrapper div:nth-child(7) > div > div input';
     this.submitMerchantNotifications = 'button[name="submitMAMerchant"]';
   }
@@ -67,8 +60,8 @@ class PsEmailAlerts extends ModuleConfiguration {
    * @returns {Promise<number>}
    */
   async setEditOrder(page: Page, toEnable: boolean): Promise<string> {
-    await this.setChecked(page, this.orderEditToggle(toEnable ? 'on' : 'off'));
-    await this.clickAndWaitForURL(page, this.saveModuleForm);
+    await this.setChecked(page, this.orderEditCheckbox(toEnable));
+    await this.clickAndWaitForURL(page, this.submitCustomerNotifications);
 
     return this.getAlertSuccessBlockContent(page);
   }
