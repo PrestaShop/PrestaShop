ce0d267c967418809ca9c2e37d3297ce8edf0bff
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2024-02-12 12:44:03 +0100

    Fix UI tests - part 3

--- tests/UI/pages/BO/international/translations/index.ts
+++ tests/UI/pages/BO/international/translations/index.ts
@@ -139,7 +139,7 @@ class Translations extends BOBasePage {
    * @returns {Promise<void>}
    */
   async modifyTranslation(page: Page, translation: string, theme: string, language: string, module: string = '',
-    emailType: string = ''): Promise<void> {
+                          emailType: string = ''): Promise<void> {
     await this.selectByVisibleText(page, this.typeOfTranslationSelect, translation);
 
     if (await this.elementVisible(page, this.selectYourThemeSelect, 2000)) {
@@ -218,7 +218,9 @@ class Translations extends BOBasePage {
    * @returns {Promise<string>}
    */
   async getLanguagesFromUpdateResult(page: Page): Promise<string> {
-    return this.getTextContent(page, this.languageToUpdateResult, false);
+    await this.waitForSelectorAndClick(page, this.languageToAddSelect);
+
+    return this.getTextContent(page, this.languageToUpdateResult);
   }
 
   /**
