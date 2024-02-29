b2e0aee35a9d61bf43abf77a4d7cea7e7e22d153
PululuK <pululuandre@hotmail.com>
2024-02-29 14:40:03 +0100

    Fix entity schema manager service name

--- classes/module/Module.php
+++ classes/module/Module.php
@@ -3815,7 +3815,7 @@ abstract class ModuleCore implements ModuleInterface
      */
     public function getEntitySchemaManager(): EntitySchemaManager
     {
-        return $this->get('prestashop.core.util.database.entity_cchema_manager');
+        return $this->get('prestashop.core.util.database.entity_schema_manager');
     }
 }
 
