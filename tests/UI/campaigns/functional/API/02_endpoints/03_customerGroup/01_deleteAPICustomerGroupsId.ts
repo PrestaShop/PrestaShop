f2bd8afc06d894a9bd6bca3ce3ad15f3db15c89c
Progi1984 <franck.lefevre@prestashop.com>
2024-03-05 09:19:00 +0100

    Functional Tests : API : DELETE /api/customers/group/{customerGroupId}

---
+++ tests/UI/campaigns/functional/API/02_endpoints/03_customerGroup/01_deleteAPICustomerGroupsId.ts
@@ -0,0 +1,234 @@
+// Import utils
+import api from '@utils/api';
+import helper from '@utils/helpers';
+import testContext from '@utils/testContext';
+
+// Import commonTests
+import {deleteAPIClientTest} from '@commonTests/BO/advancedParameters/authServer';
+import setFeatureFlag from '@commonTests/BO/advancedParameters/newFeatures';
+import loginCommon from '@commonTests/BO/loginBO';
+
+// Import pages
+import featureFlagPage from '@pages/BO/advancedParameters/featureFlag';
+import apiClientPage from 'pages/BO/advancedParameters/APIClient';
+import addNewApiClientPage from '@pages/BO/advancedParameters/APIClient/add';
+import dashboardPage from '@pages/BO/dashboard';
+import customerSettingsPage from '@pages/BO/shopParameters/customerSettings';
+import groupsPage from '@pages/BO/shopParameters/customerSettings/groups';
+import addGroupPage from '@pages/BO/shopParameters/customerSettings/groups/add';
+
+// Import data
+import APIClientData from '@data/faker/APIClient';
+import GroupData from '@data/faker/group';
+
+import {expect} from 'chai';
+import type {APIRequestContext, BrowserContext, Page} from 'playwright';
+
+const baseContext: string = 'functional_API_endpoints_customerGroup_deleteAPICustomerGroupsId';
+
+describe('API : DELETE /api/customers/group/{customerGroupId}', async () => {
+  let apiContext: APIRequestContext;
+  let browserContext: BrowserContext;
+  let page: Page;
+  let numberOfGroups: number;
+  let idCustomerGroup: number;
+  let clientSecret: string;
+  let accessToken: string;
+
+  const clientScope: string = 'customer_group_write';
+  const clientClient: APIClientData = new APIClientData({
+    enabled: true,
+    scopes: [
+      clientScope,
+    ],
+  });
+  const createGroupData: GroupData = new GroupData();
+
+  before(async function () {
+    browserContext = await helper.createBrowserContext(this.browser);
+    page = await helper.newTab(browserContext);
+
+    apiContext = await helper.createAPIContext(global.BO.URL);
+  });
+
+  after(async () => {
+    await helper.closeBrowserContext(browserContext);
+  });
+
+  // Pre-condition: Enable experimental feature : Authorization server
+  setFeatureFlag(featureFlagPage.featureFlagAuthorizationServer, true, `${baseContext}_enableAuthorizationServer`);
+
+  describe('BackOffice : Fetch the access token', async () => {
+    it('should login in BO', async function () {
+      await loginCommon.loginBO(this, page);
+    });
+
+    it('should go to \'Advanced Parameters > API Client\' page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToAuthorizationServerPage', baseContext);
+
+      await dashboardPage.goToSubMenu(
+        page,
+        dashboardPage.advancedParametersLink,
+        dashboardPage.authorizationServerLink,
+      );
+
+      const pageTitle = await apiClientPage.getPageTitle(page);
+      expect(pageTitle).to.eq(apiClientPage.pageTitle);
+    });
+
+    it('should check that no records found', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'checkThatNoRecordFound', baseContext);
+
+      const noRecordsFoundText = await apiClientPage.getTextForEmptyTable(page);
+      expect(noRecordsFoundText).to.contains('warning No records found');
+    });
+
+    it('should go to add New API Client page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToNewAPIClientPage', baseContext);
+
+      await apiClientPage.goToNewAPIClientPage(page);
+
+      const pageTitle = await addNewApiClientPage.getPageTitle(page);
+      expect(pageTitle).to.eq(addNewApiClientPage.pageTitleCreate);
+    });
+
+    it('should create API Client', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'createAPIClient', baseContext);
+
+      const textResult = await addNewApiClientPage.addAPIClient(page, clientClient);
+      expect(textResult).to.contains(addNewApiClientPage.successfulCreationMessage);
+
+      const textMessage = await addNewApiClientPage.getAlertInfoBlockParagraphContent(page);
+      expect(textMessage).to.contains(addNewApiClientPage.apiClientGeneratedMessage);
+    });
+
+    it('should copy client secret', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'copyClientSecret', baseContext);
+
+      await addNewApiClientPage.copyClientSecret(page);
+
+      clientSecret = await addNewApiClientPage.getClipboardText(page);
+      expect(clientSecret.length).to.be.gt(0);
+    });
+
+    it('should request the endpoint /admin-dev/api/oauth2/token', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'requestOauth2Token', baseContext);
+
+      const apiResponse = await apiContext.post('api/oauth2/token', {
+        form: {
+          client_id: clientClient.clientId,
+          client_secret: clientSecret,
+          grant_type: 'client_credentials',
+          scope: clientScope,
+        },
+      });
+      expect(apiResponse.status()).to.eq(200);
+      expect(api.hasResponseHeader(apiResponse, 'Content-Type')).to.eq(true);
+      expect(api.getResponseHeader(apiResponse, 'Content-Type')).to.contains('application/json');
+
+      const jsonResponse = await apiResponse.json();
+      expect(jsonResponse).to.have.property('access_token');
+      expect(jsonResponse.token_type).to.be.a('string');
+
+      accessToken = jsonResponse.access_token;
+    });
+  });
+
+  describe('BackOffice : Create a Customer Group', async () => {
+    it('should go to \'Shop Parameters > Customer Settings\' page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToCustomerSettingsPage', baseContext);
+
+      await dashboardPage.goToSubMenu(
+        page,
+        dashboardPage.shopParametersParentLink,
+        dashboardPage.customerSettingsLink,
+      );
+      await customerSettingsPage.closeSfToolBar(page);
+
+      const pageTitle = await customerSettingsPage.getPageTitle(page);
+      expect(pageTitle).to.contains(customerSettingsPage.pageTitle);
+    });
+
+    it('should go to \'Groups\' page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToGroupsPage', baseContext);
+
+      await customerSettingsPage.goToGroupsPage(page);
+
+      const pageTitle = await groupsPage.getPageTitle(page);
+      expect(pageTitle).to.contains(groupsPage.pageTitle);
+    });
+
+    it('should reset all filters and get number of groups in BO', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'resetFilterFirst', baseContext);
+
+      numberOfGroups = await groupsPage.resetAndGetNumberOfLines(page);
+      expect(numberOfGroups).to.be.above(0);
+    });
+
+    it('should go to add new group page', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'goToAddNewGroup', baseContext);
+
+      await groupsPage.goToNewGroupPage(page);
+
+      const pageTitle = await addGroupPage.getPageTitle(page);
+      expect(pageTitle).to.contains(addGroupPage.pageTitleCreate);
+    });
+
+    it('should create group and check result', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'createGroup', baseContext);
+
+      const textResult = await addGroupPage.createEditGroup(page, createGroupData);
+      expect(textResult).to.contains(groupsPage.successfulCreationMessage);
+
+      const numberOfGroupsAfterCreation = await groupsPage.getNumberOfElementInGrid(page);
+      expect(numberOfGroupsAfterCreation).to.be.equal(numberOfGroups + 1);
+    });
+
+    it('should filter list by name', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'filterForCreation', baseContext);
+
+      await groupsPage.resetFilter(page);
+      await groupsPage.filterTable(page, 'input', 'b!name', createGroupData.name);
+
+      const numberOfGroupsAfterCreation = await groupsPage.getNumberOfElementInGrid(page);
+      expect(numberOfGroupsAfterCreation).to.be.equal(1);
+
+      const textEmail = await groupsPage.getTextColumn(page, 1, 'b!name');
+      expect(textEmail).to.contains(createGroupData.name);
+
+      idCustomerGroup = parseInt(await groupsPage.getTextColumn(page, 1, 'id_group'), 10);
+      expect(idCustomerGroup).to.be.gt(0);
+    });
+  });
+
+  describe('API : Delete the Customer Group', async () => {
+    it('should request the endpoint /api/customers/group/{customerGroupId}', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'requestEndpoint', baseContext);
+
+      const apiResponse = await apiContext.delete(`api/customers/group/${idCustomerGroup}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      expect(apiResponse.status()).to.eq(204);
+    });
+  });
+
+  describe('BackOffice : Check the Customer Group is deleted', async () => {
+    it('should filter list by name', async function () {
+      await testContext.addContextItem(this, 'testIdentifier', 'filterAfterDeletion', baseContext);
+
+      await groupsPage.resetFilter(page);
+      await groupsPage.filterTable(page, 'input', 'b!name', createGroupData.name);
+
+      const numberOfGroupsAfterCreation = await groupsPage.getNumberOfElementInGrid(page);
+      expect(numberOfGroupsAfterCreation).to.be.equal(0);
+    });
+  });
+
+  // Pre-condition: Create an API Client
+  deleteAPIClientTest(`${baseContext}_postTest`);
+
+  // Post-condition: Disable experimental feature : Authorization server
+  setFeatureFlag(featureFlagPage.featureFlagAuthorizationServer, false, `${baseContext}_disableAuthorizationServer`);
+});
