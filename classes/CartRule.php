2e272487e47d38a4d0e8057849be9bda943ad86a
Prestasafe <contact@prestasafe.com>
2024-04-05 11:53:54 +0200

    Fix error: CartRuleCore::checkProductRestrictionsFromCart(): Argument #1 ($cart) must be of type CartCore, null given,
    Fix error of cart null in customer ViewAction:
    ```
    CartRuleCore::checkProductRestrictionsFromCart(): Argument #1 ($cart) must be of type CartCore, null given, called in /home/xxx/www/classes/CartRule.php on line 469
    
    [TypeError 0]
    ```
    The cart to analyse should be the customer cart and not the context cart.

--- classes/CartRule.php
+++ classes/CartRule.php
@@ -395,7 +395,7 @@ class CartRuleCore extends ObjectModel
         // Basic part of the query, we are selecting all cart rules
         $sql = '
             SELECT SQL_NO_CACHE * FROM `' . _DB_PREFIX_ . 'cart_rule` cr
-            LEFT JOIN `' . _DB_PREFIX_ . 'cart_rule_lang` crl 
+            LEFT JOIN `' . _DB_PREFIX_ . 'cart_rule_lang` crl
             ON (cr.`id_cart_rule` = crl.`id_cart_rule` AND crl.`id_lang` = ' . (int) $id_lang . ')';
 
         // We will definitely include vouchers for this specific customer
@@ -472,7 +472,7 @@ class CartRuleCore extends ObjectModel
             foreach ($result as $key => $cart_rule) {
                 if ($cart_rule['product_restriction']) {
                     $cr = new CartRule((int) $cart_rule['id_cart_rule']);
-                    $r = $cr->checkProductRestrictionsFromCart(Context::getContext()->cart, false, false);
+                    $r = $cr->checkProductRestrictionsFromCart($cart, false, false);
                     if ($r !== false) {
                         continue;
                     }
