7e9df5bdfaffed4f360a5033241fa2c8e2b1f146
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2024-04-17 15:18:25 +0200

    Add new test to select color on hover on product list

---
+++ tests/UI/campaigns/functional/FO/classic/06_homePage/05_selectColor.ts
@@ -0,0 +1,92 @@
+// Import utils
+import helper from '@utils/helpers';
+import testContext from '@utils/testContext';
+
+// Import pages
+import {homePage} from '@pages/FO/classic/home';
+import {productPage} from '@pages/FO/classic/product';
+
+// Import demo data
+import Products from '@data/demo/products';
+
+import {expect} from 'chai';
+import type {BrowserContext, Page} from 'playwright';
+
+const baseContext: string = 'functional_FO_classic_homePage_selectColor';
+
+describe('FO - Home Page : Select color on hover on product list', async () => {
+  let browserContext: BrowserContext;
+  let page: Page;
+
+  // before and after functions
+  before(async function () {
+    browserContext = await helper.createBrowserContext(this.browser);
+    page = await helper.newTab(browserContext);
+  });
+
+  after(async () => {
+    await helper.closeBrowserContext(browserContext);
+  });
+
+  it('should open the shop page', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'goToShopFO', baseContext);
+
+    await homePage.goTo(page, global.FO.URL);
+
+    const result = await homePage.isHomePage(page);
+    expect(result).to.equal(true);
+  });
+
+  it('should put the mouse over the first product and check that quick view button is displayed', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'putMouseOnFirstProduct', baseContext);
+
+    const isQuickViewLinkVisible = await homePage.isQuickViewLinkVisible(page, 1);
+    expect(isQuickViewLinkVisible).to.equal(true);
+
+    const isBoxesVisible = await homePage.isColoredBoxesVisible(page, 1);
+    expect(isBoxesVisible).to.equal(true);
+  });
+
+  it('should select the color White for the first product in list', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'selectColor1', baseContext);
+
+    await homePage.selectProductColor(page, 1, 'White');
+
+    const pageTitle = await productPage.getPageTitle(page);
+    expect(pageTitle).to.contains(Products.demo_1.name);
+  });
+
+  it('should check that the displayed product is white', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'checkDisplayedProduct', baseContext);
+
+    const pageURL = await productPage.getCurrentURL(page);
+    expect(pageURL).to.contains('color-white')
+      .and.to.contains('size-m');
+  });
+
+  it('should go to Home page', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'goToHomePage', baseContext);
+
+    await productPage.goToHomePage(page);
+
+    const isHomePage = await homePage.isHomePage(page);
+    expect(isHomePage, 'Home page is not displayed').to.eq(true);
+  });
+
+  it('should select the color Black for the first product in list', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'selectColor2', baseContext);
+
+    await homePage.selectProductColor(page, 1, 'Black');
+
+    const pageTitle = await productPage.getPageTitle(page);
+    expect(pageTitle).to.contains(Products.demo_1.name);
+  });
+
+  it('should check that the displayed product is white', async function () {
+    await testContext.addContextItem(this, 'testIdentifier', 'checkDisplayedProduct2', baseContext);
+
+    const pageURL = await productPage.getCurrentURL(page);
+    expect(pageURL).to.contains('color-black')
+      .and.to.contains('size-s');
+  });
+});
