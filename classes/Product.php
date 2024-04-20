1eb09c7f72eef6a53a218b27a5865e307c1d7ca9
maxldn5 <166727247+maxldn5@users.noreply.github.com>
2024-04-21 01:41:09 +0200

    Use attribute public names instead of private when appending combinations to URL
    The query in getAttributeParams() currently fetches al.name, which contains attribute private names. Since this is publicly visible to the user in the URL, we should fetch the public_name instead.

--- classes/Product.php
+++ classes/Product.php
@@ -7146,7 +7146,7 @@ class ProductCore extends ObjectModel
 
         if (!Cache::isStored($cache_id)) {
             $result = Db::getInstance()->executeS('
-            SELECT a.`id_attribute`, a.`id_attribute_group`, al.`name`, agl.`name` as `group`,
+            SELECT a.`id_attribute`, a.`id_attribute_group`, al.`name`, agl.`public_name` as `group`,
             pa.`reference`, pa.`ean13`, pa.`isbn`, pa.`upc`, pa.`mpn`,
             pal.`available_now`, pal.`available_later`
             FROM `' . _DB_PREFIX_ . 'attribute` a
