8a6e725d26e85cc356a74d9f5d1c9219989f8f24
Jonathan Lelievre <jo.lelievre@gmail.com>
2024-04-24 20:56:47 +0200

    Adapt AccessDeniedListener

--- src/PrestaShopBundle/EventListener/Admin/AccessDeniedListener.php
+++ src/PrestaShopBundle/EventListener/Admin/AccessDeniedListener.php
@@ -38,6 +38,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Contracts\Translation\TranslatorInterface;
@@ -58,7 +59,7 @@ class AccessDeniedListener
     public function onKernelException(ExceptionEvent $event)
     {
         if (!$event->isMainRequest()
-            || !$event->getThrowable() instanceof AccessDeniedException
+            || (!$event->getThrowable() instanceof AccessDeniedException && !$event->getThrowable() instanceof AccessDeniedHttpException)
         ) {
             return;
         }
