3b8034fe54aa5b7b9f893bb4e744d2376d6fa636
Jonathan Lelievre <jo.lelievre@gmail.com>
2024-06-26 10:16:16 +0200

    Use email type for forgotten password to get front js validation

--- src/PrestaShopBundle/Form/Admin/Login/RequestPasswordResetType.php
+++ src/PrestaShopBundle/Form/Admin/Login/RequestPasswordResetType.php
@@ -27,10 +27,10 @@
 namespace PrestaShopBundle\Form\Admin\Login;
 
 use PrestaShopBundle\Form\Admin\Type\ButtonCollectionType;
+use PrestaShopBundle\Form\Admin\Type\EmailType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Email;
@@ -46,7 +46,7 @@ class RequestPasswordResetType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email_forgot', TextType::class, [
+            ->add('email_forgot', EmailType::class, [
                 'required' => true,
                 'label' => $this->translator->trans('Email address', [], 'Admin.Global'),
                 'constraints' => [
