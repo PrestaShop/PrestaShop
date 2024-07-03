aa59f76fd56e529e1203e11b20ec2bee3479e28b
Hlavtox <daniel.hlavacek@hotmail.cz>
2024-07-03 20:24:57 +0200

    Fix precision of tax calculator

--- src/Core/Cart/Calculator.php
+++ src/Core/Cart/Calculator.php
@@ -174,16 +174,34 @@ class Calculator
             throw new \Exception('Cart must be processed before getting its total');
         }
 
-        $amount = $this->getRowTotalWithoutDiscount();
+        /*
+         * First, we get the total price of items in the cart with no discounts.
+         * We will round everything once again, because some prices in the sum may not be rounded
+         * to keep their precision.
+         */
+        $amount = $this->rounded($this->getRowTotalWithoutDiscount(), $this->computePrecision);
+
+        /*
+         * Now, we subtract the amount of discounts from this sum.
+         */
         $amount = $amount->sub($this->rounded($this->getDiscountTotal(), $this->computePrecision));
+
+        /*
+         * Next thing, we add a rounded value for shipping fees.
+         */
         $shippingFees = $this->fees->getInitialShippingFees();
         if (null !== $shippingFees) {
             $amount = $amount->add($this->rounded($shippingFees, $this->computePrecision));
         }
+
+        /*
+         * And absolutely last, we add wrapping fees if they are present.
+         */
         $wrappingFees = $this->fees->getFinalWrappingFees();
         if (null !== $wrappingFees) {
             $amount = $amount->add($this->rounded($wrappingFees, $this->computePrecision));
         }
+
 
         return $amount;
     }
@@ -204,6 +222,9 @@ class Calculator
     }
 
     /**
+     * This method returns sum of all cart lines - products.
+     * It returns the prices with their original price, without applying any cart rules on them.
+     *
      * @return AmountImmutable
      *
      * @throws \Exception
@@ -212,7 +233,19 @@ class Calculator
     {
         $amount = new AmountImmutable();
         foreach ($this->cartRows as $cartRow) {
-            $amount = $amount->add($cartRow->getInitialTotalPrice());
+            /*
+             * We round the tax included price only, to keep the calculations using
+             * the displayed prices the user sees. If he sees 999 EUR, we will use this
+             * rounded values for calculations, no magic 999.5 EUR behind this.
+             *
+             * For tax excluded price however, we keep the non-rounded value, because we could
+             * have a big difference at the end of the calculation. We will round the tax
+             * excluded price as a last thing.
+             *
+             * If we decide one day to support using tax excluded price as the primary one, we
+             * can reverse this the other way around.
+             */
+            $amount = $amount->add($this->roundedTaxIncluded($cartRow->getInitialTotalPrice(), $this->computePrecision));
         }
 
         return $amount;
@@ -312,7 +345,7 @@ class Calculator
     }
 
     /**
-     * calculate only product rows.
+     * Calculate only product rows.
      */
     public function calculateRows()
     {
@@ -322,7 +355,7 @@ class Calculator
     }
 
     /**
-     * calculate only cart rules (rows and fees have to be calculated first).
+     * Calculate only cart rules (rows and fees have to be calculated first).
      */
     public function calculateCartRules()
     {
@@ -363,6 +396,9 @@ class Calculator
     }
 
     /**
+     * Rounds both values in the amount by given precision. Useful for total sums,
+     * where no additional precision is required and we want the final price.
+     *
      * @param AmountImmutable $amount
      * @param int $computePrecision
      *
@@ -373,6 +409,24 @@ class Calculator
         return new AmountImmutable(
             Tools::ps_round($amount->getTaxIncluded(), $computePrecision),
             Tools::ps_round($amount->getTaxExcluded(), $computePrecision)
+        );
+    }
+
+    /**
+     * Rounds only tax included price by given precision and leaves the tax
+     * excluded price with original precision. Useful for inter-calculations
+     * when we have one primary price to use.
+     *
+     * @param AmountImmutable $amount
+     * @param int $computePrecision
+     *
+     * @return AmountImmutable
+     */
+    private function roundedTaxIncluded(AmountImmutable $amount, int $computePrecision)
+    {
+        return new AmountImmutable(
+            Tools::ps_round($amount->getTaxIncluded(), $computePrecision),
+            $amount->getTaxExcluded()
         );
     }
 }
