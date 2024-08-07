70f9da8d452932de3a6d7cf57bb6e3277b254927
boherm <boris.hermans@prestashop.com>
2024-08-07 17:01:15 +0200

    Fix tests for carrier form data provider

--- tests/Unit/Core/Form/IdentifiableObject/DataProvider/CarrierFormDataProviderTest.php
+++ tests/Unit/Core/Form/IdentifiableObject/DataProvider/CarrierFormDataProviderTest.php
@@ -27,9 +27,14 @@
 namespace Core\Form\IdentifiableObject\DataProvider;
 
 use PHPUnit\Framework\TestCase;
+use PrestaShop\PrestaShop\Adapter\Form\ChoiceProvider\ZoneByIdChoiceProvider;
 use PrestaShop\PrestaShop\Core\CommandBus\CommandBusInterface;
+use PrestaShop\PrestaShop\Core\ConfigurationInterface;
 use PrestaShop\PrestaShop\Core\Context\ShopContext;
+use PrestaShop\PrestaShop\Core\Currency\CurrencyDataProviderInterface;
 use PrestaShop\PrestaShop\Core\Domain\Carrier\Query\GetCarrierForEditing;
+use PrestaShop\PrestaShop\Core\Domain\Carrier\Query\GetCarrierRanges;
+use PrestaShop\PrestaShop\Core\Domain\Carrier\QueryResult\CarrierRangesCollection;
 use PrestaShop\PrestaShop\Core\Domain\Carrier\QueryResult\EditableCarrier;
 use PrestaShop\PrestaShop\Core\Domain\Carrier\ValueObject\OutOfRangeBehavior;
 use PrestaShop\PrestaShop\Core\Form\IdentifiableObject\DataProvider\CarrierFormDataProvider;
@@ -38,37 +43,80 @@ class CarrierFormDataProviderTest extends TestCase
 {
     public function testGetData(): void
     {
+        // Create a mock for CommandBusInterface
         $queryBus = $this->createMock(CommandBusInterface::class);
         $queryBus
             ->method('handle')
-            ->with($this->isInstanceOf(GetCarrierForEditing::class))
-            ->willReturn(new EditableCarrier(
-                42,
-                'Carrier name',
-                5,
-                'http://track.to',
-                1,
-                true,
-                [
-                    1 => 'English delay',
-                    2 => 'French delay',
-                ],
-                1234,
-                1123,
-                3421,
-                1657,
-                [1, 2, 3],
-                false,
-                true,
-                1,
-                1,
-                OutOfRangeBehavior::USE_HIGHEST_RANGE,
-                [1, 3],
-                '/img/c/45.jkg',
-            ))
+            ->withConsecutive(
+                [$this->isInstanceOf(GetCarrierForEditing::class)],
+                [$this->isInstanceOf(GetCarrierRanges::class)]
+            )
+            ->willReturnOnConsecutiveCalls(
+                new EditableCarrier(
+                    42,
+                    'Carrier name',
+                    5,
+                    'http://track.to',
+                    1,
+                    true,
+                    [
+                        1 => 'English delay',
+                        2 => 'French delay',
+                    ],
+                    1234,
+                    1123,
+                    3421,
+                    1657,
+                    [1, 2, 3],
+                    false,
+                    true,
+                    1,
+                    1,
+                    OutOfRangeBehavior::USE_HIGHEST_RANGE,
+                    [1, 3],
+                    '/img/c/45.jkg',
+                ),
+                new CarrierRangesCollection([
+                    ['id_zone' => 1, 'range_from' => 0, 'range_to' => 10, 'range_price' => '10.00'],
+                    ['id_zone' => 1, 'range_from' => 10, 'range_to' => 20, 'range_price' => '11.00'],
+                    ['id_zone' => 1, 'range_from' => 20, 'range_to' => 25, 'range_price' => '12.00'],
+                    ['id_zone' => 2, 'range_from' => 0, 'range_to' => 10, 'range_price' => '20.00'],
+                    ['id_zone' => 2, 'range_from' => 10, 'range_to' => 20, 'range_price' => '21.00'],
+                    ['id_zone' => 2, 'range_from' => 20, 'range_to' => 25, 'range_price' => '22.00'],
+                ])
+            )
         ;
 
-        $formDataProvider = new CarrierFormDataProvider($queryBus, $this->createMock(ShopContext::class));
+        // Create a mock for CurrencyDataProviderInterface
+        $currencyDataProvider = $this->createMock(CurrencyDataProviderInterface::class);
+        $currencyDataProvider
+            ->method('getDefaultCurrencySymbol')
+            ->willReturn('€');
+
+        // Create a mock for ConfigurationInterface
+        $configuration = $this->createMock(ConfigurationInterface::class);
+        $configuration
+            ->method('get')
+            ->with('PS_WEIGHT_UNIT')
+            ->willReturn('kg');
+
+        // Create a mock for ZoneByIdChoiceProvider
+        $zonesChoiceProvider = $this->createMock(ZoneByIdChoiceProvider::class);
+        $zonesChoiceProvider
+            ->method('getChoices')
+            ->willReturn([
+                'Zone A' => 1,
+                'Zone B' => 2,
+                'Zone C' => 3,
+            ]);
+
+        $formDataProvider = new CarrierFormDataProvider(
+            $queryBus,
+            $this->createMock(ShopContext::class),
+            $currencyDataProvider,
+            $configuration,
+            $zonesChoiceProvider
+        );
         $formData = $formDataProvider->getData(42);
         $this->assertEquals([
             'general_settings' => [
@@ -90,6 +138,28 @@ class CarrierFormDataProviderTest extends TestCase
                 'shipping_method' => 1,
                 'id_tax_rule_group' => 1,
                 'range_behavior' => OutOfRangeBehavior::USE_HIGHEST_RANGE,
+                'zones' => [1, 2],
+                'ranges' => [
+                    'data' => json_encode([
+                        ['from' => '0', 'to' => '10'],
+                        ['from' => '10', 'to' => '20'],
+                        ['from' => '20', 'to' => '25'],
+                    ]),
+                ],
+                'ranges_costs' => [
+                    'zones' => [
+                        ['zoneId' => 1, 'zoneName' => 'Zone A', 'ranges' => [
+                            ['range' => '0kg - 10kg', 'from' => '0', 'to' => '10', 'price' => '10'],
+                            ['range' => '10kg - 20kg', 'from' => '10', 'to' => '20', 'price' => '11'],
+                            ['range' => '20kg - 25kg', 'from' => '20', 'to' => '25', 'price' => '12'],
+                        ]],
+                        ['zoneId' => 2, 'zoneName' => 'Zone B', 'ranges' => [
+                            ['range' => '0kg - 10kg', 'from' => '0', 'to' => '10', 'price' => '20'],
+                            ['range' => '10kg - 20kg', 'from' => '10', 'to' => '20', 'price' => '21'],
+                            ['range' => '20kg - 25kg', 'from' => '20', 'to' => '25', 'price' => '22'],
+                        ]],
+                    ],
+                ],
             ],
             'size_weight_settings' => [
                 'max_width' => 1234,
@@ -104,7 +174,13 @@ class CarrierFormDataProviderTest extends TestCase
     {
         $shopContext = $this->createMock(ShopContext::class);
         $shopContext->method('getAssociatedShopIds')->willReturn([2, 4]);
-        $formDataProvider = new CarrierFormDataProvider($this->createMock(CommandBusInterface::class), $shopContext);
+        $formDataProvider = new CarrierFormDataProvider(
+            $this->createMock(CommandBusInterface::class),
+            $shopContext,
+            $this->createMock(CurrencyDataProviderInterface::class),
+            $this->createMock(ConfigurationInterface::class),
+            $this->createMock(ZoneByIdChoiceProvider::class)
+        );
         $this->assertEquals([
             'general_settings' => [
                 'grade' => 0,
