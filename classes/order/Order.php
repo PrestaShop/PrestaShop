74d5c5c924350feab0e6b3707bdb1e9488252ccf
Dominik Shaim Ulrich <czshaimagal@gmail.com>
2024-08-21 00:16:44 +0200

    Update Order.php

--- classes/order/Order.php
+++ classes/order/Order.php
@@ -752,10 +752,7 @@ class OrderCore extends ObjectModel
             $product['current_stock'] = StockAvailable::getQuantityAvailableByProduct($product['product_id'], $product['product_attribute_id'], (int) $this->id_shop);
         }
 
-        $product['location'] = StockAvailable::getLocation($product['product_id'], $product['product_attribute_id'], (int) $this->id_shop);
-        if ($product['location'] == null) {
-            $product['location'] = false;
-        }
+        $product['location'] = (string) StockAvailable::getLocation($product['product_id'], $product['product_attribute_id'], (int) $this->id_shop);
     }
 
     /**
