216874fb86833942c02d852e8e34fc13f79559c0
Codencode <info@codencode.it>
2024-09-20 11:24:38 +0200

    Update Cart.php
    Added check on the 'active' field and verified the voucher expiration

--- classes/Cart.php
+++ classes/Cart.php
@@ -480,8 +480,10 @@ class CartCore extends ObjectModel
                 WHERE `id_cart` = ' . (int) $this->id . '
                 ' . ($filter == CartRule::FILTER_ACTION_SHIPPING ? 'AND free_shipping = 1' : '') . '
                 ' . ($filter == CartRule::FILTER_ACTION_GIFT ? 'AND gift_product != 0' : '') . '
-                ' . ($filter == CartRule::FILTER_ACTION_REDUCTION ? 'AND (reduction_percent != 0 OR reduction_amount != 0)' : '')
-                . ' ORDER by cr.priority ASC, cr.gift_product DESC'
+                ' . ($filter == CartRule::FILTER_ACTION_REDUCTION ? 'AND (reduction_percent != 0 OR reduction_amount != 0)' : '') . '
+                AND `active` = 1
+                AND NOW() BETWEEN `date_from` AND `date_to`
+                ORDER by cr.priority ASC, cr.gift_product DESC'
             );
             Cache::store($cache_key, $result);
         } else {
