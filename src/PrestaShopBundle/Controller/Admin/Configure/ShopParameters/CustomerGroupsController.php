6c4a351b9199c43658a609472d7f3340f2b050bd
Jonathan LELIEVRE <jonathan.lelievre@prestashop.com>
2024-09-23 22:18:49 +0200

    Refact Customer group controller

--- src/PrestaShopBundle/Controller/Admin/Configure/ShopParameters/CustomerGroupsController.php
+++ src/PrestaShopBundle/Controller/Admin/Configure/ShopParameters/CustomerGroupsController.php
@@ -28,16 +28,19 @@ declare(strict_types=1);
 
 namespace PrestaShopBundle\Controller\Admin\Configure\ShopParameters;
 
+use PrestaShop\PrestaShop\Adapter\LegacyContext;
+use PrestaShop\PrestaShop\Core\Grid\GridFactoryInterface;
 use PrestaShop\PrestaShop\Core\Search\Filters\CustomerGroupsFilters;
-use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
+use PrestaShopBundle\Controller\Admin\PrestaShopAdminController;
 use PrestaShopBundle\Security\Attribute\AdminSecurity;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Controller responsible for "Configure > Shop Parameters > Customer Settings > Groups" page.
  */
-class CustomerGroupsController extends FrameworkBundleAdminController
+class CustomerGroupsController extends PrestaShopAdminController
 {
     /**
      * Show Groups tab.
@@ -48,14 +51,17 @@ class CustomerGroupsController extends FrameworkBundleAdminController
      * @return Response
      */
     #[AdminSecurity("is_granted('read', request.get('_legacy_controller'))", message: 'Access denied.')]
-    public function indexAction(Request $request, CustomerGroupsFilters $filters): Response
-    {
-        $customerGroupsGridFactory = $this->get('prestashop.core.grid.factory.customer_groups');
+    public function indexAction(
+        Request $request,
+        CustomerGroupsFilters $filters,
+        #[Autowire(service: 'prestashop.core.grid.factory.customer_groups')]
+        GridFactoryInterface $customerGroupsGridFactory,
+    ): Response {
         $customerGroupsGrid = $customerGroupsGridFactory->getGrid($filters);
 
         return $this->render('@PrestaShop/Admin/Configure/ShopParameters/CustomerSettings/Groups/index.html.twig', [
             'customerGroupsGrid' => $this->presentGrid($customerGroupsGrid),
-            'layoutTitle' => $this->trans('Groups', 'Admin.Navigation.Menu'),
+            'layoutTitle' => $this->trans('Groups', [], 'Admin.Navigation.Menu'),
             'help_link' => $this->generateSidebarLink($request->attributes->get('_legacy_controller')),
             'enableSidebar' => true,
         ]);
@@ -67,13 +73,12 @@ class CustomerGroupsController extends FrameworkBundleAdminController
      * @return Response
      */
     #[AdminSecurity("is_granted('create', request.get('_legacy_controller'))", redirectRoute: 'admin_customer_groups_index', message: 'You need permission to create this.')]
-    public function createAction(): Response
+    public function createAction(LegacyContext $legacyContext): Response
     {
         return $this->redirect(
-            $this->getContext()->link->getAdminLink(
+            $legacyContext->getAdminLink(
                 'AdminGroups',
                 true,
-                [],
                 [
                     'addgroup' => '',
                 ]
@@ -89,13 +94,14 @@ class CustomerGroupsController extends FrameworkBundleAdminController
      * @return Response
      */
     #[AdminSecurity("is_granted('update', request.get('_legacy_controller'))", redirectRoute: 'admin_customer_groups_index', message: 'You need permission to edit this.')]
-    public function editAction(int $groupId): Response
-    {
+    public function editAction(
+        int $groupId,
+        LegacyContext $legacyContext,
+    ): Response {
         return $this->redirect(
-            $this->getContext()->link->getAdminLink(
+            $legacyContext->getAdminLink(
                 'AdminGroups',
                 true,
-                [],
                 [
                     'updategroup' => '',
                     'id_group' => $groupId,
