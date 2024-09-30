5eb8cd6b9a30df42ac3be6a7a557342daa42b261
Codencode <info@codencode.it>
2024-09-30 09:11:39 +0200

    Update Cart.php

--- classes/Cart.php
+++ classes/Cart.php
@@ -481,9 +481,12 @@ class CartCore extends ObjectModel
                 ' . ($filter == CartRule::FILTER_ACTION_SHIPPING ? 'AND free_shipping = 1' : '') . '
                 ' . ($filter == CartRule::FILTER_ACTION_GIFT ? 'AND gift_product != 0' : '') . '
                 ' . ($filter == CartRule::FILTER_ACTION_REDUCTION ? 'AND (reduction_percent != 0 OR reduction_amount != 0)' : '') . '
-                AND `active` = 1
-                AND NOW() BETWEEN `date_from` AND `date_to`
-                ORDER by cr.priority ASC, cr.gift_product DESC'
+                AND cr.`active` = 1
+                AND cr.`quantity` > 0
+                AND cr.`deleted` = 0
+                AND cr.`date_from` <= "' . date('Y-m-d H:i:s') . '"
+                AND cr.`date_to` >= "' . date('Y-m-d H:i:s') . '"
+                ORDER by cr.`priority` ASC, cr.`gift_product` DESC'
             );
             Cache::store($cache_key, $result);
         } else {
