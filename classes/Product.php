800e8e9865f618381feed6238e36e1cb857e8c49
mrkalchemy <123658706+mrkalchemy@users.noreply.github.com>
2024-11-06 09:15:55 +0000

    style-change
    PS folk don't like non-strict comparisons.

--- classes/Product.php
+++ classes/Product.php
@@ -3893,7 +3893,7 @@ class ProductCore extends ObjectModel
             !(
                 $specific_price['price'] >= 0 &&
                 $specific_price['id_currency'] &&
-                $id_currency == $specific_price['id_currency']
+                (int) $id_currency === (int) $specific_price['id_currency']
             )
         ) {
             $price = Tools::convertPrice($price, $id_currency);
