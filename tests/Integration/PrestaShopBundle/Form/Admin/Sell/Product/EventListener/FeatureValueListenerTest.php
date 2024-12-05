e2963307722f1a094c76a9c7ad7a0dbcb528e846
Touxten <156076965+Touxten@users.noreply.github.com>
2024-12-05 08:10:09 +0100

    Update FeatureValueListenerTest.php

--- tests/Integration/PrestaShopBundle/Form/Admin/Sell/Product/EventListener/FeatureValueListenerTest.php
+++ tests/Integration/PrestaShopBundle/Form/Admin/Sell/Product/EventListener/FeatureValueListenerTest.php
@@ -136,7 +136,7 @@ class FeatureValueListenerTest extends FormListenerTestCase
                 'feature_id' => 42,
                 'custom_value' => [
                     1 => '',
-                    2 => null,
+                    2 => false,
                 ],
             ],
             [
@@ -200,7 +200,7 @@ class FeatureValueListenerTest extends FormListenerTestCase
                 'feature_id' => 42,
                 'custom_value' => [
                     1 => 'custom',
-                    2 => null,
+                    2 => false,
                 ],
             ],
             [],
