4cb6397ab57096af1bfeb8cac96dd7a36a9327e8
Nicolas Dabène <ndabene@businesstech.fr>
2025-03-28 14:12:35 +0100

    Add hook for additional content in cart product lines
    Introduced a new hook, `displayCartProductLineAdditionalContent`, to allow for the inclusion of additional content for each product line in the cart. This enhances the cart's extensibility by enabling developers to customize the display of product information.

--- classes/Cart.php
+++ classes/Cart.php
@@ -845,6 +845,16 @@ class CartCore extends ObjectModel
                 }
 
                 $this->_products[] = $product;
+
+                // Add hook for additional content on each cart product line
+                $additionalContent = Hook::exec('displayCartProductLineAdditionalContent', [
+                    'product' => $product,
+                    'cart' => $this
+                ], null, true);
+
+                if (!empty($additionalContent)) {
+                    $product['additional_content'] = $additionalContent;
+                }
             }
         } else {
             $this->_products = $products;
