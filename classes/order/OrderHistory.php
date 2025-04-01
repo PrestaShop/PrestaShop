46ecb1a8ce6ca749e1a75c9c05dcb1feff84ead2
jeremie <jeremie@kiwik.com>
2025-04-01 09:25:25 +0200

    move the duplicate part in a new method

--- classes/order/OrderHistory.php
+++ classes/order/OrderHistory.php
@@ -388,54 +388,12 @@ class OrderHistoryCore extends ObjectModel
                 foreach ($invoices as $invoice) {
                     /** @var OrderInvoice $invoice */
                     $rest_paid = $invoice->getRestPaid();
-                    if ($rest_paid > 0) {
-                        $payment = new OrderPayment();
-                        $payment->order_reference = Tools::substr($order->reference, 0, 9);
-                        $payment->id_currency = $order->id_currency;
-                        $payment->amount = $rest_paid;
-                        $payment->payment_method = isset($payment_method) && $payment_method instanceof Module ? $payment_method->displayName : null;
-                        $payment->conversion_rate = $order->conversion_rate;
-                        $payment->save();
-
-                        // Update total_paid_real value for backward compatibility reasons
-                        $order->total_paid_real += $rest_paid;
-                        $order->save();
-
-                        Db::getInstance()->insert(
-                            'order_invoice_payment',
-                            [
-                                'id_order_invoice' => (int) $invoice->id,
-                                'id_order_payment' => (int) $payment->id,
-                                'id_order' => (int) $order->id,
-                            ]
-                        );
-                    }
+                    $this->createOrderPayment($order, $rest_paid, (int) $invoice->id);
                 }
             } else {
                 // Disabled invoices? Then deduce $rest_paid from order payments, if any
                 $rest_paid = $order->total_paid - $order->getTotalPaid();
-                if ($rest_paid > 0) {
-                    $payment = new OrderPayment();
-                    $payment->order_reference = Tools::substr($order->reference, 0, 9);
-                    $payment->id_currency = $order->id_currency;
-                    $payment->amount = $rest_paid;
-                    $payment->payment_method = isset($payment_method) && $payment_method instanceof Module ? $payment_method->displayName : null;
-                    $payment->conversion_rate = $order->conversion_rate;
-                    $payment->save();
-
-                    // Update total_paid_real value for backward compatibility reasons
-                    $order->total_paid_real += $rest_paid;
-                    $order->save();
-
-                    Db::getInstance()->insert(
-                        'order_invoice_payment',
-                        [
-                            'id_order_invoice' => 0,
-                            'id_order_payment' => (int) $payment->id,
-                            'id_order' => (int) $order->id,
-                        ]
-                    );
-                }
+                $this->createOrderPayment($order, $rest_paid);
             }
         }
 
@@ -643,5 +601,41 @@ class OrderHistoryCore extends ObjectModel
         } else {
             return $this->add();
         }
+    }
+
+
+    /**
+     * Creates a payment for an order and optionally associates it with an invoice.
+     *
+     * @param Order $order The order object associated with the payment.
+     * @param float $restPaid The remaining amount to be paid.
+     * @param int $invoiceId The ID of the invoice to associate the payment with.
+     *
+     * @return void
+     */
+    protected function createOrderPayment(Order $order, float $restPaid, int $invoiceId = 0): void {
+        if ($restPaid <= 0) {
+            return;
+        }
+        $payment = new OrderPayment();
+        $payment->order_reference = Tools::substr($order->reference, 0, 9);
+        $payment->id_currency = $order->id_currency;
+        $payment->amount = $restPaid;
+        $payment->payment_method = isset($payment_method) && $payment_method instanceof Module ? $payment_method->displayName : null;
+        $payment->conversion_rate = $order->conversion_rate;
+        $payment->save();
+
+        // Update total_paid_real value for backward compatibility reasons
+        $order->total_paid_real += $restPaid;
+        $order->save();
+
+        Db::getInstance()->insert(
+            'order_invoice_payment',
+            [
+                'id_order_invoice' => (int) $invoiceId,
+                'id_order_payment' => (int) $payment->id,
+                'id_order' => (int) $order->id,
+            ]
+        );
     }
 }
