67f26b6fbe2c6d54e26b7b2a7163feef339368f3
Markus-Gurkcity <63096826+Markus-Gurkcity@users.noreply.github.com>
2025-04-15 08:31:01 +0200

    Update classes/pdf/PDF.php
    Co-authored-by: Krystian Podemski <kpodemski@users.noreply.github.com>

--- classes/pdf/PDF.php
+++ classes/pdf/PDF.php
@@ -180,9 +180,9 @@ class PDFCore
         $class = false;
         $class_name = 'HTMLTemplate' . $this->template;
 
-        $class = $this->getTemplateObjectFromModules($object, $this->smarty, $this->send_bulk_flag, $this->template);
+        $templateObjectFromModule = $this->getTemplateObjectFromModules($object, $this->smarty, $this->send_bulk_flag, $this->template);
 
-        if (false === $class && class_exists($class_name)) {
+        if (false === $templateObjectFromModule && class_exists($class_name)) {
             // Some HTMLTemplateXYZ implementations won't use the third param but this is not a problem (no warning in PHP),
             // the third param is then ignored if not added to the method signature.
             $class = new $class_name($object, $this->smarty, $this->send_bulk_flag);
