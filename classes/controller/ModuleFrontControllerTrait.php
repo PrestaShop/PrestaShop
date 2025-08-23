123d82c8f04c02345e2c5a80581857f604228e2e
Paolo Cunti @codencode <info@codencode.it>
2025-08-23 09:38:10 +0200

    Refacto: create ModuleFrontControllerTrait to reuse module logic

---
+++ classes/controller/ModuleFrontControllerTrait.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.md.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/OSL-3.0
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@prestashop.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+ * versions in the future. If you wish to customize PrestaShop for your
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
+ *
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
+ * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ */
+trait ModuleFrontControllerTrait
+{
+    /** @var Module */
+    public $module;
+
+    protected function bootModuleFromRequest(): void
+    {
+        $moduleName = (string) Tools::getValue('module');
+        $this->module = Module::getInstanceByName($moduleName);
+
+        if (!($this->module instanceof Module) || !$this->module->active) {
+            Tools::redirect('index');
+        }
+
+        $this->page_name = 'module-' . $this->module->name . '-' . Dispatcher::getInstance()->getController();
+        $this->controller_type = 'modulefront';
+    }
+
+    /**
+     * Assigns module template for page content.
+     *
+     * @param string $template Template filename
+     *
+     * @throws PrestaShopException
+     */
+    public function setTemplate($template, $params = [], $locale = null)
+    {
+        if (strpos($template, 'module:') === 0) {
+            $this->template = $template;
+        } else {
+            parent::setTemplate($template, $params, $locale);
+        }
+    }
+
+    /**
+     * Assign template vars related to page content.
+     *
+     * @see FrontController::initContent()
+     */
+    public function initContent()
+    {
+        if (Tools::isSubmit('module') && Tools::getValue('controller') == 'payment') {
+            $currency = Currency::getCurrency((int) $this->context->cart->id_currency);
+            $minimalPurchase = Tools::convertPrice((float) Configuration::get('PS_PURCHASE_MINIMUM'), $currency);
+            Hook::exec('overrideMinimalPurchasePrice', [
+                'minimalPurchase' => &$minimalPurchase,
+            ]);
+            if ($this->context->cart->getOrderTotal(false, Cart::ONLY_PRODUCTS) < $minimalPurchase) {
+                Tools::redirect($this->context->link->getPageLink(
+                    'order',
+                    null,
+                    null,
+                    ['step' => 1]
+                ));
+            }
+        }
+        parent::initContent();
+    }
+
+    /**
+     * Legacy helper. Preferisci $this->trans() dove possibile.
+     */
+    protected function l($string, $specific = false, $class = null, $addslashes = false, $htmlentities = true)
+    {
+        return $this->module ? $this->module->l($string, $specific) : $string;
+    }
+}
--- classes/controller/ModuleProductListingFrontController.php
+++ classes/controller/ModuleProductListingFrontController.php
@@ -24,83 +24,16 @@
  * @copyright Since 2007 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
  */
+include_once _PS_CLASS_DIR_ . 'controller' . DIRECTORY_SEPARATOR . 'ModuleFrontControllerTrait.php';
+
 abstract class ModuleProductListingFrontControllerCore extends ProductListingFrontController
 {
-    /** @var Module */
-    public $module;
+    use ModuleFrontControllerTrait;
 
     public function __construct()
     {
-        $this->module = Module::getInstanceByName(Tools::getValue('module'));
-        if (!($this->module instanceof Module) || !$this->module->active) {
-            Tools::redirect('index');
-
-            return;
-        }
-
-        $this->page_name = 'module-' . $this->module->name . '-' . Dispatcher::getInstance()->getController();
+        $this->bootModuleFromRequest();
 
         parent::__construct();
-
-        $this->controller_type = 'modulefront';
-    }
-
-    /**
-     * Assigns module template for page content.
-     *
-     * @param string $template Template filename
-     *
-     * @throws PrestaShopException
-     */
-    public function setTemplate($template, $params = [], $locale = null)
-    {
-        if (strpos($template, 'module:') === 0) {
-            $this->template = $template;
-        } else {
-            parent::setTemplate($template, $params, $locale);
-        }
-    }
-
-    /**
-     * Assign template vars related to page content.
-     *
-     * @see FrontController::initContent()
-     */
-    public function initContent()
-    {
-        if (Tools::isSubmit('module') && Tools::getValue('controller') == 'payment') {
-            $currency = Currency::getCurrency((int) $this->context->cart->id_currency);
-            $minimalPurchase = Tools::convertPrice((float) Configuration::get('PS_PURCHASE_MINIMUM'), $currency);
-            Hook::exec('overrideMinimalPurchasePrice', [
-                'minimalPurchase' => &$minimalPurchase,
-            ]);
-            if ($this->context->cart->getOrderTotal(false, Cart::ONLY_PRODUCTS) < $minimalPurchase) {
-                Tools::redirect($this->context->link->getPageLink(
-                    'order',
-                    null,
-                    null,
-                    ['step' => 1]
-                ));
-            }
-        }
-        parent::initContent();
-    }
-
-    /**
-     * Non-static translation method for frontoffice modules.
-     *
-     * @deprecated use Context::getContext()->getTranslator()->trans($id, $parameters, $domain, $locale); instead
-     *
-     * @param string $string Term or expression in english
-     * @param false|string $specific Specific name, only for ModuleFrontController
-     * @param string|null $class Name of the class
-     * @param bool $addslashes If set to true, the return value will pass through addslashes(). Otherwise, stripslashes()
-     * @param bool $htmlentities If set to true(default), the return value will pass through htmlentities($string, ENT_QUOTES, 'utf-8')
-     *
-     * @return string The translation if available, or the english default text
-     */
-    protected function l($string, $specific = false, $class = null, $addslashes = false, $htmlentities = true)
-    {
-        return $this->module->l($string, $specific);
     }
 }
