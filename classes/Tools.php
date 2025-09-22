2e9f512693ea7a1a04227b0aa4e8b1f3c35437bf
Dominik Shaim Ulrich <czshaimagal@gmail.com>
2025-09-23 01:43:08 +0200

    Update classes/Tools.php
    Co-authored-by: Krystian Podemski <kpodemski@users.noreply.github.com>

--- classes/Tools.php
+++ classes/Tools.php
@@ -2309,6 +2309,7 @@ class ToolsCore
         fwrite($write_fd, "AddType application/font-woff .woff\n");
         fwrite($write_fd, "AddType font/woff2 .woff2\n");
         fwrite($write_fd, "AddType image/webp .webp\n");
+        fwrite($write_fd, "AddType image/avif .avif\n");
         fwrite($write_fd, "<IfModule mod_headers.c>
     <FilesMatch \"\.(ttf|ttc|otf|eot|woff|woff2|svg)$\">
         Header set Access-Control-Allow-Origin \"*\"
