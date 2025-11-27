9c220cbefa65eec27fcdc32a3a037f446126c1f7
Thomas Nabord <thomas.nabord@prestashop.com>
2025-11-27 10:40:13 +0000

    Reference for tests

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
  * @author    PrestaShop SA and Contributors <contact@prestashop.com>
  * @copyright Since 2007 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
- */
+ */ 
 
 use PrestaShop\PrestaShop\Core\Util\CacheClearLocker;
 use Symfony\Component\Dotenv\Dotenv;
