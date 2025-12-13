b998895aefe4691291d4ede93ad851c658fbae33
Solls <229480808+Sollsy@users.noreply.github.com>
2025-12-13 15:10:06 +0100

    Update SpecificPrice.php
    csfixer

--- classes/SpecificPrice.php
+++ classes/SpecificPrice.php
@@ -580,85 +580,85 @@ class SpecificPriceCore extends ObjectModel
             $conditions = 'AND IF(`from_quantity` > 1, `from_quantity`, 0) <= ';
             $conditions .= (static::$psQtyDiscountOnCombination || !$id_cart || !$real_quantity) ? (int) $quantity : max(1, (int) $real_quantity);
             $conditions .= ' ORDER BY `id_product_attribute` DESC, `id_cart` DESC, `from_quantity` DESC, `id_specific_price_rule` ASC, `score` DESC, `to` DESC, `from` DESC';
-            
+
             $queryAll = $query;
             // keep the old query as a fallback
             $query .= $query_extra;
             $query .= $conditions;
-            if($id_product_attribute){
-                // we want to get specific prices for all product variants			
+            if ($id_product_attribute) {
+                // we want to get specific prices for all product variants	
                 $query_extraAll = self::computeExtraConditions($id_product, null, $id_customer, $id_cart);
                 $queryAll .= $query_extraAll;
                 $queryAll .= $conditions;
                 // cache all product variants to limit calls for the same product
                 $result = Db::getInstance(_PS_USE_SQL_SLAVE_)->executeS($queryAll);
 
-                foreach(array_column(Product::getProductAttributesIds($id_product),'id_product_attribute') as $variantId){
-                    switch(true){
+                foreach (array_column(Product::getProductAttributesIds($id_product), 'id_product_attribute') as $variantId) {
+                    switch (true) {
                         // product variant matching specific price
-                        case in_array($variantId,array_column($result,'id_product_attribute')):
-                            $k=array_search($variantId,array_column($result,'id_product_attribute'));
+                        case in_array($variantId, array_column($result, 'id_product_attribute')):
+                            $k = array_search($variantId, array_column($result, 'id_product_attribute'));
                             $key = self::computeKey(
-                            $id_product,
-                            $id_shop,
-                            $id_currency,
-                            $id_country,
-                            $id_group,
-                            $quantity,
-                            (int)$variantId,
-                            $id_customer,
-                            $id_cart,
-                            $real_quantity
+                                $id_product,
+                                $id_shop,
+                                $id_currency,
+                                $id_country,
+                                $id_group,
+                                $quantity,
+                                (int)$variantId,
+                                $id_customer,
+                                $id_cart,
+                                $real_quantity
                             );
                             if(!array_key_exists($key, self::$_specificPriceCache)){
                                 self::$_specificPriceCache[$key] = $result[$k];
                             }
-                        break; 
-                        // specific price rules case where rule applies to all variants
-                        case in_array(0,array_column($result,'id_product_attribute')):
-                            $k=array_search(0,array_column($result,'id_product_attribute'));
+                            break;
+                            // specific price rules case where rule applies to all variants
+                        case in_array(0, array_column($result, 'id_product_attribute')):
+                            $k = array_search(0, array_column($result, 'id_product_attribute'));
                             $key = self::computeKey(
-                            $id_product,
-                            $id_shop,
-                            $id_currency,
-                            $id_country,
-                            $id_group,
-                            $quantity,
-                            (int)$variantId,
-                            $id_customer,
-                            $id_cart,
-                            $real_quantity
+                                $id_product,
+                                $id_shop,
+                                $id_currency,
+                                $id_country,
+                                $id_group,
+                                $quantity,
+                                (int)$variantId,
+                                $id_customer,
+                                $id_cart,
+                                $real_quantity
                             );
-                            if(!array_key_exists($key, self::$_specificPriceCache)){
+                            if (!array_key_exists($key, self::$_specificPriceCache)){
                                 self::$_specificPriceCache[$key] = $result[$k];
                             }
-                        break;
-                        // if product variant doesnt have any matching specific price we have to return false
+                            break;
+                            // if product variant doesnt have any matching specific price we have to return false
                         default:
                             $key = self::computeKey(
-                            $id_product,
-                            $id_shop,
-                            $id_currency,
-                            $id_country,
-                            $id_group,
-                            $quantity,
-                            (int)$variantId,
-                            $id_customer,
-                            $id_cart,
-                            $real_quantity
+                                $id_product,
+                                $id_shop,
+                                $id_currency,
+                                $id_country,
+                                $id_group,
+                                $quantity,
+                                (int)$variantId,
+                                $id_customer,
+                                $id_cart,
+                                $real_quantity
                             );
-                            if(!array_key_exists($key, self::$_specificPriceCache)){
+                            if (!array_key_exists($key, self::$_specificPriceCache)){
                                 self::$_specificPriceCache[$key] = false;
                             }
                         }
                     }
                 }
-            if(!array_key_exists($key, self::$_specificPriceCache)){
+            if (!array_key_exists($key, self::$_specificPriceCache)){
                 // keep the old query as a fallback
                 self::$_specificPriceCache[$key] = Db::getInstance(_PS_USE_SQL_SLAVE_)->getRow($query);
             }
 
-        }   
+        } 
         return self::$_specificPriceCache[$key];
     }
 
