af61513b488e4dafd25632e7d9383b293f0999b0
Boris HERMANS <boris.hermans@prestashop.com>
2026-01-19 18:37:03 +0100

    Add uri variables in denormalize command result for admin api

--- src/PrestaShopBundle/ApiPlatform/Processor/CommandProcessor.php
+++ src/PrestaShopBundle/ApiPlatform/Processor/CommandProcessor.php
@@ -101,13 +101,13 @@ class CommandProcessor implements ProcessorInterface
      */
     protected function denormalizeCommandResult(mixed $commandResult, Operation $operation, array $uriVariables): mixed
     {
+        $normalizedCommandResult = [];
         if (!empty($commandResult)) {
             $normalizedCommandResult = $this->domainSerializer->normalize($commandResult, null, [NormalizationMapper::NORMALIZATION_MAPPING => $this->getCQRSCommandMapping($operation)]);
-        } else {
-            // Use URI variables as fallback when the command returned no result as it probably contains the ID that will be needed to create the CQRS query
-            $normalizedCommandResult = $uriVariables;
         }
-        $normalizedCommandResult = array_merge($normalizedCommandResult, $this->contextParametersProvider->getContextParameters());
+
+        // Merge uri variables and normalized command result to have all needed data to create the CQRS query (it probably contains all the needed info to create the CQRS query)
+        $normalizedCommandResult = array_merge($uriVariables, $normalizedCommandResult, $this->contextParametersProvider->getContextParameters());
 
         $queryClass = $this->getCQRSQueryClass($operation);
         if (!$queryClass) {
@@ -146,7 +146,7 @@ class CommandProcessor implements ProcessorInterface
         $CQRSQuery = $this->domainSerializer->denormalize($normalizedCommandResult, $CQRSQueryClass, null, [NormalizationMapper::NORMALIZATION_MAPPING => $this->getCQRSQueryMapping($operation)]);
         $CQRSQueryResult = $this->commandBus->handle($CQRSQuery);
 
-        return $this->denormalizeQueryResult($CQRSQueryResult, $operation);
+        return $this->denormalizeQueryResult($CQRSQueryResult, $operation, $normalizedCommandResult);
     }
 
     /**
