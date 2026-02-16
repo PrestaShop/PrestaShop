1d4e418fd9566cda8502e7edb6e1fd304ac5c324
Carangeot <kevin.carangeot@prestashop.com>
2026-02-16 15:33:58 +0100

    feat: add feature flag for One-page checkout

--- classes/lang/KeysReference/FeatureFlagLang.php
+++ classes/lang/KeysReference/FeatureFlagLang.php
@@ -76,3 +76,7 @@ trans('Enable / Disable the new discount system.', 'Admin.Advparameters.Help');
 
 trans('Tag', 'Admin.Advparameters.Feature');
 trans('Enable / Disable the tag page.', 'Admin.Advparameters.Help');
+
+// One-Page Checkout feature flag
+trans('One-page checkout', 'Admin.Advparameters.Feature');
+trans('Enable / Disable one page checkout flow. All checkout steps (address, delivery, payment) are grouped on a single page to reduce friction and improve conversion rate.', 'Admin.Advparameters.Help');
