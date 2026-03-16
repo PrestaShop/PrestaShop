4b3c87748809f9294ab8e0c593564cd3000b1113
Jean-François Viguier <jf.viguier@gmail.com>
2026-03-16 17:24:28 +0100

    harden and depreciate /init.php

--- init.php
+++ init.php
@@ -3,6 +3,14 @@
  * For the full copyright and license information, please view the
  * docs/licenses/LICENSE.txt file that was distributed with this source code.
  */
+/**
+ * @deprecated
+ *
+ * Not used anymore, will be removed in next major version
+ */
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
 
 if (isset(Context::getContext()->controller)) {
     $controller = Context::getContext()->controller;
