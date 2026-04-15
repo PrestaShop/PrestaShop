f89447652ad9858b3e09074640f3f70b8611dea2
Jonathan Danse <j.danse@prestaedit.com>
2026-04-15 15:41:22 +0200

    Add getOrder method to OrderReturn class

--- classes/order/OrderReturn.php
+++ classes/order/OrderReturn.php
@@ -115,6 +115,15 @@ class OrderReturnCore extends ObjectModel
         return (int) $data['total'];
     }
 
+    public function getOrder()
+    {
+        if (!$this->order) {
+            $this->order = new Order($this->id_order);
+        }
+
+        return $this->order;
+    }
+
     public static function getOrdersReturn($customer_id, $order_id = false, $no_denied = false, ?Context $context = null, ?int $idOrderReturn = null)
     {
         if (!$context) {
