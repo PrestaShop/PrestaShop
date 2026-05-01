593bf0df178c5a2e47b926b512e5daff51c22b2b
Thomas <thomas.nabord@prestashop.com>
2026-05-01 17:13:33 +0100

    Fix the storage of the id_state value

--- src/PrestaShopBundle/Form/Admin/Configure/ShopParameters/Store/StoreType.php
+++ src/PrestaShopBundle/Form/Admin/Configure/ShopParameters/Store/StoreType.php
@@ -22,6 +22,8 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
@@ -231,5 +233,22 @@ class StoreType extends TranslatorAwareType
                 'required' => false,
             ]);
         }
+
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event): void {
+            $data = $event->getData();
+            $form = $event->getForm();
+            $countryId = (int) ($data['id_country'] ?? 0);
+            $stateChoices = $countryId > 0 ? $this->statesChoiceProvider->getChoices(['id_country' => $countryId]) : [];
+            $form->add('id_state', ChoiceType::class, [
+                'label' => $this->trans('State', 'Admin.Global'),
+                'required' => false,
+                'choices' => $stateChoices,
+                'row_attr' => ['class' => 'js-store-state-row' . (empty($stateChoices) ? ' d-none' : '')],
+                'attr' => [
+                    'data-toggle' => 'select2',
+                    'data-country-id' => $countryId,
+                ],
+            ]);
+        });
     }
 }
