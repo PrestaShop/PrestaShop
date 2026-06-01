92e4e197359f783f8ac63a73218c2963d3cced79
nesrineabdmouleh <nesrine.abdmouleh.info@gmail.com>
2026-06-01 15:06:06 +0200

    Fix file name

--- tests/UI/campaigns/functional/API/02_endpoints/address/01_getAddressesList.ts
+++ tests/UI/campaigns/functional/API/02_endpoints/address/01_getAddressesList.ts
@@ -16,7 +16,7 @@ import {
   utilsPlaywright,
 } from '@prestashop-core/ui-testing';
 
-const baseContext: string = 'functional_API_endpoints_address_getAddressList';
+const baseContext: string = 'functional_API_endpoints_address_getAddressesList';
 
 describe('API : GET /addresses', async () => {
   let apiContext: APIRequestContext;
