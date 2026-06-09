1b9ba90cdb77e67559b5b83fc15a24398476ee29
metsw24-max <metsw24@gmail.com>
2026-06-09 10:23:49 +0530

    restrict admin back url to current host in BackUrlProvider

--- tests/Unit/Core/Util/Url/BackUrlProviderTest.php
+++ tests/Unit/Core/Util/Url/BackUrlProviderTest.php
@@ -17,11 +17,85 @@ class BackUrlProviderTest extends TestCase
         $backUrlProvider = new BackUrlProvider();
 
         $actualResult = $backUrlProvider->getBackUrl(
-            new Request([
-                'back' => 'http%3A%2F%2Flocalhost',
-            ])
+            new Request(
+                ['back' => 'http%3A%2F%2Flocalhost'],
+                [],
+                [],
+                [],
+                [],
+                ['HTTP_HOST' => 'localhost']
+            )
         );
 
         $this->assertEquals('http://localhost', $actualResult);
+    }
+
+    /**
+     * @dataProvider provideBackUrls
+     */
+    public function testItOnlyKeepsBackUrlsPointingToTheCurrentShop(string $backUrl, string $expected)
+    {
+        $backUrlProvider = new BackUrlProvider();
+
+        $actualResult = $backUrlProvider->getBackUrl(
+            new Request(
+                ['back' => rawurlencode($backUrl)],
+                [],
+                [],
+                [],
+                [],
+                ['HTTP_HOST' => 'localhost.org']
+            )
+        );
+
+        $this->assertEquals($expected, $actualResult);
+    }
+
+    public static function provideBackUrls(): iterable
+    {
+        yield 'relative url stays' => [
+            '/admin-dev/index.php?controller=AdminCustomers&conf=4',
+            '/admin-dev/index.php?controller=AdminCustomers&conf=4',
+        ];
+
+        yield 'bare relative url stays' => [
+            'index.php?controller=AdminProducts',
+            'index.php?controller=AdminProducts',
+        ];
+
+        yield 'absolute url on the same host stays' => [
+            'http://localhost.org/admin-dev/index.php/sell/customers/2/view',
+            'http://localhost.org/admin-dev/index.php/sell/customers/2/view',
+        ];
+
+        yield 'external host is dropped' => [
+            'https://evil.example/login',
+            '',
+        ];
+
+        yield 'protocol relative url is dropped' => [
+            '//evil.example/login',
+            '',
+        ];
+
+        yield 'backslash host is dropped' => [
+            '/\\evil.example',
+            '',
+        ];
+
+        yield 'javascript scheme is dropped' => [
+            'javascript:alert(document.cookie)',
+            '',
+        ];
+
+        yield 'data scheme is dropped' => [
+            'data:text/html,<script>alert(1)</script>',
+            '',
+        ];
+
+        yield 'look-alike host is dropped' => [
+            'http://localhost.org.evil.example/',
+            '',
+        ];
     }
 }
