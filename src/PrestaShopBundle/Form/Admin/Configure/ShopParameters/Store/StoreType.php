b8dbfd87dfc3a50a1788f68640443437fcbb3a6e
Thomas Nabord <thomas.nabord@prestashop.com>
2026-06-15 12:01:24 +0100

    Fix reported issues from CI

--- src/PrestaShopBundle/Form/Admin/Configure/ShopParameters/Store/StoreType.php
+++ src/PrestaShopBundle/Form/Admin/Configure/ShopParameters/Store/StoreType.php
@@ -23,8 +23,6 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\Length;
