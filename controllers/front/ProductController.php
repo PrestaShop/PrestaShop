9c2ab5714dd3482e5f61fad84585730f4fcda9b4
Daniel Hlaváček <daniel.hlavacek@hotmail.cz>
2026-06-24 22:43:13 +0200

    Check if manufacturer image exist before setting the fallback property

--- controllers/front/ProductController.php
+++ controllers/front/ProductController.php
@@ -858,7 +858,7 @@ class ProductControllerCore extends ProductPresentingFrontControllerCore
             );
 
             // These two variables are deprecated are kept just for backward compatibility and will be removed in v10
-            $manufacturerImageUrl = $productManufacturer['image']['small']['url'];
+            $manufacturerImageUrl = $productManufacturer['image']['small']['url'] ?? null;
             $productBrandUrl = $productManufacturer['url'];
         }
 
