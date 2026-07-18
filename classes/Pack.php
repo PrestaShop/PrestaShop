e08b65b2cd51e6e2c573dc31512cdef4b1ccd1b8
ruby42 <marek.rygula@samil.pl>
2026-07-18 21:51:14 +0200

    Remove trailing whitespace

--- classes/Pack.php
+++ classes/Pack.php
@@ -293,7 +293,7 @@ class PackCore extends Product
         if ($packStockType === null || $packStockType === '' || (int) $packStockType == self::STOCK_TYPE_DEFAULT) {
             $packStockType = Configuration::get('PS_PACK_STOCK_TYPE');
         }
-        
+
         // If the quantity of the pack depends only on the pack or both packs and products,
         // we need to load the quantity of the pack from stock_available table.
         if (in_array($packStockType, [self::STOCK_TYPE_PACK_ONLY, self::STOCK_TYPE_PACK_BOTH])) {
