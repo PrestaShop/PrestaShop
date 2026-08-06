08e99ee55c37410f9b56fb5d68990ef7a158d7f3
PululuK <pululuandre@hotmail.com>
2026-08-06 22:42:22 +0200

    Register the grid_views feature flag (beta)

--- src/Core/FeatureFlag/FeatureFlagSettings.php
+++ src/Core/FeatureFlag/FeatureFlagSettings.php
@@ -31,6 +31,7 @@ class FeatureFlagSettings
     public const PREFIX = 'PS_FF_';
 
     public const FEATURE_FLAG_ADMIN_API_MULTISTORE = 'admin_api_multistore';
+    public const FEATURE_FLAG_GRID_VIEWS = 'grid_views';
     public const FEATURE_FLAG_ADMIN_API_EXPERIMENTAL_ENDPOINTS = 'admin_api_experimental_endpoints';
     public const FEATURE_FLAG_FRONT_CONTAINER_V2 = 'front_container_v2';
     public const FEATURE_FLAG_IMPROVED_SHIPMENT = 'improved_shipment';
