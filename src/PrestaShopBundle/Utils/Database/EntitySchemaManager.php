866e3877b072528639644fbd30693660a204f62f
PululuK <pululuandre@hotmail.com>
2026-08-17 21:50:22 +0200

    Update to current codebase standards
    - replace outdated OSL license headers with the current short header
      (fixes the 'Check license headers' lint job)
    - declare(strict_types=1) in every file, final classes
    - catch specific Doctrine exceptions instead of generic Exception
    - autowire the service declaration like the other utils services

--- src/PrestaShopBundle/Utils/Database/EntitySchemaManager.php
+++ src/PrestaShopBundle/Utils/Database/EntitySchemaManager.php
@@ -1,56 +1,37 @@
 <?php
 /**
- * Copyright since 2007 PrestaShop SA and Contributors
- * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.md.
- * It is also available through the world-wide-web at this URL:
- * https://opensource.org/licenses/OSL-3.0
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@prestashop.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to https://devdocs.prestashop.com/ for more information.
- *
- * @author    PrestaShop SA and Contributors <contact@prestashop.com>
- * @copyright Since 2007 PrestaShop SA and Contributors
- * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * For the full copyright and license information, please view the
+ * docs/licenses/LICENSE.txt file that was distributed with this source code.
  */
 declare(strict_types=1);
 
 namespace PrestaShopBundle\Utils\Database;
 
 use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\DBAL\Exception as DBALException;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
 use Doctrine\ORM\Tools\SchemaTool;
+use Doctrine\ORM\Tools\ToolsException;
 use Doctrine\Persistence\Mapping\Driver\MappingDriverChain;
-use Exception;
 use PrestaShop\PrestaShop\Core\Exception\DatabaseException;
 use PrestaShop\PrestaShop\Core\Util\Database\EntitySchemaManagerInterface;
 
 /**
  * Class EntitySchemaManager help to manage an entity schema: update, create, drop.
  */
-class EntitySchemaManager implements EntitySchemaManagerInterface
+final class EntitySchemaManager implements EntitySchemaManagerInterface
 {
     /**
      * @var SchemaTool
      */
-    protected SchemaTool $schemaTool;
+    private SchemaTool $schemaTool;
 
     /**
      * @param EntityManagerInterface $entityManager
      */
-    public function __construct(protected EntityManagerInterface $entityManager)
+    public function __construct(private EntityManagerInterface $entityManager)
     {
         $this->schemaTool = new SchemaTool($this->entityManager);
     }
@@ -117,7 +98,7 @@ class EntitySchemaManager implements EntitySchemaManagerInterface
 
         try {
             $this->schemaTool->createSchema($this->getClassesMetadata($entitiesClassesName));
-        } catch (Exception $exception) {
+        } catch (ToolsException|DBALException $exception) {
             throw new DatabaseException($exception->getMessage(), 0, $exception);
         }
 
@@ -137,7 +118,7 @@ class EntitySchemaManager implements EntitySchemaManagerInterface
     {
         try {
             $this->schemaTool->updateSchema($this->getClassesMetadata($entitiesClassesName), true);
-        } catch (Exception $exception) {
+        } catch (ToolsException|DBALException $exception) {
             throw new DatabaseException($exception->getMessage(), 0, $exception);
         }
 
@@ -157,7 +138,7 @@ class EntitySchemaManager implements EntitySchemaManagerInterface
     {
         try {
             $this->schemaTool->dropSchema($this->getClassesMetadata($entitiesClassesName));
-        } catch (Exception $exception) {
+        } catch (ToolsException|DBALException $exception) {
             throw new DatabaseException($exception->getMessage(), 0, $exception);
         }
 
--- tests/Integration/PrestaShopBundle/Utils/Database/EntitySchemaManagerTest.php
+++ tests/Integration/PrestaShopBundle/Utils/Database/EntitySchemaManagerTest.php
@@ -1,41 +1,23 @@
 <?php
 /**
- * Copyright since 2007 PrestaShop SA and Contributors
- * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.md.
- * It is also available through the world-wide-web at this URL:
- * https://opensource.org/licenses/OSL-3.0
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@prestashop.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to https://devdocs.prestashop.com/ for more information.
- *
- * @author    PrestaShop SA and Contributors <contact@prestashop.com>
- * @copyright Since 2007 PrestaShop SA and Contributors
- * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * For the full copyright and license information, please view the
+ * docs/licenses/LICENSE.txt file that was distributed with this source code.
  */
+
+declare(strict_types=1);
 
 namespace Tests\Integration\PrestaShopBundle\Utils\Database;
 
 use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Exception as DBALException;
 use Doctrine\ORM\EntityManagerInterface;
-use Exception;
 use PrestaShop\PrestaShop\Core\Util\Database\EntitySchemaManagerInterface;
 use PrestaShopBundle\Entity\Lang;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Tests\Resources\Entity\TestEntityOne;
 use Tests\Resources\Entity\TestEntityTwo;
 
-class EntitySchemaManagerTest extends KernelTestCase
+final class EntitySchemaManagerTest extends KernelTestCase
 {
     private const SERVICE_NAME = 'prestashop.util.database.entity_schema_manager';
     private ?Connection $connection;
@@ -140,7 +122,7 @@ class EntitySchemaManagerTest extends KernelTestCase
             $result = $this->connection->executeQuery('SHOW TABLES LIKE ?', [$tableName]);
 
             return $result->rowCount() > 0;
-        } catch (Exception $e) {
+        } catch (DBALException $e) {
             return false;
         }
     }
