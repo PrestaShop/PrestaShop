8d3b3954ad0af589504a1991080418eb00edc23d
Michał Kaleta <michal.kaleta@waynet.pl>
2026-08-21 21:42:50 +0200

    feat(b2b): insert default roles data during installation

---
+++ classes/lang/B2bRoleLang.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * For the full copyright and license information, please view the
+ * docs/licenses/LICENSE.txt file that was distributed with this source code.
+ */
+
+class B2bRoleLangCore extends DataLangCore
+{
+    protected $domain = 'Shop.Theme.Customeraccount';
+    protected $keys = ['id_role'];
+    protected $fieldsToUpdate = ['name'];
+}
