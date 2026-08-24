1e6ec8784558ed07683c09fe2651448828c234ed
Michał Kaleta <michal.kaleta@waynet.pl>
2026-08-24 10:04:06 +0200

    style: cs-fix

--- classes/lang/B2bRoleLang.php
+++ classes/lang/B2bRoleLang.php
@@ -3,7 +3,6 @@
  * For the full copyright and license information, please view the
  * docs/licenses/LICENSE.txt file that was distributed with this source code.
  */
-
 class B2bRoleLangCore extends DataLangCore
 {
     protected $domain = 'Shop.Theme.Customeraccount';
