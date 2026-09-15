332b9e2304218713566977883dbf1da16dd2f598
Michał Kaleta <michal.kaleta@waynet.pl>
2026-09-15 17:52:28 +0200

    refactor(b2b): change the parent class of `B2bRoleRepository` to adhere to existing conventions

--- src/PrestaShopBundle/Entity/Repository/B2bRoleRepository.php
+++ src/PrestaShopBundle/Entity/Repository/B2bRoleRepository.php
@@ -8,24 +8,18 @@ declare(strict_types=1);
 
 namespace PrestaShopBundle\Entity\Repository;
 
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityRepository;
 use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\ORM\QueryBuilder;
-use Doctrine\Persistence\ManagerRegistry;
 use PrestaShopBundle\Entity\B2B\B2bRole;
 
 /**
- * @extends ServiceEntityRepository<B2bRole>
+ * @extends EntityRepository<B2bRole>
  *
  * @experimental
  */
-class B2bRoleRepository extends ServiceEntityRepository
+class B2bRoleRepository extends EntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, B2bRole::class);
-    }
-
     public function createByLanguageIdQueryBuilder(int $languageId): QueryBuilder
     {
         return $this
