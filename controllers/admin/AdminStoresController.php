a20bdba7f6c90c5b4d21a441c4cf92f75e9b000e
Thomas Nabord <thomas.nabord@prestashop.com>
2026-09-23 13:45:44 +0100

    Update deprecation notice

--- controllers/admin/AdminStoresController.php
+++ controllers/admin/AdminStoresController.php
@@ -7,7 +7,7 @@
 use PrestaShop\PrestaShop\Core\Image\ImageFormatConfiguration;
 
 /**
- * @deprecated since 9.2 and will be removed in 10.0 - This controller was migrated in 9.2
+ * @deprecated since 9.3 and will be removed in 10.0 - This controller was migrated in 9.3
  *
  * @property Store $object
  */
